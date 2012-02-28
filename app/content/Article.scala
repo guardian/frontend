01b9c53c4f2e421beb5551c91d7b5b8694f5a57b
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-02-28 12:43:38 +0000

    Pull request tweak: Tidy visibility on Tag class property.

--- app/content/Article.scala
+++ app/content/Article.scala
@@ -1,15 +1,15 @@
 package content
 
-import com.gu.openplatform.contentapi.model.Content
+import com.gu.openplatform.contentapi.model.{ Content => ApiContent }
 import com.gu.openplatform.contentapi.model.{ Tag => ApiTag }
 import conf.Logging
 
-class Tag(tag: ApiTag) {
+class Tag(private val tag: ApiTag) {
   lazy val url: String = tag.webUrl
   lazy val name: String = tag.webTitle
 }
 
-class Article(private val content: Content) {
+class Article(private val content: ApiContent) {
   lazy val headline: String = content.safeFields("headline")
   lazy val body: String = content.safeFields("body")
   lazy val tags: Seq[Tag] = content.tags map { new Tag(_) }
