0c7449c657b29c39752d9af53b76df271ceefec4
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-03-16 16:05:02 +0000

    Move content domain objects from conf. Shouldn't be in use here.

--- app/conf/context.scala
+++ app/conf/context.scala
@@ -1,32 +1,14 @@
 package conf
 
 import com.gu.conf.ConfigurationFactory
-import com.gu.openplatform.contentapi.connection.MultiThreadedApacheHttpClient
-import com.gu.openplatform.contentapi.model.Content
-import com.gu.openplatform.contentapi.{ ApiError, Api }
-import content.Article
 import play.api.Logger
 
 object `package` extends Logging {
-  implicit def content2IsArticle(content: Content) = new {
-    lazy val isArticle = content.tags.exists(_.id == "type/article")
-  }
-
   implicit def string2ToIntOption(s: String) = new {
     lazy val toIntOption: Option[Int] = try {
       Some(s.toInt)
     } catch {
       case _ => None
-    }
-  }
-
-  def suppressApi404(block: => Option[Article]) = {
-    try {
-      block
-    } catch {
-      case ApiError(404, _) =>
-        log.info("Got a 404 while calling content api")
-        None
     }
   }
 }
@@ -46,36 +28,21 @@ object Configuration {
   }
 
   object proxy {
-    lazy val isDefined = hostOption.isDefined && portOption.isDefined
+    lazy val isDefined: Boolean = hostOption.isDefined && portOption.isDefined
 
     private lazy val hostOption = Option(System.getProperty("http.proxyHost"))
     private lazy val portOption = Option(System.getProperty("http.proxyPort")) flatMap { _.toIntOption }
 
-    lazy val host = hostOption.get
-    lazy val port = portOption.get
-  }
-}
+    lazy val host: String = hostOption getOrElse {
+      throw new IllegalStateException("HTTP proxy host not configured")
+    }
 
-object ContentApi extends Api with MultiThreadedApacheHttpClient with Logging {
-  import Configuration._
-
-  override val targetUrl = contentApi.host
-  apiKey = Some(contentApi.key)
-
-  maxConnections(1000)
-
-  def setProxy(host: String, port: Int) {
-    httpClient.getHostConfiguration.setProxy(host, port)
+    lazy val port: Int = portOption getOrElse {
+      throw new IllegalStateException("HTTP proxy port not configured")
+    }
   }
 
-  if (proxy.isDefined) {
-    log.info("Setting HTTP proxy to: %s:%s".format(proxy.host, proxy.port))
-    setProxy(proxy.host, proxy.port)
-  }
-
-  override protected def fetch(url: String, parameters: Map[String, Any]) = {
-    super.fetch(url, parameters + ("user-tier" -> "internal"))
-  }
+  override def toString(): String = configuration.toString
 }
 
 trait Logging {
---
+++ app/content/package.scala
@@ -0,0 +1,22 @@
+package content
+
+import com.gu.openplatform.contentapi.ApiError
+import com.gu.openplatform.contentapi.model.Content
+import conf.Logging
+
+object `package` extends Logging {
+
+  implicit def content2IsArticle(content: Content) = new {
+    lazy val isArticle = content.tags.exists(_.id == "type/article")
+  }
+
+  def suppressApi404(block: => Option[Article]) = {
+    try {
+      block
+    } catch {
+      case ApiError(404, _) =>
+        log.info("Got a 404 while calling content api")
+        None
+    }
+  }
+}
