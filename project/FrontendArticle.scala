f3a1dee3f8628a5f1db9c16b60aeecf2fd7c84bb
Grant Klopper <grant.klopper@guardian.co.uk>
2012-03-21 10:56:48 +0000

    Dist target now adds deploy.json file to artifact, and appropriately renames the artifact

--- project/FrontendArticle.scala
+++ project/FrontendArticle.scala
@@ -1,9 +1,16 @@
+import collection.Seq
+import java.io.File
+import java.util.UUID
 import sbt._
 import Keys._
 import PlayProject._
 
 object FrontendArticle extends Build {
 
+  private implicit def file2relative(file: File) = new {
+    def relativePathFrom(parent: File): String = file.getAbsolutePath.replace(parent.getAbsolutePath, "")
+  }
+  
   val appName = "frontend-article"
   val appVersion = "1-SNAPSHOT"
 
@@ -18,6 +25,32 @@ object FrontendArticle extends Build {
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     // Disable Specs options to use ScalaTest
-    testOptions in Test := Nil
+    testOptions in Test := Nil,
+    dist ~= addMagentaConfig
   )
+
+  def addMagentaConfig = { originalZipFile: File =>
+    import IO._
+
+    println("Adding deploy.json to artifact")
+    val tmpDir = createTemporaryDirectory
+    unzip(originalZipFile, tmpDir)
+    copyFile(file("conf") / "deploy.json", tmpDir / "deploy.json")
+
+    val originalZipFileDirectory = originalZipFile.getParentFile
+    delete(originalZipFile)
+
+    val buildNumber = Option(System.getenv("BUILD_NUMBER")).getOrElse("LOCAL")
+    val filesToZip = (tmpDir ** "*").get map {file =>
+      (file -> file.relativePathFrom(tmpDir).replaceFirst(appVersion, buildNumber))
+    }
+
+    println("Renaming " + originalZipFile.getName +" -> " + "artifacts.zip")
+    val modifiedZipFile = new File(originalZipFileDirectory, "artifacts.zip")
+    zip(filesToZip, modifiedZipFile)
+    delete(tmpDir)
+
+    modifiedZipFile
+  }
 }
+
