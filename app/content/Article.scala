55a74d951aadabc0526def82df6c105c26c1a7f8
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-03-27 17:39:00 +0100

    first pass at some basic responsive design, including images and a scalable fluid grid for layout. replaced twitter bootstrap with guardian pasteup (woohoo) and changed the default Play favicon to an inverted guardian one, because we're badasses.

--- app/content/Article.scala
+++ app/content/Article.scala
@@ -2,17 +2,29 @@ package content
 
 import com.gu.openplatform.contentapi.model.{ Content => ApiContent }
 import com.gu.openplatform.contentapi.model.{ Tag => ApiTag }
+import com.gu.openplatform.contentapi.model.{ MediaAsset => ApiMedia }
 import conf.Logging
 
-class Tag(private val tag: ApiTag) {
+case class Tag(private val tag: ApiTag) {
   lazy val url: String = tag.webUrl
   lazy val name: String = tag.webTitle
 }
 
-class Article(private val content: ApiContent) {
+case class Image(private val media: ApiMedia) {
+  private val fields = media.fields.getOrElse(Map.empty[String, String])
+
+  lazy val mediaType: String = media.`type`
+  lazy val rel: String = media.rel
+  lazy val url: Option[String] = media.file
+  lazy val caption: Option[String] = fields.get("caption")
+  lazy val width: Int = fields.get("width").map(_.toInt).getOrElse(0)
+}
+
+case class Article(private val content: ApiContent) {
   lazy val headline: String = content.safeFields("headline")
   lazy val body: String = content.safeFields("body")
-  lazy val tags: Seq[Tag] = content.tags map { new Tag(_) }
+  lazy val tags: Seq[Tag] = content.tags map { Tag(_) }
+  lazy val images: Seq[Image] = content.mediaAssets.filter{ _.`type` == "picture" } map { Image(_) }
 }
 
 object Article extends Logging {
@@ -23,8 +35,9 @@ object Article extends Logging {
     ContentApi.item
       .showTags("all")
       .showFields("all")
+      .showMedia("all")
       .itemId(path)
       .response
-      .content.filter { _.isArticle } map { new Article(_) }
+      .content.filter { _.isArticle } map { Article(_) }
   }
 }
