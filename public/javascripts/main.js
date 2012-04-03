9cf6922a1488ccb759994a87063d870587254517
Andy Hume <andyhume@gmail.com>
2012-04-03 13:24:12 +0100

    Removed jQuery, ported util to requirejs module, added detection code and more debug information.

---
+++ public/javascripts/main.js
@@ -0,0 +1,21 @@
+require(["detect/detect", "detect/images"], function(detect, images) {
+
+    var gu_debug = {
+        screenHeight: screen.height,
+        screenWidth: screen.width,
+        windowWidth: window.innerWidth || document.body.offsetWidth || 0,
+        windowHeight: window.innerHeight || document.body.offsetHeight || 0,
+        layout: detect.getLayoutMode(),
+        bandwidth: detect.getConnectionSpeed(),
+        battery: detect.getBatteryLevel(),
+        pixelratio: detect.getPixelRatio(),
+        retina: (detect.getPixelRatio() === 2) ? 'true' : 'false'
+    }
+    for (var key in gu_debug) {
+        document.getElementById(key).innerText = gu_debug[key];
+    }
+
+    // Find and upgrade images.
+    images.upgrade();
+
+});
