510357b5a2ec29d582804afe5db7816b1f905d3b
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-12 15:27:13 +0100

    Update to sbt-frontend-build 1.2.
    Still would like to include sbt-version-info-plugin using
    sbt-frontend-build.

--- project/FrontendArticle.scala
+++ project/FrontendArticle.scala
@@ -15,16 +15,12 @@ object FrontendArticle extends Build {
   private val appName = "frontend-article"
   private val appVersion = "1-SNAPSHOT"
 
-  private val staticPathsFile = SettingKey[File]("static-paths-file",
-    "The location of the file that static paths are generated in")
-
   private val appDependencies = Seq(
     //dependencies included in distribution
     "com.gu.openplatform" %% "content-api-client" % "1.13",
     "com.gu" %% "configuration" % "3.6",
     "com.gu" %% "management-play" % "5.7",
     "com.gu" %% "management-logback" % "5.7",
-
     "com.gu" %% "frontend-common" % "1.0",
 
     //dependencies in test only
@@ -37,11 +33,6 @@ object FrontendArticle extends Build {
 	    resolvers += "Guardian Github Releases" at "http://guardian.github.com/maven/repo-releases",
 	    // Disable Specs options to use ScalaTest
 	    testOptions in Test := Nil,
-	    organization := "com.gu",
-	    scalaVersion := "2.9.1",
-	    maxErrors := 20,
-	    javacOptions ++= Seq("-source", "1.6", "-target", "1.6", "-encoding", "utf8"),
-	    scalacOptions ++= Seq("-unchecked", "-optimise", "-deprecation", "-Xcheckinit", "-encoding", "utf8"),
 	    jarName in assembly := "%s.jar" format appName,
 	    templatesImport ++= Seq(
 	      "content.Article",
@@ -51,7 +42,6 @@ object FrontendArticle extends Build {
 	      (base: Seq[File]) =>
 	      //todo
 	        ((base / "logger.xml") +++ (base / "META-INF" / "MANIFEST.MF")).get
-	    },
-	    staticPathsFile in Compile <<= (resourceManaged in Compile) / "static-paths.properties"
+	    }
   )
 }
