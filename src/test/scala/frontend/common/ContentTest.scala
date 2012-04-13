5dd93b5a4e973d858047aff58576134d530470cf
Grant Klopper <grant.klopper@guardian.co.uk>
2012-04-13 18:05:00 +0100

    Added Content

--- src/test/scala/frontend/common/ContentTest.scala
+++ src/test/scala/frontend/common/ContentTest.scala
@@ -2,9 +2,9 @@ package frontend.common
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
-import com.gu.openplatform.contentapi.model.{MediaAsset, Content => ApiContent}
 
 import org.joda.time.DateTime
+import com.gu.openplatform.contentapi.model.{MediaAsset, Content => ApiContent, Tag => ApiTag}
 
 class ContentTest extends FlatSpec with ShouldMatchers {
 
@@ -71,6 +71,69 @@ class ContentTest extends FlatSpec with ShouldMatchers {
     }
 
     images.imageOfWidth(70, tolerance = 10).get.caption should be (Some("find me"))
+  }
+  
+  "Tags" should "understand tag types" in {
+   
+    val theKeywords = Seq(tag("/keyword1", "keyword"), tag("/keyword2", "keyword"))
+    val theSeries = Seq(tag("/series", "series"))
+    val theContributors = Seq(tag("/contributor", "contributor"))
+    val theTones = Seq(tag("/tone", "tone"))
+    val theBlogs = Seq(tag("/blog", "blog"))
+
+    val tags =new Tags {
+      override val tags = theBlogs ++ theTones ++ theContributors ++ theSeries ++ theKeywords
+    }
+
+    tags.keywords should be (theKeywords)
+
+    tags.contributors should be (theContributors)
+
+    tags.blogs should be (theBlogs)
+
+    tags.tones should be (theTones)
+
+    tags.series should be (theSeries)
+
+  }
+  
+  "Content" should "understand the meta data used by the plugins framework" in {
+
+    def metaData = Map[String, Any](
+      //people (including 3rd parties) rely on the names of these things, avoid changing them
+      "keywords" -> keywords.map{_.name}.mkString(","),
+      "description" -> trailText.getOrElse(""),
+      "page-id" -> id,
+      "section" -> section,
+      //"publication" -> publication,
+      "tag-ids" -> tags.map(_.id).mkString(","),
+      "author" -> contributors.map(_.name).mkString(","),
+      "tones" -> tones.map(_.name).mkString(","),
+      "series" -> series.map(_.name).mkString(","),
+      "blogs" -> blogs.map(_.name).mkString(","),
+      "web-publication-date" -> webPublicationDate,
+      "short-url" -> shortUrl,
+      "api-url" -> content.apiUrl,
+      "web-title" -> content.webTitle,
+      "byline" -> content.safeFields.get("byline").getOrElse("") ,
+      "commentable" -> content.safeFields.get("commentable").getOrElse("false")
+    )
+
+    val fields = Map(
+      "publication" -> "The Guardian",
+      "short-url" -> "http://gu.com/p/12345"
+    )
+
+    val apiContent = ApiContent("foo/2012/jan/07/bar", None, None, new DateTime, "Some article",
+      "http://www.guardian.co.uk/foo/2012/jan/07/bar",
+      "http://content.guardianapis.com/foo/2012/jan/07/bar", fields = fields)
+    
+    
+    
+  }
+  
+  private def tag(id: String, tagType: String) ={
+    Tag(ApiTag(id = id, `type` = tagType, None, None, "title", "weburl", "apiurl", Nil))
   }
 
   private def image(caption: String, width: Int) = {
