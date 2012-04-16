264bee5b21ada86c747b3df0deebac07ade47557
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-16 11:48:13 +0100

    Move to frontend plugin version 1.3. This includes sbt-version-info.

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -2,11 +2,10 @@ import sbt._
 
 object Plugins extends Build {
 
-  val frontendPluginVersion = "1.2"
+  val frontendPluginVersion = "1.3"
 
   //we automatically include some plugins (including the Play plugin) from frontend-build
 
   lazy val plugins = Project("frontend-article", file("."))
     .dependsOn(uri("git://github.com/guardian/sbt-frontend-build.git#" + frontendPluginVersion))
-    .dependsOn(uri("git://github.com/guardian/sbt-version-info-plugin.git#2.1"))
 }
