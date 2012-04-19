77a10334eb8bd6255efc89634d8a4854e1e198cf
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-19 14:39:15 +0100

    Move package utility code out of frontend-article.

---
+++ src/main/scala/frontend/common/package.scala
@@ -0,0 +1,29 @@
+package frontend.common
+
+import com.gu.openplatform.contentapi.ApiError
+import com.gu.openplatform.contentapi.model.{ Content => ApiContent }
+import play.api.Logger
+
+object `package` {
+  implicit def string2ToIntOption(s: String) = new {
+    lazy val toIntOption: Option[Int] = try {
+      Some(s.toInt)
+    } catch {
+      case _ => None
+    }
+  }
+
+  implicit def content2IsArticle(content: ApiContent) = new {
+    lazy val isArticle: Boolean = content.tags exists { _.id == "type/article" }
+  }
+
+  def suppressApi404[T](block: => Option[T])(implicit log: Logger): Option[T] = {
+    try
+      block
+    catch {
+      case ApiError(404, _) =>
+        log.info("Got a 404 while calling content api")
+        None
+    }
+  }
+}
