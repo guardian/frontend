0920e11475ee64c130a6eee52fdc191ab54bdf96
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-04-19 14:51:13 +0100

    first pass at a real design, adds some images and CSS

--- app/content/Article.scala
+++ app/content/Article.scala
@@ -3,12 +3,17 @@ package content
 import conf.Logging
 import com.gu.openplatform.contentapi.model.{ItemResponse, Content => ApiContent}
 import frontend.common.{Image, Trail, Tag}
+import org.joda.time.DateTime
 
 case class Article(private val content: ApiContent, relatedContent: Seq[Trail] = Nil) {
   lazy val headline: String = content.safeFields("headline")
+  lazy val standfirst: String = content.safeFields("standfirst")
+  lazy val byline: String = content.safeFields("byline")
+  lazy val webPublicationDate: DateTime = content.webPublicationDate
   lazy val body: String = content.safeFields("body")
   lazy val tags: Seq[Tag] = content.tags map { Tag(_) }
   lazy val images: Seq[Image] = content.mediaAssets.filter{ _.`type` == "picture" } map { Image(_) }
+  lazy val shortUrlPath: String = content.safeFields("shortUrl").replace("http://gu.com", "")
 }
 
 object Article extends Logging {
