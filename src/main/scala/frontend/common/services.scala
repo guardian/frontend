942b7b8ec0fe68aa6736ac754d92075bd0c73dde
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-19 16:30:25 +0100

    Add ContentApi service to common.

---
+++ src/main/scala/frontend/common/services.scala
@@ -0,0 +1,29 @@
+package frontend.common
+
+import com.gu.openplatform.contentapi.Api
+import com.gu.openplatform.contentapi.connection.MultiThreadedApacheHttpClient
+
+class ContentApi(configuration: Configuration) extends Api
+    with MultiThreadedApacheHttpClient
+    with Logging {
+
+  import configuration._
+
+  override val targetUrl = contentApi.host
+  apiKey = Some(contentApi.key)
+
+  maxConnections(1000)
+
+  def setProxy(host: String, port: Int) {
+    httpClient.getHostConfiguration.setProxy(host, port)
+  }
+
+  if (proxy.isDefined) {
+    log.info("Setting HTTP proxy to: %s:%s".format(proxy.host, proxy.port))
+    setProxy(proxy.host, proxy.port)
+  }
+
+  override protected def fetch(url: String, parameters: Map[String, Any]) = {
+    super.fetch(url, parameters + ("user-tier" -> "internal"))
+  }
+}
