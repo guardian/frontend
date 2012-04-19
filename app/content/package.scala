b5d4734ba00a1b33ff88f06abd92371c455dbc62
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-19 12:55:52 +0100

    Pass logger as implicit to support methods.

--- app/content/package.scala
+++ app/content/package.scala
@@ -2,15 +2,16 @@ package content
 
 import com.gu.openplatform.contentapi.ApiError
 import com.gu.openplatform.contentapi.model.{Content => ApiContent}
-import conf.Logging
+import frontend.common.Logging
+import play.api.Logger
 
-object `package` extends Logging {
+object `package` {
 
   implicit def content2IsArticle(content: ApiContent) = new {
     lazy val isArticle = content.tags.exists(_.id == "type/article")
   }
 
-  def suppressApi404(block: => Option[Article]) = {
+  def suppressApi404(block: => Option[Article])(implicit log: Logger) = {
     try {
       block
     } catch {
