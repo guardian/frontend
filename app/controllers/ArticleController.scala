640cc7920d0a5db503a6071860b3b92c7c6d2099
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-19 16:50:28 +0100

    Update to latest frontend-common.
    Includes revised models, configuration and Content Api service support.

--- app/controllers/ArticleController.scala
+++ app/controllers/ArticleController.scala
@@ -1,13 +1,32 @@
 package controllers
 
-import play.api.mvc._
-import content.Article
+import com.gu.openplatform.contentapi.model.{ItemResponse, Content => ApiContent}
+import conf._
+import frontend.common._
+import play.api.mvc.{Controller, Action}
 
-object ArticleController extends Controller {
-
+case class ArticleAndRelated(article: Article, related: List[Trail])
+  
+object ArticleController extends Controller with Logging { 
   def render(path: String) = Action {
-    Article.byId(path) map { renderArticle } getOrElse { NotFound }
+    lookup(path) map { renderArticle } getOrElse { NotFound }
   }
 
-  private def renderArticle(article: Article) = Ok(views.html.article(article))
+  private def lookup(path: String): Option[ArticleAndRelated] = suppressApi404 {
+    log.info("Fetching article: " + path)
+    val response: ItemResponse = ContentApi.item
+      .showTags("all")
+      .showFields("all")
+      .showMedia("all")
+      .showRelated(true)
+      .itemId(path)
+      .response
+      
+    val article = response.content.filter { _.isArticle } map { new Article(_) }    
+    val related = response.relatedContent map { new Content(_) }
+    
+    article map { ArticleAndRelated(_, related) }
+  }
+    
+  private def renderArticle(model: ArticleAndRelated) = Ok(views.html.article(model.article, model.related))
 }
--- app/controllers/Management.scala
+++ app/controllers/Management.scala
@@ -16,4 +16,3 @@ object Management extends ManagementController {
     new LogbackLevelPage
   )
 }
-
