4ad344fd55293bb55d70e7f31a390e77e9b757cf
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-19 16:09:48 +0100

    Changed how Trail model is dealt with, add Article model.

--- src/main/scala/frontend/common/urls.scala
+++ src/main/scala/frontend/common/urls.scala
@@ -2,6 +2,7 @@ package frontend.common
 
 import com.gu.openplatform.contentapi.model.{ Content => ApiContent, Tag => ApiTag }
 
+// Just calling this RelativeUrl doesn't make it not UrlBuilder, y'know.
 object RelativeUrl {
   def apply(c: ApiContent): String = "/%s" format c.id
   def apply(t: ApiTag): String = "/%s" format t.id
