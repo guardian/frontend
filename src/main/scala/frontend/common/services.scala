b41fba8ff891c98e7425f8a906e2637fbd682180
Grant Klopper <grant.klopper@guardian.co.uk>
2012-04-23 11:41:49 +0100

    Got rid of unnecessary setProxy method

--- src/main/scala/frontend/common/services.scala
+++ src/main/scala/frontend/common/services.scala
@@ -14,10 +14,6 @@ class ContentApi(configuration: Configuration) extends Api
 
   maxConnections(1000)
 
-  def setProxy(host: String, port: Int) {
-    httpClient.getHostConfiguration.setProxy(host, port)
-  }
-
   if (proxy.isDefined) {
     log.info("Setting HTTP proxy to: %s:%s".format(proxy.host, proxy.port))
     setProxy(proxy.host, proxy.port)
