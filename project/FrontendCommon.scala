158f11e0499060ced2b5acdbcfa2f2f5d5457ca9
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-04-30 16:42:06 +0100

    Update for 1.30-SNAPSHOT development cycle

--- project/FrontendCommon.scala
+++ project/FrontendCommon.scala
@@ -6,7 +6,7 @@ import frontend.Frontend._
 object FrontendCommon extends Build {
 
   private val appName = "frontend-common"
-  private val appVersion = "1.29-SNAPSHOT"
+  private val appVersion = "1.29"
 
   private val appDependencies = Seq(
     "com.gu.openplatform" %% "content-api-client" % "1.15",
