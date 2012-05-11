253b0a0a876a7a9b14081f1aaab6372e5ff0f907
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-05-11 13:14:29 +0100

    Included refactored sbt plugins

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -3,11 +3,11 @@ import sbt.Keys._
 
 object Plugins extends Build {
 
-  val frontendPluginVersion = "1.14"
+  val playAssetHashPluginVersion = "1.2"
 
-  //we automatically include some plugins (including the Play plugin) from frontend-build
+  //we automatically include some plugins (including the Play plugin) from sbt-play-assethash
 
   lazy val plugins = Project("frontend-article", file("."))
     .settings(resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/")
-    .dependsOn(uri("git://github.com/guardian/sbt-frontend-build.git#" + frontendPluginVersion))
+    .dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
 }
