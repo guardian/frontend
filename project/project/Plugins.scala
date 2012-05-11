da65d7e350ff085067d6dabd72be8398b794ea6a
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-05-11 13:28:29 +0100

    Migrated to use refactored SBT plugins

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -3,11 +3,11 @@ import sbt.Keys._
 
 object Plugins extends Build {
 
-  val frontendPluginVersion = "1.15"
+  val playAssetHashPluginVersion = "1.2"
 
   //we automatically include some plugins (including the Play plugin) from frontend-build
 
   lazy val plugins = Project("frontend-tag", file("."))
     .settings(resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/")
-    .dependsOn(uri("git://github.com/guardian/sbt-frontend-build.git#" + frontendPluginVersion))
+    .dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
 }
