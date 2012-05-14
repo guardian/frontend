3b0e716f32c604c99b256ffb36d33cafd641a2a7
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-05-14 17:17:06 +0100

    Update version numbers. SBT -> 0.11.3, sbt-play-assethash -> 1.3.

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -1,13 +1,11 @@
 import sbt._
-import sbt.Keys._
 
 object Plugins extends Build {
 
-  val playAssetHashPluginVersion = "1.2"
+  val playAssetHashPluginVersion = "1.3"
 
-  //we automatically include some plugins (including the Play plugin) from sbt-play-assethash
+  // We automatically include some plugins (including the Play plugin) from sbt-play-assethash.
 
-  lazy val plugins = Project("frontend-article", file("."))
-    .settings(resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/")
-    .dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
+  lazy val plugins = Project("frontend-common", file(".")).
+    dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
 }
