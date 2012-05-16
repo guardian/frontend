3858b503c21dfa71cbfded41e158bcc638a807c9
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-05-16 16:58:55 +0100

    Eek. My bad.

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -7,6 +7,5 @@ object Plugins extends Build {
   // We automatically include some plugins (including the Play plugin) from sbt-play-assethash.
 
   lazy val plugins = Project("frontend-article", file(".")).
-//    dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
-    dependsOn(uri("file:///home/docrual/frontend/sbt-play-assethash"))
+    dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
 }
