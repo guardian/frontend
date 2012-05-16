f3b3843bacfed04fb24b07481c5a1d100502a471
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-05-16 16:29:11 +0100

    Use AssetsPlugin

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -2,10 +2,11 @@ import sbt._
 
 object Plugins extends Build {
 
-  val playAssetHashPluginVersion = "1.4"
+  val playAssetHashPluginVersion = "1.5"
 
   // We automatically include some plugins (including the Play plugin) from sbt-play-assethash.
 
   lazy val plugins = Project("frontend-article", file(".")).
-    dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
+//    dependsOn(uri("git://github.com/guardian/sbt-play-assethash.git#" + playAssetHashPluginVersion))
+    dependsOn(uri("file:///home/docrual/frontend/sbt-play-assethash"))
 }
