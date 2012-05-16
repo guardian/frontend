277783fbecf539c0135060e04a9cca2e0af18a72
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-05-16 17:25:08 +0100

    Update to use AssetsPlugin.

--- test/GalleryControllerTest.scala
+++ test/GalleryControllerTest.scala
@@ -7,12 +7,12 @@ import org.scalatest.FlatSpec
 
 class GalleryControllerTest extends FlatSpec with ShouldMatchers {
 
-  "Gallery Controller" should "200 when content type is gallery" in {
+  "Gallery Controller" should "200 when content type is gallery" in Fake {
     val result = controllers.GalleryController.render("news/gallery/2012/may/02/picture-desk-live-kabul-burma")(FakeRequest())
     status(result) should be(200)
   }
 
-  it should "404 when content type is not gallery" in {
+  it should "404 when content type is not gallery" in Fake {
     val result = controllers.GalleryController.render("world/video/2012/feb/10/inside-tibet-heart-protest-video")(FakeRequest())
     status(result) should be(404)
   }
