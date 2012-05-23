bdc48c529e7e0614a7acb49c66b08c84f011d203
Grant Klopper <grant.klopper@guardian.co.uk>
2012-05-23 11:38:36 +0100

    teamcity properties file

--- common/test/common/ConfigurationTest.scala
+++ common/test/common/ConfigurationTest.scala
@@ -7,6 +7,6 @@ class ConfigurationTest extends FlatSpec with ShouldMatchers {
 
   "Configuration" should "expose guardian.page properties" in {
     val config = new Configuration("test")
-    config.javascript.pageData should be (Map("guardian.page.bar" -> "hello world", "guardian.page.foo" -> "eight"))
+    config.javascript.pageData should be(Map("guardian.page.bar" -> "hello world", "guardian.page.foo" -> "eight"))
   }
 }
