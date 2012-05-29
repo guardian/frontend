6074ff05f208b76b1943d3c680f607c6d54cab34
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-05-29 17:20:11 +0100

    some partial work on making NF trailblock expandable (is currently buggy)

--- front/app/assets/javascripts/main.js
+++ front/app/assets/javascripts/main.js
@@ -1,1 +1,8 @@
 require([guardian.js.modules.commonPlugins], function(common){});
+
+//lower priority modules
+require([guardian.js.modules.trailExpander],
+    function(trailExpander) {
+        trailExpander.bindExpanders();
+    }
+);
