9a7ab649933ee83538fafa8be5d3061a94273e6b
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-05-29 12:50:37 +0100

    some refactoring: moved more common JS for debugging/image replacement into common JS file and removed duplicate calls from all other places. also created a debugInfo fragment to avoid this duplication. finally removed some dupe references to main.js/require.js

--- article/app/assets/javascripts/main.js
+++ article/app/assets/javascripts/main.js
@@ -3,41 +3,8 @@
 //});
 
 //High priority modules
-require([guardian.js.modules.detect, 
-    guardian.js.modules.images, 
-    guardian.js.modules.fetchDiscussion, 
-    guardian.js.modules.discussionBinder,
-    "bean",
-    "bonzo"],
-    function(detect, images, discussion, discussionBinder, bean, bonzo) {
-
-
-        var gu_debug = {
-            screenHeight: screen.height,
-            screenWidth: screen.width,
-            windowWidth: window.innerWidth || document.body.offsetWidth || 0,
-            windowHeight: window.innerHeight || document.body.offsetHeight || 0,
-            layout: detect.getLayoutMode(),
-            bandwidth: detect.getConnectionSpeed(),
-            battery: detect.getBatteryLevel(),
-            pixelratio: detect.getPixelRatio(),
-            retina: (detect.getPixelRatio() === 2) ? 'true' : 'false'
-        };
-
-        for (var key in gu_debug) {
-            document.getElementById(key).innerText = gu_debug[key];
-        }
-
-        // find and upgrade images (if supported)
-        images.upgrade();
-
-        // fetch comments html
-        discussion.fetchCommentsForContent(
-            guardian.page.shortUrl, 
-            guardian.config.discussion.numCommentsPerPage, 
-            1, // pageOffset
-            discussionBinder.renderDiscussion // callback to send HTML output to
-        );
+require([guardian.js.modules.detect, "bean", "bonzo"],
+    function(detect, bean, bonzo) {
 
         // toggle the nav submenu state
         var sectionExpander = document.getElementById('js-show-sections');
