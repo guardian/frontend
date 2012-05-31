2ef152bf6c45d639a29759573c8e8362c8cf454b
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-05-31 16:08:33 +0100

    Spike AsyncResult in ArticleController.

--- article/app/controllers/ArticleController.scala
+++ article/app/controllers/ArticleController.scala
@@ -4,17 +4,26 @@ import com.gu.openplatform.contentapi.model.ItemResponse
 import conf._
 import common._
 import play.api.mvc.{ Content => Unwanted, _ }
+import play.api.libs.concurrent.Promise
+import com.gu.openplatform.contentapi.parser.JsonParser
+import play.api.libs.ws.WS
+import org.joda.time.ReadableInstant
+import java.net.URLEncoder
+import org.joda.time.format.ISODateTimeFormat
 
 case class ArticlePage(article: Article, related: List[Trail], storyPackage: List[Trail])
 object ArticleController extends Controller with Logging {
 
   def render(path: String) = Action {
-    lookup(path).map { renderArticle }.getOrElse { NotFound }
+    Async {
+      lookup(path) map { _ map renderArticle getOrElse { NotFound } }
+    }
   }
 
-  private def lookup(path: String): Option[ArticlePage] = suppressApi404 {
+  private def lookup(path: String): Promise[Option[ArticlePage]] = {
     log.info("Fetching article: " + path)
-    val response: ItemResponse = ContentApi.item
+
+    val query = ContentApi.item
       .showInlineElements("picture")
       .showTags("all")
       .showFields("all")
@@ -22,17 +31,38 @@ object ArticleController extends Controller with Logging {
       .showRelated(true)
       .showStoryPackage(true)
       .itemId(path)
-      .response
 
-    val articleOption = response.content.filter { _.isArticle } map { new Article(_) }
-    val related = response.relatedContent map { new Content(_) }
-    val storyPackage = response.storyPackage map { new Content(_) }
+    val url = apiUrl(query)
 
-    articleOption map { article => ArticlePage(article, related, storyPackage.filterNot(_.id == article.id)) }
+    WS.url(url).get() map { wsResponse =>
+      val response: ItemResponse = JsonParser.parseItem(wsResponse.body)
+
+      val articleOption = response.content.filter { _.isArticle } map { new Article(_) }
+      val related = response.relatedContent map { new Content(_) }
+      val storyPackage = response.storyPackage map { new Content(_) }
+
+      articleOption map { article => ArticlePage(article, related, storyPackage.filterNot(_.id == article.id)) }
+    }
   }
 
   private def renderArticle(model: ArticlePage): Result =
     CachedOk(model.article) {
       views.html.article(model.article, model.related, model.storyPackage)
     }
+
+  protected def apiUrl(query: ContentApi.ItemQuery): String = {
+    val url = query._apiUrl.get
+    val parameters = query.parameters + ("user-tier" -> "internal")
+
+    require(!url.contains('?'), "must not specify parameters in url")
+
+    def encodeParameter(p: Any): String = p match {
+      case dt: ReadableInstant => URLEncoder.encode(ISODateTimeFormat.dateTimeNoMillis.print(dt), "UTF-8")
+      case other => URLEncoder.encode(other.toString, "UTF-8")
+    }
+
+    val queryString = parameters.map { case (k, v) => k + "=" + encodeParameter(v) }.mkString("&")
+
+    url + "?" + queryString
+  }
 }
