64babe401653506f362ded6f519c84d14ac0e585
Grant Klopper <grant.klopper@guardian.co.uk>
2012-06-07 14:54:43 +0100

    GFE-4 - only show captions on in body elements larger than 54 pix

--- common/app/views/support/package.scala
+++ common/app/views/support/package.scala
@@ -53,7 +53,10 @@ object PictureTransformerHtml {
       img.replaceWith(wrapper)
       wrapper.appendChild(img)
 
-      imageFromApi foreach { i: Image =>
+      //The content API is not getting the captions correctly.
+      //this has been bought to their attention and will be fixed soon,
+      //but in the meantime we are only adding captions if the image is larger than 54
+      imageFromApi filter (_.width > 54) foreach { i: Image =>
         i.caption foreach { c =>
           val caption = body.createElement("p")
           caption.attr("class", "caption")
