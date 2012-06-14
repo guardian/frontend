c408fe817f4bdce0d1c24f2f18cb76e23267567f
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-06-14 13:17:22 +0100

    Naive async requests degrade performance, reverting.

--- article/test/ArticleControllerTest.scala
+++ article/test/ArticleControllerTest.scala
@@ -7,13 +7,13 @@ import org.scalatest.FlatSpec
 
 class ArticleControllerTest extends FlatSpec with ShouldMatchers {
 
-  //  "Article Controller" should "200 when content type is article" in Fake {
-  //    val result = controllers.ArticleController.render("environment/2012/feb/22/capitalise-low-carbon-future")(FakeRequest())
-  //    status(result) should be(200)
-  //  }
-  //
-  //  it should "404 when content type is not article" in Fake {
-  //    val result = controllers.ArticleController.render("world/video/2012/feb/10/inside-tibet-heart-protest-video")(FakeRequest())
-  //    status(result) should be(404)
-  //  }
+  "Article Controller" should "200 when content type is article" in Fake {
+    val result = controllers.ArticleController.render("environment/2012/feb/22/capitalise-low-carbon-future")(FakeRequest())
+    status(result) should be(200)
+  }
+
+  it should "404 when content type is not article" in Fake {
+    val result = controllers.ArticleController.render("world/video/2012/feb/10/inside-tibet-heart-protest-video")(FakeRequest())
+    status(result) should be(404)
+  }
 }
