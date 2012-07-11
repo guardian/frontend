099da6bdeced83a00ed794a53c73a452fd796afc
Grant Klopper <grant.klopper@guardian.co.uk>
2012-07-11 12:26:35 +0100

    Remove the count when trail is expanded

--- common/app/assets/javascripts/libs/expanderBinder.js
+++ common/app/assets/javascripts/libs/expanderBinder.js
@@ -2,19 +2,31 @@ define(["bean", guardian.js.modules["$g"]], function(bean, $g) {
     function toggle(item) {
         var trails = $g.qsa("li", item)
 
+        var opening = false;
+
         for (i = 0; i < trails.length; i++) {
             var trail = trails[i];
 
             //interestingly, style.display does not work in this case...
-            var isVisible = trail.getClientRects().length > 0
+            var isVisible = trail.getClientRects().length > 0;
 
             if (!isVisible) {
                 trail.style.display = "block";
                 trail.setAttribute("was-hidden", "true");
+                opening = true;
             } else if (trail.getAttribute("was-hidden") == "true") {
                 trail.style.display = "none"
+                opening = false;
             }
         }
+
+        var count = $g.qs('.count', item);
+        if (count && opening) {
+            count.style.display = "none";
+        } else if (count) {
+            count.style.display = "block";
+        }
+
     }
 
     function init(items) {
