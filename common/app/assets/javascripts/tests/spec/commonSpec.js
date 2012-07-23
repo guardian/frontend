06f3fb580c240b5e517cb8c323951d797a9e4dbd
Matt Chadburn <matt@commuterjoy.co.uk>
2012-07-23 18:29:08 +0100

    a working unit test

---
+++ common/app/assets/javascripts/tests/spec/commonSpec.js
@@ -0,0 +1,11 @@
+define(['common'], function(common) {
+    
+    describe("Common", function() {
+        
+        it("test", function() {
+            expect(1).toBe(1);
+        });
+
+    });
+
+});
