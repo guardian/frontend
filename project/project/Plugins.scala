ca8f6f41dc9cd97f747166a6826cc2ab18e076eb
Grant Klopper <grant.klopper@guardian.co.uk>
2012-07-31 15:38:01 +0100

    Updated version of sbt-requirejs plugin.

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -3,7 +3,7 @@ import sbt._
 object Plugins extends Build {
 
   val playAssetHashPluginVersion = "1.14"
-  val sbtRequireJsVersion = "0.3"
+  val sbtRequireJsVersion = "0.4"
 
   // We automatically include some plugins (including the Play plugin) from sbt-play-assethash.
 
