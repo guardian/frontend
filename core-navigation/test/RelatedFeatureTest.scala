7305be151c31988a9a44e4442fc01aae7f164535
Matt Chadburn <matt@commuterjoy.co.uk>
2012-08-07 15:48:49 +0100

    Pending test for deduplication (until I find a test case)

--- core-navigation/test/RelatedFeatureTest.scala
+++ core-navigation/test/RelatedFeatureTest.scala
@@ -18,14 +18,6 @@ class RelatedFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatch
       }
     }
 
-/*
-   Scenario: If has no Story Package, then show Related Links
-   Scenario: Each item in the list should contain a relative date stamp - Eg, 'published 1 minute/hour/day ago' 
-   Scenario: Links in the story package should *not* contain the current article (deduplicated)
-   Scenario: 404
-   Scenario: 5xx 
-*/
-
     scenario("Shows article metadata for each related link") {
 
       given("there is an article 'Woman tortured during burglary tells of waterboarding ordeal'")
@@ -35,18 +27,20 @@ class RelatedFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatch
         then("I should see the headline, trail text for each related link")
 
         val article = findFirst("li")
-        article.findFirst("a").getAttribute("href") should include ("/uk/2009/oct/22/robbery-burglary-theft-rise-crime")
+        article.findFirst("a").getAttribute("href") should include("/uk/2009/oct/22/robbery-burglary-theft-rise-crime")
         article.findFirst("p").getText should be("Crime falls by 4%, latest figures show")
         article.findFirst(".trailtext").getText should be("Knife murders down by a third, but burglaries and robberies on the rise")
 
         and("I should see the pictures where they exist")
         article.findFirst("img").getAttribute("src") should be("http://static.guim.co.uk/sys-images/Money/Pix/pictures/2007/09/26/Burglar84.jpg")
-        
+
         val articleWithNoPicture = find("li", 3)
         articleWithNoPicture.find("img") should have length 0
 
       }
     }
+
+    scenario("Related links should *not* contain the current article")(pending)
 
   }
 }
