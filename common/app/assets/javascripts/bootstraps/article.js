69d9dae7146a0ec4f7e30daf0d201a5c459dd74e
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-08-09 15:31:28 +0100

    fixed some broken javascript to properly load popular/related. also renamed Image object which could conflict with native Image object, now called Images

--- common/app/assets/javascripts/bootstraps/article.js
+++ common/app/assets/javascripts/bootstraps/article.js
@@ -1,18 +1,23 @@
-define(['modules/related', 'modules/image', 'modules/popular'], function(Related, Image, Popular){
+define(['modules/related', 'modules/images', 'modules/popular'], function(Related, Images, Popular){
 
     return {
         init: function(config) {
             
             // upgrade images
-            new Image().upgrade();
+            new Images().upgrade();
 
             // most popular
             var popularUrl = config.page.coreNavigationUrl + '/most-popular/UK/' + config.page.section;
-            new Popular(document.getElementById('popular')).load(popularUrl);
+            new Popular(document.getElementById('js-popular')).load(popularUrl);
             
             // load related
-            var relatedUrl = config.page.coreNavigationUrl + '/related/UK/' + config.page.pageId;
-            new Related(document.getElementById('related')).load(relatedUrl);
+            // todo: make this a proper test around page metadata not the existence of divs
+            var hasStoryPackage = !document.getElementById('js-related');
+
+            if (!hasStoryPackage) {
+                var relatedUrl = config.page.coreNavigationUrl + '/related/UK/' + config.page.pageId;
+                new Related(document.getElementById('js-related')).load(relatedUrl);
+            }
         }
     }
 
--- common/app/assets/javascripts/modules/related.js
+++ common/app/assets/javascripts/modules/related.js
@@ -3,33 +3,27 @@ define(['common', 'reqwest'], function(common, reqwest){
     function Related(attachTo) {
         
         // View 
-        
         this.view = {
-        
             attachTo: attachTo,
-
             render: function(html) {
                 attachTo.innerHTML = html;
             }
-        
         }
 
         // Bindings
-        
         common.pubsub.on('modules:related:loaded', this.view.render);
         
         // Model
-        
         this.load = function(url){
             return reqwest({
-                    url: url,
-                    type: 'jsonp',
-                    jsonpCallback: 'callback',
-                    jsonpCallbackName: 'showRelated',
-                    success: function(json) {
-                        common.pubsub.emit('modules:related:loaded', [json.html])
-                    }
-            })
+                url: url,
+                type: 'jsonp',
+                jsonpCallback: 'callback',
+                jsonpCallbackName: 'showRelated',
+                success: function(json) {
+                    common.pubsub.emit('modules:related:loaded', [json.html])
+                }
+            });
         }  
 
     }
