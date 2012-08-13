459a97eb6d6d8eadc08d7f0263f352bce29ad8be
Matt Chadburn <matt@commuterjoy.co.uk>
2012-08-13 22:34:02 +0100

    Clarified the logic around images and trail text
    @kungpochicken wants this :-
    
     Then I should see the headline, trail text for each of the first five
        related links
     And I should see the pictures for the first three trails, where they exist
     And I should see no images beyond first three trails
     And I should see no trailtext beyond the first five trails

--- core-navigation/test/RelatedFeatureTest.scala
+++ core-navigation/test/RelatedFeatureTest.scala
@@ -6,7 +6,7 @@ class RelatedFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatch
 
   feature("Related links") {
 
-    scenario("Shows related links") {
+    scenario("Shows related links for each article") {
 
       given("there is an article 'Woman tortured during burglary tells of waterboarding ordeal'")
       HtmlUnit("/related/UK/uk/2012/aug/07/woman-torture-burglary-waterboard-surrey") { browser =>
@@ -24,7 +24,7 @@ class RelatedFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatch
       HtmlUnit("/related/UK/uk/2012/aug/07/woman-torture-burglary-waterboard-surrey") { browser =>
         import browser._
 
-        then("I should see the headline, trail text for each related link")
+        then("I should see the headline, trail text for each of the first five related link")
 
         //We cannot guarantee what exactly gets returned from the content api as related is an algorithm
         //so just checking that items exist and not their values
@@ -34,10 +34,21 @@ class RelatedFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatch
         article.findFirst("p").getText.length should be > 0
         article.findFirst(".trailtext").getText.length should be > 0
 
-        and("I should see the pictures where they exist")
+        and("I should see the pictures for the first three trails, where they exist")
         article.findFirst("img").getAttribute("src").length should be > 0
+
+        and("I should see no images beyond first three trails")
+
+        val trailWithImage = find("li", 7)
+        trailWithImage.find("img") should have length 0
+
+        and("I should see no trailtext beyond the first five trails")
+
+        find("li .trailtext") should have length 5
+
       }
     }
+
   }
 }
 
