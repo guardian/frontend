099661237066c34cad507284d9792a3678bda31c
Matt Chadburn <matt@commuterjoy.co.uk>
2012-08-13 12:06:04 +0100

    Design tweaks and story package expandables
    - Expandable works for Related Links (xhr) and Story Packages (non-xhr)
    - Now uses "show *count* more" in the call to action
    - Corrected CSS in line with news

--- common/app/assets/javascripts/bootstraps/article.js
+++ common/app/assets/javascripts/bootstraps/article.js
@@ -1,26 +1,28 @@
-define(['modules/related', 'modules/images', 'modules/popular', 'modules/expandable'], function(Related, Images, Popular, Expandable){
+define(['common', 'modules/related', 'modules/images', 'modules/popular', 'modules/expandable'], function(common, Related, Images, Popular, Expandable){
 
     return {
         init: function(config) {
 
-            // expandable
-            new Expandable({ id: 'related-trails', expanded: false });
-
             // upgrade images
             new Images().upgrade();
 
-            // most popular
+            // load most popular
             var popularUrl = config.page.coreNavigationUrl + '/most-popular/UK/' + config.page.section;
             new Popular(document.getElementById('js-popular')).load(popularUrl);
             
-            // load related
-            // todo: make this a proper test around page metadata not the existence of divs
+            // load related or story package
             var hasStoryPackage = !document.getElementById('js-related');
+            
+            var relatedExpandable = new Expandable({ id: 'related-trails', expanded: false });
 
-            if (!hasStoryPackage) {
+            if (hasStoryPackage) {
+                relatedExpandable.initalise();
+            } else { 
+                common.mediator.on('modules:related:render', relatedExpandable.initalise);
                 var relatedUrl = config.page.coreNavigationUrl + '/related/UK/' + config.page.pageId;
                 new Related(document.getElementById('js-related')).load(relatedUrl);
             }
+        
         }
     }
 
--- common/test/assets/javascripts/spec/Expandable.spec.js
+++ common/test/assets/javascripts/spec/Expandable.spec.js
@@ -4,11 +4,9 @@ define(['common', 'modules/expandable'], function(common, Expandable) {
 
         it("should be able to operate multiple exapandables on a single page", function(){
             
-            var a = new Expandable({ id: 'trail-a' });
-            var b = new Expandable({ id: 'trail-b' });
-
-            common.mediator.emit('modules:related:render');
-            
+            var a = new Expandable({ id: 'trail-a' }).initalise();
+            var b = new Expandable({ id: 'trail-b' }).initalise();
+             
             expect(common.$('#trail-a .count')[0].innerHTML).toBe('5');
             expect(common.$('#trail-b .count')[0].innerHTML).toBe('2');
 
@@ -20,21 +18,16 @@ define(['common', 'modules/expandable'], function(common, Expandable) {
         });
 
         it("should correctly render the default shut state", function(){
-            var a = new Expandable({ id: 'trail-c', expanded: false });
-            
-            common.mediator.emit('modules:related:render');
+            var a = new Expandable({ id: 'trail-c', expanded: false }).initalise();
             
             expect(common.$('#trail-c')[0].className).toBe('shut');
-            expect(common.$('#trail-c .cta')[0].innerHTML).toBe('more');
+            expect(common.$('#trail-c .cta')[0].innerText).toBe('Show 1 more');
         });
 
         it("should expand and contract a panel", function(){
-            var x = new Expandable({ id: 'trail-d' })
-
-            common.mediator.emit('modules:related:render');
+            var x = new Expandable({ id: 'trail-d' }).initalise()
     
             // shut
-    
             common.mediator.emit('modules:expandable:expandedChange:trail-d', false);
             expect(document.getElementById('trail-d').className).toBe('shut');
            
@@ -45,7 +38,7 @@ define(['common', 'modules/expandable'], function(common, Expandable) {
         });
         
         it("should visually represent the number of items in the panel", function(){
-            var x = new Expandable({ id: 'trail-e' });
+            var x = new Expandable({ id: 'trail-e' }).initalise();
             common.mediator.emit('modules:related:render');
             expect(common.$('#trail-e .count')[0].innerHTML).toBe('3');
         });
