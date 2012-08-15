dde41f2201e831be45d7cd72240ab9bbd517eec0
Grant Klopper <grant.klopper@guardian.co.uk>
2012-08-15 15:27:31 +0100

    Most popular now calls section and global so we can do tabs

--- core-navigation/app/controllers/MostPopularController.scala
+++ core-navigation/app/controllers/MostPopularController.scala
@@ -6,18 +6,25 @@ import conf._
 import model._
 import play.api.mvc.{ RequestHeader, Controller, Action }
 
-case class MostPopular(heading: String, trails: Seq[Trail])
-
 object MostPopularController extends Controller with Logging {
 
   def render(edition: String, path: String) = Action { implicit request =>
-    lookup(edition, path) map { renderMostPopular } getOrElse { NotFound }
+
+    val globalPopular = lookup(edition, "/").toList
+
+    val sectionPopular = if (path != "/") lookup(edition, path).toList else Nil
+
+    (sectionPopular ++ globalPopular) match {
+      case Nil => NotFound
+      case popular => renderMostPopular(popular)
+    }
   }
 
   def renderGlobal(edition: String) = render(edition, "/")
 
   private def lookup(edition: String, path: String)(implicit request: RequestHeader): Option[MostPopular] = suppressApi404 {
     log.info("Fetching most popular: " + path + " for edition " + edition)
+
     val response: ItemResponse = ContentApi.item(path, edition)
       .showMostViewed(true)
       .response
@@ -25,10 +32,10 @@ object MostPopularController extends Controller with Logging {
     val heading = response.section.map(s => "Popular on " + s.webTitle).getOrElse("Popular right now")
     val popular = response.mostViewed map { new Content(_) } take (10)
 
-    Some(MostPopular(heading, popular))
+    if (popular.size == 0) None else Some(MostPopular(heading, popular))
   }
 
-  private def renderMostPopular(model: MostPopular)(implicit request: RequestHeader) =
-    Cached(900)(JsonComponent(views.html.fragments.relatedTrails(model.trails, model.heading, 5, true)))
+  private def renderMostPopular(popular: Seq[MostPopular])(implicit request: RequestHeader) =
+    Cached(900)(JsonComponent(views.html.fragments.mostPopular(popular, 5)))
 
 }
