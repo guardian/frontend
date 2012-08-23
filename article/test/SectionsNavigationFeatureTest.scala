2681f0790af955bc3a406be7167b43eee55cc711
Matt Chadburn <matt@commuterjoy.co.uk>
2012-08-23 15:31:11 +0100

    Passing section navigation feature tests

---
+++ article/test/SectionsNavigationFeatureTest.scala
@@ -0,0 +1,33 @@
+package test
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{ Informer, GivenWhenThen, FeatureSpec }
+import collection.JavaConversions._
+import collection.JavaConverters._
+import org.fluentlenium.core.domain.{ FluentWebElement, FluentList }
+import conf.Configuration
+
+class SectionNavigationFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatchers {
+
+  implicit val config = Configuration
+
+  feature("Section Navigation") {
+
+    scenario("Links to sections", ArticleComponents) {
+
+      given("I am on any guardian.co.uk page")
+      HtmlUnit("/world/2012/aug/23/australia-mining-boom-end") { browser =>
+        import browser._
+
+        then("I should see a list of top sections")
+
+        val sections = find("div#sections li a")
+        sections.length should be > 0
+
+        and("a link to that list")
+        findFirst(".sections").getAttribute("href") should include("australia-mining-boom-end#sections")
+
+      }
+    }
+  }
+}
