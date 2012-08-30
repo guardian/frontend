3825c158751c1f25bdd278491e62341fdcc6b661
Matt Chadburn <matt@commuterjoy.co.uk>
2012-08-30 11:08:40 +0100

    Reworked Omniture modules passing tests

---
+++ common/app/assets/javascripts/modules/analytics/omniture.js
@@ -0,0 +1,99 @@
+define(['common', 'modules/detect', 'vendor/bean-0.4.11-1'], function(common, detect, bean) {
+
+    // https://developer.omniture.com/en_US/content_page/sitecatalyst-tagging/c-tagging-overview
+
+    function Omniture(config) {
+
+        var config = config;
+
+        this.logView = function() {
+            s.t();
+        }
+        
+        this.logTag = function(params) {
+            var delay = params[0],
+                tag = params[1];
+
+            s.tl(delay, 'o', tag);
+        }
+
+        var log = function recordClick(){
+
+            var isAjaxLink = element.getAttribute("data-is-ajax");
+            var linkHref = element.getAttribute('href');
+            var shouldDelay = (linkHref && (linkHref.indexOf('#') === 0 || linkHref.indexOf('javascript') === 0)) ? true : this;
+            if (isAjaxLink == "true") {
+                shouldDelay = false;
+            }
+        
+            s.linkTrackVars = 'eVar37,events';
+            s.linkTrackEvents = 'event37';
+            s.events = 'event37';
+            s.eVar37 = componentName;
+                
+        }       
+
+        this.populate = function(s) {
+       
+            s.linkInternalFilters += ',localhost,gucode.co.uk,gucode.com,guardiannews.com';
+        
+            var webTitle = (config.page.webTitle || '').trim();
+            if (webTitle.length > 72) {
+                webTitle = webTitle.substring(0, 72);
+            }
+            s.pageName  = webTitle + ':' + (config.page.contentType || '') + ':' + (config.page.pageCode || '');
+              
+            s.pageType  = config.page.contentType || '';  //pageType
+            s.prop9     = config.page.contentType || '';  //contentType
+              
+            s.channel   = config.page.section || '';
+            s.prop4     = config.page.keywords || '';
+            s.prop6     = config.page.author || '';
+            s.prop8     = config.page.pageCode || '';
+            s.prop10    = config.page.tones || '';
+
+            s.prop11    = config.page.section || ''; //Third Level Mlc
+            s.prop13    = config.page.series || '';
+            s.prop25    = config.page.blogs || '';
+                  
+            s.prop14    = config.page.buildNumber || '';
+              
+            var platform = "frontend";
+            s.prop19     = platform;
+            s.eVar19     = platform;
+                   
+            s.prop47    = config.page.edition || '';
+
+            s.prop48    = detect.getConnectionSpeed();
+
+            s.prop56    = detect.getLayoutMode();
+        
+            if (config.page.webPublicationDate) {
+                s.prop30 = 'content';
+            } else {
+                s.prop30 = 'non-content';
+            }
+            
+            return s;
+        }
+
+        this.init = function() {
+
+            // must be set before the Omniture file is parsed
+            s_account = config.page.omnitureAccount;
+    
+            var that = this;
+
+            require(['omniture'], function(placeholder){
+                that.populate(window.s);
+                common.mediator.on('modules:clickstream:click', that.logTag )
+            });
+        
+        }    
+     
+    }
+    
+    return Omniture;
+
+});
+
