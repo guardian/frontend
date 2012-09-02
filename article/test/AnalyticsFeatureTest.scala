b05facf9870d673efd06242c7beee6417c01bf9b
Matt Chadburn <matt@commuterjoy.co.uk>
2012-09-02 13:12:03 +0100

    Test a few token properties of the Omniture webbug

--- article/test/AnalyticsFeatureTest.scala
+++ article/test/AnalyticsFeatureTest.scala
@@ -23,19 +23,24 @@ class AnalyticsFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMat
 
     scenario("Omniture tracks user actions") {
 
-      given("I am on an article entitled 'Liu Xiang pulls up in opening race at second consecutive Olympics'")
-      HtmlUnit("/sport/2012/aug/07/liu-xiang-injured-olympics") { browser =>
+      given("I am on an article entitled 'Olympic opening ceremony will recreate countryside with real animals'")
+      HtmlUnit("/sport/2012/jun/12/london-2012-olympic-opening-ceremony") { browser =>
         import browser._
 
         then("the Omniture web bug should record my visit")
         val webbug = findFirst("#omnitureNoScript img")
-        
-        webbug.getAttribute("src") should startWith("http://hits.guardian.co.uk/b/ss/guardiangu-mobiledev/1/H.24.1.1/") 
+
+        webbug.getAttribute("src") should startWith("http://hits.guardian.co.uk/b/ss/guardiangu-mobiledev/1/H.24.1.1/")
+
+        // test a few token properties in the web bug
+        webbug.getAttribute("src") should include("c11=sport")
+        webbug.getAttribute("src") should include("c8=1758359")
+        webbug.getAttribute("src") should include("pageName=Olympic+opening+ceremony+will+recreate+countryside+with+real+animals%3AArticle%3A1758359")
 
       }
     }
-    
-    scenario("Ophan tracks user actions") (pending)
+
+    scenario("Ophan tracks user actions")(pending)
 
   }
 }
