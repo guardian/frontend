06e743b5170e78e8c439885b3e9e830c5415f06e
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-09-10 17:56:32 +0100

    fixing omniture issues with local tracking being duplicated, and swapping params for correct handling of ajax/internal links to avoid 500ms delay

--- common/app/assets/javascripts/modules/analytics/clickstream.js
+++ common/app/assets/javascripts/modules/analytics/clickstream.js
@@ -7,37 +7,40 @@ define(['common', 'modules/detect', 'vendor/bean-0.4.11-1'], function(common, de
 
         var filterSource = function(element) {
             return filters.filter(function(f) {
-                return (f == element)
-                })
+                return (f == element);
+            });
         }
  
         var getTag = function(element, tag) {
 
             var elementName = element.tagName.toLowerCase();
             
-            if (elementName === 'body')
+            if (elementName === 'body') {
                 return tag.reverse().join(' | ');
+            }
             
-            if (dataLinkName = element.getAttribute("data-link-name"))
+            if (dataLinkName = element.getAttribute("data-link-name")) {
                 tag.push(dataLinkName);
+            }
 
-            return getTag(element.parentNode, tag)
+            return getTag(element.parentNode, tag);
         }
 
         // delegate, emit the derived tag
         bean.add(document.body, "click", function(event) {
           
-            if (!filterSource(event.target.tagName.toLowerCase()).length > 0)
+            if (!filterSource(event.target.tagName.toLowerCase()).length > 0) {
                 return false;
+            }
             
-            var target = event.target,
+            var target = event.target, // possibly worth looking at tab.js' getTargetElement() - this is not x-browser
                 dataIsXhr = target.getAttribute("data-is-ajax"),
                 href = target.getAttribute("href");
              
             var isXhr = (dataIsXhr) ? true : false;
             var isInternalAnchor = (href && (href.indexOf('#') === 0)) ? true : false;
 
-            common.mediator.emit('module:clickstream:click', [getTag(target, []), isXhr, isInternalAnchor]) 
+            common.mediator.emit('module:clickstream:click', [getTag(target, []), isXhr, isInternalAnchor]);
         });
      
     }
