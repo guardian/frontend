47ba6a9b31b4f7d3cdf0741c0a85c3485dc01bf9
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-09-19 17:03:58 +0100

    quick bit os visual styling for show/hide trailblocks link

--- common/app/assets/javascripts/bootstraps/article.js
+++ common/app/assets/javascripts/bootstraps/article.js
@@ -133,8 +133,8 @@ define([
             },
 
             showTrailblockToggles: function (config) {
-                var tt = new TrailblockToggle;
                 var edition = config.page.edition;
+                var tt = new TrailblockToggle;
                 tt.go(edition);
             }
          
