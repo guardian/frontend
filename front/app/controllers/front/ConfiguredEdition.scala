7c9d4710e3300463e359f75e9b2c9a8192252288
Grant Klopper <grant.klopper@guardian.co.uk>
2012-09-25 16:19:21 +0100

    added logging for configured front updates

--- front/app/controllers/front/ConfiguredEdition.scala
+++ front/app/controllers/front/ConfiguredEdition.scala
@@ -22,6 +22,7 @@ trait ConfiguredEdition extends AkkaSupport with HttpSupport with Logging {
   val configAgent = play_akka.agent[Seq[TrailblockAgent]](Nil)
 
   def refresh() = configAgent.sendOff { oldAgents =>
+    log.info("loading front configuration from: " + configUrl)
     http.GET(configUrl) match {
       case Response(200, json, _) => refreshAgents(json, oldAgents)
       case Response(errorCode, _, errorMessage) =>
@@ -43,7 +44,7 @@ trait ConfiguredEdition extends AkkaSupport with HttpSupport with Logging {
     oldAgents.filterNot(old => newAgents.exists(_.description == old.description)).foreach(_.close())
 
     newAgents.foreach(_.refresh())
-
+    newAgents.foreach(a => log.info("Front configuration loaded: " + a.description))
     newAgents
   }
 
