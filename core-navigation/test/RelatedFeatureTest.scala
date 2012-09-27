ab76658f6c3bb480e46f7501c3f5d1617b9b05b5
Patrick Hamann <patrick.hamann@guardian.co.uk>
2012-09-27 21:18:25 +0100

    Fixed issues relating to failing test before pull request

--- core-navigation/test/RelatedFeatureTest.scala
+++ core-navigation/test/RelatedFeatureTest.scala
@@ -53,10 +53,10 @@ class RelatedFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatch
         val article = findFirst("li")
         article.findFirst("a").getAttribute("href").length should be > 0
         article.findFirst("p").getText.length should be > 0
-        article.findFirst(".trailtext").getText.length should be > 0
+        article.findFirst(".gt-base").getText.length should be > 0
         article.findFirst(".relative-timestamp").getAttribute("data-timestamp") should be("1344360038000")
 
-        find("li .trailtext") should have length 5
+        find("li .gt-base") should have length 5
 
         and("I should see the pictures for the first three trails, where they exist")
         article.findFirst("img").getAttribute("src").length should be > 0
