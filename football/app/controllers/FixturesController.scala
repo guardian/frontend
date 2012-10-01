8f74b7eca9de72e1bb017dce2f2cb310e34184ce
Grant Klopper <grant.klopper@guardian.co.uk>
2012-10-01 17:47:39 +0100

    Added boilerplate to fixtures page

--- football/app/controllers/FixturesController.scala
+++ football/app/controllers/FixturesController.scala
@@ -3,13 +3,20 @@ package controllers
 import common._
 import play.api.mvc.{ Action, Controller }
 import feed.Competitions
+import model.{ CachedOk, Competition, MetaData, Page }
+
+case class FixturesPage(page: MetaData, competitions: Seq[Competition])
 
 object FixturesController extends Controller with Logging {
 
+  val page = Page("http://www.guardian.co.uk/football/matches", "football/fixtures", "football", "", "Football fixtures")
+
   def render() = Action { implicit request =>
 
-    val compeititons = Competitions.all
+    val competitions = Competitions.all
 
-    Ok(views.html.fixtures(compeititons))
+    CachedOk(page) {
+      Compressed(views.html.fixtures(FixturesPage(page, competitions)))
+    }
   }
 }
