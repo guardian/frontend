be7119ca91dcc1e7ef965259617f2264b897119b
Grant Klopper <grant.klopper@guardian.co.uk>
2012-10-01 16:26:16 +0100

    Startup Front in dev-build

--- dev-build/app/Global.scala
+++ dev-build/app/Global.scala
@@ -1,3 +1,28 @@
+import common.RequestMetrics
+import com.gu.management.play.{ RequestTimer, StatusCounters }
+import controllers.front.{ ConfiguredEdition, Front }
 import play.api.GlobalSettings
 
-object Global extends GlobalSettings
+object Global extends GlobalSettings with RequestTimer with StatusCounters {
+
+  import RequestMetrics._
+
+  override val requestTimer = RequestTimingMetric
+  override val okCounter = Request200s
+  override val errorCounter = Request50xs
+  override val notFoundCounter = Request404s
+  override val redirectCounter = Request30xs
+
+  override def onStart(app: play.api.Application) {
+    super.onStart(app)
+    Front.startup()
+
+    //warmup is only for dev machines, do not propagate outside of dev-build
+    Front.warmup()
+  }
+
+  override def onStop(app: play.api.Application) {
+    Front.shutdown()
+    super.onStop(app)
+  }
+}
