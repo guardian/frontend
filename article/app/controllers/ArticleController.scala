86d99651d94473b8f53ffb38082d6ac5115da11f
Grant Klopper <grant.klopper@guardian.co.uk>
2012-10-03 14:25:15 +0100

    Fixed Caching...
    we had 2 different things adding cache headers, now we have 1

--- article/app/controllers/ArticleController.scala
+++ article/app/controllers/ArticleController.scala
@@ -1,9 +1,9 @@
 package controllers
 
 import com.gu.openplatform.contentapi.model.ItemResponse
-import conf._
 import common._
 import model._
+import conf._
 import play.api.mvc.{ Content => _, _ }
 import play.api.libs.concurrent.Akka
 import play.api.Play.current
@@ -34,7 +34,7 @@ object ArticleController extends Controller with Logging {
   }
 
   private def renderArticle(model: ArticlePage)(implicit request: RequestHeader): Result =
-    CachedOk(model.article) {
-      Compressed(views.html.article(model.article, model.storyPackage))
+    Cached(model.article) {
+      Ok(Compressed(views.html.article(model.article, model.storyPackage)))
     }
 }
