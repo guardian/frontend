077fcdabdf81901750991c142e74880ec2035509
Grant Klopper <grant.klopper@guardian.co.uk>
2012-10-10 15:57:37 +0100

    Compressed HTML from Football endpoints

--- section/app/controllers/SectionsController.scala
+++ section/app/controllers/SectionsController.scala
@@ -2,16 +2,15 @@ package controllers
 
 import common._
 import model._
-import play.api.mvc.{ RequestHeader, Controller, Action }
-import model.Page
+import play.api.mvc.{ Controller, Action }
 
 object SectionsController extends Controller with Logging {
 
-  val page = Page("http://www.guardian.co.uk/", "sections", "sections", "http://content.guardianapis.com/sections", "All fixtures")
+  val page = new Page("http://www.guardian.co.uk/", "sections", "sections", "http://content.guardianapis.com/sections", "All fixtures")
 
   def render = Action { implicit request =>
-    CachedOk(page) {
-      Compressed(views.html.sections(page))
+    Cached(page) {
+      Ok(Compressed(views.html.sections(page)))
     }
   }
 
