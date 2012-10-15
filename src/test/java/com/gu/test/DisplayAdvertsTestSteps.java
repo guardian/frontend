97b01f1fac139a3abe7f8c0b8376a13e115f54f2
Patrick Hamann <patrick.hamann@guardian.co.uk>
2012-10-15 12:26:26 +0100

    Added tests for adverts

--- src/test/java/com/gu/test/DisplayAdvertsTestSteps.java
+++ src/test/java/com/gu/test/DisplayAdvertsTestSteps.java
@@ -15,33 +15,35 @@ public class DisplayAdvertsTestSteps {
 
 	@Given("^I visit any page on the Guardian site$")
 	public void I_visit_any_page_on_the_Guardian_site() throws Throwable {
+		page = new BrowserCommandsPage();
 
+		//host = article.getHost();
+		host = "http://beta.gucode.co.uk"; //cant compile local host
+
+		page.open(host + "/uk/2012/sep/19/police-manchester");
 	}
 
-	@Then("^an Advert will be at the top of the page$")
+	@Then("^an advert will be at the top of the page$")
 	public void an_Advert_will_be_at_the_top_of_the_page() throws Throwable {
-
+		waitFor(1000);
+		Assert.assert(page.findElement(By.cssSelector('#ad-slot-top-banner-ad .ad')));
 	}
 
 	@Then("^an Advert will be at the foot of the page$")
 	public void an_Advert_will_be_at_the_foot_of_the_page() throws Throwable {
-
+		waitFor(1000);
+		Assert.assert(page.findElement(By.cssSelector('#ad-slot-bottom-banner-ad .ad')));
 	}
 
-	@Given("^I'm on a guardian page with adverts$")
-	public void I_m_on_a_guardian_page_with_adverts() throws Throwable {
+	// @When("^I click an advert$")
+	// public void I_click_an_advert() throws Throwable {
+		
+	// }
 
-	}
+	// @Then("^the brands site is displayed on a new window$")
+	// public void the_brands_site_is_displayed_on_a_new_window() throws Throwable {
 
-	@When("^I click an advert$")
-	public void I_click_an_advert() throws Throwable {
-
-	}
-
-	@Then("^the brands site is displayed on a new window$")
-	public void the_brands_site_is_displayed_on_a_new_window() throws Throwable {
-
-	}
+	// }
 
 }
 
--- src/test/resources/com/gu/test/displayadverts.feature
+++ src/test/resources/com/gu/test/displayadverts.feature
@@ -11,15 +11,15 @@ Feature: Display Adverts
     	
     Scenario: Adverts will appear at top of all pages
         Given I visit any page on the Guardian site
-        Then an Advert will be at the top of the page
+        Then an advert will be at the top of the page
    
    	Scenario: Adverts will appear at the foot of all pages
         Given I visit any page on the Guardian site
-        Then an Advert will be at the foot of the page
+        Then an advert will be at the foot of the page
 
-	Scenario: Click Adverts to navigate to brand site
-         Given I'm on a guardian page with adverts 
-         When I click an advert
-         Then the brands site is displayed on a new window
+	#Scenario: Click Adverts to navigate to brand site
+    #     Given I'm on a guardian page with adverts 
+    #     When I click an advert
+    #     Then the brands site is displayed on a new window
 
 
