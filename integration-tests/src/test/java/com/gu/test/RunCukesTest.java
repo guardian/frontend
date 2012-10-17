f3fd21e3a767360c9f5d883161d23249c8edacb4
Darren Hurley <ironsidevsquincy@googlemail.com>
2012-10-17 17:06:16 +0100

    only navigation tests

--- integration-tests/src/test/java/com/gu/test/RunCukesTest.java
+++ integration-tests/src/test/java/com/gu/test/RunCukesTest.java
@@ -4,7 +4,7 @@ import cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(tags = "~@navigation", format = {"pretty", "html:target/cucumber-html-report"})
+@Cucumber.Options(tags = "@navigation", format = {"pretty", "html:target/cucumber-html-report"})
 
 public class RunCukesTest {
 }
