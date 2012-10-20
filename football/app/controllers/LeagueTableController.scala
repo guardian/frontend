0e86f6bee5aa2c3598d36e94f6538f1191230984
grant Klopper <grant.klopper@gmail.com>
2012-10-20 10:26:51 +0100

    League tables now understand groups

--- football/app/controllers/LeagueTableController.scala
+++ football/app/controllers/LeagueTableController.scala
@@ -5,8 +5,12 @@ import feed.Competitions
 import play.api.mvc.{ Action, Controller }
 import model._
 import model.Page
+import pa.{ Round, LeagueTableEntry }
 
-case class TablesPage(page: Page, competitions: Seq[Competition])
+case class TablesPage(page: Page, tables: Seq[Table])
+
+case class Group(round: Option[Round], entries: Seq[LeagueTableEntry])
+case class Table(competition: Competition, groups: Seq[Group])
 
 object LeagueTableController extends Controller with Logging with CompetitionFixtureFilters {
 
@@ -16,8 +20,16 @@ object LeagueTableController extends Controller with Logging with CompetitionFix
 
     val competitons = Competitions.competitions.filter(_.hasLeagueTable)
 
+    val groups = competitons.map { comp =>
+      val groups = comp.leagueTable
+        .groupBy(_.round)
+        .map { case (round, table) => Group(round, table) }
+        .toSeq.sortBy(_.round.map(_.roundNumber).getOrElse(""))
+      Table(comp, groups)
+    }
+
     Cached(page) {
-      Ok(Compressed(views.html.tables(TablesPage(page, competitons))))
+      Ok(Compressed(views.html.tables(TablesPage(page, groups))))
     }
   }
 }
