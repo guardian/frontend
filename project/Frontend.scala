919fe8f1b2bd17a1c8ecd463379f50dd58432d76
grant Klopper <grant.klopper@gmail.com>
2012-10-20 08:16:14 +0100

    Updated version of PAClient

--- project/Frontend.scala
+++ project/Frontend.scala
@@ -30,7 +30,7 @@ object Frontend extends Build with Prototypes {
   val video = application("video").dependsOn(commonWithTests)
 
   val football = application("football").dependsOn(commonWithTests).settings(
-      libraryDependencies += "com.gu" %% "pa-client" % "2.3"
+      libraryDependencies += "com.gu" %% "pa-client" % "2.4"
   )
 
   val dev = application("dev-build")
