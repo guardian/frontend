a163d18df1798dbbaf38444b934f83f5ef09be74
Grant Klopper <grant.klopper@guardian.co.uk>
2012-10-24 17:24:55 +0100

    Consolidated results controller

--- football/app/controllers/ResultsController.scala
+++ football/app/controllers/ResultsController.scala
@@ -1,8 +1,8 @@
 package controllers
 
 import common._
-import feed.Competitions
-import play.api.mvc.{ Action, Controller }
+import feed.{ CompetitionSupport, Competitions }
+import play.api.mvc.{ RequestHeader, Action, Controller }
 import model._
 import org.joda.time.DateMidnight
 import org.joda.time.format.DateTimeFormat
@@ -10,32 +10,27 @@ import model.Page
 import scala.Some
 import play.api.templates.Html
 
-object ResultsController extends Controller with Logging with CompetitionResultFilters {
+sealed trait ResultsRenderer extends Controller with Logging with CompetitionResultFilters {
 
   val daysToDisplay = 3
   val datePattern = DateTimeFormat.forPattern("yyyyMMMdd")
-  val page = new Page("http://www.guardian.co.uk/football/matches", "football/results", "football", "", "All results")
 
-  def allCompetitionsOn(year: String, month: String, day: String) = allCompetitions(
-    Some(datePattern.parseDateTime(year + month + day).toDateMidnight)
-  )
-
-  def allCompetitions(date: Option[DateMidnight] = None) = Action { implicit request =>
-
+  def renderResults(page: Page,
+    competitions: CompetitionSupport,
+    competitionName: Option[String],
+    date: Option[DateMidnight])(implicit request: RequestHeader) = {
     val startDate = date.getOrElse(new DateMidnight)
 
-    val resultsDays = Competitions.withTodaysMatchesAndPastResults.previousMatchDates(startDate, daysToDisplay)
+    val resultsDays = competitions.previousMatchDates(startDate, daysToDisplay)
 
-    val results = resultsDays.map { day => MatchesOnDate(day, Competitions.withMatchesOn(day).competitions) }
+    val results = resultsDays.map { day => MatchesOnDate(day, competitions.withMatchesOn(day).competitions) }
 
-    //going forward in time
-    val nextPage = Competitions.withTodaysMatchesAndPastResults.nextMatchDates(startDate.plusDays(1), daysToDisplay)
-      .lastOption.map(toNextPreviousUrl)
+    val nextPage = competitions.nextMatchDates(startDate.plusDays(1), daysToDisplay)
+      .lastOption.map(toNextPreviousUrl(_, competitionName))
 
-    //going backward in time
     val previousPage = resultsDays.lastOption.flatMap { date =>
-      Competitions.withTodaysMatchesAndPastResults.previousMatchDates(date.minusDays(1), daysToDisplay).headOption
-    }.map(toNextPreviousUrl)
+      competitions.previousMatchDates(date.minusDays(1), daysToDisplay).headOption
+    }.map(toNextPreviousUrl(_, competitionName))
 
     val resultsPage = MatchesPage(page, None, results.filter(_.competitions.nonEmpty),
       nextPage, previousPage, "results", filters)
@@ -49,8 +44,52 @@ object ResultsController extends Controller with Logging with CompetitionResultF
     }
   }
 
-  private def toNextPreviousUrl(date: DateMidnight) = date match {
+  def toNextPreviousUrl(date: DateMidnight, competition: Option[String]): String
+
+}
+
+object ResultsController extends ResultsRenderer with Logging {
+
+  val page = new Page("http://www.guardian.co.uk/football/matches", "football/results", "football", "", "All results")
+
+  def renderFor(year: String, month: String, day: String) = render(
+    Some(datePattern.parseDateTime(year + month + day).toDateMidnight)
+  )
+
+  def render(date: Option[DateMidnight] = None) = Action { implicit request =>
+    renderResults(page, Competitions.withTodaysMatchesAndPastResults, None, date)
+  }
+
+  override def toNextPreviousUrl(date: DateMidnight, competition: Option[String]) = date match {
     case today if today == DateMidnight.now => "/football/results"
     case other => "/football/results/%s" format (other.toString("yyyy/MMM/dd").toLowerCase)
+  }
+}
+
+object CompetitionResultsController extends ResultsRenderer with Logging {
+
+  override val daysToDisplay = 20
+
+  def renderFor(year: String, month: String, day: String, competition: String) = render(
+    competition, Some(datePattern.parseDateTime(year + month + day).toDateMidnight)
+  )
+
+  def render(competitionName: String, date: Option[DateMidnight] = None) = Action { implicit request =>
+
+    Competitions.competitions.find(_.url.endsWith(competitionName)).map { competition =>
+      val page = new Page("http://www.guardian.co.uk/football/matches", competition.url.drop(1) + "/results",
+        "football", "", competition.fullName + " results")
+      renderResults(page, Competitions.withCompetitionFilter(competitionName), Some(competitionName), date)
+    }.getOrElse(NotFound)
+
+  }
+
+  def competitionDoesNotExist(competition: String): Boolean = {
+    Competitions.withCompetitionFilter(competition).competitions.isEmpty
+  }
+
+  override def toNextPreviousUrl(date: DateMidnight, competition: Option[String]) = date match {
+    case today if today == DateMidnight.now => "/football/%s/results" format (competition.get)
+    case other => "/football/%s/results/%s" format (competition.get, other.toString("yyyy/MMM/dd").toLowerCase)
   }
 }
