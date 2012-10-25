9d477165b0eda2a3957dfb5c5e6d9e9024ec66c1
Andy Hume <andyhume@gmail.com>
2012-10-25 17:09:14 +0100

    Don't load ads above the fold or if userPref switches them off.

--- common/app/assets/javascripts/modules/adverts/adverts.js
+++ common/app/assets/javascripts/modules/adverts/adverts.js
@@ -1,6 +1,7 @@
 define(['common', 'reqwest', 'modules/detect', 'modules/adverts/iframeadslot', 'modules/adverts/dimensionMap'], function (common, reqwest, detect, IframeAdSlot, dimensionMap) {
 
     var config,
+        adsSwitchedOn,
         slots = [];
 
     function init(c) {
@@ -8,6 +9,8 @@ define(['common', 'reqwest', 'modules/detect', 'modules/adverts/iframeadslot', '
 
         var slotHolders = document.querySelectorAll('.ad-slot'),
             size = (window.innerWidth > 728) ? 'median' : 'base';
+
+        adsSwitchedOn = !guardian.userPrefs.isOff('adverts');
 
         // Run through slots and create IframeAdSlots for each.
         // Other ad types to be plugged in later.
@@ -19,18 +22,23 @@ define(['common', 'reqwest', 'modules/detect', 'modules/adverts/iframeadslot', '
         }
     }
 
-
     function loadAds() {
-        //Run through adslots and check if they are on screen. Load if so.
-        for (var i = 0, j = slots.length; i<j; ++i) {
-            if (!slots[i].loaded && isOnScreen(slots[i].el)) {
-                slots[i].load();
+        if (adsSwitchedOn) {
+            //Run through adslots and check if they are on screen. Load if so.
+            for (var i = 0, j = slots.length; i<j; ++i) {
+                if (!slots[i].loaded && isOnScreen(slots[i].el)) {
+                    console.log("is on screen:", i);
+                    slots[i].load();
+                }
             }
         }
     }
 
     function isOnScreen(el) {
-        return (el.offsetTop < (window.innerHeight + window.pageYOffset));
+        return (
+            el.offsetTop < (window.innerHeight + window.pageYOffset) &&
+            (el.offsetTop + el.offsetHeight) > window.pageYOffset
+        );
     }
 
     return {
