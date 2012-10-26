e24f94f81ed0b0d6272790cbf882faa0bc746999
Grant Klopper <grant.klopper@guardian.co.uk>
2012-10-26 12:14:50 +0100

    Added endpoint for football scores component

---
+++ football/app/controllers/FrontScoresController.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import common._
+import feed.Competitions
+import play.api.mvc.{ Action, Controller }
+import model._
+import model.Page
+import play.api.templates.Html
+
+object FrontScoresController extends Controller with Logging {
+
+  /*
+   * Finds the first competition with matches on today in a list of competition ids passed in
+   */
+  def render() = Action { implicit request =>
+    val competitionIds = request.queryString("competitionId")
+    val todaysCompetitions = Competitions.withTodaysMatches.competitions
+
+    //keep competitions in same order as passed in ids
+    val competition = competitionIds.flatMap(id => todaysCompetitions.find(_.id == id)).headOption
+    competition.map { comp =>
+      Cached(60) {
+        val html = views.html.fragments.frontMatchBlock(comp)
+        request.getQueryString("callback").map { callback =>
+          JsonComponent(html)
+        } getOrElse {
+          Cached(60) {
+            Ok(Compressed(html))
+          }
+        }
+      }
+    } getOrElse (NotFound)
+  }
+}
