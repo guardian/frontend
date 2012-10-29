bbc840a0555daaaf39c2049af82e8cd8e86c82fb
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-10-29 13:11:46 +0000

    Add CompetitionListFilter for new endpoint competitions.

--- football/app/controllers/CompetitionListController.scala
+++ football/app/controllers/CompetitionListController.scala
@@ -4,13 +4,13 @@ import common._
 import model._
 import play.api.mvc.{ Controller, Action }
 
-object CompetitionListController extends Controller with Logging {
+object CompetitionListController extends Controller with CompetitionListFilters with Logging {
 
   val page = Page("http://www.guardian.co.uk/", "competitions", "competitions", "http://content.guardianapis.com/competitions", "Leagues & competitions", "GFE:Football:Leagues & competitions")
 
   def render = Action { implicit request =>
     Cached(page) {
-      Ok(Compressed(views.html.competitions(page)))
+      Ok(Compressed(views.html.competitions(filters, page)))
     }
   }
 
