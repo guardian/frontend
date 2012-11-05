a3200708450412cf174aaf38e31648dff5dfeb37
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-11-05 17:52:03 +0000

    a whole host of styling tweaks and enhancements to football pages

--- football/app/feed/Competitions.scala
+++ football/app/feed/Competitions.scala
@@ -64,17 +64,11 @@ trait Competitions extends CompetitionSupport with AkkaSupport with Logging {
 
   val competitionAgents = Seq(
 
-    CompetitionAgent(Competition("500", "/football/championsleague", "Champions League", "Champions League", "European")),
-
     CompetitionAgent(Competition("100", "/football/premierleague", "Premier League", "Premier League", "English")),
 
-    CompetitionAgent(Competition("701", "/football/world-cup-2014-qualifiers", "World Cup 2014 qualifiers", "World Cup 2014 qualifiers", "Internationals")),
+    CompetitionAgent(Competition("500", "/football/championsleague", "Champions League", "Champions League", "European")),
 
     CompetitionAgent(Competition("510", "/football/uefa-europa-league", "Europa League", "Europa League", "European")),
-
-    CompetitionAgent(Competition("127", "/football/fa-cup", "FA Cup", "FA Cup", "English")),
-
-    CompetitionAgent(Competition("301", "/football/capital-one-cup", "Capital One Cup", "Capital One Cup", "English")),
 
     CompetitionAgent(Competition("101", "/football/championship", "Championship", "Championship", "English")),
 
@@ -82,13 +76,13 @@ trait Competitions extends CompetitionSupport with AkkaSupport with Logging {
 
     CompetitionAgent(Competition("103", "/football/leaguetwofootball", "League Two", "League Two", "English")),
 
-    CompetitionAgent(Competition("650", "/football/laligafootball", "La Liga", "La Liga", "Rest of world")),
+    CompetitionAgent(Competition("127", "/football/fa-cup", "FA Cup", "FA Cup", "English")),
+
+    CompetitionAgent(Competition("301", "/football/capital-one-cup", "Capital One Cup", "Capital One Cup", "English")),
+
+    CompetitionAgent(Competition("213", "/football/community-shield", "Community Shield", "Community Shield", "English")),
 
     CompetitionAgent(Competition("120", "/football/scottishpremierleague", "Scottish Premier League", "Scottish Premier League", "Scottish")),
-
-    CompetitionAgent(Competition("320", "/football/scottishcup", "Scottish Cup", "Scottish Cup", "Scottish")),
-
-    CompetitionAgent(Competition("320", "/football/cis-insurance-cup", "Scottish League Cup", "Scottish League Cup", "Scottish")),
 
     CompetitionAgent(Competition("121", "/football/scottish-division-one", "Scottish Division One", "Scottish Division One", "Scottish")),
 
@@ -96,7 +90,13 @@ trait Competitions extends CompetitionSupport with AkkaSupport with Logging {
 
     CompetitionAgent(Competition("123", "/football/scottish-division-three", "Scottish Division Three", "Scottish Division Three", "Scottish")),
 
-    CompetitionAgent(Competition("213", "/football/community-shield", "Community Shield", "Community Shield", "English"))
+    CompetitionAgent(Competition("320", "/football/scottishcup", "Scottish Cup", "Scottish Cup", "Scottish")),
+
+    CompetitionAgent(Competition("320", "/football/cis-insurance-cup", "Scottish League Cup", "Scottish League Cup", "Scottish")),
+
+    CompetitionAgent(Competition("701", "/football/world-cup-2014-qualifiers", "World Cup 2014 qualifiers", "World Cup 2014 qualifiers", "Internationals")),
+
+    CompetitionAgent(Competition("650", "/football/laligafootball", "La Liga", "La Liga", "Rest of world"))
 
   )
 
