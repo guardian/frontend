644f6caa2725fb339ca02ebe1c4400554bfa39bf
Matt Chadburn <matt.chadburn@guardian.co.uk>
2012-11-09 16:16:59 +0000

    Finer grain diagnostics
    - Split out namespaces - fonts and js
    - For js there's now some breakdown of browser and os versions

--- diagnostics/app/metrics/NginxLog.scala
+++ diagnostics/app/metrics/NginxLog.scala
@@ -7,77 +7,156 @@ import net.sf.uadetector.UADetectorServiceFactory
 import org.apache.commons.io.input.{ TailerListenerAdapter, Tailer }
 
 object NginxLog {
+
   val csv = new CSVParser()
   val agent = UADetectorServiceFactory.getResourceModuleParser()
 
-  object pxgif {
-    val total = CountMetric("diagnostics", "px_gif", "px.gif accesses", "Accesses to /px.gif")
+  def isHealthCheck(userAgent: String): Boolean = {
+    userAgent startsWith "ELB-HealthChecker"
+  }
 
-    val mobileSafari = CountMetric("diagnostics", "mobile_safari", "Mobile Safari", "Accesses to /px.gif from Mobile Safari agents")
-    val safari = CountMetric("diagnostics", "safari", "Safari", "Accesses to /px.gif from Safari agents")
-    val mobileFirefox = CountMetric("diagnostics", "mobile_firefox", "Mobile Firefox", "Accesses to /px.gif from Mobile Firefox agents")
-    val firefox = CountMetric("diagnostics", "firefox", "Firefox", "Accesses to /px.gif from Firefox agents")
-    val operaMobile = CountMetric("diagnostics", "opera_mobile", "Opera Mobile", "Accesses to /px.gif from Opera Mobile agents")
-    val opera = CountMetric("diagnostics", "opera", "Opera", "Accesses to /px.gif from Opera agents")
-    val chromeMobile = CountMetric("diagnostics", "chrome_mobile", "Chrome Mobile", "Accesses to /px.gif from Chrome Mobile agents")
-    val chrome = CountMetric("diagnostics", "chrome", "Chrome", "Accesses to /px.gif from Chrome agents")
-    val ieMobile = CountMetric("diagnostics", "ie_mobile", "IE Mobile", "Accesses to /px.gif from IE Mobile agents")
-    val ie = CountMetric("diagnostics", "ie", "IE", "Accesses to /px.gif from IE agents")
-    val androidWebkit = CountMetric("diagnostics", "android_webkit", "Android Webkit", "Accesses to /px.gif from Android Webkit agents")
-    val nokia = CountMetric("diagnostics", "nokia", "Nokia Web Browser", "Accesses to /px.gif from Nokia Web Browser agents")
-    val other = CountMetric("diagnostics", "other", "Other", "Accesses to /px.gif from other agents")
+  def isPxGif(path: String): Boolean = {
+    path.startsWith("/px.gif")
+  }
+
+  // all errors
+  object entry {
+
+    val total = CountMetric("diagnostics", "px_gif", "px.gif accesses", "Accesses to px.gif")
+    val metrics: Seq[Metric] = Seq(total)
+
+    def apply() {
+      total.recordCount(1)
+    }
+  }
+
+  // handle _fonts_ namespaced errors
+  object fonts {
+
+    val total = CountMetric("diagnostics", "fonts", "px.gif/fonts accesses", "Font render time warnings")
+    val metrics: Seq[Metric] = Seq(total)
+
+    def apply() {
+      total.recordCount(1)
+    }
+  }
+
+  // handle _js_ namespaced errors
+  object js {
+
+    val total = CountMetric("diagnostics", "js", "px.gif/js accesses", "JavaScript errors")
+
+    /*  iOS */
+    val js_ios_6_mobilesafari = CountMetric("diagnostics", "js_ios_6_safari", "", "")
+    val js_ios_5_mobilesafari = CountMetric("diagnostics", "js_ios_5_safari", "", "")
+    val js_ios_4_and_lower_mobilesafari = CountMetric("diagnostics", "js_ios_4_and_lower_safari", "", "")
+    val js_ios_x_chrome = CountMetric("diagnostics", "js_ios_x_chrome", "", "")
+    val js_ios_other = CountMetric("diagnostics", "js_ios_other", "", "")
+
+    /*  Android */
+    val js_android_4_safari = CountMetric("diagnostics", "js_android_4_safari", "", "")
+    val js_android_3_and_lower_safari = CountMetric("diagnostics", "js_android_3_and_lower_safari", "", "")
+    val js_android_other = CountMetric("diagnostics", "js_android_other", "", "")
+
+    /* Windows */
+    val js_windows_7_iemobile = CountMetric("diagnostics", "js_windows_7_iemobile", "", "")
+    val js_windows_other = CountMetric("diagnostics", "js_windows_other", "", "")
+
+    /* OSX */
+    val js_osx_safari = CountMetric("diagnostics", "js_osx_safari", "", "")
+    val js_osx_other = CountMetric("diagnostics", "js_osx_other", "", "")
+
+    /* RIM, Symbian, Linux, Other */
+    val js_rimos = CountMetric("diagnostics", "js_rimos_safari", "", "")
+    val js_linux = CountMetric("diagnostics", "js_linux", "", "")
+    val js_symbianos = CountMetric("diagnostics", "js_symbianos", "", "")
+    val js_other = CountMetric("diagnostics", "js_other", "Other", "Accesses to /px.gif from other agents")
 
     val metrics: Seq[Metric] = Seq(total,
-      mobileSafari, safari,
-      mobileFirefox, firefox,
-      operaMobile, opera,
-      chromeMobile, chrome,
-      ieMobile, ie,
-      androidWebkit,
-      nokia,
-      other)
+      js_ios_6_mobilesafari, js_ios_5_mobilesafari, js_ios_4_and_lower_mobilesafari, js_ios_x_chrome, js_ios_other,
+      js_android_4_safari, js_android_3_and_lower_safari, js_android_other,
+      js_windows_7_iemobile, js_windows_other,
+      js_osx_safari, js_osx_other,
+      js_rimos, js_linux, js_symbianos, js_other
+    )
 
     def apply(userAgent: String) {
+
       total.recordCount(1)
 
-      agent.parse(userAgent).getFamily match {
-        case "Mobile Safari" => mobileSafari.recordCount(1)
-        case "Safari" => safari.recordCount(1)
-        case "Mobile Firefox" => mobileFirefox.recordCount(1)
-        case "Firefox" => firefox.recordCount(1)
-        case "Opera Mobile" => operaMobile.recordCount(1)
-        case "Opera" => opera.recordCount(1)
-        case "Chrome Mobile" => chromeMobile.recordCount(1)
-        case "Chrome" => chrome.recordCount(1)
-        case "IE Mobile" => ieMobile.recordCount(1)
-        case "IE" => ie.recordCount(1)
-        case "Android Webkit" => androidWebkit.recordCount(1)
-        case "Nokia Web Browser" => nokia.recordCount(1)
-        case _ => other.recordCount(1)
+      val ua = agent.parse(userAgent)
+      val os = ua.getOperatingSystem
+
+      val uaFamily = ua.getFamily.replaceAll(" ", "")
+      val osFamily = os.getFamilyName.replaceAll(" ", "")
+      val osVersion = os.getVersionNumber.getMajor
+
+      val key = Array(osFamily.toLowerCase, osVersion, uaFamily.toLowerCase).mkString("_")
+
+      osFamily.toLowerCase match {
+
+        case "ios" => key match {
+          case "ios_6_mobilesafari" => js_ios_6_mobilesafari.recordCount(1)
+          case "ios_5_mobilesafari" => js_ios_5_mobilesafari.recordCount(1)
+          case "ios_4_mobilesafari" | "js_ios_3_mobilesafari" => js_ios_4_and_lower_mobilesafari.recordCount(1)
+          case "ios_5_chromemobile" | "js_ios_6_chromemobile" => js_ios_x_chrome.recordCount(1)
+          case _ => js_ios_other.recordCount(1)
+        }
+
+        case "android" => key match {
+          case "android_4_androidwebkit" => js_android_4_safari.recordCount(1)
+          case "android_3_androidwebkit" | "js_android_2_androidwebkit" => js_android_3_and_lower_safari.recordCount(1)
+          case _ => js_android_other.recordCount(1)
+        }
+
+        case "windows" => key match {
+          case "windows_7_iemobile" => js_windows_7_iemobile.recordCount(1)
+          case _ => js_windows_other.recordCount(1)
+        }
+
+        case "osx" => key match {
+          case "osx_10_safari" => js_osx_safari.recordCount(1)
+          case _ => js_osx_other.recordCount(1)
+        }
+
+        case "rimos" => js_rimos.recordCount(1)
+        case "symbianos" => js_symbianos.recordCount(1)
+        case "linux" => js_linux.recordCount(1)
+        case _ => js_other.recordCount(1)
+
       }
     }
   }
 
-  val metrics: Seq[Metric] = pxgif.metrics
+  // combine all the metrics
+  val metrics: Seq[Metric] = entry.metrics ++ js.metrics ++ fonts.metrics
 
   Tailer.create(new File(conf.Configuration.nginx.log), new TailerListenerAdapter() {
     override def handle(line: String) {
-
       var fields = Array("")
       var path = Option("")
       var userAgent = ""
-      
+
       try {
         fields = csv.parseLine(line)
         path = fields(2).trim.split(" ").toList.drop(1).headOption
-        userAgent = fields(6)
+        userAgent = fields(6) + fields(7)
       } catch {
         case _ => return
       }
 
-      // px.gif metrics
-      path filter { _ startsWith "/px.gif" } foreach { _ =>
-        pxgif(userAgent)
+      path filter { path => isPxGif(path) && (!isHealthCheck(userAgent)) } foreach { _ =>
+
+        val namespace = path.getOrElse("").split("[?\\/]").toList.drop(2).headOption
+
+        entry()
+
+        namespace.getOrElse("unknown") match {
+          case "fonts" => fonts()
+          case "js" => js(userAgent)
+          case _ => null
+        }
+
       }
     }
   })
