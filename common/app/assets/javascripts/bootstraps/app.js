805846b584728dc6e28f1f4e2472e951a76dcc9b
Patrick Hamann <patrick.hamann@guardian.co.uk>
2012-11-12 12:28:19 +0000

    Added another football route to fiux failing integratiobn tests

--- common/app/assets/javascripts/bootstraps/app.js
+++ common/app/assets/javascripts/bootstraps/app.js
@@ -28,8 +28,9 @@ define([
 
             //Football
             r.get('/football', function(req) { Football.init(req, config, userPrefs); });
-            r.get('/football/:page', function(req) { Football.init(req, config, userPrefs); });
+            r.get('/football/:action', function(req) { Football.init(req, config, userPrefs); });
             r.get('/football/:tag/:action', function(req) { Football.init(req, config, userPrefs); });
+            r.get('/football/:tag/:action/:year/:month/:day', function(req) { Football.init(req, config, userPrefs); });
 
             //Articles
             if(config.page.contentType === "Article") {
