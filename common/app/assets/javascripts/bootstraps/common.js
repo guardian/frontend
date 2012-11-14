f0ee5180d83b3174da543b82f0964d43583da243
Patrick Hamann <patrick.hamann@guardian.co.uk>
2012-11-14 12:05:28 +0000

    Hacked bean and omniture to get around curl issues

--- common/app/assets/javascripts/bootstraps/common.js
+++ common/app/assets/javascripts/bootstraps/common.js
@@ -106,7 +106,7 @@ define([
         },
 
         loadOphanAnalytics: function () {
-            require(['http://s.ophan.co.uk/js/t6.min.js'], function (ophan) {});
+            require(['http://s.ophan.co.uk/js/t6.min'], function (ophan) {});
         },
 
         loadAdverts: function (config) {
--- common/app/assets/javascripts/vendor/bean-0.4.11-1.js
+++ common/app/assets/javascripts/vendor/bean-0.4.11-1.js
@@ -7,11 +7,7 @@
   * dperini: https://github.com/dperini/nwevents
   * the entire mootools team: github.com/mootools/mootools-core
   */
-!function (name, context, definition) {
-  if (typeof module !== 'undefined') module.exports = definition(name, context);
-  else if (typeof define === 'function' && typeof define.amd  === 'object') define(definition);
-  else context[name] = definition(name, context);
-}('bean', this, function (name, context) {
+define('bean', function (name, context) {
   var win = window
     , old = context[name]
     , overOut = /over|out/
