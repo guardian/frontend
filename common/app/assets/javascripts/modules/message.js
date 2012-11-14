4218fb646c700bc9ea4b5c7674bad753fe08fac9
Darren Hurley <darren.hurley@bbc.co.uk>
2012-11-14 12:24:31 +0000

    adding message fragment

---
+++ common/app/assets/javascripts/modules/message.js
@@ -0,0 +1,13 @@
+define(['common', 'bonzo', 'bean'], function (common, bonzo, bean) {
+    
+    return {
+        
+        init: function() {
+            // find all messages
+            var messages = common.$g('.message')
+            // add close button
+        }
+        
+    }
+
+});
