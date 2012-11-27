4fa6ffcf29b7e7bb9037975cf6fc0ea96464c092
Darren Hurley <darren.hurley@guardian.co.uk>
2012-11-27 22:36:41 +0000

    deleting ee

--- common/app/assets/javascripts/components/eventEmitter/tools/dist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-node_modules/.bin/uglifyjs -o EventEmitter.min.js EventEmitter.js
-cp EventEmitter.min.js dist/EventEmitter-${1-dev}.min.js
--- common/app/assets/javascripts/components/eventEmitter/tools/doc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-node_modules/.bin/dox < EventEmitter.js > docs/data.json
-node_modules/.bin/dustc --name=api docs/api.dust docs/api.dust.js
-node docs/render.js
