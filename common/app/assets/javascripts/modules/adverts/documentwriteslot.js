ccd723ca5bb9aca8847464ce6f603abad2f4344b
Patrick Hamann <patrick.hamann@guardian.co.uk>
2012-12-04 12:39:35 +0000

    Removed .has-content class

--- common/app/assets/javascripts/modules/adverts/documentwriteslot.js
+++ common/app/assets/javascripts/modules/adverts/documentwriteslot.js
@@ -23,7 +23,6 @@ define([
             var slot = this.el;
             domwrite.render(slot);
             this.loaded = true;
-            common.$g(this.el).addClass('has-content');
          } catch(e) {
              common.mediator.emit('module:error', e, 'document-write.js');
         }
