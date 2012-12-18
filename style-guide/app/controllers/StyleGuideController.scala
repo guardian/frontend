9ccb72aeeb107265302150f69842c1435d5c9b84
Matt Andrews <matthew.andrews@guardian.co.uk>
2012-12-18 15:06:05 +0000

    initial nav menu

--- style-guide/app/controllers/StyleGuideController.scala
+++ style-guide/app/controllers/StyleGuideController.scala
@@ -7,14 +7,14 @@ import play.api.mvc.{ Controller, Action }
 object StyleGuideController extends Controller with Logging {
 
   def renderIndex = Action { implicit request =>
-    val page = Page(canonicalUrl = None, "style-guide", "style-guide", "Style-guide", "GFE:Style-guide")
+    val page = Page(canonicalUrl = None, "style-guide", "style-guide", "Style guide", "GFE:Style-guide")
     Cached(60) {
       Ok(Compressed(views.html.index(page)))
     }
   }
 
   def renderBase(path: String) = Action { implicit request =>
-    val page = Page(canonicalUrl = None, "style-guide", "style-guide", "Style-guide", "GFE:Style-guide")
+    val page = Page(canonicalUrl = None, "style-guide", "style-guide", "Style guide", "GFE:Style-guide")
     Cached(60) {
       Ok(Compressed(views.html.index(page)))
     }
