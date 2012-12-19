db3748de2f9473a449b7fd3bcfc34a91d4caec69
Patrick Hamann <patrick.hamann@guardian.co.uk>
2012-12-19 13:21:50 +0000

    Fixed failing scala test

--- style-guide/test/StyleGuideControllerTest.scala
+++ style-guide/test/StyleGuideControllerTest.scala
@@ -8,12 +8,7 @@ import org.scalatest.FlatSpec
 class StyleGuideControllerTest extends FlatSpec with ShouldMatchers {
 
   "Style-guide Controller" should "200 when content type is style-guide" in Fake {
-    val result = controllers.StyleGuideController.render("/style-guide")(FakeRequest())
+    val result = controllers.StyleGuideController.renderIndex()(FakeRequest("GET", "/style-guide"))
     status(result) should be(200)
-  }
-
-  it should "404 when content type is not video" in Fake {
-    val result = controllers.StyleGuideController.render("uk/2012/jun/27/queen-martin-mcguinness-shake-hands")(FakeRequest())
-    status(result) should be(404)
   }
 }
