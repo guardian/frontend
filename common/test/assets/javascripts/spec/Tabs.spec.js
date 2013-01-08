c0bf99646f6ae06229da7518ad8e17a043041fbb
Matt Andrews <matthew.andrews@guardian.co.uk>
2013-01-08 12:47:36 +0000

    removing some uses of  which hindered accessibility, and clarifiying its purpose in the styleguide

--- common/test/assets/javascripts/spec/Tabs.spec.js
+++ common/test/assets/javascripts/spec/Tabs.spec.js
@@ -4,16 +4,16 @@ define(['common', 'modules/tabs', 'bean'], function(common, Tabs, bean) {
 
         var t = new Tabs();
         t.init();
-        
+
         var tab1 = document.getElementById('tab1'); // is selected by default
         var tab2 = document.getElementById('tab2');
         var tab1panel = document.getElementById('tab1panel');
         var tab2panel = document.getElementById('tab2panel');
-           
+
         var independentTabPanel = document.getElementById('tab2panel_2');
         var allTabs = document.querySelectorAll('ol.tabs a');
         var fakeTab = document.getElementById('fake-tab');
-        
+
         // only way i can think of to test if a valid URL would actually execute
         for (var i in allTabs) {
             tab = allTabs[i];
@@ -41,23 +41,23 @@ define(['common', 'modules/tabs', 'bean'], function(common, Tabs, bean) {
         it("should show the correct panel when a tab is clicked", function(){
             bean.fire(tab2, 'click');
             expect(tab2panel.getAttribute('style')).toBe('');
-            expect(tab2panel.getAttribute('class')).not.toContain('initially-off');
+            expect(tab2panel.getAttribute('class')).not.toContain('js-hidden');
         });
 
         it("should hide other panels when a tab is clicked", function(){
             bean.fire(tab1, 'click');
             expect(tab2panel.getAttribute('style')).toContain('display: none');
         });
-        
+
         it("should operate independently of other tabsets on the page", function(){
             bean.fire(tab2, 'click');
-            expect(independentTabPanel.getAttribute('class')).toContain('initially-off');
+            expect(independentTabPanel.getAttribute('class')).toContain('js-hidden');
         });
 
         it("should allow 'fake' tabs with URL hrefs instead of ID selectors", function(){
             bean.fire(fakeTab, 'click');
             expect(fakeTab.getAttribute('data-valid-link')).toBe('1');
         });
-        
+
     });
 });
