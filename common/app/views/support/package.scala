8090ac5278337b9829fdecff4d59f8d45532249f
Grant Klopper <grant.klopper@guardian.co.uk>
2013-01-11 16:05:03 +0000

    First pass at styling for witness

--- common/app/views/support/package.scala
+++ common/app/views/support/package.scala
@@ -3,7 +3,7 @@ package views.support
 import common._
 import java.net.URLEncoder._
 import model._
-import org.jsoup.nodes.Document
+import org.jsoup.nodes.{ Element, Document }
 import org.jsoup.Jsoup
 import org.jsoup.safety.Whitelist
 import org.jboss.dna.common.text.Inflector
@@ -104,6 +104,28 @@ case class RowInfo(rowNum: Int, isLast: Boolean = false) {
 
 trait HtmlCleaner {
   def clean(d: Document): Document
+}
+
+object WitnessCleaner extends HtmlCleaner {
+  override def clean(document: Document): Document = {
+    val witnessEmbeds = document.getElementsByClass("element-witness")
+
+    witnessEmbeds.foreach { embed: Element =>
+
+      embed.getElementsByClass("guardian-witness-brand").foreach { brand: Element =>
+        val icon = document.createElement("i")
+        icon.attr("class", "i i-witness-logo")
+        brand.children().foreach(_.remove)
+        brand.text("")
+        brand.appendChild(icon)
+      }
+
+      embed.getElementsByTag("time").foreach { time: Element =>
+        time.addClass("js-timestamp")
+      }
+    }
+    document
+  }
 }
 
 object BlockNumberCleaner extends HtmlCleaner {
