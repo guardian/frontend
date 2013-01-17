51041834c36efaf53a71688bf0c9b1ea69316800
Grant Klopper <grant.klopper@guardian.co.uk>
2013-01-17 17:37:41 +0000

    Finished styling of witness embeds

--- common/app/views/support/WitnessCleaner.scala
+++ common/app/views/support/WitnessCleaner.scala
@@ -9,11 +9,19 @@ object WitnessCleaner extends HtmlCleaner {
 
     witnessEmbeds.foreach { embed: Element =>
 
+      //remove height from video iframe
+      if (embed.hasClass("element-witness-video")) {
+        embed.getElementsByClass("main").foreach { main =>
+          main.getElementsByTag("iframe").foreach(_.attr("height", ""))
+        }
+      }
+
       // remove witness brand and replace it with an icon at the top of the embed
       embed.getElementsByClass("guardian-witness-brand").foreach { brand: Element =>
         val icon = document.createElement("i")
         icon.attr("class", "i i-witness-logo")
         icon.attr("title", "Guardian Witness")
+        icon.html("&nbsp;")
         brand.children().foreach(_.remove)
         brand.text("")
         brand.appendChild(icon)
@@ -32,10 +40,22 @@ object WitnessCleaner extends HtmlCleaner {
 
             metaData.getElementsByClass("author").foreach { author: Element =>
 
-              val avatar = document.createElement("i")
-              avatar.attr("class", "i i-default-witness-avatar")
               author.appendChild(time)
-              metaData.prependChild(avatar)
+
+              val avatar = author.getElementsByTag("img").headOption
+              avatar.foreach { img =>
+                img.attr("width", "32px")
+                img.attr("height", "32px")
+                img.addClass("witness-avatar")
+                //img.remove()
+                //metaData.prependChild(img)
+              }
+
+              if (!avatar.isDefined) {
+                val defaultAvatar = document.createElement("i")
+                defaultAvatar.attr("class", "i i-default-witness-avatar witness-avatar")
+                author.prependChild(defaultAvatar)
+              }
             }
           }
         }
