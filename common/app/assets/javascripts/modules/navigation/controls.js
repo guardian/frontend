57a8cf9551b02f625ea75b5b8119018e50ed58ef
Andy Hume <andyhume@gmail.com>
2013-02-08 13:46:55 +0000

    Removed unused double touch hack.

--- common/app/assets/javascripts/modules/navigation/controls.js
+++ common/app/assets/javascripts/modules/navigation/controls.js
@@ -14,12 +14,7 @@ define(['common', 'bean', 'bonzo'], function (common, bean, bonzo) {
             bindEventToButton: function() {
                 if(dom[0] === undefined) { return; } //This is for pages with top-stories
                 bean.add(dom[0], 'click', function (e) {
-                    var current = new Date().getTime();
-                    var delta = current - lastClickTime;
-                    if (delta >= delay) {
-                        view.renderState();
-                        lastClickTime = current;
-                    }
+                    view.renderState();
                     e.preventDefault();
                 });
             },
