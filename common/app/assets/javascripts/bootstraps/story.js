c0ce1cc1e599ee7c739ca3ad9e49474f45fb73ce
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-02-27 15:24:18 +0000

    Added interactive timeline

--- common/app/assets/javascripts/bootstraps/story.js
+++ common/app/assets/javascripts/bootstraps/story.js
@@ -1,10 +1,16 @@
 define([
+    "common",
+    "bean",
+
     "modules/accordion",
     "modules/gallery"
 ], function(
+    common,
+    bean,
+
     Accordion,
     Gallery
-    ) {
+) {
 
     var modules = {
         initAccordion: function () {
@@ -18,11 +24,31 @@ define([
                 var g = new Gallery().init();
             }
         },
+
+        initTimeline: function() {
+            var timeline = document.querySelector('.timeline');
+
+            if(timeline) {
+                common.$g('.event-articles').hide();
+                bean.on(timeline, 'click', '.date-line', function(e) {
+                    var block = common.$g(this).parent();
+
+                    common.$g('.event-articles').hide();
+
+                    if(common.$g('.event-articles', block).css('display') === "none") {
+                        common.$g('.event-articles', block).show();
+                    } else {
+                        common.$g('.event-articles', block).hide();
+                    }
+                });
+            }
+        }
     };
 
     var init = function(req, config) {
         modules.initAccordion();
         modules.initGallery();
+        modules.initTimeline();
     };
 
     return {
