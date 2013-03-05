733ac9a125927c657a19d2e0feaba5dcf4435bc5
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-03-05 14:51:18 +0000

    Setting up grunt directory structure

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -3,11 +3,13 @@ import sbt._
 object Plugins extends Build {
 
   //val sbtRequireJsVersion = "v0.3"
+  val sbtGruntVersion = "0.1"
   val sbtJsHintVersion = "1.0"
   val teamcityReportingPluginVersion = "1.2"
 
   lazy val plugins = sbt.Project("build", file(".")).
     //dependsOn(uri("git://github.com/ironsidevsquincy/sbt-requirejs.git#" + sbtRequireJsVersion)).
+    dependsOn(uri("git://github.com/guardian/sbt-grunt-plugin.git#" + sbtGruntVersion)).
     dependsOn(uri("git://github.com/guardian/sbt-jshint-plugin.git#" + sbtJsHintVersion)).
     dependsOn(uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin.git#" + teamcityReportingPluginVersion))
 }
