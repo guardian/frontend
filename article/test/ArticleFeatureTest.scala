ea74ba066eb1d68028014864e7b74714d63ebde6
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-03-16 15:59:26 +0000

    Typo - ARAI -> ARIA

--- article/test/ArticleFeatureTest.scala
+++ article/test/ArticleFeatureTest.scala
@@ -316,14 +316,14 @@ class ArticleFeatureTest extends FeatureSpec with GivenWhenThen with ShouldMatch
     }
     
     // http://www.w3.org/WAI/intro/aria
-    scenario("Make the document accessible with ARAI support") {
+    scenario("Make the document accessible with ARIA support") {
 
       Given("I read an article")
       CommonSwitches.SocialSwitch.switchOn
       HtmlUnit("/world/2013/jan/27/brazil-nightclub-blaze-high-death-toll") { browser =>
         import browser._
 
-        Then("I should see the main ARAI roles described")
+        Then("I should see the main ARIA roles described")
         findFirst("#related-trails").getAttribute("role") should be("complementary")
         findFirst("#js-related").getAttribute("role") should be("complementary")
         findFirst("#js-popular").getAttribute("role") should be("complementary")
