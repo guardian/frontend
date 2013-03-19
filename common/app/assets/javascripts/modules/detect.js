6605052817432728acf28718a08163bb0437efd3
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-03-19 10:27:23 +0000

    Reduced connection detection by 25%

--- common/app/assets/javascripts/modules/detect.js
+++ common/app/assets/javascripts/modules/detect.js
@@ -78,9 +78,9 @@ define(function () {
         var speed = "high";
 
         if (load_time) {
-            if (load_time > 1000) { // One second
+            if (load_time > 750) { // One second
                 speed = 'medium';
-                if (load_time > 4000) { // Four seconds
+                if (load_time > 3000) { // Four seconds
                     speed = 'low';
                 }
             }
