f24395a2fe1e0a3ded76563630ed3d4beacf16fa
Darren Hurley <darren.hurley@guardian.co.uk>
2013-03-21 10:39:50 +0000

    on DEV, output error to console (rather than pixel tracking)

--- common/app/assets/javascripts/modules/errors.js
+++ common/app/assets/javascripts/modules/errors.js
@@ -1,9 +1,12 @@
 define(['common'], function (common) {
 
-    var Errors = function (w) {
-
-        var path = '/px.gif',
-            win = w || window,
+    var Errors = function (config) {
+    	
+    	var c = config || {};
+    		isDev = (c.isDev !== undefined) ? c.isDev : false,
+        	path = '/px.gif',
+        	cons = c.console || window.console,
+            win = c.window || window,
             body = document.body,
             createImage = function(url) {
                 var image = new Image();
@@ -16,8 +19,12 @@ define(['common'], function (common) {
                 return path + '?js/' + encodeURIComponent(properties.join(','));
             },
             log = function(message, filename, lineno) {
-                var url = makeUrl([message, filename, lineno]);
-                createImage(url);
+            	if (isDev) {
+            		cons.error({message: message.toString(), filename: filename, lineno: lineno});
+            	} else {
+                    var url = makeUrl([message, filename, lineno]);
+                    createImage(url);
+            	}
             },
             init = function() {
                 win.onerror = log;
