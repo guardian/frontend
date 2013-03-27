d4499b55d814f1d6543b415e93bb67e63145fc5f
Darren Hurley <darren.hurley@guardian.co.uk>
2013-03-27 10:32:31 +0000

    Revert "Merge pull request #590 from guardian/revert-dev-error-tracking"

--- common/app/assets/javascripts/modules/errors.js
+++ common/app/assets/javascripts/modules/errors.js
@@ -17,13 +17,32 @@ define(['modules/userPrefs', 'common'], function (userPrefs, common) {
                 body.appendChild(image);
             },
             makeUrl = function(properties) {
-                return url + path + '?js/' + encodeURIComponent(properties.join(','));
+                var query = [];
+                for (var name in properties) {
+                    query.push(name + '=' + encodeURIComponent(properties[name]));
+                }
+                return url + path + '?js/' + query.join('&');
             },
             log = function(message, filename, lineno) {
+<<<<<<< HEAD
                 if (isDev) {
                     cons.error({message: message.toString(), filename: filename, lineno: lineno});
                 } else {
                     var url = makeUrl([message, filename, lineno]);
+=======
+                var error = {message: message.toString(), filename: filename, lineno: lineno};
+                
+                if (filename === 'modules/adverts/documentwriteslot.js' || message === 'Script error.') {
+                   error.isAd = 1; 
+                } else {
+                    error.isAd = 0;
+                }
+                
+                if (isDev) {
+                    cons.error(error);
+                } else {
+                    var url = makeUrl(error);
+>>>>>>> Revert "Merge pull request #590 from guardian/revert-dev-error-tracking"
                     createImage(url);
                 }
                 return (userPrefs.isOn('showErrors')) ? false : true;
--- common/test/assets/javascripts/spec/Errors.spec.js
+++ common/test/assets/javascripts/spec/Errors.spec.js
@@ -11,15 +11,47 @@ define(['common', 'modules/errors'], function(common, Errors) {
             e = new Errors({window: w});
             e.init();
         });
+        
+        afterEach(function() {
+            common.$g('#js-err').remove();
+        });
 
         it("should listen for uncaught errors on the window object", function(){
             expect(w.onerror).toBeDefined();
         });
 
         it("should log javascript errors with the error message, line number and file", function(){
-            var fakeError = { 'message': 'foo', lineno: 1, filename: 'foo.js' }
-            e.log(fakeError.message, fakeError.lineno, fakeError.filename);
-            expect(document.getElementById('js-err').getAttribute('src')).toContain('/px.gif?js/foo%2C1%2Cfoo.js');
+            e.log(fakeError.message, fakeError.filename, fakeError.lineno);
+            expect(document.getElementById('js-err').getAttribute('src')).toContain('/px.gif?js/message=foo&filename=foo.js&lineno=1&isAd=0');
+        });
+
+        it("if DEV, should log to console", function(){
+        	var cons = {
+	        		error: jasmine.createSpy('error')
+	        	},
+            	e = new Errors({ isDev: true, window: w, console: cons });
+            e.log(fakeError.message, fakeError.filename, fakeError.lineno);
+            expect(cons.error.mostRecentCall.args[0]).toEqual({
+            	message: fakeError.message, filename: fakeError.filename, lineno: fakeError.lineno, isAd: 0
+        	});
+        });
+        
+        describe('Advert errors', function() {
+            
+            it("it should log 'documentwriteslot.js' errors as advert errors", function(){
+                e.log(fakeError.message, 'modules/adverts/documentwriteslot.js', fakeError.lineno);
+                expect(document.getElementById('js-err').getAttribute('src')).toContain(
+                    '/px.gif?js/message=foo&filename=modules%2Fadverts%2Fdocumentwriteslot.js&lineno=1&isAd=1'
+                );
+            });
+            
+            it("it should log 'Script error.' errors as advert errors", function(){
+                e.log('Script error.', fakeError.filename, fakeError.lineno);
+                expect(document.getElementById('js-err').getAttribute('src')).toContain(
+                    '/px.gif?js/message=Script%20error.&filename=foo.js&lineno=1&isAd=1'
+                );
+            });
+            
         });
 
         it("if DEV, should log to console", function(){
