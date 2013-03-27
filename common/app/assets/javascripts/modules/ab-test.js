0dbb09d00ebed6586520ea818363c14b7420d1ce
Darren Hurley <darren.hurley@guardian.co.uk>
2013-03-27 17:03:14 +0000

    start method

--- common/app/assets/javascripts/modules/ab-test.js
+++ common/app/assets/javascripts/modules/ab-test.js
@@ -1,6 +1,10 @@
 define([], function () {
     
     var tests = {};
+    
+    function storeVariant(name) {
+        
+    }
 
     var abTest = {
             
@@ -14,6 +18,18 @@ define([], function () {
         },
         
         start: function(testName) {
+            // put in random variant
+            var variantNames = ['control'],
+                randomVariantName;
+            for (var variantName in tests[testName]) {
+                variantNames.push(variantName);
+            }
+            randomVariantName = variantNames[Math.floor(Math.random() * messages.length)];
+            // store variant
+            storeVariant(randomVariantName);
+            // run variant (if not control)
+            (randomVariantName === 'control') || variants[randomVariantName]();
+            
             return abTest;
         }
             
