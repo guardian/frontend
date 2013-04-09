2d161c21d8906f8a42680b73316b08cb800f3aaa
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-04-09 13:39:28 +0100

    Prefer faster getElementsByTagName to querySelectorAll

--- common/app/assets/javascripts/modules/images.js
+++ common/app/assets/javascripts/modules/images.js
@@ -16,7 +16,7 @@ define(['common', 'modules/detect', 'bonzo'], function (common, detect, bonzo) {
                 }
 
                 //upgrade other images;
-                [].forEach.call(context.querySelectorAll('img'), function(image) {
+                [].forEach.call(context.getElementsByTagName('img'), function(image) {
                     image = bonzo(image);
                     if (!image.attr('data-fullsrc')) {
                         return;
