dc7a43307018faafec18be36ecfafd9c2e473f99
Stephan Fowler <stephanfowler@gmail.com>
2013-04-13 00:39:30 +0100

    Use prototype obj for Control view methods

--- common/app/assets/javascripts/modules/navigation/control.js
+++ common/app/assets/javascripts/modules/navigation/control.js
@@ -2,53 +2,22 @@ define(['common', 'bean', 'bonzo'], function (common, bean, bonzo) {
 
     var Control = function () {
 
-        var delay = 400,
-            lastClickTime = 0,
-            self = this;
-
-        this.view = {
-            toggle: function(controls, control, context) {
-                Array.prototype.forEach.call(controls, function(c){
-                    if (c === control) {
-                        self.view[bonzo(c).hasClass('is-active') ? 'close' : 'open'](c, context);
-                    }
-                    else {
-                        self.view.close(c, context);
-                    }
-                });
-            },
-
-            open: function(c, context) {
-                bonzo(c).addClass('is-active');
-                bonzo(self.model.getTarget(c, context)).removeClass('is-off');
-            },
-            
-            close: function(c, context) {
-                bonzo(c).removeClass('is-active');
-                bonzo(self.model.getTarget(c, context)).addClass('is-off');
-            }
-        };
-
-        this.model = {
-            getTarget: function(control, context) {
-                var target = bonzo(control).data('control-for');
-                if (target) {
-                    return context.querySelector('.' + target);
-                }
-            }
-        };
+        var self = this,
+            delay = 400,
+            lastClickTime = 0;
 
         this.init = function(context) {
             var controls = context.querySelectorAll('.control');
+
             Array.prototype.forEach.call(controls, function(control){
-                var target = self.model.getTarget(control, context);
+                var target = self.getTarget(control, context);
                 if (target) {
                     bean.add(control, 'click touchstart', function (e) {
                         var current = new Date().getTime();
                         var delta = current - lastClickTime;
                         if (delta >= delay) {
                             lastClickTime = current;
-                            self.view.toggle(controls, control, context);
+                            self.toggle(controls, control, context);
                             common.mediator.emit('modules:control:change');
                         }
                         e.preventDefault();
@@ -58,6 +27,34 @@ define(['common', 'bean', 'bonzo'], function (common, bean, bonzo) {
         };
     };
 
-    return Control;
+    Control.prototype.toggle = function(controls, control, context) {
+        var self = this;
+        Array.prototype.forEach.call(controls, function(c){
+            if (c === control) {
+                self[bonzo(c).hasClass('is-active') ? 'close' : 'open'](c, context);
+            }
+            else {
+                self.close(c, context);
+            }
+        });
+    };
 
+    Control.prototype.getTarget = function(control, context) {
+        var target = bonzo(control).data('control-for');
+        if (target) {
+            return context.querySelector('.' + target);
+        }
+    };
+
+    Control.prototype.open = function(c, context) {
+        bonzo(c).addClass('is-active');
+        bonzo(this.getTarget(c, context)).removeClass('is-off');
+    };
+    
+    Control.prototype.close = function(c, context) {
+        bonzo(c).removeClass('is-active');
+        bonzo(this.getTarget(c, context)).addClass('is-off');
+    };
+
+    return Control;
 });
