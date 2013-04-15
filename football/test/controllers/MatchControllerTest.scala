5b5282868a40e7354e43d34df6095578a7f87ee5
Darren Hurley <darren.hurley@guardian.co.uk>
2013-04-15 17:30:06 +0100

    correct test data for match

--- football/test/controllers/MatchControllerTest.scala
+++ football/test/controllers/MatchControllerTest.scala
@@ -8,13 +8,13 @@ import org.scalatest.FlatSpec
 class MatchControllerTest extends FlatSpec with ShouldMatchers {
   
   "Match Controller" should "200 when content type is match" in Fake {
-    val result = controllers.MatchController.renderMatch("2012", "oct", "20", "tottenham-hotspur", "chelsea")(TestRequest())
+    val result = controllers.MatchController.renderMatch("2012", "oct", "20", "derbycounty", "blackburn")(TestRequest())
     status(result) should be(200)
   }
 
   it should "return JSONP when callback is supplied to match" in Fake {
-    val fakeRequest = FakeRequest(GET, "/football/match/2012/oct/20/tottenham-hotspur-v-chelsea?callback=foo").withHeaders("host" -> "localhost:9000")
-    val result = controllers.MatchController.renderMatch("2012", "oct", "20", "tottenham-hotspur", "chelsea")(fakeRequest)
+    val fakeRequest = FakeRequest(GET, "/football/match/2012/oct/20/derbycounty-v-blackburn?callback=foo").withHeaders("host" -> "localhost:9000")
+    val result = controllers.MatchController.renderMatch("2012", "oct", "20", "derbycounty-hotspur", "blackburn")(fakeRequest)
     status(result) should be(200)
     header("Content-Type", result).get should be("application/javascript")
   }
