40d8604239a6eb5471ad36ebce4726cda0b91a8e
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-04-16 16:51:24 +0100

    Wrap sample article paras

--- integration-tests/src/test/java/com/gu/test/StorySteps.java
+++ integration-tests/src/test/java/com/gu/test/StorySteps.java
@@ -39,8 +39,8 @@ public class StorySteps {
 
     @Then("^the first (\\d+) blocks from article is shown$")
     public void the_first_blocks_from_article_is_shown(int stories) throws Throwable {
-        List<WebElement> s = webDriver.findElements(By.cssSelector(".story-latest .article-body > *"));
-        assertEquals((s.size() - 2), stories);
+        List<WebElement> s = webDriver.findElements(By.cssSelector(".story-latest .article-body .sample-paras p"));
+        assertEquals(s.size(), stories);
     }
 
     @Then("^a \"([^\"]*)\" of events is displayed$")
