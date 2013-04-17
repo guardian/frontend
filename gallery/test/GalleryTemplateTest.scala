9d1045b69cdeb044005a6139cef0aba6345abb73
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-04-17 15:02:57 +0100

    Update jasmine and integration tests

--- gallery/test/GalleryTemplateTest.scala
+++ gallery/test/GalleryTemplateTest.scala
@@ -28,10 +28,10 @@ class GalleryTemplateTest extends FlatSpec with ShouldMatchers {
 
     $("p.caption").getTexts.firstNonEmpty.get should include("A TV grab from state-owned French television station France 2 showing")
 
-    $("p.gallery-nav a#js-gallery-prev").getAttributes("href").toList should be(List("javascript:")) // and this is how it's hidden
+    $("p.gallery-nav a.js-gallery-prev").getAttributes("href").toList should be(List("javascript:")) // and this is how it's hidden
 
-    $("p.gallery-nav a#js-gallery-next").getTexts.toList should be(List("Next"))
-    $("p.gallery-nav a#js-gallery-next").getAttributes("href").toList should be(List(WithHost("/news/gallery/2012/may/02/picture-desk-live-kabul-burma?index=2")))
+    $("p.gallery-nav a.js-gallery-next").getTexts.toList should be(List("Next"))
+    $("p.gallery-nav a.js-gallery-next").getAttributes("href").toList should be(List(WithHost("/news/gallery/2012/may/02/picture-desk-live-kabul-burma?index=2")))
   }
 
   it should "render caption and navigation on second image page" in HtmlUnit("/news/gallery/2012/may/02/picture-desk-live-kabul-burma?index=2") { browser =>
