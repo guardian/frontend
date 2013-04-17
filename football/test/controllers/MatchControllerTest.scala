7fcea35dfaefc697b9e1d172e07af1c7ee7d6990
Darren Hurley <darren.hurley@guardian.co.uk>
2013-04-17 16:21:41 +0100

    handle dynamic time changes for data

--- football/test/controllers/MatchControllerTest.scala
+++ football/test/controllers/MatchControllerTest.scala
@@ -4,17 +4,23 @@ import play.api.test._
 import play.api.test.Helpers._
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
+import org.joda.time.DateMidnight
 
 class MatchControllerTest extends FlatSpec with ShouldMatchers {
   
+  val today = new DateMidnight()
+  val year = today.toString("yyyy")
+  val month = today.toString("MMM")
+  val day = today.toString("dd")
+
   "Match Controller" should "200 when content type is match" in Fake {
-    val result = controllers.MatchController.renderMatch("2012", "oct", "20", "derbycounty", "blackburn")(TestRequest())
+    val result = controllers.MatchController.renderMatch(year, month, day, "derbycounty", "blackburn")(TestRequest())
     status(result) should be(200)
   }
 
   it should "return JSONP when callback is supplied to match" in Fake {
-    val fakeRequest = FakeRequest(GET, "/football/match/2012/oct/20/derbycounty-v-blackburn?callback=foo").withHeaders("host" -> "localhost:9000")
-    val result = controllers.MatchController.renderMatch("2012", "oct", "20", "derbycounty-hotspur", "blackburn")(fakeRequest)
+    val fakeRequest = FakeRequest(GET, "/football/match/" + year + "/" + month + "/" + day + "/derbycounty-v-blackburn?callback=foo").withHeaders("host" -> "localhost:9000")
+    val result = controllers.MatchController.renderMatch(year, month, day, "derbycounty-hotspur", "blackburn")(fakeRequest)
     status(result) should be(200)
     header("Content-Type", result).get should be("application/javascript")
   }
