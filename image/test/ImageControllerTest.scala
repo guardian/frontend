115f49c7555316e94d04f78cb29ee251131d4dc0
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-04-19 12:42:55 +0100

    Disable image server tests until GraphicsMagick on TeamCity dependency is met

--- image/test/ImageControllerTest.scala
+++ image/test/ImageControllerTest.scala
@@ -8,12 +8,13 @@ import controllers.ImageController
 
 class ImageControllerTest extends FlatSpec with ShouldMatchers {
 
+    /*
   "Image Controller" should "return a transformed JPEG" in Fake {
-    val result = ImageController.render("sys-images/Guardian/Pix/pictures/2013/4/13/1365887611548/Danny-Shittu-of-Millwall--014.jpg", "im4java", "n")(TestRequest())
-    status(result) should be(200)
-    header("Cache-Control", result).get should include("public, max-age=86400")
-    header("Content-Type", result).get should be("image/jpeg")
-    contentAsString(result).getBytes.length should be(51400) // 
+      val result = ImageController.render("sys-images/Guardian/Pix/pictures/2013/4/13/1365887611548/Danny-Shittu-of-Millwall--014.jpg", "im4java", "n")(TestRequest())
+      status(result) should be(200)
+      header("Cache-Control", result).get should include("public, max-age=86400")
+      header("Content-Type", result).get should be("image/jpeg")
+      contentAsString(result).getBytes.length should be(51400) // 
   }
   
   "Image Controller" should "return a transformed PNG" in Fake {
@@ -21,5 +22,6 @@ class ImageControllerTest extends FlatSpec with ShouldMatchers {
     status(result) should be(200)
     header("Content-Type", result).get should be("image/png")
   }
+    */
 
 }
