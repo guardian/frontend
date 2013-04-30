f812c775bf68ad39a3f33a9347b11fbf7ab65052
Darren Hurley <darren.hurley@guardian.co.uk>
2013-04-30 14:24:18 +0100

    nwtoerk front more-stories

--- core-navigation/app/controllers/MoreStoriesController.scala
+++ core-navigation/app/controllers/MoreStoriesController.scala
@@ -13,26 +13,34 @@ object MoreStoriesController extends Controller with Logging {
 
   def render(path: String) = Action { implicit request =>
     val edition = Site(request).edition
-    val promiseOfMoreStories = lookup(edition, path).map(_.toList)
+    val section = path.split("/").headOption.getOrElse("")
+    val promiseOfMoreStories = mostViewed(edition, section).map(_.toList)
     Async {
       promiseOfMoreStories.map {
         sectionPopular =>
           sectionPopular match {
             case Nil => JsonNotFound()
-            case moreStories => Cached(900){ JsonComponent(("stories" -> toJson(moreStories.map { story => toJson(Map("url" -> story.url)) }))) }
+            case moreStories => {
+              val currentPage = toJson(Map("url" -> s"/$path"))
+              var stories = currentPage +: moreStories.filter { _.url != s"/$path" }.map { story => toJson(Map("url" -> story.url)) } 
+              // append section front at the end, if we're not currently on it
+              if (!path.equals(section)) {
+                stories = stories ++ Seq(toJson(Map("url" -> s"/$section")))
+              }
+              Cached(900){ JsonComponent(("stories" -> toJson(stories))) }
+            }
           }
       }
     }
   }
 
-  private def lookup(edition: String, path: String)(implicit request: RequestHeader) = {
+  private def mostViewed(edition: String, path: String)(implicit request: RequestHeader) = {
     log.info(s"Fetching more stories: $path for edition $edition")
     ContentApi.item(path, edition)
       .tag(None)
-      .showRelated(true)
       .showMostViewed(true)
       .response.map{response =>
-          SupportedContentFilter((response.relatedContent ++ response.mostViewed) map { new Content(_) })
+          SupportedContentFilter(response.mostViewed map { new Content(_) })
     }
   }
 }
