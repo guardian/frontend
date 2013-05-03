7cf6d92bb0f3894e156c4234afb845754f726c4c
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-05-03 13:01:17 +0100

    New story gallery functionality, image sizing, and UI tweaks

--- common/app/model/content.scala
+++ common/app/model/content.scala
@@ -133,6 +133,7 @@ class Gallery(private val delegate: ApiContent, storyItems: Option[StoryItems] =
   def apply(index: Int): Image = lookup(index)
   lazy val size = images.size
   lazy val contentType = "Gallery"
+  lazy val landscapes = images.filter(i => i.aspectRatio >= 1.5 && i.aspectRatio <= 1.55)
   override lazy val analyticsName = s"GFE:$section:$contentType:${id.substring(id.lastIndexOf("/") + 1)}"
   override lazy val metaData: Map[String, Any] = super.metaData + ("content-type" -> contentType, "gallerySize" -> size)
 }
