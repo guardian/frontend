693346fe5a8450ee64f4db7dde0b2b8dd1b5a08a
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-05-07 11:29:41 +0100

    Pass config to the aware bootstrap

--- common/app/assets/javascripts/bootstraps/article.js
+++ common/app/assets/javascripts/bootstraps/article.js
@@ -67,7 +67,7 @@ define([
         
         aware: function() {
             common.mediator.on('page:article:ready', function(config, context) {
-               Aware.logVisit(config.page.section);
+               Aware.logVisit(config);
             });
         }
     };
