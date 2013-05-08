fffd30065406c77571cb7e833c981475d64bc489
Francis Carr <francis.carr@guardian.co.uk>
2013-05-08 11:22:34 +0100

    Remove australia specific code

--- front/app/controllers/front/AustraliaConfiguredEdition.scala
+++
@@ -1,9 +0,0 @@
-package controllers.front
-
-import model.TrailblockDescription
-
-class AustraliaConfiguredEdition(edition: String, descriptions: Seq[TrailblockDescription])
-  extends ConfiguredEdition(edition, descriptions) {
-
-  override val manualAgents = descriptions.map(AustraliaTrailblockAgent(_, edition))
-}
