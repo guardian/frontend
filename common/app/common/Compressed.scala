90f23c04d034fd09b42429b5ca364435b6ca2913
grant <grant.klopper@guardian.co.uk>
2013-05-13 15:37:14 +0100

    Disable html compressing, JStack says we are doing too much work in there

--- common/app/common/Compressed.scala
+++ common/app/common/Compressed.scala
@@ -10,13 +10,14 @@ object Compressed extends Logging {
   compressor.setCompressCss(true)
 
   def apply(block: Html): Html = {
-    val originalBody = block.body
-    val compressedBody = compressor.compress(originalBody)
-
-    //not a perfect size measurement, but it gives an idea
-    log.trace("%s -> %s : %.2f percent" format (originalBody.length, compressedBody.length,
-      compressedBody.length.toDouble / originalBody.length.toDouble * 100))
-
-    Html(compressedBody)
+//    val originalBody = block.body
+//    val compressedBody = compressor.compress(originalBody)
+//
+//    //not a perfect size measurement, but it gives an idea
+//    log.trace("%s -> %s : %.2f percent" format (originalBody.length, compressedBody.length,
+//      compressedBody.length.toDouble / originalBody.length.toDouble * 100))
+//
+//    Html(compressedBody)
+    block
   }
 }
