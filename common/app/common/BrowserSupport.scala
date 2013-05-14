8c9c62d3627d809fc611ad0839ea6150799cbf9c
Andy Hume <andyhume@gmail.com>
2013-05-14 11:33:50 +0100

    Corrected HTMLPreparser logic.

--- common/app/common/BrowserSupport.scala
+++ common/app/common/BrowserSupport.scala
@@ -5,8 +5,7 @@ import play.api.mvc.RequestHeader
 class Browser extends Logging {
     
 	def HTMLPreparser(request: RequestHeader): Boolean = {
-		log.warn(request.headers.get("X-Gu-Feature-HTMLPreparser").getOrElse("nothing"))
-		!request.headers.get("X-Gu-Feature-HTMLPreparser").isDefined
+		request.headers.get("X-Gu-Feature-HTMLPreparser").isDefined
 	}
 
 }
