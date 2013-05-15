91716109652cd50a0977daf54d9edadee157158f
Andy Hume <andyhume@gmail.com>
2013-05-15 13:01:01 +0100

    Striped ID module of unused UI and API code.

--- common/app/assets/javascripts/components/gu-id/id.js
+++ common/app/assets/javascripts/components/gu-id/id.js
@@ -1,17 +1,6 @@
-// TODO: Check on preprocessing of endpoints
-var IDENTITY = IDENTITY || {};
-
-(function (name, context, definition) {
+define(function() {
     'use strict';
-    if (typeof module !== 'undefined' && module.exports) module.exports = definition();
-    else if (typeof define === 'function' && define.amd) {
-        define(name, definition);
-        define(definition);
-    } else context[name] = definition();
-}('guardian_idToolkit', IDENTITY, function() {
-    'use strict';
-    var pubsub, ajax, sdk,
-        empty = function() {},
+    var sdk,
         urls = {
             validFrom: 'guardian.co.uk',
             api: 'https://id.guardianapis.com',
@@ -32,165 +21,14 @@ var IDENTITY = IDENTITY || {};
                                             , {packageCode: 'YCL', path: '/sys/policies/clippings'}
                                             , {packageCode: 'GHO', path: '/sys/policies/holiday-offers'}
                                             , {packageCode: 'N0TICE', path: '/sys/policies/n0tice'}
-                                    ],
-        endpoints = {
-            'user/me': {
-                uri: '/user/me?encode=signedResponse',
-                methods: ['get'],
-                cache: true,
-                exports: 'user',
-                preprocess: function(res) {
-                    var userData,
-                        responseParts = res.split('.'),
-                        rawData = responseParts[0];
-
-                    userData = JSON.parse(decodeBase64(rawData));
-
-                    if (userData.status === 'ok') {
-                        return userData.user;
-                    } else {
-                        return userData;
-                    }
-                }
-            },
-            //depricated
-            'user/me/email/subscribe': {
-                uri: '/useremails/:me/subscriptions',
-                methods: ['get', 'post', 'delete'],
-                exports: 'subscriptions'
-            },
-            'useremails/me/subscriptions': {
-                uri: '/useremails/:me/subscriptions',
-                methods: ['get', 'post', 'delete'],
-                exports: 'subscriptions'
-            }
-        };
-
-    /**
-     * API
-     */
-    function api() {
-        var call, url, data,
-            success, error, callback, failure,
-            params = {}, method = 'get',
-            arg, argType,
-            endpoint;
-
-        for (var i = 0, len = arguments.length; i < len; i++) {
-            arg = arguments[i];
-
-            if (i === 0) { call = arg; continue; }
-            argType = (typeof arg).toLowerCase();
-
-            switch (argType) {
-                case 'string':
-                    method = arg;
-                    break;
-                case 'object':
-                    params = arg;
-                    break;
-                case 'function':
-                    if (callback) { failure = arg; }
-                    else { callback = arg; }
-                    break;
-            }
-        }
-
-        endpoint = endpoints[call];
-        if (!endpoint) {
-            endpoint = {
-                uri : '/' + call,
-                methods: ['get', 'post', 'delete']
-            }
-        }
-
-        // callback handling
-        if (!callback) { success = function(){}; }
-        if (!failure) { failure = function(){}; }
-
-        error = failure; // This might come in handy
-
-        if (endpoint.preprocess) {
-            success = function(res) {
-                res = endpoint.preprocess(res);
-                callback(res);
-            };
-        } else { success = callback; }
-
-        // a little magic
-        url = urls.api + endpoint.uri.replace(':me', localUserData().id);
-        data = { body: JSON.stringify(params) };
-        if (method) { data.method = method; } // this is just till we have a better way
-
-        if (!endpoint.cache) {
-            ajax({
-                url: url,
-                dataType: 'jsonp',
-                data: data,
-                success: function(res) {
-                    if (res.status === 'ok') {
-                        success(res);
-                    } else {
-                        error(res);
-                    }
-                }
-            });
-        } else {
-            if (!endpoint.started) {
-                endpoint.started = true;
-                endpoint.callbacks = endpoint.callbacks || { success: [], error: [] };
-                endpoint.callbacks.success.push(success);
-                endpoint.callbacks.error.push(failure);
-
-                ajax({
-                    url: url,
-                    dataType: 'jsonp',
-                    data: data,
-                    success: function(res) {
-                        if (res.status === 'ok') {
-                            endpoint.res = res;
-                            endpoint.finished = true;
-                            for (var i = 0, len = endpoint.callbacks.success.length; i < len; i++) {
-                                endpoint.callbacks.success[i](res);
-                            }
-                        } else {
-                            endpoint.res = res;
-                            endpoint.started = false; // don't cache errors?
-                            for (var i = 0, len = endpoint.callbacks.error.length; i < len; i++) {
-                                endpoint.callbacks.error[i](res);
-                            }
-                        }
-                    }
-                });
-            } else if (endpoint.started && !endpoint.finished) {
-                endpoint.callbacks.success.push(success);
-                endpoint.callbacks.error.push(failure);
-            } else {
-                success(endpoint.res);
-            }
-        }
-    }
+                                    ];
 
     /**
      * SDK functions
      */
-    function setAjaxLib(lib, options) {
-        ajax = lib;
-    }
-
-    function throwDomainError(){
-        throw new Error("Cannot access Guardian user data from this domain");
-    }
 
     function isLoggedIn() {
         return !!localUserData();
-    }
-
-    function showLoginIfNotLoggedIn(domFramework) {
-        if(isLoggedIn()) { return localUserData(); }
-        loginOverlay.init(domFramework); // optional dom framework for overlay's init
-        loginOverlay.open();
-        return false;
     }
 
     /**
@@ -248,154 +86,6 @@ var IDENTITY = IDENTITY || {};
     }
 
     /*
-     * UI
-     */
-    var loginOverlay = (function(){
-        var visible = false,
-            options = {
-                animate: true,
-                animationTime: 200,
-                ajaxTimeout: 2000,
-                height: '380px',
-                width: '620px'
-            },
-            isLtIE9 = (function()
-            {
-                var div = document.createElement('div');
-                div.innerHTML = '<!--[if lt IE 9]><i></i><![endif]-->';
-                return (div.getElementsByTagName('i').length === 1);
-            }()),
-            blackout, overlay, jQ;
-
-        var closeOverlayCallback = function(e) {
-            if (27 === e.keyCode) { close(); }
-        };
-
-        /**
-         * Close the overlay and remove any contents.
-         */
-        var close = function() {
-            if (visible) {
-                if (options.animate) {
-                    blackout.fadeOut(options.animationTime);
-                    overlay.fadeOut(options.animationTime, function() {
-                        jQ(this).find('.identity-overlay-inner').empty();
-                    });
-                }
-                else {
-                    blackout.hide();
-                    overlay.hide().empty();
-                }
-
-                jQ(window).unbind("keyup", closeOverlayCallback);
-                jQ(window).trigger('identity.overlay.closed');
-            }
-            visible = false;
-        };
-
-        /**
-         * Show the identity login overlay
-         */
-        var open = function(returnUrl, targetHref) {
-            returnUrl = encodeURIComponent(returnUrl || document.location.href);
-            var href = (targetHref || urls.web + "/signin?minimal=true&returnUrl=" + returnUrl);
-            overlay.find('.identity-overlay-inner').html('<iframe height="100%" width="100%" src="' + href + '"></iframe>');
-            if (options.animate) {
-                blackout.fadeIn(options.animationTime);
-                overlay.fadeIn(options.animationTime);
-            }
-            else {
-                blackout.show();
-                overlay.show();
-            }
-            overlay.css({ 'margin-left': -(overlay.width() / 2) });
-
-            jQ(window).bind("keyup", closeOverlayCallback);
-            jQ(window).trigger('identity.overlay.launched');
-            visible = true;
-        };
-
-        /**
-         * Creates the unpopulated overlay elements in the DOM ready for use.
-         */
-        var createOverlay = function() {
-            if (!blackout) {
-                blackout = jQ('<div class="identity-overlay-blackout" />')
-                        .attr('tabindex', -1)
-                        .click(close);
-                if(isLtIE9) {
-                    blackout.addClass("lt-ie9");
-                }
-                jQ('body').prepend(blackout);
-            }
-
-            if (!overlay) {
-                overlay = jQ('<div class="identity-overlay" />');
-
-                var closeElem = jQ('<span class="identity-overlay-close">Close</span>')
-                        .click(close);
-
-                overlay
-                        .append(closeElem)
-                        .append('<div class="identity-overlay-inner" />')
-                        .height(options.height)
-                        .width(options.width)
-                        .appendTo(jQ('body'));
-            }
-
-            jQ(window).trigger('identity.overlay.created');
-        };
-
-        /**
-         * Set up the dom framework
-         */
-        var init = function(domFramework){
-            if (domFramework === undefined) {
-                // check for supported DOM frameworks
-                if (typeof window.jQuery === "function") {
-                    jQ = jQuery;
-                } else if (typeof window.Zepto === "function") {
-                    (function(zepto){
-                        zepto.extend(zepto.fn, {
-                            fadeIn: function(duration, complete){
-                                complete = complete || empty;
-                                this.css({"opacity": 0}).show();
-                                return this.animate({'opacity': 1}, duration, complete);
-                            },
-                            fadeOut: function(duration, complete){
-                                complete = complete || empty;
-                                return this.animate({'opacity': 0}, duration, function(){
-                                    this.hide();
-                                    complete.apply(this, arguments);
-                                });
-                            }
-                        });
-                    }(window.Zepto));
-                    jQ = window.Zepto;
-                } else {
-                    throw new Error("No DOM framework detected");
-                }
-            } else {
-                jQ = domFramework;
-            }
-                jQ('<style>.identity-overlay-blackout {\n    background-color: rgba(0,0,0,.6);\n    cursor: pointer;\n    display: none;\n    position: fixed;\n    top: 0; right: 0; bottom: 0; left: 0;\n    z-index: 2001;\n}\n\n.identity-overlay {\n    background-color: #fff;\n    -webkit-box-shadow: 0 0 20px rgba(0,0,0,.5);\n    -moz-box-shadow: 0 0 20px rgba(0,0,0,.5);\n    box-shadow: 0 0 20px rgba(0,0,0,.5);\n    display: none;\n    overflow: hidden;\n    position: fixed;\n    left: 50%; top: 100px;\n    z-index: 2002;\n}\n\n.identity-overlay-inner {\n    height: 100%;\n}\n\n.identity-overlay-close {\n    background: transparent url(http:\/\/id.guim.co.uk\/static\/251\/cs\/images\/close.png) top left no-repeat;\n    cursor: pointer;\n    display: block;\n    height: 32px;\n    overflow: hidden;\n    position: absolute;\n    top: 0; right: 0;\n    text-indent: 100%;\n    white-space: nowrap;\n    width: 32px;\n}\n\n.identity-overlay iframe {\n    border: 0;\n}\n\n.identity-overlay-blackout.lt-ie9 {\n    background: transparent;\n    -ms-filter: \"progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000,endColorstr=#99000000)\";\n    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000,endColorstr=#99000000);\n    zoom: 1;\n}</style>').appendTo('head');
-            createOverlay();
-        };
-
-        return {
-            init: init,
-            open: function(returnUrl, targetHref){
-                init();
-                open(returnUrl, targetHref);
-            },
-            close: function(){
-                init();
-                close();
-            }
-        };
-    }());
-
-    /*
      * Utility functions
      */
     // based on https://github.com/davidchambers/Base64.js/blob/master/base64.js
@@ -449,13 +139,9 @@ var IDENTITY = IDENTITY || {};
     }
 
     sdk = {
-        api: api,
-        setAjaxLib: setAjaxLib,
         validDomain: true,
         isLoggedIn: isLoggedIn,
-        loginOverlay: loginOverlay,
-        localUserData: localUserData,
-        showLoginIfNotLoggedIn: showLoginIfNotLoggedIn
+        localUserData: localUserData
     }
     return sdk;
-}));
+});
