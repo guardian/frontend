64831c53de4edfdec8b35f6eaa76e1113adb5125
Francis Carr <francis.carr@guardian.co.uk>
2013-05-17 12:04:28 +0100

    Add new australia sport and culture query to Us edition

--- common/app/common/editions/Us.scala
+++ common/app/common/editions/Us.scala
@@ -75,8 +75,18 @@ object Us extends Edition("US", "US edition", DateTimeZone.forID("America/New_Yo
 
     "australia" -> Seq(
       ItemTrailblockDescription("", "News", numItemsVisible = 5, style = Some(Featured), showMore = true),
-      ItemTrailblockDescription("sport", "Sport", numItemsVisible = 5, style = Some(Featured), showMore = true),
-      ItemTrailblockDescription("culture", "Culture", numItemsVisible = 3, style = Some(Thumbnail), showMore = true),
+      ItemTrailblockDescription("sport", "Sport", numItemsVisible = 3, style = Some(Featured), showMore = true),
+      ItemTrailblockDescription("sport/australia-sport", "Australia sport", numItemsVisible = 3, style = Some(Featured), showMore = true)(Au),
+      QueryTrailblockDescription("culture", "Culture", numItemsVisible = 3, style = Some(Featured), showMore = true,
+        customQuery=ContentApi.item.itemId("culture")
+          .edition("au")
+          .showTags("all")
+          .showFields(trailFields)
+          .showInlineElements(inlineElements)
+          .showMedia("all")
+          .showReferences(references)
+          .showStoryPackage(true)
+          .tag(s"-stage/stage,-artanddesign/art,-stage/theatre,-stage/dance,-stage/comedy,-stage/musicals,-artanddesign/photography,($supportedTypes)")),
       QueryTrailblockDescription("commentisfree", "Comment is free", numItemsVisible = 3, style = Some(Featured), showMore = true,
         customQuery=ContentApi.item.itemId("commentisfree")
           .edition("au")
@@ -86,7 +96,7 @@ object Us extends Edition("US", "US edition", DateTimeZone.forID("America/New_Yo
           .showMedia("all")
           .showReferences(references)
           .showStoryPackage(true)
-          .tag(supportedTypes)),
+          .tag(s"world/australia,($supportedTypes)")),
       ItemTrailblockDescription("technology", "Technology", numItemsVisible = 1, style = Some(Thumbnail))
     )
   )
