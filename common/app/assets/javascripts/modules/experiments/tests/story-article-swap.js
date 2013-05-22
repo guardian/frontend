72a6f6497262d3c9110b97201b96536be12be349
Darren Hurley <darren.hurley@guardian.co.uk>
2013-05-22 14:46:20 +0100

    set up ab test

---
+++ common/app/assets/javascripts/modules/experiments/tests/story-article-swap.js
@@ -0,0 +1,77 @@
+define(['common', 'ajax', 'bonzo', 'bootstraps/story'], function (common, ajax, bonzo, story) {
+    
+    var _config,
+        _context;
+    
+    function fetchStory(storyId) {
+        ajax({
+            url: '/stories/' + storyId,
+            type: 'jsonp',
+            jsonpCallbackName: 'story',
+            success: function (response) {
+                if (!response) {
+                    return;
+                }
+                common.$g('.parts__body', _context).replaceWith('<div class="parts__body">' + response.html + '</div>');
+                
+                story.init(_config, _context);
+            }
+        });
+    }
+    
+    function fetchStoriesArticles() {
+        ajax({
+            url: '/stories/articles',
+            type: 'jsonp',
+            jsonpCallbackName: 'storiesArticles',
+            success: function (response) {
+                if (!response) {
+                    return;
+                }
+                // are we on one of these articles
+                var currentUrl = window.location.pathname,
+                    storiesArticles = bonzo.create(response.html);
+                for(var i = 0; i < storiesArticles.length; i++) {
+                    var $articleLink = common.$g('h2 a', storiesArticles[i]),
+                        articleUrl = $articleLink.attr('data-article-url');
+                    if (currentUrl === articleUrl) {
+                        // pull in story
+                        fetchStory($articleLink.attr('href').split('/')[2]);
+                        return true;
+                    }
+                }
+            }
+        });
+    }
+
+    var StoryArticleSwap = function () {
+        
+        this.id = 'StoryArticleSwap';
+        this.audience = 1;
+        this.description = 'Swap the latest article within the story with the story itself';
+        this.canRun = function(config, context) {
+            _config = config;
+            _context = context;
+            // only run on article pages
+            return config.page.contentType === 'Article';
+        };
+        this.variants = [
+            {
+                id: 'control',
+                test: function () {
+                    return true;
+                }
+            },
+            {
+                id: 'test',
+                test: function () {
+                    // pull in stories' latest article
+                    fetchStoriesArticles();
+                }
+            }
+        ];
+    };
+
+    return StoryArticleSwap;
+
+});
