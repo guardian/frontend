a2e044ef87bf08fdab0ffbdb53b889c663186b7a
Kaelig <kaelig@deloumeau.fr>
2013-06-07 12:33:14 +0100

    Copy tweaks to buttons

--- common/app/assets/javascripts/modules/discussion.js
+++ common/app/assets/javascripts/modules/discussion.js
@@ -30,8 +30,8 @@ define([
             actionsTemplate       = '<button class="d-actions__link js-show-more-comments cta" data-link-name="Show more comments">Show more comments</button>' +
                 '<div class="d-actions">' +
                 '<a class="d-actions__link" href="' + config.page.canonicalUrl + '#start-of-comments">' +
-                    'Want to comment? Visit the desktop site</a>' +
-                '<button class="top js-show-article" data-link-name="Discussion: Back to article">Back to article</button></div>',
+                    'Want to comment? Visit the desktop site ›</a>' +
+                '<button class="top js-show-article" data-link-name="Discussion: Return to article">Return to article</button></div>',
             self;
 
         return {
