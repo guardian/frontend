6f7e67da4f237adefe4628d69e0a68567569233c
Darren Hurley <darren.hurley@guardian.co.uk>
2013-06-07 16:55:35 +0100

    adding format param to front endpoints

--- front/test/FrontControllerTest.scala
+++ front/test/FrontControllerTest.scala
@@ -9,6 +9,7 @@ import conf.CommonSwitches
 class FrontControllerTest extends FlatSpec with ShouldMatchers {
   
   val articleUrl = "/environment/2012/feb/22/capitalise-low-carbon-future"
+  val callbackName = "foo"
 
   "Front Controller" should "200 when content type is front" in Fake {
     val result = controllers.FrontController.render("front")(TestRequest())
@@ -16,10 +17,24 @@ class FrontControllerTest extends FlatSpec with ShouldMatchers {
   }
 
   it should "return JSONP when callback is supplied to front" in Fake {
-    val fakeRequest = FakeRequest(GET, "?callback=foo").withHeaders("host" -> "localhost:9000")
+    val fakeRequest = FakeRequest(GET, s"?callback=$callbackName")
+        .withHeaders("host" -> "localhost:9000")
+        
     val result = controllers.FrontController.render("front")(fakeRequest)
     status(result) should be(200)
-    header("Content-Type", result).get should be("application/javascript")
+    contentType(result).get should be("application/javascript")
+    contentAsString(result) should startWith(callbackName + "({\"config\"")
+  }
+
+  it should "return JSON when .json format is supplied to front" in Fake {
+    val fakeRequest = FakeRequest("GET", ".json")
+      .withHeaders("Host" -> "localhost:9000")
+      .withHeaders("Origin" -> "http://www.theorigin.com")
+      
+    val result = controllers.FrontController.render("front")(fakeRequest)
+    status(result) should be(200)
+    contentType(result).get should be("application/json")
+    contentAsString(result) should startWith("{\"config\"")
   }
 
   it should "200 when content type is front trails" in Fake {
@@ -28,10 +43,24 @@ class FrontControllerTest extends FlatSpec with ShouldMatchers {
   }
 
   it should "return JSONP when callback is supplied to front trails" in Fake {
-    val fakeRequest = FakeRequest(GET, "/culture/trails?callback=foo").withHeaders("host" -> "localhost:9000")
+    val fakeRequest = FakeRequest(GET, s"/culture/trails?callback=$callbackName")
+        .withHeaders("host" -> "localhost:9000")
+        
     val result = controllers.FrontController.renderTrails("front")(fakeRequest)
     status(result) should be(200)
-    header("Content-Type", result).get should be("application/javascript")
+    contentType(result).get should be("application/javascript")
+    contentAsString(result) should startWith(callbackName + "({\"html\"")
+  }
+
+  it should "return JSON when .json format is supplied to front trails" in Fake {
+    val fakeRequest = FakeRequest("GET", "/culture/trails.json")
+      .withHeaders("Host" -> "localhost:9000")
+      .withHeaders("Origin" -> "http://www.theorigin.com")
+      
+    val result = controllers.FrontController.renderTrails("front")(fakeRequest)
+    status(result) should be(200)
+    contentType(result).get should be("application/json")
+    contentAsString(result) should startWith("{\"html\"")
   }
 
   it should "200 for australia" in Fake {
