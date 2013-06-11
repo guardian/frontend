5de965af4382e42502f1d6f4b7b98903a789bb56
Darren Hurley <darren.hurley@guardian.co.uk>
2013-06-11 10:26:03 +0100

    move event over to .json

--- event/app/controllers/StoryController.scala
+++ event/app/controllers/StoryController.scala
@@ -62,7 +62,7 @@ object StoryController extends Controller with Logging with ExecutionContexts {
     }
   }
 
-  def byId(id: String) = Action {
+  def byId(id: String, format: String = "html") = Action {
     implicit request =>
       val edition = Edition(request)
       val promiseOfStory = Future(Story.mongo.byId(id))
