aba62d8f5345b3d4f485114e3edbbcba60164081
Kaelig <kaelig@deloumeau.fr>
2013-06-18 17:14:17 +0100

    Fix test names so they are catched by the framework

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -147,7 +147,7 @@ object Switches extends Collections {
     "If this is switched on an AB test runs to measure the impact of not showing fallback fonts while fonts download.",
     safeState = Off)
 
-  val ParagraphSpacingSwitch = Switch("A/B Tests", "paragraph-spacing",
+  val ABParagraphSpacingSwitch = Switch("A/B Tests", "ab-paragraph-spacing",
     "If this is switched on an AB test runs to measure the impact of macro typography tweaks on readability.",
     safeState = Off)
 
@@ -185,7 +185,7 @@ object Switches extends Collections {
     ImageServerSwitch,
     AustraliaFrontSwitch,
     FontDelaySwitch,
-    ParagraphSpacingSwitch,
+    ABParagraphSpacingSwitch,
     IntegrationTestSwitch
   )
 
