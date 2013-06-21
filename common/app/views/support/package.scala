a490e802b1df06d9daf884933fbc908922c5b955
grant <grant.klopper@guardian.co.uk>
2013-06-21 15:16:43 +0100

    Stop edition urls leaking into m.guardian.co.uk

--- common/app/views/support/package.scala
+++ common/app/views/support/package.scala
@@ -189,7 +189,7 @@ case class InBodyLinkCleaner(dataLinkName: String)(implicit val edition: Edition
     val links = body.getElementsByTag("a")
 
     links.foreach { link =>
-      link.attr("href", LinkTo(link.attr("href")))
+      link.attr("href", LinkTo(link.attr("href"), edition))
       link.attr("data-link-name", dataLinkName)
     }
     body
