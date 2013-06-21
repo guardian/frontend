fcfba6e213856b90a114ba7ec26d8266a1458469
Kaelig <kaelig@deloumeau.fr>
2013-06-21 16:49:17 +0100

    Test images get inserted

--- common/test/assets/javascripts/spec/Images.spec.js
+++ common/test/assets/javascripts/spec/Images.spec.js
@@ -5,9 +5,10 @@ define(['modules/images', 'fixtures'], function(Images, fixtures) {
         var conf = {
                     id: 'images',
                     fixtures: [
-                        '<div id="upgrade"><img data-upgrade="true" data-fullsrc="http://placekitten.com/1/1" src="http://placekitten.com/2/2" data-thumb-width="5" data-full-width="100" id="upgradeImages" class="visible" /></div>',
-                        '<div id="force-upgrade"><img data-upgrade="true" data-fullsrc="http://placekitten.com/3/3" src="http://placekitten.com/2/2" data-force-upgrade="1" data-thumb-width="5" data-full-width="100" id="upgradeImagesForce" class="visible" /></div>',
-                        '<div id="upgrade-svg"><img data-svgsrc="http://x.y.z/b.svg" id="upgradeSvgImages" class="visible" src="http://x.y.z/c.svg"/></div>'
+                        '<div id="insert"><img data-upgrade="true" data-lowsrc="http://placekitten.com/2/2" id="upgradeImagesLowOnly" class="visible" /></div>',
+                        '<div id="upgrade"><img data-upgrade="true" data-fullsrc="http://placekitten.com/1/1" data-lowsrc="http://placekitten.com/2/2" data-thumb-width="5" data-full-width="100" id="upgradeImages" class="visible" /></div>',
+                        '<div id="force-upgrade"><img data-upgrade="true" data-fullsrc="http://placekitten.com/3/3" data-lowsrc="http://placekitten.com/2/2" data-force-upgrade="1" data-thumb-width="5" data-full-width="100" id="upgradeImagesForce" class="visible" /></div>',
+                        '<div id="upgrade-svg"><img data-svgsrc="http://x.y.z/b.svg" id="upgradeSvgImages" class="visible" data-lowsrc="http://x.y.z/c.svg"/></div>'
                     ]
                    }
 
@@ -16,7 +17,19 @@ define(['modules/images', 'fixtures'], function(Images, fixtures) {
             fixtures.render(conf);
         })
 
-        it("swap a low resolution image for a full resolution", function(){
+        it("should set the src of an image with low resolution", function(){
+            
+            window.innerWidth = 2000; 
+            window.performance = { timing: { requestStart: 1, responseEnd: 10 } };
+
+            var i = new Images().upgrade(document.getElementById('insert'));
+            
+            var img = document.getElementById('upgradeImagesLowOnly');
+            expect(img.src).toContain('http://placekitten.com/2/2');
+
+        });
+
+        it("should swap a low resolution image for a full resolution", function(){
             
             window.innerWidth = 2000; 
             window.performance = { timing: { requestStart: 1, responseEnd: 10 } };
@@ -29,7 +42,7 @@ define(['modules/images', 'fixtures'], function(Images, fixtures) {
             
         });
         
-        it("force upgrade on small layouts when data-force-upgrade attribute is present", function() {
+        it("should force upgrade on small layouts when data-force-upgrade attribute is present", function() {
             
             window.innerWidth = 100; // small viewport
             window.performance = { timing: { requestStart: 1, responseStart: 10 } };
@@ -38,7 +51,7 @@ define(['modules/images', 'fixtures'], function(Images, fixtures) {
             expect(document.getElementById('upgradeImagesForce').src).toContain('http://placekitten.com/3/3');
         })
         
-        it("swap a low resolution image for an SVG", function(){
+        it("should swap a low resolution image for an SVG", function(){
 
             window.innerWidth = 1024;
             window.performance = { timing: { requestStart: 1, responseStart: 10 } };
