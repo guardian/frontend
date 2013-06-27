f33b144abb624df829df81b2591f69615180fb27
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-06-27 21:52:42 +0100

    Interactive JS bootstrap

--- common/app/assets/javascripts/modules/interactive.js
+++ common/app/assets/javascripts/modules/interactive.js
@@ -2,8 +2,8 @@ define([], function () {
 
     function Interactive(context, config) {
         
-        var host = 'http://s3-eu-west-1.amazonaws.com/aws-frontend-interactives' // FIXME
-          , path = config.page.pageId;
+        var host = 'http://s3-eu-west-1.amazonaws.com/aws-frontend-interactives',
+            path = config.page.pageId;
 
         this.init = function () {
             require([host, path, 'boot.js'].join('/'), function (interactive) {
