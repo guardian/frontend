b1a5ebf58cb1d2975dbd8a7294159baf9c54f91b
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-06-28 17:06:00 +0100

    Jasmine test for data-attribute

--- common/test/assets/javascripts/spec/Interactive.spec.js
+++ common/test/assets/javascripts/spec/Interactive.spec.js
@@ -7,25 +7,30 @@ define([ 'common',
             var conf = {
                     id: 'interactive',
                     fixtures: [
-                                '<figure id="abcde" class="interactive" data-interactive="path/to/interactive">' +
-                                '  <p>foo</p>' +
+                                '<figure class="interactive" data-interactive="path/to/interactive">' +
+                                '  <p>.</p>' +
                                 '</div>'
                               ]
             };
 
             var config = {
                 page: {
-                  interactiveUrl: 'foo',
-                  pageId: 'bar'
+                  interactiveUrl: 'http://foo/'
                 }
             };
 
             beforeEach(function() {
                 fixtures.render(conf);
+                require = jasmine.createSpy();
             });
 
             it("Should exist", function() {
                 expect(new Interactive(document.querySelector('figure.interactive'), document, config)).toBeDefined();
+            });
+            
+            it("Should load the interactive resource defined in the data-attribute", function() {
+                new Interactive(document.querySelector('figure.interactive'), document, config).init();
+                expect(require.mostRecentCall.args[0]).toBe('http://foo/path/to/interactive/boot.js');
             });
 
         });
