3298b71f9373fd2c0d37f52803435b44d45008fa
Francis Carr <francis.carr@guardian.co.uk>
2013-06-28 16:02:53 +0100

    Tests for the API trait Add a mutable map version of the api

--- fronts-api/app/frontsapi/FrontsApi.scala
+++ fronts-api/app/frontsapi/FrontsApi.scala
@@ -36,4 +36,63 @@ class CustomRedisClient(address: String, port: Int) extends RedisClient(address,
     send("LINSERT", List(key, position, pivot, value))(asLong)
 }
 
-object FrontsApi extends FrontsApiRedis(new CustomRedisClient("localhost", 6379))
+class FrontsApiMap extends FrontsApi {
+  val db: scala.collection.mutable.Map[String, List[String]] = scala.collection.mutable.Map[String, List[String]]()
+
+  def getList(listName: String) = db.get(listName) getOrElse Nil
+
+  def addAtPosition(listName: String, position: Int, value: String): Option[Boolean] = {
+    db.get(listName) map { l =>
+      db += (listName -> insertAtPosition(l, position, value))
+      true
+    } orElse {
+      db += (listName -> List(value))
+      Option(true)
+    }
+  }
+
+  private def insertAtPosition(l: List[String], position: Int, value: String): List[String] = {
+    val splitList = l splitAt position
+    splitList._1 ++ List(value) ++ splitList._2
+  }
+
+  def add(listName: String, pivot: String, value: String, after: Option[Boolean]): Option[Int] = {
+    db.get(listName).map { l =>
+      val index = after match {
+        case Some(true) => l.indexOf(pivot) + 1
+        case _          => l.indexOf(pivot)
+      }
+      val newList = insertAtPosition(l, index, value)
+      db += (listName -> newList)
+      l.length
+    } orElse {
+      db += (listName -> List(value))
+      Option(1)
+    }
+  }
+
+  def removeItem(listName: String, value: String): Option[Long] = {
+    db.get(listName).map { l =>
+      val newList = l.filterNot(_ == value)
+      db += (listName -> newList)
+      newList.length
+    }
+  }
+
+  def removeList(listName: String): Option[scala.collection.mutable.Map[String, List[String]]] = {
+    Option(db -= listName)
+  }
+
+  def push(listName: String, value: String) = {
+    db.get(listName).map { l =>
+      db += (listName -> (l :+ value))
+      Some(l.length)
+    } getOrElse {
+      db += (listName -> List(value))
+      Some(1L)
+    }
+  }
+
+}
+
+object FrontsApi extends FrontsApiMap
