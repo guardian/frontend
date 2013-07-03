ed7f016a2eb04091e5e042a2f110cb2032984c69
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2013-07-03 13:18:33 +0100

    Treemaps for Operating System and Browser statistics.

--- admin/app/tools/charts/charts.scala
+++ admin/app/tools/charts/charts.scala
@@ -67,20 +67,18 @@ object PageviewsGraph extends Chart {
   val name = "Pageviews"
   lazy val labels = Seq("Date", "pageviews")
 
-  def dataset = Pageviews() map { d => DataPoint(
-    d.date.toString("dd/MM"),
-    Seq(d.total)
-  )}
+  def dataset = Analytics.pageviews() map {
+    case (date, total) => DataPoint(date.toString("dd/MM"), Seq(total))
+  }
 }
 
 object NewPageviewsGraph extends Chart {
   val name = "Pageviews (new users)"
   lazy val labels = Seq("Date", "pageviews")
 
-  def dataset = Pageviews.newCookies() map { d => DataPoint(
-    d.date.toString("dd/MM"),
-    Seq(d.total)
-  )}
+  def dataset = Analytics.newCookies() map {
+    case (date, total) => DataPoint(date.toString("dd/MM"), Seq(total))
+  }
 }
 
 object PageviewsGeoGraph extends Chart {
@@ -89,5 +87,49 @@ object PageviewsGeoGraph extends Chart {
 
   override lazy val form: String = "GeoChart"
 
-  def dataset = Pageviews.countries() map { d => DataPoint(d.country, Seq(d.total) )}
+  def dataset = Analytics.countries() map {
+    case (country, total) => DataPoint(country, Seq(total))
+  }
+}
+
+object PageviewsOSTreeMapGraph extends Chart {
+  val name = ""
+
+  override lazy val form: String = "TreeMap"
+
+  lazy val labels = Seq("OS", "pageviews")
+  def dataset = Analytics.operatingSystems() map {
+    case (os, total) => DataPoint(os, Seq(total))
+  }
+
+  override def asDataset = s"[[$labelString], [$rootElement], $dataString]"
+  private def labelString = "'OS','parent','pageviews'"
+  private def rootElement = "'Operating System', null, 0"
+
+  private def dataString = dataset.map(datapointString).mkString(",")
+  private def datapointString(point: DataPoint) = {
+    val data = point.values.mkString(",")
+    s"['${point.name}', 'Operating System', $data]"
+  }
+}
+
+object PageviewsBrowsersTreeMapGraph extends Chart {
+  val name = ""
+
+  override lazy val form: String = "TreeMap"
+
+  lazy val labels = Seq("Browsers", "pageviews")
+  def dataset = Analytics.browsers() map {
+    case (browser, total) => DataPoint(browser, Seq(total))
+  }
+
+  override def asDataset = s"[[$labelString], [$rootElement], $dataString]"
+  private def labelString = "'Browser','parent','pageviews'"
+  private def rootElement = "'Browser', null, 0"
+
+  private def dataString = dataset.map(datapointString).mkString(",")
+  private def datapointString(point: DataPoint) = {
+    val data = point.values.mkString(",")
+    s"['${point.name}', 'Browser', $data]"
+  }
 }
