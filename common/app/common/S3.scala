7cead6315daea730f760c63fa2f2b40d42ff78e5
grant <grant.klopper@guardian.co.uk>
2013-07-11 17:55:59 +0100

    Not everything is private...

--- common/app/common/S3.scala
+++ common/app/common/S3.scala
@@ -3,7 +3,7 @@ package common
 import conf.Configuration
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model._
-import com.amazonaws.services.s3.model.CannedAccessControlList.Private
+import com.amazonaws.services.s3.model.CannedAccessControlList.{Private, PublicRead}
 import com.amazonaws.util.StringInputStream
 import scala.io.Source
 
@@ -31,12 +31,20 @@ trait S3 extends Logging {
     client.shutdown()
   }
 
-  def put(key: String, value: String, contentType: String) {
+  def putPublic(key: String, value: String, contentType: String) {
+    put(key: String, value: String, contentType: String, PublicRead)
+  }
+
+  def putPrivate(key: String, value: String, contentType: String) {
+    put(key: String, value: String, contentType: String, Private)
+  }
+
+  private def put(key: String, value: String, contentType: String, accessControlList: CannedAccessControlList) {
     val metadata = new ObjectMetadata()
     metadata.setCacheControl("no-cache,no-store")
     metadata.setContentType(contentType)
 
-    val request = new PutObjectRequest(bucket, key, new StringInputStream(value), metadata).withCannedAcl(Private)
+    val request = new PutObjectRequest(bucket, key, new StringInputStream(value), metadata).withCannedAcl(accessControlList)
 
     client.putObject(request)
     client.shutdown()
