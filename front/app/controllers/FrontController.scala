e0a5b29431efe0cac3116c04e15b0e1d849a6210
grant <grant.klopper@guardian.co.uk>
2013-07-11 09:45:36 +0100

    Fixed analytics names of front pages

--- front/app/controllers/FrontController.scala
+++ front/app/controllers/FrontController.scala
@@ -43,6 +43,48 @@ object FrontPage {
     },
 
     new MetaData {
+      override val canonicalUrl = Some("http://www.guardian.co.uk/money")
+      override val id = "money"
+      override val section = "money"
+      override val webTitle = "Money"
+      override lazy val analyticsName = "GFE:money"
+
+      override lazy val metaData: Map[String, Any] = super.metaData ++ Map(
+        "keywords" -> "Money",
+        "content-type" -> "Section",
+        "is-front" -> true
+      )
+    },
+
+    new MetaData {
+      override val canonicalUrl = Some("http://www.guardian.co.uk/commentisfree")
+      override val id = "commentisfree"
+      override val section = "commentisfree"
+      override val webTitle = "commentisfree"
+      override lazy val analyticsName = "GFE:commentisfree"
+
+      override lazy val metaData: Map[String, Any] = super.metaData ++ Map(
+        "keywords" -> "Comment is free",
+        "content-type" -> "Section",
+        "is-front" -> true
+      )
+    },
+
+    new MetaData {
+      override val canonicalUrl = Some("http://www.guardian.co.uk/business")
+      override val id = "business"
+      override val section = "business"
+      override val webTitle = "business"
+      override lazy val analyticsName = "GFE:business"
+
+      override lazy val metaData: Map[String, Any] = super.metaData ++ Map(
+        "keywords" -> "Business",
+        "content-type" -> "Section",
+        "is-front" -> true
+      )
+    },
+
+    new MetaData {
       override val canonicalUrl = Some("http://www.guardian.co.uk/culture")
       override val id = "culture"
       override val section = "culture"
@@ -98,7 +140,11 @@ class FrontController extends Controller with Logging with JsonTrails with Execu
     // go live
     val realPath = editionPath(path, Edition(request))
 
-    FrontPage(realPath).map { frontPage =>
+    // TODO - needed till after www.theguardian.com
+    val pageId = realPath.drop(3)  //removes the edition
+
+
+    FrontPage(pageId).map { frontPage =>
 
       // get the trailblocks
       val trailblocks: Seq[Trailblock] = front(realPath).filterNot { trailblock =>
