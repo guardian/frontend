bfa7dd8998c1493a6fa4d7e8f80b5d81a1e2f89b
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-07-24 09:24:53 +0100

    Pingdom status on the radiator (sans account configuration)

--- admin/app/controllers/RadiatorController.scala
+++ admin/app/controllers/RadiatorController.scala
@@ -5,8 +5,13 @@ import common.Logging
 import tools.{PageviewsByDayGraph, CloudWatch}
 import model.MetaData
 import conf.Switches
+import play.api.libs.ws.WS
+import com.ning.http.client.Realm
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.Json
 
 object RadiatorController extends Controller with Logging with AuthLogging {
+
   def render() = AuthAction{ implicit request =>
       val metadata = new MetaData {
         def id: String = ""
@@ -21,4 +26,24 @@ object RadiatorController extends Controller with Logging with AuthLogging {
 
       Ok(views.html.radiator(graphs, metadata, Switches.all))
   }
+
+  def pingdom() = AuthAction{ implicit request =>
+  
+    val url = "https://api.pingdom.com/api/2.0/checks"
+    val user = ""
+    val password = ""
+    val apiKey = ""
+
+    Async {
+          WS.url(url)
+            .withAuth(user, password,  Realm.AuthScheme.BASIC)
+            .withHeaders("App-Key" ->  apiKey)
+            .get().map { response =>
+              Ok(Json.toJson(response.body))
+            }
+      }
+  }
+
 }
+
+
