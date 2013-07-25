7bf2ed011de62eb4335e9e5d9dbd2c9fd9b294ab
David Vella <david.vella@guardian.co.uk>
2013-07-25 15:58:54 +0100

    Adding lightbox galleries AB test

---
+++ common/app/assets/javascripts/modules/experiments/tests/lightbox-galleries.js
@@ -0,0 +1,47 @@
+define(['common', 'modules/lightbox-gallery'], function (common, LightboxGallery) {
+
+    var _config;
+
+    var LightboxGalleries = function () {
+
+        this.id = 'LightboxGalleries';
+        this.expiry = '2013-08-08';
+        this.audience = 1;
+        this.description = 'Tests the lightbox gallery variants between no lightbox, lightbox and lightbox with swipe';
+        this.canRun = function(config) {
+            _config = config;
+            return true;
+        };
+        this.variants = [
+            {
+                id: 'control',
+                test: function () {
+                   return true;
+                }
+            },
+            {
+                id: 'lightbox',
+                test: function (context) {
+                    var galleries = new LightboxGallery(_config, context);
+                    galleries.init({
+                        overrideSwitch: true,
+                        disableSwipe: true
+                    });
+                }
+            },
+            {
+                id: 'lightbox-swipe',
+                test: function (context) {
+                    var galleries = new LightboxGallery(_config, context);
+                    galleries.init({
+                        overrideSwitch: true
+                    });
+                }
+            }
+
+        ];
+    };
+    
+    return LightboxGalleries;
+
+});
