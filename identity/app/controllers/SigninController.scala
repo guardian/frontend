615f98fc4c76c5ae3eb55dc7603d49ca7a1a537c
James Gorrie <james.gorrie@guardian.co.uk>
2013-07-25 15:12:47 +0100

    Removed forgotten password link

--- identity/app/controllers/SigninController.scala
+++ identity/app/controllers/SigninController.scala
@@ -11,14 +11,10 @@ import model._
 import conf._
 
 object SigninController extends Controller {
-  def renderForm = Action { implicit request =>
-    val page = Page(canonicalUrl = None, "style-guide", "style-guide", "Style guide: home", "GFE:Style-guide")
+  def signin = Action { implicit request =>
+    val page = Page(canonicalUrl = None, "identity", "identity", "Identity - Sign in", "")
     Cached(60) {
       Ok(views.html.signin(page))
     }
-
-    // SimpleResult(
-    //   header = ResponseHeader(200, Map("Cache-Control" -> "max-age=60")),
-    //   body = Enumerator("Ok - %s".format(request.cookies.get("GU_U").map("GU_U=" + _.value).getOrElse("Not logged in"))))
   }
 }
