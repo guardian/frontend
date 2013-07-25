fe52c711a6554c84f166e72a5425ed38b03f3fc6
James Gorrie <james.gorrie@guardian.co.uk>
2013-07-25 11:15:47 +0100

    Basic form added

--- identity/app/controllers/SigninController.scala
+++ identity/app/controllers/SigninController.scala
@@ -6,10 +6,19 @@ import play.api.mvc.ResponseHeader
 import play.api.mvc.SimpleResult
 import play.api.libs.iteratee.Enumerator
 
+import common._
+import model._
+import conf._
+
 object SigninController extends Controller {
-  def renderForm = Action { request =>
-    SimpleResult(
-      header = ResponseHeader(200, Map("Cache-Control" -> "max-age=60")),
-      body = Enumerator("Ok - %s".format(request.cookies.get("GU_U").map("GU_U=" + _.value).getOrElse("Not logged in"))))
+  def renderForm = Action { implicit request =>
+    val page = Page(canonicalUrl = None, "style-guide", "style-guide", "Style guide: home", "GFE:Style-guide")
+    Cached(60) {
+      Ok(views.html.signin(page))
+    }
+
+    // SimpleResult(
+    //   header = ResponseHeader(200, Map("Cache-Control" -> "max-age=60")),
+    //   body = Enumerator("Ok - %s".format(request.cookies.get("GU_U").map("GU_U=" + _.value).getOrElse("Not logged in"))))
   }
 }
