231b0496dee9ee69e39abfca6580d906a7d0d61b
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2013-07-26 18:11:13 +0100

    Added basic fragment for the identity topnav

---
+++ identity/app/controllers/TopBarFragments.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import com.google.inject.Inject
+import com.gu.identity.cookie.{GuUDecoder, GuUCookieData}
+import conf.FrontendIdentityCookieDecoder
+import play.api.mvc._
+import common.{Logging, ExecutionContexts}
+
+class TopBarFragments @Inject()(cookieDecoder: FrontendIdentityCookieDecoder) extends Controller with Logging {
+  def signin = Action { implicit request =>
+    request.cookies.get("GU_U")
+      .flatMap(cookie => cookieDecoder.getUserDataForGuU(cookie.value))
+      .map(cookieData => Ok(views.html.topbar.signedin(cookieData)))
+      .getOrElse(Ok(views.html.topbar.notsignedin()))
+
+  }
+
+}
