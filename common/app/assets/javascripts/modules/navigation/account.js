152d065e1c9b82b3aa237f3533971f50c935899a
James Gorrie <james.gorrie@guardian.co.uk>
2013-07-26 14:33:51 +0100

    updated account control spec to include rendering

--- common/app/assets/javascripts/modules/navigation/account.js
+++ common/app/assets/javascripts/modules/navigation/account.js
@@ -5,24 +5,22 @@ define(['common', 'ajax', 'bonzo'], function (common, ajax, bonzo) {
     }
 
     /**
-     * @enum {string} 
+     * @enum {string}
      */
     Account.CONFIG = {
         contentUrl: '/identity/fragments/account',
-        className: 'control--account'
+        className: 'js-account-info',
+        eventName: 'modules:accountcontrol'
     }
 
-    /**
-     *
-     */
-    Account.prototype.context;
+    /** @type {Element|null} */
+    Account.prototype.context = null;
 
     /**
-     * @param {Function} callback
      * @return {Element}
      */
-    Account.prototype.getAccountFragment = function(callback) {
-        ajax({
+    Account.prototype.getAccountFragment = function() {
+        return ajax({
             url: Account.CONFIG.contentUrl,
             type: 'json'
         }).then(this.emitLoadedEvent, this.emitErrorEvent);
@@ -31,20 +29,32 @@ define(['common', 'ajax', 'bonzo'], function (common, ajax, bonzo) {
     /**
      * @param {Element} context
      */
-    Account.prototype.render = function(context) {
-        this.getAccountFragment(function(resp) {
-            console.log(resp)
+    Account.prototype.render = function(context, callback) {
+        var self = this;
+        common.mediator.on(Account.CONFIG.eventName + ':loaded', function(resp) {
+            self.renderControl(resp);
         });
-    }
-
-    /**  */
-    Account.prototype.emitLoadedEvent = function() {
-        common.mediator.emit('modules:accountcontrol:loaded');
+        this.getAccountFragment();
     }
 
     /** */
-    Account.prototype.emitErrorEvent = function() {
-        common.mediator.emit('modules:accountcontrol:error');
+    Account.prototype.renderControl = function(resp) {
+        var content = resp.html,
+            container = this.context.querySelector('.' + Account.CONFIG.className);
+
+        bonzo(container).html(content);
+        
+        common.mediator.emit(Account.CONFIG.eventName + ':rendered', resp);
+    }
+
+    /**  */
+    Account.prototype.emitLoadedEvent = function(resp) {
+        common.mediator.emit(Account.CONFIG.eventName + ':loaded', resp);
+    }
+
+    /** */
+    Account.prototype.emitErrorEvent = function(resp) {
+        common.mediator.emit(Account.CONFIG.eventName + ':error', resp);
     }
 
     return Account;
--- common/test/assets/javascripts/spec/AccountControl.spec.js
+++ common/test/assets/javascripts/spec/AccountControl.spec.js
@@ -1,27 +1,32 @@
 define(['common', 'fixtures', 'ajax', 'modules/navigation/account'], function(common, fixtures, ajax, Account) {
 
     describe('Account Control', function() {
-        var control = new Account(),
+        var context,
+            control = new Account(),
             server = sinon.fakeServer.create(),
-            containerId = 'account-control',
+            containerId = 'account-control-container',
             controlFixtures = {
                 id: containerId,
                 fixtures: [
-                    '<div id="account-control-container"></div>'
+                    '<div class="' + Account.CONFIG.className + '"></div>'
                 ]
             };
 
-        // setup server & ajax
-        server.autoRespond = true;
         ajax.init({page: {
             ajaxUrl: '',
             edition: 'UK'
         }});
 
         fixtures.render(controlFixtures);
+        context = document.getElementById(containerId);
 
         describe('Fetching the HTML fragment', function() {
             control = new Account();
+
+            beforeEach(function() {
+                server = sinon.fakeServer.create();
+                server.autoRespond = true;
+            });
 
             afterEach(function() {
                 server.restore();
@@ -32,7 +37,7 @@ define(['common', 'fixtures', 'ajax', 'modules/navigation/account'], function(co
                 common.mediator.on('modules:accountcontrol:loaded', callback);
 
                 server.respondWith([200, {}, '{ "html": "<i>Account fragment</i>" }']);
-                control.getAccountFragment(callback);
+                control.getAccountFragment();
 
                 waitsFor(function() {
                     return callback.calledOnce === true;
@@ -44,27 +49,43 @@ define(['common', 'fixtures', 'ajax', 'modules/navigation/account'], function(co
                 common.mediator.on('modules:accountcontrol:error', callback);
 
                 server.respondWith([500, {}, '{ "error": "Server did a bad bad thing" }']);
-                control.getAccountFragment(callback);
+                control.getAccountFragment();
 
                 waitsFor(function() {
                     return callback.calledOnce === true;
-                }, 'Account control to fetch HTML fragment', 500);
+                }, 'Account control to throw error', 500);
             });
 
         });
 
         describe('Rendering the HTML fragment', function() {
-            control = new Account();
+            control = new Account(context);
+            
+            beforeEach(function() {
+                server = sinon.fakeServer.create();
+                server.autoRespond = true;
+            });
 
             afterEach(function() {
                 server.restore();
             });
 
-            it('Renders the content fetched from ID', function() {
-                server.respondWith([200, {}, '{ "html": "<i>Account fragment</i>" }']);
-                
-                var context = document.getElementById(containerId);
-                control.render(context);
+            it('Renders the content fetched from ID fragment', function() {
+                var content = '<i>auserwithmassintent</i>',
+                    callback = sinon.stub();
+
+                server.respondWith([200, {}, '{ "html": "' + content + '" }']);
+
+                common.mediator.on('modules:accountcontrol:rendered', callback);
+                control.render();
+
+                waitsFor(function() {
+                    return callback.calledOnce === true;
+                }, 'Render to fetch content', 500);
+
+                runs(function() {
+                     expect(context.querySelector('.' + Account.CONFIG.className).innerHTML).toEqual(content);
+                });
             });
 
         });
