0c6dd73178e614c8f97d531338a38d2d3477a83e
David Vella <dave@deepcobalt.com>
2013-07-26 13:29:44 +0100

    JShint fix

--- common/app/assets/javascripts/modules/overlay.js
+++ common/app/assets/javascripts/modules/overlay.js
@@ -64,7 +64,7 @@ define(["bean",
         // Restore previous scroll pos
         // Due to how swipe sets the height of the content at set intervals,
         // we need to wait long enough for that to happen before restoring scroll position
-        var scrollDelay = (document.body.className.indexOf('has-swipe') != -1) ? 1010 : 0;
+        var scrollDelay = (document.body.className.indexOf('has-swipe') !== -1) ? 1010 : 0;
         setTimeout(function() {
             window.scrollTo(window.pageXOffset, self._savedPos);
         }, scrollDelay);
