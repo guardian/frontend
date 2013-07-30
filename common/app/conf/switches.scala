f8fe976c8ff8e0fad9b8c4026b7298586f925bc7
James Gorrie <james.gorrie@guardian.co.uk>
2013-07-30 11:38:46 +0100

    Identity switch turned to safeMode off

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -161,7 +161,7 @@ object Switches extends Collections {
 
   val IdentityProfileNavigationSwitch = Switch("Feature Switches", "id-profile-navigation",
     "ShowsIf this switch is on you will see the link in the topbar taking you through to the users profile or sign in..",
-    safeState = On)
+    safeState = Off)
 
 
   // A/B Test Switches
