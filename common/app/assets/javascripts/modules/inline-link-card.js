e9e364e2e4ac16c4ea024d95f67f6dfe95687ddc
Kaelig <kaelig@deloumeau.fr>
2013-08-05 12:50:25 +0100

    Load the content properly

--- common/app/assets/javascripts/modules/inline-link-card.js
+++ common/app/assets/javascripts/modules/inline-link-card.js
@@ -4,38 +4,59 @@
 */
 define([
     'common',
+    'modules/detect',
     'ajax',
     'bean',
     'bonzo'
 ], function (
     common,
+    detect,
     ajax,
     bean,
     bonzo
 ) {
 
-    function InlineLinkCard(link, options) {
-        var opts = options || {};
-
-        // initialise
-        this.init = function() {
-            var href = link.getAttribute('href');
-            // make request to endpoint
-            ajax({
-                url: href + '.json',
-                type: 'json',
-                crossOrigin: true
-            }).then(
-                function(resp) {
-                    // console.log(resp);
-                    // console.log(bonzo(bonzo.create(resp.html))[1]);
-
-                    // bonzo($p.previous()).before(linkToCardify.parent());
-                }, function(req) { }
-            );
-
-        };
+    function InlineLinkCard(link) {
+        this.link = link;
+        this.hasLoadedCard = false;
     }
+
+    InlineLinkCard.prototype.init = function() {
+        var self = this;
+
+        self.loadCard();
+
+        bean.on(window, 'resize', common.debounce(function(e){
+            self.loadCard();
+        }, 200));
+    };
+
+    InlineLinkCard.prototype.loadCard = function() {
+        var layoutMode = detect.getLayoutMode();
+        if(layoutMode.match('desktop|extended') && !this.hasLoadedCard) {
+            this.fetchData();
+        }
+    };
+
+    InlineLinkCard.prototype.fetchData = function() {
+        var href = this.link.getAttribute('href'),
+            self = this;
+
+        // make request to endpoint
+        ajax({
+            url: href + '.json',
+            type: 'json',
+            crossOrigin: true
+        }).then(
+            function(resp) {
+                self.hasLoadedCard = true;
+                // console.log(resp);
+                // console.log(bonzo(bonzo.create(resp.html))[1]);
+
+                // bonzo($p.previous()).before(linkToCardify.parent());
+            }, function(req) { }
+        );
+    };
 
     return InlineLinkCard;
 
