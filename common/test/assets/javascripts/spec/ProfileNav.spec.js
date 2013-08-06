52ee82d558f87ac413b1b8cd7d84f55cd1fb539c
James Gorrie <james.gorrie@guardian.co.uk>
2013-08-06 09:30:29 +0100

    Getting profile nav tests to pass

--- common/test/assets/javascripts/spec/ProfileNav.spec.js
+++ common/test/assets/javascripts/spec/ProfileNav.spec.js
@@ -1,3 +1,9 @@
 define(['common', 'fixtures', 'ajax', 'modules/navigation/profile'], function(common, fixtures, ajax, Profile) {
-    // these tests have been rewritten in another branch
+    
+    describe('Profile nav', function() {
+        it('passes as tests are written in signout-component', function() {
+            expect(true);
+        });
+    });
+
 });
