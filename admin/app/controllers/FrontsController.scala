0a100173a83d3376702b7ab1c07f2db1c3b8032d
Francis Carr <francis.carr@guardian.co.uk>
2013-08-06 12:06:41 +0100

    Refactor WIP

--- admin/app/controllers/FrontsController.scala
+++ admin/app/controllers/FrontsController.scala
@@ -13,10 +13,6 @@ import tools.FrontsApi
 
 
 object FrontsController extends Controller with Logging {
-  implicit val updateListRead = Json.reads[UpdateList]
-  implicit val blockActionRead = Json.reads[BlockAction]
-  implicit val updateMetaRead = Json.reads[UpdateTrailblockConfig]
-  implicit val updateTrailblockRead = Json.reads[UpdateTrailblock]
 
   def index() = AuthAction{ request =>
     Ok(views.html.fronts(Configuration.environment.stage))
@@ -28,84 +24,50 @@ object FrontsController extends Controller with Logging {
     } getOrElse NotFound
   }
 
-
   def readBlock(edition: String, section: String, blockId: String) = AuthAction{ request =>
     S3FrontsApi.getBlock(edition, section, blockId) map { json =>
       Ok(json).as("application/json")
     } getOrElse NotFound
   }
 
-  def updateBlock(edition: String, section: String, blockId: String): Action[AnyContent] = AuthAction{ request =>
-    request.body.asJson.map { json =>
-        json.asOpt[UpdateList].map { update: UpdateList =>
-            if (update.item == update.position.getOrElse(""))
-              Conflict
-            else {
-              val identity = Identity(request).get
-              FrontsApi.getBlock(edition, section, blockId).map { block =>
-                  updateBlock(edition, section, blockId, update, identity, block)
-                  FrontsApi.archive(edition, section, block)
-                  Ok
-              } getOrElse {
-                createBlock(edition, section, blockId, identity, update)
-                Created
-              }
-            }
-        } orElse json.asOpt[UpdateTrailblock]
-          .map {update =>
-            FrontsApi.getBlock(edition, section, blockId).map { block =>
-              val newBlock = block.copy(
-                contentApiQuery = update.config.contentApiQuery orElse None,
-                //These defaults will move somewhere better during refactor
-                min = update.config.min orElse Some(0),
-                max = update.config.max orElse Some(20)
-              )
-              FrontsApi.putBlock(edition, section, newBlock)
-              Ok
-            } getOrElse NotFound
-        } orElse json.asOpt[BlockAction]
-          .map {blockAction => blockAction
-          blockAction.publish.filter {_ == true}
-            .map { _ => FrontsApi.publishBlock(edition, section, blockId) }
-            .orElse {
-            blockAction.discard.filter {_ == true}.map(_ => FrontsApi.discardBlock(edition, section, blockId))
-          } getOrElse NotFound("Invalid JSON")
+  def updateBlock(edition: String, section: String, blockId: String): Action[AnyContent] = AuthAction { request =>
+    request.body.asJson flatMap JsonExtract.build map {
+      case update: UpdateList if update.item == update.position.getOrElse("") => Conflict
+      case update: UpdateList => {
+        val identity = Identity(request).get
+        FrontsApi.getBlock(edition, section, blockId).map { block =>
+          UpdateActions.updateBlock(edition, section, blockId, update, identity, block)
+          FrontsApi.archive(edition, section, block)
           Ok
+        } getOrElse {
+          UpdateActions.createBlock(edition, section, blockId, identity, update)
+          Created
+        }
+      }
+      case blockAction: BlockActionJson => {
+        blockAction.publish.filter {_ == true}
+          .map { _ => FrontsApi.publishBlock(edition, section, blockId) }
+          .orElse {
+          blockAction.discard.filter {_ == true}.map(_ => FrontsApi.discardBlock(edition, section, blockId))
         } getOrElse NotFound("Invalid JSON")
-    } getOrElse NotFound("Problem parsing json")
+        Ok
+      }
+      case updateTrailblock: UpdateTrailblockJson => {
+        FrontsApi.getBlock(edition, section, blockId).map { block =>
+          val newBlock = block.copy(
+            contentApiQuery = updateTrailblock.config.contentApiQuery orElse None,
+            //These defaults will move somewhere better during refactor
+            min = updateTrailblock.config.min orElse Some(0),
+            max = updateTrailblock.config.max orElse Some(20)
+          )
+          FrontsApi.putBlock(edition, section, newBlock)
+          Ok
+        } getOrElse NotFound
+      }
+      case _ => NotFound
+    } getOrElse NotFound
   }
 
-  private def updateBlock(edition: String, section: String, blockId: String, update: UpdateList, identity: Identity, block: Block): Unit = {
-    var newBlock: Block = block.copy(lastUpdated = DateTime.now.toString, updatedBy = identity.fullName, updatedEmail = identity.email)
-    if (update.draft) {
-      val trails = updateList(update, block.draft)
-      newBlock = newBlock.copy(draft = trails)
-    }
-    if (update.live) {
-      val trails = updateList(update, block.live)
-      newBlock = newBlock.copy(live = trails)
-    }
-
-    newBlock = newBlock.copy(areEqual = newBlock.live==newBlock.draft)
-
-    FrontsApi.putBlock(edition, section, blockId, newBlock)
-  }
-
-  private def updateList(update: UpdateList, blocks: List[Trail]): List[Trail] = {
-    val listWithoutItem = blocks.filterNot(_.id == update.item)
-    val index = update.after.filter {_ == true}
-      .map {_ => listWithoutItem.indexWhere(_.id == update.position.getOrElse("")) + 1}
-      .getOrElse { listWithoutItem.indexWhere(_.id == update.position.getOrElse("")) }
-    val splitList = listWithoutItem.splitAt(index)
-    splitList._1 ++ List(Trail(update.item, None, None, None)) ++ splitList._2
-  }
-
-  private def createBlock(edition: String, section: String, block: String, identity: Identity, update: UpdateList) {
-      FrontsApi.putBlock(edition, section, block, Block(block, None, List(Trail(update.item, None, None, None)), List(Trail(update.item, None, None, None)), areEqual=true, DateTime.now.toString, identity.fullName, identity.email, None, None, None))
-  }
-  /**
-   * @todo
-   */
   def updateTrail(edition: String, section: String, blockId: String, trailId: String) = AuthAction{ request =>
     request.body.asJson.map{ json =>
     }
@@ -113,25 +75,13 @@ object FrontsController extends Controller with Logging {
   }
 
   def deleteTrail(edition: String, section: String, blockId: String) = AuthAction { request =>
-    request.body.asJson.map { json =>
-      json.asOpt[UpdateList].map { update: UpdateList =>
-        FrontsApi.getBlock(edition, section, blockId) map { block: Block =>
-          var newBlock = block.copy()
-          if (update.draft) {
-            val trails = block.draft.filterNot(_.id == update.item)
-            newBlock = newBlock.copy(draft = trails)
-          }
-          if (update.live) {
-            val trails = block.live.filterNot(_.id == update.item)
-            newBlock = newBlock.copy(live = trails)
-          }
-          newBlock = newBlock.copy(areEqual = newBlock.live==newBlock.draft)
-
-          FrontsApi.putBlock(edition, section, block.id, newBlock)
-          Ok
-        } getOrElse NotFound("No edition or section") //To be more silent in the future?
-      } getOrElse NotFound("Invalid JSON")
-    } getOrElse NotFound("Problem parsing json")
+    request.body.asJson flatMap JsonExtract.build map {
+      case update: UpdateList => {
+        UpdateActions.updateActionFilter(edition, section, blockId, update)
+        Ok
+      }
+      case _ => NotFound
+    } getOrElse NotFound
   }
 
 }
--- admin/app/model/frontsapi.scala
+++ admin/app/model/frontsapi.scala
@@ -1,6 +1,12 @@
 package frontsapi.model
 
-case class Block(
+import play.api.libs.json.{Json, JsValue}
+import play.api.mvc.Results.Status
+import tools.FrontsApi
+import controllers.Identity
+import org.joda.time.DateTime
+
+sealed case class Block(
                   id: String,
                   name: Option[String],
                   live: List[Trail],
@@ -14,15 +20,104 @@ case class Block(
                   contentApiQuery: Option[String]
                   )
 
-case class Trail(
+sealed case class Trail(
                   id: String,
                   title: Option[String],
                   trailImage: Option[String],
                   linkText: Option[String]
                   )
 
-case class UpdateList(item: String, position: Option[String], after: Option[Boolean], live: Boolean, draft: Boolean)
-case class BlockAction(publish: Option[Boolean], discard: Option[Boolean])
 
-case class UpdateTrailblock(config: UpdateTrailblockConfig)
-case class UpdateTrailblockConfig(contentApiQuery: Option[String], max: Option[Int], min: Option[Int])
+case class BlockActionJson(publish: Option[Boolean], discard: Option[Boolean])
+
+trait Update
+case class Publish(id: String)
+case class Discard(id: String)
+
+case class UpdateTrailblockJson(config: UpdateTrailblockConfigJson)
+case class UpdateTrailblockConfigJson(contentApiQuery: Option[String], max: Option[Int], min: Option[Int])
+
+trait DeleteAction
+
+case class UpdateList(item: String, position: Option[String], after: Option[Boolean], live: Boolean, draft: Boolean) {
+
+  def updateList(update: UpdateList, blocks: List[Trail]): List[Trail] = {
+    val listWithoutItem = blocks.filterNot(_.id == update.item)
+    val index = update.after.filter {_ == true}
+      .map {_ => listWithoutItem.indexWhere(_.id == update.position.getOrElse("")) + 1}
+      .getOrElse { listWithoutItem.indexWhere(_.id == update.position.getOrElse("")) }
+    val splitList = listWithoutItem.splitAt(index)
+    splitList._1 ++ List(Trail(update.item, None, None, None)) ++ splitList._2
+  }
+
+}
+
+object JsonExtract {
+  implicit val updateListRead = Json.reads[UpdateList]
+  implicit val trailActionRead = Json.reads[Trail]
+  implicit val blockActionRead = Json.reads[Block]
+  implicit val blockActionJsonRead = Json.reads[BlockActionJson]
+  implicit val updateMetaRead = Json.reads[UpdateTrailblockConfigJson]
+  implicit val updateTrailblockRead = Json.reads[UpdateTrailblockJson]
+
+  private def extractJson(v: JsValue): Either[String, Any] =
+    v.asOpt[Block]
+      .orElse{v.asOpt[UpdateList]}
+      .orElse{v.asOpt[UpdateTrailblockJson]}
+      .orElse{v.asOpt[BlockActionJson]}
+      .toRight("Invalid Json")
+
+  def build(v: JsValue) = extractJson(v).right.toOption
+}
+
+trait UpdateActions {
+
+  def updateActionFilter(edition: String, section: String, blockId: String, update: UpdateList): Either[String, String] =
+    FrontsApi.getBlock(edition, section, blockId) map { block: Block =>
+      var newBlock = block.copy()
+      if (update.draft) {
+        val trails = block.draft.filterNot(_.id == update.item)
+        newBlock = newBlock.copy(draft = trails)
+      }
+      if (update.live) {
+        val trails = block.live.filterNot(_.id == update.item)
+        newBlock = newBlock.copy(live = trails)
+      }
+      newBlock = newBlock.copy(areEqual = newBlock.live==newBlock.draft)
+
+      FrontsApi.putBlock(edition, section, block.id, newBlock)
+      Right("OK")
+    } getOrElse Left("No edition or section") //To be more silent in the future?
+
+
+  private def updateList(update: UpdateList, blocks: List[Trail]): List[Trail] = {
+    val listWithoutItem = blocks.filterNot(_.id == update.item)
+    val index = update.after.filter {_ == true}
+      .map {_ => listWithoutItem.indexWhere(_.id == update.position.getOrElse("")) + 1}
+      .getOrElse { listWithoutItem.indexWhere(_.id == update.position.getOrElse("")) }
+    val splitList = listWithoutItem.splitAt(index)
+    splitList._1 ++ List(Trail(update.item, None, None, None)) ++ splitList._2
+  }
+
+  def updateBlock(edition: String, section: String, blockId: String, update: UpdateList, identity: Identity, block: Block): Unit = {
+    var newBlock: Block = block.copy(lastUpdated = DateTime.now.toString, updatedBy = identity.fullName, updatedEmail = identity.email)
+    if (update.draft) {
+      val trails = updateList(update, block.draft)
+      newBlock = newBlock.copy(draft = trails)
+    }
+    if (update.live) {
+      val trails = updateList(update, block.live)
+      newBlock = newBlock.copy(live = trails)
+    }
+
+    newBlock = newBlock.copy(areEqual = newBlock.live==newBlock.draft)
+
+    FrontsApi.putBlock(edition, section, blockId, newBlock)
+  }
+
+  def createBlock(edition: String, section: String, block: String, identity: Identity, update: UpdateList) {
+    FrontsApi.putBlock(edition, section, block, Block(block, None, List(Trail(update.item, None, None, None)), List(Trail(update.item, None, None, None)), areEqual = true, DateTime.now.toString, identity.fullName, identity.email, None, None, None))
+  }
+}
+
+object UpdateActions extends UpdateActions
