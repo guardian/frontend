47be6c2fd57e203c9f40e1b5109bc89df92d30b0
David Vella <david.vella@guardian.co.uk>
2013-08-08 14:48:45 +0100

    Initialise lightbox when in variant

--- common/app/assets/javascripts/modules/experiments/tests/gallery-style.js
+++ common/app/assets/javascripts/modules/experiments/tests/gallery-style.js
@@ -1,26 +1,34 @@
-define(['common'], function (common) {
+define(['common', 'modules/lightbox-gallery'], function (common, LightboxGallery) {
 
     var GalleryStyle = function () {
+
+        var _config;
 
         this.id = 'GalleryStyle';
         this.expiry = '2013-08-20';
         this.audience = 1;
         this.description = 'Tests the new gallery style contact sheet CTA instead of the current style';
         this.canRun = function(config) {
+            _config = config;
             return config.page.contentType === 'Gallery';
         };
         this.variants = [
             {
                 id: 'control',
                 test: function (context) {
-                   context.querySelector('.gallery--var-b').style.display = 'none';
+                   context.querySelector('.gallerytest--var-b').style.display = 'none';
                    return true;
                 }
             },
             {
                 id: 'new-style',
                 test: function (context) {
-                    context.querySelector('.gallery--var-a').style.display = 'none';
+                    context.querySelector('.gallerytest--var-a').style.display = 'none';
+
+                    var galleries = new LightboxGallery(_config, context);
+                    galleries.init({
+                        overrideSwitch: true
+                    });
                 }
             }
         ];
