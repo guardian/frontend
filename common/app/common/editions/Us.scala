3cc95e1156eb53678444bb74d4d76028d3ab6bf7
Francis Carr <francis.carr@guardian.co.uk>
2013-08-08 16:44:56 +0100

    Update US network front to reflect what is on mobile prod front

--- common/app/common/editions/Us.scala
+++ common/app/common/editions/Us.scala
@@ -103,9 +103,14 @@ object Us extends Edition(
 
   val configuredFrontsFacia = Map(
     (Editionalise("", Us), Seq(
-      RunningOrderTrailblockDescription("news", "us/news/top-stories", "Top Stories", 5),
-      RunningOrderTrailblockDescription("news", "us/news/features", "Features", 5),
-      RunningOrderTrailblockDescription("news", "us/news/editors-picks", "Editor's Picks", 5)
+      RunningOrderTrailblockDescription("news", "us/news/top-stories", "Top Stories", 5, style = Some(Featured)),
+      RunningOrderTrailblockDescription("news", "us/sport/top-stories", "Sports", 5, style = Some(Featured)),
+      RunningOrderTrailblockDescription("news", "us/commentisfree/top-stories", "Comment is free", 3, style = Some(Featured)),
+      RunningOrderTrailblockDescription("news", "us/culture/top-stories", "Culture", 3, style = Some(Thumbnail)),
+      RunningOrderTrailblockDescription("news", "us/business/top-stories", "Business", 1),
+      RunningOrderTrailblockDescription("news", "us/lifeandstyle/top-stories", "Life and style", 1, style = Some(Thumbnail)),
+      RunningOrderTrailblockDescription("news", "us/technology/top-stories", "Technology", 1, style = Some(Thumbnail)),
+      RunningOrderTrailblockDescription("news", "us/travel/top-stories", "Travel", 1, style = Some(Thumbnail))
     )),
 
     (Editionalise("culture", Us), Seq(
