df4703ceb90092a9ab96a90142df8724f6c03872
Francis Carr <fran@fcarr.com>
2013-08-27 00:00:17 +0100

    Add map for facia fronts, tie in refresh to Front for facia fronts

--- facia/app/controllers/front/Front.scala
+++ facia/app/controllers/front/Front.scala
@@ -15,6 +15,10 @@ class Front extends Logging {
     if (editions.contains(sectionId)) "" else sectionId
   }
 
+  lazy val faciaFronts: Map[String, PageFront] = Edition.all.map {e =>
+    e.id.toLowerCase -> new PageFront(e.id.toLowerCase, e)
+  }.toMap
+
   lazy val fronts: Map[String, FrontEdition] = Edition.all.flatMap{ edition =>
     edition.configuredFrontsFacia.filter{ front => isEditionalised(idFromEditionKey(front._1)) || (!isEditionalised(idFromEditionKey(front._1)) && edition == Edition.defaultEdition) }.map{
       case (name, trailblockDescriptions) => name ->  new FrontEdition(edition, trailblockDescriptions)
@@ -24,6 +28,7 @@ class Front extends Logging {
   def refresh() {
     log.info("Refreshing Front")
     allFronts.foreach(_.refresh())
+    faciaFronts.values.foreach(_.refresh())
   }
 
   def apply(path: String): Seq[Trailblock] = fronts(path)()
