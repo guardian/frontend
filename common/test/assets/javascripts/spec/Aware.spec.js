6c41143a7594eef58ebe5690889e32f63326c4a4
Darren Hurley <darren.hurley@guardian.co.uk>
2013-08-30 16:11:34 +0100

    fixing fake time states

--- common/test/assets/javascripts/spec/Aware.spec.js
+++ common/test/assets/javascripts/spec/Aware.spec.js
@@ -1,16 +1,19 @@
 define(['modules/experiments/aware'], function(Aware) {
-    
+
+    // make the date static so tests are stable
+    var fakeLastVisit = Date.parse('2013-04-21T12:00:00+01:00'),
+        fakeNow = Date.parse('2013-04-22T12:00:00+01:00');
+
     describe('Aware', function() {
 
         beforeEach(function() {
             localStorage.clear();
+            sinon.useFakeTimers(fakeLastVisit, "Date");
         });
-        
-        // make the date static so tests are stable
-    	var fakeLastVisit = Date.parse('2013-04-21T12:00:00+01:00'),
-            fakeNow = Date.parse('2013-04-22T12:00:00+01:00');
-        
-        sinon.useFakeTimers(fakeLastVisit, "Date");
+
+        afterEach(function() {
+            sinon.useFakeTimers().restore();
+        });
 
         it('should exist', function() {
             expect(Aware).toBeDefined();
@@ -35,13 +38,13 @@ define(['modules/experiments/aware'], function(Aware) {
         });
 
         it('should identify the frequency of visits "today"', function() {
-            
-            Aware.init() 
-            
+
+            Aware.init()
+
             // move day forward 6 hours
             var fakeNow = Date.parse('2013-04-22T04:00:00+01:00');
             sinon.useFakeTimers(fakeNow, "Date");
-            
+
             // log another visit 'today' and check we only count a single visit today
             Aware.logVisit()
 
@@ -51,10 +54,10 @@ define(['modules/experiments/aware'], function(Aware) {
             Aware.logVisit()
             expect(Aware.visits()).toBe(2);
             expect(Aware.visitsToday()).toBe(2);
-            
+
             var fakeNow = Date.parse('2013-04-25T12:00:00+01:00');
             sinon.useFakeTimers(fakeNow, "Date");
-            
+
             Aware.logVisit()
             expect(Aware.visits()).toBe(3);
             expect(Aware.visitsToday(fakeNow)).toBe(1);
@@ -62,16 +65,16 @@ define(['modules/experiments/aware'], function(Aware) {
         });
 
         it('should count frequency of visits to different sections over the last few days', function() {
-           
-            var section1 = 'foo' 
-            var section2 = 'bar' 
-            
+
+            var section1 = 'foo'
+            var section2 = 'bar'
+
             Aware.logVisit(section1)
             Aware.logVisit(section1)
             Aware.logVisit(section2)
             expect(Aware.visitsBySection('foo')).toBe(2);
             expect(Aware.visits()).toBe(3);
-            
+
         });
 
     });
