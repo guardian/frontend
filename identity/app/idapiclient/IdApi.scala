fd80dbb814f7ad78b010618a4d1f5fe5047fd3b3
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2013-09-04 15:29:16 +0100

    adding client auth headers idapi calls

--- identity/app/idapiclient/IdApi.scala
+++ identity/app/idapiclient/IdApi.scala
@@ -32,7 +32,7 @@ abstract class IdApi(apiRootUrl: String, http: Http, jsonBodyParser: JsonBodyPar
   // AUTH
 
   def authApp(auth: Auth, trackingData: OmnitureTracking): Future[Response[AccessTokenResponse]] = {
-    val response = http.GET(apiUrl("auth"), auth.parameters ++ trackingData.parameters ++ clientAuth.parameters, auth.headers)
+    val response = http.GET(apiUrl("auth"), auth.parameters ++ trackingData.parameters ++ clientAuth.parameters, auth.headers ++ clientAuth.headers)
     response map jsonBodyParser.extract[AccessTokenResponse](jsonField("accessToken"))
   }
 
@@ -46,13 +46,13 @@ abstract class IdApi(apiRootUrl: String, http: Http, jsonBodyParser: JsonBodyPar
 
   def user(userId: String, auth: Auth = Anonymous): Future[Response[User]] = {
     val apiPath = urlJoin("user", userId)
-    val response = http.GET(apiUrl(apiPath), auth.parameters ++ clientAuth.parameters, auth.headers)
+    val response = http.GET(apiUrl(apiPath), auth.parameters ++ clientAuth.parameters, auth.headers ++ clientAuth.headers)
     response map jsonBodyParser.extract[User](jsonField("user"))
   }
 
   def me(auth: Auth): Future[Response[User]] = {
     val apiPath = urlJoin("user", "me")
-    val response = http.GET(apiUrl(apiPath), auth.parameters ++ clientAuth.parameters, auth.headers)
+    val response = http.GET(apiUrl(apiPath), auth.parameters ++ clientAuth.parameters, auth.headers ++ clientAuth.headers)
     response map jsonBodyParser.extract[User](jsonField("user"))
   }
 
@@ -61,21 +61,21 @@ abstract class IdApi(apiRootUrl: String, http: Http, jsonBodyParser: JsonBodyPar
   def userForToken( token : String ): Future[Response[User]] = {
     val apiPath = urlJoin("pwd-reset", "user-for-token")
     val params = Iterable(("token", token))
-    val response = http.GET(apiUrl(apiPath), params ++ clientAuth.parameters)
+    val response = http.GET(apiUrl(apiPath), params ++ clientAuth.parameters, clientAuth.headers)
     response map jsonBodyParser.extract[User](jsonField("user"))
   }
 
   def resetPassword( token : String, newPassword : String ): Future[Response[Unit]] = {
     val apiPath = urlJoin("pwd-reset", "reset-pwd-for-user")
     val postBody = write(TokenPassword(token, newPassword))
-    val response = http.POST(apiUrl(apiPath), Some(postBody), clientAuth.parameters)
+    val response = http.POST(apiUrl(apiPath), Some(postBody), clientAuth.parameters, clientAuth.headers)
     response map {_.right.map({_ => })}
   }
 
   def sendPasswordResetEmail( emailAddress : String ): Future[Response[Unit]] = {
     val apiPath = urlJoin("pwd-reset","send-password-reset-email")
     val params = Iterable(("email-address", emailAddress), ("type", "reset"))
-    val response = http.GET(apiUrl(apiPath), params ++ clientAuth.parameters)
+    val response = http.GET(apiUrl(apiPath), params ++ clientAuth.parameters, clientAuth.headers)
     response map {_.right.map({_ => })}
   }
 
