c110bf470ac188765c4df2ea0745fd37bd3e1ec0
Kaelig <kaelig@deloumeau.fr>
2013-09-04 12:15:58 +0100

    Integration Test is more readable and always points to the mobile version

--- integration-tests/casper/tests/live-blog.spec.js
+++ integration-tests/casper/tests/live-blog.spec.js
@@ -9,19 +9,30 @@
 
 var casper = require('casper').create();
 
-casper.start(host+ '/football/2013/sep/02/transfer-deadline-day-2013-ozil-fellaini-herrera-bale-live');
+casper.start(host+ "/football/2013/sep/02/transfer-deadline-day-2013-ozil-fellaini-herrera-bale-live?view=mobile");
 
-casper.test.begin('Toggle event type', function(test) {
-    casper.waitForSelector('.live-toggler-wrapper', function() {
-        casper.click('.live-toggler--all', "Toggle to see key events only");
-        test.assertNotVisible('.article-body > .block:not(.is-key-event)', "Posts other than key events are hidden");
-        test.assertVisible('.article-body > .block.is-key-event', "Key events are visible");
+casper.test.begin("Show key events only / all posts", function(test) {
+    casper.waitForSelector(".live-toggler-wrapper", function() {
 
-        casper.click('.live-toggler--key-events', "Toggle to see all posts");
-        test.assertVisible('.article-body > .block', "All posts are visible");
+        casper.click(".live-toggler--all", "Toggle to show key events only");
+        test.assertNotVisible(
+            ".article-body > .block:not(.is-key-event)",
+            "Posts other than key events are hidden"
+        );
+        test.assertVisible(
+            ".article-body > .block.is-key-event",
+            "Key events are visible"
+        );
+
+        casper.click(".live-toggler--key-events", "Toggle to show all posts");
+        test.assertVisible(
+            ".article-body > .block",
+            "All posts are visible"
+        );
+
         test.done();
     }, function timeout(){
-        test.fail('failed to find toggling buttons');
+        test.fail("Failed to find toggling buttons");
     }, 10000);
 });
 
