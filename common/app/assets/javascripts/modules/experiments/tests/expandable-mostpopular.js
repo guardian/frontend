82938270bb288c30d087ad84d66cb04751da0a68
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-09-05 09:47:25 +0100

    Remove nasty JS and move hiding responsibility to CSS

--- common/app/assets/javascripts/modules/experiments/tests/expandable-mostpopular.js
+++ common/app/assets/javascripts/modules/experiments/tests/expandable-mostpopular.js
@@ -1,17 +1,7 @@
 /*global guardian */
-define(['common', 'bean', 'modules/popular', 'modules/related'], function (common, bean, popular, related) {
+define(['common', 'bean', 'modules/popular'], function (common, bean, popular, related) {
 
     var ExperimentExpandableMostPopular = function () {
-
-        var cleanStoryPackage = function(context) {
-            var trails = context.getElementsByClassName('related-trails')[0].getElementsByClassName('trail');
-            common.toArray(trails).forEach(function(el){
-                var img = el.getElementsByClassName('trail__img')[0],
-                    text = el.getElementsByClassName('trail__text')[0];
-                if(img) { img.parentNode.removeChild(img); }
-                if(text) { text.parentNode.removeChild(text); }
-            });
-        };
 
         this.id = 'ExpandableMostPopular';
         this.expiry = '2013-09-30';
@@ -24,19 +14,14 @@ define(['common', 'bean', 'modules/popular', 'modules/related'], function (commo
             {
                 id: 'control',
                 test: function (context) {
-                    related(guardian.config, context);
                     popular(guardian.config, context);
                 }
             },
             {
                 id: 'expandable-most-popular',
                 test: function (context) {
-                    common.mediator.on('modules:related:loaded', function() {
-                        cleanStoryPackage(context);
-                    });
-                    related(guardian.config, context);
-
                     if((/^Video|Article|Gallery$/).test(guardian.config.page.contentType)) {
+                        document.body.className += ' ab-expandable-most-popular';
                         popular(guardian.config, context, true);
                         bean.on(document.body, 'change', '.trail__expander-trigger', function(e) {
                             var trail = e.target.parentNode;
