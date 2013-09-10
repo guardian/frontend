240efcd5d09e12b16713ff275c9d8ffb3549c7da
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-09-10 16:13:51 +0100

    Add a/b test for live blog show more

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -186,6 +186,10 @@ object Switches extends Collections {
     "If this is switched on an AB test runs to trial the impact of having content cards in right hand column",
     safeState = Off)
 
+  val ABLiveBlogShowMore = Switch("A/B Tests", "ab-live-blog-show-more",
+    "If this is switched on an AB test runs to trial the impact of only displaying 10 live blog blocks with a show more cta",
+    safeState = Off)
+
 
   // Sport Switch
 
