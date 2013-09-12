fd0cb61a9d548c2346d655a55e12c89bf3bd86af
David Vella <david.vella@guardian.co.uk>
2013-09-12 12:37:19 +0100

    keeping JSHint happy

--- common/app/assets/javascripts/modules/detect.js
+++ common/app/assets/javascripts/modules/detect.js
@@ -248,23 +248,7 @@ define(['modules/userPrefs', 'common'], function (userPrefs, common) {
         // Taken from http://stackoverflow.com/a/1060034
         var hidden = "hidden";
 
-        // Standards:
-        if (hidden in document)
-            document.addEventListener("visibilitychange", onchange);
-        else if ((hidden = "mozHidden") in document)
-            document.addEventListener("mozvisibilitychange", onchange);
-        else if ((hidden = "webkitHidden") in document)
-            document.addEventListener("webkitvisibilitychange", onchange);
-        else if ((hidden = "msHidden") in document)
-            document.addEventListener("msvisibilitychange", onchange);
-        // IE 9 and lower:
-        else if ('onfocusin' in document)
-            document.onfocusin = document.onfocusout = onchange;
-        // All others:
-        else
-            window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onchange;
-
-        function onchange (evt) {
+        function onchange(evt) {
             var v = 'visible',
                 h = 'hidden',
                 evtMap = {
@@ -284,6 +268,25 @@ define(['modules/userPrefs', 'common'], function (userPrefs, common) {
             }
 
             common.mediator.emit('modules:detect:pagevisibility:' + pageVisibility);
+        }
+
+        // Standards:
+        if (hidden in document) {
+            document.addEventListener("visibilitychange", onchange);
+        } else if ((hidden = "mozHidden") in document) {
+            document.addEventListener("mozvisibilitychange", onchange);
+        } else if ((hidden = "webkitHidden") in document) {
+            document.addEventListener("webkitvisibilitychange", onchange);
+        } else if ((hidden = "msHidden") in document) {
+            document.addEventListener("msvisibilitychange", onchange);
+        }
+        // IE 9 and lower:
+        else if ('onfocusin' in document) {
+            document.onfocusin = document.onfocusout = onchange;
+        }
+        // All others:
+        else {
+            window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onchange;
         }
     }
 
