8de0a5954e5064ff5bc6adac09c622e602fc7f55
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-09-13 12:09:31 +0100

    Javascript changes for new a/b test

--- common/app/assets/javascripts/modules/experiments/tests/most-popular-from-facebook.js
+++ common/app/assets/javascripts/modules/experiments/tests/most-popular-from-facebook.js
@@ -1,13 +1,20 @@
-define(['facebook'], function (facebook) {
+define(['bonzo', 'modules/popular'], function (bonzo, popular) {
 
     return function () {
+
+        var _config;
 
         this.id = 'MostPopularFromFacebook';
         this.expiry = '2013-09-30';
         this.audience = 0.5;
         this.description = 'Tests whether showing Most Popular for visitors referred from Facebook to visitors referred from Facebook increases clickthrough';
         this.canRun = function (config) {
-            return (config.page && config.page.contentType === "Article") ? true : false;
+            _config = config;
+
+            var isArticle = config.page && config.page.contentType === "Article",
+                isFromFacebook = true; //document.referrer.indexOf('www.facebook.com') != -1;
+
+            return isArticle && isFromFacebook;
         };
         this.variants = [
             {
@@ -19,8 +26,12 @@ define(['facebook'], function (facebook) {
             {
                 id: 'facebook',
                 test: function (context) {
-                    facebook(context.querySelector('#mostpopular')).hide();
-                    facebook(context.querySelector('#mostpopular-fb')).show();
+                    var $jsPopularEl = bonzo(context.querySelector('.js-popular'));
+
+                    $jsPopularEl.hide();
+                    $jsPopularEl.after('<div class="js-popular-facebook"></div>');
+
+                    popular(_config, context, false, '/most-read-facebook', '.js-popular-facebook');
                 }
             }
         ];
