dcb6d3b5b8a94f0a98248dad5b608347bf53b205
Darren Hurley <darren.hurley@guardian.co.uk>
2013-09-16 12:35:03 +0100

    mobile, with show more

--- common/app/assets/javascripts/modules/top-stories-show-more.js
+++ common/app/assets/javascripts/modules/top-stories-show-more.js
@@ -7,7 +7,7 @@ define(['common', 'bonzo', 'bean', 'qwery', 'modules/detect'], function (common,
                 bonzo(section).append(_button);
                 bean.on(_button, 'click', function(e) {
                     // show x more, depending on current breakpoint
-                    var rowSize = _getRowSize(),
+                    var rowSize = _getOptions().show,
                         moreHidden = qwery('.item.u-h', topStories).some(function(item, index) {
                             if (index === rowSize) {
                                 return true;
@@ -19,23 +19,35 @@ define(['common', 'bonzo', 'bean', 'qwery', 'modules/detect'], function (common,
                     }
                 });
             },
-            _getRowSize = function () {
+            _getOptions = function () {
                 switch (detect.getBreakpoint()) {
                     case 'wide':
-                        return 4;
+                        return {
+                            initial: 4,
+                            show: 4
+                        };
                     case 'desktop':
-                        return 3;
+                        return {
+                            initial: 3,
+                            show: 3
+                        };
                     case 'tablet':
-                        return 2;
+                        return {
+                            initial: 2,
+                            show: 2
+                        };
                     default:
-                        return 1;
+                        return {
+                            initial: 1,
+                            show: 4
+                        };
                 }
             };
 
-        var $overflowStories = common.$g('.item:nth-child(n + ' + (_getRowSize() + 1) + ')', topStories);
+        var $overflowStories = common.$g('.item:nth-child(n + ' + (_getOptions().initial + 1) + ')', topStories);
         // hide stories
         $overflowStories.each(function(item) {
-            bonzo(item).addClass('u-h');
+            bonzo(item).addClass('u-h item--headline');
         });
         // add toggle button, if we have hidden stories
         if ($overflowStories.length) {
