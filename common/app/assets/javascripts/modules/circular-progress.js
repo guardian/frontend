f3a237b4831cf08fb1cccc071f96e8c46f50d94f
David Vella <david.vella@guardian.co.uk>
2013-09-17 08:59:04 +0100

    Refactored Circular Progress

--- common/app/assets/javascripts/modules/circular-progress.js
+++ common/app/assets/javascripts/modules/circular-progress.js
@@ -9,28 +9,54 @@ define(["bean",
     function CircularProgress(opts) {
         this.$el = bonzo(opts.el);
 
-        var self     = this,
-            baseColour = opts.baseColour || '#cccccc',
-            activeColour = opts.activeColour || '#cc0000',
-            arcWidth = opts.arcWidth || 8,
-            radius   = this.$el.dim().width / 2;
+        var baseColour = opts.baseColour || '#cccccc',
+            activeColour = opts.activeColour || '#214583',
+            bgColour = opts.bgColour || '#f4f4ee',
+            arcWidth = opts.arcWidth || 3,
+            width    = this.$el.dim().width,
+            radius   = (width - arcWidth) / 2,
+            centre   = width / 2,
             template = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">' +
-                         '<circle cx="'+radius+'" cy="'+radius+'" r="'+radius+'"'+' fill="'+baseColour+'"></circle>' +
-                         '<path d="M50,50 " fill="none" stroke="'+activeColour+'" stroke-width="'+arcWidth+'">' +
-                         '<circle cx="'+radius+'" cy="'+radius+'" r="'+(radius - width)+'" fill="#FFFFFF"></circle>' +
-                        '</svg>';
+                       '  <circle cx="'+centre+'" cy="'+centre+'" r="'+radius+'"'+' stroke="'+baseColour+'"' +
+                       '     stroke-width="'+arcWidth+'" fill="transparent"></circle>' +
+                       '  <path fill="transparent" stroke="'+activeColour+'" stroke-width="'+arcWidth+'"></path>' +
+                       '  <circle cx="'+centre+'" cy="'+centre+'" r="'+(radius - arcWidth/2)+'" fill="'+bgColour+'"></circle>' +
+                       '</svg>' +
+                       '<span class="counter"></span>';
 
 
+        this.$el.html(template);
+
+        this.centre = width / 2;
+        this.radius = (width - arcWidth) / 2;
+
+        this.pathEl = this.$el[0].querySelector('path');
+        this.pathEl.setAttribute('transform', 'translate(' + this.centre + ', ' + this.centre + ')');
+
+        this.labelEl = this.$el[0].querySelector('.counter');
     }
 
-    CircularProgress.prototype.setProgress = function(percent) {
 
+    CircularProgress.prototype.render = function(label, percent) {
+        // Based on http://stackoverflow.com/questions/5230148/create-svg-progress-circle
+        var angle    = percent * 3.5999, // percent to angle
+            rad      = angle * (Math.PI / 180), // to radians
+            x        = this.radius * Math.sin(rad),
+            y        = -this.radius * Math.cos(rad),
+            largeArc = (angle > 180) ? 1 : 0, // use large Arc switch when past 180
+            pathStr  = [
+                          'M', 0, 0,
+                          'v', -this.radius,
+                          'A', this.radius, this.radius, 1,
+                          largeArc, 1,
+                          x, y
+                       ];
+
+        this.pathEl.setAttribute('d', pathStr.join(' '));
+        this.labelEl.innerHTML = label;
     };
 
 
-    CircularProgress.prototype.setProgress = function(percent) {
-
-    };
 
     return CircularProgress;
 });
