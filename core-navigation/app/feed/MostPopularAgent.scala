c3c9574fc0877e252851fd6ddb55f735d702d612
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-09-17 10:41:43 +0100

    Refactor sort of Facebook Most Popular trails

--- core-navigation/app/feed/MostPopularAgent.scala
+++ core-navigation/app/feed/MostPopularAgent.scala
@@ -23,10 +23,10 @@ object MostPopularAgent extends Logging with ExecutionContexts {
       ContentApi.item("/", edition)
         .showMostViewed(true)
         .response.foreach{ response =>
-        val mostViewed = SupportedContentFilter(response.mostViewed map { Content(_) }) take 10
-        agent.send{ old =>
-          old + (edition.id -> mostViewed)
-        }
+          val mostViewed = SupportedContentFilter(response.mostViewed map { Content(_) }) take 10
+          agent.send{ old =>
+            old + (edition.id -> mostViewed)
+          }
       }
     }
   }
@@ -73,13 +73,7 @@ object MostPopularFromFacebookAgent extends Logging with ExecutionContexts {
         val results = response.results map (Content(_))
         log.debug("Content API list from IDs of most popular referred from facebook: " + results.map(_.id).mkString(","))
         val mostViewed = SupportedContentFilter(results) take 10
-        val sortedMostViewed = mostViewed sortWith {
-          case (content1, content2) => {
-            val idx1 = ophanList.indexOf(content1.id)
-            val idx2 = ophanList.indexOf(content2.id)
-            idx1.compareTo(idx2) < 0
-          }
-        }
+        val sortedMostViewed = mostViewed sorted Ordering.by[Content, Int](content => ophanList.indexOf(content.id))
         agent.send(old => old + ("facebook" -> sortedMostViewed))
     }
   }
