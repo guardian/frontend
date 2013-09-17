be9d07a15c54ff0e072cb51a1bb1ecaf3efaa91c
James Gorrie <james.gorrie@guardian.co.uk>
2013-09-17 16:35:03 +0100

    Recommend comment tests updated to reflect functionality

--- common/test/assets/javascripts/spec/discussion/RecommendComment.spec.js
+++ common/test/assets/javascripts/spec/discussion/RecommendComment.spec.js
@@ -14,13 +14,13 @@ define([
     describe('Recommend Comment', function() {
         var context,
             recommendComments,
-            buttons, button1, button2,
+            buttons, button,
+            currentCount,
             fixturesId = 'recommend-comment-container',
             recommendCommentFixture = {
                 id: fixturesId,
                 fixtures: [
-                    '<div class="d-comment__recommend js-recommend-comment" data-comment-id="200" data-recommend-count="3" title="3 recommendations"><i class="i i-recommend"></i><span class="d-comment__recommend-count js-recommend-count">3</span></div>'+
-                    '<div class="d-comment__recommend js-recommend-comment" data-comment-id="201" data-recommend-count="567" title="567 recommendations"><i class="i i-recommend"></i><span class="d-comment__recommend-count js-recommend-count">567</span></div>'
+                    '<div class="d-comment__recommend js-recommend-comment" data-comment-id="200" data-recommend-count="3" title="3 recommendations"><i class="i i-recommend"></i><span class="d-comment__recommend-count js-recommend-count">3</span></div>'
                 ]
             };
 
@@ -31,66 +31,119 @@ define([
         }});
         server = sinon.fakeServer.create();
         server.autoRespond = true;
-        fixtures.render(recommendCommentFixture);
-        context = document.getElementById(fixturesId);
-        buttons = context.querySelectorAll('.d-comment__recommend');
-        button1 = buttons[0];
-        button2 = buttons[1];
+        
+        // rerender the button each time    
+        beforeEach(function() {
+            if(document.getElementById(fixturesId)) {
+                document.getElementById(fixturesId).parentNode.removeChild(document.getElementById(fixturesId));
+            }
+            fixtures.render(recommendCommentFixture);
+            context = document.getElementById(fixturesId);
+            button = context.querySelectorAll('.d-comment__recommend')[0];
+            RecommendComments.init(context);
+        });
 
-        RecommendComments.init(context);
 
         describe('init', function() {
-            it('Should make buttons look clickable', function() {
-                expect(button1.className.match('d-comment__recommend--active')).toBeTruthy();
+            it('Should make recommend counts active', function() {
+                expect(button.className.match(RecommendComments.CONFIG.classes.active)).toBeTruthy();
             });
         });
 
-        // TODO: Perhaps break this into success and failure
-        //       There is multiple things each need to do (especially success)
         describe('click', function() {
-            it('Should update the recommend count and remain unchanged on success', function() {
-                var currentCount = parseInt(button1.getAttribute('data-recommend-count'), 10),
-                    successFunction = jasmine.createSpy();
+            beforeEach(function() {
+                currentCount = parseInt(button.getAttribute('data-recommend-count'), 10);
+                bean.fire(button, 'click');
+            });
 
-                common.mediator.on(RecommendComments.getEvent('success'), successFunction);
+            it('should update recommend count', function() {
+                expect(parseInt(button.getAttribute('data-recommend-count'), 10)).toEqual(currentCount+1);
+                expect(button.querySelector('.'+ RecommendComments.CONFIG.classes.count).innerHTML).toEqual(''+(currentCount+1));
+            });
+
+            it('should remove the button active state', function() {
+                expect(button.className.match(RecommendComments.CONFIG.classes.active)).not.toBeTruthy();
+            });
+
+            it('should add the recommended state to the button', function() {
+                expect(button.className.match(RecommendComments.CONFIG.classes.recommended)).not.toBeNull();
+            });
+        });
+
+        describe('success', function() {
+            it('should fire a success event', function() {
+                var successFunction = jasmine.createSpy();
                 server.respondWith([200, {}, '{"status": "ok", "message": "63 total recommendations", "statusCode": 200}']);
+                common.mediator.on(RecommendComments.getEvent('success'), successFunction);
 
                 runs(function() {
-                    bean.fire(button1, 'click');
+                    bean.fire(button, 'click');
                 });
 
                 waitsFor(function() {
                     return successFunction.calls.length > 0;
                 }, 500);
-
-                runs(function() {
-                    expect(parseInt(button1.getAttribute('data-recommend-count'), 10)).toEqual(currentCount+1);
-                    expect(button1.querySelector('.'+ RecommendComments.CONFIG.classes.count).innerHTML).toEqual(''+(currentCount+1));
-                });
-            });
-
-            it('Should reset the recommend count on fail', function() {
-                var currentCount = parseInt(button2.getAttribute('data-recommend-count'), 10),
-                    failFunction = jasmine.createSpy();
-
-                common.mediator.on(RecommendComments.getEvent('fail'), failFunction);
-                server.respondWith([400, {}, '{"status": "error", "message": "wrong", "statusCode": 400}']);
-
-                runs(function() {
-                    bean.fire(button2, 'click');
-                });
-
-                waitsFor(function() {
-                    return failFunction.calls.length > 0;
-                }, 500);
-
-                runs(function() {
-                    expect(parseInt(button2.getAttribute('data-recommend-count'), 10)).toEqual(currentCount);
-                    expect(button2.querySelector('.'+ RecommendComments.CONFIG.classes.count).innerHTML).toEqual(''+(currentCount));
-                });
             });
         });
 
+        describe('fail', function() {
+            // these test are somewhat dependant on the "click" tests to pass
+            // we use passCondition to make sure it fails at least once
+            // this is to prove it is happening on the XHR response
+            // TODO: perhaps find a way to have that check abstracted
+            var clicked, passCondition;
+
+            beforeEach(function() {
+                clicked = false;
+                passCondition = false;
+                server.respondWith([400, {}, '{"status": "error", "message": "wrong", "statusCode": 400}']);
+                currentCount = parseInt(button.getAttribute('data-recommend-count'), 10);
+            });
+            
+            it('should reset the comment count', function() {
+                runs(function() {
+                    bean.fire(button, 'click');
+                });
+
+                waitsFor(function() {
+                    var passCondition = (parseInt(button.getAttribute('data-recommend-count'), 10) === currentCount &&
+                                        button.querySelector('.'+ RecommendComments.CONFIG.classes.count).innerHTML === ''+(currentCount));
+
+                    if (passCondition === false) {
+                        clicked = true;
+                    }
+                    return clicked && passCondition;
+                }, 500);
+            });
+
+            it('should make button active', function() {
+                runs(function() {
+                    bean.fire(button, 'click');
+                });
+
+                waitsFor(function() {
+                    passCondition = button.className.match(RecommendComments.CONFIG.classes.active);
+                    if (passCondition === null) {
+                        clicked = true;
+                    }
+                    return clicked && passCondition;
+                }, 500);
+            });
+
+            it('should remove recommended state from button', function() {
+                runs(function() {
+                    bean.fire(button, 'click');
+                });
+
+                waitsFor(function() {
+                    passCondition = !button.className.match(RecommendComments.CONFIG.classes.userRecommended);
+                    if (passCondition === false) {
+                        clicked = true;
+                    }
+                    return clicked && passCondition;
+                }, 500);
+            });
+        });
     });
 
 });
