535afcac7a1b95d8552fde1194319623edab71bf
richardnguyen <richard.nguyen@guardian.co.uk>
2013-09-18 13:31:22 +0100

    Do not aggregate during sbt assembly

--- project/Prototypes.scala
+++ project/Prototypes.scala
@@ -89,6 +89,7 @@ trait Prototypes {
     test in assembly := {},
     executableName <<= (name) { "frontend-%s" format _ },
     jarName in assembly <<= (executableName) map { "%s.jar" format _ },
+    aggregate in assembly := false,
 
     mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
       {
