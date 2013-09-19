40c25779d513db8f45e81eee72d15cac39ee0fb0
Darren Hurley <darren.hurley@guardian.co.uk>
2013-09-19 16:16:58 +0100

    add standfirst to popular section

--- common/app/assets/javascripts/modules/facia-popular.js
+++ common/app/assets/javascripts/modules/facia-popular.js
@@ -7,12 +7,13 @@ define([
 ], function (common, ajax, bonzo, RelativiseTimestamp, ItemsShowMore) {
 
     var collectionTmpl =
-        '<section class="collection collection--popular-full-width" data-collection-type="popular-full-width">' +
-            '<h2 class="collection__title">Popular</h2>' +
-        '</section>',
+            '<section class="collection collection--popular-full-width" data-collection-type="popular-full-width">' +
+                '<h2 class="collection__title">Popular</h2>' +
+            '</section>',
         itemTmpl  =
             '<li class="item">' +
                 '<h2 class="item__title"><a href="" class="item__link"></a></h2>' +
+                '<p class="item__standfirst"></p>' +
                 '<div class="item__meta item__meta--grey">' +
                     '<time class="item__timestamp js-item__timestamp" itemprop="datePublished" datetime="" data-timestamp="">' +
                         '<i class="i"></i><span class="timestamp__text"></span>' +
@@ -33,17 +34,24 @@ define([
                     var $items = bonzo(bonzo.create('<ul class="unstyled items"></ul>'));
                     resp.fullTrails.forEach(function(trail) {
                         var $item = bonzo(bonzo.create(itemTmpl));
+
                         // update template
                         common.$g('.item__link', $item)
                             .attr('href', trail.url)
                             .text(trail.headline);
-                        var timestamp = common.$g('.item__timestamp', $item)
+                        common.$g('.item__standfirst', $item)
+                            .html(trail.trailText);
+
+                        // timestamp
+                        var $timestamp = common.$g('.item__timestamp', $item)
                             .attr('data-timestamp', trail.published.unix)
                             .attr('datetime', trail.published.datetime);
                         // relativise timestamp
-                        new RelativiseTimestamp(timestamp).relativise();
+                        new RelativiseTimestamp($timestamp).relativise();
+
                         // is there an image
                         if (trail.mainPicture) {
+                            $item.addClass('item--with-image');
                             var $image = bonzo(bonzo.create(imageTmpl));
                             // update template
                             common.$g('.item__link', $image)
@@ -52,10 +60,11 @@ define([
                                 .attr('src', trail.mainPicture.path);
                             $item.append($image);
                         }
+
                         // add it to the collection
                         $item.appendTo($items);
                     });
-                    // add the popular collection after
+                    // add the popular collection before the sports zone
                     bonzo(bonzo.create(collectionTmpl))
                         .append($items)
                         .insertBefore('.collection--news.collection--sport-section');
