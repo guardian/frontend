e27673b242e9535cb1f9758cc109a7cb88c9a7dd
Darren Hurley <darren.hurley@guardian.co.uk>
2013-09-20 14:04:34 +0100

    styling sports stats

--- common/app/assets/javascripts/bootstraps/facia.js
+++ common/app/assets/javascripts/bootstraps/facia.js
@@ -1,15 +1,19 @@
 define([
     //Common libraries
     'common',
+    'bonzo',
     //Modules
     'modules/facia-popular',
     'modules/facia-relativise-timestamp',
-    'modules/facia-items-show-more'
+    'modules/facia-items-show-more',
+    'modules/footballfixtures'
 ], function (
     common,
+    bonzo,
     popular,
     RelativiseTimestamp,
-    ItemsShowMore
+    ItemsShowMore,
+    FootballFixtures
 ) {
 
     var modules = {
@@ -36,6 +40,34 @@ define([
                         .addShowMore();
                 });
             });
+        },
+
+        showFootballFixtures: function(path) {
+            common.mediator.on('page:front:ready', function(config, context) {
+                if (config.page.edition === 'UK' && config.page.pageId === "") {
+                    var $statsItem = bonzo(bonzo.create('<li class="item item--sport-stats"></li>'));
+                    // slightly hacky way to add item class to newly added stats module
+                    common.mediator.on('modules:footballfixtures:render', function() {
+                        // only show 7 rows
+                        common.$g('.match:nth-child(n + 8)', $statsItem)
+                            .addClass('u-h');
+                        common.$g('.collection--news.collection--sport-section .item:first-child')
+                            .after($statsItem);
+                        // now hide one of the shown ones
+                        common.$g('.collection--news.collection--sport-section .item.u-h')
+                            .first()
+                            .previous()
+                            .addClass('u-h');
+                    });
+                    new FootballFixtures({
+                        prependTo: $statsItem,
+                        attachMethod: 'append',
+                        competitions: ['500', '510', '100', '400'],
+                        contextual: false,
+                        expandable: false
+                    }).init();
+                }
+            });
         }
 
     };
@@ -46,6 +78,7 @@ define([
             modules.showPopular();
             modules.relativiseTimestamps();
             modules.showItemsShowMore();
+            modules.showFootballFixtures();
         }
         common.mediator.emit("page:front:ready", config, context);
     };
