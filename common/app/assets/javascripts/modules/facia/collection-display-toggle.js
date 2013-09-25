581809885b65c6db7fd9d1577e7175c441c5ce00
Darren Hurley <darren.hurley@guardian.co.uk>
2013-09-25 10:27:54 +0100

    better way of hiding children

--- common/app/assets/javascripts/modules/facia/collection-display-toggle.js
+++ common/app/assets/javascripts/modules/facia/collection-display-toggle.js
@@ -8,10 +8,11 @@ define(['common', 'bonzo', 'bean'], function (common, bonzo, bean) {
                 displayed: 'Hide'
             },
             _initialState = 'displayed',
-            _dataAttr = 'data-toggle-state';
+            _dataAttr = 'data-toggle-state',
+            _elmsNotToHide = ['collection__title', 'collection__display-toggle'];
 
         this.addToggle =  function () {
-            var $button = bonzo(bonzo.create('<button class="collection__display-toggle">Hide</button>'))
+            var $button = bonzo(bonzo.create('<button class="collection__display-toggle">Hide<span class="u-h"> section</span></button>'))
                 .text(_toggleText[_initialState]);
             // append toggle button
             _$collection
@@ -21,8 +22,15 @@ define(['common', 'bonzo', 'bean'], function (common, bonzo, bean) {
             bean.on($button[0], 'click', function(e) {
                 var newState = (_$collection.attr(_dataAttr) === 'displayed') ? 'hidden' : 'displayed';
                 // hide everything which isn't the title
-                common.$g('*:not(.collection__title):not(.collection__display-toggle)', _$collection[0])
-                    [newState === 'displayed' ? 'show' : 'hide']();
+                common.toArray(_$collection[0].children).forEach(function(child) {
+                    var $child = bonzo(child),
+                        show = _elmsNotToHide.some(function(className) {
+                            return $child.hasClass(className);
+                        });
+                    if (!show) {
+                        $child[newState === 'displayed' ? 'show' : 'hide']();
+                    }
+                });
                 _$collection.attr(_dataAttr, newState);
                 $button.text(_toggleText[newState]);
             });
