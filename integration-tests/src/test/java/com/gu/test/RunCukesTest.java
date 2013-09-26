c97b2111e40613756cdf642bc24b984b4953d6f2
Grant Klopper <grant.klopper@guardian.co.uk>
2013-09-26 10:25:48 +0100

    Killed off the old style integration tests.
    Article for example has been broken since 3 Sep and nobody has bothered fixing it.
    
    These are just getting in our way, we failed to spot that assets had stopped gzipping because it was just another broken build in a sea of red.
    
    We have been shipping for a month just ignoring these and no kittens have died.

--- integration-tests/src/test/java/com/gu/test/RunCukesTest.java
+++
@@ -1,9 +0,0 @@
-package com.gu.test;
-
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@Cucumber.Options(format = {"progress", "html:target/cucumber-html-report"})
-public class RunCukesTest {
-}
