355adec7dff83c0977de2aa4deba3da400ff9c78
David Vella <dave@deepcobalt.com>
2013-09-27 01:17:27 +0100

    Code cleanup and added desaturate

--- tools/grid-bookmarklet/grid.js
+++ tools/grid-bookmarklet/grid.js
@@ -11,8 +11,11 @@ require([
 
     var scriptBase = document.querySelector('script[src*="grid.js"]').src.split('grid.js?')[0],
         cssUrl     = scriptBase + 'grid.css',
+        colWidth   = 80, // 60 + 20
+        rowHeight  = 48, // 36 + 12
         docW,
         docH,
+        gridIsVisible = true,
         $gridEl;
 
 
@@ -28,24 +31,34 @@ require([
         var gridTemplate = '<div class="grid-overlay">' +
                            '  <div class="grid-cols"></div>' +
                            '  <div class="grid-rows"></div>' +
-                           '</div>'
-        $gridEl = bonzo(bonzo.create(gridTemplate));
+                           '</div>';
+
+        if (!$gridEl) {
+            console.log('appending');
+            $gridEl = bonzo(bonzo.create(gridTemplate));
+            bonzo(document.body).append($gridEl);
+        }
+
         layout();
 
         var $colsEl = bonzo($gridEl[0].querySelector('.grid-cols')),
             $rowsEl = bonzo($gridEl[0].querySelector('.grid-rows'));
 
         // First came the columns...
-        for (var i=0; i<(docW/80); i++) {
-            $colsEl.append('<div class="grid-column"></div>');
+        //$colsEl.empty();
+        for (var i=0; i<(docW/colWidth); i++) {
+            leftPos = i * colWidth;
+            $colsEl.append('<div class="grid-column" style="left: '+leftPos+'px"></div>');
         }
 
         // ...and then the rows
-        for (var i=0; i<(docH/60); i++) {
-            $rowsEl.append('<div class="grid-row"></div>');
+        //$rowsEl.empty();
+        for (var i=0; i<(docH/rowHeight); i++) {
+            topPos = i * rowHeight;
+            $rowsEl.append('<div class="grid-row" style="top: '+topPos+'px"></div>');
         }
 
-        bonzo(document.body).append($gridEl);
+        layout();
     }
 
 
@@ -72,43 +85,77 @@ require([
 
         bean.on(window, 'resize', common.debounce(function(e){
             buildGrid();
-            layout();
         }, 500));
 
         bean.on(document.body, 'keydown', function(e) {
-            if (e.keyCode === 71) { // g key - toggle grid visiblity
-                $gridEl.toggleClass('is-hidden');
-            }
-        });
-
-        bean.on(document.querySelector('.grid-overlay'), 'mousedown', function(e) {
-            dragging = true;
-            startingX = e.pageX;
-            startingY = e.pageY;
             startingGridTop = parseInt($gridEl.css('top'), 10);
             startingGridLeft = parseInt($gridEl.css('left'), 10);
-        });
 
-        bean.on(document.querySelector('.grid-overlay'), 'mousemove', function(e) {
-            if (dragging == true) {
-                var offsetX = e.pageX - startingX,
-                    offsetY = e.pageY - startingY;
+            console.log(e.keyCode);
+            if (e.keyCode === 71) {
+                // g key - toggle grid visiblity
+                $gridEl.toggleClass('is-hidden');
+                gridIsVisible = gridIsVisible ? false : true;
+            } else if (e.keyCode === 68) {
+                // d key - desaturate content area
+                bonzo(document.body).toggleClass('is-desaturated');
+            } else if (e.keyCode === 38) {
+                // Up
+                $gridEl[0].style.top = (startingGridTop - 1) + 'px';
+            } else if (e.keyCode === 40) {
+                // Down
+                $gridEl[0].style.top = (startingGridTop + 1) + 'px';
+            } else if (e.keyCode === 37) {
+                // Left
+                $gridEl[0].style.left = (startingGridLeft - 1) + 'px';
+            } else if (e.keyCode === 39) {
+                // Right
+                $gridEl[0].style.left = (startingGridLeft + 1) + 'px';
+            }
 
-                $gridEl[0].style.left = (startingGridLeft + offsetX) + 'px';
-                $gridEl[0].style.top  = (startingGridTop + offsetY) + 'px';
+            if (gridIsVisible) {
+                e.preventDefault();
             }
         });
 
-        bean.on(document.querySelector('.grid-overlay'), 'mouseup', function(e) {
-            dragging = false;
+        bean.on(document.querySelector('.grid-overlay'), {
+            mousedown: function(e) {
+                dragging = true;
+                startingX = e.pageX;
+                startingY = e.pageY;
+                startingGridTop = parseInt($gridEl.css('top'), 10);
+                startingGridLeft = parseInt($gridEl.css('left'), 10);
+            },
+
+            mousemove: function(e) {
+                if (dragging == true) {
+                    var offsetX = e.pageX - startingX,
+                        offsetY = e.pageY - startingY;
+
+                    $gridEl[0].style.left = (startingGridLeft + offsetX) + 'px';
+                    $gridEl[0].style.top  = (startingGridTop + offsetY) + 'px';
+                }
+            },
+
+            mouseup: function(e) {
+                dragging = false;
+            }
         });
+
+
+        // Check for document size changes, and repaint the grid if needed
+        setInterval(function() {
+            var $doc = bonzo(document);
+            if (docW !== $doc.dim().width || docH !== $doc.dim().height) {
+                //buildGrid();
+            }
+        }, 2000);
     }
 
 
 
     init();
     buildGrid();
-    layout();
     bindEvents();
 
 });
