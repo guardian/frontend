7be9b87aca083500f939e97861c3c145bce63e35
David Vella <david.vella@guardian.co.uk>
2013-10-01 10:09:23 +0100

    Cleaner test file

--- common/test/assets/javascripts/spec/RelativeDates.spec.js
+++ common/test/assets/javascripts/spec/RelativeDates.spec.js
@@ -1,8 +1,8 @@
-define(['modules/relativedates', 
-        'bonzo', 
+define(['modules/relativedates',
+        'bonzo',
         'helpers/fixtures',
         'qwery'], function(RelativeDates, bonzo, fixtures, qwery) {
-    
+
     var conf =  {
             id: 'relative-dates',
             fixtures: [
@@ -15,7 +15,7 @@ define(['modules/relativedates',
         date;
 
     describe("Relative dates", function() {
-       
+
         beforeEach(function() {
             fixtures.render(conf);
             date = sinon.useFakeTimers(fakeNow, "Date");
@@ -29,93 +29,89 @@ define(['modules/relativedates',
         var epochBug = '2038-01-19T03:14:07';
 
         it('Show relative dates for timestamps formatted as YYYY-MM-DD HH:MM:SS', function(){
-	   	
-	    	var datesToTest = {
-	        	'lessThanAMinuteAgo': {
-	        		'date'			 :      '2012-08-13T11:59:50+01:00',
-	        		'expectedOutput' :      '10s',
+
+            var datesToTest = {
+                'lessThanAMinuteAgo': {
+                    'date'           :      '2012-08-13T11:59:50+01:00',
+                    'expectedOutput' :      '10s',
                     'expectedShortOutput' : '10s'
-	    		},
-	    		'oneMinuteAgo': {  // singular
-	        		'date'			 :      '2012-08-13T11:58:40+01:00',
-	        		'expectedOutput' :      '1m',
+                },
+                'oneMinuteAgo': {  // singular
+                    'date'           :      '2012-08-13T11:58:40+01:00',
+                    'expectedOutput' :      '1m',
                     'expectedShortOutput' : '1m'
-	    		},
-	    		'upToEightMinutesAgo': { // plural
-	        		'date' 			 :      '2012-08-13T11:52:30+01:00',
-	        		'expectedOutput' :      '8m',
+                },
+                'upToEightMinutesAgo': { // plural
+                    'date'           :      '2012-08-13T11:52:30+01:00',
+                    'expectedOutput' :      '8m',
                     'expectedShortOutput' : '8m'
-	    		},
-	    		'oneHourAgo': { // singular
-	        		'date' 			 :      '2012-08-13T11:00:00+01:00',
-	        		'expectedOutput' :      '1h',
+                },
+                'oneHourAgo': { // singular
+                    'date'           :      '2012-08-13T11:00:00+01:00',
+                    'expectedOutput' :      '1h',
                     'expectedShortOutput' : '1h'
-	    		},
-	    		'betweenNinetyMinutesAndOneHour': {  // bug GFE-38
-	        		'date' 			 :      '2012-08-13T10:25:00+01:00',
-	        		'expectedOutput' :      '2h',
+                },
+                'betweenNinetyMinutesAndOneHour': {  // bug GFE-38
+                    'date'           :      '2012-08-13T10:25:00+01:00',
+                    'expectedOutput' :      '2h',
                     'expectedShortOutput' : '2h'
-	    		},
-	    		'lessThanFiveHoursAgo': { // plural
-	        		'date' 			 :      '2012-08-13T08:30:00+01:00',
-	        		'expectedOutput' :      '4h',
+                },
+                'lessThanFiveHoursAgo': { // plural
+                    'date'           :      '2012-08-13T08:30:00+01:00',
+                    'expectedOutput' :      '4h',
                     'expectedShortOutput' : '4h'
-	    		},
-	    		'moreThanFiveHoursAgo': { // ... but still today
-	        		'date' 			 :      '2012-08-13T02:03:00+01:00',
-	        		'expectedOutput' :      '10h',
+                },
+                'moreThanFiveHoursAgo': { // ... but still today
+                    'date'           :      '2012-08-13T02:03:00+01:00',
+                    'expectedOutput' :      '10h',
                     'expectedShortOutput' : '10h'
-	    		},
-	    		'yesterday': {
-	        		'date' 			 :      '2012-08-12T23:45:00+01:00',
-	        		'expectedOutput' :      'Yesterday, 11:45pm',
+                },
+                'yesterday': {
+                    'date'           :      '2012-08-12T23:45:00+01:00',
+                    'expectedOutput' :      'Yesterday, 11:45pm',
                     'expectedShortOutput' : '1d'
-	    		},
-	    		'moreThanTwoDaysAgo': {
-	        		'date' 			 :      '2012-08-09T08:34:00+01:00',
-	        		'expectedOutput' :      'Thursday 9 Aug 2012',
+                },
+                'moreThanTwoDaysAgo': {
+                    'date'           :      '2012-08-09T08:34:00+01:00',
+                    'expectedOutput' :      'Thursday 9 Aug 2012',
                     'expectedShortOutput' : '4d'
-	    		},
-	    		'moreThanFiveDaysAgo': {
-	        		'date' 			 :      '2012-08-05T21:30:00+01:00',
-	        		'expectedOutput' :      '5 Aug 2012',
+                },
+                'moreThanFiveDaysAgo': {
+                    'date'           :      '2012-08-05T21:30:00+01:00',
+                    'expectedOutput' :      '5 Aug 2012',
                     'expectedShortOutput' : '5 Aug 2012'
-	    		},
-	    		'oneMinuteAgoInAnotherTimeZone': { 
-	        		'date'			 :      '2012-08-13T12:58:40+02:00',
-	        		'expectedOutput' :      '1m',
+                },
+                'oneMinuteAgoInAnotherTimeZone': {
+                    'date'           :      '2012-08-13T12:58:40+02:00',
+                    'expectedOutput' :      '1m',
                     'expectedShortOutput' : '1m'
-	    		}
-	    	};
+                }
+            };
 
-	    	for (var category in datesToTest) {
-		  		var d = datesToTest[category]
-                var epoch = Date.parse(d.date)
-		  		expect(RelativeDates.makeRelativeDate(epoch)).toBe(d.expectedOutput);
-			}
-
-            // Do the same but in short format - used for fronts
             for (var category in datesToTest) {
-                var d = datesToTest[category]
-                var epoch = Date.parse(d.date)
+                var d = datesToTest[category];
+                var epoch = Date.parse(d.date);
+                expect(RelativeDates.makeRelativeDate(epoch)).toBe(d.expectedOutput);
+
+                // Do the same but in short format
                 expect(RelativeDates.makeRelativeDate(epoch, { format: 'short' })).toBe(d.expectedShortOutput);
             }
-		});
+        });
 
-		it("Return the input date if said date is in the future", function(){
-			expect(RelativeDates.makeRelativeDate(Date.parse(epochBug))).toBeFalsy();
-		});
+        it("Return the input date if said date is in the future", function(){
+            expect(RelativeDates.makeRelativeDate(Date.parse(epochBug))).toBeFalsy();
+        });
 
-		it("Fail politely if given non-date / invalid input for either argument", function(){
-			expect(RelativeDates.makeRelativeDate('foo')).toBeFalsy();
-		});
-		
+        it("Fail politely if given non-date / invalid input for either argument", function(){
+            expect(RelativeDates.makeRelativeDate('foo')).toBeFalsy();
+        });
+
         it("Convert valid timestamps in the HTML document into their expected output", function(){
             RelativeDates.init();
             expect(document.getElementById('time-valid').innerHTML).toBe('Yesterday, 7:43pm');
             expect(document.getElementById('time-valid').getAttribute('title')).toBe('12th August');
         });
-        
+
         // each XHR load event fires replaceValidTimestamps(), so we want to avoid replacing date twice
         it("Once converted remove the need to convert them again", function(){
             RelativeDates.init();
@@ -129,4 +125,4 @@ define(['modules/relativedates',
 
     });
 
-})
+});
