0e0f395cfe548fba462f6cf9e1936e79d538aebf
Grant Klopper <grant.klopper@guardian.co.uk>
2013-10-07 11:00:55 +0100

    Fixed CompetitionAgent test

--- sport/test/CompetitionAgentTest.scala
+++ sport/test/CompetitionAgentTest.scala
@@ -8,27 +8,19 @@ import concurrent.Promise
 import concurrent.{Future, Await}
 import concurrent.duration._
 import conf.FootballClient
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Span}
+import org.scalatest.concurrent.PatienceConfiguration
+import org.joda.time.DateMidnight
 
 
-class CompetitionAgentTest extends FlatSpec with ShouldMatchers with implicits.Football {
+class CompetitionAgentTest extends FlatSpec with ShouldMatchers with implicits.Football with Eventually {
 
+  override implicit lazy val patienceConfig = PatienceConfig(timeout = scaled(Span(3000, Millis)), interval = scaled(Span(100, Millis)))
 
-  ignore should "load fixtures" in Fake {
+  lazy val seasonStart = Some(new DateMidnight(2012, 8, 1))
 
-    object TestCompetitions extends Competitions {
-      override val competitionAgents = Seq(
-        CompetitionAgent(Competition("100", "/football/premierleague", "Premier League", "Premier League", "English", showInTeamsList = true))
-      )
-    }
-
-    awaitTestData(TestCompetitions)
-
-    TestCompetitions.matches.filter(_.isFixture).map(_.id) should contain ("3519484")
-
-    TestCompetitions.stop()
-  }
-
-  ignore should "load results" in Fake {
+  "CompetitionAgentTest" should "load fixtures" in Fake {
 
     object TestCompetitions extends Competitions {
       override val competitionAgents = Seq(
@@ -36,14 +28,33 @@ class CompetitionAgentTest extends FlatSpec with ShouldMatchers with implicits.F
       )
     }
 
-    awaitTestData(TestCompetitions)
+    TestCompetitions.competitionAgents.foreach(_.refreshFixtures())
 
-    TestCompetitions.matches.filter(_.isResult).map(_.id) should contain ("3528302")
+    eventually(
+      TestCompetitions.matches.filter(_.isFixture).map(_.id) should contain ("3519484")
+    )
 
     TestCompetitions.stop()
   }
 
-  ignore should "load live matches" in Fake {
+  it should "load results" in Fake {
+
+    object TestCompetitions extends Competitions {
+      override val competitionAgents = Seq(
+        CompetitionAgent(Competition("100", "/football/premierleague", "Premier League", "Premier League", "English", showInTeamsList = true, startDate = seasonStart))
+      )
+    }
+
+    TestCompetitions.competitionAgents.foreach(_.refreshResults())
+
+    eventually(
+      TestCompetitions.matches.filter(_.isResult).map(_.id) should contain ("3528302")
+    )
+
+    TestCompetitions.stop()
+  }
+
+  it should "load live matches" in Fake {
 
     object TestCompetitions extends Competitions {
       override val competitionAgents = Seq(
@@ -51,15 +62,14 @@ class CompetitionAgentTest extends FlatSpec with ShouldMatchers with implicits.F
       )
     }
 
-    //await(TestCompetitions.refreshCompetitionData()).foreach(await)
-    //await(TestCompetitions.refreshMatchDay()).foreach(await)
+    TestCompetitions.refreshMatchDay()
 
-    TestCompetitions.matches.filter(_.isLive).map(_.id) should contain ("3518286")
+    eventually(TestCompetitions.matches.filter(_.isLive).map(_.id) should contain ("3518286"))
 
     TestCompetitions.stop()
   }
 
-  ignore should "load league tables" in Fake {
+  it should "load league tables" in Fake {
 
     object TestCompetitions extends Competitions {
       override val competitionAgents = Seq(
@@ -67,30 +77,11 @@ class CompetitionAgentTest extends FlatSpec with ShouldMatchers with implicits.F
       )
     }
 
-    //await(TestCompetitions.refreshCompetitionData()).foreach(await)
+    TestCompetitions.competitionAgents.foreach(_.refresh())
 
-    TestCompetitions.competitionAgents.foreach{ agent =>
-      //await(agent.refreshLeagueTable())
-    }
-
-    TestCompetitions.competitions(0).leagueTable(0).team.id should be ("4")
+    eventually(TestCompetitions.competitions(0).leagueTable(0).team.id should be ("4"))
 
     TestCompetitions.stop()
   }
 
-  private def awaitTestData(competitions: Competitions) {
-
-    val premierAgent = competitions.competitionAgents.find(_.competition.id == "100").head
-
-    competitions.refreshCompetitionData()
-    competitions.refreshMatchDay()
-
-    //TODO
-    //give test data a chance to load
-
-  }
-
-  private def isTestDataLoaded(comp: Competition) = {
-    (comp.hasFixtures && comp.hasLiveMatches && comp.hasResults && comp.hasLeagueTable)
-  }
 }
