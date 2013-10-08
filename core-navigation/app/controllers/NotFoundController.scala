afa2cd08490d7c3ec176890b91a5caac303736ed
Grant Klopper <grant.klopper@guardian.co.uk>
2013-10-08 14:04:33 +0100

    Simple 404 page

--- core-navigation/app/controllers/NotFoundController.scala
+++ core-navigation/app/controllers/NotFoundController.scala
@@ -7,20 +7,11 @@ import model.{Page, MostPopular}
 
 object NotFoundController extends Controller with ExecutionContexts {
 
-<<<<<<< HEAD
-  val page = new Page("404", "news", "404", "GFE:404 error page") {
+  val page = new Page("404", "uk", "404", "GFE:404 error page") {
     override def metaData = super.metaData + ("content-type" -> "errorPage")
   }
 
   def render404() = Action{ implicit request =>
     NotFound(views.html.notFound(page, MostPopularAgent.mostPopular(Edition(request))))
-=======
-  val page = Page("", "news", "404", "GFE:404")
-
-  def render404() = Action{ implicit request =>
-    val popular = MostPopular("The Guardian", "", MostPopularAgent.mostPopular(Edition(request)))
-    NotFound(views.html.notFound(page, popular))
->>>>>>> 8ad9d307fa0795992aec04d4905a01fa868877bf
   }
-
 }
