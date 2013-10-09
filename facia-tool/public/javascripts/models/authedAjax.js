227d0c4d933f89d088fe0cdc2cc10e6ae4d55a7e
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-10-09 15:41:41 +0100

    Don't mutate the ajax options object

--- facia-tool/public/javascripts/models/authedAjax.js
+++ facia-tool/public/javascripts/models/authedAjax.js
@@ -1,18 +1,14 @@
 define([], function () {
     return function (opts) {
-        opts.dataType = 'json';
-        opts.contentType = 'application/json';
 
-        var promise = $.ajax(opts)
-            .fail(function(xhr) {
-                if (xhr.status === 403) {
-                    window.location.href = '/logout';
-                }
-            })
-            .then(function(data) {
-                return data;
-            });
-
-        return promise;
+        return $.ajax(
+            _.extend({}, opts, {dataType: 'json', contentType: 'application/json'})
+        ).fail(function(xhr) {
+            if (xhr.status === 403) {
+                window.location.href = '/logout';
+            }
+        }).then(function(data) {
+            return data;
+        });
     }
 });
