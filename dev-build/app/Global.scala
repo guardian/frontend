55fe84ba997a340c71cb45e0aedeb0dae6b59f3c
Grant Klopper <grant.klopper@guardian.co.uk>
2013-10-09 15:02:03 +0100

    While in dev-build, devs will see a warning in their console if they are working on something that does not have a Cache-Control header.
    EVERYTHING is cached.

--- dev-build/app/Global.scala
+++ dev-build/app/Global.scala
@@ -1,7 +1,28 @@
+import common.ExecutionContexts
 import conf.RequestMeasurementMetrics
 import controllers.front.FrontLifecycle
 import dev.DevParametersLifecycle
-import play.api.mvc.WithFilters
+import play.api.mvc._
 
-object Global extends WithFilters(RequestMeasurementMetrics.asFilters: _*) with MostPopularLifecycle
+// obviously this is only for devbuild and should never end up in one of our
+// prod projects
+object DevCacheWarningFilter extends EssentialFilter with ExecutionContexts {
+  def apply(next: EssentialAction) = new EssentialAction {
+    def apply(rh: RequestHeader) = {
+      next(rh).map{ result =>
+        val header = result.header
+        if (header.status == 200 && !header.headers.keySet.contains("Cache-Control")) {
+
+          // nice big warning to devs if they are working on something uncached
+          println("\n\n\n---------------------------- WARNING ------------------------------------")
+          println(s"URL ${rh.path}${rh.rawQueryString} has NO CACHE-CONTROL header")
+          println("-------------------------------------------------------------------------------\n\n\n")
+        }
+        result
+      }
+    }
+  }
+}
+
+object Global extends WithFilters(DevCacheWarningFilter :: RequestMeasurementMetrics.asFilters: _*) with MostPopularLifecycle
   with FrontLifecycle with DevParametersLifecycle
