bcc6a1be9b0371c46d2578108febd1fc5a2fa9ed
richardnguyen <richard.nguyen@guardian.co.uk>
2013-10-09 10:48:12 +0100

    Move lookup out of image content controller

--- applications/app/controllers/ImageContentController.scala
+++ applications/app/controllers/ImageContentController.scala
@@ -4,17 +4,16 @@ import common._
 import conf._
 import model._
 import play.api.mvc.{SimpleResult, RequestHeader, Controller, Action}
-import com.gu.openplatform.contentapi.model.{Content => ApiContent,ItemResponse}
-import scala.concurrent.Future
+import services.ImageQuery
 
 case class ImageContentPage(image: Content, storyPackage: List[Trail])
 
-object ImageContentController extends Controller with Logging with ExecutionContexts {
+object ImageContentController extends Controller with ImageQuery with Logging with ExecutionContexts {
 
   def renderJson(path: String) = render(path)
 
   def render(path: String) = Action.async { implicit request =>
-    lookup(path).map {
+    image(Edition(request), path).map {
         case Left(content) => renderImageContent(content)
         case Right(result) => result
     }
@@ -24,20 +23,5 @@ object ImageContentController extends Controller with Logging with ExecutionCont
     val htmlResponse = () => views.html.imageContent(page)
     val jsonResponse = () => views.html.fragments.imageContentBody(page)
     renderFormat(htmlResponse, jsonResponse, page.image, Switches.all)
-  }
-
-  private def lookup(path: String)(implicit request: RequestHeader): Future[Either[ImageContentPage, SimpleResult]]= {
-    val edition = Edition(request)
-    log.info(s"Fetching image content: $path for edition ${edition.id}")
-    val response = ContentApi.item(path, edition)
-      .showExpired(true)
-      .showFields("all")
-      .response.map { response =>
-        val mainContent: Option[Content] = response.content.filter { c => c.isImageContent } map {Content(_)}
-        val storyPackage: List[Trail] = response.storyPackage map { Content(_) }
-        mainContent.map { content => Left(ImageContentPage(content,storyPackage)) }.getOrElse(Right(NotFound))
-      }
-
-    response recover suppressApiNotFound
   }
 }
--- applications/app/services/repositories.scala
+++ applications/app/services/repositories.scala
@@ -8,6 +8,9 @@ import com.gu.openplatform.contentapi.model.ItemResponse
 import org.joda.time.DateTime
 import org.scala_tools.time.Implicits._
 import contentapi.QueryDefaults
+import controllers.ImageContentPage
+import scala.concurrent.Future
+import play.api.mvc.SimpleResult
 
 case class IndexPage(page: MetaData, trails: Seq[Trail])
 
@@ -44,5 +47,22 @@ trait Index extends ConciergeRepository with QueryDefaults {
     val latest: Seq[Content] = response.results.map(Content(_)).filterNot(c => leadContent.map(_.id).exists(_ == c.id))
     val allTrails = (leadContent ++ editorsPicks ++ latest).distinctBy(_.id).take(20)
     tag map { IndexPage(_, allTrails) }
+  }
+}
+
+trait ImageQuery extends ConciergeRepository with QueryDefaults {
+
+  def image(edition: Edition, path: String): Future[Either[ImageContentPage, SimpleResult]]= {
+    log.info(s"Fetching image content: $path for edition ${edition.id}")
+    val response = ContentApi.item(path, edition)
+      .showExpired(true)
+      .showFields("all")
+      .response.map { response =>
+      val mainContent: Option[Content] = response.content.filter { c => c.isImageContent } map {Content(_)}
+      val storyPackage: List[Trail] = response.storyPackage map { Content(_) }
+      mainContent.map { content => Left(ImageContentPage(content,storyPackage)) }.getOrElse(Right(NotFound))
+    }
+
+    response recover suppressApiNotFound
   }
 }
