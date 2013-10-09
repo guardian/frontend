b15d1a9d63100dd37fa96a56ac778119b4024925
Francis Carr <francis.carr@guardian.co.uk>
2013-10-09 14:58:03 +0100

    Add FaciaTestData and have FaciaControllerTest use it

--- facia/test/FaciaControllerTest.scala
+++ facia/test/FaciaControllerTest.scala
@@ -1,14 +1,11 @@
 package test
 
-import concurrent.{Future, Await}
 import play.api.test._
 import play.api.test.Helpers._
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.{BeforeAndAfterAll, FlatSpec}
 import common.ExecutionContexts
-import controllers.front.Front
-import concurrent.duration._
-import org.scalatest.time.{Millis, Span}
+import controllers.FaciaController
 
 class FaciaControllerTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll with ExecutionContexts {
 
@@ -17,41 +14,35 @@ class FaciaControllerTest extends FlatSpec with ShouldMatchers with BeforeAndAft
 
   val responsiveRequest = FakeRequest().withHeaders("host" -> "www.theguardian.com")
 
-  override def beforeAll() = {
-    running(FakeApplication()) {
-      val loadFrontFuture = for {
-        config <- Front.refreshConfig()
-        pageFrontAgent <- Front.refreshPageFrontAgent()
-        s <- Future.sequence(Front.pageFrontAgent().values.map(_.refresh()))
-      } yield s
-      Await.result(loadFrontFuture, 60.seconds)
-    }
+  object Front extends TestFront
+  object FaciaController extends FaciaController {
+    override val front = Front
   }
 
   "FaciaController" should "200 when content type is front" in Fake {
-    val result = controllers.FaciaController.renderEditionFront("uk")(TestRequest())
+    val result = FaciaController.renderEditionFront("uk")(TestRequest())
     status(result) should be(200)
   }
 
   it should "redirect base page to edition page if on www.theguardian.com" in Fake {
 
-    val result = controllers.FaciaController.renderFront("")(responsiveRequest.withHeaders("X-GU-Edition" -> "US"))
+    val result = FaciaController.renderFront("")(responsiveRequest.withHeaders("X-GU-Edition" -> "US"))
     status(result) should be(303)
     header("Location", result) should be (Some("/us"))
 
-    val result2 = controllers.FaciaController.renderFront("culture")(responsiveRequest.withHeaders("X-GU-Edition" -> "AU"))
+    val result2 = FaciaController.renderFront("culture")(responsiveRequest.withHeaders("X-GU-Edition" -> "AU"))
     status(result2) should be(303)
     header("Location", result2) should be (Some("/au/culture"))
 
   }
 
   it should "understand the editionalised network front" in Fake {
-    val result2 = controllers.FaciaController.renderEditionFront("uk")(TestRequest())
+    val result2 = FaciaController.renderEditionFront("uk")(TestRequest())
     status(result2) should be(200)
   }
 
   it should "understand editionalised section fronts" in Fake {
-    val result2 = controllers.FaciaController.renderEditionSectionFront("uk/culture")(TestRequest())
+    val result2 = FaciaController.renderEditionSectionFront("uk/culture")(TestRequest())
     status(result2) should be(200)
   }
 
@@ -59,7 +50,7 @@ class FaciaControllerTest extends FlatSpec with ShouldMatchers with BeforeAndAft
     val fakeRequest = FakeRequest(GET, s"?callback=$callbackName")
       .withHeaders("host" -> "localhost:9000")
 
-    val result = controllers.FaciaController.renderEditionFront("uk")(fakeRequest)
+    val result = FaciaController.renderEditionFront("uk")(fakeRequest)
     status(result) should be(200)
     contentType(result).get should be("application/javascript")
     contentAsString(result) should startWith(s"""$callbackName({\"html\"""")
@@ -70,7 +61,7 @@ class FaciaControllerTest extends FlatSpec with ShouldMatchers with BeforeAndAft
       .withHeaders("Host" -> "localhost:9000")
       .withHeaders("Origin" -> "http://www.theorigin.com")
 
-    val result = controllers.FaciaController.renderEditionFrontJson("uk")(fakeRequest)
+    val result = FaciaController.renderEditionFrontJson("uk")(fakeRequest)
     status(result) should be(200)
     contentType(result).get should be("application/json")
     contentAsString(result) should startWith("{\"html\"")
@@ -78,10 +69,10 @@ class FaciaControllerTest extends FlatSpec with ShouldMatchers with BeforeAndAft
 
   it should "200 when content type is front trails" in Fake {
     //Fronts never redirected, this redirects if you hit naked slash
-    val result = controllers.FaciaController.renderTrails("")(TestRequest())
+    val result = FaciaController.renderTrails("")(TestRequest())
     status(result) should be(303)
 
-    val result2 = controllers.FaciaController.renderTrails("uk")(TestRequest())
+    val result2 = FaciaController.renderTrails("uk")(TestRequest())
     status(result2) should be(200)
   }
 
@@ -89,7 +80,7 @@ class FaciaControllerTest extends FlatSpec with ShouldMatchers with BeforeAndAft
     val fakeRequest = FakeRequest(GET, s"/culture/trails?callback=$callbackName")
       .withHeaders("host" -> "localhost:9000")
 
-    val result = controllers.FaciaController.renderTrails("uk")(fakeRequest)
+    val result = FaciaController.renderTrails("uk")(fakeRequest)
     status(result) should be(200)
     contentType(result).get should be("application/javascript")
     contentAsString(result) should startWith(s"""$callbackName({\"html\"""")
@@ -100,14 +91,14 @@ class FaciaControllerTest extends FlatSpec with ShouldMatchers with BeforeAndAft
       .withHeaders("Host" -> "localhost:9000")
       .withHeaders("Origin" -> "http://www.theorigin.com")
 
-    val result = controllers.FaciaController.renderTrails("uk")(fakeRequest)
+    val result = FaciaController.renderTrails("uk")(fakeRequest)
     status(result) should be(200)
     contentType(result).get should be("application/json")
     contentAsString(result) should startWith("{\"html\"")
   }
 
   it should "200 when hitting the front" in Fake {
-    val result = controllers.FaciaController.renderEditionFront("uk")(TestRequest())
+    val result = FaciaController.renderEditionFront("uk")(TestRequest())
     status(result) should be(200)
   }
 }
