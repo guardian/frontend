81accaf3e0f5ee0fe1ec14e6d83d842e62251ea1
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-10-10 10:56:59 +0100

    Stop redundant cache fill
    Now that Facebook most popular test is over.

--- core-navigation/app/Global.scala
+++ core-navigation/app/Global.scala
@@ -1,7 +1,7 @@
 import common.{ CoreNavivationMetrics, Jobs }
 import conf.RequestMeasurementMetrics
 import dev.DevParametersLifecycle
-import feed.{MostPopularFromFacebookAgent, MostPopularExpandableAgent, MostPopularAgent}
+import feed.{MostPopularExpandableAgent, MostPopularAgent}
 import play.api.mvc.WithFilters
 import play.api.{ Application => PlayApp, Play, GlobalSettings }
 import play.api.Play.current
@@ -12,7 +12,6 @@ trait MostPopularLifecycle extends GlobalSettings {
     super.onStart(app)
 
     Jobs.deschedule("MostPopularAgentRefreshJob")
-    Jobs.deschedule("MostPopularFromFacebookAgentRefreshJob")
 
     // fire every min
     Jobs.schedule("MostPopularAgentRefreshJob",  "0 * * * * ?", CoreNavivationMetrics.MostPopularLoadTimingMetric) {
@@ -20,23 +19,14 @@ trait MostPopularLifecycle extends GlobalSettings {
       MostPopularExpandableAgent.refresh()
     }
 
-    // fire every 15 mins
-    Jobs.schedule("MostPopularFromFacebookAgentRefreshJob",  "0 2/15 * * * ?", CoreNavivationMetrics.MostPopularLoadTimingMetric) {
-      MostPopularFromFacebookAgent.refresh()
-    }
-    // refresh facebook referral list immediately to avoid initial wait of up to 15 mins
-    MostPopularFromFacebookAgent.refresh()
-
     if (Play.isTest) {
       MostPopularAgent.refresh()
       MostPopularAgent.await()
-      MostPopularFromFacebookAgent.await()
     }
   }
 
   override def onStop(app: PlayApp) {
     Jobs.deschedule("MostPopularAgentRefreshJob")
-    Jobs.deschedule("MostPopularFromFacebookAgentRefreshJob")
     super.onStop(app)
   }
 }
