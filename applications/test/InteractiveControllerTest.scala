8faa162e03fc093e30cb7856a8e1fe79f3a76834
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-10-11 10:23:34 +0100

    Merge interactive in to applications

---
+++ applications/test/InteractiveControllerTest.scala
@@ -0,0 +1,16 @@
+package test
+
+import play.api.test._
+import play.api.test.Helpers._
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+
+class InteractiveControllerTest extends FlatSpec with ShouldMatchers {
+ 
+  val url = "/music/interactive/2013/aug/20/matthew-herbert-quiz-hearing"
+
+  "Interactive Controller" should "200 when content type is 'interactive'" in Fake {
+    val result = controllers.InteractiveController.renderInteractive(url)(TestRequest())
+    status(result) should be(200)
+  }
+}
