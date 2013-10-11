78f231da4e114f70be850f7e373c29c9e04df22a
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-10-11 17:03:56 +0100

    Add logic for inview bucket for alpha advert test

--- common/app/assets/javascripts/modules/analytics/adverts.js
+++ common/app/assets/javascripts/modules/analytics/adverts.js
@@ -1,20 +1,21 @@
 define(['common', 'bean', 'modules/inview'], function (common, bean, Inview) {
 
-    function InViewAdverts(config, context) {
+    function AdvertsAnalytics(context) {
+
+        // Setup listeners
+        bean.on(context, 'inview', function(e) {
+            var inviewName = e.target.getAttribute('data-inview-name');
+
+            common.mediator.emit('module:analytics:adimpression', inviewName);
+        });
 
         // Label up ad slots
         common.$g('.ad-slot', context).each(function() {
             this.setAttribute('data-inview-name', this.getAttribute('data-link-name'));
         });
 
-        // Label up paragraphs
-        if (config.page.contentType === 'Article') {
-            common.$g('.article-body p:nth-of-type(10n)', context).attr('data-inview-name','every 10th para');
-        }
-
-
         var inview = new Inview('[data-inview-name]', context);
     }
 
-    return InViewAdverts;
+    return AdvertsAnalytics;
 });
--- common/app/assets/javascripts/modules/experiments/tests/alpha-adverts.js
+++ common/app/assets/javascripts/modules/experiments/tests/alpha-adverts.js
@@ -1,7 +1,18 @@
 /*global guardian */
-define(['bonzo'], function (bonzo) {
+define(['query', 'bonzo', 'analytics/adverts'], function (query, bonzo, inview) {
 
     var AlphaAdverts = function () {
+
+        var nParagraphs = '10',
+            alphaOasUrl = 'www.theguardian-alpha.com',
+            inlineTmp = '<div class="ad-slot ad-slot-inview"><div class="ad-container"></div></div>';
+
+        // Label up ad slots
+        var labelSlots = function() {
+            bonzo(query('.ad-slot'), document).each(function() {
+                this.setAttribute('data-inview-name', this.getAttribute('data-link-name'));
+            });
+        };
 
         this.id = 'AlphaAdverts';
         this.expiry = '2013-11-30';
@@ -9,6 +20,7 @@ define(['bonzo'], function (bonzo) {
         this.description = 'Test new advert formats for aplha release';
         this.canRun = function(config) {
             if(config.page.contentType === 'Article') {
+                guardian.config.oasSiteIdHost = alphaOasUrl;
                 guardian.config.switches.adverts = false;
                 return true;
             } else {
@@ -19,10 +31,17 @@ define(['bonzo'], function (bonzo) {
             {
                 id: 'Inline', //Article A
                 test: function() {
-
-
-
-
+                    var article = document.getElementsByClassName('js-article__container')[0];
+                    bonzo(query('p:nth-of-type('+ nParagraphs +'n)'), article).each(function() {
+                        bonzo(bonzo.create(inlineTmp)).attr({
+                            'data-inview-name' : 'every '+ nParagraphs +'th para',
+                            'data-inview-advert' : 'true',
+                            'data-base' : 'Bottom3',
+                            'data-median' : 'Middle',
+                            'data-extended' : 'Middle'
+                        }).insertAfter(this);
+                    });
+                    inview(document);
                     return true;
                 }
             },
@@ -46,7 +65,7 @@ define(['bonzo'], function (bonzo) {
                     guardian.config.switches.adverts = true;
                     return true;
                 }
-            },
+            }
         ];
     };
 
