e9e79e0912acf52e6d639712375271164f3b0acb
David Vella <david.vella@guardian.co.uk>
2013-10-11 17:10:59 +0100

    Remove gallery tracking

--- common/app/assets/javascripts/bootstraps/gallery.js
+++ common/app/assets/javascripts/bootstraps/gallery.js
@@ -1,33 +1,15 @@
 define([
     "common",
-
-    "modules/lightbox-gallery",
-    "modules/analytics/gallery"
+    "modules/lightbox-gallery"
 ], function(
     common,
-    LightboxGallery,
-    Tracking
+    LightboxGallery
 ) {
 
     var modules = {
         augmentGallery: function () {
             common.mediator.on('page:gallery:ready', function(config, context) {
                 var galleries = new LightboxGallery(config, context).init();
-            });
-        },
-        initOphanTracking:  function(config) {
-            common.mediator.on('page:gallery:ready', function(config, context) {
-                var gallerySize = config.page.gallerySize;
-                if(gallerySize !== "") {
-                    var t = new Tracking({
-                        id: config.page.id,
-                        el : context.querySelector('.js-gallery-holder'),
-                        imageCount: parseInt(gallerySize, 10),
-                        ophanUrl: config.page.ophanUrl
-                    });
-
-                    t.init();
-                }
             });
         }
     };
@@ -37,7 +19,6 @@ define([
             this.initialised = true;
             common.lazyLoadCss('gallery', config);
             modules.augmentGallery();
-            modules.initOphanTracking();
         }
         common.mediator.emit("page:gallery:ready", config, context);
     };
