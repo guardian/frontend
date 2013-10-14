0161dd76c9cd646dee6283dbc03bd687a25f4823
David Vella <david.vella@guardian.co.uk>
2013-10-14 20:11:27 +0100

    Remove redundant code

--- common/app/assets/javascripts/bootstraps/gallery.js
+++ common/app/assets/javascripts/bootstraps/gallery.js
@@ -1,24 +1,12 @@
 define([
-    "common",
-    "modules/lightbox-gallery"
+    "common"
 ], function(
-    common,
-    LightboxGallery
+    common
 ) {
-
-    var modules = {
-        augmentGallery: function () {
-            common.mediator.on('page:gallery:ready', function(config, context) {
-                var galleries = new LightboxGallery(config, context).init();
-            });
-        }
-    };
 
     var ready = function (config, context) {
         if (!this.initialised) {
             this.initialised = true;
-            common.lazyLoadCss('gallery', config);
-            modules.augmentGallery();
         }
         common.mediator.emit("page:gallery:ready", config, context);
     };
