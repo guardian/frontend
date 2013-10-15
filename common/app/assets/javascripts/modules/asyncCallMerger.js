23b6657dbb8f5d3488da1a60a77b0c76797491d0
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2013-10-15 17:03:57 +0100

    added test for asyncCallMerger

--- common/app/assets/javascripts/modules/asyncCallMerger.js
+++ common/app/assets/javascripts/modules/asyncCallMerger.js
@@ -31,7 +31,7 @@ define([], function () {
             } else if(status === "waiting") {
                 callbacks.push(callback);
             } else {
-                callback(callbackArguments);
+                callback.apply(null, callbackArguments);
             }
         }
 
