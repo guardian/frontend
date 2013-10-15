bf39f61dcb63af0dc6223a8dec80f0e8e60ba743
David Vella <dave@deepcobalt.com>
2013-10-15 08:49:26 +0100

    Updating gallery integration tests

--- integration-tests/casper/tests/gallery/gallery.spec.js
+++ integration-tests/casper/tests/gallery/gallery.spec.js
@@ -9,34 +9,59 @@
 
 var casper = require('casper').create();
 
-casper.start(host + "environment/gallery/2013/aug/28/wildlife-photographer-of-the-year-2013-in-pictures?view=mobile");
+casper.start(host + "environment/gallery/2012/oct/18/veolia-environnement-wildlife-photographer-2012-in-pictures?view=mobile");
 
 casper.test.begin("Display gallery contact sheet", function(test) {
-    if (casper.exists('.gallerythumbs')) {
-        test.assertVisible('.gallerythumbs__item');
+    casper.viewport(1024, 768);
+    casper.waitForSelector('.gallery-page .gallerythumbs', function() {
+        test.assertVisible('.gallery-page .gallerythumbs', 'Thumbnails are visible');
+        test.assertEvalEquals(function() {
+            return document.querySelectorAll('.gallery-page .gallerythumbs__item').length;
+        }, 3, '3 items should be available');
         test.done();
-    }
+    });
 });
 
 
-
-casper.test.begin("Display latest summary before events on small viewports", function(test) {
-    if (casper.exists('.js-article__summary')) {
-        casper.viewport(320, 480);
-        test.assertNotVisible('[data-link-name="summary after content"]');
-        test.assertVisible('[data-link-name="summary before content"]');
+casper.test.begin("Display lhe lightbox when clicking an image", function(test) {
+    casper.click('.gallery-page .gallerythumbs__item:first-child a');
+    casper.waitForSelector('.gallery--lightbox', function() {
+        test.assertVisible('.overlay', 'Lightbox did open');
+        test.assertSelectorHasText('.js-image-index', '1', 'Image counter is at 1');
         test.done();
-    }
+    }, function() {
+        test.fail('Lightbox did not open');
+    });
 });
 
-casper.test.begin("Display latest summary on the right side of article on wide viewports", function(test) {
-    if (casper.exists('.js-article__summary')) {
-        casper.viewport(1024, 768);
-        test.assertVisible('[data-link-name="summary after content"]');
-        test.assertNotVisible('[data-link-name="summary before content"]');
-        test.done();
-    }
+
+casper.test.begin("Navigation", function(test) {
+    casper.click('.js-gallery-next');
+    test.assertVisible('.js-gallery-item-2');
+    test.assertSelectorHasText('.js-image-index', '2', 'Image counter is at 2');
+
+    casper.click('.js-gallery-next');
+    test.assertVisible('.js-gallery-item-3');
+    test.assertSelectorHasText('.js-image-index', '3', 'Image counter is at 3');
+
+
+    casper.click('.js-gallery-prev');
+    test.assertVisible('.js-gallery-item-2');
+    test.assertSelectorHasText('.js-image-index', '2', 'Image counter is at 2');
+
+    test.done();
 });
+
+
+casper.test.begin("Caption control", function(test) {
+    test.assertVisible('.gallerycaption', 'Caption starts off visible');
+
+    casper.click('.captioncontrol--hide');
+    test.assertNotVisible('.gallerycaption', 'Caption has been hidden');
+
+    test.done();
+});
+
 
 
 casper.run(function() {
