d32f4ba55520b3f7736195f1f505b09589edbbaa
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2013-10-15 18:55:43 +0100

    Adding tests for idApi calls

--- common/test/assets/javascripts/spec/Id.spec.js
+++ common/test/assets/javascripts/spec/Id.spec.js
@@ -1,10 +1,34 @@
-define(['modules/cookies', 'modules/id'], function(Cookies, Id) {
+define(['modules/cookies', 'modules/id', 'ajax'], function(Cookies, Id, ajax) {
     var cookieData = 'WyIyMzEwOTU5IiwiamdvcnJpZUBnbWFpbC5jb20iLCJqYW1lc2dvcnJpZSIsIjUzNCIsMTM4Mjk1MzAzMTU5MSwxXQ.MC0CFBsFwIEITO91EGONK4puyO2ZgGQcAhUAqRa7PVDCoAjrbnJNYYvMFec4fAY';
-    
-    describe('Get user data', function() {
+    var config = {
+        'page' : {
+            'idApiUrl' : "https://idapi.theguardian.com"
+        }
+    };
 
+    var reqwestReturn = {'then':function(){}}
+
+
+    beforeEach(function () {
+        sinon.stub(ajax, 'reqwest');
+        ajax.reqwest.returns(reqwestReturn);
+        sinon.stub(reqwestReturn, 'then');
+
+        sinon.stub(Cookies, 'get');
+        ajax.init(config);
+        Id.init(config);
+    });
+
+    afterEach(function () {
+        ajax.reqwest.restore();
+        Cookies.get.restore();
+        reqwestReturn.then.restore();
+    });
+
+    describe('Get user data', function() {
         it ('gets user from cookie', function() {
-            Cookies.add('GU_U', cookieData, 365);
+            Cookies.get.withArgs("GU_U").returns(cookieData);
+
             var user = Id.getUserFromCookie();
             expect(user.displayName).toBe('jamesgorrie');
         });
@@ -17,6 +41,28 @@ define(['modules/cookies', 'modules/id'], function(Cookies, Id) {
             expect(decodedString).toBe(string);
         });
 
+        it ('does not call api if not cookie is available', function() {
+            Cookies.get.withArgs("GU_U").returns(cookieData);
+
+            var expectedUser = {};
+
+            reqwestReturn.then.callsArgWith(
+                0,
+                {
+                    'status' : 'ok',
+                    'user' : expectedUser
+                }
+            );
+
+            var apiCallback = sinon.spy();
+            var user = Id.getUserFromApi(apiCallback);
+
+            expect(apiCallback.getCall(0).args[0]).toBe(expectedUser)
+
+            expect(ajax.reqwest.getCall(0).args[0]["url"]).toBe("https://idapi.theguardian.com/user/me?_edition=undefined");
+            expect(ajax.reqwest.getCall(0).args[0]["type"]).toBe("jsonp");
+            expect(ajax.reqwest.getCall(0).args[0]["crossOrigin"]).toBe(true);
+        });
     });
 
 })
