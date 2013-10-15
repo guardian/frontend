b03b1ddc70a39d8379f47f14af0c1a9cafb042f1
Darren Hurley <darren.hurley@guardian.co.uk>
2013-10-15 13:02:40 +0100

    adding tests for colelction show/hide

--- integration-tests/casper/tests/facia/network-front.spec.js
+++ integration-tests/casper/tests/facia/network-front.spec.js
@@ -9,7 +9,7 @@
 casper.start(host + 'uk?view=mobile');
 
 /**
- * Scenario: Users can hide sections
+ * Scenario: Users can hide collections
  *    Given I visit the network front
  *    When I hide a collection
  *    Then the collection will be hidden
@@ -20,11 +20,58 @@ casper.test.begin('Users can hide collections', {
         casper.reload();
     },
     test: function(test) {
-        var collection = '.js-collection--display-toggle',
-            btn = collection + ' .collection__display-toggle';
+        var collection = '.collection--sport',
+            button = collection + ' .collection__display-toggle';
 
-        casper.waitForSelector(btn, function(){
-            test.assertEquals(this.getElementBounds(collection + ' .items').height, 0, 'When I hide a collection then the collection will be hidden');
+        casper.waitForSelector(button, function(){
+            this.click(button);
+            test.assertFalse(this.visible(collection + ' .items'), 'When I hide a collection then the collection will be hidden');
+            test.done();
+        });
+    }
+});
+
+/**
+ * Scenario: Users can show hidden collections
+ *     Given I visit the network front
+ *         And a collection is hidden
+ *     When I show a collection
+ *     Then the collection will be shown
+ **/
+casper.test.begin('Users can show hidden sections', function(test) {
+
+    var collection = '.collection--sport',
+        button = collection + ' .collection__display-toggle';
+
+    casper.waitForSelector(button, function(){
+        casper.click(button);
+        test.assertTrue(this.visible(collection + ' .items'), 'When I show a collection then the collection will be shown');
+        test.done();
+    });
+
+});
+
+/**
+ * Scenario: Collection state should remember user's preference
+ *    Given I've previously closed a collection on the network front
+ *    When I visit the network front
+ *    Then the collection should be closed
+ **/
+casper.test.begin('Users can hide collections', {
+    setUp: function() {
+        clearLocalStorage();
+        casper.reload();
+    },
+    test: function(test) {
+        var collection = '.collection--sport',
+            button = collection + ' .collection__display-toggle';
+
+        casper.waitForSelector(button, function(){
+            this.click(button);
+        });
+        casper.reload();
+        casper.waitForSelector(button, function(){
+            test.assertFalse(this.visible(collection + ' .items'), 'When I show a collection then the collection will be shown');
             test.done();
         });
     }
