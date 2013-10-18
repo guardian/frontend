14fc0ba66a2954a7d10bbb8578be0f96dbae9a73
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-10-18 11:53:50 +0100

    Add tests of travel offers

--- commercial/app/controllers/commercial/TravelOffers.scala
+++ commercial/app/controllers/commercial/TravelOffers.scala
@@ -14,13 +14,13 @@ object TravelOffers extends Controller with ExecutionContexts {
 
   def listOffers = Action {
     implicit request =>
-      val allOffers = OffersAgent.allOffers
-      val offers = request.queryString.get("k").map {
-        keywords => allOffers.filter {
-          offer => (keywords.map(_.toLowerCase).toSet & offer.keywords.map(_.name.toLowerCase).toSet).size > 0
-        }
-      }.getOrElse(allOffers)
-      Ok(views.html.fragments.travelOffer(offers(0), offers(1))) withHeaders ("Cache-Control" -> "max-age=60")
+      val keywords = request.queryString.get("k")
+      val offers = keywords map (OffersAgent.offers(_)) getOrElse OffersAgent.allOffers
+      if (offers.size > 1) {
+        Ok(views.html.fragments.travelOffer(offers(0), offers(1))) withHeaders ("Cache-Control" -> "max-age=60")
+      } else {
+        Ok("No offers") withHeaders ("Cache-Control" -> "max-age=60")
+      }
   }
 
 }
