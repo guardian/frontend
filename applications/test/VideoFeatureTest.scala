d8e4101c3cc5c24a5fdf4cb55636079143bbdaf6
Kaelig <kaelig@deloumeau.fr>
2013-10-21 17:31:45 +0100

    Whitespace

--- applications/test/VideoFeatureTest.scala
+++ applications/test/VideoFeatureTest.scala
@@ -30,7 +30,7 @@ class VideoFeatureTest extends FeatureSpec with GivenWhenThen with Matchers {
         $(".player object") should have size (1)
 
         And("the ultimate fallback should be an image")
-        findFirst(".player object img").getAttribute("src") should endWith("/sys-images/Guardian/Pix/audio/video/2013/8/13/1376401925584/Chloe-Grace-Moretz-talks--016.jpg")
+        findFirst(".player object img").getAttribute("src") should endWith ("/sys-images/Guardian/Pix/audio/video/2013/8/13/1376401925584/Chloe-Grace-Moretz-talks--016.jpg")
       }
     }
   }
