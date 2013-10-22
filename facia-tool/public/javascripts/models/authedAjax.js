3fd4b7727acaf47397dfe0648cff0f25956a7633
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-10-22 18:43:27 +0100

    Bust cache for ajax. Fixes IE9 issue.

--- facia-tool/public/javascripts/models/authedAjax.js
+++ facia-tool/public/javascripts/models/authedAjax.js
@@ -1,7 +1,11 @@
 define([], function () {
     return function (opts) {
         return $.ajax(
-            _.extend({}, opts, {dataType: 'json', contentType: 'application/json'})
+            _.extend({}, opts, {
+                dataType: 'json',
+                contentType: 'application/json',
+                cache: false
+            })
         ).fail(function(xhr) {
             if (xhr.status === 403) {
                 window.location.href = window.location.href;
