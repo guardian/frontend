8bc152598faf494bf16e92fe7caf37cec3fabbbb
Darren Hurley <darren.hurley@guardian.co.uk>
2013-10-24 14:17:01 +0100

    compile straight to target

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,8 @@ module.exports = function (grunt) {
         jasmineSpec = grunt.option('spec') || '*',
         env = grunt.option('env') || 'code',
         screenshotsDir = './screenshots',
-        timestampDir = require('moment')().format('YYYY/MM/DD/HH:mm:ss/');
+        timestampDir = require('moment')().format('YYYY/MM/DD/HH:mm:ss/'),
+        staticTargetDir = 'static/target/';
 
     if (isDev) {
         grunt.log.subhead('Running Grunt in DEV mode');
@@ -23,7 +24,7 @@ module.exports = function (grunt) {
                     expand: true,
                     cwd: 'common/app/assets/stylesheets',
                     src: ['*.scss', '!_*'],
-                    dest: 'static/target/compiled/stylesheets/',
+                    dest: staticTargetDir + 'stylesheets/',
                     rename: function(dest, src) {
                         return dest + src.replace('scss', 'css');
                     }
@@ -47,7 +48,7 @@ module.exports = function (grunt) {
                 options: {
                     baseUrl: "common/app/assets/javascripts",
                     name: "bootstraps/app",
-                    out: "static/target/compiled/javascripts/bootstraps/app.js",
+                    out: staticTargetDir + "javascripts/bootstraps/app.js",
                     paths: {
                         "bean": "components/bean/bean",
                         "bonzo": "components/bonzo/src/bonzo",
@@ -74,7 +75,7 @@ module.exports = function (grunt) {
         webfontjson: {
             WebAgateSansWoff: {
                 options: {
-                    "filename": "static/target/compiled/fonts/WebAgateSans.woff.js",
+                    "filename": staticTargetDir + "fonts/WebAgateSans.woff.js",
                     "callback": "guFont",
                     "fonts": [
                         {
@@ -87,7 +88,7 @@ module.exports = function (grunt) {
             },
             WebAgateSansTtf: {
                 options: {
-                    "filename": "static/target/compiled/fonts/WebAgateSans.ttf.js",
+                    "filename": staticTargetDir + "fonts/WebAgateSans.ttf.js",
                     "callback": "guFont",
                     "fonts": [
                         {
@@ -100,7 +101,7 @@ module.exports = function (grunt) {
             },
             WebEgyptianWoff: {
                 options: {
-                    "filename": "static/target/compiled/fonts/WebEgyptian.woff.js",
+                    "filename": staticTargetDir + "fonts/WebEgyptian.woff.js",
                     "callback": "guFont",
                     "fonts": [
                         {
@@ -145,7 +146,7 @@ module.exports = function (grunt) {
             },
             WebEgyptianTtf: {
                 options: {
-                    "filename": "static/target/compiled/fonts/WebEgyptian.ttf.js",
+                    "filename": staticTargetDir + "fonts/WebEgyptian.ttf.js",
                     "callback": "guFont",
                     "fonts": [
                         {
@@ -197,8 +198,8 @@ module.exports = function (grunt) {
 
         shell: {
             // grunt-mkdir wouldn't do what it was told for this
-            webfontjson: {
-                command: 'mkdir -p static/target/compiled/fonts',
+            mkdirFontTarget: {
+                command: 'mkdir -p ' + staticTargetDir + 'fonts',
 
                 options: {
                     stdout: true,
@@ -207,7 +208,7 @@ module.exports = function (grunt) {
                 }
             },
 
-            icons: {
+            spriteGeneration: {
                 command: [
                     'cd tools/sprites/',
                     'node spricon.js global-icon-config.json'
@@ -235,50 +236,44 @@ module.exports = function (grunt) {
         },
 
         imagemin: {
-            compile: {
-                files: [{
-                    expand: true,
-                    cwd: 'common/app/assets/images/',
-                    src: ['**/*.png'],
-                    dest: 'static/target/compiled/images/'
-                },{
-                    expand: true,
-                    cwd: 'static/target/generated/images/',
-                    src: ['**/*.{png,gif,jpg}'],
-                    dest: 'static/target/compiled/images/'
-                },{
-                    expand: true,
-                    cwd: 'common/app/public/images/',
-                    src: ['**/*.{png,gif,jpg}', '!favicons/windows_tile_144_b.png'],
-                    dest: 'static/target/compiled/images/'
-                }]
+            files: {
+                expand: true,
+                cwd: staticTargetDir + 'images/',
+                src: ['**/*.{png,gif,jpg}', '!favicons/windows_tile_144_b.png'],
+                dest: staticTargetDir + 'images/'
             }
         },
 
         copy: {
-            compile: {
+            publicImages: {
                 files: [{
                     expand: true,
-                    cwd: 'common/app/assets/images',
-                    src: ['**/*.ico'],
-                    dest: 'static/target/compiled/images'
-                },{
-                    expand: true,
-                    cwd: 'common/app/public/',
+                    cwd: 'common/app/public/images',
                     src: ['**/*'],
-                    dest: 'static/target/compiled/'
-                },{
+                    dest: staticTargetDir
+                }]
+            },
+            publicJs: {
+                files: [{
+                    expand: true,
+                    cwd: 'common/app/public/javascript',
+                    src: ['**/*'],
+                    dest: staticTargetDir
+                }]
+            },
+            flash: {
+                files: [{
                     expand: true,
                     cwd: 'common/app/assets/flash',
                     src: ['**/*.swf'],
-                    dest: 'static/target/compiled/flash'
+                    dest: staticTargetDir + 'flash'
                 }]
             },
             headCss: {
                 files: [{
                     expand: true,
-                    cwd: 'static/target/compiled/stylesheets',
-                    src: ['**/*.css'],
+                    cwd: 'static/target/hashed/stylesheets',
+                    src: ['**/head*.css'],
                     dest: 'common/conf/assets'
                 }]
             }
@@ -289,18 +284,18 @@ module.exports = function (grunt) {
                 // assets.map must go where Play can find it from resources at runtime.
                 // Everything else goes into frontend-static bundling.
                 mapping: 'common/conf/assets/assets.map',
-                srcBasePath: 'static/target/compiled',
-                destBasePath: 'static/target/hashed',
+                srcBasePath: staticTargetDir,
+                destBasePath: staticTargetDir,
                 flatten: false,
                 hashLength: (isDev) ? 0 : 32
             },
 
             files: {
                 expand: true,
-                cwd: 'static/target/compiled/',
+                cwd: staticTargetDir,
                 src: '**/*',
                 filter: 'isFile',
-                dest: 'static/target/hashed/',
+                dest: staticTargetDir,
                 rename: function(dest, src) {
                     // remove .. when hash length is 0
                     return dest + src.split('/').slice(0, -1).join('/');
@@ -419,7 +414,6 @@ module.exports = function (grunt) {
             ],
             common: [
                 'common/app/assets/javascripts/bootstraps/*.js',
-                'common/app/assets/javascripts/modules/*.js',
                 'common/app/assets/javascripts/modules/**/*.js'
             ]
         },
@@ -489,7 +483,7 @@ module.exports = function (grunt) {
          */
         cssmetrics: {
             common: {
-                src: ['static/target/compiled/stylesheets/*.min.css'],
+                src: [staticTargetDir + 'stylesheets/*.css'],
                 options: {
                     quiet: false,
                     maxRules: 4096, //IE max rules
@@ -523,11 +517,8 @@ module.exports = function (grunt) {
         // Clean stuff up
         clean: {
             compile: [
-                'static/target',
-                'common/conf/assets/head.min.css',
-                'common/conf/assets/head.facia.min.css',
-                'common/conf/assets/head.identity.min.css',
-                'common/conf/assets/assets.map'
+                staticTargetDir,
+                'common/conf/assets'
             ],
 
             // Clean any pre-commit hooks in .git/hooks directory
@@ -560,13 +551,13 @@ module.exports = function (grunt) {
             cssSourceMaps: {
                 files: [{
                   expand: true,
-                  cwd: 'static/target/compiled/stylesheets',
+                  cwd: staticTargetDir + 'stylesheets',
                   src: ['**/*.map'],
-                  dest: 'static/target/compiled/stylesheets'
+                  dest: staticTargetDir + 'stylesheets'
                 }],
                 options: {
                   replacements: [{
-                    pattern: /\.\.\/\.\.\/\.\.\/\.\.\/common\/app\/assets\/stylesheets\//g,
+                    pattern: /\.\.\/\.\.\/\.\.\/common\/app\/assets\/stylesheets\//g,
                     replacement: ''
                   }]
                 }
@@ -598,17 +589,20 @@ module.exports = function (grunt) {
 
     // Compile tasks
     grunt.registerTask('compile', [
-        'sass:compile',
-        'requirejs:compile',
-        'shell:webfontjson',
-        'webfontjson',
-        'shell:icons',
-        'imagemin:compile',
-        'copy:compile',
+        'compile:images',
+        'compile:css',
+        'compile:js',
+        'compile:fonts',
+        'compile:flash',
+        // below should not run in dev
         'copy:headCss',
         'hash'
     ]);
-    grunt.registerTask('compile:css', ['sass:compile', 'string-replace:cssSourceMaps', 'copy:headCss', 'hash']);
+    grunt.registerTask('compile:images', ['copy:publicImages', 'shell:spriteGeneration', 'imagemin']);
+    grunt.registerTask('compile:css', ['sass:compile', 'string-replace:cssSourceMaps']);
+    grunt.registerTask('compile:js', ['copy:publicJs', 'requirejs:compile']);
+    grunt.registerTask('compile:fonts', ['shell:mkdirFontTarget', 'webfontjson']);
+    grunt.registerTask('compile:flash', ['copy:flash']);
 
     // Test tasks
     grunt.registerTask('test:integration', function(app) {
