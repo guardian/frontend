60e58f9fe9dbfc570608c715a9345614fc384027
Adam Fisher <adam.fisher@guardian.co.uk>
2013-10-25 15:08:47 +0100

    Simplify Auth implementations and add ScGuU provider

--- identity/app/idapiclient/Auth.scala
+++ identity/app/idapiclient/Auth.scala
@@ -5,35 +5,33 @@ import client.{Auth, Parameters}
 
 case class EmailPassword(email: String, password: String) extends Auth {
   override def parameters: Parameters = List(("email", email), ("password", password))
-  override def headers: Parameters = Iterable.empty
 }
 
 case class UserToken(userAccessToken: String) extends Auth {
   override def parameters: Parameters = List(("accessToken", userAccessToken))
-  override def headers: Parameters = Iterable.empty
 }
 
 case class UserCookie(cookieValue: String) extends Auth {
   override def parameters: Parameters = List(("SC_GU_U", cookieValue))
-  override def headers: Parameters = Iterable.empty
 }
 
 case class UserTokenExchange(userAccessToken: String, clientId: String) extends Auth {
   override def parameters: Parameters = List(("user-access-token", userAccessToken), ("target-client-id", clientId))
-  override def headers: Parameters = Iterable.empty
 }
 
 abstract class SocialAccessToken(parameterName: String, accessToken: String) extends Auth {
   override def parameters: Parameters = List((parameterName, accessToken))
-  override def headers: Parameters = Iterable.empty
 }
 case class FacebookToken(accessToken: String) extends SocialAccessToken("facebook-access-token", accessToken)
 
 case class GoogleToken(accessToken: String) extends SocialAccessToken("google-access-token", accessToken)
 
 case class ClientAuth(clientAccessToken: String) extends Auth {
-  def parameters: Parameters = List(("accessToken", clientAccessToken))
-  override def headers: Parameters = Iterable.empty
+  override def parameters: Parameters = List(("accessToken", clientAccessToken))
+}
+
+class ScGuU(scGuUValue: String) extends Auth {
+  override def headers: client.Parameters = Iterable("X-GU-ID-FOWARDED-SC-GU-U" -> scGuUValue)
 }
 
 case class TrackingData(returnUrl:Option[String], registrationType: Option[String], omnitureSVi: Option[String],
