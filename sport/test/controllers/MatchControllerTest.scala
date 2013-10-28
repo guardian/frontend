13adfc5cba7e2bd57421606a2b9f7c84f961bd76
grant Klopper <grant.klopper@gmail.com>
2013-10-28 17:28:03 +0000

    Describe the test better

--- sport/test/controllers/MatchControllerTest.scala
+++ sport/test/controllers/MatchControllerTest.scala
@@ -7,7 +7,7 @@ import test.{TestRequest, Fake}
 
 class MatchControllerTest extends FlatSpec with Matchers {
   
-  "MatchController" should "redirect when content match is not found" in Fake {
+  "MatchController" should "redirect to results when match is not found" in Fake {
     val result = football.controllers.MatchController.renderMatchId("12345")(TestRequest())
     status(result) should be(302)
     header("Location", result).head should be ("/football/results")
