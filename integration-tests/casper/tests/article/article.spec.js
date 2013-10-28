dd37b3e7bc759aabb18f1c6aadef6607cfae8a84
Marc Jones <marc.jones@guardian.co.uk>
2013-10-28 12:32:33 +0000

    Remove extra tests which duplicate html unit tests

--- integration-tests/casper/tests/article/article.spec.js
+++ integration-tests/casper/tests/article/article.spec.js
@@ -10,33 +10,6 @@
 casper.start(host + "business/2010/feb/08/fsa-european-directive-hedge-funds?view=mobile");
 
 casper.then(function() {
-
-	casper.test.begin("Article head", function(test) {
-		test.assertVisible('a[data-link-name="article section"]', 'Section link visible');
-		test.assertVisible('.article__head h1.article__headline', 'Headline visible');
-		test.assertVisible('.article__head p.article__standfirst', 'Standfirst visible');
-		test.assertVisible('.article__head p.article__dateline time', 'Publish date visible');
-		test.assertVisible('.article__head p.article__dateline a.commentcount', 'Comment count visible');
-		test.done();
-	});
-
-	casper.test.begin("Article main content", function(test) {
-		test.assertVisible('img.main-image', 'Article main image visible');
-		test.assertVisible('figcaption.main-caption', 'Main image caption visible');
-		test.assertVisible('.article__meta-container p.byline', 'Byline visible');
-		test.assertVisible('.js-article__container .article-body', 'Article body visible');
-		test.assertVisible('.article__keywords ul.inline-list', 'Keyword list visible');
-		test.done();
-	});
-
-	casper.test.begin("Article share links", function(test) {
-		test.assertVisible('.article-v2__main-column a.social__action--email', 'Email share link visible');
-		test.assertVisible('.article-v2__main-column a.social__action--facebook', 'Facebook share link visible');
-		test.assertVisible('.article-v2__main-column a.social__action--twitter', 'Twitter share link visible');
-		test.assertVisible('.article-v2__main-column a.social__action--gplus', 'Google+ share link visible');
-		test.done();
-	});
-
 	casper.test.begin("Related content", function(test) {
 		test.assertVisible('.js-related', 'Related content trailblock visible');
 		test.assertExists('.js-related .related-trails.shut', 'Additional related trails hidden on page load');
