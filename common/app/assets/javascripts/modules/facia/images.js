2e3fc8ae311dd8437e218349953dbbc33db3802f
Darren Hurley <darren.hurley@guardian.co.uk>
2013-10-28 16:41:43 +0000

    no delay; using

--- common/app/assets/javascripts/modules/facia/images.js
+++ common/app/assets/javascripts/modules/facia/images.js
@@ -7,14 +7,14 @@ define(['common', 'bonzo'], function (common, bonzo) {
                 return;
             }
             require(['js!imager'], function() {
-                context = context || 'body';
-                var images = common.toArray(document.querySelectorAll('.item__image-container'), context).filter(function(img) {
+                context = context || document;
+                var images = common.toArray(document.getElementsByClassName('item__image-container')).filter(function(img) {
                         return bonzo(img).css('display') !== 'none';
                     }),
                     options = {
                         availableWidths: [ 140, 220, 300, 460, 620, 700 ],
                         strategy: 'container',
-                        replacementDelay: 200
+                        replacementDelay: 0
                     };
                 Imager.init(images, options);
             });
