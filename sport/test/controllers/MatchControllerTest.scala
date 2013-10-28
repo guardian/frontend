0fa2a56fdf658d6821d9cc97529936c1cf5f9c4b
grant Klopper <grant.klopper@gmail.com>
2013-10-28 17:22:07 +0000

    We do not keep old match data, so if we cannot find the match simply redirect to /football/results

---
+++ sport/test/controllers/MatchControllerTest.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api.test.Helpers._
+import org.scalatest.Matchers
+import org.scalatest.FlatSpec
+import test.{TestRequest, Fake}
+
+class MatchControllerTest extends FlatSpec with Matchers {
+  
+  "MatchController" should "redirect when content match is not found" in Fake {
+    val result = football.controllers.MatchController.renderMatchId("12345")(TestRequest())
+    status(result) should be(302)
+    header("Location", result).head should be ("/football/results")
+  }
+}
