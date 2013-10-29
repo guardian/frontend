513e2495fcc38576d0ca9f67640bdd57612bee15
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2013-10-29 16:03:04 +0000

    adding signin protection to profile

--- identity/app/utils/ApiUserDataAction.scala
+++ identity/app/utils/ApiUserDataAction.scala
@@ -6,39 +6,41 @@ import scala.concurrent.{ExecutionContext, Future}
 import play.api.mvc.SimpleResult
 import play.api.mvc.Results
 import idapiclient.{IdApiClient, UserCookie}
-import client.{Response, Error}
+import client.{Auth, Response, Error}
 import play.api.mvc.Results._
 import com.google.inject.Inject
 import services.{IdentityRequest, IdRequestParser}
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class UserFromApiActionBuilder @Inject()(apiUserDataActionHandler:UserApiDataRequestHandler, idRequestParser: IdRequestParser) extends ActionBuilder[UserFromApiRequest] with Results {
+class UserFromApiActionBuilder @Inject()(apiUserDataActionHandler:UserApiDataRequestHandler, idRequestParser: IdRequestParser, authService: AuthenticationService) extends ActionBuilder[UserFromApiRequest] with Results {
   protected def invokeBlock[A](request: Request[A], block: (UserFromApiRequest[A]) => Future[SimpleResult]) = {
-    apiUserDataActionHandler
-      .handleRequest(request)
-      .flatMap { userResponse =>
-        userResponse.fold (
-          { Future.successful(_) },
-          { user => block(UserFromApiRequest(user, idRequestParser(request), request, UserCookie(request.cookies("SC_GU_U").value))) }
-      )
-    }
+    val authResult = authService.handleAuthenticatedRequest(request);
+    val idRequest = idRequestParser(request)
+
+    authResult.fold[Future[SimpleResult]](
+      { error =>
+          Future.successful(error)},
+      { auth =>
+        apiUserDataActionHandler.handleRequest(request, auth).flatMap {
+          _.fold[Future[SimpleResult]](
+            { error => Future.successful(error) },
+            { user => block(UserFromApiRequest(user, idRequest, request, auth.auth)) }
+          )
+        }
+      }
+    )
   }
 }
 
 class UserApiDataRequestHandler @Inject()(identityApiClient: IdApiClient) {
-  def handleRequest(request: Request[_]): Future[Either[SimpleResult, User]] = {
-    request
-      .cookies
-      .get("SC_GU_U")
-      .map { scGuUCookie =>
-      identityApiClient.me(UserCookie(scGuUCookie.value)).map(apiResponse =>
-        apiResponse.left.map {errors =>
-          InternalServerError
-        }
-      )
-    }.get
+  def handleRequest[A](request: Request[A], authRequest: AuthRequest[A]): Future[Either[SimpleResult, User]] = {
+    identityApiClient.me(authRequest.auth).map(apiResponse =>
+      apiResponse.left.map {errors =>
+        InternalServerError
+      }
+    )
   }
 }
 
 
-case class UserFromApiRequest[A] (user:User, identityRequest: IdentityRequest, request: Request[A], userAuth: UserCookie) extends WrappedRequest[A](request)
+case class UserFromApiRequest[A] (user:User, identityRequest: IdentityRequest, request: Request[A], userAuth: Auth) extends WrappedRequest[A](request)
--- identity/app/utils/AuthAction.scala
+++ identity/app/utils/AuthAction.scala
@@ -8,7 +8,7 @@ import com.google.inject.{Singleton, Inject}
 import play.api.mvc.Results._
 import services.{IdentityUrlBuilder, IdRequestParser}
 import java.net.URLEncoder
-import client.Auth
+import client.{Response, Logging, Auth}
 import idapiclient.ScGuU
 import scala.Some
 import play.api.mvc.SimpleResult
@@ -17,12 +17,23 @@ import play.api.mvc.SimpleResult
 case class AuthRequest[A](request: Request[A], user: User, auth: Auth) extends WrappedRequest(request)
 
 @Singleton
-class AuthAction @Inject()(cookieDecoder: FrontendIdentityCookieDecoder,
-                           idRequestParser: IdRequestParser,
-                           identityUrlBuilder: IdentityUrlBuilder)
+class AuthAction @Inject()(authService: AuthenticationService)
   extends ActionBuilder[AuthRequest] with SafeLogging {
 
   protected def invokeBlock[A](request: Request[A], block: (AuthRequest[A]) => Future[SimpleResult]) = {
+    authService.handleAuthenticatedRequest(request).fold(
+      { error => Future.successful(error) },
+      { auth => block(auth)}
+    )
+  }
+}
+
+
+@Singleton
+class AuthenticationService @Inject()(cookieDecoder: FrontendIdentityCookieDecoder,
+                            idRequestParser: IdRequestParser,
+                            identityUrlBuilder: IdentityUrlBuilder) extends Logging {
+  def handleAuthenticatedRequest[A](request: Request[A]): Either[SimpleResult, AuthRequest[A]] = {
     request.cookies.get("SC_GU_U").flatMap { cookie =>
       cookieDecoder.getUserDataForScGuU(cookie.value).map { user =>
         AuthRequest(request, user, new ScGuU(cookie.value))
@@ -30,12 +41,12 @@ class AuthAction @Inject()(cookieDecoder: FrontendIdentityCookieDecoder,
     } match {
       case Some(authRequest) => {
         logger.trace("user is logged in")
-        block(authRequest)
+        Right(authRequest)
       }
       case None => {
         logger.debug("No user logged in, redirecting to signin")
         val returnUrl = URLEncoder.encode(identityUrlBuilder.buildUrl(request.uri), "UTF-8")
-        Future.successful(SeeOther(identityUrlBuilder.buildUrl(s"/signin?returnUrl=$returnUrl")))
+        Left(SeeOther(identityUrlBuilder.buildUrl(s"/signin?returnUrl=$returnUrl")))
       }
     }
   }
