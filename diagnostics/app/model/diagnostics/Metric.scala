3e1380c11fd7f353683e5676292ab19e17a4d77e
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-10-31 20:31:20 +0000

    Use a singleton for maintaining the Metric state

--- diagnostics/app/model/diagnostics/Metric.scala
+++ diagnostics/app/model/diagnostics/Metric.scala
@@ -5,28 +5,21 @@ import common.{Logging}
 import play.cache._
 
 object Metric extends Logging {
-  
+
+  var i = 0
+
   def increment(prefix: String) = {
-    
-    val now = new DateTime().toString("HHmm")
-    val cacheKey = s"${prefix}.${now}"
-    val expiry = 300
-
-    log.info(s"${cacheKey}")
-
-    val count = Cache.get(cacheKey)
-
-    if (count == null){
-      Cache.set(cacheKey, 1, expiry)
-    } else {
-      Cache.set(cacheKey, count.toString.toInt + 1, expiry)
-    }
-    
-    count
+    i = i + 1
+    log.info(s"${i}")
+    i
   }
 
   def get(key: String) = {
-    Cache.get(key)
+    i
+  }
+
+  def reset(key: String) = {
+    i = 0
   }
 
 }
