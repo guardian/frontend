1a5f8dda01cc16e8f4bfe0f3f35a1b0a629db976
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-11-01 08:17:14 +0000

    Only load data to CloudWatch in production

--- diagnostics/app/model/diagnostics/DiagnosticsLoadJob.scala
+++ diagnostics/app/model/diagnostics/DiagnosticsLoadJob.scala
@@ -8,14 +8,13 @@ object DiagnosticsLoadJob extends Logging {
 
   def run() {
     
-    /*log.info("Uploading %d new metric data points" format fresh.size)
-        Configuration.environment.stage.toUpperCase match {
-                case "PROD" => CloudWatch.put("Fastly", fresh)
-    case _ => log.info("DISABLED: Metrics uploaded in PROD only to limit duplication.")
-        }*/
-
     log.info("Loading diagnostics data in to CloudWatch")
-    CloudWatch.put("Diagnostics", Metric.all)
+    Configuration.environment.stage.toUpperCase match {
+      case "PROD" => CloudWatch.put("Diagnostics", Metric.all)
+      case _ => log.info("DISABLED: Metrics uploaded in PROD only to limit duplication.")
+    }
+    
+    
 
   }
 
