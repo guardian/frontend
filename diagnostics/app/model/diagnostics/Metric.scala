57bbbdb64f1f6619a6c3a55b7213cdb7dcbe50e6
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-11-01 07:24:15 +0000

    Do a proper job of clearing the map

--- diagnostics/app/model/diagnostics/Metric.scala
+++ diagnostics/app/model/diagnostics/Metric.scala
@@ -2,24 +2,23 @@ package model.diagnostics
 
 import org.joda.time.DateTime
 import common.{Logging}
-import play.cache._
 
 object Metric extends Logging {
 
   val metrics = collection.mutable.Map[String, Int]().withDefaultValue(0)
-  
+
   def increment(prefix: String) = {
     metrics.update(prefix, metrics(prefix) + 1)
     log.info(s"${prefix} - ${metrics(prefix)}")
     metrics(prefix)
-  }
+  } 
 
   def count(prefix: String) = {
     metrics(prefix)
   }
 
   def reset = {
-    metrics.drop(100) // FIXME clear the Map nicely
+    metrics.foreach(m => metrics.remove(m._1))
   }
 
 }
