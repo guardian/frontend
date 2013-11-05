7fd4fff1a9f16886df0198610df3ceb1558d780b
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-11-05 19:38:41 +0000

    Recast things to a User model

--- diagnostics/app/model/diagnostics/User.scala
+++ diagnostics/app/model/diagnostics/User.scala
@@ -4,13 +4,22 @@ import common.Logging
 import java.util.concurrent.ConcurrentHashMap
 import com.google.common.util.concurrent.AtomicDouble
 
-object Session extends Logging {
-
+class User {
+  
   private lazy val metric = new AtomicDouble()
 
   def increment = {
     metric.addAndGet(1.0)
   }
+  
+  def count = { 
+    metric.get
+  }
 
   def reset() {}
+
 }
+
+object View extends User with Logging {} 
+object Session extends User with Logging {}
+
