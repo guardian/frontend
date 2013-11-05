923f5722b4ba9048ed27b65795695675606e0254
Richard Nguyen <richard.nguyen@guardian.co.uk>
2013-11-05 16:41:14 +0000

    Add web socket for detect, defend websocket data

--- common/app/assets/javascripts/modules/content-websocket.js
+++ common/app/assets/javascripts/modules/content-websocket.js
@@ -1,22 +1,30 @@
 define([
     'common',
-    'bonzo'
-], function (common, bonzo) {
+    'bonzo',
+    'modules/detect'
+], function (common, bonzo, detect) {
 
     function init(config, context, options) {
 
-        var WS = window.MozWebSocket ? window.MozWebSocket : WebSocket;
-        var chatSocket = new WS(config.page.onwardWebSocket);
+        if (!detect.hasWebSocket()) {
+            return;
+        }
+
+        var chatSocket = new window.WebSocket(config.page.onwardWebSocket);
 
         var receiveEvent = function(event) {
-            var data = JSON.parse(event.data);
+            if (event && 'data' in event) {
+                var data = JSON.parse(event.data);
 
-            if (data.error) {
-                chatSocket.close();
+                if (data.error) {
+                    chatSocket.close();
+                } else {
+                    var $pushedContent = bonzo.create('<div>' + data.headline + ' ' + data.url + '</div>');
+                    bonzo($pushedContent).addClass('pushed-content lazyloaded');
+                    common.$g(".monocolumn-wrapper").after($pushedContent);
+                }
             } else {
-                var $pushedContent = bonzo.create('<div>' + data.headline + ' ' + data.url + '</div>');
-                bonzo($pushedContent).addClass('pushed-content lazyloaded');
-                common.$g(".monocolumn-wrapper").after($pushedContent);
+                common.mediator.emit('module:error', 'Invalid data returned from socket, module/websocket.js');
             }
         };
 
