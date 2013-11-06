f887410944a54640d466f17966c9d7bc9ed592a0
Darren Hurley <darren.hurley@guardian.co.uk>
2013-11-06 13:31:21 +0000

    fixing test selectors

--- integration-tests/casper/tests/applications/tag.spec.js
+++ integration-tests/casper/tests/applications/tag.spec.js
@@ -1,20 +1,20 @@
 /**
  *
- * Network front feature tests
+ * Tag page feature tests
  *
  **/
 casper.test.setUp(function() {
-    casper.start(host + 'uk/culture?view=mobile', function() {
+    casper.start(host + 'profile/charliebrooker?view=mobile', function() {
         clearLocalStorage();
     });
 });
 
 /**
 * Scenario: Popular container appears at the bottom of the page
-*    Given I visit the network front
+*    Given I visit a tag page
 *    Then I should see the popular container
 **/
-casper.test.begin('Popular collection appears at the bottom of the page', function(test) {
+casper.test.begin('Popular container appears at the bottom of the page', function(test) {
     var popContainerSelector = '.container--popular';
     casper.waitForSelector(popContainerSelector, function(){
         test.assertElementCount(popContainerSelector, 1, 'exactly one popular container should be displayed');
