98a170267ae6e6f0bea74bce4aefb440ddabee87
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2013-11-06 15:55:02 +0000

    adds tests for autosignin and facebook-authorizer

--- common/test/assets/javascripts/spec/identity/FacebookAuthorizer.spec.js
+++ common/test/assets/javascripts/spec/identity/FacebookAuthorizer.spec.js
@@ -11,6 +11,8 @@ define([
 
         beforeEach(function() {
 
+            userDetailsCallBack = sinon.stub();
+
             authorizer = new FacebookAuthorizer("123");
 
             authorizer._loadFacebookScript = function () {
@@ -29,16 +31,8 @@ define([
                 };
             };
 
-            authorizer._handleScriptLoaded = function() {
-                console.log("Handle");
-                window.FB.init();
-                this.onFBScriptLoaded.resolve(window.FB);
-            }
-
-            //authorizer.onFBScriptLoaded = function() { console.log("Resolve"); };
-           // authorizer.onUserDataLoaded.then(userDetailsCallBack);
-            //userDetailsCallBack = sinon.stub();
-            //sinon.spy(authorizer, "getLoginStatus");
+            authorizer.onUserDataLoaded.then(userDetailsCallBack);
+            sinon.spy(authorizer, "getLoginStatus");
         });
 
         afterEach(function() {
@@ -49,7 +43,6 @@ define([
         });
 
         function whenTheScriptLoads() {
-            console.log("+++++++++++++  Load");
            authorizer._handleScriptLoaded();
         }
 
@@ -58,7 +51,7 @@ define([
                 authorizer.getLoginStatus();
                 console.log(window.FB);
                 whenTheScriptLoads();
-                expect(window.FB.init.callCount).toBe(1);
+                expect(FB.init.callCount).toBe(1);
             });
 
             it('Does not load the facebook script or call FB.init more than once', function() {
@@ -67,6 +60,82 @@ define([
                 expect(FB.init.callCount).toEqual(1);
                 authorizer.getLoginStatus();    1
                 expect(FB.init.callCount).toEqual(1);
+            });
+
+            it('Should queue calls to get login status', function() {
+
+                var callback1 = sinon.stub(), callback2 = sinon.stub();
+                loginResponse = {
+                    status: 'connected',
+                    authResponse: {
+                        accessToken: '123',
+                        userID: '123456'
+                    }
+                };
+
+                authorizer.getLoginStatus().then(callback1);
+
+                authorizer.getLoginStatus().then(callback2);
+
+                whenTheScriptLoads();
+
+                expect(callback1.callCount).toEqual(1);
+                expect(callback1.getCall(0).args[0]).toBe(window.FB);
+                expect(callback1.getCall(0).args[1]).toBe(loginResponse.authResponse);
+
+                expect(callback2.callCount).toEqual(1);
+                expect(callback2.getCall(0).args[0]).toBe(window.FB);
+                expect(callback1.getCall(0).args[1]).toBe(loginResponse.authResponse);
+            });
+
+            it("Should load facebook api if requested to auth the user", function() {
+
+                loginResponse = {
+                    status: 'connected',
+                    authResponse: {
+                        accessToken: '123',
+                        userID: '123456'
+                    }
+                };
+
+                authorizer.login();
+                whenTheScriptLoads();
+
+                expect(FB.init.callCount).toEqual(1);
+                expect(authorizer.accessToken).toEqual('123');
+                expect(authorizer.userId).toEqual('123456');
+            });
+
+            it("should not try to login to facebook more than once", function() {
+                authorizer.login();
+                authorizer.login();
+
+                whenTheScriptLoads();
+                expect(FB.login.callCount).toEqual(1);
+            });
+
+            it("Should get the user data from facebook", function() {
+                loginResponse = { status : 'not_authorized'};
+
+                userData = {"name" : "Scala refugee"};
+
+                authorizer.getLoginStatus();
+
+                whenTheScriptLoads();
+
+                expect(userDetailsCallBack.callCount).toEqual(1);
+                expect(userDetailsCallBack.getCall(0).args[0]).toBe(userData);
+            });
+
+            it("Should not get the user data user when an error occurs", function() {
+                loginResponse = { status : 'not_authorized'};
+
+                userData = {"error" : "badness happened"};
+
+                authorizer.getLoginStatus();
+
+                expect(userDetailsCallBack.callCount).toEqual(0);
+
             });
         });
 
