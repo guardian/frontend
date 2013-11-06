070a18a27b30d702d8e3087cd643c598243150dd
Grant Klopper <grant.klopper@guardian.co.uk>
2013-11-06 14:15:50 +0000

    Changed terminology
    r2 -> desktop
    nextgen -> responsive

--- diagnostics/app/model/diagnostics/DiagnosticsLoadJob.scala
+++ diagnostics/app/model/diagnostics/DiagnosticsLoadJob.scala
@@ -13,16 +13,16 @@ object DiagnosticsLoadJob extends Logging {
     log.info("Loading diagnostics data in to CloudWatch")
     Configuration.environment.stage.toUpperCase match {
       case "PROD" => {
-        CloudWatch.put( "Diagnostics", Metric.averages ++ 
-                        Map("views.r2" -> R2View.count) ++
-                        Map("viewsOverSessions.r2" -> R2View.count / R2Session.count) ++
-                        Map("views.nextgen" -> NextGenView.count) ++
-                        Map("viewsOverSessions.nextgen" -> NextGenView.count / NextGenSession.count))
+        CloudWatch.put( "Diagnostics", Metric.averages ++ Map(
+                        "views.desktop" -> DesktopView.count,
+                        "viewsOverSessions.desktop" -> DesktopView.count / DesktopSession.count,
+                        "views.responsive" -> ResponsiveView.count,
+                        "viewsOverSessions.responsive" -> ResponsiveView.count / ResponsiveSession.count))
         Metric.reset()
-        NextGenView.reset()
-        NextGenSession.reset()
-        R2View.reset()
-        R2Session.reset()
+        ResponsiveView.reset()
+        ResponsiveSession.reset()
+        DesktopView.reset()
+        DesktopSession.reset()
       }
       case _ => log.info("DISABLED: Metrics uploaded in PROD only to limit duplication.")
     }
