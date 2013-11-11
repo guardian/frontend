04ea02cf83042525ff9016f41ddd614e470049c7
David Vella <david.vella@guardian.co.uk>
2013-11-11 16:42:29 +0000

    Travel component to come out as JSON

--- commercial/app/controllers/commercial/TravelOffers.scala
+++ commercial/app/controllers/commercial/TravelOffers.scala
@@ -2,7 +2,7 @@ package controllers.commercial
 
 import play.api.mvc._
 import model.commercial.travel.OffersAgent
-import common.ExecutionContexts
+import common.{JsonComponent, ExecutionContexts}
 
 object TravelOffers extends Controller with ExecutionContexts with ExpectsSegmentInRequests {
 
@@ -11,7 +11,9 @@ object TravelOffers extends Controller with ExecutionContexts with ExpectsSegmen
       val offers = OffersAgent.matchingAds(segment)
       if (offers.size > 1) {
         val view = views.html.fragments.travelOffer(offers)
-        Ok(view) withHeaders ("Cache-Control" -> "max-age=60")
+        JsonComponent {
+          "html" -> views.html.fragments.travelOffer(offers)
+        } withHeaders ("Cache-Control" -> "max-age=60")
       } else {
         Ok("No offers") withHeaders ("Cache-Control" -> "max-age=60")
       }
