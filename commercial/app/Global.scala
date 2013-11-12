d1de8f642ad292c0301f34369b308973ee5a751b
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-11-12 16:02:21 +0000

    Schedule refresh of ad feeds

--- commercial/app/Global.scala
+++ commercial/app/Global.scala
@@ -1,39 +1,57 @@
-import common.{AkkaAsync, CommercialMetrics, Jobs}
+import common.{Logging, AkkaAsync, CommercialMetrics, Jobs}
 import conf.RequestMeasurementMetrics
 import dev.DevParametersLifecycle
 import model.commercial.jobs.JobsAgent
+import model.commercial.soulmates.SoulmatesAgent
 import model.commercial.travel.OffersAgent
 import play.api.mvc.WithFilters
 import play.api.{Application => PlayApp, GlobalSettings}
+import scala.util.Random
 
-trait CommercialLifecycle extends GlobalSettings {
+trait CommercialLifecycle extends GlobalSettings with Logging {
 
   override def onStart(app: PlayApp) {
+
+    def randomStartSchedule = s"0 ${Random.nextInt(60)}/15 * * * ?"
+
     super.onStart(app)
 
     Jobs.deschedule("TravelOffersRefreshJob")
     Jobs.deschedule("JobsRefreshJob")
-
+    Jobs.deschedule("SoulmatesRefreshJob")
 
     // fire every 15 mins
-    Jobs.schedule("TravelOffersRefreshJob", "0 2/15 * * * ?", CommercialMetrics.TravelOffersLoadTimingMetric) {
+    val travelRefreshSchedule = randomStartSchedule
+    log.info(s"Travel offers refresh on schedule $travelRefreshSchedule")
+    Jobs.schedule("TravelOffersRefreshJob", travelRefreshSchedule, CommercialMetrics.TravelOffersLoadTimingMetric) {
       OffersAgent.refresh()
     }
 
     // fire every 15 mins
-    Jobs.schedule("JobsRefreshJob", "0 7/15 * * * ?", CommercialMetrics.JobsLoadTimingMetric) {
+    val jobsRefreshSchedule = randomStartSchedule
+    log.info(s"Jobs refresh on schedule $jobsRefreshSchedule")
+    Jobs.schedule("JobsRefreshJob", jobsRefreshSchedule, CommercialMetrics.JobsLoadTimingMetric) {
       JobsAgent.refresh()
     }
 
-    AkkaAsync{
+    // fire every 15 mins
+    val soulmatesRefreshSchedule = randomStartSchedule
+    log.info(s"Soulmates refresh on schedule $soulmatesRefreshSchedule")
+    Jobs.schedule("SoulmatesRefreshJob", soulmatesRefreshSchedule, CommercialMetrics.SoulmatesLoadTimingMetric) {
+      SoulmatesAgent.refresh()
+    }
+
+    AkkaAsync {
       OffersAgent.refresh()
       JobsAgent.refresh()
+      SoulmatesAgent.refresh()
     }
   }
 
   override def onStop(app: PlayApp) {
     Jobs.deschedule("TravelOffersRefreshJob")
     Jobs.deschedule("JobsRefreshJob")
+    Jobs.deschedule("SoulmatesRefreshJob")
     super.onStop(app)
   }
 }
