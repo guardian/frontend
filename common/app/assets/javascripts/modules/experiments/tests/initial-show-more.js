2a2079ad54eeb4c1a07f928a4061a08c710eb64e
Darren Hurley <darren.hurley@guardian.co.uk>
2013-11-13 09:57:18 +0000

    2% test audience

--- common/app/assets/javascripts/modules/experiments/tests/initial-show-more.js
+++ common/app/assets/javascripts/modules/experiments/tests/initial-show-more.js
@@ -4,7 +4,7 @@ define(['utils/mediator'], function(mediator) {
 
         this.id = 'InitialShowMore';
         this.expiry = '2013-11-30';
-        this.audience = 0.2;
+        this.audience = 0.02;
         this.description = 'Test how many items to initially show in the news container';
         this.canRun = function(config) {
             return config.page.contentType === 'Network Front';
