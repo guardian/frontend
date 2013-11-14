2d51c181a7f718a0340691a20d00d2490931684f
Richard Nguyen <richard.nguyen@guardian.co.uk>
2013-11-14 14:23:49 +0000

    Always use id as supported url

--- common/app/model/SupportedUrl.scala
+++ common/app/model/SupportedUrl.scala
@@ -4,11 +4,9 @@ import com.gu.openplatform.contentapi.model.{ Content => ApiContent, Tag => ApiT
 
 // NEVER FORGET - Just calling this SupportedUrl doesn't make it not UrlBuilder, y'know.
 object SupportedUrl {
-  def apply(c: ApiContent): String = if (isSupportedInApp(c)) "/%s" format c.id else c.webUrl
+  def apply(c: ApiContent): String = s"/${c.id}"
 
-  def apply(t: ApiTag): String = "/%s" format t.id
+  def apply(t: ApiTag): String = s"/${t.id}"
 
-  def apply(s: ApiSection): String = "/%s" format s.id
-
-  private def isSupportedInApp(c: ApiContent) = c.isArticle || c.isGallery || c.isVideo
+  def apply(s: ApiSection): String = s"/${s.id}"
 }
