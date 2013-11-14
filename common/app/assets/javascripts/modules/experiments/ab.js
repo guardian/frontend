44549e489e389ac8763811761cdc877a6b382086
Darren Hurley <darren.hurley@guardian.co.uk>
2013-11-14 17:57:56 +0000

    adding show more layout test

--- common/app/assets/javascripts/modules/experiments/ab.js
+++ common/app/assets/javascripts/modules/experiments/ab.js
@@ -8,7 +8,8 @@ define([
     'modules/experiments/tests/alpha-adverts',
     'modules/experiments/tests/commercial-components',
     'modules/experiments/tests/story-package-question',
-    'modules/experiments/tests/initial-show-more'
+    'modules/experiments/tests/initial-show-more',
+    'modules/experiments/tests/show-more-layout'
 ], function (
     common,
     store,
@@ -19,6 +20,7 @@ define([
     CommercialComponentsTest,
     StoryPackageQuestion,
     InitialShowMore
+    ShowMoreLayout
     ) {
 
     var TESTS = [
@@ -27,7 +29,8 @@ define([
             new AlphaAdverts(),
             new CommercialComponentsTest(),
             new StoryPackageQuestion(),
-            new InitialShowMore()
+            new InitialShowMore(),
+            new ShowMoreLayout()
         ],
         participationsKey = 'gu.ab.participations';
 
