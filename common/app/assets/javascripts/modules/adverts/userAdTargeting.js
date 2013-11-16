6cb8c27e7a8e52ec407455ad33a4625dbbceedff
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-11-16 11:18:20 +0000

    Time is now a utility

--- common/app/assets/javascripts/modules/adverts/userAdTargeting.js
+++ common/app/assets/javascripts/modules/adverts/userAdTargeting.js
@@ -1,4 +1,4 @@
-define(['modules/storage', 'modules/identity/api', 'modules/time' ], function(storage, id, time) {
+define(['modules/storage', 'modules/identity/api', 'utils/time' ], function(storage, id, time) {
     var userSegmentsKey = "gu.ads.userSegmentsData";
 
     function getUserSegments() {
