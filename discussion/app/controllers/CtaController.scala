c70fe834aee76cdb44ca4c56b74b96bc3207ee9b
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2013-11-20 13:08:27 +0000

    Integrtion test around fetching topcomment and displaying it

--- discussion/app/controllers/CtaController.scala
+++ discussion/app/controllers/CtaController.scala
@@ -14,86 +14,24 @@ import ExecutionContext.Implicits.global
 import conf.Configuration
 import play.api.libs.ws.Response
 
-trait CtaController extends DiscussionController {
+trait CtaController extends DiscussionController with OpenCtaApi {
 
-  val openCtaApi = new OpenCtaApi {}
-
-  def cta(): Action[AnyContent] = Action.async {
-
+  def cta(shortUrl: String): Action[AnyContent] = Action.async {
     implicit request =>
-      openCtaApi.getTopComment map {
-        ctaJson => {
-          Cached(60) {
-            JsonComponent("html" -> views.html.fragments.commentCta(Comment(ctaJson)))
-          }
-        }
-      }
+      getTopComment() map {ctaJson => Cached(60){JsonComponent("html" -> views.html.fragments.commentCta(Comment(ctaJson))) }}
   }
-
 }
 
 trait OpenCtaApi extends Logging {
 
-  def onError(r: Response) =
+  def onError(r: Response): String =
     s"Error loading callToAction, status: ${r.status}, message: ${r.statusText}, response: ${r.body}"
 
 
   def getTopComment(): Future[JsValue] = {
     val url: String = Configuration.open.ctaApiRoot + "/ctasforarticle/123"
-    stubGetJsonOrError(url, onError) map {
+    getJsonOrError(url, onError) map {
       json => ((json \\ "components")(0) \\ "comments")(0)(0)
-    }
-  }
-
-  //TODO Remove this when you have fixed issue with real request ---Ikenna
-  def stubGetJsonOrError(url: String, onError: (Response) => String, headers: (String, String)*): Future[JsValue] = {
-    future {
-      Json.parse(
-        """
-          |
-          |{
-          |    "status": "ok",
-          |    "self": "http://cta.guardian.com/article/p/1234",
-          |    "components": [
-          |        {
-          |            "type": "top-comments-for-article",
-          |            "discussionMetadata": {
-          |                "key": "/p/323gn",
-          |                "webUrl": "http://www.theguardian.com/commentisfree/2011/sep/21/you-tell-us",
-          |                "apiUrl": "http://discussion.guardianapis.com/discussion-api/discussion//p/323gn",
-          |                "title": "Ideas for September 21-22"
-          |            },
-          |            "comments": [
-          |                {
-          |                    "apiUrl": "http://discussion.guardianapis.com/discussion-api/comment/28656413",
-          |                    "body": "<p>In terms of the diversity and originality of his work as well as his lasting influence, it's probably Aristotle. Marx's ideas and arguments have only been around for 160 years, and have been thoroughly critiqued in theory and practice. Aristotle not only founded most of our academic disciplines, but his ideas on logic, epistemology, metaphysics, physics, biology, ethics, psychology, the nature of God, poetry, rhetoric, and more, were the generally accepted view of things in the West for over 1,600 years. He was also pretty influential in the Islamic world, too.</p>",
-          |                    "date": "07 November 2013 11:59am",
-          |                    "id": 28656413,
-          |                    "isHighlighted": true,
-          |                    "isoDateTime": "2013-11-07T11:59:46Z",
-          |                    "numRecommends": 8,
-          |                    "numResponses": 1,
-          |                    "status": "visible",
-          |                    "userProfile": {
-          |                        "apiUrl": "http://discussion.guardianapis.com/discussion-api/profile/4235958",
-          |                        "avatar": "http://static.guim.co.uk/sys-images/discussion/avatars/2010/09/23/JamesDavid/0663561f-c609-43e2-a713-0c0006c1ec45/60x60.png",
-          |                        "badge": [ ],
-          |                        "displayName": "JamesDavid",
-          |                        "secureAvatarUrl": "https://static-secure.guim.co.uk/sys-images/discussion/avatars/2010/09/23/JamesDavid/0663561f-c609-43e2-a713-0c0006c1ec45/60x60.png",
-          |                        "userId": "4235958",
-          |                        "webUrl": "http://www.theguardian.com/discussion/user/id/4235958"
-          |                    },
-          |                    "webUrl": "http://discussion.theguardian.com/comment-permalink/28656413"
-          |                }
-          |            ]
-          |        },
-          |        {
-          |            "type": "discussion-prompt",
-          |            "content": {}
-          |        }
-          |    ]
-          |}
-        """.stripMargin)
     }
   }
 
@@ -116,6 +54,5 @@ trait OpenCtaApi extends Logging {
 
   protected def GET(url: String): Future[Response] =
     WS.url(url)
-      .withHeaders("Host" -> "opencontent.guardianapis.com") //TODO: set this correctly  --ikenna
-      .withRequestTimeout(2000).get() // TODO: make this a property. --IKenna
+      .withRequestTimeout(2000).get()
 }
--- discussion/test/controllers/OpenCtaApiTest.scala
+++ discussion/test/controllers/OpenCtaApiTest.scala
@@ -1,96 +1,24 @@
 package controllers
 
-import org.scalatest.FunSuite
-import scala.concurrent.{Await, Future}
-import play.api.libs.json.{JsString, JsValue, Json}
-import scala.concurrent.duration.Duration
-import java.util.concurrent.TimeUnit
-import play.api.libs.ws.Response
-import scala.concurrent._
-import ExecutionContext.Implicits.global
+import org.scalatest.{FlatSpecLike, ShouldMatchers}
+import play.api.test._
+import play.api.test.Helpers._
+import java.net.ServerSocket
 
-class OpenCtaApiTest extends FunSuite with OpenCtaApi {
+class OpenCtaApiTest extends OpenCtaApi with FlatSpecLike with ShouldMatchers {
 
+  val availablePort: Int = new ServerSocket(0).getLocalPort
 
-  test("That we can actually fetch top comment") {
-    val topCommentFuture: Future[JsValue] = getTopComment()
-    val topCommentJson = Await.result(topCommentFuture, Duration(1, TimeUnit.SECONDS))
-    assert(expectedComment === topCommentJson)
+  "CtaController" should "respond to request for callouts for a given discussion key" in {
+    running(TestServer(availablePort)) {
+     val alternativeUrl =  "/open/topcomments/p/k123.json"
+      val Some(response) = route(FakeRequest("GET", "/open/cta/p/k123.json"))
+
+      status(response) should be(OK)
+      contentAsString(response) should include("html")
+      contentAsString(response) should include("refreshStatus")
+    }
   }
-  
-  override protected def getJsonOrError(url: String, onError: (Response) => String, headers: (String, String)*): Future[JsValue] = {
-    future{Json.parse(
-      """
-        |
-        |{
-        |    "status": "ok",
-        |    "self": "http://cta.guardian.com/article/p/1234",
-        |    "components": [
-        |        {
-        |            "type": "top-comments-for-article",
-        |            "discussionMetadata": {
-        |                "key": "/p/323gn",
-        |                "webUrl": "http://www.theguardian.com/commentisfree/2011/sep/21/you-tell-us",
-        |                "apiUrl": "http://discussion.guardianapis.com/discussion-api/discussion//p/323gn",
-        |                "title": "Ideas for September 21-22"
-        |            },
-        |            "comments": [
-        |                {
-        |                    "apiUrl": "http://discussion.guardianapis.com/discussion-api/comment/28656413",
-        |                    "body": "<p>In terms of the diversity and originality of his work as well as his lasting influence, it's probably Aristotle. Marx's ideas and arguments have only been around for 160 years, and have been thoroughly critiqued in theory and practice. Aristotle not only founded most of our academic disciplines, but his ideas on logic, epistemology, metaphysics, physics, biology, ethics, psychology, the nature of God, poetry, rhetoric, and more, were the generally accepted view of things in the West for over 1,600 years. He was also pretty influential in the Islamic world, too.</p>",
-        |                    "date": "07 November 2013 11:59am",
-        |                    "id": 28656413,
-        |                    "isHighlighted": true,
-        |                    "isoDateTime": "2013-11-07T11:59:46Z",
-        |                    "numRecommends": 8,
-        |                    "numResponses": 1,
-        |                    "status": "visible",
-        |                    "userProfile": {
-        |                        "apiUrl": "http://discussion.guardianapis.com/discussion-api/profile/4235958",
-        |                        "avatar": "http://static.guim.co.uk/sys-images/discussion/avatars/2010/09/23/JamesDavid/0663561f-c609-43e2-a713-0c0006c1ec45/60x60.png",
-        |                        "badge": [ ],
-        |                        "displayName": "JamesDavid",
-        |                        "secureAvatarUrl": "https://static-secure.guim.co.uk/sys-images/discussion/avatars/2010/09/23/JamesDavid/0663561f-c609-43e2-a713-0c0006c1ec45/60x60.png",
-        |                        "userId": "4235958",
-        |                        "webUrl": "http://www.theguardian.com/discussion/user/id/4235958"
-        |                    },
-        |                    "webUrl": "http://discussion.theguardian.com/comment-permalink/28656413"
-        |                }
-        |            ]
-        |        },
-        |        {
-        |            "type": "discussion-prompt",
-        |            "content": {}
-        |        }
-        |    ]
-        |}
-      """.stripMargin)}
-  }
-
-  private val expectedComment:
-                         JsValue = Json.parse(
-                       """{
-    "apiUrl": "http://discussion.guardianapis.com/discussion-api/comment/28656413",
-    "body": "<p>In terms of the diversity and originality of his work as well as his lasting influence, it's probably Aristotle. Marx's ideas and arguments have only been around for 160 years, and have been thoroughly critiqued in theory and practice. Aristotle not only founded most of our academic disciplines, but his ideas on logic, epistemology, metaphysics, physics, biology, ethics, psychology, the nature of God, poetry, rhetoric, and more, were the generally accepted view of things in the West for over 1,600 years. He was also pretty influential in the Islamic world, too.</p>",
-    "date": "07 November 2013 11:59am",
-    "id": 28656413,
-    "isHighlighted": true,
-    "isoDateTime": "2013-11-07T11:59:46Z",
-    "numRecommends": 8,
-    "numResponses": 1,
-    "status": "visible",
-    "userProfile": {
-      "apiUrl": "http://discussion.guardianapis.com/discussion-api/profile/4235958",
-      "avatar": "http://static.guim.co.uk/sys-images/discussion/avatars/2010/09/23/JamesDavid/0663561f-c609-43e2-a713-0c0006c1ec45/60x60.png",
-      "badge": [],
-      "displayName": "JamesDavid",
-      "secureAvatarUrl": "https://static-secure.guim.co.uk/sys-images/discussion/avatars/2010/09/23/JamesDavid/0663561f-c609-43e2-a713-0c0006c1ec45/60x60.png",
-      "userId": "4235958",
-      "webUrl": "http://www.theguardian.com/discussion/user/id/4235958"
-    },
-    "webUrl": "http://discussion.theguardian.com/comment-permalink/28656413"
-  }""")
-
 
 }
 
