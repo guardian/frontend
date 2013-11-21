e86ba9da176e6c9227c67684d9490d734ed2b3bc
marcjones <marc.jones@guardian.co.uk>
2013-11-21 14:03:38 +0000

    Fix spacing

--- integration-tests/casper/tests/article/live-live-blog.spec.js
+++ integration-tests/casper/tests/article/live-live-blog.spec.js
@@ -15,47 +15,47 @@ casper.test.setUp(function() {
 });
 
 casper.test.begin("Auto update toggle on / off", function(test) {
-	casper.then(function testLiveBlogAutoUpdate() {
-		casper.waitForSelector('.item__live-indicator', function() {
-    	    livePageUri = casper.getElementAttribute(x('//*[@class="item__live-indicator"]/ancestor::a'), 'href');
-	        if (host.indexOf("http://localhost") !=-1) {
+    casper.then(function testLiveBlogAutoUpdate() {
+        casper.waitForSelector('.item__live-indicator', function() {
+            livePageUri = casper.getElementAttribute(x('//*[@class="item__live-indicator"]/ancestor::a'), 'href');
+            if (host.indexOf("http://localhost") !=-1) {
                 livePageUri = host + livePageUri.substring(1);
             }
-    	    casper.thenOpen(livePageUri + "?view=mobile", function() {
-	        	casper.waitForSelector('.live-toolbar .update', function() {
-	        		test.assertVisible(
-			        '.circular-progress--is-on',
-			        "Auto update toggle on by default"
-		        );
-    		    casper.click('.live-toggler--circle.js-auto-update');
-	    	    test.assertVisible(
-		        	'.circular-progress--is-off',
-		        	"Auto update toggle switched OFF on click"
-		        );
-    		    test.assertNotVisible(
-	    	    	'.circular-progress--is-on',
-		        	"Toggle on button displayed when auto update off"
-	    	    );
-    		    casper.click('.live-toggler--circle.js-auto-update');
-	    	    test.assertVisible(
-		        	'.circular-progress--is-on',
-		        	"Auto update toggle switched ON on click"
-		        );
-    		    test.assertNotVisible(
-	    	    	'.circular-progress--is-off',
-		        	"Toggle off button displayed when auto update on"
-	    	    );
-    		    test.done();
-	        	}, function timeout(){
-	        		casper.capture('live-blog-auto-update-fail.png');
+            casper.thenOpen(livePageUri + "?view=mobile", function() {
+                casper.waitForSelector('.live-toolbar .update', function() {
+                    test.assertVisible(
+                    '.circular-progress--is-on',
+                    "Auto update toggle on by default"
+                );
+                casper.click('.live-toggler--circle.js-auto-update');
+                test.assertVisible(
+                    '.circular-progress--is-off',
+                    "Auto update toggle switched OFF on click"
+                );
+                test.assertNotVisible(
+                    '.circular-progress--is-on',
+                    "Toggle on button displayed when auto update off"
+                );
+                casper.click('.live-toggler--circle.js-auto-update');
+                test.assertVisible(
+                    '.circular-progress--is-on',
+                    "Auto update toggle switched ON on click"
+                );
+                test.assertNotVisible(
+                    '.circular-progress--is-off',
+                    "Toggle off button displayed when auto update on"
+                );
+                test.done();
+                }, function timeout(){
+                    casper.capture('live-blog-auto-update-fail.png');
                     test.fail("Failed to find auto update toggle");
                 }, 10000);
-	        });
+            });
         }, function timeout(){
             casper.capture('live-blog-live-trail-fail.png');
             casper.echo('Failed to find trail for currently live content');
         }, 10000);
-	});
+    });
 });
 
 casper.run(function() {
