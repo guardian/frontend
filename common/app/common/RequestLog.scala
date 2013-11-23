58e48390730dd871445a4eef6843bb83088314c2
grant Klopper <grant.klopper@gmail.com>
2013-11-23 08:11:20 +0000

    Piggyback external healthcheck off of ELB Healthcheck

---
+++ common/app/common/RequestLog.scala
@@ -0,0 +1,20 @@
+package common
+
+import play.api.mvc.{Headers, RequestHeader}
+
+
+object RequestLog {
+
+  private implicit class Get2Safe(headers: Headers) {
+    def safe(key: String) = headers.get(key).getOrElse("")
+  }
+
+  def apply(r: RequestHeader): String = Seq(
+    r.headers.safe("X-Forwarded-For"),
+    r.host,
+    r.headers.safe("X-Gu-Edition"),
+    r.headers.safe("X-GU-GeoLocation"),
+    s"${r.headers.safe("User-Agent")}"
+  ).mkString(", ")
+
+}
