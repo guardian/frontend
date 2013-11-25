63b056562543e0f3e2f9fcfb8aa446e3f8df1467
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-11-25 09:19:45 +0000

    Add total-ads-seen counter to the dashboard

--- admin/app/controllers/RadiatorController.scala
+++ admin/app/controllers/RadiatorController.scala
@@ -61,10 +61,11 @@ object RadiatorController extends Controller with Logging with AuthLogging {
   
   def adsInView() = Authenticated { implicit request =>
     val topSeconds = CloudWatch.adsInView("ads.top.secondsInView")
+    val topCount = CloudWatch.adsInView("ads.top.count")
     val bottomSeconds = CloudWatch.adsInView("ads.bottom.secondsInView")
     val inlineSeconds = CloudWatch.adsInView("ads.inline.secondsInView")
     val mpuSeconds = CloudWatch.adsInView("ads.mpu.secondsInView")
     val pageviews = CloudWatch.adsInView("ads.views")
-    NoCache(Ok(views.html.adsInView(topSeconds, bottomSeconds, inlineSeconds, mpuSeconds, pageviews, Configuration.environment.stage)))
+    NoCache(Ok(views.html.adsInView(topSeconds, topCount, bottomSeconds, inlineSeconds, mpuSeconds, pageviews, Configuration.environment.stage)))
   }
 }
