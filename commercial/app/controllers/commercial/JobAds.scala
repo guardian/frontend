82a425ce402b2b56e2f2e9cf3fedee49cf7d1daf
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-11-28 15:50:01 +0000

    Handle jobs feed as utf-8

--- commercial/app/controllers/commercial/JobAds.scala
+++ commercial/app/controllers/commercial/JobAds.scala
@@ -7,12 +7,14 @@ import model.Cached
 
 object JobAds extends Controller {
 
+  implicit val codec = Codec.utf_8
+
   def jobs = Action {
     implicit request =>
       JobsAgent.adsTargetedAt(segment) match {
         case Nil => JsonNotFound.apply()
         case jobs => {
-          Cached(60)(JsonComponent(views.html.jobs(jobs take 5)))
+          Cached(60)(JsonComponent(views.html.jobs(jobs take 5)).as(JSON))
         }
       }
   }
