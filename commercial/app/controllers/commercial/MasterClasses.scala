d30739c17e9fc561fed5868f485c2b59ae8ccaab
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-12-02 11:28:51 +0000

    Remove character-encoding settings from commercial controllers
    Now that JsonComponent sets character encoding part of content-type header.

--- commercial/app/controllers/commercial/MasterClasses.scala
+++ commercial/app/controllers/commercial/MasterClasses.scala
@@ -7,14 +7,12 @@ import model.Cached
 
 object MasterClasses extends Controller {
 
-  implicit val codec = Codec.utf_8
-
   def list = Action {
     implicit request =>
       MasterClassAgent.getUpcoming match {
         case Nil => JsonNotFound.apply()
         case upcoming => {
-          Cached(60)(JsonComponent(views.html.masterclasses(upcoming take 3)).as(JSON))
+          Cached(60)(JsonComponent(views.html.masterclasses(upcoming take 3)))
         }
       }
   }
