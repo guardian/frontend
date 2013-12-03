4a9810987f3fd22b9873cd3481fa8d0e33d55f78
Kaelig <kaelig@deloumeau.fr>
2013-12-03 11:47:35 +0000

    Refactor font loading method condition

--- common/app/assets/javascripts/bootstraps/app.js
+++ common/app/assets/javascripts/bootstraps/app.js
@@ -95,13 +95,7 @@ define('bootstraps/app', [
         },
 
         loadFonts: function(config, ua) {
-            if (
-                config.switches.webFonts &&
-                !(
-                    guardian.platformsGettingHintedFonts.test(navigator.userAgent) &&
-                    !guardian.platformsNotGettingHintedFonts.test(navigator.userAgent)
-                )
-            ) {
+            if (config.switches.webFonts && !guardian.shouldLoadFontsAsynchronously) {
                 var fileFormat = detect.getFontFormatSupport(ua),
                     fontStyleNodes = document.querySelectorAll('[data-cache-name].initial');
                 var f = new Fonts(fontStyleNodes, fileFormat);
