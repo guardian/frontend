4a90c2078ca760056cf03cf9b4514bb63cccd81b
Francis Carr <francis.carr@guardian.co.uk>
2013-12-04 17:20:45 +0000

    Change cookie name

--- core-navigation/app/controllers/ChangeBucketController.scala
+++ core-navigation/app/controllers/ChangeBucketController.scala
@@ -4,12 +4,12 @@ import play.api.mvc._
 
 object ChangeBucketController extends Controller with PreferenceController {
 
-  val abCookieName: String = "GU_ALPHA"
+  val abCookieName: String = "GU_ALPHA_TEST"
 
   def render(optaction: String, redirectUrl: String) = Action { implicit request =>
     optaction match {
-      case "optin"  => switchTo(abCookieName -> "True", redirectUrl)
-      case "optout" => switchTo(abCookieName -> "False", redirectUrl)
+      case "optin"  => switchTo(abCookieName -> "true", redirectUrl)
+      case "optout" => switchTo(abCookieName -> "false", redirectUrl)
       case _ => NotFound("unknown action")
     }
   }
