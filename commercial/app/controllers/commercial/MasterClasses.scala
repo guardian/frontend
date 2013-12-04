14417ca94bc6e477e37bb78d37d69129e48de996
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-12-04 16:26:20 +0000

    Roberto and Ken grabbing a commit

--- commercial/app/controllers/commercial/MasterClasses.scala
+++ commercial/app/controllers/commercial/MasterClasses.scala
@@ -7,6 +7,18 @@ import model.Cached
 
 object MasterClasses extends Controller {
 
+  implicit val codec = Codec.utf_8
+
+  def renderMasterclass = Action {
+    implicit request =>
+      MasterClassAgent.getUpcoming match {
+        case Nil => NotFound
+        case upcoming => {
+          Cached(60)(Ok(views.html.masterclasses(upcoming take 3)))
+        }
+      }
+  }
+
   def list = Action {
     implicit request =>
       MasterClassAgent.getUpcoming match {
