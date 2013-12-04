57e0cfce6f0cddb8fd756dc5ac6f1b58a22963eb
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-12-04 10:17:14 +0000

    Add 2nd study, to /uk-alpha

--- common/app/public/javascripts/vendor/userzoom-uk-alpha.js
+++ common/app/public/javascripts/vendor/userzoom-uk-alpha.js
@@ -1,6 +1,6 @@
 var _uzactions= _uzactions|| [];
-_uzactions.push(['_setID', '20771CAA655BE31190590022196C4538']);
-_uzactions.push(['_setSID', '1F771CAA655BE31190590022196C4538']);
+_uzactions.push(['_setID', 'A5D60F93BF5CE31190590022196C4538']);
+_uzactions.push(['_setSID', 'A4D60F93BF5CE31190590022196C4538']);
 _uzactions.push(['_start']);
 (function() {
   var uz = document.createElement('script'); uz.type = 'text/javascript'; uz.async = true; uz.charset = 'utf-8';
