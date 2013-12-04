ce8768791aa7b52c75ec310141ddaf8476951cbb
chrisfinch <chrisfinchy@gmail.com>
2013-12-04 14:02:28 +0000

    Added test for show more comments

--- integration-tests/casper/tests/discussion/discussion.spec.js
+++ integration-tests/casper/tests/discussion/discussion.spec.js
@@ -27,6 +27,35 @@ casper.test.begin('Read top level comments', function(test) {
     });
 });
 
+casper.test.begin('Show more comments', function (test) { // Testing on an article with topComments present
+    
+    var showCta = '.discussion__comments__container .d-show-cta',
+        commentLi = '.discussion__comments__container li.d-comment--top-level';
+
+    casper.waitForSelector('.discussion__comments__container', function then () {
+        test.assertExists('.discussion__comments__container');
+        test.assertVisible('.discussion__comments__container');
+        test.assertExists(showCta);
+        test.assertVisible(showCta);
+        test.assertElementCount(commentLi, 10);
+        test.assertEval(function () {
+            return document.querySelector('.discussion__comments__container li.d-comment--top-level').className.match(/(u-h)/).length > 1;
+        }, "Comment Li's should not have u-h class");
+        casper.click(showCta);
+        casper.waitFor(function check () {
+            return this.evaluate(function () {
+                return document.querySelector('.discussion__comments__container li.d-comment--top-level').className.match(/(u-h)/) === null; // Only checks one - should check all
+            });
+        }, function then () {
+            test.assertVisible(commentLi);
+            test.assertEval(function () {
+                return document.querySelector('.discussion__comments__container .d-show-cta').className.match(/(u-h)/).length > 1;
+            }, "Show CTA should not have u-h class");
+        });
+
+    });
+});
+
 // casper.test.begin('Hash comment permalink links to relevant comment', function(test) {
 //     casper.thenOpen(host +'football/2013/sep/25/manchester-united-liverpool-capital-one-cup#comment-27353447', function() {
 //         casper.waitForSelector('#comment-27353447', function then() {
