287dbe62658a9b00b9a39366c002e07997e971b5
Darren Hurley <darren.hurley@guardian.co.uk>
2013-12-04 14:56:07 +0000

    template mapping for 'other' special on uk-alpha

--- facia/app/views/support/package.scala
+++ facia/app/views/support/package.scala
@@ -54,7 +54,8 @@ object GetContainer {
       ("uk-alpha/features/feature-stories", FeaturesContainer(headerLink = false)),
       ("uk-alpha/special/special-story", SportContainer(headerLink = false)),
       ("uk-alpha/contributors/feature-stories", CommentContainer(headerLink = false)),
-      ("uk-alpha/people-in-the-news/feature-stories", SportContainer(headerLink = false))
+      ("uk-alpha/people-in-the-news/feature-stories", SportContainer(headerLink = false)),
+      ("uk-alpha/special-other/special-story", SportContainer(headerLink = false))
     )),
     ("uk/business", Map(
       ("uk/business/regular-stories", SportContainer(showMore = false))
