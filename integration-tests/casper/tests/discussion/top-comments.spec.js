01ad741cf1663945b4baa1a4f2a407eb31bed843
James Gorrie <james.gorrie@guardian.co.uk>
2013-12-06 14:50:07 +0000

    Discussion integration test updates

--- integration-tests/casper/tests/discussion/top-comments.spec.js
+++ integration-tests/casper/tests/discussion/top-comments.spec.js
@@ -71,35 +71,6 @@ casper.test.begin('Test top comments expand functionality', function (test) {
     );
 });
 
-casper.test.begin('Show more comments', function (test) { // Testing on an article with topComments present
-    
-    var showCta = '.discussion__comments__container .d-show-cta',
-        commentLi = '.discussion__comments__container li.d-comment--top-level';
-
-    casper.waitForSelector('.discussion__comments__container', function then () {
-        test.assertExists('.discussion__comments__container');
-        test.assertVisible('.discussion__comments__container');
-        test.assertExists(showCta);
-        test.assertVisible(showCta);
-        test.assertElementCount(commentLi, 10);
-        test.assertEval(function () {
-            return document.querySelector('.discussion__comments__container li.d-comment--top-level').className.match(/(u-h)/).length > 1;
-        }, "Comment Li's should not have u-h class");
-        casper.click(showCta);
-        casper.waitFor(function check () {
-            return this.evaluate(function () {
-                return document.querySelector('.discussion__comments__container li.d-comment--top-level').className.match(/(u-h)/) === null; // Only checks one - should check all
-            });
-        }, function then () {
-            test.assertVisible(commentLi);
-            test.assertEval(function () {
-                return document.querySelector('.discussion__comments__container .d-show-cta').className.match(/(u-h)/).length > 1;
-            }, "Show CTA should not have u-h class");
-            test.done();
-        });
-    });
-});
-
 casper.run(function() {
     this.test.renderResults(true, 0, this.cli.get('xunit') + 'discussion.xml');
 });
