3e97a96c1bc068ac358b39296aa4cb26e8795500
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-12-09 11:14:30 +0000

    Refactor keyword lookup

--- commercial/app/model/commercial/Keyword.scala
+++ commercial/app/model/commercial/Keyword.scala
@@ -17,18 +17,19 @@ case class Keyword(id: String, webTitle: String) {
 object Keyword extends ExecutionContexts with Logging {
 
   def lookup(term: String, section: Option[String] = None): Future[Seq[Keyword]] = {
-    val baseQuery = SwitchingContentApi().tags.stringParam("type", "keyword").stringParam("q", term).pageSize(50)
-    val query = section.foldLeft(baseQuery)((acc, s) => acc.stringParam("section", s))
+    val baseQuery = SwitchingContentApi().tags.q(term).tagType("keyword").pageSize(50)
+    val query = section.foldLeft(baseQuery)((acc, sectionName) => acc section sectionName)
+
     val result = query.response.map {
       _.results.map(tag => Keyword(tag.id, tag.webTitle))
     } recover {
       case e =>
-        log.warn(s"Failed to look up $term: ${e.getMessage}")
+        log.warn(s"Failed to look up [$term]: ${e.getMessage}")
         Nil
     }
 
     result onSuccess {
-      case keywords => log.info(s"Looking up $term gave ${keywords.map(_.id)}")
+      case keywords => log.info(s"Looking up [$term] gave ${keywords.map(_.id)}")
     }
 
     result
