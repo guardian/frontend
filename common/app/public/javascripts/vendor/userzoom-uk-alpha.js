edab03fdfc974b06d3daa90d83cc795a31c254e3
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-12-09 13:39:36 +0000

    Change userzoom ids

--- common/app/public/javascripts/vendor/userzoom-uk-alpha.js
+++ common/app/public/javascripts/vendor/userzoom-uk-alpha.js
@@ -1,6 +1,6 @@
 var _uzactions= _uzactions|| [];
-_uzactions.push(['_setID', 'A5D60F93BF5CE31190590022196C4538']);
-_uzactions.push(['_setSID', 'A4D60F93BF5CE31190590022196C4538']);
+_uzactions.push(['_setID', 'B7FC8AD4CF60E31190590022196C4538']);
+_uzactions.push(['_setSID', 'B6FC8AD4CF60E31190590022196C4538']);
 _uzactions.push(['_start']);
 (function() {
   var uz = document.createElement('script'); uz.type = 'text/javascript'; uz.async = true; uz.charset = 'utf-8';
