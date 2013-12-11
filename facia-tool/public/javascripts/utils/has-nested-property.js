21dd3d656894d561f9d12f35a83e0a0696ea3b0d
Darren Hurley <darren.hurley@guardian.co.uk>
2013-12-11 13:07:26 +0000

    removing unused module

--- facia-tool/public/javascripts/utils/has-nested-property.js
+++
@@ -1,9 +0,0 @@
-/* global _: true */
-define(function() {
-    return function (obj, path) {
-        if(obj.hasOwnProperty(path[0])) {
-            return path.length === 1 ? true : this.hasNestedProperty(obj[path[0]], _.rest(path));
-        }
-        return false;
-    };
-});
