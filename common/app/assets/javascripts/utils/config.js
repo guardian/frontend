708a3ee2d9b51b3a2cd0aa0be1baa5470d8c9bfc
Grant Klopper <grant.klopper@guardian.co.uk>
2013-12-16 15:09:54 +0000

    Little bit of cleanup

--- common/app/assets/javascripts/utils/config.js
+++ common/app/assets/javascripts/utils/config.js
@@ -16,36 +16,33 @@ define(['lodash/objects/assign'], function (extend) {
 
     var config = guardian.config;
 
-    return extend(
-        {
-            hasTone: function (name) {
-                return (config.page.tones || "").indexOf(name) > -1;
-            },
-            hasSeries: function (name) {
-                return (config.page.series || "").indexOf(name) > -1;
-            },
-            referencesOfType: function (name) {
-                return (config.page.references || []).filter(function (reference) {
-                    return typeof reference[name] !== 'undefined';
-                }).map(function (reference) {
-                        return reference[name];
-                    });
-            },
-            referenceOfType: function (name) {
-                return this.referencesOfType(name)[0];
-            },
-
-            // the date nicely formatted and padded for use as part of a url
-            // looks like    2012/04/31
-            webPublicationDateAsUrlPart: function () {
-                if (this.page.webPublicationDate) {
-                    var pubDate = new Date(this.page.webPublicationDate);
-                    return pubDate.getFullYear() + '/' +
-                        pad(pubDate.getMonth() + 1, 2) + '/' +
-                        pad(pubDate.getDate(), 2);
-                }
-            }
+    return extend({
+        hasTone: function (name) {
+            return (config.page.tones || "").indexOf(name) > -1;
         },
-        config);
+        hasSeries: function (name) {
+            return (config.page.series || "").indexOf(name) > -1;
+        },
+        referencesOfType: function (name) {
+            return (config.page.references || []).filter(function (reference) {
+                return typeof reference[name] !== 'undefined';
+            }).map(function (reference) {
+                    return reference[name];
+                });
+        },
+        referenceOfType: function (name) {
+            return this.referencesOfType(name)[0];
+        },
 
+        // the date nicely formatted and padded for use as part of a url
+        // looks like    2012/04/31
+        webPublicationDateAsUrlPart: function () {
+            if (this.page.webPublicationDate) {
+                var pubDate = new Date(this.page.webPublicationDate);
+                return pubDate.getFullYear() + '/' +
+                    pad(pubDate.getMonth() + 1, 2) + '/' +
+                    pad(pubDate.getDate(), 2);
+            }
+        }
+    }, config);
 });
