2971bd0312b6ca7ebeea0644cfa67014f2839883
Marc Hibbins <marchibbins@gmail.com>
2013-12-17 10:30:21 +0000

    Clean up Formstack module

--- common/app/assets/javascripts/modules/identity/formstack.js
+++ common/app/assets/javascripts/modules/identity/formstack.js
@@ -12,8 +12,12 @@ define([
 
     function Formstack(el, formstackId, context, config) {
 
+        var self = this,
+            dom = {},
+            formId = formstackId.split('-')[0];
+
         config = common.extend({
-            classes: {
+            idClasses: {
                 form: 'form',
                 field: 'form-field',
                 note: 'form-field__note form-field__note--below',
@@ -23,114 +27,78 @@ define([
                 submit: 'submit-input',
                 fieldError: 'form-field--error',
                 formError: 'form__error',
-                hide: 'u-h',
-                ready: 'form-ready'
+                sectionHeader: 'formstack-heading',
+                sectionHeaderFirst: 'formstack-heading--first',
+                sectionText: 'formstack-section',
+                hide: 'u-h'
             },
-            selectors: {
-                fsForm: '#fsForm',
-                fsBody: '.fsBody',
-                fsField: '.fsRow',
-                fsNote: '.fsSupporting, .showMobile',
-                fsLabel: '.fsLabel',
-                fsTextInput: '.fsField[type="text"], .fsField[type="email"], .fsField[type="number"], .fsField[type="tel"]',
-                fsTextArea: 'textarea.fsField',
-                fsSubmit: '.fsSubmitButton',
-                fsFormError: '.fsError',
-                fsFieldError: '.fsValidationError',
-                fsHide: '.hidden, .fsHidden, .fsRequiredMarker',
-                fsIdUser: '[type="number"]',
-                fsIdEmail: '[type="email"]'
+            fsSelectors: {
+                form: '#fsForm' + formId,
+                field: '.fsRow',
+                note: '.fsSupporting, .showMobile',
+                label: '.fsLabel',
+                textInput: '.fsField[type="text"], .fsField[type="email"], .fsField[type="number"], .fsField[type="tel"]',
+                textArea: 'textarea.fsField',
+                submit: '.fsSubmitButton',
+                fieldError: '.fsValidationError',
+                formError: '.fsError',
+                sectionHeader: '.fsSectionHeading',
+                sectionHeaderFirst: '.fsSection:first-child .fsSectionHeading',
+                sectionText: '.fsSectionText',
+                hide: '.hidden, .fsHidden, .fsRequiredMarker'
+            },
+            hiddenSelectors: {
+                userId: '[type="number"]',
+                email: '[type="email"]'
             }
         }, config);
 
-        var self = this,
-            dom = {},
-            user = {},
-            formId = formstackId.split('-')[0];
-
         self.init = function() {
-            if (!$(el).hasClass(config.classes.ready)) {
+            // User object required to populate fields
+            var user = idApi.getUserOrSignIn();
 
-                // User object required to populate fields
-                user = idApi.getUserOrSignIn();
+            self.dom(user);
+            $(el).removeClass(config.idClasses.hide);
 
-                self.dom();
-                self.populate();
-                self.decorate();
-
-                // Update iframe height, see "modules/identity/formstack-iframe"
-                self.postMessage('ready');
-                self.postMessage('refreshHeight');
-
-                $(el).addClass(config.classes.ready).removeClass(config.classes.hide);
-            }
+            // Update iframe height, see "modules/identity/formstack-iframe"
+            self.postMessage('ready');
+            self.postMessage('refreshHeight');
         };
 
-        self.dom = function() {
-            // TODO: Setup DOM, not like this:
-            dom.$form = $(config.selectors.fsForm + formId);
-            dom.$formBody = $(config.selectors.fsBody);
+        self.dom = function(user) {
+            // Formstack generates some awful HTML, so we'll remove the CSS links,
+            // loop their selectors and add our own classes instead
+            dom.$form = $(config.fsSelectors.form).addClass(config.idClasses.form);
+            $('link', el).remove();
 
-            dom.$fields = $(config.selectors.fsField, dom.$form);
-            dom.$notes = $(config.selectors.fsNote, dom.$form);
-            dom.$labels = $(config.selectors.fsLabel, dom.$form);
+            for (var selector in config.fsSelectors) {
+                $(config.fsSelectors[selector], dom.$form).addClass(config.idClasses[selector]);
+            }
 
-            dom.$textInput = $(config.selectors.fsTextInput, dom.$form);
-            dom.$textArea = $(config.selectors.fsTextArea, dom.$form);
-            dom.$submit = $(config.selectors.fsSubmit, dom.$form);
-            dom.$hide = $(config.selectors.fsHide, dom.$form);
+            // Formstack also don't have capturable hidden fields,
+            // so we remove ID text inputs and append hidden equivalents
+            var $userId = $(config.hiddenSelectors.userId, dom.$form).remove(),
+                $email = $(config.hiddenSelectors.email, dom.$form).remove(),
 
-            dom.$idUser = $(config.selectors.fsIdUser, dom.$form);
-            dom.$idEmail = $(config.selectors.fsIdEmail, dom.$form);
+                html = '<input type="hidden" name="' + $userId.attr('name') + '" value="' + user.id + '">'
+                     + '<input type="hidden" name="' + $email.attr('name') + '" value="' + user.primaryEmailAddress + '">';
 
-            dom.$cssLinks = $('link', dom.$formBody);
+            dom.$form.append(html);
 
             // Events
             bean.on(dom.$form[0], 'submit', self.submit);
         };
 
-        self.populate = function() {
-            dom.$idUser.val(user.id);
-            dom.$idEmail.val(user.primaryEmailAddress);
-        };
-
-        self.decorate = function() {
-            // Remove Formstack styles
-            dom.$cssLinks.remove();
-
-            // Add our form classes to pick up Identity styles
-            dom.$form.addClass(config.classes.form);
-            dom.$fields.addClass(config.classes.field);
-            dom.$notes.addClass(config.classes.note);
-            dom.$labels.addClass(config.classes.label);
-
-            dom.$textInput.addClass(config.classes.textInput);
-            dom.$textArea.addClass(config.classes.textArea);
-            dom.$submit.addClass(config.classes.submit);
-
-            dom.$hide.addClass(config.classes.hide);
-
-            // TODO: not this:
-            $('.fsSection .fsSectionHeading').css({ 'font-size': '20px' });
-            $('.fsSection:first-child .fsSectionHeading').css({ 'font-size': '24px' });
-            $('.fsSectionHeader .fsSectionText').css({ 'margin-bottom': '24px' });
-            $('.fsPage').css({ 'margin-top': '16px' });
-        };
-
         self.submit = function(event) {
-            self.errors();
-        };
-
-        self.errors = function() {
             // TODO: FML
             setInterval(function() {
                 // Remove any existing errors
-                $('.' + config.classes.formError).removeClass(config.classes.formError);
-                $('.' + config.classes.fieldError).removeClass(config.classes.fieldError);
+                $('.' + config.idClasses.formError).removeClass(config.idClasses.formError);
+                $('.' + config.idClasses.fieldError).removeClass(config.idClasses.fieldError);
 
-                // Update errors
-                $(config.selectors.fsFieldError, dom.$form).addClass(config.classes.fieldError);
-                $(config.selectors.fsFormError, dom.$form).addClass(config.classes.formError);
+                // Handle new errors
+                $(config.fsSelectors.formError, dom.$form).addClass(config.idClasses.formError);
+                $(config.fsSelectors.fieldError, dom.$form).addClass(config.idClasses.fieldError);
 
                 self.postMessage('refreshHeight');
             }, 100);
