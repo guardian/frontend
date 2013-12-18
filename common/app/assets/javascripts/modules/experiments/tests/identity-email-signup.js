83c862ebc848d6a370c92bb05ac5dcafa5e2eb25
chrisfinch <chrisfinchy@gmail.com>
2013-12-18 15:10:22 +0000

    Latest changes, responsiveness

--- common/app/assets/javascripts/modules/experiments/tests/identity-email-signup.js
+++ common/app/assets/javascripts/modules/experiments/tests/identity-email-signup.js
@@ -4,38 +4,38 @@ define([
     "modules/identity/api"
 ], function (mediator, EmailSignup, IdApi) {
      
-     var EmailSignupTest = function () {
+    var EmailSignupTest = function () {
 
-         var _config;
-         this.id = 'EmailSignup';
-         this.expiry = '2014-01-15';
-         this.audience = 0.2;
-         this.description = 'Displays email signup button on relevant article pages';
-         this.canRun = function (config) {
-             _config = config;
-             return IdApi.isUserLoggedIn();
-         };
-         this.variants = [
-             {
-                 id: 'control',
-                 test: function (context) {
+        var _config;
+        this.id = 'EmailSignup';
+        this.expiry = '2014-01-15';
+        this.audience = 0.2;
+        this.description = 'Displays email signup button on relevant article pages';
+        this.canRun = function (config) {
+            _config = config;
+            return IdApi.isUserLoggedIn();
+        };
+        this.variants = [
+            {
+                id: 'control',
+                test: function (context) {
                     return true;
-                 }
-             },
-             {
-                 id: 'email-signup-inline',
-                 test: function() {
-                    EmailSignup.init(document.getElementById('preload-1'), 'inline');
-                 }
-             },
-             {
-                 id: 'email-signup-left',
-                 test: function() {
-                    EmailSignup.init(document.getElementById('preload-1'), 'left');
-                 }
-             }
-         ];
-     };
+                }
+            },
+            {
+                id: 'email-signup-inline',
+                test: function() {
+                    this.component = new EmailSignup(document.getElementById('preload-1'), 'inline');
+                }
+            },
+            {
+                id: 'email-signup-left',
+                test: function() {
+                    this.component = new EmailSignup(document.getElementById('preload-1'), 'left');
+                }
+            }
+        ];
+    };
 
-     return EmailSignupTest;
+    return EmailSignupTest;
 });
