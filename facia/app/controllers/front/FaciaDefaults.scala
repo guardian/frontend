f05e2d84bf0c6c68e71453318e3c6bf7ec014c16
Francis Carr <francis.carr@guardian.co.uk>
2013-12-18 17:51:00 +0000

    Fix the FaciaDefaults trait to return the new Config json style

--- facia/app/controllers/front/FaciaDefaults.scala
+++ facia/app/controllers/front/FaciaDefaults.scala
@@ -1,8 +1,8 @@
 package controllers.front
 
-import model.{Collection, Config}
+import model.Collection
 import common.Edition
-import conf.ContentApi
+import play.api.libs.json.{JsObject, Json}
 
 trait FaciaDefaults {
 
@@ -14,19 +14,62 @@ trait FaciaDefaults {
     "au", "au/commentisfree", "au/sport", "au/culture", "au/business", "au/money"
   )
 
-  def createConfig(id: String): Config = Config(
-    id              = "%s/%s".format(id, defaultStyle),
-    contentApiQuery = Option(ContentApi.FaciaDefaults.generateContentApiQuery(id)),
-    displayName     = None,
-    collectionTone  = None
+  val frontsMap: Map[String, List[String]] = Map(
+    ("uk",                List("uk/news/regular-stories")),
+    ("us",                List("us/news/regular-stories")),
+    ("au",                List("au/news/regular-stories")),
+    ("uk/commentisfree",  List("uk/commentisfree/regular-stories")),
+    ("us/commentisfree",  List("us/commentisfree/regular-stories")),
+    ("au/commentisfree",  List("au/commentisfree/regular-stories")),
+    ("uk/sport",          List("uk/sport/regular-stories")),
+    ("us/sport",          List("us/sport/regular-stories")),
+    ("au/sport",          List("au/sport/regular-stories")),
+    ("uk/culture",        List("uk/culture/regular-stories")),
+    ("us/culture",        List("us/culture/regular-stories")),
+    ("au/culture",        List("au/culture/regular-stories")),
+    ("uk/business",       List("uk/business/regular-stories")),
+    ("us/business",       List("us/business/regular-stories")),
+    ("au/business",       List("au/business/regular-stories")),
+    ("uk/money",          List("uk/money/regular-stories")),
+    ("us/money",          List("us/money/regular-stories")),
+    ("au/money",          List("au/money/regular-stories"))
+  )
+
+  val collectionsMap: Map[String, Map[String, String]] = Map(
+    ("uk/news/regular-stories",           Map(("apiQuery", "?edition=UK"))),
+    ("us/news/regular-stories",           Map(("apiQuery", "?edition=US"))),
+    ("au/news/regular-stories",           Map(("apiQuery", "?edition=AU"))),
+    ("uk/commentisfree/regular-stories",  Map(("apiQuery", "commentisfree?edition=UK"))),
+    ("us/commentisfree/regular-stories",  Map(("apiQuery", "commentisfree?edition=US"))),
+    ("au/commentisfree/regular-stories",  Map(("apiQuery", "commentisfree?edition=AU"))),
+    ("uk/sport/regular-stories",          Map(("apiQuery", "sport?edition=UK"))),
+    ("us/sport/regular-stories",          Map(("apiQuery", "sport?edition=US"))),
+    ("au/sport/regular-stories",          Map(("apiQuery", "sport?edition=AU"))),
+    ("uk/culture/regular-stories",        Map(("apiQuery", "culture?edition=UK"))),
+    ("us/culture/regular-stories",        Map(("apiQuery", "culture?edition=US"))),
+    ("au/culture/regular-stories",        Map(("apiQuery", "culture?edition=AU"))),
+    ("uk/business/regular-stories",       Map(("apiQuery", "business?edition=UK"))),
+    ("us/business/regular-stories",       Map(("apiQuery", "business?edition=US"))),
+    ("au/business/regular-stories",       Map(("apiQuery", "business?edition=AU"))),
+    ("uk/money/regular-stories",          Map(("apiQuery", "money?edition=UK"))),
+    ("us/money/regular-stories",          Map(("apiQuery", "money?edition=US"))),
+    ("au/money/regular-stories",          Map(("apiQuery", "money?edition=AU")))
+  )
+
+  def getDefaultConfig: JsObject = Json.obj(
+    "fronts"      -> defaultIds.foldLeft(Json.obj()){case (m, id) => m ++
+      Json.obj(id ->
+        Json.obj("collections" ->
+          Json.arr(frontsMap.get(id))
+        )
+      )
+    },
+    "collections" -> frontsMap.values.flatten.foldLeft(Json.obj()){case (m, id) => m ++
+      Json.obj(id -> collectionsMap.get(id))
+    }
   )
 
   def getEdition(id: String): Edition = Edition.all.find(edition => id.toLowerCase.startsWith(edition.id.toLowerCase)).getOrElse(Edition.defaultEdition)
-
-  def configTuple(id: String): Config = createConfig(id)
-
-  def getDefaultSetup: List[(Config, Collection)] = defaultIds.map (id => (createConfig(id), emptyCollection))
-
 }
 
 object FaciaDefaults extends FaciaDefaults
