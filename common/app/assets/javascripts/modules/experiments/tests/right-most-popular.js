650ada82eaaa261b72d573a25040c6f61b9b49e5
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-12-18 12:26:52 +0000

    Add control group to right hand most popular

--- common/app/assets/javascripts/modules/experiments/tests/right-most-popular.js
+++ common/app/assets/javascripts/modules/experiments/tests/right-most-popular.js
@@ -12,17 +12,13 @@ define([
 
         this.id = 'RightMostPopular';
         this.expiry = '2013-12-31';
-        this.audience = 0.45;
+        this.audience = 0.3;
         this.audienceOffset = 0.7;
         this.description = 'Test whether a most popular component in RHS wil increase page views per session for desktop users';
         this.canRun = function(config) {
             return (detect.getBreakpoint() === 'wide' || detect.getBreakpoint() === 'desktop') && config.page.contentType === 'Article';
         };
         this.variants = [
-            {
-                id: 'Control',
-                test: function () {}
-            },
             {
                 id: 'Trail-3',
                 test: function () {
@@ -33,12 +29,6 @@ define([
                 id: 'Trail-5',
                 test: function () {
                     var r = new RightHandMostPopular(mediator, { type: 'image', maxTrails: 5 });
-                }
-            },
-            {
-                id: 'Trail-6',
-                test: function () {
-                    var r = new RightHandMostPopular(mediator, { type: 'image', maxTrails: 6 });
                 }
             },
             {
@@ -57,12 +47,6 @@ define([
                 id: 'List-5',
                 test: function () {
                     var r = new RightHandMostPopular(mediator, { type: 'list', maxTrails: 5 });
-                }
-            },
-            {
-                id: 'List-6',
-                test: function () {
-                    var r = new RightHandMostPopular(mediator, { type: 'list', maxTrails: 6 });
                 }
             },
             {
