4f41b6776dfa2813835f3b46f62edd9e167462c1
Marc Hibbins <marchibbins@gmail.com>
2013-12-18 11:01:08 +0000

    Handle Formstack not found page

--- common/app/assets/javascripts/modules/identity/formstack-iframe.js
+++ common/app/assets/javascripts/modules/identity/formstack-iframe.js
@@ -22,9 +22,12 @@ define([
 
             bean.on(window, 'resize', self.refreshHeight);
 
-            // Listen for load of form confirmation page,
+            // Listen for load of form confirmation or error page,
             // which has no form, so won't instantiate the Formstack module
-            bean.on(el, 'load', self.refreshHeight);
+            bean.on(el, 'load', function(event) {
+                self.show();
+                self.refreshHeight();
+            });
         };
 
         self.onMessage = function(event) {
