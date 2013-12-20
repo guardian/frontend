2d22e4906a7314060e5e73a0d507e4addbf3771a
Richard Nguyen <richard.nguyen@guardian.co.uk>
2013-12-20 13:34:37 +0000

    Fix commercial test spec paths

--- common/test/assets/javascripts/spec/CommercialLoader.spec.js
+++ common/test/assets/javascripts/spec/CommercialLoader.spec.js
@@ -1,7 +1,7 @@
 define([
-    'utils/mediator',
-    'utils/ajax',
-    'modules/commercial/loader',
+    'common/utils/mediator',
+    'common/utils/ajax',
+    'common/modules/commercial/loader',
     'helpers/fixtures'
 ], function(
     mediator,
