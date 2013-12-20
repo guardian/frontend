f0ee9560d423f27552641fb452942305841fbadc
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-12-20 17:59:04 +0000

    Non-sticky MPU and inline Middle/Middle1 slots

--- common/app/assets/javascripts/modules/experiments/tests/commercial-in-article.js
+++ common/app/assets/javascripts/modules/experiments/tests/commercial-in-article.js
@@ -16,27 +16,20 @@ define([
     Sticky
 ) {
 
-    var inlineAdvertSlot = '<div class="ad-slot ad-slot--inline"><div class="ad-container">...</div></div>',
-        supportsSticky = detect.hasCSSSupport('position', 'sticky'),
-        supportsFixed  = detect.hasCSSSupport('position', 'fixed', true);
+    var inlineAdvertSlot = '<div class="ad-slot ad-slot--inline" data-base="%slot%" data-median="%slot%"><div class="ad-container"></div></div>',
+        mpuSlot = '<div class="ad-slot ad-slot--mpu-banner-ad" data-link-name="ad slot mpu-banner-ad" data-base="%slot%" data-median="%slot%"><div class="ad-container"></div></div>';
 
     // inserts a few inline advert slots in to the page 
-    var createInlineAdSlots = function () {
+    var createInlineAdSlots = function (id) {
         var article = document.getElementsByClassName('js-article__container')[0];
         bonzo(qwery('p:nth-of-type(10n)'), article).each(function(el, i) {
             var cls = (i % 2 === 0) ? 'is-odd' : 'is-even';
-            bonzo(bonzo.create(inlineAdvertSlot)).addClass(cls).insertAfter(this);
+            bonzo(bonzo.create(inlineAdvertSlot.replace(/%slot%/g, id))).addClass(cls).insertAfter(this);
         });
     };
     
-    // inserts a few inline advert slots in to the page 
-    var createAdhesiveAdSlots = function () {
-        if(!supportsSticky && supportsFixed) {
-            var s = new Sticky({
-                elCls: 'ad-slot--top-banner-ad',
-                id: 'Top2'
-            });
-        }
+    var createMpuAdSlot = function (id) {
+        bonzo(qwery('.js-mpu-ad-slot .social-wrapper')).after(bonzo.create(mpuSlot.replace(/%slot%/g, id))[0]);
     };
 
     var CommercialInArticles = function () {
@@ -46,35 +39,28 @@ define([
         this.id = 'CommercialInArticles';
         this.expiry = '2014-01-07';
         this.audience = 0.1;
-        this.audienceOffset = 0;
+        this.audienceOffset = 0.5;
         this.description = 'Inserts commercial components in to the articles to monitor CTR';
         this.canRun = function(config) {
-            //console.log((/wide|desktop/).test(detect.getBreakpoint()) && supportsSticky && supportsFixed && config.page.contentType === 'Article');
-            return (   config.page.contentType === 'Article'
-               && (/wide|desktop/).test(detect.getBreakpoint())
-               && supportsSticky
-               && supportsFixed
-              );
+            return (config.page.contentType === 'Article'
+                && (/wide|desktop/).test(detect.getBreakpoint())
+            );
         };
         this.variants = [
             {
-                id: 'commercialAdhesive',
+                id: 'commercialInline',
                 test: function(context, config) {
-                    // Commercial components served into adhesive position
-                    // Advertising served into in-article position
                     guardian.config.page.oasSiteIdHost = 'www.theguardian-alpha1.com';
-                    createInlineAdSlots();
-                    createAdhesiveAdSlots();
+                    createInlineAdSlots('Middle1');
+                    createMpuAdSlot('Middle');
                 }
             },
             {
-                id: 'commercialInline',
-                test: function(context, config, isBoth) {
-                    // Commercial components served into in-article position,
-                    // Advertising served into adhesive position and; 
-                    guardian.config.page.oasSiteIdHost = 'www.theguardian-alpha2.com';
-                    createInlineAdSlots();
-                    createAdhesiveAdSlots();
+                id: 'commercialAdhesive',
+                test: function(context, config) {
+                    guardian.config.page.oasSiteIdHost = 'www.theguardian-alpha1.com';
+                    createInlineAdSlots('Middle');
+                    createMpuAdSlot('Middle1');
                 }
             },
             {
