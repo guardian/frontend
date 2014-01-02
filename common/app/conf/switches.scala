4ec5e92b3c0e7aa68950aa15654a70562ab2fa3f
Matt Chadburn <matt.chadburn@guardian.co.uk>
2014-01-02 16:53:18 +0000

    Underline test off by default

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -242,7 +242,7 @@ object Switches extends Collections {
 
   val ABUnderlineLinks = Switch("A/B Tests", "ab-underline-links",
     "If this is switched on an AB test runs whereby links in articles are underline (with CSS)",
-    safeState = On)
+    safeState = Off)
 
   // Sport Switch
 
