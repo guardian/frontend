eba9b8ce4b7d998b9781fa71bc6d6d6719723830
Francis Carr <francis.carr@guardian.co.uk>
2014-01-08 10:52:24 +0000

    Ensure two items do not appear in the same list

--- common/app/model/facia.scala
+++ common/app/model/facia.scala
@@ -12,7 +12,10 @@ case class Collection(curated: Seq[Trail],
                       results: Seq[Trail],
                       displayName: Option[String]) {
 
-  def items: Seq[Trail] = curated ++ editorsPicks.filterNot(ep => curated.exists(_.url == ep.url)) ++ results.filterNot (r => curated.exists(_.url == r.url))
+  def items: Seq[Trail] =
+    curated ++
+    editorsPicks.filterNot(ep => curated.exists(_.url == ep.url)) ++
+    results.filterNot (r => curated.exists(_.url == r.url) || editorsPicks.exists(_.url == r.url))
 }
 
 object Collection {
