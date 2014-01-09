3c3625ae4f8faf131a9a4105c879934b599ec2bd
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-01-09 11:56:58 +0000

    Add line charts to ab admin page

--- admin/app/model/abtests/AbTestJob.scala
+++ admin/app/model/abtests/AbTestJob.scala
@@ -13,14 +13,18 @@ object AbTestJob extends Logging with ExecutionContexts {
 
     CloudWatch.AbMetricNames map { result =>
       // Group variant names by test name
-      val tests = result.getMetrics.asScala.map(_.getMetricName.split("-")).collect {
-                    case Array(test: String, variant: String, _*) => (test, variant) }.groupBy(_._1).withDefaultValue(Nil)
+      val tests = result.getMetrics.asScala.map(_.getMetricName.split("-").toList).collect {
+                    case test :: variant => (test, variant.mkString("-")) }.groupBy(_._1)
 
       val switches = conf.Switches.all.filter(_.name.startsWith("ab-")).map(switch => JavaScriptVariableName(switch.name))
 
       val testVariants = switches.foldLeft(Map.empty[String, Seq[String]]) ( (acc, switch ) => {
-        // Update map with a list of variants for the ab-test switch.
-        acc.updated(switch, tests(switch).map(_._2))
+        if (tests.isDefinedAt(switch)) {
+          // Update map with a list of variants for the ab-test switch.
+          acc.updated(switch, tests(switch).map(_._2))
+        } else {
+          acc
+        }
       })
 
       AbTests.update(testVariants)
--- admin/app/model/abtests/AbTests.scala
+++ admin/app/model/abtests/AbTests.scala
@@ -1,5 +1,9 @@
 package model.abtests
 
+import tools.{CloudWatch, LineChart}
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest
+import org.joda.time.DateTime
+
 object AbTests {
 
   private val abTests = common.AkkaAgent[Map[String, Seq[String]]](Map.empty)
@@ -10,5 +14,30 @@ object AbTests {
 
   def update(testVariants: Map[String, Seq[String]]){
       abTests.send(testVariants)
+  }
+
+  case class AbChart(testId: String, variants: Seq[String])
+
+  def getAbCharts(): Seq[LineChart] = {
+    val abTests = getTests()
+
+    abTests.keys.map { abTest =>
+        val variants = abTests(abTest)
+
+        val cloudwatchResults = variants.map { variant =>
+          CloudWatch.euWestClient.getMetricStatisticsAsync( new GetMetricStatisticsRequest()
+            .withStartTime(new DateTime().minusHours(24).toDate)
+            .withEndTime(new DateTime().toDate)
+            .withPeriod(3600)
+            .withStatistics("Average")
+            .withNamespace("AbTests")
+            .withMetricName(s"$abTest-$variant")
+            .withDimensions(CloudWatch.stage),
+            CloudWatch.asyncHandler)
+        }
+
+        // Make a list of line charts
+        new LineChart(abTest, Seq("Time") ++ variants, cloudwatchResults:_*)
+    }.toSeq
   }
 } 
