58b48aaba040c200dd230bd771c9a480eacbd2c2
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2014-01-09 10:48:14 +0000

    Refactoring - creating BlankComment

--- discussion/app/controllers/CommentsController.scala
+++ discussion/app/controllers/CommentsController.scala
@@ -4,10 +4,7 @@ import model.Cached
 import scala.concurrent.Future
 import common.JsonComponent
 import play.api.mvc.{ Action, RequestHeader, SimpleResult }
-import play.api.libs.json.Json
-import discussion.model.{ DiscussionKey, Comment }
-import play.api.data._
-import play.api.data.Forms._
+import discussion.model.{BlankComment, DiscussionKey}
 
 trait CommentsController extends DiscussionController {
 
@@ -44,31 +41,12 @@ trait CommentsController extends DiscussionController {
   def getComments(key: DiscussionKey, page: String = "1", orderBy: String = "newest", isTopComments: Boolean = false)(implicit request: RequestHeader):Future[SimpleResult] = {
     val allResponses = request.getQueryString("allResponses").exists(_ == "true")
     val commentPage = if (isTopComments) discussionApi.topCommentsFor(key) else discussionApi.commentsFor(key, page, orderBy, allResponses)
-    val blankComment = Comment(Json.parse("""{
-      "id": 5,
-      "body": "",
-      "responses": [],
-      "userProfile": {
-        "userId": "",
-        "displayName": "",
-        "webUrl": "",
-        "apiUrl": "",
-        "avatar": "",
-        "secureAvatarUrl": "",
-        "badge": []
-      },
-      "isoDateTime": "2011-10-10T09:25:49Z",
-      "status": "visible",
-      "numRecommends": 0,
-      "isHighlighted": false
-    }"""))
-
     commentPage map {
       page =>
         Cached(60) {
           if (request.isJson)
             JsonComponent(
-              "html" -> views.html.fragments.commentsBody(page, blankComment, isTopComments).toString,
+              "html" -> views.html.fragments.commentsBody(page, BlankComment(), isTopComments).toString,
               "hasMore" -> page.hasMore,
               "currentPage" -> page.currentPage,
               "currentCommentCount" -> page.comments.length
