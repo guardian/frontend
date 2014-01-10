bb43ce5c6cefa7caed2592c54bb533d16ed3de6f
Grant Klopper <grant.klopper@gmail.com>
2014-01-10 12:29:24 +0000

    Cleanup after new API rollout

--- common/test/package.scala
+++ common/test/package.scala
@@ -26,9 +26,8 @@ trait TestSettings {
   val recorder = new ContentApiHttpRecorder {
     override lazy val baseDir = new File(System.getProperty("user.dir"), "data/database")
   }
-                                                                      // TODO otherKey is just temporary so we can
-                                                                      // roll this out without TC downtime
-  private def verify(property: String, hash: String, message: String, otherKey: Option[String] = None) {
+
+  private def verify(property: String, hash: String, message: String) {
 
     val expectedHash = DigestUtils.sha256Hex(property)
     if (expectedHash != hash && !otherKey.exists(_ == expectedHash)) {
@@ -36,8 +35,7 @@ trait TestSettings {
       // the println makes it easier to spot what is wrong in tests
       println()
       println(s"----------- $message -----------")
-      println(hash)
-      println(DigestUtils.sha256Hex(property))
+      println()
 
       throw new RuntimeException(message)
     }
@@ -56,9 +54,8 @@ trait TestSettings {
 
     verify(
       Configuration.contentApi.elasticSearchHost,
-      "973dff7baa408e6f2334e3cf4ca36a960f1743b6d09911ff68723db9cbe62163",
-      "YOU ARE NOT USING THE CORRECT ELASTIC SEARCH CONTENT API HOST",
-      Some("37f3bee67d016a9fec7959aa5bc5e53fa7fdc688f987c0dea6fa0f6af6979079")
+      "37f3bee67d016a9fec7959aa5bc5e53fa7fdc688f987c0dea6fa0f6af6979079",
+      "YOU ARE NOT USING THE CORRECT ELASTIC SEARCH CONTENT API HOST"
     )
 
     verify(
