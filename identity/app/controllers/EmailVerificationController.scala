8b983900e569bdfc6d3b73c0852acb84c07143fe
Nick-Smith <nicholas.smith@guardian.co.uk>
2014-01-13 12:07:17 +0000

    Updated the resend validation email URL.

--- identity/app/controllers/EmailVerificationController.scala
+++ identity/app/controllers/EmailVerificationController.scala
@@ -9,14 +9,38 @@ import utils.SafeLogging
 import model.IdentityPage
 
 @Singleton
-class EmailVerificationController @Inject()( api : IdApiClient, idRequestParser: IdRequestParser, idUrlBuilder: IdentityUrlBuilder )
+class EmailVerificationController @Inject()( api: IdApiClient,
+                                             idRequestParser: IdRequestParser,
+                                             idUrlBuilder: IdentityUrlBuilder)
   extends Controller with ExecutionContexts with SafeLogging {
+  import ValidationState._
 
   val page = IdentityPage("/verify-email", "Verify Email", "verify-email")
 
-  def verify( token : String) = Action { implicit request =>
-    val idRequest = idRequestParser(request)
-    Ok(views.html.email_verified(page, idRequest, idUrlBuilder))
-  }
+  def verify(token: String) = Action.async {
+    implicit request =>
+      val idRequest = idRequestParser(request)
 
+      api.validateEmail(token, idRequest.trackingData) map {
+        response =>
+          val validationState = response match {
+            case Left(errors) =>
+              errors.head.message match {
+                case "User Already Validated" => validated
+                case "Token expired" => expired
+                case _ => invalid
+              }
+
+            case Right(ok) => validated
+          }
+          Ok(views.html.email_verified(validationState, page, idRequest, idUrlBuilder))
+      }
+  }
+}
+
+sealed case class ValidationState(isValidated: Boolean, isExpired: Boolean)
+object ValidationState {
+  val validated = new ValidationState(true, false)
+  val expired = new ValidationState(false, true)
+  val invalid = new ValidationState(false, false)
 }
