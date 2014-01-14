13a9bd2fb595c734802459e241eb52abb29a5a75
Ben Holland <ben.holland@guardian.co.uk>
2014-01-14 16:18:57 +0000

    Added unit tests for article body adverts

---
+++ common/test/assets/javascripts/spec/ArticleBodyAdverts.spec.js
@@ -0,0 +1,43 @@
+define([ 'common/common',
+         'bean',
+         'bonzo',
+         'common/modules/adverts/article-body-adverts',
+         'helpers/fixtures'], function(common, bean, bonzo, ArticleBodyAdverts, fixtures) {
+
+
+        describe("ArticleBodyAdverts", function() {
+            var style;
+            var conf = {
+                    id: 'article',
+                    fixtures: [
+                                '<div class="mpu-container js-mpu-ad-slot"><div class="social-wrapper social-wrapper--aside"><h2 class="article__meta-heading tone-colour">Share this article</h2></div></div>',
+                                '<div class="article-body from-content-api"><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p></div>'
+                              ]
+            };
+
+            beforeEach(function() {
+                fixtures.render(conf);
+            });
+
+            afterEach(function() {
+                fixtures.clean();
+                style.remove();
+            });
+
+            it("Should insert an ad container in the secondary column", function() {
+                style = bonzo(bonzo.create('<style type="text/css"></style>'))
+                    .html('body:after{ content: "wide"}')
+                    .appendTo('head');
+
+                ArticleBodyAdverts.init();
+
+                expect(document.querySelectorAll('.ad-slot--mpu-banner-ad').length).toBe(1);
+            });
+
+            it("Should insert an 2 inline ad containers to the content", function() {
+                ArticleBodyAdverts.init();
+
+                expect(document.querySelectorAll('.ad-slot--inline').length).toBe(2);
+            });
+        });
+    });
