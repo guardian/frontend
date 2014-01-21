f5635d36edac595fd39f82612c549ac06db8d7b0
Darren Hurley <darren.hurley@guardian.co.uk>
2014-01-21 10:25:28 +0000

    Revert "Merge pull request #2801 from guardian/303-preference-status"
    This reverts commit 6e78a30085b13ca5d8d644650d358022ccfdfe65, reversing
    changes made to 66c486200fc811108b1996bd69cf79314301a5f5.

--- core-navigation/app/controllers/ChangeAlphaController.scala
+++ core-navigation/app/controllers/ChangeAlphaController.scala
@@ -7,8 +7,8 @@ object ChangeAlphaController extends Controller with PreferenceController {
   def render(networkFront: String, optaction: String, redirectUrl: String) = Action { implicit request =>
     val abCookieName: String = s"GU_${networkFront.toUpperCase()}_ALPHA"
     optaction match {
-      case "optin"  => switchTo303(abCookieName -> "true", redirectUrl)
-      case "optout" => switchTo303(abCookieName -> "false", redirectUrl)
+      case "optin"  => switchTo(abCookieName -> "true", redirectUrl)
+      case "optout" => switchTo(abCookieName -> "false", redirectUrl)
       case _ => NotFound("unknown action")
     }
   }
