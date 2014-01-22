ba2fa04023af3651ac1ce493e63e693cb29a6383
Ben Holland <ben.holland@guardian.co.uk>
2014-01-22 11:04:03 +0000

    Refactored to remove growing number of ad placement rules
    Now placing the ad before the target paragraph. This means we don't
    have to worrry about what comes after the paragraph, i.e. h2 or figure

--- common/app/assets/javascripts/modules/adverts/article-body-adverts.js
+++ common/app/assets/javascripts/modules/adverts/article-body-adverts.js
@@ -39,31 +39,20 @@ define([
             article           = document.getElementsByClassName('js-article__container')[0];
 
         $(paragraphSelector, article).each(function(el, i) {
-            // Checking if we've exceeded the limit
-            if(limit !== null && limit < (i + 1)) {
+            var $el = $(el),
+                cls = (i % 2 === 0) ? 'is-odd' : 'is-even';
+
+            /*
+             - Checks if limit is set and if so, checks it hasn't been exceeded
+             - Checks is the $target element exists. If not, then you are at the end of the article
+             - Checks if the text length is below 120 characters - helps prevent against empty paragraphs
+               and paragraphs being used instead of order/unordered lists
+             */
+            if(limit !== null && limit < (i + 1) || $el.next()[0] === undefined || $el.text().length < 120) {
                 return false;
             }
 
-            // This protects against empty paragraph tags and paragraphs being used
-            // instead of order/unordered lists
-            if(bonzo(el).text().length < 120) {
-                return false;
-            }
-
-            var target = el,
-                cls    = (i % 2 === 0) ? 'is-odd' : 'is-even';
-
-            // Places the advert after h2 tags on all breakpoints except mobile
-            if(detect.getBreakpoint() !== 'mobile' && target.nextElementSibling && target.nextElementSibling.nodeName.toLowerCase() === 'h2') {
-                target = target.nextElementSibling;
-            }
-
-            // If the next element is a figure, then place the ad after that.
-            if(target.nextElementSibling && target.nextElementSibling.nodeName.toLowerCase() === 'figure') {
-                target = target.nextElementSibling;
-            }
-
-            bonzo(bonzo.create(template.replace(/%slot%/g, id))).addClass(cls).insertAfter(target);
+            bonzo(bonzo.create(template.replace(/%slot%/g, id))).addClass(cls).insertBefore($el);
         });
     };
 
--- common/test/assets/javascripts/spec/ArticleBodyAdverts.spec.js
+++ common/test/assets/javascripts/spec/ArticleBodyAdverts.spec.js
@@ -14,7 +14,7 @@ define([ 'common/common',
                     id: 'article',
                     fixtures: [
                                 '<div class="mpu-container js-mpu-ad-slot"><div class="social-wrapper social-wrapper--aside"><h2 class="article__meta-heading tone-colour">Share this article</h2></div></div>',
-                                '<div class="article-body from-content-api"><h2>The winner</h2><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><h2>The year to date</h2><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><figure id=test_figure class="img img--extended img--landscape"><img src=http://i.guim.co.uk/n/sys-images/Guardian/Pix/pictures/2014/1/20/1390230017580/Yume-Kitchen-Bristol-001.jpg alt="Yume Kitchen, Bristol" width=460 height=276 class=gu-image></figure><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><h2>Top 10 films 13-15 December</h2><p><strong>1.</strong><a href=/film/movie/154178/hobbit data-link-name="in body link" class=u-underline>The Hobbit: The Desolation of Smaug</a> , £9,325,626 from 580 sites ( <strong>New</strong>)</p><p><strong>2.</strong><a href=/film/movie/155770/frozen data-link-name="in body link" class=u-underline>Frozen</a> , £4,212,920 from 515 sites. Total: £10,298,514</p><p><strong>3.</strong><a href=/film/movie/158612/hunger-games data-link-name="in body link" class=u-underline>The Hunger Games: Catching Fire</a> , £1,368,662 from 492 sites. Total: £28,824,306</p><p><strong>4.</strong><a href=/film/movie/156058/gravity data-link-name="in body link" class=u-underline>Gravity</a> , £592,321 from 359 sites. Total: £25,763,353</p><p><strong>5.</strong><a href=/film/movie/156373/saving-mr-banks data-link-name="in body link" class=u-underline>Saving Mr Banks</a> , £359,134 from 393 sites. Total: £2,802,991</p><p><strong>6.</strong>Falstaff – Met Opera (live event), £224,763 from 165 sites ( <strong>New</strong>)</p><p><strong>7.</strong><a href=/film/movie/158970/homefront data-link-name="in body link" class=u-underline>Homefront</a> , £202,518 from 259 sites. Total: £942,561</p><p><strong>8.</strong><a href=/film/movie/156233/free-birds data-link-name="in body link" class=u-underline>Free Birds</a> , £191,656 from 416 sites. Total: £1,788,256</p><p><strong>9.</strong><a href=/film/movie/155322/butler data-link-name="in body link" class=u-underline>The Butler</a> , £144,573 from 207 sites. Total: £3,794,786</p><p><strong>10.</strong><a href=/film/movie/150238/carrie data-link-name="in body link" class=u-underline>Carrie</a> , £118,097 from 224 sites. Total: £1,658,491</p></div>'
+                                '<div class="article-body from-content-api"><h2>The winner</h2><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><h2>The year to date</h2><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p>A sentence less than 120 characters</p><p id=short_paragraph>A sentence less than 120 characters</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p><p>A sentence that is long enough to have an advert placed next to it because there are enough characters in this sentence!!!</p></div>'
                               ]
             };
 
@@ -36,38 +36,17 @@ define([ 'common/common',
 
             it("Should insert an ad container in the secondary column", function() {
                 articleBodyAdverts.init();
-                expect(document.querySelectorAll('.ad-slot--mpu-banner-ad').length).toBe(1);
+                expect($('.ad-slot--mpu-banner-ad').length).toBe(1);
             });
 
             it("Should insert an 2 inline ad containers to the content", function() {
                 articleBodyAdverts.init();
-                expect(document.querySelectorAll('.ad-slot--inline').length).toBe(2);
+                expect($('.ad-slot--inline').length).toBe(2);
             });
 
             it("Should ignore any paragraphs less than 120 characters", function() {
                 articleBodyAdverts.init();
-                expect(document.querySelectorAll('.ad-slot--inline').length).toBe(2);
-            });
-
-            it("Should insert an ad container after a H2 tag", function() {
-                articleBodyAdverts.init();
-                expect(document.querySelectorAll('.ad-slot--inline')[0].previousElementSibling.nodeName).toBe('H2');
-            });
-
-            it("Should insert an ad container after a figure tag", function() {
-                articleBodyAdverts.init();
-                expect($('#test_figure').next().hasClass('ad-slot--inline')).toBeTruthy();
-            });
-
-            it("Should NOT insert an ad container after a H2 tag when on a mobile device", function() {
-                style.remove();
-
-                style = bonzo(bonzo.create('<style type="text/css"></style>'))
-                    .html('body:after{ content: "mobile"}')
-                    .appendTo('head');
-
-                articleBodyAdverts.init();
-                expect(document.querySelectorAll('.ad-slot--inline')[0].previousElementSibling.nodeName).toBe('P');
+                expect($('#short_paragraph').previous().hasClass('ad-slot--inline')).toBeFalsy();
             });
 
             it("Should destroy the ads", function() {
