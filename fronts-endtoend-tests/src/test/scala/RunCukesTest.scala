0b61b43e40cca1242140d0251673e6170ceaa1d6
grant Klopper <grant.klopper@gmail.com>
2014-01-26 17:00:31 +0000

    Deleted end-to-end tests. I do not think they are used at all

--- fronts-endtoend-tests/src/test/scala/RunCukesTest.scala
+++
@@ -1,8 +0,0 @@
-package com.gu.fronts.endtoend
-
-import org.junit.runner.RunWith
-import cucumber.api.junit.Cucumber
-
-@RunWith(classOf[Cucumber])
-@Cucumber.Options(tags = Array("~@ignore"))
-class RunCukesTest
