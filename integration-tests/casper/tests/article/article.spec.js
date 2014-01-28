a7889fa02de39e339c06f79f6a69ffbf8154eaa6
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-01-28 12:34:42 +0000

    added additional check for desktop viewport

--- integration-tests/casper/tests/article/article.spec.js
+++ integration-tests/casper/tests/article/article.spec.js
@@ -18,15 +18,29 @@ casper.test.begin("Show Related Content trails", function(test) {
           test.assertEval(function() {
             return __utils__.findAll(".related__container .item__container").length == 5;
             }, "5 related content items are in the DOM");
+          test.assertVisible('.related__container .item:nth-child(5)','5th trail is visible');
           test.done();
-
         }, function timeout(){
             casper.capture(screens + 'related-content-heading-fail.png');
             test.fail("Failed to find related content");
-
         });
     });
 });
+
+casper.test.begin("Correct amount of related content trails are visible at different viewports", function(test) {
+        casper.viewport(viewports.desktop.width, viewports.desktop.height);
+        test.assertNotVisible('.related__container .item:nth-child(5)','5th trail is not visible');
+        test.done();
+        }, function timeout(){
+    casper.capture(screens + 'related-content-desktop-fail.png');
+    test.fail("5th trail is visible at desktop viewpoer");
+        });
+
+
+
+
+
+
 
 
 casper.run(function() {
