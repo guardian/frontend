81aecc6195c203c22fed13b6e09e40a16f1bdb4a
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-01-28 11:37:54 +0000

    added tests for related content

---
+++ integration-tests/casper/tests/article/article.spec.js
@@ -0,0 +1,33 @@
+/* global document */
+'use strict';
+
+/**
+ *
+ * Article feature tests
+ *
+ **/
+
+casper.test.setUp(function() {
+    casper.start(host + "environment/2014/jan/26/power-failures-future-frequent-blackouts-electricity-supply");
+});
+
+casper.test.begin("Show Related Content trails", function(test) {
+    casper.then(function testRelatedContent() {
+        casper.waitForSelector("#related-content-head", function() {
+          var relatedArticles = this.evaluate(function() { return document.querySelectorAll('.related__container .item__container').length; })
+          test.assertSelectorHasText('#related-content-head', 'Related content');
+          test.assertEquals(relatedArticles, 5, '5 related content items are in the DOM');
+          test.done();
+
+        }, function timeout(){
+            casper.capture(screens + 'related-content-heading-fail.png');
+            test.fail("Failed to find related content");
+
+        });
+    });
+});
+
+
+casper.run(function() {
+    this.test.renderResults(true, 0, this.cli.get("xunit") + "article.xml");
+});
