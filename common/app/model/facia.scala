53e7a3db6be65406a2e1775879b414ec01c45cc9
Francis Carr <francis.carr@guardian.co.uk>
2014-01-29 12:02:35 +0000

    Change to Option String

--- common/app/model/facia.scala
+++ common/app/model/facia.scala
@@ -24,16 +24,16 @@ case class Collection(curated: Seq[Trail],
                       mostViewed: Seq[Trail],
                       results: Seq[Trail],
                       displayName: Option[String],
-                      lastUpdated: String,
-                      updatedBy: String,
-                      updatedEmail: String) extends implicits.Collections {
+                      lastUpdated: Option[String],
+                      updatedBy: Option[String],
+                      updatedEmail: Option[String]) extends implicits.Collections {
 
   lazy val items: Seq[Trail] = (curated ++ editorsPicks ++ mostViewed ++ results).distinctBy(_.url)
 }
 
 object Collection {
-  def apply(curated: Seq[Trail]): Collection = Collection(curated, Nil, Nil, Nil, None, DateTime.now.toString, "", "")
-  def apply(curated: Seq[Trail], displayName: Option[String]): Collection = Collection(curated, Nil, Nil, Nil, displayName, DateTime.now.toString, "", "")
+  def apply(curated: Seq[Trail]): Collection = Collection(curated, Nil, Nil, Nil, None, Option(DateTime.now.toString), None, None)
+  def apply(curated: Seq[Trail], displayName: Option[String]): Collection = Collection(curated, Nil, Nil, Nil, displayName, Option(DateTime.now.toString), None, None)
 }
 
 case class FaciaPage(
