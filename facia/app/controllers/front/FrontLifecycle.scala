1b288d8418a966b006529dbb2b07d766609b1081
Francis Carr <francis.carr@guardian.co.uk>
2014-01-29 14:51:52 +0000

    Remove FrontRefreshJob from FrontLifeCycle for facia

--- facia/app/controllers/front/FrontLifecycle.scala
+++ facia/app/controllers/front/FrontLifecycle.scala
@@ -7,26 +7,9 @@ import scala.concurrent.duration._
 trait FrontLifecycle extends GlobalSettings {
   override def onStart(app: play.api.Application) {
     super.onStart(app)
-
-    Jobs.deschedule("FrontRefreshJob")
-    Jobs.schedule("FrontRefreshJob", "0 * * * * ?") {
-
-      // stagger refresh jobs to avoid dogpiling the api
-      Front.refreshJobs().zipWithIndex.foreach{ case (job, index) =>
-        val sec = (index * 2) % 60
-        AkkaAsync.after(sec.seconds){
-          job()
-        }
-      }
-    }
-
-    Front.refreshCollections()
   }
 
   override def onStop(app: play.api.Application) {
-    Jobs.deschedule("FrontRefreshJob")
-    ConfigAgent.close()
-    CollectionAgent.close()
     super.onStop(app)
   }
 }
