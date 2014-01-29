0c47f8e36f03245293dd6d7eaa5d9f4dd92b3fca
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-01-29 11:03:24 +0000

    fix indentation with @kaelig watching over me

--- integration-tests/casper/tests/article/article.spec.js
+++ integration-tests/casper/tests/article/article.spec.js
@@ -14,10 +14,10 @@ casper.test.setUp(function() {
 casper.test.begin("Show Related Content trails", function(test) {
     casper.then(function testRelatedContent() {
         casper.waitForSelector("#related-content-head", function() {
-          test.assertSelectorHasText('#related-content-head', 'Related content');
-          test.assertElementCount('.related__container .item__container', 5, '5 related content items are in the DOM');
-          test.assertVisible('.related__container .item:nth-child(5)','5th trail is visible');
-          test.done();
+            test.assertSelectorHasText('#related-content-head', 'Related content');
+            test.assertElementCount('.related__container .item__container', 5, '5 related content items are in the DOM');
+            test.assertVisible('.related__container .item:nth-child(5)','5th trail is visible');
+            test.done();
         }, function timeout(){
             casper.capture(screens + 'related-content-heading-fail.png');
             test.fail("Failed to find related content");
@@ -27,13 +27,13 @@ casper.test.begin("Show Related Content trails", function(test) {
 
 casper.test.begin("Correct amount of related content trails are visible at different viewports", function(test) {
     casper.viewport(viewports.desktop.width, viewports.desktop.height);
-          test.assertVisible('.related__container .item','1st trail is visible at desktop viewport');
-          test.assertNotVisible('.related__container .item:nth-child(5)','5th trail is not visible at desktop viewport');
-          test.done();
-        }, function timeout(){
-            casper.capture(screens + 'related-content-desktop-fail.png');
-            test.fail("5th trail is visible at desktop viewport");
-        });
+    test.assertVisible('.related__container .item','1st trail is visible at desktop viewport');
+    test.assertNotVisible('.related__container .item:nth-child(5)','5th trail is not visible at desktop viewport');
+    test.done();
+}, function timeout() {
+    casper.capture(screens + 'related-content-desktop-fail.png');
+    test.fail("5th trail is visible at desktop viewport");
+});
 
 casper.run(function() {
     this.test.renderResults(true, 0, this.cli.get("xunit") + "article.xml");
