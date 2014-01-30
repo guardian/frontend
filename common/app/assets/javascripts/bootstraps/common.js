9c4b5d6bd4ab00c6cf0969e1f747fd4c9f0002ae
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-01-30 18:05:19 +0000

    Update bean event handler to write cookies

--- common/app/assets/javascripts/bootstraps/common.js
+++ common/app/assets/javascripts/bootstraps/common.js
@@ -343,7 +343,7 @@ define([
 
         displayOnboardMessage: function (config) {
             if(window.location.hash === '#opt-in-message' && config.switches.networkFrontOptIn) {
-                bean.on('click', '.js-site-message-close', function() {
+                bean.on(document, 'click', '.js-site-message-close', function() {
                     Cookies.add("GU_VIEW", "mobile", 365);
                     Cookies.add("GU_ALPHA", "true", 365);
                 });
