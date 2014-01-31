863470145d6166ea4260a19f43fce7558b4b11d6
chrisfinch <chrisfinchy@gmail.com>
2014-01-31 17:00:10 +0000

    Email signups code

--- common/app/assets/javascripts/modules/experiments/tests/identity-email-signup.js
+++ common/app/assets/javascripts/modules/experiments/tests/identity-email-signup.js
@@ -3,12 +3,12 @@ define([
     "common/modules/identity/email-signup",
     "common/modules/identity/api"
 ], function (mediator, EmailSignup, IdApi) {
-     
+
     var EmailSignupTest = function () {
 
         var _config;
         this.id = 'EmailSignup';
-        this.expiry = '2014-05-24';
+        this.expiry = '2014-02-14';
         this.audience = 0.2;
         this.audienceOffset = 0.1;
         this.description = 'Displays email signup button on relevant article pages';
