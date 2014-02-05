359c196934ef1d867a3f63d05d41a2eeaf86779d
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-02-05 17:34:05 +0000

    Use responsive and classic view values in gu view cookie

--- onward/app/controllers/ChangeViewController.scala
+++ onward/app/controllers/ChangeViewController.scala
@@ -6,7 +6,7 @@ object ChangeViewController extends Controller with PreferenceController {
 
   def render(platform: String, redirectUrl: String) = Action { implicit request =>
     platform match {
-      case "mobile" | "desktop" => switchTo("GU_VIEW" -> platform, redirectUrl)
+      case "mobile" | "desktop" | "classic" | "responsive" => switchTo("GU_VIEW" -> platform, redirectUrl)
       case _ => NotFound("unknown platform")
     }
   }
