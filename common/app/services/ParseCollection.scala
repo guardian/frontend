778a84f23fddf620b5b05fcce840c2ccbab1b885
Francis Carr <francis.carr@guardian.co.uk>
2014-02-05 12:11:05 +0000

    Reduce show fields down to only what is needed

--- common/app/services/ParseCollection.scala
+++ common/app/services/ParseCollection.scala
@@ -135,7 +135,7 @@ trait ParseCollection extends ExecutionContexts with Logging {
           lazy val supportingLinks: List[CollectionItem] = retrieveSupportingLinks(collectionItem)
           if (!hasParent) getArticles(supportingLinks, edition, hasParent=true) else Future.successful(Nil)
         }
-        val response = ContentApi.item(collectionItem.id, edition).showFields("all").response
+        val response = ContentApi.item(collectionItem.id, edition).showFields("trailText,headline,shortUrl").response
 
         response.onFailure{case t: Throwable => log.warn("%s: %s".format(collectionItem.id, t.toString))}
         supportingAsContent.onFailure{case t: Throwable => log.warn("Supporting links: %s: %s".format(collectionItem.id, t.toString))}
@@ -169,7 +169,7 @@ trait ParseCollection extends ExecutionContexts with Logging {
           .pageSize(20)
         val newSearch = queryParamsWithEdition.foldLeft(search){
           case (query, (key, value)) => query.stringParam(key, value)
-        }.showFields("all")
+        }.showFields("trailText,headline,shortUrl")
         newSearch.response map { r =>
           Result(Nil, Nil, Nil, r.results.map(Content(_)))
         }
@@ -181,7 +181,7 @@ trait ParseCollection extends ExecutionContexts with Logging {
           .pageSize(20)
         val newSearch = queryParamsWithEdition.foldLeft(search){
           case (query, (key, value)) => query.stringParam(key, value)
-        }.showFields("all")
+        }.showFields("trailText,headline,shortUrl")
         newSearch.response map { r =>
           Result(Nil, r.editorsPicks.map(Content(_)), r.mostViewed.map(Content(_)), r.results.map(Content(_)))
         }
