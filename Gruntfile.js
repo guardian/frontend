0194973ced245a768652ae5b95d72582673a1a39
Darren Hurley <darren.hurley@guardian.co.uk>
2014-02-06 13:02:12 +0000

    add location of phantom exec

--- Gruntfile.js
+++ Gruntfile.js
@@ -479,6 +479,7 @@ module.exports = function (grunt) {
         env: {
             casperjs: {
                 ENVIRONMENT : (process.env.ENVIRONMENT) ? process.env.ENVIRONMENT : (isDev) ? "dev" : "code",
+                PHANTOMJS_EXECUTABLE : "node_modules/phantomjs/bin/phantomjs",
                 extend: {
                     PATH: {
                         value: 'node_modules/.bin',
