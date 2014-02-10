7060eebb1b1d7f7d6dc8af521d027bd992921471
Francis Carr <francis.carr@guardian.co.uk>
2014-02-10 16:19:12 +0000

    Add no-cache to change endpoints in FaciaToolController

--- facia-tool/app/controllers/FaciaToolController.scala
+++ facia-tool/app/controllers/FaciaToolController.scala
@@ -70,47 +70,51 @@ object FaciaToolController extends Controller with Logging with ExecutionContext
     val block = FaciaApi.publishBlock(id, identity)
     block.foreach(b => pressCollectionId(b.id))
     notifyContentApi(id)
-    Ok
+    NoCache(Ok)
   }
 
   def discardCollection(id: String) = AjaxExpiringAuthentication { request =>
     val identity = Identity(request).get
     FaciaApi.discardBlock(id, identity)
-    Ok
+    NoCache(Ok)
   }
 
   def updateCollectionMeta(id: String): Action[AnyContent] = AjaxExpiringAuthentication { request =>
     FaciaToolMetrics.ApiUsageCount.increment()
-    request.body.asJson flatMap(_.asOpt[CollectionMetaUpdate]) map {
-      case update: CollectionMetaUpdate => {
-        val identity = Identity(request).get
-        UpdateActions.updateCollectionMeta(id, update, identity)
-        notifyContentApi(id)
-        Ok
-      }
-      case _ => NotFound
-    } getOrElse NotFound
+    NoCache {
+      request.body.asJson flatMap(_.asOpt[CollectionMetaUpdate]) map {
+        case update: CollectionMetaUpdate => {
+          val identity = Identity(request).get
+          UpdateActions.updateCollectionMeta(id, update, identity)
+          notifyContentApi(id)
+          Ok
+        }
+        case _ => NotFound
+      } getOrElse NotFound
+    }
   }
 
   def collectionEdits(): Action[AnyContent] = AjaxExpiringAuthentication { request =>
     FaciaToolMetrics.ApiUsageCount.increment()
-    request.body.asJson flatMap (_.asOpt[Map[String, UpdateList]]) map {
-      case update: Map[String, UpdateList] => {
-        val identity: Identity = Identity(request).get
-        val updatedCollections: Map[String, Block] = update.collect {
-          case ("update", updateList) => UpdateActions.updateCollectionList(updateList.id, updateList, identity)
-          case ("remove", updateList) => UpdateActions.updateCollectionFilter(updateList.id, updateList, identity)
-        }.flatten.map(b => (b.id, b)).toMap
+    NoCache {
+      request.body.asJson flatMap (_.asOpt[Map[String, UpdateList]]) map {
+        case update: Map[String, UpdateList] => {
+          val identity: Identity = Identity(request).get
+          val updatedCollections: Map[String, Block] = update.collect {
+            case ("update", updateList) => UpdateActions.updateCollectionList(updateList.id, updateList, identity)
+            case ("remove", updateList) => UpdateActions.updateCollectionFilter(updateList.id, updateList, identity)
+          }.flatten.map(b => (b.id, b)).toMap
 
-        pressCollectionIds(updatedCollections.keySet)
+          pressCollectionIds(updatedCollections.keySet)
 
-        if (updatedCollections.nonEmpty)
-          Ok(Json.toJson(updatedCollections)).as("application/json")
-        else
-          NotFound
-      }
-      case _ => NotFound
-    } getOrElse NotFound
+          if (updatedCollections.nonEmpty)
+            Ok(Json.toJson(updatedCollections)).as("application/json")
+          else
+            NotFound
+        }
+        case _ => NotFound
+      } getOrElse NotFound
+    }
   }
 
   def notifyContentApi(id: String): Option[Future[Response]] =
