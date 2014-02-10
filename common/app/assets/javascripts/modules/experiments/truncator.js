714c287719f16d02cb37785e854e54002d64ec89
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-02-10 18:03:13 +0000

    Working article truncation

--- common/app/assets/javascripts/modules/experiments/truncator.js
+++ common/app/assets/javascripts/modules/experiments/truncator.js
@@ -2,24 +2,36 @@ define([
     'lodash/objects/assign',
     'lodash/collections/toArray',
     'lodash/collections/find',
+
     'qwery',
-    'bonzo'
+    'bonzo',
+    'bean'
 ], function(
     extend,
     toArray,
     find,
+
     qwery,
-    bonzo
+    bonzo,
+    bean
 ) {
 
     var Truncator = function(config) {
         this.config = extend(this.config, config);
-        this.init();
+        this.el = this.findParagraphAtPercentage(this.getParagraphs());
+        this.truncate();
+    };
+
+    Truncator.prototype.classes =  {
+        el: 'js-truncate',
+        btn: 'js-continue-reading',
+        hidden: 'u-h'
     };
 
     Truncator.prototype.config = {
-        contentEl: qwery('.js-article__container'),
-        percentageCap: 40
+        contentEl: qwery('.js-article__body')[0],
+        percentageCap: 40,
+        template: '<button class="continue-reading js-continue-reading">continue reading</button>'
     };
 
     Truncator.prototype.getParagraphs = function() {
@@ -30,20 +42,46 @@ define([
         return el.innerHTML.split(/\s+/).length;
     };
 
+    Truncator.prototype.getPercentage = function() {
+        return parseInt(this.config.wordCount/100*this.config.percentageCap, 10);
+    };
+
     Truncator.prototype.findParagraphAtPercentage = function(paras) {
         var count = 0;
         return find(paras, function(el) {
             count += this.getWordCount(el);
-            return count > parseInt(this.config.wordCount/100*this.config.percentageCap, 10);
+            return count > this.getPercentage();
         }, this);
     };
 
-    Truncator.prototype.truncate = function(el) {
-        bonzo(el).addClass('truncate');
+    Truncator.prototype.getContentAfterEl = function() {
+        var contentChildren = toArray(this.config.contentEl.children);
+        return contentChildren.slice(contentChildren.indexOf(this.el) + 1, contentChildren.length);
     };
 
-    Truncator.prototype.init = function() {
-        this.truncate(this.findParagraphAtPercentage(this.getParagraphs()));
+    Truncator.prototype.toggleContent = function() {
+        this.getContentAfterEl().forEach(function(el){
+            bonzo(el).toggleClass(this.classes.hidden);
+        }, this);
+    };
+
+    Truncator.prototype.showCta = function() {
+        bonzo(this.el).append(this.config.template);
+    };
+
+    Truncator.prototype.hideCta = function() {
+        qwery('.' + this.classes.btn)[0].remove();
+    };
+
+    Truncator.prototype.bindListeners = function() {
+        bean.on(this.el, 'click', '.' + this.classes.btn, this.toggleContent.bind(this));
+        bean.on(this.el, 'click', '.' + this.classes.btn, this.hideCta.bind(this));
+    };
+
+    Truncator.prototype.truncate = function() {
+        this.showCta();
+        this.bindListeners();
+        this.toggleContent();
     };
 
     return Truncator;
