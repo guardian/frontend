25b218233c9f4433b2cb14fbbef154ad9f6a3e3e
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-02-12 18:33:31 +0000

    Add is-not-truncated styling hook

--- common/app/assets/javascripts/modules/experiments/truncator.js
+++ common/app/assets/javascripts/modules/experiments/truncator.js
@@ -77,6 +77,7 @@ define([
 
     Truncator.prototype.hideCta = function() {
         qwery('.' + this.classes.btn)[0].remove();
+        bonzo(qwery('.' + this.classes.actions)).addClass('is-not-truncated');
     };
 
     Truncator.prototype.truncate = function() {
