eb53741da0e7912f546947078f4fb041d2e15007
Francis Carr <francis.carr@guardian.co.uk>
2014-02-12 16:55:24 +0000

    Schedule RefreshFrontsJob to run every minute

--- admin/app/Global.scala
+++ admin/app/Global.scala
@@ -1,5 +1,6 @@
-import common.CloudWatchApplicationMetrics
+import common.{Jobs, CloudWatchApplicationMetrics}
 import conf.Management
+import jobs.RefreshFrontsJob
 import model.AdminLifecycle
 import play.api.mvc.{Results, RequestHeader}
 import scala.concurrent.Future
@@ -10,4 +11,25 @@ object Global extends AdminLifecycle with CloudWatchApplicationMetrics with Resu
   override def onError(request: RequestHeader, ex: Throwable) = Future.successful(InternalServerError(
     views.html.errorPage(ex)
   ))
+
+  def scheduleJobs() {
+    Jobs.schedule("FrontPressJob", "0 * * * * ?") {
+      RefreshFrontsJob.run()
+    }
+  }
+
+  def descheduleJobs() {
+    Jobs.deschedule("FrontPressJob")
+  }
+
+  override def onStart(app: play.api.Application) {
+    super.onStart(app)
+    descheduleJobs()
+    scheduleJobs()
+  }
+
+  override def onStop(app: play.api.Application) {
+    descheduleJobs()
+    super.onStop(app)
+  }
 }
