431aecee797b3c38f688d819ca12eaf318b791dc
Francis Carr <francis.carr@guardian.co.uk>
2014-02-18 12:56:27 +0000

    Add facia-tool cloudwatch metrics

--- facia-tool/app/Global.scala
+++ facia-tool/app/Global.scala
@@ -1,13 +1,26 @@
-import common.Jobs
+import common.{FaciaToolMetrics, CloudWatchApplicationMetrics, FaciaMetrics, Jobs}
+import conf.Management
 import frontpress.FaciaToolConfigAgent
 import java.io.File
 import jobs.FrontPressJob
 import play.api._
 import services.FaciaToolLifecycle
 
-object Global extends FaciaToolLifecycle with GlobalSettings {
+object Global extends FaciaToolLifecycle with GlobalSettings with CloudWatchApplicationMetrics {
 
   lazy val devConfig = Configuration.from(Map("session.secure" -> "false"))
+
+  override lazy val applicationName = Management.applicationName
+  override def applicationMetrics: Map[String, Double] = Map(
+    ("api-usage", FaciaToolMetrics.ApiUsageCount.getValue().toDouble),
+    ("api-proxy-usage", FaciaToolMetrics.ProxyCount.getValue().toDouble),
+    ("content-api-put-failure", FaciaToolMetrics.ContentApiPutFailure.getValue().toDouble),
+    ("content-api-put-success", FaciaToolMetrics.ContentApiPutSuccess.getValue().toDouble),
+    ("draft-publish", FaciaToolMetrics.DraftPublishCount.getValue().toDouble),
+    ("auth-expired", FaciaToolMetrics.ExpiredRequestCount.getValue().toDouble),
+    ("front-press-failure", FaciaToolMetrics.FrontPressFailure.getValue().toDouble),
+    ("front-press-success", FaciaToolMetrics.FrontPressSuccess.getValue().toDouble)
+  )
 
   override def onLoadConfig(config: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
     val newConfig: Configuration = if (mode == Mode.Dev) config ++ devConfig else config
