b0d00094bc794425281417a154bb233ad789d0bf
meza <meza@meza.hu>
2014-02-19 17:03:18 +0000

    resurrecting

--- fronts-endtoend-tests/src/test/java/com/gu/fronts/endtoend/EditorSetupSteps.java
+++ fronts-endtoend-tests/src/test/java/com/gu/fronts/endtoend/EditorSetupSteps.java
@@ -7,6 +7,7 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import hu.meza.tools.galib.GoogleAuthenticator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -24,8 +25,9 @@ public class EditorSetupSteps {
 
     @Before
     public void createAnEditor() {
-//        editors.addActor("an editor", createEditor());
-        isATrailBlockEditor("the editor");
+        TrailBlockEditor editor = createEditor();
+        editors.addActor("the editor", editor);
+        editors.addActor("an editor", editor);
     }
 
     @Given("^(.*) is a trailblock editor$")
@@ -34,22 +36,36 @@ public class EditorSetupSteps {
     }
 
     private TrailBlockEditor createEditor() {
+        Cookie cookie = null;
+        int tries = 0;
         try {
             WebDriver driver = new FirefoxDriver();
-            driver.get(config.baseUrl() + "/logout");
+            while (cookie == null) {
+                if (tries > 3) {
+                    throw new RuntimeException("Could not get user cookie");
+                }
+                driver.get(config.baseUrl() + "/logout");
 
-            driver.findElement(By.id("login-button")).click();
-            driver.findElement(By.id("Email")).sendKeys(config.getUsename());
-            driver.findElement(By.id("Passwd")).sendKeys(config.getPassword());
-            driver.findElement(By.id("signIn")).click();
+                driver.findElement(By.id("login-button")).click();
+                driver.findElement(By.id("Email")).sendKeys(config.getUsename());
+                driver.findElement(By.id("Passwd")).sendKeys(config.getPassword());
+                driver.findElement(By.id("signIn")).click();
 
 
-            GoogleAuthenticator ga = new GoogleAuthenticator(config.getGASecret());
-            driver.findElement(By.id("smsUserPin")).sendKeys(ga.getCode());
-            driver.findElement(By.id("smsVerifyPin")).click();
-            TrailBlockEditor editor = new TrailBlockEditor(config.baseUrl(), driver.manage().getCookieNamed("PLAY_SESSION"));
-            driver.close();
-            return editor;
+                GoogleAuthenticator ga = new GoogleAuthenticator(config.getGASecret());
+                driver.findElement(By.id("smsUserPin")).sendKeys(ga.getCode());
+                driver.findElement(By.id("smsVerifyPin")).click();
+
+                cookie = driver.manage().getCookieNamed("PLAY_SESSION");
+                if (cookie != null) {
+                    TrailBlockEditor editor = new TrailBlockEditor(config.baseUrl(), cookie);
+                    driver.close();
+                    return editor;
+                }
+
+                tries++;
+            }
+            throw new RuntimeException("Could not get user cookie");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
