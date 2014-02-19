1e29900c7a5a936723ca7d1f0954d2965fa62637
James Gorrie <james.gorrie@guardian.co.uk>
2014-02-19 17:53:39 +0000

    Cors tidy and adding flexible content to ajax URLs

--- common/app/model/Cors.scala
+++ common/app/model/Cors.scala
@@ -7,12 +7,9 @@ import conf.Configuration.ajax._
 
 object Cors extends Results {
   def apply(result: SimpleResult)(implicit request: RequestHeader): SimpleResult = {
-    println(request.headers.get("Origin"))
     request.headers.get("Origin") match {
       case None => result
       case Some(requestOrigin) =>
-        println(requestOrigin)
-        println(corsOrigins.find(_ == requestOrigin).getOrElse("*"))
         result.withHeaders(
         "Access-Control-Allow-Origin" -> corsOrigins.find(_ == requestOrigin).getOrElse("*"),
         "Access-Control-Allow-Credentials" -> "true"
