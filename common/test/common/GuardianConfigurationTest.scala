9da2c1ce6d0446a2504ebcc896980cc93eb4eb8a
Francis Carr <francis.carr@guardian.co.uk>
2014-02-20 11:09:00 +0000

    Update md5 hash in GuardianConfigurationTest

--- common/test/common/GuardianConfigurationTest.scala
+++ common/test/common/GuardianConfigurationTest.scala
@@ -21,7 +21,7 @@ class GuardianConfigurationTest extends FlatSpec with Matchers {
     // There is a document (that would have been shared with you) that contains all
     // the properties needed to run this project. Make sure you update it if there 
     // are any required changes, update the hash below, and off you go.
-    hash should be ("55b9c065b2d91eaecba4f49ab5e52802")
+    hash should be ("17da268edd30594f5e2a449edb218037")
 
   }
 }
