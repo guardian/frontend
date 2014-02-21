48cbb546acb8c52c8dfc5e9ab43ead3e64c8e9e8
Matt Chadburn <matt.chadburn@guardian.co.uk>
2014-02-21 11:31:03 +0000

    Corrected location of files in s3

--- archive/test/ArchiveControllerTest.scala
+++ archive/test/ArchiveControllerTest.scala
@@ -12,41 +12,41 @@ class ArchiveControllerTest extends FlatSpec with Matchers with UsesElasticSearc
   "Archive Controller" should "return a HTTP 303 when it finds a match in DynamoDB" in Fake {
     val url = "www.theguardian.com/media/emailservices/article/0,,1694396,.html"
     val result = controllers.ArchiveController.lookup(url)(TestRequest())
-    //header("Location", result) should be(Some("http://www.theguardian.com/media/2006/jan/25/1"))
-    //status(result) should be(303)
+    header("Location", result) should be(Some("http://www.theguardian.com/media/2006/jan/25/1"))
+    status(result) should be(303)
   }
   
   it should "return a HTTP 200 when it finds a resource in S3" in Fake {
-    val url = "travel.theguardian.com/askatraveller/0,,345059,00.html"
+    val url = "www.theguardian.com/travel/askatraveller/0,,345059,00.html"
     val result = controllers.ArchiveController.lookup(url)(TestRequest())
-    //status(result) should be(200)
-    //contentType(result) should be(Some("text/html"))
+    status(result) should be(200)
+    contentType(result) should be(Some("text/html"))
   }
 
   it should "return a HTTP 404 when it can not find a resource" in Fake {
     val url = "www.theguardian.com/i/am/not/here"
     val result = controllers.ArchiveController.lookup(url)(TestRequest())
-    //status(result) should be(404)
+    status(result) should be(404)
   }
 
   it should "fetch the HTML of a valid archived URL" in Fake {
-    val result = controllers.ArchiveController.isArchived("travel.theguardian.com/askatraveller/0,,345059,00.html")
-    //result.get should include ("<title>Ask a traveller | guardian.co.uk Travel</title>")
+    val result = controllers.ArchiveController.isArchived("www.theguardian.com/travel/askatraveller/0,,345059,00.html")
+    result.get should include ("<title>Ask a traveller | guardian.co.uk Travel</title>")
   }
   
   it should "return None for a invalid archive URL" in Fake {
     val result = controllers.ArchiveController.isArchived("not/here")
-    //result should be(None)
+    result should be(None)
   }
   
   it should "fetch the destination of a valid redirected URL" in Fake {
     val result = controllers.ArchiveController.isRedirect("www.theguardian.com/media/emailservices/article/0,,1694396,.html")
-    //result should be (Some("http://www.theguardian.com/media/2006/jan/25/1"))
+    result should be (Some("http://www.theguardian.com/media/2006/jan/25/1"))
   }
   
   it should "return Noen for a path that has not been redirected" in Fake {
     val result = controllers.ArchiveController.isRedirect("not/here")
-    //result should be(None)
+    result should be(None)
   }
 
 }
