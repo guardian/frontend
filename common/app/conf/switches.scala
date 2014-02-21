a7ef31c90ccf596cc44c445718708a6331916fa8
Darren Hurley <darren.hurley@guardian.co.uk>
2014-02-21 16:43:54 +0000

    increase expiration

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -387,17 +387,17 @@ object Switches extends Collections {
 
   val NetworkFrontUkAlpha = Switch("Facia", "network-front-uk-alpha",
     "If this is switched on then the uk alpha network fronts will be served if a GU_UK_ALPHA cookie has been dropped",
-    safeState = Off, sellByDate = new DateMidnight(2014, 2, 22)
+    safeState = Off, sellByDate = new DateMidnight(2014, 3, 1)
   )
 
   val NetworkFrontUsAlpha = Switch("Facia", "network-front-us-alpha",
     "If this is switched on then the us alpha network fronts will be served if a GU_US_ALPHA cookie has been dropped",
-    safeState = Off, sellByDate = new DateMidnight(2014, 2, 22)
+    safeState = Off, sellByDate = new DateMidnight(2014, 3, 1)
   )
 
   val NetworkFrontAuAlpha = Switch("Facia", "network-front-au-alpha",
     "If this is switched on then the au alpha network fronts will be served if a GU_AU_ALPHA cookie has been dropped",
-    safeState = Off, sellByDate = new DateMidnight(2014, 2, 22)
+    safeState = Off, sellByDate = new DateMidnight(2014, 3, 1)
   )
 
   // Facia Tool Switches
