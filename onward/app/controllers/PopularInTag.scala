f350bb655c7fe0893087b961b17120d5133b21e5
Matt Osborn <matthew.osborn@theguardian.com>
2014-02-26 12:37:30 +0000

    popular in tag controller

---
+++ onward/app/controllers/PopularInTag.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import common._
+import model._
+import play.api.mvc.{ RequestHeader, Controller, Action }
+import services._
+
+object PopularInTag extends Controller with Related with Logging with ExecutionContexts {
+
+  def renderJson(path: String) = render(path)
+  def render(tag: String) = Action.async { implicit request =>
+    val edition = Edition(request)
+    getPopularInTag(edition, tag) map {
+      case Nil => JsonNotFound()
+      case trails => renderRelated(trails)
+    }
+  }
+
+  private def renderRelated(trails: Seq[Trail])(implicit request: RequestHeader) = Cached(900) {
+    val html = views.html.fragments.relatedTrails(trails, "Related content", 5)
+
+    if (request.isJson)
+      JsonComponent(
+	"html" -> html
+      )
+    else
+      Ok(html)
+  }
+}
