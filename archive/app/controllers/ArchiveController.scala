4a77ab8eaa8a4a6791b500b27bc863ed0e318417
Matt Chadburn <matt.chadburn@guardian.co.uk>
2014-02-26 16:52:54 +0000

    Refactor to remove nested cases

--- archive/app/controllers/ArchiveController.scala
+++ archive/app/controllers/ArchiveController.scala
@@ -11,22 +11,27 @@ import java.net.{URLDecoder, URLEncoder}
 
 object ArchiveController extends Controller with Logging with ExecutionContexts {
  
-  def isRedirect(path: String) = {
+  def isRedirect(path: String): Option[String] = {
     val redirects = DynamoDB.destinationFor(path)
     log.info(s"Checking '${path}' is a redirect in DynamoDB: ${!redirects.isEmpty}")
     redirects
   }
 
-  def isArchived(path: String) = {
+  def isArchived(path: String): Option[String] = {
     val archive = services.S3Archive.getHtml(path)
     log.info(s"Checking '${path}' is a archived in S3: ${!archive.isEmpty}")
     archive
   }
 
-  def isEncoded(path: String) = {
+  def isEncoded(path: String): Option[String] = {
     val decodedPath = URLDecoder.decode(path, "UTF-8")
     if (decodedPath != path) Some(decodedPath) else None
   } 
+
+  // arts/gallery/image/0,,-126 -> arts/pictures/image/0,,-126
+  /*def isGallery(path: String) = {
+    path.replace("/gallery/", "/pictures/") 
+  }*/
 
   // Our redirects are 'normalised' Vignette URLs, Ie. path/to/0,<n>,123,<n>.html -> path/to/0,,123,.html
   def normalise(path: String, zeros: String = "") = {
@@ -39,28 +44,19 @@ object ArchiveController extends Controller with Logging with ExecutionContexts 
       case _ => None
     }
   }
-  
+ 
+  // TODO - 1) fix URLs with JSOUP, 2) , 3)
+
   def lookup(path: String) = Action { implicit request =>
    
-    val encoded = isEncoded(path)
-
-    // is it a redirect -> 301, if not is it archived -> 200, if not then 404
-    encoded match {
-      case Some(_) => Redirect(s"http://${encoded.get}", 301)
-      case _ => {
-        val destination = isRedirect(normalise(path).getOrElse(path))
-        destination match { 
-          case Some(_) => Redirect(destination.get, 301)
-          case _ => {
-            val s3content = isArchived(normalise(path, zeros = "00").getOrElse(path))
-            s3content match {
-              case Some(_) => Ok(views.html.archive(s3content)).as("text/html")
-              case _ => NotFound
-            }
-          } 
-        }
-      }
-    }
+    isEncoded(path)
+      .map { url => Redirect(s"http://${url}", 301) }
+      .orElse { isRedirect(normalise(path).getOrElse(path)) }
+      .map { url => Redirect(s"${url}", 301) }
+      .orElse { isArchived(normalise(path, zeros = "00").getOrElse(path)) }
+      .map { body =>  Ok(views.html.archive(body)).as("text/html") }
+      .getOrElse(NotFound)
+  
   }
 
 }
