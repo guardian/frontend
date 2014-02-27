3a4a422b04d2eac3466cf806fe21bfa7d8bc368b
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-02-27 08:59:07 +0000

    Convert layoutHints module into a class

--- common/app/assets/javascripts/modules/experiments/layoutHints.js
+++ common/app/assets/javascripts/modules/experiments/layoutHints.js
@@ -1,27 +1,30 @@
 define([
-    "common/$",
+    'bonzo',
+    'common/$',
     'common/modules/ui/images'
 ], function($, images){
 
-    var content = {
+    function Layout(config) {
+        var slugs = config.page.pageId.split("/"),
+            slug = slugs[slugs.length -1];
+
+        if(slug in this.content) {
+            this.container = document.getElementById('article');
+            $(this.container).addClass('layout-hints ' + slug);
+            this.content[slug]();
+        }
+    }
+
+    Layout.prototype.content = {
         "fast-ice-rescue-from-antarctica" : function() {
             var img = new Image();
             img.className = 'responsive-img';
             img.setAttribute('data-src', 'http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/2/26/1393435299650/Antarcticaleadimage.jpg');
-            $('.article__main-column').before(img);
+            $('.article__main-column', this.container).before(img);
+            images.upgrade(this.container);
         }
     };
 
-    function init(config) {
-        var slugs = config.page.pageId.split("/"),
-            slug = slugs[slugs.length -1];
-
-        $('article').addClass('layout-hints ' + slug);
-        content[slug]();
-    }
-
-    return {
-        init: init
-    };
+    return Layout;
 
 });
