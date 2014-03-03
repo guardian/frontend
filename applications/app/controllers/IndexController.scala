0d441946616f17c2912abdfaff7d17aeb5c347f1
Grant Klopper <grant.klopper@gmail.com>
2014-03-03 17:14:53 +0000

    Log Googlebot.
    I want to see what effect the pagination has

--- applications/app/controllers/IndexController.scala
+++ applications/app/controllers/IndexController.scala
@@ -12,15 +12,24 @@ trait IndexController extends Controller with Index with Logging with Paging wit
   implicit def getTemplateDedupingInstance: TemplateDeduping = TemplateDeduping()
 
   def renderCombiner(leftSide: String, rightSide: String) = DogpileAction { implicit request =>
+    logGoogleBot(request)
     index(Edition(request), leftSide, rightSide, extractPage(request)).map {
       case Left(page) => renderFaciaFront(page)
       case Right(other) => other
     }
   }
 
+
+  private def logGoogleBot(request: Request[AnyContent]) = {
+    request.headers.get("User-Agent").filter(_.contains("Googlebot")).foreach { bot =>
+      log.info(s"GoogleBot => ${request.uri}")
+    }
+  }
+
   def renderJson(path: String) = render(path)
 
   def render(path: String) = DogpileAction { implicit request =>
+    logGoogleBot(request)
     index(Edition(request), path, extractPage(request)) map {
       case Left(model) => renderFaciaFront(model)
       case Right(other) => other
