02edd19e898c01ef61eefc27fc17ed7d9ca7851a
chrisfinch <chrisfinchy@gmail.com>
2014-03-04 14:53:02 +0000

    javascript to detect form changes

--- common/app/assets/javascripts/modules/identity/account-profile.js
+++ common/app/assets/javascripts/modules/identity/account-profile.js
@@ -10,6 +10,15 @@ define([
         var self = this;
 
         var onInputChange = function (event) {
+            bonzo(event.target.form).addClass("changed");
+        };
+
+        var bindInputs = function (form) {
+            var inputs = Array.prototype.slice.call(form.querySelectorAll(".text-input"));
+            for (var i = inputs.length - 1; i >= 0; i--) {
+                inputs[i].form = form;
+                inputs[i].addEventListener("change", onInputChange);
+            }
         };
 
         return {
@@ -18,12 +27,8 @@ define([
                 var accountProfileForms = context.querySelector(".js-account-profile-forms");
 
                 if (accountProfileForms) {
-                    var forms = accountProfileForms.querySelectorAll('form');
-                    var inputs = Array.prototype.slice.call(document.querySelectorAll(".text-input"));
-                    for (var i = inputs.length - 1; i >= 0; i--) {
-                        inputs[i].addEventListener("change", onInputChange);
-                    }
-
+                    bindInputs(accountProfileForms.querySelector('.js-account-details-form'));
+                    bindInputs(accountProfileForms.querySelector('.js-public-profile-form'));
                 }
             }
         };
