4037cd76f9873a4a15fa1e36b100f254d4654e7d
Francis Carr <francis.carr@guardian.co.uk>
2014-03-11 15:31:57 +0000

    Remove IndexQuery trait

--- facia/app/services/repositories.scala
+++ facia/app/services/repositories.scala
@@ -121,20 +121,3 @@ trait Index extends ConciergeRepository with QueryDefaults {
   val SinglePart = """([\w\d\.-]+)""".r
   val SeriesInSameSection = """(series/[\w\d\.-]+)""".r
 }
-
-//trait ImageQuery extends ConciergeRepository with QueryDefaults {
-//
-//  def image(edition: Edition, path: String): Future[Either[ImageContentPage, SimpleResult]]= {
-//    log.info(s"Fetching image content: $path for edition ${edition.id}")
-//    val response = SwitchingContentApi().item(path, edition)
-//      .showExpired(true)
-//      .showFields("all")
-//      .response.map { response =>
-//      val mainContent: Option[Content] = response.content.filter { c => c.isImageContent } map {Content(_)}
-//      val storyPackage: List[Trail] = response.storyPackage map { Content(_) }
-//      mainContent.map { content => Left(ImageContentPage(content,storyPackage)) }.getOrElse(Right(NotFound))
-//    }
-//
-//    response recover convertApiExceptions
-//  }
-//}
