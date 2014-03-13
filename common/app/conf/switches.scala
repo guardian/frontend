e4d499a19606648b4ffd55a23dc993e80c6c20bf
Darren Hurley <darren.hurley@guardian.co.uk>
2014-03-13 16:24:40 +0000

    remove ab tests and shitty code

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -331,21 +331,6 @@ object Switches extends Collections {
     safeState = Off, sellByDate = new DateMidnight(2014, 3, 17)
   )
 
-  val ABUkContainers = Switch("A/B Tests", "ab-uk-containers",
-    "If this is switched on an A/B test runs that tries a variation of the containers on the UK network front.",
-    safeState = Off, sellByDate = new DateMidnight(2014, 3, 15)
-  )
-
-  val ABUsContainers = Switch("A/B Tests", "ab-us-containers",
-    "If this is switched on an A/B test runs that tries a variation of the containers on the US network front.",
-    safeState = Off, sellByDate = new DateMidnight(2014, 3, 15)
-  )
-
-  val ABAuContainers = Switch("A/B Tests", "ab-au-containers",
-    "If this is switched on an A/B test runs that tries a variation of the containers on the AU network front.",
-    safeState = Off, sellByDate = new DateMidnight(2014, 3, 15)
-  )
-
   // Sport Switch
 
   val LiveCricketSwitch = Switch("Live Cricket", "live-cricket",
@@ -478,9 +463,6 @@ object Switches extends Collections {
     ShowAllArticleEmbedsSwitch,
     PressedFacia,
     FrontPressJobSwitch,
-    ABUkContainers,
-    ABUsContainers,
-    ABAuContainers,
     LayoutHintsSwitch,
     HelveticaEasterEggSwitch,
     LeadAdTopPageSwitch
