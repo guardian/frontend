1d48b7aefbcceabf9da7d911a21b54d6887cfa30
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-03-14 15:25:07 +0000

    Add user timing to app

---
+++ common/app/assets/javascripts/utils/userTiming.js
@@ -0,0 +1,16 @@
+define([
+], function () {
+
+    var perf = window.performance || window.msPerformance || window.webkitPerformance || window.mozPerformance;
+
+    function mark(label) {
+
+        if (perf && perf.mark) {
+             perf.mark("gu." + label);
+        }
+    }
+
+    return {
+        mark: mark
+    };
+});
