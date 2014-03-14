9a048822e3d59f54bd478c8c126834654eb30fb6
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-03-14 15:32:14 +0000

    Use in keyword

--- common/app/assets/javascripts/utils/userTiming.js
+++ common/app/assets/javascripts/utils/userTiming.js
@@ -1,10 +1,10 @@
-define(function () {
+define( function() {
 
     var perf = window.performance || window.msPerformance || window.webkitPerformance || window.mozPerformance;
 
     function mark(label) {
 
-        if (perf && perf.mark) {
+        if (perf && 'mark' in perf) {
              perf.mark("gu." + label);
         }
     }
