2204b1f32f8cfd602c368185d4de6807f6da954f
Adam Fisher <adam.fisher@guardian.co.uk>
2014-03-21 16:56:28 +0000

    Typos and obj name fix

--- sport/test/football/views/colourCheckTest.scala
+++ sport/test/football/views/colourCheckTest.scala
@@ -1,24 +1,24 @@
 package football.views
 
 import org.scalatest.{ShouldMatchers, FunSuite}
-import views.colourCheck
+import views.ColourTools
 
 class colourCheckTest extends FunSuite with ShouldMatchers {
   test("Whtie should be a light colour") {
-    colourCheck.isLight("ffffff") should equal(true)
+    ColourTools.isLight("ffffff") should equal(true)
   }
 
   test("black should be a dark colour") {
-    colourCheck.isLight("000000") should equal(false)
+    ColourTools.isLight("000000") should equal(false)
   }
 
   test("Marginal colours should be correctly identified") {
-    colourCheck.isLight("333333") should equal(false)
-    colourCheck.isLight("00ccff") should equal(true)
-    colourCheck.isLight("ff00ff") should equal(false)
+    ColourTools.isLight("333333") should equal(false)
+    ColourTools.isLight("00ccff") should equal(true)
+    ColourTools.isLight("ff00ff") should equal(false)
   }
 
   test("drops the leading # if present") {
-    colourCheck.isLight("#ffffff") should equal(true)
+    ColourTools.isLight("#ffffff") should equal(true)
   }
 }
