8ed42a0a443c93c73b3f9d4f3dbc65db9cffc7b3
Roberto Tyley <roberto.tyley@guardian.co.uk>
2014-03-28 16:54:37 +0000

    Fix resendEmailValidationEmail - calls API with POST method
    It would be nice to have the user-facing page be POST only too, but our
    returnUrl stuff doesn't support this at the moment.

--- identity/app/idapiclient/IdApi.scala
+++ identity/app/idapiclient/IdApi.scala
@@ -154,7 +154,7 @@ abstract class IdApi(val apiRootUrl: String, http: Http, jsonBodyParser: JsonBod
   def resendEmailValidationEmail(auth: Auth, trackingParameters: TrackingData): Future[Response[Unit]] = {
     val apiPath = urlJoin("user","send-validation-email")
     val params = buildParams(tracking = Some(trackingParameters), auth = Some(auth))
-    val response = http.GET(apiUrl(apiPath), params)
+    val response = http.POST(apiUrl(apiPath), None, params, buildHeaders())
     response map jsonBodyParser.extractUnit
   }
 }
