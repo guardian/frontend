460ffb8fd854ab5bd00a761d40f8ba013ed44e24
Grant Klopper <grant.klopper@gmail.com>
2014-03-28 12:29:55 +0000

    Added debug info to CAPI calls.
    We are currently getting spikes in traffic and it is not easy to figure out exactly where they came from.

--- common/app/contentapi/http.scala
+++ common/app/contentapi/http.scala
@@ -8,6 +8,8 @@ import java.util.concurrent.TimeoutException
 import play.api.libs.ws.WS
 import com.gu.management.{CountMetric, TimingMetric}
 import common.ContentApiMetrics.ContentApi404Metric
+import java.net.InetAddress
+import scala.util.Try
 
 class WsHttp(val httpTimingMetric: TimingMetric, val httpTimeoutMetric: SimpleCountMetric) extends Http[Future]
                                                                                               with ExecutionContexts {
@@ -16,11 +18,14 @@ class WsHttp(val httpTimingMetric: TimingMetric, val httpTimeoutMetric: SimpleCo
 
   override def GET(url: String, headers: Iterable[(String, String)]) = {
 
+    //append with a & as there are always params in there already
+    val urlWithDebugInfo = s"$url&${RequestDebugInfo.debugParams}"
+
     val contentApiTimeout = Configuration.contentApi.timeout
 
     val start = currentTimeMillis
 
-    val response = WS.url(url).withHeaders(headers.toSeq: _*).withRequestTimeout(contentApiTimeout).get()
+    val response = WS.url(urlWithDebugInfo).withHeaders(headers.toSeq: _*).withRequestTimeout(contentApiTimeout).get()
 
     // record metrics
     response.onSuccess {
@@ -58,3 +63,18 @@ trait DelegateHttp extends Http[Future] with ExecutionContexts {
   override def GET(url: String, headers: scala.Iterable[scala.Tuple2[String, String]]) = _http.GET(url, headers)
 }
 
+private object RequestDebugInfo {
+
+  import java.net.URLEncoder.encode
+
+  private lazy val host: String = Try(InetAddress.getLocalHost.getCanonicalHostName).getOrElse("unable-to-determine-host")
+  private lazy val stage: String = Configuration.environment.stage
+  private lazy val project: String = Configuration.environment.projectName
+
+  lazy val debugParams = Seq(
+    s"ngw-host=${encode(host, "UTF-8")}",
+    s"ngw-stage=${encode(stage, "UTF-8")}",
+    s"ngw-project=${encode(project, "UTF-8")}"
+  ).mkString("&")
+
+}
