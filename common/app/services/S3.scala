7f5bb4f26ff6f47b3f8c8ec49d4178d28d824320
Francis Carr <francis.carr@guardian.co.uk>
2014-04-03 16:33:00 +0100

    Add getPressedKeyForPath to S3FrontsApi

--- common/app/services/S3.scala
+++ common/app/services/S3.scala
@@ -86,6 +86,9 @@ object S3FrontsApi extends S3 {
   val namespace = "frontsapi"
   lazy val location = s"$stage/$namespace"
 
+  def getPressedKeyForPath(path: String): String =
+    s"$location/pressed/$path/pressed.json"
+
   def getSchema = get(s"$location/schema.json")
   def getConfig(id: String) = get(s"$location/config/$id/config.json")
   def getMasterConfig: Option[String] = get(s"$location/config/config.json")
@@ -122,11 +125,11 @@ object S3FrontsApi extends S3 {
   def getConfigIds(prefix: String): List[String] = getListing(prefix, "/config.json")
   def getCollectionIds(prefix: String): List[String] = getListing(prefix, "/collection.json")
 
-  def putPressedJson(id: String, json: String) =
-    putPrivate(s"$location/pressed/$id/pressed.json", json, "application/json")
+  def putPressedJson(path: String, json: String) =
+    putPrivate(getPressedKeyForPath(path), json, "application/json")
 
   def getPressedLastModified(path: String): Option[String] =
-    getLastModified(s"$location/pressed/$path/pressed.json").map(_.toString)
+    getLastModified(getPressedKeyForPath(path)).map(_.toString)
 }
 
 trait SecureS3Request extends implicits.Dates with Logging {
