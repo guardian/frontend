d742741eae27615107b31abb26b2ae25848bf0d5
Darren Hurley <darren.hurley@guardian.co.uk>
2014-04-03 17:15:11 +0100

    correct selector for commercial components

--- common/app/assets/javascripts/bootstraps/common.js
+++ common/app/assets/javascripts/bootstraps/common.js
@@ -464,7 +464,7 @@ define([
 
         loadCommercialComponent: function(config) {
             var commercialComponent = /^#commercial-component=(.*)$/.exec(window.location.hash),
-                slot = qwery('.ad-slot__commercial-component').shift();
+                slot = qwery('.ad-slot--commercial-component').shift();
             if (commercialComponent && slot) {
                 new CommercialLoader({ config: config })
                     .init(commercialComponent[1], slot);
