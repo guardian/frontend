b00179825f4d0cd2d5a77cc4a12da16d15eb924d
Grant Klopper <grant.klopper@gmail.com>
2014-04-04 12:49:19 +0100

    Switch to using 'official' AWS version of endpoints

--- common/app/services/AwsEndpoints.scala
+++ common/app/services/AwsEndpoints.scala
@@ -1,10 +1,16 @@
 package services
 
+import conf.Configuration.aws
+import com.amazonaws.regions.{Regions, Region}
+import com.amazonaws.regions.ServiceAbbreviations._
+import com.amazonaws.regions.ServiceAbbreviations.{S3 => S3Endpoint}
+
 object AwsEndpoints {
-  import conf.Configuration.aws
-  lazy val sns: String = s"sns.${aws.region}.amazonaws.com"
-  lazy val elb: String = s"elasticloadbalancing.${aws.region}.amazonaws.com"
-  lazy val monitoring: String = s"monitoring.${aws.region}.amazonaws.com"
-  lazy val dynamoDb: String = s"dynamodb.${aws.region}.amazonaws.com"
-  lazy val s3: String = s"s3-${aws.region}.amazonaws.com"
+  private lazy val region = Region.getRegion(Regions.fromName(aws.region))
+
+  lazy val sns: String = region.getServiceEndpoint(SNS)
+  lazy val elb: String = region.getServiceEndpoint(ElasticLoadbalancing)
+  lazy val monitoring: String = region.getServiceEndpoint(CloudWatch)
+  lazy val dynamoDb: String = region.getServiceEndpoint(Dynamodb)
+  lazy val s3: String = region.getServiceEndpoint(S3Endpoint)
 }
