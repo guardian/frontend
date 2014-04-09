11749135fdc856f0e135fce89290aebeb844a943
Chris Mulholland <chris.mulholland@gmail.com>
2014-04-09 11:03:38 +0100

    Update blended-containers-au.js
    Changing descriptions for test

--- common/app/assets/javascripts/modules/experiments/tests/blended-containers-au.js
+++ common/app/assets/javascripts/modules/experiments/tests/blended-containers-au.js
@@ -29,9 +29,14 @@ define([
     return function() {
         this.id = 'BlendedContainersAu';
         this.expiry = '2014-04-18';
+        this.author = 'Stephan';
+        this.description = 'Final step of testing new blended containers on the AU network front';
+        this.successMeasure = 'Ensuring CTR of Au network front is within 2% of control. Ensuring page views per visit are within 2% tolerance.';
+        this.idealOutcome = 'CTRs and PVPV are improved with blended version.';
+        this.audienceCriteria = 'All';
         this.audience = 1;
         this.audienceOffset = 0.0;
-        this.description = 'Testing new blended containers on the AU network front';
+
         this.canRun = function() {
             return ['/au', '/au-alpha'].indexOf(window.location.pathname) === 0;
         };
