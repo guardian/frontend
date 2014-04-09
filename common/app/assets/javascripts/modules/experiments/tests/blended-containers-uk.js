d310a0dfe7b20e89db4173441f764f642f58a3e0
Chris Mulholland <chris.mulholland@gmail.com>
2014-04-09 11:05:47 +0100

    Update blended-containers-uk.js
    Updating test descriptions

--- common/app/assets/javascripts/modules/experiments/tests/blended-containers-uk.js
+++ common/app/assets/javascripts/modules/experiments/tests/blended-containers-uk.js
@@ -29,9 +29,13 @@ define([
     return function() {
         this.id = 'BlendedContainersUk';
         this.expiry = '2014-04-18';
+        this.author = 'Stephan';
+        this.description = 'Final step of testing new blended containers on the UK network front';
+        this.successMeasure = 'Ensuring CTR of UK network front is within 2% of control. Ensuring page views per visit are within 2% tolerance.';
+        this.idealOutcome = 'CTRs and PVPV are improved with blended version.';
+        this.audienceCriteria = 'All';
         this.audience = 1;
         this.audienceOffset = 0.0;
-        this.description = 'Testing new blended containers on the UK network front';
         this.canRun = function() {
             return ['/uk', '/uk-alpha'].indexOf(window.location.pathname) === 0;
         };
