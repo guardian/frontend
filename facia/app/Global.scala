d4b08a3ab5090062357f5b9ad828f5d5f3f4fafd
Grant Klopper <grant.klopper@gmail.com>
2014-04-10 17:00:44 +0100

    And this explains the missing metrics

--- facia/app/Global.scala
+++ facia/app/Global.scala
@@ -9,7 +9,7 @@ object Global extends WithFilters(RequestMeasurementMetrics.asFilters: _*) with 
                                                         with DevParametersLifecycle with CloudWatchApplicationMetrics {
   override lazy val applicationName = Management.applicationName
 
-  override def applicationMetrics: Map[String, Double] = Map(
+  override def applicationMetrics: Map[String, Double] = super.applicationMetrics ++ Map(
     ("s3-authorization-error", FaciaMetrics.S3AuthorizationError.getAndReset.toDouble),
     ("json-parsing-error", FaciaMetrics.JsonParsingErrorCount.getAndReset.toDouble),
     ("elastic-content-api-calls", ContentApiMetrics.ElasticHttpTimingMetric.getAndReset.toDouble),
