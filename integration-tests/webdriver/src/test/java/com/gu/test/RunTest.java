ae6093f58ebfed9ec6cb7ff1ea38499054090852
Tori Johanne Lau <tori.lau@guardian.co.uk>
2014-04-10 16:59:51 +0100

    Checking that I can commit in InteliJ - removing an unused import and a commented out line.

--- integration-tests/webdriver/src/test/java/com/gu/test/RunTest.java
+++ integration-tests/webdriver/src/test/java/com/gu/test/RunTest.java
@@ -1,10 +1,9 @@
 package com.gu.test;
 
-import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(tags = "~@ignore", snippets = SnippetType.CAMELCASE)
+@Cucumber.Options(tags = "~@ignore")
 public class RunTest {
 }
