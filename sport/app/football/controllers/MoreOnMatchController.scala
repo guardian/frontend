928c2128cae3d94d34cabb5d5860995dd5ab1f71
James Gorrie <james.gorrie@guardian.co.uk>
2014-04-10 14:39:15 +0100

    Moved dropdown to common

--- sport/app/football/controllers/MoreOnMatchController.scala
+++ sport/app/football/controllers/MoreOnMatchController.scala
@@ -49,7 +49,7 @@ object MoreOnMatchController extends Controller with Football with Requests with
             "matchSummary" -> football.views.html.fragments.matchSummary(theMatch),
             "scoreSummary" -> football.views.html.fragments.scoreSummary(theMatch),
             "hasStarted" -> theMatch.hasStarted,
-            "dropdown" -> football.views.html.fragments.dropdown("")(Html(""))
+            "dropdown" -> views.html.fragments.dropdown("")(Html(""))
           )
         }
       }
