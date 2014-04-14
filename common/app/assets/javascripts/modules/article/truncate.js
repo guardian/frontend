ba8eaa2d77860cd2e9064d5da99d31d20825ede9
James Gorrie <james.gorrie@guardian.co.uk>
2014-04-14 16:20:03 +0100

    Adding truncation to football liveblogs

---
+++ common/app/assets/javascripts/modules/article/truncate.js
@@ -0,0 +1,49 @@
+define([
+    'common/$',
+    'bean',
+    'bonzo',
+    'common/utils/config',
+    'common/utils/context',
+    'common/utils/detect'
+], function(
+    $,
+    bean,
+    bonzo,
+    config,
+    context,
+    detect
+) {
+
+context = context();
+function shouldTruncate() {
+    return config.page.isLiveBlog &&
+        detect.getBreakpoint() === 'mobile' &&
+        config.page.section === 'football' &&
+        $('.live-blog__blocks').length > 1;
+}
+
+function truncate() {
+    var articleElongateClass = 'article-elongator',
+        truncatedClass = 'article--truncated',
+        $article = $('#article', context).addClass(truncatedClass);
+
+    if (shouldTruncate()) {
+        $.create(
+            '<button class="u-fauxlink u-button-reset '+ articleElongateClass +'">'+
+                'View all updates'+
+            '</button>'
+        ).each(function(el) {
+            $('.article-body', context).append(el);
+        });
+
+        bean.on(context, 'click', '.'+articleElongateClass, function(e) {
+            e.preventDefault();
+            $article.removeClass(truncatedClass);
+            bonzo(e.currentTarget).addClass('u-h');
+        });
+    }
+}
+
+return truncate;
+
+}); // define
