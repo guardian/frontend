51f68abbfc09aa6bd92941608350747c47ce59ab
James Gorrie <james.gorrie@guardian.co.uk>
2014-04-15 08:54:32 +0100

    Abstraction of show more button

--- common/app/assets/javascripts/modules/article/truncate.js
+++ common/app/assets/javascripts/modules/article/truncate.js
@@ -14,36 +14,37 @@ define([
     detect
 ) {
 
-context = context();
-function shouldTruncate() {
-    return config.page.isLiveBlog &&
-        detect.getBreakpoint() === 'mobile' &&
-        config.page.section === 'football' &&
-        $('.live-blog__blocks').length > 1;
-}
-
-function truncate() {
-    var articleElongateClass = 'article-elongator',
-        truncatedClass = 'article--truncated',
-        $article = $('#article', context).addClass(truncatedClass);
-
-    if (shouldTruncate()) {
-        $.create(
-            '<button class="u-fauxlink u-button-reset '+ articleElongateClass +'">'+
-                'View all updates'+
-            '</button>'
-        ).each(function(el) {
-            $('.article-body', context).append(el);
-        });
-
-        bean.on(context, 'click', '.'+articleElongateClass, function(e) {
-            e.preventDefault();
-            $article.removeClass(truncatedClass);
-            bonzo(e.currentTarget).addClass('u-h');
-        });
+    context = context();
+    function shouldTruncate() {
+        return config.page.isLiveBlog &&
+            detect.getBreakpoint() === 'mobile' &&
+            config.page.section === 'football' &&
+            $('.live-blog__blocks').length > 1;
     }
-}
 
-return truncate;
+    function truncate() {
+        var articleElongateClass = 'article-elongator',
+            truncatedClass = 'article--truncated',
+            $article = $('#article', context).addClass(truncatedClass);
+
+        if (shouldTruncate()) {
+            $.create(
+                '<button class="u-fauxlink u-button-reset button--show-more '+ articleElongateClass +'">'+
+                    '<i class="i i-plus-white-med tone-background"></i>'+
+                    'View all updates'+
+                '</button>'
+            ).each(function(el) {
+                $('.article-body', context).append(el);
+            });
+
+            bean.on(context, 'click', '.'+articleElongateClass, function(e) {
+                e.preventDefault();
+                $article.removeClass(truncatedClass);
+                bonzo(e.currentTarget).addClass('u-h');
+            });
+        }
+    }
+
+    return truncate;
 
 }); // define
