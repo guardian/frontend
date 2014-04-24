fb65a141dc5a96314723a987087e52b8990695c3
Francis Carr <francis.carr@guardian.co.uk>
2014-04-24 11:46:02 +0100

    Start using the meta data whitelist when creating a trail and a collection

--- facia-tool/app/model/frontsapi.scala
+++ facia-tool/app/model/frontsapi.scala
@@ -174,9 +174,9 @@ trait UpdateActions extends Logging {
 
   def createBlock(id: String, identity: Identity, update: UpdateList): Option[Block] = {
     if (update.live)
-      Option(FaciaApi.putBlock(id, Block(None, List(Trail(update.item, DateTime.now, update.itemMeta)), None, DateTime.now.toString, identity.fullName, identity.email, None, None, None), identity))
+      Option(FaciaApi.putBlock(id, Block(None, List(Trail(update.item, DateTime.now, update.itemMeta.map(itemMetaWhiteList))), None, DateTime.now.toString, identity.fullName, identity.email, None, None, None), identity))
     else
-      Option(FaciaApi.putBlock(id, Block(None, Nil, Some(List(Trail(update.item, DateTime.now, update.itemMeta))), DateTime.now.toString, identity.fullName, identity.email, None, None, None), identity))
+      Option(FaciaApi.putBlock(id, Block(None, Nil, Some(List(Trail(update.item, DateTime.now, update.itemMeta.map(itemMetaWhiteList)))), DateTime.now.toString, identity.fullName, identity.email, None, None, None), identity))
   }
 
   def capCollection(block: Block): Block =
