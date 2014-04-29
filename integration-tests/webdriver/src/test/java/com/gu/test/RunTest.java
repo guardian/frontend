363e3a5957f8acf8aed8be3e0e29263cd941c2ba
Tori Johanne Lau <tori.lau@guardian.co.uk>
2014-04-29 12:50:00 +0100

    Simplifying runner of tests

--- integration-tests/webdriver/src/test/java/com/gu/test/RunTest.java
+++ integration-tests/webdriver/src/test/java/com/gu/test/RunTest.java
@@ -5,7 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = {"src/test/resources/com/gu/test/"}
-)public class RunTest {
+
+public class RunTest {
 }
