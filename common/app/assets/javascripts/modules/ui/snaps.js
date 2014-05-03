74167062450820b634e7df1a8b11da3d9bcbe5ad
Stephan Fowler <stephanfowler@gmail.com>
2014-05-04 00:52:42 +0100

    Do fewer DOM queries/mods

--- common/app/assets/javascripts/modules/ui/snaps.js
+++ common/app/assets/javascripts/modules/ui/snaps.js
@@ -3,76 +3,79 @@ define([
     'bonzo',
     'common/utils/ajax',
     'common/utils/mediator',
-    'common/utils/to-array',
-    'lodash/collections/find'
+    'common/utils/to-array'
 ], function(
     $,
     bonzo,
     ajax,
     mediator,
-    toArray,
-    find
+    toArray
 ) {
 
-    function snaps(selector) {
-        fetchSnaps(selector);
+    function init(selector) {
+        var snaps = toArray($(selector));
+
+        if (!snaps.length) { return; }
+
+        fetchSnaps(snaps);
+
         mediator.on('window:resize', function() {
-            resizeSnaps(selector);
+            snaps.forEach(function(el) {
+                setSnapPoint(el, true);
+            });
         });
     }
 
-    function setSnapPoint(el) {
-        var $el = bonzo(el),
+    function setSnapPoint(el, isResize) {
+        var width = el.offsetWidth,
+            $el = bonzo(el),
             prefix = 'facia-snap-point--';
 
-        var breakpoints = [
-            { width: 0, name: 'tiny' },
+        [
+            { width: 0,   name: 'tiny' },
             { width: 180, name: 'mini' },
             { width: 220, name: 'small' },
             { width: 300, name: 'medium' },
             { width: 700, name: 'large' }
-        ];
+        ]
+        .map(function(breakpoint, i, arr) {
+            var isAdd = !arr[i+1] || (width >= breakpoint.width && width < arr[i+1].width);
 
-        breakpoints.forEach(function(breakpoint) {
-            $el.removeClass(prefix + breakpoint.name);
+            breakpoint.action = isAdd ? 'addClass' : isResize ? 'removeClass' : false;
+            return breakpoint;
+        })
+        .filter(function(breakpoint) {return breakpoint.action; })
+        .forEach(function(breakpoint) {
+            $el[breakpoint.action](prefix + breakpoint.name);
         });
+    }
 
-        $el.addClass(prefix + find(breakpoints, function(breakpoint, i, arr) {
-            return !arr[i+1] || (el.offsetWidth >= breakpoint.width && el.offsetWidth < arr[i+1].width);
-        }).name);
+    function setSnapHtml(el, html) {
+        bonzo(el).html(html);
     }
 
     function setSnapEmbedded(el) {
         bonzo(el).addClass('facia-snap-embed');
     }
 
-    function resizeSnaps(selector) {
-        $(selector).each(function(el) {
-            setSnapPoint(el);
+    function fetchSnaps(snaps) {
+        snaps
+        .filter(function(el) { return el.getAttribute('data-snap-uri'); })
+        .forEach(function(el) {
+            ajax({
+                url: el.getAttribute('data-snap-uri'),
+                crossOrigin: true
+            }).then(function(resp) {
+                $.create(resp.html).each(function(html) {
+                    setSnapEmbedded(el);
+                    setSnapPoint(el);
+                    setSnapHtml(el, html);
+                });
+            });
         });
     }
 
-    function fetchSnaps(selector) {
-        toArray($(selector))
-            .filter(function(el) { return el.getAttribute('data-snap-uri'); })
-            .forEach(function(el) {
-                ajax({
-                    url: el.getAttribute('data-snap-uri'),
-                    crossOrigin: true
-                }).then(function(resp) {
-                    $.create(resp.html).each(function(html) {
-                        bonzo(el)
-                            .empty()
-                            .append(html);
-
-                        setSnapEmbedded(el);
-                        setSnapPoint(el);
-                    });
-                });
-            });
-    }
-
     return {
-        init: snaps
+        init: init
     };
 });
