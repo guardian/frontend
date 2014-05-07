5be71845b441d61368b97f77dd9eef0b252536d1
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-05-07 13:44:38 +0100

    Ad a css param, repurpose the type param

--- admin/app/football/model/SnapFields.scala
+++ admin/app/football/model/SnapFields.scala
@@ -3,10 +3,10 @@ package football.model
 import java.net.URLEncoder
 
 
-case class SnapFields(`type`: String, uri: String, fallbackUrl: String, fallbackHeadline: String, fallbackTrailtext: String) {
+case class SnapFields(`type`: String, `css`: String, uri: String, fallbackUrl: String, fallbackHeadline: String, fallbackTrailtext: String) {
   private implicit class RichString(string: String) {
     def encode = URLEncoder.encode(string, "UTF-8")
   }
 
-  val snapUrl = s"$fallbackUrl?gu-snapType=${`type`.encode}&gu-snapUri=${uri.encode}&gu-headline=${fallbackHeadline.encode}&gu-trailText=${fallbackTrailtext.encode}"
+  val snapUrl = s"$fallbackUrl?snap-type=${`type`.encode}&snap-css=${`css`.encode}&snap-uri=${uri.encode}&snap-headline=${fallbackHeadline.encode}&snap-trailText=${fallbackTrailtext.encode}"
 }
