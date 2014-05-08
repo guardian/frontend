acbddaf2f3189fda320a17a86547b9b948ecfff5
Matt Osborn <matthew.osborn@theguardian.com>
2014-05-08 14:32:36 +0100

    various tonal template stuff

--- common/app/model/content.scala
+++ common/app/model/content.scala
@@ -331,6 +331,7 @@ class Article(content: ApiContentWithMeta) extends Content(content) {
   lazy val contentType = "Article"
   lazy val isReview = tones.exists(_.id == "tone/reviews")
   lazy val isFeature = tones.exists(_.id == "tone/features")
+  lazy val isComment = tones.exists(_.id == "tone/comment")
 
   lazy val hasVideoAtTop: Boolean = Jsoup.parseBodyFragment(body).body().children().headOption
     .exists(e => e.hasClass("gu-video") && e.tagName() == "video")
