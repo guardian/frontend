bfe47f3f5d2b4c404b95c2959e07b3af52b3688f
Ken Lim <kennethlimfc@gmail.com>
2014-05-08 11:42:55 +0100

    Adding Criteo loader to the site

--- common/app/assets/javascripts/modules/analytics/commercial/tags/common/criteo.js
+++ common/app/assets/javascripts/modules/analytics/commercial/tags/common/criteo.js
@@ -24,4 +24,35 @@ define([
         getSegments: getSegments
     };
 
+    function load() {
+        if (config.switches.criteo) {
+            function crtg_getCookie(a) {
+                var b, c, d, e = document.cookie.split(";");
+                for (b = 0; b < e.length; b++) {
+                    c = e[b].substr(0, e[b].indexOf("="));
+                    d = e[b].substr(e[b].indexOf("=") + 1);
+                    c = c.replace(/^\s+|\s+$/g, "");
+                    if (c == a) {
+                        return unescape(d)
+                    }
+                }
+                return""
+            }
+
+            var crtg_nid = "1476";
+            var crtg_cookiename = "cto2_guardian";
+            var crtg_content = crtg_getCookie(crtg_cookiename);
+            var crtg_rnd = Math.floor(Math.random() * 99999999999);
+            var crtg_url = "http://rtax.criteo.com/delivery/rta/rta.js?netid=" + escape(crtg_nid);
+            crtg_url += "&cookieName=" + escape(crtg_cookiename);
+            crtg_url += "&rnd=" + crtg_rnd;
+            crtg_url += "&varName=crtg_content";
+            var crtg_script = document.createElement("script");
+            crtg_script.type = "text/javascript";
+            crtg_script.src = crtg_url;
+            crtg_script.async = true;
+            if (document.getElementsByTagName("head").length > 0)document.getElementsByTagName("head")[0].appendChild(crtg_script); else if (document.getElementsByTagName("body").length > 0)document.getElementsByTagName("body")[0].appendChild(crtg_script)
+        }
+    }
+
 });
