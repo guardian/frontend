9ca5338dcc5b06497028d245e354c74febe09a88
Tori Johanne Lau <tori.lau@guardian.co.uk>
2014-05-09 16:33:34 +0100

    New, simpler automation. Basic selenium and Java. No more actions/actors/asserts in their own classes.

---
+++ integration-tests/webdriver/README.md
@@ -0,0 +1,9 @@
+Automation
+===================
+
+
+-To run these tests on code:
+ -Go to the Webdriver directory and run ``mvn test``
+ -
+ -
+ -To run on your local machine, edit baseUrl in the TestRunner class to point to your localhost.
---
+++ integration-tests/webdriver/src/main/java/com.gu.test/pages/FrontPage.java
@@ -0,0 +1,54 @@
+package com.gu.test.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Random;
+
+public class FrontPage {
+    private WebDriver driver;
+    private String frontPageURL;
+    private String dataComponent;
+
+    public FrontPage(WebDriver driver, String frontPageURL) {
+        this.driver = driver;
+        this.frontPageURL = frontPageURL;
+        driver.get(frontPageURL);
+    }
+
+
+    public FrontPage goToEdition(String edition) {
+        driver.findElement(By.cssSelector("[data-link-name=\"switch to " + edition + " edition\"]")).click();
+        return this;
+    }
+
+
+    public WebElement expandSection() {
+        WebElement section = driver.findElement(By.cssSelector("button.collection__show-more"));
+        WebElement parent = section.findElement(By.xpath(".."));
+        section.click();
+        return parent;
+    }
+
+
+    public Article goToFirstArticle(WebDriver driver) {
+        driver.findElement(By.xpath("/descendant::a[@data-link-name=\"article\"][1]")).click();
+        return new Article(driver);
+
+    }
+
+
+    public Article goToArticleInTopStories(WebDriver driver) {
+        driver.findElement(By.xpath("/descendant::section[@data-component=\"top-stories\"]//a[@data-link-name=\"article\"][1]")).click();
+        return new Article(driver);
+    }
+
+    public Article goToArticleInSportContainer(WebDriver driver) {
+        driver.findElement(By.xpath("/descendant::section[@data-component=\"sport\"]//a[@data-link-name=\"article\"][1]")).click();
+        return new Article(driver);
+    }
+
+
+}
