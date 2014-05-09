b042a2c14ad4a8cbc0897d5a55d69d3d7ab9518d
Francis Carr <francis.carr@guardian.co.uk>
2014-05-09 15:15:37 +0100

    Add FaciaPage to facia project with switching seo functionality

---
+++ facia/app/model/FaciaPage.scala
@@ -0,0 +1,29 @@
+package model
+
+import common.Edition
+import controllers.FrontPage
+import conf.Switches
+
+case class FaciaPage(
+                      id: String,
+                      seoData: SeoData,
+                      collections: List[(Config, Collection)]) extends MetaData {
+
+  lazy val frontPage: FrontPage = FrontPage.getFrontPageFromFaciaPage(this)
+
+  private val isOldSeoOn: Boolean = Switches.NewSeoSwitch.isSwitchedOn
+  def oldOrNewSeo[T](oldValue: T, newValue: T): T = if (isOldSeoOn) oldValue else newValue
+
+  override lazy val description: Option[String] = oldOrNewSeo(frontPage.description, seoData.description)
+  override lazy val section: String = oldOrNewSeo(frontPage.section, seoData.section.get)
+  override lazy val analyticsName: String = oldOrNewSeo(frontPage.analyticsName, s"GFE:$getContentType")
+  override lazy val webTitle: String = oldOrNewSeo(frontPage.webTitle, seoData.webTitle.get)
+
+  override lazy val metaData: Map[String, Any] = super.metaData + ("newSeo" -> (!isOldSeoOn).toString)
+
+  lazy val getContentType: String =
+    Edition.all.find(edition => id.endsWith(edition.id)) match {
+      case Some(_) => "Network Front"
+      case None    => "Section"
+    }
+}
