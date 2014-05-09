5043aefada00c6ddecc5d3f64c8a5066d4854c27
Francis Carr <francis.carr@guardian.co.uk>
2014-05-09 17:11:26 +0100

    Change isOldSeoOn to isNewSeoOn

--- facia/app/model/FaciaPage.scala
+++ facia/app/model/FaciaPage.scala
@@ -11,15 +11,15 @@ case class FaciaPage(
 
   lazy val frontPage: FrontPage = FrontPage(this)
 
-  private val isOldSeoOn: Boolean = Switches.NewSeoSwitch.isSwitchedOn
-  def oldOrNewSeo[T](oldValue: T, newValue: T): T = if (isOldSeoOn) oldValue else newValue
+  private val isNewSeoOn: Boolean = Switches.NewSeoSwitch.isSwitchedOn
+  def oldOrNewSeo[T](oldValue: T, newValue: T): T = if (isNewSeoOn) newValue else oldValue
 
   override lazy val description: Option[String] = oldOrNewSeo(frontPage.description, seoData.description)
   override lazy val section: String = oldOrNewSeo(frontPage.section, seoData.section.get)
   override lazy val analyticsName: String = oldOrNewSeo(frontPage.analyticsName, s"GFE:$getContentType")
   override lazy val webTitle: String = oldOrNewSeo(frontPage.webTitle, seoData.webTitle.get)
 
-  override lazy val metaData: Map[String, Any] = super.metaData + ("newSeo" -> (!isOldSeoOn).toString)
+  override lazy val metaData: Map[String, Any] = super.metaData + ("newSeo" -> isNewSeoOn.toString)
 
   lazy val getContentType: String =
     Edition.all.find(edition => id.endsWith(edition.id)) match {
