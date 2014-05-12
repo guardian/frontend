dc13ce2571728385107de403699d430e48119b9a
tudorraul <tudorraul@gmail.com>
2014-05-12 17:13:26 +0100

    Search box label - add support for A/B test

---
+++ common/app/assets/javascripts/modules/experiments/searchText.js
@@ -0,0 +1,38 @@
+define(['bonzo'], function (bonzo) {
+
+    var HeaderSearchText = function () {
+
+        this.id = 'HeaderSearchText';
+        this.start = '2014-05-12';
+        this.expiry = '2014-06-9';
+        this.author = 'Raul Tudor';
+        this.description = 'The header search box will display a label for tablet and desktop.';
+        this.audience = 0.1;
+        this.audienceOffset = 0.4;
+        this.successMeasure = 'Clicks/taps on the search box.';
+        this.audienceCriteria = 'Users who are not on mobile.';
+        this.dataLinkNames = 'header search box label';
+        this.idealOutcome = 'Increased number of clicks/taps on the search box.';
+
+        this.canRun = function(config) {
+            detect.getBreakpoint() !== 'mobile';
+        };
+
+        this.variants = [
+            {
+                id: 'control',
+                test: function (context, config) {
+                }
+            },
+            {
+                id: 'hide',
+                test: function (context, config) {
+                    bonzo(context.querySelector('.control--search .control__info')).hide();
+                }
+            }
+        ];
+    };
+
+    return GeoMostPopular;
+
+});
