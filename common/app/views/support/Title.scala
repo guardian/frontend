1b90bc98f60eb855c32dcc373226ed5a6671f608
Francis Carr <francis.carr@guardian.co.uk>
2014-05-15 11:04:20 +0100

    Fix title so that the first and middle parts are not the same

--- common/app/views/support/Title.scala
+++ common/app/views/support/Title.scala
@@ -10,12 +10,15 @@ object Title {
   def apply(page: MetaData)(implicit request: RequestHeader): Html = Html{
     val section = Navigation.topLevelItem(Edition(request).navigation, page).map(_.name.title)
     val title = page match {
-      case c: Content => s"${c.webTitle}${pagination(page)} | ${section.getOrElse(c.sectionName).capitalize}"
-      case t: Tag     => s"${t.webTitle}${pagination(page)}${section.map(s => s" | ${s.capitalize}").getOrElse("")}"
-      case _          => s"${page.title}${pagination(page)}${section.map(s => s" | ${s.capitalize}").getOrElse("")}"
+      case c: Content => s"${c.webTitle}${pagination(page)}${getSectionConsideringWebtitle(c.webTitle, section.orElse(Option(c.sectionName)))}"
+      case t: Tag     => s"${t.webTitle}${pagination(page)}${getSectionConsideringWebtitle(t.webTitle, section)}"
+      case _          => s"${page.title}${pagination(page)}${getSectionConsideringWebtitle(page.title, section)}"
     }
     s"${title.trim} | The Guardian"
   }
+
+  private def getSectionConsideringWebtitle(webTitle: String, section: Option[String]): String =
+    section.filterNot(_.toLowerCase == webTitle.toLowerCase).map{ s => s" | ${s.capitalize}"}.getOrElse("")
 
   private def pagination(page: MetaData) = page.pagination.filterNot(_.isFirstPage).map{ pagination =>
     s" | Page ${pagination.currentPage} of ${pagination.lastPage}"
