a53b29eafaddc86f89d6fd5706f8479f5c3a10d5
Francis Carr <francis.carr@guardian.co.uk>
2014-05-15 17:05:57 +0100

    Change generation of Title and use the MetaData.section instead of Navigation section

--- common/app/views/support/Title.scala
+++ common/app/views/support/Title.scala
@@ -8,11 +8,10 @@ import play.api.mvc.RequestHeader
 object Title {
 
   def apply(page: MetaData)(implicit request: RequestHeader): Html = Html{
-    val section = Navigation.topLevelItem(Edition(request).navigation, page).map(_.name.title)
     val title = page match {
-      case c: Content => s"${c.webTitle}${pagination(page)}${getSectionConsideringWebtitle(c.webTitle, section.orElse(Option(c.sectionName)))}"
-      case t: Tag     => s"${t.webTitle}${pagination(page)}${getSectionConsideringWebtitle(t.webTitle, section)}"
-      case _          => s"${page.title}${pagination(page)}${getSectionConsideringWebtitle(page.title, section)}"
+      case c: Content => s"${c.webTitle}${pagination(page)}${getSectionConsideringWebtitle(c.webTitle, Option(page.section).orElse(Option(c.sectionName)))}"
+      case t: Tag     => s"${t.webTitle}${pagination(page)}${getSectionConsideringWebtitle(t.webTitle, Option(page.section))}"
+      case _          => s"${page.title}${pagination(page)}${getSectionConsideringWebtitle(page.title, Option(page.section))}"
     }
     s"${title.trim} | The Guardian"
   }
