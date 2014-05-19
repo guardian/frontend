30137e4599820ae89353c73b18b075f6d42e8f4e
Francis Carr <francis.carr@guardian.co.uk>
2014-05-19 11:59:17 +0100

    Fix the pipes in Title

--- common/app/views/support/Title.scala
+++ common/app/views/support/Title.scala
@@ -10,15 +10,15 @@ object Title {
   def apply(page: MetaData)(implicit request: RequestHeader): Html = Html{
     val section: String = Navigation.topLevelItem(Edition(request).navigation, page).map(_.name.title).getOrElse(page.section)
     val title = page match {
-      case c: Content => s"${c.webTitle}${pagination(page)}${getSectionConsideringWebtitle(c.webTitle, Option(section))}"
-      case t: Tag     => s"${t.webTitle}${pagination(page)}${getSectionConsideringWebtitle(t.webTitle, Option(section))}"
-      case _          => page.title.getOrElse(s"${pagination(page)}${getSectionConsideringWebtitle(page.webTitle, Option(section))}")
+      case c: Content => s"${c.webTitle}${pagination(page)} | ${getSectionConsideringWebtitle(c.webTitle, Option(page.section))}"
+      case t: Tag     => s"${t.webTitle}${pagination(page)} | ${getSectionConsideringWebtitle(t.webTitle, Option(page.section))}"
+      case _          => page.title.filter(_.nonEmpty).getOrElse(s"${page.webTitle} | ${pagination(page)}${getSectionConsideringWebtitle(page.webTitle, Option(page.section))}")
     }
     s"${title.trim} | The Guardian"
   }
 
   private def getSectionConsideringWebtitle(webTitle: String, section: Option[String]): String =
-    section.filter(_.nonEmpty).filterNot(_.toLowerCase == webTitle.toLowerCase).fold(""){ s => s" | ${s.capitalize}"}
+    section.filter(_.nonEmpty).filterNot(_.toLowerCase == webTitle.toLowerCase).fold(webTitle){ s => s"${s.capitalize}"}
 
   private def pagination(page: MetaData) = page.pagination.filterNot(_.isFirstPage).map{ pagination =>
     s" | Page ${pagination.currentPage} of ${pagination.lastPage}"
