da55646d90beabfe9b7a16dcea5430cd88cfa1e3
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-05-19 10:47:04 +0100

    Simplify Badge Slot Test

--- article/test/ArticleFeatureTest.scala
+++ article/test/ArticleFeatureTest.scala
@@ -544,20 +544,6 @@ class ArticleFeatureTest extends FeatureSpec with GivenWhenThen with Matchers {
       }
     }
 
-    // There are no sponsored articles at the moment
-    ignore("Signify to the user an article is sponsored") {
-      // scenario("Signify to the user an article is sponsored"){
-      Given("I visit a sponsored article entitled 'Feeling hungry? Try the fine flavours of floral gastronomy'")
-      StandardAdvertsSwitch.switchOn()
-      HtmlUnit("/lifeandstyle/live-better") { browser =>
-        import browser._
-        Then("I should see a message")
-        val adSlot = $(".ad-slot--paid-for-badge")
-        adSlot.getAttribute("data-name") should be ("spbadge")
-        adSlot.findFirst(".ad-slot__container").getId should be ("dfp-ad--spbadge")
-      }
-    }
-
     scenario("Health check"){
       HtmlUnit("/world/2013/sep/15/obama-rouhani-united-nations-meeting") { browser =>
         Await.result(WS.url("http://localhost:9000/_cdn_healthcheck").get(), 10.seconds).status should be (503)
--- common/app/dfp/DfpAgent.scala
+++ common/app/dfp/DfpAgent.scala
@@ -20,7 +20,7 @@ object DfpAgent extends ExecutionContexts with Logging {
 
   private def dfpData: Option[DfpData] = {
     if (Play.isTest) {
-      Some(DfpData(Seq(LineItem(0, Nil))))
+      Some(defaultDfpData)
     } else {
       dfpDataAgent get()
     }
@@ -49,21 +49,15 @@ object DfpAgent extends ExecutionContexts with Logging {
 
   def refresh() {
 
-    implicit val targetReads: Reads[Target] = (
-      (JsPath \ "name").read[String] and
-        (JsPath \ "op").read[String] and
-        (JsPath \ "values").read[Seq[String]]
-      )(Target.apply _)
-
-    implicit val targetSetReads: Reads[TargetSet] = (
+    implicit val targetReads: Reads[Target] = ((JsPath \ "name").read[String] and
       (JsPath \ "op").read[String] and
-        (JsPath \ "targets").read[Seq[Target]]
-      )(TargetSet.apply _)
+      (JsPath \ "values").read[Seq[String]])(Target.apply _)
 
-    implicit val lineItemReads: Reads[LineItem] = (
-      (JsPath \ "id").read[Long] and
-        (JsPath \ "targetSets").read[Seq[TargetSet]]
-      )(LineItem.apply _)
+    implicit val targetSetReads: Reads[TargetSet] = ((JsPath \ "op").read[String] and
+      (JsPath \ "targets").read[Seq[Target]])(TargetSet.apply _)
+
+    implicit val lineItemReads: Reads[LineItem] = ((JsPath \ "id").read[Long] and
+      (JsPath \ "targetSets").read[Seq[TargetSet]])(LineItem.apply _)
 
     // See http://stackoverflow.com/questions/18625185/parsing-a-list-of-models-in-play-2-1-x
     implicit val reader = new Reads[DfpData] {
@@ -76,10 +70,8 @@ object DfpAgent extends ExecutionContexts with Logging {
 
       def logDataChanges(freshData: Option[DfpData]) {
         def minus(optData: Option[DfpData], optSubData: Option[DfpData]): Seq[LineItem] = {
-          for {
-            data <- optData
-            subData <- optSubData
-          } yield
+          for {data <- optData
+               subData <- optSubData} yield
             data.lineItems filterNot (subData.lineItems contains _)
         }.getOrElse(Nil)
         val removedLineItems = minus(dfpData, freshData)
@@ -107,6 +99,10 @@ object DfpAgent extends ExecutionContexts with Logging {
   def stop() {
     dfpDataAgent close()
   }
+
+  private val defaultDfpData = DfpData(Seq(
+    LineItem(0, Seq(TargetSet("AND", Seq(Target("slot","IS",Seq("spbadge")),Target("k","IS",Seq("media"))))))
+  ))
 }
 
 
