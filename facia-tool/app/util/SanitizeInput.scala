c3752171f43ecf8bf3c526cc38980c1c0a1cebbb
Francis Carr <francis.carr@guardian.co.uk>
2014-05-20 17:39:23 +0100

    Renaming

---
+++ facia-tool/app/util/SanitizeInput.scala
@@ -0,0 +1,24 @@
+package util
+
+import frontsapi.model.{Front, Config}
+import scala.util.matching.Regex
+
+
+object SanitizeInput {
+
+  val sanitizeRegex: Regex = "(<.*?>|<[^>]*$)".r
+
+  def fromString(s: String) = sanitizeRegex.replaceAllIn(s, "")
+
+  def fromConfigSeo(config: Config): Config = {
+    config.copy(fronts = config.fronts.mapValues(sanitizeSeoInputFromFront))
+  }
+
+  private def sanitizeSeoInputFromFront(front: Front): Front =
+    front.copy(
+      title = front.title.map(fromString),
+      webTitle = front.webTitle.map(fromString),
+      section = front.section.map(fromString),
+      description = front.description.map(fromString)
+    )
+}
--- facia-tool/test/util/SanitizeInputTest.scala
+++ facia-tool/test/util/SanitizeInputTest.scala
@@ -3,7 +3,7 @@ package util
 import frontsapi.model.{Front, Config}
 import org.scalatest.{FlatSpec, Matchers}
 
-class StripTagsTest extends FlatSpec with Matchers {
+class SanitizeInputTest extends FlatSpec with Matchers {
 
   private def createConfigWithFront(
     section: Option[String] = None,
@@ -14,33 +14,33 @@ class StripTagsTest extends FlatSpec with Matchers {
 
   "StripTags" should "strip tag from title" in {
     val config = createConfigWithFront(title = Option("<strip><me>now"))
-    StripTags.stripTagsFromConfigSeo(config).fronts.head._2.title.get should be ("now")
+    SanitizeInput.fromConfigSeo(config).fronts.head._2.title.get should be ("now")
   }
 
   it should "strip tag from webTitle" in {
     val config = createConfigWithFront(webTitle = Option("a<strip>b</me>c"))
-    StripTags.stripTagsFromConfigSeo(config).fronts.head._2.webTitle.get should be ("abc")
+    SanitizeInput.fromConfigSeo(config).fronts.head._2.webTitle.get should be ("abc")
   }
 
   it should "strip tag from description" in {
     val config = createConfigWithFront(description = Option("<strip><me>"))
-    StripTags.stripTagsFromConfigSeo(config).fronts.head._2.description.get should be ("")
+    SanitizeInput.fromConfigSeo(config).fronts.head._2.description.get should be ("")
   }
 
   it should "strip tag from section" in {
     val config = createConfigWithFront(section = Option("<strip>hello<me>"))
-    StripTags.stripTagsFromConfigSeo(config).fronts.head._2.section.get should be ("hello")
+    SanitizeInput.fromConfigSeo(config).fronts.head._2.section.get should be ("hello")
   }
 
   it should "strip empty brackets regex" in {
-    StripTags.stripTags("a<>b") should be ("ab")
+    SanitizeInput.fromString("a<>b") should be ("ab")
   }
 
   it should "strip tag with attributes" in {
-    StripTags.stripTags("""<strip href="stuff.jpg">hello<me>""") should be ("hello")
+    SanitizeInput.fromString("""<strip href="stuff.jpg">hello<me>""") should be ("hello")
   }
 
   it should "strip unclosed tag" in {
-    StripTags.stripTags("a<script") should be ("a")
+    SanitizeInput.fromString("a<script") should be ("a")
   }
 }
