6bb326f779997694ecaeb7e60ea1f4fec6d25345
James Gorrie <james.gorrie@guardian.co.uk>
2014-05-20 16:00:06 +0100

    Update profile activity test to search for class name

--- discussion/test/controllers/ProfileActivityControllerTest.scala
+++ discussion/test/controllers/ProfileActivityControllerTest.scala
@@ -9,17 +9,16 @@ import test.TestRequest
 
 class ProfileActivityControllerTest extends FlatSpec with Matchers {
 
-  val callbackName = "foo"
   val userId = "10000001"
 
-  "CommenterActivity" should "return JSONP when callback is supplied" in Fake {
+  "CommenterActivity" should "return profile discussions component" in Fake {
     val action = DiscussionApp.profileDiscussions(userId)
-    val fakeRequest = FakeRequest(GET, "/discussion/p/37v3a.json?callback=" + callbackName).withHeaders("host" -> "localhost:9000")
+    val fakeRequest = FakeRequest(GET, "/discussion/profile/"+ userId +"/discussions.json").withHeaders("host" -> "localhost:9000")
     val result = action(fakeRequest)
 
     status(result) should be(200)
-    contentType(result).get should be("application/javascript")
-    contentAsString(result).substring(0, 10) should startWith(callbackName + "({") // the callback
+    contentType(result).get should be("application/json")
+    contentAsString(result) should include("class=\\\"profile-discussions\\\"")
   }
 
 }
