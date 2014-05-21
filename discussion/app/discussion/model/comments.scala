7cc1e420f5270b5e7e6b3e8dfbe52b6539a6df74
James Gorrie <james.gorrie@guardian.co.uk>
2014-05-21 08:49:31 +0100

    Revert "Revert "Merge pull request #4328 from guardian/discussion-activity""
    This reverts commit 7d0c2157afdaec1c351150a8383ae4c0d7b057ea.

---
+++ discussion/app/discussion/model/comments.scala
@@ -0,0 +1,99 @@
+package discussion.model
+
+import play.api.libs.json.{JsObject, JsArray, JsValue}
+
+trait Comments {
+  val comments: Seq[Comment]
+  val pagination: Pagination
+  val switches: Seq[Switch]
+}
+
+case class DiscussionComments(
+  discussion: Discussion,
+  comments: Seq[Comment],
+  pagination: Pagination,
+  commentCount: Int,
+  topLevelCommentCount: Int,
+  commenterCount: Int,
+  switches: Seq[Switch]
+) extends Comments
+
+object DiscussionComments {
+
+  def apply(json: JsValue): DiscussionComments = {
+    val discussion = Discussion(json \ "discussion")
+    val comments = (json \ "discussion" \"comments").as[JsArray].value map {Comment(_, None, Some(discussion))}
+    DiscussionComments(
+      discussion = discussion,
+      comments = comments,
+      pagination = Pagination(json),
+      commentCount = (json \ "discussion" \ "commentCount").as[Int],
+      topLevelCommentCount = (json \ "discussion" \ "topLevelCommentCount").as[Option[Int]] getOrElse 0,
+      commenterCount =  (json \ "discussion" \ "commenterCount").as[Option[Int]] getOrElse 0,
+      switches = (json \ "switches").as[Seq[JsObject]] map {Switch(_)}
+    )
+  }
+}
+
+case class ProfileComments(
+  profile: Profile,
+  comments: Seq[Comment],
+  pagination: Pagination
+) extends Comments {
+  val switches = Nil
+}
+
+object ProfileComments{
+
+  def apply(json: JsValue): ProfileComments = {
+    val profile = Profile(json)
+    val comments = (json \ "comments").as[JsArray].value map {Comment(_, Some(profile), None)}
+    ProfileComments(
+      profile = profile,
+      comments = comments,
+      pagination = Pagination(json)
+    )
+  }
+}
+
+case class ProfileDiscussions(
+  profile: Profile,
+  discussions: Seq[DiscussionComments]
+)
+
+object ProfileDiscussions{
+
+  def apply(json: JsValue): ProfileDiscussions = {
+    val profile = Profile(json)
+    val discussions = (json \ "discussions").as[JsArray].value map { d =>
+      val discussion = Discussion(d)
+      DiscussionComments(
+        discussion = discussion,
+        comments = (d \ "comments").as[JsArray].value.map { Comment(_, Some(profile), Some(discussion)) },
+        pagination = Pagination(json),
+        commentCount = 0,
+        topLevelCommentCount = 0,
+        commenterCount = 0,
+        switches = Seq()
+      )
+    }
+    ProfileDiscussions(
+      profile = profile,
+      discussions = discussions
+    )
+  }
+}
+
+case class Pagination(currentPage: Int, pages: Int, pageSize: Int, orderBy: String){
+  lazy val hasMore: Boolean = currentPage < pages
+}
+
+object Pagination {
+
+  def apply(json: JsValue): Pagination = Pagination(
+    (json \ "currentPage").as[Int],
+    (json \ "pages").as[Option[Int]] getOrElse -1,
+    (json \ "pageSize").as[Option[Int]] getOrElse -1,
+    (json \ "orderBy").as[String]
+  )
+}
