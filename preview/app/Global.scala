f27f522adbdedd7ef01c3aef22c9cb7cc64b09df
Grant Klopper <grant.klopper@theguardian.com>
2014-05-23 09:29:25 +0100

    Those really should have better names

--- preview/app/Global.scala
+++ preview/app/Global.scala
@@ -8,8 +8,8 @@ import services.ConfigAgentLifecycle
 // OBVIOUSLY this is only for the preview server
 // NOT to be used elsewhere...
 object NoCacheFilter extends Filter with ExecutionContexts {
-  override def apply(f: (RequestHeader) => Future[SimpleResult])(rh: RequestHeader): Future[SimpleResult] =
-    f(rh).map(_.withHeaders("Cache-Control" -> "no-cache"))
+  override def apply(nextFilter: (RequestHeader) => Future[SimpleResult])(request: RequestHeader): Future[SimpleResult] =
+    nextFilter(request).map(_.withHeaders("Cache-Control" -> "no-cache"))
 }
 
 object Global extends WithFilters(NoCacheFilter :: Filters.common: _*) with CommercialLifecycle
