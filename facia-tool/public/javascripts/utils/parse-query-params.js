05ae91aceae1c9c87ef7a68154b318838a5eb90c
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-05-27 16:17:33 +0100

    Improve readability

--- facia-tool/public/javascripts/utils/parse-query-params.js
+++ facia-tool/public/javascripts/utils/parse-query-params.js
@@ -1,16 +1,20 @@
 /* global _: true */
 define(['utils/url-query'], function(urlQuery) {
+    /**
+     * Returns query params as an object.
+     * @param {function} opts                  optional options
+     * @param {function} opts.predicate        fn to determine which vals get included
+     * @param {string}   opts.namespace        prefix for param keys
+     * @param {boolean}  opts.excludeNamespace whether to exclude or include prefixed keys
+     * @param {boolean}  opts.stripNamespace   whether to strip prefix from returned object's keys
+     */
+
     return function(url, opts) {
         opts = opts || {};
 
-        /**
-         * Returns query params as an object.
-         * @param {function} opts                  optional options
-         * @param {function} opts.predicate        fn to determine which vals get included
-         * @param {string}   opts.namespace        prefix for param keys
-         * @param {boolean}  opts.excludeNamespace whether to exclude or include prefixed keys
-         * @param {boolean}  opts.stripNamespace   whether to strip prefix from returned object's keys
-         */
+        var nsIndex = opts.excludeNamespace ? -1 : 0,
+            nsStrip = opts.namespace && opts.stripNamespace && !opts.excludeNamespace,
+            nsLength = opts.namespace ? ('' + opts.namespace).length : 0;
 
         return _.chain(urlQuery(url).split('&'))
             .filter(function(kv) {
@@ -23,11 +27,11 @@ define(['utils/url-query'], function(urlQuery) {
                 return _.isFunction(opts.predicate) ? opts.predicate(kv[1]) : true; })
 
             .filter(function(kv) {
-                return !opts.namespace || kv[0].indexOf(opts.namespace) === (opts.excludeNamespace ? -1 : 0); })
+                return !opts.namespace || kv[0].indexOf(opts.namespace) === nsIndex; })
 
             .map(function(kv) {
                 return [
-                    opts.namespace && opts.stripNamespace && !opts.excludeNamespace ? kv[0].slice(opts.namespace.length) : kv[0],
+                    nsStrip ? kv[0].slice(nsLength) : kv[0],
                     kv[1] === undefined ? undefined : decodeURIComponent(kv[1].replace(/\+/g, ' '))
                 ]; })
 
