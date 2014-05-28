47680a87e36503d22e2e49423d5ffa0fa19a45d7
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-05-28 14:23:10 +0100

    Fix Keywords for Commercial Components

--- commercial/app/model/commercial/Keyword.scala
+++ commercial/app/model/commercial/Keyword.scala
@@ -1,40 +1,29 @@
 package model.commercial
 
-import scala.concurrent.Future
-import conf.ContentApi
-import common.{Edition, Logging, ExecutionContexts}
-import model.{ImageElement, Content}
 import com.gu.openplatform.contentapi
+import com.gu.openplatform.contentapi.model.Tag
+import common.{Edition, Logging, ExecutionContexts}
+import conf.ContentApi
 import model.commercial.masterclasses.EventbriteMasterClass
-
-case class Keyword(id: String, webTitle: String) {
-
-  /* has to do same transformation as getKeywords in
-   * common/app/assets/javascripts/modules/adverts/document-write.js
-   * so that it can be used for matching
-   */
-  val name = webTitle.toLowerCase.replaceAll( """\s""", "-")
-
-}
+import model.{ImageElement, Content}
+import scala.concurrent.Future
 
 case class MasterClass(eventBriteEvent: EventbriteMasterClass, imageElement: Option[model.ImageElement])
 
 object Lookup extends ExecutionContexts with Logging {
   def thumbnail(contentId: String): Future[Option[ImageElement]] = {
-    ContentApi.item(contentId, Edition.defaultEdition).response.map{response =>
+    ContentApi.item(contentId, Edition.defaultEdition).response.map { response =>
       val option: Option[contentapi.model.Content] = response.content
       option.flatMap(Content(_).thumbnail)
     }
   }
 
 
-  def keyword(term: String, section: Option[String] = None): Future[Seq[Keyword]] = {
+  def keyword(term: String, section: Option[String] = None): Future[Seq[Tag]] = {
     val baseQuery = ContentApi.tags.q(term).tagType("keyword").pageSize(50)
     val query = section.foldLeft(baseQuery)((acc, sectionName) => acc section sectionName)
 
-    val result = query.response.map {
-      _.results.map(tag => Keyword(tag.id, tag.webTitle))
-    } recover {
+    val result = query.response.map(_.results) recover {
       case e =>
         log.warn(s"Failed to look up [$term]: ${e.getMessage}")
         Nil
