d26323537b4f8271504a026370bcbbc0f205ebf9
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-06-02 15:39:12 +0100

    moving badge ad slot to js

--- common/app/assets/javascripts/modules/adverts/badges.js
+++ common/app/assets/javascripts/modules/adverts/badges.js
@@ -1,20 +1,41 @@
 define([
+    'qwery',
     'common/$',
     'common/modules/adverts/dfp',
     'lodash/functions/once'
 ], function (
+    qwery,
     $,
     dfp,
     once
 ) {
 
+    var hadSponsoredBadge = false,
+        hadAdvertisementFeatureBadge = false,
+        createAdSlot = function(container, isSponsored) {
+            if ((isSponsored && hadSponsoredBadge) || (!isSponsored && hadAdvertisementFeatureBadge)) {
+                return;
+            }
+            console.log($('.container__header', container));
+            $('.container__header', container)
+                .after(dfp.createAdSlot((isSponsored ? 'sp' : 'ad') + 'badge', 'paid-for-badge'));
+            if (isSponsored) {
+                hadSponsoredBadge = true;
+            } else {
+                hadAdvertisementFeatureBadge = true;
+            }
+        };
+
     return {
 
         init: once(function() {
+            $('.facia-container--sponsored, .facia-container--advertisement-feature').each(function(faciaContainer) {
+                createAdSlot(qwery('.container', faciaContainer)[0], $(faciaContainer).hasClass('facia-container--sponsored'));
+            });
             $('.container--sponsored, .container--advertisement-feature').each(function(container) {
-                var isSponsored = $(container).hasClass('container--sponsored');
-                $('.container__header', container)
-                    .after(dfp.createAdSlot((isSponsored ? 'sp' : 'ad') + 'badge', 'paid-for-badge'));
+                if (qwery('.paid-for-badge', container).length === 0) {
+                    createAdSlot(container, $(container).hasClass('container--sponsored'));
+                }
             });
         })
 
