0d72f84ac7bd33bb10b388c8ece59cbe37640dc4
Francis Carr <francis.carr@guardian.co.uk>
2014-06-03 14:21:09 +0100

    Wrap set in StaleWrapper Use FiniteDuration Change to get Add a require on the parameters for cache

--- common/app/performance/MemcachedStaleCache.scala
+++ common/app/performance/MemcachedStaleCache.scala
@@ -6,15 +6,16 @@ import implicits.Dates
 import shade.memcached.{Configuration => MemcachedConf, Codec, Memcached}
 import conf.Configuration
 import scala.concurrent.Future
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{FiniteDuration, Duration}
+import shade.memcached.MemcachedCodecs._
 
 sealed trait CacheResponse[A] {
-  val a: A
+  val get: A
 }
 
-case class CacheHit[A](a: A) extends CacheResponse[A]
-case class CacheStale[A](a: A) extends CacheResponse[A]
-case class CacheMiss[A](a: A) extends CacheResponse[A]
+case class CacheHit[A](get: A) extends CacheResponse[A]
+case class CacheStale[A](get: A) extends CacheResponse[A]
+case class CacheMiss[A](get: A) extends CacheResponse[A]
 
 case class StaleWrapper[A](insertedAt: DateTime, response: A)
 
@@ -22,16 +23,20 @@ object MemcachedStaleCache extends ExecutionContexts with Dates {
   lazy val host = Configuration.memcached.host.head
   lazy val memcached = Memcached(MemcachedConf(host), actorSystem.scheduler, memcachedExecutionContext)
 
-  def cache[A: Codec](
+  def cache[A](
       key: String,
-      staleAfter: Duration,
-      invalidAfter: Duration
+      staleAfter: FiniteDuration,
+      invalidAfter: FiniteDuration
   )(f: => Future[A]): Future[CacheResponse[A]] = {
+    require(invalidAfter gt staleAfter, "Stale after duration () must be less than or equal to ")
+
+    implicit val staleWrapperCodec = AnyRefBinaryCodec[StaleWrapper[A]]
+
     def triggerRequest(): Future[A] = {
       val futureA = f
 
       futureA onSuccess {
-        case a => memcached.set(key, a, invalidAfter)
+        case a => memcached.set(key, StaleWrapper(DateTime.now, a), invalidAfter)
       }
 
       futureA
@@ -45,7 +50,8 @@ object MemcachedStaleCache extends ExecutionContexts with Dates {
       case Some(StaleWrapper(_, response)) =>
         Future.successful(CacheHit(response))
 
-      case None => triggerRequest().map(CacheMiss.apply)
+      case None =>
+        triggerRequest().map(CacheMiss.apply)
     }
   }
 }
