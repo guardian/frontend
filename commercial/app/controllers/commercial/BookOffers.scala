71ad6a92a8cf7a1a95df83aa4b992e43d6d7cd3a
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-06-04 12:20:23 +0100

    Cache Book Offers

--- commercial/app/controllers/commercial/BookOffers.scala
+++ commercial/app/controllers/commercial/BookOffers.scala
@@ -1,41 +1,45 @@
 package controllers.commercial
 
-import play.api.mvc._
-import model.{NoCache, Cached}
 import common.{ExecutionContexts, JsonComponent}
 import model.commercial.books.{BookFinder, BestsellersAgent}
+import model.{NoCache, Cached}
+import performance.MemcachedAction
+import play.api.mvc._
+import scala.concurrent.Future
 
 object BookOffers extends Controller with ExecutionContexts {
 
-  def bestsellers(format: String) = Action {
-    implicit request =>
+  def bestsellers(format: String) = MemcachedAction { implicit request =>
+    Future.successful {
       BestsellersAgent.adsTargetedAt(segment) match {
         case Nil => NoCache(NotFound)
         case books if format == "json" =>
-          Cached(60)(JsonComponent(views.html.books.bestsellers(books)))
+          Cached(componentMaxAge)(JsonComponent(views.html.books.bestsellers(books)))
         case books if format == "html" =>
-          Cached(60)(Ok(views.html.books.bestsellers(books)))
+          Cached(componentMaxAge)(Ok(views.html.books.bestsellers(books)))
       }
-  }
-  
-  def bestsellersHigh(format: String) = Action {
-    implicit request =>
-      BestsellersAgent.adsTargetedAt(segment) match {
-        case Nil => NoCache(NotFound)
-        case books if format == "json" =>
-          Cached(60)(JsonComponent(views.html.books.bestsellersHigh(books)))
-        case books if format == "html" =>
-          Cached(60)(Ok(views.html.books.bestsellersHigh(books)))
-      }
+    }
   }
 
-  def singleBook(pageId: String, format: String) = Action.async {
+  def bestsellersHigh(format: String) = MemcachedAction { implicit request =>
+    Future.successful {
+      BestsellersAgent.adsTargetedAt(segment) match {
+        case Nil => NoCache(NotFound)
+        case books if format == "json" =>
+          Cached(componentMaxAge)(JsonComponent(views.html.books.bestsellersHigh(books)))
+        case books if format == "html" =>
+          Cached(componentMaxAge)(Ok(views.html.books.bestsellersHigh(books)))
+      }
+    }
+  }
+
+  def singleBook(pageId: String, format: String) = MemcachedAction {
     implicit request =>
       BookFinder.findByPageId(pageId) map {
         case Some(book) if format == "json" =>
-          Cached(60)(JsonComponent(views.html.books.singleBook(book)))
+          Cached(componentMaxAge)(JsonComponent(views.html.books.singleBook(book)))
         case Some(book) if format == "html" =>
-          Cached(60)(Ok(views.html.books.singleBook(book)))
+          Cached(componentMaxAge)(Ok(views.html.books.singleBook(book)))
         case _ => NoCache(NotFound)
       }
   }
