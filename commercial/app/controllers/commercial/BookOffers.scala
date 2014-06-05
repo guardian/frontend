67a744cc80d3248a99da272fc578f836c1fb8f80
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-06-05 13:23:42 +0100

    Refactor Book Offers Controller

--- commercial/app/controllers/commercial/BookOffers.scala
+++ commercial/app/controllers/commercial/BookOffers.scala
@@ -1,59 +1,94 @@
 package controllers.commercial
 
-import common.{ExecutionContexts, JsonComponent}
+import common.{JsonNotFound, ExecutionContexts, JsonComponent}
 import model.commercial.books.{Book, BookFinder, BestsellersAgent}
 import model.{NoCache, Cached}
 import performance.MemcachedAction
 import play.api.mvc._
+import play.api.templates.Html
 import scala.concurrent.Future
 
 object BookOffers extends Controller with ExecutionContexts with implicits.Collections {
 
-  def bestsellers(format: String) = MemcachedAction { implicit request =>
-    Future.successful {
-      (BestsellersAgent.getSpecificBooks(specificIds) ++ BestsellersAgent.adsTargetedAt(segment))
-        .distinctBy(_.isbn).take(5) match {
-        case Nil => NoCache(NotFound)
-        case books if format == "json" =>
-          Cached(componentMaxAge)(JsonComponent(views.html.books.bestsellers(books)))
-        case books if format == "html" =>
-          Cached(componentMaxAge)(Ok(views.html.books.bestsellers(books)))
+  private def renderBestsellers(relevance: Relevance, format: Format) =
+    MemcachedAction { implicit request =>
+      Future.successful {
+        (BestsellersAgent.getSpecificBooks(specificIds) ++ BestsellersAgent.adsTargetedAt(segment))
+          .distinctBy(_.isbn).take(5) match {
+          case Nil => NoCache(format.nilResult)
+          case books => Cached(componentMaxAge) {
+            format.result(relevance.view(books))
+          }
+        }
       }
+    }
+
+  def bestsellersLowJson = renderBestsellers(LowRelevance, JsonFormat)
+
+  def bestsellersLowHtml = renderBestsellers(LowRelevance, HtmlFormat)
+
+  def bestsellersMediumJson = renderBestsellers(MediumRelevance, JsonFormat)
+
+  def bestsellersMediumHtml = renderBestsellers(MediumRelevance, HtmlFormat)
+
+  def bestsellersHighJson = renderBestsellers(HighRelevance, JsonFormat)
+
+  def bestsellersHighHtml = renderBestsellers(HighRelevance, HtmlFormat)
+
+  def singleBookJson(pageId: String) = MemcachedAction { implicit request =>
+    BookFinder.findByPageId(pageId) map {
+      case Some(book) => Cached(componentMaxAge) {
+        JsonComponent(views.html.books.singleBook(book))
+      }
+      case None => NoCache(JsonNotFound.apply())
     }
   }
 
-  def bestsellersMedium(format: String) = MemcachedAction { implicit request =>
-    Future.successful {
-      BestsellersAgent.adsTargetedAt(segment) match {
-        case Nil => NoCache(NotFound)
-        case books if format == "json" =>
-          Cached(60)(JsonComponent(views.html.books.bestsellersMedium(books)))
-        case books if format == "html" =>
-          Cached(60)(Ok(views.html.books.bestsellersMedium(books)))
+  def singleBookHtml(pageId: String) = MemcachedAction { implicit request =>
+    BookFinder.findByPageId(pageId) map {
+      case Some(book) => Cached(componentMaxAge) {
+        Ok(views.html.books.singleBook(book))
       }
+      case None => NoCache(NotFound)
     }
   }
-  
-  def bestsellersHigh(format: String) = MemcachedAction { implicit request =>
-    Future.successful {
-      BestsellersAgent.adsTargetedAt(segment) match {
-        case Nil => NoCache(NotFound)
-        case books if format == "json" =>
-          Cached(componentMaxAge)(JsonComponent(views.html.books.bestsellersHigh(books)))
-        case books if format == "html" =>
-          Cached(componentMaxAge)(Ok(views.html.books.bestsellersHigh(books)))
-      }
-    }
-  }
+}
 
-  def singleBook(pageId: String, format: String) = MemcachedAction {
-    implicit request =>
-      BookFinder.findByPageId(pageId) map {
-        case Some(book) if format == "json" =>
-          Cached(componentMaxAge)(JsonComponent(views.html.books.singleBook(book)))
-        case Some(book) if format == "html" =>
-          Cached(componentMaxAge)(Ok(views.html.books.singleBook(book)))
-        case _ => NoCache(NotFound)
-      }
-  }
+
+sealed trait Relevance {
+  def view(books: Seq[Book])(implicit request: RequestHeader): Html
+}
+
+object LowRelevance extends Relevance {
+  override def view(books: Seq[Book])(implicit request: RequestHeader): Html = views.html.books.bestsellers(books)
+}
+
+object MediumRelevance extends Relevance {
+  override def view(books: Seq[Book])(implicit request: RequestHeader): Html = views.html.books.bestsellersMedium(books)
+}
+
+object HighRelevance extends Relevance {
+  override def view(books: Seq[Book])(implicit request: RequestHeader): Html = views.html.books.bestsellersHigh(books)
+}
+
+
+sealed trait Format {
+
+  def nilResult(implicit request: RequestHeader): SimpleResult
+
+  def result(view: Html)(implicit request: RequestHeader): SimpleResult
+}
+
+object HtmlFormat extends Format {
+
+  override def nilResult(implicit request: RequestHeader): SimpleResult = Results.NotFound
+
+  override def result(view: Html)(implicit request: RequestHeader): SimpleResult = Results.Ok(view)
+}
+
+object JsonFormat extends Format {
+
+  override def nilResult(implicit request: RequestHeader): SimpleResult = JsonNotFound.apply()
+
+  override def result(view: Html)(implicit request: RequestHeader): SimpleResult = JsonComponent(view)
 }
