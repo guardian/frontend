a8629ee7ef04a01bf9eeef3a22e78efc9e683e28
Chris Mulholland <chris.mulholland@gmail.com>
2014-06-06 10:26:58 +0100

    Reverting addition of plugins for Time of Day

--- common/app/assets/javascripts/components/omniture/omniture.js
+++ common/app/assets/javascripts/components/omniture/omniture.js
@@ -21,18 +21,6 @@ s.linkLeaveQueryString=false;
 s.linkTrackVars="None";
 s.linkTrackEvents="None";
 
-/* Plugin Config */
-s._tpDST = {
-    2012:'3/25,10/28',
-    2013:'3/31,10/27',
-    2014:'3/30,10/26',
-    2015:'3/29,10/25',
-    2016:'3/27,10/30',
-    2017:'3/26,10/29',
-    2018:'3/25,10/28',
-    2019:'3/31,10/27'
-};
-
 /************************ DO PLUGINS SECTION ************************/
 s.usePlugins=true;
 function s_doPlugins(s) {
@@ -239,24 +227,6 @@ s.getValOnce=new Function("v","c","e","t",""
     +"var s=this,a=new Date,v=v?v:'',c=c?c:'s_gvo',e=e?e:0,i=t=='m'?6000"
     +"0:86400000;k=s.c_r(c);if(v){a.setTime(a.getTime()+e*i);s.c_w(c,v,e"
     +"==0?0:a);}return v==k?'':v");
-
-/*
- * Plugin: Days since last Visit 1.1.H - capture time from last visit
- */
-s.getDaysSinceLastVisit=new Function("c",""
-    +"var s=this,e=new Date(),es=new Date(),cval,cval_s,cval_ss,ct=e.getT"
-    +"ime(),day=24*60*60*1000,f1,f2,f3,f4,f5;e.setTime(ct+3*365*day);es.s"
-    +"etTime(ct+30*60*1000);f0='Cookies Not Supported';f1='First Visit';f"
-    +"2='More than 30 days';f3='More than 7 days';f4='Less than 7 days';f"
-    +"5='Less than 1 day';cval=s.c_r(c);if(cval.length==0){s.c_w(c,ct,e);"
-    +"s.c_w(c+'_s',f1,es);}else{var d=ct-cval;if(d>30*60*1000){if(d>30*da"
-    +"y){s.c_w(c,ct,e);s.c_w(c+'_s',f2,es);}else if(d<30*day+1 && d>7*day"
-    +"){s.c_w(c,ct,e);s.c_w(c+'_s',f3,es);}else if(d<7*day+1 && d>day){s."
-    +"c_w(c,ct,e);s.c_w(c+'_s',f4,es);}else if(d<day+1){s.c_w(c,ct,e);s.c"
-    +"_w(c+'_s',f5,es);}}else{s.c_w(c,ct,e);cval_ss=s.c_r(c+'_s');s.c_w(c"
-    +"+'_s',cval_ss,es);}}cval_s=s.c_r(c+'_s');if(cval_s.length==0) retur"
-    +"n f0;else if(cval_s!=f1&&cval_s!=f2&&cval_s!=f3&&cval_s!=f4&&cval_s"
-    +"!=f5) return '';else return cval_s;");
 
 /*
  *  Plug-in: crossVisitParticipation v1.7 - stacks values from
