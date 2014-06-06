32693aeced093d04fdebc0b7634912d090025153
Francis Carr <francis.carr@guardian.co.uk>
2014-06-06 17:22:07 +0100

    Add methods for putting pressed draft and live json

--- facia-tool/app/frontpress/FrontPressJob.scala
+++ facia-tool/app/frontpress/FrontPressJob.scala
@@ -84,7 +84,7 @@ object FrontPressJob extends Logging with implicits.Collections {
 
   def pressByPathId(path: String): Future[JsObject] = {
     FrontPress.generateJson(path).map { json =>
-      (json \ "id").asOpt[String].foreach(S3FrontsApi.putPressedJson(_, Json.stringify(json)))
+      (json \ "id").asOpt[String].foreach(S3FrontsApi.putLivePressedJson(_, Json.stringify(json)))
       json
     }
   }
