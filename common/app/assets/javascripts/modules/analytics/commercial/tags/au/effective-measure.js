d3ea8d7e23b83fa741f63785ec4d83409b9555eb
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-06-10 15:41:34 +0100

    move all tag switches into object

--- common/app/assets/javascripts/modules/analytics/commercial/tags/au/effective-measure.js
+++ common/app/assets/javascripts/modules/analytics/commercial/tags/au/effective-measure.js
@@ -1,9 +1,15 @@
-define([], function() {
+define([
+    'common/utils/config'
+], function(
+    config
+    ) {
 
     var effectiveMeasureUrl = 'js!' + (document.location.protocol === 'https:' ? 'https://au-ssl' : 'http://au-cdn') + '.effectivemeasure.net/em.js';
 
     function load() {
-        require([effectiveMeasureUrl], function() {});
+        if (config.switches.effectiveMeasure) {
+            require([effectiveMeasureUrl], function () {});
+        }
     }
 
     return {
