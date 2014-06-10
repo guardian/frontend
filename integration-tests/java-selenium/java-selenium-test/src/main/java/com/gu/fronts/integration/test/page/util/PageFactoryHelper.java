647840dd9048df3604559b494be3a3b1b8f91fd4
Shahin Kordasti <shahin.kordasti@guardian.co.uk>
2014-06-10 12:10:16 +0100

    updated javadoc for PageHelperFactory

--- integration-tests/java-selenium/java-selenium-test/src/main/java/com/gu/fronts/integration/test/page/util/PageFactoryHelper.java
+++ integration-tests/java-selenium/java-selenium-test/src/main/java/com/gu/fronts/integration/test/page/util/PageFactoryHelper.java
@@ -9,10 +9,9 @@ public class PageFactoryHelper {
 
     /**
      * Simple wrapper of
-     * {@link PageFactory#initElements(org.openqa.selenium.support.pagefactory.ElementLocatorFactory, Object)} which
-     * cast the initialized page to the proper class
+     * {@link PageFactory#initElements(org.openqa.selenium.support.pagefactory.ElementLocatorFactory, Object)}
      */
     public <Page> Page loadPage(Class<Page> pageClass, WebDriver webDriver) {
-        return (Page) PageFactory.initElements(webDriver, pageClass);
+        return PageFactory.initElements(webDriver, pageClass);
     }
 }
