7c944a0a02c56dfd93f8ce4c525af5d3a9761453
Ken Lim <kennethlimfc@gmail.com>
2014-06-11 21:22:52 +0100

    Obtained pageskin ad unit ids.

--- admin/test/dfp/DfpApiExploratoryTest.scala
+++ admin/test/dfp/DfpApiExploratoryTest.scala
@@ -16,14 +16,31 @@ class DfpApiSpec extends FlatSpec with Matchers {
     }
   }
 
-  "DfpAPI" should "be able to grab all line items that target 'out of page' slots" in running(FakeApplication()) {
+  ignore should "be able to grab all line items that target 'out of page' slots" in running(FakeApplication()) {
     val slots: Seq[DfpApiLineItem] = DfpApi.fetchCurrentLineItemsWithOutOfPageSlots()
 
     for (item <- slots) {
       println(item.getId())
     }
 
-    slots.size shouldEqual(0)
+    slots.size shouldEqual(11)
 
+  }
+
+  ignore should "extract the URL targets from the line items" in running(FakeApplication()) {
+    val ids: Seq[String] = DfpApi.fetchAdUnitIdsThatAreTargettedByPageSkins()
+
+    val expected: List[String] = List("59357607", "59357607", "59357607", "59342247", "59360247",
+      "59360247", "59360007", "59360367", "59360007", "59360007", "59359887")
+
+    ids.size shouldEqual(expected.size)
+    for(item <- ids) {
+      expected should contain(item)
+    }
+  }
+
+  "DfpApi" should "grab ad units for list of Ids provided" in running(FakeApplication()) {
+    List("59357607", "59357607", "59357607", "59342247", "59360247",
+      "59360247", "59360007", "59360367", "59360007", "59360007", "59359887")
   }
 }
