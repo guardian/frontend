7c3047d6b51a719d81ac827e48278665a1104699
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-06-13 12:32:52 +0100

    better naming

--- admin/app/assets/javascripts/bootstraps/commercial.js
+++ admin/app/assets/javascripts/bootstraps/commercial.js
@@ -12,17 +12,15 @@ define([
 
     function init() {
 
-        var $adsRenderTime = $('.ads-render-time'),
-            $adRenderTime = $('.ad-render-time');
+        var $adsRenderTime = $('.render-time--ads'),
+            $adRenderTime = $('.render-time--ad');
 
+        $adsRenderTime.addClass('graph-loading');
         ajax({
             url: '/ophan/ads/render-time?platform=next-gen',
             type: 'json'
         }).then(
             function(nextGenData) {
-                $adsRenderTime
-                    .removeClass('graph-action')
-                    .removeClass('graph-failed');
                 ajax({
                     url: '/ophan/ads/render-time?platform=r2',
                     type: 'json'
@@ -36,7 +34,7 @@ define([
                         ];
                     }));
 
-                    new google.visualization.LineChart(qwery('ads-render-time__graph')[0])
+                    new google.visualization.LineChart(qwery('#render-time--ads__graph')[0])
                         .draw(google.visualization.arrayToDataTable(graphData), {
                             fontName: 'Georgia',
                             title: 'Ads\' average render time (secs)',
@@ -60,9 +58,6 @@ define([
                 url: '/ophan/ads/render-time?platform=next-gen&ad-slot=' + adSlotName,
                 type: 'json'
             }).then(function(data) {
-                $adRenderTime
-                    .removeClass('graph-action')
-                    .removeClass('graph-failed');
 
                 var graphData = [['Time', 'Next-Gen']].concat(data.buckets.map(function(bucket) {
                     return [
@@ -71,7 +66,7 @@ define([
                     ];
                 }));
 
-                new google.visualization.LineChart(qwery('#ad-render-time__graph')[0])
+                new google.visualization.LineChart(qwery('#render-time--ad__graph')[0])
                     .draw(google.visualization.arrayToDataTable(graphData), {
                         fontName: 'Georgia',
                         legend: 'none',
@@ -96,14 +91,20 @@ define([
             'right',
             'merchandising-high',
             'merchandising'
-        ].forEach(function(adSlotName) {
-                $select.append('<option value="' + adSlotName + '">' + adSlotName + '</option>');
+        ].forEach(function(adSlotName, i) {
+                $.create('<option></option>')
+                    .val(adSlotName)
+                    .text(adSlotName)
+                    .attr('selected', i === 0)
+                    .appendTo($select);
             });
-        $.create('<form class="ad-render-time__form"></form>')
+        $.create('<form></form>')
+            .addClass('render-time--ad__form')
             .append($select)
-            .prependTo(qwery('.ad-render-time')[0]);
+            .prependTo(qwery('.render-time--ad')[0]);
 
         bean.on($select[0], 'change', function(e) {
+            $('#render-time--ad__graph').html('');
             daramAdRenderTime(e.target.value);
         });
 
