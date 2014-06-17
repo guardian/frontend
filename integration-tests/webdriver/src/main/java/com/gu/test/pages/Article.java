fd6184c7b1059f0fabd93df5565ee9d7eaf64d90
Tori Johanne Lau <tori.lau@guardian.co.uk>
2014-06-17 14:49:42 +0100

    adding some waits.

--- integration-tests/webdriver/src/main/java/com/gu/test/pages/Article.java
+++ integration-tests/webdriver/src/main/java/com/gu/test/pages/Article.java
@@ -6,83 +6,84 @@ import org.openqa.selenium.WebDriver;
 
 public class Article {
 
-    private WebDriver driver;
+        private WebDriver driver;
 
-    public Article(WebDriver driver) {
-        this.driver = driver;
-    }
+        public Article(WebDriver driver) {
+                this.driver = driver;
+        }
 
+        public void shareViaEmail() {
+                driver.findElement(By.cssSelector(".i-share-email")).click();
+                WaitHelper.waitForPageLoad(driver);
+        }
 
-    public void shareViaEmail() {
-        driver.findElement(By.cssSelector(".i-share-email")).click();
-            WaitHelper waitHelper = new WaitHelper();
-            waitHelper.waitForPageLoad(driver);
-    }
+        public void shareOnFacebook() {
+                driver.findElement(By.cssSelector(".i-share-facebook")).click();
+                WaitHelper.waitForPageLoad(driver);
+        }
 
-    public void shareOnFacebook() {
-        driver.findElement(By.cssSelector(".i-share-facebook")).click();
-    }
+        public void shareOnTwitter() {
+                driver.findElement(By.cssSelector(".i-share-twitter")).click();
+                WaitHelper.waitForPageLoad(driver);
+        }
 
-    public void shareOnTwitter() {
-        driver.findElement(By.cssSelector(".i-share-twitter")).click();
-    }
+        public void shareOnGooglePlus() {
+                driver.findElement(By.cssSelector(".i-share-gplus")).click();
+                WaitHelper.waitForPageLoad(driver);
+        }
 
-    public void shareOnGooglePlus() {
-        driver.findElement(By.cssSelector(".i-share-gplus")).click();
-    }
+        public boolean hasMostPopularRight() {
 
+                return driver.findElement(By.cssSelector(".right-most-popular")).isDisplayed();
+        }
 
-    public boolean hasMostPopularRight() {
+        public Article goToFirstArticleInMostPopularRight() {
+                driver.findElement(By.cssSelector(".right-most-popular-item__url")).click();
+                return new Article(driver);
+        }
 
-        return driver.findElement(By.cssSelector(".right-most-popular")).isDisplayed();
-    }
+        public String getFirstArticleInMostPopularRight() {
+                return driver.findElement(By.cssSelector(".right-most-popular-item__url")).getText();
+        }
 
-    public Article goToFirstArticleInMostPopularRight() {
-        driver.findElement(By.cssSelector(".right-most-popular-item__url")).click();
-        return new Article(driver);
-    }
+        public String getArticleHeadline() {
+                return driver.findElement(By.cssSelector(".content__headline")).getText();
+        }
 
-    public String getFirstArticleInMostPopularRight() {
-        return driver.findElement(By.cssSelector(".right-most-popular-item__url")).getText();
-    }
+        public boolean hasRelatedContentContainer() {
+                return driver.findElement(By.cssSelector(".related")).isDisplayed();
+        }
 
-    public String getArticleHeadline() {
-        return driver.findElement(By.cssSelector(".content__headline")).getText();
-    }
+        public String getFirstRelatedArticle() {
+                return driver.findElement(By.cssSelector(".related .item__link")).getText();
+        }
 
-    public boolean hasRelatedContentContainer() {
-        return driver.findElement(By.cssSelector(".related")).isDisplayed();
-    }
+        public Article goToFirstRelatedArticle() {
+                driver.findElement(By.cssSelector(".related .item__link")).click();
+                return new Article(driver);
+        }
 
-    public String getFirstRelatedArticle() {
-        return driver.findElement(By.cssSelector(".related .item__link")).getText();
-    }
+        public String getFirstArticleInMostPopularBelow() {
+                return driver.findElement(By.cssSelector(".article__popular div#tabs-popular-1 .headline-list__body"))
+                        .getText();
+        }
 
-    public Article goToFirstRelatedArticle() {
-        driver.findElement(By.cssSelector(".related .item__link")).click();
-        return new Article(driver);
-    }
+        public boolean hasMostPopularBelow() {
+                return driver.findElement(By.cssSelector(".article__popular")).isDisplayed();
+        }
 
-    public String getFirstArticleInMostPopularBelow() {
-        return driver.findElement(By.cssSelector(".article__popular div#tabs-popular-1 .headline-list__body")).getText();
-    }
+        public Article goToFirstArticleInMostPopularBottom() {
+                driver.findElement(By.cssSelector(".article__popular div#tabs-popular-1 a")).click();
+                return new Article(driver);
+        }
 
-    public boolean hasMostPopularBelow() {
-        return driver.findElement(By.cssSelector(".article__popular")).isDisplayed();
-    }
+        public boolean hasComments() {
+                return driver.findElement(By.cssSelector(".discussion-content")).isDisplayed();
+        }
 
-    public Article goToFirstArticleInMostPopularBottom() {
-        driver.findElement(By.cssSelector(".article__popular div#tabs-popular-1 a")).click();
-        return new Article(driver);
-    }
-
-    public boolean hasComments() {
-        return driver.findElement(By.cssSelector(".discussion-content")).isDisplayed();
-    }
-
-    public boolean hasTweets() {
-        return driver.findElement(By.cssSelector(".element-tweet")).isDisplayed();
-    }
+        public boolean hasTweets() {
+                return driver.findElement(By.cssSelector(".element-tweet")).isDisplayed();
+        }
 }
 
 
