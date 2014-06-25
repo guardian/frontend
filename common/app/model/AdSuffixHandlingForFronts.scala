e7e71d8d93f38bc6db297c2d658a970849574a66
Ken Lim <kennethlimfc@gmail.com>
2014-06-25 11:27:04 +0100

    Added test to keep us honest.

--- common/app/model/AdSuffixHandlingForFronts.scala
+++ common/app/model/AdSuffixHandlingForFronts.scala
@@ -1,14 +1,19 @@
 package model
 
+// This should only be extended by objects that display 'fronts' of the site.
 trait AdSuffixHandlingForFronts extends MetaData{
-  override lazy val adUnitSuffix = {
-    val supportedCountries: List[String] = List("uk", "us", "au")
+  val supportedCountries: List[String] = List("uk", "us", "au")
 
-    val split = id.split("/").toList
-    split match {
-      case cc :: Nil  if supportedCountries contains cc => (cc :: "front" :: Nil).mkString("/")
-      case cc :: restOfIt if supportedCountries contains cc => (restOfIt ::: "front" :: Nil).mkString("/")
-      case whatever => (whatever ::: "front" :: Nil).mkString("/")
+  override lazy val adUnitSuffix = {
+    extractAdUnitSuffixFrom(id)
+  }
+
+  def extractAdUnitSuffixFrom(path: String) = {
+    val frontSuffixList = List("front")
+    path.split("/").toList match {
+      case cc :: Nil  if supportedCountries contains cc => (cc :: frontSuffixList).mkString("/")
+      case cc :: pathList if supportedCountries contains cc => (pathList ::: frontSuffixList).mkString("/")
+      case pathList => (pathList ::: frontSuffixList).mkString("/")
     }
   }
 }
