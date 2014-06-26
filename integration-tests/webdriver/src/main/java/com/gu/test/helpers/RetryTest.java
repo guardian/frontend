693cdf1e73a162a39ce80f92d7ed9b7d90e23ba0
Tori Johanne Lau <tori.lau@guardian.co.uk>
2014-06-26 12:31:24 +0100

    Adding a retry test as a temporary measure while we convert to saucelabs

---
+++ integration-tests/webdriver/src/main/java/com/gu/test/helpers/RetryTest.java
@@ -0,0 +1,39 @@
+package com.gu.test.helpers;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class RetryTest implements TestRule {
+        private int retryCount;
+
+        public RetryTest(int retryCount) {
+            this.retryCount = retryCount;
+        }
+
+        public Statement apply(Statement base, Description description) {
+            return statement(base, description);
+        }
+
+        private Statement statement(final Statement base, final Description description) {
+            return new Statement() {
+                @Override
+                public void evaluate() throws Throwable {
+                    Throwable caughtThrowable = null;
+
+                    // implement retry logic here
+                    for (int i = 0; i < retryCount; i++) {
+                        try {
+                            base.evaluate();
+                            return;
+                        } catch (Throwable t) {
+                            caughtThrowable = t;
+                            System.err.println(description.getDisplayName() + ": run " + (i+1) + " failed");
+                        }
+                    }
+                    System.err.println(description.getDisplayName() + ": giving up after " + retryCount + " failures");
+                    throw caughtThrowable;
+                }
+            };
+        }
+    }
