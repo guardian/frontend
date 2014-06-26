673cc82426f2cf4afa36cfff6e8bb1b206d5d07f
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-06-26 13:53:39 +0100

    Add a try catch around the application

--- common/app/assets/javascripts/bootstraps/go.js
+++ common/app/assets/javascripts/bootstraps/go.js
@@ -1,13 +1,12 @@
 /*global guardian:true */
-require([
-    'core',
-    'bootstraps/app',
-    'domReady!'
-], function(
-    core,
-    bootstrap
-) {
-    if (guardian.isModernBrowser) {
-        bootstrap.go();
+require(['core', 'domReady!'], function(core) {
+
+    try {
+        require(['bootstraps/app'], function(app) {
+            if (guardian.isModernBrowser) {
+                app.go();
+            }
+        });
+    } catch(e) {
     }
 });
