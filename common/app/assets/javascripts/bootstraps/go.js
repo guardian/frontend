beba23fc9027e77923138da9c79ea2805eb72dbb
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-06-26 16:08:47 +0100

    Remove try catch

--- common/app/assets/javascripts/bootstraps/go.js
+++ common/app/assets/javascripts/bootstraps/go.js
@@ -1,12 +1,10 @@
 /*global guardian:true */
-require(['core', 'domReady!'], function(core) {
-
-    try {
-        require(['bootstraps/app'], function(app) {
-            if (guardian.isModernBrowser) {
-                app.go();
-            }
-        });
-    } catch(e) {
+require([
+    'core',
+    'bootstraps/app',
+    'domReady!'
+], function (core, bootstrap) {
+    if (guardian.isModernBrowser) {
+        bootstrap.go();
     }
 });
