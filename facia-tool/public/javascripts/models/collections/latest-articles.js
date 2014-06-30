d9cc506cb24f98403ecf89fafa774768665fcee6
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-06-30 18:27:44 +0100

    Switch for draft content

--- facia-tool/public/javascripts/models/collections/latest-articles.js
+++ facia-tool/public/javascripts/models/collections/latest-articles.js
@@ -88,6 +88,10 @@ define([
 
             opts = opts || {};
 
+            if (!vars.model.switches()['facia-tool-draft-content']) {
+                self.showingDrafts(false);
+            }
+
             clearTimeout(deBounced);
             deBounced = setTimeout(function(){
 
--- facia-tool/public/javascripts/modules/vars.js
+++ facia-tool/public/javascripts/modules/vars.js
@@ -39,8 +39,8 @@ define([
         capiBatchSize:         10,
 
         collectionsPollMs:     10000,
-        latestArticlesPollMs:  20000,
-        configSettingsPollMs:  5000,
+        latestArticlesPollMs:  30000,
+        configSettingsPollMs:  30000,
         cacheExpiryMs:         60000,
         sparksRefreshMs:       300000,
         pubTimeRefreshMs:      30000,
