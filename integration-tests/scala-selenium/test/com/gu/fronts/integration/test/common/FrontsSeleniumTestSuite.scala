69e286c9eef9e9f75ab60fcf3ab7242bf7758735
Shahin Kordasti <shahin.kordasti@guardian.co.uk>
2014-06-30 17:15:06 +0100

    Changed the way beta site is loaded

--- integration-tests/scala-selenium/test/com/gu/fronts/integration/test/common/FrontsSeleniumTestSuite.scala
+++ integration-tests/scala-selenium/test/com/gu/fronts/integration/test/common/FrontsSeleniumTestSuite.scala
@@ -1,15 +1,13 @@
 package com.gu.fronts.integration.test.common
 
-import org.openqa.selenium.WebDriver
-import org.scalatest.BeforeAndAfter
-import org.scalatest.FunSuite
-import org.scalatest.Matchers
+import org.openqa.selenium.{ Cookie, WebDriver }
+import org.scalatest.{ BeforeAndAfter, FunSuite, Matchers }
+
+import com.gu.fronts.integration.test.config.PropertyLoader
 import com.gu.fronts.integration.test.config.PropertyLoader.getProperty
 import com.gu.fronts.integration.test.config.WebdriverFactory
-import com.gu.fronts.integration.test.page.util.CustomPageFactory
 import com.gu.fronts.integration.test.page.nwfront.NetworkFrontPage
-import org.openqa.selenium.Cookie
-import com.gu.fronts.integration.test.config.PropertyLoader
+import com.gu.fronts.integration.test.page.util.CustomPageFactory
 
 abstract class FrontsSeleniumTestSuite extends FunSuite with BeforeAndAfter with Matchers {
 
@@ -18,10 +16,10 @@ abstract class FrontsSeleniumTestSuite extends FunSuite with BeforeAndAfter with
   def frontsBaseUrl: String = getProperty("fronts.base.url")
 
   lazy val pageFactory: CustomPageFactory = new CustomPageFactory()
-  
-  before{
+
+  before {
     webDriver = WebdriverFactory.getDefaultWebDriver
-    System.setProperty(PropertyLoader.PROP_FILE_PATH_ENV_KEY, System.getProperty("user.home")+"/fronts-test-override.properties");
+    System.setProperty(PropertyLoader.PROP_FILE_PATH_ENV_KEY, System.getProperty("user.home") + "/fronts-test-override.properties");
   }
 
   after {
@@ -30,10 +28,8 @@ abstract class FrontsSeleniumTestSuite extends FunSuite with BeforeAndAfter with
   }
 
   protected def openNetworkFrontPage(): NetworkFrontPage = {
-    // need to do this because selenium need to first go to a page of that domain before setting the cookie
-    webDriver.get(frontsBaseUrl + "/pagenotfound");
-    webDriver.manage().addCookie(betaSiteCookie);
-    webDriver.get(frontsBaseUrl)
+    // need to append this stuff to the url to make it switch to beta site
+    webDriver.get(buildBetaSiteUrl(frontsBaseUrl));
     pageFactory.initPage(webDriver, classOf[NetworkFrontPage])
   }
 
@@ -41,4 +37,15 @@ abstract class FrontsSeleniumTestSuite extends FunSuite with BeforeAndAfter with
     new Cookie("GU_VIEW", "responsive")
   }
 
+  /**
+   * This will append the request parameters needed to switch to beta site. However, for some reason, this does not work on
+   * localhost so had to make a check
+   */
+  private def buildBetaSiteUrl(frontsBaseUrl: String): String = {
+    if (frontsBaseUrl.startsWith("http://localhost")) {
+      frontsBaseUrl
+    } else {
+      frontsBaseUrl + "/preference/platform/mobile?page=" + frontsBaseUrl + "&view=mobile"
+    }
+  }
 }
--- integration-tests/scala-selenium/test/com/gu/fronts/integration/test/journeys/NetworkFrontSuite.scala
+++ integration-tests/scala-selenium/test/com/gu/fronts/integration/test/journeys/NetworkFrontSuite.scala
@@ -1,11 +1,16 @@
 package com.gu.fronts.integration.test.journeys
 
 import com.gu.fronts.integration.test.common.FrontsSeleniumTestSuite
+import com.gu.fronts.integration.test.page.nwfront.NetworkFrontPage
 
 class NetworkFrontSuite extends FrontsSeleniumTestSuite {
 
-  test("should load network front page") {
+  test("should click through the gallery overlay") {
     val nwFront = openNetworkFrontPage()
     nwFront.isDisplayed should be(true)
+    
+    val picContainer = nwFront.containers.containerWithId(NetworkFrontPage.IN_PICTURES_CONTAINER_ID)
+    picContainer.isDisplayed should be(true)
+    picContainer.galleryAt(0).isDisplayed should be(true)
   }
 }
