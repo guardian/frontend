e7578c435e38cec1819f4b7067e054ed32583078
tudorraul <tudorraul@gmail.com>
2014-07-01 16:56:05 +0100

    refactor and add tests

--- common/app/dev/HttpSwitch.scala
+++ common/app/dev/HttpSwitch.scala
@@ -3,6 +3,10 @@ package dev
 import play.api.mvc.RequestHeader
 import conf.Configuration
 
+trait EnvConfig {
+  protected def isNonProd = Configuration.environment.isNonProd
+}
+
 /*
   Switch wrapper that enables it to be turned on/off in non production environments based on query string parameters
   Example
@@ -12,41 +16,41 @@ import conf.Configuration
     HttpSwitch(DemoSwitch).isSwitchedOn // will yield true
     DemoSwitch.isSwitchedOn // will yield false
  */
-case class HttpSwitch(switch :conf.Switch)(implicit val request: RequestHeader) {
+case class HttpSwitch(switch :conf.Switch) extends EnvConfig {
   import HttpSwitch._
 
-  def isSwitchedOn: Boolean =
-    if(Configuration.environment.isNonProd)
-      if(onSwitches.split(",").contains(switch.name)) true
-      else if(offSwitches.split(",").contains(switch.name)) false
+  def isSwitchedOn(implicit request: RequestHeader): Boolean =
+    if(isNonProd)
+      if(onSwitches.exists(_.split(",").contains(switch.name))) true
+      else if(offSwitches.exists(_.split(",").contains(switch.name))) false
       else switch.isSwitchedOn
     else switch.isSwitchedOn
 
-  def isSwitchedOff: Boolean = !isSwitchedOn
+  def isSwitchedOff(implicit request: RequestHeader): Boolean = !isSwitchedOn
 }
 
-object HttpSwitch {
+trait HttpSwitchParameters extends EnvConfig{
   private val switchesOn = "switchesOn"
   private val switchesOff = "switchesOff"
 
   def queryString(url: String)(implicit request: RequestHeader) =
-    if(Configuration.environment.isNonProd) url
-    else {
-      lazy val on = onSwitches
-      lazy val off = offSwitches
-      url +
-        (if(on.nonEmpty || off.nonEmpty)
-          (if(url.contains("?")) "&" else "?") +
-            List(
-              if(on.nonEmpty){switchesOn + "=" + on} else {""},
-              if(off.nonEmpty){switchesOff + "=" + off} else {""}
-            ).filter(_.nonEmpty).mkString("&")
-        else "")
+    if(isNonProd) {
+      val on = onSwitches.map{switchesOn + "=" + _}
+      val off = offSwitches.map{switchesOff + "=" + _}
+      if(on.isDefined || off.isDefined){
+        val separator = if(url.contains("?")) "&" else "?"
+        url + separator + List(on, off).filter(_.isDefined).map{_.get}.mkString("&")
+      }
+      else url
     }
+    else url
 
   def onSwitches(implicit request: RequestHeader) = queryParameter(switchesOn)
 
   def offSwitches(implicit request: RequestHeader) = queryParameter(switchesOff)
 
-  private def queryParameter(name: String)(implicit request: RequestHeader): String = request.queryString.getOrElse(name, List()).mkString(",")
+  private def queryParameter(name: String)(implicit request: RequestHeader): Option[String] =
+    request.queryString.get(name).map{_.mkString(",")}
 }
+
+object HttpSwitch extends HttpSwitchParameters
