329361a3beae0d139df3a8db1586752bb07a22eb
Shahin Kordasti <shahin.kordasti@guardian.co.uk>
2014-07-02 16:12:12 +0100

    Added test class and method to sauce labs job name

--- integration-tests/webdriver/src/main/java/com/gu/test/WebDriverFactory.java
+++ integration-tests/webdriver/src/main/java/com/gu/test/WebDriverFactory.java
@@ -1,6 +1,7 @@
 package com.gu.test;
 
 import static com.gu.test.PropertyLoader.getProperty;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -18,11 +19,18 @@ public class WebDriverFactory {
     public static final String OS_VERSION = "Windows 7";
 
     public static WebDriver createWebDriver() throws Exception {
+        return createWebDriver(null);
+    }
+
+    public static WebDriver createWebDriver(String jobName) throws Exception {
 
         // Choose the browser, version, and platform to test
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("version", BROWSER_VERSION);
         capabilities.setCapability("platform", OS_VERSION);
+        if (isNotBlank(jobName)) {
+            capabilities.setCapability("name", jobName);
+        }
 
         String sauceLabsUrl = getProperty("saucelabs.remotedriver.url");
         // Create the connection to Sauce Labs to run the tests
