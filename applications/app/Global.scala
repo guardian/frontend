4a646bc37a52176dda66f6df83c4365be1f2067c
Ken Lim <kennethlimfc@gmail.com>
2014-07-02 11:16:59 +0100

    SurgingAgent to other Apps and Facia too

--- applications/app/Global.scala
+++ applications/app/Global.scala
@@ -2,11 +2,13 @@ import common.CloudWatchApplicationMetrics
 import conf.{Management, Filters}
 import dev.DevParametersLifecycle
 import dfp.DfpAgentLifecycle
+import ophan.SurgingContentAgentLifecycle
 import play.api.mvc.WithFilters
 
 object Global extends WithFilters(Filters.common: _*)
                       with DevParametersLifecycle
                       with CloudWatchApplicationMetrics
-                      with DfpAgentLifecycle {
+                      with DfpAgentLifecycle
+                      with SurgingContentAgentLifecycle{
   override lazy val applicationName = Management.applicationName
 }
