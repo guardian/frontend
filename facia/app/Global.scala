67e59be4fcf31d57380d56c7bbe4a62b3d849dbe
Francis Carr <francis.carr@guardian.co.uk>
2014-07-03 11:05:19 +0100

    Remove metrics from facia

--- facia/app/Global.scala
+++ facia/app/Global.scala
@@ -18,9 +18,7 @@ with SurgingContentAgentLifecycle {
   override def applicationMetrics: Map[String, Double] = super.applicationMetrics ++ Map(
     ("s3-authorization-error", FaciaMetrics.S3AuthorizationError.getAndReset.toDouble),
     ("json-parsing-error", FaciaMetrics.JsonParsingErrorCount.getAndReset.toDouble),
-    ("elastic-content-api-calls", ContentApiMetrics.ElasticHttpTimingMetric.getAndReset.toDouble),
     ("solr-content-api-calls", ContentApiMetrics.HttpTimingMetric.getAndReset.toDouble),
-    ("elastic-content-api-timeouts", ContentApiMetrics.ElasticHttpTimeoutCountMetric.getAndReset.toDouble),
     ("solr-content-api-timeouts", ContentApiMetrics.HttpTimeoutCountMetric.getAndReset.toDouble),
     ("content-api-client-parse-exceptions", ContentApiMetrics.ContentApiJsonParseExceptionMetric.getAndReset.toDouble),
     ("content-api-client-mapping-exceptions", ContentApiMetrics.ContentApiJsonMappingExceptionMetric.getAndReset.toDouble),
