3b208b60c188c13e32caa5515e626af940b96185
Shahin Kordasti <shahin.kordasti@guardian.co.uk>
2014-07-03 13:58:41 +0100

    refactored test

--- integration-tests/scala-selenium/test/com/gu/fronts/integration/test/journeys/NetworkFrontJourneys.scala
+++ integration-tests/scala-selenium/test/com/gu/fronts/integration/test/journeys/NetworkFrontJourneys.scala
@@ -2,21 +2,18 @@ package com.gu.fronts.integration.test.journeys
 
 import com.gu.fronts.integration.test.common.FrontsSeleniumTestSuite
 import com.gu.fronts.integration.test.page.nwfront.NetworkFrontPage.IN_PICTURES_CONTAINER_ID
+import com.gu.fronts.integration.test.page.nwfront.NetworkFrontPage
+import com.gu.fronts.integration.test.page.common.FaciaContainer
+import com.gu.fronts.integration.test.page.common.GalleryOverlay
 
 class NetworkFrontJourneys extends FrontsSeleniumTestSuite {
 
   test("click through the picture gallery overlay journey") {
-    val nwFront = openNetworkFrontPage()
-    nwFront.isDisplayed should be(true)
-
+    val nwFront = goToNWFrontPage
     val picContainer = nwFront.containers.containerWithId(IN_PICTURES_CONTAINER_ID)
     picContainer.isDisplayed should be(true)
 
-    //select the first valid picture gallery item
-    val firstGallery = picContainer.firstGalleryItem
-    firstGallery.isDisplayed should be(true)
-    val picOverlay = firstGallery.clickPicture
-    picOverlay.isDisplayed should be(true)
+    val picOverlay = clickFirstValidPictureOn(picContainer)
 
     //check that nw front is still in the background
     nwFront.isDisplayed should be(true)
@@ -24,15 +21,30 @@ class NetworkFrontJourneys extends FrontsSeleniumTestSuite {
     picOverlay.clickGalleryGridMode
     picOverlay.clickGalleryFullMode
 
+    val networkFrontPageAfterClose = picOverlay.close();
+    networkFrontPageAfterClose.isDisplayed should be(true)
+  }
+
+  private def goToNWFrontPage: NetworkFrontPage = {
+    val nwFront = openNetworkFrontPage()
+    nwFront.isDisplayed should be(true)
+    nwFront
+  }
+
+  private def clickFirstValidPictureOn(picContainer: FaciaContainer) = {
+    val firstGallery = picContainer.firstGalleryItem
+    firstGallery.isDisplayed should be(true)
+    val picOverlay = firstGallery.clickPicture
+    picOverlay.isDisplayed should be(true)
+    picOverlay
+  }
+
+  private def clickFirstValidPictureOn(picOverlay: GalleryOverlay) = {
     val displayedImageBefore = picOverlay.getDisplayedImage();
     picOverlay.clickNextGallery();
     // give some time for next image to become active
     Thread.sleep(200);
     val displayedImageAfter = picOverlay.getDisplayedImage();
-
-    displayedImageBefore should not be(displayedImageAfter)
-
-    val networkFrontPageAfterClose = picOverlay.close();
-    networkFrontPageAfterClose.isDisplayed should be(true)
+    displayedImageBefore should not be (displayedImageAfter)
   }
 }
