6d49fef43d286b40a15e4895b112febd665a3815
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-07-03 12:01:42 +0100

    AND-ify the search term

--- facia-tool/public/javascripts/models/collections/latest-articles.js
+++ facia-tool/public/javascripts/models/collections/latest-articles.js
@@ -111,7 +111,7 @@ define([
                     url += '&content-set=' + (self.showingDrafts() ? 'preview&use-date=last-modified' : 'web-live');
                     url += '&page-size=' + (vars.CONST.searchPageSize || 25);
                     url += '&page=' + self.page();
-                    url += self.term() ? '&q=' + encodeURIComponent(self.term()) : '';
+                    url += self.term() ? '&q=' + encodeURIComponent(self.term().trim().replace(/ +/g,' AND ')) : '';
                     url += '&' + self.filterType().param + '=' + encodeURIComponent(self.filter());
                     propName = 'results';
                 }
