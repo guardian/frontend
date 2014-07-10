41cd345d78ea503fb1d595662d8bef36ac313823
shakor <shahin.kordasti@guardian.co.uk>
2014-07-10 13:53:45 +0100

    cleaning up missing imports

--- integration-tests/scala-fw-tests/src/main/scala/com/gu/fronts/integration/test/pages/article/ArticlePage.scala
+++ integration-tests/scala-fw-tests/src/main/scala/com/gu/fronts/integration/test/pages/article/ArticlePage.scala
@@ -3,7 +3,6 @@ package com.gu.fronts.integration.test.page.nwfront
 import org.openqa.selenium.{ WebDriver, WebElement }
 
 import com.gu.fronts.integration.test.config.PropertyLoader._
-import com.gu.fronts.integration.test.page.common.FaciaContainer
 import com.gu.fronts.integration.test.pages.common.FrontsParentPage
 
 class ArticlePage(implicit var driver: WebDriver) extends FrontsParentPage() {
--- integration-tests/scala-fw-tests/src/main/scala/com/gu/fronts/integration/test/pages/common/MostPopularModule.scala
+++ integration-tests/scala-fw-tests/src/main/scala/com/gu/fronts/integration/test/pages/common/MostPopularModule.scala
@@ -1,9 +1,8 @@
 package com.gu.fronts.integration.test.page.nwfront
 
-import org.openqa.selenium.{ WebDriver, WebElement }
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 
-import com.gu.fronts.integration.test.config.PropertyLoader._
-import com.gu.fronts.integration.test.page.common.FaciaContainer
 import com.gu.fronts.integration.test.pages.common.FrontsParentPage
 
 class MostPopularModule(implicit var driver: WebDriver) extends FrontsParentPage() {
