3b45c8de1b81521262843fb656876b7a6b63583a
tudorraul <tudorraul@gmail.com>
2014-07-15 11:56:29 +0100

    add more menu

--- common/app/assets/javascripts/bootstraps/common.js
+++ common/app/assets/javascripts/bootstraps/common.js
@@ -23,6 +23,7 @@ define([
     'common/modules/navigation/profile',
     'common/modules/navigation/sections',
     'common/modules/navigation/search',
+    'common/modules/navigation/servicesNav',
     'common/modules/navigation/newNavigation',
     'common/modules/ui/tabs',
     'common/modules/ui/toggles',
@@ -70,6 +71,7 @@ define([
     Profile,
     Sections,
     Search,
+    ServicesNav,
     newNavigation,
 
     Tabs,
@@ -119,6 +121,8 @@ define([
             }
 
             search.init(header);
+
+            ServicesNav.init(config);
         },
 
         initialiseNavigation: function (config) {
