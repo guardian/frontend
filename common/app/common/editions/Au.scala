49de36446965c5f4baa2164e979c568b76be149d
tudorraul <tudorraul@gmail.com>
2014-07-17 13:40:17 +0100

    restructure new navigation so that it displays different items in top bar vs global nv

--- common/app/common/editions/Au.scala
+++ common/app/common/editions/Au.scala
@@ -13,7 +13,7 @@ object Au extends Edition(id = "AU", displayName = "Australia edition", DateTime
 
   implicit val AU = Au
 
-  def navigation: Seq[NavItem] = Seq(
+  override val navigation: Seq[NavItem] = Seq(
     NavItem(home),
     NavItem(australia),
     NavItem(world, Seq(uk, us, asia, europeNews, americas, africa, middleEast)),
@@ -23,10 +23,26 @@ object Au extends Edition(id = "AU", displayName = "Australia edition", DateTime
     NavItem(technology, Seq(games)),
     NavItem(culture, Seq(film, music, books, televisionAndRadio , artanddesign, stage)),
     NavItem(lifeandstyle, Seq(foodanddrink, healthandwellbeing, loveAndSex, family, women, fashion)),
-    NavItem(economy, Seq(markets, companies, money, media)),
+    NavItem(economy, Seq(markets, companies, money)),
     NavItem(travel, Seq(australasiaTravel, asiaTravel, uktravel, europetravel, usTravel)),
     NavItem(science),
     NavItem(environment, Seq(cities, globalDevelopment)),
-    NavItem(education, Seq(students))
+    NavItem(media)
+  )
+
+  override val briefNav: Seq[NavItem] = Seq(
+    NavItem(home),
+    NavItem(australia),
+    NavItem(world, Seq(uk, us, asia, europeNews, americas, africa, middleEast)),
+    NavItem(cif),
+    NavItem(sport, Seq(australiaSport, football, cricket, rugbyunion, rugbyLeague , tennis, cycling, boxing, afl, nrl)),
+    NavItem(football, aLeague :: footballNav.toList),
+    NavItem(technology, Seq(games)),
+    NavItem(culture, Seq(film, music, books, televisionAndRadio , artanddesign, stage)),
+    NavItem(lifeandstyle, Seq(foodanddrink, healthandwellbeing, loveAndSex, family, women, fashion)),
+    NavItem(economy, Seq(markets, companies, money)),
+    NavItem(travel, Seq(australasiaTravel, asiaTravel, uktravel, europetravel, usTravel)),
+    NavItem(science),
+    NavItem(environment, Seq(cities, globalDevelopment))
   )
 }
