f190636301b46225702f375af0acf9d67f2e7794
shakor <shahin.kordasti@guardian.co.uk>
2014-07-18 11:16:56 +0100

    updated the test and related files to fully use version 1.16 of the scala fw

--- integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/SeleniumTest.scala
+++ integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/SeleniumTest.scala
@@ -2,17 +2,13 @@ package com.gu.integration.test
 
 import org.openqa.selenium.WebDriver
 import org.scalatest.Matchers
-
 import com.gu.automation.core.WebDriverFeatureSpec
-import com.gu.integration.test.config.WebdriverFactory
+import com.gu.automation.core.WebDriverFactory
+import com.gu.integration.test.config.WebdriverInitialiser.initWebDriver
 
 abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers {
 
   override protected def startDriver(testName: String): WebDriver = {
-    WebdriverFactory.createWebDriver(testClassAndMethod(testName))
-  }
-
-  private def testClassAndMethod(testName: String): String = {
-    getClass().getSimpleName() + "." + testName
+   initWebDriver(WebDriverFactory.newInstance(s"${getClass().getSimpleName()}.$testName"))
   }
 }
