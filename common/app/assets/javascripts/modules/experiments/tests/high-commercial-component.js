1c4afea2dedb979016172c19a50bdc7a768c8c07
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-07-21 18:19:11 +0100

    Adding ab test to dfp targeting

--- common/app/assets/javascripts/modules/experiments/tests/high-commercial-component.js
+++ common/app/assets/javascripts/modules/experiments/tests/high-commercial-component.js
@@ -5,7 +5,7 @@ define(function () {
         this.start = '2014-07-09';
         this.expiry = '2015-07-09';
         this.author = 'Darren Hurley';
-        this.description = 'Increase the size of inline1 ad slot to 300x250 for mobile users';
+        this.description = 'Using as a DFP targeting hook to test different high relevance components';
         this.audience = 0.1;
         this.audienceOffset = 0;
         this.successMeasure = '';
