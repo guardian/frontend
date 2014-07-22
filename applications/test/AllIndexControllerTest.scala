3a8212bb12c5302824bf4a283603e3088dce29d6
Grant Klopper <grant.klopper@theguardian.com>
2014-07-22 14:48:57 +0100

    Fixed a small bug with timezones

--- applications/test/AllIndexControllerTest.scala
+++ applications/test/AllIndexControllerTest.scala
@@ -1,5 +1,6 @@
 package test
 
+import org.joda.time.DateTimeZone
 import org.scalatest.{Matchers, FlatSpec}
 import play.api.test.Helpers._
 
@@ -7,6 +8,7 @@ class AllIndexControllerTest extends FlatSpec with Matchers {
 
   private val PermanentRedirect = 301
   private val TemporaryRedirect = 302
+  private val OK = 200
 
   it should "redirect dated tag pages to the equivalent /all page" in Fake {
     val result = controllers.AllIndexController.on("football/series/thefiver/2014/jan/23")(TestRequest())
@@ -30,6 +32,18 @@ class AllIndexControllerTest extends FlatSpec with Matchers {
     val result = controllers.AllIndexController.allOn("sport/cycling", "25", "dec", "2013")(TestRequest())
     status(result) should be(TemporaryRedirect)
     header("Location", result).head should endWith ("/sport/cycling/2013/dec/23/all")
+  }
+
+  it should "correctly parse the date" in Fake {
+    //this would only error in UTC
+    val oldTimezone = DateTimeZone.getDefault
+    DateTimeZone.setDefault(DateTimeZone.UTC)
+    try {
+      val result = controllers.AllIndexController.allOn("sport/surfing", "12", "jul", "2014")(TestRequest())
+      status(result) should be(OK)
+    } finally {
+      DateTimeZone.setDefault(oldTimezone)
+    }
   }
 
 }
