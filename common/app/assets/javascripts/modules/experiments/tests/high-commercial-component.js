c41372dd9845b054a55bf6d9e9ec75cec677eede
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-07-22 15:44:50 +0100

    add switch for a/b test

--- common/app/assets/javascripts/modules/experiments/tests/high-commercial-component.js
+++ common/app/assets/javascripts/modules/experiments/tests/high-commercial-component.js
@@ -3,6 +3,7 @@ define(function () {
     return function () {
         this.id = 'HighCommercialComponent';
         this.start = '2014-07-22';
+        // far future expiration, only really using the test to bucket users, which we can use for targeting in dfp
         this.expiry = '2015-07-09';
         this.author = 'Darren Hurley';
         this.description = 'Using as a DFP targeting hook to test different high relevance components';
