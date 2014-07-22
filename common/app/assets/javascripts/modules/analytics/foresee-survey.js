3007ca577e9c7fa63e8114ea6fba79e0a69a3482
Matt Osborn <matthew.osborn@theguardian.com>
2014-07-22 14:40:57 +0100

    renaming test cookie

--- common/app/assets/javascripts/modules/analytics/foresee-survey.js
+++ common/app/assets/javascripts/modules/analytics/foresee-survey.js
@@ -6,7 +6,7 @@ define(['common/utils/cookies'], function(Cookie) {
             hasForcedOptIn = /forceForesee/.test(location.hash);
 
         // the Foresee code is large, we only want to load it in when necessary.
-        if (!Cookie.get('gu.testagent') && (sample || hasForcedOptIn)) {
+        if (!Cookie.get('gu.test') && (sample || hasForcedOptIn)) {
             require(['js!foresee'], function() {});
         }
     }
