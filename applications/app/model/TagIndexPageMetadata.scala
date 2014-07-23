a951e9c4edf34a39c1edef61510bfe5c529090dd
Robert Berry <robert.berry@theguardian.com>
2014-07-23 16:57:57 +0100

    Use tag type as section name in index page metadata

--- applications/app/model/TagIndexPageMetadata.scala
+++ applications/app/model/TagIndexPageMetadata.scala
@@ -3,10 +3,10 @@ package model
 class TagIndexPageMetaData(val tagType: String, val page: Char) extends MetaData {
   override def id: String = s"/index/${tagType}s/$page"
 
-  override def section: String = "info"
+  override def section: String = tagType + "s"
 
   /** TODO figure out what ought to go here */
   override def analyticsName: String = id
 
-  override def webTitle: String = s"${tagType.capitalize}s, ${page.toUpper}"
+  override def webTitle: String = page.toUpper.toString
 }
