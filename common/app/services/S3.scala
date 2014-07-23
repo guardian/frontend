5e744fb538043416bc2363fdd3fe8f9426061a62
Francis Carr <francis.carr@guardian.co.uk>
2014-07-23 15:50:06 +0100

    Move over to the new identity type; com.gu.googleauth.UserIdentity

--- common/app/services/S3.scala
+++ common/app/services/S3.scala
@@ -14,8 +14,8 @@ import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 import sun.misc.BASE64Encoder
 import com.amazonaws.auth.AWSSessionCredentials
-import controllers.Identity
 import common.S3Metrics.S3ClientExceptionsMetric
+import com.gu.googleauth.UserIdentity
 
 trait S3 extends Logging {
 
@@ -120,7 +120,7 @@ object S3FrontsApi extends S3 {
   def putBlock(id: String, json: String) =
     putPublic(s"$location/collection/$id/collection.json", json, "application/json")
 
-  def archive(id: String, json: String, identity: Identity) = {
+  def archive(id: String, json: String, identity: UserIdentity) = {
     val now = DateTime.now
     putPrivate(s"$location/history/collection/$id/${now.year.get}/${"%02d".format(now.monthOfYear.get)}/${"%02d".format(now.dayOfMonth.get)}/${now}.${identity.email}.json", json, "application/json")
   }
@@ -128,9 +128,9 @@ object S3FrontsApi extends S3 {
   def putMasterConfig(json: String) =
     putPublic(s"$location/config/config.json", json, "application/json")
 
-  def archiveMasterConfig(json: String, identity: Identity) = {
+  def archiveMasterConfig(json: String, identity: UserIdentity) = {
     val now = DateTime.now
-    putPublic(s"${location}/history/config/${now.year.get}/${"%02d".format(now.monthOfYear.get)}/${"%02d".format(now.dayOfMonth.get)}/${now}.${identity.email}.json", json, "application/json")
+    putPublic(s"$location/history/config/${now.year.get}/${"%02d".format(now.monthOfYear.get)}/${"%02d".format(now.dayOfMonth.get)}/${now}.${identity.email}.json", json, "application/json")
   }
 
   private def getListing(prefix: String, dropText: String): List[String] = {
