6df211c72cb50d7b80361f5cce95b95b218d1b50
Gwyn Lockett <gwyn.lockett@theguardian.com>
2014-07-29 11:25:58 +0100

    Removed autor details on PageSupport.scala

--- integration-tests/scala-discussion-tests/src/test/scala/com/gu/discussion/page/PageSupport.scala
+++ integration-tests/scala-discussion-tests/src/test/scala/com/gu/discussion/page/PageSupport.scala
@@ -5,8 +5,6 @@ import org.openqa.selenium.By._
 import org.openqa.selenium.{WebDriver}
 import org.openqa.selenium.support.ui.{ExpectedCondition}
 
-
-/** Created by glockett.  */
 class PageSupport(implicit driver: WebDriver) {
 
   private def commentRootById(commentId: String) = driver.findElement(id(s"$commentId"))
