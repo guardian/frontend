01b8c91e444be916123dd1bfdf76e2e9a46ab988
rich-nguyen <richnoin@gmail.com>
2014-07-31 00:20:07 +0100

    Applications test port 9002

--- applications/app/conf/context.scala
+++ applications/app/conf/context.scala
@@ -1,10 +1,10 @@
 package conf
 
 import common.Metrics
+import play.api.{Mode, Play}
 import com.gu.management.{ PropertiesPage, StatusPage, ManifestPage }
 import com.gu.management.play.{ Management => GuManagement }
 import com.gu.management.logback.LogbackLevelPage
-
 
 object HealthCheck extends AllGoodHealthcheckController(
   "/books",
@@ -12,7 +12,16 @@ object HealthCheck extends AllGoodHealthcheckController(
   "/travel/gallery/2012/nov/20/st-petersburg-pushkin-museum",
   "/travel/gallery/2012/nov/20/st-petersburg-pushkin-museum?index=2",
   "/world/video/2012/nov/20/australian-fake-bomber-sentenced-sydney-teenager-video"
-)
+) {
+  val testPort = 9002
+
+  override lazy val port = {
+    Play.current.mode match {
+      case Mode.Test => testPort
+      case _ => 9000
+    }
+  }
+}
 
 object Management extends GuManagement {
   val applicationName = "frontend-applications"
--- applications/test/package.scala
+++ applications/test/package.scala
@@ -5,9 +5,7 @@ import collection.JavaConversions._
 
 object `package` {
 
-  object HtmlUnit extends EditionalisedHtmlUnit
-
-  object Fake extends FakeApp
+  object HtmlUnit extends EditionalisedHtmlUnit(conf.HealthCheck.testPort.toString)
 
   implicit class ListString2FirstNonEmpty(list: JList[String]) {
     lazy val firstNonEmpty: Option[String] = list find { !_.isEmpty }
