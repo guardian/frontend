9344432c7c677de9f5e676492caa98ad01329648
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-07-30 18:11:51 +0100

    Article tests use port 9001

--- article/test/package.scala
+++ article/test/package.scala
@@ -1,13 +1,12 @@
 package test
 
-import conf.Configuration
 import org.scalatest.Tag
 
 object ArticleComponents extends Tag("article components")
 
 object `package` {
 
-  object HtmlUnit extends EditionalisedHtmlUnit
+  object HtmlUnit extends EditionalisedHtmlUnit("9001")
 
-  object Fake extends FakeApp
+  object Fake extends FakeApp("9001")
 }
