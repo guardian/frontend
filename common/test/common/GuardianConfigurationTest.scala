1baea3f38af0badbbb89689d3f21e245e5172b98
Francis Carr <francis.carr@guardian.co.uk>
2014-07-30 17:02:59 +0100

    Update GuardianConfigurationTest hash

--- common/test/common/GuardianConfigurationTest.scala
+++ common/test/common/GuardianConfigurationTest.scala
@@ -22,7 +22,7 @@ class GuardianConfigurationTest extends FlatSpec with Matchers {
     // are any required changes, update the hash below, and off you go.
 
     withClue("Look at comment in GuardianConfigurationTest.scala!") {
-      hash should be("7f3b50237f1795563accfd7a216a81ff")
+      hash should be("a1a739e7e464bfabfd32abf353b7bc10")
     }
   }
 }
