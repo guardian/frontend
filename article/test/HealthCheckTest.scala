1e2b2078ee083652f5dca7c269ad65ec1c9e8014
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-07-31 10:36:39 +0100

    Remove article management healthcheck test

--- article/test/HealthCheckTest.scala
+++ article/test/HealthCheckTest.scala
@@ -9,15 +9,8 @@ class HealthCheckTest extends FlatSpec with Matchers {
 
   import play.api.Play.current
 
-  ignore should "pass" in HtmlWithManagement("/world/2014/feb/05/libya-says-chemical-weapons-destroyed"){ browser =>
-
-    Await.result(WS.url("http://localhost:18080/management/healthcheck").get(), 10.seconds).status should be (200)
-  }
-
-  "Healthchecks" should "pass" in HtmlWithManagement("/world/2014/feb/05/libya-says-chemical-weapons-destroyed"){ browser =>
+  "Healthchecks" should "pass" in HtmlUnit("/world/2014/feb/05/libya-says-chemical-weapons-destroyed"){ browser =>
 
     Await.result(WS.url(s"http://localhost:${HtmlUnit.port}/_healthcheck").get(), 10.seconds).status should be (200)
   }
-
-  private object HtmlWithManagement extends EditionalisedHtmlUnit(HtmlUnit.port)
 }
