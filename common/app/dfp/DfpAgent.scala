5862dd632d04bec9d7f1ba49fed325e431de84b5
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-07-31 18:12:36 +0100

    use tag's isKeyword and isSeries

--- common/app/dfp/DfpAgent.scala
+++ common/app/dfp/DfpAgent.scala
@@ -31,11 +31,9 @@ trait DfpAgent {
     containerSponsoredTag(config, p).isDefined
   }
 
-  private def getKeywordTags(tags: Seq[Tag]): Seq[Tag] = tags filter(_.tagType == "keyword")
+  private def getKeywordTags(tags: Seq[Tag]): Seq[Tag] = tags filter(_.isKeyword)
 
-  private def getKeywordOrSeriesTags(tags: Seq[Tag]): Seq[Tag] = tags filter{ tag =>
-    Seq("keyword", "series").contains(tag.tagType)
-  }
+  private def getKeywordOrSeriesTags(tags: Seq[Tag]): Seq[Tag] = tags.filter(t => t.isSeries || t.isKeyword)
 
   def isSponsored(tags: Seq[Tag]): Boolean = getKeywordOrSeriesTags(tags) exists (tag => isSponsored(tag.id))
   def isSponsored(tagId: String): Boolean = sponsorships exists (_.hasTag(tagId))
