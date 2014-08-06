b7aeb82f11b04d69aed1c11dced8dcb656c0f867
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-08-06 18:00:56 +0100

    franken-ponent endpoint

---
+++ commercial/app/controllers/commercial/Multi.scala
@@ -0,0 +1,41 @@
+package controllers.commercial
+
+import common.ExecutionContexts
+import model.commercial.books.{BestsellersAgent, Book}
+import model.{Cached, NoCache}
+import performance.MemcachedAction
+import play.api.mvc._
+import play.twirl.api.Html
+
+import scala.concurrent.Future
+import model.commercial.soulmates.{SoulmatesAggregatingAgent, Member}
+import model.commercial.Ad
+import model.commercial.travel.TravelOffersAgent
+import model.commercial.jobs.JobsAgent
+import model.commercial.masterclasses.MasterClassAgent
+
+object Multi extends Controller with ExecutionContexts with implicits.Collections {
+
+  def renderMulti() = MemcachedAction { implicit request =>
+
+    Future.successful {
+      val ads: Seq[Ad] = request.queryString("c").map { c =>
+        c match {
+          case "jobs"          => JobsAgent.adsTargetedAt(segment)
+          case "books"         => BestsellersAgent.adsTargetedAt(segment)
+          case "travel"        => TravelOffersAgent.adsTargetedAt(segment)
+          case "masterclasses" => MasterClassAgent.adsTargetedAt(segment)
+        }
+      }.flatten
+
+      ads match {
+        case Nil => NoCache(jsonFormat.nilResult)
+        case ads => Cached(componentMaxAge) {
+          htmlFormat.result(views.html.multi(ads))
+        }
+      }
+    }
+
+  }
+
+}
