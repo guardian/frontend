e7fb7d8cd0d5003a445738a5f4c90bb01ce25490
shakor <shahin.kordasti@guardian.co.uk>
2014-08-07 10:46:19 +0100

    Changed the way to get current dir

--- integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/features/util/IntegrationSeleniumTestSuite.scala
+++ integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/features/util/IntegrationSeleniumTestSuite.scala
@@ -1,9 +1,11 @@
 package com.gu.integration.test.features.util
 
+
 import com.gu.integration.test.SeleniumTestSuite
+import com.gu.integration.test.util.FileUtil._
 
 trait IntegrationSeleniumTestSuite extends SeleniumTestSuite {
   //this is needed because of the sbt multimodule nature of this test project
   //without it, it will try to load the local.conf from the root folder
-  System.setProperty("local.conf.loc", s"${sys.props.get("user.dir").getOrElse("")}/scala-fw-tests/local.conf")
+  System.setProperty("local.conf.loc", s"${currentPath()}/scala-fw-tests/local.conf")
 }
