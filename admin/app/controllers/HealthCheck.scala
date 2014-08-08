a430bdf5ff5dca2a7556434ddafd7b7ddad9e4d5
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-08-08 17:34:29 +0100

    Move object healthcheck to class for atomic health checking

--- admin/app/controllers/HealthCheck.scala
+++ admin/app/controllers/HealthCheck.scala
@@ -1,18 +1,11 @@
 package controllers
 
-import java.util.concurrent.atomic.AtomicBoolean
-
 import conf.AllGoodHealthcheckController
-import play.api.{Mode, Play}
 import play.api.mvc.Action
 
 object HealthCheck extends AllGoodHealthcheckController(9001, "/login") {
 
-  private val status = new AtomicBoolean(true)
-  def isOK = status.get
-  def setUnhealthy(){ status.set(false) }
-
-  override def healthcheck() = if (!isOK) {
+  override def healthcheck() = if (!isOk) {
     Action(InternalServerError("JAXP00010001"))
   } else {
     super.healthcheck()
--- article/app/conf/context.scala
+++ article/app/conf/context.scala
@@ -1,21 +1,3 @@
 package conf
 
-import java.util.concurrent.atomic.AtomicBoolean
-
-import play.api.mvc.{Action, Results}
-
-
-object HealthCheck extends AllGoodHealthcheckController(9004, "/world/2012/sep/11/barcelona-march-catalan-independence") with Results {
-
-  // this is for an "offline" healthcheck that the CDN hits
-  private val status = new AtomicBoolean(false)
-  def break() = status.set(false)
-
-  override def healthcheck() = Action.async{ request =>
-    val result = super.healthcheck()(request)
-    result.foreach(r => status.set(r.header.status == 200))
-    result
-  }
-
-  def isOk = status.get
-}
+object HealthCheck extends AllGoodHealthcheckController(9004, "/world/2012/sep/11/barcelona-march-catalan-independence")
