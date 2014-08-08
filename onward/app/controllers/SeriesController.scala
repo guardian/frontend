4013370ab237bf0a83ad150a49f668def339aa0d
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-08-08 16:17:40 +0100

    Add tag description to series container

--- onward/app/controllers/SeriesController.scala
+++ onward/app/controllers/SeriesController.scala
@@ -49,7 +49,7 @@ object SeriesController extends Controller with Logging with Paging with Executi
 
   private def renderSeriesTrails(series: Series)(implicit request: RequestHeader) = {
     implicit val config = Config(id = series.tag.webTitle, href = Some(series.id), displayName = Some("Series:") )
-    val response = () => views.html.fragments.containers.series(Collection(series.trails.take(7)), SeriesContainer(), 0)
+    val response = () => views.html.fragments.containers.series(Collection(series.trails.take(7)), SeriesContainer(), 0, series.tag.description)
     renderFormat(response, response, 1)
   }
 }
