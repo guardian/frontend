b5332f5534a7123a8295a92cb24e3fd658f2456f
shakor <shahin.kordasti@guardian.co.uk>
2014-08-08 15:29:52 +0100

    Changed class to trait

--- integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/features/util/IntegrationSeleniumTestSuite.scala
+++ integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/features/util/IntegrationSeleniumTestSuite.scala
@@ -5,7 +5,7 @@ import com.gu.integration.test.SeleniumTestSuite
 import com.gu.integration.test.util.FileUtil._
 import com.gu.integration.test.util.PropertyUtil._
 
-class IntegrationSeleniumTestSuite extends SeleniumTestSuite {
+trait IntegrationSeleniumTestSuite extends SeleniumTestSuite {
   //only used because of the need to have a static init block, which the companion object below acts as
   IntegrationSeleniumTestSuite
 }
