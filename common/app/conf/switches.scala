0d1083970de19685cb8609f4cc9112ec6a725fc7
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-08-08 14:57:12 +0100

    correct label for commercial tags

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -133,25 +133,20 @@ object Switches extends Collections {
 
   // Commercial Tags
 
-  val AudienceScienceSwitch = Switch("Ad Targeting", "audience-science",
+  val AudienceScienceSwitch = Switch("Commercial Tags", "audience-science",
     "If this switch is on, Audience Science segments will be used to target ads.",
     safeState = Off, sellByDate = new LocalDate(2014, 11, 1))
 
-  val AudienceScienceGatewaySwitch = Switch("Ad Targeting", "audience-science-gateway",
+  val AudienceScienceGatewaySwitch = Switch("Commercial Tags", "audience-science-gateway",
     "If this switch is on, Audience Science Gateway segments will be used to target ads.",
     safeState = Off, sellByDate = new LocalDate(2014, 11, 1))
 
-  val CriteoSwitch = Switch("Ad Targeting", "criteo",
+  val CriteoSwitch = Switch("Commercial Tags", "criteo",
     "If this switch is on, Criteo segments will be used to target ads.",
     safeState = Off, sellByDate = new LocalDate(2014, 11, 1))
 
   val EffectiveMeasureSwitch = Switch("Commercial Tags", "effective-measure",
     "Enable the Effective Measure audience segment tracking.",
-    safeState = Off, sellByDate = never)
-
-  // We don't foresee this service being switched off
-  val ForeseeSwitch = Switch("Performance Switches", "foresee",
-    "Enable Foresee surveys for a sample of our audience",
     safeState = Off, sellByDate = never)
 
   val ImrWorldwideSwitch = Switch("Commercial Tags", "imr-worldwide",
@@ -170,6 +165,12 @@ object Switches extends Collections {
 
   val OutbrainSwitch = Switch("Content Recommendation", "outbrain",
     "Enable the Outbrain content recommendation widget.",
+    safeState = Off, sellByDate = never)
+
+
+  // We don't foresee this service being switched off
+  val ForeseeSwitch = Switch("Performance Switches", "foresee",
+    "Enable Foresee surveys for a sample of our audience",
     safeState = Off, sellByDate = never)
 
   // Commercial Feeds
