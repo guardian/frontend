b42579c2f27d40dad1adfba1e50926c1159477ec
shakor <shahin.kordasti@guardian.co.uk>
2014-08-08 10:09:53 +0100

    Moved the system prop init to a companion object to have it run only once

--- integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/features/util/IntegrationSeleniumTestSuite.scala
+++ integration-tests/scala-fw-tests/src/test/scala/com/gu/integration/test/features/util/IntegrationSeleniumTestSuite.scala
@@ -5,6 +5,10 @@ import com.gu.integration.test.SeleniumTestSuite
 import com.gu.integration.test.util.FileUtil._
 
 trait IntegrationSeleniumTestSuite extends SeleniumTestSuite {
+  //only used because of the need to have a static init block, which the companion object below acts as
+}
+
+object IntegrationSeleniumTestSuite{
   //this is needed because of the sbt multimodule nature of this test project
   //without it, it will try to load the local.conf from the root folder
   System.setProperty("local.conf.loc", s"${currentPath()}/scala-fw-tests/local.conf")
