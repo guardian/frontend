6b9e28b8071784107648a5eec04a602f5f1bf6cd
shakor <shahin.kordasti@guardian.co.uk>
2014-08-11 14:18:15 +0100

    Reverted change which should not have been committed.

--- integration-tests/common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
+++ integration-tests/common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
@@ -1,12 +1,12 @@
 package com.gu.integration.test
 
 import com.gu.automation.core.WebDriverFeatureSpec
-import com.gu.automation.support.{TestRetries, Browser}
+import com.gu.automation.support.{Browser, TestRetries}
 import com.gu.integration.test.config.WebdriverInitialiser.augmentWebDriver
 import org.openqa.selenium.WebDriver
-import org.scalatest.{BeforeAndAfterAll, Matchers}
+import org.scalatest.Matchers
 
-abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers /*with TestRetries*/ {
+abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers with TestRetries {
 
   override protected def startDriver(testName: String, targetBrowser: Browser): WebDriver = {
     println("local conf location " + System.getProperty("local.conf.loc"))
