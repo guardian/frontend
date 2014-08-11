e847256af027b6bb187102fc22464d799dc30489
Kaelig <kaelig@deloumeau.fr>
2014-08-11 16:47:34 +0100

    Add Quiche Lorraine container

--- common/app/views/support/package.scala
+++ common/app/views/support/package.scala
@@ -120,6 +120,10 @@ case class CassouletContainer(showMore: Boolean = true) extends Container {
   val containerType = "cassoulet"
   val tone = "feature"
 }
+case class QuicheLorraineContainer(showMore: Boolean = true) extends Container {
+  val containerType = "quichelorraine"
+  val tone = "feature"
+}
 
 
 /**
