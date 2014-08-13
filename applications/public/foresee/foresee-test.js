e937a3f72ee242f4b2b800c91594787098a314b2
Matt Osborn <matthew.osborn@theguardian.com>
2014-08-13 17:46:55 +0100

    moving foresee to theguardian.com

--- applications/public/foresee/foresee-test.js
+++ applications/public/foresee/foresee-test.js
@@ -7,7 +7,7 @@ var $$FSR = {
    'sessionreplay': true,
    'auto' : true,
    'encode' : true,
-   'files': 'http://assets.guim.co.uk/javascripts/vendor/foresee4/',
+   'files': '/foresee/',
    // needs to be set when foresee-transport.swf is not located at 'files'
    //'swf_files': '__swf_files_'
    'id': 'fdIXz/UlLcgRJ+Qwv25S0Q==',
