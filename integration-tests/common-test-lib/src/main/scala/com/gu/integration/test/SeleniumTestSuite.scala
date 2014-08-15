eb5973f586c0cb56f9e3fdbce1eb547d40fa14d6
shakor <shahin.kordasti@guardian.co.uk>
2014-08-15 16:43:15 +0100

    Cleanup

--- integration-tests/common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
+++ integration-tests/common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
@@ -6,7 +6,7 @@ import com.gu.integration.test.config.WebdriverInitialiser.augmentWebDriver
 import org.openqa.selenium.WebDriver
 import org.scalatest.Matchers
 
-abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers /*with TestRetries*/ {
+abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers with TestRetries {
 
   override protected def startDriver(testName: String, targetBrowser: Browser): WebDriver = {
     augmentWebDriver(super.startDriver(testName, targetBrowser))
