5fab5e493464083f3d513e5199830d2c3e7fde25
shakor <shahin.kordasti@guardian.co.uk>
2014-08-18 10:18:08 +0100

    added missing files and changed public elements to private

--- integration-tests/identity-tests/src/main/scala/com/gu/identity/integration/test/pages/EditAccountDetailsModule.scala
+++ integration-tests/identity-tests/src/main/scala/com/gu/identity/integration/test/pages/EditAccountDetailsModule.scala
@@ -5,8 +5,8 @@ import com.gu.integration.test.util.ElementLoader._
 import org.openqa.selenium.{WebDriver, WebElement}
 
 class EditAccountDetailsModule(implicit driver: WebDriver) extends ParentPage {
-  def emailField: WebElement = findByTestAttribute("email-address")
-  def saveChangesButton: WebElement = findByTestAttribute("save-changes")
+  private def emailField: WebElement = findByTestAttribute("email-address")
+  private def saveChangesButton: WebElement = findByTestAttribute("save-changes")
   private def validationErrors: List[WebElement] = findAllByTestAttribute("form-field-error")
 
   def enterEmailAddress(email: String) = {
@@ -14,7 +14,15 @@ class EditAccountDetailsModule(implicit driver: WebDriver) extends ParentPage {
     emailField.sendKeys(email)
   }
 
+  def getEmailAddress() = {
+    emailField.getText
+  }
+
   def getAllValidationErrorElements(): List[WebElement] = {
     displayedElements(validationErrors)
+  }
+
+  def saveChanges() ={
+    saveChangesButton.click()
   }
 }
