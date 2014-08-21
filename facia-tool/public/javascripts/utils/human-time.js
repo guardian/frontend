264bb5d066ea5f636acf4e1d58f6ccf693a8a552
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-08-21 13:19:35 +0100

    Better relative date strings

--- facia-tool/public/javascripts/utils/human-time.js
+++ facia-tool/public/javascripts/utils/human-time.js
@@ -1,20 +1,34 @@
 /* global _: true */
 define(function () {
     return function(date) {
-        if (!date) { return; }
-
-        var elapsed = (new Date() - new Date(date))/1000,
-            abs = Math.abs(elapsed),
-            period = _.find([
+        var periods = [
                 { secs: 30758400, unit: 'year'},
                 { secs: 2563200, unit: 'month'},
                 { secs: 86400, unit: 'day'},
                 { secs: 3600, unit: 'hour'},
                 { secs: 60, unit: 'min'}
-            ], function(period) { return abs >= period.secs; }),
-            units = period ? Math.round(abs/period.secs) : null;
+            ],
+            period,
+            elapsed,
+            abs,
+            units,
+            str;
 
-        // 60 second leeway for "just now"
-        return !period ? 'just now' : (elapsed < abs ? 'in ' : '') + units + ' ' + period.unit + (units !== 1 ? 's' : '');
+        if (!date) { return; }
+
+        elapsed = (new Date() - new Date(date))/1000;
+
+        abs = Math.abs(elapsed);
+
+        period = _.find(periods, function(period) { return abs >= period.secs; });
+
+        units = period ? Math.round(abs/period.secs) : null;
+
+        if (period) {
+            str = units + ' ' + period.unit + (units !== 1 ? 's' : '');
+            return elapsed < abs ? 'in ' + str : str + ' ago';
+        } else {
+            return 'just now';
+        }
     };
 });
