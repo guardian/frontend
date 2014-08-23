0a290dbd762946175c46a9566754b0512475dfd6
Grant Klopper <grant.klopper@theguardian.com>
2014-08-23 07:26:25 +0100

    Cleaned up domain verification

--- identity/app/services/ReturnUrlVerifier.scala
+++ identity/app/services/ReturnUrlVerifier.scala
@@ -1,54 +1,42 @@
 package services
 
-import java.net.{URISyntaxException, URI}
+import java.net.URI
 
-import play.api.mvc.RequestHeader
-import conf.IdentityConfiguration
 import com.google.inject.Inject
+import conf.IdentityConfiguration
+import play.api.mvc.RequestHeader
 import utils.SafeLogging
-
 import scala.util.Try
 
-
 class ReturnUrlVerifier @Inject()(conf: IdentityConfiguration) extends SafeLogging {
-  val domainRegExp = """^https?://([^:/\?]+).*""".r
-  val returnUrlDomains = List(conf.id.domain)
+
+  private val returnUrlDomains = List(conf.id.domain)
+
   val defaultReturnUrl = "http://www." + conf.id.domain
 
-  def getVerifiedReturnUrl(request: RequestHeader): Option[String] = {
-    getVerifiedReturnUrl(
-      request
-        .getQueryString("returnUrl")
-        .orElse(request.headers.get("Referer").filterNot(_.startsWith(conf.id.url)).filter(validUrl)
-      )
+  def getVerifiedReturnUrl(request: RequestHeader): Option[String] = getVerifiedReturnUrl(
+    request
+      .getQueryString("returnUrl")
+      .orElse(request.headers.get("Referer")
+      .filterNot(_.startsWith(conf.id.url))
     )
-  }
+  )
 
   def getVerifiedReturnUrl(returnUrl: Option[String]): Option[String] = {
     returnUrl.flatMap(getVerifiedReturnUrl)
   }
 
-  def getVerifiedReturnUrl(returnUrl: String): Option[String] = {
-    hasVerifiedReturnUrl(returnUrl) match {
-      case true => Some(returnUrl)
-      case false => {
-        logger.warn("Invalid returnURL: %s".format(returnUrl))
-        None
-      }
-    }
+  def getVerifiedReturnUrl(returnUrl: String): Option[String] = if (hasVerifiedReturnUrl(returnUrl)) {
+    Some(returnUrl)
+  } else {
+      logger.warn("Invalid returnURL: %s".format(returnUrl))
+      None
   }
 
-  def hasVerifiedReturnUrl(returnUrl: String): Boolean = {
-    validUrl(returnUrl) && (returnUrl match {
-      case domainRegExp(domain) if validDomain(domain) => true
-      case _ => false
-    })
-  }
+  def hasVerifiedReturnUrl(returnUrl: String): Boolean = Try(new URI(returnUrl).getHost)
+    .map(isValidDomain).getOrElse(false)
 
-  private def validUrl(url: String) = Try(new URI(url)).isSuccess
-
-  private def validDomain(domain: String) = returnUrlDomains.exists(validDomain =>
+  private def isValidDomain(domain: String) = returnUrlDomains.exists(validDomain =>
     domain == validDomain || domain.endsWith("." + validDomain)
   )
-
 }
