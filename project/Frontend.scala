55540f2105db78d172a195f511dfa5adaa955f9a
Robert Berry <robert.berry@theguardian.com>
2014-08-27 16:38:44 +0100

    Update Content API client to 2.19

--- project/Frontend.scala
+++ project/Frontend.scala
@@ -14,7 +14,7 @@ object Frontend extends Build with Prototypes {
   val common = application("common").settings(
     libraryDependencies ++= Seq(
       "com.gu" %% "configuration" % "3.9",
-      "com.gu" %% "content-api-client" % "2.18",
+      "com.gu" %% "content-api-client" % "2.19",
 
       "com.typesafe.akka" %% "akka-agent" % "2.3.4",
 
