b4931132b8fe267c5c72860e40f15e5fd3b0c16b
Matt Osborn <matthew.osborn@theguardian.com>
2014-08-28 16:32:28 +0100

    amending vh relative gallery images for safari

--- common/app/assets/javascripts/bootstraps/gallery.js
+++ common/app/assets/javascripts/bootstraps/gallery.js
@@ -1,10 +1,31 @@
 define([
-    'common/utils/mediator'
+    'common/utils/mediator',
+    'common/utils/$',
+    'bean'
 ], function(
-    mediator
+    mediator,
+    $,
+    bean
 ) {
 
+    var verticalHeightCss = function() {
+        $('.gallery2__img').css('max-height', '90vh');
+    };
+
+    var safariVerticalHeightPolyfill = function() {
+        var setHeight = function() {
+            $('.js-vh-polyfill').css('max-height', window.innerHeight * 0.9);
+        };
+        setHeight();
+        bean.on(window, 'resize', setHeight);
+        bean.on(window, 'orientationchange', setHeight);
+    };
+
     var ready = function (config, context) {
+        (window.navigator.userAgent.match(/(Safari)/)) ? safariVerticalHeightPolyfill() : verticalHeightCss();
+        $('.js-delayed-image-upgrade').removeClass('js-delayed-image-upgrade').addClass('js-image-upgrade');
+        mediator.emit('ui:images:upgrade');
+
         mediator.emit('page:gallery:ready', config, context);
     };
 
