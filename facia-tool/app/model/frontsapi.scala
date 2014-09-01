f438dad07256c00b132f24e245873cc6fb805fb6
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-09-01 13:31:19 +0100

    Remove 'special' container

--- facia-tool/app/model/frontsapi.scala
+++ facia-tool/app/model/frontsapi.scala
@@ -104,15 +104,6 @@ case class UpdateList(
   draft: Boolean
 )
 
-object CollectionMetaUpdate {
-  implicit val jsonFormat = Json.format[CollectionMetaUpdate]
-}
-
-case class CollectionMetaUpdate(
-  displayName: Option[String],
-  href: Option[String]
-)
-
 case class StreamUpdate(update: JsValue, action: String, email: String)
 
 object StreamUpdate {
@@ -138,9 +129,6 @@ trait UpdateActions extends Logging {
     "isBreaking",
     "showMainVideo"
   )
-
-  implicit val collectionMetaWrites = Json.writes[CollectionMetaUpdate]
-  implicit val updateListWrite = Json.writes[UpdateList]
 
   def getBlock(id: String): Option[Block] = FaciaApi.getBlock(id)
 
@@ -174,9 +162,6 @@ trait UpdateActions extends Logging {
       block.copy(draft=block.draft orElse Option(block.live) map { l => l.filterNot(_.id == update.item) } filter(_ != block.live) )
     else
       block
-
-  def updateCollectionMeta(block: Block, update: CollectionMetaUpdate, identity: UserIdentity): Block =
-    block.copy(displayName=update.displayName, href=update.href)
 
   def putBlock(id: String, block: Block, identity: UserIdentity): Block =
     FaciaApi.putBlock(id, block, identity)
@@ -231,12 +216,6 @@ trait UpdateActions extends Logging {
       .map(archiveDeleteBlock(id, _, updateJson, identity))
       .map(putBlock(id, _, identity))
   }
-
-  def updateCollectionMeta(id: String, update: CollectionMetaUpdate, identity: UserIdentity): Option[Block] =
-    getBlock(id)
-      .map(updateCollectionMeta(_, update, identity))
-      .map(_.sortByGroup)
-      .map(putBlock(id, _, identity))
 
   private def updateList(update: UpdateList, blocks: List[Trail]): List[Trail] = {
     val trail: Trail = blocks
