f272c72c1776295ef3158c67d3c5a4ed0b27eaf6
Robert Berry <robert.berry@theguardian.com>
2014-09-02 13:47:00 +0100

    Remove admin's dependencies on JavaFutures

---
+++ common/app/awswrappers/cloudwatch.scala
@@ -0,0 +1,22 @@
+package awswrappers
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClient
+import com.amazonaws.services.cloudwatch.model.{GetMetricStatisticsResult, GetMetricStatisticsRequest, PutMetricDataRequest}
+
+import scala.concurrent.Future
+
+/** NB: We ought to switch to this library once we update to Scala 2.11.2:
+  *
+  * https://github.com/guardian/aws-sdk-scala-wrappers
+  *
+  * Then we can delete these manually written wrappers.
+  */
+object cloudwatch {
+  implicit class RichAsyncCloudWatchClient(client: AmazonCloudWatchAsyncClient) {
+    def putMetricDataFuture(request: PutMetricDataRequest): Future[Void] =
+      asFuture[PutMetricDataRequest, Void](client.putMetricDataAsync(request, _))
+
+    def getMetricStatisticsFuture(request: GetMetricStatisticsRequest): Future[GetMetricStatisticsResult] =
+      asFuture[GetMetricStatisticsRequest, GetMetricStatisticsResult](client.getMetricStatisticsAsync(request, _))
+  }
+}
