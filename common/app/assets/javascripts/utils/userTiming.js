ce2e287baf363bc0d893404b37670d31e25c977f
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-09-04 17:49:54 +0100

    adding jscs

--- common/app/assets/javascripts/utils/userTiming.js
+++ common/app/assets/javascripts/utils/userTiming.js
@@ -1,11 +1,11 @@
-define( function() {
+define(function () {
 
     var perf = window.performance || window.msPerformance || window.webkitPerformance || window.mozPerformance;
 
     function mark(label) {
 
         if (perf && 'mark' in perf) {
-             perf.mark('gu.' + label);
+            perf.mark('gu.' + label);
         }
     }
 
