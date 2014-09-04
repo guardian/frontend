5c231ad08952a3d6d4f2ae95760c5b528003af12
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-09-04 12:39:50 +0100

    Tidy

--- facia-tool/public/javascripts/utils/sanitize-html.js
+++ facia-tool/public/javascripts/utils/sanitize-html.js
@@ -7,11 +7,12 @@ define(function() {
         if (_.isString(s)) {
             el = document.createElement('div');
             el.innerHTML = s;
+
             scripts = el.getElementsByTagName("script");
             Array.prototype.forEach.call(scripts, function(script) {
                 script.parentElement.removeChild(script);
             });
-            console.log(el.innerHTML);
+
             return el.innerHTML;
         } else {
             return s;
