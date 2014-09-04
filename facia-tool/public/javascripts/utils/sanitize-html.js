006cb290c4419e1e3c9799f0eb1ac961a86851d4
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-09-04 14:46:29 +0100

    Single quotes

--- facia-tool/public/javascripts/utils/sanitize-html.js
+++ facia-tool/public/javascripts/utils/sanitize-html.js
@@ -8,7 +8,7 @@ define(function() {
             el = document.createElement('div');
             el.innerHTML = s;
 
-            scripts = el.getElementsByTagName("script");
+            scripts = el.getElementsByTagName('script');
             Array.prototype.forEach.call(scripts, function(script) {
                 script.parentElement.removeChild(script);
             });
