05182a680a75beed4bbc3d1a3f4bff3f9e630e6d
Stephan Fowler <stephan.fowler@guardian.co.uk>
2014-09-04 12:38:37 +0100

    Better script tag remover

--- facia-tool/public/javascripts/utils/sanitize-html.js
+++ facia-tool/public/javascripts/utils/sanitize-html.js
@@ -1,13 +1,17 @@
 /* global _: true */
 define(function() {
-    var rx = new RegExp(/<script.*$/);
-
     return function(s) {
-        var el;
+        var el,
+            scripts;
 
         if (_.isString(s)) {
             el = document.createElement('div');
-            el.innerHTML = s.replace(rx, '');
+            el.innerHTML = s;
+            scripts = el.getElementsByTagName("script");
+            Array.prototype.forEach.call(scripts, function(script) {
+                script.parentElement.removeChild(script);
+            });
+            console.log(el.innerHTML);
             return el.innerHTML;
         } else {
             return s;
