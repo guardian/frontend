aa9344d9c70a3181e95ddb9e3bde60786a185d50
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-09-08 13:13:04 +0100

    Fix merge conflicts

--- onward/app/controllers/MostViewedGalleryController.scala
+++ onward/app/controllers/MostViewedGalleryController.scala
@@ -4,36 +4,28 @@ import common._
 import model._
 import views.support.{TemplateDeduping, ForceGroupsCollection, MultimediaContainer}
 import play.api.mvc.{RequestHeader, Controller, Action}
-import scala.concurrent.Future
-import conf.LiveContentApi
-import feed.MostReadAgent
+import feed.MostViewedGalleryAgent
 
 object MostViewedGalleryController extends Controller with Logging with ExecutionContexts {
 
   def renderMostViewed() = Action.async { implicit request =>
 
-    val edition = Edition(request)
-    getMostViewedGallery(edition) map {
+    getMostViewedGallery map {
       case Nil => JsonNotFound()
       case galleries => renderMostViewedGallery(galleries)
     }
 
   }
 
-  private def getMostViewedGallery(edition: Edition)(implicit request: RequestHeader): Future[Seq[Content]] = {
+  private def getMostViewedGallery()(implicit request: RequestHeader): Seq[Content] = {
 
-    val size = request.getQueryString("size").getOrElse("4").toInt
-    val response = LiveContentApi.search(edition)
-      .tag("type/gallery")
-      .pageSize(50)
-      .response
-    response.map { response =>
-      response.results.map(Gallery(_)).sortBy(content => - MostReadAgent.getViewCount(content.id).getOrElse(0)).take(size)
-    }
+    val size = request.getQueryString("size").getOrElse("6").toInt
+    MostViewedGalleryAgent.mostViewedGalleries().take(size)
 
   }
 
   private def renderMostViewedGallery(galleries: Seq[Content])(implicit request: RequestHeader) = Cached(900) {
+
     val page = Page("More galleries", "inpictures", "More Galleries", "more galleries")
     val config = Config("multimedia/gallery", None, Some("more galleries"), None, Nil, Some("multimedia/gallery"))
     val collection = Collection(galleries)
