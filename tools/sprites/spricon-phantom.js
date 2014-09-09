26e561cab1c24550799f57919da545452d1d3914
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-09-09 17:39:41 +0100

    more tightening of icons

--- tools/sprites/spricon-phantom.js
+++ tools/sprites/spricon-phantom.js
@@ -112,7 +112,7 @@ function processFile() {
             //If we want to generate base64 svg css
             if(generatesvg) {
                 // add rules to svg data css file
-                datacssrules.push( "    %svg-" + cssprefix + filenamenoext +", .svg-" + cssprefix + filenamenoext +" { background-image: url(" + svgdatauri + "); background-position: 0 0; background-repeat: no-repeat; }\n    .svg ." + cssprefix + filenamenoext + " { @extend %svg-" + cssprefix + filenamenoext +" !optional; }\n" );
+                datacssrules.push( "    %svg-" + cssprefix + filenamenoext +", .svg-" + cssprefix + filenamenoext +" { background-image: url(" + svgdatauri + "); background-position: 0 0; }\n    .svg ." + cssprefix + filenamenoext + " { @extend %svg-" + cssprefix + filenamenoext +" !optional; }\n" );
             }
 
             // set page viewport size to svg dimensions
