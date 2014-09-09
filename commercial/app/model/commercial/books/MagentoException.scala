45d9e2cdbdb4bfd4c722a39e8b169f4227ca8ebc
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-09-09 15:38:35 +0100

    Add Magento Exception

---
+++ commercial/app/model/commercial/books/MagentoException.scala
@@ -0,0 +1,28 @@
+package model.commercial.books
+
+import common.Logging
+import play.api.libs.json.{JsError, JsSuccess, JsValue}
+
+case class MagentoException(code: Int, message: String)
+
+object MagentoException extends Logging {
+
+  def apply(json: JsValue): Option[MagentoException] = {
+    val error = (json \ "messages" \ "error")(0)
+
+    val parseResult = for {
+      code <- (error \ "code").validate[Int]
+      message <- (error \ "message").validate[String]
+    } yield {
+      MagentoException(code, message)
+    }
+
+    parseResult match {
+      case JsError(e) =>
+        log.error(s"MagentoException failed to parse $json: ${JsError.toFlatJson(e).toString()}")
+        None
+      case JsSuccess(magentoException, _) => Some(magentoException)
+    }
+  }
+
+}
