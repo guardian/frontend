b0a046b406fb30b2e1695bdb75359fd93ddea283
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-09-10 12:26:10 +0100

    add 88,70 slot size, for responsive ads

--- article/test/ArticleFeatureTest.scala
+++ article/test/ArticleFeatureTest.scala
@@ -349,9 +349,9 @@ class ArticleFeatureTest extends FeatureSpec with GivenWhenThen with Matchers {
 
         And("the placeholder has the correct data attributes")
         adPlaceholder.getAttribute("data-name") should be("top-above-nav")
-        adPlaceholder.getAttribute("data-mobile") should be("728,90")
-        adPlaceholder.getAttribute("data-right-col") should be("728,90|940,230|900,250")
-        adPlaceholder.getAttribute("data-wide") should be("728,90|940,230|900,250|970,250")
+        adPlaceholder.getAttribute("data-mobile") should be("88,70|728,90")
+        adPlaceholder.getAttribute("data-right-col") should be("88,70|728,90|940,230|900,250")
+        adPlaceholder.getAttribute("data-wide") should be("88,70|728,90|940,230|900,250|970,250")
 
         And("the placeholder has the correct class name")
         adPlaceholder.getAttribute("class") should be("ad-slot ad-slot--dfp ad-slot--top-above-nav ad-slot--top-banner-ad")
