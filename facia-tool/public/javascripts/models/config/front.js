0f7a038bcd73a1f54b5a36738b82333b1e926c1c
tudorraul <tudorraul@gmail.com>
2014-09-10 17:06:09 +0100

    editorial type automatic update

--- facia-tool/public/javascripts/models/config/front.js
+++ facia-tool/public/javascripts/models/config/front.js
@@ -81,8 +81,8 @@ define([
             }
         }, this);
 
-        this.capiProps.type.subscribe(function (x) {
-            this.props.editorialType(x);
+        this.capiProps.type.subscribe(function (type) {
+            this.props.editorialType(type);
         }, this);
 
         this.provisionalImageUrl = ko.observable();
@@ -159,7 +159,6 @@ define([
 
         if(checkUniqueness && _.filter(vars.model.fronts(), function(front) { return front.id() === self.id(); }).length > 1) {
             this.id(undefined);
-            return;
         }
     };
 
@@ -173,15 +172,18 @@ define([
     };
 
     Front.prototype.openProps = function() {
-        var self = this;
-
         this.state.isOpenProps(true);
         this.collections.items().map(function(collection) { collection.close(); });
 
-        contentApi.fetchMetaForPath(this.id())
-        .done(function(meta) {
+        this.loadCapiProps();
+    };
+
+    Front.prototype.loadCapiProps = function(){
+        var self = this;
+        contentApi.fetchMetaForPath(self.id())
+        .done(function (meta) {
             meta = meta || {};
-            _.each(self.capiProps, function(val, key) {
+            _.each(self.capiProps, function (val, key) {
                 val(meta[key]);
             });
         });
@@ -199,6 +201,11 @@ define([
         collection.parents.push(this);
         this.collections.items.push(collection);
         vars.model.collections.unshift(collection);
+
+        var isNewFront = this.collections.items().length === 1;
+        if(isNewFront){
+            this.loadCapiProps();
+        }
     };
 
     Front.prototype._depopulateCollection = function(collection) {
