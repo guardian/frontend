69bb363dff75002f75e1d0b6e6c956eca7cf0a94
Grant Klopper <grant.klopper@theguardian.com>
2014-09-11 21:42:34 +0100

    Temorary disable auth loggin

--- common/app/common/authentication.scala
+++ common/app/common/authentication.scala
@@ -7,9 +7,9 @@ import play.api.mvc._
 trait AuthLogging {
   self: Logging =>
   def log[U](msg: String, request: Request[AnyContent]) {
-    request match {
-      case auth: Security.AuthenticatedRequest[AnyContent, U] => log.info(auth.user + ": " + msg)
-      case _ => throw new IllegalStateException("Expected an authenticated request")
-    }
+//    request match {
+//      case auth: Security.AuthenticatedRequest[AnyContent, U] => log.info(auth.user + ": " + msg)
+//      case _ => throw new IllegalStateException("Expected an authenticated request")
+//    }
   }
 }
