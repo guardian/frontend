90dbc7e3eeb161bf86378265221142bd21c9f9d7
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-09-12 16:29:29 +0100

    Add Magento Bestsellers Feed
    To be used temporarily when old bookshop shut down.
    Eventually this will be replaced by a new categorised endpoint.

--- commercial/app/model/commercial/books/Book.scala
+++ commercial/app/model/commercial/books/Book.scala
@@ -1,6 +1,7 @@
 package model.commercial.books
 
 import common.ExecutionContexts
+import conf.Switches.MagentoServiceSwitch
 import model.commercial.{Ad, AdAgent, Segment, intersects, lastPart}
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Reads._
@@ -57,7 +58,7 @@ object Book {
 
 object BestsellersAgent extends AdAgent[Book] with ExecutionContexts {
 
-  private val feeds = Seq(
+  private val bertramFeeds = Seq(
     GeneralBestsellersFeed,
     TravelBestsellersFeed,
     ScienceBestsellersFeed,
@@ -71,6 +72,8 @@ object BestsellersAgent extends AdAgent[Book] with ExecutionContexts {
     FoodDrinkBestsellersFeed
   )
 
+  private val magentoFeeds = Seq(MagentoBestsellersFeed)
+
   def getSpecificBook(isbn: String) = currentAds find(_.isbn == isbn)
   def getSpecificBooks(specifics: Seq[String]) = currentAds.filter(specifics contains _.isbn)
 
@@ -81,6 +84,8 @@ object BestsellersAgent extends AdAgent[Book] with ExecutionContexts {
   def refresh() {
 
     val bookListsLoading: Future[Seq[Seq[Book]]] = Future.sequence {
+      val feeds = if (MagentoServiceSwitch.isSwitchedOn) magentoFeeds else bertramFeeds
+
       feeds.foldLeft(Seq[Future[Seq[Book]]]()) {
         (soFar, feed) =>
           soFar :+ feed.loadAds().recover {
