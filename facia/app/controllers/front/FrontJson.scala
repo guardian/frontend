4f3b2a070ab2f68924696586bbfb68f9c3bc6c72
tudorraul <tudorraul@gmail.com>
2014-09-12 17:27:43 +0100

    code format

--- facia/app/controllers/front/FrontJson.scala
+++ facia/app/controllers/front/FrontJson.scala
@@ -135,8 +135,8 @@ trait FrontJson extends ExecutionContexts with Logging {
       href            = (json \ "href").asOpt[String],
       groups          = (json \ "groups").asOpt[List[String]].getOrElse(Nil),
       collectionType  = (json \ "type").asOpt[String],
-      showTags = (json \ "showTags").asOpt[Boolean] getOrElse false,
-      showSections = (json \ "showSections").asOpt[Boolean] getOrElse false
+      showTags        = (json \ "showTags").asOpt[Boolean] getOrElse false,
+      showSections    = (json \ "showSections").asOpt[Boolean] getOrElse false
     )
   }
 
