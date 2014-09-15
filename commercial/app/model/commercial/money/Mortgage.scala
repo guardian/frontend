e3da946a03dfff8fa55580f66bc2a2a6f755bd41
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-09-15 17:21:10 +0100

    Eliminate XmlAdsApi from MortgageApi

--- commercial/app/model/commercial/money/Mortgage.scala
+++ commercial/app/model/commercial/money/Mortgage.scala
@@ -1,8 +1,12 @@
 package model.commercial.money
 
-import model.commercial.{XmlAdsApi, Segment, Ad}
-import scala.xml.Elem
-import conf.{CommercialConfiguration, Switches}
+import common.{ExecutionContexts, Logging}
+import conf.CommercialConfiguration
+import conf.Switches._
+import model.commercial._
+
+import scala.concurrent.Future
+import scala.xml.{Elem, XML}
 
 case class Mortgage(lender: String,
                     rate: String,
@@ -15,11 +19,7 @@ case class Mortgage(lender: String,
 }
 
 
-object MortgagesApi extends XmlAdsApi[Mortgage] {
-
-  protected val switch = Switches.LCMortgageFeedSwitch
-
-  protected val adTypeName = "Mortgages"
+object MortgagesApi extends ExecutionContexts with Logging {
 
   protected def url = CommercialConfiguration.getProperty("lc.mortgages.api.url")
 
@@ -33,6 +33,30 @@ object MortgagesApi extends XmlAdsApi[Mortgage] {
           (product \ "OverallCost").text.toDouble,
           (product \ "Link").text
         )
+    }
+  }
+
+  def loadAds(): Future[Seq[Mortgage]] = {
+    url map { u =>
+      val result = FeedReader.read(FeedRequest(LCMortgageFeedSwitch, u)) { body =>
+        val xml = XML.loadString(body)
+        parse(xml)
+      }
+      result map {
+        case Left(FeedReadWarning(message)) =>
+          log.warn(s"Reading Mortgages feed failed: $message")
+          Nil
+        case Left(FeedReadException(message)) =>
+          log.error(s"Reading Mortgages feed failed: $message")
+          Nil
+        case Right(jobs) => jobs
+        case other =>
+          log.error(s"Something unexpected has happened: $other")
+          Nil
+      }
+    } getOrElse {
+      log.warn("Reading Mortgages feed failed: missing URL")
+      Future.successful(Nil)
     }
   }
 }
