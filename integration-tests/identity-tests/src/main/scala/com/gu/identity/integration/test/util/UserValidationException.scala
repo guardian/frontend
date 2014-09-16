1d6ec179c6881f277c2ff83de5aa7377566eea41
Grant Klopper <grant.klopper@theguardian.com>
2014-09-16 13:11:18 +0100

    Deleted broken integration tests

--- integration-tests/identity-tests/src/main/scala/com/gu/identity/integration/test/util/UserValidationException.scala
+++
@@ -1,7 +0,0 @@
-package com.gu.identity.integration.test.util
-
-import org.openqa.selenium.WebElement
-
-case class UserValidationException(validationErrorElements : List[FormError]) extends RuntimeException{
-
-}
