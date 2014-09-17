1bbb2793df802570bed7abf6534758c2dfebea64
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2014-09-17 11:20:00 +0100

    properly round figures and tidy up graph labels

--- admin/app/controllers/ContentPerformanceController.scala
+++ admin/app/controllers/ContentPerformanceController.scala
@@ -9,8 +9,16 @@ import play.api.mvc._
 import tools.FormattedChart.{Cell, Row, Column}
 import tools._
 import jobs.OmnitureReports._
+import scala.math.pow
 
 object ContentPerformanceController extends Controller with AuthLogging with Logging with ExecutionContexts {
+
+  implicit class ExtendedDouble(n: Double) {
+    def rounded(x: Int) = {
+      val w = pow(10, x)
+      (n * w).toLong.toDouble / w
+    }
+  }
 
   case class ReportResult(
     name: String,
@@ -53,11 +61,12 @@ object ContentPerformanceController extends Controller with AuthLogging with Log
       case (false, true) => NoCache(Ok("Sentry reports not generated"))
       case (true, false) => NoCache(Ok("Omniture report not generated yet"))
       case _ =>
-        val sentryErrorColumns = List(Column("time", "Time", "date"), Column("Gallery Errors", "Gallery Js errors per day as % gallery visits ", "number"))
+        val sentryErrorColumns = List(Column("time", "Time", "date"), Column("Gallery Errors", "Gallery Js errors per gallery visits(%) ", "number"))
         val sentryRows = sentryData.zip(reportsObject).map {
           case (sentry, omniture) =>
               val dateCell = Cell(sentry.jsonDate)
-              val count = Cell(((sentry.errorCount / omniture.views) * 100).toString)
+              val roundedCount = ((sentry.errorCount / omniture.views) * 100).rounded(3).toString
+              val count = Cell(roundedCount)
               Row(List(dateCell, count))
         }
         val sentryChart = FormattedChart("Sentry errors per day as a % of number of gallery visits", sentryErrorColumns, sentryRows, ChartFormat(Colour.`tone-live-1`))
@@ -65,7 +74,8 @@ object ContentPerformanceController extends Controller with AuthLogging with Log
         val lightboxColumns = List(Column("time", "Time", "date"), Column("lightboxes", "Lightbox views per gallery", "number"))
         val lightboxRows = reportsObject.map { row =>
           val dateCell = Cell(row.jsonDate)
-          val lightboxCount = Cell(row.lightboxLaunchesPerVisit.toString)
+          val roundedCount = row.lightboxLaunchesPerVisit.rounded(3).toString
+          val lightboxCount = Cell(roundedCount)
           Row(List(dateCell, lightboxCount))
         }
         val lightboxChart = FormattedChart("Lightbox Views per Gallery Page View", lightboxColumns, lightboxRows, ChartFormat(Colour.`tone-features-3`))
@@ -73,7 +83,8 @@ object ContentPerformanceController extends Controller with AuthLogging with Log
         val galleryColumns = List(Column("time", "Time", "date"), Column("pvv", "Gallery views per visit", "number"))
         val galleryRows = reportsObject.map { row =>
           val dateCell = Cell(row.jsonDate)
-          val pageViews = Cell(row.pageViewsPerVisit.toString)
+          val roundedPageViews = row.pageViewsPerVisit.rounded(3).toString
+          val pageViews = Cell(roundedPageViews)
           Row(List(dateCell, pageViews))
         }
         val galleryChart = FormattedChart("Gallery Page Views per Gallery Visit", galleryColumns, galleryRows, ChartFormat(Colour.`tone-comment-2`))
@@ -83,7 +94,8 @@ object ContentPerformanceController extends Controller with AuthLogging with Log
         val shareRows = reportsObject.map {
           row =>
             val dateCell = Cell(row.jsonDate)
-            val galleryShares = Cell(row.percentageOfViewsShared.toString)
+            val roundedShares = row.percentageOfViewsShared.rounded(3).toString
+            val galleryShares = Cell(roundedShares)
             Row(List(dateCell, galleryShares))
         }
         val shareChart = FormattedChart("Social shares  per Gallery Page View(%)", shareColumns, shareRows, ChartFormat(Colour.`tone-live-2`))
@@ -129,7 +141,7 @@ object ContentPerformanceController extends Controller with AuthLogging with Log
       shares <- gallerySocialShares.counts.headOption.map(_.toDouble)
     } yield {
         val date = new DateTime(galleryViews.year, galleryViews.month, galleryViews.day, 0, 0)
-        GalleryPerformance(date, views, views / visits, lightboxes / views, (shares / views) * 100)
+        GalleryPerformance(date, views, (views / visits), lightboxes / views, (shares / views) * 100)
     }
     reportsObject.toSeq.sortBy(_.simpleDate)
   }
