6cccb2be895c9bcab2469880991a7a3c21ad4883
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-09-17 12:01:19 +0100

    Add LineItemId to Sponsorship

--- common/app/common/configuration.scala
+++ common/app/common/configuration.scala
@@ -232,7 +232,7 @@ class GuardianConfiguration(val application: String, val webappConfDirectory: St
     lazy val traveloffers_url = configuration.getStringProperty("traveloffers.api.url") map (u => s"$u/consumerfeed")
 
     private lazy val dfpRoot = s"${environment.stage.toUpperCase}/commercial/dfp"
-    lazy val dfpSponsoredTagsDataKey = s"$dfpRoot/sponsored-tags-v2.json"
+    lazy val dfpSponsoredTagsDataKey = s"$dfpRoot/sponsored-tags-v3.json"
     lazy val dfpAdvertisementFeatureTagsDataKey = s"$dfpRoot/advertisement-feature-tags-v2.json"
     lazy val dfpInlineMerchandisingTagsDataKey = s"$dfpRoot/inline-merchandising-tags-v3.json"
     lazy val dfpPageSkinnedAdUnitsKey = s"$dfpRoot/pageskinned-adunits-v5.json"
