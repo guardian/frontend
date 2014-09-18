9795cab7186985fd7bbacc597a197f596814405d
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-09-18 15:42:27 +0100

    Unpackage archive test suite

--- archive/test/package.scala
+++ archive/test/package.scala
@@ -9,7 +9,7 @@ object `package` {
   implicit class ListString2FirstNonEmpty(list: JList[String]) {
     lazy val firstNonEmpty: Option[String] = list find { !_.isEmpty }
   }
-
-  class ArchiveTestSuite extends Suites (
-    new ArchiveControllerTest ) with SingleServerSuite
 }
+
+class ArchiveTestSuite extends Suites (
+  new ArchiveControllerTest ) with SingleServerSuite
