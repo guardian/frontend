74d0e1141495c5e082189681df00c870b9deb9bb
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-09-22 12:28:13 +0100

    also remove from short domain

--- common/app/assets/javascripts/utils/cookies.js
+++ common/app/assets/javascripts/utils/cookies.js
@@ -36,6 +36,8 @@ define([
     function remove(name) {
         // Remove cookie, implicitly using the document's domain.
         document.cookie = name + '=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        // also try removing from the short domain
+        document.cookie = name + '=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=' + getShortDomain() + ';';
     }
 
     function add(name, value, daysToLive) {
