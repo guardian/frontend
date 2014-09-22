cb4f8c667c7e0029bc4beb40d97f13d3a017a218
Robert Berry <robert.berry@theguardian.com>
2014-09-22 16:56:32 +0100

    Show fewer items on mobile before hiding

--- common/app/layout/Card.scala
+++ common/app/layout/Card.scala
@@ -2,7 +2,19 @@ package layout
 
 import model.Trail
 
+sealed trait Breakpoint
+
+case object Mobile extends Breakpoint
+case object Desktop extends Breakpoint
+
 case class Card(
   index: Int,
-  item: Trail
-)
+  item: Trail,
+  hideUpTo: Option[Breakpoint]
+) {
+  def cssClasses = hideUpTo match {
+    case Some(Mobile) => "fc-show-more__hide-on-mobile"
+    case Some(Desktop) => "fc-show-more__hide"
+    case _ => ""
+  }
+}
---
+++ facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
@@ -0,0 +1,46 @@
+define([
+    'common/utils/$',
+    'bonzo',
+    'bean',
+    'qwery',
+    'common/utils/mediator'
+], function (
+    $,
+    bonzo,
+    bean,
+    qwery,
+    mediator
+    ) {
+    return function(container) {
+        var $container = bonzo(container),
+            className ='fc-show-more--hidden',
+            $button = bonzo(bonzo.create(
+                '<button class="collection__show-more tone-background tone-news" data-link-name="Show more | 1">' +
+                '<span class="collection__show-more__icon">' +
+                '<span class="i i-plus-white-mask"></span>' +
+                '<span class="i i-plus-white"></span>' +
+                '</span>' +
+                '<span class="u-h">Show more</span>' +
+                '</button>'
+        ));
+
+        function removeButton() {
+            // listen to the clickstream, as happens later, before removing
+            mediator.on('module:clickstream:click', function(clickSpec) {
+                if (qwery(clickSpec.target)[0] === $button[0]) {
+                    $button.remove();
+                }
+            });
+        }
+
+        function showMore() {
+            removeButton();
+            $container.removeClass(className);
+        }
+
+        $container.addClass(className);
+        $button.appendTo($container);
+        bean.on($button[0], 'click', showMore);
+        $container.removeClass('js-container--fc-show-more');
+    };
+});
