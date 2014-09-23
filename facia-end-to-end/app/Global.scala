bf52ac3b1289d979e11a8f3b5e5fbe05f94e97db
Francis Carr <francis.carr@guardian.co.uk>
2014-09-23 10:44:42 +0100

    Add facia end to end project folder

---
+++ facia-end-to-end/app/Global.scala
@@ -0,0 +1,62 @@
+import common.ExecutionContexts
+import conf.Filters
+import dev.DevParametersLifecycle
+import dfp.DfpAgentLifecycle
+import frontpress.ToolPressQueueWorker
+import implicits.Requests
+import ophan.SurgingContentAgentLifecycle
+import play.api.mvc.{EssentialAction, EssentialFilter, RequestHeader, WithFilters}
+import services.ConfigAgentLifecycle
+
+// obviously this is only for devbuild and should never end up in one of our
+// prod projects
+object DevCacheWarningFilter extends EssentialFilter with ExecutionContexts {
+  def apply(next: EssentialAction) = new EssentialAction {
+    def apply(rh: RequestHeader) = {
+      next(rh).map{ result =>
+        val header = result.header
+        val path = rh.path
+        if (
+          header.status == 200 &&
+            !header.headers.keySet.contains("Cache-Control") &&
+            path != "/favicon.ico" &&
+            !path.startsWith("/assets/") // these are only used on DEV machines
+        ) {
+          // nice big warning to devs if they are working on something uncached
+          println("\n\n\n---------------------------- WARNING ------------------------------------")
+          println(s"URL $path has NO CACHE-CONTROL header")
+          println("-------------------------------------------------------------------------------\n\n\n")
+        }
+        result
+      }
+    }
+  }
+}
+
+// obviously this is only for devbuild and should never end up in one of our
+// prod projects
+object DevJsonExtensionFilter extends EssentialFilter with ExecutionContexts with Requests {
+  def apply(next: EssentialAction) = new EssentialAction {
+    def apply(rh: RequestHeader) = {
+      if (rh.isJson && !rh.path.endsWith(".json") && !rh.path.endsWith(".js")) {
+        // makes it easy for devs to see what has happened
+        println("\n\n\n---------------------------- WARNING ------------------------------------")
+        println(s"URL ${rh.path} does not have a .json extension")
+        println("-------------------------------------------------------------------------------\n\n\n")
+        throw new IllegalArgumentException("JSON endpoints must end with '.json'")
+      }
+      next(rh)
+    }
+  }
+}
+
+
+object Global extends WithFilters(
+  Filters.common: _*
+)
+with ConfigAgentLifecycle {
+  override def onStart(app: play.api.Application) {
+    super.onStart(app)
+    ToolPressQueueWorker.start()
+  }
+}
