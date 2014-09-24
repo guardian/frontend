7f023597c6c2a093777f811e91b0ca54ff64b310
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-09-24 16:55:56 +0100

    Rename facia full and mega-full items

--- common/app/slices/Slice.scala
+++ common/app/slices/Slice.scala
@@ -441,7 +441,7 @@ case object HalfHalf extends Slice {
  * |         ##########################|
  * `-----------------------------------'
  */
-case object FullThreeQuarterImage extends Slice {
+case object Full extends Slice {
   val layout = SliceLayout(
     cssClassName = "f",
     columns = Seq(
@@ -463,7 +463,7 @@ case object FullThreeQuarterImage extends Slice {
  * |                                   |
  * `-----------------------------------'
  */
-case object Full extends Slice {
+case object MegaFull extends Slice {
   val layout = SliceLayout(
     cssClassName = "mf",
     columns = Seq(
