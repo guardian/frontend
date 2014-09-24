8a4afc7410cd87fa7c7d13063526e6f280fd7a94
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-09-24 18:08:04 +0100

    Synchronise aria attributes when using liveblog dropdowns

--- common/app/assets/javascripts/modules/ui/dropdowns.js
+++ common/app/assets/javascripts/modules/ui/dropdowns.js
@@ -13,26 +13,31 @@ define([
         content: '.dropdown__content'
     };
     function init() {
-
-        function ancestor(el, c) {
-            if (!el.parentNode || bonzo(el.parentNode).hasClass(c.substring(1))) {
-                return el.parentNode;
-            } else {
-                ancestor(el.parentNode, c);
-            }
+        bean.on(document.body, 'click', s.button, function(e){
+            var $container = bonzo(ancestor(e.currentTarget, s.container))
+            $container.toggleClass('dropdown--active');
+            updateAria($container);
+        });
+    }
+    function ancestor(el, c) {
+        if (!el.parentNode || bonzo(el.parentNode).hasClass(c.substring(1))) {
+            return el.parentNode;
+        } else {
+            ancestor(el.parentNode, c);
         }
-        bean.on(document.body, 'click', s.button, function(e) {
-            bonzo(ancestor(e.currentTarget, s.container))
-                .toggleClass('dropdown--active')
-                .each(function(d) {
-                    var v = bonzo(d).hasClass('dropdown--active');
-                    $(s.content, d).attr('aria-hidden', !v);
-                    $(s.content, d).attr('aria-expanded', v);
-                });
+    }
+
+    function updateAria($container) {
+        $container.each(function(d) {
+            var v = bonzo(d).hasClass('dropdown--active');
+            $(s.content, d).attr('aria-hidden', !v);
+            $(s.button, d).attr('aria-expanded', v);
+            $(s.content, d).attr('aria-expanded', v);
         });
     }
 
     return {
-        init: init
+        init: init,
+        updateAria: updateAria
     };
 });
