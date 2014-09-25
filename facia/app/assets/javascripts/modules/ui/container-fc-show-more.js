6b21a6168d1c4c6128decdb3bca17c83530525a7
Robert Berry <robert.berry@theguardian.com>
2014-09-25 12:11:02 +0100

    Prefer qwery over dollar

--- facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
+++ facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
@@ -1,17 +1,19 @@
 define([
     'common/utils/$',
     'bonzo',
-    'bean'
+    'bean',
+    'qwery'
 ], function (
     $,
     bonzo,
-    bean
+    bean,
+    qwery
 ) {
     return function (container) {
         var $container = bonzo(container),
             className = 'fc-show-more--hidden',
-            itemsHiddenOnDesktop = $('.js-hide', $container).length > 0,
-            itemsHiddenOnMobile = $('.js-hide-on-mobile', $container).length > 0,
+            itemsHiddenOnDesktop = qwery('.js-hide', $container).length > 0,
+            itemsHiddenOnMobile = qwery('.js-hide-on-mobile', $container).length > 0,
             $button = $.create(
             '<button class="collection__show-more tone-background tone-news" data-link-name="Show more | 1">' +
             '<span class="collection__show-more__icon">' +
