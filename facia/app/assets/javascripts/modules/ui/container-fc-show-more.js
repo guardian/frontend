928bd8d74fbfd962e9116b07b5f39bb7bc2aadc6
Robert Berry <robert.berry@theguardian.com>
2014-09-25 10:50:11 +0100

    Fix issue with show more button being visible with nothing more to show

--- facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
+++ facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
@@ -10,6 +10,8 @@ define([
     return function (container) {
         var $container = bonzo(container),
             className = 'fc-show-more--hidden',
+            itemsHiddenOnDesktop = $(".js-hide", $container).length,
+            itemsHiddenOnMobile = $(".js-hide-on-mobile", $container).length,
             $button = $.create(
             '<button class="collection__show-more tone-background tone-news" data-link-name="Show more | 1">' +
             '<span class="collection__show-more__icon">' +
@@ -29,9 +31,15 @@ define([
             $container.removeClass(className);
         }
 
-        $container.addClass(className);
-        $button.appendTo($container);
-        bean.on($button[0], 'click', showMore);
-        $container.removeClass('js-container--fc-show-more');
+        if (itemsHiddenOnMobile || itemsHiddenOnDesktop) {
+            if (!itemsHiddenOnDesktop) {
+                $container.addClass("fc-show-more--mobile-only");
+            }
+
+            $container.addClass(className);
+            $button.appendTo($container);
+            bean.on($button[0], 'click', showMore);
+            $container.removeClass('js-container--fc-show-more');
+        }
     };
 });
