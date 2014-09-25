e166f23635ae129632aadeadb9517120bea25d12
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-09-25 16:08:03 +0100

    Add test data-attrinute hook to show-more button

--- facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
+++ facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
@@ -15,7 +15,7 @@ define([
             itemsHiddenOnDesktop = qwery('.js-hide', $container).length > 0,
             itemsHiddenOnMobile = qwery('.js-hide-on-mobile', $container).length > 0,
             $button = $.create(
-            '<button class="collection__show-more tone-background tone-news" data-link-name="Show more | 1">' +
+            '<button class="collection__show-more tone-background tone-news" data-test-id="show-more" data-link-name="Show more | 1">' +
             '<span class="collection__show-more__icon">' +
             '<span class="i i-plus-white-mask"></span>' +
             '<span class="i i-plus-white"></span>' +
