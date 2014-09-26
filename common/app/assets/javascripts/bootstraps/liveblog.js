eaa25b17aa67f4961085855cf7e826c90a2276f4
Matt Osborn <matthew.osborn@theguardian.com>
2014-09-26 16:26:20 +0100

    truncate desktop blockshare icons

--- common/app/assets/javascripts/bootstraps/liveblog.js
+++ common/app/assets/javascripts/bootstraps/liveblog.js
@@ -232,6 +232,22 @@ define([
                 60000
             );
 
+        },
+
+        truncateBlockShareIcons: function(blockShareEl) {
+            var truncated = qwery('> *', blockShareEl).slice(2);
+            bonzo(truncated).addClass('u-h');
+            $('.js-blockshare-expand', blockShareEl).removeClass('u-h');
+        },
+
+        initBlockSharing: function() {
+            bean.on(document.body, 'click', '.js-blockshare-expand', function(e) {
+                var expandButton = bonzo(e.currentTarget),
+                    container = expandButton.parent()[0];
+                $('> *', container).removeClass('u-h');
+                expandButton.addClass('u-h');
+            });
+            $.forEachElement('.block-share', modules.truncateBlockShareIcons);
         }
     };
 
@@ -242,6 +258,7 @@ define([
         modules.createAutoRefresh();
         modules.showFootballLiveBlogMessage();
         modules.keepTimestampsCurrent();
+        modules.initBlockSharing();
 
         // re-use modules from article bootstrap
         article.modules.initOpen(config);
