0b7fd34fcd4ca1aa26c9da523d89c6161898c3f4
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-09-29 14:33:47 +0100

    starting audio release message

--- static/src/javascripts/projects/common/modules/release-message.js
+++ static/src/javascripts/projects/common/modules/release-message.js
@@ -1,16 +1,23 @@
 define([
     'common/utils/detect',
     'common/utils/preferences',
+    'common/utils/template',
     'common/modules/ui/message'
 ], function (
     detect,
     preferences,
+    template,
     Message
 ) {
 
     var messages = {
             video: {
                 text: 'We’ve redesigned our video pages to make it easier to find and experience our best video content. We’d love to hear what you think.',
+                blog: 'http://next.theguardian.com/blog/video-redesign/',
+                survey: 'https://www.surveymonkey.com/s/guardianvideo'
+            },
+            audio: {
+                text: 'We’ve redesigned our audio pages to make it easier to find and experience our best audio content. We’d love to hear what you think.',
                 blog: 'http://next.theguardian.com/blog/video-redesign/',
                 survey: 'https://www.surveymonkey.com/s/guardianvideo'
             },
@@ -26,33 +33,33 @@ define([
             }
         },
         releaseMessage = {
-            show: function (messageText, blogLink, surveyLink) {
-                var msg =
-                    '<p class="site-message__message" id="site-message__message">' +
-                        messageText +
-                    '</p>' +
+            show: function (type, msgConfig) {
+                var msg = template(
+                    '<p class="site-message__message" id="site-message__message">{{text}}</p>' +
                     '<ul class="site-message__actions u-unstyled">' +
                         '<li class="site-message__actions__item">' +
                             '<i class="i i-arrow-white-right"></i>' +
-                            '<a href="' + blogLink + '" target="_blank">Find out more</a>' +
+                            '<a href="{{blog}}" target="_blank">Find out more</a>' +
                         '</li>' +
                         '<li class="site-message__actions__item">' +
                             '<i class="i i-arrow-white-right"></i>' +
-                            '<a href="' + surveyLink + '" target="_blank">Leave feedback</a>' +
+                            '<a href="{{survey}}" target="_blank">Leave feedback</a>' +
                         '</li>' +
-                    '</ul>';
-
-                new Message('video', {pinOnHide: true}).show(msg);
+                    '</ul>',
+                    msgConfig
+                );
+                new Message(type + '-release', {pinOnHide: true}).show(msg);
             },
             init: function (config) {
-                if (detect.getBreakpoint() !== 'mobile' &&
-                        !preferences.hasOptedIntoResponsive() && config.page.contentType) {
-
+                if (
+                    detect.getBreakpoint() !== 'mobile' &&
+                    !preferences.hasOptedIntoResponsive() &&
+                    config.page.contentType
+                ) {
                     var contentType = config.page.contentType.toLowerCase(),
-                        msg = messages[contentType];
-                    if (messages[contentType]) {
-                        msg = messages[contentType];
-                        releaseMessage.show(msg.text, msg.blog, msg.survey);
+                        msgConfig   = messages[contentType];
+                    if (msgConfig) {
+                        releaseMessage.show(contentType, msgConfig);
                     }
                 }
             }
