4ca26c266a7e4c7a24b48801c62fb9b0bf20baf5
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-09-29 17:20:56 +0100

    add survey (but no blog) to audio release message

--- static/src/javascripts/projects/common/modules/release-message.js
+++ static/src/javascripts/projects/common/modules/release-message.js
@@ -18,8 +18,7 @@ define([
             },
             audio: {
                 text: 'We’ve redesigned our audio pages to make it easier to find and experience our best audio content. We’d love to hear what you think.',
-                blog: 'http://next.theguardian.com/blog/video-redesign/',
-                survey: 'https://www.surveymonkey.com/s/guardianvideo'
+                survey: 'https://www.surveymonkey.com/s/guardian_audio'
             },
             gallery: {
                 text: 'We’ve redesigned our gallery pages to make it easier to find and experience our best gallery content. We’d love to hear what you think.',
@@ -36,11 +35,19 @@ define([
             show: function (type, msgConfig) {
                 var msg = template(
                     '<p class="site-message__message" id="site-message__message">{{text}}</p>' +
-                    '<ul class="site-message__actions u-unstyled">' +
+                    '<ul class="site-message__actions u-unstyled">',
+                    msgConfig
+                );
+                if (msgConfig.blog) {
+                    msg += template(
                         '<li class="site-message__actions__item">' +
                             '<i class="i i-arrow-white-right"></i>' +
                             '<a href="{{blog}}" target="_blank">Find out more</a>' +
-                        '</li>' +
+                        '</li>',
+                        msgconfig
+                    );
+                }
+                msg += template(
                         '<li class="site-message__actions__item">' +
                             '<i class="i i-arrow-white-right"></i>' +
                             '<a href="{{survey}}" target="_blank">Leave feedback</a>' +
