0fe3fe20212492459c76b1e7084ab9f5da607e9e
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-09-30 10:29:13 +0100

    Style and behaviour for 'order by' pop up

--- common/app/assets/javascripts/modules/live/filter.js
+++ common/app/assets/javascripts/modules/live/filter.js
@@ -4,49 +4,38 @@
 */
 define([
     'bonzo',
+    'bean',
+    'qwery',
     'common/utils/$',
-    'common/modules/component',
     'common/utils/mediator',
     'lodash/collections/toArray'
 ], function (
     bonzo,
+    bean,
+    qwery,
     $,
-    component,
     mediator,
     toArray
 ) {
-    'use strict';
-
     function Filter(context) {
         this.context = context;
-        this.componentClass = 'live-filter';
-        this.useBem = true;
+        this.order = 'newest';
     }
 
-    component.define(Filter);
-
-    Filter.prototype.template = '<div><span class="live-toggler__label">Order by:</span>' +
-        '  <button class="u-button-reset live-toggler live-toggler--latest js-live-toggler" title="Sort by oldest first">' +
-        '    <span class="live-toggler__value">Latest</span>' +
-        '    <i class="i i-arrow-grey-down"></i>' +
-        '  </button>' +
-        '  <button class="u-button-reset live-toggler live-toggler--oldest js-live-toggler" title="Sort by latest first">' +
-        '    <span class="live-toggler__value">Oldest</span>' +
-        '    <i class="i i-arrow-grey-down"></i>' +
-        '  </button></div>';
-
     Filter.prototype.ready = function() {
-        this.on('click', '.js-live-toggler', this.toggle);
+        bean.on(qwery('.js-live-oldest')[0], 'click', this.toggle.bind(this, 'oldest'));
+        bean.on(qwery('.js-live-newest')[0], 'click', this.toggle.bind(this, 'newest'));
     };
 
-    Filter.prototype.toggle = function() {
-        var blocks = toArray($('.block', this.context).detach());
-        blocks.reverse();
-
-        this.toggleState('order-by-oldest');
-        bonzo(this.context).prepend(blocks);
-
-        mediator.emit('module:filter:toggle', this.hasState('order-by-oldest'));
+    Filter.prototype.toggle = function(order) {
+        bean.fire(qwery('button[data-toggle="popup--live-blog"]')[0], 'click');
+        if (this.order !== order) {
+            var blocks = toArray($('.block', this.context).detach());
+            blocks.reverse();
+            bonzo(this.context).prepend(blocks);
+            mediator.emit('module:filter:toggle', order === 'oldest');
+            this.order = order;
+        }
     };
 
     return Filter;
