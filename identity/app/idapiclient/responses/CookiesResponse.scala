c4dea250221c6c743e0178131f0c757d03e49d0b
Roberto Tyley <roberto.tyley@theguardian.com>
2014-09-30 12:06:18 +0100

    Respect the `sessionCookie` flag for the SC_GU_LA cookie [#MEM-403]
    This is part of the work to enforce that users are recently signed-in
    when they view their personal profile:
    
    https://profile.theguardian.com/account/edit
    
    The Identity API will now be proving Cookie info for the new SC_GU_LA
    cookie, which stores when the user last authenticated:
    
    https://github.com/guardian/identity/pull/176
    
    ...this change to NGW Frontend is required to get it to respect the new
    `sessionCookie` flag, so that the SC_GU_LA cookie is only session-scoped
    and dies when the user closes the browser.
    
    https://jira.gutools.co.uk/browse/MEM-403

--- identity/app/idapiclient/responses/CookiesResponse.scala
+++ identity/app/idapiclient/responses/CookiesResponse.scala
@@ -2,5 +2,8 @@ package idapiclient.responses
 
 import org.joda.time.DateTime
 
-case class CookieResponse(key: String, value: String)
+case class CookieResponse(key: String, value: String, sessionCookie: Option[Boolean]) {
+  val isSessionCookie = sessionCookie.getOrElse(false)
+}
+
 case class CookiesResponse(expiresAt: DateTime, values: List[CookieResponse])
