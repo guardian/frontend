eed47b1a98fcd617b340742a2bf9ecb460e22c51
Robert Berry <robert.berry@theguardian.com>
2014-10-01 17:37:18 +0100

    Add logic for custom cut out

--- common/app/views/support/CutOut.scala
+++ common/app/views/support/CutOut.scala
@@ -1,14 +1,16 @@
 package views.support
 
-import model.Trail
+import model.{ImageElement, Trail}
 
 object CutOut {
   def fromTrail(trail: Trail): Option[CutOut] = {
-    if (trail.isComment && trail.hasLargeContributorImage) {
-      for {
-        contributor <- trail.contributors.find(_.contributorLargeImagePath.isDefined)
-        imagePath <- contributor.contributorLargeImagePath
-      } yield CutOut(contributor.name, imagePath)
+    if (trail.isComment || trail.imageCutoutReplace) {
+      trail.customImageCutout map { case ImageElement(src, _, _) => CutOut("", src) } orElse {
+        for {
+          contributor <- trail.contributors.find(_.contributorLargeImagePath.isDefined)
+          imagePath <- contributor.contributorLargeImagePath
+        } yield CutOut(contributor.name, imagePath)
+      }
     } else {
       None
     }
