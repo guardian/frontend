c59426e3f9449b5e04c42b3d1e9181baa1a1a768
Alex Sanders <a@snd.rs>
2014-10-02 17:25:42 +0100

    fix test
    thanks @robertberry

--- common/test/views/support/ItemKickerTest.scala
+++ common/test/views/support/ItemKickerTest.scala
@@ -49,14 +49,14 @@ class ItemKickerTest extends FlatSpec with Matchers with OptionValues {
   "ItemKicker" should "prefer item level tag kicker to collection level section kicker" in {
     ItemKicker.fromTrail(
       createTrailFixture(showTag = true, showSection = false),
-      Config("").copy(showSections = true)
+      Some(Config("").copy(showSections = true))
     ).value shouldEqual TagKicker("Test Tag", "testtag")
   }
 
   it should "prefer item level section kicker to collection level tag kicker" in {
     ItemKicker.fromTrail(
       createTrailFixture(showTag = false, showSection = true),
-      Config("").copy(showTags = true)
+      Some(Config("").copy(showTags = true))
     ).value shouldEqual SectionKicker("Test Section", "/testsection")
   }
 }
