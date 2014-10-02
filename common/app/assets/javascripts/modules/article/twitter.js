be7b046e2a4b5e2653a56f088d6b80a3c788d2ce
Josh Holder <jholder112233@gmail.com>
2014-10-02 12:08:26 +0100

    Remove logs

--- common/app/assets/javascripts/modules/article/twitter.js
+++ common/app/assets/javascripts/modules/article/twitter.js
@@ -37,7 +37,6 @@ define([
         tweetElements.forEach( function(element) {
             var $el = bonzo(element);
             if(((bonzo(document.body).scrollTop() + (viewportHeight * 2.5)) > $el.offset().top) && (bonzo(document.body).scrollTop() < $el.offset().top)) {
-                console.log("Upgrade! Scroll: " + bonzo(document.body).scrollTop() + " / Element: " + $el.offset().top);
                 $(element).removeClass('js-tweet').addClass('twitter-tweet');
             }
         });
