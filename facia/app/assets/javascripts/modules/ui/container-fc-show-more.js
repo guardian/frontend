aff77c9a1c14a9f538a44e3474ee229dbfdc30ff
Robert Berry <robert.berry@theguardian.com>
2014-10-02 14:34:11 +0100

    Re-add my code that Patrick deleted

--- facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
+++ facia/app/assets/javascripts/modules/ui/container-fc-show-more.js
@@ -1,14 +1,18 @@
 define([
     'common/utils/$',
     'bonzo',
-    'bean'
+    'bean',
+    'qwery'
 ], function (
     $,
     bonzo,
-    bean
+    bean,
+    qwery
 ) {
     return function (container) {
         var $container = bonzo(container),
+            itemsHiddenOnDesktop = qwery('.js-hide', $container).length > 0,
+            itemsHiddenOnMobile = qwery('.js-hide-on-mobile', $container).length > 0,
             className = 'fc-show-more--hidden',
             $button = $.create(
             '<button class="collection__show-more tone-background tone-news" data-test-id="show-more" data-link-name="Show more | 1">' +
@@ -29,8 +33,15 @@ define([
             $container.removeClass(className);
         }
 
-        $button.appendTo($container);
-        bean.on($button[0], 'click', showMore);
-        $container.removeClass('js-container--fc-show-more');
+        if (itemsHiddenOnMobile || itemsHiddenOnDesktop) {
+            if (!itemsHiddenOnDesktop) {
+                $container.addClass('fc-show-more--mobile-only');
+            }
+
+            $container.addClass(className)
+                .append($button)
+                .removeClass('js-container--fc-show-more');
+            bean.on($button[0], 'click', showMore);
+        }
     };
 });
