d0ba7f32df6451e91663725b8e097a78c28167ff
Alex Sanders <a@snd.rs>
2014-10-03 16:53:26 +0100

    add orientation class to custom cutouts

--- common/app/views/support/CutOut.scala
+++ common/app/views/support/CutOut.scala
@@ -5,11 +5,16 @@ import model.{FaciaImageElement, Trail}
 object CutOut {
   def fromTrail(trail: Trail): Option[CutOut] = {
     if (trail.isComment || trail.imageCutoutReplace) {
-      trail.customImageCutout map { case FaciaImageElement(src, _, _) => CutOut("", src) } orElse {
+      trail.customImageCutout map {
+        case FaciaImageElement(src, width, height) => CutOut("", src, Orientation.fromDimensions(width, height))
+      } orElse {
+        /** We're assuming here that standard contributor images from CAPI are in landscape, as unfortunately they
+          * do not come with dimensions attached.
+          */
         for {
           contributor <- trail.contributors.find(_.contributorLargeImagePath.isDefined)
           imagePath <- contributor.contributorLargeImagePath
-        } yield CutOut(contributor.name, imagePath)
+        } yield CutOut(contributor.name, imagePath, Landscape)
       }
     } else {
       None
@@ -17,4 +22,18 @@ object CutOut {
   }
 }
 
-case class CutOut(altText: String, imageUrl: String)
+object Orientation {
+  def fromDimensions(width: Int, height: Int) = if (width > height) Landscape else Portrait
+}
+
+sealed trait Orientation
+
+case object Landscape extends Orientation
+case object Portrait extends Orientation
+
+case class CutOut(altText: String, imageUrl: String, orientation: Orientation) {
+  def cssClass = orientation match {
+    case Landscape => "image--landscape"
+    case Portrait => "image--portrait"
+  }
+}
