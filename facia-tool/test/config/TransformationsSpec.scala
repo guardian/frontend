dc24f172ff5193f359cf042e4ab0dbf49e0321f9
Robert Berry <robert.berry@theguardian.com>
2014-10-03 16:27:22 +0100

    Fix transformations spec

--- facia-tool/test/config/TransformationsSpec.scala
+++ facia-tool/test/config/TransformationsSpec.scala
@@ -73,6 +73,7 @@ import test.ConfiguredTestSuite
         imageWidth = None,
         imageHeight = None,
         isImageDisplayed = None,
+        isHidden = None,
         priority = Some("high")
       ))
   }
