a89ee5030f453a2737d31c056f790e00ea2f6ff9
Robert Berry <robert.berry@theguardian.com>
2014-10-06 16:21:28 +0100

    Add weird trim function to minimise whitespace in kickers

---
+++ common/app/views/support/KickerHtml.scala
@@ -0,0 +1,10 @@
+package views.support
+
+import play.twirl.api.{HtmlFormat, Html}
+
+/** Because I've given up on whitespace reduction in Play templates */
+object KickerHtml {
+  def trimAndAppend(left: Html, right: Html): Html = {
+    HtmlFormat.raw(left.body.trim + right.body.trim)
+  }
+}
