ebbe269802a3c3c37d107bbcd1aa03bb7e1108e2
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-10-08 13:47:49 +0100

    Combine NonThreaded controller into Comments controller

--- discussion/test/discussion/DiscussionApiTest.scala
+++ discussion/test/discussion/DiscussionApiTest.scala
@@ -1,6 +1,5 @@
 package discussion
 
-import controllers.DiscussionParams
 import discussion.model.DiscussionKey
 import org.scalatest.{DoNotDiscover, FreeSpec}
 import play.api.libs.ws.WSResponse
@@ -12,7 +11,7 @@ import scala.language.postfixOps
 @DoNotDiscover class DiscussionApiTest extends FreeSpec with ConfiguredTestSuite {
 
   "Should do GET request on correct URL for topComments " in {
-    val expectedUrl: String = "/discussion/p/3tycg/topcomments?pageSize=50&page=1&orderBy=newest&showSwitches=true"
+    val expectedUrl: String = "/discussion/p/3tycg/topcomments?pageSize=50&page=1&orderBy=newest&displayThreaded=true&showSwitches=true"
 
     val discussionApi = new DiscussionApi {
       override protected def GET(url: String, headers: (String, String)*): Future[WSResponse] = {
@@ -28,12 +27,14 @@ import scala.language.postfixOps
       page = "1",
       pageSize = "50",
       maxResponses = None,
-      topComments = true
+      topComments = true,
+      displayThreaded = true
     )), 2 seconds)
   }
 
   "Should do GET request on correct URL for comments " in {
-    val expectedUrl: String = "/discussion/p/3tycg?pageSize=50&page=1&orderBy=newest&showSwitches=true"
+    val expectedUrl: String = "/discussion/p/3tycg?pageSize=50&page=1&orderBy=newest&displayThreaded=true&showSwitches=true"
+
 
     val discussionApi = new DiscussionApi {
       override protected def GET(url: String, headers: (String, String)*): Future[WSResponse] = {
@@ -49,7 +50,8 @@ import scala.language.postfixOps
       page = "1",
       pageSize = "50",
       maxResponses = None,
-      topComments = false
+      topComments = false,
+      displayThreaded = true
     )), 2 seconds)
   }
 
