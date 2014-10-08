5110449ed0e2b36ac018baa30b7db27a04cf9935
Francis Carr <francis.carr@guardian.co.uk>
2014-10-08 11:06:10 +0100

    Update facia-scala-client dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val dfpAxis = "com.google.api-ads" % "dfp-axis" % "1.27.0"
   val dnaCommon = "org.jboss.dna" % "dna-common" % "0.6"
   val exactTargetClient = "com.gu" %% "exact-target-client" % "2.23"
-  val faciaScalaClient = "com.gu" %% "facia-api-client" % "0.10"
+  val faciaScalaClient = "com.gu" %% "facia-api-client" % "0.11"
   val flexibleContentBlockToText = "com.gu" %% "flexible-content-block-to-text" % "0.1"
   val flexibleContentBodyParser = "com.gu" %% "flexible-content-body-parser" % "0.3"
   val guardianConfiguration = "com.gu" %% "configuration" % "3.9"
