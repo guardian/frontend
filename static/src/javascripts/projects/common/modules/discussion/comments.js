49d4ab055d75ded1b6d85091de817ba9e0efb011
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-10-10 13:55:59 +0100

    Fix build break

--- static/src/javascripts/projects/common/modules/discussion/comments.js
+++ static/src/javascripts/projects/common/modules/discussion/comments.js
@@ -118,7 +118,6 @@ Comments.prototype.ready = function() {
     );
 
     if (this.options.commentId) {
-        var comment = $('#comment-'+ this.options.commentId);
         this.showHiddenComments();
         $('.d-discussion__show-all-comments').addClass('u-h');
         window.location.replace('#comment-'+ this.options.commentId);
