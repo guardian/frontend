ac5a9d999165b0ee63205b497cd4cd2f86ee52a2
Paul Robert Lloyd <paul.lloyd@theguardian.com>
2014-10-14 16:18:31 +0100

    Live blog tonal header fixes/simplification, showcase image fix

--- common/app/views/support/TrailCssClasses.scala
+++ common/app/views/support/TrailCssClasses.scala
@@ -5,14 +5,14 @@ import model.Trail
 object TrailCssClasses {
   def toneClass(trail: Trail) = {
     val tone = CardStyle(trail) match {
-      case Media => "media"
+      case Analysis => "analysis"
       case Comment => "comment"
-      case LiveBlog => "live"
-      case DeadBlog => "dead"
       case Editorial => "editorial"
       case Feature => "feature"
-      case Analysis => "analysis"
       case Review => "review"
+      case LiveBlog => "live"
+      case DeadBlog => "dead"
+      case Media => "media"
       case Podcast => "podcast"
       case _ => "news"
     }
@@ -24,6 +24,7 @@ object TrailCssClasses {
   val SupportedArticleTones: Set[CardStyle] = Set(
     Analysis,
     Comment,
+    Editorial,
     Feature,
     Review,
     LiveBlog,
