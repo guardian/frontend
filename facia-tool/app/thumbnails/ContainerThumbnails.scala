7f9016a16f586068893c85278a39b998d1d52430
Robert Berry <robert.berry@theguardian.com>
2014-10-18 07:54:03 +0100

    Tweak styling of SVGs

--- facia-tool/app/thumbnails/ContainerThumbnails.scala
+++ facia-tool/app/thumbnails/ContainerThumbnails.scala
@@ -3,47 +3,100 @@ package thumbnails
 import layout._
 import slices.{Slice, FixedContainers}
 
+case class Rectangle(x: Double, y: Double, width: Double, height: Double)
+
 object ContainerThumbnails {
   val SliceHeight = 25
+  // if rows are not stretched to accommodate another column
+  val BaseRowHeight = 7
   val Width = 100
 
-  val Style = "fill:#ccc;stroke:white;stroke-width:1"
+  val Style = "fill: #94b1ca; stroke: white; stroke-width: 1"
 
   def totalSpan(columns: Seq[Column]) = columns.map(_.colSpan).sum
 
-  def drawSlice(slice: Slice, index: Int) = {
+  def sliceHeight(slice: Slice) = {
+    slice.layout.columns match {
+      case Rows(_, _, rows, _) +: Nil => (rows * (BaseRowHeight + 1)) + 1
+      case _ => SliceHeight
+    }
+  }
+
+  def summing[A](as: Seq[A])(f: A => Double) = as.inits.toSeq.reverse map { xs =>
+    xs.map(f).sum
+  }
+
+  def drawSlice(slice: Slice, yPos: Double) = {
     val columns = slice.layout.columns
 
     val totalColSpan = totalSpan(columns)
     val unitSpanWidth = Width.toDouble / totalColSpan
 
-    val columnLeftSides = columns.inits.toSeq.reverse map { columnsSoFar =>
-      totalSpan(columnsSoFar) * unitSpanWidth
-    }
+    val columnLeftSides = summing(columns)(_.colSpan * unitSpanWidth)
 
     ((columns zip columnLeftSides) map { case (column, x) =>
-      drawColumn(column, x, index * SliceHeight, column.colSpan * unitSpanWidth, SliceHeight)
+      drawColumn(column, x, yPos, column.colSpan * unitSpanWidth, sliceHeight(slice))
     }).flatten
+  }
+
+  def drawImage(x: Double, y: Double, width: Double, height: Double, itemClasses: ItemClasses) = {
+    lazy val third = (width - 2) / 3
+
+    ((itemClasses.tablet match {
+      case "list-media" =>
+        Some(Rectangle(x + 1, y + 1, width / 2, height - 2))
+      case "three-quarters" =>
+        Some(Rectangle(x + 1 + third, y + 1, third * 2, height - 2))
+      case "three-quarters-right" =>
+        Some(Rectangle(x + 1, y + 1, third * 2, height - 2))
+      case "standard" | "half" | "third" =>
+        Some(Rectangle(x + 1, y + 1, width - 2, height * 2.0 / 3.0))
+      case _ =>
+        None
+    }) map { rectDef =>
+      <rect x={rectDef.x.toString}
+            y={rectDef.y.toString}
+            width={rectDef.width.toString}
+            height={rectDef.height.toString}
+            style="fill: #005689; stroke-width: 0"
+        />
+    }).toSeq
   }
 
   def drawColumn(column: Column, x: Double, y: Double, width: Double, height: Double) = {
     def box = <rect x={x.toString} y={y.toString} width={width.toString} height={height.toString} style={Style} />
 
-    column match {
-      case _: SingleItem => box
+    def image(itemClasses: ItemClasses) =
+      drawImage(x, y, width, height, itemClasses)
 
-      case Rows(_, columns, rows, _) =>
+    column match {
+      case SingleItem(_, itemClasses) =>
+        <g>
+          {box}
+          {image(itemClasses)}
+        </g>
+
+
+      case Rows(_, columns, rows, itemClasses) =>
         val rowWidth = width / columns.toDouble
         val rowHeight = height / rows.toDouble
 
         for {
           col <- 0 until columns
           row <- 0 until rows
-        } yield <rect x={(x + col * rowWidth).toString}
-                      y={(y + row * rowHeight).toString}
-                      width={rowWidth.toString}
-                      height={rowHeight.toString}
-                      style={Style} />
+        } yield {
+          val left = x + col * rowWidth
+          val top = y + row * rowHeight
+
+          <g>
+            <rect x={left.toString}
+                  y={top.toString}
+                  width={rowWidth.toString}
+                  height={rowHeight.toString}
+                  style={Style}/>
+            {drawImage(left, top, rowWidth, rowHeight, itemClasses)}
+          </g>
+        }
 
       case _: MPU =>
         val centreX = x + width / 2
@@ -71,11 +124,13 @@ object ContainerThumbnails {
 
   def fromId(id: String) = {
     FixedContainers.unapply(Some(id)) map { container =>
+      val yPositions = summing(container.slices)(sliceHeight)
+
       <svg xmlns="http://www.w3.org/2000/svg"
            xmlns:xlink="http://www.w3.org/1999/xlink"
            width={(Width + 2).toString}
            height={((container.slices.length * SliceHeight) + 2).toString}>
-        {container.slices.zipWithIndex.map((drawSlice _).tupled)}
+        {container.slices.zip(yPositions).map((drawSlice _).tupled)}
       </svg>
     }
   }
