e35a4f7952a140d7861a41bf4dfed9adb5eedb58
Steve Vadocz <steve.vadocz@guardian.co.uk>
2014-10-18 08:14:53 +0100

    Updated pagination algorithm.

--- common/app/common/Pagination.scala
+++ common/app/common/Pagination.scala
@@ -5,6 +5,7 @@ case class Pagination(currentPage: Int, lastPage: Int, totalContent: Int) {
   val next: Option[Int] = if (lastPage > currentPage) Some(currentPage + 1) else None
   val previous: Option[Int] = if (currentPage > 1) Some(currentPage -1) else None
   val isFirstPage: Boolean = currentPage == 1
+  val isLastPage: Boolean = currentPage == totalContent
 
   /**
    * Returns the next/prev 5 navigation pages with the current page as close to the center as possible.
@@ -14,13 +15,22 @@ case class Pagination(currentPage: Int, lastPage: Int, totalContent: Int) {
    * if the current page is 10 (and it is the last page) 6,7,8,9,10
    */
   lazy val pages: Seq[Int] = {
-    def distanceFromCenter(i: Seq[Int]) = math.abs(2 - i.indexOf(currentPage))
-    val lowerBoundry = math.max(currentPage - 4, 1)
-    val upperBoundry = math.min(lastPage + 1, currentPage + 5)
+    def distanceFromCenter(i: Seq[Int]) = math.abs((2 - (getOffset - 1)) - i.indexOf(currentPage))
+    val lowerBoundry = math.max(currentPage - 4 - getOffset, 1)
+    val upperBoundry = math.min(lastPage + 1, currentPage + 5 - getOffset)
+
+    println(totalContent, getOffset, currentPage)
 
     Range(lowerBoundry, upperBoundry)
-      .sliding(5)
+      .sliding(5 - getOffset)
       .toSeq
       .sortBy(distanceFromCenter).head
   }
+
+  def getOffset: Int =
+    if (totalContent > 5) {
+      if (isFirstPage || currentPage < 4) 1
+      else if (isLastPage || currentPage > totalContent - 3) 1
+        else 2
+    } else 0
 }
