542710c5caabe9a8f257c1a1693f6360134ef582
Steve Vadocz <steve.vadocz@guardian.co.uk>
2014-10-20 15:14:11 +0100

    Working algorithm.

--- common/app/common/Pagination.scala
+++ common/app/common/Pagination.scala
@@ -16,16 +16,42 @@ case class Pagination(currentPage: Int, lastPage: Int, totalContent: Int) {
    * Page 4-7:  4 5 6
    */
   lazy val pages: Seq[Int] = {
-    def distanceFromCenter(i: Seq[Int]) = math.abs(1 - i.indexOf(currentPage))
-    val lowerBoundry = math.max(currentPage - 3, 1)
-    val upperBoundry = math.min(lastPage + 1, currentPage + 4)
+    def distanceFromCenter(i: Seq[Int]) = math.abs(2 - getOffset - i.indexOf(currentPage))
+    val lowerBoundry = math.max(currentPage - 4 + getOffset, 1)
+    val upperBoundry = math.min(lastPage + 1, currentPage + 5 - getOffset)
 
 
-    if (currentPage <= 3) 1 to 4
-    else if (currentPage >= lastPage - 2) (currentPage - 3 to lastPage).takeRight(4)
+    /**
+     * Make sure that edge cases are handled.
+     *
+     * If we have only 5 or less pages we want to show whole range.
+     * Example:
+     *
+     * 1 2 3 for 3 pages
+     * 1 2 3 4 5 for 5 pages
+     *
+     * If we have 5+ pages we want to show only 4 pages on the edges
+     * Example:
+     *
+     * 1 2 3 4 ... 10 if we are on the one of the first 3 pages
+     * 1 ... 7 8 9 10 if we are on the one of the last 3 pages
+     * 1 ... 4 5 6 ... 10 if we are somewhere in between
+     */
+    if (currentPage <= 3 && lastPage > 5) 1 to 4
+    else if (currentPage >= lastPage - 2 && lastPage > 5) (currentPage - 3 to lastPage).takeRight(4)
     else Range(lowerBoundry, upperBoundry)
-      .sliding(3)
+      .sliding(5 - (getOffset * 2))
       .toSeq
       .sortBy(distanceFromCenter).head
+  }
+
+  /**
+   * In the case of having just 5 or less pages we want range to be 1-5
+   * In the case of having 5+ pages we want range to be 1-3
+   *
+   * This function is returning proper offset to use in page range calculation above
+   */
+  def getOffset: Int = {
+    if (lastPage > 5) 1 else 0
   }
 }
