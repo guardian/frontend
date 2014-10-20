379fdb56b2a98382c6ece007498452e5c8ff6db7
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-10-20 15:40:24 +0100

    fix test after rebase

--- static/test/javascripts/spec/common/commercial/dfp.spec.js
+++ static/test/javascripts/spec/common/commercial/dfp.spec.js
@@ -248,7 +248,7 @@ define([
                         name: 'right',
                         type: 'mpu-banner-ad',
                         html: '<div id="dfp-ad--right" class="ad-slot ad-slot--dfp ad-slot--right ad-slot--mpu-banner-ad" ' +
-                            'data-link-name="ad slot right" data-test-id="ad-slot-right" data-name="right" data-mobile="300,250|300,600"></div>'
+                            'data-link-name="ad slot right" data-test-id="ad-slot-right" data-name="right" data-mobile="300,250|300,251|300,600"></div>'
                     },
                     {
                         name: 'im',
@@ -262,7 +262,7 @@ define([
                         type: 'inline',
                         html: '<div id="dfp-ad--inline1" class="ad-slot ad-slot--dfp ad-slot--inline1 ad-slot--inline" ' +
                             'data-link-name="ad slot inline1" data-test-id="ad-slot-inline1" data-name="inline1" data-mobile="300,50" ' +
-                            'data-mobile-landscape="300,50|320,50" data-tablet="300,250"></div>'
+                            'data-mobile-landscape="300,50|320,50" data-tablet="300,250" data-desktop="300,1|300,250"></div>'
                     },
                     {
                         name: 'inline2',
