c3f79b366e1bc90193eecf496fa470ba2388f507
Austin Harris <austin@dotmail.co.uk>
2014-10-20 14:39:09 +0100

    adding image to template

--- commercial/app/controllers/commercial/ContentApiOffers.scala
+++ commercial/app/controllers/commercial/ContentApiOffers.scala
@@ -12,6 +12,8 @@ object ContentApiOffers extends Controller with ExecutionContexts {
 
   private def renderItems(format: Format) = MemcachedAction { implicit request =>
     val optKeyword = request.queryString get "k" map (_.head)
+    
+    val optLogo = request.queryString get "l" map (_.head)
 
     val futureLatestByKeyword = optKeyword.map { keyword =>
       Lookup.latestContentByKeyword(keyword, 4)
@@ -25,7 +27,7 @@ object ContentApiOffers extends Controller with ExecutionContexts {
     futureContents map {
       case Nil => NoCache(format.nilResult)
       case contents => Cached(componentMaxAge) {
-        format.result(views.html.contentapi.items(contents))
+        format.result(views.html.contentapi.items(contents, optLogo))
       }
     }
   }
