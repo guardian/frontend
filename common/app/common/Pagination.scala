c3db1d2a61a0b0d56647abfadde9d4ce85ffcb88
Steve Vadocz <steve.vadocz@guardian.co.uk>
2014-10-20 14:14:57 +0100

    Pagination algorithm changed to better handle edge cases.

--- common/app/common/Pagination.scala
+++ common/app/common/Pagination.scala
@@ -5,32 +5,27 @@ case class Pagination(currentPage: Int, lastPage: Int, totalContent: Int) {
   val next: Option[Int] = if (lastPage > currentPage) Some(currentPage + 1) else None
   val previous: Option[Int] = if (currentPage > 1) Some(currentPage -1) else None
   val isFirstPage: Boolean = currentPage == 1
-  val isLastPage: Boolean = currentPage == totalContent
 
   /**
-   * Returns the next/prev 5 navigation pages with the current page as close to the center as possible.
-   * e.g.
-   * if the current page is 6 then 4,5,6,7,8
-   * if the current page is 1 then 1,2,3,4,5
-   * if the current page is 10 (and it is the last page) 6,7,8,9,10
+   * Returns the next/prev 3 navigation pages with the current page as close to the center as possible.
+   * For first and last 4 pages it shows 4 pages.
+   *
+   * For 10 pages:
+   * Page 1-3:  1 2 3 4
+   * Page 8-10: 7 8 9 10
+   * Page 4-7:  4 5 6
    */
   lazy val pages: Seq[Int] = {
-    def distanceFromCenter(i: Seq[Int]) = math.abs((2 - (getOffset - 1)) - i.indexOf(currentPage))
-    val lowerBoundry = math.max(currentPage - 4 - getOffset, 1)
-    val upperBoundry = math.min(lastPage + 1, currentPage + 5 - getOffset)
+    def distanceFromCenter(i: Seq[Int]) = math.abs(1 - i.indexOf(currentPage))
+    val lowerBoundry = math.max(currentPage - 3, 1)
+    val upperBoundry = math.min(lastPage + 1, currentPage + 4)
 
-    println(totalContent, getOffset, currentPage)
 
-    Range(lowerBoundry, upperBoundry)
-      .sliding(5 - getOffset)
+    if (currentPage <= 3) 1 to 4
+    else if (currentPage >= lastPage - 2) (currentPage - 3 to lastPage).takeRight(4)
+    else Range(lowerBoundry, upperBoundry)
+      .sliding(3)
       .toSeq
       .sortBy(distanceFromCenter).head
   }
-
-  def getOffset: Int =
-    if (totalContent > 5) {
-      if (isFirstPage || currentPage < 4) 1
-      else if (isLastPage || currentPage > totalContent - 3) 1
-        else 2
-    } else 0
 }
