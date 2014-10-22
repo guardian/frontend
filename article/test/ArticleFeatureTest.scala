6c4b977ed1457c33b62535cd7ee2d34b58bf4129
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-10-22 15:54:10 +0100

    Use css name for social data link

--- article/test/ArticleFeatureTest.scala
+++ article/test/ArticleFeatureTest.scala
@@ -489,10 +489,10 @@ import collection.JavaConversions._
       goTo("/film/2012/nov/11/margin-call-cosmopolis-friends-with-kids-dvd-review") { browser =>
         import browser._
 
-        val mailShareUrl = s"mailto:?subject=Mark%20Kermode%27s%20DVD%20round-up&body=http%3A%2F%2Flocalhost%3A${port}%2Ffilm%2F2012%2Fnov%2F11%2Fmargin-call-cosmopolis-friends-with-kids-dvd-review"
-        val fbShareUrl = s"https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A${port}%2Ffilm%2F2012%2Fnov%2F11%2Fmargin-call-cosmopolis-friends-with-kids-dvd-review&ref=responsive"
-        val twitterShareUrl = "https://twitter.com/intent/tweet?text=Mark+Kermode%27s+DVD+round-up&url=http%3A%2F%2Fgu.com%2Fp%2F3bk2f%2Ftw"
-        val gplusShareUrl = s"https://plus.google.com/share?url=http%3A%2F%2Flocalhost%3A${port}%2Ffilm%2F2012%2Fnov%2F11%2Fmargin-call-cosmopolis-friends-with-kids-dvd-review&hl=en-GB&wwc=1"
+        val mailShareUrl = "mailto:?subject=Mark+Kermode%27s+DVD+round-up&amp;body=http%3A%2F%2Fgu.com%2Fp%2F3bk2f%2Fsbl"
+        val fbShareUrl = "https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fgu.com%2Fp%2F3bk2f%2Fsfb&ref=responsive"
+        val twitterShareUrl = "https://twitter.com/intent/tweet?text=Mark+Kermode%27s+DVD+round-up&url=http%3A%2F%2Fgu.com%2Fp%2F3bk2f%2Fstw"
+        val gplusShareUrl = "https://plus.google.com/share?url=http%3A%2F%2Fgu.com%2Fp%2F3bk2f%2Fsgp&amp;hl=en-GB&amp;wwc=1"
 
         Then("I should see buttons for my favourite social network")
         findFirst(".social__item[data-link-name=email] .social__action").getAttribute("href") should be(mailShareUrl)
