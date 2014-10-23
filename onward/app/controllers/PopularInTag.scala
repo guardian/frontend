f135961be62512f49761d020f60471bc0b50e3aa
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-10-23 11:57:01 +0100

    Refactor popular in media container to use new facia pattern

--- onward/app/controllers/PopularInTag.scala
+++ onward/app/controllers/PopularInTag.scala
@@ -1,9 +1,12 @@
 package controllers
 
+import com.gu.facia.client.models.CollectionConfig
 import common._
+import layout.ContainerLayout
 import model._
 import play.api.mvc.{ RequestHeader, Controller, Action }
 import services._
+import slices.FixedContainers
 
 object PopularInTag extends Controller with Related with Logging with ExecutionContexts {
 
@@ -15,9 +18,15 @@ object PopularInTag extends Controller with Related with Logging with ExecutionC
     }
   }
 
-  private def renderPopularInTag(trails: Seq[Trail])(implicit request: RequestHeader) = Cached(600) {
+  private def renderPopularInTag(trails: Seq[Content])(implicit request: RequestHeader) = Cached(600) {
+    val dataId: String = "related content"
+    val displayName = Some(dataId)
+    val properties = FrontProperties.empty
+    val collection = Collection(trails.take(8), displayName)
+    val layout = ContainerLayout(FixedContainers.all("fixed/medium/fast-XII"), collection, None)
+    val config = CollectionConfig.withDefaults(displayName = displayName)
 
-    val html = views.html.fragments.relatedTrails(trails, "Related content", 10)
+    val html = views.html.fragments.containers.facia_cards.container(collection, layout, 1, properties, dataId)(request, new views.support.TemplateDeduping, config)
 
     JsonComponent(
       "html" -> html
