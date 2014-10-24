bd224b9250c7e27e6406a7afcbcebaa9372fc6ea
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-10-24 17:27:49 +0100

    Styling for new most popular container

--- onward/app/controllers/MostViewedAudioController.scala
+++ onward/app/controllers/MostViewedAudioController.scala
@@ -1,9 +1,12 @@
 package controllers
 
+import com.gu.facia.client.models.CollectionConfig
 import common._
-import model.{Audio, Cached}
+import layout.ContainerLayout
+import model.{Collection, FrontProperties, Audio, Cached}
 import play.api.mvc.{RequestHeader, Controller, Action}
 import feed.MostViewedAudioAgent
+import slices.FixedContainers
 import views.support.TemplateDeduping
 
 object MostViewedAudioController extends Controller with Logging with ExecutionContexts {
@@ -23,8 +26,15 @@ object MostViewedAudioController extends Controller with Logging with ExecutionC
   }
 
   private def renderMostViewedAudio(audios: Seq[Audio])(implicit request: RequestHeader) = Cached(900) {
+    val dataId = "audio/most-viewed"
+    val displayName = Some("popular in audio")
+    val properties = FrontProperties.empty
+    val collection = Collection(audios.take(8), displayName)
+    val layout = ContainerLayout(FixedContainers.all("fixed/medium/fast-XII"), collection, None)
+    val config = CollectionConfig.withDefaults(displayName = displayName)
 
-    val html = views.html.fragments.mostViewedAudio(audios)
+    val html = views.html.fragments.containers.facia_cards.container(collection, layout, 0, properties, dataId)(request, new views.support.TemplateDeduping, config)
+
     JsonComponent(
       "html" -> html
     )
