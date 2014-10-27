fb8f4ca77d013ce53f22673d6688d8b727763f4f
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-10-27 15:56:45 +0000

    Revert changes to most popular audio

--- onward/app/controllers/MostViewedAudioController.scala
+++ onward/app/controllers/MostViewedAudioController.scala
@@ -1,12 +1,9 @@
 package controllers
 
-import com.gu.facia.client.models.CollectionConfig
 import common._
-import layout.ContainerLayout
-import model.{Collection, FrontProperties, Audio, Cached}
+import model.{Audio, Cached}
 import play.api.mvc.{RequestHeader, Controller, Action}
 import feed.MostViewedAudioAgent
-import slices.FixedContainers
 import views.support.TemplateDeduping
 
 object MostViewedAudioController extends Controller with Logging with ExecutionContexts {
@@ -26,15 +23,8 @@ object MostViewedAudioController extends Controller with Logging with ExecutionC
   }
 
   private def renderMostViewedAudio(audios: Seq[Audio])(implicit request: RequestHeader) = Cached(900) {
-    val dataId = "audio/most-viewed"
-    val displayName = Some("popular in audio")
-    val properties = FrontProperties.empty
-    val collection = Collection(audios.take(8), displayName)
-    val layout = ContainerLayout(FixedContainers.all("fixed/medium/fast-XII"), collection, None)
-    val config = CollectionConfig.withDefaults(displayName = displayName)
 
-    val html = views.html.fragments.containers.facia_cards.container(collection, layout, 0, properties, dataId)(request, new views.support.TemplateDeduping, config)
-
+    val html = views.html.fragments.mostViewedAudio(audios)
     JsonComponent(
       "html" -> html
     )
