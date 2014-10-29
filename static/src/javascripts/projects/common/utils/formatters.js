4f1806c86b82d4716ccce477a8e39b99e7b75603
Matt Osborn <matthew.osborn@theguardian.com>
2014-10-29 14:22:13 +0000

    linting..

--- static/src/javascripts/projects/common/utils/formatters.js
+++ static/src/javascripts/projects/common/utils/formatters.js
@@ -1,10 +1,11 @@
 define(function () {
     return {
         integerCommas: function (val) {
-            var num = parseInt(val, 10);
+            var num = parseInt(val, 10),
+                digits, i;
             if (!isNaN(num)) {
-                var digits = num.toFixed(0).split('');
-                for (var i = digits.length - 1; i >= 3; i--) {
+                digits = num.toFixed(0).split('');
+                for (i = digits.length - 1; i >= 3; i--) {
                     if (i % 3 === 0) {
                         digits.splice(digits.length - i, 0, ',');
                     }
