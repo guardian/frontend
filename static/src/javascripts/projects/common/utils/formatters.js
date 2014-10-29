7c6ac6aed958b45f12463101fb1470b2bda0d892
Matt Osborn <matthew.osborn@theguardian.com>
2014-10-29 14:20:42 +0000

    fixing comma formatter

--- static/src/javascripts/projects/common/utils/formatters.js
+++ static/src/javascripts/projects/common/utils/formatters.js
@@ -1,9 +1,15 @@
 define(function () {
     return {
         integerCommas: function (val) {
-            if (typeof (val) === 'string' || typeof (val) === 'number') {
-                var strVal = typeof (val) === 'string' ? val : val.toFixed(0);
-                return strVal.length < 4 ? strVal : strVal.substr(0, strVal.length - 3) + ',' + strVal.substr(-3);
+            var num = parseInt(val, 10);
+            if (!isNaN(num)) {
+                var digits = num.toFixed(0).split('');
+                for (var i = digits.length - 1; i >= 3; i--) {
+                    if (i % 3 === 0) {
+                        digits.splice(digits.length - i, 0, ',');
+                    }
+                }
+                return digits.join('');
             }
         }
     };
