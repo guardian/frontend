a6da87170f6d0ab3e36ee7aa7f0ba9b4b48c9488
Matt Osborn <matthew.osborn@theguardian.com>
2014-10-29 12:23:34 +0000

    validation

--- static/src/javascripts/projects/common/utils/formatters.js
+++ static/src/javascripts/projects/common/utils/formatters.js
@@ -1,13 +1,10 @@
 define(function () {
     return {
-        integerCommas: function(val) {
-            if (typeof(val) === 'string' || typeof(val) === 'number') {
-                var strVal = typeof(val) === 'string' ? val : val.toFixed(0);
+        integerCommas: function (val) {
+            if (typeof (val) === 'string' || typeof (val) === 'number') {
+                var strVal = typeof (val) === 'string' ? val : val.toFixed(0);
                 return strVal.length < 4 ? strVal : strVal.substr(0, strVal.length - 3) + ',' + strVal.substr(-3);
             }
-
         }
-    }
+    };
 });
-
-
