a8b39084ba66c1b0e1fc325c209952063bf89b85
Robert Berry <robert.berry@theguardian.com>
2014-11-02 09:17:23 +0000

    Simplify item template

--- common/app/layout/Sublink.scala
+++ common/app/layout/Sublink.scala
@@ -1,6 +1,25 @@
 package layout
 
+import common.LinkTo
+import model.Trail
+import play.api.mvc.RequestHeader
 import views.support.ItemKicker
 
+object Sublink {
+  def fromTrail(trail: Trail) =
+    Sublink(ItemKicker.fromTrail(trail, None), trail.headline, { rh: RequestHeader =>
+      LinkTo(trail)(rh)
+    })
+}
 
-case class Sublink(kicker: ItemKicker, text: String, href: String)
+case class Sublink(kicker: Option[ItemKicker], text: String, url: RequestHeader => Option[String])
+
+
+
+case class FaciaCard(
+  id: String,
+
+  itemTypes: ItemClasses,
+
+  sublinks: Seq[Sublink]
+)
