b351621dddd2623309fdcf39adff334c733199ea
Robert Berry <robert.berry@theguardian.com>
2014-11-02 08:58:41 +0000

    Remove unnecessary lens

--- common/app/layout/FrontPostProcessing.scala
+++ common/app/layout/FrontPostProcessing.scala
@@ -2,7 +2,7 @@ package layout
 
 import views.support.CutOut
 
-import scalaz.{Lens, Traverse}
+import scalaz.Traverse
 import scalaz.std.list._
 import scalaz.std.option._
 
@@ -25,29 +25,26 @@ import scalaz.std.option._
 */
 
 object FrontPostProcessing {
-  val layoutStructureLens = Lens.lensu[Front, List[Option[List[List[List[Card]]]]]](
-    set = { (front, structure) =>
-      val newContainers = (front.containers zip structure) map { case (containerAndCollection, structure1) =>
-        val newLayout = (containerAndCollection.containerLayout zip structure1) map { case (layout, structure2) =>
-          val newSlices = (layout.slices zip structure2) map { case (slices, structure3) =>
-            val newColumns = (slices.columns zip structure3) map { case (column, structure4) =>
-              column.copy(cards = structure4)
-            }
-            slices.copy(columns = newColumns)
+  private def setStructure(front: Front, structure: List[Option[List[List[List[Card]]]]]) = {
+    val newContainers = (front.containers zip structure) map { case (containerAndCollection, structure1) =>
+      val newLayout = (containerAndCollection.containerLayout zip structure1) map { case (layout, structure2) =>
+        val newSlices = (layout.slices zip structure2) map { case (slices, structure3) =>
+          val newColumns = (slices.columns zip structure3) map { case (column, structure4) =>
+            column.copy(cards = structure4)
           }
-          layout.copy(slices = newSlices)
+          slices.copy(columns = newColumns)
         }
-        containerAndCollection.copy(containerLayout = newLayout.headOption)
+        layout.copy(slices = newSlices)
       }
-      front.copy(containers = newContainers)
-    },
-    get = { front =>
-      front.containers.toList.map(
-        _.containerLayout.map(
-          _.slices.toList.map(
-            _.columns.toList.map(_.cards.toList))))
+      containerAndCollection.copy(containerLayout = newLayout.headOption)
     }
-  )
+    front.copy(containers = newContainers)
+  }
+
+  private def getStructure(front: Front) = front.containers.toList.map(
+    _.containerLayout.map(
+      _.slices.toList.map(
+        _.columns.toList.map(_.cards.toList))))
 
   val layoutStructureTraverse =
     Traverse[List] compose
@@ -57,18 +54,16 @@ object FrontPostProcessing {
       Traverse[List]
 
   def deduplicateCutouts(front: Front) = {
-    layoutStructureLens.mod({ structure =>
-      layoutStructureTraverse.mapAccumL(structure, Set.empty[CutOut]) { (cutOutsSeen, card) =>
-        val newCard = if (card.cutOut.exists(cutOutsSeen.contains)) {
-          card.copy(cutOut = None)
-        } else {
-          card
-        }
+    val structure = getStructure(front)
 
-        (cutOutsSeen ++ card.cutOut.toSet, newCard)
+    setStructure(front, layoutStructureTraverse.mapAccumL(structure, Set.empty[CutOut]) { (cutOutsSeen, card) =>
+      val newCard = if (card.cutOut.exists(cutOutsSeen.contains)) {
+        card.copy(cutOut = None)
+      } else {
+        card
       }
 
-      structure
-    }, front)
+      (cutOutsSeen ++ card.cutOut.toSet, newCard)
+    }._2)
   }
 }
