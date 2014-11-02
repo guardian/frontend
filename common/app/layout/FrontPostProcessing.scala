edc3423b5254245c650e0def1fc7c64487dd35e7
Robert Berry <robert.berry@theguardian.com>
2014-11-02 09:01:33 +0000

    Simplify further

--- common/app/layout/FrontPostProcessing.scala
+++ common/app/layout/FrontPostProcessing.scala
@@ -26,22 +26,18 @@ import scalaz.std.option._
 
 object FrontPostProcessing {
   private def setStructure(front: Front, structure: List[Option[List[List[List[Card]]]]]) = {
-    val newContainers = (front.containers zip structure) map { case (containerAndCollection, structure1) =>
-      val newLayout = (containerAndCollection.containerLayout zip structure1) map { case (layout, structure2) =>
-        val newSlices = (layout.slices zip structure2) map { case (slices, structure3) =>
-          val newColumns = (slices.columns zip structure3) map { case (column, structure4) =>
+    front.copy(containers = (front.containers zip structure) map { case (containerAndCollection, structure1) =>
+      containerAndCollection.copy(containerLayout = ((containerAndCollection.containerLayout zip structure1) map { case (layout, structure2) =>
+        layout.copy(slices = (layout.slices zip structure2) map { case (slices, structure3) =>
+          slices.copy(columns = (slices.columns zip structure3) map { case (column, structure4) =>
             column.copy(cards = structure4)
-          }
-          slices.copy(columns = newColumns)
-        }
-        layout.copy(slices = newSlices)
-      }
-      containerAndCollection.copy(containerLayout = newLayout.headOption)
-    }
-    front.copy(containers = newContainers)
+          })
+        })
+      }).headOption)
+    })
   }
 
-  private def getStructure(front: Front) = front.containers.toList.map(
+  private def getStructure(front: Front): List[Option[List[List[List[Card]]]]] = front.containers.toList.map(
     _.containerLayout.map(
       _.slices.toList.map(
         _.columns.toList.map(_.cards.toList))))
