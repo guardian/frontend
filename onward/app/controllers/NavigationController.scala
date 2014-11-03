0add9ceab1e8a2669f1d12b32e416e93cb705762
Grant Klopper <grant.klopper@theguardian.com>
2014-11-03 17:56:28 +0000

    That needed a rename

--- onward/app/controllers/NavigationController.scala
+++ onward/app/controllers/NavigationController.scala
@@ -8,23 +8,23 @@ import play.api.mvc.{Action, Controller}
 
 object NavigationController extends Controller {
 
-  private case class SectionLinkAndEdition(item: SectionLink, edition: Edition)
-  private case class NavItemAndEdition(item: NavItem, edition: Edition)
+  private case class SectionLinkAndEdition(link: SectionLink, edition: Edition)
+  private case class NavItemAndEdition(link: NavItem, edition: Edition)
 
   def nav() = Action{ implicit request =>
     Cached(500) {
 
       implicit val sectionLinkWrites = new Writes[SectionLinkAndEdition] {
         def writes(item: SectionLinkAndEdition) = obj(
-          "title" -> item.item.title,
-          "href" -> LinkTo(item.item.href, item.edition)
+          "title" -> item.link.title,
+          "href" -> LinkTo(item.link.href, item.edition)
         )
       }
 
       implicit val navItemWrites = new Writes[NavItemAndEdition] {
         def writes(item: NavItemAndEdition) = obj(
-          "section" -> SectionLinkAndEdition(item.item.name, item.edition),
-          "subSections" -> item.item.links.map(link => SectionLinkAndEdition(link, item.edition))
+          "section" -> SectionLinkAndEdition(item.link.name, item.edition),
+          "subSections" -> item.link.links.map(link => SectionLinkAndEdition(link, item.edition))
         )
       }
 
