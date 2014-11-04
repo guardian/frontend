f90956560b6e31b506ec9478953da2c2bb053442
Robert Berry <robert.berry@theguardian.com>
2014-11-04 21:10:27 +0000

    Fix indentation

--- static/src/javascripts/projects/common/modules/crosswords/thumbnails.js
+++ static/src/javascripts/projects/common/modules/crosswords/thumbnails.js
@@ -31,29 +31,30 @@ define([
                     crossOrigin: true,
                     success: function (data) {
                         var cells = _.flatten(_.map(_.range(columns), function (column) {
-                            return _.map(_.range(rows), function (row) {
-                                var enteredText = savedState[column][row],
-                                    el,
-                                    top,
-                                    left;
+                                return _.map(_.range(rows), function (row) {
+                                    var enteredText = savedState[column][row],
+                                        el,
+                                        top,
+                                        left;
 
-                                if (enteredText) {
-                                    el = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-                                    top = row * 31 + 1;
-                                    left = column * 31 + 1;
+                                    if (enteredText) {
+                                        el = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+                                        top = row * 31 + 1;
+                                        left = column * 31 + 1;
 
-                                    bonzo(el).attr({
-                                        x: left + 15,
-                                        y: top + 19,
-                                        className: 'crossword__cell-text'
-                                    }).text(enteredText);
+                                        bonzo(el).attr({
+                                            x: left + 15,
+                                            y: top + 19,
+                                            className: 'crossword__cell-text'
+                                        }).text(enteredText);
 
-                                    return [el];
-                                } else {
-                                    return [];
-                                }
-                            });
-                        })), svg = qwery('svg', data)[0];
+                                        return [el];
+                                    } else {
+                                        return [];
+                                    }
+                                });
+                            })),
+                            svg = qwery('svg', data)[0];
 
                         bonzo(svg).append(cells);
 
