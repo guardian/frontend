02619cb691350f08d8b5e3ea9abc48baf9460323
Robert Berry <robert.berry@theguardian.com>
2014-11-06 16:31:34 +0000

    Add class for tone to most popular items

--- common/app/views/support/TrailCssClasses.scala
+++ common/app/views/support/TrailCssClasses.scala
@@ -8,25 +8,5 @@ object TrailCssClasses {
 
   def toneClass(trail: Trail) = toneClassFromStyle(CardStyle(trail))
 
-  /** Article will soon support all tone classes so we'll be able to remove this silliness */
-  val SupportedArticleTones: Set[CardStyle] = Set(
-    Analysis,
-    Comment,
-    Letters,
-    Editorial,
-    DeadBlog,
-    Feature,
-    LiveBlog,
-    Media,
-    Podcast,
-    Review
-  )
-
-  def articleToneClass(trail: Trail) = {
-    if (SupportedArticleTones.contains(CardStyle(trail))) {
-      toneClass(trail)
-    } else {
-      "tone-news"
-    }
-  }
+  def articleToneClass(trail: Trail) = toneClass(trail)
 }
