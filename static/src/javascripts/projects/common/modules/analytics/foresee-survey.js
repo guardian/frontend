1fe3341e646fbb44e44b78e4572ae2f6e3e5c9e1
Matt Osborn <matthew.osborn@theguardian.com>
2014-11-06 12:58:36 +0000

    open foresee when ready

--- static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
+++ static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
@@ -12,13 +12,12 @@ define([
 
     function load() {
 
-        var sampleRate = detect.getBreakpoint() === 'mobile' ? 0.08 : 0.006; // 8% mobile & 0.6% >mobile
-
-        var sample = Math.random() <= sampleRate,
+        var sampleRate = detect.getBreakpoint() === 'mobile' ? 0.08 : 0.006, // 8% mobile & 0.6% >mobile
+            sample = Math.random() <= sampleRate,
             hasForcedOptIn = /forceForesee/.test(location.hash);
 
         // the Foresee code is large, we only want to load it in when necessary.
-        if (!Cookie.get('GU_TEST') && (sample || hasForcedOptIn)) {
+        if (!Cookie.get('GU_TEST') && (window.openForeseeWhenReady || sample || hasForcedOptIn)) {
             openForesee();
         }
 
