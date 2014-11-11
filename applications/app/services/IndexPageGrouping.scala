ab1f06e08f3a1483810b94e0d0a35ed765c1667c
Robert Berry <robert.berry@theguardian.com>
2014-11-11 13:39:33 +0000

    Never group by year

--- applications/app/services/IndexPageGrouping.scala
+++ applications/app/services/IndexPageGrouping.scala
@@ -11,7 +11,7 @@ import common.Maps.RichMapSeq
 case class TrailAndDate(trail: Content, date: LocalDate)
 
 object IndexPageGrouping extends Collections {
-  val MinimumPopOutFrequency = 2
+  val MinimumPerDayPopOutFrequency = 2
 
   def fromContent(trails: Seq[Content], timezone: DateTimeZone): Seq[IndexPageGrouping] = {
     val trailsAndDates = trails.zipWith(_.webPublicationDate.withZone(timezone).toLocalDate).map(TrailAndDate.tupled)
@@ -19,21 +19,17 @@ object IndexPageGrouping extends Collections {
     trailsAndDates.groupBy(_.date.withDayOfYear(1)).toSeq.sortBy(_._1).reverse flatMap { case (startOfYear, trailsThatYear) =>
       val trailsByMonth = trailsThatYear.groupBy(_.date.withDayOfMonth(1))
 
-      if (trailsByMonth.meanFrequency >= MinimumPopOutFrequency) {
-        trailsByMonth.toSeq.sortBy(_._1).reverse flatMap {
-          case (startOfMonth, trailsThatMonth) =>
-            val trailsByDay = trailsThatMonth.groupBy(_.date)
+      trailsByMonth.toSeq.sortBy(_._1).reverse flatMap {
+        case (startOfMonth, trailsThatMonth) =>
+          val trailsByDay = trailsThatMonth.groupBy(_.date)
 
-            if (trailsByDay.meanFrequency >= MinimumPopOutFrequency) {
-              trailsByDay.toSeq.sortBy(_._1).reverse map { case (day, trailsThatDay) =>
-                Day(day, trailsThatDay.map(_.trail))
-              }
-            } else {
-              Seq(Month(startOfMonth, trailsThatMonth.map(_.trail)))
+          if (trailsByDay.meanFrequency >= MinimumPerDayPopOutFrequency) {
+            trailsByDay.toSeq.sortBy(_._1).reverse map { case (day, trailsThatDay) =>
+              Day(day, trailsThatDay.map(_.trail))
             }
-        }
-      } else {
-        Seq(Year(startOfYear, trailsThatYear.map(_.trail)))
+          } else {
+            Seq(Month(startOfMonth, trailsThatMonth.map(_.trail)))
+          }
       }
     }
   }
@@ -52,8 +48,4 @@ case class Day(day: LocalDate, items: Seq[Content]) extends IndexPageGrouping {
 
 case class Month(day: LocalDate, items: Seq[Content]) extends IndexPageGrouping {
   override val dateFormatString: String = "MMMM yyyy"
-}
-
-case class Year(day: LocalDate, items: Seq[Content]) extends IndexPageGrouping {
-  override val dateFormatString: String = "yyyy"
 }
