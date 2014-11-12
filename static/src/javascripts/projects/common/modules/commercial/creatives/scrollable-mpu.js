6b7720386064e3609c7b59087ba42d2fd4fe3569
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-11-12 17:20:57 +0000

    not all devices like background-attachment: fixed

--- static/src/javascripts/projects/common/modules/commercial/creatives/scrollable-mpu.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/scrollable-mpu.js
@@ -1,16 +1,23 @@
 define([
     'bonzo',
     'common/utils/$',
+    'common/utils/detect',
     'common/utils/mediator'
 ], function (
     bonzo,
     $,
+    detect,
     mediator
 ) {
 
-    var adClass          = 'js-scrollable-mpu',
-        updateBgPosition = function ($ad) {
-            $ad.css('background-position', $ad.offset().left + 'px 0');
+    var adClass = 'js-scrollable-mpu',
+        /**
+         * TODO: rather blunt instrument this, due to the fact *most* mobile devices don't have a fixed
+         * background-attachment - need to make this more granular
+         */
+        limitedBgAttachment = detect.isIOS() || detect.isAndroid(),
+        updateBgPosition    = function ($ad) {
+            $ad.css('background-position', $ad.offset().left + 'px 100%');
         };
 
     /**
@@ -20,14 +27,21 @@ define([
 
         run: function () {
             $('.' + adClass).each(function (ad) {
-                var $ad = bonzo(ad);
+                var staticImage,
+                    $ad = bonzo(ad);
 
-                // update bg position
-                updateBgPosition($ad);
-                // to be safe, also update on window resize
-                mediator.on('window:resize', function () {
+                if (limitedBgAttachment) {
+                    staticImage = $ad.data('static-image');
+                    $ad.css('background-image', staticImage);
+                } else {
+                    $ad.css('background-attachment', 'fixed');
+                    // update bg position
                     updateBgPosition($ad);
-                });
+                    // to be safe, also update on window resize
+                    mediator.on('window:resize', function () {
+                        updateBgPosition($ad);
+                    });
+                }
 
                 $ad.removeClass(adClass);
             });
