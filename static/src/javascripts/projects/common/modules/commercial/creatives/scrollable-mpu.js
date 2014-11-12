92d6077709c2b4a228bf757b7c6b0e7f52b4014a
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-11-12 15:54:49 +0000

    Revert "Use background-attachment for scrollable MPU"

--- static/src/javascripts/projects/common/modules/commercial/creatives/scrollable-mpu.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/scrollable-mpu.js
@@ -1,17 +1,23 @@
 define([
+    'bean',
     'bonzo',
     'common/utils/$',
+    'common/utils/detect',
     'common/utils/mediator'
 ], function (
+    bean,
     bonzo,
     $,
+    detect,
     mediator
 ) {
 
-    var adClass          = 'js-scrollable-mpu',
-        updateBgPosition = function ($ad) {
-            $ad.css('background-position', $ad.offset().left + 'px 0');
-        };
+    var adClass = 'js-scrollable-mpu',
+        /**
+         * TODO: rather blunt instrument this - due to the fact *most* mobile devices have a crappy onscroll
+         * implementation, i.e. it only fires on scroll end
+         */
+        badOnScroll = detect.isIOS() || detect.isAndroid();
 
     /**
      * https://www.google.com/dfp/59666047#delivery/CreateCreativeTemplate/creativeTemplateId=10026567
@@ -19,16 +25,19 @@ define([
     return {
 
         run: function () {
+
             $('.' + adClass).each(function (ad) {
-                var $ad = bonzo(ad);
+                var staticImage,
+                    $ad = bonzo(ad);
 
-                // update bg position
-                updateBgPosition($ad);
-                // to be safe, also update on window resize
-                mediator.on('window:resize', function () {
-                    updateBgPosition($ad);
-                });
-
+                if (badOnScroll) {
+                    staticImage = $ad.data('static-image');
+                    $ad.css('background-image', staticImage);
+                } else {
+                    mediator.on('window:scroll', function () {
+                        ad.style.backgroundPosition = '0 ' + window.pageYOffset + 'px';
+                    });
+                }
                 $ad.removeClass(adClass);
             });
 
