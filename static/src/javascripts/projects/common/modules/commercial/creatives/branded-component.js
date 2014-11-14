c6a96d7cb5904232e31a79c54934892efa86e216
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-11-14 14:37:07 +0000

    adding soulmates branded component

--- static/src/javascripts/projects/common/modules/commercial/creatives/branded-component.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/branded-component.js
@@ -3,21 +3,51 @@ define([
     'common/utils/$',
     'common/utils/config',
     'common/utils/template',
-    'text!common/views/commercial/creatives/branded-component.html'
+    'text!common/views/commercial/creatives/branded-component-jobs.html',
+    'text!common/views/commercial/creatives/branded-component-membership.html',
+    'text!common/views/commercial/creatives/branded-component-soulmates.html'
 ], function (
     qwery,
     $,
     config,
     template,
-    brandedComponentTpl
+    brandedComponentJobsTpl,
+    brandedComponentMembershipTpl,
+    brandedComponentSoulmatesTpl
 ) {
+
+    var templates = {
+        jobs: {
+            template: brandedComponentJobsTpl,
+            config:   {
+                imgUrl: config.images.commercial.brandedComponentJobs
+            }
+        },
+        membership: {
+            template: brandedComponentMembershipTpl,
+            config:   {
+                imgUrl: config.images.commercial.brandedComponentJobs
+            }
+        },
+        soulmates: {
+            template: brandedComponentSoulmatesTpl,
+            config:   {}
+        }
+    };
 
     return {
 
-        run: function () {
-            $.create(template(brandedComponentTpl, { imgUrl: config.images.commercial.brandedComponentJobs }))
+        run: function (type) {
+            var templateConfig = templates[type];
+
+            if (!templateConfig) {
+                return false;
+            }
+
+            $.create(template(templateConfig.template, templateConfig.config))
                 .appendTo(qwery('.content__secondary-column')[0]);
 
+            // assumption - this will always be targeted to the 88x87 slot
             $('#dfp-ad--merchandising-high').css('display', 'none');
         }
 
