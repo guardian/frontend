1c12a1eccae18f112fabe133204c103b4d2c2e14
Ken Lim <kennethlimfc@gmail.com>
2014-11-17 12:22:36 +0000

    Removed redundant lines and added test.

--- static/test/javascripts/spec/common/commercial/tags/krux.spec.js
+++ static/test/javascripts/spec/common/commercial/tags/krux.spec.js
@@ -1,19 +1,9 @@
 define([
     'common/utils/$',
-    'lodash/arrays/zipObject',
     'jasq'
 ], function (
-    $,
-    zipObject
+    $
 ) {
-
-
-    function retrieveParams(url) {
-        return zipObject(url.split('?').pop().split('!').shift().split('&').map(function(param) {
-            return param.split('=');
-        }));
-    }
-
     describe('Krux', {
         moduleName: 'common/modules/commercial/tags/krux',
         mock: function () {
@@ -34,15 +24,23 @@ define([
             });
 
             afterEach(function () {
-                $('.krux-script').remove();
                 requireStub.restore();
             });
+
 
             it('should not load if switch is off', function (krux, deps) {
                 deps['common/utils/config'].switches.krux = false;
 
                 expect(krux.load()).toBeFalsy();
             });
+
+            it('should send correct "netid" param', function (krux) {
+                krux.load();
+                var url = requireStub.args[0][0][0];
+
+                expect(url).toBe('js!//cdn.krxd.net/controltag?confid=JVZiE3vn');
+            });
+
         }
     });
 
