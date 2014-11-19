3979f8a403161502d0b861e9af5e94f75e79fb0f
Matt Osborn <matthew.osborn@theguardian.com>
2014-11-19 09:59:29 +0000

    disable foresee on network fronts and tweak sample rates

--- static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
+++ static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
@@ -1,8 +1,10 @@
 define([
     'common/utils/cookies',
+    'common/utils/config',
     'common/utils/detect'
 ], function (
     Cookie,
+    config,
     detect
 ) {
 
@@ -12,12 +14,13 @@ define([
 
     function load() {
 
-        var sampleRate = detect.getBreakpoint() === 'mobile' ? 0.03 : 0.003, // 3% mobile & 0.3% >mobile
+        var isNetworkFront = config.page.contentType === "Network Front",
+            sampleRate = detect.isBreakpoint({max: 'mobile'}) ? 0.008 : 0.006, // 0.8% mobile and 0.6% rest
             sample = Math.random() <= sampleRate,
             hasForcedOptIn = /forceForesee/.test(location.hash);
 
         // the Foresee code is large, we only want to load it in when necessary.
-        if (!Cookie.get('GU_TEST') && (window.openForeseeWhenReady || sample || hasForcedOptIn)) {
+        if (!Cookie.get('GU_TEST') && !isNetworkFront && (window.openForeseeWhenReady || sample || hasForcedOptIn)) {
             openForesee();
         }
 
