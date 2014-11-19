99d0d372934e542e8e7fa540e6009ea8616f5ca2
Matt Osborn <matthew.osborn@theguardian.com>
2014-11-19 10:24:38 +0000

    quote style

--- static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
+++ static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
@@ -14,7 +14,7 @@ define([
 
     function load() {
 
-        var isNetworkFront = config.page.contentType === "Network Front",
+        var isNetworkFront = config.page.contentType === 'Network Front',
             sampleRate = detect.isBreakpoint({max: 'mobile'}) ? 0.008 : 0.006, // 0.8% mobile and 0.6% rest
             sample = Math.random() <= sampleRate,
             hasForcedOptIn = /forceForesee/.test(location.hash);
