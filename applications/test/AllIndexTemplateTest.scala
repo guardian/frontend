5d3fc505fd67707ab9cdb9d977449069b5e129b2
Robert Berry <robert.berry@theguardian.com>
2014-11-21 16:18:48 +0000

    Fix broken test

--- applications/test/AllIndexTemplateTest.scala
+++ applications/test/AllIndexTemplateTest.scala
@@ -10,9 +10,11 @@ import org.scalatest.{DoNotDiscover, Matchers, FlatSpec}
     url() should endWith("/world/2014/oct/02/all")
     $("[rel=next]").first.getAttribute("href") should endWith ("/world/2014/oct/03/newer")
     $("[rel=next]").first.click()
-    url() should endWith("/world/2014/oct/02/all")
+    url() should endWith("/world/2014/oct/03/all")
 
     $("[rel=prev]").first.click()
+    url() should endWith("/world/2014/oct/02/all")
+
     $("[rel=prev]").first.click()
     url() should endWith("/world/2014/sep/30/all")
   }
