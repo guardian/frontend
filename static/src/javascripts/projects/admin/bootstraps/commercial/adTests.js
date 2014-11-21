c77214fa79070752ba5c5ae759a9bbc2ab51936b
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-11-21 16:12:36 +0000

    Use Iframe to Set Cookie

--- static/src/javascripts/projects/admin/bootstraps/commercial/adTests.js
+++ static/src/javascripts/projects/admin/bootstraps/commercial/adTests.js
@@ -6,36 +6,56 @@ define([
              $,
              cookies) {
 
-    var cookieName = 'adtest';
+    var cookieName = 'adtest',
+
+    // cookie in this domain to track adtest cookie in theguardian.com
+        trackingCookieName = 'adtest_tracker';
 
     function status() {
-        var adTestCookie = cookies.get(cookieName);
+        var trackingCookie = cookies.get(trackingCookieName);
 
-        if (adTestCookie == null) {
+        if (trackingCookie == null) {
             $('#cookie_status')
                 .text('Your browser has no test cookie set.')
                 .addClass('hasnt_cookie');
             $('#clear_cookie').hide();
         } else {
             $('#cookie_status')
-                .text('Your browser has test cookie \'' + adTestCookie + '\' set.')
+                .text('Your browser has test cookie \'' + trackingCookie + '\' set.')
                 .addClass('has_cookie');
         }
+    }
+
+    function createIframe() {
+        ifrm = document.createElement('iframe');
+        ifrm.style.width = '1px';
+        ifrm.style.height = '1px';
+        document.body.appendChild(ifrm);
+        return ifrm;
+    }
+
+    function setCookie(ifrm, cookieValue) {
+        ifrm.setAttribute('src',
+            'http://www.theguardian.com/uk?' + cookieName + '=' + cookieValue);
     }
 
     function init() {
 
         status();
 
+        var ifrm = createIframe();
+
         bean.on(document, 'click', '.cookie', function (event) {
             var cookieValue = event.srcElement.text;
-            cookies.add(cookieName, cookieValue, 10);
+            setCookie(ifrm, cookieValue);
+            cookies.add(trackingCookieName, cookieValue, 10);
             $('#clear_cookie').show();
             status();
         });
 
         bean.on(document, 'click', '#clear_cookie', function () {
-            cookies.remove(cookieName);
+            setCookie(ifrm, 'clear');
+            cookies.remove(trackingCookieName);
             $('#clear_cookie').hide();
             status();
         });
