8cb1efda4a66a692bb821167ce0cf174fce65ad2
Stephan Fowler <stephanfowler@gmail.com>
2014-11-23 01:22:16 +0000

    Use $ util

--- static/src/javascripts/projects/common/modules/onward/history-nav.js
+++ static/src/javascripts/projects/common/modules/onward/history-nav.js
@@ -1,27 +1,24 @@
 define([
-    'bean',
-    'bonzo',
+    'common/utils/$',
     'common/utils/_',
     'common/modules/onward/history',
     'common/utils/template'
 ], function (
-    bean,
-    bonzo,
+    $,
     _,
     history,
     template
 ) {
     function init() {
-        var container = document.querySelector('.js-history-nav-placeholder'),
-            popular = history.getPopular();
+        var popular = history.getPopular();
 
-        if (container && popular) {
-            bonzo(container).html(
+        if (popular.length) {
+            $('.js-history-nav-placeholder').html(
                 '<ul class="history-nav">' +
                     popular.map(function (tag) {
                         return template(
                             '<li class="local-navigation__item">' +
-                               '<a href="/{{id}}" class="local-navigation__action" data-link-name="nav : history : {{name}}">{{name}}</a>' +
+                                '<a href="/{{id}}" class="local-navigation__action" data-link-name="nav : history : {{name}}">{{name}}</a>' +
                             '</li>',
                             {id: tag[0], name: tag[1]}
                         );
