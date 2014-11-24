b9990dca71657d94bcb1649a28ec7c113c714e3d
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2014-11-24 17:51:31 +0000

    whitespace

--- common/app/model/content.scala
+++ common/app/model/content.scala
@@ -259,7 +259,7 @@ class Content protected (val apiContent: ApiContentWithMeta) extends Trail with 
   }
 
   lazy val seriesTag: Option[Tag] = {
-      blogs.find{tag => tag.id != "commentisfree/commentisfree"}.orElse(series.headOption)
+    blogs.find{tag => tag.id != "commentisfree/commentisfree"}.orElse(series.headOption)
   }
 }
 
