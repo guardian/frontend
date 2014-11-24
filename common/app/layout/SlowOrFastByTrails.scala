00a4187d4ccd5a5db8f5552303b692beb37bda21
Robert Berry <robert.berry@theguardian.com>
2014-11-24 17:55:29 +0000

    Determine whether an index page is slow by the frequency of items with given tags

---
+++ common/app/layout/SlowOrFastByTrails.scala
@@ -0,0 +1,38 @@
+package layout
+
+import implicits.Collections
+import model.Trail
+
+object TagHistogram extends Collections {
+  def fromTrails(trails: Seq[Trail]): TagHistogram = TagHistogram(trails.foldLeft(Map.empty[String, Int]) {
+    case (histogram, trail) =>
+      trail.tags.map(_.id).toSet.foldLeft(histogram) {
+        case (hist, tagId) =>
+          hist + (tagId -> (hist.getOrElse(tagId, 0) + 1))
+      }
+  }, trails.length)
+}
+
+case class TagHistogram(frequencyById: Map[String, Int], numberOfItems: Int) {
+  def frequency(id: String): Double =
+    frequencyById.get(id) map { _.toDouble / numberOfItems } getOrElse 0d
+}
+
+object SlowOrFastByTrails {
+  val SlowTags = Set(
+    "world/series/eyewitness",
+    "type/cartoon",
+    "type/gallery",
+    "type/picture",
+    "lifeandstyle/series/last-bites",
+    "news/series/ten-best-photographs-of-the-day"
+  )
+
+  val FrequencyThreshold = 0.8
+
+  def isSlow(trails: Seq[Trail]) = {
+    val histogram = TagHistogram.fromTrails(trails)
+
+    SlowTags exists { tag => histogram.frequency(tag) > FrequencyThreshold }
+  }
+}
