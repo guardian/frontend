22087eda6875d4fd63cca43115a63fd67431c6e1
Robert Berry <robert.berry@theguardian.com>
2014-11-25 15:17:04 +0000

    Treat any subdomain of theguardian.com as internal

--- common/app/common/ExternalLinks.scala
+++ common/app/common/ExternalLinks.scala
@@ -7,11 +7,10 @@ import conf.Configuration
 import scala.util.Try
 
 object ExternalLinks {
-  val GuardianDomains = Configuration.ajax.corsOrigins flatMap { url =>
-    Try {
-      new URI(url).getHost
-    }.toOption
-  }
+  val GuardianDomains = Seq(
+    "theguardian.com",
+    "dev-theguardian.com"
+  )
 
   def external(url: String) = Try(Option(new URI(url).getHost).exists({ host => !GuardianDomains.exists({ domain =>
     host == domain || host.endsWith(s".$domain")
