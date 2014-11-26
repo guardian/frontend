983b690050dfec6a1239c3272665120600924ee3
Darren Hurley <ironsidevsquincy@googlemail.com>
2014-11-26 17:58:01 +0000

    Adding standard ad mpu tempalte

--- static/src/javascripts/projects/common/modules/commercial/creatives/template.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/template.js
@@ -1,25 +1,34 @@
 define([
     'common/utils/$',
     'common/utils/template',
+    'text!common/views/commercial/creatives/ad-feature-mpu.html',
     'text!common/views/commercial/creatives/ad-feature-mpu-large.html'
 ], function (
     $,
     template,
+    adFeatureMpuTpl,
     adFeatureMpuLargeTpl
 ) {
 
     /**
      * Generic template creative -
      *
+     *  * https://www.google.com/dfp/59666047#delivery/CreateCreativeTemplate/creativeTemplateId=10021527
      *  * https://www.google.com/dfp/59666047#delivery/CreateCreativeTemplate/creativeTemplateId=10028127
      */
-    var Template = function ($adSlot, params) {
-        this.$adSlot = $adSlot;
-        this.params  = params;
-    };
+    var creativeTemplates = {
+            'ad-feature-mpu': adFeatureMpuTpl,
+            'ad-feature-mpu-large': adFeatureMpuLargeTpl
+        },
+        Template = function ($adSlot, params) {
+            this.$adSlot = $adSlot;
+            this.params  = params;
+        };
 
     Template.prototype.create = function () {
-        $.create(template(adFeatureMpuLargeTpl, this.params))
+        var creativeHtml = template(creativeTemplates[this.params.creative], this.params);
+
+        $.create(creativeHtml)
             .appendTo(this.$adSlot);
     };
 
