7efdb3a24e8246ecf1e9cd3b9a6c5cb4670f66df
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-11-27 12:04:47 +0000

    Make Frankenponent Work Iff All Components Work
    Gives a 404 if any component not populated.

--- commercial/app/controllers/commercial/SoulmateAds.scala
+++ commercial/app/controllers/commercial/SoulmateAds.scala
@@ -1,11 +1,11 @@
 package controllers.commercial
 
-import common.{JsonNotFound, JsonComponent}
 import model.commercial.soulmates._
-import model.{NoCache, Cached}
+import model.{Cached, NoCache}
 import performance.MemcachedAction
 import play.api.mvc._
 import play.twirl.api.Html
+
 import scala.concurrent.Future
 
 object SoulmateAds extends Controller {
@@ -19,10 +19,10 @@ object SoulmateAds extends Controller {
     override def view(soulmates: Seq[Member])(implicit request: RequestHeader): Html =
       views.html.soulmatesHigh(soulmates)
   }
-  
+
   private def renderMixed(relevance: Relevance[Member], format: Format) = MemcachedAction { implicit request =>
     Future.successful {
-      SoulmatesAggregatingAgent.sampleMembers(segment) match {
+      SoulmatesAggregatingAgent.sampleMembers match {
         case Nil => NoCache(format.nilResult)
         case members => Cached(componentMaxAge) {
           format.result(relevance.view(members))
