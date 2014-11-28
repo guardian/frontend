73e5a1feaab45702f6671b3ab2e578d2baef46f2
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2014-11-28 12:57:31 +0000

    revert cors change request again

--- common/app/model/Cors.scala
+++ common/app/model/Cors.scala
@@ -1,8 +1,19 @@
 package model
 
-/**
- * Created by nbennett on 28/11/14.
- */
-object Cors {
+import play.api.mvc.{RequestHeader, Result, Results}
+import play.api.mvc.Result
+import conf.Configuration.ajax._
 
+
+object Cors extends Results {
+  def apply(result: Result)(implicit request: RequestHeader): Result = {
+    request.headers.get("Origin") match {
+      case None => result
+      case Some(requestOrigin) =>
+        result.withHeaders(
+          "Access-Control-Allow-Origin" -> corsOrigins.find(_ == requestOrigin).getOrElse("*"),
+          "Access-Control-Allow-Credentials" -> "true"
+        )
+    }
+  }
 }
