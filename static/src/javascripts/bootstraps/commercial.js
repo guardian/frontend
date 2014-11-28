51c212f236780311c0a831011cfa80d6a02157bb
John Duffell <john.duffell@guardian.co.uk>
2014-11-28 11:33:14 +0000

    use direct comparison rather than regex for the hash check

--- static/src/javascripts/bootstraps/commercial.js
+++ static/src/javascripts/bootstraps/commercial.js
@@ -108,7 +108,7 @@ define([
                 !userPrefs.isOff('adverts') &&
                 !config.page.shouldHideAdverts &&
                 (!config.page.isSSL || config.page.section === 'admin') &&
-                !/#noads/.test(window.location.hash)
+                window.location.hash !== '#noads'
             ) {
                 modules.commercialLoaderHelper();
                 modules.tagContainer();
