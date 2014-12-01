ad113348eb735457a07ca119a601733a30e246e9
Robert Berry <robert.berry@theguardian.com>
2014-12-01 12:16:41 +0000

    Fix issue where bad sections were appearing in titles

--- common/app/views/support/Title.scala
+++ common/app/views/support/Title.scala
@@ -1,11 +1,14 @@
 package views.support
 
-import model.{Tag, Content, MetaData}
+import model.{Section, Tag, Content, MetaData}
 import play.twirl.api.Html
 import play.api.mvc.RequestHeader
 import common.Localisation
 
 object Title {
+  val SectionsToIgnore = Set(
+    "global"
+  )
 
   def apply(page: MetaData)(implicit request: RequestHeader): Html = Html{
     val title = page match {
@@ -13,6 +16,8 @@ object Title {
         s"${c.webTitle}${pagination(c)}${getSectionConsideringWebtitle(c.webTitle, Option(c.sectionName))}"
       case t: Tag     =>
         s"${Localisation(t.webTitle)}${pagination(page)}${getSectionConsideringWebtitle(t.webTitle, Option(t.sectionName))}"
+      case s: Section =>
+        s"${Localisation(s.webTitle)}${pagination(page)}"
       case _          =>
         page.title.filter(_.nonEmpty).map(Localisation(_)).getOrElse(
           s"${Localisation(page.webTitle)}${pagination(page)}${getSectionConsideringWebtitle(page.webTitle, Option(page.section))}"
@@ -22,7 +27,10 @@ object Title {
   }
 
   private def getSectionConsideringWebtitle(webTitle: String, section: Option[String]): String =
-    section.filter(_.nonEmpty).filterNot(_.toLowerCase == webTitle.toLowerCase).fold(""){ s => s" | ${s.capitalize}"}
+    section.filter(_.nonEmpty)
+      .filterNot(_.toLowerCase == webTitle.toLowerCase)
+      .filterNot(SectionsToIgnore.contains)
+      .fold("") { s => s" | ${s.capitalize}"}
 
   private def pagination(page: MetaData) = page.pagination.filterNot(_.isFirstPage).map{ pagination =>
     s" | Page ${pagination.currentPage} of ${pagination.lastPage}"
