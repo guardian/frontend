235b2aa90efbff3b15dfc1d5b814612a510218da
Stephan Fowler <stephanfowler@gmail.com>
2014-12-03 20:21:29 +0000

    Dedupe from main nav. Reorder main nav.

--- static/src/javascripts/projects/common/modules/onward/history-nav.js
+++ static/src/javascripts/projects/common/modules/onward/history-nav.js
@@ -9,27 +9,65 @@ define([
     history,
     template
     ) {
+
+    function stripOuterSlashes(path) {
+        return (path || '').replace(/^\/|\/$/g, '');
+    }
+
+    function collapseTag(t) {
+        t = t.replace(/^(uk|us|au)\//, '');
+        t = t.split('/');
+        t = t.length === 2 && t[0] === t[1] ? [t[0]] : t;
+        return t.join('/');
+    }
+
     function init() {
-        var popular = history.getPopular();
+        var popular = history.getPopular(),
+            topNav,
+            topNavItems,
+            topNavItemIds = [],
+            myNav = [];
 
         if (popular.length) {
-            $('.js-history-nav-placeholder').html(
+            topNav =  document.querySelector('.top-navigation');
+
+            topNavItems = $('.top-navigation__item', topNav);
+
+            topNavItemIds = topNavItems.map(function(item) {
+                return collapseTag(stripOuterSlashes($('a', item).attr('href')));
+            });
+
+            _.chain(popular)
+                .reverse()
+                .forEach(function(tag) {
+                    var pos = topNavItemIds.indexOf(tag[0]);
+
+                    if (pos > -1) {
+                        $(topNavItems[pos]).detach().insertAfter(topNavItems[0]);
+                    } else {
+                        myNav.unshift(tag);
+                    }
+                });
+
+            if (myNav.length) {
+                $('.js-history-nav-placeholder').html(
                     '<ul class="signposting">' +
-                    '<li class="signposting__item signposting__item--home">' +
-                    '<a class="signposting__action" href="/" data-link-name="nav : signposting : jump to">jump to</a>' +
-                    '</li>' +
+                        '<li class="signposting__item signposting__item--home">' +
+                            '<a class="signposting__action" href="/" data-link-name="nav : signposting : jump to">jump to</a>' +
+                        '</li>' +
                     '</ul>' +
                     '<ul class="local-navigation">' +
-                    popular.map(function (tag) {
-                        return template(
+                        myNav.map(function (tag) {
+                            return template(
                                 '<li class="local-navigation__item">' +
-                                '<a href="/{{id}}" class="local-navigation__action" data-link-name="nav : history : {{name}}">{{name}}</a>' +
+                                   '<a href="/{{id}}" class="local-navigation__action" data-link-name="nav : history : {{name}}">{{name}}</a>' +
                                 '</li>',
-                            {id: tag[0], name: tag[1]}
-                        );
-                    }).join('') +
+                                {id: tag[0], name: tag[1]}
+                            );
+                        }).join('') +
                     '</ul>'
-            );
+                );
+            }
         }
     }
 
