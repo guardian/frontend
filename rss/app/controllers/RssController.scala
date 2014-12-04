e6bce3b26e179f42eabef4ca8f00937e618860e1
Robert Berry <robert.berry@theguardian.com>
2014-12-04 16:54:56 +0000

    Fix RSS Controller

--- rss/app/controllers/RssController.scala
+++ rss/app/controllers/RssController.scala
@@ -2,9 +2,12 @@ package controllers
 
 import common._
 import model._
-import play.api.mvc.{Result, RequestHeader, Controller}
+import play.api.mvc.{Result, RequestHeader}
 import services.IndexPage
 
 object RssController extends IndexControllerCommon {
-  override protected def renderFaciaFront(model: IndexPage)(implicit request: RequestHeader): Result = ???
+  override protected def renderFaciaFront(model: IndexPage)(implicit request: RequestHeader): Result = Cached(model.page) {
+    Ok(TrailsToRss(model.page, model.trails))
+      .as("text/xml; charset=utf-8")
+  }
 }
