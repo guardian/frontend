1ce7c783c01c2630c5b5486f8f2f86bb3aff71a2
Josh Holder <josh.holder@theguardian.com>
2014-12-05 12:33:35 +0000

    Spacing changes

--- static/src/javascripts/projects/common/modules/ui/relativedates.js
+++ static/src/javascripts/projects/common/modules/ui/relativedates.js
@@ -186,11 +186,9 @@ define([
             if (relativeDate) {
                 // If we find .timestamp__text (facia), use that instead
                 targetEl = $el[0].querySelector('.timestamp__text') || $el[0];
-
                 if (targetEl.getAttribute('title') == 'Permalink to this comment' || !targetEl.getAttribute('title')) {
                     targetEl.setAttribute('title', bonzo(targetEl).text());
                 }
-
                 targetEl.innerHTML = relativeDate;
             } else if (opts.notAfter) {
                 $el.addClass('modern-hidden');
