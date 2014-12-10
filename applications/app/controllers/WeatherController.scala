75b67ed48877d3e75dc842da0fa538d985ccec70
Francis Carr <francis.carr@guardian.co.uk>
2014-12-10 14:48:23 +0000

    Add initial WeatherController to applications

---
+++ applications/app/controllers/WeatherController.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import common.ExecutionContexts
+import play.api.libs.ws.WS
+import play.api.mvc.{Action, Controller}
+
+object WeatherController extends Controller with ExecutionContexts {
+  import play.api.Play.current
+
+  case class City(name: String) extends AnyVal
+  case class CityId(id: String) extends AnyVal
+
+  val weatherApiKey: String = ""
+
+  val weatherCityUrl: String = "http://api.accuweather.com/currentconditions/v1/"
+  val weatherSearchUrl: String = "http://api.accuweather.com/locations/v1/cities/geoposition/search.json"
+
+  private def weatherUrlForCity(city: City): String =
+   s"$weatherSearchUrl?apikey=$weatherApiKey&q=${city.name}"
+
+  private def weatherUrlForCityId(cityId: CityId): String =
+    s"$weatherCityUrl${cityId.id}.json?apikey=$weatherApiKey"
+
+  def getWeatherForCity(name: String) = Action.async { implicit request =>
+    for {
+      cityJson <- WS.url(weatherUrlForCity(City(name))).get().map(_.json)
+      cityId = (cityJson \ "Key").as[String]
+      weatherJson <- WS.url(weatherUrlForCityId(CityId(cityId))).get().map(_.json)
+    } yield Ok(weatherJson)
+  }
+}
