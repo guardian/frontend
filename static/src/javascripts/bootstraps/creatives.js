5f7149eef83b391b095c3caeaaae99dca66fef5f
Austin Harris <austin@dotmail.co.uk>
2014-12-10 12:21:11 +0000

    creating templates and splitting for mobile and not mobile

--- static/src/javascripts/bootstraps/creatives.js
+++ static/src/javascripts/bootstraps/creatives.js
@@ -2,6 +2,7 @@ define([
     'common/modules/commercial/creatives/branded-component',
     'common/modules/commercial/creatives/commercial-component',
     'common/modules/commercial/creatives/expandable',
+    'common/modules/commercial/creatives/expandableV2',
     'common/modules/commercial/creatives/fluid250',
     'common/modules/commercial/creatives/fluid250GoogleAndroid',
     'common/modules/commercial/creatives/scrollable-mpu',
