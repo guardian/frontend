3c4c42b90378d58dd496a13aa9d5b6be2710ad07
Kelvin Chappell <kelvin.chappell@theguardian.com>
2014-12-11 17:20:11 +0000

    Refactor and Integrate New Agents

--- admin/app/dfp/CustomTargetingValueAgent.scala
+++ admin/app/dfp/CustomTargetingValueAgent.scala
@@ -2,7 +2,7 @@ package dfp
 
 import com.google.api.ads.dfp.axis.utils.v201411.StatementBuilder
 
-object CustomKeyAgent extends DataAgent[Long, String] {
+object CustomTargetingValueAgent extends DataAgent[Long, String] {
 
   override def loadFreshData() = {
     DfpServiceRegistry().fold(Map[Long, String]()) { serviceRegistry =>
@@ -10,4 +10,5 @@ object CustomKeyAgent extends DataAgent[Long, String] {
       values.map { v => v.getId.longValue() -> v.getName}.toMap
     }
   }
+
 }
