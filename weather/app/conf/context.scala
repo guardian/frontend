71634fa6c0c42a9978fd75de586694adcf63d976
Francis Carr <francis.carr@guardian.co.uk>
2014-12-15 15:43:55 +0000

    No need for override

--- weather/app/conf/context.scala
+++ weather/app/conf/context.scala
@@ -1,20 +1,7 @@
 package conf
 
-import contentapi.SectionsLookUp
-import play.api.mvc.{AnyContent, Action}
-
-import scala.concurrent.Future
-
 object HealthCheck extends AllGoodHealthcheckController(
   9020,
   "/weather/city.json",
   "/weather/city/sydney.json"
-) {
-  override def healthcheck(): Action[AnyContent] = Action.async { request =>
-    if (!SectionsLookUp.isLoaded()) {
-      Future.successful(InternalServerError("Error with weather API"))
-    } else {
-      super.healthcheck()(request)
-    }
-  }
-}
+)
