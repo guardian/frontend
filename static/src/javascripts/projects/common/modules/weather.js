0c769c6775c066896fb9d6bd282657f6ba996667
Sam Morris <mail@sammorrisdesign.co.uk>
2014-12-15 17:05:42 +0000

    Rename icon classes

--- static/src/javascripts/projects/common/modules/weather.js
+++ static/src/javascripts/projects/common/modules/weather.js
@@ -127,6 +127,7 @@ define([
 
         bindEvents: function () {
             bean.on($('.js-detect-location')[0], 'click', self.detectPosition);
+            bean.on($('.js-close-location')[0], 'click', Toggles.prototype.close($('.js-get-location')[0]));
             mediator.on('autocomplete:fetch', this.getCityCoordinates);
         },
 
