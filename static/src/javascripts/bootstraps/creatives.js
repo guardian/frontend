aef95f72c02c0235623a1eba6463d588228a8937
austinh7 <austin@dotmail.co.uk>
2014-12-17 12:54:17 +0000

    renmaing js file

--- static/src/javascripts/bootstraps/creatives.js
+++ static/src/javascripts/bootstraps/creatives.js
@@ -2,7 +2,7 @@ define([
     'common/modules/commercial/creatives/branded-component',
     'common/modules/commercial/creatives/commercial-component',
     'common/modules/commercial/creatives/expandable',
-    'common/modules/commercial/creatives/expandableV2',
+    'common/modules/commercial/creatives/expandable-v2',
     'common/modules/commercial/creatives/fluid250',
     'common/modules/commercial/creatives/fluid250GoogleAndroid',
     'common/modules/commercial/creatives/scrollable-mpu',
