a3d3862f1441b912f086c1ad37b7f355cf203f12
Robert Berry <robert.berry@theguardian.com>
2014-12-17 12:16:13 +0000

    Change to look up weather by city ID

--- weather/app/controllers/WeatherController.scala
+++ weather/app/controllers/WeatherController.scala
@@ -4,17 +4,18 @@ import common.ExecutionContexts
 import play.api.mvc.{Action, Controller}
 import models.{City, CityId}
 import weather.WeatherApi
-
 import scala.concurrent.Future
 
 object WeatherController extends Controller with ExecutionContexts {
-  import play.api.Play.current
-
   def forCity(name: String) = Action.async {
     WeatherApi.getCityIdForCity(City(name)).flatMap {
       case Some(cityId) => WeatherApi.getWeatherForCityId(cityId).map(Ok(_))
       case None => Future.successful(NotFound)
     }
+  }
+
+  def forCityId(cityId: String) = Action.async {
+    WeatherApi.getWeatherForCityId(CityId(cityId)).map(Ok(_))
   }
 
   def forRequest() = Action.async { implicit request =>
