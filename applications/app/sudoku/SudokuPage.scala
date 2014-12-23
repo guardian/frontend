3b6d4223d213e1fe4acb656d0563de91194caf56
Robert Berry <robert.berry@theguardian.com>
2014-12-23 15:35:19 +0000

    Add correct page attributes to page model for Sudoku

--- applications/app/sudoku/SudokuPage.scala
+++ applications/app/sudoku/SudokuPage.scala
@@ -1,6 +1,7 @@
 package sudoku
 
 import model.MetaData
+import play.api.libs.json.{JsString, JsValue}
 
 class SudokuPage(val sudoku: Sudoku) extends MetaData {
   override def id: String = sudoku.id
@@ -10,4 +11,9 @@ class SudokuPage(val sudoku: Sudoku) extends MetaData {
   override def analyticsName: String = id
 
   override def webTitle: String = sudoku.title
+
+  override def metaData: Map[String, JsValue] = super.metaData ++ Map(
+    "section" -> JsString("lifeandstyle"),
+    "series" -> JsString("Sudoku")
+  )
 }
