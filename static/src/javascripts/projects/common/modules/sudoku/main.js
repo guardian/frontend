ee44cdae45c2bfb1e0694e5d86677cc08582d005
Robert Berry <robert.berry@theguardian.com>
2015-01-07 17:55:11 +0000

    Guh

--- static/src/javascripts/projects/common/modules/sudoku/main.js
+++ static/src/javascripts/projects/common/modules/sudoku/main.js
@@ -16,25 +16,27 @@ define([
 ) {
     return function () {
         $('.js-sudoku').each(function (element) {
-            var $element = bonzo(element);
+            var $element = bonzo(element),
+                sudokuData,
+                cells;
 
             if ($element.attr('data-sudoku-data')) {
-                var sudokuData = JSON.parse($element.attr('data-sudoku-data')),
-                    cells = flatMap(_.range(9), function (y) {
-                        return _.map(_.range(9), function (x) {
-                            return {
-                                x: x,
-                                y: y,
-                                value: sudokuData[x][y],
-                                jottings: [],
-                                isEditable: sudokuData[x][y] === null,
-                                isFocussed: false,
-                                isHighlighted: false,
-                                isSameValue: false,
-                                isError: false
-                            };
-                        });
+                sudokuData = JSON.parse($element.attr('data-sudoku-data'));
+                cells = flatMap(_.range(9), function (y) {
+                    return _.map(_.range(9), function (x) {
+                        return {
+                            x: x,
+                            y: y,
+                            value: sudokuData[x][y],
+                            jottings: [],
+                            isEditable: sudokuData[x][y] === null,
+                            isFocussed: false,
+                            isHighlighted: false,
+                            isSameValue: false,
+                            isError: false
+                        };
                     });
+                });
 
                 React.renderComponent(Grid({
                     cells: cells
