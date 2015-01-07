eaf60cded8a3f282baafef831382d9935f8a11b5
Austin Harris <austin@dotmail.co.uk>
2015-01-07 17:22:06 +0000

    adding video option to fluid 250 template

--- static/src/javascripts/projects/common/modules/commercial/creatives/fluid250.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/fluid250.js
@@ -23,15 +23,31 @@ define([
     };
 
     Fluid250.prototype.create = function () {
+
         var templateOptions = {
             showLabel: (this.params.showAdLabel === 'hide') ?
                 'creative__label--hidden' : ''
         };
-        $.create(template(fluid250Tpl, merge(this.params, templateOptions))).appendTo(this.$adSlot);
+        
+        var leftMarDesk = (this.params.videoPositionH == 'center' ?
+            -(this.params.videoWidth/2) : 0
+        )
+        
+        var topMarDesk = (this.params.videoPositionV == 'center' ?
+            -(this.params.videoHeight/2) : 0
+        )
+        
+        var videoDesktop = {
+            videoD: (this.params.videoURL != '') ?
+                '<iframe width="' + this.params.videoWidth + '" height="' + this.params.videoHeight + '" src="' + this.params.videoURL + '?rel=0&amp;controls=0&amp;showinfo=0&amp;title=0&amp;byline=0&amp;portrait=0" frameborder="0" class="fluid250_video fluid250_video--desktop fluid250_video--vert-pos-' + this.params.videoPositionV + ' fluid250_video--horiz-pos-' + this.params.videoPositionH + '" style="margin-left: ' + leftMarDesk + 'px; margin-top: ' + topMarDesk + 'px;"></iframe>' : ''
+        }
+        
+        $.create(template(fluid250Tpl, merge(this.params, templateOptions, videoDesktop))).appendTo(this.$adSlot);
 
         if (this.params.trackingPixel) {
             this.$adSlot.before('<img src="' + this.params.trackingPixel + this.params.cacheBuster + '" class="creative__tracking-pixel" height="1px" width="1px"/>');
         }
+        
         this.$adSlot.addClass('ad-slot__fluid250');
     };
 
