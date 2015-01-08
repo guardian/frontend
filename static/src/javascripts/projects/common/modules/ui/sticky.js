7533f8b24f84d6126fbdc21d7b5dcdd67320337e
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-01-08 14:39:47 +0000

    handle moving sticky element

--- static/src/javascripts/projects/common/modules/ui/sticky.js
+++ static/src/javascripts/projects/common/modules/ui/sticky.js
@@ -14,30 +14,26 @@ define([
      * @todo: check if browser natively supports "position: sticky"
      */
     var Sticky = function (element, options) {
-        this.element  = element;
         this.$element = bonzo(element);
+        this.$parent  = this.$element.parent();
         this.opts     = defaults(options || {}, {
             top: 0
         });
     };
 
     Sticky.prototype.init = function () {
-        // position of element from document top
-        this.elementDocOffset = this.element.getBoundingClientRect().top + window.scrollY;
-
         mediator.on('window:scroll', throttle(this.updatePosition.bind(this), 10));
         // kick off an initial position update
         this.updatePosition();
     };
 
     Sticky.prototype.updatePosition = function () {
-        var parent, fixedTop, css;
+        var fixedTop, css;
 
         // have we scrolled past the element
-        if (window.scrollY >= this.elementDocOffset - this.opts.top) {
-            parent = this.$element.parent()[0];
+        if (window.scrollY >= this.$parent.offset().top - this.opts.top) {
             // make sure the element stays within its parent
-            fixedTop = Math.min(this.opts.top, parent.getBoundingClientRect().bottom - this.$element.dim().height);
+            fixedTop = Math.min(this.opts.top, this.$parent[0].getBoundingClientRect().bottom - this.$element.dim().height);
 
             css = {
                 position: 'fixed',
