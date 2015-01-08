d482174b97ff3aef23701a40c9c07f775bd31cd2
Austin Harris <austin@dotmail.co.uk>
2015-01-08 15:32:59 +0000

    more js linting woes

--- static/src/javascripts/projects/common/modules/commercial/creatives/fluid250.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/fluid250.js
@@ -24,38 +24,40 @@ define([
 
     Fluid250.prototype.create = function () {
 
-        var templateOptions = {
+        var templateOptions, leftMargin, leftPosition, rightPosition, topMargin, videoDesktop;
+
+        templateOptions = {
             showLabel: (this.params.showAdLabel === 'hide') ?
                 'creative__label--hidden' : ''
         };
-        
-        var leftMargin = (this.params.videoPositionH === 'center' ?
-            -(this.params.videoWidth/2) : 0
+
+        leftMargin = (this.params.videoPositionH === 'center' ?
+            -(this.params.videoWidth / 2) : 0
         );
-        
-        var leftPosition = (this.params.videoPositionH === 'left' ?
+
+        leftPosition = (this.params.videoPositionH === 'left' ?
             this.params.videoHorizSpace + 'px' : 'auto'
         );
-        
-        var rightPosition = (this.params.videoPositionH === 'right' ?
+
+        rightPosition = (this.params.videoPositionH === 'right' ?
             this.params.videoHorizSpace + 'px' : 'auto'
         );
-        
-        var topMargin = (this.params.videoPositionV === 'center' ?
-            -(this.params.videoHeight/2) : 0
+
+        topMargin = (this.params.videoPositionV === 'center' ?
+            -(this.params.videoHeight / 2) : 0
         );
-        
-        var videoDesktop = {
+
+        videoDesktop = {
             videoD: (this.params.videoURL !== '') ?
                 '<iframe width="' + this.params.videoWidth + '" height="' + this.params.videoHeight + '" src="' + this.params.videoURL + '?rel=0&amp;controls=0&amp;showinfo=0&amp;title=0&amp;byline=0&amp;portrait=0" frameborder="0" class="fluid250_video fluid250_video--desktop fluid250_video--vert-pos-' + this.params.videoPositionV + ' fluid250_video--horiz-pos-' + this.params.videoPositionH + '" style="margin-left: ' + leftMargin + 'px; margin-top: ' + topMargin + 'px; left: ' + leftPosition + '; right: ' + rightPosition + ';"></iframe>' : ''
         };
-        
+
         $.create(template(fluid250Tpl, merge(this.params, templateOptions, videoDesktop))).appendTo(this.$adSlot);
 
         if (this.params.trackingPixel) {
             this.$adSlot.before('<img src="' + this.params.trackingPixel + this.params.cacheBuster + '" class="creative__tracking-pixel" height="1px" width="1px"/>');
         }
-        
+
         this.$adSlot.addClass('ad-slot__fluid250');
     };
 
