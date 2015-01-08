ac59e6f2897ca88cd09862169a6303f51507a781
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-01-08 14:41:28 +0000

    no need for testCssSupport now

--- static/src/javascripts/projects/common/utils/detect.js
+++ static/src/javascripts/projects/common/utils/detect.js
@@ -7,11 +7,9 @@
 /*global DocumentTouch: true */
 
 define([
-    'lodash/collections/map',
     'common/utils/_',
     'common/utils/mediator'
 ], function (
-    map,
     _,
     mediator
 ) {
@@ -326,18 +324,6 @@ define([
         }
     }
 
-    function testCssSupport(property, value) {
-        var prefixes = ['', '-webkit-', '-moz-', '-o-', '-ms-'],
-            el       = document.createElement('div'),
-            style    = el.style;
-
-        style.cssText = map(prefixes, function (prefix) {
-            return property + ':' + prefix + value;
-        }).join(';');
-
-        return style[property].indexOf(value) !== -1;
-    }
-
     return {
         hasCrossedBreakpoint: hasCrossedBreakpoint,
         getConnectionSpeed: getConnectionSpeed,
@@ -356,8 +342,7 @@ define([
         hasWebSocket: hasWebSocket,
         getPageSpeed: getPageSpeed,
         breakpoints: breakpoints,
-        shouldUseHintedFonts: shouldUseHintedFonts(),
-        testCssSupport: testCssSupport
+        shouldUseHintedFonts: shouldUseHintedFonts()
     };
 
 });
