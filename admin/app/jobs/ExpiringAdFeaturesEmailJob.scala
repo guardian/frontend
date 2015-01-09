b3a988df7af43422cdc3a2bdd90f24a4df0c8c69
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-01-09 11:08:00 +0000

    Create Expiring Ad Feature Email

---
+++ admin/app/jobs/ExpiringAdFeaturesEmailJob.scala
@@ -0,0 +1,43 @@
+package jobs
+
+import common.Logging
+import conf.Configuration.commercial._
+import dfp.{AdvertisementFeature, GuLineItem}
+import services.EmailService
+import tools.Store
+
+object ExpiringAdFeaturesEmailJob extends Logging {
+
+  def run(): Unit = {
+    val adFeatureTags =
+      Store.getDfpPaidForTags().paidForTags filter (_.paidForType == AdvertisementFeature)
+
+    def adFeatures(p: GuLineItem => Boolean): Seq[GuLineItem] = {
+      adFeatureTags.filter(_.lineItems.forall(p)).flatMap(_.lineItems).sortBy(_.id).distinct
+    }
+
+    for {
+      adTech <- adTechTeam
+    } {
+
+      val expiredAdFeatures = adFeatures(_.isExpired)
+      val expiringAdFeatures = adFeatures(_.isExpiringSoon)
+
+      if (expiredAdFeatures.nonEmpty || expiringAdFeatures.nonEmpty) {
+
+        val htmlBody = {
+          views.html.commercial.email.expiringAdFeatures(
+            expiredAdFeatures,
+            expiringAdFeatures
+          ).body
+        }
+
+        EmailService.send(
+          from = adTech,
+          to = Seq(adTech),
+          subject = "Expiring Advertisement Features",
+          htmlBody = Some(htmlBody))
+      }
+    }
+  }
+}
