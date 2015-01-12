b4735661709b5a2a7d2bed90ab2c94730a646a61
Robert Berry <robert.berry@theguardian.com>
2015-01-12 15:14:09 +0000

    Only fire load test on network fronts

--- static/src/javascripts/projects/facia/modules/ui/what-is-my-city-load-test.js
+++ static/src/javascripts/projects/facia/modules/ui/what-is-my-city-load-test.js
@@ -1,11 +1,13 @@
 define([
+    'common/utils/_',
     'common/utils/ajax',
     'common/utils/config'
 ], function (
+    _,
     ajax,
     config
 ) {
-    if (config.switches.whatIsMyCityLoad) {
+    if (_.contains(['uk', 'us', 'au'], config.page.pageId) && config.switches.whatIsMyCityLoad) {
         ajax({
             url: '/weather/fake-what-is-my-city',
             type: 'json',
