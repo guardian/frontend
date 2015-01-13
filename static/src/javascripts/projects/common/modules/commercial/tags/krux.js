915782694501d38d538366fc7262b5c24d60d97c
Zofia <zofia.korcz@guardian.co.uk>
2015-01-13 13:09:36 +0000

    user timing of crux

--- static/src/javascripts/projects/common/modules/commercial/tags/krux.js
+++ static/src/javascripts/projects/common/modules/commercial/tags/krux.js
@@ -5,6 +5,7 @@ define([
 ) {
     function load() {
         if (config.switches.krux) {
+            userTiming.mark('Krux Begin');
             return require(['js!' + '//cdn.krxd.net/controltag?confid=JVZiE3vn']);
         }
     }
