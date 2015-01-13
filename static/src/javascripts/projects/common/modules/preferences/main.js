397544dcd083b88bf8720afa29570161c51d1d82
Stephan Fowler <stephanfowler@gmail.com>
2015-01-13 15:50:40 +0000

    Tidy

--- static/src/javascripts/projects/common/modules/preferences/main.js
+++ static/src/javascripts/projects/common/modules/preferences/main.js
@@ -62,7 +62,7 @@ define([
                         }, {});
 
                     if (!_.isEmpty(tags)) {
-                        tags['_finally'] = React.DOM.p(null, 'Remove links that don\'t interest you by clicking the \'X\' next to them.');
+                        tags._p1 = React.DOM.p(null, '(remove links that don\'t interest you by clicking the \'X\' next to them.)');
                         return React.DOM.div(null, tags);
                     }
                 }
