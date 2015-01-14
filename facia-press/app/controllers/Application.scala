6dd5bf464d70a701c72078f078733e0a5b9c2ba4
Francis Carr <francis.carr@guardian.co.uk>
2015-01-14 14:05:39 +0000

    Add endpoints for generating JSON for path

--- facia-press/app/controllers/Application.scala
+++ facia-press/app/controllers/Application.scala
@@ -1,14 +1,25 @@
 package controllers
 
+import frontpress.FrontPress
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import services.ConfigAgent
 
 object Application extends Controller {
+  import scala.concurrent.ExecutionContext.Implicits.global
   def index = Action {
     Ok("Hello, I am the Facia Press.")
   }
-  
+
   def showCurrentConfig = Action {
     Ok(ConfigAgent.contentsAsJsonString).withHeaders("Content-Type" -> "application/json")
+  }
+
+  def generateDraftPressedFor(path: String) = Action.async { request =>
+    FrontPress.pressDraftByPathId(path).map(Json.stringify(_)).map(Ok.apply(_))
+  }
+
+  def generateLivePressedFor(path: String) = Action.async { request =>
+    FrontPress.pressLiveByPathId(path).map(Json.stringify(_)).map(Ok.apply(_))
   }
 }
