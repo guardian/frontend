15730f3d6a404441a36325ae8456e36adc59b30d
Robert Berry <robert.berry@theguardian.com>
2015-01-15 15:52:39 +0000

    Hook up hideous hack

--- onward/app/business/HideousHackLifecycle.scala
+++ onward/app/business/HideousHackLifecycle.scala
@@ -1,5 +1,15 @@
 package business
 
-trait HideousHackLifecycle {
+import play.api.{Application, GlobalSettings}
 
+trait HideousHackLifecycle extends GlobalSettings {
+  override def onStart(app: Application): Unit = {
+    super.onStart(app)
+    HideousHack.start()
+  }
+
+  override def onStop(app: Application): Unit = {
+    HideousHack.stop()
+    super.onStop(app)
+  }
 }
