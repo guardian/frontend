e4dcf1c04ff0395179826ff0e6c37cb2e5776978
Francis Carr <francis.carr@guardian.co.uk>
2015-01-15 16:35:27 +0000

    Use ExecutionContexts in FaciaJsonClient

--- facia-tool/app/services/FaciaJsonClient.scala
+++ facia-tool/app/services/FaciaJsonClient.scala
@@ -1,9 +1,9 @@
 package services
 
 import com.gu.facia.client.{ApiClient, AmazonSdkS3Client}
+import common.ExecutionContexts
 import conf.Configuration
 
-object FaciaJsonClient {
-  import scala.concurrent.ExecutionContext.Implicits.global
+object FaciaJsonClient extends ExecutionContexts {
   val client = ApiClient(Configuration.aws.bucket, Configuration.facia.stage, AmazonSdkS3Client.default)
 }
