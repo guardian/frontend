83559135f01080fb371ad81a1257f9fb59c64d1d
Robert Berry <robert.berry@theguardian.com>
2015-01-15 15:55:04 +0000

    Grant you mad genius

--- onward/app/controllers/StocksController.scala
+++ onward/app/controllers/StocksController.scala
@@ -4,15 +4,14 @@ import common.JsonComponent
 import model.Cached
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import business.{Data => BusinessData, Stocks}
+import business.HideousHack
 import scala.concurrent.duration._
 
 object StocksController extends Controller {
   def stocks = Action { implicit request =>
-    BusinessData.agent.get() match {
+    HideousHack.get match {
       case None => InternalServerError("Business data not loaded")
-      case Some(data) =>
-        val stocks = Stocks.fromFingerpost(data)
+      case Some(stocks) =>
         Cached(1.minute)(JsonComponent.forJsValue(Json.toJson(stocks)))
     }
   }
