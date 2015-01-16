83f024458a0839891540ed53a76b062f3fcba2ef
Francis Carr <francis.carr@guardian.co.uk>
2015-01-16 14:15:03 +0000

    Remove draft and add NoCache

--- facia-press/app/controllers/Application.scala
+++ facia-press/app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import common.ExecutionContexts
 import frontpress.FrontPress
+import model.NoCache
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import services.ConfigAgent
@@ -15,11 +16,8 @@ object Application extends Controller with ExecutionContexts {
     Ok(ConfigAgent.contentsAsJsonString).withHeaders("Content-Type" -> "application/json")
   }
 
-  def generateDraftPressedFor(path: String) = Action.async { request =>
-    FrontPress.pressDraftByPathId(path).map(Json.stringify(_)).map(Ok.apply(_))
-  }
-
   def generateLivePressedFor(path: String) = Action.async { request =>
-    FrontPress.pressLiveByPathId(path).map(Json.stringify(_)).map(Ok.apply(_))
-  }
+    FrontPress.pressLiveByPathId(path).map(Json.stringify(_))
+      .map(Ok.apply(_))
+      .map(NoCache.apply)}
 }
