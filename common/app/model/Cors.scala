8461afbb0032e5b585c3805d5f364e0e2a81ca70
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-01-19 13:35:19 +0000

    Add an options endpoint for cors css

--- common/app/model/Cors.scala
+++ common/app/model/Cors.scala
@@ -1,19 +1,26 @@
 package model
 
-import play.api.mvc.{RequestHeader, Result, Results}
+import play.api.mvc.{RequestHeader, Results}
 import play.api.mvc.Result
-import conf.Configuration.ajax._
-
+import conf.Configuration.ajax.corsOrigins
 
 object Cors extends Results {
-  def apply(result: Result)(implicit request: RequestHeader): Result = {
+  def apply(result: Result, allowedMethods: Option[String] = None)(implicit request: RequestHeader): Result = {
+
+    val controlHeaders = List(
+      allowedMethods.map("Access-Control-Allow-Methods" -> _),
+      request.headers.get("Access-Control-Request-Headers").map("Access-Control-Allow-Headers" -> _)
+    ).flatten
+
     request.headers.get("Origin") match {
       case None => result
-      case Some(requestOrigin) =>
-        result.withHeaders(
+      case Some(requestOrigin) => {
+        val headers = controlHeaders ++ List(
           "Access-Control-Allow-Origin" -> corsOrigins.find(_ == requestOrigin).getOrElse("*"),
-          "Access-Control-Allow-Credentials" -> "true"
-        )
+          "Access-Control-Allow-Credentials" -> "true",
+          "Access-Control-Allow-Headers" -> "accept, content-type")
+        result.withHeaders(headers: _*)
+      }
     }
   }
 }
--- diagnostics/app/controllers/TinyResponse.scala
+++ diagnostics/app/controllers/TinyResponse.scala
@@ -1,8 +1,10 @@
 package controllers
 
-import model.NoCache
-import play.api.mvc.{Result, Results}
+import model.{Cors, NoCache}
+import play.api.mvc.{RequestHeader, Result, Results}
 
 object TinyResponse extends Results {
-  def apply(): Result = NoCache(NoContent)
+  def apply(allowedMethods: Option[String] = None)(implicit request: RequestHeader): Result = {
+    Cors(NoCache(NoContent), allowedMethods)
+  }
 }
