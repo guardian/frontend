3992d123208ca48a729adb45be02316e8051370c
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-01-20 18:38:02 +0000

    Always respond with X-Requested-With

--- common/app/model/Cors.scala
+++ common/app/model/Cors.scala
@@ -7,16 +7,15 @@ import conf.Configuration.ajax.corsOrigins
 object Cors extends Results {
   def apply(result: Result, allowedMethods: Option[String] = None)(implicit request: RequestHeader): Result = {
 
-    val controlHeaders = List(
-      allowedMethods.map("Access-Control-Allow-Methods" -> _),
-      request.headers.get("Access-Control-Request-Headers").map("Access-Control-Allow-Headers" -> _)
-    ).flatten
+    val responseHeaders = request.headers.get("Access-Control-Request-Headers").toList
+      .:+("X-Requested-With").mkString(",")
 
     request.headers.get("Origin") match {
       case None => result
       case Some(requestOrigin) => {
-        val headers = controlHeaders ++ List(
+        val headers = allowedMethods.map("Access-Control-Allow-Methods" -> _).toList ++ List(
           "Access-Control-Allow-Origin" -> corsOrigins.find(_ == requestOrigin).getOrElse("*"),
+          "Access-Control-Allow-Headers" -> responseHeaders,
           "Access-Control-Allow-Credentials" -> "true")
         result.withHeaders(headers: _*)
       }
