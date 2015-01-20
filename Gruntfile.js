7eb49a24b53ef3e3c18bb743a6ac8ec08f5fc7b8
Alex Sanders <a@snd.rs>
2015-01-20 16:08:15 +0000

    make sure svgs are moved before js compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function (grunt) {
 
     });
     grunt.registerTask('compile:js', function(fullCompile) {
-        grunt.task.run(['requirejs', 'copy:javascript']);
+        grunt.task.run(['copy:inlineSVGs', 'svgmin:inlineSVGs', 'requirejs', 'copy:javascript']);
         if (!options.isDev) {
             grunt.task.run('uglify:javascript');
         }
@@ -90,7 +90,7 @@ module.exports = function (grunt) {
     });
     grunt.registerTask('compile:fonts', ['mkdir:fontsTarget', 'webfontjson']);
     grunt.registerTask('compile:flash', ['copy:flash']);
-    grunt.registerTask('compile:conf', ['copy:headJs', 'copy:headCss', 'copy:assetMap', 'copy:inlineSVGs', 'svgmin:inlineSVGs']);
+    grunt.registerTask('compile:conf', ['copy:headJs', 'copy:headCss', 'copy:assetMap']);
     grunt.registerTask('compile', [
         'concurrent:compile',
         'compile:fonts',
