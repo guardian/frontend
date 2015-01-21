05f4be5b6e9b66beb8e20620872c0e08f42fa471
Robert Berry <robert.berry@theguardian.com>
2015-01-21 11:09:59 +0000

    Add more logging for headers

--- weather/app/controllers/LocationsController.scala
+++ weather/app/controllers/LocationsController.scala
@@ -27,11 +27,17 @@ object LocationsController extends Controller with ExecutionContexts with Loggin
     WeatherMetrics.whatIsMyCityRequests.increment()
 
     def cityFromRequestEdition = CityResponse.fromEdition(Edition(request))
+    
+    val maybeCity = request.headers.get(CityHeader)
+    val maybeRegion = request.headers.get(RegionHeader)
+    val maybeCountry = request.headers.get(CountryHeader)
+
+    log.info(s"What is my city request with headers $maybeCity $maybeRegion $maybeCountry")
 
     tuple3(
-      request.headers.get(CityHeader),
-      request.headers.get(RegionHeader),
-      request.headers.get(CountryHeader)
+      maybeCity,
+      maybeRegion,
+      maybeCountry
     ) match {
       case Some((city, region, country)) =>
         log.info(s"Received what is my city? request. Geo info: City=$city Region=$region Country=$country")
