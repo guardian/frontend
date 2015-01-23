36022131725c64e1bc59cd4d5748a82d8b2dfa5c
Robert Berry <robert.berry@theguardian.com>
2015-01-23 15:11:07 +0000

    Minor refactor

--- diagnostics/app/model/diagnostics/css/Css.scala
+++ diagnostics/app/model/diagnostics/css/Css.scala
@@ -2,26 +2,25 @@ package model.diagnostics.css
 
 import play.api.libs.json._
 
+object CssReport {
+  implicit val jsonReads = Json.reads[CssReport]
+}
+
 case class CssReport(
   href: String,
   breakpoint: String,
   className: String,
   contentType: String,
-  selectors: JsObject ) {
-
-  override def toString(): String = {
-    selectors.value.toList.collect {
-      case (key: String, exists:JsBoolean) => s"css ; $key ; $exists ; $contentType ; $breakpoint ; $className $href"
+  selectors: Map[String, Boolean]
+) {
+  override def toString: String = {
+    selectors.toList.map {
+      case (key: String, exists: Boolean) => s"css ; $key ; $exists ; $contentType ; $breakpoint ; $className $href"
     }.mkString("\n")
   }
 }
 
-object CssReport {
-  implicit val reads = Json.reads[CssReport]
-}
-
 object Css extends common.Logging {
-
   def report(requestBody: JsValue): Unit = {
     requestBody.validate[CssReport] match {
       case JsSuccess(report, _) => log.info("\n" + report.toString)
