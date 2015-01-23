784fa04ac842de0a044311e021a785530400f399
Alex Sanders <a@snd.rs>
2015-01-23 14:21:29 +0000

    add images to core.js

---
+++ static/src/javascripts/projects/common/views/svgs.js
@@ -0,0 +1,14 @@
+// Include any images needed in templates here
+
+define([
+    'inlineSvg!svgs/marque-36!icon',
+    'inlineSvg!svgs/marque-36!icon!creative__marque'
+], function (
+    marque_36_icon,
+    marque_36_icon_creative__marque
+) {
+    return {
+        marque_36_icon: marque_36_icon,
+        marque_36_icon_creative__marque: marque_36_icon_creative__marque
+    }
+})
