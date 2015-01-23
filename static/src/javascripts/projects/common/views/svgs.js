3c315f0ed0dbe2a9f44121900d19718a6ed2aa44
Alex Sanders <a@snd.rs>
2015-01-23 15:21:26 +0000

    use commonjs style to reduce duplication

--- static/src/javascripts/projects/common/views/svgs.js
+++ static/src/javascripts/projects/common/views/svgs.js
@@ -1,14 +1,8 @@
 // Include any images needed in templates here
 
-define([
-    'inlineSvg!svgs/marque-36!icon',
-    'inlineSvg!svgs/marque-36!icon!creative__marque'
-], function (
-    marque_36_icon,
-    marque_36_icon_creative__marque
-) {
+define(function (require, exports, module) {
     return {
-        marque_36_icon: marque_36_icon,
-        marque_36_icon_creative__marque: marque_36_icon_creative__marque
+        marque_36_icon: require('inlineSvg!svgs/marque-36!icon'),
+        marque_36_icon_creative__marque: require('inlineSvg!svgs/marque-36!icon!creative__marque')
     }
 })
