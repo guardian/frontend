e699f1e789ce8a63bf9dcc0dc7d7ec1ad054d84b
Zofia <zofia.korcz@guardian.co.uk>
2015-01-26 10:18:03 +0000

    data-link-name added

--- static/src/javascripts/projects/common/modules/ui/message.js
+++ static/src/javascripts/projects/common/modules/ui/message.js
@@ -26,8 +26,8 @@ define([
         this.permanent = opts.permanent || false;
         this.type = opts.type || 'banner';
         this.pinOnHide = opts.pinOnHide || false;
-        this.siteMessageLinkName = opts.siteMessageLinkName;
-        this.siteMessageCloseBtn = opts.siteMessageCloseBtn;
+        this.siteMessageLinkName = opts.siteMessageLinkName || '';
+        this.siteMessageCloseBtn = opts.siteMessageCloseBtn || '';
         this.prefs = 'messages';
 
         this.$footerMessage = $('.js-footer-message');
@@ -54,8 +54,6 @@ define([
         if (this.siteMessageCloseBtn) {
             $('.site-message__close-btn', '.site-message').attr('data-link-name', this.siteMessageCloseBtn);
         }
-
-        this.siteMessageCloseBtn = opts.siteMessageCloseBtn;)
 
         $('.site-message').addClass('site-message--' + this.type).addClass('site-message--' +  this.id);
         $('.site-message').removeClass('is-hidden');
