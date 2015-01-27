3ed2d2b4dcf145958b47a151503f950bff373166
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-01-27 17:34:46 +0000

    Do not clean up the GU_VIEW cookie

--- static/src/javascripts/bootstraps/common.js
+++ static/src/javascripts/bootstraps/common.js
@@ -238,7 +238,7 @@ define([
             },
 
             cleanupCookies: function () {
-                cookies.cleanUp(['mmcore.pd', 'mmcore.srv', 'mmid', 'GU_ABFACIA', 'GU_FACIA', 'GU_ALPHA', 'GU_ME', 'GU_VIEW']);
+                cookies.cleanUp(['mmcore.pd', 'mmcore.srv', 'mmid', 'GU_ABFACIA', 'GU_FACIA', 'GU_ALPHA', 'GU_ME']);
             },
 
             // display a flash message to devices over 600px who don't have the mobile cookie
