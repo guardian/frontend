9a732975553c23b8dc6acc53ce55828b58c974aa
Francis Carr <francis.carr@guardian.co.uk>
2015-01-27 14:58:50 +0000

    Add diff to ArchiveRequest and mix in correctly

--- facia-tool/app/tools/FaciaToolArchive.scala
+++ facia-tool/app/tools/FaciaToolArchive.scala
@@ -6,13 +6,13 @@ import com.amazonaws.services.dynamodbv2.model._
 import common.{ExecutionContexts, Logging}
 import conf.{Switches, Configuration}
 import org.joda.time.{DateTimeZone, DateTime}
-import play.api.libs.json.{Json, JsValue}
+import play.api.libs.json._
 import scala.collection.JavaConverters._
 import awswrappers.dynamodb._
 
 import scala.util.{Failure, Success}
 
-case class ArchiveRequest(email: String, updateJson: JsValue)
+case class ArchiveRequest(email: String, updateJson: JsValue, diff: JsValue)
 
 object FaciaToolArchive extends ExecutionContexts with Logging {
   val TableName = "FaciaToolUpdateHistory"
@@ -29,19 +29,24 @@ object FaciaToolArchive extends ExecutionContexts with Logging {
   def archive(archiveRequest: ArchiveRequest): Unit = {
     dynamoClient match {
       case Some(client) if Switches.FaciaDynamoArchive.isSwitchedOn =>
-        val now = DateTime.now().withZone(DateTimeZone.UTC)
-        val putItemRequest = new PutItemRequest()
-          .withTableName(TableName)
-          .withItem(Map[String, AttributeValue](
-          ("day", new AttributeValue().withS(dayKey(now))),
-          ("time", new AttributeValue().withS(timeKey(now))),
-          ("email", new AttributeValue().withS(archiveRequest.email)),
-          ("rawupdate", new AttributeValue().withS(Json.stringify(archiveRequest.updateJson)))).asJava)
+        Json.toJson(archiveRequest.updateJson).transform[JsObject](Reads.JsObjectReads) match {
+          case JsSuccess(result, _) =>
+            val archiveJson: String = Json.prettyPrint(result + ("diff", archiveRequest.diff))
+            val now = DateTime.now().withZone(DateTimeZone.UTC)
+            val putItemRequest = new PutItemRequest()
+              .withTableName(TableName)
+              .withItem(Map[String, AttributeValue](
+              ("day", new AttributeValue().withS(dayKey(now))),
+              ("time", new AttributeValue().withS(timeKey(now))),
+              ("email", new AttributeValue().withS(archiveRequest.email)),
+              ("rawupdate", new AttributeValue().withS(archiveJson))).asJava)
 
-        client.putItemFuture(putItemRequest).onComplete {
-          case Success(_) => log.info(s"Successfully put archive record for ${archiveRequest.email}")
-          case Failure(t) => log.warn(s"Error putting archive record for ${archiveRequest.email}: $t")
-        }
+            client.putItemFuture(putItemRequest).onComplete {
+              case Success(_) => log.info(s"Successfully put archive record for ${archiveRequest.email}")
+              case Failure(t) => log.warn(s"Error putting archive record for ${archiveRequest.email}: $t")
+            }
+
+          case JsError(errors)  => log.warn(s"Could not archive request from ${archiveRequest.email}: $errors")}
       case Some(_) => log.warn(s"Did not archive to dynamo for ${archiveRequest.email}; switched OFF")
       case None    => log.warn(s"No client to archive record for ${archiveRequest.email}, is this PROD")
     }
