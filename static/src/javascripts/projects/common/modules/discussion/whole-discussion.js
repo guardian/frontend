e99e260ccde04b69a40c7c25810f8479bc25e478
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-01-27 14:41:08 +0000

    Always load oldest comments first, and order them after

--- static/src/javascripts/projects/common/modules/discussion/whole-discussion.js
+++ static/src/javascripts/projects/common/modules/discussion/whole-discussion.js
@@ -31,8 +31,10 @@ define([
 
     WholeDiscussion.prototype.firstPageLoaded = function (resp) {
         // Add the first page of comments to the discussion object.
+        this.storeCommentPage(resp, 1);
+
         // Keep the container so it can be easily reduced.
-        this.discussion[1] = $('ul', bonzo.create(resp.commentsHtml));
+        this.discussionContainer = $('ul', bonzo.create(resp.commentsHtml)).empty();
         this.postedCommentHtml = resp.postedCommentHtml;
         this.lastPage = resp.lastPage;
 
@@ -40,10 +42,22 @@ define([
         return _.range(2, this.lastPage + 1);
     };
 
+    // Caches a bonzo object/array of comments, so that they can be re-assembled when the load is complete.
+    WholeDiscussion.prototype.storeCommentPage = function (response, page) {
+        var comments = $('li', bonzo.create(response.commentsHtml));
+        if (this.params.orderBy === 'newest') {
+            comments = comments.map(function (comment) {
+                return comment;
+            }).reverse();
+        }
+        this.discussion[page] = comments;
+    };
+
     WholeDiscussion.prototype.loadPage = function (pageNumber) {
 
+        // Always load in oldest order, to ensure pages are consistent whilst new comments are posted.
         var queryParams = {
-            orderBy: this.params.orderBy,
+            orderBy: 'oldest',
             page: pageNumber,
             pageSize: commentsPerPage,
             displayThreaded: this.params.displayThreaded
@@ -68,18 +82,21 @@ define([
         .then(function (responses) {
             _.forEach(responses, function (response, index) {
                 // The first page has been loaded, and pages are not zero-based, so adjust the index.
-                var page = index + 2;
-                this.discussion[page] = $('li', bonzo.create(response.commentsHtml));
+                this.storeCommentPage(response, index + 2);
             }.bind(this));
         }.bind(this));
     };
 
     WholeDiscussion.prototype.makeDiscussionResponseObject = function () {
 
+        if (this.params.orderBy === 'newest') {
+            this.discussion.reverse();
+        }
+
         var comments = this.discussion.reduce(function (result, comments) {
             result.append(comments);
             return result;
-        });
+        }, this.discussionContainer);
 
         return {
             paginationHtml: '',
@@ -92,25 +109,7 @@ define([
     WholeDiscussion.prototype.loadAllComments = function () {
 
         // Always load the first page, to retrieve the number of comments in the discussion.
-        var queryParams = {
-            orderBy: this.params.orderBy,
-            page: 1,
-            pageSize: commentsPerPage,
-            displayThreaded: this.params.displayThreaded
-        };
-
-        if (this.params.maxResponses) {
-            queryParams.maxResponses = this.params.maxResponses;
-        }
-
-        return ajaxPromise(
-        {
-            url: '/discussion/' + this.discussionId + '.json',
-            type: 'json',
-            method: 'get',
-            crossOrigin: true,
-            data: queryParams
-        })
+        return this.loadPage(1)
         .then(this.firstPageLoaded.bind(this))
         .then(this.loadRemainingPages.bind(this))
         .then(this.makeDiscussionResponseObject.bind(this));
