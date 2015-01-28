e1d75fd06b2c945c9f4570ccc0420e49692eb097
Matt Osborn <matthew.osborn@theguardian.com>
2015-01-28 12:03:42 +0000

    remove foresee sampling

--- static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
+++ static/src/javascripts/projects/common/modules/analytics/foresee-survey.js
@@ -15,12 +15,10 @@ define([
     function load() {
 
         var isNetworkFront = config.page.contentType === 'Network Front',
-            sampleRate = detect.isBreakpoint({max: 'mobile'}) ? 0.008 : 0.006, // 0.8% mobile and 0.6% rest
-            sample = Math.random() <= sampleRate,
             hasForcedOptIn = /forceForesee/.test(location.hash);
 
         // the Foresee code is large, we only want to load it in when necessary.
-        if (!Cookie.get('GU_TEST') && !isNetworkFront && (window.openForeseeWhenReady || sample || hasForcedOptIn)) {
+        if (!Cookie.get('GU_TEST') && !isNetworkFront && (window.openForeseeWhenReady || hasForcedOptIn)) {
             openForesee();
         }
 
