b4eb5a920db1a47d7115df27ad885c954c42094e
Robert Berry <robert.berry@theguardian.com>
2015-01-29 17:03:22 +0000

    Only do one look up for data-discussion-id elements

--- static/src/javascripts/projects/common/modules/discussion/comment-count.js
+++ static/src/javascripts/projects/common/modules/discussion/comment-count.js
@@ -32,9 +32,17 @@ define([
         },
         defaultTemplate = commentCountTemplate;
 
-    function getContentIds() {
-        return _.chain(qwery('[' + attributeName + ']'))
-                    .map(function (el) { return bonzo(el).attr(attributeName); })
+    function getElementsIndexedById() {
+        var elements = qwery('[' + attributeName + ']');
+
+        return _.zipObject(_.map(elements, function (el) {
+            return bonzo(el).attr(attributeName);
+        }), elements)
+    }
+
+    function getContentIds(indexedElements) {
+        return _.chain(indexedElements)
+                    .keys()
                     .uniq()
                     .sortBy()
                     .join(',')
@@ -46,43 +54,43 @@ define([
         return (a ? a.pathname : '') + '#comments';
     }
 
-    function renderCounts(counts) {
+    function renderCounts(counts, indexedElements) {
         counts.forEach(function (c) {
-            forEach(qwery('[data-discussion-id="' + c.id + '"]'), function (node) {
-                var format,
-                    $node = bonzo(node),
-                    commentOrComments = (c.count === 1 ? 'comment' : 'comments'),
-                    url = $node.attr('data-discussion-url') || getContentUrl(node),
-                    $container,
-                    meta,
-                    html;
+            var node = indexedElements[c.id],
+                format,
+                $node = bonzo(node),
+                commentOrComments = (c.count === 1 ? 'comment' : 'comments'),
+                url = $node.attr('data-discussion-url') || getContentUrl(node),
+                $container,
+                meta,
+                html;
 
-                if ($node.attr('data-discussion-closed') === 'true' && c.count === 0) {
-                    return; // Discussion is closed and had no comments, we don't want to show a comment count
-                }
-                $node.removeClass('u-h');
+            if ($node.attr('data-discussion-closed') === 'true' && c.count === 0) {
+                return; // Discussion is closed and had no comments, we don't want to show a comment count
+            }
+            $node.removeClass('u-h');
 
-                // put in trail__meta, if exists
-                meta = qwery('.js-item__meta', node);
+            format = $node.data('commentcount-format');
+            html = template(templates[format] || defaultTemplate, {
+                url: url,
+                count: formatters.integerCommas(c.count),
+                label: commentOrComments
+            });
 
-                format = $node.data('commentcount-format');
-                html = template(templates[format] || defaultTemplate, {
-                    url: url,
-                    count: formatters.integerCommas(c.count),
-                    label: commentOrComments
-                });
-                $container = meta.length ? bonzo(meta) : $node;
+            meta = qwery('.js-item__meta', node);
+            $container = meta.length ? bonzo(meta) : $node;
 
-                requestAnimationFrame(function () {
-                    $container.append(html);
-                    $node.removeAttr(attributeName);
-                });
+            requestAnimationFrame(function () {
+                $container.append(html);
+                $node.removeAttr(attributeName);
             });
         });
     }
 
     function getCommentCounts() {
-        var ids = getContentIds();
+        var indexedElements = getElementsIndexedById(),
+            ids = getContentIds(indexedElements);
+
         ajax({
             url: countUrl + ids,
             type: 'json',
@@ -90,7 +98,7 @@ define([
             crossOrigin: true,
             success: function (response) {
                 if (response && response.counts) {
-                    renderCounts(response.counts);
+                    renderCounts(response.counts, indexedElements);
                     mediator.emit('modules:commentcount:loaded', response.counts);
                 }
             }
