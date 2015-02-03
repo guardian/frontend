daeceb05935e2bd05a759e525844a4f42816ec7f
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-02-03 13:46:27 +0000

    Ctrl-c Ctrl-v
    Copy paste URL anywere and they'll appear in the clipboard

--- facia-tool/public/js/utils/global-listeners.js
+++ facia-tool/public/js/utils/global-listeners.js
@@ -1,23 +1,42 @@
-define([
-    'EventEmitter'
-], function (
-    EventEmitter
-) {
-    var bus = new EventEmitter(),
-        registeredListeners = {};
+/* global _ */
+define(function () {
+    var registeredListeners = {};
 
-    function registerListener (event, callback) {
+    function registerListener (event, callback, scope) {
         if (!registeredListeners[event]) {
-            registeredListeners[event] = function (eventObject) {
-                bus.emit(event, eventObject);
-            };
+            registeredListeners[event] = [];
 
-            (event === 'resize' ? $(window) : $('document, body')).on(event, registeredListeners[event]);
+            (event === 'resize' ? $(window) : $('document, body')).on(event, handle.bind(null, event));
         }
-        bus.on(event, callback);
+        registeredListeners[event].push({
+            callback: callback,
+            scope: scope
+        });
+    }
+
+    function handle (event, eventObject) {
+        return !_.find(registeredListeners[event], function (handler) {
+            // Stop when a callback return false
+            return handler.callback.call(handler.scope || this, eventObject) === false;
+        });
+    }
+
+    function removeListener (event, callback, scope) {
+        if (!registeredListeners[event]) {
+            return;
+        }
+        registeredListeners[event] = _.filter(registeredListeners[event], function (handler) {
+            if (callback && callback !== handler.callback) {
+                return true;
+            } else if (scope && handler.scope !== scope) {
+                return true;
+            }
+            return false;
+        });
     }
 
     return {
-        on: registerListener
+        on: registerListener,
+        off: removeListener
     };
 });
