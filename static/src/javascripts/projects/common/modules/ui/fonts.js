2ec204ae1e4af4055498642afaf41a7ccd2f4861
Alex Sanders <a@snd.rs>
2015-02-03 08:39:04 +0000

    load fonts if font-smoothing is enabled

--- static/src/javascripts/projects/common/modules/ui/fonts.js
+++ static/src/javascripts/projects/common/modules/ui/fonts.js
@@ -1,4 +1,7 @@
-/*jshint loopfunc: true */
+// This module attempts to load webfonts in over XMLHTTP, add them to the
+// current page and then save them to `localStorage` for next time.
+
+/* jshint loopfunc: true */
 define([
     'bonzo',
     'qwery',
@@ -27,52 +30,73 @@ define([
         fileFormat    = detect.getFontFormatSupport(navigator.userAgent);
 
     return {
-
         load: function () {
-
+            // If any of the following conditions are met, we just leave it and
+            // `return false`:
+            // - fonts switch is off
+            // - fonts have already been loaded by inserting `link` elements
+            //   from `javaScriptLaterSteps.scala.html#loadFontsAsynchronously`
+            // - user has switched off fonts with a cookie
             if (!config.switches.webFonts || qwery('.webfonts').length || cookies.get('GU_fonts') === 'off') {
                 return false;
             }
 
-            if (!storage.local.get(storagePrefix + 'format')) {
-                storage.local.set(storagePrefix + 'format', fileFormat);
-            }
-
-            $('.webfont:not([data-cache-full])').each(function (webfont) {
-                var $webFont      = bonzo(webfont),
-                    fontFile      = $webFont.data('cache-file-' + (detect.fontHinting === 'Off' ? '' : 'hinted-' + detect.fontHinting + '-') + fileFormat),
-                    minBreakpoint = $webFont.data('min-breakpoint');
-
-                if (minBreakpoint && !detect.isBreakpoint({ min: minBreakpoint })) {
-                    return;
+            // All good, so check to see if we should should use webfonts.
+            if (detect.fontSmoothing()) {
+                // Make sure we will remember what format we want later.
+                if (!storage.local.get(storagePrefix + 'format')) {
+                    storage.local.set(storagePrefix + 'format', fileFormat);
                 }
 
-                ajax({
-                    url:               fontFile,
-                    type:              'jsonp',
-                    jsonpCallbackName: 'guFont',
-                    success:            function (resp) {
-                        if (!resp) {
-                            raven.captureMessage('Failed to load fonts');
-                            return;
-                        }
+                // Now we want to load them. Query the `style.webfont` elements
+                // for each font's URI, if it's not already been loaded from `localStorage`
+                $('.webfont:not([data-cache-full])').each(function (webfont) {
+                    var $webFont      = bonzo(webfont),
+                        fontFile      = $webFont.data('cache-file-' + (detect.fontHinting === 'Off' ? '' : 'hinted-' + detect.fontHinting + '-') + fileFormat),
+                        minBreakpoint = $webFont.data('min-breakpoint');
 
-                        // clear old font and store
-                        var fontInfo = fontFile.match(/fonts\/([^/]*?)\/?([^/]*)\.(woff2|woff|tff).json$/),
-                            fontHash = fontInfo[1],
-                            fontName = fontInfo[2];
-
-                        storage.local.clearByPrefix(storagePrefix + fontName.replace(/(CleartypeHinted|AutoHinted)$/, ''));
-                        storage.local.set(storagePrefix + fontName + '.' + fontHash, resp.css);
-
-                        $webFont.text(resp.css)
-                            .attr('data-cache-full', 'data-cache-full');
-
-                        mediator.emit('modules:fonts:loaded', name);
+                    // Some fonts are not loaded is the viewport is small enough
+                    // (i.e. mobile).
+                    if (minBreakpoint && !detect.isBreakpoint({ min: minBreakpoint })) {
+                        return;
                     }
-                });
-            });
 
+                    // Request them. Once we've got them, we add the font data
+                    // to the page by injecting it into the relevent
+                    // `style.webfont` element, and save it to `localStorage`
+                    // for next time too.
+                    ajax({
+                        url:               fontFile,
+                        type:              'jsonp',
+                        jsonpCallbackName: 'guFont',
+                        success:            function (resp) {
+
+                            // If the response is no good, record an error and go.
+                            if (!resp) {
+                                raven.captureMessage('Failed to load fonts');
+                                return;
+                            }
+
+                            // Handle the response.
+                            var fontInfo = fontFile.match(/fonts\/([^/]*?)\/?([^/]*)\.(woff2|woff|tff).json$/),
+                                fontHash = fontInfo[1],
+                                fontName = fontInfo[2];
+
+                            // Insert the css from the response into the style element on the page…
+                            $webFont.text(resp.css).attr('data-cache-full', 'data-cache-full');
+
+                            // …then clear out any old fonts
+                            storage.local.clearByPrefix(storagePrefix + fontName.replace(/(CleartypeHinted|AutoHinted)$/, ''));
+
+                            // …then save the new ones
+                            storage.local.set(storagePrefix + fontName + '.' + fontHash, resp.css);
+
+                            // …and finally announce the new arrival.
+                            mediator.emit('modules:fonts:loaded', name);
+                        }
+                    });
+                });
+            }
         }
 
     };
