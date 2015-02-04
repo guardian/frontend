6a8fe43b2365f433b875c922af14e8f383fb80f0
Alex Sanders <a@snd.rs>
2015-02-04 16:12:55 +0000

    Record errors within fastdom

---
+++ static/src/javascripts/projects/common/utils/fastdom.js
@@ -0,0 +1,19 @@
+// Record errors within fastdom:
+// https://github.com/wilsonpage/fastdom#exceptions
+
+define([
+    'fastdom',
+    'raven'
+],
+function (
+    fastdom,
+    raven
+) {
+    fastdom.onError = function (error) {
+        raven.captureException(new Error('Error inside fastdom: ' + error), {
+            tags: {
+                javascript: 'fastdom'
+            }
+        });
+    };
+});
