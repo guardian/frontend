4c21e43050cbe06e30854925d5ed170cfb002ce5
Stephan Fowler <stephanfowler@gmail.com>
2015-02-05 14:03:46 +0000

    Do the math. Properly.

--- static/src/javascripts/projects/common/modules/analytics/css-logging.js
+++ static/src/javascripts/projects/common/modules/analytics/css-logging.js
@@ -15,8 +15,27 @@ define([
     mediator,
     beacon
 ) {
-    var rxPsuedoClass = new RegExp(/:+[^\s\,]+/g),
+    var sample = 500,
+        rxPsuedoClass = new RegExp(/:+[^\s\,]+/g),
         rxSeparator = new RegExp(/\s*,\s*/g);
+
+    function getSelectors(all) {
+        var rand,
+            len,
+            rules = _.chain(getStylesheets())
+            .pluck('rules' || 'cssRules')
+            .map(_.values)
+            .flatten()
+            .value();
+
+        if (all) {
+            return rules;
+        } else {
+            len = rules.length;
+            rand = _.random(0, len);
+            return rules.slice(rand, rand + sample).concat(rand + sample < len ? [] : rules.slice(0, (rand + sample) % len));
+        }
+    }
 
     function getStylesheets() {
         return _.chain(document.styleSheets)
@@ -29,58 +48,33 @@ define([
             .value();
     }
 
-    function getRandomStylesheet() {
-        var stylesheets = getStylesheets(),
-            stylesheetLengths = scan(
-                stylesheets.map(function (sheet) { return _.values(sheet.rules || sheet.cssRules).length; }),
-                function (x, y) { return x + y; },
-                0
-            ),
-            totalRules = stylesheetLengths.pop(),
-            randomRule = _.random(0, totalRules);
-
-        return stylesheets[_.reduce(stylesheetLengths, function (acc, len, i) { return randomRule > len ? i : acc; }, 0)];
-    }
-
-    function sendReport(stylesheet, allRules) {
-        var sampleSize = 1000,
-            offset,
-            rules = _.chain(stylesheet.rules || stylesheet.cssRules)
+    function sendReport(rules, forceAjax) {
+        beacon.postJson('/css', JSON.stringify({
+            selectors: _.chain(rules)
                 .map(function (r) { return r && r.selectorText; })
                 .compact()
-                .value();
-
-        if (!allRules) {
-            offset = _.random(0, Math.max(0, rules.length - sampleSize));
-            rules = rules.slice(offset, offset + sampleSize);
-        }
-
-        beacon.postJson('/css', JSON.stringify({
-            selectors: rules.reduce(function (isUsed, rule) {
-                _.each(rule.replace(rxPsuedoClass, '').split(rxSeparator), function (s) {
-                    if (_.isUndefined(isUsed[s])) {
-                        isUsed[s] = !!document.querySelector(s);
-                    }
-                });
-                return isUsed;
-            }, {}),
+                .reduce(function (isUsed, rule) {
+                    _.each(rule.replace(rxPsuedoClass, '').split(rxSeparator), function (s) {
+                        if (_.isUndefined(isUsed[s])) {
+                            isUsed[s] = !!document.querySelector(s);
+                        }
+                    });
+                    return isUsed;
+                }, {})
+                .value(),
             contentType: config.page.contentType,
-            breakpoint: detect.getBreakpoint(),
-            href: stylesheet.href ? url.getPath(stylesheet.href).replace(/stylesheets\/\w+\//, '') : '',
-            className: stylesheet.ownerNode ? stylesheet.ownerNode.className : ''
-        }), allRules);
+            breakpoint: detect.getBreakpoint()
+        }), forceAjax);
     }
 
     function makeSender(sendAll) {
-        return function (clickSpec) {
+        return _.debounce(function (clickSpec) {
             if (!clickSpec || clickSpec.samePage) {
                 setTimeout(function () {
-                    _.each(sendAll ? getStylesheets() : [getRandomStylesheet()], function (stylesheet) {
-                        sendReport(stylesheet, sendAll);
-                    });
+                    sendReport(getSelectors(sendAll), sendAll);
                 }, sendAll ? 0 : _.random(0, 3000));
             }
-        };
+        }, 300);
     }
 
     return function (sendAll) {
@@ -88,7 +82,7 @@ define([
 
         sendAll = sendAll || window.location.hash === '#csslogging';
 
-        if (sendAll || _.random(1, 5000) === 1) {
+        if (sendAll || _.random(1, 2500) === 1) {
             sender = makeSender(sendAll);
             sender();
             mediator.on('module:clickstream:interaction', sender);
