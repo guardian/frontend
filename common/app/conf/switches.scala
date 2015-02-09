a26d3f6b93fa7735093c68baeb963d11aeee2962
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-02-09 15:19:25 +0000

    Add switch for hide interactive UI

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -364,6 +364,11 @@ object Switches {
     sellByDate = never
   )
 
+  val HideInteractiveUi = Switch("Feature", "hide-interactive-ui",
+    "If this is switched on interactives can be rendered without page funiture.",
+    safeState = On, sellByDate = new LocalDate(2015, 2, 28)
+  )
+
   val Hmtl5MediaCompatibilityCheck = Switch("Feature", "html-5-media-compatibility-check",
     "If switched on then will will infer the video player tech priority based on the video source codec",
     safeState = On, sellByDate = never)
