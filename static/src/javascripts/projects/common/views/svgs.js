f56f4db4c71ac003f09b589781ae317bdddf897d
Sam Morris <mail@sammorrisdesign.co.uk>
2015-02-10 14:25:25 +0000

    lol I don't know

--- static/src/javascripts/projects/common/views/svgs.js
+++ static/src/javascripts/projects/common/views/svgs.js
@@ -2,20 +2,27 @@
 // This file is only required by core, and so has a long cache time.
 
 define([
+    'bonzo',
     'inlineSvg!svgs/marque-36!icon'
 ], function (
+    bonzo,
     marque36icon
 ) {
     var svgs = {
         marque36icon: marque36icon
     };
 
-    return function (name, classes) {
+    return function (name, classes, attributes) {
         var svg = svgs[name];
 
         // Only mess with classes if we actually need to.
         if (classes) {
             svg = svg.replace(/class="/, '$&' + classes.join(' ') + ' ');
+        }
+
+        // Only mess with attributes if we actually need to.
+        if (attributes) {
+            svg = bonzo(svg).attr('test', 'something');
         }
 
         return svg;
