18c291e538baccaf139b60c9ccd06f7f851ef81c
Zofia <zofia.korcz@guardian.co.uk>
2015-02-12 17:56:19 +0000

    expandable unit test

--- static/test/javascripts/spec/common/commercial/creatives/expandable.spec.js
+++ static/test/javascripts/spec/common/commercial/creatives/expandable.spec.js
@@ -1,13 +1,47 @@
 define([
-    'common/modules/commercial/creatives/expandable'
+    'common/modules/commercial/creatives/expandable',
+    'helpers/fixtures',
+    'qwery'
 ], function(
-    Expandable
+    Expandable,
+    fixtures,
+    qwery
 ) {
+
+    var fixturesConfig = {
+        id: 'expandable-ad-slot',
+        fixtures: [
+            '<div class="expandable-ad-slot"></div>'
+        ]
+    };
 
     describe('Expandable', function() {
 
+        var expandable,
+            $fixturesContainer;
+
         it('should exist', function() {
             expect(Expandable).toBeDefined();
+        });
+
+        it('should be always defined', function () {
+            $fixturesContainer = fixtures.render(fixturesConfig);
+            expandable = new Expandable(qwery('.expandable-ad-slot'), {
+                clickMacro: 'clickMacro',
+                slide1: 'slide1'
+            });
+            expect(expandable).toBeDefined();
+        });
+
+        it('should have expand and close buttons', function () {
+            $fixturesContainer = fixtures.render(fixturesConfig);
+            expandable = new Expandable(qwery('.expandable-ad-slot'), {
+                clickMacro: 'clickMacro',
+                slide1: 'slide1'
+            });
+            expandable.create();
+            expect(qwery('.ad-exp--expand').length).toBe(1);
+            expect(qwery('.ad-exp__close-button').length).toBe(1);
         });
 
     });
