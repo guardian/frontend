d79a4cd2e0632521a66ec3e8302256b28af0e27c
Zofia <zofia.korcz@guardian.co.uk>
2015-02-13 16:42:52 +0000

    fluid250 unit tests

--- static/test/javascripts/spec/common/commercial/creatives/fluid250.spec.js
+++ static/test/javascripts/spec/common/commercial/creatives/fluid250.spec.js
@@ -29,16 +29,28 @@ define([
         it('should be defined', function() {
             $fixturesContainer = fixtures.render(fixturesConfig);
 
-            fluid250 = new Fluid250(qwery('.ad-slot', $fixturesContainer), {});
+            fluid250 = new Fluid250($('.ad-slot', $fixturesContainer), {});
             expect(fluid250).toBeDefined();
         });
 
-        it('ad slot should have a proper fluid250 css class', function() {
+        it('ad slot should always have a proper fluid250 css class', function() {
             $fixturesContainer = fixtures.render(fixturesConfig);
 
-            fluid250 = new Fluid250(qwery('.ad-slot', $fixturesContainer), {}).create();
-            console.log(qwery('.ad-slot'), fluid250);
-            expect(qwery('.ad-slot').hasClass('ad-slot__fluid250')).toBe('true');
+            new Fluid250($('.ad-slot', $fixturesContainer), {}).create();
+            expect($('.ad-slot').hasClass('ad-slot__fluid250')).toBe(true);
+        });
+
+        it('ad slot should have a video iframe with proper styles', function() {
+            $fixturesContainer = fixtures.render(fixturesConfig);
+
+            new Fluid250($('.ad-slot', $fixturesContainer), {
+                videoPositionH: 'left',
+                videoHorizSpace: 10,
+                videoPositionV: 'top',
+                videoURL: 'exampleVideoUrl'
+            }).create();
+            expect(qwery('.fluid250_video--vert-pos-top', '.ad-slot').length).toBe(1);
+            expect(qwery('.fluid250_video--horiz-pos-left', '.ad-slot').length).toBe(1);
         });
 
     });
