c3dda34131df77dc5bcb6444c223a3686f5fb442
Grant Klopper <grant.klopper@theguardian.com>
2015-02-13 10:56:15 +0000

    Fixed preview and Training healthchecks.

--- training/test/PreviewServerTest.scala
+++ training/test/PreviewServerTest.scala
@@ -6,7 +6,7 @@ class TrainingTestSuite extends Suites (
   new TrainingServerTest
 ) with SingleServerSuite {
 
-  override lazy val port: Int = conf.HealthCheck.testPort
+  override lazy val port: Int = controllers.TrainingHealthCheck.port
 }
 
 @DoNotDiscover class TrainingServerTest extends FlatSpec with Matchers with ConfiguredTestSuite {
@@ -14,7 +14,7 @@ class TrainingTestSuite extends Suites (
   // These features are tested elsewhere, this is actually just here to ensure that the
   // training server can start up and serve a page
 
-  "traingin Server" should "be able to serve an article" in goTo("/technology/2014/may/18/de-rosa-idol-bicycle-review-martin-love") { browser =>
+  "training Server" should "be able to serve an article" in goTo("/technology/2014/may/18/de-rosa-idol-bicycle-review-martin-love") { browser =>
     browser.$("body").getText should include ("Debating the pros and cons of each of these materials is the kind of thing that keeps passionate cyclists awake at night")
   }
 }
