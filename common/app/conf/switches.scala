c0576910dbd383bcdc5b492f84c8ea5a5a36009c
Marc Hibbins <marc.hibbins@guardian.co.uk>
2015-02-17 12:36:28 +0000

    Implement a HTTP switch to direct social signin attempts to new Identity Auth app

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -394,6 +394,11 @@ object Switches {
     safeState = On, sellByDate = never
   )
 
+  val IdentitySocialOAuthSwitch = Switch("Feature", "id-social-oauth",
+    "If this switch is on then social sign-in attempts will be directed to Identity OAuth app, rather than the Webapp.",
+    safeState = Off, sellByDate = never
+  )
+
   val FacebookAutoSigninSwitch = Switch("Feature", "facebook-autosignin",
     "If this switch is on then users who have previously authorized the guardian app in facebook and who have not recently signed out are automatically signed in.",
     safeState = Off, sellByDate = never
