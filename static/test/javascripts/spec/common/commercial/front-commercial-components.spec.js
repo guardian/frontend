09372a40b2b5739aef08b5bd20ac2ee5f9e05f68
Zofia <zofia.korcz@guardian.co.uk>
2015-02-23 11:53:15 +0000

    cr fix

--- static/test/javascripts/spec/common/commercial/front-commercial-components.spec.js
+++ static/test/javascripts/spec/common/commercial/front-commercial-components.spec.js
@@ -48,18 +48,18 @@ define([
                 });
 
                 it('should place ad between 3rd and 4th containers if there are 4 or more containers', function () {
-                    '1234'.split('').forEach(function () {
+                    for (var i = 0; i<4; i++) {
                         appendContainer($fixturesContainer);
-                    });
+                    }
                     frontCommercialComponents.init();
 
                     expect(qwery('.ad-slot', $fixturesContainer).length).toBe(1);
                 });
 
                 it('should place ad between 1st and 2nd containers if there are less than 4 containers', function () {
-                    '12'.split('').forEach(function () {
+                    for (var i = 0; i<2; i++) {
                         appendContainer($fixturesContainer);
-                    });
+                    }
                     frontCommercialComponents.init();
 
                     expect(qwery('.ad-slot', $fixturesContainer).length).toBe(1);
@@ -70,9 +70,9 @@ define([
                 it('should place ad between between the 4th and 5th container if a network front', function () {
                     mocks.store['common/utils/config'].page.contentType = 'Network Front';
 
-                    '12345'.split('').forEach(function () {
+                    for (var i = 0; i<5; i++) {
                         appendContainer($fixturesContainer);
-                    });
+                    }
                     frontCommercialComponents.init();
 
                     expect(qwery('.ad-slot', $fixturesContainer).length).toBe(1);
@@ -82,10 +82,10 @@ define([
                 });
 
                 it('should have 1,1 slot for wide breakpoint if there is a page skin', function () {
-                    mocks.store['common/utils/config'].page.hasPageSkin = true;
-                    '12'.split('').forEach(function () {
+                    for (var i = 0; i<2; i++) {
                         appendContainer($fixturesContainer);
-                    });
+                    }
+                    mocks.store['common/utils/config'].page.hasPageSkin = true;
                     frontCommercialComponents.init();
 
                     expect(qwery('.ad-slot', $fixturesContainer).length).toBe(1);
