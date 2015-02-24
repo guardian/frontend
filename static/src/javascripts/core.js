703eb926d82357190474806e464a72a9b7f75fe9
Robert Berry <robert.berry@theguardian.com>
2015-02-24 14:01:18 +0000

    Add fallback image for non-modern browsers

--- static/src/javascripts/core.js
+++ static/src/javascripts/core.js
@@ -29,6 +29,7 @@ require([
     'common/modules/experiments/ab',
     'common/modules/lazyload',
     'common/modules/ui/images',
+    'common/modules/ui/non-modern-images',
     'common/modules/ui/tabs',
     'common/modules/user-prefs',
     'common/views/svgs'
