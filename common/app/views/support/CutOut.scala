d8525f4bb367ad33358176b3a5c34e9d2e50e923
Francis Carr <francis.carr@guardian.co.uk>
2015-02-24 17:06:29 +0000

    Fix CutOut fromTrail to rely on the CutOut coming from facia-scala-client more (Removes logic)

--- common/app/views/support/CutOut.scala
+++ common/app/views/support/CutOut.scala
@@ -9,31 +9,16 @@ import scala.util.{Failure, Success, Try}
 
 object CutOut extends Logging {
   def fromTrail(faciaContent: FaciaContent): Option[CutOut] = {
-    (faciaContent.imageCutout match {
+    faciaContent.imageCutout match {
         case Some(ImageCutout(imageCutoutSrc, Some(imageCutoutSrcHeight), Some(imageCutoutSrcWidth))) =>
-          Try(FaciaImageElement(imageCutoutSrc, imageCutoutSrcWidth.toInt, imageCutoutSrcHeight.toInt)) match {
-            case Success(faciaImageElement) => Option(faciaImageElement)
+          Try((imageCutoutSrcWidth.toInt, imageCutoutSrcHeight.toInt)) match {
+            case Success((width, height)) => Option(CutOut(imageCutoutSrc, Orientation.fromDimensions(width, height)))
             case Failure(t) =>
               log.warn(s"Could not convert width and height to INT: $t")
               None
           }
+        case Some(ImageCutout(imageCutoutSrc, _, _)) => Option(CutOut(imageCutoutSrc, Landscape))
         case _ => None
-      })
-      .map {
-        case FaciaImageElement(src, width, height) => CutOut(src, Orientation.fromDimensions(width, height))
-      } orElse {
-        //TODO: Get rid of this, logic exists in facia-scala-client
-        /** We're assuming here that standard contributor images from CAPI are in landscape, as unfortunately they
-          * do not come with dimensions attached.
-          */
-        if (faciaContent.contributors.length == 1) {
-          for {
-            contributor <- faciaContent.contributors.map(Tag.apply(_)).find(_.contributorLargeImagePath.isDefined)
-            imagePath <- contributor.contributorLargeImagePath
-          } yield CutOut(imagePath, Landscape)
-        } else {
-          None
-        }
       }
   }
 }
