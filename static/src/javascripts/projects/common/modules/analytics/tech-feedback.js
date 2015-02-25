357005824ad0b7f033903b3f814da34f0f9c8a7a
John Duffell <john.duffell@guardian.co.uk>
2015-02-25 12:57:11 +0000

    fix validation errors

--- static/src/javascripts/projects/common/modules/analytics/tech-feedback.js
+++ static/src/javascripts/projects/common/modules/analytics/tech-feedback.js
@@ -12,15 +12,15 @@ define([
 
     return {
         init: function () {
-                var link = $('.js-tech-feedback');
-                bean.on(link[0], 'click', function (e) {
-                    beacon.fire('/counts.gif?c=tech-feedback');
-                    fastdom.write(function () {
-                        $('.js-feedback-thanks').removeClass('footer__feedback--hide');
-                        link.addClass('footer__feedback--hide');
-                    });
+            var link = $('.js-tech-feedback');
+            bean.on(link[0], 'click', function () {
+                beacon.fire('/counts.gif?c=tech-feedback');
+                fastdom.write(function () {
+                    $('.js-feedback-thanks').removeClass('footer__feedback--hide');
+                    link.addClass('footer__feedback--hide');
                 });
-                link.removeClass('footer__feedback--hide');
+            });
+            link.removeClass('footer__feedback--hide');
         }
     };
 });
