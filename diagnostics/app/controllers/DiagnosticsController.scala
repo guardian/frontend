9c16f31c1f460d2f2fda7e9cd30123526fbb02b7
John Duffell <john.duffell@guardian.co.uk>
2015-03-03 15:36:48 +0000

    update wording to be less scary!

--- diagnostics/app/controllers/DiagnosticsController.scala
+++ diagnostics/app/controllers/DiagnosticsController.scala
@@ -38,18 +38,19 @@ object DiagnosticsController extends Controller with Logging {
 
   def escape(values: Seq[Option[(String, String)]]) = {
     val lines = values.flatten.map{ case (name, value) => s"$name: $value"}.mkString("\r\n\r\n")
-    val body = s"\r\n\r\n\r\nAdditional technical data about your request - please do not edit:\r\n\r\n$lines\r\n\r\n"
+    val body = s"\r\n\r\n\r\n\r\n------------------------------\r\nAdditional technical data about your request - please do not edit:\r\n\r\n$lines\r\n\r\n"
     // URLEncoder is wrong, but it's close enough for percent encoding
     "?body=" + URLEncoder.encode(body, "UTF-8")// + "&subject=Technical%20Feedback"
   }
 
   def techFeedback() = Action { implicit request =>
     Analytics.report("tech-feedback")
-    val uri = request.queryString.get("uri").map(uri => ("URL", uri.fold("")(_+" "+_)))
+    val uri = request.queryString.get("uri").map(_.fold("")(_+" "+_))
+    val uriKV = uri.map(uri => ("URL", uri))
     val width = request.queryString.get("width").map(value => ("Browser width", value.fold("")(_+" "+_)))
     val browser = request.headers.get("User-Agent").map(("User-Agent", _))
-    val emailParams = escape(Seq(uri, browser, width))
-    NoCache(Ok(views.html.feedback(model.Page(request.path, "info", "Thanks for your report", "GFE:Tech Feedback"), emailParams)))
+    val emailParams = escape(Seq(uriKV, browser, width))
+    NoCache(Ok(views.html.feedback(model.Page(request.path, "info", "Thanks for your report", "GFE:Tech Feedback"), emailParams, uri)))
   }
 
   // e.g.  .../counts?c=pv&c=vv&c=ve
