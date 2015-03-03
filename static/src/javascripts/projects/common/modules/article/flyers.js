edce9fcddd4373a58e487a914e7bb3bfb32b256d
Oliver Joseph Ash <oliverjash@gmail.com>
2015-03-03 16:10:36 +0000

    Rich links (flyers): don't show tag-targeted rich links on "sensitive content"

--- static/src/javascripts/projects/common/modules/article/flyers.js
+++ static/src/javascripts/projects/common/modules/article/flyers.js
@@ -59,7 +59,8 @@ define([
     }
 
     function insertTagFlyer() {
-        if (config.page.richLink && config.page.richLink.indexOf(config.page.pageId) === -1) {
+        if (config.page.richLink && !config.page.shouldHideAdverts && !config.page.showRelatedContent
+            && config.page.richLink.indexOf(config.page.pageId) === -1) {
             var space = spacefinder.getParaWithSpace(getSpacefinderRules());
             if (space) {
                 $.create(template(richLinkTagTmpl, {href: config.page.richLink}))
