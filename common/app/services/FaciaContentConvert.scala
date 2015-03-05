ebf2c32d37168768473a57baf2348a7a0ff468fa
Francis Carr <francis.carr@guardian.co.uk>
2015-03-05 15:42:44 +0000

    Fix SupportingContent and ItemKicker during conversion of model.Content

--- common/app/services/FaciaContentConvert.scala
+++ common/app/services/FaciaContentConvert.scala
@@ -1,28 +1,32 @@
 package services
 
 import com.gu.facia.api.models.{ImageReplace, CuratedContent, FaciaContent}
+import com.gu.facia.api.utils.{ResolvedMetaData, ItemKicker}
+import com.gu.facia.client.models.TrailMetaData
 
 object FaciaContentConvert {
-  def frontentContentToFaciaContent(frontendContent: model.Content): FaciaContent =
+  def frontentContentToFaciaContent(frontendContent: model.Content): FaciaContent = {
+    val resolvedMetaData = ResolvedMetaData.fromContentAndTrailMetaData(frontendContent.apiContent.delegate, frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty))
     CuratedContent(
       content = frontendContent.apiContent.delegate,
-      supportingContent = Nil,
+      supportingContent = frontendContent.apiContent.supporting.map(FaciaContentConvert.frontentContentToFaciaContent),
       headline = frontendContent.headline,
       href = frontendContent.delegate.safeFields.get("href"),
       trailText = frontendContent.trailText,
       group = frontendContent.group.getOrElse("0"),
       imageReplace = imageFromContent(frontendContent),
-      isBreaking = false,
-      isBoosted = false,
-      imageHide = false,
-      showMainVideo = false,
-      showKickerTag = false,
+      isBreaking = resolvedMetaData.isBreaking,
+      isBoosted = resolvedMetaData.isBoosted,
+      imageHide = resolvedMetaData.imageHide,
+      showMainVideo = resolvedMetaData.showMainVideo,
+      showKickerTag = resolvedMetaData.showKickerTag,
       frontendContent.byline,
-      showByLine = false,
-      kicker = None,
+      showByLine = resolvedMetaData.showByline,
+      kicker = ItemKicker.fromContentAndTrail(frontendContent.apiContent.delegate, frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty), resolvedMetaData, None),
       imageCutout = None,
-      showBoostedHeadline = false,
-      showQuotedHeadline = false)
+      showBoostedHeadline = resolvedMetaData.showBoostedHeadline,
+      showQuotedHeadline = resolvedMetaData.showQuotedHeadline)
+  }
 
   private def imageFromContent(frontendContent: model.Content): Option[ImageReplace] =
     for {
