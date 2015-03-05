1eb787ccc4170585fa7139e10ee8445e99ffe654
Francis Carr <francis.carr@guardian.co.uk>
2015-03-05 17:16:47 +0000

    Change FaciaContentConvert to accept CollectionConfig to pass onto ItemKicker methods

--- common/app/services/FaciaContentConvert.scala
+++ common/app/services/FaciaContentConvert.scala
@@ -1,17 +1,17 @@
 package services
 
-import com.gu.facia.api.models.{ImageCutout, ImageReplace, CuratedContent, FaciaContent}
+import com.gu.facia.api.models._
 import com.gu.facia.api.utils.{ResolvedMetaData, ItemKicker}
 import com.gu.facia.client.models.TrailMetaData
 
 object FaciaContentConvert {
-  def frontentContentToFaciaContent(frontendContent: model.Content): FaciaContent = {
+  def frontentContentToFaciaContent(frontendContent: model.Content, maybeCollectionConfig: Option[CollectionConfig] = None): FaciaContent = {
     val resolvedMetaData = ResolvedMetaData.fromContentAndTrailMetaData(frontendContent.apiContent.delegate, frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty))
     val trailMetaData = frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty)
     val contentApiContent = frontendContent.apiContent.delegate
     CuratedContent(
       content = frontendContent.apiContent.delegate,
-      supportingContent = frontendContent.apiContent.supporting.map(FaciaContentConvert.frontentContentToFaciaContent),
+      supportingContent = frontendContent.apiContent.supporting.map(FaciaContentConvert.frontentContentToFaciaContent(_, maybeCollectionConfig)),
       headline = frontendContent.headline,
       href = frontendContent.delegate.safeFields.get("href"),
       trailText = frontendContent.trailText,
@@ -24,9 +24,11 @@ object FaciaContentConvert {
       showKickerTag = resolvedMetaData.showKickerTag,
       frontendContent.byline,
       showByLine = resolvedMetaData.showByline,
-      kicker = ItemKicker.fromContentAndTrail(contentApiContent, trailMetaData, resolvedMetaData, None),
+      kicker = ItemKicker.fromContentAndTrail(contentApiContent, trailMetaData, resolvedMetaData, maybeCollectionConfig),
       imageCutout = ImageCutout.fromContentAndTrailMeta(contentApiContent, trailMetaData),
       showBoostedHeadline = resolvedMetaData.showBoostedHeadline,
       showQuotedHeadline = resolvedMetaData.showQuotedHeadline)
   }
+
+  def frontentContentToFaciaContent(frontendContent: model.Content): FaciaContent = frontentContentToFaciaContent(frontendContent, None)
 }
