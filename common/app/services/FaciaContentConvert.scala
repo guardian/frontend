66fef3e204081121db65b865a6b00b4194da7ef8
Francis Carr <francis.carr@guardian.co.uk>
2015-03-05 15:46:17 +0000

    Fix Cutouts for FaciaContentConvert

--- common/app/services/FaciaContentConvert.scala
+++ common/app/services/FaciaContentConvert.scala
@@ -1,12 +1,14 @@
 package services
 
-import com.gu.facia.api.models.{ImageReplace, CuratedContent, FaciaContent}
+import com.gu.facia.api.models.{ImageCutout, ImageReplace, CuratedContent, FaciaContent}
 import com.gu.facia.api.utils.{ResolvedMetaData, ItemKicker}
 import com.gu.facia.client.models.TrailMetaData
 
 object FaciaContentConvert {
   def frontentContentToFaciaContent(frontendContent: model.Content): FaciaContent = {
     val resolvedMetaData = ResolvedMetaData.fromContentAndTrailMetaData(frontendContent.apiContent.delegate, frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty))
+    val trailMetaData = frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty)
+    val contentApiContent = frontendContent.apiContent.delegate
     CuratedContent(
       content = frontendContent.apiContent.delegate,
       supportingContent = frontendContent.apiContent.supporting.map(FaciaContentConvert.frontentContentToFaciaContent),
@@ -22,8 +24,8 @@ object FaciaContentConvert {
       showKickerTag = resolvedMetaData.showKickerTag,
       frontendContent.byline,
       showByLine = resolvedMetaData.showByline,
-      kicker = ItemKicker.fromContentAndTrail(frontendContent.apiContent.delegate, frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty), resolvedMetaData, None),
-      imageCutout = None,
+      kicker = ItemKicker.fromContentAndTrail(contentApiContent, trailMetaData, resolvedMetaData, None),
+      imageCutout = ImageCutout.fromContentAndTrailMeta(contentApiContent, trailMetaData),
       showBoostedHeadline = resolvedMetaData.showBoostedHeadline,
       showQuotedHeadline = resolvedMetaData.showQuotedHeadline)
   }
