184aa3ccd724b1889279cddd49e039a1468d9fef
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-03-16 11:37:47 +0000

    Add new comment count icon to svg.js list

--- static/src/javascripts/projects/common/views/svgs.js
+++ static/src/javascripts/projects/common/views/svgs.js
@@ -2,17 +2,20 @@
 // This file is only required by core, and so has a long cache time.
 
 define([
+    'inlineSvg!svgs/comment-white-16!icon',
     'inlineSvg!svgs/marque-36!icon',
     'inlineSvg!svgs/marque-54!icon',
     'inlineSvg!svgs/logo-guardian!logo',
     'inlineSvg!svgs/logo-soulmates!commercial'
 ], function (
+    commentCount16icon,
     marque36icon,
     marque54icon,
     logoguardian,
     logosoulmates
 ) {
     var svgs = {
+        commentCount16icon: commentCount16icon,
         marque36icon: marque36icon,
         marque54icon: marque54icon,
         logoguardian: logoguardian,
