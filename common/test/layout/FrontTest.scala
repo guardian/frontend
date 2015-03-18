c1fd8f18b83622c423440db5acaead6c236eae2e
Francis Carr <francis.carr@guardian.co.uk>
2015-03-18 10:56:26 +0000

    Fix FrontTest; The reason for the slashes is that overriding before ensured that no slashes existed. But now, as we rely on the underlaying implementation for url, we get the url back with a leading slash

--- common/test/layout/FrontTest.scala
+++ common/test/layout/FrontTest.scala
@@ -1,7 +1,8 @@
 package layout
 
-import com.gu.facia.api.models.FaciaContent
+import com.gu.facia.api.models.{LatestSnap, FaciaContent}
 import model.{ApiContentWithMeta, Content}
+import org.joda.time.DateTime
 import org.scalatest.{Matchers, FlatSpec}
 import services.FaciaContentConvert
 import slices._
@@ -11,7 +12,7 @@ import implicits.FaciaContentImplicits._
 
 class FrontTest extends FlatSpec with Matchers {
   def trailWithUrl(theUrl: String): FaciaContent = FaciaContentConvert.frontentContentToFaciaContent(new Content(ApiContentWithMeta(
-      emptyApiContent,
+      emptyApiContent.copy(id = theUrl, webUrl = theUrl),
       Nil
     )) {
       override lazy val url: String = theUrl
@@ -20,17 +21,46 @@ class FrontTest extends FlatSpec with Matchers {
     }
   )
 
-  def dreamSnapWithUrl(theUrl: String) = FaciaContentConvert.frontentContentToFaciaContent(new Content(ApiContentWithMeta(
-      emptyApiContent,
-      Nil
-    )) {
-      override lazy val url: String = theUrl
+  def dreamSnapWithUrl(theUrl: String) = {
+    val content: ApiContent = new ApiContent(
+      id = theUrl,
+      sectionId = None,
+      sectionName = None,
+      webPublicationDateOption = Option(DateTime.now()),
+      webTitle = "",
+      webUrl = theUrl,
+      apiUrl = "",
+      fields = None,
+      tags = Nil,
+      elements = None,
+      references = Nil,
+      isExpired = None)
 
-      override lazy val webUrl: String = theUrl
+    LatestSnap(
+      id = theUrl,
+      snapUri = None,
+      snapCss = None,
+      latestContent = Option(content),
+      headline = None,
+      href = None,
+      trailText = None,
+      group = "",
+      image = None,
+      isBreaking = false,
+      isBoosted = false,
+      imageHide = false,
+      imageReplace = false,
+      showMainVideo = false,
+      showKickerTag = false,
+      byline = None,
+      showByLine = false,
+      kicker = None,
+      imageCutout = None,
+      showBoostedHeadline = false,
+      showQuotedHeadline = false)}
 
-      override lazy val snapType: Option[String] = Some("latest")
-    }
-  )
+
+
 
   "itemsVisible" should "return a correct count of items visible (not behind 'show more')" in {
     Front.itemsVisible(FixedContainers.fixedMediumFastXI) shouldEqual 11
@@ -46,57 +76,57 @@ class FrontTest extends FlatSpec with Matchers {
       trailWithUrl("three")
     ))
 
-    dedupedTrails.map(_.webUrl) shouldEqual Seq("one", "two", "three")
+    dedupedTrails.flatMap(_.webUrl) shouldEqual Seq("one", "two", "three")
   }
 
   it should "include items seen in a dynamic container in the set of urls for further deduplication" in {
-    val (nowSeen, _) = Front.deduplicate(Set("one", "two"), Dynamic(DynamicFast), Seq(
+    val (nowSeen, _) = Front.deduplicate(Set("/one", "/two"), Dynamic(DynamicFast), Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    nowSeen shouldEqual Set("one", "two", "three")
+    nowSeen shouldEqual Set("/one", "/two", "/three")
   }
 
   it should "remove items from a fixed container" in {
-    val (_, dedupedTrails) = Front.deduplicate(Set("one", "two"), Fixed(FixedContainers.fixedMediumFastXI), Seq(
+    val (_, dedupedTrails) = Front.deduplicate(Set("/one", "/two"), Fixed(FixedContainers.fixedMediumFastXI), Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    dedupedTrails.map(_.webUrl) shouldEqual Seq("three")
+    dedupedTrails.flatMap(_.webUrl) shouldEqual Seq("three")
   }
 
   it should "include items seen in a fixed container in the set of urls for further deduplication" in {
-    val (nowSeen, _) = Front.deduplicate(Set("one", "two"), Fixed(FixedContainers.fixedMediumFastXI), Seq(
+    val (nowSeen, _) = Front.deduplicate(Set("/one", "/two"), Fixed(FixedContainers.fixedMediumFastXI), Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    nowSeen shouldEqual Set("one", "two", "three")
+    nowSeen shouldEqual Set("/one", "/two", "/three")
   }
 
   it should "not remove items from a nav list" in {
-    val (_, dedupedTrails) = Front.deduplicate(Set("one", "two"), NavList, Seq(
+    val (_, dedupedTrails) = Front.deduplicate(Set("/one", "/two"), NavList, Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    dedupedTrails.map(_.webUrl) shouldEqual Seq("one", "two", "three")
+    dedupedTrails.flatMap(_.webUrl) shouldEqual Seq("one", "two", "three")
   }
 
   it should "not include items seen in a nav list in the set of urls for further deduplication" in {
-    val (nowSeen, _) = Front.deduplicate(Set("one", "two"), NavList, Seq(
+    val (nowSeen, _) = Front.deduplicate(Set("/one", "/two"), NavList, Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    nowSeen shouldEqual Set("one", "two")
+    nowSeen shouldEqual Set("/one", "/two")
   }
 
   it should "not remove items from a nav media list" in {
@@ -106,7 +136,7 @@ class FrontTest extends FlatSpec with Matchers {
       trailWithUrl("three")
     ))
 
-    dedupedTrails.map(_.webUrl) shouldEqual Seq("one", "two", "three")
+    dedupedTrails.flatMap(_.webUrl) shouldEqual Seq("one", "two", "three")
   }
 
   it should "not include items seen in a singleton container in the set of urls for further deduplication" in {
@@ -122,37 +152,37 @@ class FrontTest extends FlatSpec with Matchers {
       trailWithUrl("one")
     ))
 
-    dedupedTrails.map(_.webUrl) shouldEqual Seq("one")
+    dedupedTrails.flatMap(_.webUrl) shouldEqual Seq("one")
   }
 
   it should "not include items seen in a nav media list in the set of urls for further deduplication" in {
-    val (nowSeen, _) = Front.deduplicate(Set("one", "two"), NavMediaList, Seq(
+    val (nowSeen, _) = Front.deduplicate(Set("/one", "/two"), NavMediaList, Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    nowSeen shouldEqual Set("one", "two")
+    nowSeen shouldEqual Set("/one", "/two")
   }
 
   it should "not remove items from most popular" in {
-    val (_, dedupedTrails) = Front.deduplicate(Set("one", "two"), MostPopular, Seq(
+    val (_, dedupedTrails) = Front.deduplicate(Set("/one", "/two"), MostPopular, Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    dedupedTrails.map(_.webUrl) shouldEqual Seq("one", "two", "three")
+    dedupedTrails.flatMap(_.webUrl) shouldEqual Seq("one", "two", "three")
   }
 
   it should "not include items seen in most popular in the set of urls for further deduplication" in {
-    val (nowSeen, _) = Front.deduplicate(Set("one", "two"), MostPopular, Seq(
+    val (nowSeen, _) = Front.deduplicate(Set("/one", "/two"), MostPopular, Seq(
       trailWithUrl("one"),
       trailWithUrl("two"),
       trailWithUrl("three")
     ))
 
-    nowSeen shouldEqual Set("one", "two")
+    nowSeen shouldEqual Set("/one", "/two")
   }
 
   it should "not deduplicate dream snaps" in {
@@ -160,7 +190,7 @@ class FrontTest extends FlatSpec with Matchers {
       dreamSnapWithUrl("one")
     ))
 
-    dedupedTrails.map(_.webUrl) shouldEqual Seq("one")
+    dedupedTrails.flatMap(_.webUrl) shouldEqual Seq("one")
   }
 
   it should "not skip dream snaps when considering items visible to be added to the set of seen urls" in {
@@ -173,7 +203,7 @@ class FrontTest extends FlatSpec with Matchers {
       trailWithUrl("six")
     ))
 
-    nowSeen shouldEqual Set("three", "four")
+    nowSeen shouldEqual Set("/three", "/four")
   }
 
   it should "not include dream snaps in the seen urls" in {
