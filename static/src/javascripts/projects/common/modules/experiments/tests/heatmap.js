80151be067b318ce2182d68794c10a11b96d93c1
Ken Lim <kennethlimfc@gmail.com>
2015-03-18 12:29:30 +0000

    Style correction

--- static/src/javascripts/projects/common/modules/experiments/tests/heatmap.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/heatmap.js
@@ -17,8 +17,9 @@ define([
         this.idealOutcome = '';
 
         this.canRun = function () {
-            var isUkNetworkFront = (config.page.edition === 'UK') && config.page.pageId === 'uk';
-            var isAshleyJudd = (config.page.pageId === 'film/2015/mar/18/ashley-judd-to-press-charges-twitter-sexual-harassment');
+            var isUkNetworkFront = (config.page.edition === 'UK') && config.page.pageId === 'uk',
+                    isAshleyJudd = (config.page.pageId === 'film/2015/mar/18/ashley-judd-to-press-charges-twitter-sexual-harassment');
+
             return isUkNetworkFront || isAshleyJudd;
         };
 
