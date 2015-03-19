bf2ffce5b3159bcedbe6ff248806c4208f313561
Alex Sanders <a@snd.rs>
2015-03-19 11:22:55 +0000

    use double quotes in the inlineSvg plugin

--- static/src/javascripts/projects/common/utils/inlineSvg.js
+++ static/src/javascripts/projects/common/utils/inlineSvg.js
@@ -1,5 +1,7 @@
+// jshint quotmark: double
+// jscs:disable validateQuoteMarks
 define([
-    'text'
+    "text"
 ], function (
     text
 ) {
@@ -9,18 +11,18 @@ define([
     return {
 
         load: function (name, req, onLoad, config) {
-            var prefix = 'inline-',
-                data = name.split('!'),
+            var prefix = "inline-",
+                data = name.split("!"),
 
                 path = data[0],
-                imageType = data[1] || '',
+                imageType = data[1] || "",
 
-                dirs = path.split('/'),
+                dirs = path.split("/"),
                 fileName = dirs.pop();
 
-            text.get(req.toUrl(dirs.join('/') + '/' + imageType + '/' + fileName + '.svg'), function (svg) {
+            text.get(req.toUrl(dirs.join("/") + "/" + imageType + "/" + fileName + ".svg"), function (svg) {
 
-                svg = '<span class="' + prefix + fileName + ' ' + (imageType !== '' ? prefix + imageType : '') + '">' + svg + '</span>';
+                svg = "<span class='" + prefix + fileName + " " + (imageType !== "" ? prefix + imageType : "") + "'>" + svg + "</span>";
 
                 if (config.isBuild) {
                     buildText[name] = text.jsEscape(svg);
@@ -33,10 +35,10 @@ define([
 
         write: function (pluginName, moduleName, write) {
             if (buildText.hasOwnProperty(moduleName)) {
-                var name = '"' + pluginName + '!' + moduleName  + '"',
-                    text = 'function () {return "' + buildText[moduleName] + '";}';
+                var name = "'" + pluginName + "!" + moduleName  + "'",
+                    text = "function () {return '" + buildText[moduleName] + "';}";
 
-                write('define(' + name + ', ' + text + ');\n');
+                write("define(" + name + ", " + text + ");\n");
             }
         }
     };
