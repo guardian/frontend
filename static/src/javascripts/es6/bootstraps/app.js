f505b7aa0f4ea35adbf70d646f3db1c39902ce80
Oliver Joseph Ash <oliverjash@gmail.com>
2015-03-25 16:30:56 +0000

    Load jspm bundles async and after cutting the mustard

--- static/src/javascripts/es6/bootstraps/app.js
+++ static/src/javascripts/es6/bootstraps/app.js
@@ -1,14 +1,5 @@
 import crosswords from './crosswords';
 import crosswordThumbnails from 'es6/projects/common/modules/crosswords/thumbnails';
 
-var boot = function () {
-    crosswords.init();
-    crosswordThumbnails.init();
-};
-
-// TODO: IE8
-if (document.readyState === 'complete') {
-    boot();
-} else {
-    document.addEventListener('DOMContentLoaded', boot);
-}
+crosswords.init();
+crosswordThumbnails.init();
