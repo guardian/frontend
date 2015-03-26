5c784401bcdc90476fd33a150af33a6af73fb4d3
Ken Lim <kennethlimfc@gmail.com>
2015-03-26 12:17:00 +0000

    Setting up a new Heatmap
    Todo: get the breakpoint in there.

--- static/src/javascripts/projects/common/modules/experiments/tests/heatmap.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/heatmap.js
@@ -5,22 +5,21 @@ define([
 ) {
     return function () {
         this.id = 'Heatmap';
-        this.start = '2015-03-16';
-        this.expiry = '2015-03-24';
+        this.start = '2015-03-24';
+        this.expiry = '2015-03-26';
         this.author = 'Ken Lim';
-        this.description = 'UK network front heatmap';
+        this.description = 'US network front desktop heatmap';
         this.audience = 0.01;
         this.audienceOffset = 0;
         this.successMeasure = '';
-        this.audienceCriteria = '1% looking at the UK front';
+        this.audienceCriteria = '1% looking at the US front';
         this.dataLinkNames = '';
         this.idealOutcome = '';
 
         this.canRun = function () {
-            var isUkNetworkFront = (config.page.edition === 'UK') && config.page.pageId === 'uk',
-                    isAshleyJudd = (config.page.pageId === 'film/2015/mar/18/ashley-judd-to-press-charges-twitter-sexual-harassment');
+            var isUsNetworkFront = (config.page.edition === 'US') && config.page.pageId === 'us';
 
-            return isUkNetworkFront || isAshleyJudd;
+            return isUsNetworkFront
         };
 
         this.variants = [
@@ -36,6 +35,9 @@ define([
                         a.src = document.location.protocol + scriptSrc + offset;
                         a.async = true;
                         a.type = 'text/javascript';
+
+                        window.CE_SNAPSHOT_NAME = "US Network front on Desktop";
+
                         b.parentNode.insertBefore(a, b);
                     }, 1);
                 }
