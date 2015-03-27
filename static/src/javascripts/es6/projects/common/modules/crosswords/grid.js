0c916318233011d21add929eaeca006f6daeb45d
Oliver Joseph Ash <oliverjash@gmail.com>
2015-03-27 11:09:58 +0000

    Fix indentation

--- static/src/javascripts/es6/projects/common/modules/crosswords/grid.js
+++ static/src/javascripts/es6/projects/common/modules/crosswords/grid.js
@@ -8,57 +8,57 @@ import constants from 'es6/projects/common/modules/crosswords/constants';
 import helpers from 'es6/projects/common/modules/crosswords/helpers';
 
 var Cell = React.createClass({
-        onClick: function (event) {
-            event.preventDefault();
-            this.props.handleSelect();
-        },
+    onClick: function (event) {
+        event.preventDefault();
+        this.props.handleSelect();
+    },
 
-        render: function () {
-            var innerNodes = [],
-                top = helpers.gridSize(this.props.y),
-                left = helpers.gridSize(this.props.x);
+    render: function () {
+        var innerNodes = [],
+            top = helpers.gridSize(this.props.y),
+            left = helpers.gridSize(this.props.x);
 
-            if (this.props.number !== undefined) {
-                innerNodes.push(
-                    React.DOM.text({
-                        x: left + 1,
-                        y: top + constants.numberSize + 1,
-                        key: 'number',
-                        className: 'crossword__cell-number'
-                    }, this.props.number)
-                );
-            }
-
-            if (this.props.value !== undefined) {
-                innerNodes.push(React.DOM.text({
-                    x: left + constants.cellSize / 2,
-                    y: top + (constants.cellSize + constants.entrySize) / 2,
-                    key: 'entry',
-                    className: classNames({
-                        'crossword__cell-text': true,
-                        'crossword__cell-text--error': this.props.isError
-                    })
-                }, this.props.value));
-            }
-
-            return React.DOM.g({
-                onClick: this.onClick
-            },
-                React.DOM.rect({
-                    x: left,
-                    y: top,
-                    width: constants.cellSize,
-                    height: constants.cellSize,
-                    className: classNames({
-                        'crossword__cell': true,
-                        'crossword__cell--focussed': this.props.isFocussed,
-                        'crossword__cell--highlighted': this.props.isHighlighted
-                    })
-                }),
-                innerNodes
+        if (this.props.number !== undefined) {
+            innerNodes.push(
+                React.DOM.text({
+                    x: left + 1,
+                    y: top + constants.numberSize + 1,
+                    key: 'number',
+                    className: 'crossword__cell-number'
+                }, this.props.number)
             );
         }
-    });
+
+        if (this.props.value !== undefined) {
+            innerNodes.push(React.DOM.text({
+                x: left + constants.cellSize / 2,
+                y: top + (constants.cellSize + constants.entrySize) / 2,
+                key: 'entry',
+                className: classNames({
+                    'crossword__cell-text': true,
+                    'crossword__cell-text--error': this.props.isError
+                })
+            }, this.props.value));
+        }
+
+        return React.DOM.g({
+            onClick: this.onClick
+        },
+            React.DOM.rect({
+                x: left,
+                y: top,
+                width: constants.cellSize,
+                height: constants.cellSize,
+                className: classNames({
+                    'crossword__cell': true,
+                    'crossword__cell--focussed': this.props.isFocussed,
+                    'crossword__cell--highlighted': this.props.isHighlighted
+                })
+            }),
+            innerNodes
+        );
+    }
+});
 
 export default React.createClass({
     handleSelect: function (x, y) {
