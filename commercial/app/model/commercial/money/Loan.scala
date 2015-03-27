dc294d2e16c6d6d5c754b5d738fc6187481ea611
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-03-27 12:48:44 +0000

    Refactor

--- commercial/app/model/commercial/money/Loan.scala
+++ commercial/app/model/commercial/money/Loan.scala
@@ -14,8 +14,23 @@ case class Loan(name: String,
                 logoUrl: String,
                 detailsUrl: String,
                 applyUrl: String,
-                categoryName: String
-                 )
+                categoryName: String)
+
+object Loan {
+  def apply(xml: Node): Loan = Loan(
+    (xml \ "LoanName").text,
+    (xml \ "LoanComments").text,
+    (xml \ "HeadlineApr").text.toDouble,
+    (xml \ "Apr").text.toDouble,
+    new BigDecimal((xml \ "MinAdvance").text),
+    new BigDecimal((xml \ "MaxAdvance").text),
+    LoanExample(xml),
+    (xml \ "LogoUrl").text,
+    (xml \ "DetailsUrl").text,
+    (xml \ "ApplyUrl").text,
+    (xml \ "CategoryName").text
+  )
+}
 
 
 case class LoanExample(amount: Double,
@@ -25,6 +40,17 @@ case class LoanExample(amount: Double,
                        totalAmountPayable: Double,
                        interestRate: Double)
 
+object LoanExample {
+  def apply(xml: Node): LoanExample = LoanExample(
+    (xml \ "LoanAmount").text.toDouble,
+    (xml \ "LoanDuration").text.toInt,
+    (xml \ "MonthlyPayment").text.toDouble,
+    (xml \ "TotalChargeForCredit").text.toDouble,
+    (xml \ "TotalAmountPayable").text.toDouble,
+    (xml \ "InterestRate").text.toDouble
+  )
+}
+
 
 object LoansFeed extends MoneySupermarketFeed[Loan] {
 
@@ -32,36 +58,7 @@ object LoansFeed extends MoneySupermarketFeed[Loan] {
 
   protected lazy val path = "loans"
 
-  def parse(xml: Elem): Seq[Loan] = {
-
-    def parseLoanExample(product: Node) = {
-      LoanExample(
-        (product \ "LoanAmount").text.toDouble,
-        (product \ "LoanDuration").text.toInt,
-        (product \ "MonthlyPayment").text.toDouble,
-        (product \ "TotalChargeForCredit").text.toDouble,
-        (product \ "TotalAmountPayable").text.toDouble,
-        (product \ "InterestRate").text.toDouble
-      )
-    }
-
-    xml \ "Product" map {
-      product =>
-        Loan(
-          (product \ "LoanName").text,
-          (product \ "LoanComments").text,
-          (product \ "HeadlineApr").text.toDouble,
-          (product \ "Apr").text.toDouble,
-          new BigDecimal((product \ "MinAdvance").text),
-          new BigDecimal((product \ "MaxAdvance").text),
-          parseLoanExample(product),
-          (product \ "LogoUrl").text,
-          (product \ "DetailsUrl").text,
-          (product \ "ApplyUrl").text,
-          (product \ "CategoryName").text
-        )
-    }
-  }
+  def parse(xml: Elem): Seq[Loan] = xml \ "Product" map (Loan(_))
 }
 
 
