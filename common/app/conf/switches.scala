e7f28d7367cd26c3032df5f165c026a6a3533066
alessandro ludovici <alessandro.ludovici@guardian.com>
2015-03-30 10:51:52 +0100

    Add sell by date

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -405,7 +405,7 @@ object Switches {
 
   val FacebookAppLinksSwitch = Switch("Feature", "facebook-applinks",
     "If this switch is on then shared links on the facebook mobile app will be opened in the native app instead of the mobile browser",
-    safeState = Off, sellByDate = never
+    safeState = Off, sellByDate = new LocalDate(2015, 4, 30)
   )
 
   val IdentityFormstackSwitch = Switch("Feature", "id-formstack",
