08f7f49385b29eab7121d0b114043d2a1ad1e2e2
Ken Lim <kennethlimfc@gmail.com>
2015-03-30 16:51:35 +0100

    Add CMP param to Formstack wrappers
    Turns out we also serve Formstack forms through a proxy.

--- common/app/views/support/CmpParamCleaner.scala
+++ common/app/views/support/CmpParamCleaner.scala
@@ -4,12 +4,17 @@ import org.jsoup.nodes.{Document, Element}
 
 import scala.collection.JavaConversions._
 
-object FormStackCleaner extends HtmlCleaner {
+object CmpParamCleaner extends HtmlCleaner {
   override def clean(document: Document): Document = {
     val formStackHost: String = "guardiannewsampampmedia.formstack.com"
+    val profileFormstackWrapper: String = "profile.theguardian.com/form/embed"
 
     document.getElementsByAttributeValueContaining("src", formStackHost).foreach { elem: Element =>
-      elem.addClass("element-formstack")
+      elem.addClass("element-pass-cmp")
+    }
+
+    document.getElementsByAttributeValueContaining("src", profileFormstackWrapper).foreach { elem: Element =>
+      elem.addClass("element-pass-cmp")
     }
 
     document
