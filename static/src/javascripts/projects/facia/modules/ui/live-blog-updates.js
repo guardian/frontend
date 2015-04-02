befb6bc0a701331452701092bfc31f6735faf00a
Stephan Fowler <stephanfowler@gmail.com>
2015-04-02 16:30:27 +0100

    Use endpoint. Use translate3d ona single wrapper.

--- static/src/javascripts/projects/facia/modules/ui/live-blog-updates.js
+++ static/src/javascripts/projects/facia/modules/ui/live-blog-updates.js
@@ -17,146 +17,88 @@ define([
     template,
     latestBlockTemplate
 ) {
-    var maxDisplayedBlocks = 5,
-        selector = '.js-live-blog-latest-blocks',
+    var forgetAfterHours = 24,
+        maxDisplayedBlocks = 5,
+        blockHeightPx = 44,
+        selectorClassname = '.js-live-blog-latest-blocks',
+        listClassname = 'fc-item__latest-blocks__inner',
         articleIdAttribute = 'data-article-id',
-        hiddenBlockClassname = 'fc-item__latest-block--hidden',
         seenBlockClassname = 'fc-item__latest-block--seen',
-        storageKeyPreviousBlocks = 'gu.liveblog.updates',
-        fakeResponse = {latestBlocks: [
-            {
-                blockId: '1',
-                posted: new Date().getTime() - 300000,
-                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-            },
-            {
-                blockId: '2',
-                posted: new Date().getTime() - 600000,
-                body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta'
-            },
-            {
-                blockId: '3',
-                posted: new Date().getTime() - 900000,
-                body: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt'
-            },
-            {
-                blockId: '4',
-                posted: new Date().getTime() - 1200000,
-                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-            },
-            {
-                blockId: '5',
-                posted: new Date().getTime() - 1500000,
-                body: 'Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta'
-            },
-            {
-                blockId: '6',
-                posted: new Date().getTime() - 1800000,
-                body: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt'
-            },
-            {
-                blockId: '7',
-                posted: new Date().getTime() - 2100000,
-                body: 'Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla Duis aute irure dolor in pariatur. Excepteur sint occaecat cupidatat non proident, sunt'
-            }
-        ]};
+        storageKeyPreviousBlocks = 'gu.liveblog.updates';
 
     function createUpdateHtml(block) {
-        var posted = new Date(Number(block.posted));
-
         return template(latestBlockTemplate, {
-            classes: block.classes,
-            dateTimeString: posted.toISOString(),
-            relativeTimeString: relativeDates.makeRelativeDate(block.posted),
+            classes: block.isNew ? '' : seenBlockClassname,
+            relativeTimeString: relativeDates.makeRelativeDate(Date(block.publishedDateTime || null)),
             blockBody: block.body
         });
     }
 
-    function renderBlocks(options) {
-        var opts = _.pick(options, ['targets', 'blocks', 'blocksAlreadyRendered', 'markAsRead', 'fakeUpdate']),
-            omitBlockIds = _.pluck(opts.blocksAlreadyRendered, 'blockId');
+    function renderBlocks(targets, blocks, lastDateTime) {
+        var fakeUpdate = _.isUndefined(lastDateTime);
 
-        _.forEach(opts.targets, function (element) {
-            _.chain(opts.blocks)
-                .slice(0, maxDisplayedBlocks)
-                .filter(function (block) {
-                    return !_.contains(omitBlockIds, block.blockId);
-                })
-                .forEachRight(function (block, index) {
-                    var classes = [];
+        _.forEach(targets, function (element) {
+            var el,
+                numNewBlocks = 0,
+                blocksHtml = _.chain(blocks)
+                    .slice(0, maxDisplayedBlocks)
+                    .sortBy('publishedDateTime')
+                    .reverse()
+                    .map(function (block, index) {
+                        if (block.publishedDateTime > lastDateTime || (fakeUpdate && index === 0)) {
+                            block.isNew = true;
+                            numNewBlocks += 1;
+                        }
+                        return createUpdateHtml(block);
+                    })
+                    .value()
+                    .join('');
 
-                    if (opts.markAsRead || (opts.fakeUpdate && index > 0)) {
-                        classes.push(seenBlockClassname);
-                    }
+            if (blocksHtml) {
+                el = bonzo.create(
+                    '<div ' +
+                        'style="transform: translate3d(0, -' + (numNewBlocks * blockHeightPx) + 'px, 0)" ' +
+                        'class="' + listClassname + '">' +
+                        blocksHtml +
+                    '</div>');
 
-                    if (opts.blocksAlreadyRendered || (opts.fakeUpdate && index === 0)) {
-                        classes.push(hiddenBlockClassname);
-                    }
+                bonzo(element).prepend(el);
 
-                    block.classes = classes.join(' ');
-                    bonzo(element).prepend(createUpdateHtml(block)).attr('data-blockId', block.blockId);
-                })
-                .value();
-
-            if (opts.blocksAlreadyRendered || opts.fakeUpdate) {
-                setTimeout(function () {
-                    $('.' + hiddenBlockClassname, element).removeClass(hiddenBlockClassname);
-                }, 1000);
+                if (numNewBlocks) {
+                    setTimeout(function () {
+                        bonzo(el).removeAttr('style');
+                    }, 1000);
+                }
             }
         });
     }
 
     function sanitizeBlocks(blocks) {
-        return _.reduce(blocks, function (blocks, block) {
-            return (blocks || []).concat({
-                blockId: block.blockId,
-                posted: block.posted,
-                body: block.body
-            });
-        }, null);
+        return _.filter(blocks, function (block) {
+            return block.id && block.body && block.body.length > 10;
+        });
     }
 
     function updateLatestBlocks(elementsById) {
-        var persistedBlocksById = storage.session.get(storageKeyPreviousBlocks) || {},
-            persistableBlocksById = {};
+        var oldBlockDates = storage.session.get(storageKeyPreviousBlocks) || {};
 
         _.forEach(elementsById, function (elements, articleId) {
-            var persistedBlocks = persistedBlocksById[articleId];
-
-            if (persistedBlocks) {
-                renderBlocks({
-                    targets: elements,
-                    blocks: persistedBlocks,
-                    markAsRead: true
-                });
-            }
-
             ajax({
-                url: '/' + articleId + '/live-blog-blocks.json',
+                url: '/' + articleId + '.json?rendered=false',
                 type: 'json',
                 method: 'get',
                 crossOrigin: true,
                 success: function (response) {
-                    var latestBlocks;
+                    var blocks = response && sanitizeBlocks(response.blocks);
 
-                    // TEMP:
-                    response = fakeResponse;
+                    if (blocks.length) {
+                        renderBlocks(elements, blocks, oldBlockDates[articleId]);
 
-                    latestBlocks = response && sanitizeBlocks(response.latestBlocks);
-
-                    if (latestBlocks) {
-                        renderBlocks({
-                            targets: elements,
-                            blocks: latestBlocks,
-                            blocksAlreadyRendered: persistedBlocks,
-                            fakeUpdate: !persistedBlocks
+                        oldBlockDates = _.omit(oldBlockDates, function (lastDate) {
+                            return (new Date() - new Date(lastDate)) / 3600000 > forgetAfterHours; // hours old
                         });
-
-                        // TEMP:
-                        persistableBlocksById[articleId] = latestBlocks.slice(2);
-                        //persistableBlocksById[articleId] = latestBlocks;
-
-                        storage.session.set(storageKeyPreviousBlocks, persistableBlocksById);
+                        oldBlockDates[articleId] = blocks[0].publishedDateTime;
+                        storage.session.set(storageKeyPreviousBlocks, oldBlockDates);
                     }
                 }
             });
@@ -166,7 +108,7 @@ define([
     function start() {
         var elementsById = {};
 
-        $(selector).each(function (element) {
+        $(selectorClassname).each(function (element) {
             if (element.hasAttribute(articleIdAttribute)) {
                 var articleId = element.getAttribute(articleIdAttribute);
 
