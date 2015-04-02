4d6077c8ff35bcb34876f91b7b4c0f45ae891e28
Oliver Joseph Ash <oliverjash@gmail.com>
2015-04-02 15:50:11 +0100

    Revert "Merge remote-tracking branch 'origin/oja-xwords-focussed-click-buttons'"
    This reverts commit 50dad5e8c69072812158a39e104dbd244caa67d5, reversing
    changes made to 8469e2a5dfecd38c4f89e0ff46c8b343c9d4d918.

--- static/src/javascripts/es6/projects/common/modules/crosswords/font.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-    let ss = document.createElement('link');
-    ss.rel = 'stylesheet';
-    ss.href = 'http://fonts.googleapis.com/css?family=Reenie+Beanie';
-    document.head.appendChild(ss);
-}
