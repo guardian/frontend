72a641876e7a36a3836fde2626ae236a235b1033
Robert Berry <robert.berry@theguardian.com>
2015-04-07 17:43:55 +0100

    Add redirect to /international for international edition

--- common/app/common/LinkTo.scala
+++ common/app/common/LinkTo.scala
@@ -26,7 +26,7 @@ trait LinkTo extends Logging {
   def apply(link: String)(implicit request: RequestHeader): String = this(link, Edition(request))
 
   def apply(url: String, edition: Edition)(implicit request: RequestHeader): String = {
-    val processedUrl: String = processUrl(url, edition).url
+    val processedUrl: String = processUrl(url, edition, InternationalEdition.isInternationalEdition(request)).url
     handleQueryStrings(processedUrl)
   }
 
@@ -35,8 +35,9 @@ trait LinkTo extends Logging {
 
   case class ProcessedUrl(url: String, shouldNoFollow: Boolean = false)
 
-  def processUrl(url: String, edition: Edition) = url match {
+  def processUrl(url: String, edition: Edition, isInternational: Boolean) = url match {
     case "http://www.theguardian.com" => ProcessedUrl(homeLink(edition))
+    case "/" if isInternational => ProcessedUrl("/international")
     case "/" => ProcessedUrl(homeLink(edition))
     case protocolRelative if protocolRelative.startsWith("//") => ProcessedUrl(protocolRelative)
     case AbsoluteGuardianUrl(path) =>  ProcessedUrl(urlFor(path, edition))
