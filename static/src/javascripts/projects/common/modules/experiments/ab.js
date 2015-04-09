fd877590d65d010dcde613f367372cf3d0fcb5ee
Stephan Fowler <stephanfowler@gmail.com>
2015-04-09 10:09:46 +0100

    AB test, jshint

--- static/src/javascripts/projects/common/modules/experiments/ab.js
+++ static/src/javascripts/projects/common/modules/experiments/ab.js
@@ -36,7 +36,7 @@ define([
 
     var ab,
         TESTS = [
-            new LiveblogBlocksOnFronts();
+            new LiveblogBlocksOnFronts(),
             new HighCommercialComponent(),
             new IdentitySocialOAuth(),
             new MtMaster(),
