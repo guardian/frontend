6050bdee23e0f1e8778eca9694b4e8be8abc621d
Alex Sanders <a@snd.rs>
2015-04-15 14:43:05 +0100

    jspm build stub

--- grunt-configs/shell.js
+++ grunt-configs/shell.js
@@ -43,7 +43,24 @@ module.exports = function(grunt, options) {
         },
 
         jspmBundleStatic: {
-            command: './node_modules/.bin/jspm bundle-sfx es6/bootstraps/app static/target/bundles/app.js',
+            command:
+                './node_modules/.bin/jspm bundle-sfx es6/bootstraps/app static/target/bundles/app.js && ' +
+                './node_modules/.bin/jspm bundle core static/target/bundles/core.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/app - core static/target/bundles/app.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/commercial - core static/target/bundles/commercial.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/sudoku - core static/target/bundles/sudoku.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/image-content - core static/target/bundles/image-content.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/facia - core static/target/bundles/facia.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/football - core static/target/bundles/football.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/preferences - core static/target/bundles/preferences.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/membership - core static/target/bundles/membership.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/ophan - core static/target/bundles/ophan.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/admin - core static/target/bundles/admin.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/video-player - core static/target/bundles/video-player.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/video-embed - core static/target/bundles/video-embed.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/dev - core static/target/bundles/dev.js && ' +
+                './node_modules/.bin/jspm bundle bootstraps/creatives - core static/target/bundles/creatives.js'
+            ,
             options: {
                 execOptions: {
                     cwd: '.'
