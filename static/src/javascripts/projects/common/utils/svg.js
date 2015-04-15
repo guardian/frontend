359f57deb6e9b3da8ebee3c325fcb51608a79b91
Alex Sanders <a@snd.rs>
2015-04-15 10:38:15 +0100

    lint

--- static/src/javascripts/projects/common/utils/svg.js
+++ static/src/javascripts/projects/common/utils/svg.js
@@ -1,24 +1,24 @@
-var _ = require("lodash");
+/* global exports */
 
-exports.translate = function(load) {
-    var prefix = "inline-";
-    var data = _.rest(load.metadata.pluginArgument.split('/'));
+var _ = require('lodash');
 
-    var fileName = data.pop();
-    var typesClasses = _.map(data, function (imageType) {
-        return prefix + imageType;
-    }).join(' ');
-
-    var svg = '<span class="' + prefix + fileName + ' ' + typesClasses + '">' + load.source + '</span>';
+exports.translate = function (load) {
+    var prefix = 'inline-',
+        data = _.rest(load.metadata.pluginArgument.split('/')),
+        fileName = data.pop(),
+        typesClasses = _.map(data, function (imageType) {
+            return prefix + imageType;
+        }).join(' '),
+        svg = '<span class="' + prefix + fileName + ' ' + typesClasses + '">' + load.source + '</span>';
 
     svg = svg.replace(/(["\\])/g, '\\$1')
-        .replace(/[\f]/g, "\\f")
-        .replace(/[\b]/g, "\\b")
-        .replace(/[\n]/g, "\\n")
-        .replace(/[\t]/g, "\\t")
-        .replace(/[\r]/g, "\\r")
-        .replace(/[\u2028]/g, "\\u2028")
-        .replace(/[\u2029]/g, "\\u2029");
+        .replace(/[\f]/g, '\\f')
+        .replace(/[\b]/g, '\\b')
+        .replace(/[\n]/g, '\\n')
+        .replace(/[\t]/g, '\\t')
+        .replace(/[\r]/g, '\\r')
+        .replace(/[\u2028]/g, '\\u2028')
+        .replace(/[\u2029]/g, '\\u2029');
 
     return 'module.exports = "' + svg + '";';
-}
+};
