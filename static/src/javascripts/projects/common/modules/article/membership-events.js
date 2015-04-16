6b4ce894c434ff303741b4f78480f33b4f615a9c
David Rapson <david.rapson@guardian.co.uk>
2015-04-16 16:23:53 +0100

    Add membership event component, based on rich link

---
+++ static/src/javascripts/projects/common/modules/article/membership-events.js
@@ -0,0 +1,50 @@
+define([
+    'fastdom',
+    'qwery',
+    'Promise',
+    'common/utils/_',
+    'common/utils/$',
+    'common/utils/ajax-promise'
+], function (
+    fastdom,
+    qwery,
+    Promise,
+    _,
+    $,
+    ajax
+) {
+
+    var ELEMENT_INITIAL_CLASS = 'element-membership--not-upgraded',
+        ELEMENT_UPGRADED_CLASS = 'element-membership--upgraded';
+
+    function upgradeEvent(el) {
+        var href = $('a', el).attr('href'),
+            matches = href.match(/https:\/\/membership.theguardian.com/);
+
+        if (matches) {
+            return ajax({
+                url: href + '/card',
+                crossOrigin: true
+            }).then(function (resp) {
+                if (resp.html) {
+                    fastdom.write(function () {
+                        $(el).html(resp.html)
+                            .removeClass(ELEMENT_INITIAL_CLASS)
+                            .addClass(ELEMENT_UPGRADED_CLASS);
+                    });
+                }
+            });
+        } else {
+            return Promise.resolve(null);
+        }
+    }
+
+    function upgradeEvents() {
+        $('.' + ELEMENT_INITIAL_CLASS).each(upgradeEvent);
+    }
+
+    return {
+        upgradeEvent: upgradeEvent,
+        upgradeEvents: upgradeEvents,
+    };
+});
