16034b94166f0f11bcedccd3ad73c8bc58402ced
Sam Morris <mail@sammorrisdesign.co.uk>
2015-04-16 12:25:04 +0100

    Check for daysLeft and decide

--- common/app/views/support/RegisterCountdown.scala
+++ common/app/views/support/RegisterCountdown.scala
@@ -3,8 +3,10 @@ package views.support
 import org.joda.time.{Period, LocalDate}
 
 object RegisterCountdown {
-    val x = LocalDate.now()
-    val lastDay = new LocalDate(2015, 4, 20)
-    val interval = new Period(x, lastDay)
-    val daysLeft = interval.getDays
+    def daysLeft = {
+        val x = LocalDate.now()
+        val lastDay = new LocalDate(2015, 4, 20)
+        val interval = new Period(x, lastDay)
+        interval.getDays
+    }
 }
