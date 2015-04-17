53d30551f7508c111b13d4db0d4c634204efeeef
Francis Carr <francis.carr@guardian.co.uk>
2015-04-17 16:55:02 +0100

    Switch over to new CardStyle and ContentProperties

--- common/app/services/FaciaContentConvert.scala
+++ common/app/services/FaciaContentConvert.scala
@@ -1,33 +1,28 @@
 package services
 
 import com.gu.facia.api.models._
-import com.gu.facia.api.utils.{ResolvedMetaData, ItemKicker}
+import com.gu.facia.api.utils.{ContentProperties, ResolvedMetaData, ItemKicker}
 import com.gu.facia.client.models.TrailMetaData
 
 object FaciaContentConvert {
   def frontentContentToFaciaContent(frontendContent: model.Content, maybeCollectionConfig: Option[CollectionConfig] = None): FaciaContent = {
-    val resolvedMetaData = ResolvedMetaData.fromContentAndTrailMetaData(frontendContent.apiContent.delegate, frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty))
     val trailMetaData = frontendContent.apiContent.metaData.getOrElse(TrailMetaData.empty)
+    val cardStyle = com.gu.facia.api.utils.CardStyle(frontendContent.delegate, trailMetaData)
+    val resolvedMetaData = ResolvedMetaData.fromContentAndTrailMetaData(frontendContent.apiContent.delegate, trailMetaData, cardStyle)
     val contentApiContent = frontendContent.apiContent.delegate
     CuratedContent(
       content = frontendContent.apiContent.delegate,
       supportingContent = frontendContent.apiContent.supporting.map(FaciaContentConvert.frontentContentToFaciaContent(_, maybeCollectionConfig)),
+      cardStyle = com.gu.facia.api.utils.CardStyle(frontendContent.delegate, TrailMetaData.empty),
       headline = frontendContent.headline,
       href = frontendContent.delegate.safeFields.get("href"),
       trailText = frontendContent.trailText,
       group = frontendContent.group.getOrElse("0"),
       imageReplace = ImageReplace.fromTrailMeta(trailMetaData),
-      isBreaking = resolvedMetaData.isBreaking,
-      isBoosted = resolvedMetaData.isBoosted,
-      imageHide = resolvedMetaData.imageHide,
-      showMainVideo = resolvedMetaData.showMainVideo,
-      showKickerTag = resolvedMetaData.showKickerTag,
+      ContentProperties.fromResolvedMetaData(resolvedMetaData),
       frontendContent.byline,
-      showByLine = resolvedMetaData.showByline,
-      kicker = ItemKicker.fromContentAndTrail(contentApiContent, trailMetaData, resolvedMetaData, maybeCollectionConfig),
-      imageCutout = ImageCutout.fromContentAndTrailMeta(contentApiContent, trailMetaData),
-      showBoostedHeadline = resolvedMetaData.showBoostedHeadline,
-      showQuotedHeadline = resolvedMetaData.showQuotedHeadline,
+      kicker = ItemKicker.fromContentAndTrail(Option(contentApiContent), trailMetaData, resolvedMetaData, maybeCollectionConfig),
+      imageCutout = ImageCutout.fromContentAndTrailMeta(contentApiContent, trailMetaData, resolvedMetaData),
       embedType = frontendContent.snapType,
       embedUri = frontendContent.snapUri,
       embedCss = frontendContent.snapCss)
