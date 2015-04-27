e0fa47eaf6066d374b06873a3b104b9ebf027714
Stephan Fowler <stephanfowler@gmail.com>
2015-04-27 15:41:05 +0100

    Liveblog updates on all fronts, except sports which has AB test

--- static/src/javascripts/bootstraps/facia.js
+++ static/src/javascripts/bootstraps/facia.js
@@ -10,11 +10,13 @@ define([
     'common/utils/storage',
     'common/utils/to-array',
     // Modules
+    'common/modules/experiments/ab',
     'common/modules/business/stocks',
     'facia/modules/onwards/geo-most-popular-front',
     'facia/modules/ui/container-toggle',
     'facia/modules/ui/container-show-more',
     'facia/modules/ui/lazy-load-containers',
+    'facia/modules/ui/live-blog-updates',
     'facia/modules/ui/slideshow/controller',
     'facia/modules/ui/snaps',
     'facia/modules/onwards/weather'
@@ -28,11 +30,13 @@ define([
     mediator,
     storage,
     toArray,
+    ab,
     stocks,
     GeoMostPopularFront,
     ContainerToggle,
     containerShowMore,
     lazyLoadContainers,
+    liveblogUpdates,
     slideshow,
     snaps,
     weather
@@ -77,6 +81,17 @@ define([
                 }
             },
 
+            showLiveblogUpdates: function () {
+                var isSport = _.contains(['sport', 'football'], page.section);
+
+                if (config.switches.liveblogFrontUpdates && !isSport ||
+                    config.switches.abLiveblogSportFrontUpdates && isSport) {
+                    mediator.on('page:front:ready', function () {
+                        liveblogUpdates.show();
+                    });
+                }
+            },
+
             startSlideshow: function () {
                 if (!detect.isBreakpoint('mobile')) {
                     mediator.on('page:front:ready', function () {
@@ -96,6 +111,7 @@ define([
                 lazyLoadContainers();
                 stocks();
                 modules.showWeather();
+                modules.showLiveblogUpdates();
                 modules.startSlideshow();
             }
             mediator.emit('page:front:ready');
