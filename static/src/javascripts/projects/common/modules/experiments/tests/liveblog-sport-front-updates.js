db59885cdf9e7dc480bfc9518093b14404750af2
Stephan Fowler <stephanfowler@gmail.com>
2015-04-27 16:17:39 +0100

    Only run AB on sports && football

--- static/src/javascripts/projects/common/modules/experiments/tests/liveblog-sport-front-updates.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/liveblog-sport-front-updates.js
@@ -1,19 +1,26 @@
-define([], function () {
+define([
+    'common/utils/config'
+], function (
+    config
+) {
+
     return function () {
         this.id = 'LiveblogSportFrontUpdates';
         this.start = '2015-04-27';
         this.expiry = '2015-05-27';
         this.author = 'Stephan Fowler';
-        this.description = 'Checking effect of showing the latest liveblog blocks on sport fronts';
+        this.description = 'Checking effect of showing the latest liveblog blocks on sport & fronts';
         this.audience = 0.2;
         this.audienceOffset = 0.5;
         this.successMeasure = '';
         this.audienceCriteria = 'Front visitors';
         this.dataLinkNames = '';
-        this.idealOutcome = 'Higher engagement, measured as increased onward journeys to ANY content on the affected sport front, or increased dewll time on that front';
+        this.idealOutcome = 'Higher engagement, measured as increased onward journeys to content on the affected front, or increased dewll time on that front';
 
         this.canRun = function () {
-            return true;
+            var section = config.page.section;
+
+            return (config.page.contentType === 'Section') && (section === 'sport' || section === 'football');
         };
 
         this.variants = [
