8361d0db8f31b711698c51e644df7f363bda348a
Francis Carr <francis.carr@guardian.co.uk>
2015-05-05 17:34:45 +0100

    Fix SupportedUrl to handle LinkSnaps of different types

--- common/app/model/SupportedUrl.scala
+++ common/app/model/SupportedUrl.scala
@@ -1,7 +1,7 @@
 package model
 
 import com.gu.contentapi.client.model.{ Content => ApiContent, Tag => ApiTag, Section => ApiSection }
-import com.gu.facia.api.models.FaciaContent
+import com.gu.facia.api.models._
 import implicits.FaciaContentImplicits._
 
 // NEVER FORGET - Just calling this SupportedUrl doesn't make it not UrlBuilder, y'know.
@@ -12,5 +12,15 @@ object SupportedUrl {
 
   def apply(s: ApiSection): String = s"/${s.id}"
 
-  def fromFaciaContent(fc: FaciaContent): String = s"/${fc.href.getOrElse(fc.id)}"
+  def fromFaciaContent(fc: FaciaContent): String = fc match {
+    case curatedContent: CuratedContent => s"/${curatedContent.href.getOrElse(fc.id)}"
+    case supportingCuratedContent: SupportingCuratedContent => s"/${supportingCuratedContent.href.getOrElse(fc.id)}"
+    case linkSnap: LinkSnap =>
+      linkSnap.href match {
+        case Some(href) if href.startsWith("/") => s"$href"
+        case Some(href) => href
+        case None => linkSnap.snapUri.getOrElse(linkSnap.id)
+      }
+    case latestSnap: LatestSnap => latestSnap.latestContent.map(_.id).orElse(latestSnap.snapUri).getOrElse(latestSnap.id)
+  }
 }
