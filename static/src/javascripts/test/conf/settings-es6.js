95b55e4ef95ea97dc73b40d89df32e293186955a
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-05-05 15:41:28 +0100

    Add inline svgs file pattern

--- static/src/javascripts/test/conf/settings-es6.js
+++ static/src/javascripts/test/conf/settings-es6.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
             { pattern: 'javascripts/projects/**/*.js', included: false},
             { pattern: 'javascripts/es6/**/*.js', included: false},
             { pattern: 'javascripts/vendor/**/*.js', included: false},
+            { pattern: 'inline-svgs/**/*.svg', included: false },
             { pattern: 'javascripts/**/views/**/*.html', included: false },
             { pattern: 'javascripts/test/fixtures/**/*', included: false },
             { pattern: 'javascripts/test/helpers/**/*.js', included: false }
