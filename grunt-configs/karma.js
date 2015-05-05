ca025bfc76ce1bf3b65ffe300626b83712fe8d8e
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-05-05 17:28:52 +0100

    Remove old config settings

--- grunt-configs/karma.js
+++ grunt-configs/karma.js
@@ -8,15 +8,6 @@ module.exports = function(grunt, options) {
             browserNoActivityTimeout: 15000,
             reportSlowerThan: 1000
         },
-        common: {
-            configFile: options.testConfDir + 'common.js'
-        },
-        facia: {
-            configFile: options.testConfDir + 'facia.js'
-        },
-        membership: {
-            configFile: options.testConfDir + 'membership.js'
-        },
         'facia-tool': {
             configFile: 'facia-tool/test/public/conf/karma.conf.js'
         },
