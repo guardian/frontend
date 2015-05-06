a2d7b6348c347366e87ca6e3ef4edf32a77fa016
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-05-06 12:15:47 +0100

    Added push banner up condition

--- static/src/javascripts/projects/common/modules/navigation/sticky-nav.js
+++ static/src/javascripts/projects/common/modules/navigation/sticky-nav.js
@@ -320,6 +320,125 @@ define([
         });
     }
 
+    // Navigation slowly dissapearing
+    function StickySlow () {
+        this.$els = {};
+    }
+
+    StickySlow.prototype.init = function () {
+        fastdom.read(function () {
+            this.$els.header        = $('#header');
+            this.$els.bannerDesktop = $('.top-banner-ad-container--above-nav');
+            this.$els.main          = $('#maincontent');
+            this.$els.navHeader     = $('.js-navigation-header');
+        }.bind(this));
+
+        mediator.on('window:scroll', _.throttle(function () {
+            this.updatePosition();
+        }.bind(this), 10));
+    }
+
+    StickySlow.prototype.updatePosition = function () {
+        var headerHeight  = this.$els.navHeader.dim().height,
+            bannerHeight,
+            scrollY;
+
+        fastdom.read(function () {
+            scrollY = $(window).scrollTop();
+        });
+
+        /*if (detect.getBreakpoint() === 'mobile') {
+            fastdom.write(function () {
+                if (scrollY > stickyTresholds.mobile) {
+                    $els.bannerMobile.css({
+                        position: 'absolute',
+                        top:      stickyTresholds.mobile + headerHeight
+                    });
+                } else {
+                    $els.bannerMobile.css({
+                        position:  'fixed',
+                        top:       headerHeight,
+                        width:     '100%',
+                        'z-index': '10000'
+                    });
+                    $els.header.css({
+                        position:  'fixed',
+                        top:       0,
+                        width:     '100%',
+                        'z-index': '10001'
+                    });
+                }
+            });
+        } else {
+            fastdom.write(function () {
+                bannerHeight = $els.bannerDesktop.dim().height;
+
+                // Add is collapsed, header is slim
+                if (scrollY > stickyTresholds.desktop.nobanner) {
+                    // Add is not sticky anymore
+                    $els.bannerDesktop.css({
+                        position: 'absolute',
+                        width: '100%',
+                        top: stickyTresholds.desktop.nobanner
+                    });
+
+                    // Sync header movement with banner disapearing
+                    $els.header.css({
+                        top: Math.round(bannerHeight - (scrollY - stickyTresholds.desktop.nobanner))
+                    });
+
+                    // Banner is not visible anymore so stick header to the top of the viewport
+                    if (scrollY > (stickyTresholds.desktop.nobanner + bannerHeight)) {
+                        $els.header.css({
+                            top: 0
+                        });
+                    }
+                // Top ad and header are visible in full height
+                } else {
+                    // Make sure that banner and header are sticky
+                    $els.bannerDesktop.css({
+                        position:  'fixed',
+                        top:       0,
+                        width:     '100%',
+                        'z-index': '10000'
+                    });
+                    $els.header.css({
+                        position:  'fixed',
+                        top:       bannerHeight,
+                        width:     '100%',
+                        'z-index': '10000'
+                    });
+
+                    // Make sure header is slim when needed
+                    (scrollY > stickyTresholds.desktop.slimnav) ? $els.header.addClass('is-slim') : $els.header.removeClass('is-slim');
+                }
+            });
+        }*/
+
+        fastdom.write(function () {
+            // Make sure that we show slim nav when page loaded with anchor
+            this.$els.bannerDesktop.css({
+                position:  'fixed',
+                top:       0,
+                width:     '100%',
+                'z-index': '10000'
+            });
+            this.$els.header.css('margin-top', this.$els.bannerDesktop.dim().height);
+
+            if (scrollY > headerHeight) {
+                // Add is not sticky anymore
+                this.$els.bannerDesktop.css({
+                    position: 'absolute',
+                    width: '100%',
+                    top: headerHeight
+                });
+            }
+        }.bind(this));
+    }
+
+    var stickySlow = new StickySlow();
+    stickySlow.init();
+
     return {
         stickyNavBurger: stickyNavBurger,
         stickyNavAll: stickyNavAll
