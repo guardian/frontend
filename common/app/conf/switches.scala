0f04f3cee774586477bd9533e2c87eefeca56918
Matt Andrews <matthew.andrews@guardian.co.uk>
2015-05-06 16:16:42 +0100

    Increase the length of this test

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -401,7 +401,7 @@ object Switches {
 
   val BecomeAMemberSwitch = Switch("Feature", "become-a-member",
     "If this switch is on the “Become a Member” button will be visible.",
-    safeState = Off, sellByDate = new LocalDate(2015, 5, 15)
+    safeState = Off, sellByDate = new LocalDate(2015, 6, 15)
   )
 
   val Hmtl5MediaCompatibilityCheck = Switch("Feature", "html-5-media-compatibility-check",
