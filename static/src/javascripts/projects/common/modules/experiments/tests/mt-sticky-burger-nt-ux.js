1544e4a5a69370ab31bcd49bcedffa267060cf6b
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-05-07 22:40:49 +0100

    Extending UX test switches for one more week

--- static/src/javascripts/projects/common/modules/experiments/tests/mt-sticky-burger-nt-ux.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/mt-sticky-burger-nt-ux.js
@@ -17,7 +17,7 @@ define([
     return function () {
         this.id = 'MtStBurgerNtUx';
         this.start = '2015-05-05';
-        this.expiry = '2015-05-10';
+        this.expiry = '2015-05-15';
         this.author = 'Zofia Korcz';
         this.description = 'Top navigation and top ad slot are sticky with navigation going to slim mode for UX testing - variant with no threshold';
         this.audience = 0.0;
