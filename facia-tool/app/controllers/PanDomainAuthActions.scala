e695a9a2ec1a48400397d34a7828b4016eee665b
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-05-14 10:49:45 +0100

    Update the authentication error text

--- facia-tool/app/controllers/PanDomainAuthActions.scala
+++ facia-tool/app/controllers/PanDomainAuthActions.scala
@@ -31,10 +31,10 @@ trait PanDomainAuthActions extends AuthActions with Results {
 
   override def invalidUserMessage(claimedAuth: AuthenticatedUser): String = {
     if( (claimedAuth.user.emailDomain == "guardian.co.uk") && !claimedAuth.multiFactor) {
-      s"${claimedAuth.user.email} is not valid for use with Fronts as you need to have two factor authentication enabled." +
-       s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools"
+      s"${claimedAuth.user.email} is not valid for use with the Fronts Tool as you need to have two factor authentication enabled." +
+       s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools."
     } else {
-      s"${claimedAuth.user.email} is not valid for use with Fronts. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in"
+      s"${claimedAuth.user.email} is not valid for use with the Fronts Tool. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in."
     }
   }
 
@@ -44,4 +44,5 @@ trait PanDomainAuthActions extends AuthActions with Results {
   override lazy val awsCredentials =
     for (key <- config.getString("pandomain.aws.key"); secret <- config.getString("pandomain.aws.secret"))
       yield { new BasicAWSCredentials(key, secret) }
+
 }
