2be7f5ff7c9178cfb2ba1ee71f17e0e6f1e96815
Francis Carr <francis.carr@guardian.co.uk>
2015-05-17 09:50:29 +0100

    Add switch around FrontPressCron

--- facia-press/app/frontpress/FrontPressCron.scala
+++ facia-press/app/frontpress/FrontPressCron.scala
@@ -5,7 +5,7 @@ import com.amazonaws.services.sqs.AmazonSQSAsyncClient
 import common.FaciaPressMetrics.{FrontPressCronFailure, FrontPressCronSuccess}
 import common.SQSQueues._
 import common.{SNSNotification, StopWatch, JsonMessageQueue, Edition}
-import conf.Configuration
+import conf.{Switches, Configuration}
 import conf.Switches.FrontPressJobSwitch
 import metrics.AllFrontsPressLatencyMetric
 
@@ -33,7 +33,12 @@ object FrontPressCron extends JsonQueueWorker[SNSNotification] {
     if (FrontPressJobSwitch.isSwitchedOn) {
       log.info(s"Cron pressing path $path")
       val stopWatch = new StopWatch
-      val pressFuture = FrontPress.pressLiveByPathId(path)
+
+      lazy val pressFuture =
+        if (Switches.FapiClientFormat.isSwitchedOn) {
+          LiveFapiFrontPress.pressByPathId(path)
+        } else {
+          FrontPress.pressLiveByPathId(path) }
 
       pressFuture onComplete {
         case Success(_) =>
