8d715c6d35e1812d32eb7bbec87946c8dccb46c1
Oliver Joseph Ash <oliverjash@gmail.com>
2015-05-18 11:08:33 +0100

    Add notificationclick event

--- applications/public/20150514/service-worker.js
+++ applications/public/20150514/service-worker.js
@@ -1,17 +1,61 @@
+/*eslint quotes: [2, "single"], curly: [2, "multi-line"], strict: 0*/
+/*eslint-env browser*/
+/*global self*/
+// TODO: eslint-env service-worker
+// https://github.com/eslint/eslint/issues/2557
+/*global clients*/
+
+// TODO: Lodash
+var findInArray = function (array, fn) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        var value = array[i];
+        if (fn(value)) return value;
+    }
+};
+
+// TODO: Tidy up
+var notificationData;
 
 self.addEventListener('push', function (event) {
-    console.log('Received a push message', event);
-
-    var title = 'Yay a message.';
-    var body = 'We have received a push message.';
-    var icon = '/images/icon-192x192.png';
-    var tag = 'simple-push-demo-notification-tag';
-
+    // TODO: var url
+    var mobileNotificationsWebHost = 'http://localhost:9000';
     event.waitUntil(
-        self.registration.showNotification(title, {
-            body: body,
-            icon: icon,
-            tag: tag
-        })
+        fetch(mobileNotificationsWebHost + '/messages/web/latest')
+            .then(function (x) { return x.json(); })
+            .then(function (data) {
+                // Warning: mutation!
+                notificationData = data;
+
+                var title = notificationData.message;
+                // TODO:
+                var icon = '/images/icon-192x192.png';
+                var tag = 'breaking-news';
+                var body = '';
+
+                return self.registration.showNotification(title, {
+                    body: body,
+                    icon: icon,
+                    tag: tag
+                });
+            })
+    );
+});
+
+self.addEventListener('notificationclick', function (event) {
+    // Android doesn't close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
+
+    var url = notificationData.link;
+
+    // Focus if already open
+    event.waitUntil(
+        clients.matchAll({ type: 'window' })
+            .then(function (clientList) {
+                var matchingClient = findInArray(clientList, function (client) {
+                    return new URL(client.url).pathname === url;
+                });
+                return matchingClient ? matchingClient.focus() : clients.openWindow(url);
+            })
     );
 });
