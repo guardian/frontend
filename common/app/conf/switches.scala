b0191cf6e55db28bfdc58b1f9759531c1459f1d6
Francis Carr <francis.carr@guardian.co.uk>
2015-05-18 12:15:58 +0100

    Change switches to represent both formats; new and old

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -655,10 +655,16 @@ object Switches {
     safeState = Off, sellByDate = new LocalDate(2015, 8, 31)
   )
 
-  val FapiClientFormat = Switch("Facia", "facia-tool-fapi-client-format",
-    "If this switch is on, facia-press will press in the new fapi-client format",
+  val FaciaPressNewFormat = Switch("Facia", "facia-press-fapi-client-format",
+    "If this switch is on, facia-press will press in the new fapi-client JSON format",
     safeState = Off, sellByDate = new LocalDate(2015, 8, 31)
   )
+
+  val FaciaPressOldFormat = Switch("Facia", "facia-tool-old-press-format",
+    "If this switch is on, facia-press will press in the old JSON format",
+    safeState = On, sellByDate = new LocalDate(2015, 8, 31)
+  )
+
   val FaciaPressOnDemand = Switch("Facia", "facia-press-on-demand",
     "If this is switched on, you can force facia to press on demand (Leave off)",
     safeState = Off, sellByDate = new LocalDate(2015, 6, 30)
