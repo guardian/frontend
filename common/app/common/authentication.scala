1508d42c51c1b403af404bd6791b6565aa058883
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-05-18 11:02:57 +0100

    Remove changes to auth

--- common/app/common/authentication.scala
+++ common/app/common/authentication.scala
@@ -2,14 +2,14 @@ package controllers
 
 import common.Logging
 import play.api.mvc._
-import play.api.Logger
+
 
 trait AuthLogging {
   self: Logging =>
   def log[U](msg: String, request: Request[AnyContent]) {
     request match {
       case auth: Security.AuthenticatedRequest[AnyContent, U] => log.info(auth.user + ": " + msg)
-      case x => throw new IllegalStateException("Expected an authenticated request")
+      case _ => throw new IllegalStateException("Expected an authenticated request")
     }
   }
 }
