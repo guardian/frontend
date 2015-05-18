36596e8c4db27bd37ae893b5853906d0677fa18c
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-05-18 15:41:48 +0100

    Tweak manifest

--- applications/app/controllers/PreferencesController.scala
+++ applications/app/controllers/PreferencesController.scala
@@ -1,36 +1,40 @@
 package controllers
 
-import common.{Logging, ExecutionContexts}
+import common.ExecutionContexts
 import conf.Static
-import model._
+import model.{Cached, PreferencesMetaData}
 import play.api.mvc.{Action, Controller}
 import play.api.libs.json.Json
 
-object PreferencesController extends Controller with ExecutionContexts with Logging {
-  def userPrefs() = Action { implicit request =>
-    Ok(views.html.preferencesPage(new PreferencesMetaData()))
-  }
+object PreferencesController extends Controller with ExecutionContexts {
 
+  case class Icon(`type`: String, sizes: String, src: String)
   case class Manifest(name: String,
                       icons: List[Icon],
                       display: String,
                       gcm_sender_id: String,
                       gcm_user_visible_only: Boolean)
 
-  case class Icon(`type`: String, sizes: String, src: String)
+  private implicit val iconWrites = Json.writes[Icon]
+  private implicit val manifestWrites = Json.writes[Manifest]
 
-  implicit val iconWrites = Json.writes[Icon]
-
-  val manifestWrites = Json.writes[Manifest]
-
-  def manifest() = Action { Cached(3600) {
-    val icon = Icon(`type`="image/png", sizes="152x152", src=Static("images/favicons/152x152.png").toString)
-
-    Ok(manifestWrites.writes(Manifest(
+  private val icon = List(Icon(`type`="image/png", sizes="152x152", src=Static("images/favicons/152x152.png").toString))
+  private val manifestData = Json.toJson(Manifest(
       name="The Guardian",
-      icons=List(icon),
+      icons=icon,
       display="browser",
       gcm_sender_id="162380663509",
-      gcm_user_visible_only=true)).toString())
-  } }
+      gcm_user_visible_only=true))
+
+  def userPrefs() = Action { implicit request =>
+    Cached(300) {
+      Ok(views.html.preferencesPage(new PreferencesMetaData()))
+    }
+  }
+
+  def manifest() = Action {
+    Cached(3600) {
+      Ok(manifestData)
+    }
+  }
 }
