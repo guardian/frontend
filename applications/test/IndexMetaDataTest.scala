a4140a167b325b4b76fe27a08e73892fbd3bea36
John Duffell <john.duffell@guardian.co.uk>
2015-05-21 15:41:25 +0100

    add test for tag page metadata json-ld

--- applications/test/IndexMetaDataTest.scala
+++ applications/test/IndexMetaDataTest.scala
@@ -1,16 +1,16 @@
 package test
 
 import org.jsoup.Jsoup
-import play.api.libs.json._
 import org.scalatest.{DoNotDiscover, FlatSpec, Matchers}
+import play.api.libs.json._
 import play.api.test.Helpers._
 
-@DoNotDiscover class ArticleMetaDataTest extends FlatSpec with Matchers with ConfiguredTestSuite {
+@DoNotDiscover class IndexMetaDataTest extends FlatSpec with Matchers with ConfiguredTestSuite {
 
-  val articleUrl = "environment/2012/feb/22/capitalise-low-carbon-future"
+  val articleUrl = "money/pensions"
 
   it should "Include organisation metadata" in {
-    val result = controllers.ArticleController.renderArticle(articleUrl, None, None)(TestRequest(articleUrl))
+    val result = controllers.IndexController.render(articleUrl)(TestRequest(articleUrl))
     val body = Jsoup.parseBodyFragment(contentAsString(result))
     status(result) should be(200)
 
@@ -27,7 +27,7 @@ import play.api.test.Helpers._
   }
 
   it should "Include webpage metadata" in {
-    val result = controllers.ArticleController.renderArticle(articleUrl, None, None)(TestRequest(articleUrl))
+    val result = controllers.IndexController.render(articleUrl)(TestRequest(articleUrl))
     val body = Jsoup.parseBodyFragment(contentAsString(result))
     status(result) should be(200)
 
@@ -37,6 +37,20 @@ import play.api.test.Helpers._
     val appIndexer: JsValue = Json.parse(script.first().html())
 
     (appIndexer \ "potentialAction" \ "target").as[String] should include(articleUrl)
+
+  }
+
+  it should "Include item list metadata" in {
+    val result = controllers.IndexController.render(articleUrl)(TestRequest(articleUrl))
+    val body = Jsoup.parseBodyFragment(contentAsString(result))
+    status(result) should be(200)
+
+    val script = body.getElementsByAttributeValue("data-schema", "ItemList")
+    script.size() should be(1)
+
+    val itemList: JsValue = Json.parse(script.first().html())
+
+    (itemList \ "itemListElement").as[JsArray].value.size should be(20)
 
   }
 
