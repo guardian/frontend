ba476ec6304bb4602386d3923518cea2c7144eca
Stephan Fowler <stephanfowler@gmail.com>
2015-05-21 10:16:58 +0100

    Remove liveblog test for sport fronts

--- static/src/javascripts/projects/common/modules/experiments/ab.js
+++ static/src/javascripts/projects/common/modules/experiments/ab.js
@@ -7,7 +7,6 @@ define([
     'common/utils/storage',
     'common/modules/analytics/mvt-cookie',
     'common/modules/experiments/tests/sticky-shares',
-    'common/modules/experiments/tests/liveblog-sport-front-updates',
     'common/modules/experiments/tests/high-commercial-component',
     'common/modules/experiments/tests/mt-rec1',
     'common/modules/experiments/tests/mt-rec2',
@@ -25,7 +24,6 @@ define([
     store,
     mvtCookie,
     StickyShares,
-    LiveblogSportFrontUpdates,
     HighCommercialComponent,
     MtRec1,
     MtRec2,
@@ -39,7 +37,6 @@ define([
     var ab,
         TESTS = _.flatten([
             new StickyShares(),
-            new LiveblogSportFrontUpdates(),
             new HighCommercialComponent(),
             new MtRec1(),
             new MtRec2(),
