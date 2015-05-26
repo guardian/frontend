72d51880d39a05f8913ecab23b48fafb4654bc27
John Duffell <john.duffell@guardian.co.uk>
2015-05-26 17:00:49 +0100

    let facia stuff from s3 get stored in the data directory

--- facia/test/FaciaMetaDataTest.scala
+++ facia/test/FaciaMetaDataTest.scala
@@ -1,14 +1,63 @@
 package metadata
 
+import java.io.File
+
 import com.gu.facia.client.models.{FrontJson, ConfigJson}
+import controllers.front.FrontJsonFapi
+import controllers.{front, FaciaController}
+import model.FaciaPage
 import org.jsoup.Jsoup
 import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, FlatSpec, Matchers}
 import play.api.libs.json._
 import play.api.test.Helpers._
+import recorder.HttpRecorder
 import services.ConfigAgent
 import test.{TestRequest, ConfiguredTestSuite}
 
+import scala.concurrent.Future
+
 @DoNotDiscover class FaciaMetaDataTest extends FlatSpec with Matchers with ConfiguredTestSuite with BeforeAndAfterAll {
+
+  // need a facia controller that stores S3 locally so it can run without deps in the unit tests
+  val faciaController = new FaciaController {
+
+    override val frontJsonFapi: FrontJsonFapi = new front.FrontJsonFapi {
+      override val bucketLocation: String = front.FrontJsonFapiLive.bucketLocation
+
+      override def getRaw(path: String): Future[Option[String]] =
+        recorder.load(path, Map()) {
+          super.getRaw(path)
+        }
+
+      val recorder = new HttpRecorder[Option[String]] {
+        override lazy val baseDir = new File(System.getProperty("user.dir"), "data/pressedPage")
+
+        //No transformation for now as we only store content that's there.
+        override def toResponse(str: String): Option[String] = Some(str)
+
+        override def fromResponse(response: Option[String]): String = response.get // we don't need to test None yet
+      }
+    }
+
+    override val frontJson: front.FrontJson = new front.FrontJson {
+      override val bucketLocation: String = front.FrontJsonLive.bucketLocation
+
+      override def getRaw(path: String): Future[Option[String]] =
+      recorder.load(path, Map()) {
+        super.getRaw(path)
+      }
+
+      val recorder = new HttpRecorder[Option[String]] {
+        override lazy val baseDir = new File(System.getProperty("user.dir"), "data/faciaPage")
+
+        //No transformation for now as we only store content that's there.
+        override def toResponse(str: String): Option[String] = Some(str)
+
+        override def fromResponse(response: Option[String]): String = response.get // we don't need to test None yet
+      }
+    }
+
+  }
 
   override def beforeAll() {
     ConfigAgent.refreshWith(
@@ -21,17 +70,17 @@ import test.{TestRequest, ConfiguredTestSuite}
   val articleUrl = "music"
 
   it should "Include organisation metadata" in {
-    val result = controllers.FaciaController.renderFront(articleUrl)(TestRequest())
+    val result = faciaController.renderFront(articleUrl)(TestRequest())
     MetaDataMatcher.ensureOrganisation(result)
   }
 
   it should "Include webpage metadata" in {
-    val result = controllers.FaciaController.renderFront(articleUrl)(TestRequest(articleUrl))
+    val result = faciaController.renderFront(articleUrl)(TestRequest(articleUrl))
     MetaDataMatcher.ensureWebPage(result, articleUrl)
   }
 
   it should "Include item list metadata" in {
-    val result = controllers.FaciaController.renderFront(articleUrl)(TestRequest(articleUrl))
+    val result = faciaController.renderFront(articleUrl)(TestRequest(articleUrl))
     val body = Jsoup.parseBodyFragment(contentAsString(result))
     status(result) should be(200)
 
