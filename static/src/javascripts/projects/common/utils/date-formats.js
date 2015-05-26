5057c870ad4cfbb04ff1f9c0ae68168e48e5328f
Robert Berry <robert.berry@theguardian.com>
2015-05-26 16:01:50 +0100

    Add useful comment

--- static/src/javascripts/projects/common/utils/date-formats.js
+++ static/src/javascripts/projects/common/utils/date-formats.js
@@ -1,4 +1,7 @@
 define([], function () {
+    /**
+     * e.g., 5th February, 1987 (birth date of the great Robert James Berry), would be represented as 1987/02/05.
+     */
     function utcDateString(date) {
         return new Date(date).toISOString().substr(0, 10).replace(/-/g, '/');
     }
