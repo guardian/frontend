2cca3c1b035c0611c10666b0138e754911bed0d4
John Duffell <john.duffell@guardian.co.uk>
2015-05-26 10:41:23 +0100

    improve formatting for case classes

--- common/app/model/meta/LinkedData.scala
+++ common/app/model/meta/LinkedData.scala
@@ -4,8 +4,8 @@ import conf.Static
 import org.joda.time.DateTime
 
 class LinkedData(
-                       val `@type`: String,
-                       val `@context`: String = "http://schema.org")
+  val `@type`: String,
+  val `@context`: String = "http://schema.org")
 
 object LinkedData {
 
@@ -18,36 +18,36 @@ object LinkedData {
 }
 
 case class Guardian(
-                     name: String = "The Guardian",
-                     url: String = "http://www.theguardian.com/",
-                     logo: String = Static("images/favicons/152x152.png").path,
-                     sameAs: List[String] = List(
-                       "https://www.facebook.com/theguardian",
-                       "https://twitter.com/guardian",
-                       "https://plus.google.com/+theGuardian",
-                       "https://www.youtube.com/user/TheGuardian"
-                     )
-                     ) extends LinkedData("Organization")
+  name: String = "The Guardian",
+  url: String = "http://www.theguardian.com/",
+  logo: String = Static("images/favicons/152x152.png").path,
+  sameAs: List[String] = List(
+    "https://www.facebook.com/theguardian",
+    "https://twitter.com/guardian",
+    "https://plus.google.com/+theGuardian",
+    "https://www.youtube.com/user/TheGuardian"
+  )
+) extends LinkedData("Organization")
 
 // https://developers.google.com/app-indexing/webmasters/server#schemaorg-markup-for-viewaction
 case class WebPage(
-                    `@id`: String,
-                    potentialAction: PotentialAction
-                    ) extends LinkedData("WebPage")
+  `@id`: String,
+  potentialAction: PotentialAction
+) extends LinkedData("WebPage")
 
 case class PotentialAction(
-                            `@type`: String = "ViewAction",
-                            target: String
-                            )
+  `@type`: String = "ViewAction",
+  target: String
+)
 
 case class ItemList(
-                    url: String,
-                    itemListElement: Seq[ListItem]
-                    ) extends LinkedData("ItemList")
+  url: String,
+  itemListElement: Seq[ListItem]
+) extends LinkedData("ItemList")
 
 case class ListItem(
-                     `@type`: String = "ListItem",
-                     position: Int,
-                     url: Option[String] = None,// either/or url and item, but needs some care serialising
-                     item: Option[ItemList] = None
-                     )
+  `@type`: String = "ListItem",
+  position: Int,
+  url: Option[String] = None,// either/or url and item, but needs some care serialising
+  item: Option[ItemList] = None
+)
