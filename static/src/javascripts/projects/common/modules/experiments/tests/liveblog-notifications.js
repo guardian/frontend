bc207da6297b6708d0c5c77c0b711997c06b7c5c
Alex Sanders <a@snd.rs>
2015-05-28 12:16:17 +0100

    style notifications message

--- static/src/javascripts/projects/common/modules/experiments/tests/liveblog-notifications.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/liveblog-notifications.js
@@ -5,7 +5,8 @@ define([
     'fastdom',
     'text!common/views/ui/notifications-subcribe-link.html',
     'common/utils/cookies',
-    'bean'
+    'bean',
+    'common/views/svgs'
 ], function (
     config,
     $,
@@ -13,7 +14,8 @@ define([
     fastdom,
     subscribeTemplate,
     cookies,
-    bean
+    bean,
+    svgs
 ) {
 
     return function () {
@@ -44,7 +46,10 @@ define([
                     fastdom.write(function () {
                         $('.js-liveblog-body').prepend(template(subscribeTemplate, {
                             url: window.location,
-                            text: cookies.get(window.location + '-notification') ? 'Following story' : 'Follow story'
+                            text: cookies.get(window.location + '-notification') ? 'Following story' : 'Follow story',
+                            imgMobile: svgs('notificationsExplainerMobile', ['mobile-only', 'notification-explainer']),
+                            imgDesktop: svgs('notificationsExplainerDesktop', ['hide-on-mobile', 'notification-explainer']),
+                            arrow: svgs('arrowWhiteRight')
                         }));
                     });
                     bean.on(document.body, 'click', '.js-notifications-subscribe-link', function () {
