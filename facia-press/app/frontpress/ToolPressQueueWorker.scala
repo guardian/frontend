6c5724b7570b41f0c618dcaeb22f755b6e5c3039
Francis Carr <francis.carr@guardian.co.uk>
2015-05-29 14:19:15 +0100

    Change ToolPressQueueWorker to press old facia format off new switch

--- facia-press/app/frontpress/ToolPressQueueWorker.scala
+++ facia-press/app/frontpress/ToolPressQueueWorker.scala
@@ -6,6 +6,7 @@ import common._
 import conf.{Switches, Configuration}
 import metrics._
 import org.joda.time.DateTime
+import play.api.libs.json.JsNull
 import services.{Draft, FrontPath, Live, PressJob}
 
 import scala.concurrent.Future
@@ -35,19 +36,19 @@ object ToolPressQueueWorker extends JsonQueueWorker[PressJob] with Logging {
 
     log.info(s"Processing job from tool to update $path on $pressType")
 
-    lazy val fapiFormat =
-      if (Switches.FaciaPressNewFormat.isSwitchedOn) {
-        pressType match {
-          case Draft => DraftFapiFrontPress.pressByPathId(path)
-          case Live => LiveFapiFrontPress.pressByPathId(path)}}
-      else { Future.successful(Unit) }
+    lazy val fapiFormat = pressType match {
+      case Draft => DraftFapiFrontPress.pressByPathId(path)
+      case Live => LiveFapiFrontPress.pressByPathId(path)}
 
     lazy val oldFormat =
+      if (Switches.FaciaPressOldFormat.isSwitchedOn) {
         pressType match {
           case Draft => FrontPress.pressDraftByPathId(path)
-          case Live => FrontPress.pressLiveByPathId(path)}
+          case Live => FrontPress.pressLiveByPathId(path)}}
+      else { Future.successful(JsNull) }
 
-    val pressFuture = oldFormat.flatMap(_ => fapiFormat)
+
+    val pressFuture = fapiFormat.flatMap(_ => oldFormat)
 
     pressFuture onComplete {
       case Success(_) =>
