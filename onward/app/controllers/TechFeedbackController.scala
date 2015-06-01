b191c285271e74c0be8b62f0aa0f9f2720efeec0
John Duffell <john.duffell@guardian.co.uk>
2015-06-01 18:19:32 +0100

    move tech-feedback system to theguardian.com/info

--- onward/app/controllers/TechFeedbackController.scala
+++ onward/app/controllers/TechFeedbackController.scala
@@ -13,17 +13,10 @@ object TechFeedbackController extends Controller with Logging {
     val lines = values.flatten.map { case (name, value) => s"$name: $value" }.mkString("\r\n\r\n")
     val body = s"\r\n\r\n\r\n\r\n------------------------------\r\nAdditional technical data about your request - please do not edit:\r\n\r\n$lines\r\n\r\n"
     // URLEncoder is wrong, but it's close enough for percent encoding
-    "?body=" + URLEncoder.encode(body, "UTF-8") // + "&subject=Technical%20Feedback"
+    "?body=" + URLEncoder.encode(body, "UTF-8")
   }
 
-  def techFeedback(stage: String) = Action { implicit request =>
-    //     if (request.method == "POST")
-    //       Analytics.report("tech-feedback")
-//    val uri = request.queryString.get("uri").map(_.fold("")(_ + " " + _))
-//    val uriKV = uri.map(uri => ("URL", uri))
-//    val width = request.queryString.get("width").map(value => ("Browser width", value.fold("")(_ + " " + _)))
-//    val browser = request.headers.get("User-Agent").map(("User-Agent", _))
-//    val emailParams = escape(Seq(uriKV, browser, width))
+  def techFeedback() = Action { implicit request =>
     Cached(900)(Ok(views.html.feedback(model.Page(request.path, "info", "Thanks for your report", "GFE:Tech Feedback"))))
   }
 
