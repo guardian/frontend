ba9795f34595ef5cd98bbfdb822f890858699705
David Rapson <david.rapson@guardian.co.uk>
2015-06-04 10:38:00 +0100

    Update copy and audience % for supporter test

--- static/src/javascripts/projects/common/modules/experiments/tests/supporter-message.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/supporter-message.js
@@ -17,11 +17,11 @@ define([
     return function () {
 
         this.id = 'Supporter';
-        this.start = '2015-06-02';
-        this.expiry = '2015-06-21';
+        this.start = '2015-06-03';
+        this.expiry = '2015-06-08';
         this.author = 'David Rapson';
         this.description = 'Test if logged in users are encouraged to become a Supporter';
-        this.audience = 1.0;
+        this.audience = 0.2;
         this.audienceOffset = 0;
         this.successMeasure = 'Users will be interested in Supporter tier';
         this.audienceCriteria = 'All users';
@@ -29,7 +29,7 @@ define([
         this.idealOutcome = 'Users will sign up as a Supporter';
 
         this.canRun = function () {
-            return config.page.contentType == 'Article' && detect.getBreakpoint() !== 'mobile' && idApi.isUserLoggedIn();
+            return config.page.contentType === 'Article' && detect.getBreakpoint() !== 'mobile' && idApi.isUserLoggedIn();
         };
 
         this.variants = [{
@@ -41,8 +41,8 @@ define([
                     siteMessageCloseBtn: 'hide'
                 }).show(template(supporterMessageTmpl, {
                     supporterLink: 'https://membership.theguardian.com/about/supporter',
-                    messageText: 'Not already a supporter? Why not become one today',
-                    linkText: 'Become a supporter today'
+                    messageText: 'Not already a member? Start supporting us today',
+                    linkText: 'Become a supporter'
                 }));
             }
         },
@@ -55,8 +55,8 @@ define([
                     siteMessageCloseBtn: 'hide'
                 }).show(template(supporterMessageTmpl, {
                     supporterLink: 'https://membership.theguardian.com/about/supporter',
-                    messageText: 'Interested in becoming a supporter?',
-                    linkText: 'Become a supporter today'
+                    messageText: 'Become a Guardian Member and support fearless investigative journalism',
+                    linkText: 'Become a supporter'
                 }));
             }
         }];
