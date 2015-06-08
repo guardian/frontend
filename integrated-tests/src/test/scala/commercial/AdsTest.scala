845d6156d18565e1556c683ad98ec29f7b94b0d7
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-06-08 10:00:59 +0100

    Make Ad Tests More Stable

--- integrated-tests/src/test/scala/commercial/AdsTest.scala
+++ integrated-tests/src/test/scala/commercial/AdsTest.scala
@@ -12,6 +12,7 @@ class AdsTest extends FlatSpec with Matchers with SharedWebDriver {
 
   override protected def get(path: String): Unit = {
     webDriver.get(s"${Config.baseUrl}/$path?test=test#gu.prefs.switchOn=adverts")
+    webDriver.navigate().refresh()
   }
 
   private def waitForElement(selector: String): Unit = {
@@ -20,17 +21,14 @@ class AdsTest extends FlatSpec with Matchers with SharedWebDriver {
 
   private def waitForAdLoad(domSlotId: String): Unit = waitForElement(s"#$domSlotId > div > iframe")
 
-  private def findComponent(path: String,
-                            domSlotId: String,
-                            selector: String): Option[WebElement] = {
+  private def findComponent(path: String, selector: String): Option[WebElement] = {
     get(path)
-    waitForAdLoad(domSlotId)
     waitForElement(selector)
     $(selector).headOption
   }
 
   private def findLogo(path: String, domSlotId: String): Option[WebElement] = {
-    findComponent(path, domSlotId, s"#$domSlotId > div > a > img")
+    findComponent(path, s"#$domSlotId > div > a > img")
   }
 
   private def shouldBeVisible(maybeComponent: => Option[WebElement]): Unit = {
