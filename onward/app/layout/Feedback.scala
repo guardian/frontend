e537dafdf39f4fadada65cf33ed7d393e2b07ad0
John Duffell <john.duffell@guardian.co.uk>
2015-06-09 17:12:14 +0100

    oh dear, more slashes were stripped

--- onward/app/layout/Feedback.scala
+++ onward/app/layout/Feedback.scala
@@ -11,14 +11,14 @@ object Feedback {
     case "" => Feedback(
       "We're sorry that you're having trouble with our web site.",
       false,
-      Some(Seq("crashing/" -> "My browser is crashing or reloading",
-        "discussion/" -> "I'm having trouble either reading or making comments",
-        "performance/" -> "The site is too slow on my device",
-        "other/" -> "I have a technical issue but it doesn't fit into any of the above categories")))
-    case "crashing/" => Feedback(s"$thanks you've experienced an issue with your browser crashing or reloading. $monitor", true)
-    case "discussion/" => Feedback(s"$thanks you're having trouble reading or making comments. $monitor", true)
-    case "performance/" => Feedback(s"$thanks the site is too slow on your device. $monitor", true)
-    case "other/" => Feedback(s"$thanks you're having an issue with our web site.", true)
+      Some(Seq("crashing" -> "My browser is crashing or reloading",
+        "discussion" -> "I'm having trouble either reading or making comments",
+        "performance" -> "The site is too slow on my device",
+        "other" -> "I have a technical issue but it doesn't fit into any of the above categories")))
+    case "crashing" => Feedback(s"$thanks you've experienced an issue with your browser crashing or reloading. $monitor", true)
+    case "discussion" => Feedback(s"$thanks you're having trouble reading or making comments. $monitor", true)
+    case "performance" => Feedback(s"$thanks the site is too slow on your device. $monitor", true)
+    case "other" => Feedback(s"$thanks you're having an issue with our web site.", true)
     case _ => Feedback("We're sorry that you're having trouble with our web site.", false)
   }
 
