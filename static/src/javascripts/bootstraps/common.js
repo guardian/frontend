31b4c09a477ea61480a1144b5ecd212a81a0b9f3
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-11 17:53:42 +0100

    Lint

--- static/src/javascripts/bootstraps/common.js
+++ static/src/javascripts/bootstraps/common.js
@@ -134,7 +134,7 @@ define([
                 // Unfortunately FastClick’s UMD exports are not consistent for
                 // all types. AMD exports FastClick, CJS exports FastClick.attach
                 // As per: https://github.com/ftlabs/fastclick/blob/master/lib/fastclick.js#L829-L840
-                var isRequireJs = !! FastClick.attach;
+                var isRequireJs = !!FastClick.attach;
                 (isRequireJs ? FastClick.attach : FastClick)(document.body);
             },
 
