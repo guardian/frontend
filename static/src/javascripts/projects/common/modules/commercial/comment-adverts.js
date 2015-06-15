ab8ad4b85267ac183ccdcc19d1040d64293e82b1
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-06-15 17:01:19 +0100

    Removing rec1 and rec2 from comments

--- static/src/javascripts/projects/common/modules/commercial/comment-adverts.js
+++ static/src/javascripts/projects/common/modules/commercial/comment-adverts.js
@@ -29,13 +29,9 @@ define([
             $adSlotContainer,
             $commentMainColumn,
             isViewabilityTest = function () {
-                var MtRec1Test = ab.getParticipations().MtRec1,
-                    MtRec2Test = ab.getParticipations().MtRec2,
-                    ViewabilityTest = ab.getParticipations().Viewability;
+                var ViewabilityTest = ab.getParticipations().Viewability;
 
-                return ab.testCanBeRun('MtRec1') && MtRec1Test && MtRec1Test.variant === 'A' ||
-                    ab.testCanBeRun('MtRec2') && MtRec2Test && MtRec2Test.variant === 'A' ||
-                    ab.testCanBeRun('Viewability') && ViewabilityTest && ViewabilityTest.variant === 'variant';
+                return ab.testCanBeRun('Viewability') && ViewabilityTest && ViewabilityTest.variant === 'variant';
             };
 
         $adSlotContainer = $(opts.adSlotContainerSelector);
