c2016ab178d4052a97ae4db48d0cc6e00a2bb5cd
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-16 12:39:25 +0100

    Use actual content ID

--- static/src/javascripts/projects/common/modules/save-for-later.js
+++ static/src/javascripts/projects/common/modules/save-for-later.js
@@ -4,6 +4,7 @@ define([
     'bean',
     'fastdom',
     'common/utils/_',
+    'common/utils/$',
     'common/utils/detect',
     'common/utils/config',
     'common/utils/mediator',
@@ -18,6 +19,7 @@ define([
     bean,
     fastdom,
     _,
+    $,
     detect,
     config,
     mediator,
@@ -27,10 +29,6 @@ define([
     saveForLaterLinkTmpl,
     saveForLaterButtonTmpl
 ) {
-    //This is because of some a/b test wierdness - '$' doesn't work
-    var $ = function (selector, context) {
-        return bonzo(qwery(selector, context));
-    };
 
     function SaveForLater() {
         this.classes = {
@@ -58,10 +56,9 @@ define([
     var bookmarkSvg = svgs('bookmark', ['i-left']);
     var shortUrl = (config.page.shortUrl || '').replace('http://gu.com', '');
 
-    var getCustomEventProperties = function () {
-        var contentType = config.page.contentType;
-        var prefix = contentType.match(/^Network Front|Section$/) ? 'Front' : 'Content';
-        return { prop74: prefix + 'ContainerSave:' + config.page.pageId };
+    var getCustomEventProperties = function (contentId) {
+        var prefix = config.page.contentType.match(/^Network Front|Section$/) ? 'Front' : 'Content';
+        return { prop74: prefix + 'ContainerSave:' + contentId };
     };
 
     SaveForLater.prototype.init = function () {
@@ -178,7 +175,8 @@ define([
                 if (isSaved) {
                     $itemSaveLink.addClass(self.classes.fcItemIsSaved);
                 } else {
-                    $itemSaveLink.attr('data-custom-event-properties', JSON.stringify(getCustomEventProperties()));
+                    var contentId = $($.ancestor($itemSaveLink[0], 'fc-item')).attr('data-id');
+                    $itemSaveLink.attr('data-custom-event-properties', JSON.stringify(getCustomEventProperties(contentId)));
                 }
                 $item.addClass('fc-item--has-metadata'); // while in test
                 $itemSaveLink.attr('data-link-name', isSaved ? 'Unsave' : 'Save');
@@ -265,10 +263,11 @@ define([
         self.createSaveArticleHandler(link, id, shortUrl);
 
         fastdom.write(function () {
+            var contentId = $($.ancestor(link, 'fc-item')).attr('data-id');
             bonzo(link)
                 .removeClass(self.classes.fcItemIsSaved)
                 .attr('data-link-name', 'Save')
-                .attr('data-custom-event-properties', JSON.stringify(getCustomEventProperties()));
+                .attr('data-custom-event-properties', JSON.stringify(getCustomEventProperties(contentId)));
         });
     };
 
