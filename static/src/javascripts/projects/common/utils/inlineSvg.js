c95d090178de17ba3845b08cbd5b14c75c01c0b1
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-17 14:19:31 +0100

    Fix icons

--- static/src/javascripts/projects/common/utils/inlineSvg.js
+++ static/src/javascripts/projects/common/utils/inlineSvg.js
@@ -16,7 +16,7 @@ define([
 
             text.get(req.toUrl(dirs.join('/') + '/' + fileName + '.svg'), function (svg) {
 
-                svg = '<span class=\"' + prefix + fileName + ' ' + (imageType !== ' ? prefix + imageType : ') + '\">' + svg + '</span>';
+                svg = '<span class=\"' + prefix + fileName + ' ' + (imageType !== '' ? prefix + imageType : '') + '\">' + svg + '</span>';
 
                 if (config.isBuild) {
                     buildText[name] = text.jsEscape(svg);
