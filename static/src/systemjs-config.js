e4b22c5fb75fabd77e44d99a1ddb3656b2a36dad
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-17 01:07:51 +0100

    Update to jspm@0.16.0-beta.2

--- static/src/systemjs-config.js
+++ static/src/systemjs-config.js
@@ -228,10 +228,8 @@ System.config({
       "inherits": "npm:inherits@2.0.1",
       "isarray": "npm:isarray@0.0.1",
       "process": "github:jspm/nodelibs-process@0.1.1",
-      "stream": "github:jspm/nodelibs-stream@0.1.0",
       "stream-browserify": "npm:stream-browserify@1.0.0",
-      "string_decoder": "npm:string_decoder@0.10.31",
-      "util": "github:jspm/nodelibs-util@0.1.0"
+      "string_decoder": "npm:string_decoder@0.10.31"
     },
     "npm:source-map@0.1.31": {
       "amdefine": "npm:amdefine@0.1.0",
