19a607889359e6d6cd4ba0f311cc7fa84504603c
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-06-19 09:50:22 +0100

    Search fix box height

--- static/src/javascripts/projects/common/modules/navigation/sticky.js
+++ static/src/javascripts/projects/common/modules/navigation/sticky.js
@@ -242,6 +242,11 @@ define([
                 this.setNavigationDefault();
             }
         }.bind(this));
+
+        // If search is open close it
+        if (this.config.direction === 'down' && $('.js-search-toggle').hasClass('is-active')) {
+            bean.fire(qwery('.js-search-toggle')[0], 'click');
+        }
     };
 
     StickyHeader.prototype.updatePositionMobile = function () {
