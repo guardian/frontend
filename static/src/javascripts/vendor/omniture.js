89788d60a65f241724e1ec9800685ea3105780a2
Oliver Joseph Ash <oliverjash@gmail.com>
2015-06-22 15:30:49 +0100

    Update eVar64 and add event16

--- static/src/javascripts/vendor/omniture.js
+++ static/src/javascripts/vendor/omniture.js
@@ -61,7 +61,7 @@ function s_doPlugins(s) {
     /* Users with a Daily Habit Diary */
     var dtmNow=new Date();
     now=Math.floor(dtmNow.getTime()/86400000);
-    var cutoff=now-13;
+    var cutoff=now-6;
     var diary=s.c_r("s_daily_habit")+"";
     diary=diary.split(",");
     newDiary=[];
@@ -69,13 +69,14 @@ function s_doPlugins(s) {
     for (var i=0,j=diary.length;i<j;++i)
     {
         var pV=diary[i];
-        var tmp=(pV>=cutoff) && (pV!=now) && (vC+=1) && newDiary.push(pV);
+        var tmp=(pV>=cutoff) && (pV<now) && (vC+=1) && newDiary.push(pV);
     }
     newDiary.push(now);
     newDiary=newDiary.join(",");
     dtmNow.setFullYear(dtmNow.getFullYear()+5);
     s.c_w("s_daily_habit",newDiary,dtmNow);
-    s.eVar64=vC+"/14";
+    if (s.getVisitStart==1) s.events = s.apl(s.events, 'event16='+vC, ',');
+    s.eVar64=vC;
 
     /* Campaign stacking */
 //    s.eVar40=s.crossVisitParticipation(s.campaign,'s_ev40','30','5','>','',1);
@@ -313,6 +314,14 @@ s.apl=new Function("l","v","d","u",""
     +"var s=this,m=0;if(!l)l='';if(u){var i,n,a=s.split(l,d);for(i=0;i<a."
     +"length;i++){n=a[i];m=m||(u==1?(n==v):(n.toLowerCase()==v.toLowerCas"
     +"e()));}}if(!m)l=l?l+d+v:v;return l");
+
+/*
+ * Plugin: getVisitStart v2.1 - return 1 on start of visit, else 0
+ */
+s.getVisitStart=new Function("c",""
+    +"var s=this,n,t=new Date;if(typeof s.callType=='function'&&s.callTyp"
+    +"e()=='+')return 0;if(!c)c='s_visit';t.setTime(t.getTime()+18e5);n=s"
+    +".c_r(c)?0:1;if(!s.c_w(c,1,t))s.c_w(c,1,0);if(!s.c_r(c))n=0;return n");
 
 /*
  * Utility Function: split v1.5 (JS 1.0 compatible)
