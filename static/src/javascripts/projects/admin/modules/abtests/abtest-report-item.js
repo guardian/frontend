e41f011e1bd7c33951bf53383caaca62f2fd8d78
Justin Pinner <justinpinner@gmail.com>
2015-06-29 11:43:18 +0100

    Simplify display of AB test hypotheses

--- static/src/javascripts/projects/admin/modules/abtests/abtest-report-item.js
+++ static/src/javascripts/projects/admin/modules/abtests/abtest-report-item.js
@@ -86,26 +86,7 @@ define([
         var ophanUrl = 'https://dashboard.ophan.co.uk/graph/breakdown?ab=' + this.config.test.id;
         this.getElem('ophan').innerHTML = '<a href="' + ophanUrl + '"">graph</a>';
 
-        if (window.abConfigSwitches.abShowHypothesesDashboard) {
-            var hypothesis;
-            var criteria = this.config.test.audienceCriteria ? this.config.test.audienceCriteria.length > 0 : false,
-                description = this.config.test.description ? this.config.test.description.length > 0 : false,
-                reason = this.config.test.reason ? this.config.test.reason.length > 0 : false,
-                success = this.config.test.successMeasure ? this.config.test.successMeasure.length > 0 : false,
-                outcome = this.config.test.idealOutcome ? this.config.test.idealOutcome.length > 0 : false;
-
-            if (criteria && description && reason && success && outcome) {
-                hypothesis = 'We believe that ' + this.config.test.audienceCriteria;
-                hypothesis += ' has a need for ' + this.config.test.description;
-                hypothesis += ' because ' + this.config.test.reason;
-                hypothesis += '. We will know this when ' + this.config.test.successMeasure;
-                hypothesis += ' has ' + this.config.test.idealOutcome;
-                hypothesis += '.';
-            } else {
-                hypothesis = 'Hypothesis incomplete: Test definition lacks sufficient criteria';
-            }
-            this.getElem('hypothesis').textContent = hypothesis;
-        }
+        this.getElem('hypothesis').textContent = this.config.test.hypothesis ? this.config.test.hypothesis : '';
 
         var participation = new Participation({ test: this.config.test });
         participation.render(this.getElem('participation'));
