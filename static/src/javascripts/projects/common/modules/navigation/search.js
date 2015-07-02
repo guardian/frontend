c8e25de7b1a580da141e9cd2b516a6c7acb71073
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-07-02 11:56:36 +0100

    Other way around

--- static/src/javascripts/projects/common/modules/navigation/search.js
+++ static/src/javascripts/projects/common/modules/navigation/search.js
@@ -125,7 +125,7 @@ define([
             // Load the Google search monolith, if not already present in this context.
             // We have to re-run their script each time we do this.
             if (!container.innerHTML) {
-                var autoComplete = (ab.shouldRunTest('Viewability', 'variant') && config.page.contentType !== 'Interactive') ? true : false;
+                var autoComplete = (ab.shouldRunTest('Viewability', 'variant') && config.page.contentType !== 'Interactive') ? false : true;
 
                 fastdom.write(function () {
                     container.innerHTML = '' +
