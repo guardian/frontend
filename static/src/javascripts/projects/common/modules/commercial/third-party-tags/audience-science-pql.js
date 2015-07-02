4ff1a84b2e26aecdaa0d8fcefecf55641fc9d0d8
Ken Lim <kennethlimfc@gmail.com>
2015-07-02 17:47:22 +0100

    Reinstating the AudSci PQL tag.
    But this time, it is NOT integrated with the Ad call.
    So not so bad.

---
+++ static/src/javascripts/projects/common/modules/commercial/third-party-tags/audience-science-pql.js
@@ -0,0 +1,55 @@
+define([
+    'common/utils/_',
+    'common/utils/config',
+    'common/utils/storage',
+    'common/utils/url'
+], function (
+    _,
+    config,
+    storage,
+    urlUtils
+) {
+
+    var gatewayUrl = 'http://pq-direct.revsci.net/pql',
+        storageKey = 'gu.ads.audsci-gateway',
+        sectionPlacements = {
+            sport:        ['FKSWod', '2xivTZ', 'MTLELH'],
+            football:     ['6FaXJO', 'ORE2W-', 'MTLELH'],
+            lifeandstyle: ['TQV1_5', 'J0tykU', 'kLC9nW', 'MTLELH'],
+            technology:   ['9a9VRE', 'TL3gqK', 'MTLELH'],
+            fashion:      ['TQV1_5', 'J0tykU', 'kLC9nW', 'MTLELH'],
+            news:         ['eMdl6Y', 'mMYVrM', 'MTLELH'],
+            'default':    ['FLh9mM', 'c7Zrhu', 'Y1C40a', 'LtKGsC', 'MTLELH']
+        },
+        section,
+        init = function () {
+            section = sectionPlacements[config.page.section] ? config.page.section : 'default';
+        },
+        load = _.once(function () {
+            if (config.switches.audienceScienceGateway) {
+                var placements = sectionPlacements[section],
+                    query = urlUtils.constructQuery({
+                        placementIdList: placements.join(','),
+                        cb: new Date().getTime()
+                    }),
+                    url = [gatewayUrl, '?', query].join('');
+
+                return require(['js!' + url], function () {
+                    var asiPlacements = window.asiPlacements;
+                    var segments = storage.local.get(storageKey) || {};
+                    // override the global value with our previously stored one
+                    window.asiPlacements = segments[section];
+                    segments[section] = asiPlacements;
+                    storage.local.set(storageKey, segments);
+                });
+            }
+        });
+
+    init();
+
+    return {
+        load: load,
+        init: init
+    };
+
+});
