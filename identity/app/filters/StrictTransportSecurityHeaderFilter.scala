e2ad954f9621ade3b073f34af4e6dc64e773cd8a
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-07-13 12:36:38 +0100

    Extract the max age for the Strict-Transport-Security header

--- identity/app/filters/StrictTransportSecurityHeaderFilter.scala
+++ identity/app/filters/StrictTransportSecurityHeaderFilter.scala
@@ -1,13 +1,14 @@
 package filters
 
 import common.ExecutionContexts
-import play.api.mvc.{Result, RequestHeader, Filter}
+import play.api.mvc.{Filter, RequestHeader, Result}
 
 import scala.concurrent.Future
 
 object StrictTransportSecurityHeaderFilter extends Filter with ExecutionContexts {
 
-  private val Header = "Strict-Transport-Security" -> "max-age=31536000; preload"
+  private val OneYearInSeconds = 31536000
+  private val Header = "Strict-Transport-Security" -> s"max-age=$OneYearInSeconds; preload"
 
   override def apply(nextFilter: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
     nextFilter(request).map(_.withHeaders(Header))
