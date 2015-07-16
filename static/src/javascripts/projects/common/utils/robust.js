c662545393e794055b5d0cdf7017dd0c9c162aad
Oliver Joseph Ash <oliverjash@gmail.com>
2015-07-16 13:10:44 +0100

    Lint

--- static/src/javascripts/projects/common/utils/robust.js
+++ static/src/javascripts/projects/common/utils/robust.js
@@ -20,7 +20,7 @@ define([
     };
 
     var log = function (name, error, reporter) {
-        console.warn('Caught error.', error.stack);
+        window.console.warn('Caught error.', error.stack);
         if (!reporter) {
             reporter = raven.captureException;
         }
@@ -39,7 +39,7 @@ define([
             var name = pair[0];
             var fn = pair[1];
             catchErrorsAndLog(name, fn);
-        })
+        });
     };
 
     return {
