bcc65e9088e032b767bf0271b1113b4673cf1926
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-07-16 20:25:40 +0100

    Use a GuiceApplicationLoader to specify an identity module

--- identity/app/Global.scala
+++ identity/app/Global.scala
@@ -6,6 +6,8 @@ import play.api.Play.current
 import play.api._
 import play.api.mvc._
 import play.api.mvc.Results._
+import play.api.inject._
+import play.api.inject.guice._
 import scala.concurrent.Future
 import utils.SafeLogging
 
@@ -16,20 +18,6 @@ object Global extends WithFilters(HeaderLoggingFilter :: StrictTransportSecurity
   with SwitchboardLifecycle {
 
   override lazy val applicationName = "frontend-identity"
-
-  private lazy val injector = {
-    val module =
-      Play.mode match {
-        case Mode.Prod => {
-          if (conf.Configuration.environment.isNonProd) new PreProdModule
-          else new ProdModule
-        }
-        case Mode.Dev => new DevModule
-        case Mode.Test => new TestModule
-      }
-
-    Guice.createInjector(module)
-  }
 
   override def onError(request: RequestHeader, ex: Throwable) = {
     logger.error("Serving error page", ex)
@@ -56,5 +44,23 @@ object Global extends WithFilters(HeaderLoggingFilter :: StrictTransportSecurity
     } else {
       super.onBadRequest(request, error)
     }
+  }
+}
+
+class IdentityApplicationLoader extends GuiceApplicationLoader() {
+
+  override def builder(context: ApplicationLoader.Context): GuiceApplicationBuilder = {
+    val module = context.environment.mode match {
+      case Mode.Prod => {
+        if (conf.Configuration.environment.isNonProd) new PreProdModule
+        else new ProdModule
+      }
+      case Mode.Dev => new DevModule
+      case Mode.Test => new TestModule
+    }
+    new GuiceApplicationBuilder()
+      .in(context.environment)
+      .loadConfig(context.initialConfiguration)
+      .bindings(module)
   }
 }
