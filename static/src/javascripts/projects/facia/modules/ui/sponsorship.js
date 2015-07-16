6536eaafcfb0493dac85d4e568a6b26102c11838
Robert Berry <robert.berry@theguardian.com>
2015-07-16 15:05:57 +0100

    Add sponsorship code

--- static/src/javascripts/projects/facia/modules/ui/sponsorship.js
+++ static/src/javascripts/projects/facia/modules/ui/sponsorship.js
@@ -1,40 +1,83 @@
 define([
     'common/utils/_',
     'bean',
-    'Promise'
+    'bonzo',
+    'fastdom',
+    'Promise',
+    'qwery'
 ], function (
     _,
     bean,
-    Promise
+    bonzo,
+    fastdom,
+    Promise,
+    qwery
 ) {
-    console.log("hi");
-
     var keyPressHistory = [],
-        cheatCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+        cheatCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+        tones = _.map([
+            "special-report",
+            "live",
+            "dead",
+            "feature",
+            "editorial",
+            "comment",
+            "podcast",
+            "media",
+            "analysis",
+            "review",
+            "letters",
+            "external",
+            "news"
+        ], function (tone) {
+            return "tone-" + tone + "--item";
+        });
 
     function listenForCheatCode() {
         return new Promise(function (resolve) {
-            bean.on(window, 'keypress', function (event) {
+            var onKeyDown = function (event) {
                 keyPressHistory.push(event.keyCode);
 
-                console.log("new history", keyPressHistory);
-
-                if (_.isEqual(cheatCode.slice(keyPressHistory.length), keyPressHistory)) {
+                if (_.isEqual(cheatCode.slice(0, keyPressHistory.length), keyPressHistory)) {
                     if (keyPressHistory.length === cheatCode.length) {
                         resolve();
+                        bean.off(document, 'keydown', onKeyDown);
                     }
                 } else {
                     keyPressHistory = [];
                 }
-            });
+            };
+
+            bean.on(document, 'keydown', onKeyDown);
         });
     }
 
-    function disco() {
-        console.log("CHEAT!");
+    function randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function randomTone() {
+        return tones[randomInt(0, tones.length - 1)];
+    }
+
+    function startToneDisco() {
+        var $items = _.map(qwery('.js-fc-item'), bonzo);
+        console.log("PPPOOOOWWWWWEEEERRRRING UP!!!!");
+
+        setInterval(function () {
+            fastdom.write(function () {
+                $items.forEach(function ($item) {
+                    tones.forEach(function (tone) {
+                        $item.removeClass(tone);
+                    });
+
+                    $item.addClass(randomTone());
+                });
+            });
+        }, 1000);
     }
 
     return function () {
-        listenForCheatCode().then(disco);
+        listenForCheatCode().then(startToneDisco);
     };
 });
