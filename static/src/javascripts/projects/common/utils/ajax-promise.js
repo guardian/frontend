30d3fd4402ab818df088f315f89a91b24ba853b1
Francis Carr <francis.carr@guardian.co.uk>
2015-07-20 11:43:40 +0100

    Change to the one text only

--- static/src/javascripts/projects/common/utils/ajax-promise.js
+++ static/src/javascripts/projects/common/utils/ajax-promise.js
@@ -16,7 +16,7 @@ define([
             .fail(function (request, text, err) {
                 var statusText = (err && err.statusText) || '';
                 var statusCode = (err && err.status) || '';
-                var errorText = text ? text : 'Error retrieving data (' + text + ') (Status: ' + statusCode + ') (StatusText: ' + statusText + ')';
+                var errorText = 'Error retrieving data (' + text + ') (Status: ' + statusCode + ') (StatusText: ' + statusText + ')';
 
                 var error = err ? err : new Error(errorText);
 
