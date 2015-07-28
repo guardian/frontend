aaff7b0a3e8033ce74281ec2ab8ea02e990dac86
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-07-28 14:23:16 +0100

    Unit test

--- static/src/javascripts/test/spec/common/commercial/third-party-tags/outbrain.spec.js
+++ static/src/javascripts/test/spec/common/commercial/third-party-tags/outbrain.spec.js
@@ -1,31 +1,8 @@
 import sut from 'common/modules/commercial/third-party-tags/outbrain';
-import mediator from 'common/utils/mediator';
 
 describe('Outbrain', function () {
 
     it('should exist', function () {
         expect(sut).toBeDefined();
-    });
-
-    it('should listen for dfp render event', function () {
-    	spyOn(sut, 'load');
-
-    	var event = {
-    		slot: {
-    			getSlotId: function () {
-    				return {
-    					getDomId: function () {
-    						return ''
-    					}
-    				}
-    			}
-    		},
-    		isEmpty: true
-    	};
-
-    	sut.init();
-    	mediator.emit('modules:commercial:dfp:rendered', event);
-
-    	expect(sut.load).toHaveBeenCalled();
     });
 });
