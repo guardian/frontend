a07055365ace93a34bb09ac218781a9261d6de6a
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2015-07-28 16:18:22 +0100

    refactor series.js

--- static/src/javascripts/es6/projects/common/modules/crosswords/series.js
+++ static/src/javascripts/es6/projects/common/modules/crosswords/series.js
@@ -5,24 +5,15 @@ import config from 'common/utils/config';
 import proximityLoader from 'common/utils/proximity-loader';
 import Series from 'common/modules/onward/onward-content';
 
-function insertOrProximity(selector, insert) {
-    if (window.location.href) {
-        insert();
-    } else {
-        const el = qwery(selector)[0];
-
-        if (el) {
-            proximityLoader(el, 1500, insert);
-        }
-    }
-}
-
-
 export default() => {
 
-    insertOrProximity('.js-onwards', function () {
-        if (config.page.seriesId && config.page.showRelatedContent) {
-            new Series(qwery('.js-onward'));
-        }
-    });
+    const el = qwery('.js-onward')[0];
+
+    if (el) {
+        proximityLoader.add(el, 1500, function () {
+            if (config.page.seriesId && config.page.showRelatedContent) {
+                new Series(qwery('.js-onward'));
+            }
+        });
+    }
 };
