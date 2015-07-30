8680cb9cb95599a351624f7acd62fc1581c79a3f
Sam Desborough <sam.desborough@gmail.com>
2015-07-30 16:29:58 +0100

    fix bug in shuffler and add extra test case to cover it

--- static/src/javascripts/test/spec/common/crosswords/anagram-helper.spec.js
+++ static/src/javascripts/test/spec/common/crosswords/anagram-helper.spec.js
@@ -1,23 +1,11 @@
 import _ from 'common/utils/_';
 import Shuffler from 'es6/projects/common/modules/crosswords/anagram-helper/shuffler';
 
-describe('Anagram Helper', function () {
-    let shuffler;
-
-    beforeEach(function () {
-        const entriesFixture = ['', '', '', '', 'l', '', 'e'].map(letter => {
-            return { value: letter };
-        });
-
-        shuffler = new Shuffler({
-            entries: entriesFixture,
-            word: 'liberal'.split('')
-        });
-    });
-
-    it('marks the correct letters as entered', function () {
-        const result = shuffler.getLetters();
-        const expected = [
+const fixtures = [
+    {
+        entries: ['', '', '', '', 'l', '', 'e'],
+        word: 'liberal',
+        expected: [
             { value: 'l', entered: true },
             { value: 'i', entered: false },
             { value: 'b', entered: false },
@@ -25,10 +13,51 @@ describe('Anagram Helper', function () {
             { value: 'r', entered: false },
             { value: 'a', entered: false },
             { value: 'l', entered: false }
-        ];
+        ]
+    },
+
+    // when there's an incorrect entry we should still highlight the correct ones
+    {
+        entries: ['s', '', '', '', 'h', '', '', '', '', '', '', ''],
+        word: 'atennistutor',
+        expected: [
+            { value: 'a', entered: false },
+            { value: 't', entered: false },
+            { value: 'e', entered: false },
+            { value: 'n', entered: false },
+            { value: 'n', entered: false },
+            { value: 'i', entered: false },
+            { value: 's', entered: true },
+            { value: 't', entered: false },
+            { value: 'u', entered: false },
+            { value: 't', entered: false },
+            { value: 'o', entered: false },
+            { value: 'r', entered: false }
+        ]
+    }
+];
+
+fdescribe('Anagram Helper', function () {
+    it('marks the correct letters as entered', function () {
+        const cases = fixtures.map(fixture => {
+            return {
+                instance: new Shuffler({
+                    entries: fixture.entries.map(letter => {
+                        return { value: letter };
+                    }),
+                    word: fixture.word.split('')
+                }),
+
+                expected: fixture.expected
+            };
+        });
 
         const sort = x => x.value + x.entered.toString();
 
-        expect(_.sortBy(result, sort)).toEqual(_.sortBy(expected, sort));
+        cases.forEach(testCase => {
+            const result = testCase.instance.getLetters();
+
+            expect(_.sortBy(result, sort)).toEqual(_.sortBy(testCase.expected, sort));
+        });
     });
 });
