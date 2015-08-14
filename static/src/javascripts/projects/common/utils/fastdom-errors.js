37851ba0f269048fc63d9bcfa82778eea3d0ca8d
Oliver Joseph Ash <oliverjash@gmail.com>
2015-08-14 12:46:57 +0100

    Log first

--- static/src/javascripts/projects/common/utils/fastdom-errors.js
+++ static/src/javascripts/projects/common/utils/fastdom-errors.js
@@ -9,12 +9,12 @@ define([
     reportError
 ) {
     fastdom.onError = function (error) {
-        reportError(error, {
-            feature: 'fastdom'
-        }, false);
         // Some environments don't support or don't always expose the console object
         if (window.console && window.console.warn) {
             window.console.warn('Caught FastDom error.', error.stack);
         }
+        reportError(error, {
+            feature: 'fastdom'
+        }, false);
     };
 });
