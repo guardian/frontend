aeec51189b6bb1bf82f09542d16fbe3738d51adf
Oliver Joseph Ash <oliverjash@gmail.com>
2015-08-17 13:23:30 +0100

    Revert "Merge branch 'oja-jspm-test-beacon'"
    This reverts commit e8d13d9af4e7be3340af196665f83d3db6cb17cb, reversing
    changes made to 7fb907deaaccb90d892a89db4523a23da0310131.

--- common/app/templates/headerInlineJS/cloudwatchBeacons.scala.js
+++ common/app/templates/headerInlineJS/cloudwatchBeacons.scala.js
@@ -73,22 +73,5 @@
             }
         }
 
-
-        // Send beacon for unique visitors in JspmTest and JspmControl server-side test variants
-        // Requires localStorage, so modern browsers only
-        if (window.guardian.isModernBrowser && guardian.config.switches.jspmTestUniqueVisitorsBeacon) {
-            var tests = guardian.config.tests;
-            var inTest = tests.jspmTest || tests.jspmControl;
-            var localStorageKey = 'gu.jspm-test.visited';
-            // Protect browsers with localStorage but permissions disabled
-            try {
-                var visited = !!window.localStorage.getItem(localStorageKey)
-                if (!visited && inTest) {
-                    var beaconName = tests.jspmTest ? 'jspm-test' : 'jspm-control';
-                    (new Image()).src = window.guardian.config.page.beaconUrl + '/count/' + beaconName + '.gif';
-                    window.localStorage.setItem(localStorageKey, true);
-                }
-            } catch (e) {}
-        }
     })(window, navigator);
 }
