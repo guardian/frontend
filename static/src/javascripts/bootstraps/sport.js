c497e74c829ee4ab533af0dd5947a04801dcb4c8
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-08-20 16:34:27 +0100

    ajax call

--- static/src/javascripts/bootstraps/sport.js
+++ static/src/javascripts/bootstraps/sport.js
@@ -1,13 +1,15 @@
 define([
     'common/utils/$',
+    'common/utils/ajax',
     'common/utils/config',
     'common/modules/component'
 ], function (
     $,
+    ajax,
     config,
     Component
 ) {
-    function init() {
+    function cricket() {
         var cricketScore, parentEl,
             matchIdentifier = config.page.cricketMatch;
 
@@ -18,6 +20,38 @@ define([
             cricketScore.endpoint = '/sport/cricket/match/' + matchIdentifier + '.json';
             cricketScore.fetch(parentEl, 'summary');
         }
+    }
+
+    function rugby() {
+
+        if (config.page.rugbyMatch) {
+
+            //var $h = $('.js-score');
+
+            ajax({
+                url: config.page.rugbyMatch + '.json',
+                type: 'json',
+                crossOrigin: true
+            }).then(
+                function (response) {
+                    response.liveScore; /// bonzo
+                }
+            );
+
+                /*scoreContainer = bonzo.create(
+                    '<div class="score-container">' +
+                    '</div>'
+                )[0];
+
+            $h.after(scoreContainer);*/
+
+        }
+    }
+
+    function init()  {
+
+        rugby();
+        cricket();
     }
 
     return {
