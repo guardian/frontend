aa597e1e9e5d4de071414554a7ea89889feead4c
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-08-21 15:23:57 +0100

    Updated information and load the A/B test on the admin page

--- static/src/javascripts/projects/common/modules/experiments/tests/rugby.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/rugby.js
@@ -1,22 +1,22 @@
 define([
     'bootstraps/sport'
 ], function (
-    sport,
+    sport
 ) {
 
     return function () {
 
         this.id = 'RugbyScores';
-        this.start = '';
-        this.expiry = '';
-        this.author = '';
-        this.description = '';
+        this.start = '2015-08-21';
+        this.expiry = '2015-11-07';
+        this.author = 'Jenny Sivapalan';
+        this.description = 'Scores for the Rugby World Cup to be loaded in on live blogs / reports etc';
         this.audience = 0;
         this.audienceOffset = 0;
-        this.successMeasure = '';
+        this.successMeasure = 'More users will come to Guadian for live updates.';
         this.audienceCriteria = 'everyone';
         this.dataLinkNames = '';
-        this.idealOutcome = '';
+        this.idealOutcome = 'More users will come to Guadian for live updates.';
 
         this.canRun = function () {
             return true;
