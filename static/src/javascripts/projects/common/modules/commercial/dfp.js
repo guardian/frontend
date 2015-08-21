4077c90f2940563466c371a9f2ef8805934f40d8
Tijana <tijana.miletic@guardian.co.uk>
2015-08-21 15:14:42 +0100

    amends

--- static/src/javascripts/projects/common/modules/commercial/dfp.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp.js
@@ -159,13 +159,11 @@ define([
 
         isSponsorshipContainerTest = function () {
             var sponsorshipIds = ['#dfp-ad--adbadge', '#dfp-ad--spbadge', '#dfp-ad--fobadge', '#dfp-ad--adbadge1', '#dfp-ad--spbadge1', '#dfp-ad--fobadge1', '#dfp-ad--adbadge2', '#dfp-ad--spbadge2', '#dfp-ad--fobadge2', '#dfp-ad--adbadge3', '#dfp-ad--spbadge3', '#dfp-ad--fobadge3', '#dfp-ad--adbadge4', '#dfp-ad--spbadge4', '#dfp-ad--fobadge4', '#dfp-ad--adbadge5', '#dfp-ad--spbadge5', '#dfp-ad--fobadge5'],
-                sponsorshipIdsReturned = [],
-                sponsorshipIdReturned;
+                sponsorshipIdsReturned = [];
 
             _.forEach(sponsorshipIds, function (value) {
                 if ($(value).length) {
-                    sponsorshipIdReturned = value;
-                    sponsorshipIdsReturned.push(sponsorshipIdReturned);
+                    sponsorshipIdsReturned.push(value);
                 }
             });
 
@@ -181,6 +179,7 @@ define([
                         var sponsorshipIdFoundEl = $(value),
                             sponsorshipIdClasses = sponsorshipIdFoundEl.attr('class').replace('ad-slot ', ''),
                             sponsorshipBadge = '<div class="' + sponsorshipIdClasses + '">' + sponsorshipIdFoundEl.html() + '</div>';
+
                         if (sponsorshipIdFoundEl.previous().length) {
                             sponsorshipIdFoundEl.previous().append(sponsorshipBadge);
                         } else {
