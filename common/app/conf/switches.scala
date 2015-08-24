f5b20c606bce863684c04c4d303ae666ea9b175c
Zofia <zofia.korcz@guardian.co.uk>
2015-08-24 17:18:24 +0100

    AB test created

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -944,6 +944,15 @@ object Switches {
     exposeClientSide = true
   )
 
+  val ABDisableAdsSurvey = Switch(
+    "A/B Tests",
+    "ab-disable-ad-survey",
+    "Switch to show the survey which tests if users will be interested in paying for the Guardian with no ads",
+    safeState = Off,
+    sellByDate = new LocalDate(2015, 9, 1),
+    exposeClientSide = true
+  )
+
   val FootballFeedRecorderSwitch = Switch(
     "Feature",
     "football-feed-recorder",
