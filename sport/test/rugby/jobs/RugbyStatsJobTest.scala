4df1f0767723366f04a6926368871cc3e49b2a16
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-08-24 14:29:14 +0100

    Add test for reversible team ids

--- sport/test/rugby/jobs/RugbyStatsJobTest.scala
+++ sport/test/rugby/jobs/RugbyStatsJobTest.scala
@@ -10,49 +10,71 @@ import scala.language.reflectiveCalls
 
 @DoNotDiscover class RugbyStatsJobTest extends FreeSpec with ShouldMatchers with ConfiguredTestSuite with Eventually {
 
+  val testDate = new DateTime(2015, 8, 24, 0, 0)
+
+  val (homeTeamOld, homeTeamNew, awayTeamOld, awayTeamNew) = (
+    Team("1", "home", Some(10)),
+    Team("1", "home", Some(20)),
+    Team("2", "away", Some(30)),
+    Team("2", "away", Some(40)))
+
+  val (oldMatch, newMatch) = (
+    Match(
+      date = testDate,
+      id = "match id",
+      homeTeam = homeTeamOld,
+      awayTeam = awayTeamOld
+    ),
+    Match(
+      date = testDate.plusSeconds(1),
+      id = "match id",
+      homeTeam = homeTeamNew,
+      awayTeam = awayTeamNew
+    ))
+
   "RugbyStatsJob" - {
     "should update a live match with new score data" in {
-
-      val testDate = new DateTime(2015, 8, 24, 0, 0)
-
-      val (homeTeamOld, homeTeamNew, awayTeamOld, awayTeamNew) = (
-        Team("1", "home", Some(10)),
-        Team("1", "home", Some(20)),
-        Team("2", "away", Some(30)),
-        Team("2", "away", Some(40)))
-
-      val (oldMatch, newMatch) = (
-        Match(
-          date = testDate,
-          id = "match id",
-          homeTeam = homeTeamOld,
-          awayTeam = awayTeamOld
-        ),
-        Match(
-          date = testDate.plusSeconds(1),
-          id = "match id",
-          homeTeam = homeTeamNew,
-          awayTeam = awayTeamNew
-        ))
 
       val testStatsJob = new RugbyStatsJob {
         def allFixtures = fixturesAndResultsMatches.get()
         def liveMatches = liveScoreMatches.get()
       }
 
-      testStatsJob.allFixtures.size should be(0)
-      testStatsJob.liveMatches.size should be(0)
+      testStatsJob.allFixtures.size should be (0)
+      testStatsJob.liveMatches.size should be (0)
 
       testStatsJob.sendLiveScores(Future.successful(List(oldMatch)))
 
       eventually {
         testStatsJob.allFixtures.size should be(0)
         testStatsJob.liveMatches.size should be(1)
+      }
+
+      val oldMatchData = testStatsJob.getLiveScore("2015", "08", "24", "1", "2")
+
+      oldMatchData shouldBe defined
+      oldMatchData.flatMap(_.homeTeam.score) should be (Some(10))
+
+      testStatsJob.sendLiveScores(Future.successful(List(newMatch)))
+
+      eventually {
+        testStatsJob.allFixtures.size should be(0)
+        testStatsJob.liveMatches.size should be(1)
 
         val matchData = testStatsJob.getLiveScore("2015", "08", "24", "1", "2")
-
-        matchData shouldBe defined
+        matchData.flatMap(_.homeTeam.score) should be (Some(20))
       }
     }
+
+    "should find a match even when the team ids are reversed" in {
+      val testStatsJob = new RugbyStatsJob {}
+
+      testStatsJob.sendLiveScores(Future.successful(List(oldMatch)))
+
+      eventually {
+        testStatsJob.getLiveScore("2015", "08", "24", "2", "1") shouldBe defined
+      }
+    }
+
   }
 }
