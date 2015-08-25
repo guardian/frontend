4c885d92f90a985a99282fe531249cf6e2395ea3
Tijana <tijana.miletic@guardian.co.uk>
2015-08-25 16:19:35 +0100

    Expandable video template test update

--- static/src/javascripts/test/spec/common/commercial/creatives/expandable-video.spec.js
+++ static/src/javascripts/test/spec/common/commercial/creatives/expandable-video.spec.js
@@ -11,15 +11,27 @@ var fixturesConfig = {
 
 describe('Expandable Video', function () {
 
-    var expandableVideo;
+    var expandableVideo,
+        $fixturesContainer;
 
     it('should exist', function () {
         expect(ExpandableVideo).toBeDefined();
     });
 
     it('should be always defined', function () {
+        $fixturesContainer = fixtures.render(fixturesConfig);
         expandableVideo = new ExpandableVideo($('.expandablevideo-ad-slot'), {});
         expect(expandableVideo).toBeDefined();
+    });
+
+    it('should have show more button', function (done) {
+        $fixturesContainer = fixtures.render(fixturesConfig);
+        new ExpandableVideo($('.expandablevideo-ad-slot', $fixturesContainer), {
+            showMoreType: 'plus-only'
+        }).create().then(function () {
+            expect($('.ad-exp__close-button').length).toBeGreaterThan(0);
+            done();
+        });
     });
 
 });
