8c641f28108b7122956e3bb91ec468512e9c3f55
John Duffell <john.duffell@guardian.co.uk>
2015-08-25 15:03:04 +0100

    fix article image handling code

--- article/app/views/package.scala
+++ article/app/views/package.scala
@@ -1,9 +1,22 @@
 package views
 
 import common.Edition
+import layout.ContentWidths.MainMedia
 import model.Article
 import play.api.mvc.RequestHeader
 import views.support._
+
+object Widths {
+
+  def apply(isShowcase: Boolean, isFeature: Boolean): layout.WidthsByBreakpoint = {
+    (isShowcase, isFeature) match {
+      case (true, true) => MainMedia.FeatureShowcase
+      case (true, _) => MainMedia.Showcase
+      case _ => MainMedia.Inline
+    }
+  }
+
+}
 
 object MainCleaner {
  def apply(article: Article, html: String)(implicit request: RequestHeader) = {
