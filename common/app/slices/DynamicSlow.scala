0c428e2eb1aa8969ae4f631a11dda2785510e570
Francis Carr <francis.carr@guardian.co.uk>
2015-08-25 14:23:40 +0100

    Add DynamicSlowMpu container type

--- common/app/slices/DynamicSlow.scala
+++ common/app/slices/DynamicSlow.scala
@@ -17,3 +17,24 @@ object DynamicSlow extends DynamicContainer {
     }
   }
 }
+
+object DynamicSlowMPU extends DynamicContainer {
+  override protected def optionalFirstSlice(stories: Seq[Story]): Option[(Slice, Seq[Story])] = {
+    val BigsAndStandards(bigs, _) = bigsAndStandards(stories)
+    val isFirstBoosted = stories.headOption.exists(_.isBoosted)
+
+    if (bigs.length == 3) {
+      Some((HalfQQ, stories.drop(3)))
+    } else if (bigs.length == 2) {
+      Some((if (isFirstBoosted) ThreeQuarterQuarter else HalfHalf, stories.drop(2)))
+    } else if (bigs.length == 1) {
+      Some(if (isFirstBoosted) ThreeQuarterQuarter else HalfHalf, stories.drop(2))
+    } else if (bigs.isEmpty) {
+      None
+    } else {
+      Some(QuarterQuarterQuarterQuarter, stories.drop(4))
+    }
+  }
+
+  override protected def standardSlices(stories: Seq[Story], firstSlice: Option[Slice]): Seq[Slice] = Seq(Hl3Mpu)
+}
