0973f5739ff37c0351c0ac8a08e32544e63fea0c
Francis Carr <francis.carr@guardian.co.uk>
2015-08-27 14:59:46 +0100

    Change DedupedController to not use tuple underscores

--- facia/app/controllers/DedupedController.scala
+++ facia/app/controllers/DedupedController.scala
@@ -13,9 +13,8 @@ trait DedupedController extends Controller with Logging with ExecutionContexts {
   def getDedupedForPath(path: String) = Action.async { request =>
     frontJsonFapi.get(path).map {
       case Some(pressedFront) =>
-        val removed = Front.fromPressedPageWithDeduped(pressedFront)._1._3
-
-        Ok(Json.toJson(removed))
+        val ((_, _, dedupedFrontResult), _) = Front.fromPressedPageWithDeduped(pressedFront)
+        Ok(Json.toJson(dedupedFrontResult))
       case None => NotFound
     }
   }
