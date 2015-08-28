76aa60c876d01e137b7b69b931d795eef93d1277
Grant Klopper <grant.klopper@theguardian.com>
2015-08-28 17:08:18 +0100

    Give healthchecks a bit of leeway to run in tests.

--- admin/test/services/AdminHealthcheckTest.scala
+++ admin/test/services/AdminHealthcheckTest.scala
@@ -1,15 +1,16 @@
 package services
 
+import common.TestWsConfig
 import org.scalatest.{DoNotDiscover, Matchers, FlatSpec}
 import play.api.libs.ws.WS
 import test.ConfiguredTestSuite
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-@DoNotDiscover class AdminHealthCheckTest extends FlatSpec with Matchers with ConfiguredTestSuite {
+@DoNotDiscover class AdminHealthCheckTest extends FlatSpec with Matchers with ConfiguredTestSuite with TestWsConfig {
 
   "Healthchecks" should "pass" in goTo("/admin"){ browser =>
 
-    Await.result(WS.url(withHost(s"/_healthcheck")).get(), 10.seconds).status should be (200)
+    Await.result(WS.clientUrl(withHost(s"/_healthcheck")).get(), 10.seconds).status should be (200)
   }
 }
