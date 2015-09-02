3c1b58d68f734da17e3a9c5aad81213439c5bc09
Oliver Joseph Ash <oliverjash@gmail.com>
2015-09-02 10:43:49 +0100

    Log error

--- applications/app/services/ContentApiGetters.scala
+++ applications/app/services/ContentApiGetters.scala
@@ -1,14 +1,14 @@
 package services
 
 import com.gu.contentapi.client.model.{Content => ApiContent, Crossword}
-import common.Edition
+import common.{Logging, Edition}
 import conf.LiveContentApi
 import controllers.WebAppController._
 import play.api.mvc.{RequestHeader, Result}
 
 import scala.concurrent.Future
 
-object ContentApiGetters {
+object ContentApiGetters extends Logging {
   def withCrossword(crosswordType: String, id: Int)(f: (Crossword, ApiContent) => Result)(implicit request: RequestHeader): Future[Result] = {
     LiveContentApi.getResponse(LiveContentApi.item(s"crosswords/$crosswordType/$id", Edition(request)).showFields("all")).map { response =>
       val maybeCrossword = for {
@@ -16,6 +16,9 @@ object ContentApiGetters {
         crossword <- content.crossword }
         yield f(crossword, content)
       maybeCrossword getOrElse InternalServerError("Crossword response from Content API invalid.")
-    } recover { case _ => InternalServerError("Content API query returned an error.") }
+    } recover { case e =>
+      log.error("Content API query returned an error.", e)
+      InternalServerError("Content API query returned an error.")
+    }
   }
 }
