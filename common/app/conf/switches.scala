8486abe2a652a6fa0db41c1a75452dc79a73d24f
Grant Klopper <grant.klopper@theguardian.com>
2015-09-02 12:37:13 +0100

    Extend No-Bounce switch till after Q4

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -1104,7 +1104,7 @@ object Switches {
     "no-bounce-indicator",
     "If this switch is on then some beacons will be dropped to gauge if people move onto a new piece of content before Omniture runs",
     safeState = On,
-    sellByDate = new LocalDate(2015, 8, 31),
+    sellByDate = new LocalDate(2016, 1, 10),
     exposeClientSide = true
   )
 
