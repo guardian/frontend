2012501af05ef5be7db559f9f456b0d485462fd2
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2015-09-04 18:07:37 +0100

    fix validation errors

--- static/src/javascripts/es6/projects/common/modules/crosswords/anagram-helper/clue-preview.js
+++ static/src/javascripts/es6/projects/common/modules/crosswords/anagram-helper/clue-preview.js
@@ -5,31 +5,28 @@ import _ from 'common/utils/_';
 export default class CluePreview extends React.Component {
 
     letterHasBoundary(separators, letterIndex) {
-       return _.some(separators, function(separator){
-           return separator === letterIndex
+        return _.some(separators, function (separator) {
+            return separator === letterIndex;
         });
     }
 
+    //Checks a object in the form{",":[4,7]}
     getSeparatorLocationsForClue (locations, letterIndex) {
-        const spaces = locations[","];
-        if(spaces && this.letterHasBoundary(spaces, letterIndex) ) {
+        const spaces = locations[','];
+        if (spaces && this.letterHasBoundary(spaces, letterIndex)) {
             return 'crossword__anagram-helper__cell crossword__anagram-helper__cell--with-space';
         }
 
-
-        const dashes = locations["-"]
+        const dashes = locations['-'];
         if (dashes && this.letterHasBoundary(dashes, letterIndex)) {
             return 'crossword__anagram-helper__cell crossword__anagram-helper__cell--with-hyphen';
         }
 
-        else
-          return 'crossword__anagram-helper__cell';
+        return 'crossword__anagram-helper__cell';
     }
 
 
     render () {
-
-        const sepLocs = this.props.clue.separatorLocations
         return (
             <div className={'crossword__anagram-helper__clue-preview ' + (this.props.entries.length >= 7 ? 'long' : '')}>
                 <div><strong>{this.props.clue.number} <span className="crossword__anagram-helper__direction">{this.props.clue.direction}</span></strong> {this.props.clue.clue}</div>
