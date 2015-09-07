ab354ca2ad13c915cb4231e36f25e8f019d8684f
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-09-07 09:52:33 +0100

    Extend Fix Top Slot Switch
    This is still being tested in Code
    and hasn't been turned on in Prod yet.

--- common/app/conf/switches.scala
+++ common/app/conf/switches.scala
@@ -534,9 +534,9 @@ object Switches {
   val FixedTopAboveNavAdSlotSwitch = Switch(
     "Commercial",
     "fixed-top-above-nav",
-    "Fixes size of top-above-nav ad slot on UK network front.",
+    "Fixes size of top-above-nav ad slot on fronts.",
     safeState = Off,
-    sellByDate = new LocalDate(2015, 9, 9),
+    sellByDate = new LocalDate(2015, 10, 7),
     exposeClientSide = false
   )
 
