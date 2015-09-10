cb47c008cc0065151a5265c3bc52d66c452aeda5
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-09-10 17:56:16 +0100

    Added three endpoints for facia press. This will help identify if it is the cron schedule or the task itself that is causing the increase in thread count.

--- admin/app/controllers/FrontPressController.scala
+++ admin/app/controllers/FrontPressController.scala
@@ -12,5 +12,20 @@ object FrontPressController extends Controller with Logging with AuthLogging wit
       case None => InternalServerError("Could not push to the SQS queue, is there an SNS topic set? (frontPressSns)")
     }
   }
+
+  def queueHighFrequencyFrontsForPress() = AuthActions.AuthActionTest { request =>
+    RefreshFrontsJob.runHighFrequency()
+    Ok("Running high frequency press job but you'll have to check the logs to see if it worked")
+  }
+
+  def queueStandardFrequencyFrontsForPress() = AuthActions.AuthActionTest { request =>
+    RefreshFrontsJob.runStandardFrequency()
+    Ok("Running standard frequency press job but you'll have to check the logs to see if it worked")
+  }
+
+  def queueLowFrequencyFrontsForPress() = AuthActions.AuthActionTest { request =>
+    RefreshFrontsJob.runLowFrequency()
+    Ok("Running low frequency press job but you'll have to check the logs to see if it worked")
+  }
 }
 
