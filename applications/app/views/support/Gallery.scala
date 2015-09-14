b78be9c1ffb12233cfb4ec6a3105770e67861e16
John Duffell <john.duffell@guardian.co.uk>
2015-09-14 12:43:53 +0100

    work out the correct bounding box

--- applications/app/views/support/Gallery.scala
+++ applications/app/views/support/Gallery.scala
@@ -4,6 +4,7 @@ import model.ImageAsset
 
 object Gallery {
 
-  def boundingBox(images: Seq[ImageAsset]): (Int, Int) = (600, 600)// TODO
+  def boundingBox(images: Seq[ImageAsset]): (Int, Int) =
+    (images.map(_.width).max, images.map(_.height).max)
 
 }
