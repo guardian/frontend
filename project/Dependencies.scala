53d4bedcf87086d7cd3b4b33a9f95172c596199f
John Duffell <john.duffell@guardian.co.uk>
2015-09-14 16:35:50 +0100

    upgrade to scalatest 2.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Dependencies {
   val romeModules = "org.rometools" % "rome-modules" % "1.0"
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.11.5" % "test"
   val scalajTime = "org.scalaj" % "scalaj-time_2.10.2" % "0.7"
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.1" % Test
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.5" % Test
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.0.6"
   val seeGuice = "com.tzavellas" % "sse-guice" % "0.7.1"
   val seleniumJava = "org.seleniumhq.selenium" % "selenium-java" % seleniumVersion
