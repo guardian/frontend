30e2391e67e8232380196ca5fc46abaff8fbbc80
Francis Carr <francis.carr@guardian.co.uk>
2015-09-22 15:13:39 +0100

    Use separate compiler options

--- common/app/common/JsMinifier.scala
+++ common/app/common/JsMinifier.scala
@@ -9,14 +9,45 @@ import play.twirl.api.Html
 import scala.collection.concurrent.TrieMap
 import scala.util.Try
 
-
 object JsMinifier {
+
+  val compilerOptions = {
+    val options = new CompilerOptions()
+
+    /* Checks */
+    options.setCheckTypes(true)
+    options.setCheckSuspiciousCode(true)
+    options.setReportMissingOverride(CheckLevel.WARNING)
+    options.setCheckProvides(CheckLevel.WARNING)
+    options.setCheckGlobalNamesLevel(CheckLevel.WARNING)
+    options.setBrokenClosureRequiresLevel(CheckLevel.WARNING)
+    options.setCheckGlobalThisLevel(CheckLevel.WARNING)
+
+    //Aggressive, you need all JS variable defined somewhere for it not to throw (Such as window or navigator)
+    //options.setCheckSymbols(true)
+
+    /* Diagnostic checks */
+    options.setWarningLevel(DiagnosticGroups.ACCESS_CONTROLS, CheckLevel.WARNING)
+    options.setWarningLevel(DiagnosticGroups.LINT_CHECKS, CheckLevel.WARNING)
+    options.setWarningLevel(DiagnosticGroups.DEPRECATED_ANNOTATIONS, CheckLevel.WARNING)
+    options.setWarningLevel(DiagnosticGroups.DEBUGGER_STATEMENT_PRESENT, CheckLevel.WARNING)
+    options.setWarningLevel(DiagnosticGroups.CHECK_REGEXP, CheckLevel.WARNING)
+    options.setWarningLevel(DiagnosticGroups.UNNECESSARY_CASTS, CheckLevel.WARNING)
+    options.setWarningLevel(DiagnosticGroups.INVALID_CASTS, CheckLevel.WARNING)
+    options.setWarningLevel(DiagnosticGroups.CHECK_USELESS_CODE, CheckLevel.WARNING)
+
+    //Aggressive
+    //options.setWarningLevel(DiagnosticGroups.DUPLICATE_VARS, CheckLevel.WARNING)
+    //options.setWarningLevel(DiagnosticGroups.MISSING_PROPERTIES, CheckLevel.WARNING)
+
+    options
+  }
 
   private def compileUnsafe(codeToCompile: String, compilationLevel: CompilationLevel): String = {
     val compiler = new Compiler()
 
-    val compilerOptions: CompilerOptions = new CompilerOptions()
     compilationLevel.setOptionsForCompilationLevel(compilerOptions)
+
 
     val extern: SourceFile = SourceFile.fromCode("extern.js", "")
     val input: SourceFile = SourceFile.fromCode("input.js", codeToCompile)
