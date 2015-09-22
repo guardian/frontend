d9d624a4a8b35feabab27caec8bb2df5492ac580
David Rapson <david.rapson@guardian.co.uk>
2015-09-22 15:54:21 +0100

    Alternate ad-block messages

--- static/src/javascripts/projects/common/modules/commercial/donot-use-adblock.js
+++ static/src/javascripts/projects/common/modules/commercial/donot-use-adblock.js
@@ -1,4 +1,5 @@
 define([
+    'common/utils/_',
     'common/utils/config',
     'common/utils/detect',
     'common/utils/storage',
@@ -9,6 +10,7 @@ define([
     'text!common/views/membership-message.html',
     'common/views/svgs'
 ], function (
+    _,
     config,
     detect,
     storage,
@@ -20,23 +22,42 @@ define([
     svgs
 ) {
     function init() {
-        var alreadyVisted = storage.local.get('alreadyVisited') || 0,
-            adblockLink = 'https://membership.theguardian.com?INTCMP=adb-mv';
+        var alreadyVisted = storage.local.get('alreadyVisited') || 0;
+        var adblockLink = 'https://membership.theguardian.com';
+        var message = _.sample([
+            {
+                id: 'monthly',
+                messageText: 'We notice you\'re using an ad-blocker. Perhaps you\'ll support us another way? Become a Supporter from just £5 per month',
+                linkText: 'Find out more'
+            },
+            {
+                id: 'annual',
+                messageText: 'We notice you\'re using an ad-blocker. Perhaps you\'ll support us another way? Become a Supporter for just £50 per a year',
+                linkText: 'Find out more'
+            },
+            {
+                id: 'weekly',
+                messageText: 'We notice you\'re using an ad-blocker. Perhaps you\'ll support us another way? Become a Supporter for less than £1 per week',
+                linkText: 'Find out more'
+            },
+            {
+                id: 'no-price',
+                messageText: 'We notice you\'re using an ad-blocker. Perhaps you\'ll support us another way?',
+                linkText: 'Become a supporter today'
+            }
+        ]);
 
         if (detect.getBreakpoint() !== 'mobile' && detect.adblockInUse && config.switches.adblock && alreadyVisted > 1) {
-            new Message('adblock', {
+            new Message('adblock-message', {
                 pinOnHide: false,
-                siteMessageLinkName: 'adblock message variant',
+                siteMessageLinkName: 'adblock message variant ' + message.id,
                 siteMessageCloseBtn: 'hide'
-            }).show(template(
-                    messageTemplate,
-                    {
-                        supporterLink: adblockLink,
-                        messageText: 'We notice you\'ve got an ad-blocker switched on. Perhaps you\'d like to support the Guardian another way?',
-                        linkText: 'Become a supporter today',
-                        arrowWhiteRight: svgs('arrowWhiteRight')
-                    }
-                ));
+            }).show(template(messageTemplate, {
+                supporterLink: adblockLink + '?INTCMP=adb-mv-' + message.id,
+                messageText: message.messageText,
+                linkText: message.linkText,
+                arrowWhiteRight: svgs('arrowWhiteRight')
+            }));
         }
     }
 
