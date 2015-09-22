2c0661c5c71cd8d286fccc9f1db9344ef73e9cae
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-09-22 16:24:01 +0100

    Don't Show Ad Slot at Page Top on Scrolling

---
+++ commercial/app/views/commercial/support/StaticPages.scala
@@ -0,0 +1,12 @@
+package views.commercial.support
+
+import common.commercial.StaticPage
+import model.MetaData
+
+object StaticPages {
+
+  val adFreeSurvey: MetaData = new StaticPage {
+    def id: String = "ad-free-survey"
+    def webTitle: String = "Ad-free Survey"
+  }
+}
