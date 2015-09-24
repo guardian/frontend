db9c48b90b8e07231ebc448e83e831f7e281dedc
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-09-24 14:06:28 +0100

    Specify artifact resources to allow fully qualified package name

--- project/Prototypes.scala
+++ project/Prototypes.scala
@@ -107,12 +107,16 @@ trait Prototypes {
     topLevelDirectory in Universal := Some(application),
     concurrentRestrictions in Universal := List(Tags.limit(Tags.All, 1)),
     riffRaffPackageType := (packageBin in Universal).value,
-    riffRaffPackageName := application,
     riffRaffBuildIdentifier := System.getProperty("build.number", "DEV").replaceAll("\"",""),
     riffRaffUploadArtifactBucket := System.getenv().getOrDefault("RIFF_RAFF_ARTIFACT_BUCKET", "aws-frontend-teamcity"),
     riffRaffUploadManifestBucket := System.getenv().getOrDefault("RIFF_RAFF_BUILD_BUCKET", "aws-frontend-teamcity"),
     riffRaffArtifactPublishPath := application,
     riffRaffManifestProjectName := s"dotcom:$application",
+    riffRaffPackageName := s"dotcom:$application",
+    riffRaffArtifactResources := Seq(
+      riffRaffPackageType.value -> s"packages/$application/${riffRaffPackageType.value.getName}",
+      baseDirectory.value / "deploy.json" -> "deploy.json"
+    ),
     artifactName := { (sv: ScalaVersion, module: ModuleID, artifact: Artifact) =>
       artifact.name + "." + artifact.extension
     }
