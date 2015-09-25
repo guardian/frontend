96b04d40d1364505b28fb14022ee9880c80a7aeb
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2015-09-25 17:24:22 +0100

    Refactor commercial-features to make it testable

--- static/src/javascripts/projects/common/modules/commercial/commercial-features.js
+++ static/src/javascripts/projects/common/modules/commercial/commercial-features.js
@@ -1,108 +1,36 @@
 define([
     'common/utils/_',
-    'common/utils/config',
-    'common/modules/commercial/user-ad-preference',
-    'common/modules/user-prefs'
+    'common/modules/commercial/commercial-feature-policies'
 ], function (
     _,
-    config,
-    userAdPreference,
-    userPrefs
+    commercialFeaturePolicies
 ) {
-    var commercialFeatures, featureFilters;
-
-    commercialFeatures =  {
-        dfpAdvertising : true,
-        articleMPUs : true,
-        sliceAdverts : true,
-        popularContentMPU : true,
-        videoPreRolls : true,
-        frontCommercialComponents : true,
-        thirdPartyTags : true,
-        badges : true
-    };
-
-    featureFilters = {
-        // Each filter encapsulates a feature policy - a condition where it is implemented,
-        // and the commercial features it disables.
-
-        sensitiveContent : function () {
-            // Avoid inappropriate adverts on pages with content of a sensitive nature
-            if (config.page.shouldHideAdverts || config.page.section === 'childrens-books-site') {
-                disableAllFeatures();
-            }
-        },
-        adfreeExperience : function () {
-            // Some paying users may opt out of _some_ advertising
-            if (userAdPreference.hideAds) {
-                commercialFeatures.articleMPUs = false;
-                commercialFeatures.sliceAdverts = false;
-                commercialFeatures.popularContentMPU = false;
-                commercialFeatures.videoPreRolls = false;
-            }
-        },
-        sslExclusions : function () {
-            if (config.page.isSSL && config.page.section !== 'admin') {
-                disableAllFeatures();
-            }
-        },
-        uriOptOuts : function () {
-            // Allow performance tests to exclude advertising
-            if (window.location.hash.match(/[#&]noads(&.*)?$/)) {
-                disableAllFeatures();
-            }
-        },
-        userPrefs : function () {
-            if (userPrefs.isOff('adverts')) {
-                disableAllFeatures();
-            }
-        },
-        switchboardSettings : function () {
-            if (!config.switches.videoAdverts) {
-                commercialFeatures.videoPreRolls = false;
-            }
-            if (!config.switches.standardAdverts) {
-                commercialFeatures.articleMPUs = false;
-                commercialFeatures.sliceAdverts = false;
-            }
-            if (!config.switches.commercialComponents) {
-                commercialFeatures.frontCommercialComponents = false;
-            }
-            if (!config.switches.sponsored) {
-                commercialFeatures.badges = false;
-            }
-        },
-        contentSpecific : function () {
-            if (config.page.contentType !== 'Article' && !config.page.isLiveBlog) {
-                commercialFeatures.articleMPUs = false;
-            }
-            if (!config.page.isFront) {
-                commercialFeatures.frontCommercialComponents = false;
-            }
-            if (config.page.contentType === 'Identity' || config.page.section === 'identity') {
-                commercialFeatures.thirdPartyTags = false;
-            }
-        },
-        previewPage : function () {
-            if (config.page.isPreview) {
-                commercialFeatures.videoPreRolls = false;
-            }
-        }
-    };
-
-    function applyFeatureFilters() {
-        _.forOwn(featureFilters, function applyFilter(filterFunction) {
-            filterFunction();
-        });
+    function init() {
+        var commercialFeatures = getFeatureSwitches();
+        commercialFeatures._init = init; // Exposed for testing
+        return commercialFeatures;
     }
 
-    function disableAllFeatures() {
-        _.forOwn(commercialFeatures, function (featureState, featureKey) {
-            commercialFeatures[featureKey] = false;
+    function getFeatureSwitches () {
+        // Take the results of all our commercial content policies and smush them together
+
+        var activeSwitches = {};
+        var switchesForPolicies = commercialFeaturePolicies.getPolicySwitches();
+
+        _.forOwn(switchesForPolicies, function mergeSwitches(policySwitchSet) {
+            _.forOwn(policySwitchSet, function (switchValue, switchKey) {
+                var activeValue = activeSwitches[switchKey];
+                if (activeValue !== false) {
+                    // A policy can only override a true or undefined switch
+                    // This gives any policy the ability to veto a commercial feature
+                    activeSwitches[switchKey] = switchValue;
+                }
+            });
         });
+
+        return activeSwitches;
     }
 
-    applyFeatureFilters();
-    return commercialFeatures;
+    return init();
 });
 
