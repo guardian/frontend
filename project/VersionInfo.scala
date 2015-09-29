c7d252c0873b164781c43b15d335a2855d94d602
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-09-29 19:41:58 +0100

    Use git to find version info

--- project/VersionInfo.scala
+++ project/VersionInfo.scala
@@ -1,34 +1,55 @@
 package com.gu.versioninfo
 
 import java.net.InetAddress
-import java.util.Date
+import org.joda.time.{DateTimeZone, DateTime}
 import sbt._
 import Keys._
-import java.lang.System
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
 object VersionInfo extends Plugin {
-
-  implicit def string2Dequote(s: String) = new {
-    lazy val dequote = s.replace("\"", "")
-  }
 
   val branch = SettingKey[String]("version-branch")
   val buildNumber = SettingKey[String]("version-build-number")
   val vcsNumber = SettingKey[String]("version-vcs-number")
 
+  private def isValidRepo(repo: java.io.File) = {
+    val gitRepo = new FileRepositoryBuilder().findGitDir(repo)
+    val hasExpectedRemote = gitRepo.build.getConfig.getString("remote", "origin", "url") == "git@github.com:guardian/frontend.git"
+    gitRepo.build.close()
+    hasExpectedRemote
+  }
+
   override val settings = Seq(
-    buildNumber := System.getProperty("build.number", "DEV"),
-    branch := System.getProperty("build.vcs.branch", "DEV"),
-    vcsNumber := System.getProperty("build.vcs.number", "DEV"),
+    buildNumber := System.getenv().getOrDefault("BUILD_NUMBER", "DEV"),
+    branch <<= baseDirectory( baseDir => {
+      if (isValidRepo(baseDir)) {
+        val gitRepo = new FileRepositoryBuilder().findGitDir(baseDir)
+        val branchName = gitRepo.build.getBranch
+        gitRepo.build.close()
+        branchName
+      } else {
+        "DEV"
+      }
+    }),
+    vcsNumber <<= baseDirectory( baseDir => {
+      if (isValidRepo(baseDir)) {
+        val gitRepo = new FileRepositoryBuilder().findGitDir(baseDir)
+        val commitHash = gitRepo.build.resolve("HEAD").getName
+        gitRepo.build.close()
+        commitHash
+      } else {
+        "DEV"
+      }
+    }),
     resourceGenerators in Compile <+= (resourceManaged in Compile, branch, buildNumber, vcsNumber, streams) map buildFile
   )
 
   def buildFile(outDir: File, branch: String, buildNum: String, vcsNum: String, s: TaskStreams) = {
     val versionInfo = Map(
-      "Revision" -> vcsNum.dequote.trim,
-      "Branch" -> branch.dequote.trim,
-      "Build" -> buildNum.dequote.trim,
-      "Date" -> new Date().toString,
+      "Revision" -> vcsNum,
+      "Branch" -> branch,
+      "Build" -> buildNum.replace("\"", "").trim,
+      "Date" -> new DateTime(DateTimeZone.UTC).toString(),
       "Built-By" -> System.getProperty("user.name", "<unknown>"),
       "Built-On" -> InetAddress.getLocalHost.getHostName)
 
