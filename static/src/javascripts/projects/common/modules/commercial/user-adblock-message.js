d2a4d6560e10d5a22b80fb301e42647e2153c7bd
Zofia <zofia.korcz@guardian.co.uk>
2015-10-08 11:19:30 +0100

    first ajax call test

--- static/src/javascripts/projects/common/modules/commercial/user-adblock-message.js
+++ static/src/javascripts/projects/common/modules/commercial/user-adblock-message.js
@@ -1,11 +1,29 @@
 define([
+    'common/utils/ajax-promise',
     'common/utils/cookies'
 ], function (
+    ajaxPromise,
     cookies
 ) {
     //right now we assume that the user who is a member (any of 3 paid tiers) and do not want to see ads, also won't see adblock messages
     function isUserMember() {
-        return cookies.get('gu_adfree_user') ? true : false;
+        var isMemberCookie = cookies.get('gu_adfree_user'),
+            promise;
+
+        if (!isMemberCookie) {
+            ajaxPromise({
+                url: 'https://members-data-api.theguardian.com/user-attributes/me/adfree',
+                type: 'json',
+                method: 'get',
+                crossOrigin: true/*,
+                data: queryParams */
+            }).then(function (resp) {
+                console.log(resp);
+                isMemberCookie = cookies.add('gu_adfree_user', resp.adfree);
+            });
+        }
+
+        return isMemberCookie ? true : false;
     }
 
     return {
