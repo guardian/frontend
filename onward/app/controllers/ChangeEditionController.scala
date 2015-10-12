c3434944f37b37f010b2355d02dad00f4ee6a1c3
Grant Klopper <grant.klopper@theguardian.com>
2015-10-12 10:06:57 +0100

    No need for index

--- onward/app/controllers/ChangeEditionController.scala
+++ onward/app/controllers/ChangeEditionController.scala
@@ -7,7 +7,6 @@ import play.api.mvc._
 object ChangeEditionController extends Controller with PreferenceController {
   def render(editionId: String) = Action { implicit request =>
     NoCache(Edition.byId(editionId).map{ edition =>
-      val index = Edition.all.indexOf(edition)
       val home = edition.homePagePath
       // This INTCMP parameter is simply to cachebust the local cache of browsers
       // For people who switch a lot of editions (mixing going to the page directly and using the edition switcher)
