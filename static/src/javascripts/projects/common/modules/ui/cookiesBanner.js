d73c49c66a5ca3ccd176dd90969beaf276722b12
Justin Pinner <justinpinner@gmail.com>
2015-10-13 09:52:19 +0100

    Lint. Part 2.

--- static/src/javascripts/projects/common/modules/ui/cookiesBanner.js
+++ static/src/javascripts/projects/common/modules/ui/cookiesBanner.js
@@ -28,15 +28,15 @@ define([
         cookieVal = cookies.get(COOKIE_IMPRESSION_KEY),
         impressions = cookieVal && !isNaN(cookieVal) ? parseInt(cookieVal, 10) : 0,
         link = 'https://www.theguardian.com/info/cookies',
-        txt = '<p class="cookie-message__copy">Welcome to the Guardian. This site uses cookies, read our policy <a href="<%=link%>" class="cookie-message__link">here</a>.</p>',
+        txt = '<p class="cookie-message__copy">Welcome to the Guardian. This site uses cookies, read our policy <a href="' + link + '" class="cookie-message__link">here</a>.</p>',
         opts = {important: true};
 
     function canShow() {
-        return document.hasOwnProperty("eu-cookie-message") && impressions == 0;
+        return document.hasOwnProperty('eu-cookie-message') && impressions == 0;
     }
 
     function showMessage() {
-        var msg = new Message("cookies");
+        var msg = new Message('cookies');
         msg.show(txt, opts);
         cookies.add(COOKIE_IMPRESSION_KEY, impressions + 1);
     }
