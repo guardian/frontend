47622901185fa2e73b4c1edf04aba711f27f5541
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-10-15 14:14:53 +0100

    Extend Fixed Top Ad Slot Size Switch
    Need more time to test.

--- common/app/conf/switches/CommercialSwitches.scala
+++ common/app/conf/switches/CommercialSwitches.scala
@@ -226,7 +226,7 @@ trait CommercialSwitches {
     "fixed-top-above-nav",
     "Fixes size of top-above-nav ad slot on fronts.",
     safeState = Off,
-    sellByDate = new LocalDate(2015, 10, 21),
+    sellByDate = new LocalDate(2015, 11, 11),
     exposeClientSide = false
   )
 
