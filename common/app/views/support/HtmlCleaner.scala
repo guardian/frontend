f041eb6f9be95e8cbfec0592d2d6eadd73d05cc0
Sam Morris <mail@sammorrisdesign.co.uk>
2015-10-20 14:54:43 +0100

    Allow I to be dropped as a cap

--- common/app/views/support/HtmlCleaner.scala
+++ common/app/views/support/HtmlCleaner.scala
@@ -484,7 +484,7 @@ case class DropCaps(isFeature: Boolean) extends HtmlCleaner {
 
   private def setDropCap(p: Element): String = {
     val html = p.html
-    if ( html.length > 200 && html.matches("^[\"a-hj-zA-HJ-Z].*") && html.split("\\s+").head.length >= 3 ) {
+    if ( html.length > 200 && html.matches("^[\"a-zA-Z].*") && html.split("\\s+").head.length >= 3 ) {
       val classes = if (html.length > 325) "drop-cap drop-cap--wide" else "drop-cap"
       s"""<span class="${classes}"><span class="drop-cap__inner">${html.head}</span></span>${html.tail}"""
     } else {
