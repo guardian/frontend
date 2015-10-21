344f76140bba3149bb8dd8ca840cd7d05ede8cfe
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-10-21 11:35:05 +0100

    Always Show 10 Items in Most Popular Container

--- common/app/views/support/MostPopular.scala
+++ common/app/views/support/MostPopular.scala
@@ -1,14 +1,8 @@
 package views.support
 
 import layout.FaciaContainer
-import play.api.mvc.RequestHeader
 
 object MostPopular extends implicits.Requests {
-
-  def numItemsToShow(container: FaciaContainer)(implicit request: RequestHeader): Int = {
-    if (container.commercialOptions.omitMPU) 9
-    else 10
-  }
 
   def showMPU(maybeContainer: Option[FaciaContainer]): Boolean = {
     !maybeContainer.exists(_.commercialOptions.omitMPU)
