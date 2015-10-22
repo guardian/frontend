e6bc8e36ca01d7ea62260bdbc2b97b92790b701b
Oliver Joseph Ash <oliverjash@gmail.com>
2015-10-22 17:17:31 +0100

    Add shrinkwrap cleaner and npm install docs
    `npm shrinkwrap` spits out different metadata depending on the npm version, which makes our diffs much too difficult to read properly.
    
    Browsing [the issue](https://github.com/npm/npm/issues/3581) I found [a little script](https://github.com/angular/angular.js/blob/master/scripts/npm/clean-shrinkwrap.js) we can use to tidy up the shrinkwrap and keep it consistent.
    
    I've also tried to document the npm install process.

---
+++ dev/clean-shrinkwrap.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+// Copied from https://github.com/angular/angular.js/blob/master/scripts/npm/clean-shrinkwrap.js
+
+/**
+ * this script is just a temporary solution to deal with the issue of npm outputting the npm
+ * shrinkwrap file in an unstable manner.
+ *
+ * See: https://github.com/npm/npm/issues/3581
+ */
+
+var _ = require('lodash');
+var sorted = require('sorted-object');
+var fs = require('fs');
+var path = require('path');
+
+
+function cleanModule(module, name) {
+
+  // keep `resolve` properties for git dependencies, delete otherwise
+  delete module.from;
+  if (!(module.resolved && module.resolved.match(/^git(\+[a-z]+)?:\/\//))) {
+    delete module.resolved;
+  }
+
+  _.forEach(module.dependencies, function(mod, name) {
+    cleanModule(mod, name);
+  });
+}
+
+
+console.log('Reading npm-shrinkwrap.json');
+var shrinkwrapPath = path.join(__dirname, '..', 'npm-shrinkwrap.json');
+var shrinkwrap = require(shrinkwrapPath);
+
+console.log('Cleaning shrinkwrap object');
+cleanModule(shrinkwrap, shrinkwrap.name);
+
+console.log('Writing cleaned to', shrinkwrapPath);
+fs.writeFileSync(shrinkwrapPath, JSON.stringify(sorted(shrinkwrap), null, 2) + "\n");
