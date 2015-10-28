5ba4841f08a67c8a9c8694fb7be2904c18d15f39
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-10-28 16:36:07 +0000

    Redirects for short urls should pass through campaign code

--- applications/app/controllers/ShortUrlsCampaignController.scala
+++ applications/app/controllers/ShortUrlsCampaignController.scala
@@ -1,33 +1,13 @@
 package controllers
 
-import common.{LinkTo, Logging}
+import common.{ExecutionContexts, LinkTo, Logging}
 import common.`package`._
+import campaigns.ShortCampaignCodes
 import conf.LiveContentApi
 import model.Cached
 import play.api.mvc.{RequestHeader, Action, Controller}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
-object ShortUrlsCampaignController extends Controller with Logging {
-
-  val campaigns = Map (
-    "iw" -> "twt_ipd",
-    "wc" -> "twt_wc",
-    "tf" -> "twt_fd",
-    "fb" -> "fb_gu",
-    "fo" -> "fb_ot",
-    "us" -> "fb_us",
-    "au" -> "soc_567",
-    "tw" -> "twt_gu",
-    "at" -> "twt_atn",
-    "sfb"-> "share_btn_fb",
-    "ip" -> "twt_iph",
-    "stw" -> "share_btn_tw",
-    "swa" -> "share_btn_wa",
-    "em" -> "email",
-    "sgp" -> "share_btn_gp",
-    "sbl" -> "share_btn_link"
-  )
+object ShortUrlsCampaignController extends Controller with Logging with ExecutionContexts {
 
   def redirectShortUrl(shortUrl: String) = Action.async { implicit request =>
     log.info(s"Redirecting short url $shortUrl")
@@ -42,10 +22,9 @@ object ShortUrlsCampaignController extends Controller with Logging {
     }.recover(convertApiExceptionsWithoutEither).map(Cached(60))
   }
 
-
   def fetchCampaignAndRedirectShortCode(shortUrl: String, campaignCode: String) = Action.async { implicit request =>
     log.info(s"Fetching campaign for $campaignCode and redirect short url")
-    val queryString = request.queryString ++ campaigns.get(campaignCode).map(code => ("CMP", Seq(code)))
+    val queryString = request.queryString ++ ShortCampaignCodes.makeQueryParameter(campaignCode)
     redirectUrl(shortUrl, queryString)
   }
 }
