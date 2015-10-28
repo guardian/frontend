81b4d3fba89d5b12cc9fd68b7fa6f9becdc882ee
Francis Carr <francis.carr@guardian.co.uk>
2015-10-28 11:15:35 +0000

    Change switch text

--- common/app/conf/switches/PerformanceSwitches.scala
+++ common/app/conf/switches/PerformanceSwitches.scala
@@ -5,10 +5,10 @@ import org.joda.time.LocalDate
 
 trait PerformanceSwitches {
 
-  val ClosureCompilerStandardOptimisation = Switch(
+  val InlineJSStandardOptimisation = Switch(
     "Performance",
-    "closure-compiler-standard",
-    "If this switch is on, the closure compiler will use standard optimisation instead of whitespace only",
+    "inline-standard-optimisation",
+    "If this switch is on, the inline JS will be compressed using closure compiler's standard optimisation instead of whitespace only",
     safeState = On,
     sellByDate = never,
     exposeClientSide = false
