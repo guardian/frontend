45e2fda5270f152c10a0784ef8c35aeaa8601da2
John Duffell <john.duffell@guardian.co.uk>
2015-10-29 18:01:40 +0000

    remove redundant code

--- static/src/javascripts/projects/common/modules/article/replicated-links.js
+++ static/src/javascripts/projects/common/modules/article/replicated-links.js
@@ -7,11 +7,7 @@ define([
     'common/utils/ajax-promise',
     'common/utils/config',
     'common/utils/detect',
-    'common/utils/mediator',
-    'common/utils/template',
-    'common/modules/article/spacefinder',
-    'common/modules/ui/images',
-    'text!common/views/content/richLinkTag.html'
+    'common/utils/mediator'
 ], function (
     fastdom,
     qwery,
@@ -21,11 +17,7 @@ define([
     ajax,
     config,
     detect,
-    mediator,
-    template,
-    spacefinder,
-    imagesModule,
-    richLinkTagTmpl
+    mediator
 ) {
     function upgradeLink(el) {
         var a = $('a', el),
@@ -45,51 +37,6 @@ define([
                         mediator.emit('replicated-link:loaded', el);
                     });
                 }
-            });
-        } else {
-            return Promise.resolve(null);
-        }
-    }
-
-    function getSpacefinderRules() {
-        return {
-            minAbove: 200,
-            minBelow: 250,
-            clearContentMeta: 50,
-            selectors: {
-                ' > h2': {minAbove: detect.getBreakpoint() === 'mobile' ? 20 : 0, minBelow: 200},
-                ' > *:not(p):not(h2):not(blockquote)': {minAbove: 35, minBelow: 300},
-                ' .ad-slot': {minAbove: 150, minBelow: 200},
-                ' .element-rich-link': {minAbove: 500, minBelow: 500}
-            }
-        };
-    }
-
-    function insertTagRichLink() {
-        var richLinkHrefs = $('.element-rich-link a')
-                .map(function (el) { return $(el).attr('href'); }),
-            testIfDuplicate = function (richLinkHref) {
-                // Tag-targeted rich links can be absolute
-                return _.contains(config.page.richLink, richLinkHref);
-            },
-            isDuplicate = richLinkHrefs.some(testIfDuplicate),
-            isSensitive = config.page.shouldHideAdverts || !config.page.showRelatedContent;
-
-        if (config.page.richLink && config.page.richLink.indexOf(config.page.pageId) === -1
-            && !isSensitive && !isDuplicate) {
-
-            return spacefinder.getParaWithSpace(getSpacefinderRules()).then(function (space) {
-                return new Promise(function (resolve) {
-                    if (space) {
-                        fastdom.write(function () {
-                            var $el = $.create(template(richLinkTagTmpl, {href: config.page.richLink}));
-                            $el.insertBefore(space);
-                            resolve(upgradeLink($el[0]));
-                        });
-                    } else {
-                        resolve(null);
-                    }
-                });
             });
         } else {
             return Promise.resolve(null);
