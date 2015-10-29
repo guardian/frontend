101bc5b569a22e5921bd48ed244b7911cf082731
sammorrisdesign <mail@sammorrisdesign.co.uk>
2015-10-29 14:32:50 +0000

    JS LINT fun

--- static/src/javascripts/projects/common/modules/article/chapters.js
+++ static/src/javascripts/projects/common/modules/article/chapters.js
@@ -6,18 +6,18 @@ define([
     $
 ) {
     return {
-        init: function() {
+        init: function () {
             this.bindEvents();
         },
 
-        bindEvents: function() {
+        bindEvents: function () {
             bean.on(document.body, 'click', '.js-toggle-chapters', function (e) {
                 e.preventDefault();
                 this.toggleChapters();
             }.bind(this));
         },
 
-        toggleChapters: function() {
+        toggleChapters: function () {
             $('.chapters').toggleClass('is-expanded');
         }
     };
