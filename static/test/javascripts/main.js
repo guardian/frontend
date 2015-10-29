faaff84a9613642b26e16496439ff469e899b400
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-10-29 16:49:05 +0000

    Remove dev-config to simplify Dev, Prod and Test

--- static/test/javascripts/main.js
+++ static/test/javascripts/main.js
@@ -11,21 +11,46 @@ requirejs.config({
     // Karma serves files from '/base'
     baseUrl: '/base/static/src/javascripts',
     paths: {
+        admin:        'projects/admin',
+        common:       'projects/common',
+        facia:        'projects/facia',
+        membership:   'projects/membership',
+        bean:         'components/bean/bean',
+        bonzo:        'components/bonzo/bonzo',
+        react:        'components/react/react',
+        enhancer:     'components/enhancer/enhancer',
+        EventEmitter: 'components/eventEmitter/EventEmitter',
+        fastclick:    'components/fastclick/fastclick',
+        fastdom:      'components/fastdom/index',
+        fence:        'components/fence/fence',
+        lodash:       'components/lodash',
+        picturefill:  'projects/common/utils/picturefill',
+        Promise:      'components/when/Promise',
+        qwery:        'components/qwery/qwery',
+        raven:        'components/raven-js/raven',
+        reqwest:      'components/reqwest/reqwest',
         // Test specific paths
-        omniture:     '/base/static/src/javascripts/vendor/omniture',
-        stripe:       '/base/static/src/javascripts/vendor/stripe/stripe.min',
+        omniture:     'vendor/omniture',
+        stripe:       'vendor/stripe/stripe.min',
         squire:       '/base/static/test/javascripts/components/squire/src/Squire',
         fixtures:     '/base/static/test/javascripts/fixtures',
-        helpers:      '/base/static/test/javascripts/helpers'
+        helpers:      '/base/static/test/javascripts/helpers',
+        svgs:         '../inline-svgs',
+        // plugins
+        text:         'components/requirejs-text/text',
+        inlineSvg:    'projects/common/utils/inlineSvg'
+    },
+    shim: {
+        googletag: {
+            exports: 'googletag'
+        }
     }
 });
 
-require(['dev-config'], function () {
-    require(['Promise', 'common/utils/to-array'], function (Promise, toArray) {
-        require(tests, function () {
-            Promise.all(toArray(arguments)).then(function () {
-                window.__karma__.start();
-            });
+require(['Promise', 'common/utils/to-array'], function (Promise, toArray) {
+    require(tests, function () {
+        Promise.all(toArray(arguments)).then(function () {
+            window.__karma__.start();
         });
     });
 });
