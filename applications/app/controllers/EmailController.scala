04ffdeb14c2799d9bbe0cbd3d8123f29047176ee
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2015-10-30 15:13:37 +0000

    Set up inital handling of post to email endpoint

--- applications/app/controllers/EmailController.scala
+++ applications/app/controllers/EmailController.scala
@@ -1,9 +1,16 @@
 package controllers
 
-import common._
+import common.ExecutionContexts
 import model._
-import play.api.mvc.{Action, Controller}
+import play.api.Play.current
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.ws.WS
+import play.api.libs.json._
+import play.api.mvc.{Action, Controller, RequestHeader, Result}
 import model.MetaData
+
+import scala.concurrent.Future
 
 class emailLandingPage() extends MetaData {
   lazy val id: String = "email-landing-page"
@@ -15,10 +22,38 @@ class emailLandingPage() extends MetaData {
 case class EmailPage (interactive: Interactive, related: RelatedContent)
 
 object EmailController extends Controller with ExecutionContexts {
+  val postUrl = "http://requestb.in/1jvf07s1"
 
   def renderPage() = Action { implicit request =>
     val emailLandingHtml = views.html.emailLanding(new emailLandingPage())
     Ok(emailLandingHtml)
   }
 
+  def submit() = Action.async { implicit request =>
+    val form = Form(
+      mapping(
+        "email" -> nonEmptyText
+      )(EmailSubmission.apply)(EmailSubmission.unapply)
+    )
+
+    form.bindFromRequest.fold(
+      _ => Future(Ok(s"error")),
+
+      form => {
+        val json = Json.obj(
+          "address" -> form.address,
+          "listId" -> form.listId
+        )
+
+        WS.url(postUrl).post(json).map(res => res.status match {
+          case 200 => Ok(s"OK: $res")
+          case _ => Ok(s"Didn't work: $res")
+        })
+      }
+    )
+  }
+}
+
+case class EmailSubmission(address: String) {
+  val listId = "xxx"
 }
