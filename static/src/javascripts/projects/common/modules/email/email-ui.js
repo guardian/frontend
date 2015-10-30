40c39beff0f89755c43eb81c61e241d841846d53
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2015-10-30 12:00:23 +0000

    Fix lint errors

--- static/src/javascripts/projects/common/modules/email/email-ui.js
+++ static/src/javascripts/projects/common/modules/email/email-ui.js
@@ -9,19 +9,19 @@ define([
 ) {
     return {
         init: function () {
-            $('.js-email-sub__inline-label').each(function(el){
+            $('.js-email-sub__inline-label').each(function (el) {
                 var $el = $(el),
                     $input = $('.js-email-sub__text-input', el),
                     $label = $('.js-email-sub__label', el);
 
                 $el.addClass('email-sub__inline-label--enabled');
 
-                bean.on($input[0], 'focus blur', function(){
+                bean.on($input[0], 'focus blur', function () {
                     if ($input.val() === '') {
                         $label.toggleClass('email-sub__label--is-hidden');
                     }
                 });
             });
         }
-    }
+    };
 });
