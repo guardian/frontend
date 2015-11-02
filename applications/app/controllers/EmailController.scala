80d2f6329625a2de536134a11c788f6c241a9397
Sam Desborough <sam.desborough@gmail.com>
2015-11-02 14:49:47 +0000

    Tidy up Play form stuff

--- applications/app/controllers/EmailController.scala
+++ applications/app/controllers/EmailController.scala
@@ -4,15 +4,16 @@ import common.ExecutionContexts
 import model._
 import play.api.Play.current
 import play.api.data._
+import play.api.data.validation.Constraints.emailAddress
 import play.api.data.Forms._
 import play.api.libs.ws.WS
 import play.api.libs.json._
-import play.api.mvc.{Action, Controller, RequestHeader, Result}
+import play.api.mvc.{Action, Controller}
 import model.MetaData
 
 import scala.concurrent.Future
 
-class emailLandingPage() extends MetaData {
+object emailLandingPage extends MetaData {
   lazy val id: String = "email-landing-page"
   lazy val section: String = ""
   lazy val analyticsName: String = id
@@ -21,38 +22,40 @@ class emailLandingPage() extends MetaData {
 
 case class EmailPage (interactive: Interactive, related: RelatedContent)
 
-case class EmailSubmission(email: String)
+case class EmailForm(email: String)
+
+object EmailForm {
+  val postUrl = "https://3b1d4pkpvi.execute-api.eu-west-1.amazonaws.com/dev/email"
+  val listId  = 12345
+
+  def submit(form: EmailForm) = {
+    WS.url(postUrl).post(Json.obj(
+      "email"  -> form.email,
+      "listId" -> listId
+    ))
+  }
+}
 
 object EmailController extends Controller with ExecutionContexts {
-  val listId  = 12345
-  val postUrl = "https://3b1d4pkpvi.execute-api.eu-west-1.amazonaws.com/dev/email"
+
+  val emailForm: Form[EmailForm] = Form(
+    mapping(
+      "email" -> nonEmptyText.verifying(emailAddress)
+    )(EmailForm.apply)(EmailForm.unapply)
+  )
 
   def renderPage() = Action { implicit request =>
-    val emailLandingHtml = views.html.emailLanding(new emailLandingPage())
-    Ok(emailLandingHtml)
+    Ok(views.html.emailLanding(emailLandingPage))
   }
 
   def submit() = Action.async { implicit request =>
-    val form = Form(
-      mapping(
-        "email" -> nonEmptyText
-      )(EmailSubmission.apply)(EmailSubmission.unapply)
-    )
+    emailForm.bindFromRequest.fold(
+      formWithErrors => Future(InternalServerError("Invalid email address")),
 
-    form.bindFromRequest.fold(
-      err => Future(Ok(s"$err")),
-
-      form => {
-        val json = Json.obj(
-          "email"  -> form.email,
-          "listId" -> listId
-        )
-
-        WS.url(postUrl).post(json).map(res => res.status match {
-          case 200 => Ok(s"OK: $res")
-          case _   => InternalServerError(s"${res.json}")
-        })
-      }
+      form => EmailForm.submit(form).map(res => res.status match {
+        case 200 => Ok(s"OK: $res")
+        case _   => InternalServerError(s"${res.json}")
+      })
     )
   }
 }
