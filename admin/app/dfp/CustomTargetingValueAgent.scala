053b7052a7811bc254e64717eac0778ce949d076
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-11-06 12:13:36 +0000

    Refresh DFP Targeting Value Agent with Recent Data Only
    So that it can be refreshed more often without fetching too much data.

--- admin/app/dfp/CustomTargetingValueAgent.scala
+++ admin/app/dfp/CustomTargetingValueAgent.scala
@@ -1,16 +1,36 @@
 package dfp
 
 import com.google.api.ads.dfp.axis.utils.v201508.StatementBuilder
+import common.Logging
+import org.joda.time.DateTime
 
 import scala.util.Try
 
-object CustomTargetingValueAgent extends DataAgent[Long, String] {
+object CustomTargetingValueAgent extends DataAgent[Long, String] with Logging {
 
-  override def loadFreshData() = Try {
-    DfpServiceRegistry().fold(Map[Long, String]()) { serviceRegistry =>
-      val values = DfpApiWrapper.fetchCustomTargetingValues(serviceRegistry, new StatementBuilder())
-      values.map { v => v.getId.longValue() -> v.getName}.toMap
+  private def targetingValues(stmtBuilder: StatementBuilder): Try[Map[Long, String]] = Try {
+    val targetValues = DfpServiceRegistry().fold(Map[Long, String]()) { serviceRegistry =>
+      val values = DfpApiWrapper.fetchCustomTargetingValues(serviceRegistry, stmtBuilder)
+      values.map { v => v.getId.longValue() -> v.getName }.toMap
     }
+
+    log.info(s"Fetched ${targetValues.size} targeting values: $targetValues")
+
+    targetValues
   }
 
+  override def loadFreshData: Try[Map[Long, String]] = {
+    log.info(s"Fetching all targeting values ...")
+    targetingValues(new StatementBuilder())
+  }
+
+  override def dataModifiedSince(threshold: DateTime): Try[Map[Long, String]] = {
+    log.info(s"Fetching targeting values modified since $threshold ...")
+
+    targetingValues(
+      new StatementBuilder()
+        .where("lastModifiedDateTime > :threshold")
+        .withBindVariableValue("threshold", threshold.getMillis)
+    )
+  }
 }
