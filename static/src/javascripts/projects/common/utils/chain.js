9f1e014cb5d16c6fb8458a943a91e7947b3dd5ce
Oliver Joseph Ash <oliverjash@gmail.com>
2015-11-09 12:00:51 +0000

    Use prototype

--- static/src/javascripts/projects/common/utils/chain.js
+++ static/src/javascripts/projects/common/utils/chain.js
@@ -3,52 +3,73 @@ define([
 ], function (
     partialRight
 ) {
-    return function chain(value) {
-        // We make a new chainable with each operation to prevent mutations and
-        // thus allow multiple usages of a given chainable.
-        var makeChainable = function (value) {
-            var chainable = {
-                and: function () {
-                    // Spread
-                    var fn = partialRight.apply(null, arguments);
-                    var newValue = fn(value);
-                    return makeChainable(newValue);
-                },
-                value: function () { return value; },
-                // Override prototype method
-                valueOf: function () { return this.value(); }
-            };
-            var selfReturningArrayMethods = [
-                'concat',
-                'join',
-                'reverse',
-                'sort'
-            ];
-            var nonSelfReturningArrayMethods = [
-                'slice',
-                'shift',
-                'pop',
-                'push',
-                'splice',
-                'unshift'
-            ];
-            selfReturningArrayMethods.forEach(function (methodName) {
-                chainable[methodName] = function () {
-                    var args = Array.prototype.slice.call(arguments);
-                    var newValue = Array.prototype[methodName].apply(value, args);
-                    return makeChainable(newValue);
-                };
-            });
-            nonSelfReturningArrayMethods.forEach(function (methodName) {
-                chainable[methodName] = function () {
-                    var args = Array.prototype.slice.call(arguments);
-                    Array.prototype[methodName].apply(value, args);
-                    return makeChainable(value);
-                };
-            });
-            return chainable;
-        };
+    // We make a new chainable with each operation to prevent mutations and
+    // thus allow multiple usages of a given chainable.
+    /* jscs:disable disallowDanglingUnderscores */
 
-        return makeChainable(value);
+    // Chainable prototype
+    var Chainable = {
+        setValue: function (value) {
+            this.__value = value;
+        },
+        and: function () {
+            // Spread
+            var fn = partialRight.apply(null, arguments);
+            var newValue = fn(this.value());
+            return makeChainable(newValue);
+        },
+        value: function () { return this.__value; },
+        // Override prototype method
+        valueOf: function () { return this.value(); }
     };
+    /* jscs:enable disallowDanglingUnderscores */
+
+    // Add array methods to chainable
+
+    var immutableArrayMethods = [
+        'concat',
+        'join',
+        'reverse',
+        'sort'
+    ];
+    var mutableArrayMethods = [
+        'slice',
+        'shift',
+        'pop',
+        'push',
+        'splice',
+        'unshift'
+    ];
+    immutableArrayMethods.forEach(function (methodName) {
+        Chainable[methodName] = function () {
+            var args = Array.prototype.slice.call(arguments);
+            var newValue = Array.prototype[methodName].apply(this.value(), args);
+            return makeChainable(newValue);
+        };
+    });
+    mutableArrayMethods.forEach(function (methodName) {
+        Chainable[methodName] = function () {
+            var args = Array.prototype.slice.call(arguments);
+            Array.prototype[methodName].apply(this.value(), args);
+            return makeChainable(this.value());
+        };
+    });
+
+    var createObject = function (prototype) {
+        if (Object.create) {
+            return Object.create(prototype);
+        } else {
+            var F = function () {};
+            F.prototype = prototype;
+            return new F();
+        }
+    };
+
+    var makeChainable = function (value) {
+        var chainable = createObject(Chainable);
+        chainable.setValue(value);
+        return chainable;
+    };
+
+    return makeChainable;
 });
