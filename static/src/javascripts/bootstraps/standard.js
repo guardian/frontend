cec3a93ee51c88efc6443fdfcc995ff7a5dd6a18
Alex Sanders <a@snd.rs>
2015-11-10 12:30:58 +0000

    have a little definition

--- static/src/javascripts/bootstraps/standard.js
+++ static/src/javascripts/bootstraps/standard.js
@@ -1,7 +1,7 @@
 // This file is intended to be downloaded and run ASAP on all pages by all
 // readers.
 //
-// While it's ok to run code from here that requires specific features, it
+// While it's ok to run code from here that requires specific host capabilities, it
 // should manage failing gracefully by itself.
 //
 // Assume *nothing* about the host...
@@ -9,4 +9,8 @@
 // This also means you should think *very hard* before adding modules to it,
 // in particular 3rd party modules.
 //
-// For this file, performance and reach should take priority over *anything*…
+// For this file, performance and breadth of support should take priority over *anything*…
+
+define([], function () {
+
+})
