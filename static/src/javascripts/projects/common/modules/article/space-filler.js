9244af30d89b7c90c453990244229d5232903751
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2015-11-10 11:00:50 +0000

    Add a 'space filler' that enqueues the various asynchronous DOM actions involved in using spacefinder and injecting content.

---
+++ static/src/javascripts/projects/common/modules/article/space-filler.js
@@ -0,0 +1,59 @@
+/* jscs:disable disallowDanglingUnderscores */
+define([
+    'common/utils/$',
+    'fastdom',
+    'Promise',
+    'raven',
+    'common/modules/article/spacefinder'
+], function (
+    $,
+    fastdom,
+    Promise,
+    raven,
+    spacefinder
+) {
+    return new SpaceFiller();
+
+    function SpaceFiller() {
+        var lastInsertion = Promise.resolve(false);
+
+        /**
+         * Given a set of spacefinder rules, applies a writer to the first matching paragraph.
+         * Uses fastdom to avoid performance issues with synchronous DOM actions, but queues async behaviour safely
+         *
+         * @param rules - a spacefinder ruleset
+         * @param writer - function for writing the desired content to a candidate paragraph
+         * @param debug - flag to enable debugging in spacefinder
+         *
+         * @returns {Promise} - resolves when insertion attempt completed
+         */
+        this.insertAtFirstSpace = function insertAtFirstSpace(rules, writer, debug) {
+            lastInsertion = lastInsertion.then(function insertNextContent() {
+                return spacefinder.getParaWithSpace(rules, debug).then(function applyParaToWriter(para){
+                    if (para) {
+                        return promiseInsertion(para);
+                    } else {
+                        return false;
+                    }
+                });
+            });
+
+            function promiseInsertion(para) {
+                return new Promise(function (resolve) {
+                    fastdom.write(function(){
+                        try {
+                            writer(para);
+                            resolve(true);
+                        } catch (e) {
+                            // log and move on
+                            raven.captureException(e);
+                            resolve(false);
+                        }
+                    });
+                });
+            }
+
+            return lastInsertion;
+        }
+    }
+});
