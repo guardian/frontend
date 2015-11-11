3b4f19dc61b2667c87b413e540091b353086840a
Zofia <zofia.korcz@guardian.co.uk>
2015-11-11 15:07:17 +0000

    fixes in description

--- static/src/javascripts/projects/common/modules/experiments/tests/video-preroll.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/video-preroll.js
@@ -8,13 +8,13 @@ define([
         this.start = '2015-11-11';
         this.expiry = '2015-12-11';
         this.author = 'Zofia Korcz';
-        this.description = 'A test to see if a non australian audience will be interested in video pre-rolls.';
+        this.description = 'A test to see if an audience from UK or INT will be interested in video pre-rolls.';
         this.audience = 0.5;
         this.audienceOffset = 0.5;
         this.successMeasure = 'We will see clear difference between user behavior';
-        this.audienceCriteria = 'Users not in AU edition';
+        this.audienceCriteria = 'Users not in AU nor US edition';
         this.dataLinkNames = '';
-        this.idealOutcome = 'Non australian audience will be interested in video pre-rolls.';
+        this.idealOutcome = 'British and international audience will be interested in video pre-rolls.';
 
         this.canRun = function () {
             return config.page.edition === 'UK' || config.page.edition === 'INT';
