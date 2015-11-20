444bdbf196073957bdf1a133219bece6d5590e84
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2015-11-20 10:37:34 +0000

    Docs

--- static/src/javascripts/projects/common/modules/article/space-filler.js
+++ static/src/javascripts/projects/common/modules/article/space-filler.js
@@ -18,8 +18,10 @@ define([
         var lastInsertion = Promise.resolve(false);
 
         /**
+         * A safer way of using spacefinder.
          * Given a set of spacefinder rules, applies a writer to the first matching paragraph.
-         * Uses fastdom to avoid performance issues with synchronous DOM actions, but queues async behaviour safely
+         * Uses fastdom to avoid layout-thrashing, but queues up asynchronous writes to avoid race conditions. We don't
+         * seek a slot for a new component until all the other component writes have finished.
          *
          * @param rules - a spacefinder ruleset
          * @param writer - function, takes a para element and injects a container for the new content synchronously
