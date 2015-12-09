3a4aeb5843690681384507ab9cf1e52503982441
Kelvin Chappell <kelvin.chappell@theguardian.com>
2015-12-09 16:04:22 +0000

    Remove DfpServiceRegistry

--- admin/app/dfp/CustomTargetingValueAgent.scala
+++ admin/app/dfp/CustomTargetingValueAgent.scala
@@ -7,11 +7,10 @@ import scala.util.Try
 object CustomTargetingValueAgent extends DataAgent[Long, String] {
 
   override def loadFreshData() = Try {
-    DfpServiceRegistry().fold(Map[Long, String]()) { serviceRegistry =>
-      val session = new SessionWrapper(serviceRegistry.session)
+    val maybeData = for (session <- SessionWrapper()) yield {
       val values = session.customTargetingValues(new StatementBuilder())
       values.map { v => v.getId.longValue() -> v.getName}.toMap
     }
+    maybeData getOrElse Map.empty
   }
-
 }
