26c4a12279b6dfc40373849758ef3f2b617ea783
NataliaLKB <natalialeebaltazar@gmail.com>
2015-12-11 23:46:47 +0000

    adding route for related in microformat2

--- onward/app/controllers/RelatedController.scala
+++ onward/app/controllers/RelatedController.scala
@@ -1,9 +1,12 @@
 package controllers
 
 
+import com.gu.facia.api.models.{CuratedContent, FaciaContent}
+import implicits.FaciaContentImplicits._
 import common._
 import containers.Containers
 import model._
+import play.api.libs.json._
 import play.api.mvc.{ RequestHeader, Controller }
 import services._
 import performance.MemcachedAction
@@ -19,11 +22,13 @@ object RelatedController extends Controller with Related with Containers with Lo
   )
 
   def renderHtml(path: String) = render(path)
-  def render(path: String) = MemcachedAction { implicit request =>
+  def renderMf2(path: String) = render(path, true)
+  def render(path: String, isMf2: Boolean = false) = MemcachedAction { implicit request =>
     val edition = Edition(request)
     val excludeTags = request.queryString.getOrElse("exclude-tag", Nil)
     related(edition, path, excludeTags) map {
       case related if related.items.isEmpty => JsonNotFound()
+      case stories if isMf2 => renderRelatedMf2(stories.items.sortBy(-_.content.trail.webPublicationDate.getMillis), "related content")
       case trails => renderRelated(trails.items.sortBy(-_.content.trail.webPublicationDate.getMillis), "related content")
     }
   }
@@ -53,5 +58,33 @@ object RelatedController extends Controller with Related with Containers with Lo
     } else {
       Ok(views.html.relatedContent(page, relatedTrails.map(_.faciaContent)))
     }
+  }
+
+  private def renderRelatedMf2(trails: Seq[RelatedContentItem], title: String)(implicit request: RequestHeader) = Cached(30.minutes) {
+    val relatedTrails = trails take 8
+
+    JsonComponent(
+      "items" -> JsArray(trails.map( collection => isCuratedContent(collection.faciaContent)))
+    )
+  }
+
+  private def isCuratedContent(content: FaciaContent): JsValue = content match {
+    case c: CuratedContent => getContent(c)
+    case _ => Json.obj()
+  }
+
+  private def getContent(content: CuratedContent): JsValue = {
+    JsObject(
+      Json.obj(
+        "headline" -> content.headline,
+        "trailText" -> content.trailText,
+        "href" -> content.href,
+        "thumbnail" -> content.maybeContent.flatMap(_.safeFields.get("thumbnail")),
+        "id" -> content.maybeContent.map(_.id),
+        "frontPublicationDate" -> content.maybeFrontPublicationDate
+      )
+      .fields
+      .filterNot{ case (_, v) => v == JsNull}
+    )
   }
 }
