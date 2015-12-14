e0629a989cbfab50ded7fb907c42e37bb3ca2582
Alex Sanders <a@snd.rs>
2015-12-14 18:58:10 +0000

    tidy and comments

--- common/app/templates/inlineJS/blocking/shouldEnhance.scala.js
+++ common/app/templates/inlineJS/blocking/shouldEnhance.scala.js
@@ -3,7 +3,7 @@
 
 // Determine whether we want to run the enhanced app or not.
 // It can come either from a preference in localStorage (see `enhanceKey`)
-// or from a temporary opt-in or -out, using `#standard` or `#enhance` hashes.
+// or from a temporary opt-in or -out override, using `#standard` or `#enhance` hash fragments.
 
 (function (window) {
     var location = window.location;
@@ -15,20 +15,18 @@
 
     var enhanceKey = 'gu.prefs.enhance';
 
-    // This been called core, no-features, universal, etc, and a few previous ways of
-    // opting in and out exist.
+    // This has been called core, no-features, universal etc, and a few previous ways of
+    // opting in and out existed.
     // Now we have settled on standard and enhanced, the previous methods are handled
     // here, temporarily. This function can go after a while.
     function normaliseNomenclature() {
-        var coreKey = 'gu.prefs.force-core';
-        var settingCorePref, corePref;
+        var coreKey = 'gu.prefs.force-core', settingCorePref, corePref;
 
+        // update any `force-core` stored pref
         try {
-            // update any `force-core` stored pref
             var localStorage = window.localStorage;
-            var forceCorePref = localStorage.getItem(coreKey);
-            if (forceCorePref) {
-                localStorage.setItem(enhanceKey, forceCorePref === 'off');
+            if (corePref = localStorage.getItem(coreKey)) {
+                localStorage.setItem(enhanceKey, corePref === 'off');
                 localStorage.removeItem(coreKey);
             }
         } catch (e) {};
@@ -48,25 +46,31 @@
         }
     };
 
-    function couldEnhance() {
-        if (hash === '#standard' || hash === `#${enhanceKey}=false`) return false;
-        try {
-            return localStorage.getItem(enhanceKey) !== 'false';
-        } catch (e) {};
-
-        return true; // presume we *want* to enhance
-    };
-
     function mustEnhance() {
         if (hash === '#enhance' || hash === `#${enhanceKey}=true`) return true;
         try {
             return localStorage.getItem(enhanceKey) === 'true';
         } catch (e) {};
 
-        return false; // presume we don't *have* to enhance
+        return false; // assume we don't *have* to enhance
     };
 
-    // If this is an older iOS, we presume it's an older device (they stop being upgradeable at some point).
+    function couldEnhance() {
+        if (hash === '#standard' || hash === `#${enhanceKey}=false`) return false;
+        try {
+            return localStorage.getItem(enhanceKey) !== 'false';
+        } catch (e) {};
+
+        return true; // assume we're going to enhance if we can
+    };
+
+    function weWantToEnhance() {
+        if isOlderIOSDevice() return false;
+        if (isFront) return !isIpad();
+        return true; // assume we want to enhance
+    };
+
+    // If this is an older iOS, we assume it's an older device (they stop being upgradeable at some point).
     // Old here means iOS 3-6.
     // For usage stats see http://david-smith.org/iosversionstats/
     function isOlderIOSDevice() {
@@ -77,14 +81,13 @@
         return (platform === 'iPad');
     };
 
-    function weWantToEnhance() {
-        if isOlderIOSDevice() return false;
-        if (isFront) return !isIpad();
-        return true;
-    };
-
+    // clean up old names before checking the ones we'll expect from now on
     normaliseNomenclature();
+
+    // down to business
     window.shouldEnhance = mustEnhance() || (couldEnhance() && weWantToEnhance());
+
+    // just so we can tell…
     console && console.info && console.info(`THIS IS${window.shouldEnhance ? ' ' : ' NOT '}ENHANCED`);
 })(window);
 
