52b4ca3f0027db25dfa9a8306ded438e4c5f1fad
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-12-16 15:04:56 +0000

    HtmlCleaner for R2 pages

--- admin/app/pagepresser/HtmlCleaner.scala
+++ admin/app/pagepresser/HtmlCleaner.scala
@@ -5,6 +5,13 @@ import scala.collection.JavaConversions._
 
 object HtmlCleaner {
 
+  def clean(document: Document): Document = {
+    removeAds(document)
+    removeGoogleSearchBox(document)
+    removeShareLinks(document)
+    removeRelatedComponent(document)
+  }
+
   def removeAds(document: Document): Document = {
     val elements = document.getElementById("sub-header")
     val ads = elements.children().toList.filterNot(e => e.attr("class") == "top-navigation twelve-col top-navigation-js")
@@ -15,4 +22,18 @@ object HtmlCleaner {
     document
   }
 
+  def removeGoogleSearchBox(document: Document): Document = removeByClass(document, "top-search-box")
+  def removeShareLinks(document: Document): Document = removeByClass(document, "share-links")
+
+  def removeRelatedComponent(document: Document): Document = {
+    val element = document.getElementById("related")
+    element.remove()
+    document
+  }
+
+  private def removeByClass(document: Document, className: String): Document = {
+    val element = document.getElementsByClass(className)
+    element.remove()
+    document
+  }
 }
