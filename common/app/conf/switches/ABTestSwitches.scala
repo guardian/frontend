8899cd384640029f940ab314d66da0fa64343164
Josh Holder <josh.holder@theguardian.com>
2015-12-18 11:32:51 +0000

    adjust date

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -27,7 +27,7 @@ trait ABTestSwitches {
     "ab-fronts-on-articles2",
     "Injects fronts on articles for the test",
     safeState = Off,
-    sellByDate = new LocalDate(2015, 1, 30),
+    sellByDate = new LocalDate(2016, 1, 30),
     exposeClientSide = true
   )
 
