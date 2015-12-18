830cfde21a57721ce7b765748a34f5fe035bc850
Josh Holder <josh.holder@theguardian.com>
2015-12-18 11:28:55 +0000

    Rename test

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -22,9 +22,9 @@ trait ABTestSwitches {
     exposeClientSide = true
   )
 
-  val ABFrontsOnArticles = Switch(
+  val ABFrontsOnArticles2 = Switch(
     "A/B Tests",
-    "ab-fronts-on-articles",
+    "ab-fronts-on-articles2",
     "Injects fronts on articles for the test",
     safeState = Off,
     sellByDate = new LocalDate(2015, 1, 30),
--- static/src/javascripts/projects/common/modules/experiments/tests/fronts-on-articles.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/fronts-on-articles.js
@@ -4,13 +4,13 @@ define([
     config
 ) {
     return function () {
-        this.id = 'FrontsOnArticles';
+        this.id = 'FrontsOnArticles2';
         this.start = '2015-12-08';
         this.expiry = '2016-1-30';
         this.author = 'Josh Holder';
         this.description = 'Inject fronts containers on articles';
         this.audience = 0.06;
-        this.audienceOffset = 0.25;
+        this.audienceOffset = 0.3;
         this.successMeasure = '';
         this.audienceCriteria = 'All users';
         this.dataLinkNames = '';
