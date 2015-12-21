4f33356a4de00bf11bb6e3c8655e504362e10df0
Justin Pinner <justinpinner@gmail.com>
2015-12-21 13:57:13 +0000

    Add calls to frontend loader and S3 presser.

--- admin/app/jobs/R2PagePressJob.scala
+++ admin/app/jobs/R2PagePressJob.scala
@@ -6,7 +6,12 @@ import com.amazonaws.services.sqs.model.ReceiveMessageRequest
 import common._
 import conf.Configuration
 import conf.switches.Switches.R2PagePressServiceSwitch
-import play.api.libs.json.{JsLookupResult, JsDefined, Json}
+import org.jsoup.Jsoup
+import play.api.libs.json.Json
+import play.api.libs.ws.WS
+import services.R2Archive
+import pagepresser.HtmlCleaner._
+import play.api.Play.current
 
 object R2PagePressJob extends ExecutionContexts with Logging {
   val waitTimeSeconds = 20
@@ -24,23 +29,45 @@ object R2PagePressJob extends ExecutionContexts with Logging {
 
   def run() {
     if (R2PagePressServiceSwitch.isSwitchedOn) {
-      log.info("R2PagePressJob: run")
+      log.info("R2PagePressJob starting")
       try {
-        queue.receive(new ReceiveMessageRequest().withWaitTimeSeconds(waitTimeSeconds)) map { messages =>
-          messages foreach { message =>
-            val url = (Json.parse(message.get) \ "Message").as[String]
-            //TODO - Send the URL to the fetcher & cleaner
-            //TODO - Send the cleaned HTML to S3
-            //TODO - Update the redirects DynamoDB table
-            queue.delete(message.handle)
-          }
-        }
-        log.info("R2PagePressJob: finished")
+        queue.receive(new ReceiveMessageRequest().withWaitTimeSeconds(waitTimeSeconds)) map ( _ foreach press )
       } catch {
-        case e: Exception => log.error(s"Failed to decode r2 url: ${e.getMessage}", e)
+        case e: Exception => log.error(s"### Failed to decode r2 url: ${e.getMessage}", e)
       }
     } else {
       log.info("R2PagePressJob is switched OFF")
     }
   }
+
+  private def press(message: Message[String]) {
+    val urlIn = (Json.parse(message.get) \ "Message").as[String]
+    if (urlIn.nonEmpty) {
+      val url = urlIn.replace("https://", "").replace("http://","")
+      WS.url(urlIn).get().map { response =>
+        response.status match {
+          case 200 => {
+            val cleanedHtmlString = clean(Jsoup.parse(response.body)).toString
+            R2Archive.putPublic(url, cleanedHtmlString, "text/html")
+            R2Archive.get(url).foreach { result =>
+              if (result == cleanedHtmlString) {
+                // TODO: update DynamoDB
+                log.info(s"Pressed $urlIn as $url BUT DON'T FORGET TO UPDATE DYNAMODB!")
+              } else {
+                log.error(s"Pressed data did not match original for $url")
+              }
+            }
+          }
+          case non200 => {
+            log.error(s"Unexpected response from $urlIn, status code: $non200")
+          }
+        }
+      }
+    } else {
+      log.error(s"### invalid url: $urlIn")
+    }
+    // TODO: only delete if everything is ok?
+    queue.delete(message.handle)
+  }
+
 }
