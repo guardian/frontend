33d848137e3c83a8a56f17d514ffdfa7b487c0a8
Justin Pinner <justinpinner@gmail.com>
2015-12-22 13:22:29 +0000

    Add file upload and batch pressing

--- admin/app/controllers/R2PressController.scala
+++ admin/app/controllers/R2PressController.scala
@@ -1,5 +1,7 @@
 package controllers.admin
 
+import java.io.File
+
 import common.{ExecutionContexts, Logging}
 import controllers.AuthLogging
 import play.api.data.Form
@@ -15,8 +17,37 @@ object R2PressController extends Controller with Logging with AuthLogging with E
 
   val pressPageForm = Form(mapping("r2url" -> text)(R2PagePress.apply)(R2PagePress.unapply))
 
-  def pressForm() = AuthActions.AuthActionTest { request =>
+  def pressForm() = AuthActions.AuthActionTest { implicit request =>
     Ok(views.html.pressR2(pressPageForm))
+  }
+
+  def batchUpload() = AuthActions.AuthActionTest { implicit request =>
+    val uploadedFile = request.body.asMultipartFormData.flatMap { files =>
+      files.file("r2urlfile").map { theFile =>
+        val rnd = Math.random().toString.replace(".","")
+        val tmpName = s"/tmp/$rnd${theFile.filename}"
+        val tmpFile = new File(tmpName)
+        theFile.ref.moveTo(tmpFile)
+        tmpFile
+      }
+    }
+    uploadedFile.foreach(pressFile)
+    Ok(views.html.pressR2(pressPageForm))
+  }
+
+  private def pressFile(file: File): Unit = {
+    val source = scala.io.Source.fromFile(file)
+    val lines = try {
+      source.getLines().foreach { line =>
+        if (line.nonEmpty) {
+          //TODO: validation?
+          R2PagePressNotifier.enqueue(line)
+        }
+      }
+    } finally {
+      source.close()
+      file.delete()
+    }
   }
 
   def press() = AuthActions.AuthActionTest { implicit request =>
