1e10a5733dfd56bb24465ba8cd8f02518e75e41f
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-12-23 12:25:29 +0000

    Press the original to S3 too

--- admin/app/pagepresser/Presser.scala
+++ admin/app/pagepresser/Presser.scala
@@ -5,16 +5,23 @@ import org.jsoup.Jsoup
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.WS
-import services.R2Archive
+import services.{R2ArchiveOriginals, R2Archive}
 
 object Presser extends Logging{
 
-  def press(url: String, outputLocation: String) = {
-    WS.url(url).get().map { response =>
+  def press(url: String) = {
+    val request = WS.url(url)
+    val path = request.uri.getPath
+
+    request.get().map { response =>
       response.status match {
         case 200 => {
-          val cleanedHtml = HtmlCleaner.clean(Jsoup.parse(response.body))
-          R2Archive.putPublic(outputLocation, cleanedHtml.toString, "text/html")
+
+
+          val originalBody = response.body
+          //R2ArchiveOriginals.putPublic(path, originalBody, "text/html")
+          val cleanedHtml = HtmlCleaner.clean(Jsoup.parse(originalBody))
+          //R2Archive.putPublic(path, cleanedHtml.toString, "text/html") //write switch to do this
         }
         case non200 => {
           log.error(s"Unexpected response from $url, status code: $non200")
