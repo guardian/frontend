7688b03e8fcd0b14eb6a7ed641cdcace71ea2a4a
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-12-29 17:41:19 +0000

    Need to await the result in order to inline replace the html

--- admin/app/pagepresser/PollsHtmlCleaner.scala
+++ admin/app/pagepresser/PollsHtmlCleaner.scala
@@ -6,7 +6,8 @@ import play.api.libs.ws.WS
 import scala.collection.JavaConversions._
 import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 object PollDeserializer {
   implicit val jsonAnswers = Json.reads[Answer]
@@ -15,8 +16,8 @@ object PollDeserializer {
 }
 
 case class Poll(pollId: String, questions: List[Question])
-case class Question(id: Int, count: Int, answers: List[Answer])
-case class Answer(id: Int, question: Int, count: Int)
+case class Question(id: Int, count: Double, answers: List[Answer])
+case class Answer(id: Int, question: Int, count: Double)
 
 object PollsHtmlCleaner extends HtmlCleaner with implicits.WSRequests {
 
@@ -26,39 +27,33 @@ object PollsHtmlCleaner extends HtmlCleaner with implicits.WSRequests {
   }
 
   def fetchAndPressPollResult(document: Document): Document = {
-    val resultsContainer = document.getElementById("results-container")
-    val pollUrl = resultsContainer.attr("data-poll-url")
+    val pollUrl = document.getElementById("results-container").attr("data-poll-url")
 
-    fetchPoll(pollUrl).map { pollOpt =>
-      for {
-        poll <- pollOpt
-      } yield {
+    fetchPoll(pollUrl).foreach { poll =>
+
         poll.questions.foreach { question =>
-          val questionElement = resultsContainer.getElementById(s"q-${question.id}")
           val mostAnsweredAnswerId = question.answers.tail.foldLeft(question.answers.head)((r, a) => if (r.count > a.count) r else a)
 
-          for (answer <- question.answers) {
-            val answerElement = questionElement.getElementById(s"a-${answer.id}")
+          for (answer <- question.answers) yield {
             val answerPercentage = Math.round(answer.count / question.count * 100)
-            answerElement.getElementsByClass("container").headOption.foreach { element =>
-              element.getElementsByClass("container").headOption.foreach { e =>
-                e.attr("title", s"Votes cast: ${answer.count} ($answerPercentage%)")
-              }
+            document.getElementById(s"a-${answer.id}").getElementsByClass("container").foreach { element =>
+                element.tagName("div")
+                element.attr("title", s"Votes cast: ${answer.count} ($answerPercentage%)")
+
             }
           }
         }
       }
-    }
-
     document
 
   }
 
 
   import PollDeserializer._
-  def fetchPoll(url: String): Future[Option[Poll]] = {
-    val jsonF = WS.url(url).withRequestTimeout(2000).getOKResponse().map(_.json)
-    for (json <- jsonF) yield json.asOpt[Poll]
+  def fetchPoll(url: String): Option[Poll] = {
+    val result = Await.result(WS.url(url).getOKResponse(), 1.second)
+
+    result.json.asOpt[Poll]
 
   }
 }
