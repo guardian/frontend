f8f96ec335fa41deec051be1ea275d017e5f452a
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-12-30 10:58:15 +0000

    Extending largeTopSlot ab test

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -18,7 +18,7 @@ trait ABTestSwitches {
     "ab-large-top-ad",
     "Testing the difference of user behaviour based on large top ad format",
     safeState = Off,
-    sellByDate = new LocalDate(2015, 12, 31),
+    sellByDate = new LocalDate(2016, 1, 13),
     exposeClientSide = true
   )
 
