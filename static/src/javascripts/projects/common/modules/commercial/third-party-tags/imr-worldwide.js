676623a6dc4437206faf49d9a6b72031e9dfa97a
Ken Lim <kenneth.lim@theguardian.com>
2016-01-05 22:00:58 +0000

    Oops, double quoted some strings.

--- static/src/javascripts/projects/common/modules/commercial/third-party-tags/imr-worldwide.js
+++ static/src/javascripts/projects/common/modules/commercial/third-party-tags/imr-worldwide.js
@@ -7,7 +7,7 @@ define([
     function load() {
         if (config.switches.imrWorldwide) {
             return require(['js!' + '//secure-au.imrworldwide.com/v60.js'], function () {
-                var pvar = { cid: "au-guardian", content: "0", server: "secure-au" };
+                var pvar = { cid: 'au-guardian', content: '0', server: 'secure-au' };
                 /*eslint-disable no-undef*/
                 var trac = nol_t(pvar);
                 trac.record().post();
