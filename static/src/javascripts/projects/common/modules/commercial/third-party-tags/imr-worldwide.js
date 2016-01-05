67835890ac9ec02097a34f6aa575326ec6204fcb
Ken Lim <kenneth.lim@theguardian.com>
2016-01-05 21:54:13 +0000

    Suppressing no-def error

--- static/src/javascripts/projects/common/modules/commercial/third-party-tags/imr-worldwide.js
+++ static/src/javascripts/projects/common/modules/commercial/third-party-tags/imr-worldwide.js
@@ -8,6 +8,7 @@ define([
         if (config.switches.imrWorldwide) {
             return require(['js!' + '//secure-au.imrworldwide.com/v60.js'], function () {
                 var pvar = { cid: "au-guardian", content: "0", server: "secure-au" };
+                /*eslint-disable no-undef*/
                 var trac = nol_t(pvar);
                 trac.record().post();
             });
