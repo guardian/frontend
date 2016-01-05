21fe2bd1c9cd794c472b17c95499d4a9e6cb3f92
Oliver Joseph Ash <oliverjash@gmail.com>
2016-01-05 11:05:10 +0000

    Fix common/app/views/support/ContentOldAgeDescriber.scala

--- common/app/views/support/ContentOldAgeDescriber.scala
+++ common/app/views/support/ContentOldAgeDescriber.scala
@@ -7,37 +7,39 @@ object ContentOldAgeDescriber extends ContentOldAgeDescriber
 
 class ContentOldAgeDescriber {
   def apply(content: model.Content): String = {
-    message(content.trail.webPublicationDate)
+    message(Some(content.trail.webPublicationDate))
   }
 
   def apply(content: model.ContentType): String = {
-    message(content.trail.webPublicationDate)
+    message(Some(content.trail.webPublicationDate))
   }
 
-  def apply(apiContent: com.gu.contentapi.client.model.Content): String = {
-    message(apiContent.webPublicationDate)
+  def apply(apiContent: com.gu.contentapi.client.model.v1.Content): String = {
+    message(apiContent.webPublicationDate.map(date => new DateTime(date.dateTime)))
   }
 
-  private def message(pubDate: DateTime) = {
-    val warnLimitDays = 30
-    if(pubDate.isBefore(DateTime.now().minusDays(warnLimitDays))) {
-      val ageMillis = DateTime.now().getMillis - pubDate.getMillis
-      val years = TimeUnit.MILLISECONDS.toDays(ageMillis) / 365
-      val months = TimeUnit.MILLISECONDS.toDays(ageMillis) / 28
-      val weeks = TimeUnit.MILLISECONDS.toDays(ageMillis) / 7
-      val days = TimeUnit.MILLISECONDS.toDays(ageMillis)
+  private def message(maybePubDate: Option[DateTime]) = {
+    maybePubDate.map(pubDate => {
+      val warnLimitDays = 30
+      if (pubDate.isBefore(DateTime.now().minusDays(warnLimitDays))) {
+        val ageMillis = DateTime.now().getMillis - pubDate.getMillis
+        val years = TimeUnit.MILLISECONDS.toDays(ageMillis) / 365
+        val months = TimeUnit.MILLISECONDS.toDays(ageMillis) / 28
+        val weeks = TimeUnit.MILLISECONDS.toDays(ageMillis) / 7
+        val days = TimeUnit.MILLISECONDS.toDays(ageMillis)
 
-      if(years >= 2) s"$years years"
-      else if(years > 0) s"$years year"
-      else if(months >= 2) s"$months months"
-      else if(months > 0) s"$months month"
-      else if(weeks >= 2) s"$weeks weeks"
-      else if(weeks > 0) s"$weeks week"
-      else s"$days days"
+        if (years >= 2) s"$years years"
+        else if (years > 0) s"$years year"
+        else if (months >= 2) s"$months months"
+        else if (months > 0) s"$months month"
+        else if (weeks >= 2) s"$weeks weeks"
+        else if (weeks > 0) s"$weeks week"
+        else s"$days days"
 
-    } else {
-      ""
-    }
+      } else {
+        ""
+      }
+    }).getOrElse("")
   }
 
 }
