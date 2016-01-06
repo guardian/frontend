57fc32d4886fea4bfadaed7f8a2c555517ecd3ea
Ken Lim <kennethlimfc@gmail.com>
2016-01-06 10:07:49 +0000

    Added explanatory comment.

--- static/src/javascripts/projects/common/modules/commercial/third-party-tags/imr-worldwide.js
+++ static/src/javascripts/projects/common/modules/commercial/third-party-tags/imr-worldwide.js
@@ -8,6 +8,8 @@ define([
         if (config.switches.imrWorldwide) {
             return require(['js!' + '//secure-au.imrworldwide.com/v60.js'], function () {
                 var pvar = { cid: 'au-guardian', content: '0', server: 'secure-au' };
+                
+                // nol_t is a global function set by the imrworldwide library
                 /*eslint-disable no-undef*/
                 var trac = nol_t(pvar);
                 trac.record().post();
