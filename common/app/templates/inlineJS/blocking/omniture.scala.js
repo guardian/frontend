468c129632091c6a70f758b2a32aadb3ea7dccd4
Alex Sanders <a@snd.rs>
2016-01-06 17:03:03 +0000

    move inline omnniture to a more natural home

---
+++ common/app/templates/inlineJS/blocking/omniture.scala.js
@@ -0,0 +1,10 @@
+@()
+
+@import conf.Static
+
+// this image fallback is duplicated in app.js by /grunt-configs/concat.js
+try {
+    @Html(Static.js.omnitureJs)
+} catch (e) {
+    (new Image()).src = window.guardian.config.page.beaconUrl + '/count/omniture-library-error.gif';
+}
