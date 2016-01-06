33f6a06fca07f9746249fd2b4935237876e251e1
Alex Sanders <a@snd.rs>
2016-01-06 16:08:44 +0000

    conact omniture as part of app.js

--- grunt-configs/concat.js
+++ grunt-configs/concat.js
@@ -5,10 +5,22 @@ module.exports = function (grunt, options) {
             // https://github.com/gruntjs/grunt-contrib-concat/issues/131
             // options: { sourceMap: true },
             src: [
+                options.staticPublic + 'javascripts/vendor/omniture.js',
+                options.staticSrcDir + 'javascripts/bootstraps/standard/omniture-pageview.js',
                 options.staticSrcDir + 'javascripts/components/curl/curl-domReady.js',
                 options.requirejsDir + '/boot.js'
             ],
-            dest: options.staticTargetDir + 'javascripts/app.js'
+            dest: options.staticTargetDir + 'javascripts/app.js',
+            options: {
+                process: function (src, path) {
+                    // make sure omniture handles an error
+                    // behaviour is duplicated in /common/app/views/fragments/omnitureScript.scala.html
+                    if (path.match(/vendor\/omniture\.js/)) {
+                        src = "try { \n" + src + "\n} catch (e) {(new Image()).src = window.guardian.config.page.beaconUrl + '/count/omniture-library-error.gif';}"
+                    }
+                    return src;
+                }
+            }
         }
     };
 };
