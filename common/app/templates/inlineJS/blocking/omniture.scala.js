8b8707ad256cec24840caa8b3a87791f05b21698
Alex Sanders <a@snd.rs>
2016-01-07 11:16:01 +0000

    escape JS properly

--- common/app/templates/inlineJS/blocking/omniture.scala.js
+++ common/app/templates/inlineJS/blocking/omniture.scala.js
@@ -1,10 +1,9 @@
 @()
-
 @import conf.Static
 
 // this image fallback is duplicated in app.js by /grunt-configs/concat.js
 try {
-    @Html(Static.js.omnitureJs)
+    @JavaScript(Static.js.omnitureJs)
 } catch (e) {
     (new Image()).src = window.guardian.config.page.beaconUrl + '/count/omniture-library-error.gif';
 }
