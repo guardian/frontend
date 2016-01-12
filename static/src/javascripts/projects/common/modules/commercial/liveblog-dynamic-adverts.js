8dc7919cd7ac90192f7caf05e2cabb8a5e6412ff
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-01-12 15:25:04 +0000

    Refactored dynamic ads using spacefiller

--- static/src/javascripts/projects/common/modules/commercial/liveblog-dynamic-adverts.js
+++ static/src/javascripts/projects/common/modules/commercial/liveblog-dynamic-adverts.js
@@ -1,240 +1,101 @@
 define([
-    'Promise',
     'bonzo',
-    'bean',
-    'fastdom',
+    'common/utils/fastdom-promise',
     'common/utils/config',
     'common/utils/mediator',
     'common/modules/commercial/create-ad-slot',
-    'common/modules/commercial/dfp-api',
-    'lodash/functions/once',
-    'lodash/functions/debounce',
-    'lodash/arrays/rest',
-    'lodash/collections/map',
-    'lodash/collections/filter',
-    'lodash/collections/forEach'
+    'common/modules/article/space-filler',
+    'common/modules/commercial/dfp-api'
 ], function (
-    Promise,
     bonzo,
-    bean,
     fastdom,
     config,
     mediator,
     createAdSlot,
-    dfp,
-    once,
-    debounce,
-    drop,
-    map,
-    filter,
-    forEach
+    spaceFiller,
+    dfp
 ) {
-    var settings = {
-        INTERVAL: 5,      // number of posts between ads
-        OFFSET: 0.5,      // ratio of the screen height from which ads are loaded
-        SCROLLTIMER: 50   // the time we wait before responding to a scroll event
-    };
+    var INTERVAL = 5;      // number of posts between ads
+    var OFFSET = 1.5;      // ratio of the screen height from which ads are loaded
 
-    var truncatedClass = 'truncated-block';
+    var slotCounter = 0, windowHeight, firstSlot;
 
-    // Keep a check if the module is currently listening to scroll events
-    var listening = false;
-
-    // Keep track of ad slots that are not yet initialized
-    var slots = [];
-    var slotCounter = 0;
-
-    var posts, altitudes, firstPost, firstPostIndex, vh, vstart, voffset, onScrollDebounced;
-
-    function init() {
-        if ('complete' === document.readyState) {
-            onLoad();
-        } else {
-            window.addEventListener('load', onLoad);
-        }
+    function startListening() {
         mediator.on('modules:autoupdate:updates', onUpdate);
     }
 
-    function onLoad() {
-        posts = document.querySelectorAll('.js-liveblog-body > .block');
-
-        return new Promise(function (resolve) {
-            fastdom.read(function () {
-                vh = document.documentElement.clientHeight;
-                vstart = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-                altitudes = map(posts, function (post) {
-                    var rect = post.getBoundingClientRect();
-                    return rect.bottom - vstart;
-                });
-                voffset = vh * settings.OFFSET;
-                resolve();
-            });
-        })
-        .then(findFirstInsertPosition)
-        .then(insertSlots)
-        .then(handleTruncatedSlots)
-        .then(listenForScrolls);
+    function stopListening() {
+        mediator.on('modules:autoupdate:updates', onUpdate);
     }
 
-    function onUpdate(newPosts) {
-        if (!newPosts || newPosts.length === 0) {
-            return;
-        }
+    function getSpaceFillerRules(windowHeight, update) {
+        var prevSlot, prevIndex;
+        update = !!update;
+        return {
+            bodySelector: '.js-liveblog-body',
+            slotSelector: ' > .block',
+            reverse: update,
+            startAt: update ? firstSlot : null,
+            minAbove: update ? 0 : windowHeight * OFFSET,
+            minBelow: 0,
+            filter: filterSlot
+        };
 
-        mediator.off('modules:autoupdate:updates', onUpdate);
-
-        // We need to inject new ad slots if there is space for them. If yes,
-        // then after the injection we just make sure firstPost is updated so
-        // it won't break the process on the next update
-        return new Promise(function (resolve) {
-            fastdom.read(function () {
-                var pcur = bonzo(firstPost).previous();
-                var pprev;
-
-                posts = [pcur[0]];
-                // Update first post if necessary
-                if (posts.length % settings.INTERVAL === 0) {
-                    firstPost = pcur[0];
-                }
-
-                while ((pprev = pcur.previous()) && pprev.length) {
-                    posts.push(pprev[0]);
-                    pcur = pprev;
-                    // Update first post if necessary
-                    if (posts.length % settings.INTERVAL === 0) {
-                        firstPost = pcur[0];
-                    }
-                }
-
-                // insertSlots expect a number of posts to be dropped from the
-                // beginning. In this case, we must make sure the first n
-                // (as per corresponding setting) are skipped.
-                resolve(settings.INTERVAL - 1);
-            });
-        })
-        .then(insertSlots)
-        .then(listenForScrolls)
-        .then(function () {
-            mediator.on('modules:autoupdate:updates', onUpdate);
-        });
-    }
-
-    function findFirstInsertPosition() {
-        // Let's find the first post that ends below the viewport
-        firstPostIndex = (function () {
-            var i = 0;
-            while (i < altitudes.length && altitudes[i] < vh) {
-                i++;
-            }
-            return i === altitudes.length ? -1 : i;
-        }());
-
-        // If all the posts appear within the viewport then we don't load any ad
-        if (firstPostIndex === -1) {
-            throw firstPostIndex;
-        }
-
-        firstPost = posts[firstPostIndex];
-
-        return firstPostIndex;
-    }
-
-    function insertSlots(firstPostIndex) {
-        // Let's get every nth post after the first post, as per the corresponding setting
-        function nth(_, index) {
-            return index % settings.INTERVAL === 0;
-        }
-
-        posts = filter(drop(posts, firstPostIndex), nth);
-
-        if (!posts.length) {
-            throw 0;
-        }
-
-        // Insert ad slots after all the selected posts and keep track of the ones
-        // sitting between truncated posts
-        return new Promise(function (resolve) {
-            fastdom.write(function () {
-                var truncated = [];
-                slots = map(posts, function (post) {
-                    var $post = bonzo(post);
-                    var $adSlot = bonzo(createAdSlot('inline1' + slotCounter++, 'liveblog-inline'));
-                    if (truncated.length ||
-                        $post.hasClass(truncatedClass) && $post.next().hasClass(truncatedClass)
-                    ) {
-                        truncated.push($adSlot);
-                        $adSlot.addClass(truncatedClass);
-                    }
-                    $post.after($adSlot);
-                    return $adSlot;
-                }).concat(slots);
-
-                resolve(truncated);
-            });
-        });
-    }
-
-    function handleTruncatedSlots(truncated) {
-        function truncation(onOrOff) {
-            bean[onOrOff](document.body, 'click', '.article-elongator', removeTruncation);
-            mediator[onOrOff]('module:liveblog:showkeyevents', removeTruncation);
-            mediator[onOrOff]('module:filter:toggle', removeTruncation);
-        }
-
-        function removeTruncation() {
-            forEach(truncated, function ($slot) {
-                $slot.removeClass(truncatedClass);
-            });
-            truncated.length = 0;
-            truncation('off');
-        }
-
-        if (truncated.length) {
-            truncation('on');
-        }
-    }
-
-    function listenForScrolls() {
-        if (!listening) {
-            if (onScrollDebounced === undefined) {
-                onScrollDebounced = debounce(onScroll, settings.SCROLLTIMER);
-            }
-            mediator.on('window:throttledScroll', onScrollDebounced);
-            listening = true;
-        }
-    }
-
-    function onScroll() {
-        // We need each ad slot current position relative to the viewport in
-        // order to decide which ones are within range and then load them
-        var rects = map(slots, function ($slot) {
-            return $slot[0].getBoundingClientRect();
-        });
-
-        // We'll filter out all the slots who are displayed so that they
-        // we don't repeat the whole process for them on the next scroll event
-        slots = filter(slots, function ($slot, index) {
-            if ($slot.hasClass('truncated-block')) {
+        function filterSlot(slot, index) {
+            if (index === 0) {
+                prevSlot = slot;
+                prevIndex = index;
+                return !update;
+            } else if (index - prevIndex >= INTERVAL && Math.abs(slot.top - prevSlot.top) >= windowHeight) {
+                prevSlot = slot;
+                prevIndex = index;
                 return true;
             }
-            var rect = rects[index];
-            if (-voffset < rect.bottom && rect.top < vh + voffset) {
-                dfp.addSlot($slot);
-                return false;
-            }
-            return true;
-        });
 
-        if (slots.length === 0) {
-            mediator.off('window:throttledScroll', onScrollDebounced);
-            listening = false;
+            return false;
         }
+    }
+
+    function insertAds(slots) {
+        var adSlots = [];
+        for (var i = 0; i < slots.length; i++) {
+            var $slot = bonzo(slots[i]);
+            var $adSlot = bonzo(createAdSlot('inline1' + slotCounter++, 'liveblog-inline'));
+            $slot.after($adSlot);
+            dfp.addSlot($adSlot);
+            adSlots.push($adSlot[0]);
+        }
+        return adSlots;
+    }
+
+    function fill(rules) {
+        return spaceFiller.fillSpace(rules, insertAds)
+            .then(function (ads) {
+                if (ads) {
+                    firstSlot = ads[0].previousSibling;
+                }
+            })
+            .then(startListening);
+    }
+
+    function onUpdate() {
+        stopListening();
+        Promise.resolve(getSpaceFillerRules(windowHeight, true)).then(fill);
+    }
+
+    function init() {
+        return fastdom.read(function () {
+            return windowHeight = document.documentElement.clientHeight;
+        })
+        .then(getSpaceFillerRules)
+        .then(fill);
     }
 
     return {
-        init: once(init),
-        settings: settings,
-        onLoad: onLoad,
-        onUpdate: onUpdate
+        init: init,
+        settings: {
+            INTERVAL: INTERVAL,
+            OFFSET: OFFSET
+        }
     };
 });
--- static/test/javascripts/spec/common/commercial/liveblog-dynamic-adverts.spec.js
+++ static/test/javascripts/spec/common/commercial/liveblog-dynamic-adverts.spec.js
@@ -10,7 +10,7 @@ define([
     $
 ) {
     var $style, body;
-    var firstAd, slotsCounter;
+    var slotsCounter;
 
     describe('Liveblog Dynamic Adverts', function () {
         var fixturesConfig = {
@@ -51,41 +51,15 @@ define([
         });
 
         it('should insert ads every ' + liveblogDynamicAdverts.settings.INTERVAL + 'th block', function () {
-            liveblogDynamicAdverts.onLoad().then(function () {
+            liveblogDynamicAdverts.init().then(function () {
                 slotsCounter = body.querySelectorAll('.ad-slot').length;
                 var candidates = document.querySelectorAll('.js-liveblog-body > *:nth-child(1+' + liveblogDynamicAdverts.settings.INTERVAL + 'n)');
                 var allSlots =
                     Array.prototype.every.call(candidates, function (c) { return c.classList.contains('ad-slot'); }) &&
                     candidates.length === slotsCounter;
-                firstAd = body.children[1];
                 expect(allSlots).toBe(true);
             });
         });
 
-        it('should insert ads every ' + liveblogDynamicAdverts.settings.INTERVAL + 'th block after an update', function () {
-            for (var i = 0; i < 12; i++) {
-                var d = document.createElement('div');
-                d.classList.add('block');
-                body.insertBefore(d, body.firstChild);
-            }
-
-            liveblogDynamicAdverts.onUpdate([0]).then(function () {
-                var candidates = [];
-                var ncur = firstAd;
-                var index = 0;
-                var nprev;
-
-                while ((nprev = ncur.previousElementSibling)) {
-                    index += 1;
-                    if (index % (liveblogDynamicAdverts.settings.INTERVAL + 1) === 0) {
-                        candidates.push(nprev);
-                    }
-                    ncur = nprev;
-                }
-                var allSlots = Array.prototype.every.call(candidates, function (c) { return c.classList.contains('ad-slot'); }) &&
-                    candidates.length === body.querySelectorAll('.ad-slot').length - slotsCounter;
-                expect(allSlots).toBe(true);
-            });
-        });
     });
 });
