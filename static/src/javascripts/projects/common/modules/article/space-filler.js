29bc7bc3deeb1b9ff700f585f76880242243a16e
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-01-15 14:51:50 +0000

    Revert spaceFill to being a singleton...

--- static/src/javascripts/projects/common/modules/article/space-filler.js
+++ static/src/javascripts/projects/common/modules/article/space-filler.js
@@ -9,20 +9,25 @@ define([
     raven,
     spacefinder
 ) {
-    var lastIteration = Promise.resolve(false);
+    var lastIteration;
 
-    /**
-     * A safer way of using spacefinder.
-     * Given a set of spacefinder rules, applies a writer to the first matching paragraph.
-     * Uses fastdom to avoid layout-thrashing, but queues up asynchronous writes to avoid race conditions. We don't
-     * seek a slot for a new component until all the other component writes have finished.
-     *
-     * @param rules - a spacefinder ruleset
-     * @param writer - function, takes a para element and injects a container for the new content synchronously. It should NOT use Fastdom.
-     *
-     * @returns {Promise} - when insertion attempt completed, resolves 'true' if inserted, or 'false' if no space found
-     */
-    function fillSpace(rules, writer) {
+    function SpaceFiller() {
+        lastIteration = Promise.resolve(false);
+
+        /**
+         * A safer way of using spacefinder.
+         * Given a set of spacefinder rules, applies a writer to the first matching paragraph.
+         * Uses fastdom to avoid layout-thrashing, but queues up asynchronous writes to avoid race conditions. We don't
+         * seek a slot for a new component until all the other component writes have finished.
+         *
+         * @param rules - a spacefinder ruleset
+         * @param writer - function, takes a para element and injects a container for the new content synchronously. It should NOT use Fastdom.
+         *
+         * @returns {Promise} - when insertion attempt completed, resolves 'true' if inserted, or 'false' if no space found
+         */
+    }
+
+    SpaceFiller.prototype.fillSpace = function (rules, writer) {
         return lastIteration = lastIteration.then(insertNextContent).catch(onInsertionError);
 
         function insertNextContent() {
@@ -35,10 +40,6 @@ define([
                 return true;
             });
         }
-    }
-
-    fillSpace.reset = function () {
-        lastIteration = Promise.resolve(false);
     };
 
     function onInsertionError(e) {
@@ -46,7 +47,5 @@ define([
         return false;
     }
 
-    return {
-        fillSpace: fillSpace
-    };
+    return new SpaceFiller();
 });
