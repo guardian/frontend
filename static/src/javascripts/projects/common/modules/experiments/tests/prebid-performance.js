07f20159c931fb352a008cdbe32eaf7b084cfa07
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2016-01-15 12:28:06 +0000

    Set 2% cohort size

--- static/src/javascripts/projects/common/modules/experiments/tests/prebid-performance.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/prebid-performance.js
@@ -5,13 +5,12 @@ define([
 ) {
     return function () {
         this.id = 'PrebidPerformance';
-        this.start = '2016-01-11';
+        this.start = '2016-01-15';
         this.expiry = '2016-01-31';
         this.author = 'Jimmy Breck-McKye';
         this.description = 'run prebid.js header-bidding auctions before displaying DFP advertising';
 
-        // todo What should these be?
-        this.audience = 0.01;
+        this.audience = 0.02;
         this.audienceOffset = 0.1;
         this.successMeasure = '';
         this.audienceCriteria = 'All users';
@@ -19,7 +18,6 @@ define([
         this.idealOutcome = '';
 
         this.canRun = function () {
-            // todo To be defined
             return true;
         };
 
