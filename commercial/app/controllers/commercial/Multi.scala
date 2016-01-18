4179a7b22117490a16eb6d62db5991e233b35dbe
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-01-18 14:51:55 +0000

    Refactoring Multi to return a sequence of Futures

--- commercial/app/controllers/commercial/Multi.scala
+++ commercial/app/controllers/commercial/Multi.scala
@@ -20,49 +20,75 @@ object Multi
   with implicits.Requests {
 
   def renderMulti() = MemcachedAction { implicit request =>
-    Future.successful {
-
       val requestedContent = request.getParameters("components")
+
+      val slotIds = request.getParameters("slotIds")
+
+      val componentsAndSpecificIds = requestedContent zip slotIds
 
       val clickMacro = request.getParameter("clickMacro")
 
       val omnitureId = request.getParameter("omnitureId")
 
-      val content = requestedContent flatMap {
-        case "jobs" =>
-          JobsAgent.jobsTargetedAt(segment).headOption map {
-            views.html.jobs.jobFragment(_, clickMacro)
+      val eventualContent:Future[Option[play.twirl.api.Html]] = componentsAndSpecificIds flatMap {
+        // case ("jobs", jobId) if jobId.nonEmpty =>
+        //   Future.successful {
+        //     JobsAgent.specificJobs(Seq(jobId)).headOption map {
+        //       views.html.jobs.jobFragment(_, clickMacro)
+        //     }
+        //   }
+        // case ("jobs", _) =>
+        //   Future.successful {JobsAgent.jobsTargetedAt(segment).headOption map {
+        //     views.html.jobs.jobFragment(_, clickMacro)
+        //   }}
+        case ("books", isbn) if isbn.nonEmpty => {
+            val x = BestsellersAgent.getSpecificBooks(Seq(isbn)) map { books =>
+              books.headOption map { book =>
+                views.html.books.bookFragment(book, clickMacro)
+              }
+            }
+            println(x)
+            Future.successful(None)
           }
-        case "books" =>
-          BestsellersAgent.bestsellersTargetedAt(segment).headOption map {
+        case ("books", _) =>
+          Future.successful {BestsellersAgent.bestsellersTargetedAt(segment).headOption map {
             views.html.books.bookFragment(_, clickMacro)
-          }
-        case "travel" =>
-          TravelOffersAgent.offersTargetedAt(segment).headOption map {
+          }}
+        case ("travel", travelId) if travelId.nonEmpty =>
+          Future.successful {TravelOffersAgent.specificTravelOffers(Seq(travelId)).headOption map {
             views.html.travel.travelFragment(_, clickMacro)
-          }
-        case "masterclasses" =>
-          MasterClassAgent.masterclassesTargetedAt(segment).headOption map {
+          }}
+        case ("travel", _) =>
+          Future.successful {TravelOffersAgent.offersTargetedAt(segment).headOption map {
+            views.html.travel.travelFragment(_, clickMacro)
+          }}
+        case ("masterclasses", eventBriteId) if eventBriteId.nonEmpty =>
+          Future.successful {MasterClassAgent.specificClasses(Seq(eventBriteId)).headOption map {
             views.html.masterClasses.masterClassFragment(_, clickMacro)
-          }
-        case "soulmates" =>
-          for {
+          }}
+        case ("masterclasses", _) =>
+          Future.successful {MasterClassAgent.masterclassesTargetedAt(segment).headOption map {
+            views.html.masterClasses.masterClassFragment(_, clickMacro)
+          }}
+        case ("soulmates", _) =>
+          Future.successful {for {
             woman <- SoulmatesAgent.womenAgent.sample().headOption
             man <- SoulmatesAgent.menAgent.sample().headOption
           } yield {
             views.html.soulmates.soulmateFragment(Random.shuffle(Seq(woman, man)), clickMacro)
-          }
-        case _ => None
+          }}
+        case _ => Future.successful(None)
       }
 
-      if (requestedContent.nonEmpty && content.size == requestedContent.size) {
-        Cached(componentMaxAge) {
-          jsonFormat.result(views.html.multi(content, omnitureId))
+      eventualContent map { content =>
+        if (requestedContent.nonEmpty && content.size == requestedContent.size) {
+          Cached(componentMaxAge) {
+            jsonFormat.result(views.html.multi(content, omnitureId))
+          }
+        } else {
+          NoCache(jsonFormat.nilResult)
         }
-      } else {
-        NoCache(jsonFormat.nilResult)
       }
-    }
   }
 
 }
