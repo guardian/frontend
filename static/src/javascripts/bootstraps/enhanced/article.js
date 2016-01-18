aedf6a0b87305f91cf0823c44d8b06ed064e20d4
Oliver Joseph Ash <oliverjash@gmail.com>
2016-01-18 16:47:37 +0000

    Initial styling of quiz

--- static/src/javascripts/bootstraps/enhanced/article.js
+++ static/src/javascripts/bootstraps/enhanced/article.js
@@ -55,6 +55,55 @@ define([
                 require(['ophan/ng'], function (ophan) {
                     mediator.on('quiz/ophan-event', ophan.record);
                 });
+            },
+
+            initAtomQuiz: function () {
+                // TODO: Only run for quizzes
+                var renderLabels = function (state) {
+                    state.$labels.each(function (labelEl) {
+                        var inputEl = $('.' + state.inputClassName, labelEl)[0];
+                        var isChecked = inputEl.checked;
+                        var $labelEl = $(labelEl);
+                        var isFocussed = document.activeElement === inputEl;
+                        if (isChecked) {
+                            $(labelEl).addClass(state.labelCheckedClassName);
+                        } else {
+                            $(labelEl).removeClass(state.labelCheckedClassName);
+                        }
+
+                        if (isFocussed) {
+                            $labelEl.addClass(state.labelFocussedClassName);
+                        } else {
+                            $labelEl.removeClass(state.labelFocussedClassName);
+                        }
+                    });
+                };
+
+                var renderQuiz = function () {
+                    var $quizzes = $('.quiz');
+                    var quizAnswerLabelClassName = 'quiz__answer';
+                    var quizAnswerLabelCheckedClassName = quizAnswerLabelClassName + '--checked';
+                    var quizAnswerLabelFocussedClassName = quizAnswerLabelClassName + '--focussed';
+                    var quizAnswerInputClassName = 'quiz__answer' + '__input';
+                    $quizzes.each(function (quizElement) {
+                        var $quizLabels = $('.quiz__answer', quizElement);
+                        var renderQuizLabels = function () {
+                            renderLabels({
+                                $labels: $quizLabels,
+                                inputClassName: quizAnswerInputClassName,
+                                labelCheckedClassName: quizAnswerLabelCheckedClassName,
+                                labelFocussedClassName: quizAnswerLabelFocussedClassName
+                            });
+                        };
+
+                        window.addEventListener('focus', renderQuizLabels, true);
+                        window.addEventListener('blur', renderQuizLabels, true);
+                        bean.on(quizElement, 'change', '.' + quizAnswerInputClassName, renderQuizLabels);
+                        renderQuizLabels();
+                    });
+                };
+
+                renderQuiz();
             }
         },
 
@@ -64,6 +113,7 @@ define([
             modules.initRightHandComponent();
             modules.initCmpParam();
             modules.initQuizListeners();
+            modules.initAtomQuiz();
             richLinks.upgradeRichLinks();
             richLinks.insertTagRichLink();
             membershipEvents.upgradeEvents();
