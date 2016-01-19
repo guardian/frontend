b66338fa40736da2e65869456eb70322b7c35007
Sam Desborough <sam.desborough@gmail.com>
2016-01-19 17:10:16 +0000

    Use request rather than param to determine email format

--- applications/app/controllers/LatestIndexController.scala
+++ applications/app/controllers/LatestIndexController.scala
@@ -12,12 +12,12 @@ import services.{IndexPage, IndexPageItem}
 import scala.concurrent.Future
 
 object LatestIndexController extends Controller with ExecutionContexts with implicits.ItemResponses with Logging {
-  def latest(path: String, email: Boolean = false) = Action.async { implicit request =>
+  def latest(path: String) = Action.async { implicit request =>
     loadLatest(path).map { _.map { index =>
       index.page match {
         case tag: Tag if tag.isSeries || tag.isBlog => index.trails.headOption.map(latest => {
-          if (email) Found(latest.metadata.url + "/email")
-          else       Found(latest.metadata.url)
+          if (request.isEmail) Found(latest.metadata.url + "/email")
+          else                 Found(latest.metadata.url)
         }).getOrElse(NotFound)
 
         case tag: Tag => MovedPermanently(s"${tag.metadata.url}/all")
