f127ebaaa531bc3219c570a0227bf39001cd2534
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-01-22 11:31:22 +0000

    Moved item buttons in their own template to make them optional

--- static/src/javascripts/projects/common/modules/commercial/creatives/template.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/template.js
@@ -3,6 +3,9 @@ define([
     'common/utils/config',
     'common/utils/template',
     'common/views/svgs',
+    'text!common/views/commercial/creatives/manual-inline-button.html',
+    'text!common/views/commercial/creatives/manual-single-button.html',
+    'text!common/views/commercial/creatives/manual-multiple-button.html',
 
     // require templates, so they're bundled up as part of the build
     'text!common/views/commercial/creatives/ad-feature-mpu.html',
@@ -17,8 +20,53 @@ define([
     $,
     config,
     template,
-    svgs
+    svgs,
+    manualInlineButtonStr,
+    manualSingleButtonStr,
+    manualMultipleButtonStr
 ) {
+
+    var manualInlineButtonTpl;
+    var manualSingleButtonTpl;
+    var manualMultipleButtonTpl;
+
+    var preprocessTemplate = {
+        'manual-inline': function(tpl) {
+            if (!manualInlineButtonTpl) {
+                manualInlineButtonTpl = template(manualInlineButtonStr);
+            }
+            // having a button is the default state, that is why we expressely
+            // test for when *not* to display one
+            tpl.params.offerButton = tpl.params.showButton === 'no' ?
+                '' :
+                manualInlineButtonTpl(tpl.params);
+        },
+
+        'manual-single': function(tpl) {
+            if (!manualSingleButtonTpl) {
+                manualSingleButtonTpl = template(manualSingleButtonStr);
+            }
+            tpl.params.offerButton = (tpl.params.offerLinkText) ?
+                 manualSingleButtonTpl(tpl.params) :
+                 '';
+        },
+
+        'manual-multiple': function(tpl) {
+            if (!manualMultipleButtonTpl) {
+                manualMultipleButtonTpl = template(manualMultipleButtonStr);
+            }
+
+            var links = ['offer1linktext', 'offer2linktext', 'offer3linktext', 'offer4linktext'];
+            for (var i = 0; i < links.length; i++) {
+                tpl.params['offer' + (i + 1) + 'Button'] = (tpl.params.offerlinktext || tpl.params[links[i]]) ?
+                    manualMultipleButtonTpl({
+                        offerlinktext: tpl.params[links[i]] || tpl.params.offerlinktext,
+                        arrowRight: tpl.params.arrowRight
+                    }) :
+                    '';
+            }
+        }
+    };
 
     /**
      * Create simple templated creatives
@@ -47,6 +95,10 @@ define([
 
     Template.prototype.create = function () {
         require(['text!common/views/commercial/creatives/' + this.params.creative + '.html'], function (creativeTpl) {
+            if (preprocessTemplate[this.params.creative]) {
+                preprocessTemplate[this.params.creative](this);
+            }
+
             var creativeHtml = template(creativeTpl, this.params);
 
             $.create(creativeHtml)
