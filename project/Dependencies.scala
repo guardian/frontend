df1261b6e87b99ae7a8862f589825c36f7b9c72e
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-01-25 14:15:37 +0000

    Update play auth

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
   val nScalaTime = "com.github.nscala-time" %% "nscala-time" % "1.2.0"
   val openCsv = "net.sf.opencsv" % "opencsv" % "2.3"
   val paClient = "com.gu" %% "pa-client" % "5.0.1-NG"
-  val playGoogleAuth = "com.gu" %% "play-googleauth" % "0.3.0"
+  val playGoogleAuth = "com.gu" %% "play-googleauth" % "0.3.3"
   val quartzScheduler = "org.quartz-scheduler" % "quartz" % "2.2.1"
   val rome = "rome" % "rome" % "1.0"
   val romeModules = "org.rometools" % "rome-modules" % "1.0"
