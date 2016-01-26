33e3889be21e61471187944b1d2d3cd91a762bd0
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-01-26 10:41:52 +0000

    linting

--- static/test/javascripts/spec/common/commercial/creatives/template.spec.js
+++ static/test/javascripts/spec/common/commercial/creatives/template.spec.js
@@ -1,3 +1,4 @@
+/*eslint-disable camelcase*/
 define([
     'helpers/fixtures',
     'common/modules/commercial/creatives/template'
@@ -82,20 +83,20 @@ define([
 
         it('should create a manual single component', function () {
             var params = {
-                creative: "manual-single",
-                toneClass:  "commercial--tone-brand",
-                omnitureId: "[%omnitureid%]",
-                baseUrl: "http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify",
-                title: "title",
-                viewAllText: "View all",
-                offerTitle: "Scientists climb to bottom of Siberian sinkhole - in pictures",
-                offerImage: "http://pagead2.googlesyndication.com/pagead/imgad?id=CICAgKDjk-jQkgEQARgBMghE750kQXQwJg",
-                offerText: "A Russian research team including scientists, a medic and a professional climber has descended a giant sinkhole on the Yamal Peninsula in northern Siberia. Photographs by Vladimir Pushkarev/Siberian Times",
-                offerLinkText: "click here",
-                offerUrl: "http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify",
-                seeMoreUrl: "http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify",
-                showCtaLink: "show-cta-link",
-                clickMacro: "%%CLICK_URL_ESC%%"
+                creative: 'manual-single',
+                toneClass:  'commercial--tone-brand',
+                omnitureId: '[%omnitureid%]',
+                baseUrl: 'http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify',
+                title: 'title',
+                viewAllText: 'View all',
+                offerTitle: 'Scientists climb to bottom of Siberian sinkhole - in pictures',
+                offerImage: 'http://pagead2.googlesyndication.com/pagead/imgad?id=CICAgKDjk-jQkgEQARgBMghE750kQXQwJg',
+                offerText: 'A Russian research team including scientists, a medic and a professional climber has descended a giant sinkhole on the Yamal Peninsula in northern Siberia. Photographs by Vladimir Pushkarev/Siberian Times',
+                offerLinkText: 'click here',
+                offerUrl: 'http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify',
+                seeMoreUrl: 'http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify',
+                showCtaLink: 'show-cta-link',
+                clickMacro: '%%CLICK_URL_ESC%%'
             };
             new Template(slot, params).create().then(function () {
                 expect(document.querySelector('.commercial', slot)).not.toBe(null);
@@ -104,19 +105,19 @@ define([
 
         it('should create a manual single component with no button', function () {
             var params = {
-                creative: "manual-single",
-                toneClass:  "commercial--tone-brand",
-                omnitureId: "[%omnitureid%]",
-                baseUrl: "http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify",
-                title: "title",
-                viewAllText: "View all",
-                offerTitle: "Scientists climb to bottom of Siberian sinkhole - in pictures",
-                offerImage: "http://pagead2.googlesyndication.com/pagead/imgad?id=CICAgKDjk-jQkgEQARgBMghE750kQXQwJg",
-                offerText: "A Russian research team including scientists, a medic and a professional climber has descended a giant sinkhole on the Yamal Peninsula in northern Siberia. Photographs by Vladimir Pushkarev/Siberian Times",
-                offerUrl: "http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify",
-                seeMoreUrl: "http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify",
-                showCtaLink: "show-cta-link",
-                clickMacro: "%%CLICK_URL_ESC%%"
+                creative: 'manual-single',
+                toneClass:  'commercial--tone-brand',
+                omnitureId: '[%omnitureid%]',
+                baseUrl: 'http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify',
+                title: 'title',
+                viewAllText: 'View all',
+                offerTitle: 'Scientists climb to bottom of Siberian sinkhole - in pictures',
+                offerImage: 'http://pagead2.googlesyndication.com/pagead/imgad?id=CICAgKDjk-jQkgEQARgBMghE750kQXQwJg',
+                offerText: 'A Russian research team including scientists, a medic and a professional climber has descended a giant sinkhole on the Yamal Peninsula in northern Siberia. Photographs by Vladimir Pushkarev/Siberian Times',
+                offerUrl: 'http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify',
+                seeMoreUrl: 'http://www.theguardian.com/technology/2014/nov/20/apple-beats-music-iphone-ipad-spotify',
+                showCtaLink: 'show-cta-link',
+                clickMacro: '%%CLICK_URL_ESC%%'
             };
             new Template(slot, params).create().then(function () {
                 expect(document.querySelector('.lineitem--dfp-single .button', slot)).toBe(null);
@@ -136,33 +137,33 @@ define([
 
         it('should create a manual multiple component', function () {
             var params = {
-                creative: "manual-multiple",
-                title: "A Title",
-                explainer: "Explainer text",
-                base__url: "http://www.theguardian.com/uk",
-                offerlinktext: "Offer link text",
-                viewalltext: "View all text",
-                offeramount: "offer-amount",
-                relevance: "high",
-                Toneclass: "commercial--tone-brand",
-                offer1title: "Offer 1 Title",
-                offer1url: "http://www.theguardian.com/uk",
-                offer1meta: "Offer 1 Meta",
-                offer1image: "http://www.catgifpage.com/gifs/247.gif",
-                offer2title: "Offer 2 Title",
-                offer2url: "http://www.theguardian.com/uk",
-                offer2meta: "Offer 1 Meta",
-                offer2image: "http://www.catgifpage.com/gifs/247.gif",
-                offer3title: "Offer 3 Title",
-                offer3url: "http://www.theguardian.com/uk",
-                offer3meta: "Offer 1 Meta",
-                offer3image: "http://www.catgifpage.com/gifs/247.gif",
-                offer4title: "Offer 4 Title",
-                offer4url: "http://www.theguardian.com/uk",
-                offer4meta: "Offer 1 Meta",
-                offer4image: "http://www.catgifpage.com/gifs/247.gif",
-                omnitureId: "[%OmnitureID%]",
-                clickMacro: "%%CLICK_URL_ESC%%"
+                creative: 'manual-multiple',
+                title: 'A Title',
+                explainer: 'Explainer text',
+                base__url: 'http://www.theguardian.com/uk',
+                offerlinktext: 'Offer link text',
+                viewalltext: 'View all text',
+                offeramount: 'offer-amount',
+                relevance: 'high',
+                Toneclass: 'commercial--tone-brand',
+                offer1title: 'Offer 1 Title',
+                offer1url: 'http://www.theguardian.com/uk',
+                offer1meta: 'Offer 1 Meta',
+                offer1image: 'http://www.catgifpage.com/gifs/247.gif',
+                offer2title: 'Offer 2 Title',
+                offer2url: 'http://www.theguardian.com/uk',
+                offer2meta: 'Offer 1 Meta',
+                offer2image: 'http://www.catgifpage.com/gifs/247.gif',
+                offer3title: 'Offer 3 Title',
+                offer3url: 'http://www.theguardian.com/uk',
+                offer3meta: 'Offer 1 Meta',
+                offer3image: 'http://www.catgifpage.com/gifs/247.gif',
+                offer4title: 'Offer 4 Title',
+                offer4url: 'http://www.theguardian.com/uk',
+                offer4meta: 'Offer 1 Meta',
+                offer4image: 'http://www.catgifpage.com/gifs/247.gif',
+                omnitureId: '[%OmnitureID%]',
+                clickMacro: '%%CLICK_URL_ESC%%'
             };
             new Template(slot, params).create().then(function () {
                 expect(document.querySelector('.commercial', slot)).not.toBe(null);
@@ -171,32 +172,32 @@ define([
 
         it('should create a manual multiple component with no button', function () {
             var params = {
-                creative: "manual-multiple",
-                title: "A Title",
-                explainer: "Explainer text",
-                base__url: "http://www.theguardian.com/uk",
-                viewalltext: "View all text",
-                offeramount: "offer-amount",
-                relevance: "high",
-                Toneclass: "commercial--tone-brand",
-                offer1title: "Offer 1 Title",
-                offer1url: "http://www.theguardian.com/uk",
-                offer1meta: "Offer 1 Meta",
-                offer1image: "http://www.catgifpage.com/gifs/247.gif",
-                offer2title: "Offer 2 Title",
-                offer2url: "http://www.theguardian.com/uk",
-                offer2meta: "Offer 1 Meta",
-                offer2image: "http://www.catgifpage.com/gifs/247.gif",
-                offer3title: "Offer 3 Title",
-                offer3url: "http://www.theguardian.com/uk",
-                offer3meta: "Offer 1 Meta",
-                offer3image: "http://www.catgifpage.com/gifs/247.gif",
-                offer4title: "Offer 4 Title",
-                offer4url: "http://www.theguardian.com/uk",
-                offer4meta: "Offer 1 Meta",
-                offer4image: "http://www.catgifpage.com/gifs/247.gif",
-                omnitureId: "[%OmnitureID%]",
-                clickMacro: "%%CLICK_URL_ESC%%"
+                creative: 'manual-multiple',
+                title: 'A Title',
+                explainer: 'Explainer text',
+                base__url: 'http://www.theguardian.com/uk',
+                viewalltext: 'View all text',
+                offeramount: 'offer-amount',
+                relevance: 'high',
+                Toneclass: 'commercial--tone-brand',
+                offer1title: 'Offer 1 Title',
+                offer1url: 'http://www.theguardian.com/uk',
+                offer1meta: 'Offer 1 Meta',
+                offer1image: 'http://www.catgifpage.com/gifs/247.gif',
+                offer2title: 'Offer 2 Title',
+                offer2url: 'http://www.theguardian.com/uk',
+                offer2meta: 'Offer 1 Meta',
+                offer2image: 'http://www.catgifpage.com/gifs/247.gif',
+                offer3title: 'Offer 3 Title',
+                offer3url: 'http://www.theguardian.com/uk',
+                offer3meta: 'Offer 1 Meta',
+                offer3image: 'http://www.catgifpage.com/gifs/247.gif',
+                offer4title: 'Offer 4 Title',
+                offer4url: 'http://www.theguardian.com/uk',
+                offer4meta: 'Offer 1 Meta',
+                offer4image: 'http://www.catgifpage.com/gifs/247.gif',
+                omnitureId: '[%OmnitureID%]',
+                clickMacro: '%%CLICK_URL_ESC%%'
             };
             new Template(slot, params).create().then(function () {
                 expect(document.querySelector('.lineitem__link .button', slot)).toBe(null);
