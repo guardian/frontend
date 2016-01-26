c7759918d4a9ffc4598b974cc56f1d899d9a4991
Josh Holder <josh.holder@theguardian.com>
2016-01-26 12:22:36 +0000

    Clean up CSS

--- static/src/javascripts/projects/common/modules/ui/autoupdate.js
+++ static/src/javascripts/projects/common/modules/ui/autoupdate.js
@@ -213,8 +213,7 @@ define([
 
         fastdom.write(function () {
             // Enables the animations for injected blocks
-            $liveblogBody.addClass('autoupdate--has-animation toast-enabled');
-            $('.js-live-toolbar').remove(); // only necessary in the AB test
+            $liveblogBody.addClass('autoupdate--has-animation');
         });
     };
 });
