72bc73123300f08f619589546deba1b787dd57d5
Steve Vadocz <steve.vadocz@guardian.co.uk>
2016-01-28 16:08:32 +0000

    Removed console

--- static/src/javascripts/projects/common/modules/commercial/dfp-api.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp-api.js
@@ -673,7 +673,6 @@ define([
                     resolve(breakoutIFrame(iFrame, $slot));
                 }
             }).then(function (items) {
-                console.log('whatever');
                 return find(items, function (item) {
                     return item.adType !== '';
                 });
