94c8a9a0ab94661edf788b8635527eb180642ff6
Alex Sanders <a@snd.rs>
2016-02-01 20:12:44 +0000

    move Array.indexOf polyfill to polyfills

--- common/app/templates/inlineJS/blocking/analytics.scala.js
+++ common/app/templates/inlineJS/blocking/analytics.scala.js
@@ -245,37 +245,6 @@ try {
 
         s.t();
 
-        // Production steps of ECMA-262, Edition 5, 15.4.4.14
-        // Reference: http://es5.github.io/#x15.4.4.14
-        if (!Array.prototype.indexOf) {
-          Array.prototype.indexOf = function(searchElement, fromIndex) {
-            var k;
-            if (this == null) {
-              throw new TypeError('"this" is null or not defined');
-            }
-            var o = Object(this);
-            var len = o.length >>> 0;
-            if (len === 0) {
-              return -1;
-            }
-            var n = +fromIndex || 0;
-            if (Math.abs(n) === Infinity) {
-              n = 0;
-            }
-            if (n >= len) {
-              return -1;
-            }
-            k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-            while (k < len) {
-              if (k in o && o[k] === searchElement) {
-                return k;
-              }
-              k++;
-            }
-            return -1;
-          };
-        }
-
         var checkForPageViewInterval = setInterval(function () {
             @*
                 s_i_guardiangu-network is a globally defined Image() object created by Omniture
