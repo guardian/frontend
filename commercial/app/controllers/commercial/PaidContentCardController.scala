abb5d3edb9a3ff2e99a2bf02dab8f6569b76a40e
Zofia <zofia.korcz@guardian.co.uk>
2016-02-02 18:21:14 +0000

    test page done

--- commercial/app/controllers/commercial/PaidContentCardController.scala
+++ commercial/app/controllers/commercial/PaidContentCardController.scala
@@ -1,52 +1,37 @@
 package controllers.commercial
 
 import common.{ExecutionContexts, Logging}
-import model.commercial.{CapiAgent, Lookup}
+import model.commercial.Lookup
 import model.{Cached, NoCache}
 import performance.MemcachedAction
 import play.api.mvc._
-
-import scala.concurrent.Future
-import scala.util.control.NonFatal
+import views.support.Item300
 
 object PaidContentCardController extends Controller with ExecutionContexts with implicits.Requests with Logging {
 
   private def renderCard(format: Format) = MemcachedAction { implicit request =>
 
-    val optKeyword = request.getParameter("keyword")
-    val articleUrl = request.getParameter("articleUrl")
-    val brandLogo = request.getParameter("brandLogo")
-    val brand = request.getParameter("brand")
-    val optClickMacro = request.getParameter("clickMacro")
-    val optOmnitureId = request.getParameter("omnitureId")
+    val shortUrl = request.getParameter("articleUrl")
 
-    val eventualLatest = optKeyword.map { keyword =>
-      // getting twice as many, as we filter out content without images
-      Lookup.latestContentByKeyword(keyword, 8)
-      //Lookup.contentByShortUrls(articleUrl)
-    }.getOrElse(Future.successful(Nil))
+    Lookup.contentByShortUrls(shortUrl.toList) map { contents =>
+      contents.headOption map { content =>
+        val articleUrl = content.metadata.webUrl
+        val articleTitle = request.getParameter("articleHeaderText") getOrElse content.metadata.webTitle
+        val articleText = request.getParameter("articleText") orElse content.fields.trailText
+        val pictureUrl: Option[String] = request.getParameter("articleImage") orElse (content.trail.trailPicture.flatMap(Item300.bestFor))
+        val brandLogo = request.getParameter("brandLogo")
+        val brand = request.getParameter("brand")
+        val linkLabel = request.getParameter("linkLabel")
+        val optClickMacro = request.getParameter("clickMacro")
+        val optOmnitureId = request.getParameter("omnitureId")
+        val trackingPixel = request.getParameter("trackingPixel")
+        val cacheBuster = request.getParameter("cacheBuster")
 
-    eventualLatest onFailure {
-      case NonFatal(e) => log.error(s"Looking up content by keyword failed: ${e.getMessage}")
-    }
-
-    val eventualSpecific = CapiAgent.contentByShortUrls(specificIds)
-
-    eventualSpecific onFailure {
-      case NonFatal(e) => log.error(s"Looking up content by short URL failed: ${e.getMessage}")
-    }
-
-    val futureContents = for {
-      specific <- eventualSpecific
-      latestByKeyword <- eventualLatest
-    } yield {
-      (specific ++ latestByKeyword.filter(_.trail.trailPicture.nonEmpty)).distinct take 4
-    }
-
-    futureContents map {
-      case Nil => NoCache(format.nilResult)
-      case contents => Cached(componentMaxAge) {
-        format.result(views.html.paidContent.card(contents.head, brandLogo, brand, optClickMacro, optOmnitureId))
+        Cached(componentMaxAge) {
+          format.result(views.html.paidContent.card(articleUrl, articleTitle, articleText, pictureUrl, brandLogo, brand, linkLabel, optClickMacro, optOmnitureId, trackingPixel, cacheBuster))
+        }
+      } getOrElse {
+        NoCache(format.nilResult)
       }
     }
   }
