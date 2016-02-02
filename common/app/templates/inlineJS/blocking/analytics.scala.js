fc4bb79fbbec57e861719f50da7ffeeed11a086f
Alex Sanders <a@snd.rs>
2016-02-02 12:03:11 +0000

    formatting

--- common/app/templates/inlineJS/blocking/analytics.scala.js
+++ common/app/templates/inlineJS/blocking/analytics.scala.js
@@ -15,9 +15,8 @@ try {
         var R2_STORAGE_KEY = 's_ni', // DO NOT CHANGE THIS, ITS IS SHARED WITH R2. BAD THINGS WILL HAPPEN!
             NG_STORAGE_KEY = 'gu.analytics.referrerVars',
             standardProps = 'channel,prop1,prop2,prop3,prop4,prop8,prop9,prop10,prop13,prop25,prop31,prop37,prop38,prop47,' +
-            'prop51,prop61,prop64,prop65,prop74,prop40,prop63,eVar7,eVar37,eVar38,eVar39,eVar50,eVar24,eVar60,eVar51,' +
-            'eVar31,eVar18,eVar32,eVar40,list1,list2,list3,events';
-
+                'prop51,prop61,prop64,prop65,prop74,prop40,prop63,eVar7,eVar37,eVar38,eVar39,eVar50,eVar24,eVar60,eVar51,' +
+                'eVar31,eVar18,eVar32,eVar40,list1,list2,list3,events';
 
         var getChannel = function () {
             if (config.page.contentType === 'Network Front') {
@@ -69,7 +68,7 @@ try {
             return decodeURIComponent(escape(utilAtob(str.replace(/-/g, '+').replace(/_/g, '/').replace(/,/g, '='))));
         }
 
-        @* http://www.electrictoolbox.com/pad-number-zeroes-javascript/ *@
+        // http://www.electrictoolbox.com/pad-number-zeroes-javascript/
         var pad = function (number, length) {
             var str = '' + number;
             while (str.length < length) {
@@ -81,7 +80,7 @@ try {
         s.trackingServer = 'hits.theguardian.com';
         s.trackingServerSecure = 'hits-secure.theguardian.com';
 
-        /* Omniture library version */
+        // Omniture library version
         s.prop62    = 'Guardian JS-1.4.1 20140914';
 
         // http://www.scribd.com/doc/42029685/15/cookieDomainPeriods
@@ -91,13 +90,9 @@ try {
 
         s.ce = 'UTF-8';
         s.pageName  = config.page.analyticsName;
-
         s.prop1     = config.page.headline || '';
-
         s.prop3     = config.page.publication || '';
-
         s.prop6     = config.page.author || '';
-
         s.prop13    = config.page.series || '';
 
         // getting clientWidth causes a reflow, so avoid using if possible
@@ -110,9 +105,7 @@ try {
         s.prop8     = config.page.pageCode || '';
         s.prop9     = config.page.contentType || '';
         s.prop10    = config.page.tones || '';
-
         s.prop25    = config.page.blogs || '';
-
         s.channel   = getChannel();
 
         if (isEmbed) {
@@ -174,18 +167,15 @@ try {
 
         s.prop56    = guardian.isModernBrowser ? 'Javascript' : 'Partial Javascript';
 
-        /* Set Time Parting Day and Hour Combination - 0 = GMT */
+        // Set Time Parting Day and Hour Combination - 0 = GMT
         s.prop20    = tpA[2] + ':' + tpA[1];
         s.eVar20    = 'D=c20';
 
-        /*
-          eVar1 contains today's date
-          in the Omniture backend it only ever holds the first
-          value a user gets, so in effect it is the first time
-          we saw this user
-        */
+        // eVar1 contains today's date
+        // in the Omniture backend it only ever holds the first
+        // value a user gets, so in effect it is the first time
+        // we saw this user
         s.eVar1 = now.getFullYear() + '/' + pad(now.getMonth() + 1, 2) + '/' + pad(now.getDate(), 2);
-
         s.prop7     = webPublicationDate ? new Date(webPublicationDate).toISOString().substr(0, 10).replace(/-/g, '/') : '';
 
         if (webPublicationDate) {
@@ -196,7 +186,7 @@ try {
 
         s.prop47    = config.page.edition || '';
 
-        /* Retrieve navigation interaction data */
+        // Retrieve navigation interaction data
         var ni;
         try {
             ni = window.sessionStorage.getItem(NG_STORAGE_KEY);
@@ -213,7 +203,7 @@ try {
         s.eVar31    = getUserFromCookie() ? 'registered user' : 'guest user';
 
         /* can this go ?? */
-        //s.prop40    = detect.adblockInUse() || detect.getFirefoxAdblockPlusInstalled();
+        // s.prop40    = detect.adblockInUse() || detect.getFirefoxAdblockPlusInstalled();
 
         if (ni) {
             var d = new Date().getTime();
@@ -238,19 +228,14 @@ try {
         s.linkTrackVars = standardProps;
         s.linkTrackEvents = 'None';
 
-        /*
-            this makes the call to Omniture.
-            `s.t()` records a page view so should only be called once
-        */
-
+        // this makes the call to Omniture.
+        // `s.t()` records a page view so should only be called once
         s.t();
 
         var checkForPageViewInterval = setInterval(function () {
-            @*
-                s_i_guardiangu-network is a globally defined Image() object created by Omniture
-                It does not sit in the DOM tree, and seems to be the only surefire way
-                to check if the intial beacon has been successfully sent
-            *@
+            // s_i_guardiangu-network is a globally defined Image() object created by Omniture
+            // It does not sit in the DOM tree, and seems to be the only surefire way
+            // to check if the intial beacon has been successfully sent
             var img = window['s_i_' + window.s_account.split(',').join('_')];
             if (typeof (img) !== 'undefined' && (img.complete === true || img.width + img.height > 0)) {
                 clearInterval(checkForPageViewInterval);
