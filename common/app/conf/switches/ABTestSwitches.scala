1e2e44afebd1182f0149816577860a145a33b434
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2016-02-02 11:39:05 +0000

    set up userzoom email survery for mobile

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -84,4 +84,13 @@ trait ABTestSwitches {
     sellByDate = new LocalDate(2016, 2, 11),
     exposeClientSide = true
   )
+
+  val UserzoomSurveyMessageMobileV3 = Switch(
+    "A/B Tests",
+    "ab-userzoom-survey-message--mobile-v3",
+    "Segment the userzoom data-team survey",
+    safeState = Off,
+    sellByDate = new LocalDate(2016, 2, 7),
+    exposeClientSide = true
+  )
 }
