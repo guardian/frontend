26fddbe9c782a5460ad984c3421e7586d1d75bc2
Oliver Joseph Ash <oliverjash@gmail.com>
2016-02-03 12:23:14 +0000

    Test all share link formats for block level

--- applications/test/ShareLinksTest.scala
+++ applications/test/ShareLinksTest.scala
@@ -33,7 +33,7 @@ import org.scalatest.concurrent.{Futures, ScalaFutures}
     }
   }
 
-  it should "provide valid block-level campaign-links in the correct order" in {
+  it should "provide valid block-level campaign-links in the correct order for galleries" in {
     val response = getResponse(
       conf.LiveContentApi.item("environment/gallery/2014/oct/22/2014-wildlife-photographer-of-the-year", edition)
     )
@@ -44,11 +44,32 @@ import org.scalatest.concurrent.{Futures, ScalaFutures}
         val gallery = model.Content(apiContent)
         val elementShares = gallery.sharelinks.elementShares(Some("2"))
 
-        elementShares.map(_.text) should be (List("Facebook", "Twitter", "Pinterest"))
-        elementShares.map(_.href) should be (List(
+        elementShares.map(_.text) should be(List("Facebook", "Twitter", "Pinterest"))
+        elementShares.map(_.href) should be(List(
           "https://www.facebook.com/dialog/share?app_id=202314643182694&href=http%3A%2F%2Fwww.theguardian.com%2Fenvironment%2Fgallery%2F2014%2Foct%2F22%2F2014-wildlife-photographer-of-the-year%3Fpage%3Dwith%3A2%26CMP%3Dshare_btn_fb%232&redirect_uri=http%3A%2F%2Fgu.com%2Fp%2F42jcb",
           "https://twitter.com/intent/tweet?text=2014%20Wildlife%20photographer%20of%20the%20Year&url=http%3A%2F%2Fwww.theguardian.com%2Fenvironment%2Fgallery%2F2014%2Foct%2F22%2F2014-wildlife-photographer-of-the-year%3Fpage%3Dwith%3A2%26CMP%3Dshare_btn_tw%232",
           "http://www.pinterest.com/pin/create/button/?description=2014+Wildlife+photographer+of+the+Year&url=http%3A%2F%2Fwww.theguardian.com%2Fenvironment%2Fgallery%2F2014%2Foct%2F22%2F2014-wildlife-photographer-of-the-year%3Fpage%3Dwith%3A2%232&media="))
+      }
+    }
+  }
+
+  it should "provide valid block-level campaign-links in the correct order for liveblogs" in {
+    val response = getResponse(
+      conf.LiveContentApi.item("politics/blog/live/2016/feb/03/eu-renegotiation-pmqs-cameron-corbyn-he-prepares-to-make-statement-to-mps-politics-live", edition)
+    )
+
+    whenReady(response) { item: ItemResponse =>
+
+      item.content.map { apiContent =>
+        val liveblog = model.Content(apiContent)
+        val elementShares = liveblog.sharelinks.elementShares(Some("2"))
+
+        elementShares.map(_.text) should be (List("Facebook", "Twitter", "Google plus"))
+        elementShares.map(_.href) should be (List(
+          "https://www.facebook.com/dialog/share?app_id=202314643182694&href=http%3A%2F%2Fwww.theguardian.com%2Fpolitics%2Fblog%2Flive%2F2016%2Ffeb%2F03%2Feu-renegotiation-pmqs-cameron-corbyn-he-prepares-to-make-statement-to-mps-politics-live%3Fpage%3Dwith%3A2%26CMP%3Dshare_btn_fb%232&redirect_uri=http%3A%2F%2Fgu.com%2Fp%2F4gc8j",
+          "https://twitter.com/intent/tweet?text=Obama%20reaffirms%20his%20support%20for%20Britain%20remaining%20in%20EU%20-%20Politics%20live&url=http%3A%2F%2Fwww.theguardian.com%2Fpolitics%2Fblog%2Flive%2F2016%2Ffeb%2F03%2Feu-renegotiation-pmqs-cameron-corbyn-he-prepares-to-make-statement-to-mps-politics-live%3Fpage%3Dwith%3A2%26CMP%3Dshare_btn_tw%232",
+          "https://plus.google.com/share?url=http%3A%2F%2Fwww.theguardian.com%2Fpolitics%2Fblog%2Flive%2F2016%2Ffeb%2F03%2Feu-renegotiation-pmqs-cameron-corbyn-he-prepares-to-make-statement-to-mps-politics-live%3Fpage%3Dwith%3A2%26CMP%3Dshare_btn_gp%232&amp;hl=en-GB&amp;wwc=1"
+        ))
       }
     }
   }
