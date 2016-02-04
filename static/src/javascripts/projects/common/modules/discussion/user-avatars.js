35260126bd752d8532b9cf939a5693c698fc7f1a
Nicolas Long <nicolaslong@gmail.com>
2016-02-04 14:53:26 +0000

    Fix profile avatars
    At the moment, when viewing someone else's profile the user's own avatar
    is shown. This commit fixes that by ensuring we only call
    /avatars/user/me if the user is viewing their own profile. Otherwise,
    the deterministic avatar url is used. As a result, this change will also
    significant reduce the number of calls to the Avatar API.

--- static/src/javascripts/projects/common/modules/discussion/user-avatars.js
+++ static/src/javascripts/projects/common/modules/discussion/user-avatars.js
@@ -1,8 +1,9 @@
 define([
     'common/utils/$',
     'bonzo',
-    'common/modules/avatar/api'
-], function ($, bonzo, avatarApi) {
+    'common/modules/avatar/api',
+    'common/utils/config'
+], function ($, bonzo, avatarApi, config) {
 
     function init() {
         $('.user-avatar').each(avatarify);
@@ -12,7 +13,13 @@ define([
         var container = bonzo(el),
             updating = bonzo(bonzo.create('<div class="is-updating"></div>')),
             avatar = bonzo(bonzo.create('<img class="user-avatar__image" alt="" />')),
-            userId = container.data('userid');
+            userId = container.data('userid'),
+            ownAvatar = userId === parseInt(config.user.id);
+
+        var updateCleanup = function () {
+            updating.remove();
+            avatar.appendTo(container);
+        };
 
         container
             .removeClass('is-hidden');
@@ -21,16 +28,20 @@ define([
             .css('display', 'block')
             .appendTo(container);
 
-        avatarApi.getActive()
-            .then(function (response) {
-                avatar.attr('src', response.data.avatarUrl);
-                updating.remove();
-                avatar.appendTo(container);
-            }, function () {
-                avatar.attr('src', avatarApi.deterministicUrl(userId));
-                updating.remove();
-                avatar.appendTo(container);
-            });
+        if (ownAvatar) {
+            avatarApi.getActive()
+                .then(function (response) {
+                    avatar.attr('src', response.data.avatarUrl);
+                }, function () {
+                    avatar.attr('src', avatarApi.deterministicUrl(userId));
+                })
+                .always(function () {
+                    updateCleanup();
+                });
+        } else {
+            avatar.attr('src', avatarApi.deterministicUrl(userId));
+            updateCleanup();
+        }
     }
 
     return {init: init, avatarify: avatarify};
