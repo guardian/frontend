003e842430913f3c2390abf9f4bb4123223ab548
Alex Sanders <a@snd.rs>
2016-02-05 14:48:01 +0000

    this is play

--- common/app/templates/inlineJS/blocking/shouldEnhance.scala.js
+++ common/app/templates/inlineJS/blocking/shouldEnhance.scala.js
@@ -57,13 +57,8 @@
     window.shouldEnhance = mustNotEnhance() ? false : mustEnhance() ? true : couldEnhance() && weWantToEnhance();
 
     // just so we can tell…
-    try {
-        if (window.guardian.config.page.isDev) {
-            var console = window.console;
-            console && console.info && console.info(`THIS IS ${window.shouldEnhance ? 'ENHANCED' : 'STANDARD ONLY'}`);
-        }
-    } catch (e) {
-        // do nothing
+    @if(play.Play.isDev()) {
+        window.console && window.console.info(`THIS IS ${window.shouldEnhance ? 'ENHANCED' : 'STANDARD ONLY'}`);
     }
 })(window);
 
