3c974736354b56404cf8bb160f69e5cb37c7bde6
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-02-05 15:15:57 +0000

    Reverted to inserting the ad *after* the first spot, to prevent the ad from being the very first thing appearing

--- static/src/javascripts/projects/common/modules/commercial/liveblog-dynamic-adverts.js
+++ static/src/javascripts/projects/common/modules/commercial/liveblog-dynamic-adverts.js
@@ -61,7 +61,7 @@ define([
     function insertAds(slots) {
         for (var i = 0; i < slots.length && slotCounter < MAX_ADS; i++) {
             var $adSlot = bonzo(createAdSlot('inline1' + slotCounter++, 'liveblog-inline'));
-            $adSlot.insertBefore(slots[i]);
+            $adSlot.insertAfter(slots[i]);
             dfp.addSlot($adSlot);
         }
     }
