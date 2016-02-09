8213fb5ea2c0bf67a8c6ac654b714f8c427ae0ac
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-02-09 16:46:05 +0000

    Linting

--- static/src/javascripts/projects/common/modules/commercial/creatives/add-tracking-pixel.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/add-tracking-pixel.js
@@ -1,4 +1,4 @@
-define(function() {
+define(function () {
     function addTrackingPixel($adSlot, url) {
         $adSlot.before('<img src="' + encodeURI(url) + '" class="creative__tracking-pixel">');
     }
