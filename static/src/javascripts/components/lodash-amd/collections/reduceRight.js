41092256d4acd3df1adfacb842697ec00ec4911c
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-02-10 15:34:06 +0000

    Replaced lodash with own version 2.4.2

--- static/src/javascripts/components/lodash-amd/collections/reduceRight.js
+++ static/src/javascripts/components/lodash-amd/collections/reduceRight.js
@@ -1,10 +1,10 @@
 /**
- * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Lo-Dash 2.4.2 (Custom Build) <https://lodash.com/>
  * Build: `lodash modularize exports="amd" -o ./compat/`
  * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
- * Available under MIT license <http://lodash.com/license>
+ * Available under MIT license <https://lodash.com/license>
  */
 define(['../functions/createCallback', './forEachRight'], function(createCallback, forEachRight) {
 
