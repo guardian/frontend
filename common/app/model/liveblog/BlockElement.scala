b319454aadbec23467b708590ee9f481b79b1fc3
Sam Desborough <sam.desborough@gmail.com>
2016-02-15 16:43:12 +0000

    Add image block elements

--- common/app/model/liveblog/BlockElement.scala
+++ common/app/model/liveblog/BlockElement.scala
@@ -1,12 +1,13 @@
 package model.liveblog
 
-import com.gu.contentapi.client.model.v1.ElementType._
-import com.gu.contentapi.client.model.v1.{BlockElement => ApiBlockElement}
-import model.{AudioAsset, VideoAsset}
+import com.gu.contentapi.client.model.v1.ElementType.{Text, Image, Audio, Video, Embed}
+import com.gu.contentapi.client.model.v1.{BlockElement => ApiBlockElement, ImageElementFields}
+import model.{ImageAsset, AudioAsset, VideoAsset}
 import play.api.libs.json._
 
 sealed trait BlockElement
 case class TextBlockElement(html: Option[String]) extends BlockElement
+case class ImageBlockElement(assets: Seq[ImageAsset], data: Map[String, String], displayCredit: Option[Boolean]) extends BlockElement
 case class AudioBlockElement(assets: Seq[AudioAsset]) extends BlockElement
 case class VideoBlockElement(assets: Seq[VideoAsset]) extends BlockElement
 case class EmbedBlockElement(html: Option[String], safe: Option[Boolean], alt: Option[String]) extends BlockElement
@@ -20,6 +21,16 @@ object BlockElement {
   def make(element: ApiBlockElement): Option[BlockElement] = {
     element.`type` match {
       case Text => Some(TextBlockElement(element.textTypeData.flatMap(_.html)))
+      case Image => Some(ImageBlockElement(
+        element.assets.zipWithIndex.map { case (a, i) => ImageAsset.make(a, i) },
+        element.imageTypeData.map { d => Map(
+          "copyright" -> d.copyright,
+          "alt" -> d.alt,
+          "caption" -> d.caption,
+          "credit" -> d.credit
+        ) collect { case (k, Some(v)) => (k,v) }} getOrElse Map(),
+        element.imageTypeData.flatMap(_.displayCredit)
+      ))
       case Audio => Some(AudioBlockElement(element.assets.map(AudioAsset.make)))
       case Video => Some(VideoBlockElement(element.assets.map(VideoAsset.make)))
       case Embed => element.embedTypeData.map(d => EmbedBlockElement(d.html, d.safeEmbedCode, d.alt))
