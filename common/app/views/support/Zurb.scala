22e6b2bd137ae5b3558342d387566b00e148b409
Sam Desborough <sam.desborough@gmail.com>
2016-02-17 16:24:19 +0000

    Add Zurb helper

---
+++ common/app/views/support/Zurb.scala
@@ -0,0 +1,28 @@
+package views.support
+
+import play.twirl.api.Html
+
+object Zurb {
+  def columnNumber(n: Int): String = {
+    Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve").lift(n - 1).getOrElse("")
+  }
+
+  def row(inner: Html): Html = Html {
+    s"""<table class="row">
+          <tr>$inner</tr>
+        </table>"""
+  }
+
+  def columns(n: Int, classes: Seq[String] = Seq(), last: Boolean = false)(inner: Html): Html = Html {
+    s"""<td class="wrapper ${if (last || n == 12) "last"}">
+      <table class="${columnNumber(n)} columns">
+        <tr>
+          <td>$inner</td>
+          <td class="expander"></td>
+        </tr>
+      </table>
+    </td>"""
+  }
+
+  def fullRow(inner: Html): Html = row(columns(12)(inner))
+}
