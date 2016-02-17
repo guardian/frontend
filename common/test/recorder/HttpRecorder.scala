4c7608f2c42889737e1e8607013ba0788febf540
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-02-17 13:20:35 +0000

    Log the header string when there is a test error

--- common/test/recorder/HttpRecorder.scala
+++ common/test/recorder/HttpRecorder.scala
@@ -23,7 +23,7 @@ trait HttpRecorder[A] extends ExecutionContexts {
     // integration test environment
     // make sure people have checked in test files
     if (Configuration.environment.stage.equalsIgnoreCase("DEVINFRA") && !new File(baseDir, fileName).exists()) {
-      throw new IllegalStateException(s"Data file has not been checked in for: $url, file: $fileName")
+      throw new IllegalStateException(s"Data file has not been checked in for: $url, file: $fileName, headers: ${headersFormat(headers)}")
     }
 
     get(fileName).map { f =>
