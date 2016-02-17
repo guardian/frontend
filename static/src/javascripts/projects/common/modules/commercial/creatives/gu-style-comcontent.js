51731f1b8c62ec56b9699dc3971f94bf8915112a
Zofia <zofia.korcz@guardian.co.uk>
2016-02-17 14:35:36 +0000

    variables added

--- static/src/javascripts/projects/common/modules/commercial/creatives/gu-style-comcontent.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/gu-style-comcontent.js
@@ -25,12 +25,25 @@ define([
     var GustyleComcontent = function ($adSlot, params) {
         this.$adSlot = $adSlot;
         this.params  = params;
+        this.contentPosition = {
+            bottom: 'bottom: 30px;',
+            top: 'top: 30px;'
+        };
+        this.fontSize = {
+            small: 'font-size: 0.9rem; line-height: 0.9rem;',
+            regular: 'font-size: 1.25rem;',
+            big: 'font-size: 1.35rem;'
+        };
     };
 
     GustyleComcontent.prototype.create = function () {
         var externalLinkIcon = svgs('externalLink', ['gu-external-icon']),
             templateOptions = {
-                clickMacro: this.params.clickMacro,
+                articleContentColor: this.params.articleContentColor === 'white' ? 'color: #ffffff;' : 'color: #000000;',
+                articleContentPosition: this.contentPosition[this.params.articleContentPosition],
+                articleHeaderFontSize: this.fontSize[this.params.articleHeaderFontSize],
+                articleTextFontSize: this.fontSize[this.params.articleTextFontSize],
+                brandLogoPosition: this.params.brandLogoPosition === 'bottom-right' ? 'right: 10px; bottom: 10px; left: auto;' : 'left: 10px; top: 0; right: auto;',
                 externalLinkIcon: externalLinkIcon
             };
 
