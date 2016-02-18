bd0217ea0ed16c107f4cb2a4cf151407ad233433
Sam Desborough <sam.desborough@gmail.com>
2016-02-18 14:37:43 +0000

    Refactor InlineStyles and add tests

--- common/app/common/InlineStyles.scala
+++ common/app/common/InlineStyles.scala
@@ -3,19 +3,21 @@ package common
 import java.io.StringReader
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import com.steadystate.css.parser.{SACParserCSS3, CSSOMParser}
 import org.w3c.css.sac.InputSource
 import org.w3c.dom.css.{CSSRule => W3CSSRule, CSSRuleList}
 import play.twirl.api.Html
 
 import scala.collection.JavaConversions._
+import scala.collection.immutable.ListMap
 import scala.util.Try
 
-case class CSSRule(selector: String, styles: String) {
+case class CSSRule(selector: String, styles: ListMap[String, String]) {
   val canInline = !selector.contains(":")
 
   // https://www.w3.org/TR/css3-selectors/#specificity
-  lazy val specifity: Int = {
+  val specifity: Int = {
     val ids = selector.count(_ == '#')
     val classes = selector.count(_ == '.')
     val attributes = selector.count(_ == '[')
@@ -25,7 +27,7 @@ case class CSSRule(selector: String, styles: String) {
     Seq(ids, (classes + attributes + pseudos), tags).map(_.toString).mkString.toInt
   }
 
-  override def toString() = s"$selector { $styles; }"
+  override def toString() = s"$selector { ${CSSRule.styleString(styles)} }"
 }
 
 object CSSRule {
@@ -33,12 +35,25 @@ object CSSRule {
     val rule = r.getCssText.split("\\{")
 
     for {
-      selectors <- Try(rule(0)).toOption
-      styles <- Try(rule(1)).toOption
+      selectors <- rule.lift(0)
+      styles <- rule.lift(1)
     } yield {
-      selectors.split(",").map(selector => CSSRule(selector.trim, styles.stripSuffix("}").trim))
+      selectors.split(",").map(selector => CSSRule(selector.trim, makeStyles(styles.stripSuffix("}").trim)))
     }
   }
+
+  def makeStyles(styles: String): ListMap[String, String] = {
+     styles.split(";").flatMap { style =>
+      val split = style.split(":")
+
+      for {
+        property <- split.lift(0)
+        value <- split.lift(1)
+      } yield property.trim -> value.trim
+    }.foldLeft(ListMap.empty[String, String])(_ + _)
+  }
+
+  def styleString(styles: ListMap[String, String]): String = styles.map { case (k, v) => s"$k: $v" }.mkString("; ")
 }
 
 object InlineStyles {
@@ -56,34 +71,46 @@ object InlineStyles {
     */
   def apply(html: Html): Html = {
     val document = Jsoup.parse(html.body)
+    val (inline, head) = styles(document)
 
-    Html(document.getElementsByTag("style").foldLeft(document) { (document, element) =>
-      val newDocument = document.clone
+    document.getElementsByTag("head").headOption map { el =>
+      el.getElementsByTag("style").headOption map(_.remove)
+      el.appendChild(document.createElement("style").text(head.mkString("\n")))
+    }
+
+    inline sortBy(_.specifity) foreach { rule =>
+      document.select(rule.selector) foreach(el => el.attr("style", mergeStyles(rule, el.attr("style"))))
+    }
+
+    Html(document.toString)
+  }
+
+  /**
+    * Convert the styles in a document's <style> tags to a pair.
+    * The first item is the styles that should stay in the head, the second is everything that should be inlined.
+    */
+  def styles(document: Document): (Seq[CSSRule], Seq[String]) = {
+    document.getElementsByTag("style").foldLeft((Seq.empty[CSSRule], Seq.empty[String])) { case ((inline, head), element) =>
       val source = new InputSource(new StringReader(element.html))
 
       Try(cssParser.parseStyleSheet(source, null, null)).toOption map { sheet =>
         val (mediaQueries, styles) = seq(sheet.getCssRules).partition(isMediaQuery)
-        val (inline, head) = styles.flatMap(CSSRule.fromW3).flatten.partition(_.canInline)
-        val style = (head.map(_.toString) ++ mediaQueries.map(_.getCssText))
+        val (inlineStyles, headStyles) = styles.flatMap(CSSRule.fromW3).flatten.partition(_.canInline)
+        val newHead = (headStyles.map(_.toString) ++ mediaQueries.map(_.getCssText)).mkString("\n")
 
-        newDocument.getElementsByTag("head").headOption map { el =>
-          el.getElementsByTag("style").headOption map(_.remove)
+        (inline ++ inlineStyles, (head :+ newHead).filter(_.nonEmpty))
+      } getOrElse {
+        println(inline)
+        (inline, head :+ element.html)
+      }
+    }
+  }
 
-          if (style.nonEmpty) {
-            el.appendChild(newDocument.createElement("style").text(style.mkString("\n")))
-          }
-        }
-
-        inline sortBy(_.specifity) foreach { rule =>
-          newDocument.select(rule.selector) foreach { el =>
-            val style = Seq(el.attr("style"), rule.styles) filter (_.nonEmpty) mkString ("; ")
-            el.attr("style", style)
-          }
-        }
-
-        newDocument
-      } getOrElse document
-    }.toString)
+  def mergeStyles(rule: CSSRule, existing: String): String = {
+    CSSRule.styleString(rule.styles.foldLeft(CSSRule.makeStyles(existing)) { case (style, (property, value)) =>
+      if (style.get(property).exists(_.contains("!important")) && !value.contains("!important")) style
+      else style + (property -> value)
+    })
   }
 
   private def seq(rules: CSSRuleList): Seq[W3CSSRule] = for (i <- 0 until rules.getLength) yield rules.item(i)
--- common/test/common/InlineStylesTest.scala
+++ common/test/common/InlineStylesTest.scala
@@ -3,16 +3,62 @@ package common
 import org.scalatest.{FlatSpec, Matchers}
 
 class InlineStylesTest extends FlatSpec with Matchers {
+  val stub: Map[String, String] = Map.empty
+
   // https://www.w3.org/TR/css3-selectors/#specificity
   it should "calculate specifity" in {
-    CSSRule("*", "").specifity should be(0)
-    CSSRule("LI", "").specifity should be(1)
-    CSSRule("UL LI", "").specifity should be(2)
-    CSSRule("UL OL +LI", "").specifity should be(3)
-    CSSRule("H1 + *[REL = up]", "").specifity should be(11)
-    CSSRule("UL OL LI.red", "").specifity should be(13)
-    CSSRule("LI.red.level", "").specifity should be(21)
-    CSSRule("#x34y", "").specifity should be(100)
-    CSSRule("#s12: not (FOO)", "").specifity should be(101)
+    CSSRule("*", stub).specifity should be(0)
+    CSSRule("LI", stub).specifity should be(1)
+    CSSRule("UL LI", stub).specifity should be(2)
+    CSSRule("UL OL +LI", stub).specifity should be(3)
+    CSSRule("H1 + *[REL = up]", stub).specifity should be(11)
+    CSSRule("UL OL LI.red", stub).specifity should be(13)
+    CSSRule("LI.red.level", stub).specifity should be(21)
+    CSSRule("#x34y", stub).specifity should be(100)
+    CSSRule("#s12: not (FOO)", stub).specifity should be(101)
+  }
+
+  it should "parse the correct styles" in {
+    val document = Jsoup.parse(
+      """
+        |<html>
+        |<head>
+        |<style>
+        |h1 { color: red; }
+        |h2 { color: blue !important; border: 10px dashed aquamarine; }
+        |</style>
+        |<style>
+        |h1 { color: green; border: 1px solid black; }
+        |h2 { color: yellow; border: 1px solid orange; }
+        |a:hover { color: blue; }
+        |</style>
+        |</head>
+        |<body>
+        |<h1>Hello</h1>
+        |<h2>Goodbye</h2>
+        |</body>
+        |</html>""".stripMargin
+    )
+
+    val (toInline, head) = InlineStyles.styles(document)
+
+    toInline should be(Seq(
+      CSSRule("h1", Map("color" -> "red")),
+      CSSRule("h2", Map("color" -> "blue !important", "border" -> "10px dashed aquamarine")),
+      CSSRule("h1", Map("color" -> "green", "border" -> "1px solid black")),
+      CSSRule("h2", Map("color" -> "yellow", "border" -> "1px solid orange"))
+    ))
+
+    head should be(Seq("a:hover { color: blue }"))
+  }
+
+  it should "merge styles correctly" in {
+    val toAdd = CSSRule("h2", Map("color" -> "blue", "border" -> "10px dashed aquamarine"))
+    val existing = "color: red !important; border: 5px dashed yellow"
+    val toAdd2 = CSSRule("td", Map("padding-right" -> "0px"))
+    val existing2 = "margin: 0; hyphens: none; vertical-align: top; color: rgb(34, 34, 34); font-family: Helvetica, Arial, sans-serif; padding-right: 0px; line-height: 19px; -moz-hyphens: none; position: relative; padding: 10px 20px 0px 0px; text-align: left; word-break: break-word; border-collapse: collapse !important; font-weight: normal; -webkit-hyphens: none; font-size: 14px"
+
+    InlineStyles.mergeStyles(toAdd, existing) should be("color: red !important; border: 10px dashed aquamarine")
+    InlineStyles.mergeStyles(toAdd2, existing2) should be("padding: 10px 10px 10px 10px; padding-left: 0px")
   }
 }
