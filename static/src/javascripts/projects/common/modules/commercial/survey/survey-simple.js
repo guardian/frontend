d8052948c31ee0df16052d87da646df41f2000ce
Steve Vadocz <steve.vadocz@guardian.co.uk>
2016-02-22 17:00:01 +0000

    Validation

--- static/src/javascripts/projects/common/modules/commercial/survey/survey-simple.js
+++ static/src/javascripts/projects/common/modules/commercial/survey/survey-simple.js
@@ -17,7 +17,7 @@ define([
         this.bannerTmpl = template(surveySimpleTemplate,
             {
                 surveyHeader: 'Take control of commercial messages',
-                surveyText: "For <em>£5 a month</em>  you can curate which Guardian products and services we show you on the Guardian site. So if you don't want to see Soulmates or Jobs again, you don't have to, you can just support our journalism.",
+                surveyText: 'For <em>£5 a month</em>  you can curate which Guardian products and services we show you on the Guardian site. So if you don\'t want to see Soulmates or Jobs again, you don\'t have to, you can just support our journalism.',
                 linkText: 'Sign-up now',
                 surveyLink: '/commercial/survey-simple',
                 arrowWhiteRight: svgs('arrowWhiteRight'),
