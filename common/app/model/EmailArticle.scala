65282356d52b522c3ee7eec83502c052ced67db3
Sam Desborough <sam.desborough@gmail.com>
2016-02-23 15:47:56 +0000

    Add email banners

---
+++ common/app/model/EmailArticle.scala
@@ -0,0 +1,27 @@
+package model
+
+import conf.Static
+
+sealed trait EmailContent {
+  def banner: String
+  def test(a: Article): Boolean
+}
+
+case object TheFiver extends EmailContent {
+  val banner = "the-fiver.png"
+  def test(a: Article) = a.content.tags.series.exists(_.id == "football/series/thefiver")
+}
+
+object EmailArticle {
+  private val defaultBanner = "default.jpg"
+  private val allEmails     = Seq(TheFiver)
+
+  implicit class EmailArticleAddons(a: Article) {
+    lazy val banner = {
+      val banner = emailFor(a) map (_.banner) getOrElse defaultBanner
+      Static(s"images/email/banners/$banner").path
+    }
+  }
+
+  private def emailFor(a: Article): Option[EmailContent] = allEmails.find(_.test(a))
+}
