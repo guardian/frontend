bb8577948dd689aff6aa767b2bf0181412b3e70a
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-02-23 18:02:25 +0000

    Simplified node logic

--- static/src/javascripts/projects/common/modules/commercial/dfp/breakout-iframe.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp/breakout-iframe.js
@@ -40,8 +40,8 @@ define([
             return type;
         }
 
-        $breakoutEl = $('.breakout__html', iFrameBody);
-        if ($breakoutEl.length) {
+        $breakoutEl = $('.breakout__html, .breakout__script', iFrameBody);
+        if ($breakoutEl.hasClass('breakout__html')) {
             fastdom.write(function () {
                 $iFrame.hide();
                 $breakoutEl.detach();
@@ -52,28 +52,25 @@ define([
                     $responsiveAds.addClass('ad--responsive--open');
                 });
             });
-        } else {
-            $breakoutEl = $('.breakout__script', iFrameBody);
-            if ($breakoutEl.length) {
-                fastdom.write(function () {
-                    $iFrame.hide();
-                }).then(function () {
-                    var breakoutContent = $breakoutEl.html();
-                    if ($breakoutEl.attr('type') === 'application/json') {
-                        var creativeConfig = JSON.parse(breakoutContent);
-                        if (creativeConfig.name === 'fluid250-v4' || creativeConfig.name === 'fluid250-v3') {
-                            creativeConfig.name = 'fluid250';
-                        }
-                        require(['common/modules/commercial/creatives/' + creativeConfig.name], function (Creative) {
-                            new Creative($slot, creativeConfig.params, creativeConfig.opts).create();
-                        });
-                        type = creativeConfig.params.adType || '';
-                    } else {
-                        // evil, but we own the returning js snippet
-                        eval(breakoutContent);
+        } else if ($breakoutEl.hasClass('breakout__script')) {
+            fastdom.write(function () {
+                $iFrame.hide();
+            }).then(function () {
+                var breakoutContent = $breakoutEl.html();
+                if ($breakoutEl.attr('type') === 'application/json') {
+                    var creativeConfig = JSON.parse(breakoutContent);
+                    if (creativeConfig.name === 'fluid250-v4' || creativeConfig.name === 'fluid250-v3') {
+                        creativeConfig.name = 'fluid250';
                     }
-                });
-            }
+                    require(['common/modules/commercial/creatives/' + creativeConfig.name], function (Creative) {
+                        new Creative($slot, creativeConfig.params, creativeConfig.opts).create();
+                    });
+                    type = creativeConfig.params.adType || '';
+                } else {
+                    // evil, but we own the returning js snippet
+                    eval(breakoutContent);
+                }
+            });
         }
 
         return type;
