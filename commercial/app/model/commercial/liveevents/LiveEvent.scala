fac8e531887d7c7d198a28c84da9c04ebba20f05
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-02-25 11:21:32 +0000

    Minor component styling changes

--- commercial/app/model/commercial/liveevents/LiveEvent.scala
+++ commercial/app/model/commercial/liveevents/LiveEvent.scala
@@ -12,4 +12,12 @@ case class LiveEvent(id: String,
                      eventUrl: String) {
 
   lazy val ratioTicketsLeft = 1 - (tickets.map(_.quantitySold).sum.toDouble / tickets.map(_.quantityTotal).sum)
+
+  lazy val displayPrice = {
+    val priceList = tickets.map(_.price).sorted.distinct
+    if (priceList.size > 1) {
+      val (low, high) = (priceList.head, priceList.last)
+      f"£$low%,.2f to £$high%,.2f"
+    } else f"£${priceList.head}%,.2f"
+  }
 }
