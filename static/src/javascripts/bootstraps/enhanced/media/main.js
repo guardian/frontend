91a7b23d605f13a43bcdf7561ca61918818d9604
James Gorrie <james.gorrie@theguardian.com>
2016-03-02 14:38:37 +0000

    add inview to video media

--- static/src/javascripts/bootstraps/enhanced/media/main.js
+++ static/src/javascripts/bootstraps/enhanced/media/main.js
@@ -2,6 +2,7 @@ define([
     'bean',
     'bonzo',
     'fastdom',
+    'common/utils/fastdom-promise',
     'raven',
     'Promise',
     'common/utils/$',
@@ -21,11 +22,13 @@ define([
     // This must be the full path because we use curl config to change it based
     // on env
     'bootstraps/enhanced/media/video-player',
-    'text!common/views/ui/loading.html'
+    'text!common/views/ui/loading.html',
+    'common/modules/experiments/ab'
 ], function (
     bean,
     bonzo,
     fastdom,
+    fastdomPromise,
     raven,
     Promise,
     $,
@@ -43,8 +46,62 @@ define([
     supportedBrowsers,
     techOrder,
     videojs,
-    loadingTmpl
+    loadingTmpl,
+    ab
 ) {
+    // For the A/B test
+    var abVideoAutoplay = ab.getParticipations().ArticleVideoAutoplay || true;
+    function elementIsInView (el, offset) {
+        var viewportHeight = window.innerHeight;
+        var rect = el.getBoundingClientRect();
+        var fromTop = rect.top + offset;
+        var fromBottom = rect.bottom - offset;
+
+        return fromTop < viewportHeight && fromBottom > 0;
+    }
+
+    function ElementViewable (element, offset, inViewOnloadCallback_) {
+        // This is rubbish, but we can refine it later.
+        var inViewOnloadCallback = inViewOnloadCallback_ || function() {};
+        var wasAlreadyInView = false;
+        var events = {
+            viewenter: function() {},
+            viewexit: function() {}
+        };
+        // TODO: latch onto debounced event
+        mediator.on('window:throttledScroll', function() {
+            var inView = elementIsInView(element, offset);
+
+            if (inView) {
+                if (!wasAlreadyInView) {
+                    wasAlreadyInView = true;
+                    events.viewenter();
+                }
+            } else {
+                if (wasAlreadyInView) {
+                    wasAlreadyInView = false;
+                    events.viewexit();
+                }
+                wasAlreadyInView = false;
+            }
+        });
+
+        fastdomPromise.read(function () {
+            wasAlreadyInView = elementIsInView(element, offset)
+            return wasAlreadyInView;
+        }).then(function (inView) {
+            if (inView) {
+                inViewOnloadCallback();
+            }
+        });
+
+        return {
+            on: function (event, func) {
+                events[event] = func;
+            }
+        }
+    }
+    // End A/B test
 
     function getAdUrl() {
         // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
@@ -267,6 +324,21 @@ define([
                     } else {
                         resolve();
                     }
+
+                    if (abVideoAutoplay) {
+                        // Annoyingly we pass the `parentNode` as the video is absolutely positioned.
+                        var parentNode = player.el().parentNode;
+                        var elementInView = ElementViewable(parentNode, parentNode.clientHeight/2, function () {
+                            player.play();
+                        });
+                        elementInView.on('viewenter', function autoplayInView () {
+                            player.play();
+                        });
+                        elementInView.on('viewexit', function autoStopInView () {
+                            player.pause();
+                        });
+                    }
+
                 } else {
                     player.playlist({
                         mediaType: 'audio',
