91f87b5b33630c28b635216f7019a9d42e86719b
Oliver Joseph Ash <oliverjash@gmail.com>
2016-03-09 15:05:59 +0000

    Next in series: add test page

--- static/src/javascripts/projects/common/modules/experiments/tests/next-in-series.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/next-in-series.js
@@ -18,34 +18,35 @@ define([
 
     var $articleBody = $('.content__article-body');
 
+    var path = '/404-test/series/';
     var allSeries = [
         {
             name: 'Experience',
             pageId: 'lifeandstyle/2016/mar/04/experience-my-head-was-crushed-by-a-rock-thrown-at-my-car',
             title: 'I fought off a mountain lion',
             trail: 'Its ears were slanted in attack position, its teeth yellow splinters buried in black gums…',
-            link: 'https://www.surveymonkey.co.uk/r/guardian-experience-remindme'
+            link: path + 'experience'
         },
         {
             name: 'Blind Date',
             pageId: 'lifeandstyle/2016/mar/05/blind-date-valerio-matt',
             title: 'Will Charles and Ross hit it off?',
             trail: 'What was I hoping for? Vin Diesel',
-            link: 'https://www.surveymonkey.co.uk/r/guardian-blinddate-remindme'
+            link: path + 'blind-date'
         },
         {
             name: 'Alanis',
             pageId: 'lifeandstyle/2016/mar/04/ask-alanis-morissette-husband-or-son',
             title: 'Dear Alanis, My friends share my secrets with their partners. What do I do?',
             trail: 'Suddenly my secrets are not so safe, and it leaves me feeling vulnerable',
-            link: 'https://www.surveymonkey.co.uk/r/guardian-alanis-remindme'
+            link: path + 'alanis'
         },
         {
             name: 'What I’m Really Thinking',
             pageId: 'lifeandstyle/2016/mar/05/what-really-thinking-theme-park-costume-character',
             title: 'What I’m Really Thinking: the mother of a baby with disabilities',
             trail: 'Normal now means a routine of tube-feeding, tracheostomy care, physiotherapy, overnight shifts and endless logistics…',
-            link: 'https://www.surveymonkey.co.uk/r/guardian-whatimreallythinking-remindme'
+            link: path + 'what-im-really-thinking'
         }
     ];
 
