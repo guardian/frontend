db53089fd06ba74c71185e5690118fd7aa462410
Joseph Smith <joseph.smith@guardian.co.uk>
2016-03-09 18:18:53 +0000

    Show membership banner to readers of international edition

--- static/src/javascripts/projects/common/modules/commercial/membership-messages.js
+++ static/src/javascripts/projects/common/modules/commercial/membership-messages.js
@@ -22,7 +22,8 @@ define([
 
     var membershipEndpoints = {
         UK:   'https://membership.theguardian.com/supporter',
-        US:   'https://membership.theguardian.com/us/supporter'
+        US:   'https://membership.theguardian.com/us/supporter',
+        INT:   'https://membership.theguardian.com/supporter'
     };
 
     var defaultData = {
@@ -50,6 +51,18 @@ define([
                 messageText: 'Support open, independent journalism. Become a Supporter for just $4.99 per month',
                 linkText: 'Find out more'
             }
+        },
+        INT: {
+            campaign:      'MEMBERSHIP_SUPPORTER_BANNER_INT',
+            code:          'membership-message-int',
+            minVisited:    10,
+            data: {
+                messageText: [
+                    'Support Guardian journalism and our coverage of critical, under-reported stories from around the world.',
+                    'Become a Supporter for just $49/€49 per year.'
+                ].join(' '),
+                linkText: 'Find out more'
+            }
         }
     };
 
@@ -64,24 +77,38 @@ define([
     }
 
     function show(edition, message) {
-        var originalMessage;
-
         var data = defaults({ linkHref: formatEndpointUrl(edition, message) }, message.data, defaultData);
 
-        originalMessage = new Message(message.code);
+        // Four cases (for a given edition, say UK):
+        // (a) has never closed any membership banner
+        // (b) has closed only 'membership-message-uk'
+        // (c) has closed only 'membership-message-uk-redisplayed'
+        //     (can't occur for US)
+        // (d) has closed both 'membership-messaged-uk' and 'membership-message-uk-redisplayed'
 
-        // Allow tracking to distinguish banners that have been re-displayed
-        // after closing from those that have only been displayed once.
-        if (originalMessage.hasSeen()) {
-            message.code += '-redisplayed';
-            message.campaign += '_REDISPLAYED';
-            message.data.linkHref = formatEndpointUrl(edition, message);
-        }
+        // once I make this change, what will the behaviour be?
+        // (a) =>
+        //   current => they see banner. closed once, it will reappear. closed again, it will not.
+        //   new     => they see banner. closed once, it will not reappear.
+        // (b) =>
+        //   current => they see banner (r). closed once, it will reappear.
+        //   new     => they will not see banner (since they've already closed it)
+        // (c) PROBLEMATIC =>
+        //   current => they do not see banner.
+        //   new     => they see banner. closed once, it will not reappear.
+        // (d) =>
+        //   current => they do not see banner.
+        //   new     => they do not see banner.
+
+        // Switching between editions, no history is preserved
+        // (e.g. if I've closed the banner only on the UK edition,
+        //       I will see it on the US & International editions)
 
         return new Message(message.code, {
             pinOnHide: false,
             siteMessageLinkName: 'membership message',
-            siteMessageCloseBtn: 'hide'
+            siteMessageCloseBtn: 'hide',
+            cssModifierClass: 'membership-message'
         }).show(template(messageTemplate, data));
     }
 
@@ -98,6 +125,7 @@ define([
     }
 
     return {
-        init: init
+        init: init,
+        messages: messages
     };
 });
