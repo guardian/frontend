42ba5e69cad8f686ca4d2be04400104c116b4c43
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-03-09 12:19:03 +0000

    Add /deploys-notify endpoint to admin app
    This endpoint allows to send a notice where a deploy step happens
    Example:
    ```
    curl -X POST -H "Content-Type: application/json"
    -H "X-Gu-Api-Key: beeaa13031f8a5bd41036b6be661d8b99e47f334"
    -d '{"step": "deploy-finished-code",
    "notices": [{"type": "slack",
    "data": {"username": "TestUser", "channel": "#dotcom-push", "hookUrl":
    "https://hooks.slack.com/services/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"}}]}'
    http://frontend.gutools.co.uk/deploys-notify/api/builds/BUILD/notify
    ```

--- admin/app/controllers/DeploysRadiatorController.scala
+++ admin/app/controllers/DeploysRadiatorController.scala
@@ -1,193 +1,25 @@
 package controllers.admin
 
 import common.Logging
-import conf.Configuration
 import controllers.AuthLogging
 import implicits.Requests
 import model.NoCache
-import play.api.libs.json._
-import play.api.libs.ws.{WSResponse, WS}
-import play.api.mvc.{Results, Result, Controller}
-import play.api.Play.current
+import model.deploys.{RiffRaffService, ApiResults}
+import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
-import scala.concurrent.Future
-import play.api.libs.json.Reads._
-import play.api.libs.functional.syntax._
-import scala.language.postfixOps
+import model.deploys._
 
-case class RiffRaffDeploy(
-  uuid: String,
-  projectName: String,
-  build: String,
-  stage: String,
-  deployer: String,
-  status: String,
-  time: String)
-object RiffRaffDeploy { implicit val format = Json.format[RiffRaffDeploy] }
+trait DeploysRadiatorController extends Controller with Logging with AuthLogging with Requests {
 
-case class Commit(sha: String, username: String, message: String)
-object Commit {
-  implicit val w = Json.writes[Commit]
-  implicit val r: Reads[Commit] = (
-    (__ \ "version").read[String] and
-      (__ \ "username").read[String] and
-      (__ \ "comment").read[String]
-    )(Commit.apply _)
-}
-case class TeamCityBuild(number: String,
-                         projectName: String,
-                         parentNumber: Option[String],
-                         revision: String,
-                         commits: List[Commit])
-
-object TeamCityBuild {
-  implicit val w = Json.writes[TeamCityBuild]
-  implicit val r: Reads[TeamCityBuild] = (
-    (__ \ "number").read[String] and
-    (__ \ "buildType").read(
-        (__ \ "projectName").read[String] and
-        (__ \ "name").read[String]
-        tupled
-    ).map{case (project, name) => project + "::" + name} and
-    (__ \ "artifact-dependencies" \ "build").read(
-      (__ \\ "number").readNullable[String]
-    ) and
-    (__ \ "revisions" \ "revision" \\ "version").read[String] and
-    (__ \ "changes" \ "change").read[List[Commit]]
-  )(TeamCityBuild.apply _)
-}
-
-trait DeploysRadiatorController extends Controller with Logging with AuthLogging with Requests{
-  //
-  // API types
-  //
-
-  case class ApiError(message: String, statusCode: Int)
-  object ApiError { implicit val format = Json.format[ApiError] }
-
-  case class ApiErrors(errors: List[ApiError]) {
-    def statusCode = errors.map(_.statusCode).max
-  }
-
-  type ApiResponse[T] = Either[ApiErrors, T]
-
-  object ApiResults extends Results {
-    def apply[T](action: => ApiResponse[T])(implicit tjs: Writes[T]): Result ={
-      action.fold(
-        apiErrors =>
-          Status(apiErrors.statusCode) {
-            JsObject(Seq(
-              "status" -> JsString("error"),
-              "statusCode" -> JsNumber(apiErrors.statusCode),
-              "errors" -> Json.toJson(apiErrors.errors)
-            ))
-          },
-        response =>
-          Ok {
-            JsObject(Seq(
-              "status" -> JsString("ok"),
-              "response" -> Json.toJson(response)
-            ))
-          }
-      )
-    }
-  }
-
-  protected def GET(url: String, queryString: Map[String, String] = Map.empty, headers: Map[String, String] = Map.empty): Future[WSResponse] = {
-    WS.url(url).withQueryString(queryString.toSeq: _*).withHeaders(headers.toSeq: _*).withRequestTimeout(10000).get()
-  }
-
-  def getRiffRaffDeploys(pageSize: Option[String], projectName: Option[String], stage: Option[String]): Future[ApiResponse[List[RiffRaffDeploy]]] = {
-    val url = s"${Configuration.riffraff.url}/api/history"
-
-    GET(url,
-      queryString = Map(
-        "key" -> Configuration.riffraff.apiKey,
-        "pageSize" -> pageSize.getOrElse(""),
-        "projectName" -> projectName.getOrElse(""),
-        "stage" -> stage.getOrElse("")
-      )
-    ).map { response =>
-      response.status match {
-        case 200 =>
-          (response.json \ "response" \ "results").validate[List[RiffRaffDeploy]] match {
-            case JsSuccess(listOfDeploys, _) => Right(listOfDeploys)
-            case JsError(error) => Left(ApiErrors(List(ApiError("Invalid JSON from RiffRaff API", 500))))
-          }
-        case statusCode => Left(ApiErrors(List(ApiError(s"Invalid status code from RiffRaff: $statusCode", 500 ))))
-      }
-    }
-  }
-
-  def getTeamCityBuild(number: String): Future[ApiResponse[TeamCityBuild]] = {
-    val apiPath = "/guestAuth/app/rest"
-    val url = s"${Configuration.teamcity.host}${apiPath}/builds/number:$number,state:any,canceled(any)"
-
-    GET(url,
-      queryString = Map("fields" -> List(
-        "number", "buildType(name,projectName)",
-        "revisions(revision(version))", "changes(change(username,comment,version))",
-        "artifact-dependencies(build(number))"
-      ).mkString(",")),
-      headers = Map("Accept" -> "application/json")
-      ).map { response =>
-        response.status match {
-          case 200 => Right(response.json)
-          case statusCode => Left(ApiErrors(List(ApiError(
-            message = s"Invalid status code from TeamCity: $statusCode",
-            statusCode = 500
-          ))))
-        }
-      }
-  }
-
-  def buildJsonToBuild(buildJson: BuildJson): ApiResponse[Build] = {
-    val maybeBuild = buildJson.revisions.revision.headOption.map(_.version).map(revision => {
-      Build(
-        number = buildJson.number,
-        projectName = s"${buildJson.buildType.projectName}::${buildJson.buildType.name}",
-        parentNumber = buildJson.`artifact-dependencies`.build.headOption.map(b => b.number),
-        revision = revision,
-        commits = buildJson.changes.change.map(change => Commit(change.version, change.username, change.comment))
-      )
-    })
-
-    maybeBuild
-      .map(Right(_))
-      .getOrElse(Left(ApiErrors(List(ApiError("Missing revision", 500)))))
-  }
-
-  def jsonToBuild(json: JsValue): ApiResponse[Build] = {
-    json.validate[BuildJson] match {
-      case JsSuccess(buildJson, _) => buildJsonToBuild(buildJson)
-      case JsError(errors) => Left(ApiErrors(List(ApiError("Failed to validate JSON", 500))))
-    }
-  }
-
-  //
-  // Routes
-  //
+  val riffRaff: RiffRaffService
+  val teamcity: TeamcityService
 
   def getDeploys(pageSize: Option[String], projectName: Option[String], stage: Option[String]) = AuthActions.AuthActionTest.async {
-    getRiffRaffDeploys(pageSize, projectName, stage).map(riffRaffDeploysJson => {
-      ApiResponse {
-        for {
-          json <- riffRaffDeploysJson.right
-          deploys <- jsonToDeploys(json).right
-        } yield deploys
-      }
-    })
+    riffRaff.getRiffRaffDeploys(pageSize, projectName, stage).map(ApiResults(_))
   }
 
   def getBuild(number: String) = AuthActions.AuthActionTest.async {
-    getTeamCityBuild(number).map(teamCityBuildJson => {
-      ApiResponse {
-        for {
-          json <- teamCityBuildJson.right
-          build <- jsonToBuild(json).right
-        } yield build
-      }
-    })
+    teamcity.getTeamCityBuild(number).map(ApiResults(_))
   }
 
   def renderDeploysRadiator() = AuthActions.AuthActionTest {
@@ -196,4 +28,8 @@ trait DeploysRadiatorController extends Controller with Logging with AuthLogging
 
 }
 
-object DeploysRadiatorController extends DeploysRadiatorController
+object DeploysRadiatorController extends DeploysRadiatorController {
+  override val riffRaff = RiffRaffService
+  override val teamcity = TeamcityService
+}
+
