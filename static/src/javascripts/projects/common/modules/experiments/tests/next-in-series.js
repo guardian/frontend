15ef4efc9fca2a16263aff1da5115b6c8d299b07
Oliver Joseph Ash <oliverjash@gmail.com>
2016-03-10 12:31:33 +0000

    Remove link

--- static/src/javascripts/projects/common/modules/experiments/tests/next-in-series.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/next-in-series.js
@@ -5,12 +5,13 @@ define([
     $,
     find
 ) {
+    var path = '/surveys/404-test/next-in-series/';
     var render = function (state) {
         return '<div class="next-in-series-test">' +
             '<h3>Coming up next week</h3>' +
             '<h4>' + state.title + '</h4>' +
             '<p class="next-in-series-test__teaser">' + state.trail + '</p>' +
-            '<a href="' + state.link + '"' +
+            '<a href="' + path + state.id + '"' +
                 ' data-link-name="next in series | remind me"' +
                 ' class="button button--large next-in-series-test__remind-me-link">Remind me</a>' +
         '</div>';
@@ -18,35 +19,30 @@ define([
 
     var $articleBody = $('.content__article-body');
 
-    var path = '/surveys/404-test/next-in-series/';
     var allSeries = [
         {
-            name: 'Experience',
+            id: 'experience',
             pageId: 'lifeandstyle/2016/mar/04/experience-my-head-was-crushed-by-a-rock-thrown-at-my-car',
             title: 'I fought off a mountain lion',
-            trail: 'Its ears were slanted in attack position, its teeth yellow splinters buried in black gums…',
-            link: path + 'experience'
+            trail: 'Its ears were slanted in attack position, its teeth yellow splinters buried in black gums…'
         },
         {
-            name: 'Blind Date',
+            id: 'blind-date',
             pageId: 'lifeandstyle/2016/mar/05/blind-date-valerio-matt',
             title: 'Will Charles and Ross hit it off?',
-            trail: 'What was I hoping for? Vin Diesel',
-            link: path + 'blind-date'
+            trail: 'What was I hoping for? Vin Diesel'
         },
         {
-            name: 'Alanis',
+            id: 'alanis',
             pageId: 'lifeandstyle/2016/mar/04/ask-alanis-morissette-husband-or-son',
             title: 'Dear Alanis, My friends share my secrets with their partners. What do I do?',
-            trail: 'Suddenly my secrets are not so safe, and it leaves me feeling vulnerable',
-            link: path + 'alanis'
+            trail: 'Suddenly my secrets are not so safe, and it leaves me feeling vulnerable'
         },
         {
-            name: 'What I’m Really Thinking',
+            id: 'what-im-really-thinking',
             pageId: 'lifeandstyle/2016/mar/05/what-really-thinking-theme-park-costume-character',
             title: 'What I’m Really Thinking: the mother of a baby with disabilities',
-            trail: 'Normal now means a routine of tube-feeding, tracheostomy care, physiotherapy, overnight shifts and endless logistics…',
-            link: path + 'what-im-really-thinking'
+            trail: 'Normal now means a routine of tube-feeding, tracheostomy care, physiotherapy, overnight shifts and endless logistics…'
         }
     ];
 
