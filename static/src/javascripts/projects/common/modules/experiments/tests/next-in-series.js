bf90253488d5a28d22d919512764c8b1a4b9db7b
Oliver Joseph Ash <oliverjash@gmail.com>
2016-03-17 11:35:41 +0000

    Revert "Merge branch 'oja-series-404-test-page'"
    This reverts commit c3a1c0e00e4981ea145be628ac9f60e649c9dd1f, reversing
    changes made to 718090a7cebee268681fe59b4baced386a498d0d.

--- static/src/javascripts/projects/common/modules/experiments/tests/next-in-series.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/next-in-series.js
@@ -18,35 +18,34 @@ define([
 
     var $articleBody = $('.content__article-body');
 
-    var path = '/surveys/404-test/next-in-series/';
     var allSeries = [
         {
             name: 'Experience',
             pageId: 'lifeandstyle/2016/mar/04/experience-my-head-was-crushed-by-a-rock-thrown-at-my-car',
             title: 'I fought off a mountain lion',
             trail: 'Its ears were slanted in attack position, its teeth yellow splinters buried in black gums…',
-            link: path + 'experience'
+            link: 'https://www.surveymonkey.co.uk/r/guardian-experience-remindme'
         },
         {
             name: 'Blind Date',
             pageId: 'lifeandstyle/2016/mar/05/blind-date-valerio-matt',
             title: 'Will Charles and Ross hit it off?',
             trail: 'What was I hoping for? Vin Diesel',
-            link: path + 'blind-date'
+            link: 'https://www.surveymonkey.co.uk/r/guardian-blinddate-remindme'
         },
         {
             name: 'Alanis',
             pageId: 'lifeandstyle/2016/mar/04/ask-alanis-morissette-husband-or-son',
             title: 'Dear Alanis, My friends share my secrets with their partners. What do I do?',
             trail: 'Suddenly my secrets are not so safe, and it leaves me feeling vulnerable',
-            link: path + 'alanis'
+            link: 'https://www.surveymonkey.co.uk/r/guardian-alanis-remindme'
         },
         {
             name: 'What I’m Really Thinking',
             pageId: 'lifeandstyle/2016/mar/05/what-really-thinking-theme-park-costume-character',
             title: 'What I’m Really Thinking: the mother of a baby with disabilities',
             trail: 'Normal now means a routine of tube-feeding, tracheostomy care, physiotherapy, overnight shifts and endless logistics…',
-            link: path + 'what-im-really-thinking'
+            link: 'https://www.surveymonkey.co.uk/r/guardian-whatimreallythinking-remindme'
         }
     ];
 
