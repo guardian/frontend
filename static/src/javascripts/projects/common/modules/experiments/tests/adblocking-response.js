1fe72cfd02a53b3c8af2fc6a57940a16679c7976
Zofia <zofia.korcz@guardian.co.uk>
2016-03-18 13:25:29 +0000

    text, links, button added

--- static/src/javascripts/projects/common/modules/experiments/tests/adblocking-response.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/adblocking-response.js
@@ -25,7 +25,7 @@ define([
         this.audienceOffset = 0;
         this.successMeasure = 'Adblock users will either become a paid member or turn off the adblock.';
         this.audienceCriteria = 'All users with adblockers turned on.';
-        this.dataLinkNames = '';
+        this.dataLinkNames = 'adblock whitelist A, adblock membership A, subscriber number A, adblock whitelist B, adblock membership B, subscriber number B';
         this.idealOutcome = 'Adblock users will either become a paid member or turn off the adblock.';
 
         this.canRun = function () {
@@ -46,13 +46,16 @@ define([
                         !storage.local.get('gu.subscriber')) {
                         var surveyOverlay = new SurveySimple({
                             surveyHeader: 'You appear to have an adblocker installed',
-                            surveyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et quam auctor, efficitur velit nec, ullamcorper elit. Sed ac pharetra mauris. Curabitur blandit est vel commodo lobortis. Curabitur ullamcorper ante in massa maximus pharetra. Aliquam erat volutpat. In sed arcu velit. Vivamus nisl eros, venenatis ac imperdiet nec, lobortis sed lorem. Vestibulum et dictum eros, et finibus dui.',
+                            surveyText: 'Fearless, quality journalism is not free. Advertising revenue helps to sustain the Guardian\'s future and independence in perpetuity. To continue enjoying the Guardian, please support us in one of the following ways:',
                             signupText: 'Whitelist',
                             membershipText: 'Become a Member',
                             signupLink: '/commercial/survey-simple-sign-up',
-                            membershipLink: '/commercial/survey-simple-membership',
-                            signupDataLink: 'adblock whitelist',
-                            membershipDataLink: 'adblock membership',
+                            membershipLink: 'https://membership.theguardian.com/?INTCMP=ADB_RESP_A',
+                            signupDataLink: 'adblock whitelist A',
+                            membershipDataLink: 'adblock membership A',
+                            subscriberLink: '/commercial/subscriber-number',
+                            subscriberText: 'Subscriber number',
+                            subscriberDataLink: 'adblock subscriber A',
                             showCloseBtn: false
                         });
                         surveyOverlay.attach();
@@ -62,7 +65,33 @@ define([
             }
         }, {
             id: 'variantB',
-            test: function () {}
+            test: function () {
+                detect.getFfOrGenericAdbockInstalled.then(function (adblockUsed) {
+                    if (adblockUsed && !config.page.isFront &&
+                        !userFeatures.isPayingMember() &&
+                        config.page.webTitle !== 'Subscriber number form' &&
+                        !storage.local.get('gu.subscriber')) {
+                        var surveyOverlay = new SurveySimple({
+                            surveyHeader: 'We need to talk about adblocking.',
+                            surveyText: 'It looks like you’re trying to browse The Guardian with an adblocker installed.',
+                            surveyTextSecond: 'We understand people can find adverts distracting and intrusive. We understand that readers have a choice how they read the news, and where they read it from. But without funding, the Guardian has no way to survive independently.',
+                            surveyTextThird: 'Before continuing, please support us in one of two ways - either by disabling your adblocker for this website, or by becoming a Guardian member, at as little as £5 a month.',
+                            signupText: 'Whitelist',
+                            membershipText: 'Become a Member',
+                            signupLink: '/commercial/survey-simple-sign-up',
+                            membershipLink: 'https://membership.theguardian.com/?INTCMP=ADB_RESP_B',
+                            signupDataLink: 'adblock whitelist B',
+                            membershipDataLink: 'adblock membership B',
+                            subscriberLink: '/commercial/subscriber-number',
+                            subscriberText: 'Subscriber number',
+                            subscriberDataLink: 'adblock subscriber B',
+                            showCloseBtn: false
+                        });
+                        surveyOverlay.attach();
+                        surveyOverlay.show();
+                    }
+                });
+            }
         }];
 
     };
