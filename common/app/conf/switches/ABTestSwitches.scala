abd239387949da855fb59fed2fa19770899f6efe
Luke Taylor <luke.taylor@guardian.co.uk>
2016-03-31 10:29:09 +0100

    Extend content recommendation tests.

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -54,7 +54,7 @@ trait ABTestSwitches {
     "ab-people-who-read-this-also-read-variants",
     "Display people who read this also read with different variants",
     safeState = Off,
-    sellByDate = new LocalDate(2016, 3, 31),
+    sellByDate = new LocalDate(2016, 4, 14),
     exposeClientSide = true
   )
 
