0a5ab8c048bb569ff6a286ead7e7b47915c9c775
Zofia <zofia.korcz@guardian.co.uk>
2016-03-31 16:03:33 +0100

    data link attr, ophan id, better styling

--- static/src/javascripts/projects/common/modules/experiments/tests/loyal-adblocking-survey.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/loyal-adblocking-survey.js
@@ -13,6 +13,11 @@ define([
     userFeatures,
     SurveySimple
 ) {
+    function getSurveyLink(adblockUsed) {
+        var ophanId = '&ophanId=' + config.ophan.pageViewId;
+        return adblockUsed ? 'https://surveys.theguardian.com/R.aspx?a=684&as=AC5tF8XE6G&t=1' + ophanId : 'https://surveys.theguardian.com/R.aspx?a=685&as=Aj5jo96J9j&t=1' + ophanId;
+    }
+
     return function () {
         this.id = 'LoyalAdblockingSurvey';
         this.start = '2016-03-21';
@@ -23,7 +28,7 @@ define([
         this.audienceOffset = 0;
         this.successMeasure = 'We want to understand what causes people to block ads on theguardian.com and what would make them consider unblocking.';
         this.audienceCriteria = 'All users';
-        this.dataLinkNames = '';
+        this.dataLinkNames = 'loyal adblocking survey overlay take part btn, loyal adblocking survey overlay hide survey message';
         this.idealOutcome = 'We want to understand what causes people to block ads on theguardian.com and what would make them consider unblocking.';
 
         this.canRun = function () {
@@ -41,8 +46,8 @@ define([
                             surveyHeader: 'We would like you to take part in a short Guardian survey',
                             surveyText: 'Your views are essential to us and the feedback we receive will help us better meet your needs.',
                             buttonText: 'Take part',
-                            buttonLink: adblockUsed ? 'https://surveys.theguardian.com/R.aspx?a=684&as=AC5tF8XE6G&t=1' : 'https://surveys.theguardian.com/R.aspx?a=685&as=Aj5jo96J9j&t=1',
-                            buttonDataLink: 'adblock membership A',
+                            buttonLink: getSurveyLink(adblockUsed),
+                            buttonDataLink: 'take part btn',
                             showCloseBtn: true,
                             closePermanently: true
                         });
