9ffa8637701523b1c501d8cb578943e9d1f03320
John Duffell <john.duffell@guardian.co.uk>
2016-04-01 10:40:20 +0100

    fix up tests!

--- article/test/liveblog/PageParserTest.scala
+++ article/test/liveblog/PageParserTest.scala
@@ -1,18 +1,18 @@
 package liveblog
 
-import controllers.PageParser
+import controllers.ParseBlockId
 import org.scalatest.{FlatSpec, Matchers}
 
 class PageParserTest extends FlatSpec with Matchers {
 
   it should "parse a with" in {
-    val result = new PageParser().blockId("with:block-asdf")
+    val result = ParseBlockId("with:block-asdf")
 
     result should be(Some("asdf"))
   }
 
   it should "not parse a blah" in {
-    val result = new PageParser().blockId("""\"'/""")
+    val result = ParseBlockId("""\"'/""")
 
     result should be(None)
   }
