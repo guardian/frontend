7c1be2be52c9835394ce032d0085e2f4e700d428
Sam Morris <mail@sammorrisdesign.co.uk>
2016-04-04 16:33:51 +0100

    jslint

--- static/src/javascripts/projects/facia/modules/ui/video-container.js
+++ static/src/javascripts/projects/facia/modules/ui/video-container.js
@@ -13,28 +13,27 @@ define([
     var containerPos = 0;
 
     function bindEvents() {
-        bean.on(document, 'click', $('.js-video-playlist-next'), function (e) {
-            moveCarousel("next");
+        bean.on(document, 'click', $('.js-video-playlist-next'), function () {
+            moveCarousel('next');
         }.bind(this));
-        bean.on(document, 'click', $('.js-video-playlist-prev'), function (e) {
-            moveCarousel("prev");
+        bean.on(document, 'click', $('.js-video-playlist-prev'), function () {
+            moveCarousel('prev');
         }.bind(this));
-    };
+    }
 
     function moveCarousel(direction) {
-        if (direction === "next") {
-            $(".js-video-playlist").removeClass("video-playlist--" + containerPos);
+        $('.js-video-playlist').removeClass('video-playlist--' + containerPos);
+
+        if (direction === 'next') {
             containerPos++;
-            $(".js-video-playlist").addClass("video-playlist--" + containerPos);
         } else {
-            $(".js-video-playlist").removeClass("video-playlist--" + containerPos);
             containerPos = containerPos - 1;
-            $(".js-video-playlist").addClass("video-playlist--" + containerPos);
         }
-    };
+
+        $('.js-video-playlist').addClass('video-playlist--' + containerPos);
+    }
 
     return function () {
-        console.log("video");
         bindEvents();
     };
 });
