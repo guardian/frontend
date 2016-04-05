895ee521f213a551ce25bb98c2ed9e1968dbb1bc
Justin Pinner <justinpinner@gmail.com>
2016-04-05 11:33:42 +0100

    Specify the expected type of the document (so we can't change it by accident)

--- admin/app/jobs/R2PagePressJob.scala
+++ admin/app/jobs/R2PagePressJob.scala
@@ -14,6 +14,7 @@ import services.{S3Archive, S3ArchiveOriginals, PagePresses}
 import play.api.Play.current
 import model.R2PressMessage
 import implicits.R2PressNotification.pressMessageFormatter
+import org.jsoup.nodes.Document
 
 import scala.concurrent.Future
 
@@ -83,7 +84,7 @@ object R2PagePressJob extends ExecutionContexts with Logging {
   private def parseAndClean(originalDocSource: String): Future[String] = {
     val cleaners = Seq(PollsHtmlCleaner, InteractiveHtmlCleaner, SimpleHtmlCleaner)
     val archiveDocument = Jsoup.parse(originalDocSource)
-    val doc = cleaners.filter(_.canClean(archiveDocument))
+    val doc: Document = cleaners.filter(_.canClean(archiveDocument))
       .map(_.clean(archiveDocument))
       .headOption
       .getOrElse(archiveDocument)
