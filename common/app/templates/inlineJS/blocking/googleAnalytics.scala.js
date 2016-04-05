426a4c652fd1bccd77c2b439c731bd7541829ea4
Grant Klopper <grant.klopper@theguardian.com>
2016-04-05 05:27:53 +0100

    Do no memoize call to googleAnalytics.

--- common/app/templates/inlineJS/blocking/googleAnalytics.scala.js
+++ common/app/templates/inlineJS/blocking/googleAnalytics.scala.js
@@ -24,7 +24,7 @@ if (gaParticipation === 'in') {
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga', null, null);
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
     ga('create', '@GoogleAnalyticsAccount.account', 'auto');
     ga('send', {
