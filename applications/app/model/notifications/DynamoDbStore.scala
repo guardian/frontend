9a45ea14668b2495eeb8227a9fe09f72ee57e734
Francis Carr <francis.carr@guardian.co.uk>
2016-04-08 15:01:44 +0100

    Change to unix timestamp

--- applications/app/model/notifications/DynamoDbStore.scala
+++ applications/app/model/notifications/DynamoDbStore.scala
@@ -24,7 +24,7 @@ object DynamoDbStore extends Logging with ExecutionContexts {
       .withKey(Map[String, AttributeValue](
         ("notificationTopicId", new AttributeValue().withS(notificationTopicId)),
         ("gcmBrowserId", new AttributeValue().withS(gcmBrowserId)),
-        ("subscribeDate", new AttributeValue().withN(DateTime.now.getMillis.toString))
+        ("subscribeDate", new AttributeValue().withN((DateTime.now.getMillis / 1000).toString))
       ).asJava)
 
     client.updateItemFuture(updateItemRequest) onFailure {
