891dff03071ba983fc256b562e338e875f40b9da
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2016-04-11 15:46:35 +0100

    Simple expansion

--- static/src/javascripts/projects/common/modules/commercial/creatives/full-width-250.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/full-width-250.js
@@ -1,10 +1,12 @@
 define([
     'bean',
+    'bonzo',
     'common/utils/fastdom-promise',
     'lodash/utilities/template',
     'text!common/views/commercial/creatives/full-width-250.html'
 ], function (
     bean,
+    bonzo,
     fastdom,
     template,
     fullWidth250Html
@@ -12,7 +14,8 @@ define([
     function FullWidth250($adSlot, params) {
         this.create = function () {
             renderContainer($adSlot);
-            renderCreative($adSlot, params);
+            var $creative = renderCreative($adSlot, params);
+            attachExpander($creative);
         };
     }
 
@@ -24,9 +27,29 @@ define([
 
     function renderCreative($adSlot, params) {
         var html = template(fullWidth250Html, params);
+        var creative = bonzo.create(html);
         fastdom.write(function () {
-            $adSlot.append(html);
+            $adSlot.append(creative);
         });
+        return bonzo(creative);
+    }
+
+    function attachExpander($creative) {
+        var expandClass = 'creative--full-width-250--expanded';
+        var isExpanded = false;
+
+        function toggleExpansion() {
+            fastdom.write(function () {
+                if (!isExpanded) {
+                    $creative.addClass(expandClass);
+                } else {
+                    $creative.removeClass(expandClass);
+                }
+                isExpanded = !isExpanded;
+            });
+        }
+
+        bean.on($creative[0], 'click', toggleExpansion);
     }
 
     return FullWidth250;
