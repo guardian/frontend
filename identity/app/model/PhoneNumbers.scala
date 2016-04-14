a0f8632e0de1daf524670ebdd8f677f0a4cf3860
markjamesbutler <markjamesbutler@gmail.com>
2016-04-14 12:58:44 +0100

    Removed lazy force early execution.

--- identity/app/model/PhoneNumbers.scala
+++ identity/app/model/PhoneNumbers.scala
@@ -6,13 +6,13 @@ import reflect.runtime.{universe => ru}
 
 object PhoneNumbers {
 
-  lazy val phoneNumberUtil = PhoneNumberUtil.getInstance()
+  val phoneNumberUtil = PhoneNumberUtil.getInstance()
 
   /**
     * Unfortunately the country code map is not accessible in PhoneNumberUtil. This code uses reflection to obtain access
     * then transform it to the correct structure.
     */
-  lazy val countryCodes = {
+  val countryCodes = {
     val m = ru.runtimeMirror(phoneNumberUtil.getClass.getClassLoader)
     val countryCallingCodeToRegionCodeMapSymb = ru.typeOf[PhoneNumberUtil].decl(ru.TermName("countryCallingCodeToRegionCodeMap")).asTerm
     val im = m.reflect(phoneNumberUtil)
