3b315b4dce24cb99baa442c54cae1a19c0cf8aca
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-04-19 14:57:45 +0100

    Removed start date as not really useful

--- static/src/javascripts/projects/common/modules/commercial/top-banner/is-permanently-sticky.js
+++ static/src/javascripts/projects/common/modules/commercial/top-banner/is-permanently-sticky.js
@@ -3,12 +3,9 @@ define([
 ], function (config) {
     return [
         function isPersonalInvestments() {
-            var startDate = new Date(2016, 3, 26);
             var endDate = new Date(2017, 3, 26);
             var now = new Date();
-            return /(^|,)personal-investments(\/|$|,)/.test(config.page.keywordIds) &&
-                startDate <= now &&
-                now < endDate;
+            return /(^|,)personal-investments(\/|$|,)/.test(config.page.keywordIds) && now < endDate;
         }
     ];
 })
