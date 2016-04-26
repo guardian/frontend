3c42745c621dbe2ee4597b3d34141eb10d6a2a04
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-04-26 18:09:03 +0100

    Add new module trackAd (excised from outbrain)

--- static/src/javascripts/projects/common/modules/commercial/third-party-tags/outbrain.js
+++ static/src/javascripts/projects/common/modules/commercial/third-party-tags/outbrain.js
@@ -7,6 +7,7 @@ define([
     'common/utils/mediator',
     'common/utils/template',
     'common/modules/identity/api',
+    'common/modules/commercial/track-ad',
     'common/modules/commercial/commercial-features',
     'common/modules/commercial/third-party-tags/outbrain-codes',
     'text!common/views/commercial/outbrain.html',
@@ -20,6 +21,7 @@ define([
     mediator,
     template,
     identity,
+    trackAd,
     commercialFeatures,
     getCode,
     outbrainStr,
@@ -92,30 +94,6 @@ define([
                     widgetId: widgetCode
                 }
             });
-        });
-    }
-
-    function trackAd(id) {
-        return new Promise(function (resolve, reject) {
-            var onAdLoaded = function (event) {
-                if (event.slot.getSlotElementId() === id) {
-                    unlisten();
-                    resolve(!event.isEmpty);
-                }
-            };
-
-            var onAllAdsLoaded = function () {
-                unlisten();
-                reject(new Error('Unable to load Outbrain widget: slot ' + id + ' was never loaded'));
-            };
-
-            function unlisten() {
-                mediator.off('modules:commercial:dfp:rendered', onAdLoaded);
-                mediator.off('modules:commercial:dfp:alladsrendered', onAllAdsLoaded);
-            }
-
-            mediator.on('modules:commercial:dfp:rendered', onAdLoaded);
-            mediator.on('modules:commercial:dfp:alladsrendered', onAllAdsLoaded);
         });
     }
 
