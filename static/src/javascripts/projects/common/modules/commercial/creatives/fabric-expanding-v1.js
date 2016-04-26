6ecdd1b391f618c5974cfe215cb9dd15344ce513
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2016-04-26 12:52:12 +0100

    Use expandable-v2 as basis for fabric-expanding-v1

--- static/src/javascripts/projects/common/modules/commercial/creatives/fabric-expanding-v1.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/fabric-expanding-v1.js
@@ -9,6 +9,7 @@ define([
     'common/utils/template',
     'common/views/svgs',
     'text!common/views/commercial/creatives/fabric-expanding-v1.html',
+    'lodash/functions/bindAll',
     'lodash/objects/merge',
     'common/modules/commercial/creatives/add-tracking-pixel',
     'Promise'
@@ -22,11 +23,13 @@ define([
     storage,
     template,
     svgs,
-    FabricExpandingV1Html,
+    fabricExpandingV1Html,
+    bindAll,
     merge,
     addTrackingPixel,
     Promise
 ) {
+
     var FabricExpandingV1 = function ($adSlot, params) {
         this.$adSlot      = $adSlot;
         this.params       = params;
@@ -34,10 +37,86 @@ define([
 
         this.closedHeight = 250;
         this.openedHeight = 500;
+
+        bindAll(this, 'updateBgPosition', 'listener');
+    };
+
+    FabricExpandingV1.hasScrollEnabled = !detect.isIOS() && !detect.isAndroid();
+
+    FabricExpandingV1.prototype.updateBgPosition = function () {
+        var scrollY = window.pageYOffset,
+            viewportHeight = bonzo.viewport().height,
+            adSlotTop = this.$adSlot.offset().top,
+            that = this;
+
+        var adHeight = (this.isClosed) ? this.closedHeight : this.openedHeight;
+        var inViewB = ((scrollY + viewportHeight) > adSlotTop);
+        var inViewT = ((scrollY - (adHeight * 2)) < adSlotTop + 20);
+        var topCusp = (inViewT &&
+        ((scrollY + (viewportHeight * 0.4) - adHeight) > adSlotTop)) ?
+            'true' : 'false';
+        var bottomCusp = (inViewB &&
+        (scrollY + (viewportHeight * 0.5)) < adSlotTop) ?
+            'true' : 'false';
+        var bottomScroll = (bottomCusp === 'true') ?
+        50 - ((scrollY + (viewportHeight * 0.5) - adSlotTop) * -0.2) : 50;
+        var topScroll = (topCusp === 'true') ?
+            ((scrollY + (viewportHeight * 0.4) - adSlotTop - adHeight) * 0.2) : 0;
+
+        switch (this.params.backgroundImagePType) {
+            case 'split':
+                this.scrollAmount = bottomScroll + topScroll + '%';
+                fastdom.write(function () {
+                    $('.ad-exp--expand-scrolling-bg').css('background-repeat', 'no-repeat');
+                });
+                break;
+            case 'fixed':
+                this.scrollAmount = (scrollY - adSlotTop) + 'px';
+                break;
+            case 'parallax':
+                this.scrollAmount = ((scrollY - adSlotTop) * 0.15) + '%';
+                break;
+        }
+
+        fastdom.write(function () {
+            $('.ad-exp--expand-scrolling-bg').css('background-position', '50%' + that.scrollAmount);
+        });
+    };
+
+    FabricExpandingV1.prototype.listener = function () {
+        var that = this;
+        if ((window.pageYOffset + bonzo.viewport().height) > (this.$adSlot.offset().top + this.openedHeight)) {
+            // expires in 1 week
+            var week = 1000 * 60 * 60 * 24 * 7;
+
+            storage.local.set('gu.commercial.expandable.' + this.params.ecid, true, { expires: Date.now() + week });
+            fastdom.write(function () {
+                that.$button.toggleClass('button-spin');
+                $('.ad-exp__open-chevron').toggleClass('chevron-down');
+                that.$ad.css('height', that.openedHeight);
+                that.isClosed = false;
+            });
+
+            return true;
+        }
     };
 
     FabricExpandingV1.prototype.create = function () {
-        var videoHeight = this.openedHeight,
+        var videoHeight = this.closedHeight - 24,
+            videoWidth = (videoHeight * 16) / 9,
+            leftMargin = (this.params.videoPositionH === 'center' ?
+                'margin-left: ' + videoWidth / -2 + 'px; ' : ''
+            ),
+            leftPosition = (this.params.videoPositionH === 'left' ?
+                'left: ' + this.params.videoHorizSpace + 'px; ' : ''
+            ),
+            rightPosition = (this.params.videoPositionH === 'right' ?
+                'right: ' + this.params.videoHorizSpace + 'px; ' : ''
+            ),
+            videoDesktop = {
+                video: (this.params.videoURL !== '') ?
+                '<iframe id="myYTPlayer" width="' + videoWidth + '" height="' + videoHeight + '" src="' + this.params.videoURL + '?rel=0&amp;controls=0&amp;showinfo=0&amp;title=0&amp;byline=0&amp;portrait=0" frameborder="0" class="expandable_video expandable_video--horiz-pos-' + this.params.videoPositionH + '" style="' + leftMargin + leftPosition + rightPosition + '"></iframe>' : ''
+            },
             showmoreArrow = {
                 showArrow: (this.params.showMoreType === 'arrow-only' || this.params.showMoreType === 'plus-and-arrow') ?
                 '<button class="ad-exp__open-chevron ad-exp__open">' + svgs('arrowdownicon') + '</button>' : ''
@@ -46,45 +125,51 @@ define([
                 showPlus: (this.params.showMoreType === 'plus-only' || this.params.showMoreType === 'plus-and-arrow') ?
                 '<button class="ad-exp__close-button ad-exp__open">' + svgs('closeCentralIcon') + '</button>' : ''
             },
-            videoSource = {
-                videoEmbed: (this.params.YoutubeVideoURL !== '') ?
-                '<iframe id="YTPlayer" width="100%" height="' + videoHeight + '" src="' + this.params.YoutubeVideoURL + '?showinfo=0&amp;rel=0&amp;controls=0&amp;fs=0&amp;title=0&amp;byline=0&amp;portrait=0" frameborder="0" class="expandable-video"></iframe>' : ''
+            scrollingbg = {
+                scrollbg: (this.params.backgroundImagePType !== '' || this.params.backgroundImagePType !== 'none') ?
+                '<div class="ad-exp--expand-scrolling-bg" style="background-image: url(' + this.params.backgroundImageP + '); background-position: ' + this.params.backgroundImagePPosition + ' 50%;"></div>' : ''
             },
-            $ExpandableVideo = $.create(template(FabricExpandingV1Html, { data: merge(this.params, showmoreArrow, showmorePlus, videoSource) })),
-            domPromise = new Promise(function (resolve) {
-                fastdom.write(function () {
+            $fabricExpandingV1 = $.create(template(fabricExpandingV1Html, { data: merge(this.params, showmoreArrow, showmorePlus, videoDesktop, scrollingbg) }));
 
-                    this.$ad = $('.ad-exp--expand', $ExpandableVideo).css('height', this.closedHeight);
+        var domPromise = new Promise(function (resolve) {
+            fastdom.write(function () {
 
-                    $('.ad-exp-collapse__slide', $ExpandableVideo).css('height', this.closedHeight);
+                this.$ad     = $('.ad-exp--expand', $fabricExpandingV1).css('height', this.closedHeight);
+                this.$button = $('.ad-exp__open', $fabricExpandingV1);
 
-                    if (this.params.trackingPixel) {
-                        addTrackingPixel(this.$adSlot, this.params.trackingPixel + this.params.cacheBuster);
-                    }
-                    $ExpandableVideo.appendTo(this.$adSlot);
-                    resolve();
-                }.bind(this));
+                $('.ad-exp-collapse__slide', $fabricExpandingV1).css('height', this.closedHeight);
+
+                if (this.params.trackingPixel) {
+                    addTrackingPixel(this.$adSlot, this.params.trackingPixel + this.params.cacheBuster);
+                }
+
+                $fabricExpandingV1.appendTo(this.$adSlot);
+
+                resolve();
             }.bind(this));
+        }.bind(this));
+
+        if (!storage.local.get('gu.commercial.expandable.' + this.params.ecid)) {
+            mediator.on('window:throttledScroll', this.listener);
+        }
 
         bean.on(this.$adSlot[0], 'click', '.ad-exp__open', function () {
             fastdom.write(function () {
-                var videoSrc = $('#YTPlayer').attr('src'),
-                    videoSrcAutoplay = videoSrc;
-                if (videoSrc.indexOf('autoplay') === -1) {
-                    videoSrcAutoplay = videoSrc + '&amp;autoplay=1';
-                } else {
-                    videoSrcAutoplay = videoSrcAutoplay.replace(this.isClosed ? 'autoplay=0' : 'autoplay=1', this.isClosed ? 'autoplay=1' : 'autoplay=0');
-                }
                 $('.ad-exp__close-button').toggleClass('button-spin');
-                $('.ad-exp__open-chevron').removeClass('chevron-up').toggleClass('chevron-down');
+                $('.ad-exp__open-chevron').toggleClass('chevron-down');
                 this.$ad.css('height', this.isClosed ? this.openedHeight : this.closedHeight);
-                $('.slide-video, .slide-video .ad-exp__layer', $(this.$adSlot[0])).css('height', this.isClosed ? this.openedHeight : this.closedHeight).toggleClass('slide-video__expand');
                 this.isClosed = !this.isClosed;
-                setTimeout(function () {
-                    $('#YTPlayer').attr('src', videoSrcAutoplay);
-                }, 1000);
             }.bind(this));
         }.bind(this));
+
+        if (FabricExpandingV1.hasScrollEnabled) {
+            // update bg position
+            this.updateBgPosition();
+
+            mediator.on('window:throttledScroll', this.updateBgPosition);
+            // to be safe, also update on window resize
+            mediator.on('window:resize', this.updateBgPosition);
+        }
 
         return domPromise;
     };
