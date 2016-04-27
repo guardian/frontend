3c397030f0bd3f547eefd08b8825830dd9281ee5
Sam Desborough <sam.desborough@gmail.com>
2016-04-27 12:42:00 +0100

    Add fake series tests
    These both cover 100% of our audience and only run on series pages. The only
    difference is that one runs on articles flagged as sensitive, which
    should help isolate a tracking problem.

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/fake-series-hide-sensitive.js
@@ -0,0 +1,31 @@
+define([], function () {
+    return function () {
+        this.id = 'FakeSeriesHideSensitive';
+        this.start = '2016-04-28';
+        this.expiry = '2016-04-30';
+        this.author = 'Sam Desborough';
+        this.description = 'A fake test to target series content that\'s hidden on sensitive articles';
+        this.audience = 1;
+        this.audienceOffset = 0;
+        this.successMeasure = '';
+        this.audienceCriteria = 'All users';
+        this.dataLinkNames = '';
+        this.idealOutcome = '';
+        this.showForSensitive = false;
+
+        this.canRun = function () {
+            return !!window.guardian.config.page.seriesId;
+        };
+
+        this.variants = [
+            {
+                id: 'control',
+                test: function () {}
+            },
+            {
+                id: 'variant',
+                test: function () {}
+            }
+        ];
+    };
+});
---
+++ static/src/javascripts/projects/common/modules/experiments/tests/fake-series-show-sensitive.js
@@ -0,0 +1,31 @@
+define([], function () {
+    return function () {
+        this.id = 'FakeSeriesShowSensitive';
+        this.start = '2016-04-28';
+        this.expiry = '2016-04-30';
+        this.author = 'Sam Desborough';
+        this.description = 'A fake test to target series content that\'s shown on sensitive articles';
+        this.audience = 1;
+        this.audienceOffset = 0;
+        this.successMeasure = '';
+        this.audienceCriteria = 'All users';
+        this.dataLinkNames = '';
+        this.idealOutcome = '';
+        this.showForSensitive = true;
+
+        this.canRun = function () {
+            return !!window.guardian.config.page.seriesId;
+        };
+
+        this.variants = [
+            {
+                id: 'control',
+                test: function () {}
+            },
+            {
+                id: 'variant',
+                test: function () {}
+            }
+        ];
+    };
+});
