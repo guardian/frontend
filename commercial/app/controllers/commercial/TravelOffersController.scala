b6838604ce6d8d3778daa552ea082695f83a2e0d
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-04-28 10:44:40 +0100

    Remove redundant future block

--- commercial/app/controllers/commercial/TravelOffersController.scala
+++ commercial/app/controllers/commercial/TravelOffersController.scala
@@ -4,29 +4,18 @@ import model.commercial.travel.TravelOffersAgent
 import model.{Cached, NoCache}
 import play.api.mvc._
 
-import scala.concurrent.Future
-
 object TravelOffersController extends Controller with implicits.Requests {
 
-  def renderTravel = Action.async { implicit request =>
-    Future.successful {
-      val travelOffers = (TravelOffersAgent.specificTravelOffers(specificIds) ++
-                      TravelOffersAgent.offersTargetedAt(segment)).distinct
-      travelOffers.toList match {
-        case Nil => NoCache(jsonFormat.nilResult)
-        case offers => Cached(componentMaxAge) {
-          val clickMacro = request.getParameter("clickMacro")
-          val omnitureId = request.getParameter("omnitureId")
-
-          request.getParameter("layout") match {
-            case Some("prominent") =>
-              jsonFormat.result(views.html.travel.travelProminent(offers.take(4), omnitureId, clickMacro))
-            case _ =>
-              jsonFormat.result(views.html.travel.travelStandard(offers.take(4), omnitureId, clickMacro))
-          }
-        }
+  def renderTravel = Action { implicit request =>
+    val travelOffers =
+      (TravelOffersAgent.specificTravelOffers(specificIds) ++ TravelOffersAgent.offersTargetedAt(segment)).distinct
+    travelOffers match {
+      case Nil => NoCache(jsonFormat.nilResult)
+      case offers => Cached(componentMaxAge) {
+        val clickMacro = request.getParameter("clickMacro")
+        val omnitureId = request.getParameter("omnitureId")
+        jsonFormat.result(views.html.travel.travelStandard(offers.take(4), omnitureId, clickMacro))
       }
     }
   }
-
 }
