44742527d93aecb3107e68cb8506a83e65567d7e
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-05-03 09:39:48 +0100

    Rename new soulmates template

--- commercial/app/controllers/commercial/SoulmatesController.scala
+++ commercial/app/controllers/commercial/SoulmatesController.scala
@@ -34,8 +34,7 @@ object SoulmatesController extends Controller with implicits.Requests {
   }
 
   def renderSoulmates(groupName: String): Action[AnyContent] = Action { implicit request =>
-    val soulmates = views.html.soulmates.soulmatesV2(_, _, _)
-    result(groupName, soulmates)
+    result(groupName, views.html.soulmates.soulmates(_, _, _))
   }
 
   def renderSoulmatesTest(groupName: String): Action[AnyContent] = Action { implicit request =>
