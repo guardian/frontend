4f84ab03cc37713ef92f2fbb45d89b9010750d77
Alex Sanders <a@snd.rs>
2016-05-04 17:22:31 +0100

    safety first

--- common/app/templates/inlineJS/blocking/cloudwatchBeacons.scala.js
+++ common/app/templates/inlineJS/blocking/cloudwatchBeacons.scala.js
@@ -3,18 +3,20 @@
 
 @if(OfflinePageView.isSwitchedOn) {
     (function () {
-        var cookies = document.cookie.split(';');
-        var offlineViews = 0;
-        for (var len = cookies.length, i = 0; i < len ; i++) {
-            var data = cookies[i].split('=');
-            if (data[0].indexOf('gu.offlineViews') !== -1) {
-                offlineViews = parseInt(data[1]);
-                break;
+        try {
+            var cookies = document.cookie.split(';');
+            var offlineViews = 0;
+            for (var len = cookies.length, i = 0; i < len ; i++) {
+                var data = cookies[i].split('=');
+                if (data[0].indexOf('gu.offlineViews') !== -1) {
+                    offlineViews = parseInt(data[1]);
+                    break;
+                }
             }
-        }
-        for (var i = 0; i < offlineViews; i++) {
-            (new Image()).src = window.guardian.config.page.beaconUrl + '/count/offline-page-view.gif';
-        }
-        document.cookie = 'gu.offlineViews=0;Max-Age=0';
+            for (var i = 0; i < offlineViews; i++) {
+                (new Image()).src = window.guardian.config.page.beaconUrl + '/count/offline-page-view.gif';
+            }
+            document.cookie = 'gu.offlineViews=0;Max-Age=0';
+        } catch (e) {};
     })();
 }
