09677b70dd411358d5f0d8e74f5881fe5bc0fe7a
Alex Sanders <a@snd.rs>
2016-05-04 16:55:25 +0100

    add offline page view metrics

--- common/app/templates/inlineJS/blocking/cloudwatchBeacons.scala.js
+++ common/app/templates/inlineJS/blocking/cloudwatchBeacons.scala.js
@@ -1,2 +1,20 @@
 @(item: model.MetaData)(implicit request: RequestHeader)
 @import conf.switches.Switches._
+
+@if(OfflinePageView.isSwitchedOn) {
+    (function () {
+        var cookies = document.cookie.split(';');
+        var offlineViews = 0;
+        for (var len = cookies.length, i = 0; i < len ; i++) {
+            var data = cookies[i].split('=');
+            if (data[0].indexOf('gu.offlineViews') !== -1) {
+                offlineViews = parseInt(data[1]);
+                break;
+            }
+        }
+        for (var i = 0; i < offlineViews; i++) {
+            (new Image()).src = window.guardian.config.page.beaconUrl + '/count/offline-page-view.gif';
+        }
+        document.cookie = 'gu.offlineViews=0;Max-Age=0';
+    })();
+}
