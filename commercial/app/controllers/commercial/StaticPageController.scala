4cfce7f9d61410de4d2f7b06dd0801e7c9e9771a
Steve Vadocz <steve.vadocz@guardian.co.uk>
2016-05-05 16:10:26 +0100

    Awesome updateds

--- commercial/app/controllers/commercial/StaticPageController.scala
+++ commercial/app/controllers/commercial/StaticPageController.scala
@@ -4,19 +4,19 @@ import model.Cached.RevalidatableResult
 import model.{MetaData, GuardianContentTypes, Cached, StandalonePage}
 import play.api.mvc.{Action, Controller}
 
-case class StaticGuardianAwesomePage() extends StandalonePage {
+case class StaticGuardianHostedPage() extends StandalonePage {
   override val metadata: MetaData = MetaData.make(
-    id = "guardian-awesome",
-    webTitle = "Guardian Awesome",
+    id = "guardian-hosted",
+    webTitle = "Guardian Hosted",
     section = "global",
     contentType = GuardianContentTypes.Interactive,
-    analyticsName = "guardian-awesome",
+    analyticsName = "guardian-hosted",
     shouldGoogleIndex = false
   )
 }
 
 object StaticPageController extends Controller {
-  def renderGuardianAwesomePage() = Action { implicit request =>
-    Cached(60)(RevalidatableResult.Ok(views.html.static.guardianAwesomePage(StaticGuardianAwesomePage().metadata)))
+  def renderGuardianHostedPage() = Action { implicit request =>
+    Cached(60)(RevalidatableResult.Ok(views.html.static.guardianHostedPage(StaticGuardianHostedPage().metadata)))
   }
 }
