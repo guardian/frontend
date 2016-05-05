22d876ba8b166b24611d2d985b6840e6d922e823
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-05-05 16:01:41 +0100

    Remove old Facia HealthCheck controller object

--- facia/app/conf/HealthCheck.scala
+++ facia/app/conf/HealthCheck.scala
@@ -3,12 +3,6 @@ package conf
 import model.NoCache
 import play.api.mvc.Action
 
-object HealthCheck extends AllGoodHealthcheckController(9008, "/uk") {
-  def cdnHealthcheck = Action{ request =>
-    NoCache(if (isOk) Ok("OK") else ServiceUnavailable("Service Unavailable"))
-  }
-}
-
 object HealthCheckController extends CachedHealthCheckController {
   override val paths = Seq("/uk/business")
   override val port = 9008
