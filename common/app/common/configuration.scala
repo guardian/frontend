7719e91f2f424f08377b602bc1705810657bdf29
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-05-06 18:09:01 +0100

    Explain useHashedBundles config

--- common/app/common/configuration.scala
+++ common/app/common/configuration.scala
@@ -228,6 +228,10 @@ class GuardianConfiguration(val application: String, val webappConfDirectory: St
 
   object assets {
     lazy val path = configuration.getMandatoryStringProperty("assets.path")
+
+    // This configuration value determines if this server will load and resolve assets using the asset map.
+    // Set this to true if you want to run the Play server in dev, and emulate prod mode asset-loading.
+    // If true in dev, assets are locally loaded from the `hash` build output, otherwise assets come from 'target' for css, and 'src' for js.
     lazy val useHashedBundles =  configuration.getStringProperty("assets.useHashedBundles")
       .map(_.toBoolean)
       .getOrElse(environment.isProd || environment.isCode)
