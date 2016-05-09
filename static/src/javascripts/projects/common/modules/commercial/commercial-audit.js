04f600ad8016e8de69832cd92fc02b1351367ddd
Ken Lim <kenneth.lim@theguardian.com>
2016-05-09 17:11:45 +0100

    Adding commercial audit endpoint

---
+++ static/src/javascripts/projects/common/modules/commercial/commercial-audit.js
@@ -0,0 +1,34 @@
+define([
+    'common/utils/config',
+    'common/utils/report-error'
+], function (config, reportError) {
+
+    return {
+        init: init
+    };
+
+    function init() {
+        var guardian = window.guardian;
+        var config = guardian.config;
+
+        if (config.switches.commercialAudit) {
+            window.addEventListener('message', receiveMessage, false);
+
+            function receiveMessage(event) {
+                var origin = event.origin || event.originalEvent.origin;
+                var message = event.data;
+
+
+                if (message.startsWith('Tracker beacon: ')) {
+                    reportError(new Error('###Ad Beacon'), {
+                        feature: 'commercial',
+                        message: message,
+                        source: origin
+                    }, false);
+                }
+                return;
+            }
+        }
+
+    }
+});
