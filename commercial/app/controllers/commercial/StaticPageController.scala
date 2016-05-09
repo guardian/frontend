ebe87f4ce53c9bcceacef948dc68b53795944346
Steve Vadocz <steve.vadocz@guardian.co.uk>
2016-05-09 14:24:44 +0100

    Hosted content switch

--- commercial/app/controllers/commercial/StaticPageController.scala
+++ commercial/app/controllers/commercial/StaticPageController.scala
@@ -1,8 +1,9 @@
 package controllers.commercial
 
 import model.Cached.RevalidatableResult
-import model.{MetaData, GuardianContentTypes, Cached, StandalonePage}
+import model.{MetaData, GuardianContentTypes, Cached, NoCache, StandalonePage}
 import play.api.mvc.{Action, Controller}
+import conf.switches.Switches.HostedContent
 
 case class StaticGuardianHostedPage() extends StandalonePage {
   override val metadata: MetaData = MetaData.make(
@@ -17,6 +18,10 @@ case class StaticGuardianHostedPage() extends StandalonePage {
 
 object StaticPageController extends Controller {
   def renderGuardianHostedPage() = Action { implicit request =>
-    Cached(60)(RevalidatableResult.Ok(views.html.static.guardianHostedPage(StaticGuardianHostedPage().metadata)))
+    if (HostedContent.isSwitchedOn) {
+      Cached(60)(RevalidatableResult.Ok(views.html.static.guardianHostedPage(StaticGuardianHostedPage().metadata)))
+    } else {
+      NoCache(NotFound)
+    }
   }
 }
