104c9acdd0df25c0d114b361a64290bcc6ef812a
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-05-09 16:06:47 +0100

    Use pre-fetch-modules in Dev and Prod

--- common/app/templates/inlineJS/blocking/curlConfig.scala.js
+++ common/app/templates/inlineJS/blocking/curlConfig.scala.js
@@ -75,6 +75,12 @@ window.curlConfig = {
             videojspersistvolume:           'components/videojs-persistvolume/videojs.persistvolume.js',
             videojsplaylist:                'components/videojs-playlist-audio/videojs.playlist.js',
 
+            // These paths are for pre-fetch-modules.js performance module, used by boot.js:
+            // The resolved paths are loaded through pre-fetch-modules XHR, not curl, so they don't inherit the standard baseUrl.
+            'enhanced-vendor':              '@{Configuration.assets.path}javascripts/enhanced-vendor.js',
+            'bootstraps/enhanced/main':     '@{Configuration.assets.path}javascripts/bootstraps/enhanced/main.js',
+            'bootstraps/commercial':        '@{Configuration.assets.path}javascripts/bootstraps/commercial.js',
+
             // plugins
             text:         'components/requirejs-text/text',// CANT RUN FACIA VIDEO.JS
             inlineSvg:    'projects/common/utils/inlineSvg'
