f8d96cf6273e365a7ab81d6dc0b3223f52d19ac5
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2016-05-10 17:04:37 +0100

    Parallax scroll type is now mandatory

--- static/src/javascripts/projects/common/modules/commercial/creatives/fabric-expanding-v1.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/fabric-expanding-v1.js
@@ -98,6 +98,8 @@ define([
                     $('.ad-exp--expand-scrolling-bg', that.$adSlot).css('background-position', '50%' + (scrollAmount + '%'));
                 });
                 break;
+            case 'none' :
+                break;
         }
     };
 
@@ -186,10 +188,9 @@ define([
             showPlus: (this.params.showMoreType === 'plus-only' || this.params.showMoreType === 'plus-and-arrow') ?
             '<button class="ad-exp__close-button ad-exp__open">' + svgs('closeCentralIcon') + '</button>' : ''
         };
-        var parallaxScrollType = this.params.backgroundImagePType;
         var scrollbgDefaultY = '0%'; // used if no parallax / fixed background scroll support
         var scrollingbg = {
-            scrollbg: (parallaxScrollType !== '' && parallaxScrollType !== 'none') ?
+            scrollbg: this.params.backgroundImagePType !== 'none' ?
             '<div class="ad-exp--expand-scrolling-bg" style="background-image: url(' + this.params.backgroundImageP + '); background-position: ' + this.params.backgroundImagePPosition + ' ' + scrollbgDefaultY + '; background-repeat: ' + this.params.backgroundImagePRepeat + ';"></div>' : ''
         };
         var $fabricExpandingV1 = $.create(template(fabricExpandingV1Html, { data: merge(this.params, showmoreArrow, showmorePlus, videoDesktop, videoMobile, scrollingbg) }));
