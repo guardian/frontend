b56cceadcfcd568f3f4e926b33716327290d67ab
James Gorrie <james.gorrie@theguardian.com>
2016-05-10 12:47:27 +0100

    add pagin pagination JS

--- static/src/javascripts/projects/common/modules/video/onward-container.js
+++ static/src/javascripts/projects/common/modules/video/onward-container.js
@@ -1,14 +1,14 @@
 define([
+    'bean',
     'common/utils/config',
     'common/modules/component',
     'common/utils/mediator'
 ], function(
+    bean,
     config,
     Component,
     mediator
 ) {
-
-    var component = new Component();
 
     function getEndpoint(mediaType) {
         var isInSeries = Boolean(config.page.seriesTags);
@@ -20,12 +20,33 @@ define([
         }
     }
 
+    function initEvents(el, manipulationType, endpoint) {
+        var component = new Component();
+        bean.on(el, 'click', '.most-viewed-navigation__link', function(ev) {
+            var page = ev.currentTarget.getAttribute('data-page');
+            var paginatedEndpoint = endpoint + '?page=' + page;
+
+            component = new Component();
+            component.manipulationType = manipulationType;
+            component.endpoint = paginatedEndpoint;
+            el.innerHTML = '';
+            component.fetch(el, 'html');
+            ev.preventDefault();
+            return false;
+        });
+    }
+
     function init(el, mediaType) {
-        component.manipulationType = mediaType === 'video' ? 'append' : 'html';
-        component.endpoint = getEndpoint(mediaType);
+        var component = new Component();
+        var manipulationType = mediaType === 'video' ? 'append' : 'html';
+        var endpoint = getEndpoint(mediaType);
+
+        component.manipulationType = manipulationType;
+        component.endpoint = endpoint;
 
         component.fetch(el, 'html').then(function () {
             mediator.emit('page:new-content');
+            initEvents(el, manipulationType, endpoint);
         });
     }
 
