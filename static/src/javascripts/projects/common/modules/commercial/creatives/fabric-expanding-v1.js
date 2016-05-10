ad5e1f0e5b5713e02e497ae407c5d47c39d05ec2
jbreck-mckye <jimmy.breck-mckye@guardian.co.uk>
2016-05-10 16:53:38 +0100

    Missing space

--- static/src/javascripts/projects/common/modules/commercial/creatives/fabric-expanding-v1.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/fabric-expanding-v1.js
@@ -189,8 +189,8 @@ define([
         var parallaxScrollType = this.params.backgroundImagePType;
         var scrollbgDefaultY = '0%'; // used if no parallax / fixed background scroll support
         var scrollingbg = {
-            scrollbg: (parallaxScrollType !== '' || parallaxScrollType !== 'none') ?
-            '<div class="ad-exp--expand-scrolling-bg" style="background-image: url(' + this.params.backgroundImageP + '); background-position: ' + this.params.backgroundImagePPosition + '' + scrollbgDefaultY + '; background-repeat: ' + this.params.backgroundImagePRepeat + ';"></div>' : ''
+            scrollbg: (parallaxScrollType !== '' && parallaxScrollType !== 'none') ?
+            '<div class="ad-exp--expand-scrolling-bg" style="background-image: url(' + this.params.backgroundImageP + '); background-position: ' + this.params.backgroundImagePPosition + ' ' + scrollbgDefaultY + '; background-repeat: ' + this.params.backgroundImagePRepeat + ';"></div>' : ''
         };
         var $fabricExpandingV1 = $.create(template(fabricExpandingV1Html, { data: merge(this.params, showmoreArrow, showmorePlus, videoDesktop, videoMobile, scrollingbg) }));
 
