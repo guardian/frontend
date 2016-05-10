adf71ed995cd34b2706eb67bb2ce86d00da340cb
NataliaLKB <natalialeebaltazar@gmail.com>
2016-05-10 17:30:42 +0100

    adding a ratio double to imageAsset

--- common/app/model/content.scala
+++ common/app/model/content.scala
@@ -717,7 +717,7 @@ case class GenericLightbox(
   properties: GenericLightboxProperties
 ) {
   lazy val mainFiltered = elements.mainPicture
-    .filter(_.images.largestEditorialCrop.map(_.ratio).getOrElse(0) > 0.7)
+    .filter(_.images.largestEditorialCrop.map(_.ratioWholeNumber).getOrElse(0) > 0.7)
     .filter(_.images.largestEditorialCrop.map(_.width).getOrElse(1) > properties.lightboxableCutoffWidth).toSeq
   lazy val bodyFiltered: Seq[ImageElement] = elements.bodyImages.filter(_.images.largestEditorialCrop.map(_.width).getOrElse(1) > properties.lightboxableCutoffWidth).toSeq
 
