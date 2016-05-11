4d72774da6fe1d2305303f8d8ecc875cfc0ccdf0
Zofia <zofia.korcz@guardian.co.uk>
2016-05-11 15:15:55 +0100

    added tracking and accesibility to the video

--- static/src/javascripts/projects/common/modules/commercial/hosted-video.js
+++ static/src/javascripts/projects/common/modules/commercial/hosted-video.js
@@ -5,26 +5,70 @@
 define([
     'bean',
     'common/utils/$',
-    'common/modules/video/tech-order',
-    'bootstraps/enhanced/media/video-player'
+    'common/utils/defer-to-analytics',
+    'common/modules/video/events',
+    'common/modules/video/supportedBrowsers',
+    'bootstraps/enhanced/media/video-player',
+    'text!common/views/ui/loading.html'
 ], function (
     bean,
     $,
-    techOrder,
-    videojs
+    deferToAnalytics,
+    events,
+    supportedBrowsers,
+    videojs,
+    loadingTmpl
 ) {
 
     var $videoEl = $('.vjs-hosted__video');
+    var player;
+
+    function initLoadingSpinner(player) {
+        player.loadingSpinner.contentEl().innerHTML = loadingTmpl;
+    }
+
+    function upgradeVideoPlayerAccessibility(player) {
+        // Set the video tech element to aria-hidden, and label the buttons in the videojs control bar.
+        // It doesn't matter what kind of tech this is, flash or html5.
+        $('.vjs-tech', player.el()).attr('aria-hidden', true);
+
+        // Hide superfluous controls, and label useful buttons.
+        $('.vjs-big-play-button', player.el()).attr('aria-hidden', true);
+        $('.vjs-current-time', player.el()).attr('aria-hidden', true);
+        $('.vjs-time-divider', player.el()).attr('aria-hidden', true);
+        $('.vjs-duration', player.el()).attr('aria-hidden', true);
+        $('.vjs-embed-button', player.el()).attr('aria-hidden', true);
+
+        $('.vjs-play-control', player.el()).attr('aria-label', 'video play');
+        $('.vjs-mute-control', player.el()).attr('aria-label', 'video mute');
+        $('.vjs-fullscreen-control', player.el()).attr('aria-label', 'video fullscreen');
+    }
 
     function init() {
         if (!$videoEl) {
             return;
         }
 
-        videojs($videoEl.get(0), {
+        var mediaId = $videoEl.attr('data-media-id');
+
+        player = videojs($videoEl.get(0), {
             controls: true,
             autoplay: false,
             preload: 'metadata'
+        });
+
+        player.ready(function () {
+            deferToAnalytics(function () {
+                events.initOmnitureTracking(player);
+                events.initOphanTracking(player, mediaId);
+
+                events.bindGlobalEvents(player);
+                events.bindContentEvents(player);
+            });
+
+            initLoadingSpinner(player);
+            upgradeVideoPlayerAccessibility(player);
+            supportedBrowsers(player);
         });
     }
 
