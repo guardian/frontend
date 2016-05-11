8df094a451349483cdf3bd621f2002a01a192658
Sam Desborough <sam.desborough@gmail.com>
2016-05-11 14:58:16 +0100

    Simplify json conversion

--- diagnostics/app/model/diagnostics/csp/CSP.scala
+++ diagnostics/app/model/diagnostics/csp/CSP.scala
@@ -1,6 +1,5 @@
 package model.diagnostics.csp
 
-import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import common.Logging
 import net.logstash.logback.marker.Markers._
@@ -14,23 +13,8 @@ case class CSPReport(
   originalPolicy: String)
 
 object CSPReport {
-  implicit val jsonReads: Reads[CSPReport] = (
-    (__ \ "document-uri").read[String] and
-      (__ \ "referrer").read[String] and
-      (__ \ "blocked-uri").read[String] and
-      (__ \ "violated-directive").read[String] and
-      (__ \ "effective-directive").read[String] and
-      (__ \ "original-policy").read[String]
-    )(CSPReport.apply _)
-
-  implicit val jsonWrites: Writes[CSPReport] = (
-    (__ \ "document-uri").write[String] and
-      (__ \ "referrer").write[String] and
-      (__ \ "blocked-uri").write[String] and
-      (__ \ "violated-directive").write[String] and
-      (__ \ "effective-directive").write[String] and
-      (__ \ "original-policy").write[String]
-    )(unlift(CSPReport.unapply))
+  implicit val jsonReads = Json.reads[CSPReport]
+  implicit val jsonWrites = Json.writes[CSPReport]
 }
 
 object CSP extends Logging {
