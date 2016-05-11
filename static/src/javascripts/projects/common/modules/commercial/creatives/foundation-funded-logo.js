5190d8eda23e8020544aab028033c4519585d8c2
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-05-11 16:06:31 +0100

    Merge foundation supported logos

--- static/src/javascripts/projects/common/modules/commercial/creatives/foundation-funded-logo.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/foundation-funded-logo.js
@@ -1,15 +1,17 @@
 define([
+    'fastdom',
     'common/utils/$',
-    'common/utils/config',
     'common/utils/template',
+    'common/modules/commercial/creatives/template-preprocessor',
 
     // require templates, so they're bundled up as part of the build
-    'text!common/views/commercial/creatives/logo-foundation-funded.html',
-    'text!common/views/commercial/creatives/logo-foundation-funded-partners.html'
+    'text!common/views/commercial/creatives/logo.html'
 ], function (
+    fastdom,
     $,
-    config,
-    template
+    template,
+    templatePreprocessor,
+    logoStr
 ) {
 
     /**
@@ -18,22 +20,19 @@ define([
      * * https://www.google.com/dfp/59666047#delivery/CreateCreativeTemplate/creativeTemplateId=10022127
      * * https://www.google.com/dfp/59666047#delivery/CreateCreativeTemplate/creativeTemplateId=10026327
      */
-    var Template = function ($adSlot, params) {
-        this.$adSlot       = $adSlot;
+    var Template = function (adSlot, params) {
+        this.adSlot        = adSlot instanceof HTMLElement ? adSlot : adSlot[0];;
         this.params        = params;
-        this.params.header = (!config.page.isFront && config.page.sponsorshipTag) ?
-            config.page.sponsorshipTag + ' is supported by' : 'Supported by';
     };
 
     Template.prototype.create = function () {
-        var templateName = 'logo-foundation-funded' + (this.params.hasPartners ? '-partners' : '');
+        // var templateName = 'logo-foundation-funded' + (this.params.hasPartners ? '-partners' : '');
+        templatePreprocessor.logo(this);
+        var logo = template(logoStr, this.params);
 
-        require(['text!common/views/commercial/creatives/' + templateName + '.html'], function (creativeTpl) {
-            var creativeHtml = template(creativeTpl, this.params);
-
-            $.create(creativeHtml)
-                .appendTo(this.$adSlot);
-        }.bind(this));
+        fastdom.write(function () {
+            this.adSlot.insertAdjacentHTML('beforeend', logo);
+        }, this);
     };
 
     return Template;
