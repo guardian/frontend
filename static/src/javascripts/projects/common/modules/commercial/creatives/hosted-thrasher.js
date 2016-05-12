c59ce782d4c5897dec1548e0024741d7c2090742
Zofia <zofia.korcz@guardian.co.uk>
2016-05-12 17:21:57 +0100

    module renamed

--- static/src/javascripts/projects/common/modules/commercial/creatives/hosted-thrasher.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/hosted-thrasher.js
@@ -31,12 +31,12 @@ define([
     var iframeVideoTpl;
     var scrollBgTpl;
 
-    var HostedVideo = function ($adSlot, params) {
+    var HostedThrasher = function ($adSlot, params) {
         this.$adSlot = $adSlot;
         this.params = params;
     };
 
-    HostedVideo.prototype.create = function () {
+    HostedThrasher.prototype.create = function () {
         if (!hostedVideoTpl) {
             hostedVideoTpl = template(hostedVideoStr);
             iframeVideoTpl = template(iframeVideoStr);
@@ -68,50 +68,12 @@ define([
         };
 
         this.$adSlot.append(hostedVideoTpl({ data: merge(this.params, templateOptions) }));
-        if (templateOptions.scrollbg) {
-            this.scrollingBg = $('.ad-scrolling-bg', this.$adSlot[0]);
-            this.layer2 = $('.hide-until-tablet .hostedVideo_layer2', this.$adSlot[0]);
-
-            if (hasScrollEnabled) {
-                // update bg position
-                fastdom.read(this.updateBgPosition, this);
-                mediator.on('window:throttledScroll', this.updateBgPosition.bind(this));
-                // to be safe, also update on window resize
-                mediator.on('window:resize', this.updateBgPosition.bind(this));
-            }
-        }
 
         if (this.params.trackingPixel) {
             addTrackingPixel(this.$adSlot, this.params.trackingPixel + this.params.cacheBuster);
         }
     };
 
-    HostedVideo.prototype.updateBgPosition = function () {
-        if (this.params.backgroundImagePType === 'parallax') {
-            var scrollAmount = Math.ceil((window.pageYOffset - this.$adSlot.offset().top) * 0.3 * -1) + 20;
-            fastdom.write(function () {
-                bonzo(this.scrollingBg)
-                    .addClass('ad-scrolling-bg-parallax')
-                    .css('background-position', '50% ' + scrollAmount + '%');
-            }, this);
-        }
-
-        this.layer2Animation();
-    };
-
-    HostedVideo.prototype.layer2Animation = function () {
-        var inViewB;
-        if (this.params.layerTwoAnimation === 'enabled' && isEnhanced && !isIE9OrLess) {
-            inViewB = (window.pageYOffset + bonzo.viewport().height) > this.$adSlot.offset().top;
-            fastdom.write(function () {
-                bonzo(this.layer2).addClass('ad-scrolling-text-hide' + (this.params.layerTwoAnimationPosition ? '-' + this.params.layerTwoAnimationPosition : ''));
-                if (inViewB) {
-                    bonzo(this.layer2).addClass('ad-scrolling-text-animate' + (this.params.layerTwoAnimationPosition ? '-' + this.params.layerTwoAnimationPosition : ''));
-                }
-            }, this);
-        }
-    };
-
-    return HostedVideo;
+    return HostedThrasher;
 
 });
