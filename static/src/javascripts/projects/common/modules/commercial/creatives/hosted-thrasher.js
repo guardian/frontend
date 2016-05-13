ac15e40d646a357931da9cefd96ec8af747a3702
Zofia <zofia.korcz@guardian.co.uk>
2016-05-13 16:30:06 +0100

    js cleanup

--- static/src/javascripts/projects/common/modules/commercial/creatives/hosted-thrasher.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/hosted-thrasher.js
@@ -8,8 +8,6 @@ define([
     'common/modules/commercial/hosted-video',
     'common/modules/commercial/creatives/add-tracking-pixel',
     'text!common/views/commercial/creatives/hosted-thrasher.html',
-    'text!common/views/commercial/creatives/iframe-video.html',
-    'text!common/views/commercial/creatives/scrollbg.html',
     'lodash/objects/merge'
 ], function (
     $,
@@ -21,15 +19,9 @@ define([
     hostedVideo,
     addTrackingPixel,
     hostedVideoStr,
-    iframeVideoStr,
-    scrollBgStr,
     merge
 ) {
-    var isEnhanced = detect.isEnhanced();
-
     var hostedVideoTpl;
-    var iframeVideoTpl;
-    var scrollBgTpl;
 
     var HostedThrasher = function ($adSlot, params) {
         this.$adSlot = $adSlot;
@@ -39,36 +31,10 @@ define([
     HostedThrasher.prototype.create = function () {
         if (!hostedVideoTpl) {
             hostedVideoTpl = template(hostedVideoStr);
-            iframeVideoTpl = template(iframeVideoStr);
-            scrollBgTpl = template(scrollBgStr);
         }
 
-        var position = {
-            position: this.params.videoPositionH === 'left' || this.params.videoPositionH === 'right' ?
-                this.params.videoPositionH + ':' + this.params.videoHorizSpace + 'px;' :
-                ''
-        };
-
-        var templateOptions = {
-            creativeHeight: this.params.creativeHeight || '',
-            isFixedHeight: this.params.creativeHeight === 'fixed',
-            showLabel: this.params.showAdLabel !== 'hide',
-            video: this.params.videoURL ? iframeVideoTpl(merge(this.params, position)) : '',
-            hasContainer: 'layerTwoAnimation' in this.params,
-            layerTwoBGPosition: this.params.layerTwoBGPosition && (
-                !this.params.layerTwoAnimation ||
-                this.params.layerTwoAnimation === 'disabled' ||
-                (!isEnhanced && this.params.layerTwoAnimation === 'enabled')
-            ) ?
-                this.params.layerTwoBGPosition :
-                '0% 0%',
-            scrollbg: this.params.backgroundImagePType && this.params.backgroundImagePType !== 'none' ?
-                scrollBgTpl(this.params) :
-                false
-        };
-
         var domPromise = fastdom.write(function () {
-            this.$adSlot.append(hostedVideoTpl({ data: merge(this.params, templateOptions) }));
+            this.$adSlot.append(hostedVideoTpl({ data: this.params}));
         }.bind(this));
 
         domPromise.then(function () {
