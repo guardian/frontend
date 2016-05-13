b87b37ff15cab373b806b41f8f3bf0e150c4f0c8
Sam Desborough <sam.desborough@gmail.com>
2016-05-13 14:51:49 +0100

    Only allow http(s) URLs in CSP reports
    This prevents things like about:blank, safari-extension:// getting
    through

--- diagnostics/app/model/diagnostics/csp/CSP.scala
+++ diagnostics/app/model/diagnostics/csp/CSP.scala
@@ -11,7 +11,9 @@ case class CSPReport(
   blockedUri: Option[String],
   violatedDirective: Option[String],
   effectiveDirective: Option[String],
-  originalPolicy: Option[String])
+  originalPolicy: Option[String]) {
+  val shouldReport = blockedUri.exists(_.startsWith("http")) && documentUri.exists(_.startsWith("http"))
+}
 
 object CSPReport {
   implicit val jsonReads: Reads[CSPReport] = (
@@ -36,7 +38,7 @@ object CSPReport {
 object CSP extends Logging {
   def report(requestBody: JsValue): Unit = {
     (requestBody \ "csp-report").validate[CSPReport] match {
-      case JsSuccess(report, _) => log.logger.info(appendRaw("csp", Json.toJson(report).toString()), "csp report")
+      case JsSuccess(report, _) => if (report.shouldReport) log.logger.info(appendRaw("csp", Json.toJson(report).toString()), "csp report")
       case JsError(e) => throw new Exception(JsError.toJson(e).toString())
     }
   }
