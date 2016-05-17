dced0f3666b8f1ff6efcf04a7096c1d6105c5eda
Frank <frank.hulley-jones@guardian.co.uk>
2016-05-17 18:06:34 +0100

    badges updated for reality check/eu

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -6,10 +6,13 @@ import layout.FaciaContainer
 case class Badge(seriesTag: String, imageUrl: String)
 
 object Badges {
-  val usElection = Badge("us-news/us-elections-2016", Static("images/USElectionlogooffset.png").path)
+  val usElection = Badge("us-news/us-elections-2016", s"${Configuration.static.path}/sys-images/Guardian/Pix/pictures/2016/2/2/1454424596176/USElectionlogooffset.png")
   val ausElection = Badge("australia-news/australian-election-2016", Static("images/AUSElectionBadge.png").path)
+  val euElection = Badge("politics/eu-referendum", Static("images/EU_Ref_Logo.svg").path)
+  val euRealityCheck = Badge("politics/series/eu-referendum-reality-check", Static("images/EU_Ref_Logo.svg").path)
+  
 
-  val allBadges = Seq(usElection, ausElection)
+  val allBadges = Seq(usElection, ausElection, euElection, euRealityCheck)
 
   def badgeFor(c: ContentType) = allBadges.find(badge => c.tags.tags.exists(tag => tag.id == badge.seriesTag))
   def badgeFor(fc: FaciaContainer) = fc.href.flatMap(href => allBadges.find(badge => href == badge.seriesTag))
