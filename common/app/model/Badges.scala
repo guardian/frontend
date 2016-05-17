4c612f30b0029e4d5b0cbae87ea95ce2926ea871
Sam Desborough <sam.desborough@gmail.com>
2016-05-17 10:47:05 +0100

    Use static path

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -6,7 +6,7 @@ import layout.FaciaContainer
 case class Badge(seriesTag: String, imageUrl: String)
 
 object Badges {
-  val usElection = Badge("us-news/us-elections-2016", s"${Configuration.static.path}/sys-images/Guardian/Pix/pictures/2016/2/2/1454424596176/USElectionlogooffset.png")
+  val usElection = Badge("us-news/us-elections-2016", Static("images/USElectionlogooffset.png").path)
   val ausElection = Badge("australia-news/australian-election-2016", Static("images/AUSElectionBadge.png").path)
 
   val allBadges = Seq(usElection, ausElection)
