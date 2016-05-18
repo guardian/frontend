280074d4d39041f9562cc447f772fb6c1fc76ee9
Steve Vadocz <steve.vadocz@guardian.co.uk>
2016-05-18 12:14:20 +0100

    More tracking updates

--- common/app/common/commercial/HostedPage.scala
+++ common/app/common/commercial/HostedPage.scala
@@ -7,7 +7,7 @@ case object HostedPage extends StandalonePage {
   override val metadata: MetaData = MetaData.make(
     id = "guardian-hosted",
     webTitle = "Guardian Hosted",
-    section = "Hosted section",
+    section = "Renault-campaign-test",
     contentType = GuardianContentTypes.Hosted,
     analyticsName = "renault-campaign:hosted:renault-is-epic"
   )
