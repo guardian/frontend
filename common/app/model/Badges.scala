b1083b51810caf7feafbb8e1999c412ba79b9623
Frank <frank.hulley-jones@guardian.co.uk>
2016-05-18 11:35:37 +0100

    eu-referendum tag disabled

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -8,7 +8,8 @@ case class Badge(seriesTag: String, imageUrl: String)
 object Badges {
   val usElection = Badge("us-news/us-elections-2016", Static("images/USElectionlogooffset.png").path)
   val ausElection = Badge("australia-news/australian-election-2016", Static("images/AUSElectionBadge.png").path)
-  val euElection = Badge("politics/eu-referendum", Static("images/EU_Ref_Logo.svg").path)
+  // Temporarily Disabled until Editorial confirm
+  /* val euElection = Badge("politics/eu-referendum", Static("images/EU_Ref_Logo.svg").path) */
   val euRealityCheck = Badge("politics/series/eu-referendum-reality-check", Static("images/EU_Ref_Logo.svg").path)
   
 
