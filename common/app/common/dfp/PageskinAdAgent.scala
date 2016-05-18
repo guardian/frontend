14699ed57fe6fa9fc11c80becff254788d4478a9
jon_norman <jon.norman@theguardian.com>
2016-05-18 11:07:54 +0100

    ensure that all pageskins are assessed before determining whether a page has a pageskin or not

--- common/app/common/dfp/PageskinAdAgent.scala
+++ common/app/common/dfp/PageskinAdAgent.scala
@@ -9,7 +9,7 @@ trait PageskinAdAgent {
 
   protected def pageSkinSponsorships: Seq[PageSkinSponsorship]
 
-  private def findSponsorship(adUnitWithoutRoot: String, edition: Edition): Option[PageSkinSponsorship] = {
+  private def findSponsorships(adUnitWithoutRoot: String, edition: Edition): Seq[PageSkinSponsorship] = {
 
     if (PageSkin.isValidAdUnit(adUnitWithoutRoot)) {
       val adUnitWithRoot = s"$dfpAdUnitRoot/$adUnitWithoutRoot"
@@ -21,24 +21,22 @@ trait PageskinAdAgent {
           !sponsorship.isR2Only
       }
 
-      pageSkinSponsorships.find { sponsorship =>
+      pageSkinSponsorships filter { sponsorship =>
         targetsAdUnitAndMatchesTheEdition(sponsorship)
       }
     } else {
-      None
+      Seq.empty
     }
   }
 
   // The ad unit is considered to have a page skin if it has a corresponding sponsorship.
   // If the sponsorship is an adTest, it is only considered outside of production.
   def hasPageSkin(adUnitWithoutRoot: String, edition: Edition): Boolean = {
-    findSponsorship(adUnitWithoutRoot, edition).exists { sponsorship =>
-      !sponsorship.targetsAdTest || !isProd
-    }
+    findSponsorships(adUnitWithoutRoot, edition) exists (sponsorship => !(isProd && sponsorship.targetsAdTest))
   }
 
   // True if there is any candidate sponsorship for this ad unit. Used to decide when to render the out-of-page ad slot.
   def hasPageSkinOrAdTestPageSkin(adUnitWithoutRoot: String, edition: Edition): Boolean = {
-    findSponsorship(adUnitWithoutRoot, edition).isDefined
+    findSponsorships(adUnitWithoutRoot, edition) nonEmpty
   }
 }
