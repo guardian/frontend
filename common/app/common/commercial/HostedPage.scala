9a52c766b9e444d7d2c3adc481dbd33e6e838573
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-05-20 11:04:38 +0100

    Update social metadata

--- common/app/common/commercial/HostedPage.scala
+++ common/app/common/commercial/HostedPage.scala
@@ -7,18 +7,36 @@ import play.api.libs.json.JsString
 case object HostedPage extends StandalonePage {
 
   override val metadata: MetaData = {
+    val title = "Designing the car of the future - video"
+    val description =
+      "Who better to dream up the transport of tomorrow than the people who'll be buying them? Follow a group of students as they work on designing the interior of self-driving cars"
+    val toneId = "tone/hosted-content"
     val toneName = "Hosted content"
     val sectionId = "renault-car-of-the-future"
+    val keywordId = s"$sectionId/$sectionId"
+    val keywordName = sectionId
     val urlSuffix = "design-competition-teaser"
     MetaData.make(
       id = s"commercial/advertiser-content/$sectionId/$urlSuffix",
-      webTitle = "Design competition",
+      webTitle = title,
       section = sectionId,
       contentType = Hosted,
       analyticsName = s"GFE:$sectionId:$Hosted:$urlSuffix",
+      description = Some(description),
       javascriptConfigOverrides = Map(
-        "keywords" -> JsString(sectionId),
+        "keywordIds" -> JsString(keywordId),
+        "keywords" -> JsString(keywordName),
+        "toneIds" -> JsString(toneId),
         "tones" -> JsString(toneName)
+      ),
+      opengraphPropertiesOverrides = Map(
+        "og:url" ->
+          "http://theguardian.com/commercial/advertiser-content/renault-car-of-the-future/design-competition-teaser",
+        "og:title" -> title,
+        "og:description" -> description,
+        "og:image" ->
+          "https://aws-frontend-static.s3.amazonaws.com/PROD/frontend-static/images/commercial/038c373fd249e5a2f4b6ae02e7cf3a93/renault-video-poster.jpg",
+        "fb:app_id" -> "180444840287"
       )
     )
   }
