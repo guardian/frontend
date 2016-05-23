414e8018e4aa3528dfe63add2deaba15013fe791
Dominic Kendrick <dominic.kendrick@guardian.co.uk>
2016-05-23 17:57:43 +0100

    only modify og url if it exists

--- common/app/facebookimages/openGraphOverlay.scala
+++ common/app/facebookimages/openGraphOverlay.scala
@@ -6,13 +6,21 @@ import views.support.{FacebookOpenGraphImage, ImgSrc}
 
 object ArticleWithOpenGraphOverlay {
   def apply(article: Article): Article = {
+    val openGraphProps = article.content.metadata.opengraphProperties
+
+    val ogUrl = {
+      openGraphProps.get("og:url").map{ url =>
+        Seq("og:url" -> s"$url?page=facebookOverlayVariant")
+      }.getOrElse(Nil).toMap
+    }
+    
     article.copy(
       content = article.content.copy(
         metadata = article.content.metadata.copy(
           opengraphPropertiesOverrides = {
-            val openGraph = article.content.metadata.opengraphProperties
-            openGraph ++ Map("og:image" -> ImgSrc(article.content.rawOpenGraphImage, FacebookOpenGraphImage, true),
-              "og:url" -> s"${openGraph.getOrElse("og:url", "")}?page=facebookOverlayVariant")
+            openGraphProps ++
+            ogUrl ++
+            Map("og:image" -> ImgSrc(article.content.rawOpenGraphImage, FacebookOpenGraphImage, true))
           }
         )
       )
