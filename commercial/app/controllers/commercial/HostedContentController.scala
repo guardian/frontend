2cdbc75414fa831cd44278b3e52f704d5c68d564
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-05-23 15:11:10 +0100

    Support different videos in hosted page template

---
+++ commercial/app/controllers/commercial/HostedContentController.scala
@@ -0,0 +1,35 @@
+package controllers.commercial
+
+import common.commercial.HostedPage
+import conf.switches.Switches
+import conf.switches.Switches.hostedContent
+import model.Cached.RevalidatableResult
+import model.{Cached, NoCache}
+import play.api.mvc.{Action, Controller}
+import views.html.hosted.{episode1Video, guardianHostedPage, teaserVideo}
+
+object HostedContentController extends Controller {
+
+  def renderHostedPage(pageName: String) = Action { implicit request =>
+    if (hostedContent.isSwitchedOn) {
+      val page = HostedPage(pageName)
+      pageName match {
+
+        case "design-competition-teaser" =>
+          Cached(60)(RevalidatableResult.Ok(guardianHostedPage(page, teaserVideo())))
+
+        case "design-competition-episode1" =>
+          if (Switches.hostedEpisode1Content.isSwitchedOn) {
+            Cached(60)(RevalidatableResult.Ok(guardianHostedPage(page, episode1Video())))
+          } else {
+            NoCache(NotFound)
+          }
+
+        case _ =>
+          NoCache(NotFound)
+      }
+    } else {
+      NoCache(NotFound)
+    }
+  }
+}
--- common/app/conf/switches/CommercialSwitches.scala
+++ common/app/conf/switches/CommercialSwitches.scala
@@ -293,12 +293,21 @@ trait CommercialSwitches {
     exposeClientSide = true
   )
 
-  val HostedContent = Switch(
+  val hostedContent = Switch(
     SwitchGroup.Commercial,
     "hosted-content",
     "If on, you can access hosted content",
     safeState = Off,
     sellByDate = new LocalDate(2016, 7, 12),
-    exposeClientSide =  false
+    exposeClientSide = false
+  )
+
+  val hostedEpisode1Content = Switch(
+    SwitchGroup.Commercial,
+    "hosted-ep1-content",
+    "If on, another hardcoded page of hosted content is available",
+    safeState = Off,
+    sellByDate = new LocalDate(2016, 7, 12),
+    exposeClientSide = false
   )
 }
