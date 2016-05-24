d713c6388791534e50178b748ecc6302adcb1263
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-05-24 09:40:15 +0100

    Remove Function.prototype.bind and String.prototype.trim from global polyfills

--- common/app/templates/inlineJS/blocking/polyfills.scala.js
+++ common/app/templates/inlineJS/blocking/polyfills.scala.js
@@ -2,36 +2,6 @@
 @import conf.Static
 @import conf.Configuration
 
-if (!Function.prototype.bind) {
-    Function.prototype.bind = function (oThis) {
-        if (typeof this !== 'function') {
-            // closest thing possible to the ECMAScript 5
-            // internal IsCallable function
-            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-        }
-
-        var aArgs = Array.prototype.slice.call(arguments, 1),
-            fToBind = this,
-            fNOP = function () {},
-            fBound = function () {
-                return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
-                aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
-
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-
-        return fBound;
-    };
-}
-
-// needed due to class manipulation in bonzo is not x-browser - https://github.com/ded/bonzo/pull/133
-if (!String.prototype.trim) {
-    String.prototype.trim = function () {
-        return this.replace(/^\s+|\s+$/g, '');
-    };
-}
-
 // JSON support needed for raven
 if (typeof JSON !== 'object') {
     (function (document) {
