a8d5d82e699e249436624cda131f532c90a07270
James Gorrie <james.gorrie@theguardian.com>
2016-05-25 15:41:56 +0100

    control player components in JS rather than CSS.

--- static/src/javascripts/bootstraps/enhanced/media/main.js
+++ static/src/javascripts/bootstraps/enhanced/media/main.js
@@ -215,10 +215,22 @@ define([
             }
         });
 
-
         player = createVideoPlayer(el, {
             techOrder: techPriority,
             controls: true,
+            textTrackDisplay: false,
+            textTrackSettings: false,
+            controlBar: {
+                children: [
+                    'playToggle',
+                    'currentTimeDisplay',
+                    'timeDivider',
+                    'durationDisplay',
+                    'progressControl',
+                    'fullscreenToggle',
+                    'volumeMenuButton'
+                ]
+            },
             // `autoplay` is always set to false.
             // If you are going to set autoplay to any other value, note it breaks
             // `preload="auto"` on < Chrome 35 and `preload="metadata"` on old Safari
