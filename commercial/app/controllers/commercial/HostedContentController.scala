723f00f4f0dad844f892f7ae54408961198cbc00
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-05-26 13:09:11 +0100

    Second Renault Video needs to have a different poster image

--- commercial/app/controllers/commercial/HostedContentController.scala
+++ commercial/app/controllers/commercial/HostedContentController.scala
@@ -1,15 +1,16 @@
 package controllers.commercial
 
 import common.commercial.HostedPage
+import conf.Static
 import conf.switches.Switches
 import model.Cached.RevalidatableResult
 import model.{Cached, NoCache}
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, AnyContent, Controller, Request}
 import views.html.hosted.{episode1Video, guardianHostedPage, teaserVideo}
 
 object HostedContentController extends Controller {
 
-  def renderHostedPage(pageName: String) = Action { implicit request =>
+  def renderHostedPage(pageName: String) = Action { implicit request: Request[AnyContent] =>
     lazy val pageUrl = routes.HostedContentController.renderHostedPage(pageName).absoluteURL
     pageName match {
 
@@ -19,7 +20,8 @@ object HostedContentController extends Controller {
           pageName,
           pageTitle = "Advertiser content hosted by the Guardian: Designing the car of the future - video",
           videoTitle = "Designing the car of the future",
-          standfirst = "Who better to dream up the cars of tomorrow than the people who'll be buying them? Students at Central St Martins are working with Renault to design the interior for cars that will drive themselves. Watch this short video to find out more about the project, and visit this page again soon to catch up on the students' progress."
+          standfirst = "Who better to dream up the cars of tomorrow than the people who'll be buying them? Students at Central St Martins are working with Renault to design the interior for cars that will drive themselves. Watch this short video to find out more about the project, and visit this page again soon to catch up on the students' progress.",
+          posterImage = Static("images/commercial/renault-video-poster.jpg").path
         )
         Cached(60)(RevalidatableResult.Ok(guardianHostedPage(page, teaserVideo())))
 
@@ -30,7 +32,8 @@ object HostedContentController extends Controller {
             pageName,
             pageTitle = "Renault shortlists 'car of the future' designs - video",
             videoTitle = "Renault shortlists 'car of the future' designs",
-            standfirst = "Renault challenged Central St Martins students to dream up the car of the future. The winning design will be announced at Clerkenwell Design Week (and on this site). Watch this short video to find out who made the shortlist."
+            standfirst = "Renault challenged Central St Martins students to dream up the car of the future. The winning design will be announced at Clerkenwell Design Week (and on this site). Watch this short video to find out who made the shortlist.",
+            posterImage = Static("images/commercial/renault-video-poster-ep1.png").path
           )
           Cached(60)(RevalidatableResult.Ok(guardianHostedPage(page, episode1Video())))
         } else {
