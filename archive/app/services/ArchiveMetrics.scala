743f87deac89afa72c30f3922c7ebce80ec17852
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-05-27 14:56:17 +0100

    Inject the first batch of lifecycle classes

--- archive/app/services/ArchiveMetrics.scala
+++ archive/app/services/ArchiveMetrics.scala
@@ -1,32 +1,32 @@
 package services
 
-import common.Jobs
+import common.{LifecycleComponent, Jobs}
 import metrics.CountMetric
 import model.diagnostics.CloudWatch
-import play.api.{GlobalSettings, Application => PlayApp}
+import play.api.inject.ApplicationLifecycle
+
+import scala.concurrent.{ExecutionContext, Future}
 
 object GoogleBotMetric {
   val Googlebot404Count = CountMetric("googlebot-404s", "Googlebot 404s")
 }
 
-trait ArchiveMetrics extends GlobalSettings {
+class ArchiveMetrics(appLifecycle: ApplicationLifecycle)(implicit ec: ExecutionContext) extends LifecycleComponent {
+
+  appLifecycle.addStopHook{ () => Future{
+    Jobs.deschedule("ArchiveSystemMetricsJob")
+  }}
 
   private def report() {
     CloudWatch.putMetrics("ArchiveMetrics", List(GoogleBotMetric.Googlebot404Count), List.empty)
   }
 
-  override def onStart(app: PlayApp) {
+  override def start():Unit = {
     Jobs.deschedule("ArchiveSystemMetricsJob")
-    super.onStart(app)
 
     Jobs.schedule("ArchiveSystemMetricsJob", "0 * * * * ?"){
       report()
     }
-  }
-
-  override def onStop(app: PlayApp) {
-    Jobs.deschedule("ArchiveSystemMetricsJob")
-    super.onStop(app)
   }
 
 }
