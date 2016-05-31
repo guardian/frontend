b34cf625deb7c8bb600ccdc68bd9bb8028834989
Sam Desborough <sam.desborough@gmail.com>
2016-05-31 13:33:42 +0100

    Add other EU referendum badges

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -7,12 +7,17 @@ import layout.FaciaContainer
 case class Badge(seriesTag: String, imageUrl: String)
 
 object Badges {
+  private val euSvg = Static("images/EU_Ref_Logo.svg")
+
   val usElection = Badge("us-news/us-elections-2016", Static("images/USElectionlogooffset.png"))
   val ausElection = Badge("australia-news/australian-election-2016", Static("images/AUSElectionBadge.png"))
-  val euElection = Badge("politics/eu-referendum", Static("images/EU_Ref_Logo.svg"))
-  val euRealityCheck = Badge("politics/series/eu-referendum-reality-check", Static("images/EU_Ref_Logo.svg"))
 
-  val allBadges = Seq(usElection, ausElection, euRealityCheck, euElection)
+  val euElection = Badge("politics/eu-referendum", euSvg)
+  val euRealityCheck = Badge("politics/series/eu-referendum-reality-check", euSvg)
+  val euBriefing = Badge("politics/series/eu-referendum-morning-briefing", euSvg)
+  val euSparrow = Badge("politics/series/eu-referendum-live-with-andrew-sparrow", euSvg)
+
+  val allBadges = Seq(usElection, ausElection, euElection, euRealityCheck, euBriefing, euSparrow)
 
   def badgeFor(c: ContentType) = c.tags.tags.map(_.id).foldLeft(None: Option[Badge]) { (maybeBadge, tag) =>
       maybeBadge orElse allBadges.find(b => b.seriesTag == tag)
