e42fcb7fa791ea29b76871705d52b0b88e46b7dd
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-06-01 13:45:17 +0100

    Documented takeWhile and dropWhile

--- static/src/javascripts/projects/common/utils/drop-while.js
+++ static/src/javascripts/projects/common/utils/drop-while.js
@@ -1,6 +1,9 @@
 define(function () {
     return dropWhile;
 
+    /**
+     * Returns a subset of arr from the first element x where f(x) is false on.
+     */
     function dropWhile(f, arr) {
         var i = -1;
         var size = arr.length;
--- static/src/javascripts/projects/common/utils/take-while.js
+++ static/src/javascripts/projects/common/utils/take-while.js
@@ -1,6 +1,10 @@
 define(function () {
     return takeWhile;
 
+    /**
+     * Returns a subset of arr from the beginning until the first element x
+     * where f(x) is false (not included).
+     */
     function takeWhile(f, arr) {
         var i = -1;
         var size = arr.length;
