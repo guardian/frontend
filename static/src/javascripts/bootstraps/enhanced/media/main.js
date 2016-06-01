3d85a82e09430ad443678640889a9f27f1359f20
James Gorrie <james.gorrie@theguardian.com>
2016-06-01 11:55:28 +0100

    only show ads if we are 100% certain we can

--- static/src/javascripts/bootstraps/enhanced/media/main.js
+++ static/src/javascripts/bootstraps/enhanced/media/main.js
@@ -197,9 +197,10 @@ define([
 
         var videoInfo = new Promise(function(resolve) {
             // We only have the canonical URL in videos embedded in articles / main media.
+            // These are set to the safest defaults that will always play video.
             var defaultVideoInfo = {
                 expired: false,
-                shouldHideAdverts: false
+                shouldHideAdverts: true
             };
 
             if (!canonicalUrl) {
