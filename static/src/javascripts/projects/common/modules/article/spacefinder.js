2a86c077cd46e628cd1fe27e0c385ad33746c08e
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-06-01 12:26:36 +0100

    Memoizing on*Loaded functions

--- static/src/javascripts/projects/common/modules/article/spacefinder.js
+++ static/src/javascripts/projects/common/modules/article/spacefinder.js
@@ -1,19 +1,21 @@
 define([
-    'common/utils/fastdom-promise',
     'qwery',
     'bean',
     'Promise',
+    'common/utils/fastdom-promise',
     'common/utils/config',
     'common/utils/mediator',
-    'lodash/functions/curry'
+    'common/modules/commercial/dfp/track-ad-load',
+    'lodash/functions/memoize'
 ], function (
-    fastdom,
     qwery,
     bean,
     Promise,
+    fastdom,
     config,
     mediator,
-    curry
+    trackAdLoaded,
+    memoize
 ) {
     // total_hours_spent_maintaining_this = 60
     //
@@ -55,12 +57,23 @@ define([
         fromBottom: false
     };
 
+    var defaultOptions = {
+        waitForImages: true,
+        waitForLinks: true,
+        waitForInteractives: false,
+        waitForAds: false
+    }
+
     function expire(resolve) {
         window.setTimeout(resolve, LOADING_TIMEOUT);
     }
 
-    function onImagesLoaded(body) {
-        var notLoaded = qwery('img', body).filter(function (img) {
+    function getFuncId(rules) {
+        return rules.bodySelector || 'document';
+    }
+
+    var onImagesLoaded = memoize(function (rules) {
+        var notLoaded = qwery('img', rules.body).filter(function (img) {
             return !img.complete;
         });
 
@@ -68,27 +81,27 @@ define([
             true :
             new Promise(function (resolve) {
                 var loadedCount = 0;
-                bean.on(body, 'load', notLoaded, function onImgLoaded() {
+                bean.on(rules.body, 'load', notLoaded, function onImgLoaded() {
                     loadedCount += 1;
                     if (loadedCount === notLoaded.length) {
-                        bean.off(body, 'load', onImgLoaded);
+                        bean.off(rules.body, 'load', onImgLoaded);
                         notLoaded = null;
                         resolve();
                     }
                 });
             });
-    }
+    }, getFuncId);
 
-    function onRichLinksUpgraded(body) {
-        return qwery('.element-rich-link--not-upgraded', body).length === 0 ?
+    var onRichLinksUpgraded = memoize(function (rules) {
+        return qwery('.element-rich-link--not-upgraded', rules.body).length === 0 ?
             true :
             new Promise(function (resolve) {
                 mediator.once('rich-link:loaded', resolve);
             });
-    }
+    }, getFuncId);
 
-    function onInteractivesLoaded(body) {
-        var notLoaded = qwery('.element-interactive', body).filter(function (interactive) {
+    var onInteractivesLoaded = memoize(function (rules) {
+        var notLoaded = qwery('.element-interactive', rules.body).filter(function (interactive) {
             var iframe = qwery(interactive.children).filter(isIframe);
             return !(iframe.length && isIframeLoaded(iframe[0]));
         });
@@ -127,7 +140,14 @@ define([
                 iframe.contentWindow.document &&
                 iframe.contentWindow.document.readyState === 'complete';
         }
-    }
+    }, getFuncId);
+
+    var onAdsLoaded = memoize(function (rules) {
+        return Promise.all(qwery('.js-ad-slot', rules.body)
+            .map(function (ad) { return ad.id })
+            .map(trackAdLoaded)
+        );
+    }, getFuncId);
 
     // test one element vs another for the given rules
     function _testCandidate(rules, challenger, opponent) {
@@ -139,7 +159,7 @@ define([
 
     // test one element vs an array of other elements for the given rules
     function _testCandidates(rules, challenger, opponents) {
-        return opponents.every(curry(_testCandidate)(rules, challenger));
+        return opponents.every(_testCandidate.bind(undefined, rules, challenger));
     }
 
     function _mapElementToComputedDimensions(el) {
