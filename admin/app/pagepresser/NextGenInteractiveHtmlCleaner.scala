7731fde6db5045eab390523ad6e342e17c8f6700
Justin Pinner <justinpinner@gmail.com>
2016-06-02 10:35:50 +0100

    Replace commented code with full-on comment

--- admin/app/pagepresser/NextGenInteractiveHtmlCleaner.scala
+++ admin/app/pagepresser/NextGenInteractiveHtmlCleaner.scala
@@ -12,7 +12,11 @@ object NextGenInteractiveHtmlCleaner extends HtmlCleaner with implicits.WSReques
 
   override def clean(document: Document, convertToHttps: Boolean) = {
     universalClean(document)
-    //removeScripts(document)
+    /*
+    This cleaner currently leaves all of the page scripts intact so as not to break the functionality
+    of the interactive content. When we have a better understanding of how to identify scripts that *can*
+    be removed, we can reinstate a call to an alternative version of removeScripts(document) at this point.
+    */
     createSimplePageTracking(document)
     removeByTagName(document, "noscript")
     if (convertToHttps) secureDocument(document)
