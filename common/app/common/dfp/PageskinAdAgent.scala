d08a263e7766492d0ddd85ee6f75a0836b9847f9
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-06-02 16:27:28 +0100

    removed overwriting booleans

--- common/app/common/dfp/PageskinAdAgent.scala
+++ common/app/common/dfp/PageskinAdAgent.scala
@@ -32,13 +32,11 @@ trait PageskinAdAgent {
   // The ad unit is considered to have a page skin if it has a corresponding sponsorship.
   // If the sponsorship is an adTest, it is only considered outside of production.
   def hasPageSkin(adUnitWithoutRoot: String, edition: Edition): Boolean = {
-    true
-//    findSponsorships(adUnitWithoutRoot, edition) exists (sponsorship => !(environmentIsProd && sponsorship.targetsAdTest))
+    findSponsorships(adUnitWithoutRoot, edition) exists (sponsorship => !(environmentIsProd && sponsorship.targetsAdTest))
   }
 
   // True if there is any candidate sponsorship for this ad unit. Used to decide when to render the out-of-page ad slot.
   def hasPageSkinOrAdTestPageSkin(adUnitWithoutRoot: String, edition: Edition): Boolean = {
-    true
-//    findSponsorships(adUnitWithoutRoot, edition).nonEmpty
+    findSponsorships(adUnitWithoutRoot, edition).nonEmpty
   }
 }
