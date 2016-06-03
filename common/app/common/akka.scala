f7877b3c5d5981614b8c4110cd4a73d2dd5e84f7
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-06-03 17:30:47 +0100

    Ensure the akka utils don't use an Application that does not exists.

--- common/app/common/akka.scala
+++ common/app/common/akka.scala
@@ -3,31 +3,38 @@ package common
 import akka.agent.Agent
 import play.api.libs.concurrent.{Akka => PlayAkka}
 import scala.concurrent.duration._
-import play.api.Play
+import play.api.{Mode, Environment, Play}
 import scala.concurrent.ExecutionContext
 import akka.actor.ActorSystem
-import play.api.Play.current
 
 object ExecutionContexts extends ExecutionContexts
 
 trait ExecutionContexts {
   implicit lazy val executionContext: ExecutionContext = play.api.libs.concurrent.Execution.Implicits.defaultContext
-  lazy val actorSystem: ActorSystem = PlayAkka.system
-  lazy val feedsRecorderExecutionContext: ExecutionContext = PlayAkka.system.dispatchers.lookup("akka.actor.feed-recorder")
+  lazy val actorSystem: ActorSystem = PlayAkka.system(Play.current)
+  lazy val feedsRecorderExecutionContext: ExecutionContext = PlayAkka.system(Play.current).dispatchers.lookup("akka.actor.feed-recorder")
 }
 
 object AkkaAgent extends ExecutionContexts {
   def apply[T](value: T): Agent[T] = Agent(value)
 }
 
-object AkkaAsync extends ExecutionContexts {
+class AkkaAsync(env: Environment, actorSystem: ActorSystem) {
+  implicit val ec: ExecutionContext = actorSystem.dispatcher
 
   def apply(body: => Unit): Unit = after(1.second){ body }
+
+  def after1s(body: => Unit): Unit = after(1.second){ body }
 
   // running scheduled jobs in tests is useless
   // it just results in unexpected data files when you
   // want to check in
-  def after(delay: FiniteDuration)(body: => Unit): Unit = if (!Play.isTest) {
-    PlayAkka.system(Play.current).scheduler.scheduleOnce(delay) { body }
+  def after(delay: FiniteDuration)(body: => Unit): Unit = if (env.mode != Mode.Test) {
+    actorSystem.scheduler.scheduleOnce(delay)(body)
   }
 }
+
+object AkkaAsync extends AkkaAsync(
+  env = Environment(Play.current.path, Play.current.classloader, Play.current.mode),
+  actorSystem = PlayAkka.system(Play.current)
+)
