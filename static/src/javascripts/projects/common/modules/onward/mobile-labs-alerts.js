9a11759c995cd9e7a8a6d5b6dd5f0438a3f55229
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2016-06-03 15:20:40 +0100

    sort out banner styling

--- static/src/javascripts/projects/common/modules/onward/mobile-labs-alerts.js
+++ static/src/javascripts/projects/common/modules/onward/mobile-labs-alerts.js
@@ -16,39 +16,34 @@ define([
     some
 ){
 
-    var usElectionTag = "us-news/us-elections-2016",
-        messageId = "mobile-labs-alert",
+
+    var usElectionTag = 'us-news/us-elections-2016',
+        messageId = 'mobile-labs-alert',
         messageOptions = {
             siteMessageLinkName: 'mobile labs | message | presidential primary alerts',
             siteMessageCloseButton: 'hide',
-            cssModifierClass: "mobile-labs"
+            cssModifierClass: 'mobile-labs'
         },
 
         messageTemplateOptions = {
             linkHref: 'http://www.gdnmobilelab.com/primaries',
             linkText: 'Sign up now',
             linkName: 'site-message--mobile-labs',
-            messageText: 'Interested in the U.S elections?',
+            messageHeadline: 'Interested in the U.S elections?',
+            messageText: 'Get experimental mobile alerts during the June 7 presidential primary',
             arrowWhiteRight: svgs('arrowWhiteRight')
         };
 
-
     function canShowPromo() {
-        var isPromoSwitchedOn = isSwitchedOn(),
-            isUsUser = UsUser(),
-            isMobileChromeUser = mobileChromeUser(),
-            isUsElectionPage = isUsElection();
-
-        console.log("++ Can we, can't we?  Switch" +  isPromoSwitchedOn + " US user: " + isUsUser + " mobile chrome " + isMobileChromeUser + " Election: " + isUsElectionPage);
-        return isPromoSwitchedOn && isUsUser && isUsElectionPage;
+        return isSwitchedOn() && UsUser() && mobileChromeUser() && isUsElection();
     }
 
     function isSwitchedOn() {
        return config.switches.mobileLabPresidentialPrimaries;
-}
+    }
 
     function UsUser() {
-        return config.page.edition && config.page.edition === 'US'
+        return config.page.edition && config.page.edition === 'US';
     }
 
     function mobileChromeUser() {
@@ -56,17 +51,15 @@ define([
     }
 
     function isUsElection() {
-        return some(config.page.keywordIds.split(","), function(keywordId) {
-            return keywordId === usElectionTag
-        })
+        return some(config.page.keywordIds.split(','), function(keywordId) {
+            return keywordId === usElectionTag;
+        });
     }
 
     return function () {
         if (canShowPromo()) {
-            console.log("Showing At: ");
             new Message(messageId, messageOptions).show(template(messageTemplate, messageTemplateOptions));
-            console.log("Shown At: ");
         }
-    }
+    };
 
 });
