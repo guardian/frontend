570340c58299ea2fd943e312781d54708773792e
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2016-06-06 14:09:36 +0100

    Remove unused TV AB test

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -32,15 +32,6 @@ trait ABTestSwitches {
     exposeClientSide = true
   )
 
-  val ABParticipationLowFricTv = Switch(
-    SwitchGroup.ABTests,
-    "ab-participation-low-fric-tv",
-    "AB test switch to insert low friction participation into tv",
-    safeState = Off,
-    sellByDate = new LocalDate(2016, 6, 15),
-    exposeClientSide = true
-  )
-
   val ABParticipationLowFricRecipes = Switch(
     SwitchGroup.ABTests,
     "ab-participation-low-fric-recipes",
