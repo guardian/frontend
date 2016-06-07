b989824798fa94dd08e244b835500bd8ae65e06f
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-06-07 17:14:17 +0100

    Hosted By - fix pinterest share button, restore ''Advertiser content hosted by the Guardian: " prefix to page titles and share buttons, and fix layout on iphone (display: block was overriding display: flex)

--- common/app/common/commercial/HostedPage.scala
+++ common/app/common/commercial/HostedPage.scala
@@ -9,13 +9,14 @@ import play.api.libs.json.JsString
 case class HostedPage(
                        pageUrl: String,
                        pageName: String,
-                       pageTitle: String,
                        standfirst: String,
                        logoUrl: String,
                        bannerUrl: String,
                        video: HostedVideo,
                        nextPageName: String
                      ) extends StandalonePage {
+
+  val pageTitle: String  = s"Advertiser content hosted by the Guardian: ${video.title} - video"
 
   override val metadata: MetaData = {
     val toneId = "tone/hosted-content"
@@ -67,7 +68,6 @@ object HostedPage {
   private val teaser: HostedPage = HostedPage(
     pageUrl = "https://www.theguardian.com/commercial/advertiser-content/renault-car-of-the-future/design-competition-teaser",
     pageName = teaserPageName,
-    pageTitle = "Advertiser content hosted by the Guardian: Designing the car of the future",
     standfirst = "Who better to dream up the cars of tomorrow than the people who'll be buying them? Students at Central St Martins are working with Renault to design the interior for cars that will drive themselves. Watch this short video to find out more about the project, and visit this page again soon to catch up on the students' progress",
     logoUrl = Static("images/commercial/logo_renault.jpg"),
     bannerUrl = Static("images/commercial/ren_commercial_banner.jpg"),
@@ -84,7 +84,6 @@ object HostedPage {
   private val episode1: HostedPage = HostedPage(
     pageUrl = "https://www.theguardian.com/commercial/advertiser-content/renault-car-of-the-future/design-competition-episode1",
     pageName = episode1PageName,
-    pageTitle = "Renault shortlists 'car of the future' designs",
     standfirst = "Renault challenged Central St Martins students to dream up the car of the future. The winning design will be announced at Clerkenwell Design Week (and on this site). Watch this short video to find out who made the shortlist",
     logoUrl = Static("images/commercial/logo_renault.jpg"),
     bannerUrl = Static("images/commercial/ren_commercial_banner.jpg"),
@@ -101,7 +100,6 @@ object HostedPage {
   private val episode2: HostedPage = HostedPage(
     pageUrl = "https://www.theguardian.com/commercial/advertiser-content/renault-car-of-the-future/design-competition-episode2",
     pageName = episode2PageName,
-    pageTitle = "Is this the car of the future?",
     standfirst = "A group of Central St Martins students took part in a competition to dream up the car of the future. The winning design is radical and intriguing. Meet the team whose blue-sky thinking may have created a blueprint for tomorrow's autonomous cars",
     logoUrl = Static("images/commercial/logo_renault.jpg"),
     bannerUrl = Static("images/commercial/ren_commercial_banner.jpg"),
