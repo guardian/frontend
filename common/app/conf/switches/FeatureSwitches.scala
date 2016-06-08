d06f5586ade3c9cfac162dd0a89aadbd70f175a6
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2016-06-08 11:12:54 +0100

    delete the correct switch. Almost whoopsy

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -379,13 +379,13 @@ trait FeatureSwitches {
     exposeClientSide = true
   )
 
-  //Owener Dotcom participation
-  val MobileLabPresidentialPrimaries = Switch(
+  // Owner: Dotcom habitual / Gareth
+  val EmailSignupLabNotes = Switch(
     SwitchGroup.Feature,
-    "mobile-lab-presidential-primaries",
-    "When ON, we present a promo banner to android, US edition users inviting them to subscribe to experimental primaries alerts",
+    "email-signup-lab-notes",
+    "When ON, insert the lab-notes email sign-up into Science section articles",
     safeState = Off,
-    sellByDate = new LocalDate(2016, 6, 8),
+    sellByDate = never,
     exposeClientSide = true
   )
 
