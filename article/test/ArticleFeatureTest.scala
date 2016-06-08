7b36ffd4934db186c8fa75114f658701dfb72c4b
Kate Whalen <whalenka88@gmail.com>
2016-06-08 10:53:39 +0100

    Update twitter card test

--- article/test/ArticleFeatureTest.scala
+++ article/test/ArticleFeatureTest.scala
@@ -546,10 +546,7 @@ import collection.JavaConversions._
         $("meta[name='twitter:site']").getAttributes("content").head should be("@guardian")
         $("meta[name='twitter:card']").getAttributes("content").head should be("summary_large_image")
         $("meta[name='twitter:app:url:googleplay']").getAttributes("content").head should startWith("guardian://www.theguardian.com/world")
-
-        // at the time of writing, Twitter does not like i.guim.co.uk
-        // will see if I can get that fixed, but in the meantime this must be static.guim.co.uk
-        $("meta[name='twitter:image']").getAttributes("content").head should be("https://static-secure.guim.co.uk/sys-images/Guardian/Pix/GU_front_gifs/2013/9/15/1379275549160/Irans-President-Hassan-Ro-010.jpg?w=1200&q=55&auto=format&usm=12&fit=max&s=2536d2d0e1a5d9daa8dc9f8466901e44")
+        $("meta[name='twitter:image']").getAttributes("content").head should contain("/2013/9/15/1379275549160/Irans-President-Hassan-Ro-010.jpg")
       }
     }
 
