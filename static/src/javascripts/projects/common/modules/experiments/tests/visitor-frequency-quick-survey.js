23a6c6b04bb8aa0bf6d9725db90f661e8fad16dc
Kate Whalen <whalenka88@gmail.com>
2016-06-14 15:49:26 +0100

    Add 'Thank you' and fade in effect

--- static/src/javascripts/projects/common/modules/experiments/tests/visitor-frequency-quick-survey.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/visitor-frequency-quick-survey.js
@@ -25,36 +25,44 @@ define([
             // check for the cookie related to this survey
         };
 
-        function createQuickSurvey() {
+        function renderQuickSurvey() {
+            // this cannot be wrapped in a fastdom function; the disable function breaks
             var submeta = document.getElementsByClassName('submeta')[0];
             var surveyHeading = document.createElement('div');
             var surveyBody = document.createElement('div');
+            var surveyThanks = document.createElement('div');
             mutateSurveyHeading(surveyHeading);
             mutateSurveyBody(surveyBody);
+            mutateSurveyThanks(surveyThanks);
             submeta.appendChild(surveyHeading);
             submeta.appendChild(surveyBody);
+            surveyBody.appendChild(surveyThanks);
         }
 
         function mutateSurveyHeading(domElement) {
             domElement.className += 'impressions-survey__heading';
             domElement.style.cssText += 'color:#fff;margin-top:20px;padding: 1em 1em 1em 3em;font-size:0.8em;background-color:#AAD7E4;';
-            domElement.innerHTML = '<h2 style="color:#333;font-family:"Guardian Text Sans Web","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif;font-weight:bold;margin-left:-1em;">Quick Survey</h2>';
+            domElement.innerHTML = '<h2 style="color:#333;font-family:Guardian Text Sans Web,Helvetica Neue,Helvetica,Arial,Lucida Grande,sans-serif;font-weight:bold;margin-left:-1em;">Quick Survey</h2>';
         }
 
         function mutateSurveyBody(domElement) {
             domElement.className += 'impressions-survey__body';
             domElement.setAttribute('data-link-name', 'impressions-frequency-survey');
-            domElement.style.cssText += 'color:#fff;padding: 1em 1em 1em 3em;font-size:0.8em;background-color:#005689;';
-            domElement.innerHTML = '<h3 style="font-weight:bold;margin-left:-1em;">How often do you read the Guardian in a digital format?</h3><div id="impressions-survey__select"><label><input type="radio" class="fi-survey__button" data-link-name="frequency_5" value="frequency_5">Every day/most days</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_4" value="frequency_4">Weekly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_3" value="frequency_3">Fortnightly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_2" value="frequency_2">Monthly or less</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_1" value="frequency_1">This is my first visit</label><br></div>';
+            domElement.style.cssText += 'color:#fff;padding: 1em 1em 1em 3em;font-size:0.8em;background-color:#005689;opacity:1;position:relative';
+            domElement.innerHTML = '<div id="surveyTextbox"><h3 style="font-weight:bold;margin-left:-1em;">How often do you read the Guardian in a digital format?</h3><div id="impressions-survey__select"><label><input type="radio" class="fi-survey__button" data-link-name="frequency_5" value="frequency_5">Every day/most days</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_4" value="frequency_4">Weekly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_3" value="frequency_3">Fortnightly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_2" value="frequency_2">Monthly or less</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_1" value="frequency_1">This is my first visit</label><br></div></div>';
+        }
+
+        function mutateSurveyThanks(domElement) {
+            domElement.className += 'impressions-survey__thanks';
+            domElement.style.cssText += 'color:#fff;font-size:2em;font-weight:bold;position:absolute;top:20%;opacity:0';
+            domElement.innerHTML = '<h1>Thank you</h1>';
         }
 
         function disableRadioButtons(buttonClassName) {
             var radioButtons = document.getElementsByClassName(buttonClassName);
-            fastdom.write(function () {
-                for (var i = 0; i < radioButtons.length; i++) {
+            for (var i = 0; i < radioButtons.length; i++) {
                     radioButtons[i].disabled=true;
                 }
-            });
         }
 
         function handleSurveyResponse(buttonClassName) {
@@ -62,14 +70,32 @@ define([
             bean.on(surveyQuestion, 'click', function () {
                 fastdom.write(function () {
                     disableRadioButtons(buttonClassName);
-                    thankyouFadeIn(surveyQuestion);
+                    surveyFadeOut();
+                    thankyouFadeIn();
                 });
             });
         }
 
-        function thankyouFadeIn(domElement) {
+        function isInPrivateBrowsingMode() {
+            // return a boolean; true is
+        }
+
+        function setCookieForSurvey() {
+            // give user a cookie to say that they have responded
+            // if user has this cookie then do not show the survey again
+        }
+
+        function surveyFadeOut() {
+            var surveyTextbox = document.getElementById('surveyTextbox');
             fastdom.write(function () {
-                domElement.style.cssText += 'visibility:hidden;opacity:0;transition:opacity 0.5s linear;';
+                surveyTextbox.style.cssText += 'visibility:hidden;opacity:0;transition:visibility 0s ease-in 0.5s,opacity 0.5s linear';
+            });
+        }
+
+        function thankyouFadeIn() {
+            var surveyThanks = document.getElementsByClassName('impressions-survey__thanks');
+            fastdom.write(function () {
+                surveyThanks[0].style.cssText += 'visibility:visible;opacity:1;transition:visibility 0s ease-in 0.5s,opacity 0.8s linear';
             });
         }
 
@@ -77,7 +103,7 @@ define([
             {
                 id: 'variant',
                 test: function () {
-                    createQuickSurvey();
+                    renderQuickSurvey();
                     handleSurveyResponse('fi-survey__button');
                 }
             },
