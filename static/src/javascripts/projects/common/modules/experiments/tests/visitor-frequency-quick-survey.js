7c26fcbe02483971b4bb31a8387d7c71d2dd6fad
Kate Whalen <whalenka88@gmail.com>
2016-06-14 11:07:23 +0100

    Remove ES6 features

--- static/src/javascripts/projects/common/modules/experiments/tests/visitor-frequency-quick-survey.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/visitor-frequency-quick-survey.js
@@ -38,14 +38,14 @@ define([
         function mutateSurveyHeading(domElement) {
             domElement.className += 'impressions-survey__heading';
             domElement.style.cssText += 'color:#fff;margin-top:20px;padding: 1em 1em 1em 3em;font-size:0.8em;background-color:#AAD7E4;';
-            domElement.innerHTML = `<h2 style="color:#333;font-family:"Guardian Text Sans Web","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif;font-weight:bold;margin-left:-1em;">Quick Survey</h2>`;
+            domElement.innerHTML = '<h2 style="color:#333;font-family:"Guardian Text Sans Web","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif;font-weight:bold;margin-left:-1em;">Quick Survey</h2>';
         }
 
         function mutateSurveyBody(domElement) {
             domElement.className += 'impressions-survey__body';
             domElement.setAttribute('data-link-name', 'impressions-frequency-survey');
             domElement.style.cssText += 'color:#fff;padding: 1em 1em 1em 3em;font-size:0.8em;background-color:#005689;';
-            domElement.innerHTML = `<h3 style="font-weight:bold;margin-left:-1em;">How often do you read the Guardian in a digital format?</h3><div id="impressions-survey__select"><label><input type="radio" class="fi-survey__button" data-link-name="frequency_5" value="frequency_5">Every day/most days</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_4" value="frequency_4">Weekly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_3" value="frequency_3">Fortnightly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_2" value="frequency_2">Monthly or less</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_1" value="frequency_1">This is my first visit</label><br></div>`;
+            domElement.innerHTML = '<h3 style="font-weight:bold;margin-left:-1em;">How often do you read the Guardian in a digital format?</h3><div id="impressions-survey__select"><label><input type="radio" class="fi-survey__button" data-link-name="frequency_5" value="frequency_5">Every day/most days</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_4" value="frequency_4">Weekly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_3" value="frequency_3">Fortnightly</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_2" value="frequency_2">Monthly or less</label><br><label><input type="radio" class="fi-survey__button" data-link-name="frequency_1" value="frequency_1">This is my first visit</label><br></div>';
         }
 
         function disableRadioButtons(buttonClassName) {
