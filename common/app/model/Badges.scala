0b088ec686eb000748500fcf91d3b7fb091b4772
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-06-14 11:47:12 -0400

    Change to modifier to optional field

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -4,19 +4,19 @@ import conf.switches.Switches
 import conf.{Configuration, Static}
 import layout.FaciaContainer
 
-case class Badge(seriesTag: String, imageUrl: String, classModifier: String)
+case class Badge(seriesTag: String, imageUrl: String, classModifier: Option[String] = None)
 
 object Badges {
   private val euSvg = Static("images/badges/eu-ref.svg")
 
-  val usElection = Badge("us-news/us-elections-2016", Static("images/badges/us-election.png"), "us-election")
-  val ausElection = Badge("australia-news/australian-election-2016", Static("images/badges/aus-election.png"), "aus-election")
-  val voicesOfAmerica = Badge("us-news/series/voices-of-america", Static("images/badges/voices-of-america.svg"), "voices-of-america")
+  val usElection = Badge("us-news/us-elections-2016", Static("images/badges/us-election.png"), Some("us-election"))
+  val ausElection = Badge("australia-news/australian-election-2016", Static("images/badges/aus-election.png"), Some("aus-election"))
+  val voicesOfAmerica = Badge("us-news/series/voices-of-america", Static("images/badges/voices-of-america.svg"), Some("voices-of-america"))
 
-  val euElection = Badge("politics/eu-referendum", euSvg, "eu-ref")
-  val euRealityCheck = Badge("politics/series/eu-referendum-reality-check", euSvg, "eu-ref")
-  val euBriefing = Badge("politics/series/eu-referendum-morning-briefing", euSvg, "eu-ref")
-  val euSparrow = Badge("politics/series/eu-referendum-live-with-andrew-sparrow", euSvg, "eu-ref")
+  val euElection = Badge("politics/eu-referendum", euSvg)
+  val euRealityCheck = Badge("politics/series/eu-referendum-reality-check", euSvg)
+  val euBriefing = Badge("politics/series/eu-referendum-morning-briefing", euSvg)
+  val euSparrow = Badge("politics/series/eu-referendum-live-with-andrew-sparrow", euSvg)
 
   val allBadges = Seq(usElection, ausElection, voicesOfAmerica, euElection, euRealityCheck, euBriefing, euSparrow)
 
