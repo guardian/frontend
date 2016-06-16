a2b5457d6a7e1ddef69875668d26690826b3529f
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-06-16 18:49:17 +0100

    Hosted By Gallery - add progress spinner

--- static/src/javascripts/projects/common/modules/commercial/hosted-gallery.js
+++ static/src/javascripts/projects/common/modules/commercial/hosted-gallery.js
@@ -16,6 +16,8 @@ define([
     function init() {
         var $scrollEl = $('.hosted-gallery__scroll-container');
         var $images = $('.hosted-gallery__image');
+        var $progress = $('.hosted-gallery__progress');
+        var $border = $('.hosted-gallery__progress--border-2');
         if (!$scrollEl.length) {
             return;
         }
@@ -23,10 +25,22 @@ define([
         bean.on($scrollEl[0], 'scroll', debounce(function (e) {
             var scrollTop = e.target.scrollTop;
             var scrollHeight = e.target.scrollHeight;
-            var opacity = $images.length * (scrollTop/scrollHeight) + 1;
+            var progress = $images.length * (scrollTop/scrollHeight) + 1;
+            var fractionProgress = progress % 1;
+            var deg = Math.ceil(fractionProgress * 360);
             fastdom.write(function () {
                 $images.each(function (image, index) {
-                    bonzo(image).css('opacity', Math.min(Math.max(opacity - index, 0), 1));
+                    var opacity = Math.min(Math.max((progress - index) * 4 / 3, 0), 1);
+                    bonzo(image).css('opacity', opacity);
+                });
+                bonzo($border).css('transform', 'rotate('+deg+'deg)');
+                bonzo($border).css('-webkit-transform', 'rotate('+deg+'deg)');
+                ['quarter-2', 'quarter-3', 'quarter-4'].forEach(function (cssClass, index) {
+                    if(4 * fractionProgress > index + 1){
+                        bonzo($progress).addClass(cssClass);
+                    } else {
+                        bonzo($progress).removeClass(cssClass);
+                    }
                 });
             });
         }, 10));
