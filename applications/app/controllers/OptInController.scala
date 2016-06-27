240eb6a042e2124d801ed1c830815db62217fbf3
Justin Pinner <justinpinner@gmail.com>
2016-06-27 17:59:53 +0100

    Switch https opt-in for opt-out behaviour

--- applications/app/controllers/OptInController.scala
+++ applications/app/controllers/OptInController.scala
@@ -6,16 +6,28 @@ import play.api.mvc.{Action, Controller, Cookie, DiscardingCookie}
 
 import scala.concurrent.duration._
 
-object OptInController extends Controller {
-  case class OptInFeature(cookieName: String, lifetime: Int = 90.days.toSeconds.toInt) {
-    def opt(choice: String) = choice match {
-      case "in" => optIn()
-      case _ => optOut()
-    }
-
-    def optIn() = SeeOther("/").withCookies(Cookie(cookieName, "true", maxAge = Some(lifetime)))
-    def optOut() = SeeOther("/").discardingCookies(DiscardingCookie(cookieName))
+trait OptInOutFeature extends Controller {
+  val cookieName: String
+  val lifetime: Int
+  def opt(choice: String) = choice match {
+    case "in" => optIn()
+    case _ => optOut()
   }
+  def optIn() = SeeOther("/").withCookies(Cookie(cookieName, "true", maxAge = Some(lifetime)))
+  def optOut() = SeeOther("/").discardingCookies(DiscardingCookie(cookieName))
+}
+
+case class OptInFeature(cookieName: String, lifetime: Int = 90.days.toSeconds.toInt) extends OptInOutFeature
+
+class HttpsOptOutFeature extends OptInOutFeature {
+  // replaces previous opt-in feature with opt-out (we're https by default now)
+  val cookieName = "https_opt_out"
+  val lifetime: Int = 365.days.toSeconds.toInt
+  override def optIn() = SeeOther("/").discardingCookies(DiscardingCookie(cookieName),DiscardingCookie("https_opt_in"))
+  override def optOut() = SeeOther("/").withCookies(Cookie(cookieName, "true", maxAge = Some(lifetime)))
+}
+
+object OptInController extends Controller {
 
   def handle(feature: String, choice: String) = Action { implicit request =>
     Cached(60)(WithoutRevalidationResult(feature match {
@@ -26,7 +38,7 @@ object OptInController extends Controller {
     }))
   }
 
-  val HTTPS = OptInFeature("https_opt_in")
+  val HTTPS = new HttpsOptOutFeature
   val Header = OptInFeature("new_header_opt_in")
   val gallery = OptInFeature("gallery_redesign_opt_in")
 }
