16d60d37f6f1655b10e474f2cbe7a41335b22d4a
NataliaLKB <natalialeebaltazar@gmail.com>
2016-06-30 12:29:50 +0100

    menu opening with animation :) :tada:

---
+++ static/src/javascripts/projects/common/modules/navigation/newHeaderNavigation.js
@@ -0,0 +1,77 @@
+define([
+    'common/utils/fastdom-promise',
+    'common/utils/$'
+], function (
+    fastdomPromise,
+    $
+) {
+    var mainMenuId = '#main-menu';
+    var html = $('html');
+    var mainMenuEl = $(mainMenuId);
+    var burgerMenu = $('.js-animate-menu');
+    var burgerLink = $('.js-change-link');
+
+    function animateMenuOpen() {
+        return fastdomPromise.write(function () {
+            mainMenuEl.addClass('shown');
+            mainMenuEl.addClass('off-screen');
+
+            burgerMenu.addClass('new-header__burger-icon--open');
+            burgerLink.attr('href', '#');
+        }).then(function () {
+            return fastdomPromise.write(function () {
+                mainMenuEl.removeClass('off-screen');
+            });
+        }).then(function () {
+            var firstButton = $('.main-navigation__item__button')[0];
+            if (firstButton) {
+                firstButton.focus();
+            }
+            // Prevents scrolling on the body
+            html.css('overflow', 'hidden');
+        });
+    }
+
+    function animateMenuClose() {
+        return fastdomPromise.write(function () {
+            mainMenuEl.addClass('off-screen');
+
+            burgerMenu.removeClass('new-header__burger-icon--open');
+            burgerLink.attr('href', mainMenuId);
+
+            // TODO: Support browsers that don't have transitions
+            // We still want to hide this
+            if (mainMenuEl.length > 0) {
+                mainMenuEl[0].addEventListener('transitionend', function handler() {
+                    return fastdomPromise.write(function () {
+                        mainMenuEl.removeClass('off-screen');
+                        mainMenuEl.removeClass('shown');
+                        mainMenuEl[0].removeEventListener('transitionend', handler);
+                    }).then(function() {
+                        $('.new-header__nav__menu-button').focus();
+                        // Users should be able to scroll again
+                        html.css('overflow', '');
+                    });
+                });
+            }
+        });
+    }
+
+    function handleHashChange () {
+        var shouldShowMenu = window.location.hash === mainMenuId;
+        var shouldHideMenu = window.location.hash === '';
+
+        if (shouldShowMenu) {
+            animateMenuOpen();
+        } else if (shouldHideMenu) {
+            animateMenuClose();
+        }
+    }
+
+    function init() {
+        window.addEventListener('hashchange', handleHashChange);
+        handleHashChange();
+    }
+
+    return init;
+});
