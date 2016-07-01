b4b218ccad31daa02f09da28351c2d995f852e37
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-07-01 15:39:20 +0100

    Module for dfp.loadAds

---
+++ static/src/javascripts/projects/common/modules/commercial/dfp/load.js
@@ -0,0 +1,56 @@
+define([
+    'qwery',
+    'common/utils/sha1',
+    'common/modules/identity/api',
+    'common/modules/commercial/commercial-features',
+    'common/modules/commercial/dfp/dfp-obj',
+    'common/modules/commercial/dfp/private/dfp-env',
+    'common/modules/commercial/dfp/private/lazy-load',
+    'common/modules/commercial/dfp/private/create-advert',
+    'common/modules/commercial/dfp/private/queue-advert',
+    'common/modules/commercial/dfp/private/display-lazy-ads',
+    'common/modules/commercial/dfp/private/display-ads',
+    'common/modules/commercial/dfp/private/refresh-on-resize'
+], function (qwery, sha1, identity, commercialFeatures, dfp, dfpEnv, lazyLoad, createAdvert, queueAdvert, displayLazyAds, displayAds, refreshOnResize) {
+    dfp.loadAds = load;
+    return load;
+
+    function load() {
+        if (commercialFeatures.dfpAdvertising) {
+            loadAdvertising();
+        }
+    }
+
+    function loadAdvertising() {
+        createAdverts();
+        window.googletag.cmd.push(
+            queueAdverts,
+            setPublisherProvidedId,
+            dfpEnv.fn.shouldLazyLoad() ? displayLazyAds : displayAds,
+            // anything we want to happen after displaying ads
+            refreshOnResize
+        );
+    }
+
+    function createAdverts() {
+        // Get all ad slots
+        dfpEnv.adverts = qwery(dfpEnv.adSlotSelector).map(createAdvert);
+    }
+
+    /**
+     * Loop through each slot detected on the page and define it based on the data
+     * attributes on the element.
+     */
+    function queueAdverts() {
+        // queue ads for load
+        dfpEnv.adverts.forEach(queueAdvert);
+    }
+
+    function setPublisherProvidedId() {
+        var user = identity.getUserFromCookie();
+        if (user) {
+            var hashedId = sha1.hash(user.id);
+            window.googletag.pubads().setPublisherProvidedId(hashedId);
+        }
+    }
+});
