3d7d0e49a9c80aeb7801d0db311470901913d26c
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-07-01 15:38:37 +0100

    Modules for addSlot/trackAdRender/trackAdLoad

---
+++ static/src/javascripts/projects/common/modules/commercial/dfp/add-slot.js
@@ -0,0 +1,37 @@
+define([
+    'common/modules/commercial/dfp/private/dfp-env',
+    'common/modules/commercial/dfp/private/create-advert',
+    'common/modules/commercial/dfp/private/queue-advert',
+    'common/modules/commercial/dfp/private/lazy-load',
+    'common/modules/commercial/dfp/dfp-obj',
+    'common/utils/mediator'
+], function (dfpEnv, createAdvert, queueAdvert, lazyLoad, dfp, mediator) {
+    dfp.addSlot = addSlot;
+    return addSlot;
+
+    function addSlot(adSlot) {
+        adSlot = adSlot instanceof HTMLElement ? adSlot : adSlot[0];
+
+        if (dfpEnv.displayed && !(adSlot.id in dfpEnv.advertIds)) { // dynamically add ad slot
+            // this is horrible, but if we do this before the initial ads have loaded things go awry
+            if (dfpEnv.rendered) {
+                displayAd(adSlot);
+            } else {
+                mediator.once('modules:commercial:dfp:rendered', function () {
+                    displayAd(adSlot);
+                });
+            }
+        }
+    }
+
+    function displayAd(adSlot) {
+        var advert = createAdvert(adSlot);
+        dfpEnv.adverts.push(advert);
+        queueAdvert(advert);
+        if (dfpEnv.fn.shouldLazyLoad()) {
+            dfpEnv.fn.enableLazyLoad();
+        } else {
+            dfpEnv.fn.loadAdvert(advert);
+        }
+    }
+});
