9fb66d696214d292ac7329621be3faaeee829a3b
David Furey <david.furey@guardian.co.uk>
2016-07-04 11:25:07 +0100

    Remove new user ad free test

--- static/src/javascripts/bootstraps/enhanced/common.js
+++ static/src/javascripts/bootstraps/enhanced/common.js
@@ -199,7 +199,7 @@ define([
             },
 
             cleanupCookies: function () {
-                cookies.cleanUp(['mmcore.pd', 'mmcore.srv', 'mmid', 'GU_ABFACIA', 'GU_FACIA', 'GU_ALPHA', 'GU_ME', 'at']);
+                cookies.cleanUp(['mmcore.pd', 'mmcore.srv', 'mmid', 'GU_ABFACIA', 'GU_FACIA', 'GU_ALPHA', 'GU_ME', 'at', 'gu_adfree_user']);
             },
 
             cleanupLocalStorage : function () {
