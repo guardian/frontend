0cd3d6bc373c9112e0dad6fbded8ab51f34952df
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-07-04 16:25:57 +0100

    Add first two videos

--- common/app/common/commercial/hosted/hardcoded/LeffeHostedPages.scala
+++ common/app/common/commercial/hosted/hardcoded/LeffeHostedPages.scala
@@ -1,35 +1,53 @@
 package common.commercial.hosted.hardcoded
 
 import common.commercial.hosted._
+import conf.Configuration.site.host
 import conf.Static
 import conf.switches.Switches
 
 object LeffeHostedPages {
 
-  private val vid1PageName = "vid1"
+
+  /*
+    Pete Lawrence
+    http://multimedia.guardianapis.com/interactivevideos/video
+    .php?file=160629GLPeteLawrence_h264_mezzanine&format=video/webm&maxbitrate=2048
+
+    Susan Derges
+    http://multimedia.guardianapis.com/interactivevideos/video
+    .php?file=160629GLSusanDerges_h264_mezzanine&format=video/webm&maxbitrate=2048
+
+    Quay Brothers
+    http://multimedia.guardianapis.com/interactivevideos/video
+    .php?file=160629GLQuayBrothers_h264_mezzanine&format=video/webm&maxbitrate=2048
+  */
+
+
+  private val willardWiganPageName = "willard-wigan"
+  private val adrienneTreebyPageName = "adrienne-treeby"
 
   private val campaign = HostedCampaign(
     id = "leffe-rediscover-time",
-    name = "TODO",
+    name = "Leffe - rediscover time",
     owner = "Leffe",
     logo = HostedLogo(Static("images/commercial/TODO.jpg"))
   )
 
-  private val vid1: HostedVideoPage = HostedVideoPage(
+  private val willardWiganPageWithoutNextPage: HostedVideoPage = HostedVideoPage(
     campaign,
-    pageUrl = "TODO",
-    pageName = vid1PageName,
+    pageUrl = s"$host/advertiser-content/${campaign.id}/$willardWiganPageName",
+    pageName = willardWiganPageName,
     standfirst = "TODO",
     bannerUrl = Static("TODO"),
     video = HostedVideo(
-      mediaId = "TODO",
-      title = "TODO",
-      duration = -1,
+      mediaId = willardWiganPageName,
+      title = "Willard Wigan",
+      duration = 127,
       posterUrl = Static("TODO"),
-      srcUrlMp4 = "TODO",
-      srcUrlWebm = "TODO",
-      srcUrlOgg = "TODO",
-      srcM3u8 = "TODO"
+      srcUrlMp4 = "https://cdn.theguardian.tv/interactive/2016/06/29/160629WillardWigan_V3_2M_H264.mp4",
+      srcUrlWebm = "https://cdn.theguardian.tv/interactive/2016/06/29/160629WillardWigan_V3_2M_vp8.webm",
+      srcUrlOgg = "https://cdn.theguardian.tv/interactive/mp4/1080/2016/06/29/160629WillardWigan_V3_hi.ogv",
+      srcM3u8 = "https://cdn.theguardian.tv/interactive/2016/06/29/HLS/160629WillardWigan_V3.m3u8"
     ),
     cta = HostedCallToAction(
       url = "TODO",
@@ -39,9 +57,44 @@ object LeffeHostedPages {
     nextPage = None
   )
 
+  private val adrienneTreebyPageWithoutNextPage: HostedVideoPage = HostedVideoPage(
+    campaign,
+    pageUrl = s"$host/advertiser-content/${campaign.id}/$adrienneTreebyPageName",
+    pageName = adrienneTreebyPageName,
+    standfirst = "TODO",
+    bannerUrl = Static("TODO"),
+    video = HostedVideo(
+      mediaId = adrienneTreebyPageName,
+      title = "Adrienne Treeby",
+      duration = 116,
+      posterUrl = Static("TODO"),
+      srcUrlMp4 = "https://cdn.theguardian" +
+                  ".tv/interactive/2016/06/29/160629AdrienneTreeby_KP-28311272_h264_mezzanine_2M_H264.mp4",
+      srcUrlWebm = "https://cdn.theguardian" +
+                   ".tv/interactive/2016/06/29/160629AdrienneTreeby_KP-28311272_h264_mezzanine_2M_vp8.webm",
+      srcUrlOgg = "https://cdn.theguardian" +
+                  ".tv/interactive/mp4/1080/2016/06/29/160629AdrienneTreeby_KP-28311272_h264_mezzanine_hi.ogv",
+      srcM3u8 = "https://cdn.theguardian" +
+                ".tv/interactive/2016/06/29/HLS/160629AdrienneTreeby_KP-28311272_h264_mezzanine.m3u8"
+    ),
+    cta = HostedCallToAction(
+      url = "TODO",
+      label = "TODO",
+      trackingCode = "TODO"
+    ),
+    nextPage = None
+  )
+
+  private val willardWiganPage = willardWiganPageWithoutNextPage
+                                 .copy(nextPage = Some(adrienneTreebyPageWithoutNextPage))
+
+  private val adrienneTreebyPage = adrienneTreebyPageWithoutNextPage
+                                   .copy(nextPage = Some(willardWiganPageWithoutNextPage))
+
   def fromPageName(pageName: String): Option[HostedPage] = {
     pageName match {
-      case `vid1PageName` if Switches.hostedLeffeShowVideo1.isSwitchedOn => Some(vid1)
+      case `willardWiganPageName` if Switches.hostedLeffeShowVideo1.isSwitchedOn => Some(willardWiganPage)
+      case `adrienneTreebyPageName` if Switches.hostedLeffeShowVideo1.isSwitchedOn => Some(adrienneTreebyPage)
       case _ => None
     }
   }
