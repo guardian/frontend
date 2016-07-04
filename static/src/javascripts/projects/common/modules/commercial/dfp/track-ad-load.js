8acf3466fb9593cee82dc3c39732b7a0b27e42f6
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-07-04 09:51:17 +0100

    Expose trackAd* in the DFP object

--- static/src/javascripts/projects/common/modules/commercial/dfp/track-ad-load.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp/track-ad-load.js
@@ -1,6 +1,8 @@
 define([
+    'common/modules/commercial/dfp/dfp-obj',
     'common/modules/commercial/dfp/private/wait-for-advert'
-], function (waitForAdvert) {
+], function (dfp, waitForAdvert) {
+    dfp.trackAdLoad = trackAdLoad;
     return trackAdLoad;
     function trackAdLoad(id) {
         return waitForAdvert(id).then(function (_) { return _.whenLoaded; });
--- static/src/javascripts/projects/common/modules/commercial/dfp/track-ad-render.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp/track-ad-render.js
@@ -1,6 +1,8 @@
 define([
+    'common/modules/commercial/dfp/dfp-obj',
     'common/modules/commercial/dfp/private/wait-for-advert'
-], function (waitForAdvert) {
+], function (dfp, waitForAdvert) {
+    dfp.trackAdRender = trackAdRender;
     return trackAdRender;
     function trackAdRender(id) {
         return waitForAdvert(id).then(function (_) { return _.whenRendered; });
