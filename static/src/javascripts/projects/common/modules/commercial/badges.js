e1fd2720cdef73ab3e9f64a2796f2b96b77dfd7d
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-07-05 15:06:42 +0100

    Use proper syntax

--- static/src/javascripts/projects/common/modules/commercial/badges.js
+++ static/src/javascripts/projects/common/modules/commercial/badges.js
@@ -26,147 +26,148 @@ define([
     map
 ) {
     var badgesConfig = {
-            sponsoredfeatures: {
-                count:      0,
-                header:     'Supported by',
-                namePrefix: 'sp'
-            },
-            'advertisement-features': {
-                count:      0,
-                header:     'Paid for by',
-                namePrefix: 'ad'
-            },
-            'foundation-features': {
-                count:      0,
-                header:     'Supported by',
-                namePrefix: 'fo'
-            }
+        sponsoredfeatures: {
+            count:      0,
+            header:     'Supported by',
+            namePrefix: 'sp'
         },
-        addPreBadge  = function ($adSlot, header, sponsor) {
-            if (sponsor) {
-                $adSlot.append(template(
-                    badgeTpl,
-                    {
-                        header:  header,
-                        sponsor: sponsor
-                    }
-                ));
-            }
+        'advertisement-features': {
+            count:      0,
+            header:     'Paid for by',
+            namePrefix: 'ad'
         },
-        renderAd = function (item, sponsorship, opts, fallback) {
-            var badgeConfig = badgesConfig[sponsorship],
-                slotTarget  = badgeConfig.namePrefix + 'badge',
-                name        = slotTarget + (++badgeConfig.count),
-                $adSlot     = bonzo(createSlot(
-                                name,
-                                ['paid-for-badge', 'paid-for-badge--front'],
-                                opts.series,
-                                opts.keywords,
-                                slotTarget
-                              ));
+        'foundation-features': {
+            count:      0,
+            header:     'Supported by',
+            namePrefix: 'fo'
+        }
+    };
 
-            addPreBadge($adSlot, badgeConfig.header, opts.sponsor);
+    function addPreBadge($adSlot, header, sponsor) {
+        if (sponsor) {
+            $adSlot.append(template(
+                badgeTpl,
+                {
+                    header:  header,
+                    sponsor: sponsor
+                }
+            ));
+        }
+    }
 
-            return new Promise(function (resolve) {
-                idleFastdom.write(function () {
-                    var placeholder = $('.js-badge-placeholder', item);
+    function renderAd(item, sponsorship, opts, fallback) {
+        var badgeConfig = badgesConfig[sponsorship],
+            slotTarget  = badgeConfig.namePrefix + 'badge',
+            name        = slotTarget + (++badgeConfig.count),
+            $adSlot     = bonzo(createSlot(
+                            name,
+                            ['paid-for-badge', 'paid-for-badge--front'],
+                            opts.series,
+                            opts.keywords,
+                            slotTarget
+                          ));
 
-                    if (placeholder.length) {
-                        placeholder.replaceWith($adSlot);
-                    } else {
-                        $(fallback, item).after($adSlot);
-                    }
+        addPreBadge($adSlot, badgeConfig.header, opts.sponsor);
 
-                    resolve($adSlot);
-                });
+        return new Promise(function (resolve) {
+            idleFastdom.write(function () {
+                var placeholder = $('.js-badge-placeholder', item);
+
+                if (placeholder.length) {
+                    placeholder.replaceWith($adSlot);
+                } else {
+                    $(fallback, item).after($adSlot);
+                }
+
+                resolve($adSlot);
             });
-        },
-        init = function () {
-            var sponsoredFrontPromise,
-                sponsoredContainersPromise,
-                sponsoredCardsPromise;
+        });
+    }
 
-            if (!commercialFeatures.badges) {
-                return false;
-            }
+    function init() {
+        var sponsoredFrontPromise,
+            sponsoredContainersPromise,
+            sponsoredCardsPromise;
 
-            sponsoredFrontPromise = Promise.all(map($('.js-sponsored-front'), function (front) {
-                var $front = bonzo(front);
+        if (!commercialFeatures.badges) {
+            return false;
+        }
+
+        sponsoredFrontPromise = Promise.all(map($('.js-sponsored-front'), function (front) {
+            var $front = bonzo(front);
+
+            return renderAd(
+                qwery('.fc-container', front)[0],
+                $front.data('sponsorship'),
+                {
+                    sponsor: $front.data('sponsor')
+                },
+                '.js-container__header'
+            );
+        }));
+
+        sponsoredContainersPromise = sponsoredFrontPromise.then(function () {
+            return Promise.all($('.js-sponsored-container').map(processItem.bind(undefined, '.js-container__header')));
+        });
+
+        sponsoredCardsPromise = sponsoredContainersPromise.then(function () {
+            return Promise.all($('.js-sponsored-card').map(processItem.bind(undefined, '.js-card__header')));
+        });
+
+        return sponsoredCardsPromise;
+
+        function processItem(fallback, item) {
+            if (qwery('.ad-slot--paid-for-badge', item).length === 0) {
+                var $item = bonzo(item);
+
+                if (!item.hasAttribute('data-sponsorship')) {
+                    return;
+                }
 
                 return renderAd(
-                    qwery('.fc-container', front)[0],
-                    $front.data('sponsorship'),
+                    item,
+                    $item.data('sponsorship'),
                     {
-                        sponsor: $front.data('sponsor')
+                        sponsor:  $item.data('sponsor'),
+                        series:   $item.data('series'),
+                        keywords: $item.data('keywords')
+                    },
+                    fallback
+                );
+            }
+        }
+    }
+
+    return {
+
+        init: init,
+
+        // add a badge to a container (if appropriate)
+        add: function (container) {
+            var $container = bonzo(container);
+
+            if (
+                $container.hasClass('js-sponsored-container') &&
+                qwery('.ad-slot--paid-for-badge', container).length === 0
+            ) {
+                return renderAd(
+                    container,
+                    $container.data('sponsorship'),
+                    {
+                        sponsor:  $container.data('sponsor'),
+                        series:   $container.data('series'),
+                        keywords: $container.data('keywords')
                     },
                     '.js-container__header'
-                );
-            }));
-
-            sponsoredContainersPromise = sponsoredFrontPromise.then(function () {
-                return Promise.all($('.js-sponsored-container').map(processItem.bind(undefined, '.js-container__header')));
-            });
-
-            sponsoredCardsPromise = sponsoredContainersPromise.then(function () {
-                return Promise.all($('.js-sponsored-card').map(processItem.bind(undefined, '.js-card__header')));
-            });
-
-            return sponsoredCardsPromise;
-
-            function processItem(fallback, item) {
-                if (qwery('.ad-slot--paid-for-badge', item).length === 0) {
-                    var $item = bonzo(item);
-
-                    if (!item.hasAttribute('data-sponsorship')) {
-                        return;
-                    }
-
-                    return renderAd(
-                        item,
-                        $item.data('sponsorship'),
-                        {
-                            sponsor:  $item.data('sponsor'),
-                            series:   $item.data('series'),
-                            keywords: $item.data('keywords')
-                        },
-                        fallback
-                    );
-                }
+                ).then(addSlot);
             }
         },
-        badges = {
 
-            init: init,
-
-            // add a badge to a container (if appropriate)
-            add: function (container) {
-                var $container = bonzo(container);
-
-                if (
-                    $container.hasClass('js-sponsored-container') &&
-                    qwery('.ad-slot--paid-for-badge', container).length === 0
-                ) {
-                    return renderAd(
-                        container,
-                        $container.data('sponsorship'),
-                        {
-                            sponsor:  $container.data('sponsor'),
-                            series:   $container.data('series'),
-                            keywords: $container.data('keywords')
-                        },
-                        '.js-container__header'
-                    ).then(addSlot);
-                }
-            },
-
-            // for testing
-            reset: function () {
-                for (var type in badgesConfig) {
-                    badgesConfig[type].count = 0;
-                }
+        // for testing
+        reset: function () {
+            for (var type in badgesConfig) {
+                badgesConfig[type].count = 0;
             }
-        };
-
-    return badges;
-
+        }
+    };
 });
