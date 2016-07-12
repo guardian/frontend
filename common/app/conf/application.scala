86dbce131c8099ac398926d351a2939aece364ec
Alex Sanders <a@snd.rs>
2016-07-12 11:39:11 +0100

    rename method to Atomise

--- common/app/conf/application.scala
+++ common/app/conf/application.scala
@@ -2,4 +2,4 @@ package conf
 
 object Configuration extends common.GuardianConfiguration
 object Static extends common.Assets.Assets(Configuration.assets.path, "assets/assets.map")
-object CssClass extends common.Assets.CssMap("assets/atomic-class-map.json")
+object Atomise extends common.Assets.CssMap("assets/atomic-class-map.json")
