5cb625e55bc6a4b56e2ee41106972076823ab467
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-07-13 11:03:40 +0100

    add try catch to allow iframe access to fail silently

--- static/src/javascripts/projects/common/modules/article/spacefinder.js
+++ static/src/javascripts/projects/common/modules/article/spacefinder.js
@@ -120,6 +120,7 @@ define([
 
                         var iframe = records[0].addedNodes[0];
                         if (isIframeLoaded(iframe)) {
+                            instance.disconnect();
                             resolve();
                         } else {
                             iframe.addEventListener('load', function () {
@@ -136,9 +137,13 @@ define([
         }
 
         function isIframeLoaded(iframe) {
-            return iframe.contentWindow &&
-                iframe.contentWindow.document &&
-                iframe.contentWindow.document.readyState === 'complete';
+           try {
+               return iframe.contentWindow &&
+                   iframe.contentWindow.document &&
+                   iframe.contentWindow.document.readyState === 'complete';
+           } catch(err) {
+               return true;
+           }
         }
     }, getFuncId);
 
