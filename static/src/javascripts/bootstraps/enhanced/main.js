c6974520d3b0508637f7247b6b34df2c965a52bf
James Gorrie <james.gorrie@theguardian.com>
2016-07-15 15:19:48 +0100

    don't disable video enhancing

--- static/src/javascripts/bootstraps/enhanced/main.js
+++ static/src/javascripts/bootstraps/enhanced/main.js
@@ -87,7 +87,7 @@ define([
 
         if (config.isMedia || qwery('video, audio').length) {
             require(['bootstraps/enhanced/media/main', 'bootstraps/enhanced/media/analytics'], function (media, mediaAnalytics) {
-                // bootstrapContext('media', media);
+                bootstrapContext('media', media);
                 bootstrapContext('media : analytics', mediaAnalytics);
             });
         }
