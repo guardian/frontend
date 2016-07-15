8bee9e7cc50e9e5019e92fbdf704e21098ba5cea
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-07-15 12:25:35 +0100

    Internal healtchecks don't expire
    This is a following this PR
    https://github.com/guardian/frontend/pull/13486
    Internal healthcheck are being executed only once to verify the service
    can serve a real request but once it is successful the internal
    healthcheck is not executed anymore. The goal is to minimize the impact
    of any upstream services on the health of frontend services

--- archive/app/controllers/HealthCheck.scala
+++ archive/app/controllers/HealthCheck.scala
@@ -1,5 +1,5 @@
 package controllers
 
-import conf.{AllGoodCachedHealthCheck, ExpiringSingleHealthCheck}
+import conf.{AllGoodCachedHealthCheck, NeverExpiresSingleHealthCheck}
 
-class HealthCheck extends AllGoodCachedHealthCheck(9003, ExpiringSingleHealthCheck("/404/www.theguardian.com/Adzip/adzip-fb.html"))
+class HealthCheck extends AllGoodCachedHealthCheck(9003, NeverExpiresSingleHealthCheck("/404/www.theguardian.com/Adzip/adzip-fb.html"))
