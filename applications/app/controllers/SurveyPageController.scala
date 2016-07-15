dbe5a2d059f566e21871b5f069392bb7941825ca
Francis Carr <francis.carr@guardian.co.uk>
2016-07-15 16:37:11 +0100

    Add route and styles to weekendReading404 page

--- applications/app/controllers/SurveyPageController.scala
+++ applications/app/controllers/SurveyPageController.scala
@@ -21,6 +21,10 @@ class SurveyPageController extends Controller with ExecutionContexts {
       Cached(60)(RevalidatableResult.Ok(views.html.survey.myDigest404(StaticPages.simpleSurveyStaticPageForId(request.path))))
    }
 
+  def renderWeekendReading404Page() = Action { implicit request =>
+      Cached(60)(RevalidatableResult.Ok(views.html.survey.weekendReading404(StaticPages.simpleSurveyStaticPageForId(request.path))))
+   }
+
   def renderFormStackSurvey(formName: String) = Action.async { implicit request =>
       WS.url(s"https://${Configuration.Survey.formStackAccountName}.formstack.com/forms/$formName")
         .head
