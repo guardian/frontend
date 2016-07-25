41a2b40be5fc6ea61a1ddbe6135ef46b2220fab9
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-07-25 13:24:20 +0100

    Add test.WithTestWSClient trait

---
+++ common/app/common/Lazy.scala
@@ -0,0 +1,20 @@
+package common
+
+import scala.language.implicitConversions
+
+object Lazy {
+  def apply[A](f: => A): Lazy[A] = new Lazy(f)
+  implicit def conversionLazy[A](l: Lazy[A]): A = l.get()
+}
+
+class Lazy[A] private(f: => A) {
+  private var option: Option[A] = None
+
+  def get(): A = option match {
+    case Some(a) => a
+    case None => val a = f; option = Some(a); a
+  }
+
+  def isDefined: Boolean = option.isDefined
+}
+
