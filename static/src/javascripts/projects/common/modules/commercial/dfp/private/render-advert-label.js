5b2f5c750bfefa72d3c2567b6b2acbed7fa7f1d0
Justin Pinner <justinpinner@gmail.com>
2016-07-26 15:59:27 +0100

    WIP - Styling

--- static/src/javascripts/projects/common/modules/commercial/dfp/private/render-advert-label.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp/private/render-advert-label.js
@@ -16,20 +16,18 @@ define([
                 try {
                     var abParticipant = store.local.get('gu.ab.participations')['AdFeedback'];
                     if (abParticipant && abParticipant.variant === 'ad-feedback-variant') {
-                        var feedBackOpts = '<span>Tell us why you\'re reporting this advertisement</span><hr/><li class="popup__item"><a href="//feedback/report-ad?slot='+ adSlotNode.id +'&asset=' + 'TBC' + '&reason=offensive">It\'s offensive</a></li>';
+                        var feedBackOpts = '<h3 class="ad-feedback popup__group-header">Tell us why you\'re reporting this advertisement</h3><li class="popup__item"><a href="//feedback/report-ad?slot='+ adSlotNode.id +'&asset=' + 'TBC' + '&reason=offensive">It\'s offensive</a></li>';
                         feedBackOpts = feedBackOpts + '<li class="popup__item"><a href="//feedback/report-ad?slot='+ adSlotNode.id +'&asset=' + 'TBC' + '&reason=makes-a-sound">Makes a sound</a></li>';
                         feedBackOpts = feedBackOpts + '<li class="popup__item"><a href="//feedback/report-ad?slot='+ adSlotNode.id +'&asset=' + 'TBC' + '&reason=too-much-movement">Too much movement</a></li>';
                         feedBackOpts = feedBackOpts + '<li class="popup__item"><a href="//feedback/report-ad?slot='+ adSlotNode.id +'&asset=' + 'TBC' + '&reason=not-relevant">Not relevant</a></li>';
-                        feedBackOpts = feedBackOpts + '<li class="popup__item"><a href="//feedback/report-ad?slot='+ adSlotNode.id +'&asset=' + 'TBC' + '&reason=other">Other</a></li>';
-                        var feedbackMenu = '<ul class="popup popup--default popup__group is-off ' + adSlotNode.id + '__popup--feedback" id="ad-feedback-menu__' + adSlotNode.id + '">' + feedBackOpts + '</ul>';
-                        //TODO: correct styling
-                        labelInner = ' <a class="popup__toggle" data-toggle="' + adSlotNode.id + '__popup--feedback" aria-haspopup="true" aria-controls="ad-feedback-menu__' + adSlotNode.id + '">(Report this ad)</a>' + feedbackMenu;
+                        var feedbackMenu = '<ul class="ad-feedback popup popup--default popup__group is-off ' + adSlotNode.id + '__popup--feedback" id="ad-feedback-menu__' + adSlotNode.id + '">' + feedBackOpts + '</ul>';
+                        labelInner = ' <a class="ad-feedback popup__toggle" data-toggle="' + adSlotNode.id + '__popup--feedback" aria-haspopup="true" aria-controls="ad-feedback-menu__' + adSlotNode.id + '">(Report this ad)</a>' + feedbackMenu;
                     }
                 } catch (x) {
                     // if we can't pull the ad feedback participation state, we'll treat it as excluded
                 }
             }
-            var labelDiv = '<div class="ad-slot__label has-popup" data-test-id="ad-slot-label">Advertisement' + labelInner + '</div>';
+            var labelDiv = '<div class="ad-slot__label" data-test-id="ad-slot-label">Advertisement' + labelInner + '</div>';
             return fastdom.write(function () {
                 adSlotNode.insertAdjacentHTML('afterbegin', labelDiv);
             });
