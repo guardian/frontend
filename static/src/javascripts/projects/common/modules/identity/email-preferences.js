9632ac8c5aba24b893bf0b4c6e27fc266ba34ab6
Kate Whalen <whalenka88@gmail.com>
2016-07-28 18:06:04 +0100

    Basic working form submission

--- static/src/javascripts/projects/common/modules/identity/email-preferences.js
+++ static/src/javascripts/projects/common/modules/identity/email-preferences.js
@@ -4,36 +4,75 @@
 
 define([
     'bean',
+    'fastdom',
     'common/utils/$'
 ], function (
     bean,
+    fastdom,
     $
 ) {
-    function doTheThing(buttonEl) {
-        console.log('found a button');
-        // use bean for click event
-        buttonEl.addEventListener('click', sendFormData);
+    function submitEmailForm(buttonEl) {
+        bean.on(buttonEl, 'click', function () {
+            // TODO: replace xhr with fetch or reqwest
+            var xhr = new XMLHttpRequest();
+            var url = '/email-prefs';
+            var params = submitFormData(buttonEl);
+
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState == 4 && xhr.status == 200) {
+                    alert(xhr.responseText);
+                }
+            };
+            xhr.send(params);
+
+            alterButton(buttonEl);
+            });
     }
 
     function addButtonClickEvents() {
-        $.forEachElement('.email-subscription__button', doTheThing);
+        $.forEachElement('.email-subscription__button', submitEmailForm);
     }
 
-    function parseFormData() {
-
+    function encodeFormData(csrfToken, buttonVal, htmlPreference) {
+        return 'csrfToken=' + csrfToken + '&' +
+        'addEmailSubscription=' + buttonVal + '&' +
+        'htmlPreference=' + htmlPreference;
     }
 
-    function sendFormData() {
-        console.log('sending the form');
-        // check the sub status of this button
-        // get the CSRFtoken and other form data
-        // parse the form data (uriencode to escape chars, ampersand to link)
+    function alterButton(buttonEl) {
+        // do this after successful POST request to update the appearance and value of the button
+        var buttonVal = buttonEl.value;
+        if (buttonVal.startsWith('unsubscribe-')) {
+            fastdom.write(function () {
+                buttonEl.value = buttonVal.replace('unsubscribe-', '');
+                buttonEl.innerHTML = 'Subscribe';
+                $.ancestor(buttonEl, 'email-subscription').classList.remove('email-subscription--subscribed');
+            });
+        } else {
+            fastdom.write(function () {
+                buttonEl.value = 'unsubscribe-' + buttonVal;
+                buttonEl.innerHTML = 'Unsubscribe';
+                $.ancestor(buttonEl, 'email-subscription').classList.add('email-subscription--subscribed');
+            });
+        }
+    }
+
+    function submitFormData(buttonEl) {
+        var formEl = $.ancestor(buttonEl, 'form');
+        var csrfToken = (formEl.elements.csrfToken.value).toString();
+        var buttonVal = buttonEl.value.toString();
+        // TODO: Grab the HTML pref from the DOM
+        var htmlPreference = 'HTML'.toString();
+        return encodeFormData(csrfToken, buttonVal, htmlPreference);
 
     }
 
     return {
         init: function () {
-            console.log('RUNNING! WOOP');
             addButtonClickEvents();
         }
     };
