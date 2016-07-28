98df296aa2964afda52accb74aecb310e291cd81
Simon Adcock <simon.adcock@guardian.co.uk>
2016-07-28 13:45:25 +0100

    Rename flags from tags function

--- common/app/views/support/AmpAd.scala
+++ common/app/views/support/AmpAd.scala
@@ -5,7 +5,7 @@ import model.Article
 import play.api.libs.json.{JsObject, Json}
 
 object AmpAd {
-  private def grabLastFragmentOfId(items: Seq[Tag]) = {
+  private def buildAdFlagsFromTags(items: Seq[Tag]) = {
     items.map { item =>
       if (item.id == "uk/uk") {
         item.id
@@ -21,13 +21,13 @@ object AmpAd {
       "targeting" -> Json.obj(
         "url" -> uri,
         "edition" -> edition,
-        "se" -> grabLastFragmentOfId(article.trail.tags.series).mkString(","),
+        "se" -> buildAdFlagsFromTags(article.trail.tags.series).mkString(","),
         "ct" -> article.metadata.contentType,
         "p" -> "amp",
         "keywordIds" -> article.trail.tags.keywords.map(_.id).mkString(","),
-        "k" -> grabLastFragmentOfId(article.trail.tags.keywords).mkString(","),
-        "co" -> grabLastFragmentOfId(article.trail.tags.contributors).mkString(","),
-        "bl" -> grabLastFragmentOfId(article.trail.tags.blogs).mkString(","),
+        "k" -> buildAdFlagsFromTags(article.trail.tags.keywords).mkString(","),
+        "co" -> buildAdFlagsFromTags(article.trail.tags.contributors).mkString(","),
+        "bl" -> buildAdFlagsFromTags(article.trail.tags.blogs).mkString(","),
         "authorIds" -> article.trail.tags.contributors.map(_.id).mkString(","),
         "section" -> article.metadata.sectionId
       )
