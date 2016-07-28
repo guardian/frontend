857dbbe76da9175fd2b02fedbc04b2e191ea3016
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-07-28 10:55:17 +0100

    Disable video autoplay AB test for zootropolis campaign. Extend test switch.

--- static/src/javascripts/projects/common/modules/experiments/tests/hosted-autoplay.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/hosted-autoplay.js
@@ -22,8 +22,10 @@ define([
         this.dataLinkNames = 'Next video autoplay: Cancel autoplay of the next video, Discover Zoe from Renault, Next Hosted Video Autoplay, Immediately play the next video, Next Hosted Video';
         this.idealOutcome = 'People will either more often click on the next hosted video or wait until end of the current video to be redirected into the next video page url.';
 
+        var testCampaigns = ['leffe-rediscover-time', 'renault-car-of-the-future'];
+
         this.canRun = function () {
-            return config.page.contentType === 'Video' && config.page.tones === 'Hosted';
+            return config.page.contentType === 'Video' && config.page.tones === 'Hosted' && testCampaigns.indexOf(config.page.section) > -1;
         };
 
         this.variants = [
