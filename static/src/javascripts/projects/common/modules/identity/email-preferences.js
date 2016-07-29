1a224df05999f4b06f3f6f7cf54be3f18633f247
Kate Whalen <whalenka88@gmail.com>
2016-07-29 17:47:27 +0100

    Swap in reqwest, sanity check the response

--- static/src/javascripts/projects/common/modules/identity/email-preferences.js
+++ static/src/javascripts/projects/common/modules/identity/email-preferences.js
@@ -1,63 +1,80 @@
 // This is a workaround for the email preferences page https://profile.thegulocal.com/email-prefs
 // We want to submit subscribe/unsubscribe requests without a full page refresh
 
-
 define([
     'bean',
+    'reqwest',
     'fastdom',
     'common/utils/$'
 ], function (
     bean,
+    reqwest,
     fastdom,
     $
 ) {
-    function submitEmailForm(buttonEl) {
+    function useReqwest(buttonEl) {
         bean.on(buttonEl, 'click', function () {
-            // TODO: replace xhr with fetch or reqwest
-            var xhr = new XMLHttpRequest();
-            var url = '/email-prefs';
-            var params = submitFormData(buttonEl);
-
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
-            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-            xhr.onreadystatechange = function() {
-                if(xhr.readyState == 4 && xhr.status == 200) {
-                    alert(xhr.responseText);
+            var formQueryString = submitFormData(buttonEl);
+            reqwest({
+                url: '/email-prefs',
+                method: 'POST',
+                data: formQueryString,
+                error: function (err) {
+                    // TODO: display the error
+                    print(err.message);
+                },
+                success: function (response) {
+                    var subscriptionState;
+                    // lets be super careful of falsy values... yay JavaScript!
+                    try {
+                        // Why array.length rather than typeof; there are multiple circumstances where the value could be undefined:
+                        // nothing came back at all
+                        // stuff came back but that one field is undefined
+                        // the data came back in the format expected as above, but with the subscribeTo value undefined
+                        if (response.subscriptions.length < 1) {
+                            // response can back as expected, but the user is not subscribed:
+                            subscriptionState = false;
+                        } else {
+                            subscriptionState = response.subscriptions[0].subscribedTo;
+                        }
+                    } catch (err) {
+                        print(err.message);
+                    } finally {
+                        updateButton(buttonEl, subscriptionState);
+                    }
                 }
-            };
-            xhr.send(params);
-
-            alterButton(buttonEl);
             });
+        });
     }
 
     function addButtonClickEvents() {
-        $.forEachElement('.email-subscription__button', submitEmailForm);
+        $.forEachElement('.email-subscription__button', useReqwest);
     }
 
     function encodeFormData(csrfToken, buttonVal, htmlPreference) {
+        // reqwest.toQueryString()
         return 'csrfToken=' + csrfToken + '&' +
         'addEmailSubscription=' + buttonVal + '&' +
         'htmlPreference=' + htmlPreference;
     }
 
-    function alterButton(buttonEl) {
+    function updateButton(buttonEl, subscriptionState) {
         // do this after successful POST request to update the appearance and value of the button
         var buttonVal = buttonEl.value;
-        if (buttonVal.startsWith('unsubscribe-')) {
+        if (subscriptionState === true) {
+            fastdom.write(function () {
+                buttonEl.value = 'unsubscribe-' + buttonVal;
+                buttonEl.innerHTML = 'Unsubscribe';
+                $.ancestor(buttonEl, 'email-subscription').classList.add('email-subscription--subscribed');
+            });
+        } else if (subscriptionState === false) {
             fastdom.write(function () {
                 buttonEl.value = buttonVal.replace('unsubscribe-', '');
                 buttonEl.innerHTML = 'Subscribe';
                 $.ancestor(buttonEl, 'email-subscription').classList.remove('email-subscription--subscribed');
             });
         } else {
-            fastdom.write(function () {
-                buttonEl.value = 'unsubscribe-' + buttonVal;
-                buttonEl.innerHTML = 'Unsubscribe';
-                $.ancestor(buttonEl, 'email-subscription').classList.add('email-subscription--subscribed');
-            });
+            throw('Something odd has happened');
         }
     }
 
