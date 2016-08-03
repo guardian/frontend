ca9788af7f3ad9124f58f9fcb7e4b322365eb576
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-08-03 14:29:54 +0100

    Remove Play.isDev in WhatIsDeduped

--- admin/app/controllers/admin/WhatIsDeduped.scala
+++ admin/app/controllers/admin/WhatIsDeduped.scala
@@ -5,7 +5,6 @@ import conf.Configuration
 import layout.DedupedFrontResult
 import model.Cached.RevalidatableResult
 import model.{NoCache, Cached}
-import play.Play
 import play.api.libs.json.{JsError, JsSuccess}
 import play.api.libs.ws.WSClient
 import play.api.mvc.Controller
@@ -13,15 +12,13 @@ import services.ConfigAgent
 
 class WhatIsDeduped(wsClient: WSClient) extends Controller with Logging with ExecutionContexts {
 
- import play.api.Play.current
-
    def index() = AuthActions.AuthActionTest { implicit request =>
      val paths: List[String] = ConfigAgent.getPathIds.sorted
      Cached(60)(RevalidatableResult.Ok(views.html.dedupePathsList(Configuration.environment.stage, paths)))
    }
 
    def dedupedFor(path: String) = AuthActions.AuthActionTest.async { implicit request =>
-     val domain = if (Play.isDev) "http://localhost:9000" else Configuration.ajax.url
+     val domain = Configuration.ajax.url
      val url = s"$domain/$path/deduped.json"
      wsClient.url(url).get().map { response =>
        response.json.validate[DedupedFrontResult] match {
