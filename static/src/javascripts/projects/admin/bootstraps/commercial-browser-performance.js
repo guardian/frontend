2c919465bbb855eea3dee0de4aba1d0bfd74a8e9
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-08-05 17:43:08 +0100

    Fetch content from reports endpoint and add to real-time graph

--- static/src/javascripts/projects/admin/bootstraps/commercial-browser-performance.js
+++ static/src/javascripts/projects/admin/bootstraps/commercial-browser-performance.js
@@ -1,13 +1,19 @@
 /*global $*/
 define([
-
+    'common/utils/config',
+    'common/utils/fetch-json',
+    'lodash/collections/find',
+    'lodash/collections/map'
 ], function (
-
+    config,
+    fetchJson,
+    find,
+    map
 ) {
     var chart = undefined;
-    var FETCH_INTERVAL = 5000;
+    var FETCH_INTERVAL = 2000;
 
-    var commercialStartTimes = [ {time: 1370044800, y: 100}, {time: 1370044801, y: 1000}, {time: 1370044802, y: 900}  ];
+    var commercialStartTimes = [];
 
     function initialise() {
 
@@ -21,7 +27,23 @@ define([
     }
 
     function fetchData() {
-        chart.redraw();
+        fetchJson(config.page.beaconUrl + '/commercial-reports/05082016-15:24:45', {
+            mode: 'cors'
+        }).then(function (logs) {
+            var appStartTimes = map(logs.reports, function(report){
+                var primaryBaseline = find(report.baselines, function(baseline){
+                    return baseline.name === 'primary';
+                })
+                return primaryBaseline ? primaryBaseline.time : 0;
+            });
+
+            //TODO push the app start times in.
+            // format : {time: 1370044800, y: 100}
+            /*chart.push([{
+                time: timestamp,
+                y:
+            }]);*/
+        });
     }
 
     return {
