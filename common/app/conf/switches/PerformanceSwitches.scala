bf0ad4049d3d42a7b5ec9d07816c8155fbb7d045
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-08-05 09:23:38 +0100

    Just add one preconnect link to isolate performance test

--- common/app/conf/switches/PerformanceSwitches.scala
+++ common/app/conf/switches/PerformanceSwitches.scala
@@ -227,4 +227,14 @@ trait PerformanceSwitches {
     sellByDate = never,
     exposeClientSide = false
   )
+
+  val UseLinkPreconnect = Switch(
+    SwitchGroup.Performance,
+    "use-link-preconnect",
+    "If this switch is on then link preconnect hints will be on the page",
+    owners = Seq(Owner.withGithub("rich-nguyen")),
+    safeState = Off,
+    sellByDate = new LocalDate(2016, 9, 5),
+    exposeClientSide = false
+  )
 }
