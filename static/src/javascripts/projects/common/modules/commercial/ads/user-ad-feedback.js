2f8766a50889dcf1212b48433afdb575a9a90479
Justin Pinner <justinpinner@gmail.com>
2016-08-05 12:19:19 +0100

    PR feedback mods. Much better now.

--- static/src/javascripts/projects/common/modules/commercial/ads/user-ad-feedback.js
+++ static/src/javascripts/projects/common/modules/commercial/ads/user-ad-feedback.js
@@ -10,39 +10,27 @@ define([
     config
 ) {
 
-    function userAdFeedback(pagePath, adSlotId, creativeid, reason) {
-        // TODO: This. Better.
-        if (reason !== 'ad-feedback-menu-opened') {
-            return recordUserAdFeedback(pagePath, adSlotId, creativeid, reason);
-        } else {
-            return recordUserAdFeedback(pagePath, adSlotId, creativeid, 'ad-feedback-menu-opened');
-        }
-    }
-
-    function recordUserAdFeedback(pagePath, adSlotId, creativeId, feedbackType) {
+    return function recordUserAdFeedback(pagePath, adSlotId, creativeId, feedbackType) {
         var feedbackUrl = 'https://j2cy9stt59.execute-api.eu-west-1.amazonaws.com/prod/adFeedback';
         var stage = config.page.isProd ? 'PROD' : 'CODE';
-        var data = '{"stage":"'+stage+'", "adPage":"'+pagePath+'", "adSlotId":"'+adSlotId+'", "adCreativeId":"'+creativeId+'", "adFeedback":"'+feedbackType+'"}';
+        var data = JSON.stringify({
+            stage: stage,
+            adPage: pagePath,
+            adSlotId: adSlotId,
+            adCreativeId: creativeId.toString(),
+            adFeedback: feedbackType
+        });
         return fetch(feedbackUrl, {
             method: 'post',
             body: data,
             mode: 'cors'
-        }).then(function() {    // ignore the response
-        }).catch(function() {    // ignore any errors
-        }).then(function() {    // we're complete - update the UI
-            if (feedbackType !== 'ad-feedback-menu-opened') {
-                var adSlot = bonzo(qwery('#' + adSlotId));
-                adSlot.each(function (ad) {
-                    for (var i = 0; i < ad.children.length; i++) {
-                        if (ad.children[i].classList.contains('ad-slot__label')) {
-                            ad.children[i].innerHTML = 'Advertisement (Thanks for your feedback)';
-                        }
-                    }
-                });
-            }
-        });
-    }
+        }).then(onComplete(adSlotId, feedbackType), function(){})
+    };
 
-    return userAdFeedback;
+    function onComplete(adSlotId, feedbackType) {    // we're complete - update the UI
+        if (feedbackType !== 'ad-feedback-menu-opened') {
+            bonzo(qwery('#' + adSlotId + '>.ad-slot__label')).text('Advertisement (Thanks for your feedback)');
+        }
+    }
 
 });
--- static/src/javascripts/projects/common/modules/commercial/dfp/private/render-advert.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp/private/render-advert.js
@@ -23,7 +23,7 @@ define([
     renderAdvertLabel,
     geoMostPopular,
     Toggles,
-    userAdFeedback
+    recordUserAdFeedback
 ) {
     /**
      * ADVERT RENDERING
@@ -124,8 +124,8 @@ define([
 
         return applyCreativeTemplate(advert.node).then(function (isRendered) {
             return renderAdvertLabel(advert.node)
-                .then(addFeedbackDropdownToggle())
-                .then(applyFeedbackOnClickListeners(slotRenderEvent.creativeId))
+                .then(addFeedbackDropdownToggle)
+                .then(function () { return applyFeedbackOnClickListeners(slotRenderEvent.creativeId); })
                 .then(callSizeCallback)
                 .then(addRenderedClass)
                 .then(function () {
@@ -159,7 +159,7 @@ define([
                         if (!bonzo(el).hasClass('js-onclick-ready')) {
                             el.addEventListener('click', function() {
                                 if(bonzo(el).hasClass('is-active')) {
-                                    userAdFeedback(window.location.pathname, advert.node.id, creativeId, 'ad-feedback-menu-opened');
+                                    recordUserAdFeedback(window.location.pathname, advert.node.id, creativeId, 'ad-feedback-menu-opened');
                                 }
                             });
                             bonzo(el).addClass('js-onclick-ready');
@@ -168,7 +168,7 @@ define([
                     bonzo(qwery('.popup__item-problem--option')).each(function(el) {
                         if (!bonzo(el).hasClass('js-onclick-ready')) {
                             el.addEventListener('click', function() {
-                                userAdFeedback(window.location.pathname, el.attributes['slot'].nodeValue, creativeId, el.attributes['problem'].nodeValue);
+                                recordUserAdFeedback(window.location.pathname, el.attributes['slot'].nodeValue, creativeId, el.attributes['problem'].nodeValue);
                             });
                             bonzo(el).addClass('js-onclick-ready');
                         }
