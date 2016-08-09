679271c97d3ad4aacff415789739b23a1cd1688a
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-08-09 12:23:20 +0100

    Script to generate TOC in docs/ README

--- docs/generate-toc.sh
+++ docs/generate-toc.sh
@@ -1,15 +1,54 @@
 #!/bin/sh
 
+cleanDirectoryName() {
+    # Remove leading number 
+    # Replace dash with space
+    # Remove trailing '/'
+    # trim spaces 
+    dirname=$(echo "$1" \
+    | sed 's/^[0-9]*//g' \
+    | sed 's/-/ /g' \
+    | sed 's/\/$//g' \
+    | xargs)
+    
+    capitalize "$dirname"
+}
+
+cleanFileTitle() {
+    # Get title 
+    # Remove formatting markdown characters
+    # Also trim spaces 
+    head -n 1 "$1" | sed 's/[*#]//g' | xargs 
+}
+
+capitalize() {
+    # Capitalize first letter
+    head=$(echo "$1" | cut -c1 | tr [a-z] [A-Z])
+    tail=$(echo "$1" | cut -c2-)
+    echo "$head$tail"
+}
+
+root=$(git rev-parse --show-toplevel)
+docs="$root/docs"
+pushd $docs
+
 echo "#Table Of Content"
 echo ""
 for dir in */; do
-    echo "##[$dir]($dir)"
+    dirname=$(cleanDirectoryName $dir)
+    echo "##[$dirname]($dir)"
     cd $dir
     for doc in *.md; do
         if [ "$doc" != "README.md" ]; then
-            echo "- [$doc]($dir$doc)"
+            filepath=$dir$doc
+            title=$(cleanFileTitle $doc)
+            echo "- [$title]($filepath)"
         fi
     done
     cd ..
     echo ""
 done
+
+cat how-to-create-a-doc-file.md
+
+popd
