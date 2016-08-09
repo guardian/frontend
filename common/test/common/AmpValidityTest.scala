e90e0341ce4a4a8b45cfd9921517070615d1ebf9
Jon Soul <jonathan.soul@guardian.co.uk>
2016-08-09 19:39:08 +0100

    Comment amp validity test framework and remove need to specify amp parameter

--- common/test/common/AmpValidityTest.scala
+++ common/test/common/AmpValidityTest.scala
@@ -9,13 +9,21 @@ import scala.sys.process._
 
 trait AmpValidityTest extends FlatSpec with Matchers with ConfiguredTestSuite {
 
-  def testAmpValidityURL(url: String): Unit = {
-    s"The AMP page at: $url" should "pass an AMP validator" in getContentString(url) { content =>
+  /**
+    * Passes the result of hitting the given url to the amphtml-validator.
+    * @param url url of the amp page to validate - the amp query string parameter need not be included
+    */
+  def testAmpPageValidity(url: String): Unit = {
+    val ampUrl = formatAmpUrl(url)
 
-      val commandInputWriter = writeToProcess(content) _
+    s"The AMP page at $url" should "pass an AMP validator" in getContentString(ampUrl) { content =>
+
+      val commandInputWriter: OutputStream => Unit = writeToProcess(content)
 
       // The process fails when not using stdout/stderr, but these may prove useful for debugging anyway
       val io = new ProcessIO(commandInputWriter, BasicIO.toStdOut, BasicIO.toStdErr)
+
+      // Pass the content to the command line tool (external process) via stdin ('-' option)
       val process = "node_modules/amphtml-validator/index.sh -".run(io)
 
       withClue("AMP validator should complete with exit value 0, the actual exit value of ") {
@@ -27,5 +35,11 @@ trait AmpValidityTest extends FlatSpec with Matchers with ConfiguredTestSuite {
   private def writeToProcess(str: String)(out: OutputStream): Unit = {
     out.write(str.getBytes)
     out.close()
+  }
+
+  // This is too simplistic, but all we need for now.
+  // It should be replaced if support for urls with existing params is needed.
+  private def formatAmpUrl(url: String): String = {
+    url + "?amp"
   }
 }
