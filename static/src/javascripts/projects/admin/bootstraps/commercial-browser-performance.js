494856971582aef541bdb005ccee322723001902
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-08-15 16:46:32 +0100

    Use heatmap to draw performance times as a histogram

--- static/src/javascripts/projects/admin/bootstraps/commercial-browser-performance.js
+++ static/src/javascripts/projects/admin/bootstraps/commercial-browser-performance.js
@@ -2,47 +2,63 @@
 define([
     'common/utils/config',
     'common/utils/fetch-json',
+    'common/utils/template',
+    'lodash/collections/countBy',
     'lodash/collections/find',
     'lodash/collections/map'
 ], function (
     config,
     fetchJson,
+    template,
+    countBy,
     find,
     map
 ) {
-    var chart = undefined;
+    var chart;
     var FETCH_INTERVAL = 2000;
+    var reportTemplateUrl = '/commercial-reports/<%=isoDate%>';
 
     var commercialStartTimes = [];
 
     function initialise() {
 
-       chart = $('#browser-live-performance-data').epoch({ type: 'time.line', data: [{
-            label: 'Commercial Start Time',
-            values: commercialStartTimes
-          }]
-       });
+       chart = $('#browser-live-performance-data').epoch({
+            type: 'time.heatmap',
+            buckets: 10,
+            bucketRange: [0, 13000],
+            axes: ['left', 'right', 'bottom'],
+
+            data: [{
+                label: 'Commercial Start Time',
+                values: commercialStartTimes
+            }]
+        });
 
        window.setInterval(fetchData, FETCH_INTERVAL);
     }
 
     function fetchData() {
-        fetchJson(config.page.beaconUrl + '/commercial-reports/05082016-15:24:45', {
+        var currentDate = new Date();
+        var fetchUrl = template(reportTemplateUrl, {
+            isoDate: currentDate.toISOString()
+        });
+
+        fetchJson(config.page.beaconUrl + fetchUrl, {
             mode: 'cors'
         }).then(function (logs) {
+
             var appStartTimes = map(logs.reports, function(report){
                 var primaryBaseline = find(report.baselines, function(baseline){
                     return baseline.name === 'primary';
-                })
+                });
                 return primaryBaseline ? primaryBaseline.time : 0;
             });
 
-            //TODO push the app start times in.
-            // format : {time: 1370044800, y: 100}
-            /*chart.push([{
-                time: timestamp,
-                y:
-            }]);*/
+            var heatmapData = {
+                time: currentDate.getTime() / 1000,
+                histogram: countBy(appStartTimes)
+            };
+            chart.push([heatmapData]);
         });
     }
 
