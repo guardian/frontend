05a438a719ea19412770ebaf9baeaea6de1c3732
Jon Soul <jonathan.soul@guardian.co.uk>
2016-08-15 12:20:37 +0100

    Add previously failing amp page (more on this story section)

--- article/test/ArticleAmpValidityTest.scala
+++ article/test/ArticleAmpValidityTest.scala
@@ -12,6 +12,7 @@ import org.scalatest.DoNotDiscover
     "/commentisfree/2016/aug/08/the-guardian-view-on-the-southern-train-strike-keep-the-doors-open-for-talks", // Editorials tone
     "/lifeandstyle/shortcuts/2016/aug/09/why-truck-drivers-are-sick-of-chips-with-everything", // Features tone
     "/business/2016/aug/09/china-uk-investment-key-questions-following-hinkley-point-c-delay", // Analysis tone
+    "/books/2011/aug/24/jorge-luis-borges-google-doodle", // More on this story
     "/uk-news/2016/aug/09/southern-rail-strike-war-of-words-heats-up-on-second-day", // Story package / tone news
     "/football/2016/jul/10/france-portugal-euro-2016-match-report" // Match summary
   ).foreach(testAmpPageValidity)
