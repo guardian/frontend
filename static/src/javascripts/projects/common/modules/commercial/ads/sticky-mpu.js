db88b055668ab2aba11c7488fe68f1977c81e270
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-08-16 10:43:56 +0100

    Compute new height of container taking the actual height of the delivered creative in

--- static/src/javascripts/projects/common/modules/commercial/ads/sticky-mpu.js
+++ static/src/javascripts/projects/common/modules/commercial/ads/sticky-mpu.js
@@ -8,8 +8,6 @@ define([
     fastdom
 ) {
 
-    var mpuHeight = 275;
-
     function stickyMpu($adSlot) {
         if ($adSlot.data('name') !== 'right') {
             return;
@@ -21,15 +19,15 @@ define([
         }
 
         return fastdom.read(function () {
-            return referenceElement[config.page.hasShowcaseMainElement ? 'offsetHeight' : 'offsetTop'];
-        }).then(function (articleBodyOffset) {
+            return (referenceElement[config.page.hasShowcaseMainElement ? 'offsetHeight' : 'offsetTop']) + $adSlot.offsetHeight;
+        }).then(function (newHeight) {
             return fastdom.write(function () {
-                $adSlot.parent().css('height', (articleBodyOffset + mpuHeight) + 'px');
-            }).then(function () {
-                //if there is a sticky 'paid by' band move the sticky mpu down so it will be always visible
-                var options = config.page.isAdvertisementFeature ? {top: 43} : {};
-                return new Sticky($adSlot[0], options).init();
+                $adSlot.parent().css('height', newHeight + 'px');
             });
+        }).then(function () {
+            //if there is a sticky 'paid by' band move the sticky mpu down so it will be always visible
+            var options = config.page.isAdvertisementFeature ? {top: 43} : {};
+            return new Sticky($adSlot[0], options).init();
         });
     }
 
