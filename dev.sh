075d12e1834f723ff2bfdd71c3662c3d7c966281
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-08-16 15:03:45 +0100

    Docker shell script: Always try to download most recent image

--- dev.sh
+++ dev.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Always try to pull most recent image
+docker push 642631414762.dkr.ecr.eu-west-1.amazonaws.com/frontend-dev:latest
+
+# Run dev container
 docker-compose run --rm --service-ports dev
