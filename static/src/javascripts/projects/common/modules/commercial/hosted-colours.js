85a0d54a687caed24befdda390aa8fe2b517c0cb
Zofia <zofia.korcz@guardian.co.uk>
2016-08-18 13:55:21 +0100

    validation errors fixed

--- static/src/javascripts/projects/common/modules/commercial/hosted-colours.js
+++ static/src/javascripts/projects/common/modules/commercial/hosted-colours.js
@@ -6,25 +6,24 @@ define([
     $
 ) {
     function hexToRGB(hex) {
-        var hex = removeHash(hex);
         var R = parseInt(hex.substring(0,2),16);
         var G = parseInt(hex.substring(2,4),16);
         var B = parseInt(hex.substring(4,6),16);
 
-        return R + ", " + G + ", " + B;
+        return R + ', ' + G + ', ' + B;
     }
 
     function removeHash(colour) {
-        return (colour.charAt(0)=="#") ? colour.substring(1,7) : colour;
+        return (colour.charAt(0)=='#') ? colour.substring(1,7) : colour;
     }
 
     function init() {
         return new Promise(function(resolve) {
             var $nextVideo = $('.js-next-video');
-            var colour = $nextVideo.data('colour');
+            var colour = removeHash($nextVideo.data('colour'));
 
             $nextVideo.css({
-                "background": "rgba(" + hexToRGB(colour) + ", 0.1)"
+                'background': 'rgba(' + hexToRGB(colour) + ', 0.1)'
             });
 
             resolve();
