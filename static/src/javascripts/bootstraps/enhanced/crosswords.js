baf50fc0e5c3bd7643cd4423534edfa386e584b5
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-08-18 11:54:41 +0100

    Remove crosswords thumbnails
    They haven't worked in a while and provide little value

--- static/src/javascripts/bootstraps/enhanced/crosswords.js
+++ static/src/javascripts/bootstraps/enhanced/crosswords.js
@@ -1,18 +1,15 @@
 define([
     'projects/common/modules/crosswords/main',
-    'projects/common/modules/crosswords/thumbnails',
     'projects/common/modules/crosswords/comments',
     'projects/common/modules/crosswords/series'
 ], function (
     init,
-    thumbnails,
     initComments,
     initSeries
 ) {
     return {
         init: function () {
             init();
-            thumbnails.init();
             initComments();
             initSeries();
         }
