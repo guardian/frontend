ea59d9e6ed7fa33c4b8e6a52ffff5bbaec7849ff
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-08-22 11:37:42 +0100

    On Desktop, move the revealer to the edge of the MPU

--- static/src/javascripts/projects/common/modules/commercial/creatives/revealer.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/revealer.js
@@ -1,9 +1,10 @@
 define([
     'common/utils/fastdom-promise',
     'common/utils/template',
+    'common/utils/detect',
     'common/modules/commercial/creatives/add-tracking-pixel',
     'text!common/views/commercial/creatives/revealer.html'
-], function(fastdom, template, addTrackingPixel, revealerStr) {
+], function(fastdom, template, detect, addTrackingPixel, revealerStr) {
     var revealerTpl;
 
     function Revealer($adSlot, params) {
@@ -15,6 +16,7 @@ define([
 
         function create() {
             var markup = revealerTpl(params);
+            var isDesktop = detect.isBreakpoint({ min: 'desktop' });
 
             return fastdom.write(function () {
                 $adSlot[0].insertAdjacentHTML('beforeend', markup);
@@ -22,6 +24,16 @@ define([
                 if (params.trackingPixel) {
                     addTrackingPixel($adSlot, params.trackingPixel + params.cacheBuster);
                 }
+            }).then(function () {
+                return fastdom.read(function () {
+                    return $adSlot[0].getBoundingClientRect();
+                });
+            }).then(function (adSlotRect) {
+                if( isDesktop ) {
+                    var background = $adSlot[0].getElementsByClassName('creative__background')[0];
+                    background.style.left = adSlotRect.left + 'px';
+                }
+                return true;
             });
         }
     }
