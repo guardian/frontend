a008162fe0b363653dbd83b1ad22b9711ad7cf7f
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-08-22 16:59:34 +0100

    Wrap DOM mutation in fastdom.write

--- static/src/javascripts/projects/common/modules/commercial/creatives/revealer.js
+++ static/src/javascripts/projects/common/modules/commercial/creatives/revealer.js
@@ -28,12 +28,14 @@ define([
                     return detect.getViewport();
                 });
             }).then(function (viewport) {
-                var background = $adSlot[0].getElementsByClassName('creative__background')[0];
-                background.style.width = viewport.width + 'px';
-                // for the height, we need to account for the height of the location bar, which
-                // may or may not be there. 70px padding is not too much.
-                background.style.height = (viewport.height + 70) + 'px';
-                return true;
+                return fastdom.write(function () {
+                    var background = $adSlot[0].getElementsByClassName('creative__background')[0];
+                    background.style.width = viewport.width + 'px';
+                    // for the height, we need to account for the height of the location bar, which
+                    // may or may not be there. 70px padding is not too much.
+                    background.style.height = (viewport.height + 70) + 'px';
+                    return true;
+                });
             });
         }
     }
