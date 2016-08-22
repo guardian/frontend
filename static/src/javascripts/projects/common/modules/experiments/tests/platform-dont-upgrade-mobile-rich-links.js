2d4a2e08effdf037d7f152f2ddb0db785b189bec
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2016-08-22 11:32:02 +0100

    Update audience, offset and success metric

--- static/src/javascripts/projects/common/modules/experiments/tests/platform-dont-upgrade-mobile-rich-links.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/platform-dont-upgrade-mobile-rich-links.js
@@ -5,15 +5,13 @@ define([], function () {
         this.expiry = '2016-09-14';
         this.author = 'Gareth Trufitt';
         this.description = 'Test whether the loyalty of users decreases with non-enhanced rich links';
-        this.audience = 0.5;
-        this.audienceOffset = 0;
-        this.successMeasure = 'Loyalty of users';
+        this.audience = 0.4;
+        this.audienceOffset = 0.2;
+        this.successMeasure = 'No major drop in overall article page CTR & no major drop in article visits per browser per day';
         this.audienceCriteria = 'All users';
         this.dataLinkNames = '';
-        this.idealOutcome = 'Loyalty of users doesn\'t decrease';
-
-
-
+        this.idealOutcome = 'Happier users & no major drop in article visits per browser per day';
+        
         this.canRun = function () {
             return true;
         };
