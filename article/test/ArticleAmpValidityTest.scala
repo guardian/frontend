d7364849829fc1dec93494f81a7e2ab4a8356a0e
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2016-08-23 17:24:03 +0100

    Add page with rich link

--- article/test/ArticleAmpValidityTest.scala
+++ article/test/ArticleAmpValidityTest.scala
@@ -20,6 +20,7 @@ import org.scalatest.DoNotDiscover
     "/business/2016/aug/09/china-uk-investment-key-questions-following-hinkley-point-c-delay", // Analysis tone
     "/books/2011/aug/24/jorge-luis-borges-google-doodle", // More on this story
     "/uk-news/2016/aug/09/southern-rail-strike-war-of-words-heats-up-on-second-day", // Story package / tone news
-    "/football/2016/jul/10/france-portugal-euro-2016-match-report" // Match summary
+    "/football/2016/jul/10/france-portugal-euro-2016-match-report", // Match summary
+    "/education/2016/aug/23/ofsted-chair-david-hoare-quits-after-isle-of-wight-ghetto-remarks" // Page with a richlink
   ).foreach(testAmpPageValidity)
 }
