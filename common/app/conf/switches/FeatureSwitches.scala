801029039b81a7ffa2c95ab4e384fe0c21d0df6d
Simon Adcock <simon.adcock@guardian.co.uk>
2016-08-24 15:16:29 +0100

    remove amp live blog switch

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -367,16 +367,6 @@ trait FeatureSwitches {
     exposeClientSide = false
   )
 
-  val LiveBlogAmpSwitch = Switch(
-    SwitchGroup.Feature,
-    "live-blog-amp",
-    "If this switch is on, link to amp pages will be in the metadata for live blogs",
-    owners = Seq(Owner.withGithub("SiAdcock")),
-    safeState = Off,
-    sellByDate = new LocalDate(2016, 8, 26),
-    exposeClientSide = false
-  )
-
   // see https://github.com/guardian/frontend/pull/13916
   val AmpLiveBlogNewsArticleSwitch = Switch(
     SwitchGroup.Feature,
