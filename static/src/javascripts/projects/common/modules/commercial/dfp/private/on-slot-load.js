2d43810f32939c50175ccddf924b8b0f48ed3735
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-08-25 15:58:47 +0100

    Add native ad detection heuristic

--- static/src/javascripts/projects/common/modules/commercial/dfp/private/on-slot-load.js
+++ static/src/javascripts/projects/common/modules/commercial/dfp/private/on-slot-load.js
@@ -2,11 +2,14 @@ define([
     'common/modules/commercial/dfp/private/get-advert-by-id',
     'common/modules/commercial/dfp/private/post-message'
 ], function (getAdvertById, postMessage) {
+    var nativeAdName = /^\d+-\d+-\d+;\d+;<!DOCTYPE/;
     return onLoad;
 
     function onLoad(event) {
         var advert = getAdvertById(event.slot.getSlotElementId());
         var iframe = advert.node.querySelector('iframe');
-        postMessage({ id: iframe.id }, iframe.contentWindow);
+        if (nativeAdName.test(iframe.name)) {
+            postMessage({ id: iframe.id }, iframe.contentWindow);
+        }
     }
 });
