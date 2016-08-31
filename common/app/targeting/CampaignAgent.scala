8604a1fe1c01796d821132c56029a97e41a5a695
currysoup <sam.cutler@guardian.co.uk>
2016-08-31 17:31:34 +0100

    Prevented lack of configured STS ARN from breaking article

--- common/app/targeting/CampaignAgent.scala
+++ common/app/targeting/CampaignAgent.scala
@@ -6,26 +6,29 @@ import com.amazonaws.services.s3.AmazonS3Client
 import conf.Configuration
 import services.AwsEndpoints
 
+import scala.util.Try
 import scala.util.control.NonFatal
 
 object CampaignAgent extends Logging with ExecutionContexts {
-  val crossAccountClient: AmazonS3Client = {
+  val crossAccountClient: Option[AmazonS3Client] = Try({
     val client = new AmazonS3Client(Configuration.targeting.crossAccountMandatoryCredentials)
     client.setEndpoint(AwsEndpoints.s3)
     client
-  }
+  }).toOption
 
   private val agent = AkkaAgent[CampaignCache](new CampaignCache())
 
   def refresh() {
-    agent.alter { old =>
-      try {
-        old.update(crossAccountClient, Configuration.targeting.campaignsBucket)
-      } catch {
-        case NonFatal(e) => log.error("Failed to update campaign list.", e)
+    crossAccountClient.foreach(client => {
+      agent.alter { old =>
+        try {
+          old.update(client, Configuration.targeting.campaignsBucket)
+        } catch {
+          case NonFatal(e) => log.error("Failed to update campaign list.", e)
+        }
+        old
       }
-      old
-    }
+    })
   }
 
   def getCampaignsForTags(tags: Seq[String]) = try {
