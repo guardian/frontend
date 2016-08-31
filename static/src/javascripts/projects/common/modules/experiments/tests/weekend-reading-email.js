4220dc1e360329ff590c99f561e5c1655a8e2dfe
Kate Whalen <whalenka88@gmail.com>
2016-08-31 11:53:19 +0100

    Add weekend reading sign-up page with  AB test

--- static/src/javascripts/projects/common/modules/experiments/tests/weekend-reading-email.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/weekend-reading-email.js
@@ -4,16 +4,14 @@ define([
     'fastdom',
     'qwery',
     'common/utils/$',
-    'common/utils/config',
-    'common/modules/email/email'
+    'common/utils/config'
 ], function (
     bean,
     reqwest,
     fastdom,
     qwery,
     $,
-    config,
-    email
+    config
 ) {
     return function () {
         this.id = 'WeekendReadingEmail';
@@ -21,7 +19,7 @@ define([
         this.expiry = '2016-09-23';
         this.author = 'Kate Whalen';
         this.description = '';
-        this.audience = 1;
+        this.audience = 0;
         this.audienceOffset = 0;
         this.successMeasure = 'Trial two different email formats to compare CTO rates';
         this.audienceCriteria = 'All users';
@@ -32,53 +30,32 @@ define([
             return config.page.contentType === 'survey';
         };
 
-        var $buttonEl = qwery('.js-email-sub__submit-input')[0];
-        var $emailListInput = qwery('.js-email-sub__listid-input')[0];
+        function passRefererParam() {
+            var campaignCodeInput = qwery('.js-email-sub__campaignCode-input')[0];
+            var refererCode = window.location.search.replace('?', '');
+            if (refererCode) {
+                campaignCodeInput.setAttribute('value', refererCode);
+            }
+        }
 
         function updateSignupForm(emailListID) {
-            // $buttonEl.setAttribute('type', 'button');
-            $emailListInput.setAttribute('value', emailListID);
-            // bean.on($buttonEl, 'click', reqwestEmailSignup);
+            var emailListInput = qwery('.js-email-sub__listid-input')[0];
+            emailListInput.setAttribute('value', emailListID);
+            passRefererParam();
         }
-
-        function generateFormQueryString() {
-            var userEmail = ($('.js-email-sub__form')[0].elements.email.value).toString();
-            var listId = ($('.js-email-sub__form')[0].elements.listId.value).toString();
-            return 'email=' + encodeURIComponent(userEmail) + '&' + 'listId=' + encodeURIComponent(listId);
-        }
-
-        function reqwestEmailSignup(buttonEl) {
-                var formQueryString = generateFormQueryString();
-                reqwest({
-                    url: '/email',
-                    method: 'POST',
-                    data: formQueryString,
-                    error: function () {
-                        renderErrorMessage(buttonEl);
-                    },
-                    success: function () {
-                        // store a cookie
-                        // update the display
-                    }
-                });
-            }
-
-        // function success(complete) {
-        //     bean.on(qwery('.js-email-sub__submit-input')[0], 'click', complete);
-        // }
 
         this.variants = [
             {
                 id: 'control',
                 test: function () {
-                    var emailListID = 123;
+                    var emailListID = 3742;
                     updateSignupForm(emailListID);
                 }
             },
             {
                 id: 'variant',
                 test: function () {
-                    var emailListID = 456;
+                    var emailListID = 3743;
                     updateSignupForm(emailListID);
                 }
             }
