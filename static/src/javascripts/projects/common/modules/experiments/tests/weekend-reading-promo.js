15d60015a97b09453e1e1eb3c90a07b8a65ef811
Kate Whalen <whalenka88@gmail.com>
2016-09-07 10:42:28 +0100

    Check for Adblock and Membership banners before running ours

--- static/src/javascripts/projects/common/modules/experiments/tests/weekend-reading-promo.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/weekend-reading-promo.js
@@ -1,6 +1,8 @@
 define([
     'common/utils/config',
+    'common/utils/detect',
     'common/utils/template',
+    'common/utils/storage',
     'common/modules/ui/message',
     'common/modules/user-prefs',
     'common/modules/commercial/user-features',
@@ -8,7 +10,9 @@ define([
     'text!common/views/experiments/weekend-reading-promo.html'
 ], function (
     config,
+    detect,
     template,
+    storage,
     Message,
     userPrefs,
     userFeatures,
@@ -29,16 +33,47 @@ define([
         this.idealOutcome = 'Visitors click on the CTA and sign-up to the Weekend Reading email';
 
         this.canRun = function () {
-            return !config.page.isAdvertisementFeature && config.page.contentType === 'Article' && !hasSeenDigestSnap();
+            return !config.page.isAdvertisementFeature &&
+                config.page.contentType === 'Article' &&
+                !isCompetingWithMembershipMessage() &&
+                !isCompetingWithAdblockMessage() &&
+                // we ran a similar Snap before, so do not show our new CTA to these users
+                !hasSeenMessage('habit-digest-message-07-16');
         };
 
-        function hasSeenDigestSnap() {
-            // we ran a similar Snap before, so do not show our new CTA to these users
-            var messageStates = userPrefs.get('messages');
-            return messageStates && messageStates.indexOf('habit-digest-message-07-16') > -1;
+        function hasSeenMembershipMessage() {
+            // This causes me intense pain to do, but the membership message codes are buried in the module and cannot be accessed.
+            // yes, this means this file will have to be updated whenever these codes get rotated. Hopefully, this test can get turned off before that happens.
+            return hasSeenMessage('membership-message-uk-2016-06-24') ||
+                hasSeenMessage('membership-message-us-2016-06-24') ||
+                hasSeenMessage('membership-message-au-2016-08-01') ||
+                hasSeenMessage('membership-message-int-2016-06-24');
+
         }
 
-        function renderDigestSnap(messageText, linkText, linkHref, variantName) {
+        // check to see if user has an ad-blocker enabled; we want the ad-block message to have priority
+        function isCompetingWithAdblockMessage() {
+            detect.adblockInUse.then(function (adblockUsed) {
+                return adblockUsed;
+            });
+        }
+
+        function isCompetingWithMembershipMessage() {
+            // membership banner only shows after 10 page visits, so we can safely show it to those with less than 10
+            if (10 > (storage.local.get('gu.alreadyVisited') || 0)) {
+                return false;
+            } else if (hasSeenMembershipMessage()) {
+                return false;
+            }
+            return true;
+        }
+
+        function hasSeenMessage(messageName) {
+            var messageStates = userPrefs.get('messages');
+            return messageStates && messageStates.indexOf(messageName) > -1;
+        }
+
+        function renderWeekendReadingSnap(messageText, linkText, linkHref, variantName) {
             var templateData = {
                 linkText: linkText,
                 messageText: messageText,
@@ -70,8 +105,8 @@ define([
                 test: function () {
                     var messageText = 'Get the best reads of the week in your inbox every Saturday';
                     var linkText = 'Sign up';
-                    var linkHref = '/survey/weekendreading?CMP=SnapBanner';
-                    renderDigestSnap(messageText, linkText, linkHref, 'weekend-reading');
+                    var linkHref = '/signup/weekendreading?CMP=SnapBanner';
+                    renderWeekendReadingSnap(messageText, linkText, linkHref, 'weekend-reading');
                 }
             }
         ];
