ddb5a78de40ae813761ee9d26dff45efd52aa506
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-09-14 11:49:28 +0100

    Inject ContentApiClient into SeriesController

--- onward/test/SeriesControllerTest.scala
+++ onward/test/SeriesControllerTest.scala
@@ -1,14 +1,22 @@
 package test
 
-import org.scalatest.{DoNotDiscover, Matchers, FlatSpec}
+import controllers.SeriesController
+import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, FlatSpec, Matchers}
 import play.api.test.Helpers._
 
-@DoNotDiscover class SeriesControllerTest extends FlatSpec with Matchers with ConfiguredTestSuite {
+@DoNotDiscover class SeriesControllerTest
+  extends FlatSpec
+  with Matchers
+  with ConfiguredTestSuite
+  with BeforeAndAfterAll
+  with WithTestWsClient
+  with WithTestContentApiClient {
 
   var series = "news/series/pass-notes"
+  lazy val seriesController = new SeriesController(testContentApiClient)
 
   "Series Controller" should "200 when content type is a tag" in {
-    val result = controllers.SeriesController.renderSeriesStories(series)(TestRequest())
+    val result = seriesController.renderSeriesStories(series)(TestRequest())
     status(result) should be (200)
   }
 
