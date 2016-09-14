0214c9329c8540e3c20bf37410926304eec70cb2
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-09-14 11:17:49 +0100

    Inject ContentApiClient into MediaInSectionController

--- standalone/app/StandaloneLifecycleComponents.scala
+++ standalone/app/StandaloneLifecycleComponents.scala
@@ -1,10 +1,11 @@
-import app.{LifecycleComponent, FrontendComponents}
+import app.{FrontendComponents, LifecycleComponent}
 import com.softwaremill.macwire._
 import commercial.CommercialLifecycle
 import common.Logback.LogstashLifecycle
 import common.dfp.FaciaDfpAgentLifecycle
 import conf.FootballLifecycle
 import conf.switches.SwitchboardLifecycle
+import contentapi.{CapiHttpClient, ContentApiClient}
 import cricket.conf.CricketLifecycle
 import feed.OnwardJourneyLifecycle
 import rugby.conf.RugbyLifecycle
@@ -12,6 +13,11 @@ import services.ConfigAgentLifecycle
 
 trait StandaloneLifecycleComponents extends SportServices with CommercialServices with FapiServices with OnwardServices {
   self: FrontendComponents =>
+
+  //Override conflicting members
+  override lazy val capiHttpClient = wire[CapiHttpClient]
+  override lazy val contentApiClient = wire[ContentApiClient]
+
   def standaloneLifecycleComponents: List[LifecycleComponent] = List(
     wire[LogstashLifecycle],
     wire[CommercialLifecycle],
