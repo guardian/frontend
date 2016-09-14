09742d6aea545f116cec4132bd472388b8988f82
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-09-14 14:55:34 +0100

    Inject ContentApiClient into ItemResponseController

--- standalone/app/controllers/ItemController.scala
+++ standalone/app/controllers/ItemController.scala
@@ -1,13 +1,17 @@
 package controllers
 
+import contentapi.ContentApiClient
+
 // If you add to this, don't forget the one in dev-build
-class ItemController(articleController: ArticleController,
+class ItemController(contentApiClient: ContentApiClient,
+                     articleController: ArticleController,
                      faciaDraftController: FaciaDraftController,
                      galleryController: GalleryController,
                      mediaController: MediaController,
                      interactiveController: InteractiveController,
                      imageContentController: ImageContentController
                     ) extends ItemResponseController(
+  contentApiClient,
   articleController,
   galleryController,
   mediaController,
