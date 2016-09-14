35cb4d7fb84f80ce2237ed2c98f2c22cabc66d17
Kate Whalen <whalenka88@gmail.com>
2016-09-14 13:35:37 +0100

    Add 'Guardian Opinion US' listing to Email Prefs page

--- identity/app/model/EmailSubscriptions.scala
+++ identity/app/model/EmailSubscriptions.scala
@@ -290,7 +290,7 @@ object EmailSubscriptions {
 
   def commentEmails(subscribedListIds: Iterable[String] = None) = List(
     EmailSubscription(
-      "The best of Guardian Opinion",
+      "Best of Guardian Opinion - UK",
       "comment",
       "Opinion's daily email newsletter",
       "Guardian Opinion's daily email newsletter with the most shared opinion, analysis and editorial articles from the last 24 hours — sign up to read, share and join the debate every afternoon.",
@@ -300,7 +300,16 @@ object EmailSubscriptions {
       subscribedTo = subscribedListIds.exists{ x => x == "2313" }
     ),
     EmailSubscription(
-      "Best of Guardian Opinion - Australia",
+      "Best of Guardian Opinion - US",
+      "comment",
+      "Opinion's daily email newsletter",
+      "Guardian Opinion's daily email newsletter with the most shared opinion, analysis and editorial articles from the last 24 hours — sign up to read, share and join the debate every afternoon.",
+      "Weekday afternoons",
+      "3228",
+      subscribedTo = subscribedListIds.exists{ x => x == "3228" }
+    ),
+    EmailSubscription(
+      "Best of Guardian Opinion - AUS",
       "comment",
       "An evening selection of the best reads from Guardian Opinion in Australia",
       "An evening selection of the best reads from Guardian Opinion in Australia",
