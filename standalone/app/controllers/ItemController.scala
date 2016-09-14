250c6011b4f6e158414244162432a8a7c7a293db
Thomas Bonnin <thomas.bonnin@gmail.com>
2016-09-14 14:28:00 +0100

    Inject ContentApiClient into ImageQuery

--- standalone/app/controllers/ItemController.scala
+++ standalone/app/controllers/ItemController.scala
@@ -1,18 +1,17 @@
 package controllers
 
-import contentapi.ContentApiClient
-
 // If you add to this, don't forget the one in dev-build
 class ItemController(articleController: ArticleController,
                      faciaDraftController: FaciaDraftController,
                      galleryController: GalleryController,
                      mediaController: MediaController,
-                     interactiveController: InteractiveController
+                     interactiveController: InteractiveController,
+                     imageContentController: ImageContentController
                     ) extends ItemResponseController(
   articleController,
   galleryController,
   mediaController,
   interactiveController,
-  ImageContentController,
+  imageContentController,
   faciaDraftController
 )
