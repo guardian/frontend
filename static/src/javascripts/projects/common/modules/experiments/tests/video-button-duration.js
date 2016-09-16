68e4476e65293c086554cb6205ffb32dff34e48e
Chris J Clarke <mrmr.mail@gmail.com>
2016-09-16 12:21:47 +0100

    ab test return focuses on video only

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/video-button-duration.js
@@ -0,0 +1,37 @@
+define([
+    'common/utils/config',
+    'bean'
+], function (
+    config,
+    bean
+) {
+    return function () {
+        this.id = 'VideoButtonDuration';
+        this.start = '2016-09-13';
+        this.expiry = '2016-09-30';
+        this.author = 'Chris J Clarke';
+        this.description = 'Test whether adding duration to the play button increases plays';
+        this.audience = 0.5;
+        this.audienceOffset = 0.5;
+        this.successMeasure = 'No significant difference in clicks between the variant and control';
+        this.audienceCriteria = 'All users';
+        this.dataLinkNames = '';
+        this.idealOutcome = 'Video plays in the control group is not more than 2% higher';
+
+        this.canRun = function () {
+          return document.getElementsByClassName('gu-media-wrapper--video').length > 0;
+        };
+
+
+        this.variants = [
+            {
+                id: 'control',
+                test: function () {}
+            },
+            {
+                id: 'video-button-duration',
+                test: function () {}
+            }
+        ];
+    };
+});
