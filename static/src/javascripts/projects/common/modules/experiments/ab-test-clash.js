4edd0b1d6ae1e35f7e1375914ec9a222db2d108e
markjamesbutler <markjamesbutler@gmail.com>
2016-09-16 17:08:54 +0100

    Add variants and button styling.

--- static/src/javascripts/projects/common/modules/experiments/ab-test-clash.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-test-clash.js
@@ -12,7 +12,7 @@ define([
 
     function _testABClash(f) {
 
-        var contributionsEpic = {name: 'ContributionsEpic20160906', variants: ['control']};
+        var contributionsEpic = {name: 'ContributionsEpic20160916', variants: ['control', 'give', 'today', 'make']};
 
         var contributionsEpicButtons = {name: 'ContributionsEpicButtons20160907', variants: ['control', 'buttons']};
 
