0c39cc6b6d64ad8c30a4ed5a516bb26e67eb92f9
Justin Pinner <justinpinner@gmail.com>
2016-09-21 18:40:04 +0100

    Redo the test

--- static/src/javascripts/projects/common/modules/commercial/survey/survey-adblock.js
+++ static/src/javascripts/projects/common/modules/commercial/survey/survey-adblock.js
@@ -25,36 +25,25 @@ define([
         this.config = config || {};
         this.bannerTmpl = template(surveyAdBlockTemplate,
             {
-                surveyHeader: this.config.surveyHeader,
-                surveyText: this.config.surveyText,
-                surveyTextSecond: this.config.surveyTextSecond,
-                surveyTextThird: this.config.surveyTextThird,
-                surveyTextMembership: this.config.surveyTextMembership,
-                surveyTextSubscriber: this.config.surveyTextSubscriber,
-                surveyTextUserHelp: this.config.surveyTextUserHelp,
-                signupText: this.config.signupText,
-                membershipText: this.config.membershipText,
-                signupLink: this.config.signupLink,
-                membershipLink: this.config.membershipLink,
-                signupDataLink: this.config.signupDataLink,
-                membershipDataLink: this.config.membershipDataLink,
-                subscriberLink: this.config.subscriberLink,
-                subscriberText: this.config.subscriberText,
-                subscriberDataLink: this.config.subscriberDataLink,
-                contributorLink: this.config.contributorLink,
-                contributorText: this.config.contributorText,
-                contributorDataLink: this.config.contributorDataLink,
-                showCloseBtn: this.config.showCloseBtn,
                 arrowWhiteRight: svgs('arrowWhiteRight'),
                 marque36icon: svgs('marque36icon'),
                 crossIcon: svgs('crossIcon'),
-                surveyOverlaySimple: svgs('surveyOverlaySimple')
+                surveyOverlaySimple: svgs('surveyOverlaySimple'),
+                adFreeDataLink: this.config.adFreeDataLink,
+                adFreeText: this.config.adFreeText,
+                whitelistDataLink: this.config.whitelistDataLink,
+                whitelistText: this.config.whitelistText,
+                whitelistGuideImage: this.config.whitelistGuideImage
+                adBlockIcon: this.config.adBlockIcon,
+                adBlockPlusIcon: this.config.adBlockPlusIcon,
+                variant: this.config.variant
             });
     };
 
     surveyAdBlock.prototype.attach = function () {
         fastdom.write(function () {
             $(document.body).append(this.bannerTmpl);
+            // TODO: dump the close button but wire up the other interactions
             if (this.config.showCloseBtn) {
                 bean.on(document, 'click', $('.js-survey-adblock__close-btn'), function () {
                     $('.survey-adblock').addClass('is-hidden');
@@ -71,6 +60,7 @@ define([
         fastdom.write(function () {
             $('.js-survey-adblock').removeClass('is-hidden');
         });
+        // TODO: dump the close button but wire up the other interactions
         if (this.config.showCloseBtn) {
             if (this.config.showCloseBtn === 'delayed') {
                 countdown.startTimer(5, function(seconds) {
