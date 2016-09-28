b4c45a934445dbfbd7c6cc80b20cc5a14cce05ec
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-09-28 14:27:30 +0100

    Should prebid advert must return false if user is participating sonobi test

--- static/src/javascripts/projects/commercial/modules/dfp/should-prebid-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/should-prebid-advert.js
@@ -1,6 +1,7 @@
 define([
+    'common/utils/config',
     'commercial/modules/dfp/dfp-env'
-], function (dfpEnv) {
+], function (config, dfpEnv) {
     var excludedAdvertIds = [
         'dfp-ad--pageskin-inread',
         'dfp-ad--merchandising-high'
@@ -9,7 +10,11 @@ define([
     return shouldPrebidAdvert;
 
     function shouldPrebidAdvert(advert) {
-        return dfpEnv.prebidEnabled &&
+
+        var participatingInSonobi = 'tests' in config && config.tests.commercialHbSonobi;
+
+        return !participatingInSonobi &&
+            dfpEnv.prebidEnabled &&
             dfpEnv.shouldLazyLoad() &&
             excludedAdvertIds.indexOf(advert.id) === -1;
     }
