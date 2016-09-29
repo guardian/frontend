226f94620b9b1407c1679fb570ff0a58e8e606a7
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-09-29 16:40:15 +0100

    Add isAdvertisementFeature flag to content pages

--- common/app/model/content.scala
+++ common/app/model/content.scala
@@ -225,7 +225,8 @@ final case class Content(
     ("showRelatedContent", JsBoolean(if (tags.isTheMinuteArticle) { false } else (showInRelated && !legallySensitive))),
     ("productionOffice", JsString(productionOffice.getOrElse(""))),
     ("isImmersive", JsBoolean(isImmersive)),
-    ("isExplore", JsBoolean(isExplore))
+    ("isExplore", JsBoolean(isExplore)),
+    ("isAdvertisementFeature", JsBoolean(isAdvertisementFeature))
   )
 
   // Dynamic Meta Data may appear on the page for some content. This should be used for conditional metadata.
