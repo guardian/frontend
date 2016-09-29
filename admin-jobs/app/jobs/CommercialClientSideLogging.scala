b642747a500a8f86dbc132565cfda77794080e0d
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-09-29 18:01:28 +0100

    Update job frequency and log message

--- admin-jobs/app/jobs/CommercialClientSideLogging.scala
+++ admin-jobs/app/jobs/CommercialClientSideLogging.scala
@@ -16,16 +16,20 @@ object CommercialClientSideLogging extends Logging {
 
   // This function takes a date interval, and finds logging data for that range.
   // It then prints this data to log.
-  def writeReportsToLog(fromTime: DateTime, duration: Duration): Unit = {
+  def writeReportsToLog(fromTime: DateTime, duration: Duration): Int = {
     val toTime = fromTime.plus(duration)
     val timestamps: List[DateTime] = Iterator.iterate(fromTime)(_.plus(step)).takeWhile(!_.isAfter(toTime)).toList
-    timestamps.foreach(timestamp => {
+    val reports = timestamps.flatMap(timestamp => {
       for {
         report <- ClientSideLogging.getReports(timestamp)
-      }{
+      } yield {
+        // Specifically use this logger to make it clear what's happening. These log lines go to a different file,
+        // with an hourly rolling policy.
         commercialLogger.info(report)
+        report
       }
     })
+    reports.length
   }
 }
 
@@ -38,21 +42,24 @@ class CommercialClientSideLoggingLifecycle(
     jobs.deschedule("CommercialClientSideLoggingJob")
   }}
 
-  private val jobFrequency = 180.seconds // 3 minutes between each log write
+  // 5 minutes between each log write.
+  private val jobFrequency = 600.seconds
 
   override def start(): Unit = {
     jobs.deschedule("CommercialClientSideLoggingJob")
 
-    jobs.scheduleEveryNMinutes("CommercialClientSideLoggingJob", 1) {
+    jobs.scheduleEveryNMinutes("CommercialClientSideLoggingJob", 5) {
       run(akkaAsync)
     }
   }
 
   def run(akkaAsync: AkkaAsync): Future[Unit] = Future {
     akkaAsync.after1s {
+      // Start searching logs by doubling the period, allowing fresh data to settle.
       val timeStart = DateTime.now.minusSeconds(jobFrequency.toSeconds.toInt * 2)
-      log.logger.info(s"Fetching commercial performance logs from Redis for time period ${timeStart.toString("yyyy-MM-dd'T'HH:mm")}")
-      CommercialClientSideLogging.writeReportsToLog(timeStart, new Duration(jobFrequency.toMillis))
+      log.logger.info(s"Fetching commercial performance logs from Redis for time period ${timeStart.toString("yyyy-MM-dd HH:mm")}")
+      val numReports = CommercialClientSideLogging.writeReportsToLog(timeStart, new Duration(jobFrequency.toMillis))
+      log.logger.info(s"Fetched $numReports logs from Redis.")
     }
   }
 }
