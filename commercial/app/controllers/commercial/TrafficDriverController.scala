57efd71940e349d9ffadc68dfa54a272d4973b92
Jamie Byers <james.byers@guardian.co.uk>
2016-10-06 11:08:37 +0100

    Added endpoint method for traffic driver json.

--- commercial/app/controllers/commercial/TrafficDriverController.scala
+++ commercial/app/controllers/commercial/TrafficDriverController.scala
@@ -1,13 +1,14 @@
 package controllers.commercial
 
-import common.{ExecutionContexts, Logging}
+import common.{Edition, ExecutionContexts, JsonComponent, Logging}
 import contentapi.ContentApiClient
-import model.commercial.{CapiAgent, Lookup}
-import model.ContentType
-import play.api.mvc.{AnyContent, Controller, Request}
+import model.commercial.{CapiAgent, CapiSingle, Lookup}
+import model.{Cached, ContentType}
+import play.api.mvc.{Action, AnyContent, Controller, Request}
 
 import scala.concurrent.Future
 import scala.util.control.NonFatal
+import scala.concurrent.duration.DurationInt
 
 class TrafficDriverController(
     contentApiClient: ContentApiClient,
@@ -32,6 +33,17 @@ class TrafficDriverController(
         }
 
         content
+
+    }
+
+    private def renderJson() = Action.async { implicit request =>
+
+        retrieveContent().map {
+            case Nil => Cached(componentNilMaxAge){ jsonFormat.nilResult }
+            case content :: _ => Cached(60.seconds) {
+                JsonComponent(CapiSingle.fromContent(content, Edition(request)))
+            }
+        }
 
     }
 
