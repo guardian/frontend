00a353691b69c231a71da3855bb4633c19d29a48
Kate Whalen <whalenka88@gmail.com>
2016-10-06 16:26:45 +0100

    Make the label stylish with no JS, and with JS

--- common/app/staticpages/StaticPages.scala
+++ common/app/staticpages/StaticPages.scala
@@ -46,7 +46,7 @@ object StaticPages {
   def simpleEmailSignupPage(id: String, webTitle: String): SimplePage = SimplePage(
     MetaData.make(
       id = id,
-      section = Option(SectionSummary(id="global", activeBrandings=None)),
+      section = Option(SectionSummary(id="email-signup-page", activeBrandings=None)),
       webTitle = webTitle,
       analyticsName = "global",
       // a currently running AB test is using `contentType = "survey"` change this to "signup" after 2016-09-13
