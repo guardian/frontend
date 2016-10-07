bd548b78d484840db6cb99bc12e05322e16ff91c
Jon Norman <jon.norman@guardian.co.uk>
2016-10-07 10:35:36 +0100

    Removes useless Object namespaces and reorders JSON macros.
    The macros need to be reodered such that the higher level macros
    are declared after the dependent lower-level macros.
    
    This is paving the way for removing a lot of these explicit reads.

--- commercial/app/model/commercial/events/Eventbrite.scala
+++ commercial/app/model/commercial/events/Eventbrite.scala
@@ -43,144 +43,106 @@ object Eventbrite extends ExecutionContexts with Logging {
     val description = Seq(name, city orElse country).flatten mkString ", "
   }
 
-  object Response {
 
-    implicit val ebResponseReads: Reads[Response] = (
-      (JsPath \ "pagination").read[Pagination] and
-        (JsPath \ "events").read[Seq[Event]]
-      ) (Response.apply _)
+  implicit val jodaDateTimeFormats: Format[DateTime] = {
+
+    val dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    Format(Reads.jodaDateReads(dateFormat), Writes.jodaDateWrites(dateFormat))
   }
 
+  implicit val ticketReads: Reads[Option[Ticket]] = (
+    (JsPath \ "hidden").read[Boolean] and
+      (JsPath \ "donation").read[Boolean] and
+      (JsPath \ "cost" \ "value").read[Double].orElse(Reads.pure(0.00)) and
+      (JsPath \ "quantity_total").read[Int] and
+      (JsPath \ "quantity_sold").read[Int]
+    ) (buildTicket _)
 
-
-  object Pagination {
-
-    implicit val readsPagination: Reads[Pagination] = (
-      (JsPath \ "page_number").read[Int] and
-        (JsPath \ "page_count").read[Int]
-      ) (Pagination.apply _)
-  }
-
-  object Event {
-
-    def apply(id: String,
-              name: String,
-              startDate: DateTime,
-              url: String,
-              description: String,
-              status: String,
-              venue: Venue,
-              tickets: Seq[Option[Ticket]],
-              capacity: Int): Event = {
-
-      new Event(
-        id,
-        name,
-        startDate,
-        url,
-        description,
-        None,
-        status,
-        venue,
-        tickets.flatten,
-        capacity
-      )
-    }
-
-    implicit val jodaDateTimeFormats: Format[DateTime] = {
-
-      val dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-      Format(Reads.jodaDateReads(dateFormat), Writes.jodaDateWrites(dateFormat))
-    }
-
-    implicit val eventReads: Reads[Event] = (
-
-      (JsPath \ "id").read[String] and
-        (JsPath \ "name" \ "text").read[String] and
-        (JsPath \ "start" \ "utc").read[DateTime] and
-        (JsPath \ "url").read[String] and
-        (JsPath \ "description" \ "html").read[String] and
-        (JsPath \ "status").read[String] and
-        (JsPath \ "venue").read[Venue] and
-        (JsPath \ "ticket_classes").read[Seq[Option[Ticket]]] and
-        (JsPath \ "capacity").read[Int]
-      ) (Event.apply(_: String, _: String, _: DateTime, _: String, _: String, _: String, _: Venue, _: Seq[Option[Ticket]], _: Int))
-
-    implicit val eventsReads: Reads[Seq[Event]] = new Reads[Seq[Event]] {
-      override def reads(json: JsValue): JsResult[Seq[Event]] = {
-        json match {
-          case JsArray(jsValues) => JsSuccess(jsValues.flatMap(_.asOpt[Event]))
-          case _ => JsError(Seq(JsPath() -> Seq(ValidationError("error.expected.jsarray"))))
-        }
+  implicit val ticketsReads: Reads[Seq[Ticket]] = new Reads[Seq[Ticket]] {
+    override def reads(json: JsValue): JsResult[Seq[Ticket]] = {
+      json match {
+        case JsArray(jsValues) => JsSuccess(jsValues.flatMap(_.as[Option[Ticket]]))
+        case _ => JsError(Seq(JsPath() -> Seq(ValidationError("error.expected.jsarray"))))
       }
     }
+  }
 
-    def buildEventWithImageSrc(event: Event, src: String) = {
-      new Event(
-        event.id,
-        event.name,
-        event.startDate,
-        event.url,
-        event.description,
-        Some(src),
-        event.status,
-        event.venue,
-        event.tickets,
-        event.capacity
-      )
+  implicit val ticketWrites: Writes[Ticket] = Json.writes[Ticket]
+
+  implicit val venueReads: Reads[Venue] = {
+
+    def captureEmptyString(x: Reads[Option[String]]): Reads[Option[String]] = {
+      x map (el => if (el.getOrElse("").length == 0) None else el)
+    }
+
+    (
+      captureEmptyString((JsPath \ "name").readNullable[String]) and
+        captureEmptyString((JsPath \ "address" \ "address_1").readNullable[String]) and
+        captureEmptyString((JsPath \ "address" \ "address_2").readNullable[String]) and
+        captureEmptyString((JsPath \ "address" \ "city").readNullable[String]) and
+        captureEmptyString((JsPath \ "address" \ "country").readNullable[String]) and
+        captureEmptyString((JsPath \ "address" \ "postal_code").readNullable[String])
+      ) (Venue.apply _)
+  }
+
+  implicit val venueWrites: Writes[Venue] = Json.writes[Venue]
+
+  implicit val eventReads: Reads[Event] = (
+
+    (JsPath \ "id").read[String] and
+      (JsPath \ "name" \ "text").read[String] and
+      (JsPath \ "start" \ "utc").read[DateTime] and
+      (JsPath \ "url").read[String] and
+      (JsPath \ "description" \ "html").read[String] and
+      (JsPath \ "imageUrl").readNullable[String] and
+      (JsPath \ "status").read[String] and
+      (JsPath \ "venue").read[Venue] and
+      (JsPath \ "ticket_classes").read[Seq[Ticket]] and
+      (JsPath \ "capacity").read[Int]
+    ) (Event.apply _)
+
+  implicit val eventsReads: Reads[Seq[Event]] = new Reads[Seq[Event]] {
+    override def reads(json: JsValue): JsResult[Seq[Event]] = {
+      json match {
+        case JsArray(jsValues) => JsSuccess(jsValues.flatMap(_.asOpt[Event]))
+        case _ => JsError(Seq(JsPath() -> Seq(ValidationError("error.expected.jsarray"))))
+      }
     }
   }
 
-  object Ticket {
+  implicit val paginationReads: Reads[Pagination] = (
+    (JsPath \ "page_number").read[Int] and
+      (JsPath \ "page_count").read[Int]
+    ) (Pagination.apply _)
 
-    def buildTicket(hidden: Boolean, donation: Boolean, valuePence: Double, quantityTotal: Int, quantitySold: Int): Option[Ticket] = {
-      if (hidden || donation) {
-        None
-      } else {
-        Some(Ticket(valuePence / 100, quantityTotal, quantitySold))
-      }
-    }
+  implicit val responseReads: Reads[Response] = (
+    (JsPath \ "pagination").read[Pagination] and
+      (JsPath \ "events").read[Seq[Event]]
+    ) (Response.apply _)
 
-    implicit val ticketReads: Reads[Option[Ticket]] = (
-      (JsPath \ "hidden").read[Boolean] and
-        (JsPath \ "donation").read[Boolean] and
-        (JsPath \ "cost" \ "value").read[Double].orElse(Reads.pure(0.00)) and
-        (JsPath \ "quantity_total").read[Int] and
-        (JsPath \ "quantity_sold").read[Int]
-      ) (Ticket.buildTicket _)
-
-    implicit val ticketsReads: Reads[Seq[Ticket]] = new Reads[Seq[Ticket]] {
-      override def reads(json: JsValue): JsResult[Seq[Ticket]] = {
-        json match {
-          case JsArray(jsValues) => JsSuccess(jsValues.flatMap(_.as[Option[Ticket]]))
-          case _ => JsError(Seq(JsPath() -> Seq(ValidationError("error.expected.jsarray"))))
-        }
-      }
-    }
-
-    implicit val ticketWrites: Writes[Ticket] = Json.writes[Ticket]
+  def buildEventWithImageSrc(event: Event, src: String) = {
+    new Event(
+      event.id,
+      event.name,
+      event.startDate,
+      event.url,
+      event.description,
+      Some(src),
+      event.status,
+      event.venue,
+      event.tickets,
+      event.capacity
+    )
   }
 
-  object Venue {
-
-    implicit val venueReads: Reads[Venue] = {
-
-      def captureEmptyString(x: Reads[Option[String]]): Reads[Option[String]] = {
-        x map (el => if (el.getOrElse("").length == 0) None else el)
-      }
-
-      (
-        captureEmptyString((JsPath \ "name").readNullable[String]) and
-          captureEmptyString((JsPath \ "address" \ "address_1").readNullable[String]) and
-          captureEmptyString((JsPath \ "address" \ "address_2").readNullable[String]) and
-          captureEmptyString((JsPath \ "address" \ "city").readNullable[String]) and
-          captureEmptyString((JsPath \ "address" \ "country").readNullable[String]) and
-          captureEmptyString((JsPath \ "address" \ "postal_code").readNullable[String])
-        ) (Venue.apply _)
+  def buildTicket(hidden: Boolean, donation: Boolean, valuePence: Double, quantityTotal: Int, quantitySold: Int): Option[Ticket] = {
+    if (hidden || donation) {
+      None
+    } else {
+      Some(Ticket(valuePence / 100, quantityTotal, quantitySold))
     }
-
-    implicit val venueWrites: Writes[Venue] = Json.writes[Venue]
   }
+
   def parsePagesOfEvents(feedMetaData: FeedMetaData, feedContent: => Option[String]): Future[ParsedFeed[Event]] = {
 
     feedMetaData.parseSwitch.isGuaranteedSwitchedOn flatMap { switchedOn =>
