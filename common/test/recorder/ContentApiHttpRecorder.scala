d27d670cab0c2ccd7f59b40d289872c8eefd2817
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-10-07 12:33:47 +0100

    Use thrift for cached capi test fixtures

--- common/test/recorder/ContentApiHttpRecorder.scala
+++ common/test/recorder/ContentApiHttpRecorder.scala
@@ -1,24 +1,31 @@
 package recorder
 
-import java.nio.charset.StandardCharsets
-
+import com.gu.contentapi.client.model.v1.ErrorResponse
+import com.gu.contentapi.client.thrift.ThriftDeserializer
 import contentapi.{Response => ContentApiResponse}
+
+import scala.util.Try
 
 trait ContentApiHttpRecorder extends HttpRecorder[ContentApiResponse] {
 
-  def toResponse(str: String) = {
-    if (str.startsWith("Error:")) {
-      ContentApiResponse(Array.empty, str.replace("Error:", "").toInt, "")
-    } else {
-      ContentApiResponse(str.getBytes(StandardCharsets.UTF_8), 200, "")
+  override def toResponse(b: Array[Byte]): ContentApiResponse = {
+    val errorResponse = Try(ThriftDeserializer.deserialize(b, ErrorResponse)).toOption
+    errorResponse map { response =>
+      if (response.status == "ok") {
+        ContentApiResponse(b, 200, "")
+      } else {
+        val statusCode = response.message match {
+          case "requested page is beyond the number of available pages" =>  400
+          case "The requested resource could not be found." =>              404
+          case "The requested resource has expired for rights reason." =>   410
+          case _ =>                                                         500
+        }
+        ContentApiResponse(b, statusCode, response.message)
+      }
+    } getOrElse {
+      ContentApiResponse(b, 200, "")
     }
   }
 
-  def fromResponse(response: ContentApiResponse) = {
-    if (response.status == 200) {
-      new String(response.body, StandardCharsets.UTF_8)
-    } else {
-      s"Error:${response.status}"
-    }
-  }
+  override def fromResponse(response: ContentApiResponse): Array[Byte] = response.body
 }
