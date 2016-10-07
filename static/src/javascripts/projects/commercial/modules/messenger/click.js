1b246c36b0b621aa13b91cb1e4107303a474a93b
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-10-07 16:46:25 +0100

    Add click tracking handler

---
+++ static/src/javascripts/projects/commercial/modules/messenger/click.js
@@ -0,0 +1,15 @@
+define([
+    'common/utils/closest',
+    'common/modules/analytics/google',
+    'commercial/modules/messenger'
+], function (closest, google, messenger) {
+    messenger.register('click', function (linkName, ret, iframe) {
+        return sendClick(closest(iframe, '.js-ad-slot') || { id: 'unknown' }, linkName)
+    });
+
+    return sendClick;
+
+    function sendClick(adSlot, linkName) {
+        google.trackNativeAdLinkClick(adSlot.id, linkName);
+    }
+});
