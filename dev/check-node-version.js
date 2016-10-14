73baa0e82350694ca048aae1bde48440f56458ac
Alex Sanders <a@snd.rs>
2016-10-14 12:03:13 +0100

    comment

--- dev/check-node-version.js
+++ dev/check-node-version.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+// given this could be any version of node that runs this, it uses no bells or whistles...
+
 var fs = require('fs');
 var path = require('path');
 
 var currentVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
 var minVersion = Number(fs.readFileSync(path.join(__dirname, '../', '.nvmrc'), 'utf8'));
+
 if (currentVersion < minVersion) {
     console.log("You are using Node v" + currentVersion + ".\n" +
         "Frontend requires Node v" + minVersion + " or later.\n" +
