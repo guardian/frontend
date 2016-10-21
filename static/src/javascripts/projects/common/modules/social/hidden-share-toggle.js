fa419c772a3cd402f5428f1e163c422af8bf9100
rjmk <rjmk>
2016-10-21 11:14:09 +0100

    Add expand and close functionality
    Expand and close uses hardcoded media queries to determine correct
    width of tray. Yank eliminated by a height on an element that is
    used in several contexts. Need to check this. Submeta displays
    all social icons

---
+++ static/src/javascripts/projects/common/modules/social/hidden-share-toggle.js
@@ -0,0 +1,31 @@
+define([
+    'fastdom',
+    'bean',
+    'common/utils/$'
+], function (
+    fastdom,
+    bean,
+    $
+) {
+
+    function toggleDisplay (_) {
+
+        $('.social__secondary').each(function (icon){
+            fastdom.write(function () {
+                $(icon).toggleClass('social--hidden');
+            });
+        });
+
+        $('.social--top').each(function (topSocial) {
+            fastdom.write(function () {
+                $(topSocial).toggleClass('social--expanded-top')
+            });
+        });
+    }
+
+    return function hiddenShareToggle () {
+        $('.social-icon--more, .social__tray-close').each(function (toggle) {
+            bean.on(toggle, 'click', toggleDisplay);
+        });
+    }
+});
