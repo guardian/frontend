c6230f4e4ccdc4f3ebc3ba82b3f382e776e0dfc2
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-10-24 12:06:02 +0100

    Add proper content and styling for hosted more from popup

--- static/src/javascripts/projects/commercial/modules/hosted/onward-journey-popup.js
+++ static/src/javascripts/projects/commercial/modules/hosted/onward-journey-popup.js
@@ -31,20 +31,25 @@ define([
     };
 
     HostedOnwardJourneyPopup.prototype.bindButtons = function() {
+        var that = this;
         this.$overlay = $('.js-hosted-more-from-overlay');
         this.$seeAllButton = $('.js-hosted-see-all-onward');
         this.$closeButton = $('.js-hosted-more-from-overlay-close');
+        this.$container = $('.js-hosted-more-from-popup-container');
+        this.$cards = $('.js-hosted-more-from-popup-content');
 
         if (this.$seeAllButton.length) {
-            var that = this;
             this.$seeAllButton.each(function(el){
                 bean.on(el, 'click', that.openPopup.bind(that));
             });
             this.$closeButton.each(function(el){
                 bean.on(el, 'click', that.closePopup.bind(that));
             });
-        }
 
+            fastdom.write(function () {
+                that.$container[0].appendChild(that.$cards[0]);
+            });
+        }
     };
 
     function init() {
