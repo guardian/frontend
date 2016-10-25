35456a7141ae977b5a03089ad3caa8e504056085
Dominic Kendrick <dominic.kendrick@guardian.co.uk>
2016-10-25 16:24:59 +0100

    passing through reponse from interactive cdn, whilst setting longer cache time

--- applications/app/controllers/InteractiveController.scala
+++ applications/app/controllers/InteractiveController.scala
@@ -7,11 +7,13 @@ import conf._
 import conf.switches.Switches
 import model.Cached.RevalidatableResult
 import model._
-import play.api.libs.ws.WSClient
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.ws.{WSResponse, WSClient}
 import play.api.mvc._
 import views.support.RenderOtherStatus
 import conf.Configuration.interactive.cdnPath
 import conf.Configuration.environment.isPreview
+import scala.concurrent.duration._
 
 import scala.concurrent.Future
 
@@ -24,14 +26,26 @@ class InteractiveController(contentApiClient: ContentApiClient, wsClient: WSClie
   def renderInteractiveJson(path: String): Action[AnyContent] = renderInteractive(path)
   def renderInteractive(path: String): Action[AnyContent] = Action.async { implicit request => renderItem(path) }
 
-  def renderInteractiveServiceworker(path: String): Action[AnyContent] = Action.async { implicit request =>
+  def proxyInteractiveServiceworker(path: String): Action[AnyContent] = Action.async { implicit request =>
     val stage = if (isPreview) "preview" else "live"
     val serviceWorkerPath = s"$cdnPath/service-workers/$stage/$path/interactive-service-worker.js"
+    /*
+      This just passes through the http response from the above url so we don't need to
+      match status codes etc and setting the correct cache headers is handled by Cached()
+     */
+
     wsClient.url(serviceWorkerPath).get().map { response =>
-      Cached(60) {
-        RevalidatableResult(Ok(response.body).as("text/javascript; charset=utf8"), response.body)
+      Cached (7 days) {
+        RevalidatableResult(convertWSResponseToResult(response), response.body)
       }
     }
+  }
+
+  def convertWSResponseToResult(response: WSResponse): Result = {
+    val headers = response.allHeaders.map { case (header, body) =>
+      (header, body.head)
+    }
+    Result(ResponseHeader(response.status, headers), Enumerator(response.body.getBytes))
   }
 
   private def lookup(path: String)
