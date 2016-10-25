c3fa6447e7965d8daf21b5d51369b76d98e9383d
Kate Whalen <whalenka88@gmail.com>
2016-10-25 17:45:42 +0100

    Confirmation messages, user flow, padding and... vertical alignment

--- static/src/javascripts/bootstraps/enhanced/signup.js
+++ static/src/javascripts/bootstraps/enhanced/signup.js
@@ -11,26 +11,25 @@ define([
     config,
     Id
 ) {
-    function shoutOut(buttonEl, emailAddress) {
-        console.log('emailAddress = ' + emailAddress);
-        Id.getUserFromApi(function (userFromId) {
-            console.log(buttonEl.value);
-            console.log(userFromId);
-        });
-    }
 
-    function updateFormForLoggedIn(userFromId, el) {
-      if (userFromId && userFromId.primaryEmailAddress) {
+    function updateFormForLoggedIn(emailAddress, el) {
         fastdom.write(function () {
             $('.js-newsletter-card__text-input', el).addClass('is-hidden');
             $('.js-newsletter-signup-button', el).removeClass('newsletter-card__lozenge--submit');
             $('.js-newsletter-preview-button', el).removeClass('is-hidden');
-            $('.js-newsletter-card__text-input', el).val(userFromId.primaryEmailAddress);
+            $('.js-newsletter-card__text-input', el).val(emailAddress);
         });
-      }
     }
 
-    function validate(emailInput, emailAddress) {
+    // show subscribed state for signed in users
+    function updateEmailSubscriptions() {
+      var emailSubscriptions = function () {
+        return Id.getUserEmailSignUps()
+                  .then();
+      }();
+    }
+
+    function validate(emailAddress) {
         // simplistic email address validation
         return typeof emailAddress === 'string' && emailAddress.indexOf('@') > -1;
     }
@@ -40,47 +39,77 @@ define([
             buttonEl.disabled = true;
             $(buttonEl).addClass('is-updating lozenge--is-updating');
         });
+        updateButton(buttonEl);
+    }
+
+    function updateButton(buttonEl) {
+      fastdom.write(function () {
+          setTimeout(function () {
+              $(buttonEl).removeClass('is-updating lozenge--is-updating');
+              buttonEl.disabled = false;
+          }, 2000);
+          addSubscriptionMessage(buttonEl);
+      });
+    }
+
+    function addSubscriptionMessage(buttonEl) {
+      var $meta = $.ancestor(buttonEl, 'js-newsletter-meta');
+      $(buttonEl.form).addClass('is-hidden');
+      $('.signup-confirmation', $meta).removeClass('is-hidden');
     }
 
     function subscribeToEmail(buttonEl) {
         bean.on(buttonEl, 'click', function () {
-          var $form = buttonEl.form;
-          var emailInput = $('.js-newsletter-card__text-input', $form);
-          var emailAddress = emailInput.val();
-
-          if (validate(emailInput, emailAddress)) {
+          addUpdatingState(buttonEl);
+          if (validate()) {
             addUpdatingState(buttonEl);
-            //submitEmailSignup();
           }
-          shoutOut(buttonEl, emailAddress, listId);
-            return fetch(config.page.ajaxUrl + '/email', {
-                method: 'POST',
-                body: 'email=' + encodeURIComponent(emailAddress) +
-                        '&listId=' + listId,
-                headers: {
-                    'Accept': 'application/json'
-                }
-            })
-            .then(function (response) {
-                if (!response.ok) {
-                    throw new Error('Fetch error: ' + response.status + ' ' + response.statusText);
-                }
-            });
-            addUpdatingState(buttonEl);
         });
     }
 
-    function enhanceNewsletters(userFromId, el) {
-      Id.getUserFromApi(function (userFromId) {
-          updateFormForLoggedIn(userFromId);
-      });
-      $.forEachElement('.js-newsletter-signup-button', subscribeToEmail);
 
+          // var $form = buttonEl.form;
+          // var emailAddress = $(
+          //   '.js-newsletter-card__text-input', $form).val();
+          // var listId = $(
+          //   '.js-email-sub__listid-input', $form).val();
+          // if (Id.isUserLoggedIn()) {
+          //   Id.emailSignup(listId);
+          // }
+          //
+          // if (validate(emailAddress)) {
+          //   submitEmailSignup(emailAddress, listId);
+          //   addUpdatingState(buttonEl);
+          // }
+        // });
+
+    function submitEmailSignup(emailAddress, listId) {
+      return fetch(config.page.ajaxUrl + '/email', {
+          method: 'POST',
+          body: 'email=' + encodeURIComponent(emailAddress) +
+          '&listId=' + encodeURIComponent(listId)
+      })
+      .then(function (response) {
+          if (!response.ok) {
+              throw new Error('Fetch error: ' + response.status + ' ' + response.statusText);
+          }
+      });
+    }
+
+    function enhanceNewsletters(userFromId) {
+      if (userFromId && userFromId.primaryEmailAddress) {
+        Id.getUserFromApi(function (userFromId) {
+            updateFormForLoggedIn(userFromId.primaryEmailAddress);
+
+        });
+      }
+      $.forEachElement('.js-newsletter-signup-button', subscribeToEmail);
     }
 
     return {
         init: function () {
             enhanceNewsletters();
+            updateEmailSubscriptions();
         }
     };
 });
