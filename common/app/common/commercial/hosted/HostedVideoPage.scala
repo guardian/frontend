d1647e06c27683743d3b5f715f9311d5d12ce822
Lydia Shepherd <lps88@users.noreply.github.com>
2016-10-26 11:40:28 +0100

    Revert "Hosted onward journey AB test"

--- common/app/common/commercial/hosted/HostedVideoPage.scala
+++ common/app/common/commercial/hosted/HostedVideoPage.scala
@@ -58,6 +58,7 @@ object HostedVideoPage extends Logging {
         cta = HostedCallToAction.fromAtom(ctaAtom),
         socialShareText = content.fields.flatMap(_.socialShareText),
         shortSocialShareText = content.fields.flatMap(_.shortSocialShareText),
+        nextPage = HostedPages.nextPages(campaignName = campaignId, pageName = content.webUrl.split(campaignId + "/")(1)).headOption,
         nextVideo = HostedPages.nextPages(campaignName = campaignId, pageName = content.webUrl.split(campaignId + "/")(1), contentType = Some(HostedContentType.Video)).headOption,
         metadata = HostedMetadata.fromContent(content).copy(openGraphImages = video.posterUrl.toList)
       )
