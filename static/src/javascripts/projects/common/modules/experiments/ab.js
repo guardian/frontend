2b913dc23a52a78fb68f85536b4237245377e321
Lydia Shepherd <lps88@users.noreply.github.com>
2016-10-26 11:58:51 +0100

    Revert "Revert "Hosted onward journey AB test""

--- static/src/javascripts/projects/common/modules/experiments/ab.js
+++ static/src/javascripts/projects/common/modules/experiments/ab.js
@@ -8,6 +8,7 @@ define([
     'lodash/functions/memoize',
     'lodash/utilities/noop',
     'common/modules/experiments/tests/discussion-promote-bottom-banner',
+    'common/modules/experiments/tests/hosted-onward-journey',
     'common/modules/experiments/tests/weekend-reading-email',
     'common/modules/experiments/tests/weekend-reading-promo',
     'common/modules/experiments/tests/membership-engagement-warp-factor-one'
@@ -21,6 +22,7 @@ define([
     memoize,
     noop,
     DiscussionPromoteBottomBanner,
+    HostedOnwardJourney,
     WeekendReadingEmail,
     WeekendReadingPromo,
     MembershipEngagementWarpFactorOne
@@ -28,6 +30,7 @@ define([
 
     var TESTS = [
         new DiscussionPromoteBottomBanner(),
+        new HostedOnwardJourney(),
         new WeekendReadingEmail(),
         new WeekendReadingPromo(),
         new MembershipEngagementWarpFactorOne()
