04d48ed1d01f5d6fa1b30f3b45e1fdceb67c254a
Alex Sanders <a@snd.rs>
2016-10-28 11:35:40 +0100

    lets not do this for now

--- tools/__tasks__/compile/css/index.js
+++ tools/__tasks__/compile/css/index.js
@@ -1,22 +1,11 @@
 module.exports = {
     description: 'Compile CSS',
     task: [
-        {
-            description: 'Prepare CSS',
-            concurrent: true,
-            task: [
-                {
-                    description: 'Generate CSS',
-                    task: [
-                        require('./clean'),
-                        require('./mkdir'),
-                        require('../images'),
-                        require('./transpile-sass')
-                    ]
-                },
-                require('./update-caniuse')
-            ]
-        },
+        require('./clean'),
+        require('./mkdir'),
+        require('../images'),
+        require('./transpile-sass'),
+        require('./update-caniuse'),
         require('./atomise'),
         require('./remify'),
         require('./postcss')
