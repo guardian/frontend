ec2d2abe54e5174ff01132319c5653259298f26e
markjamesbutler <markjamesbutler@gmail.com>
2016-11-02 18:09:23 +0000

    First attempt at adding Google Analytics.

--- static/src/javascripts/projects/common/modules/video/youtube-tracking.js
+++ static/src/javascripts/projects/common/modules/video/youtube-tracking.js
@@ -1,21 +1,42 @@
 define([
     'common/utils/mediator',
-    'lodash/collections/forEach'
+    'lodash/collections/forEach',
+    'common/utils/config',
 ], function (
     mediator,
-    forEach
+    forEach,
+    config
 ) {
 
-    function track(property) {
-        mediator.emit(property);
-    }
-
     function initYoutubeEvents(videoId) {
+
+        var ga = window.ga,
+            gaTracker = config.googleAnalytics.trackers.editorial;
+
+        var events = {
+            metricMap: {
+                'play': 'metric1',
+                'skip': 'metric2',
+                '25': 'metric3',
+                '50': 'metric4',
+                '75': 'metric5',
+                'end': 'metric6'
+            },
+            baseEventObject: {
+                eventCategory: 'Media',
+                eventAction: 'video content',
+                eventLabel: window.location.pathname,
+                dimension19: self.trackingId,
+                dimension20: 'guardian-youtube'
+            }
+        };
+
         var eventList = ['play', '25', '50', '75', 'end'];
 
         forEach(eventList, function(event) {
             mediator.once(event, function() {
                 ophanRecord(event);
+                ga(gaTracker + '.send', 'event', buildGoogleAnalyticsEvent(event, events, window.location.pathname, videoId));
             });
         });
 
@@ -30,10 +51,52 @@ define([
                 ophan.record(eventObject);
             });
         }
+
+
+        function buildGoogleAnalyticsEvent(event, metrics, canonicalUrl, videoId) {
+            var category = 'Media';
+            var playerName = 'gu-video-youtube';
+            var action = 'video content'
+            var fieldsObject = {
+                eventCategory: category,
+                eventAction: action,
+                eventLabel: canonicalUrl,
+                dimension19: videoId,
+                dimension20: playerName
+            };
+            // Increment the appropriate metric based on the event type
+            var metricId = metrics[event];
+            if (metricId) {
+                fieldsObject[metricId] = 1;
+            }
+            return fieldsObject;
+        }
+
+        /**
+         *
+         * @param mediaId {string}
+         * @param mediaType {string} audio|video
+         * @param eventType {string} e.g. firstplay, firstend
+         * @param isPreroll {boolean}
+         * @returns {{mediaId: string, mediaType: string, eventType: string, isPreroll: boolean}}
+         */
+        function MediaEvent(mediaId, mediaType, eventType, isPreroll) {
+            return {
+                mediaId: mediaId,
+                mediaType: mediaType,
+                eventType: eventType,
+                isPreroll: isPreroll
+            };
+        }
+
     }
 
     function init(videoId) {
         initYoutubeEvents(videoId);
+    }
+
+    function track(property) {
+        mediator.emit(property);
     }
 
     return {
