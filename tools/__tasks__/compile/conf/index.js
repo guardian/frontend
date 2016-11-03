71d2d039c95e57987e4ff65041dc9461a3f2e443
Alex Sanders <a@snd.rs>
2016-11-03 11:41:57 +0000

    clean conf at the start

--- tools/__tasks__/compile/conf/index.js
+++ tools/__tasks__/compile/conf/index.js
@@ -1,7 +1,6 @@
 module.exports = {
     description: 'Compile assets for template rendering in Play',
     task: [
-        require('./clean'),
         require('./copy'),
         require('../inline-svgs'),
         require('./minify')
