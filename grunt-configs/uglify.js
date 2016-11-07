279ca6e149864ce536455e41249924126f52db7f
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2016-11-07 16:29:20 +0000

    Remove Omniture

--- grunt-configs/uglify.js
+++ grunt-configs/uglify.js
@@ -21,31 +21,7 @@ module.exports = function (grunt, options) {
                     dest: options.staticTargetDir + 'javascripts'
                 }
             ]
-        },
-        conf: {
-            files: [{
-                expand: true,
-                cwd: 'static/src/javascripts',
-                src: [
-                    'vendor/omniture/omniture.js'
-                ],
-                dest: 'common/conf/assets'
-            },
-            {
-                expand: true,
-                cwd: 'static/src/javascripts/',
-                src: [
-                    'projects/common/modules/analytics/analytics.js'
-                ],
-                dest: 'common/conf/assets'
-            }
-            ],
-            options: options.isDev ? {} : {
-                compress: {
-                    // Set to false retain constant expressions, used to avoid writing HTML like </script>.
-                    evaluate: false
-                }
-            }
+
         }
     };
 };
