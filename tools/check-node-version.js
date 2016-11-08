8d0372f2c6438d015bf35a13e724043196f4ce8f
Alex Sanders <a@snd.rs>
2016-11-08 15:55:42 +0000

    allowing logging here

--- tools/check-node-version.js
+++ tools/check-node-version.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-console */
+
 // given this could be any version of node that runs this, it uses no bells or whistles...
 
 var fs = require('fs');
@@ -9,8 +11,8 @@ var currentVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
 var minVersion = Number(fs.readFileSync(path.join(__dirname, '../', '.nvmrc'), 'utf8'));
 
 if (currentVersion < minVersion) {
-    console.log("You are using Node v" + currentVersion + ".\n" +
-        "Frontend requires Node v" + minVersion + " or later.\n" +
-        "If you're using NVM, you can 'nvm use'...");
+    console.log('You are using Node v' + currentVersion + '.\n' +
+        'Frontend requires Node v' + minVersion + ' or later.\n' +
+        'If you\'re using NVM, you can \'nvm use\'...');
     process.exit(1);
 }
