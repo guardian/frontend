3e39042655349ff0dcd7c08182d6b97f40c64de1
Alex Sanders <a@snd.rs>
2016-11-08 15:10:27 +0000

    dont’ autofix js lint

--- tools/__tasks__/lint/javascript.js
+++ tools/__tasks__/lint/javascript.js
@@ -2,10 +2,10 @@ module.exports = {
     description: 'Lint JS',
     task: [{
         description: 'Lint tests',
-        task: 'eslint static/test/javascripts/**/*.js --fix --ignore-path static/test/javascripts/.eslintignore --rulesdir dev/eslint-rules --quiet --color'
+        task: 'eslint static/test/javascripts/**/*.js --ignore-path static/test/javascripts/.eslintignore --rulesdir dev/eslint-rules --quiet --color'
     },{
         description: 'Lint app JS',
-        task: 'eslint static/src/**/*.js --fix --ignore-path static/src/.eslintignore --rulesdir dev/eslint-rules --quiet --color'
+        task: 'eslint static/src/**/*.js --ignore-path static/src/.eslintignore --rulesdir dev/eslint-rules --quiet --color'
     }],
     concurrent: true
 };
