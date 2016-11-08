d58b031b29e407e227cef8fe4b0422b79fb21514
Alex Sanders <a@snd.rs>
2016-11-08 13:50:09 +0000

    ignore errors

--- dev/check-yarn.js
+++ dev/check-yarn.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-console */
+
 const childProcess = require('child_process');
 
 // given we could have no local app-wide node_modules yet,
@@ -9,8 +11,10 @@ const semver = require('semver');
 
 const requiredVersion = '>=0.16.0';
 
-childProcess.exec('yarn --version', (error, actualVersion, stderr) => {
-    if (error || stderr || !semver.satisfies(actualVersion, requiredVersion)) {
+childProcess.exec('yarn --version', (e, actualVersion) => {
+    // we're assuming all errors mean it's not installed
+    // so they're not handled
+    if (!semver.satisfies(actualVersion, requiredVersion)) {
         if (actualVersion) {
             console.log(`Updating yarn to ${requiredVersion}...`);
         } else {
