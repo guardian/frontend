f5709f342b4047a59dd66de1c9c0aa3ad81a8558
Simon Adcock <simon.adcock@guardian.co.uk>
2016-11-09 16:28:23 +0000

    concise task call, uglify bundle

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         filename: 'app-webpack.js'
     },
     plugins: [
-        //new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.DedupePlugin()
     ]
 };
