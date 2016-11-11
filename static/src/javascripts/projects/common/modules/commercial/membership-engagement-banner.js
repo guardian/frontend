1e7bdd4ff093ac90316a56a64f60ac9b56e152f7
rupertbates <rupert.bates@guardian.co.uk>
2016-11-11 10:25:13 +0000

    Use apostrophes instead of single quotes throughout the message text

--- static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
@@ -102,7 +102,7 @@ define([
         if (messagingTestVariant && messagingTestVariant !== 'notintest') {
             var variantMessages = {
                 Get_round_to: 'Not got round to supporting us yet? Now is the time. Give £5 a month today',
-                Give_upfront: 'Give £5 a month to help support our journalism and make the Guardian\'s future more secure',
+                Give_upfront: 'Give £5 a month to help support our journalism and make the Guardian’s future more secure',
                 Together_informed: 'Together we can keep the world informed. Give £5 a month to support our journalism',
                 Coffee_5: 'For less than the price of a coffee a week, you could help secure the Guardian’s future. Support our journalism for £5 a month'
             };
