d2857c716900939b0f07304d30b15002f938a395
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-11-14 14:13:10 +0000

    Include section sponsorships in pressed content

--- common/app/model/Section.scala
+++ common/app/model/Section.scala
@@ -4,7 +4,7 @@ import campaigns.PersonalInvestmentsCampaign
 import com.gu.contentapi.client.model.v1.{Section => ApiSection}
 import common.commercial.{BrandHunter, Branding}
 import common.{Edition, Pagination}
-import play.api.libs.json.{JsBoolean, JsString, JsValue}
+import play.api.libs.json.{JsBoolean, JsString, JsValue, Json}
 
 object Section {
   def make(section: ApiSection, pagination: Option[Pagination] = None): Section = {
@@ -64,6 +64,8 @@ case class SectionSummary(
 )
 
 object SectionSummary {
+
+  implicit val jsonFormat = Json.format[SectionSummary]
 
   def fromCapiSection(section: ApiSection): SectionSummary = {
     SectionSummary(
