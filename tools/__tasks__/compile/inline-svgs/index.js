77f504eaf9f3517c3e33d6a2c9876d0bf620116d
Alex Sanders <a@snd.rs>
2016-11-16 15:57:41 +0000

    pify

--- tools/__tasks__/compile/inline-svgs/index.js
+++ tools/__tasks__/compile/inline-svgs/index.js
@@ -4,35 +4,30 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const glob = require('glob');
 const SVGO = require('svgo');
+
+const pify = require('pify');
+const readFile = pify(fs.readFile);
+const writeFile = pify(fs.writeFile);
+const mkdirpp = pify(mkdirp);
+
+const {src, conf} = require('../../config').paths;
+
 const svgo = new SVGO({
     plugins: [{
         removeXMLNS: true
     }]
 });
-
-const {src, conf} = require('../../config').paths;
-
 const srcDir = path.resolve(src, 'inline-svgs');
 
-const optimiseAndSave = svgPath => new Promise((resolve, reject) => {
-    const svg = fs.readFileSync(path.resolve(srcDir, svgPath), 'utf-8');
-    const dest = path.resolve(conf, 'inline-svgs', svgPath);
-
-    svgo.optimize(svg, result => {
-        mkdirp.sync(path.dirname(dest));
-        fs.writeFile(dest, result.data, err => {
-            if (err) reject(err);
-            resolve();
-        });
-    });
-});
-
 module.exports = {
-    description: 'Compile SVGs for inlining',
-    task: () => {
-        const optimised = glob.sync('**/*.svg', {
-            cwd: srcDir
-        }).map(optimiseAndSave);
-        return Promise.all(optimised);
-    }
+    description: 'Prepare inline SVGs',
+    task: () => Promise.all(
+        glob.sync('**/*.svg', {cwd: srcDir}).map(svgPath => {
+            const dest = path.resolve(conf, 'inline-svgs', svgPath);
+            return mkdirpp(path.dirname(dest))
+                .then(() => readFile(path.resolve(srcDir, svgPath), 'utf-8'))
+                .then(fileData => new Promise(resolve => svgo.optimize(fileData, resolve)))
+                .then(optimisedFileData => writeFile(dest, optimisedFileData.data));
+        })
+    )
 };
