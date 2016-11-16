c8b9d25ba9c8057248db74448683ba1709ef81cd
Alex Sanders <a@snd.rs>
2016-11-16 13:20:31 +0000

    optimise svgs in one step

--- tools/__tasks__/compile/inline-svgs/index.js
+++ tools/__tasks__/compile/inline-svgs/index.js
@@ -1,7 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+
+const mkdirp = require('mkdirp');
+const glob = require('glob');
+const SVGO = require('svgo');
+const svgo = new SVGO({
+    plugins: [{
+        removeXMLNS: true
+    }]
+});
+
+const {src, conf} = require('../../config').paths;
+
+const srcDir = path.resolve(src, 'inline-svgs');
+
+const optimiseAndSave = svgPath => new Promise((resolve, reject) => {
+    const svg = fs.readFileSync(path.resolve(srcDir, svgPath), 'utf-8');
+    const dest = path.resolve(conf, 'inline-svgs', svgPath);
+
+    svgo.optimize(svg, result => {
+        mkdirp.sync(path.dirname(dest));
+        fs.writeFile(dest, result.data, err => {
+            if (err) reject(err);
+            resolve();
+        });
+    });
+});
+
 module.exports = {
-    description: 'Copy/minify SVGs',
-    task: [
-        require('./copy'),
-        require('./minify')
-    ]
+    description: 'Compile SVGs for inlining',
+    task: () => {
+        const optimised = glob.sync('**/*.svg', {
+            cwd: srcDir
+        }).map(optimiseAndSave);
+        return Promise.all(optimised);
+    }
 };
