ef9ea76ba0a59e92a97e65c349734bc5eb95ad19
Alex Sanders <a@snd.rs>
2016-11-17 11:43:08 +0000

    explicity resolve only if successful

--- tools/__tasks__/compile/images/icons.js
+++ tools/__tasks__/compile/images/icons.js
@@ -15,13 +15,16 @@ const svgo = new SVGO();
 function getSVG (iconPath) {
     return new Promise((resolve, reject) => {
         fs.readFile(iconPath, { encoding: 'utf-8' }, (err, data) => {
-            if (err) { reject(err); }
-            svgo.optimize(data, result => {
-                resolve({
-                    name: path.parse(iconPath).name,
-                    data: result
+            if (err) {
+                reject(err);
+            } else {
+                svgo.optimize(data, result => {
+                    resolve({
+                        name: path.parse(iconPath).name,
+                        data: result
+                    });
                 });
-            });
+            }
         });
     });
 }
@@ -67,8 +70,11 @@ function saveSass (sass, dest, fileName) {
                     ${sass}
                 }
             `.trim().replace(/ {16}/g, ''), err => {
-                if (err) reject(err);
-                resolve();
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
             }
         );
     });
