c46185b1066dc3bfd06de679cfbd400f57ddccd1
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-11-22 17:44:05 +0000

    Remove ophan logging

--- static/src/javascripts/projects/commercial/modules/dfp/performance-logging.js
+++ static/src/javascripts/projects/commercial/modules/dfp/performance-logging.js
@@ -2,14 +2,12 @@ define([
     'common/utils/raven',
     'common/utils/config',
     'common/utils/user-timing',
-    'common/modules/analytics/beacon',
-    'commercial/modules/dfp/get-advert-by-id'
+    'common/modules/analytics/beacon'
 ], function (
     raven,
     config,
     userTiming,
-    beacon,
-    getAdvertById
+    beacon
 ) {
 
     var performanceLog = {
@@ -19,104 +17,11 @@ define([
             adverts: [],
             baselines: []
         };
-    var initial = new Date().getTime(); // For backwards compatibility below; whilst we still use the old ophan format.
     var primaryBaseline = 'primary';
     var secondaryBaseline = 'secondary';
 
-    function trackPerformance(googletag) {
-
-        var adTimings = {};
-        spyOnAdDebugTimings();
-        reportAdsAndTimingsOnRender();
-
-        function spyOnAdDebugTimings() {
-            // Spy on undocumented GPT debugging API and store granular ad timing data in 'adTimings' map
-
-            if (!googletag.debug_log || !googletag.debug_log.log) {
-                return;
-            }
-
-            var originalDebugger = googletag.debug_log.log,
-                lifecycleIdToTimingAttr = {
-                    3: 'fetch',
-                    4: 'receive',
-                    6: 'render'
-                },
-                lifecycleIdToAdvertTiming = {
-                    3: 'dfpFetching',
-                    4: 'dfpReceived',
-                    6: 'dfpRendered'
-                };
-
-            googletag.debug_log.log = function interceptedGptDebugger(level, message, service, slot) {
-                var lifecycleId = message.getMessageId(), slotId, timingAttr;
-
-                if (lifecycleId && slot) {
-                    slotId = slot.getSlotElementId();
-                    adTimings[slotId] = adTimings[slotId] || {};
-                    timingAttr = lifecycleIdToTimingAttr[lifecycleId];
-                    adTimings[slotId][timingAttr] = new Date().getTime();
-
-                    var advert = getAdvertById(slotId);
-                    var timing = lifecycleIdToAdvertTiming[lifecycleId];
-                    if (advert && timing in advert.timings) {
-                        advert.timings[timing] = userTiming.getCurrentTime();
-                    }
-                }
-
-                // Call original debugger as normal
-                return originalDebugger.apply(this, arguments);
-            };
-        }
-
-        function reportAdsAndTimingsOnRender() {
-            // We do not know if and when the debugger will be called, so we cannot wait for its timing
-            // data before sending Ophan calls
-
-            googletag.pubads().addEventListener('slotRenderEnded', raven.wrap(function reportAdToOphan(event) {
-                require(['ophan/ng'], function (ophan) {
-
-                    var slotId = event.slot.getSlotElementId(),
-                        slotTiming = adTimings[slotId] || {};
-
-                    function lineItemIdOrEmpty(event) {
-                        if (event.isEmpty) {
-                            return '__empty__';
-                        } else {
-                            return event.lineItemId;
-                        }
-                    }
-
-                    ophan.record({
-                        ads: [{
-                            slot: event.slot.getSlotElementId(),
-                            campaignId: lineItemIdOrEmpty(event),
-                            creativeId: event.creativeId,
-                            timeToRenderEnded: safeDiff(initial, new Date().getTime()),
-
-                            // overall time to make an ad request
-                            timeToAdRequest: safeDiff(initial, slotTiming.fetch),
-
-                            // delay between requesting and receiving an ad
-                            adRetrievalTime: safeDiff(slotTiming.fetch, slotTiming.receive),
-
-                            // delay between receiving and rendering an ad
-                            adRenderTime: safeDiff(slotTiming.receive, slotTiming.render),
-
-                            adServer: 'DFP'
-                        }]
-                    });
-
-                    reportTrackingData();
-                });
-            }));
-
-            function safeDiff(first, last) {
-                if (first && last) {
-                    return last - first;
-                }
-            }
-        }
+    function init(googletag) {
+        googletag.pubads().addEventListener('slotRenderEnded', raven.wrap(reportTrackingData));
     }
 
     // moduleCheckpoint() is called when a module has finished execution.
@@ -213,7 +118,7 @@ define([
     }
 
     return {
-        trackPerformance : trackPerformance,
+        init : init,
         moduleCheckpoint : moduleCheckpoint,
         moduleStart: moduleStart,
         moduleEnd: moduleEnd,
