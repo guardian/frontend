c00a3d0f272769851286332469243f5fd3eb834d
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-11-22 14:53:40 +0000

    Add support for amp validation in commercial test suite

--- commercial/test/test/CommercialAmpValidityTest.scala
+++ commercial/test/test/CommercialAmpValidityTest.scala
@@ -1,11 +1,24 @@
 package test
 
+import commercial.controllers.HostedContentController
 import org.scalatest.DoNotDiscover
+import play.api.mvc._
+import play.api.test.Helpers._
+import play.api.test._
 
+@DoNotDiscover class CommercialAmpValidityTest extends AmpValidityTest with WithTestContentApiClient {
 
-@DoNotDiscover class CommercialAmpValidityTest extends AmpValidityTest {
+  override protected def getContentString[T](path: String)(block: (String) => T): T = {
+    val controller = new HostedContentController(testContentApiClient)
+    val pathParts = path.split('?').head.split('/').drop(1)
+    val campaign = pathParts.head
+    val page = pathParts.last
+    val request = FakeRequest("GET", s"$path=1", FakeHeaders(), AnyContentAsEmpty)
+    val result = controller.renderHostedPage(campaign, page).apply(request)
+    val bodyText = contentAsString(result)
+    block(bodyText)
+  }
 
-  Seq(
-    "/advertiser-content/audi-history-of-audi/audi-and-innovation" // Hosted article page
-  ).foreach(testAmpPageValidity)
+  val hostedArticleId = "advertiser-content/audi-history-of-audi/audi-and-innovation"
+  testAmpPageValidity(hostedArticleId)
 }
