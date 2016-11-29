1327a0601925439054ac06aca6e6e1d6dbeb3abc
Alex Sanders <a@snd.rs>
2016-11-29 10:06:30 +0000

    clarity

--- tools/check-yarn.js
+++ tools/check-yarn.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-console */
-
 // We use yarn to install our deps from npm.
 
 // We want to ensure a specific minimum version of yarn is installed.
 const requiredVersion = '~0.17.0';
+
+const childProcess = require('child_process');
 
 // The easiest way to do this cross-platform is to use the semver npm package.
 // We can't guarantee it has been installed before running this script,
 // e.g. if it's the first run.
 // So if semver has not been installed already, quickly install it locally.
 // It's specified as a depenency, so this will almost never be a problem.
-const childProcess = require('child_process');
-
 new Promise(resolve => {
     try {
         require.resolve('semver');
@@ -27,6 +25,7 @@ new Promise(resolve => {
         });
     }
 }).then(() => {
+    // we know we have semver installed
     childProcess.exec('yarn --version', (e, actualVersion) => {
         const semver = require('semver');
         if (!semver.satisfies(actualVersion, requiredVersion)) {
