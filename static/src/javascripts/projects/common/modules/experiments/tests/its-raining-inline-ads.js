e6915c2cdcaab1477466559d9cfc05b99c41711f
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-12-05 10:26:13 +0000

    Describe each variant

--- static/src/javascripts/projects/common/modules/experiments/tests/its-raining-inline-ads.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/its-raining-inline-ads.js
@@ -31,16 +31,20 @@ define([
                 test: function () {},
                 success: success.bind(this)
             },
+            // In this variant, we leave the geo most popular component there
+            // and offset ads in the right-hand column
             {
                 id: 'geo',
                 test: function () {},
                 success: success.bind(this)
             },
+            // Here, the geo most pop is removed and ads are offset to the right
             {
                 id: 'nogeo',
                 test: function () {},
                 success: success.bind(this)
             },
+            // Here, the geo most pop is removed and ads remain inline
             {
                 id: 'none',
                 test: function () {},
