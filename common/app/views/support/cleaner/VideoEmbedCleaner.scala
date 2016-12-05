ec725414b1bde975c92fed471c67342bf44ce68e
Zef <zef.cherry-kynaston.casual@theguardian.com>
2016-12-05 11:33:01 +0000

    Moved video caption icon to correct location

--- common/app/views/support/cleaner/VideoEmbedCleaner.scala
+++ common/app/views/support/cleaner/VideoEmbedCleaner.scala
@@ -28,9 +28,6 @@ case class VideoEmbedCleaner(article: Article) extends HtmlCleaner {
         val figcaption = element.getElementsByTag("figcaption")
         if (figcaption.length < 1) {
           element.addClass("fig--no-caption")
-        } else {
-            val informationIcon = views.html.fragments.inlineSvg("information", "icon", List("centered-icon", "rounded-icon")).toString()
-            figcaption.prepend(informationIcon)
         }
       }
     })
@@ -43,6 +40,7 @@ case class VideoEmbedCleaner(article: Article) extends HtmlCleaner {
 
     document.getElementsByClass("element-video").foreach { figure: Element =>
       val canonicalUrl = figure.attr("data-canonical-url")
+      val figcaption = figure.getElementsByTag("figcaption")
 
       figure.attr("data-component", "video-inbody-embed")
       figure.getElementsByClass("gu-video").foreach { element: Element =>
@@ -54,6 +52,11 @@ case class VideoEmbedCleaner(article: Article) extends HtmlCleaner {
 
         if (! canonicalUrl.isEmpty) {
           element.attr("data-canonical-url", new URL(canonicalUrl).getPath.stripPrefix("/"))
+        }
+
+        if (figcaption.nonEmpty) {
+            val informationIcon = views.html.fragments.inlineSvg("information", "icon", List("centered-icon", "rounded-icon")).toString()
+            figcaption.prepend(informationIcon)
         }
 
         val mediaId = element.attr("data-media-id")
