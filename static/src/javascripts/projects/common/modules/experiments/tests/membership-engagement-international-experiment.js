9d53c2b273e0d0c7a067f1c1ac2329e660292d80
rupertbates <rupert.bates@guardian.co.uk>
2016-12-05 10:17:17 +0000

    Extend the membership international engagement banner test

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-international-experiment.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-international-experiment.js
@@ -20,7 +20,7 @@ define([
     return function () {
         this.id = 'MembershipEngagementInternationalExperiment';
         this.start = '2016-11-10';
-        this.expiry = '2016-12-1';
+        this.expiry = '2017-1-15';
         this.author = 'Rupert Bates';
         this.description = 'Test varying the number of visits before showing the membership engagement banner';
         this.audience = 1;    // 100% (of International audience)
