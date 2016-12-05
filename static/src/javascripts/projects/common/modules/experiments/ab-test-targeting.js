25deb7234f6cc4d36e8df0516d70d6d0bc4e35d5
Sam Desborough <sam.desborough@gmail.com>
2016-12-05 16:55:09 +0000

    Add module to check an A/B test against the targeting tool
    The contributions team is planning to use the [targeting
    tool](https://github.com/guardian/targeting) to manage the tags used in
    Epic messages, so we can control where it shows up without having to
    make any code changes. To help with this I've added a small module that
    checks the campaigns in `guardian.config` (populated by the targeting
    tool) against a given A/B test, which will give us a value to use in the
    test's `canRun` criteria.

---
+++ static/src/javascripts/projects/common/modules/experiments/ab-test-targeting.js
@@ -0,0 +1,39 @@
+define([
+    'common/utils/config',
+], function(
+    config
+) {
+    function clean(string) {
+        return string.trim().toLowerCase();
+    }
+
+    /**
+     * Get the campaigns for a test.
+     *
+     * @param test      an A/B test object (with an 'id' property)
+     * @return {Array}  campaigns associated with this test
+     */
+    function campaignsFor(test) {
+        try {
+            return config.page.campaigns.filter(function (campaign) {
+                return campaign.fields && clean(campaign.fields.campaignId) === clean(test.id);
+            });
+        } catch (e) {
+            return [];
+        }
+    }
+
+    /**
+     * Check if any of the active campaigns (from the targeting tool) apply to a given A/B test.
+     *
+     * @param test        an A/B test object (with an 'id' property)
+     * @return {Boolean}
+     */
+    function isAbTestTargeted(test) {
+        return campaignsFor(test).length > 0;
+    }
+
+    return {
+        isAbTestTargeted: isAbTestTargeted
+    };
+});
