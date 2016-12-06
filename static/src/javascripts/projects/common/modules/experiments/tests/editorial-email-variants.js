864c22989ead2d68e87e36bb9f288ab05111adf1
Kate Whalen <whalenka88@gmail.com>
2016-12-06 16:26:07 +0000

    Update the email listID and make the iframe takeover speeder

--- static/src/javascripts/projects/common/modules/experiments/tests/editorial-email-variants.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/editorial-email-variants.js
@@ -27,55 +27,44 @@ define([
         this.idealOutcome = 'Similar quantity of users in each list in ExactTarget';
 
         this.canRun = function () {
-            return (config.page.seriesId.toLowerCase() === 'info/series/newsletter-sign-ups');
+            return (config.page.webTitle.toLowerCase() === 'sign up for weekend reading');
         };
 
-        function passRefererParam(emailForm) {
-            var referrerInput = qwery('.js-email-sub__referrer-input')[0];
-            var referrer = window.location.search.replace('?', '');
-            if (referrer) {
-                emailForm.setAttribute('data-link-name', 'referrer-' + referrer);
-                referrerInput.setAttribute('value', referrer);
-            }
-        }
-
-        // use this if we are using the form, form will still work with no-JS
-        function updateSignupForm(emailListID) {
-            var emailListInput = qwery('.js-email-sub__listid-input')[0];
-            var emailForm = qwery('.js-email-sub__form')[0];
-            emailForm.setAttribute('data-email-list-id', emailListID);
-            emailListInput.setAttribute('value', emailListID);
-            passRefererParam(emailForm);
+        function updateExampleUrl(exampleUrl) {
+          return fastdomPromise.write(function () {
+            var example = $('.js-email-example')[0];
+            example.setAttribute('href', exampleUrl);
+          });
         }
 
         // loads the correct iframe if we can...
         function enhanceWebView(emailListID) {
-          return fastdomPromise.write(function () {
-              var insertionPoint = $('.js-email-sub__form')[0];
-              $(insertionPoint).addClass('is-hidden');
-              var emailForm = $.create(
-                    '<iframe src="/email/form/plaintone/' + emailListID +
-                    '" height="60px" width="100%" scrolling="no" frameborder="0" seamless' + 'class="iframed--overflow-hidden js-email-sub__iframe js-email-sub__iframe--article">' + '</iframe>'
-                  );
-              $(emailForm).insertAfter(insertionPoint);
-          });
+          var insertionPoint = $('.js-email-sub__iframe')[0];
+          $(insertionPoint).addClass('is-hidden');
+          var emailForm = $.create(
+                '<iframe src="/email/form/plaintone/' + emailListID +
+                '" height="60px" width="100%" scrolling="no" frameborder="0" seamless' + 'class="iframed--overflow-hidden js-email-sub__iframe js-email-sub__iframe--article">' + '</iframe>'
+              );
+          $(emailForm).insertAfter(insertionPoint);
         }
 
         this.variants = [
             {
-                id: 'card',
+                id: 'The-Flyer-Cards',
                 test: function () {
-                    var emailListID = 3743;
-                    updateSignupForm(emailListID);
+                    var emailListID = 3806;
+                    var exampleUrl = 'https://www.theguardian.com/email/the-flyer?format=email';
                     enhanceWebView(emailListID);
+                    updateExampleUrl(exampleUrl);
                 }
             },
             {
-                id: 'connected',
+                id: 'The-Flyer-Connected',
                 test: function () {
-                    var emailListID = 3744;
-                    updateSignupForm(emailListID);
+                    var emailListID = 3807;
+                    var exampleUrl = 'https://www.theguardian.com/email/the-flyer?format=email-connected';
                     enhanceWebView(emailListID);
+                    updateExampleUrl(exampleUrl);
                 }
             }
         ];
