fcb32b7e2a182841351d5add022a0c45710a1426
Diego Martin Vazquez Nanini <diego@vazqueznanini.com>
2016-12-12 18:21:38 +0000

    Just use the default play injector

--- onward/app/feed/MostPopularSocialAutoRefresh.scala
+++ onward/app/feed/MostPopularSocialAutoRefresh.scala
@@ -4,7 +4,6 @@ import akka.actor.ActorSystem
 import app.LifecycleComponent
 import common.AutoRefresh
 import play.api.inject.ApplicationLifecycle
-import play.api.{Application, GlobalSettings}
 import services.{MostReadItem, OphanApi}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
