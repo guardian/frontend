77c2a418c66e5a699a18f31cf9393fa1ed639894
guy.dawson <guy.dawson@theguardian.com>
2016-12-12 17:22:32 +0000

    bolder variant removed; date of us-pre-end-of-year test extended

--- static/src/javascripts/projects/common/modules/experiments/ab-test-clash.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-test-clash.js
@@ -24,7 +24,7 @@ define([
 
     var ContributionsEpicUsPreEndOfYear = {
         name: 'ContributionsEpicUsPreEndOfYear',
-        variants: ['control', 'bolder', 'endOfYear']
+        variants: ['control', 'endOfYear']
     };
 
     var ContributionsEpicAlwaysAskStrategy = {
