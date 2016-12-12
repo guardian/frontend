009faced2fd6b24f34dfca55a258f70ea176cd5f
Alex Sanders <a@snd.rs>
2016-12-12 15:06:35 +0000

    lint screen sshots

--- tools/__tasks__/screenshot/check-network.js
+++ tools/__tasks__/screenshot/check-network.js
@@ -1,13 +1,13 @@
 const tcpp = require('tcp-ping');
 const pify = require('pify');
 
-const {port, domain} = require('./config');
+const { port, domain } = require('./config');
 
 module.exports = {
     description: `Probing ${domain} on port ${port}...`,
-    task: () => pify(tcpp.probe, {multiArgs: true})(domain, port).then((result) => {
+    task: () => pify(tcpp.probe, { multiArgs: true })(domain, port).then((result) => {
         if (!result[0]) {
             throw new Error(`Cannot reach ${domain}:${port} - is your server running?`);
         }
-    })
+    }),
 };
