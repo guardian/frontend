0011e85d4bce43855830af1103125009879f3dbd
Alex Sanders <a@snd.rs>
2016-12-12 14:28:28 +0000

    lint deploy radiator

--- static/src/deploys-radiator/app/model-helpers.js
+++ static/src/deploys-radiator/app/model-helpers.js
@@ -1,7 +1,7 @@
 // A started deploy is all except status "Not running"
 // This is because a deploy is not transactional, so we can't
 // be sure if the box has updated or not once the job has started.
-export const hasDeployStarted = (deploy) => deploy.status !== 'Not running';
+export const hasDeployStarted = deploy => deploy.status !== 'Not running';
 export const getStartedDeploysFor = (projectName, deploys) => (deploys
     .filter(hasDeployStarted)
     .filter(deploy => deploy.projectName === projectName)
@@ -10,4 +10,4 @@ export const isDeployMostRecent = (deploy, deploys) => {
     const mostRecent = getStartedDeploysFor(deploy.projectName, deploys).last();
     return mostRecent ? (deploy.uuid === mostRecent.uuid) : false;
 };
-export const getMostRecentDeploys = (deploys) => (deploys.filter(deploy => isDeployMostRecent(deploy, deploys)).toList());
+export const getMostRecentDeploys = deploys => (deploys.filter(deploy => isDeployMostRecent(deploy, deploys)).toList());
