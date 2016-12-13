12869091b19ca6465ef449461b6848f7d7dff555
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-12-13 16:24:23 +0000

    Remove throttle call in enableLazyLoad

--- static/src/javascripts/projects/commercial/modules/dfp/lazy-load.js
+++ static/src/javascripts/projects/commercial/modules/dfp/lazy-load.js
@@ -1,6 +1,5 @@
 define([
     'fastdom',
-    'lodash/functions/throttle',
     'common/utils/config',
     'common/utils/detect',
     'common/utils/user-timing',
@@ -8,56 +7,39 @@ define([
     'commercial/modules/dfp/load-advert',
     'commercial/modules/dfp/performance-logging',
     'commercial/modules/dfp/get-advert-by-id'
-], function (fastdom, throttle, config, detect, userTiming, dfpEnv, loadAdvert, performanceLogging, getAdvertById) {
-    /* nbOfFrames: integer. Number of refresh frames we want to throttle the scroll handler */
-    var nbOfFrames = 6;
-
-    /* durationOfFrame: integer. Number of miliseconds a refresh frame typically lasts */
-    var durationOfFrame = 16;
-
+], function (fastdom, config, detect, userTiming, dfpEnv, loadAdvert, performanceLogging, getAdvertById) {
     /* depthOfScreen: double. Top and bottom margin of the visual viewport to check for the presence of an advert */
     var depthOfScreen = 1.5;
 
-    /* loadQueued: boolean. Set to true when a lazyload task is scheduled */
-    var loadQueued = false;
-
-    var lazyLoad = dfpEnv.lazyLoadObserve ? onIntersect : throttle(onScroll, nbOfFrames * durationOfFrame);
+    var lazyLoad = dfpEnv.lazyLoadObserve ? onIntersect : onScroll;
 
     return lazyLoad;
 
     function onScroll() {
         var viewportHeight = detect.getViewport().height;
 
-        if( loadQueued ) {
-            return;
+        var lazyLoad = dfpEnv.advertsToLoad
+        .filter(function (advert) {
+            var rect = advert.node.getBoundingClientRect();
+            var isNotHidden = rect.top + rect.left + rect.right + rect.bottom !== 0;
+            var isNotTooFarFromTop = (1 - depthOfScreen) * viewportHeight < rect.bottom;
+            var isNotTooFarFromBottom = rect.top < viewportHeight * depthOfScreen;
+            // load the ad only if it's setting within an acceptable range
+            return isNotHidden && isNotTooFarFromTop && isNotTooFarFromBottom;
+        })
+        .map(function (advert) {
+            return advert.id;
+        });
+
+        dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function (advert) {
+            return lazyLoad.indexOf(advert.id) < 0;
+        });
+
+        if (dfpEnv.advertsToLoad.length === 0) {
+            disableLazyLoad();
         }
 
-        loadQueued = true;
-        fastdom.read(function () {
-            loadQueued = false;
-            var lazyLoad = dfpEnv.advertsToLoad
-            .filter(function (advert) {
-                var rect = advert.node.getBoundingClientRect();
-                var isNotHidden = rect.top + rect.left + rect.right + rect.bottom !== 0;
-                var isNotTooFarFromTop = (1 - depthOfScreen) * viewportHeight < rect.bottom;
-                var isNotTooFarFromBottom = rect.top < viewportHeight * depthOfScreen;
-                // load the ad only if it's setting within an acceptable range
-                return isNotHidden && isNotTooFarFromTop && isNotTooFarFromBottom;
-            })
-            .map(function (advert) {
-                return advert.id;
-            });
-
-            dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function (advert) {
-                return lazyLoad.indexOf(advert.id) < 0;
-            });
-
-            if (dfpEnv.advertsToLoad.length === 0) {
-                disableLazyLoad();
-            }
-
-            lazyLoad.forEach(displayAd);
-        });
+        lazyLoad.forEach(displayAd);
     }
 
     function disableLazyLoad() {
