8d628ddcf14de12b4f9825351b37cae6660f5f29
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-12-14 11:07:45 +0000

    update raven to use asynchronous adblock detect

--- static/src/javascripts/components/raven-js/raven.js
+++ static/src/javascripts/components/raven-js/raven.js
@@ -1740,16 +1740,19 @@ function send(data) {
     }
 
     // Check if the request should be filtered or not
-    if (isFunction(globalOptions.shouldSendCallback) && !globalOptions.shouldSendCallback(data)) {
-        return;
+    if (isFunction(globalOptions.shouldSendCallback)) {
+        console.log("test")
+        globalOptions.shouldSendCallback(data).then(function (shouldSendCallback) {
+            if (shouldSendCallback) {
+                // Send along an event_id if not explicitly passed.
+                // This event_id can be used to reference the error within Sentry itself.
+                // Set lastEventId after we know the error should actually be sent
+                lastEventId = data.event_id || (data.event_id = uuid4());
+
+                makeRequest(data);
+            }
+        });
     }
-
-    // Send along an event_id if not explicitly passed.
-    // This event_id can be used to reference the error within Sentry itself.
-    // Set lastEventId after we know the error should actually be sent
-    lastEventId = data.event_id || (data.event_id = uuid4());
-
-    makeRequest(data);
 }
 
 
--- static/src/javascripts/projects/common/utils/raven.js
+++ static/src/javascripts/projects/common/utils/raven.js
@@ -37,7 +37,7 @@ define([
             shouldSendCallback: function (data) {
                 var isDev = config.page.isDev;
                 var isIgnored = typeof(data.tags.ignored) !== 'undefined' && data.tags.ignored;
-                var adBlockerOn = detect.adblockInUseSync();
+                var adBlockerOn = detect.adblockInUse;
 
                 if (isDev && !isIgnored) {
                     // Some environments don't support or don't always expose the console object
@@ -46,8 +46,11 @@ define([
                     }
                 }
 
-                return config.switches.enableSentryReporting &&
+                return adBlockerOn.then(function(adBlockerOn){
+                   return config.switches.enableSentryReporting &&
                     Math.random() < 0.1 && !isIgnored && !adBlockerOn && !isDev; // don't actually notify sentry in dev mode
+                })
+
             }
         }
     );
