5c74d94a3d6d3d33ad6586ab3b79d24ae34f62d5
Calum Campbell <campbell.cj@hotmail.co.uk>
2016-12-19 12:21:28 +0000

    fix indentation

--- static/src/javascripts/projects/common/modules/onward/tech-feedback.js
+++ static/src/javascripts/projects/common/modules/onward/tech-feedback.js
@@ -58,21 +58,21 @@ define([
         return function (link) {
             return function () {
                 var oldHref = link.attr('href');
-                    var props = {
-                        browser: window.navigator.userAgent,
-                        page: window.location,
-                        width: window.innerWidth,
-                        adBlock: adblockBeingUsed,
-                        devicePixelRatio: window.devicePixelRatio,
-                        ophanId: config.ophan.pageViewId,
-                        gu_u: cookies.get('GU_U'),
-                        payingMember: cookies.get('gu_paying_member'),
-                        abTests : summariseAbTests(ab.getParticipations())
-                    };
-                    var body = '\r\n\r\n\r\n\r\n------------------------------\r\nAdditional technical data about your request - please do not edit:\r\n\r\n'
-                        + objToString(assign(props, storedValues))
-                        + '\r\n\r\n';
-                    link.attr('href', oldHref + '?body=' + encodeURIComponent(body));
+                var props = {
+                    browser: window.navigator.userAgent,
+                    page: window.location,
+                    width: window.innerWidth,
+                    adBlock: adblockBeingUsed,
+                    devicePixelRatio: window.devicePixelRatio,
+                    ophanId: config.ophan.pageViewId,
+                    gu_u: cookies.get('GU_U'),
+                    payingMember: cookies.get('gu_paying_member'),
+                    abTests : summariseAbTests(ab.getParticipations())
+                };
+                var body = '\r\n\r\n\r\n\r\n------------------------------\r\nAdditional technical data about your request - please do not edit:\r\n\r\n'
+                    + objToString(assign(props, storedValues))
+                    + '\r\n\r\n';
+                link.attr('href', oldHref + '?body=' + encodeURIComponent(body));
             };
         };
     }
