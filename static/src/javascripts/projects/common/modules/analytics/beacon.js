b66e09f8d2f00aec1c9bec195f890e44908ced4e
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-12-19 11:43:40 +0000

    Remove beaconCounts and counts api

--- static/src/javascripts/projects/common/modules/analytics/beacon.js
+++ static/src/javascripts/projects/common/modules/analytics/beacon.js
@@ -11,21 +11,6 @@ define([
 ) {
     var canBeacon = !!navigator.sendBeacon;
 
-    function buildCounts(keys) {
-        return map(isArray(keys) ? keys : [keys], function (key) {
-            return 'c=' + key;
-        }).join('&');
-    }
-
-    // note, support is reasonably limited https://developer.mozilla.org/en-US/docs/Web/API/navigator.sendBeacon
-    function beaconCounts(keys) {
-        var url;
-        if (canBeacon) {
-            url = config.page.beaconUrl + '/accept-beacon?' + buildCounts(keys);
-            return navigator.sendBeacon(url, '');
-        }
-    }
-
     return {
         fire: function (path) {
             var img = new Image();
@@ -50,16 +35,6 @@ define([
                     crossOrigin: true
                 });
             }
-        },
-        counts: function (keys) {
-            if (canBeacon) {
-                return beaconCounts(keys);
-            } else {
-                var query = buildCounts(keys);
-                return this.fire('/counts.gif?' + query);
-            }
-        },
-
-        beaconCounts: beaconCounts
+        }
     };
 });
