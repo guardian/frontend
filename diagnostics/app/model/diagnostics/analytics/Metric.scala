f935b7ebfff10e3f84cebac2cca7e9fdf0e2087e
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-12-19 11:46:03 +0000

    Add commercial comments to distinguish dfp beacon from app beacon

--- diagnostics/app/model/diagnostics/analytics/Metric.scala
+++ diagnostics/app/model/diagnostics/analytics/Metric.scala
@@ -13,7 +13,9 @@ object Metric extends Logging {
     ("pv", CountMetric("kpis-page-views", "raw page views - simple <img> in body, no javascript involved")),
     ("pvg", CountMetric("kpis-analytics-page-views-google", "page view fires after Google Analytics")),
 
+    // ad-render is fired when the commercial js app has loaded an advert.
     ("ad-render", CountMetric("first-ad-rendered", "first-ad-rendered")),
+    // ad-wrapper is added to the page through a DFP creative wrapper. This means it can count n times for a page, where n is the number of adverts.
     ("ad-wrapper", CountMetric("dfp-served-ad", "dfp-served-ad")),
 
     // error pages
