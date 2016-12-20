7974bb330afb9e88fdfb8e5073b176fd02235539
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2016-12-20 15:35:41 +0000

    added a test to check renderHtml returns html and render returns json

---
+++ onward/test/ContentCardControllerTest.scala
@@ -0,0 +1,54 @@
+package test
+import controllers.{ContentCardController, RichLinkController}
+import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, FlatSpec, Matchers}
+import play.api.test.Helpers._
+import play.api.test._
+
+@DoNotDiscover class ContentCardControllerTest
+  extends FlatSpec
+  with Matchers
+  with ConfiguredTestSuite
+  with BeforeAndAfterAll
+  with WithMaterializer
+  with WithTestWsClient
+  with WithTestContext
+  with WithTestContentApiClient {
+
+  val article = "/world/2014/nov/18/hereford-hospital-patient-tested-for-ebola"
+  lazy val contentCardController = new ContentCardController(testContentApiClient)
+
+  "Content Card Controller" should "200 when the content is found" in {
+      val result = contentCardController.renderHtml(article, None, None)(TestRequest())
+      status(result) should be(200)
+      contentType(result) should be ("text/html")
+  }
+
+  it should "return JSON when .json format is supplied" in {
+    val fakeRequest = FakeRequest(GET, s"/embed/card/$article.json")
+      .withHeaders("host" -> "localhost:9000")
+      .withHeaders("Origin" -> "http://www.theorigin.com")
+
+    val result = contentCardController.render(article, None, None)(fakeRequest)
+    status(result) should be(200)
+    contentType(result).get should be("application/json")
+
+    val resultAsString = contentAsString(result)
+    resultAsString should startWith("{\"html\"")
+
+  }
+
+  it should "accept group and card index parameters for json endpoint" in {
+    val fakeRequest = FakeRequest(GET, s"/embed/card/$article.json")
+      .withHeaders("host" -> "localhost:9000")
+      .withHeaders("Origin" -> "http://www.theorigin.com")
+
+    val result = contentCardController.render(article, Some(0), Some(0))(fakeRequest)
+    status(result) should be(200)
+    contentType(result).get should be("application/json")
+
+    val resultAsString = contentAsString(result)
+    resultAsString should startWith("{\"html\"")
+    resultAsString should contain("<h1>")
+
+  }
+}
