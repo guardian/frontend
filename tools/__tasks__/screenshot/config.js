5646e100cc01a883c19f2baae3f5d5bf5fd9fa33
Matthew Walls <matthew.walls@guardian.co.uk>
2016-12-21 11:21:43 +0000

    Add ci build and paths to make screenshot task

--- tools/__tasks__/screenshot/config.js
+++ tools/__tasks__/screenshot/config.js
@@ -1,26 +1,43 @@
 // TODO: Vary on environment
-const environment = 'dev'; // Hardcode this for the moment until we're running anywhere but local
+const environment = 'ci'; // Hardcode this for the moment until we're running anywhere but local
 
 const domain = {
     prod: 'theguardian.com',
     code: 'm.code.dev-theguardian.com',
     dev: 'localhost',
+    ci: 'localhost'
 }[environment];
 
 const port = {
     prod: '80',
     code: '80',
     dev: '9000',
+    ci: '9000'
 }[environment];
 
 const host = `http://${domain}:${port}/`;
 
 // TODO: Add lots more useful paths - interactives, liveblogs, immersives etc
-const paths = [
-    'uk',
-    'us',
-    'au',
-];
+const paths = {
+    prod: [
+        'uk',
+        'us',
+        'au'
+    ],
+    code: [
+        'uk',
+        'us',
+        'au'
+    ],
+    dev: [
+        'uk',
+        'us',
+        'au'
+    ],
+    ci: [
+        'books/2014/may/21/guardian-journalists-jonathan-freedland-ghaith-abdul-ahad-win-orwell-prize-journalism'
+    ]
+}[environment];
 
 module.exports = {
     environment,
