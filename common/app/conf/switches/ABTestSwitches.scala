d2eb028dd4d38ace085b6bef2ef8f4440d3fa6fe
Justin Pinner <justinpinner@gmail.com>
2016-12-21 15:22:12 +0000

    Either Roberto's code will remove it or it will live a bit longer.

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -41,7 +41,7 @@ trait ABTestSwitches {
     "Test alternate short messages on membership engagement banner",
     owners = Seq(Owner.withGithub("justinpinner")),
     safeState = Off,
-    sellByDate = new LocalDate(2016, 12, 22), // Thursday 22nd December
+    sellByDate = new LocalDate(2017, 1, 5), // Thursday 22nd December
     exposeClientSide = true
   )
 
