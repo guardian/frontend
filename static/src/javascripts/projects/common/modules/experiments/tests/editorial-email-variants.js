0f05b4d97ef8fab58f7f33c023384864b69c30d6
Kate Whalen <whalenka88@gmail.com>
2017-01-05 16:25:25 +0000

    Make AB test work with Newsletters page, fix a linter warning

--- static/src/javascripts/projects/common/modules/experiments/tests/editorial-email-variants.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/editorial-email-variants.js
@@ -26,8 +26,11 @@ define([
         this.dataLinkNames = '';
         this.idealOutcome = 'Similar quantity of users in each list in ExactTarget';
 
+        var FLYERURL = 'info/ng-interactive/2016/dec/07/sign-up-for-the-flyer';
+
         this.canRun = function () {
-          return (config.page.contentId === 'info/ng-interactive/2016/dec/07/sign-up-for-the-flyer');
+          return (config.page.contentId === FLYERURL ||
+            config.page.pageId === '/email-newsletters');
         };
 
         function updateExampleUrl(exampleUrl) {
@@ -42,14 +45,32 @@ define([
           emailForm.setAttribute('src', 'https://www.theguardian.com/email/form/plaintone/' + emailListID);
         }
 
+        // Runs the test on https://www.theguardian.com/email-newsletters
+        function updateNewslettersPage(emailListID) {
+          return fastdomPromise.write(function () {
+            var flyerInput = $('input[value="2211"]')[0];
+            var flyerButton = $('button[value="2211"]')[0];
+            flyerInput.setAttribute('value', emailListID);
+            flyerButton.setAttribute('value', emailListID);
+          });
+        }
+
+        function runTheTest(emailListID, exampleUrl) {
+          if (config.page.contentId === FLYERURL) {
+            enhanceWebView(emailListID);
+          } else {
+            updateNewslettersPage(emailListID);
+          }
+          updateExampleUrl(exampleUrl);
+        }
+
         this.variants = [
             {
                 id: 'The-Flyer-Cards',
                 test: function () {
                     var emailListID = 3806;
                     var exampleUrl = 'https://www.theguardian.com/email/the-flyer?format=email';
-                    enhanceWebView(emailListID);
-                    updateExampleUrl(exampleUrl);
+                    runTheTest(emailListID, exampleUrl);
                 }
             },
             {
@@ -57,8 +78,7 @@ define([
                 test: function () {
                     var emailListID = 3807;
                     var exampleUrl = 'https://www.theguardian.com/email/the-flyer?format=email-connected';
-                    enhanceWebView(emailListID);
-                    updateExampleUrl(exampleUrl);
+                    runTheTest(emailListID, exampleUrl);
                 }
             }
         ];
