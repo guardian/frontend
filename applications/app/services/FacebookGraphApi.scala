98a3a5b399e48f3dacd004272159fde6d45b10d1
Jon Soul <jonathan.soul@guardian.co.uk>
2017-01-05 12:21:24 +0000

    Small refactor

--- applications/app/services/FacebookGraphApi.scala
+++ applications/app/services/FacebookGraphApi.scala
@@ -18,27 +18,26 @@ case class ShareObject(share_count: Int)
 class FacebookGraphApiClient(wsClient: WSClient) extends implicits.WSRequests with ExecutionContexts {
   val apiRootUrl = s"https://graph.facebook.com/v${Configuration.facebook.graphApi.version}/"
 
-  def GET[T](endpoint: Option[String], queryString: (String, String)*)(asResult: WSResponse => T) =
+  def GET(endpoint: Option[String], queryString: (String, String)*) =
     wsClient
       .url(apiRootUrl + endpoint.getOrElse(""))
       .withQueryString(addAccessToken(queryString): _*)
       .getOKResponse()
-      .map(asResult)
 
   protected def makeUrl(endpoint: Option[String]) = apiRootUrl + endpoint.getOrElse("")
-  protected def addAccessToken(queryString: Seq[(String, String)]) =
+  private def addAccessToken(queryString: Seq[(String, String)]) =
     ("access_token", Configuration.facebook.graphApi.accessToken) +: queryString
 }
 
-class FacebookGraphApi(facebookGraphApiClient: FacebookGraphApiClient) {
+class FacebookGraphApi(facebookGraphApiClient: FacebookGraphApiClient) extends ExecutionContexts {
 
   def shareCount(path: String): Future[Int] = {
     import URLResponseDeserializer._
 
-    facebookGraphApiClient.GET[Int](
+    facebookGraphApiClient.GET(
       endpoint = None,
       queryString = ("id", s"https://www.theguardian.com/$path")
-    ) { response =>
+    ) map { response =>
       response.json.asOpt[URLResponse]
         .map(_.share.share_count)
         .getOrElse(0)
--- applications/test/helpers/FacebookGraphApiTestClient.scala
+++ applications/test/helpers/FacebookGraphApiTestClient.scala
@@ -3,7 +3,7 @@ package helpers
 import java.io.File
 import java.net.URLEncoder
 
-import play.api.libs.ws.{WSClient, WSResponse}
+import play.api.libs.ws.WSClient
 import recorder.DefaultHttpRecorder
 import services.FacebookGraphApiClient
 
@@ -14,12 +14,11 @@ class FacebookGraphApiTestClient(wsClient: WSClient) extends FacebookGraphApiCli
     override lazy val baseDir = new File(System.getProperty("user.dir"), "data/facebook-graph-api")
   }
 
-  override def GET[T](endpoint: Option[String], params: (String, String)*)(asResult: WSResponse => T) = {
-    val augmentedParams = addAccessToken(params)
-    val queryString = augmentedParams.map({ case (key, value) => key + "=" + URLEncoder.encode(value, "UTF-8")}).mkString("&")
+  override def GET(endpoint: Option[String], params: (String, String)*) = {
+    val queryString = params.map({ case (key, value) => key + "=" + URLEncoder.encode(value, "UTF-8")}).mkString("&")
 
     recorder.load(s"${makeUrl(endpoint)}?$queryString", Map.empty) {
-      super.GET[WSResponse](endpoint, params: _*)(r => r)
-    } map asResult
+      super.GET(endpoint, params: _*)
+    }
   }
 }
