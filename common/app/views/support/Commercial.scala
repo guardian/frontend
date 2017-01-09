66ef357ae427fa00508ce32e8061cecc022ffb3e
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-01-09 17:32:35 +0000

    Use Maybe[String] instead of just String

--- common/app/views/support/Commercial.scala
+++ common/app/views/support/Commercial.scala
@@ -95,8 +95,8 @@ object Commercial {
     // The sizesOverride parameter is for testing only.
     def cssClasses(metadata: model.MetaData): String = {
       val topBannerDisableSticky = metadata.javascriptConfigOverrides.get("disableStickyTopBanner") match {
-        case Some(JsBoolean(true)) => "top-banner-ad-container--not-sticky"
-        case _                     => ""
+        case Some(JsBoolean(true)) => Some("top-banner-ad-container--not-sticky")
+        case _                     => None
       }
       val classes = Seq(
         "top-banner-ad-container",
