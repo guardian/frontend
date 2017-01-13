5a05e62e2cb4b63fd580765f050ea78e077a2dd3
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-01-13 12:58:56 +0000

    Hosted Gallery unit tests - cover a few more scenarios, and put in a fix for the tracking

--- static/test/javascripts/spec/commercial/hosted/gallery.spec.js
+++ static/test/javascripts/spec/commercial/hosted/gallery.spec.js
@@ -54,21 +54,59 @@ define([
             done();
         });
 
-        it('should open the caption on click', function (done) {
-            document.querySelector('.js-gallery-caption-button').click();
+        it('should open and close the caption on click', function (done) {
+            var button = document.querySelector('.js-gallery-caption-button');
+            button.click();
             expect($('.js-gallery-caption-bar', $fixturesContainer).hasClass('hosted-gallery--show-caption')).toBeTruthy();
+            button.click();
+            expect($('.js-gallery-caption-bar', $fixturesContainer).hasClass('hosted-gallery--show-caption')).toBeFalsy();
             done();
         });
 
-        it('should open the caption on pressing "i"', function (done) {
+        it('should open and close  the caption on pressing "i"', function (done) {
             gallery.handleKeyEvents({keyCode : 73});
             expect($('.js-gallery-caption-bar', $fixturesContainer).hasClass('hosted-gallery--show-caption')).toBeTruthy();
+            gallery.handleKeyEvents({keyCode : 73});
+            expect($('.js-gallery-caption-bar', $fixturesContainer).hasClass('hosted-gallery--show-caption')).toBeFalsy();
             done();
         });
 
-        it('should log navigation in GA', function (done) {
+        it('should open and close the onward journey on click', function (done) {
+            var button = document.querySelector('.js-hosted-gallery-oj-close');
+            button.click();
+            expect($('.js-hosted-gallery-oj', $fixturesContainer).hasClass('minimise-oj')).toBeTruthy();
+            button.click();
+            expect($('.js-hosted-gallery-oj', $fixturesContainer).hasClass('minimise-oj')).toBeFalsy();
+            done();
+        });
+
+        it('should log navigation in GA when using arrow key navigation', function (done) {
             gallery.handleKeyEvents({keyCode : 40, preventDefault: noop});
-            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("undefinedKeyPress:down - image 2");
+            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("KeyPress:down - image 2");
+            gallery.handleKeyEvents({keyCode : 39, preventDefault: noop});
+            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("KeyPress:right - image 3");
+            gallery.handleKeyEvents({keyCode : 38, preventDefault: noop});
+            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("KeyPress:up - image 2");
+            gallery.handleKeyEvents({keyCode : 37, preventDefault: noop});
+            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("KeyPress:left - image 1");
+            done();
+        });
+
+        it('should log navigation in GA when clicking through images', function (done) {
+            gallery.initScroll.call(gallery);
+            document.querySelector('.inline-arrow-down').click();
+            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("Click - image 2");
+            document.querySelector('.inline-arrow-up').click();
+            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("Click - image 1");
+            done();
+        });
+
+        it('should log navigation in GA when scrolling through images', function (done) {
+            gallery.fadeContent({target: {
+                scrollTop: 20,
+                scrollHeight: 30
+            }});
+            expect(interactionTracking.trackNonClickInteraction).toHaveBeenCalledWith("Scroll - image 3");
             done();
         });
 
