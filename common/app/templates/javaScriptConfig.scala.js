31dc73b547c126e0b4112c9aca78c9b79779a1e6
Alex Sanders <a@snd.rs>
2017-01-16 13:36:11 +0000

    add libs urls to config

--- common/app/templates/javaScriptConfig.scala.js
+++ common/app/templates/javaScriptConfig.scala.js
@@ -42,6 +42,11 @@
                 "editorialProd": "@{GoogleAnalyticsAccount.editorialProd.trackerName}",
                 "editorial": "@{GoogleAnalyticsAccount.editorialTracker(context).trackerName}"
             }
+        },
+        "libs": {
+            "foresee": 'vendor/foresee/20150703/foresee-trigger.js',
+            "googletag": '@{Configuration.javascript.config("googletagJsUrl")}',
+            "sonobi": '@{ if(!environment.isCode) Configuration.javascript.config("sonobiHeaderBiddingJsUrl") else "//mtrx.go.sonobi.com/morpheus.theguardian.12911_us_.js"}',
         }
     }
 }
