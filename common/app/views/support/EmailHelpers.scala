d25ecc5ac80b0e47d62f7ee975650b05a64a71b4
Joseph Smith <joseph.smith@guardian.co.uk>
2017-01-16 18:35:58 +0000

    Add support for sub-columns

--- common/app/views/support/EmailHelpers.scala
+++ common/app/views/support/EmailHelpers.scala
@@ -7,8 +7,8 @@ import play.twirl.api.Html
 import play.api.mvc._
 
 object EmailHelpers {
-  def columnNumber(n: Int): String = {
-    Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve").lift(n - 1).getOrElse("")
+  def columnWidth(width: Int): String = {
+    Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve").lift(width - 1).getOrElse("")
   }
 
   def row(inner: Html): Html = Html {
@@ -17,9 +17,9 @@ object EmailHelpers {
         </table>"""
   }
 
-  def columns(n: Int, innerClasses: Seq[String] = Seq(), last: Boolean = false, style: Option[String] = None)(inner: Html): Html = Html {
-    s"""<td class="wrapper ${if (last || n == 12) "last" else ""}" ${style.map(css => s"""style="$css"""").getOrElse("")}>
-      <table class="${columnNumber(n)} columns">
+  def column(width: Int, innerClasses: Seq[String] = Seq(), last: Boolean = false, style: Option[String] = None)(inner: Html): Html = Html {
+    s"""<td class="wrapper ${if (last || width == 12) "last" else ""}" ${style.map(css => s"""style="$css"""").getOrElse("")}>
+      <table class="${columnWidth(width)} columns">
         <tr>
           <td ${if (innerClasses.nonEmpty) s"""class="${innerClasses.mkString(" ")}" """ else ""}>$inner</td>
           <td class="expander"></td>
@@ -28,10 +28,26 @@ object EmailHelpers {
     </td>"""
   }
 
-  def fullRow(inner: Html): Html = row(columns(12)(inner))
-  def fullRow(classes: Seq[String] = Seq.empty)(inner: Html): Html = row(columns(12, classes)(inner))
-  def paddedRow(inner: Html): Html = row(columns(12, Seq("panel"))(inner))
-  def paddedRow(classes: Seq[String] = Seq.empty)(inner: Html): Html = row(columns(12, classes ++ Seq("panel"))(inner))
+  def columnWithSubColumns(width: Int, innerClasses: Seq[String] = Seq(), last: Boolean = false, style: Option[String] = None)(inner: Html): Html = Html {
+    s"""<td class="wrapper ${if (last || width == 12) "last" else ""}" ${style.map(css => s"""style="$css"""").getOrElse("")}>
+      <table class="${columnWidth(width)} columns">
+        <tr>
+          $inner
+          <td class="expander"></td>
+        </tr>
+      </table>
+    </td>"""
+  }
+
+  def subColumn(width: Int, classes: Seq[String] = Seq(), last: Boolean = false)(inner: Html): Html = Html {
+    s"""<td class="${columnWidth(width)} sub-columns ${if (last || width == 12) "last" else ""} ${classes.mkString(" ")}">$inner</td>"""
+  }
+
+  def fullRow(inner: Html): Html = row(column(12)(inner))
+  def fullRow(classes: Seq[String] = Seq.empty)(inner: Html): Html = row(column(12, classes)(inner))
+  def fullRowWithSubColumns(classes: Seq[String] = Seq.empty)(inner: Html): Html = row(columnWithSubColumns(12, classes)(inner))
+  def paddedRow(inner: Html): Html = row(column(12, Seq("panel"))(inner))
+  def paddedRow(classes: Seq[String] = Seq.empty)(inner: Html): Html = row(column(12, classes ++ Seq("panel"))(inner))
 
   def imageUrlFromCard(contentCard: ContentCard): Option[String] = {
     for {
