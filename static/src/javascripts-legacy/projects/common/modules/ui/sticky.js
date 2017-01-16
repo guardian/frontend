4b058a1b36a576a002a43c8045657039789c5db7
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-01-16 13:56:16 +0000

    Use element initial position to enable/disable sticky

--- static/src/javascripts-legacy/projects/common/modules/ui/sticky.js
+++ static/src/javascripts-legacy/projects/common/modules/ui/sticky.js
@@ -17,6 +17,7 @@ define([
      */
     function Sticky(element, options) {
         this.element  = element;
+        this.sticks   = false;
         this.opts     = defaults(options || {}, {
             top: 0,
             containInParent: true,
@@ -34,23 +35,28 @@ define([
     };
 
     Sticky.prototype.updatePosition = function updatePosition() {
+        var elementRect = this.element.getBoundingClientRect();
         var parentRect = this.element.parentNode.getBoundingClientRect();
-        var elementHeight = this.element.offsetHeight;
-        var css = {}, message, stick;
+        var elementHeight = elementRect.height;
+        var css, message, stick;
 
         // have we scrolled past the element
-        if (parentRect.top < this.opts.top + paidforBandHeight) {
-            // make sure the element stays within its parent
-            var fixedTop = this.opts.containInParent && parentRect.bottom < this.opts.top + elementHeight ?
-                Math.floor(parentRect.bottom - elementHeight - this.opts.top) :
-                this.opts.top;
-            stick = true;
-            css = { top: fixedTop };
-            message = 'fixed';
+        if (this.sticks) {
+            if (window.pageYOffset < this.absolutePos) {
+                stick = false;
+                css = { top: null };
+                message = 'unfixed';
+            }
         } else {
-            stick = false;
-            css = { top: 0 };
-            message = 'unfixed';
+            if (elementRect.top <= this.opts.top) {
+                // make sure the element stays within its parent
+                var fixedTop = this.opts.containInParent && parentRect.bottom < this.opts.top + elementHeight ?
+                    Math.floor(parentRect.bottom - elementHeight - this.opts.top) :
+                    this.opts.top;
+                stick = true;
+                css = { top: fixedTop };
+                message = 'fixed';
+            }
         }
 
         if (this.opts.emitMessage && message && message !== this.lastMessage) {
@@ -60,6 +66,7 @@ define([
 
         if (css) {
             fastdom.write(function () {
+                this.sticks = stick;
                 if (stick) {
                     bonzo(this.element).addClass('is-sticky').css(css);
                 } else {
