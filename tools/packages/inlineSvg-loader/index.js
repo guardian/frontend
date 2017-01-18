b295c78d217fed5179c7a6d06bd73f2aa0ef15dc
Alex Sanders <a@snd.rs>
2017-01-18 10:59:56 +0000

    make packages use global lint settings

--- tools/packages/inlineSvg-loader/index.js
+++ tools/packages/inlineSvg-loader/index.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
-module.exports = function (content) {
-    this.cacheable && this.cacheable();
+module.exports = (content) => {
+    // eslint-disable-next-line no-unused-expressions
+    this.cacheable && this.cacheable(); // webpack-specfic weirdness
 
     const match = content.match(/<svg([^>]+)+>([\s\S]+)<\/svg>/i);
     const prefix = 'inline-';
     const imageType = path.dirname(this.resourcePath).split('/').pop();
     const fileName = path.basename(this.resourcePath).split('.svg')[0];
     const svg = match ? match[0].replace(/\n/g, ' ').trim() : '';
-    const markup = `<span class=\"${prefix}${fileName} ${(imageType !== '' ? prefix + imageType : '')}\">${svg}</span>`;
+    const markup = `<span class="${prefix}${fileName} ${(imageType !== '' ? prefix + imageType : '')}">${svg}</span>`;
 
     this.value = markup;
 
