118118f7c24f0afe06059effd2dfc1903fac75df
Matthew Walls <matthew.walls@guardian.co.uk>
2017-01-18 11:50:57 +0000

    Add google verification for indexing api

--- applications/app/controllers/SiteVerificationController.scala
+++ applications/app/controllers/SiteVerificationController.scala
@@ -10,6 +10,7 @@ class SiteVerificationController extends Controller {
   private val acceptedGoogleAccounts = List(
     "f2ddac7ca1547968", // Main Guardian channel - https://www.youtube.com/user/TheGuardian
     "b49f4f3dd93492fb", // HTTPS protocol on www.theguardian.com
+    "14fca0dabd19b468", // Used by: Indexing API on amp.theguardian.com
     "bbb4e09fa25b64ba"  // Used by:
       // Football channel - https://www.youtube.com/user/GuardianFootball
       // Owen Jones channel - https://www.youtube.com/channel/UCSYCo8uRGF39qDCxF870K5Q
