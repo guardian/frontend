f834bdb735252b315ea324026ec59fd239bf99b9
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-01-19 16:19:32 +0000

    Add check for ga on window and store values for later send

--- static/src/javascripts-legacy/projects/common/modules/analytics/google.js
+++ static/src/javascripts-legacy/projects/common/modules/analytics/google.js
@@ -1,7 +1,9 @@
 define([
-    'common/utils/config'
+    'common/utils/config',
+    'common/utils/mediator'
 ], function (
-    config
+    config,
+    mediator
 ) {
     var ga = window.ga;
     var trackerName = config.googleAnalytics.trackers.editorial;
@@ -48,6 +50,15 @@ define([
         });
     }
 
+    function mapEvents() {
+        config.googleAnalytics.timingEvents.map(sendPerformanceEvent);
+        mediator.off('modules:ga:ready', mapEvents);
+    }
+
+    function sendPerformanceEvent(event) {
+        window.ga(send, 'timing', event.timingCategory, event.timingVar, event.timeSincePageLoad, event.timingLabel);
+    }
+
     // Track important user timing metrics so that we can be notified and measure over time in GA
     // https://developers.google.com/analytics/devguides/collection/analyticsjs/user-timings
     // Tracks into Behaviour > Site Speed > User Timings in GA
@@ -56,7 +67,28 @@ define([
         if (window.performance) {
             // Value must be an integer - grabs the number of milliseconds since page load
             var timeSincePageLoad = Math.round(window.performance.now());
-            ga(send, 'timing', timingCategory, timingVar, timeSincePageLoad, timingLabel);
+
+            if (window.ga) {
+                sendPerformanceEvent({
+                    timingCategory: timingCategory,
+                    timingVar: timingVar,
+                    timeSincePageLoad: timeSincePageLoad,
+                    timingLabel: timingLabel
+                });
+            } else {
+                if (!config.googleAnalytics.timingEvents) {
+                    config.googleAnalytics.timingEvents = [];
+                }
+
+                mediator.on('modules:ga:ready', mapEvents);
+
+                config.googleAnalytics.timingEvents.push({
+                    timingCategory: timingCategory,
+                    timingVar: timingVar,
+                    timeSincePageLoad: timeSincePageLoad,
+                    timingLabel: timingLabel
+                })
+            }
         }
     }
 
