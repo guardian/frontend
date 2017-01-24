98a59fe259d723b6b73479ddcb91ada7c49dd735
Alex Sanders <a@snd.rs>
2017-01-24 16:30:30 +0000

    measure more about the module loaders

--- static/src/javascripts-legacy/boot.js
+++ static/src/javascripts-legacy/boot.js
@@ -20,11 +20,17 @@ Only if we detect we should run enhance.
 define([
     'Promise',
     'domReady',
-    'common/utils/raven'
+    'common/utils/raven',
+    'common/utils/user-timing',
+    'common/utils/robust',
+    'common/modules/analytics/google'
 ], function (
     Promise,
     domReady,
-    raven
+    raven,
+    userTiming,
+    robust,
+    ga
 ) {
     // curl’s promise API is broken, so we must cast it to a real Promise
     // https://github.com/cujojs/curl/issues/293
@@ -39,7 +45,13 @@ define([
 
     var bootStandard = function () {
         return promiseRequire(['bootstraps/standard/main'])
-            .then(function (boot) { boot(); });
+            .then(function (boot) {
+                userTiming.mark('standard boot');
+                robust.catchErrorsAndLog('ga-user-timing-standard-boot', function () {
+                    ga.trackPerformance('Javascript Load', 'standardBoot', 'Standard boot time');
+                });
+                boot();
+            });
     };
 
     var bootCommercial = function () {
@@ -57,10 +69,18 @@ define([
             });
         }
 
+        userTiming.mark('commerical request');
+        robust.catchErrorsAndLog('ga-user-timing-commercial-request', function () {
+            ga.trackPerformance('Javascript Load', 'commericalRequest', 'Commerical request time');
+        });
         return promiseRequire(['bootstraps/commercial'])
             .then(raven.wrap(
                     { tags: { feature: 'commercial' } },
                     function (commercial) {
+                        userTiming.mark('commerical boot');
+                        robust.catchErrorsAndLog('ga-user-timing-commercial-boot', function () {
+                            ga.trackPerformance('Javascript Load', 'commericalBoot', 'Commerical boot time');
+                        });
                         commercial.init();
                     }
                 )
@@ -69,8 +89,16 @@ define([
 
     var bootEnhanced = function () {
         if (guardian.isEnhanced) {
+            userTiming.mark('enhanced request');
+            robust.catchErrorsAndLog('ga-user-timing-enhanced-request', function () {
+                ga.trackPerformance('Javascript Load', 'enhancedRequest', 'Enhanced request time');
+            });
             return promiseRequire(['bootstraps/enhanced/main'])
                 .then(function (boot) {
+                    userTiming.mark('enhanced boot');
+                    robust.catchErrorsAndLog('ga-user-timing-enhanced-boot', function () {
+                        ga.trackPerformance('Javascript Load', 'enhancedBoot', 'Enhanced boot time');
+                    });
                     boot();
                 });
         }
