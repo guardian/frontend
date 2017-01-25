4c90e64c59236569f15135aec5e5a691011cc561
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-01-25 18:47:34 +0000

    extract template into html for ad feedback popup

--- static/src/javascripts-legacy/projects/commercial/modules/dfp/render-advert-label.js
+++ static/src/javascripts-legacy/projects/commercial/modules/dfp/render-advert-label.js
@@ -1,10 +1,14 @@
 define([
     'Promise',
     'common/utils/fastdom-promise',
+    'common/utils/template',
+    'text!commercial/views/ad-feedback-popup.html',
     'common/utils/config'
 ], function (
     Promise,
     fastdom,
+    template,
+    popupTemplate,
     config
 ) {
     function renderAdvertLabel(adSlotNode) {
@@ -12,23 +16,17 @@ define([
             var zIndexOverlay = 1050;
             var labelInner = '';
             if (config.switches.adFeedback) {
-                var feedbackOpts = [
-                    ['distracting'],
-                    ['obscures', 'Obscures content'],
-                    ['inappropriate'],
-                    ['repetitive'],
-                    ['irrelevant']
-                ].map(function (_) {
-                    return '<li class="popup__item"><a><div class="popup__item-problem--option" slot="' + adSlotNode.id + '" problem="' + _[0] + '">' + (_[1] || _[0].replace(/^[a-z]/, function ($1) {
-                            return $1.toUpperCase();
-                        })) + '</div></a></li>';
-                }).join('');
-                var feedbackMenu = '<div class="ad-feedback popup popup--default popup__group is-off ' + adSlotNode.id + '__popup--feedback" id="ad-feedback-menu__' + adSlotNode.id + '" style="z-index: ' + zIndexOverlay + ';"><h3 class="ad-feedback popup__group-header"><p>Thank you for reporting this advert.</p><p>To help improve the ad experience on the Guardian, can you tell us why you\'re reporting it?</p></h3><ul>' + feedbackOpts + '</ul></div>';
-                labelInner = ' <a class="ad-feedback popup__toggle" data-toggle="' + adSlotNode.id + '__popup--feedback" aria-haspopup="true" aria-controls="ad-feedback-menu__' + adSlotNode.id + '">Report this ad</a>' + feedbackMenu;
-
-                if (labelInner.length > 0) {
-                    adSlotNode.style.zIndex = zIndexOverlay;
-                }
+                adSlotNode.style.zIndex = zIndexOverlay;
+                labelInner = template(popupTemplate, {
+                    feedbackOptions: {
+                        'distracting': 'Distracting',
+                        'obscures' : 'Obscures content',
+                        'inappropriate': 'Inappropriate',
+                        'repetitive': 'Repetitive',
+                        'irrelevant': 'Irrelevant'
+                    },
+                    slot: adSlotNode.id
+                });
             }
             var labelDiv = '<div class="ad-slot__label" data-test-id="ad-slot-label" style="z-index: 2010;">Advertisement' + labelInner + '</div>';
             return fastdom.write(function () {
