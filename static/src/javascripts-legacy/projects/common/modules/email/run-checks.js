68b8f71224477c0a38f393ec090e7f37a267aee7
David Furey <david.furey@guardian.co.uk>
2017-01-25 13:54:59 +0000

    $outbrain is an array

--- static/src/javascripts-legacy/projects/common/modules/email/run-checks.js
+++ static/src/javascripts-legacy/projects/common/modules/email/run-checks.js
@@ -90,7 +90,7 @@ define([
 
     function obWidgetIsShown() {
         var $outbrain = $('.js-outbrain-container');
-        return $outbrain && $outbrain.children && $outbrain.children.length > 0;
+        return $outbrain && $outbrain[0] && $outbrain[0].children && $outbrain[0].children.length > 0;
     }
 
     var canRunList = {
