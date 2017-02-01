ae403bd73f488ddf50ed7881d0dc10da55904a56
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-02-01 10:08:06 +0000

    Add custom timing to paid band

--- static/src/javascripts-legacy/projects/commercial/modules/paidfor-band.js
+++ static/src/javascripts-legacy/projects/commercial/modules/paidfor-band.js
@@ -2,20 +2,24 @@ define([
     'Promise',
     'common/utils/config',
     'common/modules/ui/sticky',
-    'common/modules/commercial/commercial-features'
-], function (Promise, config, Sticky, commercialFeatures) {
-    function init() {
+    'common/modules/commercial/commercial-features',
+    'commercial/modules/dfp/performance-logging',
+], function (Promise, config, Sticky, commercialFeatures, performanceLogging) {
+    function init(moduleName) {
         if (!commercialFeatures.paidforBand) {
             return Promise.resolve(false);
         }
 
-        return new Promise(function (resolve) {
-            var elem = document.querySelector('.paidfor-band');
-            if (elem) {
-                new Sticky(elem).init();
-            }
-            resolve();
-        });
+        performanceLogging.moduleStart(moduleName);
+
+        var elem = document.querySelector('.paidfor-band');
+        if (elem) {
+            new Sticky(elem).init();
+        }
+
+        performanceLogging.moduleEnd(moduleName);
+
+        return Promise.resolve();
     }
 
     return {
