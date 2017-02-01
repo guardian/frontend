63e78a25a4d0c738c1666ac7cf32a5dbd4971408
Alex Sanders <a@snd.rs>
2017-02-01 15:06:36 +0000

    move sass watching to gulp entirely

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -5,45 +5,44 @@ const path = require('path');
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 
+const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const pxtorem = require('postcss-pxtorem');
+const sassGrapher = require('gulp-sass-grapher');
+const filter = require('gulp-filter');
+
+const browserSync = require('browser-sync').create();
 
 const STATIC = path.resolve(__dirname, '..', 'static');
-const CSS = path.resolve(STATIC, 'target', 'stylesheets');
-const JS = path.resolve(STATIC, 'src', 'javascripts-legacy');
-const TRANSPILED = path.resolve(STATIC, 'transpiled', 'javascripts');
+const SASS_SRC = path.resolve(STATIC, 'src', 'stylesheets');
+const SASS_TARGET = path.resolve(STATIC, 'target', 'stylesheets');
 
-console.log(CSS);
+const { sassSettings, browserslist, remifications } = require('../dev/css-settings');
+const bsConfig = require('./bs-config');
 
 gulp.task('watch', (done) => {
+    browserSync.init(bsConfig);
+
+    sassGrapher.init(SASS_SRC, { loadPaths: SASS_SRC });
+
     gulp.watch([
-        `${CSS}/**/*.css`,
-        `!${CSS}/ie9.*.css`,
-        `!${CSS}/old-ie.*.css`,
-        `!${CSS}/webfonts-*.css`,
+        `${SASS_SRC}/**/*.scss`,
     ], (event) => {
-        gulp.src(event.path, { base: CSS })
+        gulp.src(event.path, { base: SASS_SRC })
+            .pipe(sassGrapher.ancestors())
+            .pipe(filter(['**', '!**/ie9*', '!**/old-ie.*', '!**/webfonts-*']))
             .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sass(Object.assign({
+                includePath: SASS_SRC,
+            }, sassSettings)))
             .pipe(postcss([
-                autoprefixer(),
-                pxtorem({
-                    replace: true,
-                    root_value: 16,
-                    unit_precision: 5,
-                    prop_white_list: [],
-                }),
+                autoprefixer(browserslist),
+                pxtorem(remifications),
             ]))
             .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest(CSS));
-    });
-
-    // copy any changed legacy js to the transpiled directory
-    gulp.watch([
-        `${JS}/**/*.js`,
-    ], (event) => {
-        gulp.src(event.path, { base: JS })
-            .pipe(gulp.dest(TRANSPILED));
+            .pipe(gulp.dest(SASS_TARGET))
+            .pipe(browserSync.stream({ match: '**/*.css' }));
     });
 
     done();
