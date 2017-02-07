693d740883efccaf588512b8c709ff892f5d852d
oilnam <manlio.poltronieri@theguardian.com>
2017-02-07 17:16:59 +0000

    Add a/b test for surveys using Tailor

--- static/src/javascripts-legacy/projects/common/modules/experiments/ab.js
+++ static/src/javascripts-legacy/projects/common/modules/experiments/ab.js
@@ -13,7 +13,8 @@ define([
     'common/modules/experiments/tests/membership-engagement-banner-tests',
     'common/modules/experiments/tests/guardian-today-messaging',
     'common/modules/experiments/acquisition-test-selector',
-    'common/modules/experiments/tests/membership-a3-a4-bundles-thrasher'
+    'common/modules/experiments/tests/membership-a3-a4-bundles-thrasher',
+    'common/modules/experiments/tests/tailor-survey'
 ], function (reportError,
              config,
              cookies,
@@ -28,7 +29,8 @@ define([
              MembershipEngagementBannerTests,
              GuardianTodayMessaging,
              acquisitionTestSelector,
-             MembershipA3A4BundlesThrasher
+             MembershipA3A4BundlesThrasher,
+             TailorSurvey
     ) {
     var TESTS = compact([
         new EditorialEmailVariants(),
@@ -36,7 +38,8 @@ define([
         new RecommendedForYou(),
         new GuardianTodayMessaging(),
         acquisitionTestSelector.getTest(),
-        new MembershipA3A4BundlesThrasher()
+        new MembershipA3A4BundlesThrasher(),
+        new TailorSurvey()
     ].concat(MembershipEngagementBannerTests));
 
     var participationsKey = 'gu.ab.participations';
