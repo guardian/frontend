48faddf2896565f764a6b233c4b136b6285848a5
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-02-09 17:19:41 +0000

    Render a page view id script in the esi call

--- diagnostics/app/controllers/CommercialPreflightController.scala
+++ diagnostics/app/controllers/CommercialPreflightController.scala
@@ -2,7 +2,11 @@ package controllers
 
 import common.Logging
 import model.NoCache
+import org.joda.time.DateTime
 import play.api.mvc.{Action, Controller}
+
+import java.lang.Long.{toString => toStringBase}
+import scala.util.Random
 
 case class AdRequest(
   loadId: String,             // load_id
@@ -19,7 +23,15 @@ case class AdRequest(
 
 class CommercialPreflightController extends Controller with Logging {
 
+  // This is based on https://github.com/guardian/ophan/blob/master/tracker-js/assets/coffee/ophan/transmit.coffee
+  def makeOphanViewId(): String = {
+    val datePart = toStringBase(new DateTime().getMillis(), 36)
+    val randPart = (for (_ <- 1 to 12) yield toStringBase(Random.nextInt(36), 36)).mkString
+    s"$datePart$randPart"
+  }
+
   def adCall() = Action { implicit request =>
-    NoCache(Ok(views.html.fragments.adCall()))
+    val pageViewId = makeOphanViewId()
+    NoCache(Ok(views.html.fragments.adCall(pageViewId)))
   }
 }
