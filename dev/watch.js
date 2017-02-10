1abb3aeee889f6f4815e6d95d848adb5656f1881
Alex Sanders <a@snd.rs>
2017-02-10 09:51:28 +0000

    Revert "Merge pull request #15803 from guardian/revert-15791-webpack-dev"
    This reverts commit dae5a8685dc109865868c3e0ab63ae3f93e75990, reversing
    changes made to 921c848dafadd83966d95e22fd9e4a9e49850f1d.

---
+++ dev/watch.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+const chalk = require('chalk');
+const browserSync = require('browser-sync').create();
+const bsConfig = require('./bs-config');
+
+// ********************************** JAVASCRIPT **********************************
+
+// webpack watch task always performs an intial bundle, and we don't want browsersync
+// listening at the point. so we use this flag to know whether any change webpack reports
+// is the initial bundle, or subsequent file changes
+let INITIAL_BUNDLE = true;
+
+// just a bit of visual feedback while webpack creates its initial bundles.
+// fakes a listr step
+const ora = require('ora');
+
+const wpNotification = ora({
+    text: 'Create initial webpack bundles',
+    color: 'yellow',
+});
+wpNotification.start();
+
+const webpack = require('webpack');
+const webpackBundler = webpack(require('../webpack.config.js')());
+
+webpackBundler.watch({
+    ignored: /node_modules/,
+}, (err, stats) => {
+    if (err) {
+        // log any unexpected error
+        console.log(chalk.red(err));
+    }
+
+    if (INITIAL_BUNDLE) {
+        INITIAL_BUNDLE = false;
+        wpNotification.succeed();
+
+        // now have the initial bundles, we can start browsersync
+        return browserSync.init(bsConfig);
+    }
+
+    // send editing errors to console and browser
+    if (stats.hasErrors() || stats.hasWarnings()) {
+        console.log(chalk.red(stats.toString('errors-only')));
+        return browserSync.sockets.emit('fullscreen:message', {
+            title: 'Webpack Error:',
+            body: stats.toString('errors-only'),
+            timeout: 100000,
+        });
+    }
+
+    // announce the changes
+    return browserSync.reload();
+});
+
+// ********************************** Sass **********************************
+
+const chokidar = require('chokidar');
+
+const sassDir = path.resolve(__dirname, '../', 'static', 'src', 'stylesheets');
+const sassGraph = require('sass-graph').parseDir(sassDir, { loadPaths: sassDir });
+
+const compileSass = require('../tools/compile-css');
+
+// when we detect a change in a sass file, we look up the tree of imports
+// and only compile what we need to. anything matching this regex, we can just ignore in dev.
+const ignoredSassRegEx = /^(_|ie9|old-ie)/;
+
+chokidar.watch(`${sassDir}/**/*.scss`)
+    .on('change', (changedFile) => {
+        // see what top-level files need to be recompiled
+        const filesToCompile = [];
+
+        sassGraph.visitAncestors(changedFile, (ancestorPath) => {
+            const ancestorFileName = path.basename(ancestorPath);
+            if (!ignoredSassRegEx.test(ancestorFileName)) {
+                filesToCompile.push(ancestorFileName);
+            }
+        });
+
+        // now recompile all files that matter
+        Promise.all(filesToCompile.map(compileSass))
+            .then(() => {
+                // clear any previous error messages
+                browserSync.sockets.emit('fullscreen:message:clear');
+
+                // announce the changes
+                browserSync.reload(filesToCompile.map(file => file.replace('scss', 'css')));
+            })
+            .catch((e) => {
+                // send editing errors to console and browser
+                console.log(chalk.red(`\n${e.formatted}`));
+                browserSync.sockets.emit('fullscreen:message', {
+                    title: 'CSS Error:',
+                    body: e.formatted,
+                    timeout: 100000,
+                });
+            });
+    });
