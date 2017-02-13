a5877fb0f679ecd2c6043f6c636c5dcde85b393c
Michael McNamara <michael.mcnamara@guardian.co.uk>
2017-02-13 14:59:19 +0000

    putting credential settings behind a switch - also adding this to other requests for the same file

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -497,4 +497,15 @@ trait FeatureSwitches {
     sellByDate = new LocalDate(2017, 2, 27),
     exposeClientSide = true
   )
+
+  val AmpOphanTrackingIncludesCredentials = Switch(
+    SwitchGroup.Feature,
+    "amp-ophan-tracking-includes-credentials",
+    "allows access to id field when page viewed from google cache so that we keep tracking consistent",
+    owners = Seq(Owner.withGithub("michaelwmcnamara")),
+    safeState = Off,
+    sellByDate = new LocalDate(2017, 3, 10),
+    exposeClientSide = true
+
+  )
 }
