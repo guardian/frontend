7b0e6c7e6eeed8beaff48f1459ef2d9b1da093cf
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-02-14 11:21:57 +0000

    Remove gimbap preprocess

--- static/src/javascripts-legacy/projects/commercial/modules/creatives/template-preprocessor.js
+++ static/src/javascripts-legacy/projects/commercial/modules/creatives/template-preprocessor.js
@@ -4,8 +4,6 @@ define([
     'common/utils/template',
     'lodash/objects/assign',
     'lodash/utilities/identity',
-    'text!commercial/views/creatives/gimbap/gimbap-simple-blob.html',
-    'text!commercial/views/creatives/gimbap/gimbap-richmedia-blob.html',
     'text!commercial/views/creatives/manual-card.html',
     'text!commercial/views/creatives/manual-card-large.html',
     'text!commercial/views/creatives/manual-card-cta.html',
@@ -19,8 +17,6 @@ define([
     template,
     assign,
     identity,
-    gimbapSimpleStr,
-    gimbapRichmediaStr,
     manualCardStr,
     manualCardLargeStr,
     manualCardCtaStr,
@@ -29,8 +25,6 @@ define([
     manualContainerCtaSoulmatesStr,
     manualContainerCtaMembershipStr
 ) {
-    var gimbapSimpleTpl;
-    var gimbapRichmediaTpl;
     var manualCardStrs = {
         'manual-card': manualCardStr,
         'manual-card-large': manualCardLargeStr
@@ -41,55 +35,6 @@ define([
     var manualContainerCtaTpl;
     var manualContainerCtaSoulmatesTpl;
     var manualContainerCtaMembershipTpl;
-
-    function preprocessGimbap(tpl) {
-        tpl.params.headless = tpl.params.headless === 'true';
-
-        // SVGs
-        tpl.params.marque36icon = svgs('marque36icon', ['gimbap-wrap__mainlogo']);
-        tpl.params.inlineQuote = svgs('quoteIcon', ['gimbap__quote']);
-        tpl.params.arrowRight = (tpl.params.linksWithArrows.indexOf('yes') !== -1) ? svgs('arrowRight', ['gimbap__arrow']) : '';
-
-        // Make sure we include right logo to the right card
-        tpl.params.offer1logo = tpl.params['logo' + tpl.params.offer1tone + 'horizontal'];
-        tpl.params.offer2logo = tpl.params['logo' + tpl.params.offer2tone + 'horizontal'];
-        tpl.params.offer3logo = tpl.params['logo' + tpl.params.offer3tone + 'horizontal'];
-        tpl.params.offer4logo = tpl.params['logo' + tpl.params.offer4tone + 'horizontal'];
-
-        tpl.params.gimbapLogoStyle = (tpl.params.style === 'reversed') ? ' gimbap-logo--reversed': '';
-
-        // Include quotes into title only if it is allowed in DFP line item
-        tpl.params.offer1HasQuotes = (tpl.params.offer1quotes.indexOf('yes') !== -1) ? tpl.params.inlineQuote : '';
-        tpl.params.offer2HasQuotes = (tpl.params.offer2quotes.indexOf('yes') !== -1) ? tpl.params.inlineQuote : '';
-        tpl.params.offer3HasQuotes = (tpl.params.offer3quotes.indexOf('yes') !== -1) ? tpl.params.inlineQuote : '';
-        tpl.params.offer4HasQuotes = (tpl.params.offer4quotes.indexOf('yes') !== -1) ? tpl.params.inlineQuote : '';
-
-        // Test for Author image
-        tpl.params.hasAuthorImage = tpl.params.offer1authorimage
-                                        && tpl.params.offer1authorimage.length > 0
-                                        && tpl.params.layout !== '1x1x1x1';
-    }
-
-    function preprocessGimbapSimple(tpl) {
-        if (!gimbapSimpleTpl) {
-            gimbapSimpleTpl = template(gimbapSimpleStr);
-        }
-        // SVGs
-        tpl.params.marque36icon = svgs('marque36icon', ['gimbap-wrap__mainlogo']);
-        tpl.params.arrowRight = (tpl.params.linksWithArrows.indexOf('yes') !== -1) ? svgs('arrowRight', ['gimbap__arrow', 'gimbap__arrow--styled']) : '';
-        tpl.params.logo = tpl.params['logo' + tpl.params.componenttone + 'horizontal'];
-
-        tpl.params.gimbapEffects = tpl.params.componenteffects === 'yes' ? ' ' + 'gimbap--effects' : '';
-
-        tpl.params.gimbapSimple = '';
-        for (var i = 1; i <= 4; i++) {
-            tpl.params.gimbapSimple += gimbapSimpleTpl(assign(tpl.params, {
-                offerurl: tpl.params['offer' + i + 'url'],
-                offertitle: tpl.params['offer' + i + 'title'],
-                offerimage: tpl.params['offer' + i + 'image']
-            }));
-        }
-    }
 
     function preprocessManualContainer(tpl) {
         var stems = {
@@ -191,40 +136,7 @@ define([
         }
     }
 
-    function preprocessGimbapRichmedia(tpl) {
-        if (!gimbapRichmediaTpl) {
-            gimbapRichmediaTpl = template(gimbapRichmediaStr);
-        }
-        // SVGs
-        tpl.params.marque36icon = svgs('marque36icon', ['gimbap-wrap__mainlogo']);
-        tpl.params.logo = tpl.params['logo' + tpl.params.componenttone + 'horizontal'];
-        tpl.params.iconClock = svgs('iconClock', ['gimbap-richmedia__icon']);
-        tpl.params.iconLocation = svgs('iconLocation', ['gimbap-richmedia__icon']);
-        tpl.params.iconBasket = svgs('iconBasket', ['gimbap-richmedia__icon']);
-
-        tpl.params.gimbapEffects = tpl.params.componenteffects === 'yes' ? ' ' + 'gimbap--effects' : '';
-
-        tpl.params.gimbapRichmedia = '';
-        for (var i = 1; i <= 2; i++) {
-            tpl.params.gimbapRichmedia += gimbapRichmediaTpl(assign(tpl.params, {
-                offerurl: tpl.params['offer' + i + 'url'],
-                offertitle: tpl.params['offer' + i + 'title'],
-                offerimage: tpl.params['offer' + i + 'image'],
-                offerHighlight: tpl.params['offer' + i + 'highlight'],
-                offerTitle: tpl.params['offer' + i + 'title'],
-                offerHeadline: tpl.params['offer' + i + 'headline'],
-                offerDate: tpl.params['offer' + i + 'date'],
-                offerPlace: tpl.params['offer' + i + 'place'],
-                offerPrice: tpl.params['offer' + i + 'price'] !== '0' ? tpl.params['offer' + i + 'price'] : '',
-                offerDiscount: tpl.params['offer' + i + 'discount']
-            }));
-        }
-    }
-
     return {
-        'gimbap': preprocessGimbap,
-        'gimbap-simple': preprocessGimbapSimple,
-        'gimbap-richmedia': preprocessGimbapRichmedia,
         'manual-container': preprocessManualContainer
     };
 });
