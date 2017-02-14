f17edae86561198f8894726cc7150f1bd77e8079
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-02-14 15:57:56 +0000

    Remove scrollable MPU template

--- static/src/javascripts-legacy/projects/commercial/modules/dfp/apply-creative-template.js
+++ static/src/javascripts-legacy/projects/commercial/modules/dfp/apply-creative-template.js
@@ -16,7 +16,6 @@ define([
     'commercial/modules/creatives/fabric-expandable-video-v2',
     'commercial/modules/creatives/fabric-video',
     'commercial/modules/creatives/hosted-thrasher-multi',
-    'commercial/modules/creatives/scrollable-mpu',
     'commercial/modules/creatives/scrollable-mpu-v2',
     'commercial/modules/creatives/template'
 ], function (
