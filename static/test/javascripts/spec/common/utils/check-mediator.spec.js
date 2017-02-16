070fc79a3a6c780b442f61c72e4959bd0385daa1
George Haberis <georgioshaberis@gmail.com>
2017-02-16 14:51:47 +0000

    Update unit tests

--- static/test/javascripts/spec/common/utils/check-mediator.spec.js
+++ static/test/javascripts/spec/common/utils/check-mediator.spec.js
@@ -4,34 +4,64 @@ define([
     checkMediator
 ) {
     describe('Check Mediator', function () {
+        var SOMECHECKSPASSED = Array.prototype.some;
+        var EVERYCHECKPASSED = Array.prototype.every;
+
         var checkList = [{
                 id: 'check-1',
-                canRun: true,
-                dependentChecks: []
+                canRun: true
             }, {
                 id: "check-2",
                 canRun: true,
-                dependentChecks: [{
-                    id: "check-3",
-                    canRun: true,
-                    dependentChecks: []
-                }, {
-                    id: "check-4",
-                    canRun: true,
-                    dependentChecks: []
-                }]
+                dependentChecks: {
+                    passCondition: EVERYCHECKPASSED,
+                    list: [{
+                        id: "check-3",
+                        canRun: true
+                    }, {
+                        id: "check-4",
+                        canRun: true
+                    }]
+                }
             }, {
                 id: "check-5",
                 canRun: true,
-                dependentChecks: [{
-                    id: "check-6",
-                    canRun: false,
-                    dependentChecks: []
-                }, {
-                    id: "check-7",
-                    canRun: true,
-                    dependentChecks: []
-                }]
+                dependentChecks: {
+                    passCondition: EVERYCHECKPASSED,
+                    list: [{
+                        id: "check-6",
+                        canRun: true
+                    }, {
+                        id: "check-7",
+                        canRun: true
+                    }]
+                }
+            }, {
+                id: "check-8",
+                canRun: true,
+                dependentChecks: {
+                    passCondition: EVERYCHECKPASSED,
+                    list: [{
+                        id: "check-9",
+                        canRun: false
+                    }, {
+                        id: "check-10",
+                        canRun: true
+                    }]
+                }
+            }, {
+                id: "check-11",
+                canRun: true,
+                dependentChecks: {
+                    passCondition: SOMECHECKSPASSED,
+                    list: [{
+                        id: "check-12",
+                        canRun: true
+                    }, {
+                        id: "check-13",
+                        canRun: true
+                    }]
+                }
             }];
 
         beforeAll(function () {
@@ -41,39 +71,61 @@ define([
         it('resolves a check with no dependent checks', function (done) {
             checkMediator.waitForCheck('check-1')
             .then(function(result) {
-                expect(result).toBe('result-1');
+                expect(result).toBe(true);
 
                 done();
             });
 
-            checkMediator.resolveCheck('check-1', 'result-1');
+            checkMediator.resolveCheck('check-1', true);
         });
 
-        it('resolves a check with multiple dependent checks', function (done) {            
+        it('resolves a check with dependent checks as true when passCondition is EVERYCHECKPASSED', function (done) {            
             checkMediator.waitForCheck('check-2')
-            .then(function(resultList) {
-                expect(resultList.length).toBe(2);
-                expect(resultList[0]).toBe('result-3');
-                expect(resultList[1]).toBe('result-4');
+            .then(function(result) {
+                expect(result).toBe(true);
 
                 done();
             });
 
-            checkMediator.resolveCheck('check-3', 'result-3');
-            checkMediator.resolveCheck('check-4', 'result-4');
+            checkMediator.resolveCheck('check-3', true);
+            checkMediator.resolveCheck('check-4', true);
         });
 
-        it('resolves a check with multiple dependent checks if dependent check canRun equals false', function (done) {            
+        it('resolves a check with dependent checks as false when passCondition is EVERYCHECKPASSED', function (done) {            
             checkMediator.waitForCheck('check-5')
-            .then(function(resultList) {
-                expect(resultList.length).toBe(1);
-                expect(resultList[0]).toBe('result-7');
+            .then(function(result) {
+                expect(result).toBe(false);
 
                 done();
             });
 
-            checkMediator.resolveCheck('check-6', 'result-6');
-            checkMediator.resolveCheck('check-7', 'result-7');
+            checkMediator.resolveCheck('check-6', true);
+            checkMediator.resolveCheck('check-7', false);
+        });
+
+
+        it('resolves a check with dependent checks as true when passCondition is SOMECHECKSPASSED', function (done) {            
+            checkMediator.waitForCheck('check-11')
+            .then(function(result) {
+                expect(result).toBe(true);
+
+                done();
+            });
+
+            checkMediator.resolveCheck('check-12', true);
+            checkMediator.resolveCheck('check-13', false);
+        });
+
+        it('resolves a check with dependent checks as true if dependent check canRun equals false', function (done) {            
+            checkMediator.waitForCheck('check-8')
+            .then(function(result) {
+                expect(result).toBe(true);
+
+                done();
+            });
+
+            checkMediator.resolveCheck('check-9', false);
+            checkMediator.resolveCheck('check-10', true);
         });
 
         it('rejects a check if not registered', function (done) {           
