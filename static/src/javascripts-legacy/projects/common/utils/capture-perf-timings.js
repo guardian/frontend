8825180a73c3ffbd14742feea97ae1a98e5f19c1
Simon Adcock <simon.adcock@guardian.co.uk>
2017-02-20 12:09:18 +0000

    all we hear is radix gaga

--- static/src/javascripts-legacy/projects/common/utils/capture-perf-timings.js
+++ static/src/javascripts-legacy/projects/common/utils/capture-perf-timings.js
@@ -27,7 +27,7 @@ define([
             assetsPerformance: marks.map(function (mark) {
                 return {
                     name: mark,
-                    timing: parseInt(userTiming.getTiming(mark) || 0),
+                    timing: parseInt(userTiming.getTiming(mark) || 0, 10),
                 };
             }),
         };
