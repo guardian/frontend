00cc92bc1cc3b86463db171077d521371cfe6f74
Simon Adcock <simon.adcock@guardian.co.uk>
2017-02-20 09:19:03 +0000

    exit function early if timing unavailable

--- static/src/javascripts-legacy/projects/common/utils/capture-perf-timings.js
+++ static/src/javascripts-legacy/projects/common/utils/capture-perf-timings.js
@@ -4,6 +4,10 @@ define([
 ], function (ophan, userTiming) {
     return function captureTiming() {
         var timing = window.performance && window.performance.timing;
+        if (!timing) {
+            return;
+        }
+
         var marks = [
             'standard boot',
             'commercial request',
@@ -11,26 +15,23 @@ define([
             'enhanced request',
             'enhanced boot'
         ];
-        var performance;
+        var performance = {
+            dns: timing.domainLookupEnd - timing.domainLookupStart,
+            connection: timing.connectEnd - timing.connectStart,
+            firstByte: timing.responseStart - timing.connectEnd,
+            lastByte: timing.responseEnd - timing.responseStart,
+            domContentLoadedEvent: timing.domContentLoadedEventStart - timing.responseEnd,
+            loadEvent: timing.loadEventStart - timing.domContentLoadedEventStart,
+            navType: window.performance.navigation.type,
+            redirectCount: window.performance.navigation.redirectCount,
+            assetsPerformance: marks.map(function (mark) {
+                return {
+                    name: mark,
+                    timing: parseInt(userTiming.getTiming(mark)),
+                };
+            }),
+        };
 
-        if (timing) {
-            performance = {
-                dns: timing.domainLookupEnd - timing.domainLookupStart,
-                connection: timing.connectEnd - timing.connectStart,
-                firstByte: timing.responseStart - timing.connectEnd,
-                lastByte: timing.responseEnd - timing.responseStart,
-                domContentLoadedEvent: timing.domContentLoadedEventStart - timing.responseEnd,
-                loadEvent: timing.loadEventStart - timing.domContentLoadedEventStart,
-                navType: window.performance.navigation.type,
-                redirectCount: window.performance.navigation.redirectCount,
-                assetsPerformance: marks.map(function(mark) {
-                    return {
-                        name: mark,
-                        timing: parseInt(userTiming.getTiming(mark)),
-                    };
-                }),
-            };
-            ophan.record({ performance: performance });
-        }
+        ophan.record({ performance: performance });
     }
 });
