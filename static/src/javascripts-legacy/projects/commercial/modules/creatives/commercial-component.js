7a9af6dce66593631d0636457e9d0b3bdbb7f6ff
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-02-22 11:02:19 +0000

    Remove legacy Books component

--- static/src/javascripts-legacy/projects/commercial/modules/creatives/commercial-component.js
+++ static/src/javascripts-legacy/projects/commercial/modules/creatives/commercial-component.js
@@ -33,22 +33,9 @@ define([
         capiSingle:     complexUrlBuilder('capi-single', false, false, true),
         capi:           complexUrlBuilder('capi', false, false, true),
         paidforCard:    complexUrlBuilder('paid', '', false, true),
-        books:          complexUrlBuilder('books/books', 'isbns'),
         masterclasses:  complexUrlBuilder('masterclasses', 'ids', true),
-        liveevents:     complexUrlBuilder('liveevents/event', 'id', true),
-        book:           bookUrlBuilder('books/book')
+        liveevents:     complexUrlBuilder('liveevents/event', 'id', true)
     };
-
-    function bookUrlBuilder(url) {
-        return function (params) {
-            var isbn = config.page.isbn || params.isbn;
-            if (isbn) {
-                return buildComponentUrl(url, merge(params, { t: isbn }));
-            } else {
-                return false;
-            }
-        };
-    }
 
     function complexUrlBuilder(url, withSpecificId, withKeywords, withSection) {
         return function (params) {
@@ -119,7 +106,6 @@ define([
      * @param {Object=} params
      */
     function CommercialComponent(adSlot, params) {
-        if(params.type == 'book') fastdom.write(function() { $(adSlot).addClass('ad-slot--books-inline');});
         this.params = params || {};
         this.type = this.params.type;
         // remove type from params
