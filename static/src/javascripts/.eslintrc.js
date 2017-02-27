30be6bb1d62e7f7a64782aff6adecdbd3fc36712
Alex Sanders <a@snd.rs>
2017-02-27 11:30:46 +0000

    scala strings are double quotes, lets copy it

--- static/src/javascripts/.eslintrc.js
+++ static/src/javascripts/.eslintrc.js
@@ -4,49 +4,49 @@
 
 module.exports = {
     settings: {
-        'import/resolver': 'webpack',
+        "import/resolver": "webpack",
     },
-    extends: ['prettier'],
-    plugins: ['guardian-frontend', 'prettier'],
+    extends: ["prettier"],
+    plugins: ["guardian-frontend", "prettier"],
     rules: {
         // require-specific overrides
-        'import/no-dynamic-require': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'import/extensions': 'off',
-        'import/no-webpack-loader-syntax': 'off', // used for require plugins still
-        'import/no-amd': 'off', // webpack dynamic requires
-        'global-require': 'off',
-        'id-blacklist': ['error', 'guardian'],
+        "import/no-dynamic-require": "off",
+        "import/no-extraneous-dependencies": "off",
+        "import/extensions": "off",
+        "import/no-webpack-loader-syntax": "off", // used for require plugins still
+        "import/no-amd": "off", // webpack dynamic requires
+        "global-require": "off",
+        "id-blacklist": ["error", "guardian"],
 
         // these are bad habits in react that we're already abusing.
         // if we go more [p]react we should look at them.
         // not saying it's ok, but we don't reuse modules or
         // develop this stuff much. disabling for now.
-        'react/prefer-es6-class': 'off',
-        'react/no-multi-comp': 'off',
-        'react/no-find-dom-node': 'off',
-        'react/jsx-no-bind': 'off',
-        'react/no-deprecated': 'off', // still on 0.13
-        'react/prop-types': 'off',
-        'react/no-string-refs': 'off',
-        'react/prefer-stateless-function': 'off',
-        'react/no-render-return-value': 'off',
+        "react/prefer-es6-class": "off",
+        "react/no-multi-comp": "off",
+        "react/no-find-dom-node": "off",
+        "react/jsx-no-bind": "off",
+        "react/no-deprecated": "off", // still on 0.13
+        "react/prop-types": "off",
+        "react/no-string-refs": "off",
+        "react/prefer-stateless-function": "off",
+        "react/no-render-return-value": "off",
 
         // disallow modules we used to use but are retiring
-        'no-restricted-imports': [
-            'error',
+        "no-restricted-imports": [
+            "error",
             {
-                paths: ['lodash'],
-                patterns: ['!lodash/*'],
+                paths: ["lodash"],
+                patterns: ["!lodash/*"],
             },
         ],
 
         // prettier settings
-        'prettier/prettier': [
-            'error',
+        "prettier/prettier": [
+            "error",
             {
-                trailingComma: 'es5',
-                singleQuote: true,
+                trailingComma: "es5",
+                singleQuote: false,
                 bracketSpacing: true,
                 tabWidth: 4,
             },
@@ -54,6 +54,6 @@ module.exports = {
 
         // our own rules for frontend
         // live in tools/eslint-plugin-guardian-frontend
-        'guardian-frontend/global-config': 2,
+        "guardian-frontend/global-config": 2,
     },
 };
--- static/src/javascripts/boot-webpack.js
+++ static/src/javascripts/boot-webpack.js
@@ -1,9 +1,9 @@
-import domready from 'domready';
-import raven from 'common/utils/raven';
-import bootStandard from 'bootstraps/standard/main';
-import config from 'common/utils/config';
-import userTiming from 'common/utils/user-timing';
-import capturePerfTimings from 'common/utils/capture-perf-timings';
+import domready from "domready";
+import raven from "common/utils/raven";
+import bootStandard from "bootstraps/standard/main";
+import config from "common/utils/config";
+import userTiming from "common/utils/user-timing";
+import capturePerfTimings from "common/utils/capture-perf-timings";
 
 // let webpack know where to get files from
 // __webpack_public_path__ is a special webpack variable
@@ -13,7 +13,7 @@ __webpack_public_path__ = `${config.page.assetsPath}javascripts/`;
 
 domready(() => {
     // 1. boot standard, always
-    userTiming.mark('standard boot');
+    userTiming.mark("standard boot");
     bootStandard();
 
     // 2. once standard is done, next is commercial
@@ -23,32 +23,32 @@ domready(() => {
                 const needsMessage = isInUse &&
                     window.console &&
                     window.console.warn;
-                const message = 'Do you have an adblocker enabled? Commercial features might fail to run, or throw exceptions.';
+                const message = "Do you have an adblocker enabled? Commercial features might fail to run, or throw exceptions.";
                 if (needsMessage) {
                     window.console.warn(message);
                 }
             });
         }
 
-        userTiming.mark('commercial request');
+        userTiming.mark("commercial request");
         require(
-            ['bootstraps/commercial'],
-            raven.wrap({ tags: { feature: 'commercial' } }, commercial => {
-                userTiming.mark('commercial boot');
+            ["bootstraps/commercial"],
+            raven.wrap({ tags: { feature: "commercial" } }, commercial => {
+                userTiming.mark("commercial boot");
                 commercial.init();
 
                 // 3. finally, try enhanced
                 // this is defined here so that webpack's code-splitting algo
                 // excludes all the modules bundled in the commercial chunk from this one
                 if (window.guardian.isEnhanced) {
-                    userTiming.mark('enhanced request');
-                    require(['bootstraps/enhanced/main'], bootEnhanced => {
-                        userTiming.mark('enhanced boot');
+                    userTiming.mark("enhanced request");
+                    require(["bootstraps/enhanced/main"], bootEnhanced => {
+                        userTiming.mark("enhanced boot");
                         bootEnhanced();
-                        if (document.readyState === 'complete') {
+                        if (document.readyState === "complete") {
                             capturePerfTimings();
                         } else {
-                            window.addEventListener('load', capturePerfTimings);
+                            window.addEventListener("load", capturePerfTimings);
                         }
                     });
                 }
