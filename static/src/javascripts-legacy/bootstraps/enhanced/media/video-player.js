4e9a4edafbde9a2c983dc29010478873da9484ba
Simon Adcock <simon.adcock@guardian.co.uk>
2017-02-27 15:28:13 +0000

    remove alias for videojs-contrib-ads

--- static/src/javascripts-legacy/bootstraps/enhanced/media/video-player.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/media/video-player.js
@@ -4,13 +4,8 @@ define([
     'videojs-ima',
     'videojs-embed',
     'videojs-persistvolume',
-    'videojs-playlist'
+    'videojs-playlist',
+    'videojs-contrib-ads'
 ], function (config, video) {
-    // #wp-rjs
-    // In rjs this module is shimmed in the config
-    // https://github.com/guardian/frontend/blob/webpack-redux/tools/__tasks__/compile/javascript/rjs.config.js#L42
-    // We can move this up to the define() when we go 100% WP
-    require('videojs-ads-lib');
-
     return video;
 });
