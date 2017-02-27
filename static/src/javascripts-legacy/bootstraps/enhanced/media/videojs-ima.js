281740d8805f9fdd304d84a3cd7d4fe800e7e897
Simon Adcock <simon.adcock@guardian.co.uk>
2017-02-27 15:38:18 +0000

    remove videojs shim modules

--- static/src/javascripts-legacy/bootstraps/enhanced/media/videojs-ima.js
+++
@@ -1,3 +0,0 @@
-// This is only used when we're not using hashedBundles.
-// When using hashedBundles, we use requirejs's shim.
-define(['js!videojs-ads-lib', 'js!videojs-ima-lib'], function () {});
