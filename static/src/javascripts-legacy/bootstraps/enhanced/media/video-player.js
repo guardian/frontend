a15eadf5f36fda984ada7b1c89f29bdb3fa64538
Simon Adcock <simon.adcock@guardian.co.uk>
2017-02-28 15:25:58 +0000

    reinstall contrib-ads as ima does not bring its own version

--- static/src/javascripts-legacy/bootstraps/enhanced/media/video-player.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/media/video-player.js
@@ -3,7 +3,8 @@ define([
     'videojs-ima',
     'videojs-embed',
     'videojs-persistvolume',
-    'videojs-playlist'
+    'videojs-playlist',
+    'videojs-contrib-ads'
 ], function (video) {
     return video;
 });
