3b69d2a9f4b8995fbd2a3f8a080b0b0447b8b231
Simon Adcock <simon.adcock@guardian.co.uk>
2017-03-01 15:27:45 +0000

    transform projects/common/utils/defer-to-analytics.js to es6 module

--- static/src/javascripts/projects/common/utils/defer-to-analytics.js
+++ static/src/javascripts/projects/common/utils/defer-to-analytics.js
@@ -1,25 +1,19 @@
-define([
-    'common/utils/mediator'
-], function (
-    mediator
-) {
+import mediator from 'common/utils/mediator';
 
-    var analyticsReady = false;
+var analyticsReady = false;
 
-    mediator.on('analytics:ready', function () {
-        analyticsReady = true;
-    });
+mediator.on('analytics:ready', function() {
+    analyticsReady = true;
+});
 
-    function deferToAnalytics(afterAnalytics) {
-        if (analyticsReady) {
+function deferToAnalytics(afterAnalytics) {
+    if (analyticsReady) {
+        afterAnalytics();
+    } else {
+        mediator.on('analytics:ready', function() {
             afterAnalytics();
-        } else {
-            mediator.on('analytics:ready', function () {
-                afterAnalytics();
-            });
-        }
+        });
     }
+}
 
-    return deferToAnalytics;
-
-}); // define
+export default deferToAnalytics; // define
