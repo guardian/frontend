52223dc93e98f1d65f07b918cb1e4d18b5a1e38d
Alex Sanders <a@snd.rs>
2017-03-07 17:37:26 +0000

    transform lib/$.js to ES6 module

--- static/src/javascripts/lib/$.js
+++ static/src/javascripts/lib/$.js
@@ -1,45 +1,37 @@
-define([
-    'bonzo',
-    'qwery',
-    'lodash/collections/forEach'
-], function (
-    bonzo,
-    qwery,
-    forEach
-) {
+import bonzo from 'bonzo';
+import qwery from 'qwery';
+import forEach from 'lodash/collections/forEach';
 
-    // Warning: side effect. This patches the bonzo module for use everywhere
-    bonzo.aug({
-        height: function () {
-            return this.dim().height;
-        }
-    });
-
-    function $(selector, context) {
-        return bonzo(qwery(selector, context));
+// Warning: side effect. This patches the bonzo module for use everywhere
+bonzo.aug({
+    height: function() {
+        return this.dim().height;
     }
+});
 
-    $.create = function (s) {
-        return bonzo(bonzo.create(s));
-    };
+function $(selector, context) {
+    return bonzo(qwery(selector, context));
+}
 
-    $.ancestor = function (el, c) {
-        if (el.nodeName.toLowerCase() === 'html') {
-            return false;
-        }
-        if (!el.parentNode || bonzo(el.parentNode).hasClass(c)) {
-            return el.parentNode;
-        } else {
-            return $.ancestor(el.parentNode, c);
-        }
-    };
+$.create = function(s) {
+    return bonzo(bonzo.create(s));
+};
 
-    $.forEachElement = function (selector, fn) {
-        var els = qwery(selector);
-        forEach(els, fn);
-        return els;
-    };
+$.ancestor = function(el, c) {
+    if (el.nodeName.toLowerCase() === 'html') {
+        return false;
+    }
+    if (!el.parentNode || bonzo(el.parentNode).hasClass(c)) {
+        return el.parentNode;
+    } else {
+        return $.ancestor(el.parentNode, c);
+    }
+};
 
-    return $;
+$.forEachElement = function(selector, fn) {
+    var els = qwery(selector);
+    forEach(els, fn);
+    return els;
+};
 
-}); // define
+export default $; // define
