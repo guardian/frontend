84bdf74108d330af551d2a93421772e2b5108930
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-03-07 11:10:29 +0000

    Remove JS-based fix for previous galleries

--- static/src/javascripts-legacy/bootstraps/enhanced/gallery.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/gallery.js
@@ -1,43 +1,17 @@
 define([
-    'bean',
-    'bonzo',
     'qwery',
-    'lib/$',
     'lib/config',
     'lib/mediator',
     'common/modules/component',
-    'bootstraps/enhanced/trail',
-    'lodash/functions/debounce'
+    'bootstraps/enhanced/trail'
 ], function (
-    bean,
-    bonzo,
     qwery,
-    $,
     config,
     mediator,
     Component,
-    trail,
-    debounce
+    trail
 ) {
-    var verticallyResponsiveImages = function () {
-            var setHeight = function () {
-                if (!bonzo(document.body).hasClass('has-overlay')) {
-                    var $imgs = $('.js-gallery-img'),
-                        min = 300, // stops images getting too small
-                        max = $imgs.parent().dim().width, // portrait images shouldn't be taller than landscapes are wide
-                        height = Math.max(min, Math.min(max, window.innerHeight * 0.9));
-                    $imgs.css('max-height', height);
-                }
-            };
-
-            setHeight();
-            mediator.addListeners({
-                'window:throttledResize': setHeight,
-                'window:orientationchange': debounce(setHeight, 200),
-                'ui:images:vh': setHeight
-            });
-        },
-        transcludeMostPopular = function () {
+    var transcludeMostPopular = function () {
             var mostViewed = new Component(),
                 container = qwery('.js-gallery-most-popular')[0];
 
@@ -50,7 +24,6 @@ define([
         },
         ready = function () {
             trail();
-            verticallyResponsiveImages();
 
             mediator.emit('ui:images:upgradePictures');
 
