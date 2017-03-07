8c6d79ffc51686201a05b38d0fcba63cacfb58ed
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-07 13:37:52 +0000

    transform projects/common/modules/sport/football/match-info.js to ES6 module

--- static/src/javascripts/projects/common/modules/sport/football/match-info.js
+++ static/src/javascripts/projects/common/modules/sport/football/match-info.js
@@ -1,35 +1,28 @@
-define([
-    'lib/$',
-    'lib/ajax'
-], function (
-    $,
-    ajax
-) {
+import $ from 'lib/$';
+import ajax from 'lib/ajax';
 
-    /**
-     * @param {Object} match
-     * @param {string} whosCalling (url)
-     */
-    var MatchInfo = function (match, whosCalling) {
-        this.endpoint += (match.id ? match.id : [match.date].concat(match.teams).join('/')) +
-                '.json?page=' + encodeURIComponent(whosCalling);
-    };
+/**
+ * @param {Object} match
+ * @param {string} whosCalling (url)
+ */
+var MatchInfo = function(match, whosCalling) {
+    this.endpoint += (match.id ? match.id : [match.date].concat(match.teams).join('/')) +
+        '.json?page=' + encodeURIComponent(whosCalling);
+};
 
-    /**
-     * @type {string}
-     */
-    MatchInfo.prototype.endpoint = '/football/api/match-nav/';
+/**
+ * @type {string}
+ */
+MatchInfo.prototype.endpoint = '/football/api/match-nav/';
 
-    /**
-     * @return Reqwest
-     */
-    MatchInfo.prototype.fetch = function () {
-        return ajax({
-            crossOrigin: true,
-            url: this.endpoint
-        });
-    };
+/**
+ * @return Reqwest
+ */
+MatchInfo.prototype.fetch = function() {
+    return ajax({
+        crossOrigin: true,
+        url: this.endpoint
+    });
+};
 
-    return MatchInfo;
-
-}); // define
+export default MatchInfo; // define
