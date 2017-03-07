18a7e5a3906ffd63a8f8e189959494604d99b2b3
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-07 13:37:53 +0000

    transform projects/common/modules/sport/football/match-info.js content to ES6

--- static/src/javascripts/projects/common/modules/sport/football/match-info.js
+++ static/src/javascripts/projects/common/modules/sport/football/match-info.js
@@ -5,24 +5,26 @@ import ajax from 'lib/ajax';
  * @param {Object} match
  * @param {string} whosCalling (url)
  */
-var MatchInfo = function(match, whosCalling) {
-    this.endpoint += (match.id ? match.id : [match.date].concat(match.teams).join('/')) +
-        '.json?page=' + encodeURIComponent(whosCalling);
-};
+class MatchInfo {
+ constructor(match, whosCalling) {
+     this.endpoint += (match.id ? match.id : [match.date].concat(match.teams).join('/')) +
+         '.json?page=' + encodeURIComponent(whosCalling);
+ }
+
+ /**
+  * @return Reqwest
+  */
+ fetch() {
+     return ajax({
+         crossOrigin: true,
+         url: this.endpoint
+     });
+ }
+}
 
 /**
  * @type {string}
  */
 MatchInfo.prototype.endpoint = '/football/api/match-nav/';
-
-/**
- * @return Reqwest
- */
-MatchInfo.prototype.fetch = function() {
-    return ajax({
-        crossOrigin: true,
-        url: this.endpoint
-    });
-};
 
 export default MatchInfo; // define
