7d8ae331bc13fe203c646c5de14f264d3fa1773f
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-07 14:32:14 +0000

    Convert modules/sport/football/match-info to ES6

--- static/src/javascripts/projects/common/modules/sport/football/match-info.js
+++ static/src/javascripts/projects/common/modules/sport/football/match-info.js
@@ -1,30 +1,34 @@
-import $ from 'lib/$';
-import ajax from 'lib/ajax';
+import fetch from 'lib/fetch';
 
 /**
  * @param {Object} match
  * @param {string} whosCalling (url)
  */
-class MatchInfo {
- constructor(match, whosCalling) {
-     this.endpoint += (match.id ? match.id : [match.date].concat(match.teams).join('/')) +
-         '.json?page=' + encodeURIComponent(whosCalling);
- }
 
- /**
-  * @return Reqwest
-  */
- fetch() {
-     return ajax({
-         crossOrigin: true,
-         url: this.endpoint
-     });
- }
+class MatchInfo {
+    constructor(match, whosCalling) {
+        const urlBase = '/football/api/match-nav';
+        const pageSlug = encodeURIComponent(whosCalling);
+        let matchSlug = match.id;
+
+        if (match.id) {
+            matchSlug = match.id;
+        } else {
+            matchSlug = [match.date].concat(match.teams).join('/');
+        }
+
+        this.endpoint = `${urlBase}/${matchSlug}.json?page=${pageSlug}`;
+    }
+
+    /**
+     * @return Reqwest
+     */
+
+    fetch() {
+        return fetch(this.endpoint, {
+            mode: 'cors',
+        });
+    }
 }
 
-/**
- * @type {string}
- */
-MatchInfo.prototype.endpoint = '/football/api/match-nav/';
-
-export default MatchInfo; // define
+export default MatchInfo;
