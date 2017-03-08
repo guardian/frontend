a5b0bdd1e7198a7c7e0ab7cdcce4db97e0d6dc26
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-08 17:25:35 +0000

    transform lib/closest.js to ES6 module

--- static/src/javascripts/lib/closest.js
+++ static/src/javascripts/lib/closest.js
@@ -1,17 +1,15 @@
-define(function () {
-    /**
-     * Adds a wrapper around Element.closest()
-     */
-    return 'closest' in Element.prototype ? closestNative : closestPolyfill;
+/**
+ * Adds a wrapper around Element.closest()
+ */
+export default 'closest' in Element.prototype ? closestNative : closestPolyfill;
 
-    function closestNative(element, selectors) {
-        return element.closest(selectors);
-    }
+function closestNative(element, selectors) {
+    return element.closest(selectors);
+}
 
-    function closestPolyfill(element, selectors) {
-        while (element && !element.matches(selectors)) {
-            element = element.parentNode;
-        }
-        return element;
+function closestPolyfill(element, selectors) {
+    while (element && !element.matches(selectors)) {
+        element = element.parentNode;
     }
-});
+    return element;
+}
