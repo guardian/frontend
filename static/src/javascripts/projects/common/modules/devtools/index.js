45a1b321681e5ce5549dfa24bb352fbd64d8821a
Simon Adcock <simon.adcock@guardian.co.uk>
2017-03-09 16:56:33 +0000

    check selected radios

--- static/src/javascripts/projects/common/modules/devtools/index.js
+++ static/src/javascripts/projects/common/modules/devtools/index.js
@@ -2,19 +2,37 @@ import template from 'lodash/utilities/template';
 import storage from 'lib/storage';
 import $ from 'lib/$';
 import bean from 'bean';
+import find from 'lodash/collections/find';
 import overlay from 'raw-loader!common/views/devtools/overlay.html';
 import getAbTests from 'common/modules/experiments/get-ab-tests';
+
+function getSelectedAbTests() {
+    return JSON.parse(storage.local.get('gu.devtools.ab')) || [];
+}
 
 function bindEvents() {
     $('.js-devtools-force-ab').each(label => {
         bean.on(label, 'click', () => {
             const testId = label.getAttribute('data-ab-test');
             const variantId = label.getAttribute('data-ab-variant');
-            const abTests = JSON.parse(storage.local.get('gu.devtools.ab')) || [];
+            const abTests = getSelectedAbTests();
+            const existingVariantForThisTest = find(abTests, { id: testId });
 
-            abTests.push({id: testId, variant: variantId});
+            if (existingVariantForThisTest) {
+                existingVariantForThisTest.variant = variantId;
+            } else {
+                abTests.push({id: testId, variant: variantId});
+            }
             storage.local.set('gu.devtools.ab', JSON.stringify(abTests));
         });
+    });
+}
+
+function selectRadios() {
+    const abTests = getSelectedAbTests();
+
+    abTests.forEach((test) => {
+       $(`#${test.id}-${test.variant}`).attr('checked', true);
     });
 }
 
@@ -24,8 +42,8 @@ export default function showDevTools() {
     const data = {
         tests: tests.map(test => ({ id: test.id, variants: test.variants })),
     };
-    console.log(tests);
 
     header.append(template(overlay, data));
     bindEvents();
+    selectRadios();
 }
