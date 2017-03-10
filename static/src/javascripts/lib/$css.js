938091142bc86985d302e9645c497d47e79a58e9
Alex Sanders <a@snd.rs>
2017-03-10 16:09:11 +0000

    convert $css to es6

--- static/src/javascripts/lib/$css.js
+++ static/src/javascripts/lib/$css.js
@@ -1,12 +1,8 @@
-define(function () {
-
-    /**
-     * x-browser function to get a style value from a bonzo object
-     */
-    return function ($el, prop) {
-        // bonzo needs these - use currentStyle (not as reliable?) if unavailable (e.g. IE8)
-        return (window.document.defaultView && window.document.defaultView.getComputedStyle)
-            ? $el.css(prop) : $el[0].currentStyle[prop];
-    };
-
-});
+// @flow
+// x-browser function to get a style value from a bonzo object
+// bonzo needs these - use currentStyle (not as reliable?) if unavailable (e.g. IE8)
+// #? – we don't support IE8
+export default ($el: bonzo, prop: string) =>
+    window.document.defaultView && window.document.defaultView.getComputedStyle
+        ? $el.css(prop)
+        : $el[0].currentStyle[prop];
