a9ec83b059f0387a852ccdb612bc09f6ea914e8f
Michael McNamara <michael.mcnamara@guardian.co.uk>
2017-03-10 17:57:04 +0000

    removing include credentials switch

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -498,14 +498,4 @@ trait FeatureSwitches {
     exposeClientSide = true
   )
 
-  val AmpOphanTrackingIncludesCredentials = Switch(
-    SwitchGroup.Feature,
-    "amp-ophan-tracking-includes-credentials",
-    "allows access to id field when page viewed from google cache so that we keep tracking consistent",
-    owners = Seq(Owner.withGithub("michaelwmcnamara")),
-    safeState = Off,
-    sellByDate = new LocalDate(2017, 3, 10),
-    exposeClientSide = true
-
-  )
 }
