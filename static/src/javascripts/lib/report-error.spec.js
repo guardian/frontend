75fc704627936c5e30d86284b5b8458872e8edf2
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-13 11:50:13 +0000

    Add basic test

--- static/src/javascripts/lib/report-error.spec.js
+++ static/src/javascripts/lib/report-error.spec.js
@@ -3,12 +3,20 @@
 import reportError from './report-error';
 
 describe('report-error', () => {
-    const error = new Error('Something broke');
+    const error = new Error('Something broke.');
     const meta = {
         test: true,
     };
 
+    test('Does not throw an error', () => {
+        expect(() => {
+            reportError(error, meta, false);
+        }).not.toThrowError(error);
+    });
+
     test('Throws an error', () => {
-        expect(reportError(error, meta)).toThrow('Something broke');
+        expect(() => {
+            reportError(error, meta);
+        }).toThrowError(error);
     });
 });
