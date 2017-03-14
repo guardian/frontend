84ca3c030f26f98c9bb0382bc3703fc764ed6447
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-14 11:24:03 +0000

    transform lib/config.js content to ES6

--- static/src/javascripts/lib/config.js
+++ static/src/javascripts/lib/config.js
@@ -5,31 +5,27 @@ import pad from 'lib/pad';
 import urlUtils from 'lib/url';
 import assign from 'lodash/objects/assign';
 // eslint-disable-next-line guardian-frontend/global-config
-var config = window.guardian.config;
+const config = window.guardian.config;
 
 export default assign({
-    hasTone: function(name) {
+    hasTone(name) {
         return (this.page.tones || '').indexOf(name) > -1;
     },
-    hasSeries: function(name) {
+    hasSeries(name) {
         return (this.page.series || '').indexOf(name) > -1;
     },
-    referencesOfType: function(name) {
-        return (this.page.references || []).filter(function(reference) {
-            return typeof reference[name] !== 'undefined';
-        }).map(function(reference) {
-            return reference[name];
-        });
+    referencesOfType(name) {
+        return (this.page.references || []).filter(reference => typeof reference[name] !== 'undefined').map(reference => reference[name]);
     },
-    referenceOfType: function(name) {
+    referenceOfType(name) {
         return this.referencesOfType(name)[0];
     },
 
     // the date nicely formatted and padded for use as part of a url
     // looks like    2012/04/31
-    webPublicationDateAsUrlPart: function() {
+    webPublicationDateAsUrlPart() {
         if (this.page.webPublicationDate) {
-            var pubDate = new Date(this.page.webPublicationDate);
+            const pubDate = new Date(this.page.webPublicationDate);
             return pubDate.getFullYear() + '/' +
                 pad(pubDate.getMonth() + 1, 2) + '/' +
                 pad(pubDate.getDate(), 2);
@@ -37,8 +33,8 @@ export default assign({
     },
 
     // returns 2014/apr/22
-    dateFromSlug: function() {
-        var s = this.page.pageId.match(/\d{4}\/\w{3}\/\d{2}/);
+    dateFromSlug() {
+        const s = this.page.pageId.match(/\d{4}\/\w{3}\/\d{2}/);
         return s ? s[0] : null;
     },
 
