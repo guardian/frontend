98cc9ba20970a84a5479e928b7b8ba3346920442
Simon Adcock <simon.adcock@guardian.co.uk>
2017-03-14 16:00:08 +0000

    WIP

---
+++ static/src/javascripts/lib/fetch-json.spec.js
@@ -0,0 +1,58 @@
+import fetchJson from 'lib/fetch-json';
+
+jest.mock('lib/config', () => ({
+    page: {
+        ajaxUrl: 'foo'
+    }
+}));
+
+describe('Fetch JSON util', function () {
+    beforeEach(function () {
+        this.xhr = sinon.useFakeXMLHttpRequest();
+        var requests = this.requests = [];
+        this.xhr.onCreate = function (xhr) {
+            requests.push(xhr);
+        };
+    });
+    afterEach(function () {
+        this.xhr.restore();
+    });
+
+    it('returns a promise which rejects on network errors', function (done) {
+        fetchJson('error-path')
+            .then(done.fail)
+            .catch(function (ex) {
+                expect(ex instanceof Error).toBe(true, 'rejects an error');
+                expect(ex.message).toMatch(/fetch error/i);
+            })
+            .then(done)
+            .catch(done.fail);
+
+        this.requests[0].respond(0, {}, 'invalid');
+    });
+
+    it('returns a promise which rejects invalid json responses', function (done) {
+        fetchJson('404-error-response')
+            .catch(function (ex) {
+                expect(ex instanceof Error).toBe(true, 'rejects an error');
+                expect(ex.message).toMatch(/json/i);
+            })
+            .then(done)
+            .catch(done.fail);
+
+        this.requests[0].respond(200, {}, 'Plain text');
+    });
+
+    it('resolves a correct response in json', function (done) {
+        fetchJson('correct-json')
+            .then(function (response) {
+                expect(response).toEqual({
+                    json: true
+                });
+            })
+            .then(done)
+            .catch(done.fail);
+
+        this.requests[0].respond(200, {}, '{"json":true}');
+    });
+});
