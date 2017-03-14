b44deadee7927302243658689c90aefc622803bd
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-14 14:05:33 +0000

    Satisfy js linter

--- tools/__tasks__/validate/sass.js
+++ tools/__tasks__/validate/sass.js
@@ -2,7 +2,7 @@ const execa = require('execa');
 
 module.exports = {
     description: 'Lint Sass',
-    task: ctx =>
+    task: () =>
         execa('sass-lint', ['--verbose', '--no-exit']).stdout.pipe(
             process.stdout
         ),
