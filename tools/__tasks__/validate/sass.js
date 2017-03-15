0292fe0562de69a701d1ebbddac990fd963595fb
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-15 12:11:55 +0000

    Simplify execa call again

--- tools/__tasks__/validate/sass.js
+++ tools/__tasks__/validate/sass.js
@@ -1,14 +1,4 @@
-const execa = require('execa');
-
 module.exports = {
     description: 'Lint Sass',
-    task: ctx => execa('sass-lint', ['--verbose', '--no-exit']).then(res => {
-        if (!res.stdout) {
-            return ctx;
-        }
-
-        const error = new Error('sass-lint failed');
-        error.stdout = res.stdout;
-        throw error;
-    }),
+    task: 'sass-lint --no-exit --verbose --max-warnings 0',
 };
