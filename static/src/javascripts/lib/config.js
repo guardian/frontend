0b968c4a24f4d9ac8ebd4fee5b33539177ce8a45
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-15 13:46:08 +0000

    Refactor functions out of config object so we don't have to use this

--- static/src/javascripts/lib/config.js
+++ static/src/javascripts/lib/config.js
@@ -4,41 +4,50 @@ import pad from 'lib/pad';
 // eslint-disable-next-line guardian-frontend/global-config
 const config = window.guardian.config;
 
+function hasTone(name: string): boolean {
+    return (config.page.tones || '').indexOf(name) > -1;
+}
+
+function hasSeries(name: string): boolean {
+    return (config.page.series || '').indexOf(name) > -1;
+}
+
+function referencesOfType(name: string): Array<string> {
+    return (config.page.references || [])
+        .filter(reference => typeof reference[name] !== 'undefined')
+        .map(reference => reference[name]);
+}
+
+function referenceOfType(name: string): string {
+    return referencesOfType(name)[0];
+}
+
+// the date nicely formatted and padded for use as part of a url
+// looks like    2012/04/31
+function webPublicationDateAsUrlPart(): ?string {
+    if (config.page.webPublicationDate) {
+        const pubDate = new Date(config.page.webPublicationDate);
+        return `${pubDate.getFullYear()}/${pad(pubDate.getMonth() + 1, 2)}/${pad(pubDate.getDate(), 2)}`;
+    }
+
+    return null;
+}
+
+// returns 2014/apr/22
+function dateFromSlug(): ?string {
+    const s = config.page.pageId.match(/\d{4}\/\w{3}\/\d{2}/);
+    return s ? s[0] : null;
+}
+
 export default Object.assign(
     {
-        hasTone(name) {
-            return (this.page.tones || '').indexOf(name) > -1;
-        },
-        hasSeries(name) {
-            return (this.page.series || '').indexOf(name) > -1;
-        },
-        referencesOfType(name) {
-            return (this.page.references || [])
-                .filter(reference => typeof reference[name] !== 'undefined')
-                .map(reference => reference[name]);
-        },
-        referenceOfType(name) {
-            return this.referencesOfType(name)[0];
-        },
-
-        // the date nicely formatted and padded for use as part of a url
-        // looks like    2012/04/31
-        webPublicationDateAsUrlPart() {
-            if (this.page.webPublicationDate) {
-                const pubDate = new Date(this.page.webPublicationDate);
-                return `${pubDate.getFullYear()}/${pad(pubDate.getMonth() + 1, 2)}/${pad(pubDate.getDate(), 2)}`;
-            }
-
-            return null;
-        },
-
-        // returns 2014/apr/22
-        dateFromSlug() {
-            const s = this.page.pageId.match(/\d{4}\/\w{3}\/\d{2}/);
-            return s ? s[0] : null;
-        },
-
-        isMedia: ['Video', 'Audio'].indexOf(config.page.contentType) > -1,
+        hasTone,
+        hasSeries,
+        referencesOfType,
+        referenceOfType,
+        webPublicationDateAsUrlPart,
+        dateFromSlug,
+        isMedia: ['Video', 'Audio'].some(config.page.contentType),
     },
     config
 );
