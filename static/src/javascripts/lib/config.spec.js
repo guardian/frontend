644172d356e3033937134e849c5ed5acf60e0e5d
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-15 18:36:29 +0000

    Hoist the setting of window.guardian.config above the import

--- static/src/javascripts/lib/config.spec.js
+++ static/src/javascripts/lib/config.spec.js
@@ -1,10 +1,8 @@
 // @flow
+/* eslint-disable guardian-frontend/global-config */
 
-import config from './config';
-
-jest.mock('lib/pad', () => jest.fn(arg => arg));
-
-const mockConfig = Object.assign(config, {
+const prevConfig = window.guardian.config;
+Object.assign(window.guardian.config, {
     page: {
         tones: 'foo',
         series: 'bar',
@@ -14,32 +12,43 @@ const mockConfig = Object.assign(config, {
     },
 });
 
+// We need to hoist setting the window.guardian.config to code under test above import
+// (but can't do it with the jest mocks that get auto-hoisted)
+// eslint-disable-next-line import/first
+import config from './config';
+
+jest.mock('lib/pad', () => jest.fn(arg => arg));
+
+afterAll(() => {
+    window.guardian.config = prevConfig;
+});
+
 describe('Config', () => {
     it('should have "hasTone" property', () => {
-        expect(mockConfig.hasTone('foo')).toBeTruthy();
-        expect(mockConfig.hasTone('foo-bad')).toBeFalsy();
+        expect(config.hasTone('foo')).toBeTruthy();
+        expect(config.hasTone('foo-bad')).toBeFalsy();
     });
 
     it('should have "hasSeries" property', () => {
-        expect(mockConfig.hasSeries('bar')).toBeTruthy();
-        expect(mockConfig.hasSeries('bar-bad')).toBeFalsy();
+        expect(config.hasSeries('bar')).toBeTruthy();
+        expect(config.hasSeries('bar-bad')).toBeFalsy();
     });
 
     it('should have "referencesOfType" property', () => {
-        expect(mockConfig.referencesOfType('baz')).toEqual(['one', 'two']);
-        expect(mockConfig.referencesOfType('bar-bad')).toEqual([]);
+        expect(config.referencesOfType('baz')).toEqual(['one', 'two']);
+        expect(config.referencesOfType('bar-bad')).toEqual([]);
     });
 
     it('should have "referenceOfType" property', () => {
-        expect(mockConfig.referenceOfType('baz')).toEqual('one');
-        expect(mockConfig.referenceOfType('bar-bad')).toBeUndefined();
+        expect(config.referenceOfType('baz')).toEqual('one');
+        expect(config.referenceOfType('bar-bad')).toBeUndefined();
     });
 
     it('should have "webPublicationDateAsUrlPart" property', () => {
-        expect(mockConfig.webPublicationDateAsUrlPart()).toBe('2013/12/20');
+        expect(config.webPublicationDateAsUrlPart()).toBe('2013/12/20');
     });
 
     it('should return the expected dateFromSlug', () => {
-        expect(mockConfig.dateFromSlug()).toBe('2017/mar/14');
+        expect(config.dateFromSlug()).toBe('2017/mar/14');
     });
 });
