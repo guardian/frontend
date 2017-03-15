138f0d483379e7231021463bf1242dfbff4a4f5b
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2017-03-15 15:49:27 +0000

    simplify time selection handlers

--- static/src/javascripts-legacy/projects/common/modules/experiments/tests/reading-time.js
+++ static/src/javascripts-legacy/projects/common/modules/experiments/tests/reading-time.js
@@ -50,16 +50,13 @@ define([
         this.variants = [
             {
                 id: 'static-links',
-                test: function () {
-                    insertReadingTimeSelection();
-                },
                 success: function(complete) {
                     insertReadingTimeSelection(complete)
                 }
             },
             {
                 id: 'control',
-                test: function () {},
+                test: function () {}
             }
         ];
 
@@ -78,37 +75,24 @@ define([
         function registerTimeSelectionHandlers(section, completeFunction) {
             var arrow = '<div class="btn"><svg width="24" height="24" viewBox="240 334 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" transform="translate(240 334)"><path fill="#F6F6F6" d="M4 12.68h13.3l-5.22 6.15.68.67L20 12.34v-.68L12.76 4.5l-.68.67 5.2 6.15H4"/></path></g></svg></div>';
 
-            bean.on($('.js-feedback-button-5', section)[0], 'click', function() {
-                var suggestion = 'If you have five minutes, you might enjoy our latest photo gallery  <a href="https://www.theguardian.com/news/series/ten-best-photographs-of-the-day/latest">'+arrow+'</a>';
+            var suggestion5 = 'If you have five minutes, you might enjoy our latest photo gallery  <a href="https://www.theguardian.com/news/series/ten-best-photographs-of-the-day/latest">'+arrow+'</a>';
+            var suggestion15 = 'If you have fifteen minutes, you might enjoy our latest politics live blog  <a href="https://www.theguardian.com/politics/series/politics-live-with-andrew-sparrow/latest">'+arrow+'</a>';
+            var suggestion30 = 'If you have thirty minutes, you might enjoy our latest long read  <a href="https://www.theguardian.com/news/series/the-long-read/latest">'+arrow+'</a>';
 
-                replaceWithLink(suggestion);
-                completeEvent(completeFunction);
-            });
-
-            bean.on($('.js-feedback-button-15', section)[0], 'click', function() {
-                var suggestion = 'If you have fifteen minutes, you might enjoy our latest politics live blog  <a href="https://www.theguardian.com/politics/series/politics-live-with-andrew-sparrow/latest">'+arrow+'</a>';
-
-                replaceWithLink(suggestion);
-                completeEvent(completeFunction);
-            });
-
-            bean.on($('.js-feedback-button-30', section)[0], 'click', function() {
-                var suggestion = 'If you have thirty minutes, you might enjoy our latest long read  <a href="https://www.theguardian.com/news/series/the-long-read/latest">'+arrow+'</a>';
-
-                replaceWithLink(suggestion);
-                completeEvent(completeFunction);
-            });
+            bean.on($('.js-feedback-button-5', section)[0], 'click', ctaReplacer(suggestion5, completeFunction));
+            bean.on($('.js-feedback-button-15', section)[0], 'click', ctaReplacer(suggestion15, completeFunction));
+            bean.on($('.js-feedback-button-30', section)[0], 'click', ctaReplacer(suggestion30, completeFunction));
         }
 
-        function replaceWithLink(suggestionHtml){
-            $('.cta').empty();
-            $('.cta').html(suggestionHtml);
-            $('.selections').empty();
-        }
+        function ctaReplacer(suggestion, completeFunction) {
+            return function () {
+                $('.cta').empty();
+                $('.cta').html(suggestion);
+                $('.selections').empty();
 
-        function completeEvent(completeFunction){
-            if (completeFunction) {
-                completeFunction();
+                if (completeFunction) {
+                    completeFunction();
+                }
             }
         }
     }
