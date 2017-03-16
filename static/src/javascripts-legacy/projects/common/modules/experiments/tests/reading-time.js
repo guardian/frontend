aff371fdbfb3130adeee7a93e635d3752347a57a
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2017-03-16 15:26:02 +0000

    better names for css classes and remove unnecessarily complicated scaling of svgs

--- static/src/javascripts-legacy/projects/common/modules/experiments/tests/reading-time.js
+++ static/src/javascripts-legacy/projects/common/modules/experiments/tests/reading-time.js
@@ -50,9 +50,7 @@ define([
         this.variants = [
             {
                 id: 'static-links',
-                success: function(complete) {
-                    insertReadingTimeSelection(complete)
-                }
+                success: insertReadingTimeSelection
             },
             {
                 id: 'control',
@@ -73,7 +71,7 @@ define([
         }
 
         function registerTimeSelectionHandlers(section, completeFunction) {
-            var arrow = '<div class="btn"><svg width="24" height="24" viewBox="240 334 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" transform="translate(240 334)"><path fill="#F6F6F6" d="M4 12.68h13.3l-5.22 6.15.68.67L20 12.34v-.68L12.76 4.5l-.68.67 5.2 6.15H4"/></path></g></svg></div>';
+            var arrow = '<div class="reading-time__arrow"><svg width="24" height="24" viewBox="240 334 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" transform="translate(240 334)"><path fill="#F6F6F6" d="M4 12.68h13.3l-5.22 6.15.68.67L20 12.34v-.68L12.76 4.5l-.68.67 5.2 6.15H4"/></path></g></svg></div>';
 
             var suggestion5 = 'If you have five minutes, you might enjoy our latest photo gallery  <a href="https://www.theguardian.com/news/series/ten-best-photographs-of-the-day/latest">'+arrow+'</a>';
             var suggestion15 = 'If you have fifteen minutes, you might enjoy our latest politics live blog  <a href="https://www.theguardian.com/politics/series/politics-live-with-andrew-sparrow/latest">'+arrow+'</a>';
@@ -86,9 +84,8 @@ define([
 
         function ctaReplacer(suggestion, completeFunction) {
             return function () {
-                $('.cta').empty();
-                $('.cta').html(suggestion);
-                $('.selections').empty();
+                $('.js-feedback-text-reading-time__cta').html(suggestion);
+                $('.reading-time__selections').empty();
 
                 if (completeFunction) {
                     completeFunction();
