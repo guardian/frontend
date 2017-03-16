aed01822ae69aceabbb0725c87f868b66006a0a9
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-16 10:31:02 +0000

    Convert everything to arrow functions

--- static/src/javascripts/lib/config.js
+++ static/src/javascripts/lib/config.js
@@ -7,40 +7,35 @@ import pad from 'lib/pad';
 // eslint-disable-next-line guardian-frontend/global-config
 const config = window.guardian.config;
 
-function hasTone(name: string): boolean {
-    return (config.page.tones || '').indexOf(name) > -1;
-}
+const hasTone = (name: string): boolean =>
+    (config.page.tones || '').indexOf(name) > -1;
 
-function hasSeries(name: string): boolean {
-    return (config.page.series || '').indexOf(name) > -1;
-}
+const hasSeries = (name: string): boolean =>
+    (config.page.series || '').indexOf(name) > -1;
 
-function referencesOfType(name: string): Array<string> {
-    return (config.page.references || [])
+const referencesOfType = (name: string): Array<string> =>
+    (config.page.references || [])
         .filter(reference => typeof reference[name] !== 'undefined')
         .map(reference => reference[name]);
-}
 
-function referenceOfType(name: string): string {
-    return referencesOfType(name)[0];
-}
+const referenceOfType = (name: string): string => referencesOfType(name)[0];
 
 // the date nicely formatted and padded for use as part of a url
 // looks like    2012/04/31
-function webPublicationDateAsUrlPart(): ?string {
+const webPublicationDateAsUrlPart = (): ?string => {
     if (config.page.webPublicationDate) {
         const pubDate = new Date(config.page.webPublicationDate);
         return `${pubDate.getFullYear()}/${pad(pubDate.getMonth() + 1, 2)}/${pad(pubDate.getDate(), 2)}`;
     }
 
     return null;
-}
+};
 
 // returns 2014/apr/22
-function dateFromSlug(): ?string {
+const dateFromSlug = (): ?string => {
     const s = config.page.pageId.match(/\d{4}\/\w{3}\/\d{2}/);
     return s ? s[0] : null;
-}
+};
 
 const isMedia: boolean = ['Video', 'Audio'].indexOf(config.page.contentType) >
     -1;
