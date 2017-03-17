33e2e73cda1fb785648673fb0dca653965ebe269
Alex Sanders <a@snd.rs>
2017-03-17 16:19:58 +0000

    better comments

--- common/app/views/support/Polyfillio.scala
+++ common/app/views/support/Polyfillio.scala
@@ -4,11 +4,18 @@ object Polyfillio {
   /*
     when changing these options, be sure to update
     static/vendor/javascripts/polyfillio.fallback.js
-    with the non-minifed version of the reponse,
-    but requested with:
-    &ua=qwerty&unknown=polyfill
 
     it's loaded as the fallback in the event this URL fails
+
+    you can just browse to the same url defined below,
+    but grab the non-minifed version of the response,
+    with these additional params:
+    &ua=qwerty&unknown=polyfill
+
+    this will give us a full fat version containing everything, but gated,
+    so that it will actually patch what is needs to.
+
+    downside is the file size.
   */
   val url: String = "https://polyfill.guim.co.uk/v2/polyfill.min.js?" +
     "features=es6,es7,default-3.6" +
