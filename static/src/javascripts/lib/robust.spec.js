ac40870412370e514f8b2f25a4c39ce0e036759f
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-21 11:57:55 +0000

    Add more tests around `catchErrorsAndLog()`

--- static/src/javascripts/lib/robust.spec.js
+++ static/src/javascripts/lib/robust.spec.js
@@ -1,29 +1,51 @@
 // @flow
 
+import reportError from 'lib/report-error';
 import robust from './robust';
 
+window.console.warn = jest.fn();
+
+jest.mock('lib/report-error', () => jest.fn());
+
 describe('robust', () => {
+    const ERROR = new Error('Something broke.');
+    const META = { module: 'test' };
+
     function noError() {
         return true;
     }
 
     function throwError() {
-        throw new Error('Something broke.');
+        throw ERROR;
     }
 
-    test('catchErrorsAndLog', () => {
+    test('catchErrorsAndLog()', () => {
         expect(() => {
             robust.catchErrorsAndLog('test', noError);
         }).not.toThrowError();
 
         expect(() => {
             robust.catchErrorsAndLog('test', throwError);
-        }).toThrowError();
+        }).not.toThrowError(ERROR);
     });
 
-    test('catchErrorsAndLog - custom reporter', () => {
+    test('catchErrorsAndLog() - default reporter', () => {
+        reportError.mockClear();
+        robust.catchErrorsAndLog('test', noError);
+        expect(reportError).not.toHaveBeenCalled();
+
+        reportError.mockClear();
+        robust.catchErrorsAndLog('test', throwError);
+        expect(reportError).toHaveBeenCalledWith(ERROR, META, false);
+    });
+
+    test('catchErrorsAndLog() - custom reporter', () => {
+        const mockedCallback = jest.fn();
+        robust.catchErrorsAndLog('test', noError, mockedCallback);
+        expect(mockedCallback).not.toHaveBeenCalled();
+
         robust.catchErrorsAndLog('test', throwError, (err, meta) => {
-            expect(err.message).toBe('Something broke.');
+            expect(err.message).toBe(ERROR.message);
             expect(meta.module).toBe('test');
         });
     });
