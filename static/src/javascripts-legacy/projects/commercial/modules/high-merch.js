8f3e91afbbce340c9420041751629165771f331f
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-03-21 14:38:44 +0000

    Load alt slot even if high merch is disabled

--- static/src/javascripts-legacy/projects/commercial/modules/high-merch.js
+++ static/src/javascripts-legacy/projects/commercial/modules/high-merch.js
@@ -20,33 +20,35 @@ define([
     }
 
     function init() {
-        if (!commercialFeatures.highMerch) {
-            return Promise.resolve();
-        }
+        if (commercialFeatures.highMerch) {
+            var anchorSelector = config.page.commentable ? '#comments + *' : '.content-footer > :first-child';
+            var anchor = document.querySelector(anchorSelector);
+            var container = document.createElement('div');
 
-        var anchorSelector = config.page.commentable ? '#comments + *' : '.content-footer > :first-child';
-        var anchor = document.querySelector(anchorSelector);
-        var container = document.createElement('div');
+            container.className = 'fc-container fc-container--commercial';
+            container.appendChild(createSlot(config.page.isPaidContent ? 'high-merch-paid' : 'high-merch'));
 
-        container.className = 'fc-container fc-container--commercial';
-        container.appendChild(createSlot(config.page.isPaidContent ? 'high-merch-paid' : 'high-merch'));
-
-        if (commercialFeatures.outbrain && isLuckyBastard()) {
-            trackAdRender('dfp-ad--merchandising-high')
-            .then(function (isHiResLoaded) {
-                return Promise.all([
-                    isHiResLoaded,
-                    isHiResLoaded ? trackAdRender('dfp-ad--merchandising') : true
-                ]);
-            })
-            .then(insertAlternativeSlot);
-        }
-
-        return fastdom.write(function () {
-            if (anchor && anchor.parentNode) {
-                anchor.parentNode.insertBefore(container, anchor);
+            if (commercialFeatures.outbrain && isLuckyBastard()) {
+                trackAdRender('dfp-ad--merchandising-high')
+                .then(function (isHiResLoaded) {
+                    return Promise.all([
+                        isHiResLoaded,
+                        isHiResLoaded ? trackAdRender('dfp-ad--merchandising') : true
+                    ]);
+                })
+                .then(insertAlternativeSlot);
             }
-        });
+
+            return fastdom.write(function () {
+                if (anchor && anchor.parentNode) {
+                    anchor.parentNode.insertBefore(container, anchor);
+                }
+            });
+        } else if (commercialFeatures.outbrain && isLuckyBastard()) {
+            insertAlternativeSlot(false, false);
+        }
+
+        return Promise.resolve();
     }
 
     function insertAlternativeSlot(args) {
