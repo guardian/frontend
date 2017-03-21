b66b60316dbd6fc7a1001841974dca77d6f61756
Alex Sanders <a@snd.rs>
2017-03-21 12:47:07 +0000

    typo

--- common/app/templates/inlineJS/blocking/loadApp.scala.js
+++ common/app/templates/inlineJS/blocking/loadApp.scala.js
@@ -4,15 +4,16 @@
 @import conf.switches.Switches.{PolyfillIO}
 
 // the app is bundled without any polyfills. babel transpiles the syntax
-// to es5 but we do provide any polyfills for missing methods.
+// to es5 but we do not provide any polyfills for missing methods.
 // for that, we use polyfill.io.
 //
 // since that's a possbile point of failure, we have a checked-in copy of
 // the pf.io response for *all* polyfills that we will serve to everyone
 // as a worst-case back up.
 //
-// they're gated, meaning they won't patch if they don't need to, but
-// they're still downloaded (currently ~20 kB gzipped)
+// in that situation, they're gated, meaning they won't patch if they don't
+// need to, but they're still downloaded (currently ~20 kB gzipped) and
+// the file is still parsed
 
 // this is a global that's called at the bottom of the pf.io response,
 // once the polyfills have run.
