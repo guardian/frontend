a0ea1636d4c745b249ee507fa95fcf813dd8e8ed
Alex Sanders <a@snd.rs>
2017-03-21 11:33:09 +0000

    dontdotdash

--- common/app/conf/switches/PerformanceSwitches.scala
+++ common/app/conf/switches/PerformanceSwitches.scala
@@ -129,7 +129,7 @@ trait PerformanceSwitches {
 
   val PolyfillIO = Switch(
     SwitchGroup.Performance,
-    "polyfill.io",
+    "polyfill-io",
     "If this switch is on we will attempt to load polyfills from polyfill.io. If it is off, only our (full, larger) fullback will be loaded.",
     owners = Seq(Owner.withGithub("sndrs")),
     safeState = Off,
