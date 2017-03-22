906fd61019a03e2db40170158678584d0d5fba19
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-03-22 16:14:58 +0000

    Linting

--- static/src/javascripts-legacy/projects/commercial/modules/dfp/queue-advert.js
+++ static/src/javascripts-legacy/projects/commercial/modules/dfp/queue-advert.js
@@ -3,7 +3,7 @@ define([
 ], function (dfpEnv) {
     return queueAdvert;
 
-    function queueAdvert(advert, index) {
+    function queueAdvert(advert) {
         dfpEnv.advertsToLoad.push(advert);
         // Add to the array of ads to be refreshed (when the breakpoint changes)
         // only if its `data-refresh` attribute isn't set to false.
