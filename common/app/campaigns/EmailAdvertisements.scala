13b2d3fd14299a1aac2aed4cf136bf54c506f198
David Furey <david.furey@guardian.co.uk>
2017-03-22 11:36:04 +0000

    Update LiveIntent ids for Guardian Today US email

--- common/app/campaigns/EmailAdvertisements.scala
+++ common/app/campaigns/EmailAdvertisements.scala
@@ -9,14 +9,14 @@ object EmailAdvertisements {
   val mpu = Map(
     guardianTodayUs -> LiveIntentMPU(
       newsletterId = guardianTodayUs.replace("/", "-"),
-      ids = ("226945", "226946", "226947", "226948", "226949")
+      ids = ("228579", "228580", "228581", "108656", "108657")
     )
   )
 
   val safeRtb = Map(
     guardianTodayUs -> LiveIntentSafeRTB(
       newsletterId = guardianTodayUs.replace("/", "-"),
-      ids = (124760900 to 124760919).map(_.toString).toList
+      ids = (123626500 to 123626519).map(_.toString).toList
     )
   )
 }
