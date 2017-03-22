74941749e87c541590453a5870d10c1458da8ab9
Simon Adcock <simon.adcock@guardian.co.uk>
2017-03-22 12:46:45 +0000

    update init type to Object

--- static/src/javascripts/lib/fetch-json.js
+++ static/src/javascripts/lib/fetch-json.js
@@ -2,7 +2,7 @@
 import config from 'lib/config';
 import fetch from 'lib/fetch';
 
-function json(input: string, init: any = {}) {
+function json(input: string, init: Object = {}) {
     // #? we should use Object.assign for this assignment, but this currently breaks
     // Karma tests that depend on fetch-json.js and have not been stubbed
     const options = init;
