446b2c6b49c988d46f03759746a4d281882c6622
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-23 17:18:50 +0000

    transform lib/create-store.js to ES6 module

--- static/src/javascripts/lib/create-store.js
+++ static/src/javascripts/lib/create-store.js
@@ -1,26 +1,34 @@
-define([], function () {
-    // Mini Redux
-    var createStore = function (reducer, initialState) {
-        // We re-assign this over time
-        var state = initialState;
-        var subscribers = [];
+// Mini Redux
+var createStore = function(reducer, initialState) {
+    // We re-assign this over time
+    var state = initialState;
+    var subscribers = [];
 
-        var notify = function () { subscribers.forEach(function (fn) { fn(); }); };
-        var dispatch = function (action) {
-            state = reducer(state, action);
-            notify();
-        };
-        var subscribe = function (fn) { subscribers.push(fn); };
-        var getState = function () { return state; };
-
-        dispatch({ type: 'INIT' });
-
-        return {
-            dispatch: dispatch,
-            subscribe: subscribe,
-            getState: getState
-        };
+    var notify = function() {
+        subscribers.forEach(function(fn) {
+            fn();
+        });
+    };
+    var dispatch = function(action) {
+        state = reducer(state, action);
+        notify();
+    };
+    var subscribe = function(fn) {
+        subscribers.push(fn);
+    };
+    var getState = function() {
+        return state;
     };
 
-    return createStore;
-});
+    dispatch({
+        type: 'INIT'
+    });
+
+    return {
+        dispatch: dispatch,
+        subscribe: subscribe,
+        getState: getState
+    };
+};
+
+export default createStore;
