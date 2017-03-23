206d9496dc9b773171c6e81cdc4767b834fbb473
Alex Sanders <a@snd.rs>
2017-03-23 17:45:13 +0000

    this has one use, to catch IE8, which commerical doesn’t run in anyway

--- static/src/javascripts/lib/$css.js
+++
@@ -1,8 +0,0 @@
-// @flow
-// x-browser function to get a style value from a bonzo object
-// bonzo needs these - use currentStyle (not as reliable?) if unavailable (e.g. IE8)
-// #? – we don't support IE8
-export default ($el: bonzo, prop: string) =>
-    window.document.defaultView && window.document.defaultView.getComputedStyle
-        ? $el.css(prop)
-        : $el[0].currentStyle[prop];
