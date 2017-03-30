a3ea1cfcdb602a5dc342fe66846d9326aada2070
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-30 15:59:53 +0100

    Wait for polyfill to load before running tests

--- static/test/javascripts-legacy/main.js
+++ static/test/javascripts-legacy/main.js
@@ -47,10 +47,22 @@ requirejs.config({
     }
 });
 
-require(['Promise', 'lodash/collections/toArray'], function (Promise, toArray) {
-    require(tests, function () {
-        Promise.all(toArray(arguments)).then(function () {
-            window.__karma__.start();
+function requireTests () {
+    require(['Promise', 'lodash/collections/toArray'], function (Promise, toArray) {
+        require(tests, function () {
+            Promise.all(toArray(arguments)).then(window.__karma__.start);
         });
     });
-});
+}
+
+// This is here so that we can polyfill the already converted ES6 stuff without breaking karma tests.
+// I've simply pasted in the url because it's temp until karma no longer exists and unlikely to change before then.
+var ref = document.getElementsByTagName('script')[0];
+var appScript = document.createElement('script');
+appScript.src = 'https://assets.guim.co.uk/polyfill.io/v2/polyfill.min.js?rum=0&features=es6,es7,default-3.6&flags=gated&callback=guardianPolyfilled';
+ref.parentNode.insertBefore(appScript, ref);
+window.guardianPolyfilled = requireTests;
+
+
+
+
