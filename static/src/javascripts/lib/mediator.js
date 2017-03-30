4810f421a3827d9941f11a1284ea8bf14dfb08a3
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-30 17:52:44 +0100

    Convert `lib/mediator` to ES6

---
+++ static/src/javascripts/lib/mediator.js
@@ -0,0 +1,14 @@
+// @flow
+
+import EventEmitter from 'EventEmitter';
+
+const { app = {} } = window.guardian.app;
+
+if (!app.mediator) {
+    const mediator = new EventEmitter();
+
+    // a singleton instance of EventEmitter across the app
+    app.mediator = mediator;
+}
+
+export default app.mediator;
