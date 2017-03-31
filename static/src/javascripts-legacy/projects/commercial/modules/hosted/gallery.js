192f9f506f8aa8f4db05470802b4dc89f1aff790
Simon Adcock <simon.adcock@guardian.co.uk>
2017-03-31 12:30:51 +0100

    refactor gallery init to remove mediator

--- static/src/javascripts-legacy/projects/commercial/modules/hosted/gallery.js
+++ static/src/javascripts-legacy/projects/commercial/modules/hosted/gallery.js
@@ -450,32 +450,29 @@ define([
         start();
 
         if (qwery('.js-hosted-gallery-container').length) {
-            loadCssPromise
-            .then(function () {
-                var gallery,
-                    match,
-                    galleryHash = window.location.hash,
-                    res;
+            return loadCssPromise
+                .then(function () {
+                    var gallery,
+                        match,
+                        galleryHash = window.location.hash,
+                        res;
 
-                gallery = new HostedGallery();
-                match = /\?index=(\d+)/.exec(document.location.href);
-                if (match) { // index specified so launch gallery at that index
-                    gallery.loadAtIndex(parseInt(match[1], 10));
-                } else {
-                    res = /^#(?:img-)?(\d+)$/.exec(galleryHash);
-                    if (res) {
-                        gallery.loadAtIndex(parseInt(res[1], 10));
+                    gallery = new HostedGallery();
+                    match = /\?index=(\d+)/.exec(document.location.href);
+                    if (match) { // index specified so launch gallery at that index
+                        gallery.loadAtIndex(parseInt(match[1], 10));
+                    } else {
+                        res = /^#(?:img-)?(\d+)$/.exec(galleryHash);
+                        if (res) {
+                            gallery.loadAtIndex(parseInt(res[1], 10));
+                        }
                     }
-                }
-                return gallery;
-            })
-            .then(function (gallery) {
-                stop();
-                mediator.emit('page:hosted:gallery', gallery);
-            });
-        } else {
-            stop();
+                    stop();
+
+                    return gallery;
+                });
         }
+        stop();
 
         return Promise.resolve();
     }
