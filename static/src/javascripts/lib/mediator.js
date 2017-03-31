45804acd8615d558af2a502251cd981cf0af6ded
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-03-31 12:04:20 +0100

    Get rid of global weirdo

--- static/src/javascripts/lib/mediator.js
+++ static/src/javascripts/lib/mediator.js
@@ -2,13 +2,4 @@
 
 import EventEmitter from 'EventEmitter';
 
-const app = window.guardian.app = window.guardian.app || {};
-
-if (!app.mediator) {
-    const mediator = new EventEmitter();
-
-    // a singleton instance of EventEmitter across the app
-    app.mediator = mediator;
-}
-
-export default app.mediator;
+export default new EventEmitter();
