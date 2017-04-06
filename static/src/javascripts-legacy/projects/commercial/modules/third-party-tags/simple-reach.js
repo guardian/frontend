e8de667e325b4ee3b683fd2d4e31f9c144f99908
Kate Whalen <whalenka88@gmail.com>
2017-04-06 18:01:27 +0100

    First pass at basic global reach config, run checks, and script loading

---
+++ static/src/javascripts-legacy/projects/commercial/modules/third-party-tags/simple-reach.js
@@ -0,0 +1,31 @@
+define([
+    'lib/config'
+], function (
+    config
+) {
+
+  var authors = config.page.author.split(',');
+  var channels = config.page.sectionName.split(',');
+  var keywords = config.page.keywords.split(',');
+
+  window.__reach_config = {
+    pid: '000000000000000000000000',
+    title: config.page.headline,
+    date: new Date(config.page.webPublicationDate),
+    authors: authors,
+    channels: channels,
+    tags: keywords,
+    article_id: config.page.pageId,
+    ignore_errors: false
+  };
+
+  var simpleReachUrl = '//d8rk54i4mohrb.cloudfront.net/js/reach.js';
+
+  return {
+      shouldRun: !config.page.isFront &&
+        config.switches.simpleReach &&
+        config.page.isPaidContent,
+      url: simpleReachUrl
+  };
+
+});
