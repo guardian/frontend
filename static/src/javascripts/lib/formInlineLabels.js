47c98a4d7fdb193a83a1184a51abfdccb4171727
Calum Campbell <campbell.cj@hotmail.co.uk>
2017-04-10 15:01:00 +0100

    transform lib/formInlineLabels.js to ES6 module

--- static/src/javascripts/lib/formInlineLabels.js
+++ static/src/javascripts/lib/formInlineLabels.js
@@ -1,56 +1,50 @@
-define([
-    'bean',
-    'qwery',
-    'lib/$',
-    'fastdom'
-], function (
-    bean,
-    qwery,
-    $,
-    fastdom
-) {
-    /**
-     * @param {String} type 'add' or 'remove'
-     * @param {Object} $el $ element
-     * @param {String} cssClass The class to add or remove
-     * @param {Function} testFunc A callback function that should return boolean describing whether we
-     * should perform the add or remove class
-     */
-    function updateClass(type, $el, cssClass, testFunc) {
-        return function () {
-            // If we pass a boolean for test, then check if we should update the class
-            var updateClass = (testFunc !== undefined) ? testFunc() : true;
+import bean from 'bean';
+import qwery from 'qwery';
+import $ from 'lib/$';
+import fastdom from 'fastdom';
+/**
+ * @param {String} type 'add' or 'remove'
+ * @param {Object} $el $ element
+ * @param {String} cssClass The class to add or remove
+ * @param {Function} testFunc A callback function that should return boolean describing whether we
+ * should perform the add or remove class
+ */
+function updateClass(type, $el, cssClass, testFunc) {
+    return function() {
+        // If we pass a boolean for test, then check if we should update the class
+        var updateClass = (testFunc !== undefined) ? testFunc() : true;
 
-            if (updateClass) {
-                fastdom.write(function () {
-                    $el[(type === 'add') ? 'addClass' : 'removeClass'](cssClass);
-                });
-            }
-        };
-    }
-
-    return {
-        init: function (el, opts) {
-            var $el = $(el),
-                $input = $(opts.textInputClass, el),
-                $label = $(opts.labelClass, el),
-                hiddenLabelClass = opts.hiddenLabelClass,
-                labelEnabledClass = opts.labelEnabledClass;
-
-            // Add the js only styling class for inline label enabled
-            updateClass('add', $el, labelEnabledClass)();
-
-            // Check if the input val is empty and if not, hide the label
-            if ($input.val() !== '') { updateClass('add', $label, hiddenLabelClass)(); }
-
-            // Bind to focus and blur handlers to update class based on input
-            // Not delegated as bean doesn't support it on focus & blur
-            bean.on($input[0], {
-                focus: updateClass('add', $label, hiddenLabelClass),
-                blur: updateClass('remove', $label, hiddenLabelClass, function () {
-                    return $input.val() === '';
-                })
+        if (updateClass) {
+            fastdom.write(function() {
+                $el[(type === 'add') ? 'addClass' : 'removeClass'](cssClass);
             });
         }
     };
-});
+}
+
+export default {
+    init: function(el, opts) {
+        var $el = $(el),
+            $input = $(opts.textInputClass, el),
+            $label = $(opts.labelClass, el),
+            hiddenLabelClass = opts.hiddenLabelClass,
+            labelEnabledClass = opts.labelEnabledClass;
+
+        // Add the js only styling class for inline label enabled
+        updateClass('add', $el, labelEnabledClass)();
+
+        // Check if the input val is empty and if not, hide the label
+        if ($input.val() !== '') {
+            updateClass('add', $label, hiddenLabelClass)();
+        }
+
+        // Bind to focus and blur handlers to update class based on input
+        // Not delegated as bean doesn't support it on focus & blur
+        bean.on($input[0], {
+            focus: updateClass('add', $label, hiddenLabelClass),
+            blur: updateClass('remove', $label, hiddenLabelClass, function() {
+                return $input.val() === '';
+            })
+        });
+    }
+};
