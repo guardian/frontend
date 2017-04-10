fc4c6a50b281b1844f48f4c289de36fab9c33f9b
Kate Whalen <whalenka88@gmail.com>
2017-04-10 15:22:27 +0100

    Wrap SimpleReach, so we only set the vars and load the script when necessary

--- static/src/javascripts-legacy/projects/commercial/modules/third-party-tags/simple-reach.js
+++ static/src/javascripts-legacy/projects/commercial/modules/third-party-tags/simple-reach.js
@@ -4,22 +4,25 @@ define([
     config
 ) {
 
-  var authors = config.page.author.split(',');
-  var channels = config.page.sectionName.split(',');
-  var keywords = config.page.keywords.split(',');
+  if (!config.page.isFront && config.switches.simpleReach &&
+    config.page.isPaidContent) {
+      var authors = config.page.author.split(',');
+      var channels = config.page.sectionName.split(',');
+      var keywords = config.page.keywords.split(',');
 
-  window.__reach_config = {
-    pid: '000000000000000000000000',
-    title: config.page.headline,
-    date: new Date(config.page.webPublicationDate),
-    authors: authors,
-    channels: channels,
-    tags: keywords,
-    article_id: config.page.pageId,
-    ignore_errors: false
-  };
+      window.__reach_config = {
+        pid: '000000000000000000000000',
+        title: config.page.headline,
+        date: new Date(config.page.webPublicationDate),
+        authors: authors,
+        channels: channels,
+        tags: keywords,
+        article_id: config.page.pageId,
+        ignore_errors: false
+      };
 
-  var simpleReachUrl = '//d8rk54i4mohrb.cloudfront.net/js/reach.js';
+      var simpleReachUrl = '//d8rk54i4mohrb.cloudfront.net/js/reach.js';
+  }
 
   return {
       shouldRun: !config.page.isFront &&
