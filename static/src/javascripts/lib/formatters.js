cc1012684b043c50b78c08fd85d7399285d776db
Nicolas Long <nicolaslong@gmail.com>
2017-04-12 15:59:05 +0100

    Fix ES6 conversion issues and migrate test file

--- static/src/javascripts/lib/formatters.js
+++ static/src/javascripts/lib/formatters.js
@@ -1,20 +1,21 @@
-export default {
-    integerCommas(val) {
-        // commafy integers. see formatters.spec.js for expected input/output
-        const num = parseInt(val, 10);
+// @flow
+const integerCommas = (val: string | number): string | void => {
+    // commafy integers. see formatters.spec.js for expected input/output
+    const num = parseInt(val, 10);
 
-        let digits;
-        let i;
-        let len;
-        if (!isNaN(num)) {
-            digits = num.toFixed(0).split('');
-            len = digits.length;
-            for (i = digits.length - 1; i >= 1; i--) {
-                if ((len - i) % 3 === 0) {
-                    digits.splice(i, 0, ',');
-                }
+    let digits;
+    let i;
+    let len;
+    if (!isNaN(num)) {
+        digits = num.toFixed(0).split('');
+        len = digits.length;
+        for (i = digits.length - 1; i >= 1; i -= 1) {
+            if ((len - i) % 3 === 0) {
+                digits.splice(i, 0, ',');
             }
-            return digits.join('');
         }
+        return digits.join('');
     }
 };
+
+export default { integerCommas };
