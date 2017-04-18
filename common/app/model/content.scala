f2237b598fd50fd1c53cba3d20c0e4f871fd563a
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-04-18 15:09:29 +0100

    use 16x9 image for video page meta itemprop image

--- common/app/model/content.scala
+++ common/app/model/content.scala
@@ -106,10 +106,6 @@ final case class Content(
   lazy val showCircularBylinePicAtSide: Boolean =
     cardStyle == Feature && tags.hasLargeContributorImage && tags.contributors.length == 1 && !tags.isInteractive
 
-  lazy val signedArticleImage: String = {
-    ImgSrc(rawOpenGraphImage, Item1200)
-  }
-
   // read this before modifying: https://developers.facebook.com/docs/opengraph/howtos/maximizing-distribution-media-content#images
   lazy val openGraphImage: String = {
     if (isPaidContent && FacebookShareImageLogoOverlay.isSwitchedOn) {
