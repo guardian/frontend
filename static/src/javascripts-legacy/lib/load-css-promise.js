f08b3e1799e5e9cfc9fcbeff64a5e7808da41056
Calum Campbell <calum.campbell@guardian.co.uk>
2017-04-18 12:10:27 +0100

    Revert "Es6 lib load css promise"

---
+++ static/src/javascripts-legacy/lib/load-css-promise.js
@@ -0,0 +1,11 @@
+define(['Promise'], function (Promise) {
+    return new Promise(function (resolve) {
+        if (window.guardian.css.loaded) {
+            // CSS has loaded, go
+            resolve();
+        } else {
+            // Push a listener for when the JS loads
+            window.guardian.css.onLoad = resolve;
+        }
+    });
+});
