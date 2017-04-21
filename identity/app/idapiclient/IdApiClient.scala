cc728c2d169c85af6eb94adb0e3692f2588d11a1
Diego Vazquez Nanini <diego@vazqueznanini.com>
2017-04-21 14:21:09 +0100

    Fix the ID test failing on local

--- identity/app/idapiclient/IdApiClient.scala
+++ identity/app/idapiclient/IdApiClient.scala
@@ -3,5 +3,5 @@ package idapiclient
 import common.ExecutionContexts
 import conf.IdentityConfiguration
 
-class IdApiClient(http: IdDispatchAsyncHttpClient, jsonParser: IdApiJsonBodyParser, conf: IdentityConfiguration)
-  extends IdApi(http, jsonParser, conf) with ExecutionContexts
+class IdApiClient(http: IdDispatchAsyncHttpClient, jsonParser: IdApiJsonBodyParser, apiRoot: String, apiClientToken: String, accDeletionApiRoot: String, accDeletionApiKey: String)
+  extends IdApi(http, jsonParser, apiRoot, apiClientToken, accDeletionApiRoot, accDeletionApiKey) with ExecutionContexts
