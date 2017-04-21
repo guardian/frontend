2f87ac5029c0cc30157955ff6d94c268ae2cabba
Alex Sanders <a@snd.rs>
2017-04-21 14:16:16 +0100

    transform lib/add-event-listener.js to ES6 module

--- static/src/javascripts/lib/add-event-listener.js
+++ static/src/javascripts/lib/add-event-listener.js
@@ -1,28 +1,26 @@
-define(function () {
-    var supportsOptions = false;
-    try {
-        var opts = Object.defineProperty({}, 'passive', {
-            get: function() {
-                supportsOptions = true;
-            }
-        });
-        window.addEventListener('test', null, opts);
-    } catch (e) { /* noop */ }
-
-    return addEventListener;
-
-    function addEventListener(node, eventName, eventHandler, options) {
-        options = options || {};
-
-        if (supportsOptions) {
-            node.addEventListener(eventName, eventHandler, options);
-        } else if (options.once) {
-            node.addEventListener(eventName, function boundEventHandler(evt) {
-                eventHandler.call(this, evt);
-                node.removeEventListener(eventName, boundEventHandler);
-            }, options.capture);
-        } else {
-            node.addEventListener(eventName, eventHandler, options.capture);
+var supportsOptions = false;
+try {
+    var opts = Object.defineProperty({}, 'passive', {
+        get: function() {
+            supportsOptions = true;
         }
+    });
+    window.addEventListener('test', null, opts);
+} catch (e) { /* noop */ }
+
+export default addEventListener;
+
+function addEventListener(node, eventName, eventHandler, options) {
+    options = options || {};
+
+    if (supportsOptions) {
+        node.addEventListener(eventName, eventHandler, options);
+    } else if (options.once) {
+        node.addEventListener(eventName, function boundEventHandler(evt) {
+            eventHandler.call(this, evt);
+            node.removeEventListener(eventName, boundEventHandler);
+        }, options.capture);
+    } else {
+        node.addEventListener(eventName, eventHandler, options.capture);
     }
-});
+}
