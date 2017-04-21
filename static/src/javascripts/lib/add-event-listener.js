1b264faee3d35553c5249415725f561135c8ad91
Alex Sanders <a@snd.rs>
2017-04-21 14:37:07 +0100

    fix lint errors with lib/add-event-listener.js after transform to ES6

--- static/src/javascripts/lib/add-event-listener.js
+++ static/src/javascripts/lib/add-event-listener.js
@@ -1,24 +1,44 @@
+// @flow
+
 let supportsOptions = false;
+
 try {
-    const opts = Object.defineProperty({}, 'passive', {
-        get() {
-            supportsOptions = true;
-        }
-    });
+    const opts = Object.defineProperty(
+        {},
+        'passive',
+        ({
+            get() {
+                supportsOptions = true;
+            },
+        }: Object) // https://github.com/facebook/flow/issues/285
+    );
     window.addEventListener('test', null, opts);
-} catch (e) { /* noop */ }
+} catch (e) {
+    /* noop */
+}
+
+const addEventListener = (
+    node: Node,
+    name: string,
+    handler: Function,
+    options: Object = {}
+): void | false => {
+    if (node) {
+        if (supportsOptions) {
+            node.addEventListener(name, handler, options);
+        } else if (options.once) {
+            node.addEventListener(
+                name,
+                function boundHandler(evt) {
+                    handler.call(this, evt);
+                    node.removeEventListener(name, boundHandler);
+                },
+                options.capture
+            );
+        } else {
+            node.addEventListener(name, handler, options.capture);
+        }
+    }
+};
 
 export default addEventListener;
-
-function addEventListener(node, eventName, eventHandler, options = {}) {
-    if (supportsOptions) {
-        node.addEventListener(eventName, eventHandler, options);
-    } else if (options.once) {
-        node.addEventListener(eventName, function boundEventHandler(evt) {
-            eventHandler.call(this, evt);
-            node.removeEventListener(eventName, boundEventHandler);
-        }, options.capture);
-    } else {
-        node.addEventListener(eventName, eventHandler, options.capture);
-    }
-}
