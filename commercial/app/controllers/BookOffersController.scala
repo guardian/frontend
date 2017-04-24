8aa140a77b7668c085b6ff3e7b075db6065593a3
Jon Norman <jon.norman@guardian.co.uk>
2017-04-24 10:56:57 +0100

    Remove Future handling resulting from BookFinder.
    Multi code has also been cleaned up - adding types and
    making the case statement consistent in matching each
    type twice: once for when an ID is provided and once
    for when it is not.

--- commercial/app/controllers/BookOffersController.scala
+++ commercial/app/controllers/BookOffersController.scala
@@ -8,9 +8,7 @@ import model.Cached
 import play.api.libs.json.{JsNull, JsValue, Json}
 import play.api.mvc._
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.control.NonFatal
 
 class BookOffersController(bookFinder: BookFinder, bestsellersAgent: BestsellersAgent)
   extends Controller
@@ -19,44 +17,35 @@ class BookOffersController(bookFinder: BookFinder, bestsellersAgent: Bestsellers
   with implicits.Collections
   with implicits.Requests {
 
-  private def booksSample(isbns: Seq[String], segment: Segment): Future[Seq[Book]] =
-    bestsellersAgent.getSpecificBooks(isbns) map { specificBooks =>
-      (specificBooks ++ bestsellersAgent.bestsellersTargetedAt(segment)).distinctBy(_.isbn).take(4)
-    }
+  private def booksSample(isbns: Seq[String], segment: Segment): Seq[Book] =
+    (bestsellersAgent.getSpecificBooks(isbns) ++ bestsellersAgent.bestsellersTargetedAt(segment)).distinctBy(_.isbn).take(4)
 
   private def isValidIsbn(isbn: String): Boolean = (isbn forall (_.isDigit)) && (isbn.length == 10 || isbn.length == 13)
 
-  def getBook = Action.async { implicit request =>
+  def getBook = Action { implicit request =>
 
     lazy val failedLookupResult: Result = Cached(30.seconds)(JsonNotFound())(request)
-    lazy val badRequestResponse: Future[Result] = Future.successful(Cached(1.day)(JsonComponent(JsNull))(request))
+    lazy val badRequestResponse: Result = Cached(1.day)(JsonComponent(JsNull))(request)
 
     specificId match {
       case Some(isbn) if isValidIsbn(isbn) =>
-        bookFinder.findByIsbn(isbn) map {
-          _ map { book: Book =>
-            Cached(1.hour){ JsonComponent(Json.toJson(book)) }
-          } getOrElse failedLookupResult
-        } recover {
-          case NonFatal(e) =>
-            log.error("Book lookup failed.", e)
-            failedLookupResult
-        }
+        bookFinder.findByIsbn(isbn) map { book: Book =>
+            Cached(1.hour)(JsonComponent(Json.toJson(book)))
+        } getOrElse failedLookupResult
       case Some(invalidIsbn) =>
         log.error(s"Book lookup called with invalid ISBN '$invalidIsbn'. Returning empty response.")
         badRequestResponse
       case None =>
-        log.error(s"Book lookup called with no ISBN. Returning empty response.");
+        log.error(s"Book lookup called with no ISBN. Returning empty response.")
         badRequestResponse
+
     }
   }
 
-  def getBooks = Action.async { implicit request =>
-    booksSample(specificIds, segment) map { books =>
-      val json: JsValue = Json.toJson(books)
+  def getBooks = Action { implicit request =>
+      val json: JsValue = Json.toJson(booksSample(specificIds, segment))
       Cached(60.seconds){
         JsonComponent(json)
       }
     }
-  }
 }
