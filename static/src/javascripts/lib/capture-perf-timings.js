20b861627a4761704a1d4d1b3d3d4ebc11243967
Calum Campbell <campbell.cj@hotmail.co.uk>
2017-04-24 12:08:24 +0100

    transform lib/capture-perf-timings.js content to ES6

--- static/src/javascripts/lib/capture-perf-timings.js
+++ static/src/javascripts/lib/capture-perf-timings.js
@@ -2,23 +2,23 @@ import ophan from 'ophan/ng';
 import userTiming from 'lib/user-timing';
 import performanceAPI from 'lib/window-performance';
 export default function captureTiming() {
-    var supportsPerformanceProperties = 'navigation' in performanceAPI &&
+    const supportsPerformanceProperties = 'navigation' in performanceAPI &&
         'timing' in performanceAPI;
 
     if (!supportsPerformanceProperties) {
         return;
     }
 
-    var timing = performanceAPI && performanceAPI.timing;
+    const timing = performanceAPI && performanceAPI.timing;
 
-    var marks = [
+    const marks = [
         'standard boot',
         'commercial request',
         'commercial boot',
         'enhanced request',
         'enhanced boot'
     ];
-    var performance = {
+    const performance = {
         dns: timing.domainLookupEnd - timing.domainLookupStart,
         connection: timing.connectEnd - timing.connectStart,
         firstByte: timing.responseStart - timing.connectEnd,
@@ -27,15 +27,13 @@ export default function captureTiming() {
         loadEvent: timing.loadEventStart - timing.domContentLoadedEventStart,
         navType: performanceAPI.navigation.type,
         redirectCount: performanceAPI.navigation.redirectCount,
-        assetsPerformance: marks.map(function(mark) {
-            return {
-                name: mark,
-                timing: parseInt(userTiming.getMarkTime(mark) || 0, 10),
-            };
-        }),
+        assetsPerformance: marks.map(mark => ({
+            name: mark,
+            timing: parseInt(userTiming.getMarkTime(mark) || 0, 10)
+        })),
     };
 
     ophan.record({
-        performance: performance
+        performance
     });
 }
