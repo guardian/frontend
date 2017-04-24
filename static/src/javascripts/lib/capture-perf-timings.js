f322ae61ccd2ea0e86dfc7cebf8088823c9193f4
Calum Campbell <campbell.cj@hotmail.co.uk>
2017-04-24 12:08:22 +0100

    transform lib/capture-perf-timings.js to ES6 module

--- static/src/javascripts/lib/capture-perf-timings.js
+++ static/src/javascripts/lib/capture-perf-timings.js
@@ -1,42 +1,41 @@
-define([
-    'ophan/ng',
-    'lib/user-timing',
-    'lib/window-performance',
-], function (ophan, userTiming, performanceAPI) {
-    return function captureTiming() {
-        var supportsPerformanceProperties = 'navigation' in performanceAPI &&
-                                            'timing' in performanceAPI;
+import ophan from 'ophan/ng';
+import userTiming from 'lib/user-timing';
+import performanceAPI from 'lib/window-performance';
+export default function captureTiming() {
+    var supportsPerformanceProperties = 'navigation' in performanceAPI &&
+        'timing' in performanceAPI;
 
-        if (!supportsPerformanceProperties) {
-            return;
-        }
-
-        var timing = performanceAPI && performanceAPI.timing;
-
-        var marks = [
-            'standard boot',
-            'commercial request',
-            'commercial boot',
-            'enhanced request',
-            'enhanced boot'
-        ];
-        var performance = {
-            dns: timing.domainLookupEnd - timing.domainLookupStart,
-            connection: timing.connectEnd - timing.connectStart,
-            firstByte: timing.responseStart - timing.connectEnd,
-            lastByte: timing.responseEnd - timing.responseStart,
-            domContentLoadedEvent: timing.domContentLoadedEventStart - timing.responseEnd,
-            loadEvent: timing.loadEventStart - timing.domContentLoadedEventStart,
-            navType: performanceAPI.navigation.type,
-            redirectCount: performanceAPI.navigation.redirectCount,
-            assetsPerformance: marks.map(function (mark) {
-                return {
-                    name: mark,
-                    timing: parseInt(userTiming.getMarkTime(mark) || 0, 10),
-                };
-            }),
-        };
-
-        ophan.record({ performance: performance });
+    if (!supportsPerformanceProperties) {
+        return;
     }
-});
+
+    var timing = performanceAPI && performanceAPI.timing;
+
+    var marks = [
+        'standard boot',
+        'commercial request',
+        'commercial boot',
+        'enhanced request',
+        'enhanced boot'
+    ];
+    var performance = {
+        dns: timing.domainLookupEnd - timing.domainLookupStart,
+        connection: timing.connectEnd - timing.connectStart,
+        firstByte: timing.responseStart - timing.connectEnd,
+        lastByte: timing.responseEnd - timing.responseStart,
+        domContentLoadedEvent: timing.domContentLoadedEventStart - timing.responseEnd,
+        loadEvent: timing.loadEventStart - timing.domContentLoadedEventStart,
+        navType: performanceAPI.navigation.type,
+        redirectCount: performanceAPI.navigation.redirectCount,
+        assetsPerformance: marks.map(function(mark) {
+            return {
+                name: mark,
+                timing: parseInt(userTiming.getMarkTime(mark) || 0, 10),
+            };
+        }),
+    };
+
+    ophan.record({
+        performance: performance
+    });
+}
