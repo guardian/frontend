192b288f2780ec1010976719a6eb441fc9df9f42
Alex Sanders <a@snd.rs>
2017-04-27 12:07:26 +0100

    transform bootstraps/admin.js to ES6 module

--- static/src/javascripts/bootstraps/admin.js
+++ static/src/javascripts/bootstraps/admin.js
@@ -1,27 +1,19 @@
-define([
-    'admin/bootstraps/abtests',
-    'admin/bootstraps/radiator',
-    'admin/bootstraps/commercial-browser-performance',
-    'domready'
-], function (
-    abTests,
-    radiator,
-    commercialBrowserPerformance,
-    domReady
-) {
-    domReady(function () {
-        switch (window.location.pathname) {
-            case '/analytics/abtests':
-                abTests.init();
-                break;
+import abTests from 'admin/bootstraps/abtests';
+import radiator from 'admin/bootstraps/radiator';
+import commercialBrowserPerformance from 'admin/bootstraps/commercial-browser-performance';
+import domReady from 'domready';
+domReady(function() {
+    switch (window.location.pathname) {
+        case '/analytics/abtests':
+            abTests.init();
+            break;
 
-            case '/radiator':
-                radiator.init();
-                break;
+        case '/radiator':
+            radiator.init();
+            break;
 
-            case '/commercial/performance/browser-dashboard':
-                commercialBrowserPerformance.init();
-                break;
-        }
-    });
+        case '/commercial/performance/browser-dashboard':
+            commercialBrowserPerformance.init();
+            break;
+    }
 });
