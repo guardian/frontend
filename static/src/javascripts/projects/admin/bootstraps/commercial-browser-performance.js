1115521efeb6378f42433bf5ccd77d28505c41c1
Francis Carr <francis.carr@guardian.co.uk>
2017-04-28 14:35:44 +0100

    fix lint errors with projects/admin/bootstraps/commercial-browser-performance.js after transform to ES6

--- static/src/javascripts/projects/admin/bootstraps/commercial-browser-performance.js
+++ static/src/javascripts/projects/admin/bootstraps/commercial-browser-performance.js
@@ -1,12 +1,11 @@
-/*global $, Epoch*/
+// @flow
+/* global $, Epoch*/
 import config from 'lib/config';
 import fetchJson from 'lib/fetch-json';
 import template from 'lodash/utilities/template';
 import countBy from 'lodash/collections/countBy';
 import find from 'lodash/collections/find';
-import map from 'lodash/collections/map';
-import reduce from 'lodash/collections/reduce';
-import filter from 'lodash/collections/filter';
+
 let chart;
 const FETCH_INTERVAL = 1000; // The frequency that we poll for report data.
 const FETCH_DELAY = 10; // The delay which we wait before we ask for a time-based datapoint, eg. 10 seconds before the present moment.
@@ -15,56 +14,7 @@ const reportTemplateUrl = '/commercial-reports/<%=isoDate%>';
 // Store the 1000 most recently fetched datapoints.
 const commercialStartTimes = [];
 
-function initialise() {
-
-    chart = $('#browser-live-performance-data').epoch({
-        type: 'time.heatmap',
-        buckets: 20,
-        bucketRange: [0, 10000],
-        axes: ['left', 'right', 'bottom'],
-        tickFormats: {
-            left: Epoch.Formats.regular,
-            right: Epoch.Formats.regular
-        },
-        data: [{
-            label: 'Commercial Start Time',
-            values: []
-        }]
-    });
-
-    window.setInterval(fetchData, FETCH_INTERVAL);
-}
-
-function fetchData() {
-    const currentDate = new Date();
-    currentDate.setSeconds(currentDate.getSeconds() - FETCH_DELAY);
-    const fetchUrl = template(reportTemplateUrl, {
-        isoDate: currentDate.toISOString()
-    });
-
-    fetchJson(config.page.beaconUrl + fetchUrl, {
-        mode: 'cors'
-    }).then(logs => {
-
-        let appStartTimes = map(logs.reports, report => {
-            const primaryBaseline = find(report.baselines, baseline => baseline.name === 'primary');
-            return primaryBaseline ? primaryBaseline.startTime : 0;
-        });
-
-        // Filter the times array from silly numbers, investigating why Date times are appearing in the array.
-        appStartTimes = filter(appStartTimes, startTime => startTime < 20000);
-
-        updateAverageStartTime(appStartTimes);
-
-        const heatmapData = {
-            time: currentDate.getTime() / 1000,
-            histogram: countBy(appStartTimes)
-        };
-        chart.push([heatmapData]);
-    });
-}
-
-function updateAverageStartTime(startTimes) {
+const updateAverageStartTime = startTimes => {
     // Push the new start times into the stored array to find an average.
     Array.prototype.push.apply(commercialStartTimes, startTimes);
     // Limit the size of the array to 1000.
@@ -76,20 +26,75 @@ function updateAverageStartTime(startTimes) {
         return;
     }
 
-    const sum = reduce(commercialStartTimes, (sum, num) => {
+    const sum = commercialStartTimes.reduce((accum, num) => {
         // Disregard silly numbers, investigating why Date times are appearing in the array.
         if (num < 20000) {
-            return sum + num;
-        } else {
-            return sum;
+            return accum + num;
         }
+        return accum;
     });
 
     const averageStartTime = (sum / commercialStartTimes.length).toFixed([2]);
 
     $('.graph__average-value').text(averageStartTime);
-}
+};
+
+const fetchData = () => {
+    const currentDate = new Date();
+    currentDate.setSeconds(currentDate.getSeconds() - FETCH_DELAY);
+    const fetchUrl = template(reportTemplateUrl, {
+        isoDate: currentDate.toISOString(),
+    });
+
+    fetchJson(config.page.beaconUrl + fetchUrl, {
+        mode: 'cors',
+    }).then(logs => {
+        type Reports = {
+            baselines: Array<{ name: string, startTime: number }>,
+        };
+        const reports: Array<Reports> = logs.reports || [];
+        let appStartTimes: Array<number> = reports.map(report => {
+            const primaryBaseline = find(
+                report.baselines,
+                baseline => baseline.name === 'primary'
+            );
+            return primaryBaseline ? primaryBaseline.startTime : 0;
+        });
+
+        // Filter the times array from silly numbers, investigating why Date times are appearing in the array.
+        appStartTimes = appStartTimes.filter(startTime => startTime < 20000);
+
+        updateAverageStartTime(appStartTimes);
+
+        const heatmapData = {
+            time: currentDate.getTime() / 1000,
+            histogram: countBy(appStartTimes),
+        };
+        chart.push([heatmapData]);
+    });
+};
+
+const initialise = () => {
+    chart = $('#browser-live-performance-data').epoch({
+        type: 'time.heatmap',
+        buckets: 20,
+        bucketRange: [0, 10000],
+        axes: ['left', 'right', 'bottom'],
+        tickFormats: {
+            left: Epoch.Formats.regular,
+            right: Epoch.Formats.regular,
+        },
+        data: [
+            {
+                label: 'Commercial Start Time',
+                values: [],
+            },
+        ],
+    });
+
+    window.setInterval(fetchData, FETCH_INTERVAL);
+};
 
 export default {
-    init: initialise
+    init: initialise,
 };
