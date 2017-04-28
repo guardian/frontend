e27dcb3d491fd183ca73ed76d812431e42b210f8
Kelvin Chappell <kelvin.chappell@theguardian.com>
2017-04-28 11:02:46 +0100

    Use logo with smaller file size

--- sport/app/football/controllers/model.scala
+++ sport/app/football/controllers/model.scala
@@ -50,7 +50,7 @@ class FootballPage(
                 sponsorName = "Virgin Media",
                 logo = Logo(
                   src =
-                    "https://static.theguardian.com/commercial/sponsor/13/Apr/2017/c6d9da81-380a-48f3-bd23-2320bb54b5f2-vm-b-logo-red-cmyk-reg140x90.png",
+                    "https://static.theguardian.com/commercial/sponsor/28/Apr/2017/8e3b5ea3-3fed-44ae-8e70-b8811ec4f874-Virgin_logo.png",
                   dimensions = Some(Dimensions(width = 140, height = 90)),
                   link = "http://www.virginmedia.com/shop/tv/virgin-media-football.html",
                   label = "Supported by"
