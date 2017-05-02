2ff8e902bd5669861108cbd0fbaae80a7a8a94e6
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-02 15:55:51 +0100

    convert tailor

---
+++ static/src/javascripts/projects/common/modules/tailor/tailor.js
@@ -0,0 +1,39 @@
+// @flow
+import fetchData from 'common/modules/tailor/fetch-data';
+
+/**
+ * Given a response from tailor, we see if the response has a survey suggestion, and if so return the first
+ * survey suggestion (there should only ever be one, but just in case).
+ *
+ * @returns {Promise.<Boolean>}
+ */
+const getSuggestedSurvey = (queryParams: Object): void =>
+    fetchData('suggestions', false, queryParams).then(response => {
+        if (response.suggestions) {
+            const surveySuggestions = response.suggestions.filter(
+                suggestion => suggestion.class === 'SurveySuggestion'
+            );
+
+            if (surveySuggestions.length > 0) {
+                return surveySuggestions[0];
+            }
+        }
+    });
+
+/**
+ * Query the user's regular status
+ *
+ * @returns {Promise.<Boolean>}
+ */
+const isRegular = (): void =>
+    fetchData('suggestions', false)
+        .then(suggestions => {
+            try {
+                return suggestions.userDataForClient.regular;
+            } catch (e) {
+                return false;
+            }
+        })
+        .catch(() => false);
+
+export { isRegular, getSuggestedSurvey };
