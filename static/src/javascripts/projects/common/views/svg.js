7782e1e5e0338c313ab4e0322965466c90a19c16
Philip Wills <philip.wills@guardian.co.uk>
2017-05-03 13:00:11 +0100

    Add types and use string interpolation

--- static/src/javascripts/projects/common/views/svg.js
+++ static/src/javascripts/projects/common/views/svg.js
@@ -1,22 +1,28 @@
-export default svg;
-
-function svg(markup, classes, title) {
+// @flow
+export default (
+    markup: string,
+    classes: ?Array<string>,
+    title: ?string
+): string => {
     // Only mess with classes if we actually need to.
+    let markupWithClasses = markup;
     if (classes) {
         if (Array.isArray(classes)) {
-            markup = markup.replace(/class="/, '$&' + classes.join(' ') + ' ');
-        } else {
+            markupWithClasses = markupWithClasses.replace(
+                /class="/,
+                `class="${classes.join(' ')} `
+            );
+        } else if (window.console && window.console.error) {
             // Some environments don't support or don't always expose the console object
-            if (window.console && window.console.error) {
-                window.console.error('Classes for inlineSvg must be an array: ', classes);
-            }
+            window.console
+                .error(`Classes for inlineSvg must be an array: ${classes}`);
         }
     }
 
     // Only mess with title if we actually need to.
-    if (title) {
-        markup = markup.replace(/<span /, '<span title="' + title + '" ');
-    }
+    const markupWithTitle = title
+        ? markupWithClasses.replace(/<span /, `<span title="${title}" `)
+        : markupWithClasses;
 
-    return markup;
-}
+    return markupWithTitle;
+};
