93fa447462989ce86b69c36aa9fd2e4c71730d54
Philip Wills <philip.wills@guardian.co.uk>
2017-05-03 12:21:06 +0100

    transform projects/common/views/svg.js to ES6 module

--- static/src/javascripts/projects/common/views/svg.js
+++ static/src/javascripts/projects/common/views/svg.js
@@ -1,24 +1,22 @@
-define(function () {
-    return svg;
+export default svg;
 
-    function svg(markup, classes, title) {
-        // Only mess with classes if we actually need to.
-        if (classes) {
-            if (Array.isArray(classes)) {
-                markup = markup.replace(/class="/, '$&' + classes.join(' ') + ' ');
-            } else {
-                // Some environments don't support or don't always expose the console object
-                if (window.console && window.console.error) {
-                    window.console.error('Classes for inlineSvg must be an array: ', classes);
-                }
+function svg(markup, classes, title) {
+    // Only mess with classes if we actually need to.
+    if (classes) {
+        if (Array.isArray(classes)) {
+            markup = markup.replace(/class="/, '$&' + classes.join(' ') + ' ');
+        } else {
+            // Some environments don't support or don't always expose the console object
+            if (window.console && window.console.error) {
+                window.console.error('Classes for inlineSvg must be an array: ', classes);
             }
         }
-
-        // Only mess with title if we actually need to.
-        if (title) {
-            markup = markup.replace(/<span /, '<span title="' + title + '" ');
-        }
-
-        return markup;
     }
-});
+
+    // Only mess with title if we actually need to.
+    if (title) {
+        markup = markup.replace(/<span /, '<span title="' + title + '" ');
+    }
+
+    return markup;
+}
