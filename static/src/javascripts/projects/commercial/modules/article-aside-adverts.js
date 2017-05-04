2e2a9f7e6db0b377f87eb85440e0ba4665f87ffc
Francis Carr <francis.carr@guardian.co.uk>
2017-05-04 11:19:09 +0100

    Fix issues with article-aside-adverts

--- static/src/javascripts/projects/commercial/modules/article-aside-adverts.js
+++ static/src/javascripts/projects/commercial/modules/article-aside-adverts.js
@@ -1,57 +1,61 @@
-import Promise from 'Promise';
+// @flow
 import $ from 'lib/$';
 import config from 'lib/config';
 import mediator from 'lib/mediator';
 import fastdom from 'lib/fastdom-promise';
 import commercialFeatures from 'commercial/modules/commercial-features';
-var minArticleHeight = 1300;
-var minImmersiveArticleHeight = 600;
 
-function minContentHeight() {
-    return config.page.isImmersive ? minImmersiveArticleHeight : minArticleHeight;
-}
+const minArticleHeight = 1300;
+const minImmersiveArticleHeight = 600;
 
-function init(start, stop) {
+const minContentHeight = () =>
+    config.page.isImmersive ? minImmersiveArticleHeight : minArticleHeight;
+
+const init = (start: () => void, stop: () => void) => {
     start();
 
-    var $col = $('.js-secondary-column');
-    var $mainCol, $adSlot;
+    const $col = $('.js-secondary-column');
 
     // are article aside ads disabled, or secondary column hidden?
-    if (!(commercialFeatures.articleAsideAdverts && $col.length && $col.css('display') !== 'none')) {
+    if (
+        !(commercialFeatures.articleAsideAdverts &&
+            $col.length &&
+            $col.css('display') !== 'none')
+    ) {
         stop();
         return Promise.resolve(false);
     }
 
-    $mainCol = $('.js-content-main-column');
-    $adSlot = $('.js-ad-slot', $col);
+    const $mainCol = $('.js-content-main-column');
+    const $adSlot = $('.js-ad-slot', $col);
 
     if (!$adSlot.length || !$mainCol.length) {
         stop();
         return Promise.resolve(false);
     }
 
-    return fastdom.read(function() {
-            return $mainCol.dim().height;
-        })
-        .then(function(mainColHeight) {
-
+    return fastdom
+        .read(() => $mainCol.dim().height)
+        .then(mainColHeight => {
             // Should switch to 'right-small' MPU for short articles
             if (mainColHeight < minContentHeight()) {
-                return fastdom.write(function() {
+                return fastdom.write(() => {
                     $adSlot.removeClass('right-sticky js-sticky-mpu is-sticky');
-                    $adSlot[0].setAttribute('data-mobile', '1,1|2,2|300,250|fluid');
+                    $adSlot[0].setAttribute(
+                        'data-mobile',
+                        '1,1|2,2|300,250|fluid'
+                    );
                     return $adSlot[0];
                 });
             }
             return $adSlot[0];
         })
-        .then(function(adSlot) {
+        .then(adSlot => {
             stop();
             mediator.emit('page:commercial:right', adSlot);
         });
-}
+};
 
 export default {
-    init: init
+    init,
 };
