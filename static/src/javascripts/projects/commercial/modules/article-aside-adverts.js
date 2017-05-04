b21439a355d6b2e642a91040e2dfd7f93b240819
Francis Carr <francis.carr@guardian.co.uk>
2017-05-04 10:49:55 +0100

    transform projects/commercial/modules/article-aside-adverts.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/article-aside-adverts.js
+++ static/src/javascripts/projects/commercial/modules/article-aside-adverts.js
@@ -1,67 +1,57 @@
-define([
-    'Promise',
-    'lib/$',
-    'lib/config',
-    'lib/mediator',
-    'lib/fastdom-promise',
-    'commercial/modules/commercial-features'
-], function (
-    Promise,
-    $,
-    config,
-    mediator,
-    fastdom,
-    commercialFeatures
-) {
-    var minArticleHeight = 1300;
-    var minImmersiveArticleHeight = 600;
+import Promise from 'Promise';
+import $ from 'lib/$';
+import config from 'lib/config';
+import mediator from 'lib/mediator';
+import fastdom from 'lib/fastdom-promise';
+import commercialFeatures from 'commercial/modules/commercial-features';
+var minArticleHeight = 1300;
+var minImmersiveArticleHeight = 600;
 
-    function minContentHeight() {
-      return config.page.isImmersive ? minImmersiveArticleHeight : minArticleHeight;
+function minContentHeight() {
+    return config.page.isImmersive ? minImmersiveArticleHeight : minArticleHeight;
+}
+
+function init(start, stop) {
+    start();
+
+    var $col = $('.js-secondary-column');
+    var $mainCol, $adSlot;
+
+    // are article aside ads disabled, or secondary column hidden?
+    if (!(commercialFeatures.articleAsideAdverts && $col.length && $col.css('display') !== 'none')) {
+        stop();
+        return Promise.resolve(false);
     }
 
-    function init(start, stop) {
-        start();
+    $mainCol = $('.js-content-main-column');
+    $adSlot = $('.js-ad-slot', $col);
 
-        var $col = $('.js-secondary-column');
-        var $mainCol, $adSlot;
+    if (!$adSlot.length || !$mainCol.length) {
+        stop();
+        return Promise.resolve(false);
+    }
 
-        // are article aside ads disabled, or secondary column hidden?
-        if (!(commercialFeatures.articleAsideAdverts && $col.length && $col.css('display') !== 'none')) {
-            stop();
-            return Promise.resolve(false);
-        }
-
-        $mainCol = $('.js-content-main-column');
-        $adSlot = $('.js-ad-slot', $col);
-
-        if (!$adSlot.length || !$mainCol.length) {
-            stop();
-            return Promise.resolve(false);
-        }
-
-        return fastdom.read(function () {
+    return fastdom.read(function() {
             return $mainCol.dim().height;
         })
-        .then(function (mainColHeight) {
+        .then(function(mainColHeight) {
 
             // Should switch to 'right-small' MPU for short articles
             if (mainColHeight < minContentHeight()) {
-              return fastdom.write(function () {
-                  $adSlot.removeClass('right-sticky js-sticky-mpu is-sticky');
-                  $adSlot[0].setAttribute('data-mobile', '1,1|2,2|300,250|fluid');
-                  return $adSlot[0];
-              });
+                return fastdom.write(function() {
+                    $adSlot.removeClass('right-sticky js-sticky-mpu is-sticky');
+                    $adSlot[0].setAttribute('data-mobile', '1,1|2,2|300,250|fluid');
+                    return $adSlot[0];
+                });
             }
             return $adSlot[0];
         })
-        .then(function (adSlot) {
+        .then(function(adSlot) {
             stop();
             mediator.emit('page:commercial:right', adSlot);
         });
-    }
+}
 
-    return {
-        init: init
-    };
-});
+export default {
+    init: init
+};
