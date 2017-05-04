787c0faf70ef87809aeec965da50041ccec484d2
Francis Carr <francis.carr@guardian.co.uk>
2017-05-04 11:53:47 +0100

    Add types to article-aside-adverts

--- static/src/javascripts/projects/commercial/modules/article-aside-adverts.js
+++ static/src/javascripts/projects/commercial/modules/article-aside-adverts.js
@@ -5,16 +5,18 @@ import mediator from 'lib/mediator';
 import fastdom from 'lib/fastdom-promise';
 import commercialFeatures from 'commercial/modules/commercial-features';
 
-const minArticleHeight = 1300;
-const minImmersiveArticleHeight = 600;
+import type { bonzo } from 'bonzo';
 
-const minContentHeight = () =>
+const minArticleHeight: number = 1300;
+const minImmersiveArticleHeight: number = 600;
+
+const minContentHeight = (): number =>
     config.page.isImmersive ? minImmersiveArticleHeight : minArticleHeight;
 
-const init = (start: () => void, stop: () => void) => {
+const init = (start: () => void, stop: () => void): Promise<boolean> => {
     start();
 
-    const $col = $('.js-secondary-column');
+    const $col: bonzo = $('.js-secondary-column');
 
     // are article aside ads disabled, or secondary column hidden?
     if (
@@ -26,8 +28,8 @@ const init = (start: () => void, stop: () => void) => {
         return Promise.resolve(false);
     }
 
-    const $mainCol = $('.js-content-main-column');
-    const $adSlot = $('.js-ad-slot', $col);
+    const $mainCol: bonzo = $('.js-content-main-column');
+    const $adSlot: bonzo = $('.js-ad-slot', $col);
 
     if (!$adSlot.length || !$mainCol.length) {
         stop();
@@ -35,8 +37,8 @@ const init = (start: () => void, stop: () => void) => {
     }
 
     return fastdom
-        .read(() => $mainCol.dim().height)
-        .then(mainColHeight => {
+        .read((): number => $mainCol.dim().height)
+        .then((mainColHeight: number) => {
             // Should switch to 'right-small' MPU for short articles
             if (mainColHeight < minContentHeight()) {
                 return fastdom.write(() => {
@@ -50,9 +52,10 @@ const init = (start: () => void, stop: () => void) => {
             }
             return $adSlot[0];
         })
-        .then(adSlot => {
+        .then((adSlot: Element) => {
             stop();
             mediator.emit('page:commercial:right', adSlot);
+            return true;
         });
 };
 
