b29fa2bf27af1110daf23ac5d53a60a358b968cc
Francis Carr <francis.carr@guardian.co.uk>
2017-05-05 16:14:01 +0100

    transform projects/commercial/modules/article-body-adverts.js content to ES6

--- static/src/javascripts/projects/commercial/modules/article-body-adverts.js
+++ static/src/javascripts/projects/commercial/modules/article-body-adverts.js
@@ -1,5 +1,4 @@
-import Promise from 'Promise';
-import qwery from 'qwery';
+// @flow
 import config from 'lib/config';
 import detect from 'lib/detect';
 import fastdom from 'lib/fastdom-promise';
@@ -13,28 +12,179 @@ import commercialFeatures from 'commercial/modules/commercial-features';
 
 /* bodyAds is a counter that keeps track of the number of inline MPUs
  * inserted dynamically. */
-var bodyAds;
-var replaceTopSlot;
-var getSlotName;
-var getSlotType;
-var isOffsetingAds = abUtils.testCanBeRun('IncreaseInlineAdsRedux') &&
+let bodyAds;
+let replaceTopSlot;
+let getSlotName;
+let getSlotType;
+
+const isOffsetingAds =
+    abUtils.testCanBeRun('IncreaseInlineAdsRedux') &&
     abUtils.getTestVariantId('IncreaseInlineAdsRedux') === 'yes';
 
-function init() {
+const getSlotNameForMobile = () =>
+    bodyAds === 1 ? 'top-above-nav' : `inline${bodyAds - 1}`;
 
+const getSlotNameForDesktop = () => `inline${bodyAds}`;
+
+const getSlotTypeForMobile = () => (bodyAds === 1 ? 'top-above-nav' : 'inline');
+
+const getSlotTypeForDesktop = () => 'inline';
+
+const insertAdAtPara = (para, name, type, classes, sizes) => {
+    const ad = createSlot(type, {
+        name,
+        classes,
+        sizes,
+    });
+
+    return fastdom
+        .write(() => {
+            para.parentNode.insertBefore(ad, para);
+        })
+        .then(() => {
+            addSlot(ad, name === 'im');
+        });
+};
+
+// Add new ads while there is still space
+const addArticleAds = (count, rules) => {
+    const insertInlineAds = paras => {
+        const slots = paras
+            .slice(0, Math.min(paras.length, count))
+            .map(para => {
+                bodyAds += 1;
+                return insertAdAtPara(
+                    para,
+                    getSlotName(),
+                    getSlotType(),
+                    `inline${isOffsetingAds && bodyAds > 1 ? ' offset-right' : ''}`,
+                    isOffsetingAds && bodyAds > 1
+                        ? {
+                              desktop: [adSizes.halfPage],
+                          }
+                        : null
+                );
+            });
+
+        return Promise.all(slots).then(() => slots.length);
+    };
+
+    return spaceFiller.fillSpace(rules, insertInlineAds, {
+        waitForImages: true,
+        waitForLinks: true,
+        waitForInteractives: true,
+    });
+};
+
+const getRules = (): Object => {
+    let prevSlot;
+    return {
+        bodySelector: '.js-article__body',
+        slotSelector: ' > p',
+        minAbove: detect.isBreakpoint({
+            max: 'tablet',
+        })
+            ? 300
+            : 700,
+        minBelow: 300,
+        selectors: {
+            ' > h2': {
+                minAbove: detect.getBreakpoint() === 'mobile' ? 100 : 0,
+                minBelow: 250,
+            },
+            ' .ad-slot': {
+                minAbove: 500,
+                minBelow: 500,
+            },
+            ' > :not(p):not(h2):not(.ad-slot)': {
+                minAbove: 35,
+                minBelow: 400,
+            },
+        },
+        filter(slot) {
+            if (
+                !prevSlot ||
+                Math.abs(slot.top - prevSlot.top) - adSizes.mpu.height >=
+                    this.selectors[' .ad-slot'].minBelow
+            ) {
+                prevSlot = slot;
+                return true;
+            }
+            return false;
+        },
+    };
+};
+
+const getAltRules = () => {
+    const altRules = getRules();
+    altRules.selectors = {
+        ' .ad-slot': {
+            minAbove: 500,
+            minBelow: 500,
+        },
+    };
+    return altRules;
+};
+
+const getLongArticleRules = () => {
+    const longArticleRules = isOffsetingAds ? getAltRules() : getRules();
+    const viewportHeight = detect.getViewport().height;
+    longArticleRules.selectors[' .ad-slot'].minAbove = viewportHeight;
+    longArticleRules.selectors[' .ad-slot'].minBelow = viewportHeight;
+    return longArticleRules;
+};
+
+const addInlineAds = () =>
+    addArticleAds(
+        2,
+        isOffsetingAds ? getAltRules() : getRules()
+    ).then(countAdded => {
+        if (countAdded === 2) {
+            return addArticleAds(8, getLongArticleRules()).then(
+                innerCountAdded => 2 + innerCountAdded
+            );
+        }
+        return countAdded;
+    });
+
+const getInlineMerchRules = () => {
+    const inlineMerchRules = getRules();
+    inlineMerchRules.minAbove = 300;
+    inlineMerchRules.selectors[' > h2'].minAbove = 100;
+    inlineMerchRules.selectors[
+        ' > :not(p):not(h2):not(.ad-slot)'
+    ].minAbove = 200;
+    return inlineMerchRules;
+};
+
+const addInlineMerchAd = () =>
+    spaceFiller.fillSpace(
+        getInlineMerchRules(),
+        paras => insertAdAtPara(paras[0], 'im', 'im').then(() => 1),
+        {
+            waitForImages: true,
+            waitForLinks: true,
+            waitForInteractives: true,
+        }
+    );
+
+const waitForMerch = (countAdded: number) =>
+    countAdded === 1 ? trackAdRender('dfp-ad--im') : Promise.resolve();
+
+const init = () => {
     if (!commercialFeatures.articleBodyAdverts) {
         return Promise.resolve(false);
     }
 
     bodyAds = 0;
     replaceTopSlot = detect.isBreakpoint({
-        max: 'phablet'
+        max: 'phablet',
     });
     getSlotName = replaceTopSlot ? getSlotNameForMobile : getSlotNameForDesktop;
     getSlotType = replaceTopSlot ? getSlotTypeForMobile : getSlotTypeForDesktop;
 
     if (config.page.hasInlineMerchandise) {
-        var im = addInlineMerchAd();
+        const im = addInlineMerchAd();
         // Whether an inline merch has been inserted or not,
         // we still want to try to insert inline MPUs. But
         // we must wait for DFP to return, since if the merch
@@ -46,165 +196,14 @@ function init() {
 
     addInlineAds();
     return Promise.resolve(true);
-}
-
-export default {
-    init: init,
-
-    '@@tests': {
-        waitForMerch: waitForMerch,
-        addInlineMerchAd: addInlineMerchAd,
-        addInlineAds: addInlineAds
-    }
 };
 
-function getSlotNameForMobile() {
-    return bodyAds === 1 ? 'top-above-nav' : 'inline' + (bodyAds - 1);
-}
+export default {
+    init,
 
-function getSlotNameForDesktop() {
-    return 'inline' + bodyAds;
-}
-
-function getSlotTypeForMobile() {
-    return bodyAds === 1 ? 'top-above-nav' : 'inline';
-}
-
-function getSlotTypeForDesktop() {
-    return 'inline';
-}
-
-function getRules() {
-    var prevSlot;
-    return {
-        bodySelector: '.js-article__body',
-        slotSelector: ' > p',
-        minAbove: detect.isBreakpoint({
-            max: 'tablet'
-        }) ? 300 : 700,
-        minBelow: 300,
-        selectors: {
-            ' > h2': {
-                minAbove: detect.getBreakpoint() === 'mobile' ? 100 : 0,
-                minBelow: 250
-            },
-            ' .ad-slot': {
-                minAbove: 500,
-                minBelow: 500
-            },
-            ' > :not(p):not(h2):not(.ad-slot)': {
-                minAbove: 35,
-                minBelow: 400
-            }
-        },
-        filter: function(slot) {
-            if (!prevSlot || Math.abs(slot.top - prevSlot.top) - adSizes.mpu.height >= this.selectors[' .ad-slot'].minBelow) {
-                prevSlot = slot;
-                return true;
-            }
-            return false;
-        }
-    };
-}
-
-function getAltRules() {
-    var altRules = getRules();
-    altRules.selectors = {
-        ' .ad-slot': {
-            minAbove: 500,
-            minBelow: 500
-        }
-    };
-    return altRules;
-}
-
-function getInlineMerchRules() {
-    var inlineMerchRules = getRules();
-    inlineMerchRules.minAbove = 300;
-    inlineMerchRules.selectors[' > h2'].minAbove = 100;
-    inlineMerchRules.selectors[' > :not(p):not(h2):not(.ad-slot)'].minAbove = 200;
-    return inlineMerchRules;
-}
-
-function getLongArticleRules() {
-    var longArticleRules = isOffsetingAds ? getAltRules() : getRules();
-    longArticleRules.selectors[' .ad-slot'].minAbove =
-        longArticleRules.selectors[' .ad-slot'].minBelow = detect.getViewport().height;
-    return longArticleRules;
-}
-
-function addInlineMerchAd() {
-    return spaceFiller.fillSpace(getInlineMerchRules(), function(paras) {
-        return insertAdAtPara(paras[0], 'im', 'im').then(function() {
-            return 1;
-        });
-    }, {
-        waitForImages: true,
-        waitForLinks: true,
-        waitForInteractives: true
-    });
-}
-
-function addInlineAds() {
-    return addArticleAds(2, isOffsetingAds ? getAltRules() : getRules())
-        .then(function(countAdded) {
-            if (countAdded === 2) {
-                return addArticleAds(8, getLongArticleRules())
-                    .then(function(countAdded) {
-                        return 2 + countAdded;
-                    });
-            } else {
-                return countAdded;
-            }
-        });
-}
-
-function waitForMerch(countAdded) {
-    return countAdded === 1 ? trackAdRender('dfp-ad--im') : Promise.resolve();
-}
-
-// Add new ads while there is still space
-function addArticleAds(count, rules) {
-    return spaceFiller.fillSpace(rules, insertInlineAds, {
-        waitForImages: true,
-        waitForLinks: true,
-        waitForInteractives: true
-    });
-
-    function insertInlineAds(paras) {
-        var slots = paras
-            .slice(0, Math.min(paras.length, count))
-            .map(function(para) {
-                bodyAds += 1;
-                return insertAdAtPara(
-                    para,
-                    getSlotName(),
-                    getSlotType(),
-                    'inline' + (isOffsetingAds && bodyAds > 1 ? ' offset-right' : ''),
-                    isOffsetingAds && bodyAds > 1 ? {
-                        desktop: [adSizes.halfPage]
-                    } : null
-                );
-            });
-
-        return Promise.all(slots)
-            .then(function() {
-                return slots.length;
-            });
-    }
-}
-
-function insertAdAtPara(para, name, type, classes, sizes) {
-    var ad = createSlot(type, {
-        name: name,
-        classes: classes,
-        sizes: sizes
-    });
-
-    return fastdom.write(function() {
-            para.parentNode.insertBefore(ad, para);
-        })
-        .then(function() {
-            addSlot(ad, name === 'im');
-        });
-}
+    '@@tests': {
+        waitForMerch,
+        addInlineMerchAd,
+        addInlineAds,
+    },
+};
