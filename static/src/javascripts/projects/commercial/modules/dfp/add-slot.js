10508a6893d7f64362cb33a35b1afd211a1a80e0
Jon Norman <jon.norman@guardian.co.uk>
2017-05-05 15:11:04 +0100

    transform projects/commercial/modules/dfp/add-slot.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/add-slot.js
+++ static/src/javascripts/projects/commercial/modules/dfp/add-slot.js
@@ -1,34 +1,31 @@
-define([
-    'lib/mediator',
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/Advert',
-    'commercial/modules/dfp/queue-advert',
-    'commercial/modules/dfp/load-advert',
-    'commercial/modules/dfp/enable-lazy-load',
-    'commercial/modules/dfp/performance-logging'
-], function (mediator, dfpEnv, Advert, queueAdvert, loadAdvert, enableLazyLoad, performanceLogging) {
-    return addSlot;
+import mediator from 'lib/mediator';
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import Advert from 'commercial/modules/dfp/Advert';
+import queueAdvert from 'commercial/modules/dfp/queue-advert';
+import loadAdvert from 'commercial/modules/dfp/load-advert';
+import enableLazyLoad from 'commercial/modules/dfp/enable-lazy-load';
+import performanceLogging from 'commercial/modules/dfp/performance-logging';
+export default addSlot;
 
-    function addSlot(adSlot, forceDisplay) {
-        window.googletag.cmd.push(function () {
-            if (!(adSlot.id in dfpEnv.advertIds)) { // dynamically add ad slot
-                displayAd(adSlot, forceDisplay);
-            }
-        });
-    }
-
-    function displayAd(adSlot, forceDisplay) {
-        var advert = Advert(adSlot);
-
-        dfpEnv.advertIds[advert.id] = dfpEnv.adverts.push(advert) - 1;
-        if (dfpEnv.shouldLazyLoad() && !forceDisplay) {
-            queueAdvert(advert);
-            performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'add-slot-lazy');
-            enableLazyLoad(advert);
-        } else {
-            performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'add-slot-instant');
-            performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', 0);
-            loadAdvert(advert);
+function addSlot(adSlot, forceDisplay) {
+    window.googletag.cmd.push(function() {
+        if (!(adSlot.id in dfpEnv.advertIds)) { // dynamically add ad slot
+            displayAd(adSlot, forceDisplay);
         }
+    });
+}
+
+function displayAd(adSlot, forceDisplay) {
+    var advert = Advert(adSlot);
+
+    dfpEnv.advertIds[advert.id] = dfpEnv.adverts.push(advert) - 1;
+    if (dfpEnv.shouldLazyLoad() && !forceDisplay) {
+        queueAdvert(advert);
+        performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'add-slot-lazy');
+        enableLazyLoad(advert);
+    } else {
+        performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'add-slot-instant');
+        performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', 0);
+        loadAdvert(advert);
     }
-});
+}
