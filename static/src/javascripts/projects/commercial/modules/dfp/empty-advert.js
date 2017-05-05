56d1327846e0b8ee2bed8ea6919d4368f7726593
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-05 16:20:14 +0100

    transform projects/commercial/modules/dfp/empty-advert.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/empty-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/empty-advert.js
@@ -1,13 +1,10 @@
-define([
-    'fastdom'
-], function (fastdom) {
-    return emptyAdvert;
+import fastdom from 'fastdom';
+export default emptyAdvert;
 
-    function emptyAdvert(advert) {
-        fastdom.write(function () {
-            window.googletag.destroySlots([advert.slot]);
-            advert.node.remove();
-            advert.node = advert.slot = null;
-        });
-    }
-});
+function emptyAdvert(advert) {
+    fastdom.write(function() {
+        window.googletag.destroySlots([advert.slot]);
+        advert.node.remove();
+        advert.node = advert.slot = null;
+    });
+}
