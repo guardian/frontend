a210a8e30be9f06d989d9a5636ec2c4ed09e6f66
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-05 15:02:17 +0100

    transform projects/commercial/modules/dfp/display-lazy-ads.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/display-lazy-ads.js
+++ static/src/javascripts/projects/commercial/modules/dfp/display-lazy-ads.js
@@ -1,39 +1,36 @@
-define([
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/load-advert',
-    'commercial/modules/dfp/enable-lazy-load',
-    'commercial/modules/dfp/performance-logging'
-], function (dfpEnv, loadAdvert, enableLazyLoad, performanceLogging) {
-    var advertsToInstantlyLoad = [
-        'dfp-ad--merchandising-high',
-        'dfp-ad--im'
-    ];
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import loadAdvert from 'commercial/modules/dfp/load-advert';
+import enableLazyLoad from 'commercial/modules/dfp/enable-lazy-load';
+import performanceLogging from 'commercial/modules/dfp/performance-logging';
+var advertsToInstantlyLoad = [
+    'dfp-ad--merchandising-high',
+    'dfp-ad--im'
+];
 
-    return displayLazyAds;
+export default displayLazyAds;
 
-    function displayLazyAds() {
-        window.googletag.pubads().collapseEmptyDivs();
-        window.googletag.enableServices();
-        instantLoad();
-        enableLazyLoad();
-    }
+function displayLazyAds() {
+    window.googletag.pubads().collapseEmptyDivs();
+    window.googletag.enableServices();
+    instantLoad();
+    enableLazyLoad();
+}
 
-    function instantLoad() {
-        var instantLoadAdverts = dfpEnv.advertsToLoad.filter(function(advert) {
-            if (advertsToInstantlyLoad.indexOf(advert.id) > -1) {
-                performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'instant');
-                performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', 0);
-                return true;
-            } else {
-                performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'lazy-load');
-                return false;
-            }
-        });
+function instantLoad() {
+    var instantLoadAdverts = dfpEnv.advertsToLoad.filter(function(advert) {
+        if (advertsToInstantlyLoad.indexOf(advert.id) > -1) {
+            performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'instant');
+            performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', 0);
+            return true;
+        } else {
+            performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'lazy-load');
+            return false;
+        }
+    });
 
-        dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function (advert) {
-            return advertsToInstantlyLoad.indexOf(advert.id) < 0;
-        });
+    dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function(advert) {
+        return advertsToInstantlyLoad.indexOf(advert.id) < 0;
+    });
 
-        instantLoadAdverts.forEach(loadAdvert);
-    }
-});
+    instantLoadAdverts.forEach(loadAdvert);
+}
