5c0d097e46e9005ba37ab5b3bd6ca535266039d8
Jon Norman <jon.norman@guardian.co.uk>
2017-05-05 15:32:54 +0100

    transform projects/commercial/modules/dfp/add-slot.js content to ES6

--- static/src/javascripts/projects/commercial/modules/dfp/add-slot.js
+++ static/src/javascripts/projects/commercial/modules/dfp/add-slot.js
@@ -1,31 +1,41 @@
-import mediator from 'lib/mediator';
+// @flow
 import dfpEnv from 'commercial/modules/dfp/dfp-env';
 import Advert from 'commercial/modules/dfp/Advert';
 import queueAdvert from 'commercial/modules/dfp/queue-advert';
 import loadAdvert from 'commercial/modules/dfp/load-advert';
 import enableLazyLoad from 'commercial/modules/dfp/enable-lazy-load';
 import performanceLogging from 'commercial/modules/dfp/performance-logging';
-export default addSlot;
 
-function addSlot(adSlot, forceDisplay) {
-    window.googletag.cmd.push(function() {
-        if (!(adSlot.id in dfpEnv.advertIds)) { // dynamically add ad slot
-            displayAd(adSlot, forceDisplay);
-        }
-    });
-}
-
-function displayAd(adSlot, forceDisplay) {
-    var advert = Advert(adSlot);
+const displayAd = (adSlot: HTMLElement, forceDisplay: Boolean) => {
+    const advert: Advert = Advert(adSlot);
 
     dfpEnv.advertIds[advert.id] = dfpEnv.adverts.push(advert) - 1;
     if (dfpEnv.shouldLazyLoad() && !forceDisplay) {
         queueAdvert(advert);
-        performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'add-slot-lazy');
+        performanceLogging.updateAdvertMetric(
+            advert,
+            'loadingMethod',
+            'add-slot-lazy'
+        );
         enableLazyLoad(advert);
     } else {
-        performanceLogging.updateAdvertMetric(advert, 'loadingMethod', 'add-slot-instant');
+        performanceLogging.updateAdvertMetric(
+            advert,
+            'loadingMethod',
+            'add-slot-instant'
+        );
         performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', 0);
         loadAdvert(advert);
     }
-}
+};
+
+const addSlot = (adSlot: HTMLElement, forceDisplay: Boolean) => {
+    window.googletag.cmd.push(() => {
+        if (!(adSlot.id in dfpEnv.advertIds)) {
+            // dynamically add ad slot
+            displayAd(adSlot, forceDisplay);
+        }
+    });
+};
+
+export default addSlot;
