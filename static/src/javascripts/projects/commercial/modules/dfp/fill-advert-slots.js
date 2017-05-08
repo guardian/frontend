7aa5078266bd379d1f01249bb144326daa2abf2e
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-08 11:01:14 +0100

    transform projects/commercial/modules/dfp/fill-advert-slots.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/fill-advert-slots.js
+++ static/src/javascripts/projects/commercial/modules/dfp/fill-advert-slots.js
@@ -1,72 +1,69 @@
-define([
-    'Promise',
-    'qwery',
-    'lib/sha1',
-    'common/modules/identity/api',
-    'commercial/modules/commercial-features',
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/Advert',
-    'commercial/modules/dfp/queue-advert',
-    'commercial/modules/dfp/display-lazy-ads',
-    'commercial/modules/dfp/display-ads',
-    'commercial/modules/dfp/refresh-on-resize',
-    'commercial/modules/dfp/prepare-switch-tag'
-], function (Promise, qwery, sha1, identity, commercialFeatures, dfpEnv, Advert, queueAdvert, displayLazyAds, displayAds, refreshOnResize, prepareSwitchTag) {
+import Promise from 'Promise';
+import qwery from 'qwery';
+import sha1 from 'lib/sha1';
+import identity from 'common/modules/identity/api';
+import commercialFeatures from 'commercial/modules/commercial-features';
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import Advert from 'commercial/modules/dfp/Advert';
+import queueAdvert from 'commercial/modules/dfp/queue-advert';
+import displayLazyAds from 'commercial/modules/dfp/display-lazy-ads';
+import displayAds from 'commercial/modules/dfp/display-ads';
+import refreshOnResize from 'commercial/modules/dfp/refresh-on-resize';
+import prepareSwitchTag from 'commercial/modules/dfp/prepare-switch-tag';
 
-    function init(start, stop) {
-        if (commercialFeatures.dfpAdvertising) {
-            fillAdvertSlots(start, stop);
-        }
-        return Promise.resolve();
+function init(start, stop) {
+    if (commercialFeatures.dfpAdvertising) {
+        fillAdvertSlots(start, stop);
     }
+    return Promise.resolve();
+}
 
-    function fillAdvertSlots(start, stop) {
+function fillAdvertSlots(start, stop) {
 
-        window.googletag.cmd.push(
-            start,
-            createAdverts,
-            queueAdverts,
-            setPublisherProvidedId,
-            prepareSwitchTag.callSwitch,
-            dfpEnv.shouldLazyLoad() ? displayLazyAds.displayLazyAds : displayAds,
-            // anything we want to happen after displaying ads
-            refreshOnResize,
-            stop
-        );
-    }
+    window.googletag.cmd.push(
+        start,
+        createAdverts,
+        queueAdverts,
+        setPublisherProvidedId,
+        prepareSwitchTag.callSwitch,
+        dfpEnv.shouldLazyLoad() ? displayLazyAds.displayLazyAds : displayAds,
+        // anything we want to happen after displaying ads
+        refreshOnResize,
+        stop
+    );
+}
 
-    function createAdverts() {
-        // Get all ad slots
-        var adverts = qwery(dfpEnv.adSlotSelector)
-        .filter(function (adSlot) {
+function createAdverts() {
+    // Get all ad slots
+    var adverts = qwery(dfpEnv.adSlotSelector)
+        .filter(function(adSlot) {
             return !(adSlot.id in dfpEnv.advertIds);
         })
         .map(Advert);
-        var currentLength = dfpEnv.adverts.length;
-        dfpEnv.adverts = dfpEnv.adverts.concat(adverts);
-        adverts.forEach(function (advert, index) {
-            dfpEnv.advertIds[advert.id] = currentLength + index;
-        });
-    }
+    var currentLength = dfpEnv.adverts.length;
+    dfpEnv.adverts = dfpEnv.adverts.concat(adverts);
+    adverts.forEach(function(advert, index) {
+        dfpEnv.advertIds[advert.id] = currentLength + index;
+    });
+}
 
-    /**
-     * Loop through each slot detected on the page and define it based on the data
-     * attributes on the element.
-     */
-    function queueAdverts() {
-        // queue ads for load
-        dfpEnv.adverts.forEach(queueAdvert);
-    }
+/**
+ * Loop through each slot detected on the page and define it based on the data
+ * attributes on the element.
+ */
+function queueAdverts() {
+    // queue ads for load
+    dfpEnv.adverts.forEach(queueAdvert);
+}
 
-    function setPublisherProvidedId() {
-        var user = identity.getUserFromCookie();
-        if (user) {
-            var hashedId = sha1.hash(user.id);
-            window.googletag.pubads().setPublisherProvidedId(hashedId);
-        }
+function setPublisherProvidedId() {
+    var user = identity.getUserFromCookie();
+    if (user) {
+        var hashedId = sha1.hash(user.id);
+        window.googletag.pubads().setPublisherProvidedId(hashedId);
     }
+}
 
-    return {
-        init: init
-    };
-});
+export default {
+    init: init
+};
