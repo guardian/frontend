486d171fe6fc0c83af0421aa6e8ac71ba7f05299
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-08 13:16:51 +0100

    transform projects/commercial/modules/dfp/get-adverts.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/get-adverts.js
+++ static/src/javascripts/projects/commercial/modules/dfp/get-adverts.js
@@ -1,17 +1,14 @@
-define([
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/get-advert-by-id'
-], function (dfpEnv, getAdvertById) {
-    return getAdverts;
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import getAdvertById from 'commercial/modules/dfp/get-advert-by-id';
+export default getAdverts;
 
-    function getAdverts(withEmpty) {
-        return Object.keys(dfpEnv.advertIds).reduce(function (advertsById, id) {
-            var advert = getAdvertById.getAdvertById(id);
-            // Do not return empty slots unless explicitely requested
-            if (withEmpty || !advert.isEmpty) {
-                advertsById[id] = advert;
-            }
-            return advertsById;
-        }, {});
-    }
-});
+function getAdverts(withEmpty) {
+    return Object.keys(dfpEnv.advertIds).reduce(function(advertsById, id) {
+        var advert = getAdvertById.getAdvertById(id);
+        // Do not return empty slots unless explicitely requested
+        if (withEmpty || !advert.isEmpty) {
+            advertsById[id] = advert;
+        }
+        return advertsById;
+    }, {});
+}
