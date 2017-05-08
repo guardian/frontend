456932dbeb5a6a04ac8d679a2e30b3fa0cf322ac
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-08 09:45:35 +0100

    transform projects/commercial/modules/dfp/enable-lazy-load.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/enable-lazy-load.js
+++ static/src/javascripts/projects/commercial/modules/dfp/enable-lazy-load.js
@@ -1,27 +1,26 @@
-define([
-    'lib/mediator',
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/lazy-load'
-], function (mediator, dfpEnv, lazyLoad) {
-    /* observer: IntersectionObserver?. The observer used to detect when ad slots enter the viewport */
-    var observer = null;
+import mediator from 'lib/mediator';
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import lazyLoad from 'commercial/modules/dfp/lazy-load';
+/* observer: IntersectionObserver?. The observer used to detect when ad slots enter the viewport */
+var observer = null;
 
-    return enableLazyLoad;
+export default enableLazyLoad;
 
-    function enableLazyLoad(advert) {
-        if (!dfpEnv.lazyLoadEnabled) {
-            dfpEnv.lazyLoadEnabled = true;
-            if (dfpEnv.lazyLoadObserve) {
-                observer = new IntersectionObserver(lazyLoad, { rootMargin: '200px 0%' });
-                dfpEnv.advertsToLoad.forEach(function (advert) {
-                    observer.observe(advert.node);
-                });
-            } else {
-                mediator.on('window:throttledScroll', lazyLoad);
-                lazyLoad();
-            }
-        } else if (dfpEnv.lazyLoadObserve && advert) {
-            observer.observe(advert.node);
+function enableLazyLoad(advert) {
+    if (!dfpEnv.lazyLoadEnabled) {
+        dfpEnv.lazyLoadEnabled = true;
+        if (dfpEnv.lazyLoadObserve) {
+            observer = new IntersectionObserver(lazyLoad, {
+                rootMargin: '200px 0%'
+            });
+            dfpEnv.advertsToLoad.forEach(function(advert) {
+                observer.observe(advert.node);
+            });
+        } else {
+            mediator.on('window:throttledScroll', lazyLoad);
+            lazyLoad();
         }
+    } else if (dfpEnv.lazyLoadObserve && advert) {
+        observer.observe(advert.node);
     }
-});
+}
