5402bac403ed60ae7431956486d8c9fed5a4d32c
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-09 13:17:26 +0100

    transform projects/commercial/modules/dfp/lazy-load.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/lazy-load.js
+++ static/src/javascripts/projects/commercial/modules/dfp/lazy-load.js
@@ -1,26 +1,24 @@
-define([
-    'fastdom',
-    'lib/config',
-    'lib/detect',
-    'lib/mediator',
-    'lib/user-timing',
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/load-advert',
-    'commercial/modules/dfp/performance-logging',
-    'commercial/modules/dfp/get-advert-by-id'
-], function (fastdom, config, detect, mediator, userTiming, dfpEnv, loadAdvert, performanceLogging, getAdvertById) {
-    /* depthOfScreen: double. Top and bottom margin of the visual viewport to check for the presence of an advert */
-    var depthOfScreen = 1.5;
+import fastdom from 'fastdom';
+import config from 'lib/config';
+import detect from 'lib/detect';
+import mediator from 'lib/mediator';
+import userTiming from 'lib/user-timing';
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import loadAdvert from 'commercial/modules/dfp/load-advert';
+import performanceLogging from 'commercial/modules/dfp/performance-logging';
+import getAdvertById from 'commercial/modules/dfp/get-advert-by-id';
+/* depthOfScreen: double. Top and bottom margin of the visual viewport to check for the presence of an advert */
+var depthOfScreen = 1.5;
 
-    var lazyLoad = dfpEnv.lazyLoadObserve ? onIntersect : onScroll;
+var lazyLoad = dfpEnv.lazyLoadObserve ? onIntersect : onScroll;
 
-    return lazyLoad;
+export default lazyLoad;
 
-    function onScroll() {
-        var viewportHeight = detect.getViewport().height;
+function onScroll() {
+    var viewportHeight = detect.getViewport().height;
 
-        var lazyLoadAds = dfpEnv.advertsToLoad
-        .filter(function (advert) {
+    var lazyLoadAds = dfpEnv.advertsToLoad
+        .filter(function(advert) {
             var rect = advert.node.getBoundingClientRect();
             var isNotHidden = rect.top + rect.left + rect.right + rect.bottom !== 0;
             var isNotTooFarFromTop = (1 - depthOfScreen) * viewportHeight < rect.bottom;
@@ -28,56 +26,55 @@ define([
             // load the ad only if it's setting within an acceptable range
             return isNotHidden && isNotTooFarFromTop && isNotTooFarFromBottom;
         })
-        .map(function (advert) {
+        .map(function(advert) {
             return advert.id;
         });
 
-        dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function (advert) {
-            return lazyLoadAds.indexOf(advert.id) < 0;
-        });
+    dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function(advert) {
+        return lazyLoadAds.indexOf(advert.id) < 0;
+    });
 
-        if (dfpEnv.advertsToLoad.length === 0) {
-            stopListening();
-        }
-
-        lazyLoadAds.forEach(displayAd);
+    if (dfpEnv.advertsToLoad.length === 0) {
+        stopListening();
     }
 
-    function stopListening() {
-        dfpEnv.lazyLoadEnabled = false;
-        mediator.off('window:throttledScroll', lazyLoad);
-    }
+    lazyLoadAds.forEach(displayAd);
+}
 
-    function onIntersect(entries, observer) {
-        var advertIds = [];
+function stopListening() {
+    dfpEnv.lazyLoadEnabled = false;
+    mediator.off('window:throttledScroll', lazyLoad);
+}
 
-        entries
-        .filter(function (entry) {
+function onIntersect(entries, observer) {
+    var advertIds = [];
+
+    entries
+        .filter(function(entry) {
             return !('isIntersecting' in entry) || entry.isIntersecting;
         })
-        .forEach(function (entry) {
+        .forEach(function(entry) {
             observer.unobserve(entry.target);
             displayAd(entry.target.id);
             advertIds.push(entry.target.id);
         });
 
-        dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function (advert) {
-            return advertIds.indexOf(advert.id) < 0;
-        });
+    dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function(advert) {
+        return advertIds.indexOf(advert.id) < 0;
+    });
 
-        if (dfpEnv.advertsToLoad.length === 0) {
-            stopObserving(observer);
-        }
+    if (dfpEnv.advertsToLoad.length === 0) {
+        stopObserving(observer);
     }
+}
 
-    function stopObserving(observer) {
-        dfpEnv.lazyLoadEnabled = false;
-        observer.disconnect();
-    }
+function stopObserving(observer) {
+    dfpEnv.lazyLoadEnabled = false;
+    observer.disconnect();
+}
 
-    function displayAd(advertId) {
-        var advert = getAdvertById.getAdvertById(advertId);
-        performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', userTiming.getCurrentTime());
-        loadAdvert(advert);
-    }
-});
+function displayAd(advertId) {
+    var advert = getAdvertById.getAdvertById(advertId);
+    performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', userTiming.getCurrentTime());
+    loadAdvert(advert);
+}
