a63f384040e622d08cc0edbba1d781ea1741d5e8
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 17:39:09 +0100

    transform projects/commercial/modules/hosted/next-video-autoplay.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/hosted/next-video-autoplay.js
+++ static/src/javascripts/projects/commercial/modules/hosted/next-video-autoplay.js
@@ -1,84 +1,75 @@
-define([
-    'bean',
-    'fastdom',
-    'commercial/modules/hosted/next-video',
-    'lib/$',
-    'common/modules/analytics/google'
-], function (
-    bean,
-    fastdom,
-    NextVideo,
-    $,
-    googleAnalytics
-) {
-    var nextVideoInterval;
-    var $hostedNext;
-    var $timer;
-    var nextVideoPage;
+import bean from 'bean';
+import fastdom from 'fastdom';
+import NextVideo from 'commercial/modules/hosted/next-video';
+import $ from 'lib/$';
+import googleAnalytics from 'common/modules/analytics/google';
+var nextVideoInterval;
+var $hostedNext;
+var $timer;
+var nextVideoPage;
 
-    function cancelAutoplay() {
-        fastdom.write(function () {
-            $hostedNext.addClass('hosted-slide-out');
-        });
-        clearInterval(nextVideoInterval);
-    }
+function cancelAutoplay() {
+    fastdom.write(function() {
+        $hostedNext.addClass('hosted-slide-out');
+    });
+    clearInterval(nextVideoInterval);
+}
 
-    function cancelAutoplayMobile($hostedNext) {
-        fastdom.write(function () {
-            $hostedNext.addClass('u-h');
-        });
-    }
+function cancelAutoplayMobile($hostedNext) {
+    fastdom.write(function() {
+        $hostedNext.addClass('u-h');
+    });
+}
 
-    function triggerAutoplay(getCurrentTimeFn, duration) {
-        nextVideoInterval = setInterval(function () {
-            var timeLeft = duration - Math.ceil(getCurrentTimeFn());
-            var countdownLength = 10; //seconds before the end when to show the timer
+function triggerAutoplay(getCurrentTimeFn, duration) {
+    nextVideoInterval = setInterval(function() {
+        var timeLeft = duration - Math.ceil(getCurrentTimeFn());
+        var countdownLength = 10; //seconds before the end when to show the timer
 
-            if (timeLeft <= countdownLength) {
-                fastdom.write(function () {
-                    $hostedNext.addClass('js-autoplay-start');
-                    $timer.text(timeLeft + 's');
-                });
-            }
-            if (timeLeft <= 0){
-                googleAnalytics.trackNonClickInteraction('Immediately play the next video');
-                window.location = nextVideoPage;
-            }
-        }, 1000);
-    }
+        if (timeLeft <= countdownLength) {
+            fastdom.write(function() {
+                $hostedNext.addClass('js-autoplay-start');
+                $timer.text(timeLeft + 's');
+            });
+        }
+        if (timeLeft <= 0) {
+            googleAnalytics.trackNonClickInteraction('Immediately play the next video');
+            window.location = nextVideoPage;
+        }
+    }, 1000);
+}
 
-    function triggerEndSlate() {
-        fastdom.write(function () {
-            $hostedNext.addClass('js-autoplay-start');
-        });
-        bean.on(document, 'click', $('.js-autoplay-cancel'), function () {
-            cancelAutoplayMobile($hostedNext);
-        });
-    }
+function triggerEndSlate() {
+    fastdom.write(function() {
+        $hostedNext.addClass('js-autoplay-start');
+    });
+    bean.on(document, 'click', $('.js-autoplay-cancel'), function() {
+        cancelAutoplayMobile($hostedNext);
+    });
+}
 
-    function addCancelListener() {
-        bean.on(document, 'click', $('.js-autoplay-cancel'), function () {
-            cancelAutoplay();
-        });
-    }
+function addCancelListener() {
+    bean.on(document, 'click', $('.js-autoplay-cancel'), function() {
+        cancelAutoplay();
+    });
+}
 
-    function canAutoplay() {
-        return $hostedNext.length && nextVideoPage;
-    }
+function canAutoplay() {
+    return $hostedNext.length && nextVideoPage;
+}
 
-    function init() {
-        return NextVideo.load().then(function() {
-            $hostedNext = $('.js-hosted-next-autoplay');
-            $timer = $('.js-autoplay-timer');
-            nextVideoPage = $timer.length && $timer.data('next-page');
-        });
-    }
+function init() {
+    return NextVideo.load().then(function() {
+        $hostedNext = $('.js-hosted-next-autoplay');
+        $timer = $('.js-autoplay-timer');
+        nextVideoPage = $timer.length && $timer.data('next-page');
+    });
+}
 
-    return {
-        init: init,
-        canAutoplay: canAutoplay,
-        triggerEndSlate: triggerEndSlate,
-        triggerAutoplay: triggerAutoplay,
-        addCancelListener: addCancelListener
-    };
-});
+export default {
+    init: init,
+    canAutoplay: canAutoplay,
+    triggerEndSlate: triggerEndSlate,
+    triggerAutoplay: triggerAutoplay,
+    addCancelListener: addCancelListener
+};
