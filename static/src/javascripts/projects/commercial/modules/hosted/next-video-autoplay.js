964a2b1949038d2964d8fadadd8700e9858c6f33
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 18:08:48 +0100

    Fix validation for next-video-autoplay.js

--- static/src/javascripts/projects/commercial/modules/hosted/next-video-autoplay.js
+++ static/src/javascripts/projects/commercial/modules/hosted/next-video-autoplay.js
@@ -1,75 +1,74 @@
+// @flow
 import bean from 'bean';
 import fastdom from 'fastdom';
 import NextVideo from 'commercial/modules/hosted/next-video';
 import $ from 'lib/$';
-import googleAnalytics from 'common/modules/analytics/google';
+import { trackNonClickInteraction } from 'common/modules/analytics/google';
+
 let nextVideoInterval;
 let $hostedNext;
 let $timer;
 let nextVideoPage;
 
-function cancelAutoplay() {
+const cancelAutoplay = () => {
     fastdom.write(() => {
         $hostedNext.addClass('hosted-slide-out');
     });
     clearInterval(nextVideoInterval);
-}
+};
 
-function cancelAutoplayMobile($hostedNext) {
+const cancelAutoplayMobile = () => {
     fastdom.write(() => {
         $hostedNext.addClass('u-h');
     });
-}
+};
 
-function triggerAutoplay(getCurrentTimeFn, duration) {
+const triggerAutoplay = (getCurrentTimeFn, duration) => {
     nextVideoInterval = setInterval(() => {
         const timeLeft = duration - Math.ceil(getCurrentTimeFn());
-        const countdownLength = 10; //seconds before the end when to show the timer
+        const countdownLength = 10; // seconds before the end when to show the timer
 
         if (timeLeft <= countdownLength) {
             fastdom.write(() => {
                 $hostedNext.addClass('js-autoplay-start');
-                $timer.text(timeLeft + 's');
+                $timer.text(`${timeLeft}s`);
             });
         }
         if (timeLeft <= 0) {
-            googleAnalytics.trackNonClickInteraction('Immediately play the next video');
+            trackNonClickInteraction('Immediately play the next video');
             window.location = nextVideoPage;
         }
     }, 1000);
-}
+};
 
-function triggerEndSlate() {
+const triggerEndSlate = () => {
     fastdom.write(() => {
         $hostedNext.addClass('js-autoplay-start');
     });
     bean.on(document, 'click', $('.js-autoplay-cancel'), () => {
-        cancelAutoplayMobile($hostedNext);
+        cancelAutoplayMobile();
     });
-}
+};
 
-function addCancelListener() {
+const addCancelListener = () => {
     bean.on(document, 'click', $('.js-autoplay-cancel'), () => {
         cancelAutoplay();
     });
-}
+};
 
-function canAutoplay() {
-    return $hostedNext.length && nextVideoPage;
-}
+const canAutoplay = () => $hostedNext.length && nextVideoPage;
 
-function init() {
-    return NextVideo.load().then(() => {
+const init = () =>
+    NextVideo.load().then(() => {
         $hostedNext = $('.js-hosted-next-autoplay');
         $timer = $('.js-autoplay-timer');
         nextVideoPage = $timer.length && $timer.data('next-page');
     });
-}
 
 export default {
     init,
     canAutoplay,
     triggerEndSlate,
     triggerAutoplay,
-    addCancelListener
+    addCancelListener,
 };
