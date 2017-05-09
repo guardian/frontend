b28fb431a161452313662a9f9c225531d0d5c347
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-09 14:50:20 +0100

    transform projects/commercial/modules/dfp/load-advert.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/load-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/load-advert.js
@@ -1,17 +1,14 @@
-define([
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/Advert'
-], function (dfpEnv, Advert) {
-    return loadAdvert;
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import Advert from 'commercial/modules/dfp/Advert';
+export default loadAdvert;
 
-    function loadAdvert(advert) {
-        advert.whenSlotReady
-            .catch(function(){
-                // The display needs to be called, even in the event of an error.
-            })
-            .then(function() {
-                Advert.startLoading(advert);
-                window.googletag.display(advert.id);
-            });
-    }
-});
+function loadAdvert(advert) {
+    advert.whenSlotReady
+        .catch(function() {
+            // The display needs to be called, even in the event of an error.
+        })
+        .then(function() {
+            Advert.startLoading(advert);
+            window.googletag.display(advert.id);
+        });
+}
