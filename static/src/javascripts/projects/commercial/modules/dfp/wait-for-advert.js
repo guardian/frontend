e8771b8ecc7de575b6a8d86cc609240b7e5d6639
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 15:14:40 +0100

    transform projects/commercial/modules/dfp/wait-for-advert.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/wait-for-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/wait-for-advert.js
@@ -1,21 +1,19 @@
-define([
-    'lodash/functions/memoize',
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/get-advert-by-id'
-], function (memoize, dfpEnv, getAdvertById) {
-    var waitForAdvert = memoize(function (id) {
-        return new Promise(function (resolve) {
-            checkAdvert();
-            function checkAdvert() {
-                var advert = getAdvertById.getAdvertById(id);
-                if (!advert) {
-                    window.setTimeout(checkAdvert, 200);
-                } else {
-                    resolve(advert);
-                }
-            }
-        });
-    });
+import memoize from 'lodash/functions/memoize';
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import getAdvertById from 'commercial/modules/dfp/get-advert-by-id';
+var waitForAdvert = memoize(function(id) {
+    return new Promise(function(resolve) {
+        checkAdvert();
 
-    return waitForAdvert;
+        function checkAdvert() {
+            var advert = getAdvertById.getAdvertById(id);
+            if (!advert) {
+                window.setTimeout(checkAdvert, 200);
+            } else {
+                resolve(advert);
+            }
+        }
+    });
 });
+
+export default waitForAdvert;
