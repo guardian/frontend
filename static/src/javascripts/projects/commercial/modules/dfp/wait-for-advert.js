72dd6f1b409f93330e471c9d1ad695a13db7c710
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 15:14:42 +0100

    transform projects/commercial/modules/dfp/wait-for-advert.js content to ES6

--- static/src/javascripts/projects/commercial/modules/dfp/wait-for-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/wait-for-advert.js
@@ -1,19 +1,17 @@
 import memoize from 'lodash/functions/memoize';
 import dfpEnv from 'commercial/modules/dfp/dfp-env';
 import getAdvertById from 'commercial/modules/dfp/get-advert-by-id';
-var waitForAdvert = memoize(function(id) {
-    return new Promise(function(resolve) {
-        checkAdvert();
+const waitForAdvert = memoize(id => new Promise(resolve => {
+    checkAdvert();
 
-        function checkAdvert() {
-            var advert = getAdvertById.getAdvertById(id);
-            if (!advert) {
-                window.setTimeout(checkAdvert, 200);
-            } else {
-                resolve(advert);
-            }
+    function checkAdvert() {
+        const advert = getAdvertById.getAdvertById(id);
+        if (!advert) {
+            window.setTimeout(checkAdvert, 200);
+        } else {
+            resolve(advert);
         }
-    });
-});
+    }
+}));
 
 export default waitForAdvert;
