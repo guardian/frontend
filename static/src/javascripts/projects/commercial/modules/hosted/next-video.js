2a4502afb47976d22ca367a3812e0714e63154d2
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 18:08:33 +0100

    Fix validation for next-video.js

--- static/src/javascripts/projects/commercial/modules/hosted/next-video.js
+++ static/src/javascripts/projects/commercial/modules/hosted/next-video.js
@@ -1,26 +1,30 @@
+// @flow
 import config from 'lib/config';
 import fetchJson from 'lib/fetch-json';
 import fastdom from 'lib/fastdom-promise';
 
-export default {
-    init: loadNextVideo,
-    load: loadNextVideo
-};
-
-function loadNextVideo() {
-
+const loadNextVideo = () => {
     const placeholders = document.querySelectorAll('.js-autoplay-placeholder');
 
     if (placeholders.length) {
-        return fetchJson(config.page.ajaxUrl + '/' + config.page.pageId + '/' + 'autoplay.json', {
-                mode: 'cors'
-            })
-            .then(json => fastdom.write(() => {
-            let i;
-            for (i = 0; i < placeholders.length; i++) {
-                placeholders[i].innerHTML = json.html;
+        return fetchJson(
+            `${config.page.ajaxUrl}/${config.page.pageId}/autoplay.json`,
+            {
+                mode: 'cors',
             }
-        }));
+        ).then(json =>
+            fastdom.write(() => {
+                let i;
+                for (i = 0; i < placeholders.length; i += 1) {
+                    placeholders[i].innerHTML = json.html;
+                }
+            })
+        );
     }
     return Promise.resolve();
-}
+};
+
+export default {
+    init: loadNextVideo,
+    load: loadNextVideo,
+};
