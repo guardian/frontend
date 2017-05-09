9ed362a46fe37d9cecc8cefa671f0064d0d94404
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 16:37:09 +0100

    transform projects/commercial/modules/high-merch.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/high-merch.js
+++ static/src/javascripts/projects/commercial/modules/high-merch.js
@@ -1,64 +1,60 @@
-define([
-    'lib/config',
-    'lib/fastdom-promise',
-    'common/modules/experiments/ab',
-    'commercial/modules/dfp/add-slot',
-    'commercial/modules/dfp/create-slot',
-    'commercial/modules/dfp/track-ad-render',
-    'commercial/modules/commercial-features'
-], function (config, fastdom, ab, addSlot, createSlot, trackAdRender, commercialFeatures) {
-    return {
-        init: init
-    };
+import config from 'lib/config';
+import fastdom from 'lib/fastdom-promise';
+import ab from 'common/modules/experiments/ab';
+import addSlot from 'commercial/modules/dfp/add-slot';
+import createSlot from 'commercial/modules/dfp/create-slot';
+import trackAdRender from 'commercial/modules/dfp/track-ad-render';
+import commercialFeatures from 'commercial/modules/commercial-features';
+export default {
+    init: init
+};
 
-    function isLuckyBastard() {
-        var testName = 'PaidContentVsOutbrain2';
-        return ab.testCanBeRun(testName) && ab.getTestVariantId(testName) === 'paid-content';
-    }
+function isLuckyBastard() {
+    var testName = 'PaidContentVsOutbrain2';
+    return ab.testCanBeRun(testName) && ab.getTestVariantId(testName) === 'paid-content';
+}
 
-    function init() {
-        if (commercialFeatures.highMerch) {
-            var anchorSelector = config.page.commentable ? '#comments + *' : '.content-footer > :first-child';
-            var anchor = document.querySelector(anchorSelector);
-            var container = document.createElement('div');
+function init() {
+    if (commercialFeatures.highMerch) {
+        var anchorSelector = config.page.commentable ? '#comments + *' : '.content-footer > :first-child';
+        var anchor = document.querySelector(anchorSelector);
+        var container = document.createElement('div');
 
-            container.className = 'fc-container fc-container--commercial';
-            container.appendChild(createSlot(config.page.isPaidContent ? 'high-merch-paid' : 'high-merch'));
+        container.className = 'fc-container fc-container--commercial';
+        container.appendChild(createSlot(config.page.isPaidContent ? 'high-merch-paid' : 'high-merch'));
 
-            if (commercialFeatures.outbrain && isLuckyBastard()) {
-                trackAdRender('dfp-ad--merchandising-high')
-                    .then(insertAlternativeSlot);
+        if (commercialFeatures.outbrain && isLuckyBastard()) {
+            trackAdRender('dfp-ad--merchandising-high')
+                .then(insertAlternativeSlot);
+        }
+
+        return fastdom.write(function() {
+            if (anchor && anchor.parentNode) {
+                anchor.parentNode.insertBefore(container, anchor);
             }
-
-            return fastdom.write(function () {
-                if (anchor && anchor.parentNode) {
-                    anchor.parentNode.insertBefore(container, anchor);
-                }
-            });
-        } else if (commercialFeatures.outbrain && isLuckyBastard()) {
-            insertAlternativeSlot(false);
-        }
-
-        return Promise.resolve();
+        });
+    } else if (commercialFeatures.outbrain && isLuckyBastard()) {
+        insertAlternativeSlot(false);
     }
 
-    function insertAlternativeSlot(isHiResLoaded) {
-        if (isHiResLoaded) {
-            return;
-        }
+    return Promise.resolve();
+}
 
-        var container = document.querySelector(
-            !(config.page.seriesId || config.page.blogIds) ?
-            '.js-related, .js-outbrain-anchor' :
-            '.js-outbrain-anchor'
-        );
-        var slot = createSlot('high-merch-lucky');
+function insertAlternativeSlot(isHiResLoaded) {
+    if (isHiResLoaded) {
+        return;
+    }
 
-        fastdom.write(function () {
+    var container = document.querySelector(!(config.page.seriesId || config.page.blogIds) ?
+        '.js-related, .js-outbrain-anchor' :
+        '.js-outbrain-anchor'
+    );
+    var slot = createSlot('high-merch-lucky');
+
+    fastdom.write(function() {
             container.parentNode.insertBefore(slot, container.nextSibling);
         })
-        .then(function () {
+        .then(function() {
             addSlot.addSlot(slot, true);
         });
-    }
-});
+}
