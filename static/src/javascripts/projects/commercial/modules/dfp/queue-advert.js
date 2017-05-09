5609af91aef593b9f3f6d89659e7211af31a1c43
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 11:35:38 +0100

    transform projects/commercial/modules/dfp/queue-advert.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/queue-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/queue-advert.js
@@ -1,15 +1,11 @@
-define([
-    'commercial/modules/dfp/dfp-env'
-], function (dfpEnv) {
-    return queueAdvert;
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+export default queueAdvert;
 
-    function queueAdvert(advert) {
-        dfpEnv.advertsToLoad.push(advert);
-        // Add to the array of ads to be refreshed (when the breakpoint changes)
-        // only if its `data-refresh` attribute isn't set to false.
-        if (advert.node.getAttribute('data-refresh') !== 'false') {
-            dfpEnv.advertsToRefresh.push(advert);
-        }
+function queueAdvert(advert) {
+    dfpEnv.advertsToLoad.push(advert);
+    // Add to the array of ads to be refreshed (when the breakpoint changes)
+    // only if its `data-refresh` attribute isn't set to false.
+    if (advert.node.getAttribute('data-refresh') !== 'false') {
+        dfpEnv.advertsToRefresh.push(advert);
     }
-
-});
+}
