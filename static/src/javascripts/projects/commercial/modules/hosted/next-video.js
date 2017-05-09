4c1b7c6925b1eb37001fbb03d7d56bc09fe9b79a
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 17:39:47 +0100

    transform projects/commercial/modules/hosted/next-video.js content to ES6

--- static/src/javascripts/projects/commercial/modules/hosted/next-video.js
+++ static/src/javascripts/projects/commercial/modules/hosted/next-video.js
@@ -9,20 +9,18 @@ export default {
 
 function loadNextVideo() {
 
-    var placeholders = document.querySelectorAll('.js-autoplay-placeholder');
+    const placeholders = document.querySelectorAll('.js-autoplay-placeholder');
 
     if (placeholders.length) {
         return fetchJson(config.page.ajaxUrl + '/' + config.page.pageId + '/' + 'autoplay.json', {
                 mode: 'cors'
             })
-            .then(function(json) {
-                return fastdom.write(function() {
-                    var i;
-                    for (i = 0; i < placeholders.length; i++) {
-                        placeholders[i].innerHTML = json.html;
-                    }
-                });
-            });
+            .then(json => fastdom.write(() => {
+            let i;
+            for (i = 0; i < placeholders.length; i++) {
+                placeholders[i].innerHTML = json.html;
+            }
+        }));
     }
     return Promise.resolve();
 }
