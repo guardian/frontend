a4b5d1846e06be3f1d852d2b8152e27a163e91eb
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 17:43:12 +0100

    Fix validation for about.js

--- static/src/javascripts/projects/commercial/modules/hosted/about.js
+++ static/src/javascripts/projects/commercial/modules/hosted/about.js
@@ -1,27 +1,27 @@
+// @flow
 import SurveySimple from 'commercial/modules/survey/survey-simple';
 
-function init() {
+const init = () => {
     const survey = new SurveySimple({
         id: 'hosted-about',
         header: 'Advertiser content',
         paragraph1: 'Advertiser content is used to describe advertisement features that are paid for, produced and controlled by the advertiser rather than the publisher​.',
         paragraph2: 'They​ are subject to regulation by the Advertising Standards Authority in the UK, the Federal Trade Commission in the US and the Advertising Standards Bureau in Australia.',
         paragraph3: 'This content is produced by the advertiser and does not involve Guardian News and Media staff.',
-        showCloseBtn: true
+        showCloseBtn: true,
     });
 
-    return survey.attach()
-        .then(() => {
-            const aboutBtn = document.querySelector('.js-hosted-about');
-            const overlay = document.querySelector('.js-survey-overlay');
+    return survey.attach().then(() => {
+        const aboutBtn = document.querySelector('.js-hosted-about');
+        const overlay = document.querySelector('.js-survey-overlay');
 
-            aboutBtn.addEventListener('click', e => {
-                e.preventDefault();
-                overlay.classList.remove('u-h');
-            });
+        aboutBtn.addEventListener('click', e => {
+            e.preventDefault();
+            overlay.classList.remove('u-h');
         });
-}
+    });
+};
 
 export default {
-    init
+    init,
 };
