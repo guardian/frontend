5266a5a420e0b58eb32bfe340df175288fa86704
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-09 13:36:05 +0100

    ES6 lazyLoad

--- static/src/javascripts/projects/commercial/modules/dfp/lazy-load.js
+++ static/src/javascripts/projects/commercial/modules/dfp/lazy-load.js
@@ -1,80 +1,92 @@
-import fastdom from 'fastdom';
-import config from 'lib/config';
+// @flow
+
+/* eslint no-use-before-define: "off" */
+
 import detect from 'lib/detect';
 import mediator from 'lib/mediator';
-import userTiming from 'lib/user-timing';
+import { getCurrentTime } from 'lib/user-timing';
 import dfpEnv from 'commercial/modules/dfp/dfp-env';
 import loadAdvert from 'commercial/modules/dfp/load-advert';
 import performanceLogging from 'commercial/modules/dfp/performance-logging';
-import getAdvertById from 'commercial/modules/dfp/get-advert-by-id';
+import { getAdvertById } from 'commercial/modules/dfp/get-advert-by-id';
+
+const IntersectionObserver = window.IntersectionObserver;
+const IntersectionObserverEntry = window.IntersectionObserverEntry;
+
 /* depthOfScreen: double. Top and bottom margin of the visual viewport to check for the presence of an advert */
-var depthOfScreen = 1.5;
+const depthOfScreen = 1.5;
 
-var lazyLoad = dfpEnv.lazyLoadObserve ? onIntersect : onScroll;
+const stopListening = (): void => {
+    dfpEnv.lazyLoadEnabled = false;
+    mediator.off('window:throttledScroll', lazyLoad);
+};
 
-export default lazyLoad;
+const stopObserving = (observer: IntersectionObserver): void => {
+    dfpEnv.lazyLoadEnabled = false;
+    observer.disconnect();
+};
 
-function onScroll() {
-    var viewportHeight = detect.getViewport().height;
+const displayAd = (advertId: string): void => {
+    const advert = getAdvertById(advertId);
+    performanceLogging.updateAdvertMetric(
+        advert,
+        'lazyWaitComplete',
+        getCurrentTime()
+    );
+    loadAdvert(advert);
+};
 
-    var lazyLoadAds = dfpEnv.advertsToLoad
-        .filter(function(advert) {
-            var rect = advert.node.getBoundingClientRect();
-            var isNotHidden = rect.top + rect.left + rect.right + rect.bottom !== 0;
-            var isNotTooFarFromTop = (1 - depthOfScreen) * viewportHeight < rect.bottom;
-            var isNotTooFarFromBottom = rect.top < viewportHeight * depthOfScreen;
+const onScroll = (): void => {
+    const viewportHeight: number = detect.getViewport().height;
+
+    const lazyLoadAds: Array<string> = dfpEnv.advertsToLoad
+        .filter(advert => {
+            const rect: window.DOMRect = advert.node.getBoundingClientRect();
+            const isNotHidden: boolean =
+                rect.top + rect.left + rect.right + rect.bottom !== 0;
+            const isNotTooFarFromTop: boolean =
+                (1 - depthOfScreen) * viewportHeight < rect.bottom;
+            const isNotTooFarFromBottom: boolean =
+                rect.top < viewportHeight * depthOfScreen;
             // load the ad only if it's setting within an acceptable range
             return isNotHidden && isNotTooFarFromTop && isNotTooFarFromBottom;
         })
-        .map(function(advert) {
-            return advert.id;
-        });
+        .map(advert => advert.id);
 
-    dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function(advert) {
-        return lazyLoadAds.indexOf(advert.id) < 0;
-    });
+    dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(
+        advert => !lazyLoadAds.includes(advert.id)
+    );
 
     if (dfpEnv.advertsToLoad.length === 0) {
         stopListening();
     }
 
     lazyLoadAds.forEach(displayAd);
-}
+};
 
-function stopListening() {
-    dfpEnv.lazyLoadEnabled = false;
-    mediator.off('window:throttledScroll', lazyLoad);
-}
-
-function onIntersect(entries, observer) {
-    var advertIds = [];
+const onIntersect = (
+    entries: Array<IntersectionObserverEntry>,
+    observer: IntersectionObserver
+): void => {
+    const advertIds: Array<string> = [];
 
     entries
-        .filter(function(entry) {
-            return !('isIntersecting' in entry) || entry.isIntersecting;
-        })
-        .forEach(function(entry) {
+        .filter(entry => !('isIntersecting' in entry) || entry.isIntersecting)
+        .forEach(entry => {
             observer.unobserve(entry.target);
             displayAd(entry.target.id);
             advertIds.push(entry.target.id);
         });
 
-    dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(function(advert) {
-        return advertIds.indexOf(advert.id) < 0;
-    });
+    dfpEnv.advertsToLoad = dfpEnv.advertsToLoad.filter(
+        advert => advertIds.indexOf(advert.id) < 0
+    );
 
     if (dfpEnv.advertsToLoad.length === 0) {
         stopObserving(observer);
     }
-}
+};
 
-function stopObserving(observer) {
-    dfpEnv.lazyLoadEnabled = false;
-    observer.disconnect();
-}
+const lazyLoad = dfpEnv.lazyLoadObserve ? onIntersect : onScroll;
 
-function displayAd(advertId) {
-    var advert = getAdvertById.getAdvertById(advertId);
-    performanceLogging.updateAdvertMetric(advert, 'lazyWaitComplete', userTiming.getCurrentTime());
-    loadAdvert(advert);
-}
+export default lazyLoad;
