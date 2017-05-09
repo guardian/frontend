82979123503b13245e78dd08db102315772a20ff
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-05-09 15:26:01 +0100

    Fix validation wait-for-advert.js

--- static/src/javascripts/projects/commercial/modules/dfp/wait-for-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/wait-for-advert.js
@@ -1,17 +1,20 @@
+// @flow
 import memoize from 'lodash/functions/memoize';
-import dfpEnv from 'commercial/modules/dfp/dfp-env';
-import getAdvertById from 'commercial/modules/dfp/get-advert-by-id';
-const waitForAdvert = memoize(id => new Promise(resolve => {
-    checkAdvert();
+import { getAdvertById } from 'commercial/modules/dfp/get-advert-by-id';
 
-    function checkAdvert() {
-        const advert = getAdvertById.getAdvertById(id);
-        if (!advert) {
-            window.setTimeout(checkAdvert, 200);
-        } else {
-            resolve(advert);
-        }
-    }
-}));
+const waitForAdvert = memoize(
+    id =>
+        new Promise(resolve => {
+            const checkAdvert = () => {
+                const advert = getAdvertById(id);
+                if (!advert) {
+                    window.setTimeout(checkAdvert, 200);
+                } else {
+                    resolve(advert);
+                }
+            };
+            checkAdvert();
+        })
+);
 
 export default waitForAdvert;
