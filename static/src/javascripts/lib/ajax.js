f656d7fbb7c8e99f9deff0e7704334eefa07b894
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-10 14:42:45 +0100

    transformed ajax to es6

--- static/src/javascripts/lib/ajax.js
+++ static/src/javascripts/lib/ajax.js
@@ -1,27 +1,32 @@
+// @flow
 import reqwest from 'reqwest';
 import config from 'lib/config';
 import raven from 'lib/raven';
 // This should no longer be used.
 // Prefer the new 'lib/fetch' or 'lib/fetch-json' library instead, which are es6 compliant.
-var ajaxHost = config.page.ajaxUrl || '';
+let ajaxHost = config.page.ajaxUrl || '';
 
-function ajax(params) {
-    var r;
+const ajax = (params: Object): any => {
+    const options = params;
 
-    if (!params.url.match('^(https?:)?//')) {
-        params.url = ajaxHost + params.url;
-        params.crossOrigin = true;
+    if (!options.url.match('^(https?:)?//')) {
+        options.url = ajaxHost + options.url;
+        options.crossOrigin = true;
     }
 
-    r = reqwest(params);
-    raven.wrap({
-        deep: true
-    }, r.then);
-    return r;
-}
+    const r = reqwest(options);
 
-ajax.setHost = function(host) {
+    raven.wrap(
+        {
+            deep: true,
+        },
+        r.then
+    );
+    return r;
+};
+
+ajax.setHost = host => {
     ajaxHost = host;
 };
 
-export default ajax;
+export { ajax };
