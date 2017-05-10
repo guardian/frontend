2050906f00e9145ff3ea3999c11a643d24a70a62
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-10 14:34:42 +0100

    transform lib/ajax.js to ES6 module

--- static/src/javascripts/lib/ajax.js
+++ static/src/javascripts/lib/ajax.js
@@ -1,33 +1,27 @@
-define([
-    'reqwest',
-    'lib/config',
-    'lib/raven'
-], function (
-    reqwest,
-    config,
-    raven
-) {
-    // This should no longer be used.
-    // Prefer the new 'lib/fetch' or 'lib/fetch-json' library instead, which are es6 compliant.
-    var ajaxHost = config.page.ajaxUrl || '';
+import reqwest from 'reqwest';
+import config from 'lib/config';
+import raven from 'lib/raven';
+// This should no longer be used.
+// Prefer the new 'lib/fetch' or 'lib/fetch-json' library instead, which are es6 compliant.
+var ajaxHost = config.page.ajaxUrl || '';
 
-    function ajax(params) {
-        var r;
+function ajax(params) {
+    var r;
 
-        if (!params.url.match('^(https?:)?//')) {
-            params.url = ajaxHost + params.url;
-            params.crossOrigin = true;
-        }
-
-        r = reqwest(params);
-        raven.wrap({ deep: true }, r.then);
-        return r;
+    if (!params.url.match('^(https?:)?//')) {
+        params.url = ajaxHost + params.url;
+        params.crossOrigin = true;
     }
 
-    ajax.setHost = function (host) {
-        ajaxHost = host;
-    };
+    r = reqwest(params);
+    raven.wrap({
+        deep: true
+    }, r.then);
+    return r;
+}
 
-    return ajax;
+ajax.setHost = function(host) {
+    ajaxHost = host;
+};
 
-});
+export default ajax;
