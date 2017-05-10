3ac707ad0038effdae968eedb753eff2ea095701
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-10 17:34:34 +0100

    disallow multiple parens in classlist

--- dev/eslint-plugin-guardian-frontend/__tests__/no-multiple-classlist-parameter.js
+++ dev/eslint-plugin-guardian-frontend/__tests__/no-multiple-classlist-parameter.js
@@ -4,9 +4,15 @@ const rule = require('../rules/no-multiple-classlist-parameters');
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
 
 ruleTester.run('no-multiple-classlist-parameter', rule, {
-    valid: ['element.classList.add("class1")'],
+    valid: [
+        'element.classList.add("class1")',
+        'element.classList.remove("class1")',
+    ],
 
-    invalid: ['element.classList.add("class1", "class2")'].map(code => ({
+    invalid: [
+        'element.classList.add("class1", "class2")',
+        'element.classList.add("class1", "class2", "class3")',
+    ].map(code => ({
         code,
         errors: [
             {
--- dev/eslint-plugin-guardian-frontend/rules/no-multiple-classlist-parameters.js
+++ dev/eslint-plugin-guardian-frontend/rules/no-multiple-classlist-parameters.js
@@ -1,12 +1,39 @@
 module.exports = {
     create(context) {
+        const isDot = token =>
+            token && token.type === 'Punctuator' && token.value === '.';
+        const isComma = token =>
+            token && token.type === 'Punctuator' && token.value === ',';
+        const isIdentifierOf = (prop, token) =>
+            token && token.type === 'Identifier' && token.value === prop;
+
         return {
             Identifier: node => {
-                if (node.name === 'classList') {
-                    context.report({
-                        node,
-                        message: 'You are using a classlist',
-                    });
+                if (node.name === 'add') {
+                    const [
+                        dot1,
+                        parent,
+                    ] = context
+                        .getSourceCode()
+                        .getTokensBefore(node, 2)
+                        .reverse();
+
+                    const [
+                        ,
+                        ,
+                        nextToken,
+                    ] = context.getSourceCode().getTokensAfter(node, 3);
+
+                    if (
+                        isDot(dot1) &&
+                        isIdentifierOf('classList', parent) &&
+                        isComma(nextToken)
+                    ) {
+                        context.report({
+                            node,
+                            message: 'You are using a classList.add',
+                        });
+                    }
                 }
             },
         };
