92bff015d5ba938ffbede4d8ef51ce9275fa05cd
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-05-10 11:06:23 +0100

    transform projects/commercial/modules/user-ad-feedback.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/user-ad-feedback.js
+++ static/src/javascripts/projects/commercial/modules/user-ad-feedback.js
@@ -1,52 +1,42 @@
-define([
-    'qwery',
-    'fastdom',
-    'lib/fetch',
-    'lib/config',
-    'lib/detect'
-], function (
-    qwery,
-    fastdom,
-    fetch,
-    config,
-    detect
-) {
+import qwery from 'qwery';
+import fastdom from 'fastdom';
+import fetch from 'lib/fetch';
+import config from 'lib/config';
+import detect from 'lib/detect';
 
-    return function recordUserAdFeedback(pagePath, adSlotId, slotRenderEvent, feedbackType, comment) {
-        var feedbackUrl = 'https://j2cy9stt59.execute-api.eu-west-1.amazonaws.com/prod/adFeedback';
-        var stage = config.page.isProd ? 'PROD' : 'CODE';
-        var ua = detect.getUserAgent;
-        var breakPoint = detect.getBreakpoint();
+export default function recordUserAdFeedback(pagePath, adSlotId, slotRenderEvent, feedbackType, comment) {
+    var feedbackUrl = 'https://j2cy9stt59.execute-api.eu-west-1.amazonaws.com/prod/adFeedback';
+    var stage = config.page.isProd ? 'PROD' : 'CODE';
+    var ua = detect.getUserAgent;
+    var breakPoint = detect.getBreakpoint();
 
-        var data = {
-            stage: stage,
-            page: pagePath,
-            adSlotId: adSlotId,
-            creativeId: slotRenderEvent.sourceAgnosticCreativeId.toString(),
-            lineId: slotRenderEvent.sourceAgnosticLineItemId.toString(),
-            feedback: feedbackType,
-            comment: comment,
-            browser: ua.browser.toString() + ua.version.toString(),
-            breakPoint: breakPoint
-        };
-
-        return fetch(feedbackUrl, {
-            method: 'post',
-            body: JSON.stringify(data),
-            mode: 'cors'
-        }).then(
-            onComplete.bind(this, adSlotId),
-            onComplete.bind(this, adSlotId)
-        );
+    var data = {
+        stage: stage,
+        page: pagePath,
+        adSlotId: adSlotId,
+        creativeId: slotRenderEvent.sourceAgnosticCreativeId.toString(),
+        lineId: slotRenderEvent.sourceAgnosticLineItemId.toString(),
+        feedback: feedbackType,
+        comment: comment,
+        browser: ua.browser.toString() + ua.version.toString(),
+        breakPoint: breakPoint
     };
 
-    function onComplete(adSlotId) {    // we're complete - update the UI
-        fastdom.write(function() {
-            var label = document.querySelector('#' + adSlotId + '>.ad-slot__label');
-            if (label) {
-                label.classList.add('feedback-submitted');
-            }
-        });
-    }
+    return fetch(feedbackUrl, {
+        method: 'post',
+        body: JSON.stringify(data),
+        mode: 'cors'
+    }).then(
+        onComplete.bind(this, adSlotId),
+        onComplete.bind(this, adSlotId)
+    );
+};
 
-});
+function onComplete(adSlotId) { // we're complete - update the UI
+    fastdom.write(function() {
+        var label = document.querySelector('#' + adSlotId + '>.ad-slot__label');
+        if (label) {
+            label.classList.add('feedback-submitted');
+        }
+    });
+}
