1d99d7899149b4817bdd28df0c4cb84b8238b89c
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-05-10 14:59:01 +0100

    transform projects/commercial/modules/paid-containers.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/paid-containers.js
+++ static/src/javascripts/projects/commercial/modules/paid-containers.js
@@ -1,42 +1,38 @@
-define([
-    'qwery',
-    'bean',
-    'fastdom'
-], function (qwery, bean, fastdom) {
+import qwery from 'qwery';
+import bean from 'bean';
+import fastdom from 'fastdom';
 
-    return {
-        init: init
-    };
+export default {
+    init: init
+};
 
-    function init() {
-        var showMores = qwery('.adverts__more > summary');
-        bean.on(document, 'click', showMores, onOpenClick);
-        bean.on(document, 'click', showMores, onKeyPress(onOpenClick));
+function init() {
+    var showMores = qwery('.adverts__more > summary');
+    bean.on(document, 'click', showMores, onOpenClick);
+    bean.on(document, 'click', showMores, onKeyPress(onOpenClick));
 
-        return Promise.resolve();
-    }
+    return Promise.resolve();
+}
 
-    function onKeyPress(handler) {
-        return function (event) {
-            if (event.keyCode === 0x20 || event.keyCode === 0x0D) {
-                handler(event);
-            }
-        };
-    }
-
-    function onOpenClick(event) {
-        var summary = event.currentTarget;
-        var details = summary.parentNode;
-        var label = summary.querySelector('.js-button__label');
-        if (details.hasAttribute('open')) {
-            fastdom.write(function () {
-                label.textContent = 'More ' + summary.getAttribute('data-text');
-            });
-        } else {
-            fastdom.write(function () {
-                label.textContent = 'Less';
-            });
+function onKeyPress(handler) {
+    return function(event) {
+        if (event.keyCode === 0x20 || event.keyCode === 0x0D) {
+            handler(event);
         }
-    }
+    };
+}
 
-});
+function onOpenClick(event) {
+    var summary = event.currentTarget;
+    var details = summary.parentNode;
+    var label = summary.querySelector('.js-button__label');
+    if (details.hasAttribute('open')) {
+        fastdom.write(function() {
+            label.textContent = 'More ' + summary.getAttribute('data-text');
+        });
+    } else {
+        fastdom.write(function() {
+            label.textContent = 'Less';
+        });
+    }
+}
