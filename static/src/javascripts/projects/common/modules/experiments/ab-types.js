a891acaabe07d3d6d1a428465197eb02d7c3935d
Sam Desborough <sam.desborough@gmail.com>
2017-05-11 15:17:55 +0100

    Other transformations required for testCanRunChecks

---
+++ static/src/javascripts/projects/common/modules/experiments/ab-types.js
@@ -0,0 +1,44 @@
+// @flow
+
+type ListenerFunction = (f: () => void) => void;
+
+export type Variant = {
+    id: string,
+    test: () => void,
+    impression?: ListenerFunction,
+    success?: ListenerFunction,
+};
+
+export type ABTest = {
+    id: string,
+    start: string,
+    expiry: string,
+    author: string,
+    description: string,
+    audience: number,
+    audienceOffset: number,
+    successMeasure: string,
+    audienceCriteria: string,
+    showForSensitive?: boolean,
+    dataLinkNames?: string,
+    idealOutcome?: string,
+    variants: Array<Variant>,
+    canRun: () => boolean,
+    notInTest?: () => void,
+};
+
+/**
+ * an individual A/B test, structured for Ophan
+ */
+export type OphanABEvent = {
+    variantName: string,
+    complete: string | boolean,
+    campaignCodes?: Array<string>,
+};
+
+/**
+ * the actual payload we send to Ophan: an object of OphanABEvents with test IDs as keys
+ */
+export type OphanABPayload = {
+    [testId: string]: OphanABEvent,
+};
--- static/src/javascripts/projects/common/modules/experiments/test-can-run-checks.js
+++ static/src/javascripts/projects/common/modules/experiments/test-can-run-checks.js
@@ -1,23 +1,24 @@
+// @flow
+import type { ABTest } from 'common/modules/experiments/ab-types';
+
 import config from 'lib/config';
 
-function isTestSwitchedOn(test) {
-    return config.switches['ab' + test.id];
-}
+const isTestSwitchedOn = test => config.switches[`ab${test.id}`];
 
-function isExpired(testExpiry) {
+export const isExpired = (testExpiry: string) => {
     // new Date(test.expiry) sets the expiry time to 00:00:00
     // Using SetHours allows a test to run until the END of the expiry day
     const startOfToday = new Date().setHours(0, 0, 0, 0);
     return startOfToday > new Date(testExpiry);
-}
+};
 
-function testCanBeRun(test) {
-    const expired = isExpired(test.expiry), isSensitive = config.page.isSensitive;
-
-    return ((isSensitive ? test.showForSensitive : true) && isTestSwitchedOn(test)) && !expired && (!test.canRun || test.canRun());
-}
-
-export default {
-    isExpired,
-    testCanBeRun
-}
+export const testCanBeRun = (test: ABTest) => {
+    const expired = isExpired(test.expiry);
+    const isSensitive = config.page.isSensitive;
+    return (
+        (isSensitive ? test.showForSensitive : true) &&
+        isTestSwitchedOn(test) &&
+        !expired &&
+        (!test.canRun || test.canRun())
+    );
+};
