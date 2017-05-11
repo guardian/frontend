07c4c8c7e548b9898fd3a42dbc00b83f9ffa23ef
Kate Whalen <whalenka88@gmail.com>
2017-05-11 16:24:24 +0100

    transform projects/commercial/modules/hosted/onward.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/hosted/onward.js
+++ static/src/javascripts/projects/commercial/modules/hosted/onward.js
@@ -1,46 +1,42 @@
-define([
-    'lib/config',
-    'lib/mediator',
-    'lib/fetch-json',
-    'lib/fastdom-promise',
-    'commercial/modules/hosted/onward-journey-carousel'
-], function (config, mediator, fetchJson, fastdom, HostedCarousel) {
+import config from 'lib/config';
+import mediator from 'lib/mediator';
+import fetchJson from 'lib/fetch-json';
+import fastdom from 'lib/fastdom-promise';
+import HostedCarousel from 'commercial/modules/hosted/onward-journey-carousel';
 
-    return {
-        init: loadOnwardComponent,
-        whenRendered: new Promise(function (resolve) {
-            mediator.on('hosted:onward:done', resolve);
-        })
-    };
+export default {
+    init: loadOnwardComponent,
+    whenRendered: new Promise(function(resolve) {
+        mediator.on('hosted:onward:done', resolve);
+    })
+};
 
-    function loadOnwardComponent(start, stop) {
-        start();
+function loadOnwardComponent(start, stop) {
+    start();
 
-        var placeholders = document.getElementsByClassName('js-onward-placeholder');
+    var placeholders = document.getElementsByClassName('js-onward-placeholder');
 
-        if (placeholders.length) {
+    if (placeholders.length) {
 
-            fetchJson(config.page.ajaxUrl + '/'
-                + config.page.pageId + '/'
-                + config.page.contentType.toLowerCase() + '/'
-                + 'onward.json', {mode: 'cors'})
-                .then(function (json) {
-                    return fastdom.write(function () {
-                        var i;
-                        for (i = 0; i < placeholders.length; i++) {
-                            placeholders[i].insertAdjacentHTML('beforeend', json.html);
-                        }
-                    });
-                })
-                .then(function () {
-                    HostedCarousel.init();
-                    mediator.emit('hosted:onward:done');
-                })
-                .then(stop);
-        } else {
-            stop();
-        }
-
-        return Promise.resolve();
+        fetchJson(config.page.ajaxUrl + '/' + config.page.pageId + '/' + config.page.contentType.toLowerCase() + '/' + 'onward.json', {
+                mode: 'cors'
+            })
+            .then(function(json) {
+                return fastdom.write(function() {
+                    var i;
+                    for (i = 0; i < placeholders.length; i++) {
+                        placeholders[i].insertAdjacentHTML('beforeend', json.html);
+                    }
+                });
+            })
+            .then(function() {
+                HostedCarousel.init();
+                mediator.emit('hosted:onward:done');
+            })
+            .then(stop);
+    } else {
+        stop();
     }
-});
+
+    return Promise.resolve();
+}
