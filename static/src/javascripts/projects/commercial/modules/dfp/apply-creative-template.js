b13ec3f194188e4f439ba8851a5576bc4e6b75da
Jon Norman <jon.norman@guardian.co.uk>
2017-05-11 16:48:54 +0100

    transform projects/commercial/modules/dfp/apply-creative-template.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/apply-creative-template.js
+++ static/src/javascripts/projects/commercial/modules/dfp/apply-creative-template.js
@@ -1,110 +1,113 @@
-define([
-    'lib/fastdom-promise',
-    'lib/report-error',
+// @flow
 
-    // These need to be bundled, so that they can be fetched asynchronously in production
-    'commercial/modules/creatives/frame',
-    'commercial/modules/creatives/revealer',
-    'commercial/modules/creatives/fabric-v1',
-    'commercial/modules/creatives/fabric-expanding-v1',
-    'commercial/modules/creatives/fabric-expandable-video-v2',
-    'commercial/modules/creatives/fabric-video',
-    'commercial/modules/creatives/scrollable-mpu-v2'
-], function (
-    fastdom,
-    reportError
-) {
-    /**
-     * Not all adverts render themselves - some just provide data for templates that we implement in commercial.js.
-     * This looks for any such data and, if we find it, renders the appropriate component.
-     */
-    return function applyCreativeTemplate(adSlot) {
-        return getAdvertIframe(adSlot).then(function (iframe) {
-            return renderCreativeTemplate(adSlot, iframe);
-        });
-    };
+import { write } from 'lib/fastdom-promise';
+import reportError from 'lib/report-error';
 
-    function getAdvertIframe(adSlot) {
-        return new Promise(function (resolve, reject) {
-            // DFP will sometimes return empty iframes, denoted with a '__hidden__' parameter embedded in its ID.
-            // We need to be sure only to select the ad content frame.
-            var contentFrame = adSlot.querySelector('iframe:not([id*="__hidden__"])');
+import a from 'commercial/modules/creatives/frame',
+import b from 'commercial/modules/creatives/revealer',
+import c from 'commercial/modules/creatives/fabric-v1',
+import d from 'commercial/modules/creatives/fabric-expanding-v1',
+import e from 'commercial/modules/creatives/fabric-expandable-video-v2',
+import f from 'commercial/modules/creatives/fabric-video',
+import g from 'commercial/modules/creatives/scrollable-mpu-v2'
 
-            if (!contentFrame) {
-                reject();
-            }
-            // On IE, wait for the frame to load before interacting with it
-            else if (contentFrame.readyState && contentFrame.readyState !== 'complete') {
-                contentFrame.addEventListener('readystatechange', function onRSC(e) {
-                    var updatedIFrame = e.srcElement;
+const creativeLookup = {
+  frame: a,
+  revealer: b,
+  'fabric-v1': c,
+  'fabric-expanding-v1': d,
+  'fabric-expandable-video-v2': e,
+  'fabric-video': f,
+  'scrollable-mpu-v2': g
+}
 
-                    if (
-                        /*eslint-disable valid-typeof*/
-                    updatedIFrame &&
-                    typeof updatedIFrame.readyState !== 'unknown' &&
-                    updatedIFrame.readyState === 'complete'
-                    /*eslint-enable valid-typeof*/
-                    ) {
-                        updatedIFrame.removeEventListener('readystatechange', onRSC);
-                        resolve(contentFrame);
-                    }
-                });
-            } else {
-                resolve(contentFrame);
-            }
-        });
-    }
+/**
+ * Not all adverts render themselves - some just provide data for templates that we implement in commercial.js.
+ * This looks for any such data and, if we find it, renders the appropriate component.
+ */
+const applyCreativeTemplate = adSlot =>
+    getAdvertIframe(adSlot).then( iframe => renderCreativeTemplate(adSlot, iframe));
 
-    function renderCreativeTemplate(adSlot, iFrame) {
-        var creativeConfig = fetchCreativeConfig();
+const getAdvertIframe = adSlot => {
+    return new Promise( (resolve, reject) => {
+        // DFP will sometimes return empty iframes, denoted with a '__hidden__' parameter embedded in its ID.
+        // We need to be sure only to select the ad content frame.
+        const contentFrame = adSlot.querySelector('iframe:not([id*="__hidden__"])');
 
-        if (creativeConfig) {
-            return hideIframe()
-                .then(JSON.parse)
-                .then(mergeViewabilityTracker)
-                .then(renderCreative)
-                .catch(function (err) {
-                  reportError('Failed to get creative JSON ' + err, {feature: 'commercial'}, false);
-                });
+        if (!contentFrame) {
+            reject();
+        }
+        // On IE, wait for the frame to load before interacting with it
+        else if (contentFrame.readyState && contentFrame.readyState !== 'complete') {
+            contentFrame.addEventListener('readystatechange', e => {
+                const updatedIFrame = e.srcElement;
+
+                if (
+                    /*eslint-disable valid-typeof*/
+                updatedIFrame &&
+                typeof updatedIFrame.readyState !== 'unknown' &&
+                updatedIFrame.readyState === 'complete'
+                /*eslint-enable valid-typeof*/
+                ) {
+                    updatedIFrame.removeEventListener('readystatechange', onRSC);
+                    resolve(contentFrame);
+                }
+            });
         } else {
-            return Promise.resolve(true);
+            resolve(contentFrame);
         }
+    });
+}
 
-        function fetchCreativeConfig() {
-            try {
-                var breakoutScript = iFrame.contentDocument.body.querySelector('.breakout__script[type="application/json"]');
-                return breakoutScript ? breakoutScript.innerHTML : null;
-            } catch (err) {
-                return null;
-            }
+const renderCreativeTemplate = (adSlot, iFrame) => {
 
-        }
-
-        function mergeViewabilityTracker(json) {
-            var viewabilityTrackerDiv = iFrame.contentDocument.getElementById('viewabilityTracker');
-            var viewabilityTracker = viewabilityTrackerDiv ?
-                viewabilityTrackerDiv.childNodes[0].nodeValue.trim() :
-                null;
-            if (viewabilityTracker) {
-                json.params.viewabilityTracker = viewabilityTracker;
-            }
-            return json;
-        }
-
-        function renderCreative(config) {
-            return new Promise(function(resolve) {
-                require(['commercial/modules/creatives/' + config.name], function (Creative) {
-                    resolve(new Creative(adSlot, config.params, config.opts).create());
-                });
-            });
-        }
-
-        function hideIframe() {
-            return fastdom.write(function () {
-                iFrame.style.display = 'none';
-                return creativeConfig;
-            });
+    const fetchCreativeConfig = () => {
+        try {
+            const breakoutScript = iFrame.contentDocument.body.querySelector('.breakout__script[type="application/json"]');
+            return breakoutScript ? breakoutScript.innerHTML : null;
+        } catch (err) {
+            return null;
         }
     }
 
-});
+    const mergeViewabilityTracker = json => {
+        const viewabilityTrackerDiv = iFrame.contentDocument.getElementById('viewabilityTracker');
+        const viewabilityTracker = viewabilityTrackerDiv ?
+            viewabilityTrackerDiv.childNodes[0].nodeValue.trim() :
+            null;
+        if (viewabilityTracker) {
+            json.params.viewabilityTracker = viewabilityTracker;
+        }
+        return json;
+    }
+
+    const renderCreative = config => {
+        return new Promise(function(resolve) {
+          const Creative = creativeLookup[config.name];
+          resolve(new Creative(adSlot, config.params, config.opts).create());
+        });
+    }
+
+    const hideIframe = () => {
+        return write(function () {
+            iFrame.style.display = 'none';
+            return creativeConfig;
+        });
+    }
+
+    const creativeConfig = fetchCreativeConfig();
+
+    if (creativeConfig) {
+        return hideIframe()
+            .then(JSON.parse)
+            .then(mergeViewabilityTracker)
+            .then(renderCreative)
+            .catch(err =>
+              reportError(`Failed to get creative JSON ${err}`,
+                          {feature: 'commercial'},
+                          false));
+    } else {
+        return Promise.resolve(true);
+    }
+}
+export default applyCreativeTemplate;
