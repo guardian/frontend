c22d414198147ecb0886e5720becadc98c4f1cf3
Sam Desborough <sam.desborough@gmail.com>
2017-05-11 16:25:26 +0100

    transform projects/common/modules/experiments/acquisition-test-selector.js to ES6 module

---
+++ static/src/javascripts/projects/common/modules/experiments/acquisition-test-selector.js
@@ -0,0 +1,63 @@
+// @flow
+import * as segmentUtil from 'common/modules/experiments/segment-util';
+import * as testCanRunChecks
+    from 'common/modules/experiments/test-can-run-checks';
+import * as viewLog from 'common/modules/commercial/acquisitions-view-log';
+import alwaysAsk
+    from 'common/modules/experiments/tests/contributions-epic-always-ask-strategy';
+import askFourEarning
+    from 'common/modules/experiments/tests/contributions-epic-ask-four-earning';
+import acquisitionsEpicLiveBlog
+    from 'common/modules/experiments/tests/acquisitions-epic-liveblog';
+import acquisitionsEpicTestimonialsRoundTwo
+    from 'common/modules/experiments/tests/acquisitions-epic-testimonials-round-two';
+/**
+ * acquisition tests in priority order (highest to lowest)
+ */
+const tests = [
+    acquisitionsEpicTestimonialsRoundTwo,
+    alwaysAsk,
+    askFourEarning,
+    acquisitionsEpicLiveBlog,
+];
+
+export const epicEngagementBannerTests = tests.reduce((out, Test) => {
+    const testInstance = new Test();
+    if (testInstance.isEngagementBannerTest) {
+        out.push(testInstance);
+    }
+    return out;
+}, []);
+
+export const abTestClashData = tests.map(Test => new Test());
+
+export const getTest = () => {
+    const eligibleTests = tests.filter(Test => {
+        const t = new Test();
+        const forced = window.location.hash.indexOf(`ab-${t.id}`) > -1;
+        const variant = segmentUtil.variantFor(t);
+
+        if (!variant.maxViews) return false;
+
+        const maxViewCount = variant.maxViews.count;
+        const withinViewLimit =
+            viewLog.viewsInPreviousDays(variant.maxViews.days) < maxViewCount;
+        const enoughDaysBetweenViews =
+            variant.maxViews &&
+            viewLog.viewsInPreviousDays(
+                variant.maxViews.minDaysBetweenViews
+            ) === 0;
+
+        const hasNotReachedRateLimit =
+            (withinViewLimit && enoughDaysBetweenViews) || variant.isUnlimited;
+
+        return (
+            forced ||
+            (testCanRunChecks.testCanBeRun(t) &&
+                segmentUtil.isInTest(t) &&
+                hasNotReachedRateLimit)
+        );
+    });
+
+    return eligibleTests[0] && new eligibleTests[0]();
+};
