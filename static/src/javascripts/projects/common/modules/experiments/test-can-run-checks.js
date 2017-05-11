2fe0b8305258409665a689843bce40a488cdacd8
Sam Desborough <sam.desborough@gmail.com>
2017-05-11 15:07:32 +0100

    transform projects/common/modules/experiments/test-can-run-checks.js to ES6 module

--- static/src/javascripts/projects/common/modules/experiments/test-can-run-checks.js
+++ static/src/javascripts/projects/common/modules/experiments/test-can-run-checks.js
@@ -1,27 +1,24 @@
-define([
-    'lib/config'
-], function(config) {
-    function isTestSwitchedOn(test) {
-        return config.switches['ab' + test.id];
-    }
+import config from 'lib/config';
 
-    function isExpired(testExpiry) {
-        // new Date(test.expiry) sets the expiry time to 00:00:00
-        // Using SetHours allows a test to run until the END of the expiry day
-        var startOfToday = new Date().setHours(0, 0, 0, 0);
-        return startOfToday > new Date(testExpiry);
-    }
+function isTestSwitchedOn(test) {
+    return config.switches['ab' + test.id];
+}
 
-    function testCanBeRun(test) {
-        var expired = isExpired(test.expiry),
-            isSensitive = config.page.isSensitive;
+function isExpired(testExpiry) {
+    // new Date(test.expiry) sets the expiry time to 00:00:00
+    // Using SetHours allows a test to run until the END of the expiry day
+    var startOfToday = new Date().setHours(0, 0, 0, 0);
+    return startOfToday > new Date(testExpiry);
+}
 
-        return ((isSensitive ? test.showForSensitive : true) && isTestSwitchedOn(test)) 
-                    && !expired && (!test.canRun || test.canRun());
-    }
+function testCanBeRun(test) {
+    var expired = isExpired(test.expiry),
+        isSensitive = config.page.isSensitive;
 
-    return {
-        isExpired: isExpired,
-        testCanBeRun: testCanBeRun
-    }
-})
+    return ((isSensitive ? test.showForSensitive : true) && isTestSwitchedOn(test)) && !expired && (!test.canRun || test.canRun());
+}
+
+export default {
+    isExpired: isExpired,
+    testCanBeRun: testCanBeRun
+}
