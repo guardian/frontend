7f801ca560343bff480ce71df7ba23cead9c847e
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-05-11 10:22:51 +0100

    convert 'plista.js' to es6

--- static/src/javascripts/projects/commercial/modules/third-party-tags/plista.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/plista.js
@@ -1,3 +1,4 @@
+// @flow
 import fastdom from 'fastdom';
 import $ from 'lib/$';
 import config from 'lib/config';
@@ -6,77 +7,81 @@ import template from 'lodash/utilities/template';
 import trackAdRender from 'commercial/modules/dfp/track-ad-render';
 import commercialFeatures from 'commercial/modules/commercial-features';
 import plistaStr from 'raw-loader!commercial/views/plista.html';
-import loadScript from 'lib/load-script';
+import { loadScript } from 'lib/load-script';
 
-var plistaTpl = template(plistaStr);
-var selectors = {
-    container: '.js-plista-container'
+const plistaTpl = template(plistaStr);
+const selectors = {
+    container: '.js-plista-container',
 };
 
-function loadInstantly() {
-    return detect.adblockInUse.then(function(adblockInUse) {
-        return !document.getElementById('dfp-ad--merchandising-high') || adblockInUse;
-    });
-}
+const module = {};
+
+const loadInstantly = function() {
+    return detect.adblockInUse.then(
+        adblockInUse =>
+            !document.getElementById('dfp-ad--merchandising-high') ||
+            adblockInUse
+    );
+};
 
 // a modification of the code provided by Plista; altered to be a lazy load rather than during DOM construction
-function embed(publickey, widgetName, geo, u, categories) {
-    var name = 'PLISTA_' + publickey;
-    var lib = window[name];
-    var $container = $(selectors.container);
+const embed = function(publickey, widgetName, geo, u, categories) {
+    const name = `PLISTA_${publickey}`;
+    const lib = window[name];
+    const $container = $(selectors.container);
 
-    $container.append(plistaTpl({
-        widgetName: widgetName
-    }));
+    $container.append(
+        plistaTpl({
+            widgetName,
+        })
+    );
     $container.css('display', 'block');
 
     if (!lib || !lib.publickey) {
         window[name] = {
-            publickey: publickey,
-            widgets: [{
-                name: widgetName,
-                pre: u
-            }],
-            geo: geo,
-            categories: categories,
-            dataMode: 'data-display'
+            publickey,
+            widgets: [
+                {
+                    name: widgetName,
+                    pre: u,
+                },
+            ],
+            geo,
+            categories,
+            dataMode: 'data-display',
         };
-        loadScript.loadScript('//static-au.plista.com/async/' + name + '.js');
+        loadScript(`//static-au.plista.com/async/${name}.js`);
     } else {
         lib.widgets.push({
             name: widgetName,
-            pre: u
+            pre: u,
         });
     }
-}
-
-function load() {
-    fastdom.write(function() {
-        embed(config.page.plistaPublicApiKey, 'innerArticle', 'au');
-    });
-}
-
-var module = {
-    load: load,
-    init: init
 };
 
-function init() {
+module.load = function() {
+    fastdom.write(() => {
+        embed(config.page.plistaPublicApiKey, 'innerArticle', 'au');
+    });
+};
+
+module.init = function() {
     if (commercialFeatures.outbrain) {
-        return loadInstantly().then(function(adBlockInUse) {
+        return loadInstantly().then(adBlockInUse => {
             if (adBlockInUse) {
                 module.load();
             } else {
-                return trackAdRender('dfp-ad--merchandising-high').then(function(isLoaded) {
+                return trackAdRender(
+                    'dfp-ad--merchandising-high'
+                ).then(isLoaded => {
                     if (!isLoaded) {
                         module.load();
                     }
                 });
             }
         });
-    } else {
-        return Promise.resolve(false);
     }
-}
+    return Promise.resolve(false);
+};
 
 export default module;
