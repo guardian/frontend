f3e80ec7b71c6e9be390d39c853474d4810bfbd3
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-05-12 15:34:09 +0100

    convert remarketing to ES6

--- static/src/javascripts/projects/commercial/modules/third-party-tags/remarketing.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/remarketing.js
@@ -1,17 +1,18 @@
+// @flow
 import config from 'lib/config';
 
-var remarketingUrl = '//www.googleadservices.com/pagead/conversion_async.js';
+const remarketingUrl = '//www.googleadservices.com/pagead/conversion_async.js';
 
-function onLoad() {
+const onLoad = function() {
     window.google_trackConversion({
         google_conversion_id: 971225648,
         google_custom_params: window.google_tag_params,
-        google_remarketing_only: true
+        google_remarketing_only: true,
     });
-}
-
-export default {
-    shouldRun: config.switches.remarketing,
-    url: remarketingUrl,
-    onLoad: onLoad
 };
+
+const shouldRun = config.switches.remarketing;
+
+const url = remarketingUrl;
+
+export { shouldRun, url, onLoad };
