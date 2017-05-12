91ec0613ba8ca91a088bd302da1c02e30f19cf2e
davidfurey <david.furey@guardian.co.uk>
2017-05-12 15:52:26 +0100

    transform projects/common/modules/experiments/tests/bookmarks-email-variants-2.js to ES6 module

--- static/src/javascripts/projects/common/modules/experiments/tests/bookmarks-email-variants-2.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/bookmarks-email-variants-2.js
@@ -1,22 +1,18 @@
-define([
-    'common/modules/experiments/tests/generic-email-variants'
-], function (
-    genericEmailTest
-) {
-    return new genericEmailTest(
-        {
-            id: 'BookmarksEmailVariants2',
-            start: '2017-03-31',
-            end: '2017-05-30',
-            author: 'David Furey',
-            audience: 1,
-            audienceOffset: 0,
-            signupPage: 'info/ng-interactive/2017/mar/30/sign-up-for-the-bookmarks-email',
-            canonicalListId: 3039,
-            testIds: [
-                { variantId: 'Control', listId: 3867 },
-                { variantId: 'Variant', listId: 3866 }
-            ]
-        }
-    );
+import genericEmailTest from 'common/modules/experiments/tests/generic-email-variants';
+export default new genericEmailTest({
+    id: 'BookmarksEmailVariants2',
+    start: '2017-03-31',
+    end: '2017-05-30',
+    author: 'David Furey',
+    audience: 1,
+    audienceOffset: 0,
+    signupPage: 'info/ng-interactive/2017/mar/30/sign-up-for-the-bookmarks-email',
+    canonicalListId: 3039,
+    testIds: [{
+        variantId: 'Control',
+        listId: 3867
+    }, {
+        variantId: 'Variant',
+        listId: 3866
+    }]
 });
