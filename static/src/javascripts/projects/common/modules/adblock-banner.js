629b0248d0c90a817e6fd47db0d463c6cd5032be
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-05-12 14:57:21 +0100

    transform projects/common/modules/adblock-banner.js to ES6 module

--- static/src/javascripts/projects/common/modules/adblock-banner.js
+++ static/src/javascripts/projects/common/modules/adblock-banner.js
@@ -1,41 +1,33 @@
-define([
-    'fastdom',
-    'lib/$',
-    'lodash/utilities/template',
-    'raw-loader!common/views/commercial/adblock-sticky-message.html',
-    'raw-loader!common/views/commercial/adblock-sticky-message-coin.html'
-], function (
-    fastdom,
-    $,
-    template,
-    adblockStickyMessage,
-    adblockStickyMessageCoin) {
+import fastdom from 'fastdom';
+import $ from 'lib/$';
+import template from 'lodash/utilities/template';
+import adblockStickyMessage from 'raw-loader!common/views/commercial/adblock-sticky-message.html';
+import adblockStickyMessageCoin from 'raw-loader!common/views/commercial/adblock-sticky-message-coin.html';
 
-    /**
-     * Message which is shown at the top of the page to the adblock users.
-     * @constructor
-     */
-    var AdblockBanner = function (template, config) {
-        this.template = template;
-        this.config = config;
+/**
+ * Message which is shown at the top of the page to the adblock users.
+ * @constructor
+ */
+var AdblockBanner = function(template, config) {
+    this.template = template;
+    this.config = config;
 
-        this.templates = {
-            'adblock-sticky-message': adblockStickyMessage,
-            'adblock-sticky-message-coin': adblockStickyMessageCoin
-        };
+    this.templates = {
+        'adblock-sticky-message': adblockStickyMessage,
+        'adblock-sticky-message-coin': adblockStickyMessageCoin
     };
+};
 
-    AdblockBanner.prototype.renderTemplate = function () {
-        return template(this.templates[this.template], this.config);
-    };
+AdblockBanner.prototype.renderTemplate = function() {
+    return template(this.templates[this.template], this.config);
+};
 
-    AdblockBanner.prototype.show = function () {
-        var bannerTmpl = this.renderTemplate();
+AdblockBanner.prototype.show = function() {
+    var bannerTmpl = this.renderTemplate();
 
-        fastdom.write(function () {
-            $('.js-top-banner').after(bannerTmpl);
-        });
-    };
+    fastdom.write(function() {
+        $('.js-top-banner').after(bannerTmpl);
+    });
+};
 
-    return AdblockBanner;
-});
+export default AdblockBanner;
