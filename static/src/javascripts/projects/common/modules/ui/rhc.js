fc1e2c742907eeb764e9744ef56b2255d287ea01
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-15 16:16:25 +0100

    minor refactor

--- static/src/javascripts/projects/common/modules/ui/rhc.js
+++ static/src/javascripts/projects/common/modules/ui/rhc.js
@@ -1,28 +1,27 @@
 // @flow
 import $ from 'lib/$';
-import bonzo from 'bonzo';
 
 const addComponent = (content: Element, importance: number = 1) => {
     const $rhc = $('.js-components-container');
     const classname = 'component--rhc';
-    let $cs;
 
     return $.create(
         `<div class="${classname}" data-importance="${importance}"></div>`
     )
         .append(content)
         .each(container => {
-            $cs = $(`.${classname}`, $rhc[0]);
-            const inferior = $cs.filter(
+            const components = $(`.${classname}`, $rhc[0]) || [];
+            const inferior = Array.from(components).filter(
                 component =>
                     !component.hasAttribute('data-importance') ||
                     importance >
                         parseInt(component.getAttribute('data-importance'), 10)
             );
+
             if (inferior.length === 0) {
                 $rhc.append(container);
             } else {
-                bonzo(inferior[0]).before(container);
+                $(inferior[0]).before(container);
             }
         });
 };
