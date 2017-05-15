f0a697262265b8c72483a367987b844aa7d1f695
Sam Desborough <sam.desborough@gmail.com>
2017-05-15 11:48:31 +0100

    transform projects/common/modules/experiments/segment-util.js to ES6 module

--- static/src/javascripts/projects/common/modules/experiments/segment-util.js
+++ static/src/javascripts/projects/common/modules/experiments/segment-util.js
@@ -1,56 +1,50 @@
-define([
-    'lodash/functions/memoize',
-    'common/modules/analytics/mvt-cookie'
-], function(
-    memoize,
-    mvtCookie
-) {
-    var NOT_IN_TEST = 'notintest';
+import memoize from 'lodash/functions/memoize';
+import mvtCookie from 'common/modules/analytics/mvt-cookie';
+var NOT_IN_TEST = 'notintest';
 
-    function getId(test) {
-        return test.id; // use test ids as memo cache keys
+function getId(test) {
+    return test.id; // use test ids as memo cache keys
+}
+
+/**
+ * Determine whether the user is in the test or not and return the associated
+ * variant ID.
+ *
+ * The test population is just a subset of mvt ids. A test population must
+ * begin from a specific value. Overlapping test ranges are permitted.
+ *
+ * @return {String} variant ID
+ */
+function variantIdFor(test) {
+    var smallestTestId = mvtCookie.getMvtNumValues() * test.audienceOffset;
+    var largestTestId = smallestTestId + mvtCookie.getMvtNumValues() * test.audience;
+    var mvtCookieId = mvtCookie.getMvtValue();
+
+    if (mvtCookieId && mvtCookieId > smallestTestId && mvtCookieId <= largestTestId) {
+        // This mvt test id is in the test range, so allocate it to a test variant.
+        var variantIds = test.variants.map(getId);
+
+        return variantIds[mvtCookieId % variantIds.length];
+    } else {
+        return NOT_IN_TEST;
     }
+}
 
-    /**
-     * Determine whether the user is in the test or not and return the associated
-     * variant ID.
-     *
-     * The test population is just a subset of mvt ids. A test population must
-     * begin from a specific value. Overlapping test ranges are permitted.
-     *
-     * @return {String} variant ID
-     */
-    function variantIdFor(test) {
-        var smallestTestId = mvtCookie.getMvtNumValues() * test.audienceOffset;
-        var largestTestId = smallestTestId + mvtCookie.getMvtNumValues() * test.audience;
-        var mvtCookieId = mvtCookie.getMvtValue();
+function variantFor(test) {
+    var variantId = variantIdFor(test);
 
-        if (mvtCookieId && mvtCookieId > smallestTestId && mvtCookieId <= largestTestId) {
-            // This mvt test id is in the test range, so allocate it to a test variant.
-            var variantIds = test.variants.map(getId);
+    return test.variants.filter(function(variant) {
+        return variant.id === variantId;
+    })[0];
+}
 
-            return variantIds[mvtCookieId % variantIds.length];
-        } else {
-            return NOT_IN_TEST;
-        }
+
+export default {
+    variantIdFor: memoize(variantIdFor, getId),
+
+    variantFor: variantFor,
+
+    isInTest: function(test) {
+        return variantIdFor(test) !== NOT_IN_TEST;
     }
-    
-     function variantFor(test) {
-         var variantId = variantIdFor(test);
-         
-         return test.variants.filter(function(variant) { 
-             return variant.id === variantId;
-         })[0];
-     }
-    
-
-    return {
-        variantIdFor: memoize(variantIdFor, getId),
-        
-        variantFor: variantFor, 
-        
-        isInTest: function(test) {
-            return variantIdFor(test) !== NOT_IN_TEST;
-        }
-    }
-});
+}
