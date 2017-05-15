e0176e2b794c86de440f93f239e919961358e94c
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-05-15 10:43:08 +0100

    convert images.js to ES6 - fix up the exports

--- static/src/javascripts/projects/common/modules/ui/images.js
+++ static/src/javascripts/projects/common/modules/ui/images.js
@@ -1,20 +1,18 @@
 // @flow
 import qwery from 'qwery';
-import picturefill from 'picturefill';
+import picturefill from 'lib/picturefill';
 import mediator from 'lib/mediator';
 
-const images = {
-    upgradePictures(context) {
-        picturefill({
-            elements: qwery('img[srcset], picture img', context || document),
-        });
-    },
-
-    listen() {
-        mediator.addListeners({
-            'ui:images:upgradePictures': images.upgradePictures,
-        });
-    },
+const upgradePictures = function(context: any): void {
+    picturefill({
+        elements: qwery('img[srcset], picture img', context || document),
+    });
 };
 
-export default images;
+const listen = function(): void {
+    mediator.addListeners({
+        'ui:images:upgradePictures': upgradePictures,
+    });
+};
+
+export { upgradePictures, listen };
