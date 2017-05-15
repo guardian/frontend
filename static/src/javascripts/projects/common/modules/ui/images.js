cba530c1dc2ecb5319e121fa25b71cd4f15d4e10
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-05-15 10:19:27 +0100

    transform projects/common/modules/ui/images.js content to ES6

--- static/src/javascripts/projects/common/modules/ui/images.js
+++ static/src/javascripts/projects/common/modules/ui/images.js
@@ -1,21 +1,20 @@
+// @flow
 import qwery from 'qwery';
 import picturefill from 'picturefill';
 import mediator from 'lib/mediator';
 
-var images = {
-
-    upgradePictures: function(context) {
+const images = {
+    upgradePictures(context) {
         picturefill({
-            elements: qwery('img[srcset], picture img', context || document)
+            elements: qwery('img[srcset], picture img', context || document),
         });
     },
 
-    listen: function() {
+    listen() {
         mediator.addListeners({
-            'ui:images:upgradePictures': images.upgradePictures
+            'ui:images:upgradePictures': images.upgradePictures,
         });
-    }
-
+    },
 };
 
 export default images;
