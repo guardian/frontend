03b777596c745bf26d2bfeedcbab128ea425d0b8
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-15 16:09:48 +0100

    linting rhc

--- static/src/javascripts/projects/common/modules/ui/rhc.js
+++ static/src/javascripts/projects/common/modules/ui/rhc.js
@@ -1,33 +1,30 @@
+// @flow
 import $ from 'lib/$';
 import bonzo from 'bonzo';
-import filter from 'lodash/collections/filter';
-var $rhc = $('.js-components-container');
 
-/**
- * @param {Element|Bonzo} c
- * @param {number} importance number (optional)
- */
-function addComponent(c, importance) {
-    importance = importance || 1;
-    var classname = 'component--rhc',
-        $cs;
+const addComponent = (content: Element, importance: number = 1) => {
+    const $rhc = $('.js-components-container');
+    const classname = 'component--rhc';
+    let $cs;
 
-    return $.create('<div class="' + classname + '" data-importance="' + importance + '"></div>')
-        .append(c)
-        .each(function(el) {
-            $cs = $('.' + classname, $rhc[0]);
-            var inferior = filter($cs, function(el) {
-                return !el.hasAttribute('data-importance') ||
-                    importance > parseInt(el.getAttribute('data-importance'), 10);
-            });
+    return $.create(
+        `<div class="${classname}" data-importance="${importance}"></div>`
+    )
+        .append(content)
+        .each(container => {
+            $cs = $(`.${classname}`, $rhc[0]);
+            const inferior = $cs.filter(
+                component =>
+                    !component.hasAttribute('data-importance') ||
+                    importance >
+                        parseInt(component.getAttribute('data-importance'), 10)
+            );
             if (inferior.length === 0) {
-                $rhc.append(el);
+                $rhc.append(container);
             } else {
-                bonzo(inferior[0]).before(el);
+                bonzo(inferior[0]).before(container);
             }
         });
-}
-
-export default {
-    addComponent: addComponent
 };
+
+export { addComponent };
