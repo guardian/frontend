dccb2c10ae17d3ac53a53e6f6e51a84db4225cdb
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-15 15:45:23 +0100

    transform projects/common/modules/ui/rhc.js to ES6 module

--- static/src/javascripts/projects/common/modules/ui/rhc.js
+++ static/src/javascripts/projects/common/modules/ui/rhc.js
@@ -1,40 +1,33 @@
-define([
-    'lib/$',
-    'bonzo',
-    'lodash/collections/filter'
-], function (
-    $,
-    bonzo,
-    filter
-) {
-    var $rhc = $('.js-components-container');
+import $ from 'lib/$';
+import bonzo from 'bonzo';
+import filter from 'lodash/collections/filter';
+var $rhc = $('.js-components-container');
 
-    /**
-     * @param {Element|Bonzo} c
-     * @param {number} importance number (optional)
-     */
-    function addComponent(c, importance) {
-        importance = importance || 1;
-        var classname = 'component--rhc',
-            $cs;
+/**
+ * @param {Element|Bonzo} c
+ * @param {number} importance number (optional)
+ */
+function addComponent(c, importance) {
+    importance = importance || 1;
+    var classname = 'component--rhc',
+        $cs;
 
-        return $.create('<div class="' + classname + '" data-importance="' + importance + '"></div>')
-            .append(c)
-            .each(function (el) {
-                $cs = $('.' +  classname, $rhc[0]);
-                var inferior = filter($cs, function (el) {
-                    return !el.hasAttribute('data-importance') ||
-                        importance > parseInt(el.getAttribute('data-importance'), 10);
-                });
-                if (inferior.length === 0) {
-                    $rhc.append(el);
-                } else {
-                    bonzo(inferior[0]).before(el);
-                }
+    return $.create('<div class="' + classname + '" data-importance="' + importance + '"></div>')
+        .append(c)
+        .each(function(el) {
+            $cs = $('.' + classname, $rhc[0]);
+            var inferior = filter($cs, function(el) {
+                return !el.hasAttribute('data-importance') ||
+                    importance > parseInt(el.getAttribute('data-importance'), 10);
             });
-    }
+            if (inferior.length === 0) {
+                $rhc.append(el);
+            } else {
+                bonzo(inferior[0]).before(el);
+            }
+        });
+}
 
-    return {
-        addComponent: addComponent
-    };
-});
+export default {
+    addComponent: addComponent
+};
