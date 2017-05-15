9d6a32d3b89a26b53a13359fbf546acc6eea5e7c
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-05-15 12:28:42 +0100

    convert tourism-australia to ES6

--- static/src/javascripts/projects/commercial/modules/third-party-tags/tourism-australia.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/tourism-australia.js
@@ -1,24 +1,23 @@
+// @flow
 import config from 'lib/config';
 
-function getUrl(location) {
-    var ta_partner = 'partner:guardian-uk';
-    var ta_account = config.page.isDev ? 'tuata-dev-internal' : 'tuatourism-australia-global';
-    var ta_pageName = ta_partner + ':' + location.pathname;
-    var ta_server = location.hostname;
-    var ta_url = location.href;
-    var ta_cachebreak = new Date().getTime();
+const getUrl = location => {
+    const taPartner = 'partner:guardian-uk';
+    const taAccount = config.page.isDev
+        ? 'tuata-dev-internal'
+        : 'tuatourism-australia-global';
+    const taPageName = `${taPartner}:${location.pathname}`;
+    const taServer = location.hostname;
+    const taUrl = location.href;
+    const taCachebreak = new Date().getTime();
 
-    return '//tourismaustralia.sc.omtrdc.net/b/ss/' + ta_account +
-        '/1/H.26.2/s' + ta_cachebreak +
-        '?AQB=1&ndh=0&ns=tourismaustralia&pageName=' + encodeURIComponent(ta_pageName) +
-        '&g= ' + encodeURIComponent(ta_url) +
-        '&server=' + encodeURIComponent(ta_server) +
-        '&v11=' + encodeURIComponent(ta_partner) +
-        '&AQE=1';
-}
-
-export default {
-    shouldRun: config.page.section === 'ashes-australia-travel' && config.switches.tourismAustralia,
-    url: getUrl(window.location),
-    useImage: true
+    return `//tourismaustralia.sc.omtrdc.net/b/ss/${taAccount}/1/H.26.2/s${taCachebreak}?AQB=1&ndh=0&ns=tourismaustralia&pageName=${encodeURIComponent(taPageName)}&g= ${encodeURIComponent(taUrl)}&server=${encodeURIComponent(taServer)}&v11=${encodeURIComponent(taPartner)}&AQE=1`;
 };
+
+const shouldRun =
+    config.page.section === 'ashes-australia-travel' &&
+    config.switches.tourismAustralia;
+const url = getUrl(window.location);
+const useImage = true;
+
+export { shouldRun, url, useImage };
