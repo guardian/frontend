a939db78c5efd253a04b6bc1c481d2052d86b6b7
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-16 10:42:59 +0100

    transform projects/commercial/modules/dfp/prepare-googletag.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/prepare-googletag.js
+++ static/src/javascripts/projects/commercial/modules/dfp/prepare-googletag.js
@@ -1,90 +1,74 @@
-define([
-    'qwery',
-    'lib/raven',
-    'lib/config',
-    'lib/load-script',
-    'lib/fastdom-promise',
-    'commercial/modules/commercial-features',
-    'commercial/modules/build-page-targeting',
-    'commercial/modules/close-disabled-slots',
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/on-slot-render',
-    'commercial/modules/dfp/on-slot-load',
-    'commercial/modules/dfp/performance-logging',
+import qwery from 'qwery';
+import raven from 'lib/raven';
+import config from 'lib/config';
+import loadScript from 'lib/load-script';
+import fastdom from 'lib/fastdom-promise';
+import commercialFeatures from 'commercial/modules/commercial-features';
+import buildPageTargeting from 'commercial/modules/build-page-targeting';
+import closeSlots from 'commercial/modules/close-disabled-slots';
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import onSlotRender from 'commercial/modules/dfp/on-slot-render';
+import onSlotLoad from 'commercial/modules/dfp/on-slot-load';
+import performanceLogging from 'commercial/modules/dfp/performance-logging';
+import 'commercial/modules/messenger/type';
+import 'commercial/modules/messenger/get-stylesheet';
+import 'commercial/modules/messenger/resize';
+import 'commercial/modules/messenger/scroll';
+import 'commercial/modules/messenger/viewport';
+import 'commercial/modules/messenger/click';
+import 'commercial/modules/messenger/background';
 
-    // These are cross-frame protocol messaging routines:
-    'commercial/modules/messenger/type',
-    'commercial/modules/messenger/get-stylesheet',
-    'commercial/modules/messenger/resize',
-    'commercial/modules/messenger/scroll',
-    'commercial/modules/messenger/viewport',
-    'commercial/modules/messenger/click',
-    'commercial/modules/messenger/background'
-], function (
-    qwery,
-    raven,
-    config,
-    loadScript,
-    fastdom,
-    commercialFeatures,
-    buildPageTargeting,
-    closeSlots,
-    dfpEnv,
-    onSlotRender,
-    onSlotLoad,
-    performanceLogging
-) {
+export default {
+    init: init
+};
 
-    return {
-        init: init
-    };
+function init(start, stop) {
 
-    function init(start, stop) {
+    function setupAdvertising() {
 
-        function setupAdvertising() {
+        performanceLogging.addTag(dfpEnv.sonobiEnabled ? 'sonobi' : 'waterfall');
 
-            performanceLogging.addTag(dfpEnv.sonobiEnabled ? 'sonobi' : 'waterfall');
+        window.googletag.cmd.push(
+            start,
+            setListeners,
+            setPageTargeting,
+            stop
+        );
 
-            window.googletag.cmd.push(
-                start,
-                setListeners,
-                setPageTargeting,
-                stop
-            );
+        // Just load googletag. Sonobi's wrapper will already be loaded, and googletag is already added to the window by sonobi.
+        return loadScript.loadScript(config.libs.googletag, {
+            async: false
+        });
+    }
 
-            // Just load googletag. Sonobi's wrapper will already be loaded, and googletag is already added to the window by sonobi.
-            return loadScript.loadScript(config.libs.googletag, { async: false });
-        }
-
-        if (commercialFeatures.dfpAdvertising) {
-            setupAdvertising()
+    if (commercialFeatures.dfpAdvertising) {
+        setupAdvertising()
             // A promise error here, from a failed module load,
             // could be a network problem or an intercepted request.
             // Abandon the init sequence.
             .catch(removeAdSlots);
-            return Promise.resolve();
-        }
-
-        return removeAdSlots();
+        return Promise.resolve();
     }
 
-    function setListeners() {
-        performanceLogging.setListeners(window.googletag);
+    return removeAdSlots();
+}
 
-        var pubads = window.googletag.pubads();
-        pubads.addEventListener('slotRenderEnded', raven.wrap(onSlotRender));
-        pubads.addEventListener('slotOnload', raven.wrap(onSlotLoad));
-    }
+function setListeners() {
+    performanceLogging.setListeners(window.googletag);
 
-    function setPageTargeting() {
-        var pubads = window.googletag.pubads();
-        var targeting = buildPageTargeting();
-        Object.keys(targeting).forEach(function (key) {
-            pubads.setTargeting(key, targeting[key]);
-        });
-    }
+    var pubads = window.googletag.pubads();
+    pubads.addEventListener('slotRenderEnded', raven.wrap(onSlotRender));
+    pubads.addEventListener('slotOnload', raven.wrap(onSlotLoad));
+}
 
-    function removeAdSlots() {
-        return closeSlots.init(true);
-    }
-});
+function setPageTargeting() {
+    var pubads = window.googletag.pubads();
+    var targeting = buildPageTargeting();
+    Object.keys(targeting).forEach(function(key) {
+        pubads.setTargeting(key, targeting[key]);
+    });
+}
+
+function removeAdSlots() {
+    return closeSlots.init(true);
+}
