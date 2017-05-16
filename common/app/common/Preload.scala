55412d5ad2ec0aaed3b0b573b734718065463079
Alex Sanders <a@snd.rs>
2017-05-16 13:05:24 +0100

    preload the correct files

--- common/app/common/Preload.scala
+++ common/app/common/Preload.scala
@@ -4,8 +4,19 @@ import model.ApplicationIdentity
 
 object Preload {
 
-  val articleDefaultPreloads: Seq[PreloadAsset] = Seq(CssPreloadAsset("content.css"), JsPreloadAsset("javascripts/graun.standard.js"), JsPreloadAsset("javascripts/graun.commercial.js"))
-  val faciaDefaultPreloads: Seq[PreloadAsset] = Seq(CssPreloadAsset("content.css"), JsPreloadAsset("javascripts/graun.standard.js"), JsPreloadAsset("javascripts/graun.commercial.js"))
+  val articleDefaultPreloads: Seq[PreloadAsset] = Seq(
+    CssPreloadAsset("content.css"),
+    ThirdPartyJsPreload(common.Assets.js.polyfillioUrl),
+    JsPreloadAsset("javascripts/graun.standard.js"),
+    JsPreloadAsset("javascripts/graun.commercial.js")
+  )
+
+  val faciaDefaultPreloads: Seq[PreloadAsset] = Seq(
+    CssPreloadAsset("facia.css"),
+    ThirdPartyJsPreload(common.Assets.js.polyfillioUrl),
+    JsPreloadAsset("javascripts/graun.standard.js"),
+    JsPreloadAsset("javascripts/graun.commercial.js")
+  )
 
   val config: Map[ApplicationIdentity, Seq[PreloadAsset]] = Map(
     ApplicationIdentity("article") -> articleDefaultPreloads,
