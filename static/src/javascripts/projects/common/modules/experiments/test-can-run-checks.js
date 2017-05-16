e5d4e2c9ab5ecccce636a639c3f9f949dc3ac88b
Sam Desborough <sam.desborough@gmail.com>
2017-05-16 12:19:24 +0100

    Add return types

--- static/src/javascripts/projects/common/modules/experiments/test-can-run-checks.js
+++ static/src/javascripts/projects/common/modules/experiments/test-can-run-checks.js
@@ -5,14 +5,14 @@ import config from 'lib/config';
 
 const isTestSwitchedOn = test => config.switches[`ab${test.id}`];
 
-export const isExpired = (testExpiry: string) => {
+export const isExpired = (testExpiry: string): boolean => {
     // new Date(test.expiry) sets the expiry time to 00:00:00
     // Using SetHours allows a test to run until the END of the expiry day
     const startOfToday = new Date().setHours(0, 0, 0, 0);
     return startOfToday > new Date(testExpiry);
 };
 
-export const testCanBeRun = (test: ABTest) => {
+export const testCanBeRun = (test: ABTest): boolean => {
     const expired = isExpired(test.expiry);
     const isSensitive = config.page.isSensitive;
     return (
