34bdd6959e9dd0effb9d605503e203044c5607f2
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-17 10:20:30 +0100

    transform projects/common/modules/avatar/api.js to ES6 module

--- static/src/javascripts/projects/common/modules/avatar/api.js
+++ static/src/javascripts/projects/common/modules/avatar/api.js
@@ -1,44 +1,38 @@
-define([
-    'lib/ajax',
-    'lib/config'
-], function (
-    ajax,
-    config
-) {
+import ajax from 'lib/ajax';
+import config from 'lib/config';
 
-    var apiUrl = config.page.avatarApiUrl + '/v1';
-    var staticUrl = config.page.avatarImagesUrl + '/user';
-    var Api = {};
+var apiUrl = config.page.avatarApiUrl + '/v1';
+var staticUrl = config.page.avatarImagesUrl + '/user';
+var Api = {};
 
-    Api.request = function (method, path, data) {
-        var params = {
-            url: apiUrl + path,
-            type: 'json',
-            data: data || {},
-            processData : false,
-            method: method,
-            crossOrigin: true,
-            withCredentials: true
-        };
-
-        return ajax.ajax(params);
+Api.request = function(method, path, data) {
+    var params = {
+        url: apiUrl + path,
+        type: 'json',
+        data: data || {},
+        processData: false,
+        method: method,
+        crossOrigin: true,
+        withCredentials: true
     };
 
-    // A user's 'active' avatar is only available to signed-in users as it
-    // includes avatars in a pre-mod state.
-    Api.getActive = function () {
-        return Api.request('GET', '/avatars/user/me/active');
-    };
+    return ajax.ajax(params);
+};
 
-    Api.updateAvatar = function (data) {
-        return Api.request('POST', '/avatars', data);
-    };
+// A user's 'active' avatar is only available to signed-in users as it
+// includes avatars in a pre-mod state.
+Api.getActive = function() {
+    return Api.request('GET', '/avatars/user/me/active');
+};
 
-    // The deterministic URL always returns an image. If the user has no avatar,
-    // a default image is returned.
-    Api.deterministicUrl = function (userId) {
-        return staticUrl + '/' + userId;
-    };
+Api.updateAvatar = function(data) {
+    return Api.request('POST', '/avatars', data);
+};
 
-    return Api;
-});
+// The deterministic URL always returns an image. If the user has no avatar,
+// a default image is returned.
+Api.deterministicUrl = function(userId) {
+    return staticUrl + '/' + userId;
+};
+
+export default Api;
