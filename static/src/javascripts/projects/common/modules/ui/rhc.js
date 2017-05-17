183abdd250bc39430648df1b12af5e11445170a1
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-17 17:37:12 +0100

    rhc tests

--- static/src/javascripts/projects/common/modules/ui/rhc.js
+++ static/src/javascripts/projects/common/modules/ui/rhc.js
@@ -2,26 +2,29 @@
 import $ from 'lib/$';
 
 const addComponent = (content: Element, importance: number = 1) => {
-    const $rhc = $('.js-components-container');
+    const container = $('.js-components-container');
     const classname = 'component--rhc';
 
     return $.create(
         `<div class="${classname}" data-importance="${importance}"></div>`
     )
         .append(content)
-        .each(container => {
-            const components = $(`.${classname}`, $rhc[0]) || [];
-            const inferior = Array.from(components).filter(
-                component =>
-                    !component.hasAttribute('data-importance') ||
+        .each(component => {
+            const existingComponents = $(`.${classname}`, container[0]) || [];
+            const inferiorComponents = Array.from(existingComponents).filter(
+                existingComponent =>
+                    !existingComponent.hasAttribute('data-importance') ||
                     importance >
-                        parseInt(component.getAttribute('data-importance'), 10)
+                        parseInt(
+                            existingComponent.getAttribute('data-importance'),
+                            10
+                        )
             );
 
-            if (inferior.length === 0) {
-                $rhc.append(container);
+            if (inferiorComponents.length === 0) {
+                container.append(component);
             } else {
-                $(inferior[0]).before(container);
+                $(inferiorComponents[0]).before(component);
             }
         });
 };
