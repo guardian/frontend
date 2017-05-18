31ccf15b57b0c61552e35e6ea7573c33316064ce
Sam Desborough <sam.desborough@gmail.com>
2017-05-18 15:55:42 +0100

    Remove unused functionality from mvt-cookie

--- static/src/javascripts/projects/common/modules/analytics/mvt-cookie.js
+++ static/src/javascripts/projects/common/modules/analytics/mvt-cookie.js
@@ -2,25 +2,14 @@
 import { addCookie, getCookie } from 'lib/cookies';
 
 const MULTIVARIATE_ID_COOKIE = 'GU_mvt_id';
-const VISITOR_ID_COOKIE = 's_vi';
-const BROWSER_ID_COOKIE = 'bwid';
 // The full mvt ID interval is [1, 1000000]
 const MAX_CLIENT_MVT_ID = 1000000;
 
 // For test purposes only.
-export const overwriteMvtCookie = (testId: string): void =>
+export const overwriteMvtCookie = (testId: number): void =>
     addCookie(MULTIVARIATE_ID_COOKIE, testId, 365);
 
 export const getMvtValue = (): number =>
     Number(getCookie(MULTIVARIATE_ID_COOKIE));
 
 export const getMvtNumValues = (): number => Number(MAX_CLIENT_MVT_ID);
-
-export const getMvtFullId = (): string => {
-    const bwidCookie = getCookie(BROWSER_ID_COOKIE) || 'unknown-browser-id';
-    const mvtidCookie = getMvtValue() || 'unknown-mvt-id';
-    const visitoridCookie =
-        getCookie(VISITOR_ID_COOKIE) || 'unknown-visitor-id';
-
-    return `${visitoridCookie} ${bwidCookie} ${mvtidCookie}`;
-};
