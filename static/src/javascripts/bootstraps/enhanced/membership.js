3d791d5fdba2f66ada06fdd0049f7045416a876b
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-05-19 11:20:52 +0100

    transform bootstraps/enhanced/membership.js content to ES6

--- static/src/javascripts/bootstraps/enhanced/membership.js
+++ static/src/javascripts/bootstraps/enhanced/membership.js
@@ -1,8 +1,10 @@
+// @flow
 import membershipTab from 'membership/membership-tab';
 import digitalpackTab from 'membership/digitalpack-tab';
-export default {
-    init: function() {
+
+export const membership = {
+    init(): void {
         membershipTab.init();
         digitalpackTab.init();
-    }
+    },
 };
