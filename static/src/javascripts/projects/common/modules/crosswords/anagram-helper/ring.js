a27e04c8a7c2cb9d18219d7eee8024b5e69e85c0
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-23 14:44:23 +0100

    transform projects/common/modules/crosswords/anagram-helper/ring.js to ES6 module

--- static/src/javascripts/projects/common/modules/crosswords/anagram-helper/ring.js
+++ static/src/javascripts/projects/common/modules/crosswords/anagram-helper/ring.js
@@ -1,53 +1,47 @@
-define([
-    'react/addons',
-    'lodash/collections/map'
-], function (
-    React,
-    map
-) {
-    var round = function (x) {
-        return Math.round(x * 100) / 100;
-    };
-    var Ring = React.createClass({
-        /**
-         * Get coordinates for a letter as percentages.
-         *
-         * To get the diameter:
-         *   (width of .crossword__anagram-helper-shuffler) - (2 * desired padding)
-         *
-         * @param  {Number} angle   angle of letters on the circle
-         * @param  {Number} i       letter index
-         * @return {Object}         with 'left' and 'top' properties in percent
-         */
-        getPosition: function (angle, i) {
-            var diameter = 40;
-            var theta = (angle * Math.PI / 180) * i;
+import React from 'react/addons';
+import map from 'lodash/collections/map';
+var round = function(x) {
+    return Math.round(x * 100) / 100;
+};
+var Ring = React.createClass({
+    /**
+     * Get coordinates for a letter as percentages.
+     *
+     * To get the diameter:
+     *   (width of .crossword__anagram-helper-shuffler) - (2 * desired padding)
+     *
+     * @param  {Number} angle   angle of letters on the circle
+     * @param  {Number} i       letter index
+     * @return {Object}         with 'left' and 'top' properties in percent
+     */
+    getPosition: function(angle, i) {
+        var diameter = 40;
+        var theta = (angle * Math.PI / 180) * i;
 
-            return {
-                left: diameter + round(diameter * Math.sin(theta)) + '%',
-                top: diameter + round(diameter * Math.cos(theta)) + '%'
-            };
-        },
+        return {
+            left: diameter + round(diameter * Math.sin(theta)) + '%',
+            top: diameter + round(diameter * Math.cos(theta)) + '%'
+        };
+    },
 
-        render: function () {
-            var angle = 360 / this.props.letters.length;
+    render: function() {
+        var angle = 360 / this.props.letters.length;
 
-            return React.createElement(
-                'div', {
-                    className: 'crossword__anagram-helper-shuffler'
-                },
-                map(this.props.letters, (function (letter, i) {
-                    return React.createElement(
-                        'div', {
-                            className: 'crossword__anagram-helper-shuffler__letter ' + (letter.entered ? 'entered' : ''),
-                            style: this.getPosition(angle, i),
-                            key: i
-                        }, letter.value
-                    );
-                }), this)
-            );
-        }
-    });
-
-    return Ring;
+        return React.createElement(
+            'div', {
+                className: 'crossword__anagram-helper-shuffler'
+            },
+            map(this.props.letters, (function(letter, i) {
+                return React.createElement(
+                    'div', {
+                        className: 'crossword__anagram-helper-shuffler__letter ' + (letter.entered ? 'entered' : ''),
+                        style: this.getPosition(angle, i),
+                        key: i
+                    }, letter.value
+                );
+            }), this)
+        );
+    }
 });
+
+export default Ring;
