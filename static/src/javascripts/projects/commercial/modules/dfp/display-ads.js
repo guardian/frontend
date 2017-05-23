b9c61f9066608e4265a778509f8b74051919cc7a
Jon Norman <jon.norman@guardian.co.uk>
2017-05-23 11:05:14 +0100

    transform projects/commercial/modules/dfp/display-ads.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/dfp/display-ads.js
+++ static/src/javascripts/projects/commercial/modules/dfp/display-ads.js
@@ -1,17 +1,13 @@
-define([
-    'commercial/modules/dfp/dfp-env',
-    'commercial/modules/dfp/load-advert'
-], function (dfpEnv, loadAdvert) {
-    return displayAds;
+import dfpEnv from 'commercial/modules/dfp/dfp-env';
+import loadAdvert from 'commercial/modules/dfp/load-advert';
+export default displayAds;
 
-    function displayAds() {
-        window.googletag.pubads().enableSingleRequest();
-        window.googletag.pubads().collapseEmptyDivs();
-        window.googletag.enableServices();
-        // as this is an single request call, only need to make a single display call (to the first ad
-        // slot)
-        loadAdvert(dfpEnv.dfpEnv.advertsToLoad[0]);
-        dfpEnv.dfpEnv.advertsToLoad.length = 0;
-      }
-
-});
+function displayAds() {
+    window.googletag.pubads().enableSingleRequest();
+    window.googletag.pubads().collapseEmptyDivs();
+    window.googletag.enableServices();
+    // as this is an single request call, only need to make a single display call (to the first ad
+    // slot)
+    loadAdvert(dfpEnv.dfpEnv.advertsToLoad[0]);
+    dfpEnv.dfpEnv.advertsToLoad.length = 0;
+}
