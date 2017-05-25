5a98d221a5d91b8a30cf00155c0a1ca94d64a9a8
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-05-25 11:15:37 +0100

    update messaage

--- static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-pre-election.js
+++ static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-pre-election.js
@@ -25,7 +25,7 @@ define([
         description: 'Test 2 variants centered around the upcoming election',
         successMeasure: 'Conversion rate',
         idealOutcome: 'We are able to determine which message has a higher conversion rate',
-        locations: ['GB'],  
+        locations: ['GB'],
         audienceCriteria: 'All',
         audience: 0.5,
         audienceOffset: 0.5,
@@ -42,7 +42,7 @@ define([
                         contributionUrl: variant.options.contributeURL,
                         componentName: variant.options.componentName,
                         quoteSvg: quoteSvg.markup,
-                        p1: '&hellip; we’ve got a small favour to ask. More people are reading the Guardian than ever, but far fewer are paying for it. Advertising revenues across the media are falling fast. And <span class="contributions__highlight">unlike many news organisations, we haven’t put up a paywall – we want to keep our journalism as open as we can</span>. So you can see why we need to ask for your help, especially during this UK election.',
+                        p1: '&hellip; we have a small favour to ask. More people are reading the Guardian than ever, but far fewer are paying for it. Advertising revenues across the media are falling fast. And <span class="contributions__highlight">unlike many news organisations, we haven’t put up a paywall – we want to keep our journalism as open as we can</span>. So you can see why we need to ask for your help, especially during this UK election.',
                         testimonialMessage: 'I’m a 19 year old student disillusioned by an unequal society with a government that has stopped even pretending to work in my generation’s interests. So for the strength of our democracy, for the voice of the young, for a credible, independent check on the government, this contribution was pretty good value for money.',
                         testimonialName: 'Jack H.'
                     })
@@ -56,7 +56,7 @@ define([
                         membershipUrl: variant.options.membershipURL,
                         contributionUrl: variant.options.contributeURL,
                         componentName: variant.options.componentName,
-                        p1: '&hellip; we’ve got a small favour to ask. Whoever wins the UK election, we promise to hold them to account with facts you can trust and opinions you can believe in. But advertising revenues are falling, <span class="contributions__highlight">the Guardian has no billionaire owner</span> and <span class="contributions__highlight">unlike many news organisations, we haven’t put up a paywall &ndash; we want to keep our journalism as open as we can</span>. So you can see why we need to ask for your help. The Guardian’s independent, investigative journalism takes a lot of time, money and hard work to produce. But we do it because we believe our perspective matters &ndash; because it might well be your perspective, too.',
+                        p1: '&hellip; we have a small favour to ask. Whoever wins the UK election, we promise to hold them to account with facts you can trust and opinions you can believe in. But advertising revenues are falling and <span class="contributions__highlight">unlike many news organisations, we haven’t put up a paywall &ndash; we want to keep our journalism as open as we can</span>. So you can see why we need to ask for your help. The Guardian’s independent, investigative journalism takes a lot of time, money and hard work to produce. But we do it because we believe our perspective matters &ndash; because it might well be your perspective, too.',
                         p2: 'If everyone who reads our reporting, who likes it, helps to support it, our future would be much more secure.',
                         p3: '',
                         cta1: 'Become a supporter',
