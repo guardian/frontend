c2a1d9059caff583015ec2005b4d7ec689e150b2
Francis Carr <francis.carr@guardian.co.uk>
2017-05-25 12:08:17 +0100

    Fix problems with conversion to es6

--- static/src/javascripts/projects/commercial/modules/creatives/add-viewability-tracker.js
+++ static/src/javascripts/projects/commercial/modules/creatives/add-viewability-tracker.js
@@ -1,13 +1,21 @@
-export default addViewabilityTracker;
-
-function addViewabilityTracker(adSlot, creativeId, viewabilityTracker) {
-
+// @flow
+const addViewabilityTracker = (
+    adSlot: Node,
+    creativeId: string,
+    viewabilityTracker: string
+): void => {
     /*
       we need to ensure that any scripts in the viewabilityTracker are parsed
       and executed; this can be done with a Range and ContextualFragment
     */
-    var range = document.createRange();
+    const range: Range = document.createRange();
     range.setStart(adSlot, 0);
     range.setEnd(adSlot, 0);
-    adSlot.appendChild(range.createContextualFragment(viewabilityTracker.replace('INSERT_UNIQUE_ID', creativeId)));
-}
+    adSlot.appendChild(
+        range.createContextualFragment(
+            viewabilityTracker.replace('INSERT_UNIQUE_ID', creativeId)
+        )
+    );
+};
+
+export default addViewabilityTracker;
