2eeb4f5321b4a76c207400471b7012a8644d3c62
Simon Adcock <simon.adcock@guardian.co.uk>
2017-05-25 15:30:11 +0100

    namespace, type and lint identity-common

--- static/src/javascripts/bootstraps/enhanced/identity-common.js
+++ static/src/javascripts/bootstraps/enhanced/identity-common.js
@@ -1,22 +1,25 @@
+// @flow
+
 /**
  * Identity crap that has to run on every page (putting in usernames, avatars, etc.)
  *
  * Explicitly NOT stuff that only runs on the identity pages. Put that in profile.js or I will hunt you down. I WILL
  * HUNT YOU DOWN.
  */
-import robust from 'lib/robust';
+import { catchErrorsWithContext } from 'lib/robust';
 import Id from 'common/modules/identity/api';
 
-function setCssClass() {
-    // Used to show elements that need signin. Use .sign-in-required
-    if (Id.isUserLoggedIn()) {
-        document.documentElement.className = document.documentElement.className
-            .replace(/\bid--signed-out\b/, 'id--signed-in');
+// Used to show elements that need signin. Use .sign-in-required
+const setCssClass = (): void => {
+    if (!Id.isUserLoggedIn() || !document.documentElement) {
+        return;
     }
-}
+    const classList = document.documentElement.classList;
 
-export default function() {
-    robust.catchErrorsWithContext([
-        ['i-css-class', setCssClass],
-    ]);
+    classList.add('id--signed-in');
+    classList.remove('id--signed-out');
+};
+
+export const init = (): void => {
+    catchErrorsWithContext([['i-css-class', setCssClass]]);
 };
