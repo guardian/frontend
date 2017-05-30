f1195b531e822b30c5443e78ade91c8a9beb8afa
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-30 11:28:30 +0100

    Create a module for message exchanges

--- static/src/javascripts/bootstraps/atoms/storyquestions.js
+++ static/src/javascripts/bootstraps/atoms/storyquestions.js
@@ -4,7 +4,7 @@ import domready from 'domready';
 import config from 'lib/config';
 import { init } from 'common/modules/atoms/story-questions';
 import fastdom from 'lib/fastdom-promise';
-import post from 'commercial/modules/messenger/post-message';
+import { send } from 'commercial/modules/messenger/send';
 
 // let webpack know where to get files from
 // __webpack_public_path__ is a special webpack variable
@@ -12,11 +12,25 @@ import post from 'commercial/modules/messenger/post-message';
 // eslint-disable-next-line camelcase,no-undef
 __webpack_public_path__ = `${config.page.assetsPath}javascripts/`;
 
-const resizeMessage = (payload: Object): Object => ({
-    id: window.name,
-    type: 'resize',
-    value: payload,
-});
+type FontWeight =
+    | '100'
+    | '200'
+    | '300'
+    | '400'
+    | '500'
+    | '600'
+    | '700'
+    | 'regular'
+    | 'bold';
+
+type FontStyle = 'italic' | 'normal';
+
+type Webfont = {
+    family: string,
+    weight?: FontWeight,
+    style?: FontStyle,
+    url?: string,
+};
 
 /* in app article will send a message when ready to interact */
 const comready = (callback: () => void): void => {
@@ -25,6 +39,42 @@ const comready = (callback: () => void): void => {
         window.removeEventListener('message', onM);
     });
 };
+
+const installWebfonts = (fonts: Webfont[]) => {
+    const css = fonts
+        .map(
+            (font: Webfont): string => `
+        @font-face {
+          font-family: ${font.family};
+          font-weight: ${font.weight || '400'};
+          font-style: ${font.style || 'normal'};
+          src: url(${font.url || ''});
+        }
+        `
+        )
+        .join('\n');
+    (document.styleSheets[0]: window.CSSStyleSheet).insertRule(css);
+};
+
+const getWebfonts = (families: Webfont[]): Promise<void> =>
+    new Promise(resolve => {
+        const msgId = send('get-webfonts', families);
+        window.addEventListener('message', function onM(evt) {
+            if (!(evt instanceof MessageEvent)) {
+                return;
+            }
+            const { id, error, result } = JSON.parse(evt.data);
+            if (id !== msgId) {
+                return;
+            }
+            if (error) {
+                throw new Error(error);
+            }
+            window.removeEventListener('message', onM);
+            installWebfonts(result);
+            resolve();
+        });
+    });
 
 Promise.all([
     window.guardian.polyfilled
@@ -43,6 +93,10 @@ Promise.all([
                 document.documentElement.getBoundingClientRect().height
         )
         .then(height => {
-            post(resizeMessage({ height }), window.top, '*');
+            send('resize', { height });
         });
+    getWebfonts([
+        { family: 'Guardian Text Egyptian Web', weight: 'bold' },
+        { family: 'Guardian Text Egyptian Web', weight: 'regular' },
+    ]);
 });
