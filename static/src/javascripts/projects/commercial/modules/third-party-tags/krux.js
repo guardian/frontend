d5779209d888fc6a14f56033faf736deb22f909a
Francis Carr <francis.carr@guardian.co.uk>
2017-05-31 16:42:18 +0100

    transform projects/commercial/modules/third-party-tags/krux.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/third-party-tags/krux.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/krux.js
@@ -1,28 +1,20 @@
-define([
-    'lib/config',
-    'lib/cookies',
-    'lib/storage'
-], function (
-    config,
-    cookies,
-    storage
-) {
-    var kruxUrl = '//cdn.krxd.net/controltag?confid=JVZiE3vn';
+import config from 'lib/config';
+import cookies from 'lib/cookies';
+import storage from 'lib/storage';
+var kruxUrl = '//cdn.krxd.net/controltag?confid=JVZiE3vn';
 
-    function retrieve(n) {
-        var k = 'kx' + n;
+function retrieve(n) {
+    var k = 'kx' + n;
 
-        return storage.local.getRaw(k) || cookies.getCookie(k + '=([^;]*)') || '';
-    }
+    return storage.local.getRaw(k) || cookies.getCookie(k + '=([^;]*)') || '';
+}
 
-    function getSegments() {
-        return retrieve('segs') ? retrieve('segs').split(',') : [];
-    }
+function getSegments() {
+    return retrieve('segs') ? retrieve('segs').split(',') : [];
+}
 
-    return {
-        shouldRun: config.switches.krux,
-        url: kruxUrl,
-        getSegments: getSegments
-    };
-
-});
+export default {
+    shouldRun: config.switches.krux,
+    url: kruxUrl,
+    getSegments: getSegments
+};
