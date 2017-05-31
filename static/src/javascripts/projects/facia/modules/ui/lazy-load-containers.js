dedf6ae22945414c3e86bc852c267e7d05c3e33e
Philip Wills <philip.wills@guardian.co.uk>
2017-05-31 17:09:17 +0100

    transform projects/facia/modules/ui/lazy-load-containers.js to ES6 module

--- static/src/javascripts/projects/facia/modules/ui/lazy-load-containers.js
+++ static/src/javascripts/projects/facia/modules/ui/lazy-load-containers.js
@@ -1,53 +1,45 @@
-define([
-    'fastdom',
-    'qwery',
-    'lib/detect',
-    'lib/mediator',
-    'lodash/functions/throttle'
-], function (
-    fastdom,
-    qwery,
-    detect,
-    mediator,
-    throttle
-) {
-    var distanceBeforeLoad = detect.getViewport().height;
+import fastdom from 'fastdom';
+import qwery from 'qwery';
+import detect from 'lib/detect';
+import mediator from 'lib/mediator';
+import throttle from 'lodash/functions/throttle';
+var distanceBeforeLoad = detect.getViewport().height;
 
-    return function () {
-        var containers = qwery('.js-container--lazy-load'),
-            lazyLoad = throttle(function () {
-                if (containers.length === 0) {
-                    mediator.off('window:throttledScroll', lazyLoad);
-                } else {
-                    fastdom.read(function () {
-                        var containersInRange = containers.reduce(function (result, container) {
-                            result[withinRange(container) ? 'in' : 'out'].push(container);
-                            return result;
-                        }, { in: [], out: [] });
-
-                        containers = containersInRange.out;
-
-                        fastdom.write(function () {
-                            containersInRange.in.forEach(displayContainer);
-                        });
+export default function() {
+    var containers = qwery('.js-container--lazy-load'),
+        lazyLoad = throttle(function() {
+            if (containers.length === 0) {
+                mediator.off('window:throttledScroll', lazyLoad);
+            } else {
+                fastdom.read(function() {
+                    var containersInRange = containers.reduce(function(result, container) {
+                        result[withinRange(container) ? 'in' : 'out'].push(container);
+                        return result;
+                    }, { in : [], out: []
                     });
-                }
-            }, 500);
 
-        mediator.on('window:throttledScroll', lazyLoad);
-        lazyLoad();
-    };
+                    containers = containersInRange.out;
 
-    // withinRange(Element) : Bool
-    // Checks whether the element is within one screenful above or below the viewport
-    function withinRange(container) {
-        var top = container.nextElementSibling.getBoundingClientRect().top;
-        return -distanceBeforeLoad < top && top < 2 * distanceBeforeLoad;
-    }
+                    fastdom.write(function() {
+                        containersInRange.in.forEach(displayContainer);
+                    });
+                });
+            }
+        }, 500);
 
-    // displayContainer(Element) : Void
-    // Removes the fc-container--lazy-load class
-    function displayContainer(container) {
-        container.classList.remove('fc-container--lazy-load');
-    }
-});
+    mediator.on('window:throttledScroll', lazyLoad);
+    lazyLoad();
+};
+
+// withinRange(Element) : Bool
+// Checks whether the element is within one screenful above or below the viewport
+function withinRange(container) {
+    var top = container.nextElementSibling.getBoundingClientRect().top;
+    return -distanceBeforeLoad < top && top < 2 * distanceBeforeLoad;
+}
+
+// displayContainer(Element) : Void
+// Removes the fc-container--lazy-load class
+function displayContainer(container) {
+    container.classList.remove('fc-container--lazy-load');
+}
