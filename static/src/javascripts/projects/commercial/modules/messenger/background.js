3b25129999a8357efce7d6541de897d3db4c0ecc
Kate Whalen <whalenka88@gmail.com>
2017-06-05 11:39:50 +0100

    transform projects/commercial/modules/messenger/background.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/messenger/background.js
+++ static/src/javascripts/projects/commercial/modules/messenger/background.js
@@ -1,59 +1,59 @@
-define([
-    'lib/events',
-    'lodash/objects/assign',
-    'lib/fastdom-promise',
-    'commercial/modules/messenger'
-], function (events, assign, fastdom, messenger) {
-    messenger.register('background', function(specs, ret, iframe) {
-        return setBackground(specs, iframe.closest('.js-ad-slot'));
-    });
+import events from 'lib/events';
+import assign from 'lodash/objects/assign';
+import fastdom from 'lib/fastdom-promise';
+import messenger from 'commercial/modules/messenger';
+messenger.register('background', function(specs, ret, iframe) {
+    return setBackground(specs, iframe.closest('.js-ad-slot'));
+});
 
-    return setBackground;
+export default setBackground;
 
-    function setBackground(specs, adSlot) {
-        if (!specs ||
-            !('backgroundImage' in specs) ||
-            !('backgroundRepeat' in specs) ||
-            !('backgroundPosition' in specs) ||
-            !('scrollType' in specs)
-        ) {
-            return null;
-        }
+function setBackground(specs, adSlot) {
+    if (!specs ||
+        !('backgroundImage' in specs) ||
+        !('backgroundRepeat' in specs) ||
+        !('backgroundPosition' in specs) ||
+        !('scrollType' in specs)
+    ) {
+        return null;
+    }
 
-        // Create an element to hold the background image
-        var background = document.createElement('div');
-        background.className = 'creative__background creative__background--' + specs.scrollType;
-        assign(background.style, specs);
+    // Create an element to hold the background image
+    var background = document.createElement('div');
+    background.className = 'creative__background creative__background--' + specs.scrollType;
+    assign(background.style, specs);
 
-        // Wrap the background image in a DIV for positioning. Also, we give
-        // this DIV a background colour if it is provided. This is because
-        // if we set the background colour in the creative itself, the background
-        // image won't be visible (think z-indexed layers)
-        var backgroundParent = document.createElement('div');
-        backgroundParent.className = 'creative__background-parent';
-        if ('backgroundColour' in specs) {
-            backgroundParent.style.backgroundColor = specs.backgroundColour;
-        }
-        backgroundParent.appendChild(background);
+    // Wrap the background image in a DIV for positioning. Also, we give
+    // this DIV a background colour if it is provided. This is because
+    // if we set the background colour in the creative itself, the background
+    // image won't be visible (think z-indexed layers)
+    var backgroundParent = document.createElement('div');
+    backgroundParent.className = 'creative__background-parent';
+    if ('backgroundColour' in specs) {
+        backgroundParent.style.backgroundColor = specs.backgroundColour;
+    }
+    backgroundParent.appendChild(background);
 
-        if( specs.scrollType === 'fixed' ) {
-            return fastdom.write(function () {
-                adSlot.insertBefore(backgroundParent, adSlot.firstChild);
-            });
-        } else {
-            var updateQueued = false;
+    if (specs.scrollType === 'fixed') {
+        return fastdom.write(function() {
+            adSlot.insertBefore(backgroundParent, adSlot.firstChild);
+        });
+    } else {
+        var updateQueued = false;
 
-            return fastdom.write(function () {
+        return fastdom.write(function() {
                 adSlot.insertBefore(backgroundParent, adSlot.firstChild);
             })
-            .then(function () {
-                events.addEventListener(window, 'scroll', onScroll, { passive: true });
+            .then(function() {
+                events.addEventListener(window, 'scroll', onScroll, {
+                    passive: true
+                });
                 onScroll();
 
                 function onScroll() {
                     if (!updateQueued) {
                         updateQueued = true;
-                        fastdom.read(function () {
+                        fastdom.read(function() {
                             updateQueued = false;
                             var rect = backgroundParent.getBoundingClientRect();
                             var dy = (0.3 * rect.top | 0) + 20;
@@ -67,6 +67,5 @@ define([
                     }
                 }
             });
-        }
     }
-});
+}
