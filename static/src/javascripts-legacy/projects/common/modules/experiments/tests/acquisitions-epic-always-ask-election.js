503b652df49b3123d34289142bba8482626425d7
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-06-06 15:28:06 +0100

    add tailoring to messages that should have it

--- static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-always-ask-election.js
+++ static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-always-ask-election.js
@@ -1,11 +1,18 @@
 define([
     'common/modules/commercial/contributions-utilities',
+    'raw-loader!common/views/acquisitions-epic-control.html',
+    'common/modules/tailor/tailor',
+    'lodash/utilities/template',
+    'common/modules/commercial/acquisitions-copy'
+
 ], function (
-    contributionsUtilities
+    contributionsUtilities,
+    acquisitionsEpicControlTemplate,
+    tailor,
+    template,
+    acquisitionsCopy
 ) {
-
-
-
+    
     return contributionsUtilities.makeABTest({
         id: 'AcquisitionsEpicAlwaysAskElection',
         campaignId: 'epic_always_ask_election',
@@ -26,7 +33,21 @@ define([
         variants: [
             {
                 id: 'control',
-                isUnlimited : true
+                isUnlimited : true,
+                successOnView: true,
+                test: function(render) {
+                    tailor.isRegular().then(function (regular) {
+                        var copy = regular ? acquisitionsCopy.regulars: acquisitionsCopy.control;
+                        return render(function(variant) {
+                            return template(acquisitionsEpicControlTemplate, {
+                                copy: copy,
+                                membershipUrl: variant.options.membershipURL,
+                                contributionUrl: variant.options.contributeURL,
+                                componentName: variant.options.componentName
+                            });
+                        });
+                    });
+                }
             }
         ]
     });
--- static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-always-ask-if-tagged.js
+++ static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-always-ask-if-tagged.js
@@ -1,12 +1,19 @@
 define([
     'common/modules/commercial/contributions-utilities',
+    'raw-loader!common/views/acquisitions-epic-control.html',
+    'common/modules/tailor/tailor',
+    'lodash/utilities/template',
+    'common/modules/commercial/acquisitions-copy'
+
 ], function (
-    contributionsUtilities
+    contributionsUtilities,
+    acquisitionsEpicControlTemplate,
+    tailor,
+    template,
+    acquisitionsCopy
 ) {
 
-
-
-    return contributionsUtilities.makeABTest({
+   return contributionsUtilities.makeABTest({
         id: 'AcquisitionsEpicAlwaysAskIfTagged',
         campaignId: 'epic_always_ask_if_tagged',
 
@@ -29,6 +36,19 @@ define([
                 id: 'control',
                 isUnlimited : true,
                 successOnView: true,
+                test: function(render) {
+                    tailor.isRegular().then(function (regular) {
+                        var copy = regular ? acquisitionsCopy.regulars: acquisitionsCopy.control;
+                        return render(function(variant) {
+                            return template(acquisitionsEpicControlTemplate, {
+                                copy: copy,
+                                membershipUrl: variant.options.membershipURL,
+                                contributionUrl: variant.options.contributeURL,
+                                componentName: variant.options.componentName
+                            });
+                        });
+                    });
+                }
             }
         ]
     });
