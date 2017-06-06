326e7bccfd0e256986fba08438b1d0f057f030aa
Jon Norman <jon.norman@guardian.co.uk>
2017-06-06 15:40:54 +0100

    transform projects/commercial/modules/creatives/fabric-video.js content to ES6

--- static/src/javascripts/projects/commercial/modules/creatives/fabric-video.js
+++ static/src/javascripts/projects/commercial/modules/creatives/fabric-video.js
@@ -1,130 +1,177 @@
+// @flow
 import qwery from 'qwery';
-import events from 'lib/events';
+import { addEventListener } from 'lib/events';
 import fastdom from 'lib/fastdom-promise';
 import detect from 'lib/detect';
 import template from 'lodash/utilities/template';
-import addTrackingPixel from 'commercial/modules/creatives/add-tracking-pixel';
-import addViewabilityTracker from 'commercial/modules/creatives/add-viewability-tracker';
-import fabricVideoStr from 'raw-loader!commercial/views/creatives/fabric-video.html';
-var fabricVideoTpl;
+import {
+    addTrackingPixel,
+} from 'commercial/modules/creatives/add-tracking-pixel';
+import addViewabilityTracker
+    from 'commercial/modules/creatives/add-viewability-tracker';
+import fabricVideoStr
+    from 'raw-loader!commercial/views/creatives/fabric-video.html';
 
-export default FabricVideo;
+const fabricVideoTpl = template(fabricVideoStr);
 
-function FabricVideo(adSlot, params) {
-    var isUpdating = false;
-    var isSmallScreen = detect.isBreakpoint({
-        max: 'phablet'
-    });
-    var hasVideo, video, layer2, inView;
+class FabricVideo {
+    isUpdating: boolean;
+    adSlot: HTMLElement;
+    params: Object;
+    layer2: ?Array<Element>;
+    video: ?HTMLVideoElement;
+    hasVideo: boolean;
+    inView: boolean;
 
-    fabricVideoTpl || (fabricVideoTpl = template(fabricVideoStr));
-
-    hasVideo = !(detect.isIOS() || detect.isAndroid() || isSmallScreen);
-
-    params.id = 'fabric-video-' + (Math.random() * 10000 | 0).toString(16);
-
-    if (isSmallScreen) {
-        params.posterMobile = '<div class="creative__poster" style="background-image:url(' + params.Videobackupimage + ')"></div>';
-    } else {
-        if (hasVideo) {
-            params.video = '<video muted class="creative__video creative__video--' + params.Videoalignment + '"><source src="' + params.VideoURL + '" type="video/mp4"></video>';
-        }
-
-        params.posterTablet = '<div class="creative__poster" style="background-image:url(' + params.BackgroundImagemobile + ')"></div>';
-    }
-
-    return Object.freeze({
-        create: create
-    });
-
-    function create() {
-        return fastdom.write(function() {
-            if (params.Trackingpixel) {
-                addTrackingPixel.addTrackingPixel(params.Trackingpixel + params.cacheBuster);
-            }
-            if (params.Researchpixel) {
-                addTrackingPixel.addTrackingPixel(params.Researchpixel + params.cacheBuster)
-            }
-            adSlot.insertAdjacentHTML('beforeend', fabricVideoTpl({
-                data: params
-            }));
-            if (params.viewabilityTracker) {
-                addViewabilityTracker(adSlot, params.id, params.viewabilityTracker);
-            }
-            adSlot.classList.add('ad-slot--fabric');
-            if (adSlot.parentNode.classList.contains('top-banner-ad-container')) {
-                adSlot.parentNode.classList.add('top-banner-ad-container--fabric');
-            }
-        }).then(function() {
-            layer2 = qwery('.creative__layer2', adSlot);
-
-            events.addEventListener(window, 'scroll', onScroll, {
-                passive: true
-            });
-            events.addEventListener(adSlot, 'animationend', function() {
-                window.removeEventListener('scroll', onScroll);
-            });
-
-            if (hasVideo) {
-                video = adSlot.getElementsByTagName('video')[0];
-                video.onended = onVideoEnded;
-            }
-
-            fastdom.read(onScroll);
-
-            return true;
+    constructor(adSlot: HTMLElement, params: Object) {
+        const isSmallScreen = detect.isBreakpoint({
+            max: 'phablet',
         });
-    }
 
-    function onVideoEnded() {
-        video.onended = null;
-        video = null;
-    }
+        this.isUpdating = false;
+        this.hasVideo = !(detect.isIOS() ||
+            detect.isAndroid() ||
+            isSmallScreen);
+        this.inView = false;
+        this.adSlot = adSlot;
+        this.params = params;
 
-    function onScroll() {
-        var viewportHeight = detect.getViewport().height;
-        var rect = adSlot.getBoundingClientRect();
-        inView = rect.top >= 0 && rect.bottom < viewportHeight;
-        if (!isUpdating) {
-            isUpdating = true;
-            fastdom.write(updateView);
-        }
-    }
+        this.params.id = `fabric-video-${Math.floor(Math.random() * 10000).toString(16)}`;
 
-    function updateView() {
-        isUpdating = false;
-        if (video) {
-            updateVideo();
-        }
-        updateAnimation();
-    }
-
-    function updateVideo() {
-        if (inView) {
-            video.play();
+        if (isSmallScreen) {
+            this.params.posterMobile = `<div class="creative__poster" style="background-image:url(${this.params.Videobackupimage})"></div>`;
         } else {
-            video.pause();
+            if (this.hasVideo) {
+                this.params.video = `<video muted class="creative__video creative__video--${this.params.Videoalignment}"><source src="${this.params.VideoURL}" type="video/mp4"></video>`;
+            }
+
+            this.params.posterTablet = `<div class="creative__poster" style="background-image:url(${this.params.BackgroundImagemobile})"></div>`;
         }
     }
 
-    function updateAnimation() {
-        if (inView) {
-            playAnimation();
+    onVideoEnded() {
+        if (this.video) {
+            this.video.onended = null;
+            this.video = null;
+        }
+    }
+
+    onScroll() {
+        const viewportHeight = detect.getViewport().height;
+        const rect = this.adSlot.getBoundingClientRect();
+        this.inView = rect.top >= 0 && rect.bottom < viewportHeight;
+        if (!this.isUpdating) {
+            this.isUpdating = true;
+            fastdom.write(this.updateView);
+        }
+    }
+
+    updateView() {
+        this.isUpdating = false;
+        if (this.video) {
+            this.updateVideo();
+        }
+        this.updateAnimation();
+    }
+
+    updateVideo() {
+        if (this.video) {
+            if (this.inView) {
+                this.video.play();
+            } else {
+                this.video.pause();
+            }
+        }
+    }
+
+    updateAnimation() {
+        if (this.inView) {
+            this.playAnimation();
         } else {
-            pauseAnimation();
+            this.pauseAnimation();
         }
     }
 
-    function playAnimation() {
-        layer2.forEach(function(l) {
-            l.classList.add('is-animating');
-        });
+    playAnimation() {
+        if (this.layer2) {
+            this.layer2.forEach(l => {
+                l.classList.add('is-animating');
+            });
+        }
     }
 
-    function pauseAnimation() {
-        layer2.forEach(function(l) {
-            l.classList.remove('is-animating');
-        });
+    pauseAnimation() {
+        if (this.layer2) {
+            this.layer2.forEach(l => {
+                l.classList.remove('is-animating');
+            });
+        }
     }
 
+    create() {
+        return fastdom
+            .write(() => {
+                if (this.params.Trackingpixel) {
+                    addTrackingPixel(
+                        this.params.Trackingpixel + this.params.cacheBuster
+                    );
+                }
+                if (this.params.Researchpixel) {
+                    addTrackingPixel(
+                        this.params.Researchpixel + this.params.cacheBuster
+                    );
+                }
+
+                this.adSlot.insertAdjacentHTML(
+                    'beforeend',
+                    fabricVideoTpl({
+                        data: this.params,
+                    })
+                );
+
+                if (this.params.viewabilityTracker) {
+                    addViewabilityTracker(
+                        this.adSlot,
+                        this.params.id,
+                        this.params.viewabilityTracker
+                    );
+                }
+                if (this.adSlot) {
+                    this.adSlot.classList.add('ad-slot--fabric');
+                }
+
+                if (
+                    this.adSlot.parentNode &&
+                    this.adSlot.parentNode instanceof Element &&
+                    this.adSlot.parentNode.classList.contains(
+                        'top-banner-ad-container'
+                    )
+                ) {
+                    this.adSlot.parentNode.classList.add(
+                        'top-banner-ad-container--fabric'
+                    );
+                }
+            })
+            .then(() => {
+                this.layer2 = qwery('.creative__layer2', this.adSlot);
+
+                addEventListener(window, 'scroll', this.onScroll, {
+                    passive: true,
+                });
+                addEventListener(this.adSlot, 'animationend', () => {
+                    window.removeEventListener('scroll', this.onScroll);
+                });
+
+                if (this.hasVideo) {
+                    this.video = this.adSlot.getElementsByTagName('video')[0];
+                    this.video.onended = this.onVideoEnded;
+                }
+
+                fastdom.read(this.onScroll);
+
+                return true;
+            });
+    }
 }
+
+export { FabricVideo };
