66ae146a0f0e674eb9b3a510682744355b8ceb58
Kate Whalen <whalenka88@gmail.com>
2017-06-07 13:17:34 +0100

    Fix ALL the Flow! Refactor bitwise and assign, fix key naming, add hack for missing CSSStyleDeclaration

--- static/src/javascripts/projects/commercial/modules/messenger/background.js
+++ static/src/javascripts/projects/commercial/modules/messenger/background.js
@@ -1,15 +1,20 @@
-import events from 'lib/events';
-import assign from 'lodash/objects/assign';
+// @flow
+import { addEventListener } from 'lib/events';
 import fastdom from 'lib/fastdom-promise';
-import messenger from 'commercial/modules/messenger';
-messenger.register('background', function(specs, ret, iframe) {
-    return setBackground(specs, iframe.closest('.js-ad-slot'));
-});
 
-export default setBackground;
+import { register } from 'commercial/modules/messenger';
 
-function setBackground(specs, adSlot) {
-    if (!specs ||
+type AdSpec = {
+    scrollType: string,
+    backgroundColor: string,
+    backgroundImage: string,
+    backgroundRepeat: string,
+    backgroundPosition: string,
+};
+
+const setBackground = (specs: AdSpec, adSlot: Node): Promise<any> | null => {
+    if (
+        !specs ||
         !('backgroundImage' in specs) ||
         !('backgroundRepeat' in specs) ||
         !('backgroundPosition' in specs) ||
@@ -18,54 +23,78 @@ function setBackground(specs, adSlot) {
         return null;
     }
 
+    const specsStyles = Object.keys(specs).reduce((result, key) => {
+        if (key !== 'scrollType') {
+            result[key] = specs[key];
+        }
+        // Flow is love, Flow is Life! DFP has been passing us `backgroundColour`
+        // all along, and the setting of this css prop has been silently failing
+        if (key === 'backgroundColour') {
+            result.backgroundColor = specs[key];
+        }
+        return result;
+    }, {});
+
     // Create an element to hold the background image
-    var background = document.createElement('div');
-    background.className = 'creative__background creative__background--' + specs.scrollType;
-    assign(background.style, specs);
+    const background = document.createElement('div');
+    background.className = `creative__background creative__background--${specs.scrollType}`;
+    background.style = Object.assign(background.style, specsStyles);
 
     // Wrap the background image in a DIV for positioning. Also, we give
     // this DIV a background colour if it is provided. This is because
     // if we set the background colour in the creative itself, the background
     // image won't be visible (think z-indexed layers)
-    var backgroundParent = document.createElement('div');
+    const backgroundParent = document.createElement('div');
     backgroundParent.className = 'creative__background-parent';
-    if ('backgroundColour' in specs) {
-        backgroundParent.style.backgroundColor = specs.backgroundColour;
+    if (specsStyles.backgroundColor) {
+        backgroundParent.style.backgroundColor = specsStyles.backgroundColor;
     }
     backgroundParent.appendChild(background);
 
     if (specs.scrollType === 'fixed') {
-        return fastdom.write(function() {
+        return fastdom.write(() => {
             adSlot.insertBefore(backgroundParent, adSlot.firstChild);
         });
-    } else {
-        var updateQueued = false;
-
-        return fastdom.write(function() {
-                adSlot.insertBefore(backgroundParent, adSlot.firstChild);
-            })
-            .then(function() {
-                events.addEventListener(window, 'scroll', onScroll, {
-                    passive: true
-                });
-                onScroll();
-
-                function onScroll() {
-                    if (!updateQueued) {
-                        updateQueued = true;
-                        fastdom.read(function() {
-                            updateQueued = false;
-                            var rect = backgroundParent.getBoundingClientRect();
-                            var dy = (0.3 * rect.top | 0) + 20;
-
-                            // We update the style in a read batch because the DIV
-                            // has been promoted to its own layer and is also
-                            // strictly self-contained. Also, without doing that
-                            // the animation is extremely jittery.
-                            background.style.backgroundPositionY = dy + '%';
-                        });
-                    }
-                }
-            });
     }
-}
+    let updateQueued = false;
+
+    const onScroll = () => {
+        if (!updateQueued) {
+            updateQueued = true;
+            fastdom.read(() => {
+                updateQueued = false;
+                const rect = backgroundParent.getBoundingClientRect();
+                const dy = Math.round(0.3 * rect.top) + 20;
+
+                // We update the style in a read batch because the DIV
+                // has been promoted to its own layer and is also
+                // strictly self-contained. Also, without doing that
+                // the animation is extremely jittery.
+
+                // #? Flow does not currently list backgroundPositionY in
+                // CSSStyleDeclaration: https://github.com/facebook/flow/issues/396
+                // ...So we have to use a more convoluted hack-around:
+                (background.style: any).backgroundPositionY = `${dy}%`;
+            });
+        }
+    };
+
+    return fastdom
+        .write(() => {
+            adSlot.insertBefore(backgroundParent, adSlot.firstChild);
+        })
+        .then(() => {
+            addEventListener(window, 'scroll', onScroll, {
+                passive: true,
+            });
+            onScroll();
+        });
+};
+
+register('background', (specs, ret, iframe) => {
+    if (iframe && specs) {
+        return setBackground(specs, iframe.closest('.js-ad-slot'));
+    }
+});
+
+export default { setBackground };
