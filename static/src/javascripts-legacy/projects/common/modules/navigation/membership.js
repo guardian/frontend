e0f11ca872133a8fc9ad48cb95502f4f67609371
Michael McNamara <michael.mcnamara@guardian.co.uk>
2017-06-07 11:01:29 +0100

    remove console logs

--- static/src/javascripts-legacy/projects/common/modules/navigation/membership.js
+++ static/src/javascripts-legacy/projects/common/modules/navigation/membership.js
@@ -4,8 +4,12 @@ define(['commercial/modules/user-features', 'lib/fastdom-promise', 'lib/$'], fun
     function init() {
         if (userFeatures.isPayingMember()) {
             var $becomeMemberLink = $('.js-become-member');
+            var $becomeSupporterLabel = $('.header-cta-item__label');
+            var $becomeSupporterNewLine = $('.header-cta-item__new-line');
             var $subscriberLink = $('.js-subscribe');
             fastdom.write(function () {
+                $becomeSupporterLabel.text('Thank you for')
+                $becomeSupporterNewLine.text('your support')
                 $becomeMemberLink.attr('hidden', 'hidden');
                 $subscriberLink.removeClass(LAST_CLASS);
             });
