0859f343ff5e3e58fc916f5df891fd28bf273c08
Michael McNamara <michael.mcnamara@guardian.co.uk>
2017-06-07 17:18:21 +0100

    fastdom to update inner html for the become a supporter label

--- static/src/javascripts-legacy/projects/common/modules/navigation/membership.js
+++ static/src/javascripts-legacy/projects/common/modules/navigation/membership.js
@@ -5,11 +5,9 @@ define(['commercial/modules/user-features', 'lib/fastdom-promise', 'lib/$'], fun
         if (userFeatures.isPayingMember()) {
             var $becomeMemberLink = $('.js-become-member');
             var $becomeSupporterLabel = $('.header-cta-item__label');
-            var $becomeSupporterNewLine = $('.header-cta-item__new-line');
             var $subscriberLink = $('.js-subscribe');
             fastdom.write(function () {
-                $becomeSupporterLabel.text('Thank you for')
-                $becomeSupporterNewLine.text('your support')
+                $becomeSupporterLabel.html('Thank you for <span class="header-cta-item__new-line">your support</span>');
                 $becomeMemberLink.attr('hidden', 'hidden');
                 $subscriberLink.removeClass(LAST_CLASS);
             });
