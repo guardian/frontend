1c62cf5815b7aa97f6616c7de4d2d16bbdcfc713
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-06-09 11:46:51 +0100

    Mock performance API to be flow compliant

--- static/src/javascripts/lib/window-performance.js
+++ static/src/javascripts/lib/window-performance.js
@@ -1,8 +1,36 @@
 // @flow
 
+const noop = (...args: Array<any>): any => {};
+
 const api = window.performance ||
 window.msPerformance ||
 window.webkitPerformance ||
-window.mozPerformance || {};
+window.mozPerformance || {
+    getEntriesByName: noop,
+
+    navigation: {
+        redirectCount: 0,
+        type: '',
+    },
+
+    mark: noop,
+    now: noop,
+
+    timing: {
+        connectStart: 0,
+        connectEnd: 0,
+
+        domainLookupEnd: 0,
+        domainLookupStart: 0,
+
+        domContentLoadedEventStart: 0,
+
+        loadEventStart: 0,
+        loadEventEnd: 0,
+
+        responseStart: 0,
+        responseEnd: 0,
+    },
+};
 
 export default api;
