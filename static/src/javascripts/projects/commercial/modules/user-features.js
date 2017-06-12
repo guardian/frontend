a507b7a998fbb778a8ed88a0040e57c7c00e03b1
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-06-12 10:20:45 +0100

    Es6 projects commercial modules user features (#16920)
    * move projects/commercial/modules/user-features.js from legacy to standard JS
    
    * transform projects/commercial/modules/user-features.js to ES6 module
    
    * fix validation issues
    
    * fix validation issues
    
    * WIP some tests failing
    
    * fix validation issues
    
    * WIP fix some tests
    
    * WIP fix some tests
    
    * Revert "WIP fix some tests"
    
    This reverts commit 7d695b0514d032a967204a75053f7d81512816b7.
    
    * fix more tests
    
    * fix test
    
    * fix tests and validation issues
    
    * fix flow annotation
    
    * add flow annotations
    
    * remove unintended change
    
    * fix imports
    
    * fix test
    
    * expose refresh function publicly
    
    as it is used by enhanced/common.js
    
    * don't call test exports in production JS
    
    * move projects/commercial/modules/user-features.js from legacy to standard JS
    
    * transform projects/commercial/modules/user-features.js to ES6 module
    
    * fix validation issues
    
    * fix validation issues
    
    * WIP some tests failing
    
    * fix validation issues
    
    * WIP fix some tests
    
    * WIP fix some tests
    
    * Revert "WIP fix some tests"
    
    This reverts commit 7d695b0514d032a967204a75053f7d81512816b7.
    
    * fix more tests
    
    * fix test
    
    * fix tests and validation issues
    
    * fix flow annotation
    
    * add flow annotations
    
    * remove unintended change
    
    * fix imports
    
    * fix test
    
    * expose refresh function publicly
    
    as it is used by enhanced/common.js
    
    * don't call test exports in production JS
    
    * Revert "don't call test exports in production JS"
    
    This reverts commit c3e328e3932e8cfb0defad58728fcc8cc6c48c9f.
    
    * WIP start porting tests to Jest
    
    Here Be Dragons 🐲. Test errors.
    
    * fix test
    
    * restore some of the missing user-features tests
    
    * restore some of the missing user-features tests
    
    * change userHasData to boolean
    
    * restore some of the missing user-features tests
    
    * remove unused test exports

---
+++ static/src/javascripts/projects/commercial/modules/user-features.js
@@ -0,0 +1,88 @@
+// @flow
+import { getCookie, removeCookie, addCookie } from 'lib/cookies';
+import config from 'lib/config';
+import fetchJson from 'lib/fetch-json';
+import identity from 'common/modules/identity/api';
+// Persistence keys
+const USER_FEATURES_EXPIRY_COOKIE = 'gu_user_features_expiry';
+const PAYING_MEMBER_COOKIE = 'gu_paying_member';
+const AD_FREE_USER_COOKIE = 'GU_AFU';
+
+const userHasData = (): boolean => {
+    const cookie =
+        getCookie(USER_FEATURES_EXPIRY_COOKIE) ||
+        getCookie(PAYING_MEMBER_COOKIE) ||
+        getCookie(AD_FREE_USER_COOKIE);
+    return !!cookie;
+};
+
+const persistResponse = (JsonResponse: () => void) => {
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + 1);
+    addCookie(USER_FEATURES_EXPIRY_COOKIE, expiryDate.getTime().toString());
+    addCookie(PAYING_MEMBER_COOKIE, !JsonResponse.adblockMessage);
+    addCookie(AD_FREE_USER_COOKIE, JsonResponse.adFree);
+};
+
+const deleteOldData = (): void => {
+    // We expect adfree cookies to be cleaned up by the logout process, but what if the user's login simply times out?
+    removeCookie(USER_FEATURES_EXPIRY_COOKIE);
+    removeCookie(PAYING_MEMBER_COOKIE);
+    removeCookie(AD_FREE_USER_COOKIE);
+};
+
+const requestNewData = (): Promise<void> =>
+    fetchJson(`${config.page.userAttributesApiUrl}/me/features`, {
+        mode: 'cors',
+        credentials: 'include',
+    })
+        .then(persistResponse)
+        .catch(() => {});
+
+const featuresDataIsMissing = (): boolean =>
+    !getCookie(USER_FEATURES_EXPIRY_COOKIE) || !getCookie(AD_FREE_USER_COOKIE);
+
+const featuresDataIsOld = (): boolean => {
+    const featuresExpiryCookie = getCookie(USER_FEATURES_EXPIRY_COOKIE);
+    const featuresExpiryTime = parseInt(featuresExpiryCookie, 10);
+    const timeNow = new Date().getTime();
+    return timeNow >= featuresExpiryTime;
+};
+
+const userNeedsNewFeatureData = (): boolean =>
+    featuresDataIsMissing() || featuresDataIsOld();
+
+const userHasDataAfterSignout = (): boolean =>
+    !identity.isUserLoggedIn() && userHasData();
+
+/**
+     * Updates the user's data in a lazy fashion
+     */
+
+const refresh = (): Promise<void> => {
+    if (identity.isUserLoggedIn() && userNeedsNewFeatureData()) {
+        return requestNewData();
+    } else if (userHasDataAfterSignout()) {
+        deleteOldData();
+    }
+    return Promise.resolve();
+};
+
+/**
+     * Does our _existing_ data say the user is a paying member?
+     * This data may be stale; we do not wait for userFeatures.refresh()
+     * @returns {boolean}
+     * @returns {boolean}
+     */
+const isPayingMember = (): boolean =>
+    // If the user is logged in, but has no cookie yet, play it safe and assume they're a paying user
+    identity.isUserLoggedIn() && getCookie(PAYING_MEMBER_COOKIE) !== 'false';
+
+const isAdFreeUser = (): boolean => {
+    if (getCookie(AD_FREE_USER_COOKIE) === null) {
+        refresh();
+    }
+    return getCookie(AD_FREE_USER_COOKIE) === 'true';
+};
+
+export { isAdFreeUser, isPayingMember, refresh };
