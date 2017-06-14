ce3ac93d5add4ab5547d82e4b9326f4998c789b0
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-06-14 18:21:10 +0100

    Add tests for JavascriptRenderer

---
+++ article/test/JavascriptRendererTest.scala
@@ -0,0 +1,29 @@
+import java.io.FileNotFoundException
+
+import helpers.ExceptionMatcher
+import org.scalatest.{FlatSpec, Matchers}
+import play.api.libs.json.{JsObject, JsString}
+import services.JavascriptRenderer
+import test.WithTestContext
+
+import scala.util.Try
+
+class JavascriptRendererTest
+  extends FlatSpec
+  with Matchers
+  with WithTestContext
+  with ExceptionMatcher {
+
+  "Rendering" should "return correct HTML string" in {
+    val rendered = new JavascriptRenderer("js/TestButtonComponent.js")
+    val state: Option[JsObject] = Some(JsObject(Seq("title" -> JsString("my title"))))
+    rendered.render(state) should be (Try("<button type='button'>my title</button>"))
+  }
+
+  it should "fail if javascript file doesn't exist" in {
+    val rendered = new JavascriptRenderer("does-not-exists.js")
+    rendered.render() should failAs(classOf[FileNotFoundException])
+  }
+
+
+}
