e79aa572746175437fd2e7a9b2696a346a969fd5
Kate Whalen <whalenka88@gmail.com>
2017-06-15 11:22:25 +0100

    transform projects/commercial/modules/messenger/get-stylesheet.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/messenger/get-stylesheet.js
+++ static/src/javascripts/projects/commercial/modules/messenger/get-stylesheet.js
@@ -1,34 +1,31 @@
-define([
-    'commercial/modules/messenger'
-], function (messenger) {
-    var aProto = Array.prototype;
+import messenger from 'commercial/modules/messenger';
+var aProto = Array.prototype;
 
-    messenger.register('get-styles', function(specs) {
-        return getStyles(specs, document.styleSheets);
-    });
-    return getStyles;
+messenger.register('get-styles', function(specs) {
+    return getStyles(specs, document.styleSheets);
+});
+export default getStyles;
 
-    function getStyles(specs, styleSheets) {
-        if (!specs || typeof specs.selector !== 'string') {
-            return null;
-        }
+function getStyles(specs, styleSheets) {
+    if (!specs || typeof specs.selector !== 'string') {
+        return null;
+    }
 
-        var i = 0;
-        var ii = styleSheets.length;
-        var result = [];
-        while (i < ii) {
-            var sheet = styleSheets[i++];
-            if (sheet.ownerNode && sheet.ownerNode.matches && sheet.ownerNode.matches(specs.selector)) {
-                if (sheet.ownerNode.tagName === 'STYLE') {
-                    result.push(sheet.ownerNode.textContent);
-                } else {
-                    result.push(aProto.reduce.call(sheet.cssRules || [], function (res, input) {
-                        return res + input.cssText;
-                    }, ''));
-                }
+    var i = 0;
+    var ii = styleSheets.length;
+    var result = [];
+    while (i < ii) {
+        var sheet = styleSheets[i++];
+        if (sheet.ownerNode && sheet.ownerNode.matches && sheet.ownerNode.matches(specs.selector)) {
+            if (sheet.ownerNode.tagName === 'STYLE') {
+                result.push(sheet.ownerNode.textContent);
+            } else {
+                result.push(aProto.reduce.call(sheet.cssRules || [], function(res, input) {
+                    return res + input.cssText;
+                }, ''));
             }
         }
-
-        return result;
     }
-});
+
+    return result;
+}
