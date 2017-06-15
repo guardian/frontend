4347d7d8e8cf65f2f77b302bd141f7b6c7fc54fb
GHaberis <george.haberis@theguardian.com>
2017-06-15 17:24:00 +0100

    convert audience.js to es6

--- static/src/javascripts/projects/admin/modules/abtests/audience.js
+++ static/src/javascripts/projects/admin/modules/abtests/audience.js
@@ -1,39 +1,35 @@
+// @flow
 /*
  Module: audience.js
  Description: Displays information about how the test users are divided.
  */
 import Component from 'common/modules/component';
-import AudienceItem from 'admin/modules/abtests/audience-item';
-import assign from 'lodash/objects/assign';
-import clone from 'lodash/objects/clone';
+import { AudienceItem } from 'admin/modules/abtests/audience-item';
 
-function Audience(config) {
-    this.config = assign(clone(this.config), config);
+class Audience extends Component {
+    constructor(config: any): void {
+        super();
+
+        this.templateName = 'audience-template';
+        this.componentClass = 'audience-breakdown';
+        this.useBem = true;
+        this.config = Object.assign(
+            {
+                tests: [],
+            },
+            config
+        );
+    }
+
+    prerender(): void {
+        const testsContainer = this.getElem('tests');
+
+        this.config.tests.forEach(test => {
+            new AudienceItem({
+                test,
+            }).render(testsContainer);
+        });
+    }
 }
 
-Component.define(Audience);
-
-Audience.prototype.config = {
-    tests: []
-};
-
-Audience.prototype.templateName = 'audience-template';
-Audience.prototype.componentClass = 'audience-breakdown';
-Audience.prototype.useBem = true;
-
-Audience.prototype.prerender = function() {
-
-    var testsContainer = this.getElem('tests');
-
-    this.config.tests.forEach(function(test) {
-        new AudienceItem({
-            test: test
-        }).render(testsContainer);
-    });
-};
-
-Audience.prototype.ready = function() {
-
-};
-
-export default Audience;
+export { Audience };
