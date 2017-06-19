84da1a3cfb573a21a2957f505a3c20db5b912dfb
Kate Whalen <whalenka88@gmail.com>
2017-06-19 12:10:12 +0100

    Fix the test

--- static/src/javascripts/projects/commercial/modules/messenger/dfp-origin.spec.js
+++ static/src/javascripts/projects/commercial/modules/messenger/dfp-origin.spec.js
@@ -8,7 +8,7 @@ describe('Cross-frame messenger: dfpOrigin', () => {
 
     // Scripts coming from DFP _NEED_ to have access to this as a DEFAULT EXPORT
     it('should have the URL as the default export', () => {
-        const url = `${location.protocol}://tpc.googlesyndication.com`;
+        const url = `${location.protocol}//tpc.googlesyndication.com`;
         expect(dfpOrigin).toBe(url);
     });
 });
