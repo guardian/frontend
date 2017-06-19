b08fc2ce1669963de3be9c6ff107e6e26c7bb121
Kate Whalen <whalenka88@gmail.com>
2017-06-19 11:21:38 +0100

    Complete conversion of dfp-origin, and add some tests for the exported const

--- static/src/javascripts/projects/commercial/modules/messenger/dfp-origin.js
+++ static/src/javascripts/projects/commercial/modules/messenger/dfp-origin.js
@@ -1,1 +1,6 @@
-export default location.protocol + '//tpc.googlesyndication.com';
+// @flow
+
+/* eslint-disable */
+
+// this HAS to be a default export; the scripts from DFP expect it to be there
+export default `${location.protocol}//tpc.googlesyndication.com`;
