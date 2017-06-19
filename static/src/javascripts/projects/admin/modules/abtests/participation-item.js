9014bc13b780eddd25d044188b7b502edde325dd
GHaberis <george.haberis@theguardian.com>
2017-06-19 16:55:44 +0100

    transform projects/admin/modules/abtests/participation-item.js to ES6 module

--- static/src/javascripts/projects/admin/modules/abtests/participation-item.js
+++ static/src/javascripts/projects/admin/modules/abtests/participation-item.js
@@ -2,37 +2,31 @@
  Module: participation-item.js
  Description: Displays opt-in link for a variant
  */
-define([
-    'common/modules/component',
-    'lodash/objects/assign'
-], function (
-    Component,
-    assign
-) {
+import Component from 'common/modules/component';
+import assign from 'lodash/objects/assign';
 
-    function ParticipationItem(config) {
-        this.config = assign(this.config, config);
-    }
+function ParticipationItem(config) {
+    this.config = assign(this.config, config);
+}
 
-    Component.define(ParticipationItem);
+Component.define(ParticipationItem);
 
-    ParticipationItem.prototype.config = {
-        test: '',
-        examplePath: '',
-        variant: ''
-    };
+ParticipationItem.prototype.config = {
+    test: '',
+    examplePath: '',
+    variant: ''
+};
 
-    ParticipationItem.prototype.templateName = 'participation-item-template';
-    ParticipationItem.prototype.componentClass = 'participation-item';
-    ParticipationItem.prototype.classes = {};
-    ParticipationItem.prototype.useBem = true;
+ParticipationItem.prototype.templateName = 'participation-item-template';
+ParticipationItem.prototype.componentClass = 'participation-item';
+ParticipationItem.prototype.classes = {};
+ParticipationItem.prototype.useBem = true;
 
-    ParticipationItem.prototype.prerender = function () {
-        var origin = /gutools.co.uk$/.test(document.location.origin) ? 'http://www.theguardian.com' : document.location.origin,
-            href = this.config.examplePath + '=' + this.config.variant;
-        this.elem.textContent = this.config.variant;
-        this.elem.href = origin + href;
-    };
+ParticipationItem.prototype.prerender = function() {
+    var origin = /gutools.co.uk$/.test(document.location.origin) ? 'http://www.theguardian.com' : document.location.origin,
+        href = this.config.examplePath + '=' + this.config.variant;
+    this.elem.textContent = this.config.variant;
+    this.elem.href = origin + href;
+};
 
-    return ParticipationItem;
-});
+export default ParticipationItem;
