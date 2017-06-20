1be8909f4925e2a0845f8863bde3555b004b24a6
Kate Whalen <whalenka88@gmail.com>
2017-06-20 10:56:18 +0100

    Disable only the default export rule, to make the intention clearer

--- static/src/javascripts/projects/commercial/modules/messenger/dfp-origin.js
+++ static/src/javascripts/projects/commercial/modules/messenger/dfp-origin.js
@@ -1,6 +1,5 @@
 // @flow
 
-/* eslint-disable */
-
 // this HAS to be a default export; the scripts from DFP expect it to be there
+/* eslint-disable guardian-frontend/no-default-export */
 export default `${location.protocol}//tpc.googlesyndication.com`;
