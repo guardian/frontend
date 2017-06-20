f8a2c82a83a94b0c46c5c1e5acd58a578dd01253
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-06-20 12:39:24 +0100

    Go with the flow

--- static/src/javascripts/projects/common/modules/navigation/user-account.js
+++ static/src/javascripts/projects/common/modules/navigation/user-account.js
@@ -2,7 +2,7 @@
 
 import fastdom from 'fastdom';
 import { getUserFromCookie, isUserLoggedIn } from 'common/modules/identity/api';
-import { getActive as getActiveUserAvatar } from 'common/modules/avatar/api';
+import avatarAPI from 'common/modules/avatar/api';
 
 const updateCommentLink = (): void => {
     const commentItem = document.querySelector('.js-show-comment-activity');
@@ -29,16 +29,19 @@ const enhanceAvatar = (): Promise<void> => {
     );
     const avatarEl = document.querySelector('.js-navigation-account-avatar');
 
-    getActiveUserAvatar().then(res => {
+    return avatarAPI.getActive().then(res => {
         if (res && res.data && res.data.avatarUrl) {
             return fastdom.write(() => {
-                fallback.classList.add('u-h');
-                avatarEl.setAttribute('href', res.data.avatarUrl);
-                avatarEl.classList.remove('u-h');
+                if (fallback) {
+                    fallback.classList.add('u-h');
+                }
+
+                if (avatarEl) {
+                    avatarEl.setAttribute('href', res.data.avatarUrl);
+                    avatarEl.classList.remove('u-h');
+                }
             });
         }
-
-        return Promise.resolve();
     });
 };
 
