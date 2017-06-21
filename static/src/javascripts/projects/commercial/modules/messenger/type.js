706d38131a60b8f2584f4c21a5d70436e184e078
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-06-21 13:30:55 +0100

    transform projects/commercial/modules/messenger/type.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/messenger/type.js
+++ static/src/javascripts/projects/commercial/modules/messenger/type.js
@@ -1,16 +1,13 @@
-define([
-    'lib/fastdom-promise',
-    'commercial/modules/messenger'
-], function (fastdom, messenger) {
-    messenger.register('type', function(specs, ret, iframe) {
-        return setType(specs, iframe.closest('.js-ad-slot'));
-    });
-
-    return setType;
-
-    function setType(type, adSlot) {
-        return fastdom.write(function () {
-            adSlot.classList.add('ad-slot--' + type);
-        });
-    }
+import fastdom from 'lib/fastdom-promise';
+import messenger from 'commercial/modules/messenger';
+messenger.register('type', function(specs, ret, iframe) {
+    return setType(specs, iframe.closest('.js-ad-slot'));
 });
+
+export default setType;
+
+function setType(type, adSlot) {
+    return fastdom.write(function() {
+        adSlot.classList.add('ad-slot--' + type);
+    });
+}
