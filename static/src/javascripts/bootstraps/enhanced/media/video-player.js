8978cac221ea0e5a36d991bbf43e1e2ca2a7a0e3
Simon Adcock <simon.adcock@guardian.co.uk>
2017-06-22 14:24:25 +0100

    add flow header comment

--- static/src/javascripts/bootstraps/enhanced/media/video-player.js
+++ static/src/javascripts/bootstraps/enhanced/media/video-player.js
@@ -1,7 +1,10 @@
+// @flow
+
 import video from 'videojs';
 import 'videojs-ima';
 import 'videojs-embed';
 import 'videojs-persistvolume';
 import 'videojs-playlist';
 import 'videojs-contrib-ads';
+
 export default video;
