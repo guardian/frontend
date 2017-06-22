db1057fcf49e2d12bcc267b3ea698228583d6d3a
Philip Wills <philip.wills@guardian.co.uk>
2017-06-22 16:52:19 +0100

    transform projects/facia/modules/ui/live-blog-updates.js content to ES6

--- static/src/javascripts/projects/facia/modules/ui/live-blog-updates.js
+++ static/src/javascripts/projects/facia/modules/ui/live-blog-updates.js
@@ -1,3 +1,4 @@
+// @flow
 import bonzo from 'bonzo';
 import relativeDates from 'common/modules/ui/relativedates';
 import $ from 'lib/$';
@@ -5,137 +6,176 @@ import detect from 'lib/detect';
 import fastdomPromise from 'lib/fastdom-promise';
 import fetchJson from 'lib/fetch-json';
 import mediator from 'lib/mediator';
-import storage from 'lib/storage';
+import { session } from 'lib/storage';
 import template from 'lodash/utilities/template';
 import blockTemplate from 'raw-loader!facia/views/liveblog-block.html';
-import compact from 'lodash/arrays/compact';
 import isUndefined from 'lodash/objects/isUndefined';
-import forEach from 'lodash/collections/forEach';
 import debounce from 'lodash/functions/debounce';
-import filter from 'lodash/collections/filter';
 import isEmpty from 'lodash/objects/isEmpty';
-var animateDelayMs = 2000,
-    animateAfterScrollDelayMs = 500,
-    refreshSecs = 30,
-    refreshDecay = 1,
-    refreshMaxTimes = 5,
 
-    selector = '.js-liveblog-blocks',
-    articleIdAttribute = 'data-article-id',
-    sessionStorageKey = 'gu.liveblog.block-dates',
+const animateDelayMs = 2000;
+const animateAfterScrollDelayMs = 500;
+let refreshSecs = 30;
+const refreshDecay = 1;
+let refreshMaxTimes = 5;
+const selector = '.js-liveblog-blocks';
+const articleIdAttribute = 'data-article-id';
+const sessionStorageKey = 'gu.liveblog.block-dates';
+const viewportHeightPx = detect.getViewport().height;
 
-    veiwportHeightPx = detect.getViewport().height;
+type Block = {
+    id: string,
+    title: string,
+    publishedDateTime: number,
+    lastUpdatedDateTime: number,
+    body: string,
+    isNew: boolean, // This is not pulled in from the response, but mutated in this module
+};
 
-function blockRelativeTime(block) {
-    var pubDate = (block || {}).publishedDateTime,
-        relDate = pubDate ? relativeDates.makeRelativeDate(new Date(pubDate)) : false;
+const blockRelativeTime = (block: Block): string => {
+    const pubDate = (block || {}).publishedDateTime;
+    const relDate = pubDate
+        ? relativeDates.makeRelativeDate(new Date(pubDate))
+        : false;
 
     return relDate || '';
-}
+};
 
-function renderBlock(articleId, block, index) {
-    var relTime = blockRelativeTime(block);
+const renderBlock = (
+    articleId: string,
+    block: Block,
+    index: number
+): string => {
+    let relTime = blockRelativeTime(block);
 
     if (relTime.match(/yesterday/i)) {
         relTime = relTime.toLowerCase();
     } else if (relTime) {
-        relTime = 'Latest update ' + relTime + ' ago';
+        relTime = `Latest update ${relTime} ago`;
     } else {
         relTime = 'Updated just now';
     }
 
     return template(blockTemplate, {
         ariaHidden: !block.isNew,
-        href: '/' + articleId + '#' + block.id,
+        href: `/${articleId}#${block.id}`,
         relativeTime: relTime,
-        text: compact([block.title, block.body.slice(0, 500)]).join('. '),
-        index: index + 1
+        text: [block.title, block.body.slice(0, 500)]
+            .filter(x => x != null)
+            .join('. '),
+        index: index + 1,
     });
-}
+};
 
-function showBlocks(articleId, targets, blocks, oldBlockDate) {
-    var fakeUpdate = isUndefined(oldBlockDate);
+const maybeAnimateBlocks = (
+    el: Element,
+    immediate?: boolean
+): Promise<boolean> =>
+    fastdomPromise
+        .read(() => el.getBoundingClientRect().top)
+        .then(vPosition => {
+            if (vPosition > 0 && vPosition < viewportHeightPx) {
+                setTimeout(() => {
+                    const $el = bonzo(el);
 
+                    fastdomPromise.write(() => {
+                        $el.removeClass(
+                            'fc-item__liveblog-blocks__inner--offset'
+                        );
+                    });
+                }, immediate ? 0 : animateDelayMs);
+                return true;
+            }
+            return false;
+        });
 
-    forEach(targets, function(element) {
-        var hasNewBlock = false,
-            wrapperClasses = [
-                'fc-item__liveblog-blocks__inner',
-                'u-faux-block-link__promote'
-            ],
-            blocksHtml = blocks.slice(0, 2).map(function(block, index) {
-                if (!hasNewBlock && (block.publishedDateTime > oldBlockDate || fakeUpdate)) {
+const animateBlocks = (el: Element): void => {
+    maybeAnimateBlocks(el).then(didAnimate => {
+        let animateOnScroll;
+
+        if (!didAnimate) {
+            animateOnScroll = debounce(() => {
+                maybeAnimateBlocks(el, true).then(animated => {
+                    if (animated) {
+                        mediator.off('window:throttledScroll', animateOnScroll);
+                    }
+                });
+            }, animateAfterScrollDelayMs);
+
+            mediator.on('window:throttledScroll', animateOnScroll);
+        }
+    });
+};
+
+const showBlocks = (
+    articleId: string,
+    targets: Array<Element>,
+    blocks: Array<Block>,
+    oldBlockDate: number
+): void => {
+    const fakeUpdate = isUndefined(oldBlockDate);
+
+    targets.forEach(element => {
+        let hasNewBlock = false;
+
+        const wrapperClasses = [
+            'fc-item__liveblog-blocks__inner',
+            'u-faux-block-link__promote',
+        ];
+
+        const blocksHtml = blocks
+            .slice(0, 2)
+            .map((block, index) => {
+                if (
+                    !hasNewBlock &&
+                    (block.publishedDateTime > oldBlockDate || fakeUpdate)
+                ) {
                     block.isNew = true;
                     hasNewBlock = true;
-                    wrapperClasses.push('fc-item__liveblog-blocks__inner--offset');
+                    wrapperClasses.push(
+                        'fc-item__liveblog-blocks__inner--offset'
+                    );
                 }
                 return renderBlock(articleId, block, index);
-            }).slice(0, hasNewBlock ? 2 : 1),
-            el = bonzo.create(
-                '<div class="' + wrapperClasses.join(' ') + '">' + blocksHtml.join('') + '</div>'
-            ),
-            $element = bonzo(element);
+            })
+            .slice(0, hasNewBlock ? 2 : 1);
 
-        fastdomPromise.write(function() {
+        const el = bonzo.create(
+            `<div class="${wrapperClasses.join(' ')}">${blocksHtml.join(
+                ''
+            )}</div>`
+        );
+
+        const $element = bonzo(element);
+
+        fastdomPromise
+            .write(() => {
                 $element.empty().append(el);
             })
-            .then(function() {
+            .then(() => {
                 if (hasNewBlock) {
                     animateBlocks(el[0]);
                 }
             });
     });
-}
+};
 
-function animateBlocks(el) {
-    maybeAnimateBlocks(el)
-        .then(function(didAnimate) {
-            var animateOnScroll;
+const sanitizeBlocks = (blocks: Array<Block>): Array<Block> =>
+    blocks.filter(
+        block =>
+            block.id &&
+            block.publishedDateTime &&
+            block.body &&
+            block.body.length >= 10
+    );
 
-            if (!didAnimate) {
-                animateOnScroll = debounce(function() {
-                    maybeAnimateBlocks(el, true).then(function(didAnimate) {
-                        if (didAnimate) {
-                            mediator.off('window:throttledScroll', animateOnScroll);
-                        }
-                    });
-                }, animateAfterScrollDelayMs);
+const showUpdatesFromLiveBlog = (): Promise<void> =>
+    fastdomPromise
+        .read(() => {
+            const elementsById = {};
 
-                mediator.on('window:throttledScroll', animateOnScroll);
-            }
-        });
-}
-
-function maybeAnimateBlocks(el, immediate) {
-    return fastdomPromise.read(function() {
-            return el.getBoundingClientRect().top;
-        })
-        .then(function(vPosition) {
-            if (vPosition > 0 && vPosition < veiwportHeightPx) {
-                setTimeout(function() {
-                    var $el = bonzo(el);
-
-                    fastdomPromise.write(function() {
-                        $el.removeClass('fc-item__liveblog-blocks__inner--offset');
-                    });
-                }, immediate ? 0 : animateDelayMs);
-                return true;
-            }
-        });
-}
-
-function sanitizeBlocks(blocks) {
-    return filter(blocks, function(block) {
-        return block.id && block.publishedDateTime && block.body && block.body.length >= 10;
-    });
-}
-
-function show() {
-    return fastdomPromise.read(function() {
-            var elementsById = {};
-
-            $(selector).each(function(element) {
-                var articleId = element.getAttribute(articleIdAttribute);
+            $(selector).each(element => {
+                const articleId = element.getAttribute(articleIdAttribute);
 
                 if (articleId) {
                     elementsById[articleId] = elementsById[articleId] || [];
@@ -144,40 +184,43 @@ function show() {
             });
             return elementsById;
         })
-        .then(function(elementsById) {
-            var oldBlockDates;
+        .then(elementsById => {
+            let oldBlockDates;
 
             if (!isEmpty(elementsById)) {
-                oldBlockDates = storage.session.get(sessionStorageKey) || {};
+                oldBlockDates = session.get(sessionStorageKey) || {};
 
-                forEach(elementsById, function(elements, articleId) {
-                    fetchJson('/' + articleId + '.json?rendered=false', {
-                            mode: 'cors'
-                        })
-                        .then(function(response) {
-                            var blocks = response && sanitizeBlocks(response.blocks);
+                elementsById.forEach((elements, articleId) => {
+                    fetchJson(`/${articleId}.json?rendered=false`, {
+                        mode: 'cors',
+                    })
+                        .then(response => {
+                            const blocks =
+                                response && sanitizeBlocks(response.blocks);
 
                             if (blocks && blocks.length) {
-                                showBlocks(articleId, elements, blocks, oldBlockDates[articleId]);
-                                oldBlockDates[articleId] = blocks[0].publishedDateTime;
-                                storage.session.set(sessionStorageKey, oldBlockDates);
+                                showBlocks(
+                                    articleId,
+                                    elements,
+                                    blocks,
+                                    oldBlockDates[articleId]
+                                );
+                                oldBlockDates[articleId] =
+                                    blocks[0].publishedDateTime;
+                                session.set(sessionStorageKey, oldBlockDates);
                             }
                         })
-                        .catch(function() {});
+                        .catch(() => {});
                 });
 
                 if (refreshMaxTimes) {
                     refreshMaxTimes -= 1;
-                    setTimeout(function() {
-                        show();
+                    setTimeout(() => {
+                        showUpdatesFromLiveBlog();
                     }, refreshSecs * 1000);
-                    refreshSecs = refreshSecs * refreshDecay;
+                    refreshSecs *= refreshDecay;
                 }
             }
-
         });
-}
 
-export default {
-    show: show
-};
+export { showUpdatesFromLiveBlog };
