9d213191a22bc03cafa572720ebc39750d9f5f0a
GHaberis <george.haberis@theguardian.com>
2017-06-26 11:26:25 +0100

    transform projects/common/modules/media/videojs-plugins/fullscreener.js to ES6 module

--- static/src/javascripts/projects/common/modules/media/videojs-plugins/fullscreener.js
+++ static/src/javascripts/projects/common/modules/media/videojs-plugins/fullscreener.js
@@ -1,44 +1,39 @@
-define([
-    'bean',
-    'bonzo'
-], function (
-    bean,
-    bonzo
-) {
-    function fullscreener() {
-        var player = this,
-            clickbox = bonzo.create('<div class="vjs-fullscreen-clickbox"></div>')[0],
-            events = {
-                click: function (e) {
-                    if (this.paused()) {
-                        this.play();
-                    } else {
-                        this.pause();
-                    }
-                    e.stop();
-                },
-                dblclick: function (e) {
-                    e.stop();
-                    if (this.isFullscreen()) {
-                        this.exitFullscreen();
-                    } else {
-                        this.requestFullscreen();
-                    }
+import bean from 'bean';
+import bonzo from 'bonzo';
+
+function fullscreener() {
+    var player = this,
+        clickbox = bonzo.create('<div class="vjs-fullscreen-clickbox"></div>')[0],
+        events = {
+            click: function(e) {
+                if (this.paused()) {
+                    this.play();
+                } else {
+                    this.pause();
                 }
-            };
-
-        bonzo(clickbox)
-            .appendTo(player.contentEl());
-
-        bean.on(clickbox, 'click', events.click.bind(player));
-        bean.on(clickbox, 'dblclick', events.dblclick.bind(player));
-
-        player.on('fullscreenchange', function () {
-            if (this.isFullscreen()) {
-                player.trigger('player:fullscreen');
+                e.stop();
+            },
+            dblclick: function(e) {
+                e.stop();
+                if (this.isFullscreen()) {
+                    this.exitFullscreen();
+                } else {
+                    this.requestFullscreen();
+                }
             }
-        });
-    }
+        };
 
-    return fullscreener;
-});
+    bonzo(clickbox)
+        .appendTo(player.contentEl());
+
+    bean.on(clickbox, 'click', events.click.bind(player));
+    bean.on(clickbox, 'dblclick', events.dblclick.bind(player));
+
+    player.on('fullscreenchange', function() {
+        if (this.isFullscreen()) {
+            player.trigger('player:fullscreen');
+        }
+    });
+}
+
+export default fullscreener;
