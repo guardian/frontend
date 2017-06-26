5a70eca8fd3b9ab77a8c1f7f2fcad731b35468b6
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-06-26 09:53:59 +0100

    add additional class when pageskin is active (#17260)

---
+++ static/src/javascripts/projects/commercial/modules/creatives/page-skin.js
@@ -0,0 +1,33 @@
+// @flow
+import config from 'lib/config';
+import detect from 'lib/detect';
+import mediator from 'lib/mediator';
+import fastdom from 'fastdom';
+
+const pageSkin = (): void => {
+    const bodyEl = document.body;
+    const hasPageSkin: boolean = config.page.hasPageSkin;
+
+    const togglePageSkinActiveClass = (): void => {
+        if (bodyEl) {
+            fastdom.write(() => {
+                bodyEl.classList.toggle(
+                    'has-active-pageskin',
+                    detect.isBreakpoint({ min: 'wide' })
+                );
+            });
+        }
+    };
+
+    const togglePageSkin = (): void => {
+        if (hasPageSkin && detect.hasCrossedBreakpoint(true)) {
+            togglePageSkinActiveClass();
+        }
+    };
+
+    togglePageSkin();
+
+    mediator.on('window:throttledResize', togglePageSkin);
+};
+
+export { pageSkin };
