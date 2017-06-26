fb0d5578697d3e9694631c8e1cbcdf2cac57f907
Jon Soul <jonathan.soul@guardian.co.uk>
2017-06-26 12:02:31 +0100

    A simpler way to make the compiler happy

--- article/app/model/structuredData/Image.scala
+++ article/app/model/structuredData/Image.scala
@@ -1,17 +1,22 @@
 package model.structuredData
 
 import model.ImageElement
-import play.api.libs.json.{JsNumber, JsString, JsValue, Json}
+import play.api.libs.json.{JsValue, Json}
 import views.support.Item700
 
 object Image {
 
-  def apply(picture: ImageElement): JsValue =
+  def apply(picture: ImageElement): JsValue = {
+    val url = Item700.bestSrcFor(picture.images).getOrElse("")
+    val height = Item700.trueHeightFor(picture.images).getOrElse(0)
+    val width = Item700.trueWidthFor(picture.images).getOrElse(0)
+
     Json.obj(
       "@type" -> "ImageObject",
-      "url" -> JsString(Item700.bestSrcFor(picture.images).getOrElse("")),
-      "height" -> JsNumber(BigDecimal(Item700.trueHeightFor(picture.images).getOrElse(0))),
-      "width" -> JsNumber(BigDecimal(Item700.trueWidthFor(picture.images).getOrElse(0)))
+      "url" -> url,
+      "height" -> height,
+      "width" -> width
     )
+  }
 
 }
