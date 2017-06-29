0c3fee8b1372418b1bb52179160344dc288a0ae8
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-06-29 13:22:56 +0100

    transform projects/common/modules/video/more-in-series-container.js to ES6 module

--- static/src/javascripts/projects/common/modules/video/more-in-series-container.js
+++ static/src/javascripts/projects/common/modules/video/more-in-series-container.js
@@ -1,29 +1,24 @@
-define([
-    'common/modules/component',
-    'lib/mediator',
-    'common/modules/atoms/youtube'
-], function(
-    Component,
-    mediator,
-    youtube
-) {
+import Component from 'common/modules/component';
+import mediator from 'lib/mediator';
+import youtube from 'common/modules/atoms/youtube';
 
-    function init(el, mediaType, section, shortUrl, series) {
-        var component = new Component();
-        var endpoint  = '/' + mediaType + '/section/' + section +
-                        (series ? '/' + series : '') +
-                        '.json?shortUrl=' + shortUrl +
-                        // exclude professional network content from video pages
-                        (mediaType === 'video' ? '&exclude-tag=guardian-professional/guardian-professional' : '');
+function init(el, mediaType, section, shortUrl, series) {
+    var component = new Component();
+    var endpoint = '/' + mediaType + '/section/' + section +
+        (series ? '/' + series : '') +
+        '.json?shortUrl=' + shortUrl +
+        // exclude professional network content from video pages
+        (mediaType === 'video' ? '&exclude-tag=guardian-professional/guardian-professional' : '');
 
-        component.endpoint = endpoint;
+    component.endpoint = endpoint;
 
-        component.fetch(el).then(function () {
-            youtube.checkElemsForVideos(el);
-            mediator.emit('page:media:moreinloaded', el);
-            mediator.emit('page:new-content', el);
-        });
-    }
+    component.fetch(el).then(function() {
+        youtube.checkElemsForVideos(el);
+        mediator.emit('page:media:moreinloaded', el);
+        mediator.emit('page:new-content', el);
+    });
+}
 
-    return { init: init };
-});
+export default {
+    init: init
+};
