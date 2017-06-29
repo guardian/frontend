0f11c80a1fd230e71684114e701f8b01012789de
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-06-29 14:06:51 +0100

    Transformation of more-in-series-container.

--- static/src/javascripts/projects/common/modules/video/more-in-series-container.js
+++ static/src/javascripts/projects/common/modules/video/more-in-series-container.js
@@ -1,24 +1,31 @@
+// @flow
 import Component from 'common/modules/component';
 import mediator from 'lib/mediator';
-import youtube from 'common/modules/atoms/youtube';
+import { checkElemsForVideos } from 'common/modules/atoms/youtube';
 
-function init(el, mediaType, section, shortUrl, series) {
-    var component = new Component();
-    var endpoint = '/' + mediaType + '/section/' + section +
-        (series ? '/' + series : '') +
-        '.json?shortUrl=' + shortUrl +
-        // exclude professional network content from video pages
-        (mediaType === 'video' ? '&exclude-tag=guardian-professional/guardian-professional' : '');
+const moreInSeriesContainerInit = (
+    el: HTMLElement,
+    mediaType: string,
+    section: string,
+    shortUrl: string,
+    series: string
+): void => {
+    const component = new Component();
+    const endpoint = `/${mediaType}/section/${section}${series
+        ? `/${series}`
+        : ''}.json?shortUrl=${shortUrl
+    // exclude professional network content from video pages
+    }${mediaType === 'video'
+        ? '&exclude-tag=guardian-professional/guardian-professional'
+        : ''}`;
 
     component.endpoint = endpoint;
 
-    component.fetch(el).then(function() {
-        youtube.checkElemsForVideos(el);
+    component.fetch(el).then(() => {
+        checkElemsForVideos([el]);
         mediator.emit('page:media:moreinloaded', el);
         mediator.emit('page:new-content', el);
     });
-}
-
-export default {
-    init: init
 };
+
+export { moreInSeriesContainerInit };
