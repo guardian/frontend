18b098f8b721003e43b7aee42acfad9e708ff525
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-06-30 17:51:45 +0100

    Only queue adverts that were locally discovered by fill-advert-slots

--- static/src/javascripts/projects/commercial/modules/dfp/fill-advert-slots.js
+++ static/src/javascripts/projects/commercial/modules/dfp/fill-advert-slots.js
@@ -36,7 +36,7 @@ const fillAdvertSlots = (): Promise<void> => {
         adverts.forEach((advert, index) => {
             dfpEnv.advertIds[advert.id] = currentLength + index;
         });
-        dfpEnv.adverts.forEach(queueAdvert);
+        adverts.forEach(queueAdvert);
 
         // Once Advert constructors are called, Switch can be called, if needed.
         prepareSwitchTag.maybeCallSwitch();
