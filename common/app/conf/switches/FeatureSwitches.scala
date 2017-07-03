dad504ab1501c117e29a07feb5892c1349434651
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-07-03 12:05:31 +0100

    Natalia is the new owner of explore-main-media switch

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -19,7 +19,7 @@ trait FeatureSwitches {
     SwitchGroup.Feature,
     "explore-main-media",
     "If this switch is on, Explore template will be applied to explore articles. This template is part of a Membership Explore test",
-    owners = Seq(Owner.withGithub("siadcock")),
+    owners = Seq(Owner.withGithub("NataliaLKB")),
     safeState = Off,
     sellByDate = new LocalDate(2017, 7, 25),
     exposeClientSide = true
