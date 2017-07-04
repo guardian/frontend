1d42ed4efc1866a8b88a44a7b5fdcdeea7cc8817
Alex Sanders <a@snd.rs>
2017-07-04 16:20:10 +0100

    transform bootstraps/enhanced/article-liveblog-common.js to ES6 module

--- static/src/javascripts/bootstraps/enhanced/article-liveblog-common.js
+++ static/src/javascripts/bootstraps/enhanced/article-liveblog-common.js
@@ -1,61 +1,49 @@
 /** Bootstrap for functionality common to articles and live blogs */
-define([
-    'fence',
-    'lib/$',
-    'lib/config',
-    'lib/mediator',
-    'lib/robust',
-    'common/modules/accessibility/helpers',
-    'common/modules/article/twitter',
-    'common/modules/open/cta',
-    'common/modules/ui/last-modified',
-    'common/modules/ui/rhc',
-    'common/modules/ui/selection-sharing'
-], function (
-    fence,
-    $,
-    config,
-    mediator,
-    robust,
-    accessibility,
-    twitter,
-    OpenCta,
-    lastModified,
-    rhc,
-    selectionSharing
-) {
-    function initOpenCta() {
-        if (config.switches.openCta && config.page.commentable) {
-            var openCta = new OpenCta(mediator, {
-                discussionKey: config.page.shortUrlId || ''
-            });
+import fence from 'fence';
+import $ from 'lib/$';
+import config from 'lib/config';
+import mediator from 'lib/mediator';
+import robust from 'lib/robust';
+import accessibility from 'common/modules/accessibility/helpers';
+import twitter from 'common/modules/article/twitter';
+import OpenCta from 'common/modules/open/cta';
+import lastModified from 'common/modules/ui/last-modified';
+import rhc from 'common/modules/ui/rhc';
+import selectionSharing from 'common/modules/ui/selection-sharing';
 
-            $.create('<div class="open-cta"></div>').each(function (el) {
-                openCta.fetch(el);
-                if (!config.page.isLiveBlog && !config.page.isMinuteArticle) { rhc.addComponent(el); }
-            });
-        }
-    }
+function initOpenCta() {
+    if (config.switches.openCta && config.page.commentable) {
+        var openCta = new OpenCta(mediator, {
+            discussionKey: config.page.shortUrlId || ''
+        });
 
-    function initFence() {
-        $('.fenced').each(function (el) {
-            fence.render(el);
+        $.create('<div class="open-cta"></div>').each(function(el) {
+            openCta.fetch(el);
+            if (!config.page.isLiveBlog && !config.page.isMinuteArticle) {
+                rhc.addComponent(el);
+            }
         });
     }
+}
 
-    function initTwitter() {
-        twitter.init();
-        twitter.enhanceTweets();
-    }
+function initFence() {
+    $('.fenced').each(function(el) {
+        fence.render(el);
+    });
+}
 
-    return function () {
-        robust.catchErrorsWithContext([
-            ['trail-a11y',       accessibility.shouldHideFlashingElements],
-            ['trail-article', initOpenCta],
-            ['trail-fence', initFence],
-            ['trail-twitter', initTwitter],
-            ['trail-sharing', selectionSharing.init],
-            ['trail-last-modified', lastModified]
-        ]);
-    };
-});
+function initTwitter() {
+    twitter.init();
+    twitter.enhanceTweets();
+}
+
+export default function() {
+    robust.catchErrorsWithContext([
+        ['trail-a11y', accessibility.shouldHideFlashingElements],
+        ['trail-article', initOpenCta],
+        ['trail-fence', initFence],
+        ['trail-twitter', initTwitter],
+        ['trail-sharing', selectionSharing.init],
+        ['trail-last-modified', lastModified]
+    ]);
+};
