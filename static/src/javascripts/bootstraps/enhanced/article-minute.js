50b68bf058e138151f35e4809a70cb4ab2fb72df
Alex Sanders <a@snd.rs>
2017-07-04 16:10:48 +0100

    transform       bootstraps/enhanced/article-minute.js content to ES6

--- static/src/javascripts/bootstraps/enhanced/article-minute.js
+++ static/src/javascripts/bootstraps/enhanced/article-minute.js
@@ -1,12 +1,12 @@
+// @flow
 import articleLiveblogCommon from 'bootstraps/enhanced/article-liveblog-common';
 import trail from 'bootstraps/enhanced/trail';
 import fullHeight from 'common/modules/ui/full-height';
-var ready = function() {
+
+const init = () => {
     articleLiveblogCommon();
     trail();
     fullHeight.init();
 };
 
-export default {
-    init: ready
-};
+export { init };
