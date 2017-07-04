12264172ab75c50262ec190679cdc76bc1cd652e
Alex Sanders <a@snd.rs>
2017-07-04 15:45:13 +0100

    transform bootstraps/enhanced/article.js       content to ES6

--- static/src/javascripts/bootstraps/enhanced/article.js
+++ static/src/javascripts/bootstraps/enhanced/article.js
@@ -1,63 +1,66 @@
-/*eslint-disable no-new*/
+// @flow
+/* eslint-disable no-new*/
 import qwery from 'qwery';
 import $ from 'lib/$';
 import detect from 'lib/detect';
 import mediator from 'lib/mediator';
-import urlutils from 'lib/url';
+import { getUrlVars } from 'lib/url';
 import richLinks from 'common/modules/article/rich-links';
 import membershipEvents from 'common/modules/article/membership-events';
-import geoMostPopular from 'common/modules/onward/geo-most-popular';
+import { geoMostPopular } from 'common/modules/onward/geo-most-popular';
 import quiz from 'common/modules/atoms/quiz';
 import storyQuestions from 'common/modules/atoms/story-questions';
 import articleLiveblogCommon from 'bootstraps/enhanced/article-liveblog-common';
 import trail from 'bootstraps/enhanced/trail';
 import ophan from 'ophan/ng';
-import snippetFeedback from 'projects/journalism/snippet-feedback';
-var modules = {
-        initCmpParam: function() {
-            var allvars = urlutils.getUrlVars();
+import { SnippetFeedback } from 'projects/journalism/snippet-feedback';
 
-            if (allvars.CMP) {
-                $('.element-pass-cmp').each(function(el) {
-                    el.src = el.src + '?CMP=' + allvars.CMP;
-                });
-            }
-        },
+const modules = {
+    initCmpParam() {
+        const allvars = getUrlVars();
 
-        initRightHandComponent: function() {
-            var mainColumn = qwery('.js-content-main-column');
-            // only render when we have >1000px or more (enough space for ad + most popular)
-            if (mainColumn[0] && mainColumn[0].offsetHeight > 1150 && detect.isBreakpoint({
-                    min: 'desktop'
-                })) {
-                geoMostPopular.geoMostPopular.render();
-            } else {
-                mediator.emit('modules:onward:geo-most-popular:cancel');
-            }
-        },
-
-        initQuizListeners: function() {
-            // This event is for older-style quizzes implemented as interactives. See https://github.com/guardian/quiz-builder
-            mediator.on('quiz/ophan-event', ophan.record);
+        if (allvars.CMP) {
+            $('.element-pass-cmp').each(el => {
+                el.src = `${el.src}?CMP=${allvars.CMP}`;
+            });
         }
     },
 
-    ready = function() {
-        trail();
-        articleLiveblogCommon();
-        modules.initRightHandComponent();
-        modules.initCmpParam();
-        modules.initQuizListeners();
-        richLinks.upgradeRichLinks();
-        richLinks.insertTagRichLink();
-        membershipEvents.upgradeEvents();
-        mediator.emit('page:article:ready');
-        quiz.handleCompletion();
-        storyQuestions.init();
-        snippetFeedback.SnippetFeedback();
-    };
+    initRightHandComponent() {
+        const mainColumn = qwery('.js-content-main-column');
+        // only render when we have >1000px or more (enough space for ad + most popular)
+        if (
+            mainColumn[0] &&
+            mainColumn[0].offsetHeight > 1150 &&
+            detect.isBreakpoint({
+                min: 'desktop',
+            })
+        ) {
+            geoMostPopular.render();
+        } else {
+            mediator.emit('modules:onward:geo-most-popular:cancel');
+        }
+    },
 
-export default {
-    init: ready,
-    modules: modules // exporting for LiveBlog bootstrap to use
+    initQuizListeners() {
+        // This event is for older-style quizzes implemented as interactives. See https://github.com/guardian/quiz-builder
+        mediator.on('quiz/ophan-event', ophan.record);
+    },
 };
+
+const init = () => {
+    trail();
+    articleLiveblogCommon();
+    modules.initRightHandComponent();
+    modules.initCmpParam();
+    modules.initQuizListeners();
+    richLinks.upgradeRichLinks();
+    richLinks.insertTagRichLink();
+    membershipEvents.upgradeEvents();
+    mediator.emit('page:article:ready');
+    quiz.handleCompletion();
+    storyQuestions.init();
+    SnippetFeedback();
+};
+
+export { init };
