dc48b27f13672f6e7b7482d004b9c1054fab1227
Kate Whalen <whalenka88@gmail.com>
2017-07-04 17:43:54 +0100

    Complete ES6 conversion, add Flow types, and use namespaced imports

--- static/src/javascripts/projects/commercial/modules/third-party-tags.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags.js
@@ -1,81 +1,97 @@
-/**
- * A regionalised container for all the commercial tags.
- */
+// @flow
+/* A regionalised container for all the commercial tags. */
+
 import $ from 'lib/$';
 import config from 'lib/config';
 import fastdom from 'lib/fastdom-promise';
 import template from 'lodash/utilities/template';
-import testCanRunChecks from 'common/modules/experiments/test-can-run-checks';
-import abUtils from 'common/modules/experiments/utils';
-import commercialFeatures from 'commercial/modules/commercial-features';
+import { testCanBeRun } from 'common/modules/experiments/test-can-run-checks';
+import { getTestVariantId } from 'common/modules/experiments/utils';
+import { commercialFeatures } from 'commercial/modules/commercial-features';
 import audienceScienceGateway from 'commercial/modules/third-party-tags/audience-science-gateway';
 import audienceSciencePql from 'commercial/modules/third-party-tags/audience-science-pql';
-import imrWorldwide from 'commercial/modules/third-party-tags/imr-worldwide';
-import imrWorldwideLegacy from 'commercial/modules/third-party-tags/imr-worldwide-legacy';
-import remarketing from 'commercial/modules/third-party-tags/remarketing';
-import simpleReach from 'commercial/modules/third-party-tags/simple-reach';
-import tourismAustralia from 'commercial/modules/third-party-tags/tourism-australia';
-import krux from 'commercial/modules/third-party-tags/krux';
-import outbrain from 'commercial/modules/third-party-tags/outbrain';
-import plista from 'commercial/modules/third-party-tags/plista';
 import PaidContentVsOutbrain2 from 'common/modules/experiments/tests/paid-content-vs-outbrain';
 import externalContentContainerStr from 'raw-loader!common/views/commercial/external-content.html';
 
-function loadExternalContentWidget() {
+// third-party-tags follow an API pattern with their exports, FlowTyped as
+// 'Service'. To avoid naming collisions, namespace imports are used here.
+/* eslint-disable import/no-namespace */
+import * as imrWorldwide from 'commercial/modules/third-party-tags/imr-worldwide';
+import * as imrWorldwideLegacy from 'commercial/modules/third-party-tags/imr-worldwide-legacy';
+import * as remarketing from 'commercial/modules/third-party-tags/remarketing';
+import * as simpleReach from 'commercial/modules/third-party-tags/simple-reach';
+import * as tourismAustralia from 'commercial/modules/third-party-tags/tourism-australia';
+import * as krux from 'commercial/modules/third-party-tags/krux';
+import * as outbrain from 'commercial/modules/third-party-tags/outbrain';
+import * as plista from 'commercial/modules/third-party-tags/plista';
 
-    var externalTpl = template(externalContentContainerStr);
+type Service = {
+    shouldRun: boolean,
+    url: string,
+    onLoad?: () => any,
+    useImage?: boolean,
+};
 
-    function findAnchor() {
-        var selector = !(config.page.seriesId || config.page.blogIds) ?
-            '.js-related, .js-outbrain-anchor' :
-            '.js-outbrain-anchor';
+const isLuckyBastard = (): boolean =>
+    testCanBeRun(PaidContentVsOutbrain2) &&
+    getTestVariantId(PaidContentVsOutbrain2.id) === 'paid-content';
+
+const loadExternalContentWidget = (): void => {
+    const externalTpl = template(externalContentContainerStr);
+
+    const findAnchor = (): Promise<any> => {
+        const selector = !(config.page.seriesId || config.page.blogIds)
+            ? '.js-related, .js-outbrain-anchor'
+            : '.js-outbrain-anchor';
         return Promise.resolve(document.querySelector(selector));
-    }
+    };
 
-    function renderWidget(widgetType, init) {
+    const renderWidget = (widgetType, init: () => void): void => {
         findAnchor()
-            .then(function(anchorNode) {
-                return fastdom.write(function() {
-                    $(anchorNode).after(externalTpl({
-                        widgetType: widgetType
-                    }));
+            .then(anchorNode =>
+                fastdom.write(() => {
+                    $(anchorNode).after(
+                        externalTpl({
+                            widgetType,
+                        })
+                    );
                 })
-            })
+            )
             .then(init);
-    }
+    };
 
-    var shouldServePlista = config.switches.plistaForOutbrainAu && config.page.edition.toLowerCase() === 'au';
+    const shouldServePlista: boolean =
+        config.switches.plistaForOutbrainAu &&
+        config.page.edition.toLowerCase() === 'au';
 
     if (shouldServePlista) {
         renderWidget('plista', plista.default.init);
     } else {
         renderWidget('outbrain', outbrain.init);
     }
-}
+};
 
-function init() {
-
-    if (!commercialFeatures.commercialFeatures.thirdPartyTags) {
-        return Promise.resolve(false);
+const insertScripts = (services: Array<Service>): void => {
+    const ref = document.scripts[0];
+    const frag = document.createDocumentFragment();
+    while (services.length) {
+        const service = services.shift();
+        if (service.useImage) {
+            new Image().src = service.url;
+        } else {
+            const script = document.createElement('script');
+            script.src = service.url;
+            script.onload = service.onLoad;
+            frag.appendChild(script);
+        }
     }
-
-    // Outbrain/Plista needs to be loaded before first ad as it is checking for presence of high relevance component on page
-    if (!isLuckyBastard()) {
-        loadExternalContentWidget();
+    if (ref.parentNode) {
+        ref.parentNode.insertBefore(frag, ref);
     }
+};
 
-    loadOther();
-
-    return Promise.resolve(true);
-}
-
-function isLuckyBastard() {
-    return testCanRunChecks.testCanBeRun(PaidContentVsOutbrain2) &&
-        abUtils.getTestVariantId(PaidContentVsOutbrain2.id) === 'paid-content';
-}
-
-function loadOther() {
-    var services = [
+const loadOther = (): void => {
+    const services = [
         audienceSciencePql,
         audienceScienceGateway,
         imrWorldwide,
@@ -83,33 +99,28 @@ function loadOther() {
         remarketing,
         simpleReach,
         tourismAustralia,
-        krux
-    ].filter(function(_) {
-        return _.shouldRun;
-    });
+        krux,
+    ].filter(_ => _.shouldRun);
 
     if (services.length) {
         insertScripts(services);
     }
-}
-
-function insertScripts(services) {
-    var ref = document.scripts[0];
-    var frag = document.createDocumentFragment();
-    while (services.length) {
-        var service = services.shift();
-        if (service.useImage) {
-            new Image().src = service.url;
-        } else {
-            var script = document.createElement('script');
-            script.src = service.url;
-            script.onload = service.onLoad;
-            frag.appendChild(script);
-        }
-    }
-    ref.parentNode.insertBefore(frag, ref);
-}
-
-export default {
-    init: init
 };
+
+const init = (): Promise<any> => {
+    if (!commercialFeatures.thirdPartyTags) {
+        return Promise.resolve(false);
+    }
+
+    // Outbrain/Plista needs to be loaded before the first ad as it is checking
+    // for the presence of high relevance component on page
+    if (!isLuckyBastard()) {
+        loadExternalContentWidget();
+    }
+
+    loadOther();
+
+    return Promise.resolve(true);
+};
+
+export { init };
