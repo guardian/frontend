f28af85da78ddc468b247ad3c25a52e7eb246c13
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-07-05 10:48:26 +0100

    Convert survey-simple.js to ES6

--- static/src/javascripts/projects/commercial/modules/survey/survey-simple.js
+++ static/src/javascripts/projects/commercial/modules/survey/survey-simple.js
@@ -1,3 +1,4 @@
+// @flow
 import fastdom from 'lib/fastdom-promise';
 import $ from 'lib/$';
 import template from 'lodash/utilities/template';
@@ -8,61 +9,67 @@ import marque36icon from 'svgs/icon/marque-36.svg';
 import crossIcon from 'svgs/icon/cross.svg';
 import paidContent from 'svgs/commercial/paid-content.svg';
 import uniq from 'lodash/arrays/uniq';
-var surveySimple = function(config) {
-    this.config = config || {};
-    this.id = this.config.id;
-    this.prefs = 'overlay-messages';
-    this.shouldClosePermanently = this.config.shouldClosePermanently || false;
-    this.bannerTmpl = template(surveySimpleTemplate, {
-        header: this.config.header,
-        paragraph1: this.config.paragraph1,
-        paragraph2: this.config.paragraph2,
-        paragraph3: this.config.paragraph3,
-        showCloseBtn: this.config.showCloseBtn,
-        arrowWhiteRight: arrowWhiteRight.markup,
-        marque36icon: marque36icon.markup,
-        crossIcon: crossIcon.markup,
-        paidContent: paidContent.markup
-    });
-};
 
-surveySimple.prototype.attach = function() {
-    if (!this.hasSeen()) {
-        return fastdom.write(function() {
-            $(document.body).append(this.bannerTmpl);
+class surveySimple {
+    constructor(config) {
+        this.config = config || {};
+        this.id = this.config.id;
+        this.prefs = 'overlay-messages';
+        this.shouldClosePermanently =
+            this.config.shouldClosePermanently || false;
+        this.bannerTmpl = template(surveySimpleTemplate, {
+            header: this.config.header,
+            paragraph1: this.config.paragraph1,
+            paragraph2: this.config.paragraph2,
+            paragraph3: this.config.paragraph3,
+            showCloseBtn: this.config.showCloseBtn,
+            arrowWhiteRight: arrowWhiteRight.markup,
+            marque36icon: marque36icon.markup,
+            crossIcon: crossIcon.markup,
+            paidContent: paidContent.markup,
+        });
+    }
 
-            if (this.config.showCloseBtn) {
-                var closeBtn = document.querySelector('.js-survey-close');
-                closeBtn.addEventListener('click', this.handleClick.bind(this));
-            }
-        }.bind(this));
-    } else {
+    attach() {
+        if (!this.hasSeen()) {
+            return fastdom.write(() => {
+                $(document.body).append(this.bannerTmpl);
+
+                if (this.config.showCloseBtn) {
+                    const closeBtn = document.querySelector('.js-survey-close');
+                    closeBtn.addEventListener(
+                        'click',
+                        this.handleClick.bind(this)
+                    );
+                }
+            });
+        }
         return Promise.resolve();
     }
-};
 
-surveySimple.prototype.handleClick = function() {
-    $('.js-survey-overlay').addClass('u-h');
-    if (this.shouldClosePermanently) {
-        this.closePermanently();
+    handleClick() {
+        $('.js-survey-overlay').addClass('u-h');
+        if (this.shouldClosePermanently) {
+            this.closePermanently();
+        }
     }
-};
 
-surveySimple.prototype.hasSeen = function() {
-    var messageStates = userPrefs.get(this.prefs);
-    return messageStates && messageStates.indexOf(this.id) > -1;
-};
+    hasSeen() {
+        const messageStates = userPrefs.get(this.prefs);
+        return messageStates && messageStates.indexOf(this.id) > -1;
+    }
 
-surveySimple.prototype.closePermanently = function() {
-    var messageStates = userPrefs.get(this.prefs) || [];
-    messageStates.push(this.id);
-    userPrefs.set(this.prefs, uniq(messageStates));
-};
+    closePermanently() {
+        const messageStates = userPrefs.get(this.prefs) || [];
+        messageStates.push(this.id);
+        userPrefs.set(this.prefs, uniq(messageStates));
+    }
 
-surveySimple.prototype.show = function() {
-    fastdom.write(function() {
-        $('.js-survey-overlay').removeClass('u-h');
-    });
-};
+    static show() {
+        fastdom.write(() => {
+            $('.js-survey-overlay').removeClass('u-h');
+        });
+    }
+}
 
 export default surveySimple;
