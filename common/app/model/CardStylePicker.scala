e4306339aef4b746177638889980d8c0558d01d1
Diego Vazquez Nanini <diego@vazqueznanini.com>
2017-07-05 15:41:54 +0100

    Refactor the function to pick SpecialReports

--- common/app/model/CardStylePicker.scala
+++ common/app/model/CardStylePicker.scala
@@ -2,20 +2,21 @@ package model
 
 import com.gu.facia.api.utils.{CardStyle, SpecialReport}
 import com.gu.facia.client.models.{MetaDataCommonFields, TrailMetaData}
-import com.gu.targeting.client.ReportFields
+import com.gu.targeting.client.{Campaign, ReportFields}
 import com.gu.contentapi.client.model.{v1 => contentapi}
 import commercial.targeting.CampaignAgent
 
 object CardStylePicker {
 
   def apply(content: contentapi.Content, trailMetaData: MetaDataCommonFields): CardStyle = {
-    specialReportFromTargeting(content) getOrElse CardStyle(content, TrailMetaData.empty)
+    extractCampaigns(content) match {
+      case Nil => CardStyle(content, TrailMetaData.empty)
+      case _ => SpecialReport
+    }
   }
 
-  private def specialReportFromTargeting(content: contentapi.Content): Option[CardStyle] = {
+  private def extractCampaigns(content: contentapi.Content): List[Campaign] = {
     CampaignAgent.getCampaignsForTags(content.tags.map(_.id))
       .filterNot(_.fields == ReportFields)
-      .map(_ => SpecialReport)
-      .headOption
   }
 }
