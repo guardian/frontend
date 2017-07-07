aa2fdd5c8a88d27d5794ecb11fbf8661a1709c64
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-07-07 15:59:20 +0100

    convert sticky-mpu.js to ES6

--- static/src/javascripts/projects/commercial/modules/sticky-mpu.js
+++ static/src/javascripts/projects/commercial/modules/sticky-mpu.js
@@ -1,53 +1,69 @@
+// @flow
 import config from 'lib/config';
 import mediator from 'lib/mediator';
 import fastdom from 'lib/fastdom-promise';
-import sticky from 'common/modules/ui/sticky';
-import messenger from 'commercial/modules/messenger';
-var noSticky = document.documentElement.classList.contains('has-no-sticky');
-var stickyElement;
-var rightSlot;
+import { Sticky } from 'common/modules/ui/sticky';
+import { register, unregister } from 'commercial/modules/messenger';
 
-function stickyMpu(adSlot) {
+const noSticky = document.documentElement.classList.contains('has-no-sticky');
+let stickyElement;
+let rightSlot;
+
+const onResize = (specs, _, iframe) => {
+    if (rightSlot.contains(iframe)) {
+        unregister('resize', onResize);
+        stickyElement.updatePosition();
+    }
+};
+
+const stickyMpu = adSlot => {
     if (adSlot.getAttribute('data-name') !== 'right') {
         return;
     }
 
     rightSlot = adSlot;
 
-    var referenceElement = document.querySelector(config.page.hasShowcaseMainElement ? '.media-primary' : '.content__article-body,.js-liveblog-body-content');
+    const referenceElement = document.querySelector(
+        config.page.hasShowcaseMainElement
+            ? '.media-primary'
+            : '.content__article-body,.js-liveblog-body-content'
+    );
     if (!referenceElement) {
         return;
     }
 
-    fastdom.read(function() {
-        return (referenceElement[config.page.hasShowcaseMainElement ? 'offsetHeight' : 'offsetTop']) + adSlot.offsetHeight;
-    }).then(function(newHeight) {
-        return fastdom.write(function() {
-            adSlot.parentNode.style.height = newHeight + 'px';
+    fastdom
+        .read(
+            () =>
+                referenceElement[
+                    config.page.hasShowcaseMainElement
+                        ? 'offsetHeight'
+                        : 'offsetTop'
+                ] + adSlot.offsetHeight
+        )
+        .then(newHeight =>
+            fastdom.write(() => {
+                adSlot.parentNode.style.height = `${newHeight}px`;
+            })
+        )
+        .then(() => {
+            if (noSticky) {
+                // if there is a sticky 'paid by' band move the sticky mpu down so it will be always visible
+                const options = config.page.isPaidContent
+                    ? {
+                          top: 43,
+                      }
+                    : {};
+                stickyElement = new Sticky(adSlot, options);
+                stickyElement.init();
+                register('resize', onResize);
+            }
+            mediator.emit('page:commercial:sticky-mpu');
         });
-    }).then(function() {
-        if (noSticky) {
-            //if there is a sticky 'paid by' band move the sticky mpu down so it will be always visible
-            var options = config.page.isPaidContent ? {
-                top: 43
-            } : {};
-            stickyElement = new sticky.Sticky(adSlot, options);
-            stickyElement.init();
-            messenger.register('resize', onResize);
-        }
-        mediator.emit('page:commercial:sticky-mpu');
-    });
-}
+};
 
-function onResize(specs, _, iframe) {
-    if (rightSlot.contains(iframe)) {
-        messenger.unregister('resize', onResize);
-        stickyElement.updatePosition();
-    }
-}
-
-stickyMpu.whenRendered = new Promise(function(resolve) {
+stickyMpu.whenRendered = new Promise(resolve => {
     mediator.on('page:commercial:sticky-mpu', resolve);
 });
 
-export default stickyMpu;
+export { stickyMpu };
