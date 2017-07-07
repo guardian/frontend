faa7def3b7ccbae71dae0edfc9a463c8a9fe5033
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2017-07-07 15:48:58 +0100

    transform projects/commercial/modules/sticky-mpu.js to ES6 module

--- static/src/javascripts/projects/commercial/modules/sticky-mpu.js
+++ static/src/javascripts/projects/commercial/modules/sticky-mpu.js
@@ -1,61 +1,53 @@
-define([
-    'lib/config',
-    'lib/mediator',
-    'lib/fastdom-promise',
-    'common/modules/ui/sticky',
-    'commercial/modules/messenger'
-], function (
-    config,
-    mediator,
-    fastdom,
-    sticky,
-    messenger
-) {
-    var noSticky = document.documentElement.classList.contains('has-no-sticky');
-    var stickyElement;
-    var rightSlot;
+import config from 'lib/config';
+import mediator from 'lib/mediator';
+import fastdom from 'lib/fastdom-promise';
+import sticky from 'common/modules/ui/sticky';
+import messenger from 'commercial/modules/messenger';
+var noSticky = document.documentElement.classList.contains('has-no-sticky');
+var stickyElement;
+var rightSlot;
 
-    function stickyMpu(adSlot) {
-        if (adSlot.getAttribute('data-name') !== 'right') {
-            return;
-        }
+function stickyMpu(adSlot) {
+    if (adSlot.getAttribute('data-name') !== 'right') {
+        return;
+    }
 
-        rightSlot = adSlot;
+    rightSlot = adSlot;
 
-        var referenceElement = document.querySelector(config.page.hasShowcaseMainElement ? '.media-primary' : '.content__article-body,.js-liveblog-body-content');
-        if (!referenceElement) {
-            return;
-        }
+    var referenceElement = document.querySelector(config.page.hasShowcaseMainElement ? '.media-primary' : '.content__article-body,.js-liveblog-body-content');
+    if (!referenceElement) {
+        return;
+    }
 
-        fastdom.read(function () {
-            return (referenceElement[config.page.hasShowcaseMainElement ? 'offsetHeight' : 'offsetTop']) + adSlot.offsetHeight;
-        }).then(function (newHeight) {
-            return fastdom.write(function () {
-                adSlot.parentNode.style.height = newHeight + 'px';
-            });
-        }).then(function () {
-            if (noSticky) {
-                //if there is a sticky 'paid by' band move the sticky mpu down so it will be always visible
-                var options = config.page.isPaidContent ? {top: 43} : {};
-                stickyElement = new sticky.Sticky(adSlot, options);
-                stickyElement.init();
-                messenger.register('resize', onResize);
-            }
-            mediator.emit('page:commercial:sticky-mpu');
+    fastdom.read(function() {
+        return (referenceElement[config.page.hasShowcaseMainElement ? 'offsetHeight' : 'offsetTop']) + adSlot.offsetHeight;
+    }).then(function(newHeight) {
+        return fastdom.write(function() {
+            adSlot.parentNode.style.height = newHeight + 'px';
         });
-    }
-
-    function onResize(specs, _, iframe) {
-        if (rightSlot.contains(iframe)) {
-            messenger.unregister('resize', onResize);
-            stickyElement.updatePosition();
+    }).then(function() {
+        if (noSticky) {
+            //if there is a sticky 'paid by' band move the sticky mpu down so it will be always visible
+            var options = config.page.isPaidContent ? {
+                top: 43
+            } : {};
+            stickyElement = new sticky.Sticky(adSlot, options);
+            stickyElement.init();
+            messenger.register('resize', onResize);
         }
-    }
-
-    stickyMpu.whenRendered = new Promise(function (resolve) {
-        mediator.on('page:commercial:sticky-mpu', resolve);
+        mediator.emit('page:commercial:sticky-mpu');
     });
+}
 
-    return stickyMpu;
+function onResize(specs, _, iframe) {
+    if (rightSlot.contains(iframe)) {
+        messenger.unregister('resize', onResize);
+        stickyElement.updatePosition();
+    }
+}
 
+stickyMpu.whenRendered = new Promise(function(resolve) {
+    mediator.on('page:commercial:sticky-mpu', resolve);
 });
+
+export default stickyMpu;
