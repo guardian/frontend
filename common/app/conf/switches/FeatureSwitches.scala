3fe3bd091be3acaa4d5e0b19619f54cf8e658c5b
Zef <zef.cherry-kynaston.casual@guardian.co.uk>
2017-07-07 11:50:40 +0100

    Wrapped it up in a switch

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -505,4 +505,14 @@ trait FeatureSwitches {
     sellByDate = never,
     exposeClientSide = false
   )
+
+  val HeaderFeedback = Switch(
+    SwitchGroup.Feature,
+    "header-feedback",
+    "When ON a feedback prompt will be visible within the header test",
+    owners = Seq(Owner.withGithub("zeftilldeath")),
+    safeState = Off,
+    sellByDate = never,
+    exposeClientSide = false
+  )
 }
