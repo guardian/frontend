e8c82d9317800d93064c34033feb5e4d71d6ac4e
Jon Norman <jon.norman@guardian.co.uk>
2017-07-11 10:39:13 +0100

    Extend video-closing switch.

--- common/app/conf/switches/CommercialSwitches.scala
+++ common/app/conf/switches/CommercialSwitches.scala
@@ -21,7 +21,7 @@ trait CommercialSwitches {
     "Deactivates the sizecallback for videos (620x1) that hides the slot.",
     owners = Seq(Owner.withGithub("JonNorman")),
     safeState = Off,
-    sellByDate = new LocalDate(2017, 7, 12),
+    sellByDate = new LocalDate(2017, 7, 26),
     exposeClientSide = true
   )
 
