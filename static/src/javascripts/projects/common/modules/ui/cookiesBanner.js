a21c8bf79a59ae90cb00259119851c00b54fe4a6
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-07-11 16:26:17 +0100

    ES6 cookiesBanner

--- static/src/javascripts/projects/common/modules/ui/cookiesBanner.js
+++ static/src/javascripts/projects/common/modules/ui/cookiesBanner.js
@@ -1,10 +1,5 @@
-import config from 'lib/config';
-import $ from 'lib/$';
-import cookies from 'lib/cookies';
-import detect from 'lib/detect';
-import storage from 'lib/storage';
-import template from 'lodash/utilities/template';
-import userPrefs from 'common/modules/user-prefs';
+// @flow
+import { getCookie, addCookie } from 'lib/cookies';
 import Message from 'common/modules/ui/message';
 import mediator from 'lib/mediator';
 /**
@@ -16,27 +11,28 @@ import mediator from 'lib/mediator';
  * Show only on FIRST page view
  * Persist close state
  */
-function init() {
-    var geoContinentCookie = cookies.getCookie('GU_geo_continent');
+const init = (): boolean => {
+    const geoContinentCookie = getCookie('GU_geo_continent');
     if (geoContinentCookie && geoContinentCookie.toUpperCase() === 'EU') {
-        var EU_COOKIE_MSG = 'GU_EU_MSG',
-            euMessageCookie = cookies.getCookie(EU_COOKIE_MSG);
-        if (!euMessageCookie || euMessageCookie != 'seen') {
-            var link = 'https://www.theguardian.com/info/cookies',
-                txt = 'Welcome to the Guardian. This site uses cookies. Read <a href="' + link + '" class="cookie-message__link">our policy</a>.',
-                opts = {
-                    important: true
-                },
-                cookieLifeDays = 365,
-                msg = new Message('cookies', opts);
+        const EU_COOKIE_MSG = 'GU_EU_MSG';
+        const euMessageCookie = getCookie(EU_COOKIE_MSG);
+        if (!euMessageCookie || euMessageCookie !== 'seen') {
+            const link = 'https://www.theguardian.com/info/cookies';
+            const txt = `Welcome to the Guardian. This site uses cookies. Read <a href="${link}" class="cookie-message__link">our policy</a>.`;
+            const opts = {
+                important: true,
+            };
+            const cookieLifeDays = 365;
+            const msg = new Message('cookies', opts);
             msg.show(txt);
-            cookies.addCookie(EU_COOKIE_MSG, 'seen', cookieLifeDays);
+            addCookie(EU_COOKIE_MSG, 'seen', cookieLifeDays);
             return true;
         }
     }
     mediator.emit('modules:ui:cookiesBanner:notShown');
-}
+    return false;
+};
 
 export default {
-    init: init
+    init,
 };
