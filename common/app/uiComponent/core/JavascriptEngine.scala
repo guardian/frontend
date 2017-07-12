1be6810ea485b17bfd9b653e33b43f2f26042d8c
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-07-12 14:03:33 +0100

    Refactor javascript compilation/evaluation and add functions not supported by Hashorn

--- common/app/uiComponent/core/JavascriptEngine.scala
+++ common/app/uiComponent/core/JavascriptEngine.scala
@@ -8,30 +8,30 @@ import jdk.nashorn.api.scripting.JSObject
 import scala.util.Try
 
 object JavascriptEngine {
+
+  type EvalResult = JSObject
+
   private val shared: ScriptEngine = new ScriptEngineManager(null).getEngineByName("nashorn")
 
   def put(name: String, value: Object)(implicit context: SimpleScriptContext): Try[Unit] =
     Try(context.getBindings(ScriptContext.ENGINE_SCOPE).put(name, value))
 
-  def eval(script: String)(implicit context: SimpleScriptContext): Try[JSObject] =
-    Try(shared.eval(script, context).asInstanceOf[JSObject])
+  def get(evaluated: EvalResult, key: String): Try[JSObject] =
+    Try(evaluated.getMember(key).asInstanceOf[JSObject])
 
-  def eval(cs: CompiledScript)(implicit context: SimpleScriptContext): Try[JSObject] =
-    Try(cs.eval(context).asInstanceOf[JSObject])
+  def eval(script: String)(implicit context: SimpleScriptContext): Try[EvalResult] =
+    Try(shared.eval(script, context).asInstanceOf[EvalResult])
 
-  def compile(reader: Reader)(implicit context: SimpleScriptContext): Try[CompiledScript] =
+  def eval(cs: CompiledScript)(implicit context: SimpleScriptContext): Try[EvalResult] =
+    Try(cs.eval(context).asInstanceOf[EvalResult])
+
+  def compile(reader: Reader): Try[CompiledScript] =
     Try(shared.asInstanceOf[Compilable].compile(reader))
 
-  def invoke(cs: CompiledScript, method: String, args: JSObject*)(implicit context: SimpleScriptContext): Try[String] = {
-    for {
-      global <- eval(cs)
-      res <- Try(
-        cs
-          .getEngine
-          .asInstanceOf[Invocable]
-          .invokeMethod(global, method, args:_*)
-          .asInstanceOf[String]
-      )
-    } yield res
+  def invoke(obj: EvalResult, method: String, args: JSObject*): Try[String] = Try {
+    shared
+      .asInstanceOf[Invocable]
+      .invokeMethod(obj, method, args:_*)
+      .asInstanceOf[String]
   }
 }
