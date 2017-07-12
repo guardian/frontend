37cb76d00d94952246611abd98d6fa8520cb11c1
Alex Sanders <a@snd.rs>
2017-07-12 10:55:17 +0100

    fix styletron race condition

--- article/resources/components/c.js
+++ article/resources/components/c.js
@@ -1,97 +1,3 @@
-var global = this;
-var self = this;
-var window = this;
-var process = {env: {}};
-var console = {};
-
-Object.assign = function (t) {
-  for (var s, i = 1, n = arguments.length; i < n; i++) {
-    s = arguments[i];
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-      t[p] = s[p];
-  }
-  return t;
-};
-
-(function consoleInit(context) {
-  var logger = context.__NASHORN_LOGGER__ || {
-      debug: print,
-      warn: print,
-      info: print,
-      error: print,
-      trace: print
-    };
-
-  console.debug = function(args){ logger.debug(args); };
-  console.warn = function(args){ logger.warn(args);};
-  console.log = function(args){ logger.info(args);};
-  console.error = function(args){ logger.error(args);};
-  console.trace = function(args){ logger.trace(args);};
-})(this);
-/*
- Source is originated from https://github.com/morungos/java-xmlhttprequest
-
- Articles about Nashorn:
- - https://blog.codecentric.de/en/2014/06/project-nashorn-javascript-jvm-polyglott/
-
- How it work:
-  in https://github.com/morungos/java-xmlhttprequest, it uses Timer to run setTimeout and setInterval task,
-  but they are run in a separate thread of the Timer creates that is different with the main JavaScript thread.
-
-  This implementation uses ScheduledExecutorService instead of Timer so the threads for task scheduling can be
-  reused instead of each JavasScript thread create a Timer thread when using Timer.
-
-  And most important thing is this adds global.nashornEventLoop and scheduled tasks only add function callback
-  object in eventLoop (ArrayQueue), and it is main JavaScript thread to run these function callback by calling
-  `global.nashornEventLoop.process();` at the end of JavaScript Application. It is just like browser or NodeJS
-  that event loop is called when the main stack is cleared.
-
-  When runs on server with Promise, remember to call `nashornEventLoop.process()` when waiting for Promise by
-  Thread.sleep(), and call `nashornEventLoop.reset()` if server thread (e.g. Servlet thread) decides to be
-  timeout so that eventLoop will be clean for next request.
- */
-(function nashornEventLoopMain(context) {
-  'use strict';
-  var Runnable = Java.type("java.lang.Runnable");
-  var eventLoop = context.__NASHORN_EVENT_LOOP__;
-  var asRunnable = function (fn, args) {
-    return new (Java.extend(Runnable, {
-      run: function() {
-        try {
-          fn.apply(context, args);
-        } catch (e) {
-          console.trace(e);
-          console.trace(fn);
-          console.trace(args);
-        }
-      },
-      toString: function() {
-        return JSON.stringify({args: args, fn: fn.toString()});
-      }
-    }))();
-  };
-  context.setTimeout = function(fn, millis) {
-    var args = [].slice.call(arguments, 2, arguments.length);
-    return eventLoop.setTimeout(asRunnable(fn, args), millis);
-  };
-  context.clearTimeout = function(timerId) {
-    eventLoop.clearTimeout(timerId);
-  };
-  context.setImmediate = function(fn) {
-    var args = [].slice.call(arguments, 1, arguments.length);
-    return eventLoop.setImmediate(asRunnable(fn, args));
-  };
-  context.clearImmediate = function(timerId) {
-    eventLoop.clearImmediate(timerId);
-  };
-  context.setInterval = function (fn, delay) {
-    var args = [].slice.call(arguments, 2, arguments.length);
-    return eventLoop.setInterval(asRunnable(fn, args), delay);
-  };
-  context.clearInterval = function(timerId) {
-    eventLoop.clearInterval(timerId);
-  };
-})(typeof global !== "undefined" && global || typeof self !== "undefined" && self || this);
 this["frontend"] =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -1317,7 +1223,7 @@ function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in ob
         style = _ref.style,
         otherAttributes = _objectWithoutProperties(_ref, ['style']);
 
-    return __WEBPACK_IMPORTED_MODULE_2_preact__["h"](__WEBPACK_IMPORTED_MODULE_1_styletron_preact__["styled"](nodeName, style), otherAttributes, children);
+    return Object(__WEBPACK_IMPORTED_MODULE_2_preact__["h"])(Object(__WEBPACK_IMPORTED_MODULE_1_styletron_preact__["styled"])(nodeName, style), otherAttributes, children);
 });
 // CONCATENATED MODULE: ./src/app/components/button/index.jsx
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__style_scss__ = __webpack_require__(31);
@@ -1431,7 +1337,8 @@ var boot_server_body = function body(props) {
 // the main export for the JVM JS interpreter to run
 // eslint-disable-next-line import/prefer-default-export
 var render = function render(props) {
-    return '\n    <html lang="en">\n        <head>\n            <title>' + props.page.headline + ' | ' + props.page.section + ' | The Guardian</title>\n            <style>\n            *, * > * {\n                margin: 0;\n                padding: 0;\n                box-sizing: border-box;\n            }\n            </style>\n            ' + styletron.getStylesheetsHtml() + '\n            <script>window.guardian = ' + JSON.stringify(props, null, 2) + ';</script>\n            <script src="/bundle.browser.js" async defer></script>\n        </head>\n        ' + __WEBPACK_IMPORTED_MODULE_1_preact_render_to_string__["render"](boot_server_body(props)) + '\n    </html>\n';
+    var bodySrc = Object(__WEBPACK_IMPORTED_MODULE_1_preact_render_to_string__["render"])(boot_server_body(props));
+    return '\n    <html lang="en">\n        <head>\n            <title>' + props.page.headline + ' | ' + props.page.section + ' | The Guardian</title>\n            <style>\n            *, * > * {\n                margin: 0;\n                padding: 0;\n                box-sizing: border-box;\n            }\n            </style>\n            ' + styletron.getStylesheetsHtml() + '\n            <script>window.guardian = ' + JSON.stringify(props, null, 2) + ';</script>\n            <script src="/bundle.browser.js" async defer></script>\n        </head>\n        ' + bodySrc + '\n    </html>\n';
 };
 
 /***/ }),
