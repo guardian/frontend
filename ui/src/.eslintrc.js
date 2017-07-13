2826877a5d6ccfa377d6d550a78259bc0fd6ed1a
Alex Sanders <a@snd.rs>
2017-07-13 12:34:00 +0100

    target lint better

--- ui/src/.eslintrc.js
+++ ui/src/.eslintrc.js
@@ -1,16 +1,14 @@
 // @flow
 module.exports = {
     parser: 'babel-eslint',
+    env: { browser: true, node: true, es6: true, 'jest/globals': true },
     extends: [
         'plugin:flowtype/recommended',
-        'airbnb',
-        'prettier',
         'prettier/flowtype',
         'prettier/react',
     ],
-    parserOptions: { sourceType: 'module', ecmaVersion: '2017', jsx: true },
-    plugins: ['flow-header', 'flowtype', 'jest', 'html', 'prettier'],
-    env: { browser: true, node: true, es6: true, 'jest/globals': true },
+    plugins: ['flow-header', 'flowtype', 'jest', 'prettier'],
+    parserOptions: { ecmaVersion: '2017' },
     settings: {
         react: { pragma: 'h' },
         'import/resolver': {
@@ -28,19 +26,8 @@ module.exports = {
                 jsx: 'never',
             },
         ],
-        'prettier/prettier': [
-            'error',
-            {
-                trailingComma: 'es5',
-                singleQuote: true,
-                bracketSpacing: true,
-                tabWidth: 4,
-                jsxBracketSameLine: false,
-            },
-        ],
         'flow-header/flow-header': 'error',
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
-        'react/jsx-filename-extension': [1, { extensions: ['.jsx'] }],
     },
 };
