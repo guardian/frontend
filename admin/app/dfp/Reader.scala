1464709a47a9ff755e729613c9c3e7f330e0500b
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-07-13 12:34:02 +0100

    revert dependency upgrade

--- admin/app/dfp/Reader.scala
+++ admin/app/dfp/Reader.scala
@@ -1,8 +1,8 @@
 package dfp
 
-import com.google.api.ads.dfp.axis.utils.v201705.StatementBuilder
-import com.google.api.ads.dfp.axis.utils.v201705.StatementBuilder.SUGGESTED_PAGE_LIMIT
-import com.google.api.ads.dfp.axis.v201705._
+import com.google.api.ads.dfp.axis.utils.v201608.StatementBuilder
+import com.google.api.ads.dfp.axis.utils.v201608.StatementBuilder.SUGGESTED_PAGE_LIMIT
+import com.google.api.ads.dfp.axis.v201608._
 
 import scala.annotation.tailrec
 
