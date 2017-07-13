9ba72720d208cde7e1d2eb95eafabc705efe68bd
Alex Sanders <a@snd.rs>
2017-07-13 12:34:17 +0100

    use env fork in code

--- ui/src/app/components/button/index.jsx
+++ ui/src/app/components/button/index.jsx
@@ -1,10 +1,9 @@
 // @flow
 import style from './style.scss';
 
+const onClick = BROWSER ? () => console.log('clicked the button!!') : null;
+
 export default (props: Object) =>
-    <button
-        style={style.button}
-        onClick={() => console.log('clicked the button!!')}
-    >
+    <button style={style.button} onClick={onClick}>
         {props.children}
     </button>;
