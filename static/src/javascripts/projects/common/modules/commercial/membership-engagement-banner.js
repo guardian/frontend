e3e45d67cfeaf5fa79e1d1eb79531e87001bd2d6
Simon Adcock <simon.adcock@guardian.co.uk>
2017-07-17 14:45:44 +0100

    transform projects/common/modules/commercial/membership-engagement-banner.js to ES6 module

--- static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
@@ -1,237 +1,210 @@
-define([
-        'bean',
-        'lib/$',
-        'lib/config',
-        'lib/storage',
-        'lodash/utilities/template',
-        'common/modules/ui/message',
-        'raw-loader!common/views/membership-message.html',
-        'commercial/modules/commercial-features',
-        'lib/mediator',
-        'lib/fastdom-promise',
-        'common/modules/experiments/test-can-run-checks',
-        'common/modules/experiments/tests/membership-engagement-banner-tests',
-        'lodash/objects/assign',
-        'lodash/collections/find',
-        'common/views/svgs',
-        'lib/fetch',
-        'common/modules/experiments/segment-util',
-        'common/modules/experiments/acquisition-test-selector',
-        'common/modules/commercial/membership-engagement-banner-parameters',
-        'common/modules/commercial/membership-engagement-banner-block',
-        'ophan/ng',
-        'lib/geolocation',
-        'lib/url'
-    ], function (bean,
-                 $,
-                 config,
-                 storage,
-                 template,
-                 Message,
-                 messageTemplate,
-                 commercialFeatures,
-                 mediator,
-                 fastdom,
-                 testCanRunChecks,
-                 MembershipEngagementBannerTests,
-                 assign,
-                 find,
-                 svgs,
-                 fetch,
-                 segmentUtil,
-                 acquisitionTestSelector,
-                 membershipEngagementBannerUtils,
-                 membershipEngagementBannerBlock,
-                 ophan,
-                 geolocation,
-                 url) {
+import bean from 'bean';
+import $ from 'lib/$';
+import config from 'lib/config';
+import storage from 'lib/storage';
+import template from 'lodash/utilities/template';
+import Message from 'common/modules/ui/message';
+import messageTemplate from 'raw-loader!common/views/membership-message.html';
+import commercialFeatures from 'commercial/modules/commercial-features';
+import mediator from 'lib/mediator';
+import fastdom from 'lib/fastdom-promise';
+import testCanRunChecks from 'common/modules/experiments/test-can-run-checks';
+import MembershipEngagementBannerTests from 'common/modules/experiments/tests/membership-engagement-banner-tests';
+import assign from 'lodash/objects/assign';
+import find from 'lodash/collections/find';
+import svgs from 'common/views/svgs';
+import fetch from 'lib/fetch';
+import segmentUtil from 'common/modules/experiments/segment-util';
+import acquisitionTestSelector from 'common/modules/experiments/acquisition-test-selector';
+import membershipEngagementBannerUtils from 'common/modules/commercial/membership-engagement-banner-parameters';
+import membershipEngagementBannerBlock from 'common/modules/commercial/membership-engagement-banner-block';
+import ophan from 'ophan/ng';
+import geolocation from 'lib/geolocation';
+import url from 'lib/url';
 
 
-        // change messageCode to force redisplay of the message to users who already closed it.
-        // messageCode is also consumed by .../test/javascripts/spec/common/commercial/membership-engagement-banner.spec.js
-        var messageCode = 'engagement-banner-2017-07-05';
+// change messageCode to force redisplay of the message to users who already closed it.
+// messageCode is also consumed by .../test/javascripts/spec/common/commercial/membership-engagement-banner.spec.js
+var messageCode = 'engagement-banner-2017-07-05';
 
-        var DO_NOT_RENDER_ENGAGEMENT_BANNER = 'do no render engagement banner';
+var DO_NOT_RENDER_ENGAGEMENT_BANNER = 'do no render engagement banner';
 
-        function getUserTest() {
-            var engagementBannerTests = MembershipEngagementBannerTests
-                .concat(acquisitionTestSelector.epicEngagementBannerTests);
+function getUserTest() {
+    var engagementBannerTests = MembershipEngagementBannerTests
+        .concat(acquisitionTestSelector.epicEngagementBannerTests);
 
-            return find(engagementBannerTests, function(test) {
-                return testCanRunChecks.testCanBeRun(test) && segmentUtil.isInTest(test)
-            });
+    return find(engagementBannerTests, function(test) {
+        return testCanRunChecks.testCanBeRun(test) && segmentUtil.isInTest(test)
+    });
+}
+
+function getUserVariant(test) {
+    return test ? segmentUtil.variantFor(test) : undefined;
+}
+
+function buildCampaignCode(offering, campaignId, variantId) {
+    var prefix = '';
+    var offerings = membershipEngagementBannerUtils.offerings;
+
+    // mem and cont chosen to be consistent with default campaign code prefixes.
+    if (offering === offerings.membership) {
+        prefix = 'mem_';
+    } else if (offering === offerings.contributions) {
+        prefix = 'cont_';
+    }
+
+    return prefix + campaignId + '_' + variantId;
+}
+
+function getUserVariantParams(userVariant, campaignId, defaultOffering) {
+
+    if (userVariant && userVariant.engagementBannerParams) {
+        var userVariantParams = userVariant.engagementBannerParams;
+
+        if (!userVariantParams.campaignCode) {
+            var offering = userVariantParams.offering || defaultOffering;
+            userVariantParams.campaignCode = buildCampaignCode(offering, campaignId, userVariant.id);
         }
 
-        function getUserVariant(test) {
-            return test ? segmentUtil.variantFor(test) : undefined;
+        return userVariantParams;
+    } else {
+        return {};
+    }
+}
+
+/*
+ * Params for the banner are overlaid in this order, earliest taking precedence:
+ *
+ *  * Variant (if the user is in an A/B testing variant)
+ *  * Edition
+ *  * Offering ('membership' or 'contributions')
+ *  * Default
+ *
+ * The 'offering' in use comes from either:
+ *
+ *  * Variant (if the user is in an A/B testing variant)
+ *  * Edition (only one offering can be the default for a given Edition)
+ *
+ * Returns either 'null' if no banner is available for this edition,
+ * otherwise a populated params object that looks like this:
+ *
+ *  {
+ *    minArticles: 5, // how many articles should the user see before they get the engagement banner?
+ *    messageText: "..."
+ *    colourStrategy: // a function to determine what css class to use for the banner's colour
+ *    buttonCaption: "Become a Supporter"
+ *  }
+ *
+ */
+function deriveBannerParams(location) {
+    var defaultParams = membershipEngagementBannerUtils.defaultParams(location);
+    var userTest = getUserTest();
+    var campaignId = userTest ? userTest.campaignId : undefined;
+    var userVariant = getUserVariant(userTest);
+
+    if (userVariant && userVariant.blockEngagementBanner) {
+        return DO_NOT_RENDER_ENGAGEMENT_BANNER;
+    }
+
+    return assign({}, defaultParams, getUserVariantParams(userVariant, campaignId, defaultParams.offering));
+}
+
+// Used to send an interaction if the engagement banner is shown.
+function recordInteraction(interaction) {
+    if (interaction) {
+        var component = interaction.component;
+        var value = interaction.value;
+
+        if (component && value) {
+            ophan.record({
+                component: component,
+                value: value
+            })
         }
+    }
+}
 
-        function buildCampaignCode(offering, campaignId, variantId) {
-            var prefix = '';
-            var offerings = membershipEngagementBannerUtils.offerings;
-
-            // mem and cont chosen to be consistent with default campaign code prefixes.
-            if (offering === offerings.membership) {
-                prefix = 'mem_';
-            }
-            else if (offering === offerings.contributions) {
-                prefix = 'cont_';
-            }
-
-            return prefix + campaignId + '_' + variantId;
-        }
-
-        function getUserVariantParams(userVariant, campaignId, defaultOffering) {
-
-            if (userVariant && userVariant.engagementBannerParams) {
-                var userVariantParams = userVariant.engagementBannerParams;
-
-                if (!userVariantParams.campaignCode) {
-                    var offering = userVariantParams.offering || defaultOffering;
-                    userVariantParams.campaignCode = buildCampaignCode(offering, campaignId, userVariant.id);
-                }
-
-                return userVariantParams;
-            } else {
-                return {};
-            }
-        }
-
-        /*
-         * Params for the banner are overlaid in this order, earliest taking precedence:
-         *
-         *  * Variant (if the user is in an A/B testing variant)
-         *  * Edition
-         *  * Offering ('membership' or 'contributions')
-         *  * Default
-         *
-         * The 'offering' in use comes from either:
-         *
-         *  * Variant (if the user is in an A/B testing variant)
-         *  * Edition (only one offering can be the default for a given Edition)
-         *
-         * Returns either 'null' if no banner is available for this edition,
-         * otherwise a populated params object that looks like this:
-         *
-         *  {
-         *    minArticles: 5, // how many articles should the user see before they get the engagement banner?
-         *    messageText: "..."
-         *    colourStrategy: // a function to determine what css class to use for the banner's colour
-         *    buttonCaption: "Become a Supporter"
-         *  }
-         *
-         */
-        function deriveBannerParams(location) {
-            var defaultParams = membershipEngagementBannerUtils.defaultParams(location);
-            var userTest = getUserTest();
-            var campaignId = userTest ? userTest.campaignId : undefined;
-            var userVariant = getUserVariant(userTest);
-
-            if (userVariant && userVariant.blockEngagementBanner) {
-                return DO_NOT_RENDER_ENGAGEMENT_BANNER;
-            }
-
-            return assign({}, defaultParams, getUserVariantParams(userVariant, campaignId, defaultParams.offering));
-        }
-
-        // Used to send an interaction if the engagement banner is shown.
-        function recordInteraction(interaction) {
-            if (interaction) {
-                var component = interaction.component;
-                var value = interaction.value;
-
-                if (component && value) {
-                    ophan.record({
-                        component: component,
-                        value: value
-                    })
-                }
-            }
-        }
-
-        var paypalAndCreditCardImage = (config.images && config.images.acquisitions && config.images.acquisitions['paypal-and-credit-card']) || '';
+var paypalAndCreditCardImage = (config.images && config.images.acquisitions && config.images.acquisitions['paypal-and-credit-card']) || '';
 
 
-        function showBanner(params) {
+function showBanner(params) {
 
 
-            if (params === DO_NOT_RENDER_ENGAGEMENT_BANNER || membershipEngagementBannerBlock.isBlocked()) {
-                return;
-            }
+    if (params === DO_NOT_RENDER_ENGAGEMENT_BANNER || membershipEngagementBannerBlock.isBlocked()) {
+        return;
+    }
 
-            var colourClass = params.colourStrategy();
+    var colourClass = params.colourStrategy();
 
-            var messageText = Array.isArray(params.messageText)?selectSequentiallyFrom(params.messageText):params.messageText;
+    var messageText = Array.isArray(params.messageText) ? selectSequentiallyFrom(params.messageText) : params.messageText;
 
-            var urlParameters = {
-                REFPVID : params.pageviewId,
-                INTCMP:  params.campaignCode
-            };
+    var urlParameters = {
+        REFPVID: params.pageviewId,
+        INTCMP: params.campaignCode
+    };
 
 
-            var linkUrl = params.linkUrl + '?' + url.constructQuery(urlParameters);
+    var linkUrl = params.linkUrl + '?' + url.constructQuery(urlParameters);
 
-            var renderedBanner = template(messageTemplate, {
-                linkHref: linkUrl,
-                messageText: messageText,
-                buttonCaption: params.buttonCaption,
-                colourClass: colourClass,
-                arrowWhiteRight: svgs.inlineSvg('arrowWhiteRight'),
-                paypalLogoSrc: paypalAndCreditCardImage
-            });
+    var renderedBanner = template(messageTemplate, {
+        linkHref: linkUrl,
+        messageText: messageText,
+        buttonCaption: params.buttonCaption,
+        colourClass: colourClass,
+        arrowWhiteRight: svgs.inlineSvg('arrowWhiteRight'),
+        paypalLogoSrc: paypalAndCreditCardImage
+    });
 
-            var messageShown = new Message.Message(
-                messageCode, {
-                    pinOnHide: false,
-                    siteMessageLinkName: 'membership message',
-                    siteMessageCloseBtn: 'hide',
-                    siteMessageComponentName: params.campaignCode,
-                    trackDisplay: true,
-                    cssModifierClass: colourClass
-                }).show(renderedBanner);
+    var messageShown = new Message(
+        messageCode, {
+            pinOnHide: false,
+            siteMessageLinkName: 'membership message',
+            siteMessageCloseBtn: 'hide',
+            siteMessageComponentName: params.campaignCode,
+            trackDisplay: true,
+            cssModifierClass: colourClass
+        }).show(renderedBanner);
 
-            if (messageShown) {
+    if (messageShown) {
 
-                recordInteraction(params.interactionOnMessageShown);
+        recordInteraction(params.interactionOnMessageShown);
 
-                mediator.emit('membership-message:display');
-            }
+        mediator.emit('membership-message:display');
+    }
 
-            mediator.emit('banner-message:complete');
-        }
+    mediator.emit('banner-message:complete');
+}
 
-        function init() {
+function init() {
 
-            return geolocation.get().then(function(location) {
+    return geolocation.get().then(function(location) {
 
-                var bannerParams = deriveBannerParams(location);
+        var bannerParams = deriveBannerParams(location);
 
-                if (bannerParams && (storage.local.get('gu.alreadyVisited') || 0) >= bannerParams.minArticles) {
-                    return commercialFeatures.commercialFeatures.asynchronous.canDisplayMembershipEngagementBanner.then(function (canShow) {
+        if (bannerParams && (storage.local.get('gu.alreadyVisited') || 0) >= bannerParams.minArticles) {
+            return commercialFeatures.commercialFeatures.asynchronous.canDisplayMembershipEngagementBanner.then(function(canShow) {
 
-                        if (canShow) {
-                            mediator.on('modules:onwards:breaking-news:ready', function (breakingShown) {
-                                if (!breakingShown) {
-                                    showBanner(bannerParams);
-                                } else {
-                                    mediator.emit('banner-message:complete');
-                                }
-                            });
+                if (canShow) {
+                    mediator.on('modules:onwards:breaking-news:ready', function(breakingShown) {
+                        if (!breakingShown) {
+                            showBanner(bannerParams);
                         } else {
                             mediator.emit('banner-message:complete');
                         }
                     });
+                } else {
+                    mediator.emit('banner-message:complete');
                 }
             });
         }
+    });
+}
 
-        function selectSequentiallyFrom(array) {
-            return array[storage.local.get('gu.alreadyVisited') % array.length];
-        }
+function selectSequentiallyFrom(array) {
+    return array[storage.local.get('gu.alreadyVisited') % array.length];
+}
 
-        return {
-            init: init,
-            messageCode: messageCode
-        };
-    }
-);
+export default {
+    init: init,
+    messageCode: messageCode
+};
