73e2ac89b16542fd7eeee91551a86193bd9dac48
Jon Norman <jon.norman@theguardian.com>
2017-07-17 14:57:39 +0100

    Remove Commercial Gallery Banner Ads test (#17452)
    * Remove Commercial Gallery Banner Ads Test.
    
    * Add types to public methods in OptInController.

--- common/app/mvt/MultiVariateTesting.scala
+++ common/app/mvt/MultiVariateTesting.scala
@@ -16,30 +16,6 @@ import conf.switches.Switches.ServerSideTests
 //    val tests = List(ExampleTest)
 // }
 
-object CommercialGalleryBannerAdsVariant extends TestDefinition(
-  name = "ab-commercial-gallery-banner-ads-variant",
-  description = "Users in this test will see banner ads in galleries",
-  owners = Seq(Owner.withGithub("JonNorman")),
-  sellByDate = new LocalDate(2017, 7, 18)
-) {
-
-  def participationGroup(implicit request: RequestHeader): Option[String] = request.headers.get("X-GU-ab-commercial-gallery-banner-ads")
-
-  def canRun(implicit request: RequestHeader): Boolean = participationGroup.contains("variant")
-}
-
-object CommercialGalleryBannerAdsControl extends TestDefinition(
-  name = "ab-commercial-gallery-banner-ads-control",
-  description = "Users in this test will not see banner ads in galleries, as a control.",
-  owners = Seq(Owner.withGithub("JonNorman")),
-  sellByDate = new LocalDate(2017, 7, 18)
-) {
-
-  def participationGroup(implicit request: RequestHeader): Option[String] = request.headers.get("X-GU-ab-commercial-gallery-banner-ads")
-
-  def canRun(implicit request: RequestHeader): Boolean = participationGroup.contains("control")
-}
-
 object CommercialClientLoggingVariant extends TestDefinition(
   name = "commercial-client-logging",
   description = "A slice of the audience who will post their commercial js performance data",
@@ -100,8 +76,6 @@ trait ServerSideABTests {
 object ActiveTests extends ServerSideABTests {
   val tests: Seq[TestDefinition] = List(
     CommercialClientLoggingVariant,
-    CommercialGalleryBannerAdsVariant,
-    CommercialGalleryBannerAdsControl,
     ABNewDesktopHeaderVariant,
     ABNewDesktopHeaderControl
   )
