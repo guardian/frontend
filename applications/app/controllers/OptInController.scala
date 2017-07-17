b941361281851731587d4a224cd7038e376bcd0a
Jon Norman <jon.norman@guardian.co.uk>
2017-07-17 11:07:32 +0100

    Add types to public methods in OptInController.

--- applications/app/controllers/OptInController.scala
+++ applications/app/controllers/OptInController.scala
@@ -2,32 +2,32 @@ package controllers
 
 import model.Cached
 import model.Cached.WithoutRevalidationResult
-import play.api.mvc.{Action, Controller, Cookie, DiscardingCookie}
+import play.api.mvc.{Action, AnyContent, Controller, Cookie, DiscardingCookie, Result}
 
 import scala.concurrent.duration._
 
 trait OptFeature extends Controller {
   val cookieName: String
   val lifetime: Int = 90.days.toSeconds.toInt
-  def opt(choice: String) = choice match {
+  def opt(choice: String): Result = choice match {
     case "in" => optIn()
     case "out" => optOut()
     case _ => optDelete()
   }
-  def optIn() = SeeOther("/").withCookies(Cookie(cookieName, "true", maxAge = Some(lifetime)))
-  def optOut() = SeeOther("/").discardingCookies(DiscardingCookie(cookieName))
-  def optDelete() = SeeOther("/").discardingCookies(DiscardingCookie(cookieName))
+  def optIn(): Result = SeeOther("/").withCookies(Cookie(cookieName, "true", maxAge = Some(lifetime)))
+  def optOut(): Result = SeeOther("/").discardingCookies(DiscardingCookie(cookieName))
+  def optDelete(): Result = SeeOther("/").discardingCookies(DiscardingCookie(cookieName))
 }
 
 case class HttpsOptFeature(cookieName: String) extends OptFeature {
-  override def optOut() = SeeOther("/").withCookies(Cookie(cookieName, "false", maxAge = Some(lifetime)))
+  override def optOut(): Result = SeeOther("/").withCookies(Cookie(cookieName, "false", maxAge = Some(lifetime)))
 }
 
 case class OptInFeature(cookieName: String) extends OptFeature
 
 class OptInController extends Controller {
 
-  def handle(feature: String, choice: String) = Action { implicit request =>
+  def handle(feature: String, choice: String): Action[AnyContent] = Action { implicit request =>
     Cached(60)(WithoutRevalidationResult(feature match {
       case "desktopheader" => newDesktopHeader.opt(choice)
       case _ => NotFound
