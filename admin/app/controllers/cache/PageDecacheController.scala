4a4a8379882d1886e67ac4c1ba315c2c33cd749e
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-07-18 16:41:23 +0100

    [Admin] Remove extra check before Fastly purge
    Since we are executing a soft purge, checking if the url can be served
    by the origin server is redundant.

--- admin/app/controllers/cache/PageDecacheController.scala
+++ admin/app/controllers/cache/PageDecacheController.scala
@@ -22,49 +22,19 @@ class PageDecacheController(wsClient: WSClient)(implicit context: ApplicationCon
 
   val authActions = new AuthActions(wsClient)
 
-  def renderPageDecache(url: Option[String] = None) = Action.async { implicit request =>
-    url match {
-      case Some(s) => renderPrePurgeTestResult(s)
-      case None => Future(NoCache(Ok(views.html.cache.pageDecache())))
-    }
-  }
-
-  private def renderPrePurgeTestResult(purgeUrl: String)(implicit request: Request[AnyContent]) = {
-    getRouterUrl(purgeUrl).map { routerUrl =>
-      wsClient.url(routerUrl)
-        .withRequestTimeout(2.second)
-        .withVirtualHost("www.theguardian.com")
-        .withFollowRedirects(true)
-        .get()
-        .map { response =>
-          PrePurgeTestResult(purgeUrl, response.status >= 200 && response.status < 300)
-        }.recoverWith {
-          case t: Throwable => successful(PrePurgeTestResult(purgeUrl, passed = false))
-        }.map { result =>
-          NoCache(Ok(views.html.cache.pageDecache(Some(result))))
-        }
-    }.getOrElse(successful(InternalServerError("Couldn't get router URL - please go back and try again")))
+  def renderPageDecache() = Action.async { implicit request =>
+      Future(NoCache(Ok(views.html.cache.pageDecache())))
   }
 
   def decache() = authActions.AuthActionTest.async { implicit request =>
     getSubmittedUrl(request).map(new URI(_)).map{ urlToDecache =>
-
       new CdnPurge(wsClient)
         .soft(SurrogateKey(urlToDecache.getPath))
-        .map { purgeSent =>
-          val message = if(purgeSent) "Purge request successfully sent" else "Purge request was not successful, please report this issue"
-          NoCache(Ok(views.html.cache.pageDecache(None, message)))
-        }
+        .map { _ => "Purge request successfully sent" }
+        .recover { case e => s"Purge request was not successful, please report this issue: '${e.getLocalizedMessage}'" }
+        .map { message => NoCache(Ok(views.html.cache.pageDecache(message))) }
     }.getOrElse(successful(BadRequest("No page submitted")))
   }
-
-  private def getRouterUrl(url: String): Option[String] =
-    LoadBalancer("frontend-router")
-      .flatMap(_.url)
-      .map { router =>
-        val path = new URL(url).getPath
-        s"http://$router$path"
-      }
 
   private def getSubmittedUrl(request: AuthenticatedRequest[AnyContent, UserIdentity]): Option[String] =
     request
--- admin/app/purge/CdnPurge.scala
+++ admin/app/purge/CdnPurge.scala
@@ -4,7 +4,7 @@ import common.{ExecutionContexts, Logging}
 import conf.AdminConfiguration.fastly
 import conf.Configuration.environment
 import implicits.Dates
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{WSClient, WSResponse}
 
 import scala.concurrent.Future
 
@@ -13,20 +13,19 @@ class CdnPurge(wsClient: WSClient) extends ExecutionContexts with Dates with Log
   private val serviceId = "2eYr6Wx3ZCUoVPShlCM61l"
 
   // Performs soft purge which will still serve stale if there is an error
-  def soft(key:String): Future[Boolean] = {
+  def soft(key:String): Future[WSResponse] = {
     // under normal circumstances we only ever want this called from PROD.
-    // Don't want too much decache going on.
+    // Don't want too much decaching going on.
     if (environment.isProd) {
       val purgeRequest = wsClient.url(s"https://api.fastly.com/service/$serviceId/purge/$key")
         .withHeaders("Fastly-Key" -> fastly.key,
                      "Fastly-Soft-Purge" -> "1")
         .post("")
 
-      purgeRequest.foreach(r => log.info(s"purge $key from Fastly with response ${r.statusText}"))
-      purgeRequest.map(_.status == 200)
+      purgeRequest.onSuccess { case r => log.info(s"purge $key from Fastly with response ${r.statusText}") }
+      purgeRequest
     } else {
-      log.info(s"mock call to Fastly to decache $key")
-      Future.successful(false)
+      Future.failed(new RuntimeException("Purging is disabled in non-production environment"))
     }
   }
 }
