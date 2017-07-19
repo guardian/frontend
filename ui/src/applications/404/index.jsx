ee2e073915649f5b757d7ba483672fccab84e5dc
Alex Sanders <a@snd.rs>
2017-07-19 12:29:29 +0100

    calls ‘views’ applications (like in play)

---
+++ ui/src/applications/404/index.jsx
@@ -0,0 +1,13 @@
+// @flow
+import Logo from 'assets/images/guardian-logo-320.svg';
+
+export default () =>
+    <div>
+        <Logo />
+        <h1>Sorry - we haven’t been able to serve the page you asked for.</h1>
+        <p>
+            You may have followed a broken or outdated link, or there may be an
+            error on our site.
+        </p>
+        <p>Please follow one of the links below to continue exploring.</p>
+    </div>;
