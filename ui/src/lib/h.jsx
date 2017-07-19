5e6f1b48dbab534a7f3abe225e73e55c1a3e5503
Alex Sanders <a@snd.rs>
2017-07-19 12:55:58 +0100

    don’t add classes where they’re not needed

--- ui/src/lib/h.jsx
+++ ui/src/lib/h.jsx
@@ -14,5 +14,6 @@ export default (
     ...children: Array<any>
 ) => {
     const { style, ...otherAttributes } = attributes || {};
-    return preact_h(styled(nodeName, style), otherAttributes, children);
+    if (style) return preact_h(styled(nodeName, style), otherAttributes, children);
+    return preact_h(nodeName, otherAttributes, children);
 };
