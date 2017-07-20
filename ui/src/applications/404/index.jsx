fb02ad8fd56daea01eb2ace153fc9d5e84e581fe
Alex Sanders <a@snd.rs>
2017-07-20 13:01:03 +0100

    add logo to 404

--- ui/src/applications/404/index.jsx
+++ ui/src/applications/404/index.jsx
@@ -1,9 +1,10 @@
 // @flow
 import Logo from 'assets/images/guardian-logo-320.svg';
+import styles from './style.scss';
 
 export default () =>
     <div>
-        <Logo />
+        <Logo styles={styles} />
         <h1>Sorry - we haven’t been able to serve the page you asked for.</h1>
         <p>
             You may have followed a broken or outdated link, or there may be an
