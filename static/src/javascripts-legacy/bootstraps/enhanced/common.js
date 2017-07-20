9f9352b3f51447bbae57cbd055dcf913bb6dff2b
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2017-07-20 15:35:33 +0100

    Remove epic for Brexit cohort (#17464)
    * Remove epic for Brexit cohort
    * remove join date cookie

--- static/src/javascripts-legacy/bootstraps/enhanced/common.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/common.js
@@ -166,7 +166,7 @@ define([
             },
 
             cleanupCookies: function () {
-                cookies.cleanUp(['mmcore.pd', 'mmcore.srv', 'mmid', 'GU_ABFACIA', 'GU_FACIA', 'GU_ALPHA', 'GU_ME', 'at', 'gu_adfree_user']);
+                cookies.cleanUp(['mmcore.pd', 'mmcore.srv', 'mmid', 'GU_ABFACIA', 'GU_FACIA', 'GU_ALPHA', 'GU_ME', 'at', 'gu_adfree_user', 'gu_join_date']);
             },
 
             cleanupLocalStorage : function () {
