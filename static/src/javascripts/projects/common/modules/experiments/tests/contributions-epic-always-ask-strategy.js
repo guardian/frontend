37c83347b90f9c85a7aa38a1e155812c7f35c401
Joseph Smith <joseph.smith@guardian.co.uk>
2017-07-21 15:32:37 +0100

    transform projects/common/modules/experiments/tests/contributions-epic-always-ask-strategy.js to ES6 module

--- static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-always-ask-strategy.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-always-ask-strategy.js
@@ -1,55 +1,46 @@
-define([
-    'commercial/modules/commercial-features',
-    'common/modules/commercial/contributions-utilities',
-    'lib/config',
-    'lib/cookies',
+import commercialFeatures from 'commercial/modules/commercial-features';
+import contributionsUtilities from 'common/modules/commercial/contributions-utilities';
+import 'lib/config';
+import 'lib/cookies';
 
+export default contributionsUtilities.makeABTest({
+    id: 'ContributionsEpicAlwaysAskStrategy',
+    campaignId: 'epic_always_ask_strategy',
 
-], function (
-            commercialFeatures,
-            contributionsUtilities
-) {
+    start: '2016-12-06',
+    expiry: '2018-07-19',
 
-    return contributionsUtilities.makeABTest({
-        id: 'ContributionsEpicAlwaysAskStrategy',
-        campaignId: 'epic_always_ask_strategy',
+    author: 'Guy Dawson',
+    description: 'Test to assess the effects of always asking readers to contribute via the Epic over a prolonged period.',
+    successMeasure: 'We are able to measure the positive and negative effects of this strategy.',
+    idealOutcome: 'There are no negative effects and this is the optimum strategy!',
 
-        start: '2016-12-06',
-        expiry: '2018-07-19',
+    audienceCriteria: 'All',
+    audience: 0.02,
+    audienceOffset: 0.88,
+    useTargetingTool: true,
 
-        author: 'Guy Dawson',
-        description: 'Test to assess the effects of always asking readers to contribute via the Epic over a prolonged period.',
-        successMeasure: 'We are able to measure the positive and negative effects of this strategy.',
-        idealOutcome: 'There are no negative effects and this is the optimum strategy!',
+    overrideCanRun: true,
+    canRun: function() {
+        return true;
+    },
 
-        audienceCriteria: 'All',
-        audience: 0.02,
-        audienceOffset: 0.88,
-        useTargetingTool: true,
+    variants: [{
+            id: 'control',
+            test: function() {},
+            isUnlimited: true
 
-        overrideCanRun: true,
-        canRun: function () {
-            return true;
         },
 
-        variants: [
-            {
-                id: 'control',
-                test: function() {},
-                isUnlimited : true
-
+        {
+            id: 'alwaysAsk',
+            test: function(render, variant, parentTest) {
+                if (contributionsUtilities.defaultCanEpicBeDisplayed(parentTest)) {
+                    render();
+                }
             },
-
-            {
-                id: 'alwaysAsk',
-                test: function (render, variant, parentTest) {
-                    if (contributionsUtilities.defaultCanEpicBeDisplayed(parentTest)) {
-                        render();
-                    }
-                },
-                isUnlimited : true,
-                successOnView: true
-            }
-        ]
-    });
+            isUnlimited: true,
+            successOnView: true
+        }
+    ]
 });
