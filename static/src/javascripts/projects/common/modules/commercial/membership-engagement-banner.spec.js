9c23802a2a205fd17df6f97578da5d966d874663
Simon Adcock <simon.adcock@guardian.co.uk>
2017-07-24 14:50:26 +0100

    group tests and mock implementations

--- static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.spec.js
+++ static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.spec.js
@@ -1,6 +1,9 @@
 // @flow
 import FakeMessage from 'common/modules/ui/message';
 import fakeMediator from 'lib/mediator';
+import fakeConfig from 'lib/config';
+import fakeMembershipEngagementParameters from 'common/modules/commercial/membership-engagement-banner-parameters';
+import fakeMembershipEngagementTests from 'common/modules/experiments/tests/membership-engagement-banner-tests';
 import { commercialFeatures } from 'commercial/modules/commercial-features';
 import { membershipEngagementBannerInit } from 'common/modules/commercial/membership-engagement-banner';
 
@@ -13,21 +16,17 @@ jest.mock('lib/storage', () => ({
     },
 }));
 jest.mock('lib/url', () => ({
-    constructQuery: jest.fn(() => 'fake-query-parameters'),
+    constructQuery: jest.fn(() => ''),
 }));
 jest.mock('lib/geolocation', () => ({
     get: jest.fn(() => Promise.resolve('GB')),
 }));
 jest.mock('common/views/svgs', () => ({
-    inlineSvg: jest.fn(() => 'fake-button-svg'),
+    inlineSvg: jest.fn(() => ''),
 }));
 jest.mock(
     'common/modules/experiments/tests/membership-engagement-banner-tests',
-    () => [
-        {
-            campaignId: 'fake-campaign-id',
-        },
-    ]
+    () => []
 );
 jest.mock('common/modules/experiments/acquisition-test-selector', () => []);
 jest.mock(
@@ -35,16 +34,9 @@ jest.mock(
     () => ({
         defaultParams: jest.fn(() => ({
             minArticles: 1,
-            colourStrategy: jest.fn(() => 'fake-colour-class'),
-            messageText: 'fake-message-text',
-            linkUrl: 'fake-link-url',
-            buttonCaption: 'fake-button-caption',
-            offering: 'fake-membership-offering',
+            colourStrategy: jest.fn(() => ''),
         })),
-        offerings: {
-            membership: 'fake-membership-offering',
-            contributions: 'fake-contributions-offering',
-        },
+        offerings: {},
     })
 );
 jest.mock('common/modules/experiments/test-can-run-checks', () => ({
@@ -52,10 +44,7 @@ jest.mock('common/modules/experiments/test-can-run-checks', () => ({
 }));
 jest.mock('common/modules/experiments/segment-util', () => ({
     isInTest: jest.fn(() => true),
-    variantFor: jest.fn(() => ({
-        id: 'fake-user-variant-id',
-        engagementBannerParams: {},
-    })),
+    variantFor: jest.fn(() => ({})),
 }));
 jest.mock('commercial/modules/commercial-features', () => ({
     commercialFeatures: {
@@ -75,8 +64,13 @@ jest.mock('ophan/ng', () => ({
     record: jest.fn(),
 }));
 jest.mock('lib/config', () => ({
-    get: jest.fn(() => 'fake-paypal-and-credit-card-image'),
+    get: jest.fn(() => ''),
 }));
+
+const fakeVariantFor: any = require('common/modules/experiments/segment-util')
+    .variantFor;
+const fakeConstructQuery: any = require('lib/url').constructQuery;
+const fakeInlineSvg: any = require('common/views/svgs').inlineSvg;
 
 beforeEach(() => {
     if (document.body) {
@@ -135,13 +129,36 @@ describe('Membership engagement banner', () => {
             }));
     });
 
-    describe('renders message with', () => {
+    describe('creates message with', () => {
         let showBanner;
 
         beforeEach(() => {
+            fakeMembershipEngagementParameters.defaultParams.mockImplementationOnce(
+                () => ({
+                    minArticles: 1,
+                    colourStrategy: jest.fn(() => 'fake-colour-class'),
+                    offering: 'fake-membership-offering',
+                })
+            );
+            fakeMembershipEngagementTests.push({
+                campaignId: 'fake-campaign-id',
+            });
+            fakeMembershipEngagementParameters.offerings = {
+                membership: 'fake-membership-offering',
+                contributions: 'fake-contributions-offering',
+            };
+            fakeVariantFor.mockImplementationOnce(() => ({
+                id: 'fake-user-variant-id',
+                engagementBannerParams: {},
+            }));
             showBanner = membershipEngagementBannerInit().then(() => {
                 fakeMediator.emit('modules:onwards:breaking-news:ready', false);
             });
+        });
+
+        afterEach(() => {
+            fakeMembershipEngagementParameters.offerings = {};
+            fakeMembershipEngagementTests.pop();
         });
 
         it('correct campaign code', () =>
@@ -156,6 +173,33 @@ describe('Membership engagement banner', () => {
                     'fake-colour-class'
                 );
             }));
+    });
+
+    describe('renders message with', () => {
+        let showBanner;
+
+        beforeEach(() => {
+            fakeMembershipEngagementParameters.defaultParams.mockImplementationOnce(
+                () => ({
+                    minArticles: 1,
+                    colourStrategy: jest.fn(() => 'fake-colour-class'),
+                    messageText: 'fake-message-text',
+                    linkUrl: 'fake-link-url',
+                    buttonCaption: 'fake-button-caption',
+                })
+            );
+            fakeConfig.get.mockImplementationOnce(
+                () => 'fake-paypal-and-credit-card-image'
+            );
+            fakeInlineSvg.mockImplementationOnce(() => 'fake-button-svg');
+            fakeConstructQuery.mockImplementationOnce(
+                () => 'fake-query-parameters'
+            );
+            showBanner = membershipEngagementBannerInit().then(() => {
+                fakeMediator.emit('modules:onwards:breaking-news:ready', false);
+            });
+        });
+
         it('message text', () =>
             showBanner.then(() => {
                 expect(FakeMessage.prototype.show.mock.calls[0][0]).toMatch(
