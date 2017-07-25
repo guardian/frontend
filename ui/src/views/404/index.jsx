c31ffbdc4a5e23a75f543bc2e9701201723fdd86
Alex Sanders <a@snd.rs>
2017-07-25 12:42:34 +0100

    update 404

--- ui/src/views/404/index.jsx
+++ ui/src/views/404/index.jsx
@@ -1,14 +1,5 @@
 // @flow
-import Logo from 'assets/images/guardian-logo-320.svg';
-import styles from './style.scss';
 
-export default () =>
-    <div>
-        <Logo styles={styles} />
-        <h1>Sorry - we haven’t been able to serve the page you asked for.</h1>
-        <p>
-            You may have followed a broken or outdated link, or there may be an
-            error on our site.
-        </p>
-        <p>Please follow one of the links below to continue exploring.</p>
-    </div>;
+import style from './style.scss';
+
+export default () => <div style={style.message}>404</div>;
