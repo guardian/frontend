5d12a7149ae6bb50497fea8c72eaef4781a81f81
Alex Sanders <a@snd.rs>
2017-07-25 14:41:38 +0100

    Revert "remove pasteup"
    This reverts commit a595d8b2db8bc15ed3ed5c4b467a4c0fc2fa6069.

---
+++ ui/src/pasteup.js
@@ -0,0 +1,20 @@
+// @flow
+export const font = {
+    size: {
+        small: '12px',
+        default: '16px',
+    },
+};
+
+export const colour = {
+    brandBlue: '#005689',
+    brandBlueDark: '#00456e',
+    features: {
+        main1: '#7d0068',
+        featuresMain2: '#b82266',
+        featuresSupport1: '#951c55',
+        featuresSupport2: '#4e0375',
+        featuresSupport3: '#fdadba',
+        featuresSupport4: '#dc2a7d',
+    },
+};
