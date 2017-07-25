9630372eb627baacbbe93f37e58c4b3d86621bb3
Alex Sanders <a@snd.rs>
2017-07-25 16:31:04 +0100

    import pasteup into sass

---
+++ ui/__tools__/pasteup-sass.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { ui } = require('../__config__/paths');
+
+require('babel-register')({
+    babelrc: false,
+    presets: [
+        [
+            'babel-preset-env',
+            {
+                modules: 'commonjs',
+            },
+        ],
+        'babel-preset-flow',
+    ],
+});
+
+// eslint-disable-next-line import/no-dynamic-require
+const pasteUpSass = require(path.resolve(ui, 'src', 'pasteup'));
+
+const toSass = (vars, prefix = false) =>
+    Object.keys(vars).reduce((sass, key) => {
+        const varName = [prefix, key].filter(Boolean).join('-');
+        if (typeof vars[key] === 'object') {
+            return `${sass} ${toSass(vars[key], varName)}`.trim();
+        }
+        return `${sass} $${varName}: ${vars[key]};`.trim();
+    }, '');
+
+module.exports = toSass(pasteUpSass);
