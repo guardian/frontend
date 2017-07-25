936f3f6e7cf25d7f90bdd1bcafcb463ba8e43e0f
Alex Sanders <a@snd.rs>
2017-07-25 18:50:40 +0100

    move reset css to to css file

--- ui/src/components/head/index.js
+++ ui/src/components/head/index.js
@@ -4,6 +4,8 @@
 // - https://meyerweb.com/eric/tools/css/reset
 // - https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice
 
+import reset from './reset.css';
+
 export default (props: any, css: string) =>
     `<head lang="en" data-page-path="/uk">
         <meta charset="utf-8"/>
@@ -13,7 +15,7 @@ export default (props: any, css: string) =>
         <meta name="format-detection" content="telephone=no"/>
         <meta name="HandheldFriendly" content="True"/>
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <style>a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,main,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,main,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}html{box-sizing:border-box}*,:after,:before{box-sizing:inherit}</style>
+        <style>${reset.toString()}</style>
         ${css}
         <script>window.guardian = ${JSON.stringify(props)};</script>
         <script src="/assets/javascripts/ui.bundle.browser.js" async defer></script>
