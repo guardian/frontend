ff5429e00aa275670778663186f659edadfda235
Alex Sanders <a@snd.rs>
2017-07-26 17:37:58 +0100

    don’t assume a state

--- ui/src/components/head/index.js
+++ ui/src/components/head/index.js
@@ -8,8 +8,7 @@ export default (props: any, css: string) =>
     `<head lang="en" data-page-path="/uk">
         <meta charset="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>${props.page.headline} | ${props.page
-        .section} | The Guardian</title>
+        <title>The Guardian</title>
         <meta name="description" content="">
         <meta name="format-detection" content="telephone=no"/>
         <meta name="HandheldFriendly" content="True"/>
