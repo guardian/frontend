b86c94ef2191ff18f7d12916abac26f1a440b214
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-07-28 11:07:37 +0100

    add google random function

--- static/src/javascripts/projects/commercial/modules/third-party-tags/doubleclick-ad-free.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/doubleclick-ad-free.js
@@ -3,9 +3,15 @@
 import { commercialFeatures } from 'commercial/modules/commercial-features';
 import config from 'lib/config';
 
+const doubleClickRandom = (): string => {
+    const axel = Math.random();
+    const a = axel * 10000000000000;
+    return a.toString();
+};
+
 export const doubleClickAdFree: ThirdPartyTag = {
     shouldRun: commercialFeatures.adFree,
     useImage: true,
     url: `//pubads.g.doubleclick.net/activity;dc_iu=/${config.page
-        .dfpAccountId}/;ord=1;af=T;dc_seg=482549580?`,
+        .dfpAccountId}/;ord=${doubleClickRandom()};af=T;dc_seg=482549580?`,
 };
