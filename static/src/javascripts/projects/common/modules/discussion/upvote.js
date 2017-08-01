17eb45f3e678bd55edb60c37f73571128e82b8e9
Simon Adcock <simon.adcock@guardian.co.uk>
2017-08-01 17:06:10 +0100

    flowtype

--- static/src/javascripts/projects/common/modules/discussion/upvote.js
+++ static/src/javascripts/projects/common/modules/discussion/upvote.js
@@ -2,28 +2,41 @@
 import fastdom from 'lib/fastdom-promise';
 import reportError from 'lib/report-error';
 import { getUrlVars, constructQuery } from 'lib/url';
+import discussionApi from 'common/modules/discussion/api';
+import config from 'lib/config';
 
 const RECOMMENDATION_CLASS = 'js-recommend-comment';
 const TOOLTIP_CLASS = 'js-rec-tooltip';
 
-const updateReturnUrl = (links, returnLink) => {
+const updateReturnUrl = (
+    links: NodeList<HTMLElement>,
+    returnLink: ?string
+): void => {
     for (let i = 0, len = links.length; i < len; i += 1) {
         const url = links[i].getAttribute('href');
-        const baseUrl = url.split('?')[0];
-        const query = getUrlVars(url.split('?')[1] || '&');
-        links[i].setAttribute(
-            'href',
-            `${baseUrl}?${constructQuery(
-                Object.assign({}, query, {
-                    returnUrl: returnLink,
-                })
-            )}`
-        );
+
+        if (url) {
+            const baseUrl = url.split('?')[0];
+            const query = getUrlVars(url.split('?')[1] || '&');
+            links[i].setAttribute(
+                'href',
+                `${baseUrl}?${constructQuery(
+                    Object.assign({}, query, {
+                        returnUrl: returnLink,
+                    })
+                )}`
+            );
+        }
     }
 };
 
-const showSignInTooltip = target => {
+const showSignInTooltip = (target: Element): Promise<void> => {
     const tooltip = document.querySelector(`.${TOOLTIP_CLASS}`);
+
+    if (!tooltip) {
+        return Promise.resolve();
+    }
+
     const links = tooltip.querySelectorAll('.js-rec-tooltip-link');
 
     return fastdom.write(() => {
@@ -33,38 +46,49 @@ const showSignInTooltip = target => {
     });
 };
 
-const isOpenForRecommendations = element =>
+const isOpenForRecommendations = (element: Element): boolean =>
     !!element.querySelector('.d-discussion--recommendations-open');
 
-const setClicked = target =>
+const setClicked = (target: Element): Promise<void> =>
     fastdom.write(() => {
         target.classList.remove(RECOMMENDATION_CLASS);
         target.classList.add('d-comment__recommend--clicked');
     });
 
-const unsetClicked = target =>
+const unsetClicked = (target: Element): Promise<void> =>
     fastdom.write(() => {
         target.classList.add(RECOMMENDATION_CLASS);
         target.classList.remove('d-comment__recommend--clicked');
     });
 
-const setRecommended = target =>
+const setRecommended = (target: Element): Promise<void> =>
     fastdom.write(() => {
         target.classList.add('d-comment__recommend--recommended');
     });
 
+type User = {
+    userId: string,
+    displayName: string,
+    webUrl: string,
+    apiUrl: string,
+    avatar: string,
+    secureAvatarUrl: string,
+    badge: Array<string>,
+    details: {
+        gender: string,
+    },
+};
+
 const handle = (
-    target,
-    container,
-    user,
-    discussionApi,
-    allowAnonymousRecommends
-) => {
-    if (!allowAnonymousRecommends && !user) {
+    target: Element,
+    container: Element,
+    user: User
+): ?Promise<void> => {
+    if (!config.switches.discussionAllowAnonymousRecommendsSwitch && !user) {
         target.setAttribute('data-link-name', 'Recommend comment anonymous');
         return showSignInTooltip(target);
     } else if (
-        (allowAnonymousRecommends || user) &&
+        (config.switches.discussionAllowAnonymousRecommendsSwitch || user) &&
         isOpenForRecommendations(container)
     ) {
         const id = target.getAttribute('data-comment-id');
@@ -84,9 +108,13 @@ const handle = (
     }
 };
 
-const closeTooltip = () =>
+const closeTooltip = (): Promise<void> =>
     fastdom.write(() => {
-        document.querySelector(`.${TOOLTIP_CLASS}`).setAttribute('hidden', '');
+        const tooltip = document.querySelector(`.${TOOLTIP_CLASS}`);
+
+        if (tooltip) {
+            tooltip.setAttribute('hidden', '');
+        }
     });
 
 export { handle, closeTooltip };
