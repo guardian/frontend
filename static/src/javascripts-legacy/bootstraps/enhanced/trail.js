efca980675f64cb948f6bd489d37dc3f6d357da1
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-08-01 11:59:07 +0100

    ES6: Convert onward/onward-content

--- static/src/javascripts-legacy/bootstraps/enhanced/trail.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/trail.js
@@ -82,7 +82,7 @@ define([
     function initOnwardContent() {
         insertOrProximity('.js-onward', function () {
             if ((config.page.seriesId || config.page.blogIds) && config.page.showRelatedContent) {
-                new Onward(qwery('.js-onward'));
+                new Onward.OnwardContent(qwery('.js-onward'));
             } else if (config.page.tones !== '') {
                 $('.js-onward').each(function (c) {
                     new TonalComponent.TonalComponent().fetch(c, 'html');
