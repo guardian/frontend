913bdc0df5203d72a87ba337bfbd7066aa34e88f
Simon Adcock <simon.adcock@guardian.co.uk>
2017-08-01 15:58:22 +0100

    linting

--- static/src/javascripts/projects/common/modules/discussion/upvote.js
+++ static/src/javascripts/projects/common/modules/discussion/upvote.js
@@ -1,87 +1,92 @@
-import assign from 'lodash/objects/assign';
+// @flow
 import fastdom from 'lib/fastdom-promise';
 import reportError from 'lib/report-error';
-import urlUtil from 'lib/url';
-var RECOMMENDATION_CLASS = 'js-recommend-comment';
-var TOOLTIP_CLASS = 'js-rec-tooltip';
+import { getUrlVars, constructQuery } from 'lib/url';
 
-function handle(target, container, user, discussionApi, allowAnonymousRecommends) {
-    if (!allowAnonymousRecommends && !user) {
-        target.setAttribute('data-link-name', 'Recommend comment anonymous');
-        return showSignInTooltip(target);
-    } else if ((allowAnonymousRecommends || user) && isOpenForRecommendations(container)) {
-        var id = target.getAttribute('data-comment-id');
+const RECOMMENDATION_CLASS = 'js-recommend-comment';
+const TOOLTIP_CLASS = 'js-rec-tooltip';
 
-        return Promise.all([
-                setClicked(target),
-                discussionApi.recommendComment(id)
-            ])
-            .then(function() {
-                return setRecommended(target);
-            })
-            .catch(function(ex) {
-                return unsetClicked(target).then(function() {
-                    reportError(ex, {
-                        feature: 'comments-recommend'
-                    });
-                });
-            });
+const updateReturnUrl = (links, returnLink) => {
+    for (let i = 0, len = links.length; i < len; i += 1) {
+        const url = links[i].getAttribute('href');
+        const baseUrl = url.split('?')[0];
+        const query = getUrlVars(url.split('?')[1] || '&');
+        links[i].setAttribute(
+            'href',
+            `${baseUrl}?${constructQuery(
+                Object.assign({}, query, {
+                    returnUrl: returnLink,
+                })
+            )}`
+        );
     }
-}
+};
 
-function isOpenForRecommendations(element) {
-    return !!element.querySelector('.d-discussion--recommendations-open');
-}
+const showSignInTooltip = target => {
+    const tooltip = document.querySelector(`.${TOOLTIP_CLASS}`);
+    const links = tooltip.querySelectorAll('.js-rec-tooltip-link');
 
-function setClicked(target) {
-    return fastdom.write(function() {
-        target.classList.remove(RECOMMENDATION_CLASS);
-        target.classList.add('d-comment__recommend--clicked');
-    });
-}
-
-function unsetClicked(target) {
-    return fastdom.write(function() {
-        target.classList.add(RECOMMENDATION_CLASS);
-        target.classList.remove('d-comment__recommend--clicked');
-    });
-}
-
-function setRecommended(target) {
-    return fastdom.write(function() {
-        target.classList.add('d-comment__recommend--recommended');
-    });
-}
-
-function showSignInTooltip(target) {
-    var tooltip = document.querySelector('.' + TOOLTIP_CLASS);
-    var links = tooltip.querySelectorAll('.js-rec-tooltip-link');
-
-    return fastdom.write(function() {
+    return fastdom.write(() => {
         updateReturnUrl(links, target.getAttribute('data-comment-url'));
         tooltip.removeAttribute('hidden');
         target.appendChild(tooltip);
     });
-}
-
-function updateReturnUrl(links, returnLink) {
-    for (var i = 0, len = links.length; i < len; i += 1) {
-        var url = links[i].getAttribute('href');
-        var baseUrl = url.split('?')[0];
-        var query = urlUtil.getUrlVars(url.split('?')[1] || '&');
-        links[i].setAttribute('href', baseUrl + '?' + urlUtil.constructQuery(assign(query, {
-            returnUrl: returnLink
-        })));
-    }
-}
-
-function closeTooltip() {
-    return fastdom.write(function() {
-        document.querySelector('.' + TOOLTIP_CLASS).setAttribute('hidden', '');
-    });
-}
-
-export default {
-    handle: handle,
-    closeTooltip: closeTooltip
 };
+
+const isOpenForRecommendations = element =>
+    !!element.querySelector('.d-discussion--recommendations-open');
+
+const setClicked = target =>
+    fastdom.write(() => {
+        target.classList.remove(RECOMMENDATION_CLASS);
+        target.classList.add('d-comment__recommend--clicked');
+    });
+
+const unsetClicked = target =>
+    fastdom.write(() => {
+        target.classList.add(RECOMMENDATION_CLASS);
+        target.classList.remove('d-comment__recommend--clicked');
+    });
+
+const setRecommended = target =>
+    fastdom.write(() => {
+        target.classList.add('d-comment__recommend--recommended');
+    });
+
+const handle = (
+    target,
+    container,
+    user,
+    discussionApi,
+    allowAnonymousRecommends
+) => {
+    if (!allowAnonymousRecommends && !user) {
+        target.setAttribute('data-link-name', 'Recommend comment anonymous');
+        return showSignInTooltip(target);
+    } else if (
+        (allowAnonymousRecommends || user) &&
+        isOpenForRecommendations(container)
+    ) {
+        const id = target.getAttribute('data-comment-id');
+
+        return Promise.all([
+            setClicked(target),
+            discussionApi.recommendComment(id),
+        ])
+            .then(() => setRecommended(target))
+            .catch(ex =>
+                unsetClicked(target).then(() => {
+                    reportError(ex, {
+                        feature: 'comments-recommend',
+                    });
+                })
+            );
+    }
+};
+
+const closeTooltip = () =>
+    fastdom.write(() => {
+        document.querySelector(`.${TOOLTIP_CLASS}`).setAttribute('hidden', '');
+    });
+
+export { handle, closeTooltip };
