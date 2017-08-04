b1ff7b71ba6be2d978c3ce848a1ad5d35c734ab7
Joseph Smith <joseph.smith@guardian.co.uk>
2017-08-04 10:51:50 +0100

    fixes

--- static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-ask-four-earning.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-ask-four-earning.js
@@ -1,6 +1,7 @@
-import contributionsUtilities from 'common/modules/commercial/contributions-utilities';
+// @flow
+import { makeABTest } from 'common/modules/commercial/contributions-utilities';
 
-export default contributionsUtilities.makeABTest({
+export const askFourEarning: ContributionsABTest = makeABTest({
     id: 'ContributionsEpicAskFourEarning',
     campaignId: 'kr1_epic_ask_four_earning',
 
@@ -8,7 +9,8 @@ export default contributionsUtilities.makeABTest({
     expiry: '2018-07-19',
 
     author: 'Jonathan Rankin',
-    description: 'This places the epic on all articles for all users, with a limit of 4 impressions in any given 30 days',
+    description:
+        'This places the epic on all articles for all users, with a limit of 4 impressions in any given 30 days',
     successMeasure: 'Conversion rate',
     idealOutcome: 'Acquires many Supporters',
 
@@ -16,16 +18,18 @@ export default contributionsUtilities.makeABTest({
     audience: 1,
     audienceOffset: 0,
 
-    variants: [{
-        id: 'control',
-        maxViews: {
-            days: 30,
-            count: 4,
-            minDaysBetweenViews: 0
-        },
+    variants: [
+        {
+            id: 'control',
+            maxViews: {
+                days: 30,
+                count: 4,
+                minDaysBetweenViews: 0,
+            },
 
-        useTailoredCopyForRegulars: true,
-        insertAtSelector: '.submeta',
-        successOnView: true
-    }]
+            useTailoredCopyForRegulars: true,
+            insertAtSelector: '.submeta',
+            successOnView: true,
+        },
+    ],
 });
