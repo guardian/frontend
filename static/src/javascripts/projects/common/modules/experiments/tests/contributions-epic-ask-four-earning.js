9d370b5418aa1eddc5902b23b5c764ed1874a680
Joseph Smith <joseph.smith@guardian.co.uk>
2017-08-04 10:47:27 +0100

    transform projects/common/modules/experiments/tests/contributions-epic-ask-four-earning.js to ES6 module

--- static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-ask-four-earning.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-ask-four-earning.js
@@ -1,38 +1,31 @@
-define([
-    'common/modules/commercial/contributions-utilities'
-], function (
-    contributionsUtilities
-) {
+import contributionsUtilities from 'common/modules/commercial/contributions-utilities';
 
-   return contributionsUtilities.makeABTest({
-        id: 'ContributionsEpicAskFourEarning',
-        campaignId: 'kr1_epic_ask_four_earning',
+export default contributionsUtilities.makeABTest({
+    id: 'ContributionsEpicAskFourEarning',
+    campaignId: 'kr1_epic_ask_four_earning',
 
-        start: '2017-01-24',
-        expiry: '2018-07-19',
+    start: '2017-01-24',
+    expiry: '2018-07-19',
 
-        author: 'Jonathan Rankin',
-        description: 'This places the epic on all articles for all users, with a limit of 4 impressions in any given 30 days',
-        successMeasure: 'Conversion rate',
-        idealOutcome: 'Acquires many Supporters',
+    author: 'Jonathan Rankin',
+    description: 'This places the epic on all articles for all users, with a limit of 4 impressions in any given 30 days',
+    successMeasure: 'Conversion rate',
+    idealOutcome: 'Acquires many Supporters',
 
-        audienceCriteria: 'All',
-        audience: 1,
-        audienceOffset: 0,
+    audienceCriteria: 'All',
+    audience: 1,
+    audienceOffset: 0,
 
-        variants: [
-            {
-                id: 'control',
-                maxViews: {
-                    days: 30,
-                    count: 4,
-                    minDaysBetweenViews: 0
-                },
+    variants: [{
+        id: 'control',
+        maxViews: {
+            days: 30,
+            count: 4,
+            minDaysBetweenViews: 0
+        },
 
-                useTailoredCopyForRegulars: true,
-                insertAtSelector: '.submeta',
-                successOnView: true
-            }
-        ]
-    });
+        useTailoredCopyForRegulars: true,
+        insertAtSelector: '.submeta',
+        successOnView: true
+    }]
 });
