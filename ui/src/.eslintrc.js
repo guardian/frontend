b4804335889374dd378816127a1b177e70f1c636
Alex Sanders <a@snd.rs>
2017-08-09 15:43:25 +0100

    oopsie

--- ui/src/.eslintrc.js
+++ ui/src/.eslintrc.js
@@ -1,6 +1,13 @@
 // @flow
 module.exports = {
     parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: '2017',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
     settings: {
         react: { pragma: 'h' },
         'import/resolver': {
@@ -17,11 +24,14 @@ module.exports = {
     plugins: ['flow-header', 'flowtype'],
     rules: {
         'flow-header/flow-header': 'error',
+
         // flow should take care of our return values
         'consistent-return': 'off',
+
         // react API stuff
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
+
         // these exentions never need to be supplied for imports
         'import/extensions': [
             'error',
