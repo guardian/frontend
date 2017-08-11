f55596490038840d9e260d994e22a4add0ca2dbd
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-08-11 16:47:08 +0100

    Move truth table nearer logic

--- static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
@@ -3,26 +3,6 @@ import { adblockInUse } from 'lib/detect';
 import { waitForCheck } from 'common/modules/check-mediator';
 import { load } from './outbrain-load';
 
-/*
- Outbrain loading behaviour follows the truth table below. This module uses a number of checks
- to determine what kind of outbrain component to show, if any.
-
- outbrain | no high-merch slot OR | high priority ad | low priority ad | contributions | email   | story     |  outbrain
- enabled  | ad blocker enabled    | loaded           | loaded          | ab test       | ab test | question  |  decision
----------------------------------------------------------------------------------------------------------------------------
-  false             n/a                 n/a               n/a              n/a           n/a        n/a       no-outbrain
-  true              true                n/a               n/a              false         false      false     compliant
-  true              true                n/a               n/a              true          n/a        n/a       non-compliant
-  true              true                n/a               n/a              false         true       n/a       non-compliant
-  true              true                n/a               n/a              false         false      true      non-compliant
-  true              false               true              true             n/a           n/a        n/a       no-outbrain
-  true              false               true              false            n/a           n/a        n/a       merchandising
-  true              false               false             n/a              true          n/a        n/a       non-compliant
-  true              false               false             n/a              false         true       n/a       non-compliant
-  true              false               false             n/a              false         false      true      non-compliant
-  true              false               false             n/a              false         false      false     compliant
-*/
-
 type OutbrainPageConditions = {
     outbrainEnabled: boolean,
     noMerchSlotsExpected: boolean,
@@ -90,6 +70,25 @@ export const getOutbrainComplianceTargeting = (): Promise<
         return new Map();
     });
 
+/*
+ Outbrain loading behaviour follows the truth table below. This module uses a number of checks
+ to determine what kind of outbrain component to show, if any.
+
+ outbrain | no high-merch slot OR | high priority ad | low priority ad | contributions | email   | story     |  outbrain
+ enabled  | ad blocker enabled    | loaded           | loaded          | ab test       | ab test | question  |  decision
+---------------------------------------------------------------------------------------------------------------------------
+  false             n/a                 n/a               n/a              n/a           n/a        n/a       no-outbrain
+  true              true                n/a               n/a              false         false      false     compliant
+  true              true                n/a               n/a              true          n/a        n/a       non-compliant
+  true              true                n/a               n/a              false         true       n/a       non-compliant
+  true              true                n/a               n/a              false         false      true      non-compliant
+  true              false               true              true             n/a           n/a        n/a       no-outbrain
+  true              false               true              false            n/a           n/a        n/a       merchandising
+  true              false               false             n/a              true          n/a        n/a       non-compliant
+  true              false               false             n/a              false         true       n/a       non-compliant
+  true              false               false             n/a              false         false      true      non-compliant
+  true              false               false             n/a              false         false      false     compliant
+*/
 export const initOutbrain = () =>
     Promise.all([
         getOutbrainDfpConditions(),
