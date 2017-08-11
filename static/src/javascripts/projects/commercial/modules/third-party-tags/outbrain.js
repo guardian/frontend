8293ec818ffaa3d744b5bcc452c4040f22f672af
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-08-11 15:54:30 +0100

    Split outbrain decision logic into dfp and page-based functions

--- static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
@@ -23,6 +23,47 @@ import { load } from './outbrain-load';
   true              false               false             n/a              false         false      false     compliant
 */
 
+type OutbrainPageConditions = {
+    'outbrain-enabled': boolean,
+    'empty-merch-slots-expected': boolean,
+    'contributions-test-visible': boolean,
+    'email-test-visible': boolean,
+    'story-questions-visible': boolean,
+};
+
+type OutbrainDfpConditions = {
+    'blocked-by-ads': boolean,
+    'use-merchandise-ad-slot': boolean,
+};
+
+const emptyMerchSlotsExpected = (): Promise<boolean> =>
+    // Loading Outbrain is dependent on successful return of high relevance component
+    // from DFP. AdBlock is blocking DFP calls so we are not getting any response and thus
+    // not loading Outbrain. As Outbrain is being partially loaded behind the adblock we can
+    // make the call instantly when we detect adBlock in use.
+    adblockInUse.then(
+        isUse =>
+            !document.getElementById('dfp-ad--merchandising-high') || !!isUse
+    );
+
+// These are the on-page conditions which influence the outbrain load. We don't need to wait for DFP to find the value of these.
+const getOutbrainPageConditions = (): Promise<OutbrainPageConditions> =>
+    Promise.all([
+        waitForCheck('isOutbrainDisabled'),
+        emptyMerchSlotsExpected(),
+        waitForCheck('isUserInContributionsAbTest'),
+        waitForCheck('isUserInEmailAbTestAndEmailCanRun'),
+        waitForCheck('isStoryQuestionsOnPage'),
+    ]).then(
+        ([outbrainDisabled, emptyMerchSlots, contributions, email, story]) => ({
+            'outbrain-enabled': !outbrainDisabled,
+            'empty-merch-slots-expected': emptyMerchSlots,
+            'contributions-test-visible': contributions,
+            'email-test-visible': email,
+            'story-questions-visible': story,
+        })
+    );
+
 const canLoadInstantly = () =>
     // Loading Outbrain is dependent on successful return of high relevance component
     // from DFP. AdBlock is blocking DFP calls so we are not getting any response and thus
@@ -104,6 +145,30 @@ const onIsOutbrainDisabled = outbrainDisabled => {
 
     return canLoadInstantly().then(onCanLoadInstantly);
 };
+
+export const getOutbrainDfpConditions = (): Promise<OutbrainDfpConditions> =>
+    Promise.all([
+        waitForCheck('isOutbrainBlockedByAds'),
+        waitForCheck('isOutbrainMerchandiseCompliant'),
+    ]).then(([blockedByAds, merchandiseCompliant]) => ({
+        'blocked-by-ads': blockedByAds,
+        'use-merchandise-ad-slot': merchandiseCompliant,
+    }));
+
+export const getOutbrainComplianceTargeting = (): Promise<
+    Map<string, string>
+> =>
+    getOutbrainPageConditions().then(pageConditions => {
+        if (
+            pageConditions['contributions-test-visible'] ||
+            pageConditions['email-test-visible'] ||
+            pageConditions['story-questions-visible'] ||
+            !pageConditions['outbrain-enabled']
+        ) {
+            return new Map([['outbrain-compliant', 'false']]);
+        }
+        return new Map();
+    });
 
 export const initOutbrain = () =>
     waitForCheck('isOutbrainDisabled').then(onIsOutbrainDisabled);
