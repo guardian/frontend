c2a4a4095d07247ddbf567a53f4f1193f90ce414
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-08-11 10:44:14 +0100

    Add truth table for outbrain decision-making

--- static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
@@ -4,12 +4,30 @@ import { waitForCheck } from 'common/modules/check-mediator';
 import { load } from './outbrain-load';
 
 /*
- Loading Outbrain is dependent on successful return of high relevance component
- from DFP. AdBlock is blocking DFP calls so we are not getting any response and thus
- not loading Outbrain. As Outbrain is being partially loaded behind the adblock we can
- make the call instantly when we detect adBlock in use.
+ Outbrain loading behaviour follows the truth table below. This module uses a number of checks
+ to determine what kind of outbrain component to show, if any.
+
+ outbrain | no high-merch slot OR | high priority ad | low priority ad | contributions | email   | story     |  outbrain
+ enabled  | ad blocker enabled    | loaded           | loaded          | ab test       | ab test | question  |  decision
+---------------------------------------------------------------------------------------------------------------------------
+  false             n/a                 n/a               n/a              n/a           n/a        n/a       no-outbrain
+  true              true                n/a               n/a              false         false      false     compliant
+  true              true                n/a               n/a              true          n/a        n/a       non-compliant
+  true              true                n/a               n/a              false         true       n/a       non-compliant
+  true              true                n/a               n/a              false         false      true      non-compliant
+  true              false               false             false            false         false      false     compliant
+  true              false               true              true             n/a           n/a        n/a       no-outbrain
+  true              false               true              false            n/a           n/a        n/a       merchandising
+  true              false               false             true             true          n/a        n/a       non-compliant
+  true              false               false             true             false         true       n/a       non-compliant
+  true              false               false             true             false         false      true      non-compliant
 */
+
 const canLoadInstantly = () =>
+    // Loading Outbrain is dependent on successful return of high relevance component
+    // from DFP. AdBlock is blocking DFP calls so we are not getting any response and thus
+    // not loading Outbrain. As Outbrain is being partially loaded behind the adblock we can
+    // make the call instantly when we detect adBlock in use.
     adblockInUse.then(
         isUse => !document.getElementById('dfp-ad--merchandising-high') || isUse
     );
