c5eaaae45607d15754923082d83a0c2823fe706c
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-08-11 18:08:18 +0100

    Do not warmup Rendering actor in test/dev

--- common/app/rendering/core/RenderingActor.scala
+++ common/app/rendering/core/RenderingActor.scala
@@ -2,29 +2,35 @@ package rendering.core
 
 import akka.actor.Actor
 import common.StopWatch
+import model.ApplicationContext
+import play.api.Mode
 import rendering.Renderable
+
 import scala.util.Try
 
-case class Rendering(renderable: Renderable, forceReload: Boolean = false)
+case class Rendering(renderable: Renderable)
 
 case class RenderingException(error: String) extends RuntimeException(error)
 
-class RenderingActor extends Actor with JavascriptRendering {
+class RenderingActor(ac: ApplicationContext) extends Actor with JavascriptRendering {
 
   override def javascriptFile: String = "ui/dist/ui.bundle.server.js"
 
-  override def preStart: Unit = {
-    // Warming up the script engine
-    val stopWatch = new StopWatch
-    (1 to 100).foreach(_ => render(None))
-    log.info(s"Warming up rendering actor completed in ${stopWatch.elapsed}s")
+  val isRunningInProd = ac.environment.mode == Mode.Prod
 
+  override def preStart: Unit = {
+    // Warming up the script engine in PROD
+    if(isRunningInProd) {
+      val stopWatch = new StopWatch
+      (1 to 100).foreach(_ => render(None))
+      log.info(s"Warming up rendering actor completed in ${stopWatch.elapsed}s")
+    }
     super.preStart()
   }
 
   override def receive: Receive = {
-    case Rendering(renderable, forceReload) =>
-      sender ! render(renderable.props, forceReload)
+    case Rendering(renderable) =>
+      sender ! render(renderable.props, isRunningInProd)
     case  _ =>
       sender ! Try(throw new RenderingException("RenderingActor received an unknown message"))
   }
