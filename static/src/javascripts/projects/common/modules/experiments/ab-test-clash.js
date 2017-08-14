f3e05dcda5852e0592f4aa1de42b9e232aec7b94
Joseph Smith <joseph.smith@guardian.co.uk>
2017-08-14 11:53:12 +0100

    transform projects/common/modules/experiments/ab-test-clash.js to ES6 module

--- static/src/javascripts/projects/common/modules/experiments/ab-test-clash.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-test-clash.js
@@ -1,43 +1,35 @@
-define([
-    'lodash/collections/some',
-    'common/modules/experiments/utils',
-    'common/modules/experiments/acquisition-test-selector'
-], function (
-    some,
-    abUtils,
-    acquisitionTestSelector
-) {
+import some from 'lodash/collections/some';
+import abUtils from 'common/modules/experiments/utils';
+import acquisitionTestSelector from 'common/modules/experiments/acquisition-test-selector';
 
-    var emailTests = [];
-    var contributionsTests = acquisitionTestSelector.abTestClashData;
-    var clashingTests = contributionsTests.concat(emailTests);
+var emailTests = [];
+var contributionsTests = acquisitionTestSelector.abTestClashData;
+var clashingTests = contributionsTests.concat(emailTests);
 
-    function userIsInAClashingAbTest(tests) {
-        tests = tests || clashingTests;
+function userIsInAClashingAbTest(tests) {
+    tests = tests || clashingTests;
 
-        return _testABClash(abUtils.isInVariant, tests);
-    }
+    return _testABClash(abUtils.isInVariant, tests);
+}
 
-    function _testABClash(f, clashingTests) {
-        if (clashingTests.length > 0) {
-            return some(clashingTests, function (test) {
-                return test.variants.filter(function (variant) {
-                    var compliant = variant && variant.options && variant.options.isOutbrainCompliant;
-                    return !compliant;
-                }).some(function (variant) {
-                    return f(test, variant);
-                });
+function _testABClash(f, clashingTests) {
+    if (clashingTests.length > 0) {
+        return some(clashingTests, function(test) {
+            return test.variants.filter(function(variant) {
+                var compliant = variant && variant.options && variant.options.isOutbrainCompliant;
+                return !compliant;
+            }).some(function(variant) {
+                return f(test, variant);
             });
-        }
-        else {
-            return false;
-        }
+        });
+    } else {
+        return false;
     }
+}
 
-    return {
-        userIsInAClashingAbTest: userIsInAClashingAbTest,
-        contributionsTests: contributionsTests,
-        emailTests: emailTests,
-        _testABClash: _testABClash // exposed for unit testing
-    };
-});
+export default {
+    userIsInAClashingAbTest: userIsInAClashingAbTest,
+    contributionsTests: contributionsTests,
+    emailTests: emailTests,
+    _testABClash: _testABClash // exposed for unit testing
+};
