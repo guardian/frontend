6d61b9ae6a034c679dd797437e2449b1e308e960
Alex Sanders <a@snd.rs>
2017-08-15 11:47:32 +0100

    add tracking back

--- ui/src/views/404/index.jsx
+++ ui/src/views/404/index.jsx
@@ -42,4 +42,42 @@ export default () =>
             <Navigation />
         </div>
         <Footer />
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                    ga('create', 'UA-78705427-1', 'auto');
+                    ga('set', 'dimension3', 'theguardian.com');
+                    ga('set', 'dimension14', '404');
+                    ga('send', 'pageview');
+                `,
+            }}
+        />
+
+        <script src="https://pasteup.guim.co.uk/js/lib/requirejs/2.1.5/require.min.js" />
+
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+                    require.config({
+                        paths: {
+                            'ophan/http-status' : '//j.ophan.co.uk/ophan.http-status',
+                        }
+                    });
+                    require(['ophan/http-status'], function(reporter) {
+                    reporter.reportStatus('next-gen', 404);
+                    });
+                `,
+            }}
+        />
+
+        <img
+            src="@{Configuration.debug.beaconUrl}/count/40x.gif"
+            alt=""
+            style={{ display: 'none' }}
+            rel="nofollow"
+        />
     </div>;
