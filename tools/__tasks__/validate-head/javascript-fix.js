02767cd97b875c48da46e2508bd9dd70692cce84
Simon Adcock <simon.adcock@guardian.co.uk>
2017-08-15 17:29:05 +0100

    fix-commit is a single task

--- tools/__tasks__/validate-head/javascript-fix.js
+++ tools/__tasks__/validate-head/javascript-fix.js
@@ -3,21 +3,13 @@ const getChangedFiles = require('../lib/get-changed-files');
 
 module.exports = {
     description: 'Fix committed linting errors',
-    task: [
-        {
-            description: 'Fix changed JS linting errors',
-            task: () =>
-                getChangedFiles().then(files => {
-                    const jsFiles = files.filter(
-                        file =>
-                            file.endsWith('.js') ||
-                            file === 'git-hooks/pre-push'
-                    );
-                    const config = ['--quiet', '--color', '--fix'];
+    task: () =>
+        getChangedFiles().then(files => {
+            const jsFiles = files.filter(
+                file => file.endsWith('.js') || file === 'git-hooks/pre-push'
+            );
+            const config = ['--quiet', '--color', '--fix'];
 
-                    return execa('eslint', jsFiles.concat(config));
-                }),
-        },
-    ],
-    concurrent: true,
+            return execa('eslint', jsFiles.concat(config));
+        }),
 };
