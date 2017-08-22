3c47159d8f60d5478e331d34f329f9eba6dc17cb
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-08-22 15:35:32 +0100

    Added paper subs types for support variant.

--- static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-support-baseline.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-support-baseline.js
@@ -125,6 +125,10 @@ export const acquisitionsSupportBaseline = makeABTest({
                 'CONTRIBUTION',
                 'RECURRING_CONTRIBUTION',
                 'DIGITAL_SUBSCRIPTION',
+                'PAPER_SUBSCRIPTION_EVERYDAY',
+                'PAPER_SUBSCRIPTION_SIXDAY',
+                'PAPER_SUBSCRIPTION_WEEKEND',
+                'PAPER_SUBSCRIPTION_SUNDAY',
             ],
 
             // EPIC
