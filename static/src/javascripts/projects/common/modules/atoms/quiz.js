3f4be2ca870a21170c008f76a9f20ad46678e557
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-08-23 10:04:54 +0100

    ES6 atom quizzes

--- static/src/javascripts/projects/common/modules/atoms/quiz.js
+++ static/src/javascripts/projects/common/modules/atoms/quiz.js
@@ -1,45 +1,56 @@
+// @flow
 import bean from 'bean';
-import $ from 'lib/$';
 import fastdom from 'lib/fastdom-promise';
-import toArray from 'lodash/collections/toArray';
+
 export default {
     // find a bucket message to show once you finish a quiz
-    handleCompletion() {
+    handleCompletion(): void {
         // we're only handling completion in browsers who can validate forms natively
         // others do a round trip to the server
-        if (HTMLFormElement.prototype.checkValidity) {
-            // quizzes can be set to only show answers at the end, in which case we do a round trip.
-            // we'll run this code only if it's an instant-reveal quiz
-            const $quizzes = $('.js-atom-quiz--instant-reveal');
-
-            if ($quizzes.length > 0) {
-                bean.on(document, 'click', toArray($quizzes), e => {
-                    const quiz = e.currentTarget;
-                    let total = $(':checked + .atom-quiz__answer__item--is-correct', quiz).length;
-
-                    if (quiz.checkValidity()) { // the form (quiz) is complete
-                        let $bucket__message = null;
-                        do {
-                            // try and find a .bucket__message for your total
-                            $bucket__message = $('.js-atom-quiz__bucket-message--' + total, quiz);
-
-                            // if we find a message for your total show it, and exit
-                            if ($bucket__message.length > 0) {
-                                fastdom.write(() => {
-                                    $bucket__message.css({
-                                        'display': 'block'
-                                    });
-                                });
-                                break;
-                            }
-
-                            // if we haven't exited, there's no .bucket__message for your score, so you must be in
-                            // a bucket with a range that begins below your total score
-                            total--;
-                        } while (total >= 0); // the lowest we'll look is for 0 correct answers
-                    }
-                });
-            }
+        if (!HTMLFormElement.prototype.checkValidity) {
+            return;
         }
-    }
+
+        // quizzes can be set to only show answers at the end, in which case we do a round trip.
+        // we'll run this code only if it's an instant-reveal quiz
+        const quizzes: Element[] = Array.from(
+            document.getElementsByClassName('js-atom-quiz--instant-reveal')
+        );
+
+        if (!quizzes.length) {
+            return;
+        }
+
+        bean.on(document, 'click', quizzes, function onClick(e: Event): void {
+            const quiz: Element = (e.currentTarget: any);
+            let total: number = quiz.querySelectorAll(
+                ':checked + .atom-quiz__answer__item--is-correct'
+            ).length;
+
+            if (!quiz.checkValidity()) {
+                // the form (quiz) is complete
+                return;
+            }
+
+            do {
+                // try and find a .bucket__message for your total
+                const bucketMessage: ?Element = quiz.querySelector(
+                    `.js-atom-quiz__bucket-message--${total}`
+                );
+
+                // if we find a message for your total show it, and exit
+                if (bucketMessage) {
+                    fastdom.write(() => {
+                        bucketMessage.style.display = 'block';
+                    });
+                    bean.off(document, 'click', onClick);
+                    break;
+                }
+
+                // if we haven't exited, there's no .bucket__message for your score, so you must be in
+                // a bucket with a range that begins below your total score
+                total -= 1;
+            } while (total >= 0); // the lowest we'll look is for 0 correct answers
+        });
+    },
 };
