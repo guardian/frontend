c68cc694433c88a735aaa5d726ccfbf9c873b65f
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-08-24 13:03:48 +0100

    ES6 a11y

--- static/src/javascripts/bootstraps/enhanced/accessibility.js
+++ static/src/javascripts/bootstraps/enhanced/accessibility.js
@@ -1,23 +1,24 @@
+// @flow
 import React from 'react/addons';
 import accessibility from 'common/modules/accessibility/main';
 
-const module = {
-    DOM_ID: 'js-accessibility-preferences',
-    init
-};
+const DOM_ID: string = 'js-accessibility-preferences';
 
-function init(callback) {
-    const BinaryToggle = React.createClass({
-        render() {
-            return React.DOM.div({
-                    className: 'form-field'
+const BinaryToggle = React.createClass({
+    render() {
+        return React.DOM.div(
+            {
+                className: 'form-field',
+            },
+            React.DOM.div(
+                {
+                    className: 'checkbox',
                 },
-                React.DOM.div({
-                        className: 'checkbox'
+                React.DOM.label(
+                    {
+                        className: 'label',
                     },
-                    React.DOM.label({
-                        className: 'label'
-                    }, [
+                    [
                         React.DOM.input({
                             key: this.props.name,
                             type: 'checkbox',
@@ -31,65 +32,85 @@ function init(callback) {
                             // The style object should be removed once we upgrade. Never probably.
                             style: {
                                 float: 'none',
-                                margin: '3px 0.5ex'
-                            }
-                        })
-                    ].concat(this.props.label))
+                                margin: '3px 0.5ex',
+                            },
+                        }),
+                    ].concat(this.props.label)
                 )
-            );
-        }
-    });
+            )
+        );
+    },
+});
 
-    const Accessibility = React.createClass({
-        getInitialState() {
-            return {
-                'flashing-elements': accessibility.isOn('flashing-elements')
-            };
-        },
-        toggle(key) {
-            const newState = {};
-            newState[key] = !this.state[key];
-            this.setState(newState, function() {
-                accessibility.saveState(this.state);
-            });
-        },
-        render() {
-            return React.DOM.form({
-                    className: 'form'
+const Accessibility = React.createClass({
+    getInitialState() {
+        return {
+            'flashing-elements': accessibility.isOn('flashing-elements'),
+        };
+    },
+
+    toggle(key: string): void {
+        const newState = {};
+        newState[key] = !this.state[key];
+        this.setState(newState, function() {
+            accessibility.saveState(this.state);
+        });
+    },
+
+    render(): Object {
+        return React.DOM.form(
+            {
+                className: 'form',
+            },
+            React.DOM.fieldset(
+                {
+                    className: 'fieldset',
                 },
-                React.DOM.fieldset({
-                    className: 'fieldset'
-                }, [
-                    React.DOM.p({
-                        key: 'p1'
-                    }, 'We aim to make this site accessible to a wide audience and to ensure a great experience for all users by conforming to World Wide Web Consortium accessibility guidelines (W3C\'s WCAG)'),
-                    React.DOM.p({
-                        key: 'p2'
-                    }, 'However, if you are having trouble reading this website you can change the way it looks or disable some of its functionalities.'),
+                [
+                    React.DOM.p(
+                        {
+                            key: 'p1',
+                        },
+                        "We aim to make this site accessible to a wide audience and to ensure a great experience for all users by conforming to World Wide Web Consortium accessibility guidelines (W3C's WCAG)"
+                    ),
+                    React.DOM.p(
+                        {
+                            key: 'p2',
+                        },
+                        'However, if you are having trouble reading this website you can change the way it looks or disable some of its functionalities.'
+                    ),
                     React.createElement(BinaryToggle, {
                         key: 'flashing-elements',
                         name: 'flashing-elements',
                         label: [
-                            React.DOM.strong({
-                                key: 'label'
-                            }, 'Allow flashing elements'),
-                            this.state['flashing-elements'] ?
-                            ' Untick this to disable flashing and moving elements' :
-                            ' Tick this to enable flashing or moving elements.'
+                            React.DOM.strong(
+                                {
+                                    key: 'label',
+                                },
+                                'Allow flashing elements'
+                            ),
+                            this.state['flashing-elements']
+                                ? ' Untick this to disable flashing and moving elements'
+                                : ' Tick this to enable flashing or moving elements.',
                         ],
                         enabled: this.state['flashing-elements'],
-                        handleChange: this.toggle.bind(this, 'flashing-elements')
-                    })
-                ])
-            );
-        }
-    });
+                        handleChange: this.toggle.bind(
+                            this,
+                            'flashing-elements'
+                        ),
+                    }),
+                ]
+            )
+        );
+    },
+});
 
+const init = (callback: () => void): void => {
     React.render(
         React.createElement(Accessibility),
-        document.getElementById(module.DOM_ID),
+        document.getElementById(DOM_ID),
         callback
     );
-}
+};
 
-export default module;
+export { DOM_ID, init };
