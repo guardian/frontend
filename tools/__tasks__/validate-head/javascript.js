4f8ba0f250e989b010cfc5d866f5157cf06e2b67
Simon Adcock <simon.adcock@guardian.co.uk>
2017-08-25 12:36:36 +0100

    lint changes as batch

--- tools/__tasks__/validate-head/javascript.js
+++ tools/__tasks__/validate-head/javascript.js
@@ -9,23 +9,65 @@ module.exports = {
             description: 'Lint committed JS',
             task: () =>
                 getChangedFiles().then(files => {
-                    const jsFiles = files
-                        .filter(
-                            file =>
-                                file.endsWith('.js') ||
-                                file === 'git-hooks/pre-push'
-                        )
-                        .join(' ');
+                    const errors = [];
+                    const jsFiles = files.filter(
+                        file =>
+                            file.endsWith('.js') ||
+                            file === 'git-hooks/pre-push'
+                    );
+                    const lint = (proc, batchedFiles) =>
+                        proc.then(() =>
+                            Promise.all(
+                                batchedFiles.map(filePath =>
+                                    execa
+                                        .shell(
+                                            `git show HEAD:${filePath} | eslint --stdin --stdin-filename ${filePath}`
+                                        )
+                                        .catch(e => {
+                                            errors.push(e);
+                                        })
+                                )
+                            )
+                        );
+                    const batch = (arr, batchSize) => {
+                        const batchFold = (xss, x) => {
+                            if (!xss.length) {
+                                return [[x]];
+                            }
+                            if (xss[0].length < batchSize) {
+                                return [xss[0].concat(x), ...xss.slice(1)];
+                            }
 
-                    return execa.shell(`eslint ${jsFiles}`).catch(e => {
-                        e.stdout += `\n${chalk.red(
-                            `✋ Linting failed. You can attempt to fix lint errors by running ${chalk.underline(
-                                'make fix-commits'
-                            )}.\nYour changes have not been pushed`
-                        )}`;
+                            return [[x], ...xss];
+                        };
 
-                        return Promise.reject(e);
-                    });
+                        return arr.reduce(batchFold, []);
+                    };
+
+                    return batch(jsFiles, 10)
+                        .reduce(lint, Promise.resolve())
+                        .then(() => {
+                            if (errors.length) {
+                                const error = errors.reduce(
+                                    (acc, curr) => {
+                                        acc.stdout += curr.stdout;
+
+                                        return acc;
+                                    },
+                                    { stdout: '' }
+                                );
+
+                                error.stdout += `\n${chalk.red(
+                                    `✋ Linting failed. You can attempt to fix lint errors by running ${chalk.underline(
+                                        'make fix-commits'
+                                    )}.\nYour changes have not been pushed`
+                                )}`;
+
+                                return Promise.reject(error);
+                            }
+
+                            return Promise.resolve();
+                        });
                 }),
         },
         {
