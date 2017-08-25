c757e6d23eaf722f94a9dbc41a2c62e5f3669076
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-08-25 17:21:43 +0100

    Fix validation errors

--- static/src/javascripts/projects/common/modules/identity/email-preferences.js
+++ static/src/javascripts/projects/common/modules/identity/email-preferences.js
@@ -1,3 +1,4 @@
+// @flow
 // This is a workaround for the email preferences page https://profile.thegulocal.com/email-prefs
 // We want to submit subscribe/unsubscribe requests without a full page refresh
 // Hopefully this will be short-lived; if it is still alive in 2017, git blame and cry
@@ -7,7 +8,110 @@ import reqwest from 'reqwest';
 import fastdom from 'fastdom';
 import $ from 'lib/$';
 
-function reqwestEmailSubscriptionUpdate(buttonEl) {
+const addUpdatingState = buttonEl => {
+    fastdom.write(() => {
+        buttonEl.disabled = true;
+        $(buttonEl).addClass('is-updating is-updating-subscriptions');
+    });
+};
+
+const generateFormQueryString = buttons => {
+    const csrfToken = $('.form')[0].elements.csrfToken.value.toString();
+    const htmlPreference = $('[name="htmlPreference"]:checked').val();
+    let buttonString = '';
+    for (let i = 0; i < buttons.length; i += 1) {
+        const value = buttons[i].value;
+        const unsubscribeMatches = value.match(/unsubscribe-(.*)/);
+        if (unsubscribeMatches) {
+            const listIds = unsubscribeMatches[1].split(',');
+            for (let j = 0; j < listIds.length; j += 1) {
+                buttonString += `removeEmailSubscriptions[]=${encodeURIComponent(
+                    listIds[j]
+                )}&`;
+            }
+        } else {
+            buttonString += `addEmailSubscriptions[]=${encodeURIComponent(
+                value
+            )}&`;
+        }
+    }
+    return `csrfToken=${encodeURIComponent(
+        csrfToken
+    )}&${buttonString}htmlPreference=${encodeURIComponent(htmlPreference)}`;
+};
+
+const clearErrorMessages = () => {
+    if ($('.form__error')) {
+        $.forEachElement('.form__error', errorEl => {
+            errorEl.parentNode.removeChild(errorEl);
+        });
+    }
+};
+
+const renderErrorMessage = buttonEl =>
+    fastdom.write(() => {
+        clearErrorMessages();
+        const errorMessage = $.create(
+            '<div class="form__error">' +
+                'Sorry, an error has occurred, please refresh the page and try again' +
+                '</div>'
+        );
+        $(errorMessage).insertAfter(buttonEl.parentNode);
+    });
+
+const updateSubscriptionButton = buttonEl => {
+    const buttonVal = buttonEl.value;
+    const isSubscribing = !/unsubscribe/.test(buttonVal);
+
+    if (isSubscribing) {
+        fastdom.write(() => {
+            $(buttonEl).removeClass('is-updating is-updating-subscriptions');
+            buttonEl.value = `unsubscribe-${buttonVal}`;
+            buttonEl.innerHTML = 'Unsubscribe';
+            $($.ancestor(buttonEl, 'email-subscription')).addClass(
+                'email-subscription--subscribed'
+            );
+            buttonEl.disabled = false;
+        });
+    } else {
+        fastdom.write(() => {
+            $(buttonEl).removeClass('is-updating is-updating-subscriptions');
+            buttonEl.value = buttonVal.replace('unsubscribe-', '');
+            buttonEl.innerHTML = 'Subscribe';
+            $($.ancestor(buttonEl, 'email-subscription')).removeClass(
+                'email-subscription--subscribed'
+            );
+            buttonEl.disabled = false;
+        });
+    }
+};
+
+const updateButton = buttonEl => {
+    if ($(buttonEl).hasClass('js-subscription-button')) {
+        updateSubscriptionButton(buttonEl);
+    } else {
+        fastdom.write(() => {
+            setTimeout(() => {
+                $(buttonEl).removeClass(
+                    'is-updating is-updating-subscriptions'
+                );
+                buttonEl.disabled = false;
+            }, 1000);
+        });
+    }
+};
+
+const resetUnsubscribeFromAll = buttonEl => {
+    fastdom.write(() => {
+        $(buttonEl).removeClass(
+            'email-unsubscribe--confirm js-confirm-unsubscribe'
+        );
+        $('.js-unsubscribe--confirm').addClass('hide');
+        $('.js-unsubscribe--basic').removeClass('hide');
+    });
+};
+
+const reqwestEmailSubscriptionUpdate = buttonEl => {
     bean.on(buttonEl, 'click', () => {
         addUpdatingState(buttonEl);
         const formQueryString = generateFormQueryString([buttonEl]);
@@ -20,12 +124,12 @@ function reqwestEmailSubscriptionUpdate(buttonEl) {
             },
             success() {
                 updateButton(buttonEl);
-            }
+            },
         });
     });
-}
+};
 
-function reqwestUnsubscribeFromAll(buttonEl, subscribedButtons) {
+const reqwestUnsubscribeFromAll = (buttonEl, subscribedButtons) => {
     const formQueryString = generateFormQueryString(subscribedButtons);
     reqwest({
         url: '/email-prefs',
@@ -35,15 +139,24 @@ function reqwestUnsubscribeFromAll(buttonEl, subscribedButtons) {
             renderErrorMessage(buttonEl);
         },
         success() {
-            for (let i = 0; i < subscribedButtons.length; i++) {
+            for (let i = 0; i < subscribedButtons.length; i += 1) {
                 updateSubscriptionButton(subscribedButtons[i]);
             }
             updateButton(buttonEl);
-        }
+        },
     });
-}
+};
 
-function unsubscribeFromAll(buttonEl) {
+const confirmUnsubscriptionFromAll = buttonEl => {
+    fastdom.write(() => {
+        $(buttonEl).addClass(
+            'email-unsubscribe--confirm js-confirm-unsubscribe'
+        );
+        $('.email-unsubscribe-all__label').toggleClass('hide');
+    });
+};
+
+const unsubscribeFromAll = buttonEl => {
     bean.on(buttonEl, 'click', () => {
         if ($(buttonEl).hasClass('js-confirm-unsubscribe')) {
             addUpdatingState(buttonEl);
@@ -53,109 +166,10 @@ function unsubscribeFromAll(buttonEl) {
             confirmUnsubscriptionFromAll(buttonEl);
         }
     });
-}
+};
 
-function confirmUnsubscriptionFromAll(buttonEl) {
-    fastdom.write(() => {
-        $(buttonEl).addClass('email-unsubscribe--confirm js-confirm-unsubscribe');
-        $('.email-unsubscribe-all__label').toggleClass('hide');
-    });
-}
-
-function resetUnsubscribeFromAll(buttonEl) {
-    fastdom.write(() => {
-        $(buttonEl).removeClass('email-unsubscribe--confirm js-confirm-unsubscribe');
-        $('.js-unsubscribe--confirm').addClass('hide');
-        $('.js-unsubscribe--basic').removeClass('hide');
-    });
-}
-
-function renderErrorMessage(buttonEl) {
-    return fastdom.write(() => {
-        clearErrorMessages();
-        const errorMessage = $.create(
-            '<div class="form__error">' +
-            'Sorry, an error has occurred, please refresh the page and try again' +
-            '</div>'
-        );
-        $(errorMessage).insertAfter(buttonEl.parentNode);
-    });
-}
-
-function clearErrorMessages() {
-    if ($('.form__error')) {
-        $.forEachElement('.form__error', errorEl => {
-            errorEl.parentNode.removeChild(errorEl);
-        });
-    }
-}
-
-function addUpdatingState(buttonEl) {
-    fastdom.write(() => {
-        buttonEl.disabled = true;
-        $(buttonEl).addClass('is-updating is-updating-subscriptions');
-    });
-}
-
-function updateSubscriptionButton(buttonEl) {
-    const buttonVal = buttonEl.value;
-    const isSubscribing = !/unsubscribe/.test(buttonVal);
-
-    if (isSubscribing) {
-        fastdom.write(() => {
-            $(buttonEl).removeClass('is-updating is-updating-subscriptions');
-            buttonEl.value = 'unsubscribe-' + buttonVal;
-            buttonEl.innerHTML = 'Unsubscribe';
-            $($.ancestor(buttonEl, 'email-subscription')).addClass('email-subscription--subscribed');
-            buttonEl.disabled = false;
-        });
-    } else {
-        fastdom.write(() => {
-            $(buttonEl).removeClass('is-updating is-updating-subscriptions');
-            buttonEl.value = buttonVal.replace('unsubscribe-', '');
-            buttonEl.innerHTML = 'Subscribe';
-            $($.ancestor(buttonEl, 'email-subscription')).removeClass('email-subscription--subscribed');
-            buttonEl.disabled = false;
-        });
-    }
-}
-
-function updateButton(buttonEl) {
-    if ($(buttonEl).hasClass('js-subscription-button')) {
-        updateSubscriptionButton(buttonEl);
-    } else {
-        fastdom.write(() => {
-            setTimeout(() => {
-                $(buttonEl).removeClass('is-updating is-updating-subscriptions');
-                buttonEl.disabled = false;
-            }, 1000);
-
-        });
-    }
-}
-
-function generateFormQueryString(buttons) {
-    const csrfToken = ($('.form')[0].elements.csrfToken.value).toString();
-    const htmlPreference = $('[name="htmlPreference"]:checked').val();
-    let buttonString = '';
-    for (let i = 0; i < buttons.length; i++) {
-        const value = buttons[i].value;
-        const unsubscribeMatches = value.match(/unsubscribe-(.*)/);
-        if (unsubscribeMatches) {
-            const listIds = unsubscribeMatches[1].split(",");
-            for (let j = 0; j < listIds.length; j++) {
-                buttonString += 'removeEmailSubscriptions[]=' + encodeURIComponent(listIds[j]) + '&';
-            }
-        } else {
-            buttonString += 'addEmailSubscriptions[]=' + encodeURIComponent(value) + '&';
-        }
-    }
-    return 'csrfToken=' + encodeURIComponent(csrfToken) + '&' +
-        buttonString + 'htmlPreference=' + encodeURIComponent(htmlPreference);
-}
-
-export function enhanceEmailPreferences() {
+export const enhanceEmailPreferences = () => {
     $.forEachElement('.js-subscription-button', reqwestEmailSubscriptionUpdate);
     $.forEachElement('.js-save-button', reqwestEmailSubscriptionUpdate);
     $.forEachElement('.js-unsubscribe', unsubscribeFromAll);
-}
+};
--- static/src/javascripts/projects/common/modules/identity/forms.js
+++ static/src/javascripts/projects/common/modules/identity/forms.js
@@ -1,59 +1,75 @@
+// @flow
 import bean from 'bean';
 import bonzo from 'bonzo';
 
-export function forgottenEmail(): void {
+export const forgottenEmail = (): void => {
     let hashEmail;
     let input;
-    const form = document.body.querySelector('.js-reset-form');
-    if (form) {
-        hashEmail = window.location.hash.match('email=([^&#]*)');
-        if (hashEmail) {
+
+    if (document.body) {
+        const form = document.body.querySelector('.js-reset-form');
+        if (form) {
+            hashEmail = window.location.hash.match('email=([^&#]*)');
             input = form.querySelector('.js-reset-email');
-            input.value = hashEmail[1];
+            if (hashEmail && input) {
+                input.value = hashEmail[1];
+            }
         }
     }
-}
+};
 
-export function forgottenPassword(): void {
+export const forgottenPassword = (): void => {
     let email;
     let link;
     let href;
-    const form = document.body.querySelector('.js-signin-form');
-    if (form) {
-        email = form.querySelector('.js-signin-email');
-        link = form.querySelector('.js-forgotten-password');
-        href = link.getAttribute('href');
 
-        bean.add(link, 'click', () => {
-            const emailAddress = email.value;
-            if (emailAddress !== '') {
-                link.setAttribute('href', href + '#email=' + emailAddress);
-            }
-        });
+    if (document.body) {
+        const form = document.body.querySelector('.js-signin-form');
+        if (form) {
+            email = form.querySelector('.js-signin-email');
+            link = form.querySelector('.js-forgotten-password');
+            href = link.getAttribute('href');
+
+            bean.add(link, 'click', () => {
+                const emailAddress = email.value;
+                if (emailAddress !== '') {
+                    link.setAttribute('href', `${href}#email=${emailAddress}`);
+                }
+            });
+        }
     }
-}
+};
 
-export function passwordToggle(): void {
+export const passwordToggle = (): void => {
     let password;
     let toggleClass;
     let toggleTmpl;
     let $toggle;
-    const form = document.body.querySelector('.js-register-form');
-    if (form) {
-        password = form.querySelector('.js-register-password');
-        toggleClass = 'js-toggle-password';
-        toggleTmpl = '<div class="form-field__note form-field__note--right mobile-only">' +
-            '<a href="#toggle-password" class="' + toggleClass + '" data-password-label="Show password"' +
-            ' data-text-label="Hide password" data-link-name="Toggle password field">Show password</a>' +
-            '</div>';
-        $toggle = bonzo(bonzo.create(toggleTmpl)).insertBefore(password);
 
-        $toggle.previous().addClass('form-field__note--left');
-        bean.add($toggle[0], '.' + toggleClass, 'click', e => {
-            e.preventDefault();
-            const link = e.target, inputType = password.getAttribute('type') === 'password' ? 'text' : 'password', label = link.getAttribute('data-' + inputType + '-label');
-            password.setAttribute('type', inputType);
-            bonzo(link).text(label);
-        });
+    if (document.body) {
+        const form = document.body.querySelector('.js-register-form');
+        if (form) {
+            password = form.querySelector('.js-register-password');
+            toggleClass = 'js-toggle-password';
+            toggleTmpl =
+                `${'<div class="form-field__note form-field__note--right mobile-only">' +
+                    '<a href="#toggle-password" class="'}${toggleClass}" data-password-label="Show password"` +
+                ` data-text-label="Hide password" data-link-name="Toggle password field">Show password</a>` +
+                `</div>`;
+            $toggle = bonzo(bonzo.create(toggleTmpl)).insertBefore(password);
+
+            $toggle.previous().addClass('form-field__note--left');
+            bean.add($toggle[0], `.${toggleClass}`, 'click', e => {
+                e.preventDefault();
+                const link = e.target;
+                const inputType =
+                    password.getAttribute('type') === 'password'
+                        ? 'text'
+                        : 'password';
+                const label = link.getAttribute(`data-${inputType}-label`);
+                password.setAttribute('type', inputType);
+                bonzo(link).text(label);
+            });
+        }
     }
-}
+};
