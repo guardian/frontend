7c46b367474d872acc76985a5e7a08c54e4796e1
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-08-25 11:24:25 +0100

    ES6 common

--- static/src/javascripts/bootstraps/enhanced/common.js
+++ static/src/javascripts/bootstraps/enhanced/common.js
@@ -1,298 +1,325 @@
-/*eslint-disable no-new*/
+// @flow
+/* eslint-disable no-new */
 /* TODO - fix module constructors */
 import bean from 'bean';
-import bonzo from 'bonzo';
-import qwery from 'qwery';
-import $ from 'lib/$';
 import config from 'lib/config';
-import cookies from 'lib/cookies';
-import detect from 'lib/detect';
+import { cleanUp, addSessionCookie } from 'lib/cookies';
 import mediator from 'lib/mediator';
-import template from 'lodash/utilities/template';
-import url from 'lib/url';
-import robust from 'lib/robust';
-import storage from 'lib/storage';
+import { getUrlVars } from 'lib/url';
+import { catchErrorsWithContext } from 'lib/robust';
+import { local as localStorage } from 'lib/storage';
 import mediaListener from 'common/modules/analytics/media-listener';
 import interactionTracking from 'common/modules/analytics/interaction-tracking';
 import register from 'common/modules/analytics/register';
 import ScrollDepth from 'common/modules/analytics/scrollDepth';
-import userAdTargeting from 'commercial/modules/user-ad-targeting';
+import { requestUserSegmentsFromId } from 'commercial/modules/user-ad-targeting';
 import donotUseAdblock from 'common/modules/commercial/donot-use-adblock';
-import userFeatures from 'commercial/modules/user-features';
+import { refresh as refreshUserFeatures } from 'commercial/modules/user-features';
 import CommentCount from 'common/modules/discussion/comment-count';
 import CookieRefresh from 'common/modules/identity/cookierefresh';
 import navigation from 'common/modules/navigation/navigation';
 import Profile from 'common/modules/navigation/profile';
 import Search from 'common/modules/navigation/search';
 import membership from 'common/modules/navigation/membership';
-import history from 'common/modules/onward/history';
-import techFeedback from 'common/modules/onward/tech-feedback';
-import accessibilityPrefs from 'common/modules/ui/accessibility-prefs';
+import {
+    logHistory,
+    logSummary,
+    showInMegaNav,
+} from 'common/modules/onward/history';
+import { initTechFeedback } from 'common/modules/onward/tech-feedback';
+import { initAccessibilityPreferences } from 'common/modules/ui/accessibility-prefs';
 import Clickstream from 'common/modules/ui/clickstream';
-import Dropdowns from 'common/modules/ui/dropdowns';
+import { init as initDropdowns } from 'common/modules/ui/dropdowns';
 import fauxBlockLink from 'common/modules/ui/faux-block-link';
-import Message from 'common/modules/ui/message';
 import cookiesBanner from 'common/modules/ui/cookiesBanner';
 import RelativeDates from 'common/modules/ui/relativedates';
-import customSmartAppBanner from 'common/modules/ui/smartAppBanner';
-import tabs from 'common/modules/ui/tabs';
-import Toggles from 'common/modules/ui/toggles';
-import userPrefs from 'common/modules/user-prefs';
-import breakingNews from 'common/modules/onward/breaking-news';
-import pinterest from 'common/modules/social/pinterest';
-import hiddenShareToggle from 'common/modules/social/hidden-share-toggle';
-import membershipEngagementBanner from 'common/modules/commercial/membership-engagement-banner';
+import { init as initCustomSmartAppBanner } from 'common/modules/ui/smartAppBanner';
+import { init as initTabs } from 'common/modules/ui/tabs';
+import { Toggles } from 'common/modules/ui/toggles';
+import { breakingNewsInit } from 'common/modules/onward/breaking-news';
+import { initPinterest } from 'common/modules/social/pinterest';
+import { hiddenShareToggle } from 'common/modules/social/hidden-share-toggle';
+import { membershipEngagementBannerInit } from 'common/modules/commercial/membership-engagement-banner';
 import email from 'common/modules/email/email';
 import emailArticle from 'common/modules/email/email-article';
-import identity from 'bootstraps/enhanced/identity-common';
-import forEach from 'lodash/collections/forEach';
+import { init as initIdentity } from 'bootstraps/enhanced/identity-common';
 import ophan from 'ophan/ng';
-const modules = {
-    initialiseTopNavItems() {
-        let profile;
-        const search = new Search();
-        const header = document.getElementById('header');
 
-        if (header) {
-            if (config.switches.idProfileNavigation) {
-                profile = new Profile({
-                    url: config.page.idUrl
-                });
-                profile.init();
-            }
-        }
+const initialiseTopNavItems = (): void => {
+    let profile;
+    const search = new Search();
+    const header = document.getElementById('header');
 
-        search.init(header);
-    },
-
-    initialiseNavigation() {
-        navigation.init();
-    },
-
-    showTabs() {
-        [
-            'modules:popular:loaded',
-            'modules:geomostpopular:ready',
-        ].forEach(event => {
-            mediator.on(event, () => {
-                tabs.init();
+    if (header) {
+        if (config.switches.idProfileNavigation) {
+            profile = new Profile({
+                url: config.page.idUrl,
             });
-        });
-    },
-
-    showToggles() {
-        const toggles = new Toggles.Toggles();
-        toggles.init();
-        toggles.reset();
-        Dropdowns.init();
-    },
-
-    showRelativeDates() {
-        const dates = RelativeDates;
-        dates.init();
-    },
-
-    initClickstream() {
-        new Clickstream({
-            filter: ['a', 'button']
-        });
-    },
-
-    showAdblockMessage() {
-        donotUseAdblock.init();
-    },
-
-    loadAnalytics() {
-        interactionTracking.init();
-        if (config.switches.ophan) {
-            if (config.switches.scrollDepth) {
-                mediator.on('scrolldepth:data', ophan.record);
-
-                new ScrollDepth({
-                    isContent: /Article|LiveBlog/.test(config.page.contentType)
-                });
-            }
+            profile.init();
         }
-    },
+    }
 
-    cleanupCookies() {
-        cookies.cleanUp(['mmcore.pd', 'mmcore.srv', 'mmid', 'GU_ABFACIA', 'GU_FACIA', 'GU_ALPHA', 'GU_ME', 'at', 'gu_adfree_user', 'gu_join_date']);
-    },
+    search.init(header);
+};
 
-    cleanupLocalStorage() {
-        const deprecatedKeys = [
-            'gu.subscriber',
-            'gu.contributor',
-            'gu.cachedRecommendations',
-            'gu.recommendationsEnabled',
-            'gu.abb3.exempt'
-        ];
-        forEach(deprecatedKeys, storage.remove);
-    },
+const initialiseNavigation = (): void => {
+    navigation.init();
+};
 
-    updateHistory() {
-        if (config.page.contentType !== 'Network Front') {
-            history.logSummary(config.page);
-        }
+const showTabs = (): void => {
+    [
+        'modules:popular:loaded',
+        'modules:geomostpopular:ready',
+    ].forEach(event => {
+        mediator.on(event, () => {
+            initTabs();
+        });
+    });
+};
 
-        history.logHistory(config.page);
-    },
+const showToggles = (): void => {
+    const toggles = new Toggles();
+    toggles.init();
+    toggles.reset();
+    initDropdowns();
+};
 
-    showHistoryInMegaNav() {
-        if (config.switches.historyTags) {
-            mediator.once('modules:nav:open', () => {
-                history.showInMegaNav();
+const showRelativeDates = (): void => {
+    const dates = RelativeDates;
+    dates.init();
+};
+
+const initClickstream = (): void => {
+    new Clickstream({
+        filter: ['a', 'button'],
+    });
+};
+
+const showAdblockMessage = (): void => {
+    donotUseAdblock.init();
+};
+
+const loadAnalytics = (): void => {
+    interactionTracking.init();
+    if (config.switches.ophan) {
+        if (config.switches.scrollDepth) {
+            mediator.on('scrolldepth:data', ophan.record);
+
+            new ScrollDepth({
+                isContent: /Article|LiveBlog/.test(config.page.contentType),
             });
         }
-    },
+    }
+};
 
-    idCookieRefresh() {
-        if (config.switches.idCookieRefresh) {
-            new CookieRefresh().init();
-        }
-    },
+const cleanupCookies = (): void => {
+    cleanUp([
+        'mmcore.pd',
+        'mmcore.srv',
+        'mmid',
+        'GU_ABFACIA',
+        'GU_FACIA',
+        'GU_ALPHA',
+        'GU_ME',
+        'at',
+        'gu_adfree_user',
+        'gu_join_date',
+    ]);
+};
 
-    windowEventListeners() {
-        ['orientationchange'].forEach(event => {
-            bean.on(window, event, mediator.emit.bind(mediator, 'window:' + event));
+const cleanupLocalStorage = (): void => {
+    const deprecatedKeys = [
+        'gu.subscriber',
+        'gu.contributor',
+        'gu.cachedRecommendations',
+        'gu.recommendationsEnabled',
+        'gu.abb3.exempt',
+    ];
+    deprecatedKeys.forEach(localStorage.remove);
+};
+
+const updateHistory = (): void => {
+    if (config.page.contentType !== 'Network Front') {
+        logSummary(config.page);
+    }
+
+    logHistory(config.page);
+};
+
+const showHistoryInMegaNav = (): void => {
+    if (config.switches.historyTags) {
+        mediator.once('modules:nav:open', () => {
+            showInMegaNav();
         });
-    },
+    }
+};
 
-    checkIframe() {
-        if (window.self !== window.top) {
-            $('html').addClass('iframed');
+const idCookieRefresh = (): void => {
+    if (config.switches.idCookieRefresh) {
+        new CookieRefresh().init();
+    }
+};
+
+const windowEventListeners = (): void => {
+    ['orientationchange'].forEach(event => {
+        bean.on(window, event, mediator.emit.bind(mediator, `window:${event}`));
+    });
+};
+
+const checkIframe = (): void => {
+    if (window.self !== window.top) {
+        const html = document.documentElement;
+        if (html) {
+            html.classList.add('iframed');
         }
-    },
+    }
+};
 
-    startRegister() {
-        register.initialise();
-    },
+const startRegister = (): void => {
+    register.initialise();
+};
 
-    initDiscussion() {
-        if (config.switches.enableDiscussionSwitch) {
-            CommentCount.init();
+const initDiscussion = (): void => {
+    if (config.switches.enableDiscussionSwitch) {
+        CommentCount.init();
+    }
+};
+
+const testCookie = (): void => {
+    const queryParams = getUrlVars();
+    if (queryParams.test) {
+        addSessionCookie('GU_TEST', encodeURIComponent(queryParams.test));
+    }
+};
+
+const initOpenOverlayOnClick = (): void => {
+    let offset: ?number;
+    const body = document.body;
+
+    if (!body) return;
+
+    bean.on(body, 'click', '[data-open-overlay-on-click]', e => {
+        const elId = (e.currentTarget: any).getAttribute(
+            'data-open-overlay-on-click'
+        );
+        offset = body.scrollTop;
+        body.classList.add('has-overlay');
+        const el = document.getElementById(elId);
+        if (el) {
+            el.classList.add('overlay--open');
+            body.appendChild(el);
         }
-    },
+    });
 
-    testCookie() {
-        const queryParams = url.getUrlVars();
-        if (queryParams.test) {
-            cookies.addSessionCookie('GU_TEST', encodeURIComponent(queryParams.test));
+    bean.on(body, 'click', '.js-overlay-close', e => {
+        const overlay = (e.target: any).closest('.overlay');
+        if (overlay) {
+            overlay.classList.remove('overlay--open');
         }
-    },
-
-    initOpenOverlayOnClick() {
-        let offset;
-
-        bean.on(document.body, 'click', '[data-open-overlay-on-click]', e => {
-            const elId = bonzo(e.currentTarget).data('open-overlay-on-click');
-            offset = document.body.scrollTop;
-            bonzo(document.body).addClass('has-overlay');
-            $('#' + elId).addClass('overlay--open').appendTo(document.body);
-        });
-
-        bean.on(document.body, 'click', '.js-overlay-close', e => {
-            const overlay = $.ancestor(e.target, 'overlay');
-            if (overlay) {
-                bonzo(overlay).removeClass('overlay--open');
-            }
-            bonzo(document.body).removeClass('has-overlay');
+        body.classList.remove('has-overlay');
+        window.setTimeout(() => {
             if (offset) {
-                window.setTimeout(() => {
-                    document.body.scrollTop = offset;
-                    offset = null;
-                }, 1);
+                body.scrollTop = offset;
+                offset = null;
             }
+        }, 1);
+    });
+};
+
+const loadBreakingNews = (): void => {
+    if (
+        config.switches.breakingNews &&
+        config.page.section !== 'identity' &&
+        !config.page.isHosted
+    ) {
+        breakingNewsInit().catch(() => {
+            // breaking news may not load if local storage is unavailable - this is fine
         });
-    },
+    }
+};
 
-    loadBreakingNews() {
-        if (config.switches.breakingNews && config.page.section !== 'identity' && !config.page.isHosted) {
-            breakingNews.breakingNewsInit().catch(() => {
-                // breaking news may not load if local storage is unavailable - this is fine
-            });
-        }
-    },
+const initPublicApi = (): void => {
+    // BE CAREFUL what you expose here...
+    window.guardian.api = {};
+};
 
-    initPublicApi() {
-        // BE CAREFUL what you expose here...
-        window.guardian.api = {};
-    },
+const startPinterest = (): void => {
+    if (/Article|LiveBlog|Gallery|Video/.test(config.page.contentType)) {
+        initPinterest();
+    }
+};
 
-    initPinterest() {
-        if (/Article|LiveBlog|Gallery|Video/.test(config.page.contentType)) {
-            pinterest.initPinterest();
-        }
-    },
+const membershipEngagementBanner = (): void => {
+    if (config.switches.membershipEngagementBanner) {
+        membershipEngagementBannerInit();
+    }
+};
 
-    membershipEngagementBanner() {
-        if (config.switches.membershipEngagementBanner) {
-            membershipEngagementBanner.membershipEngagementBannerInit();
-        }
-    },
+const initEmail = (): void => {
+    // Initalise email embedded in page
+    email.init();
 
-    initEmail() {
-        // Initalise email embedded in page
-        email.init();
+    // Initalise email insertion into articles
+    if (config.switches.emailInArticle) {
+        emailArticle.init();
+    }
 
-        // Initalise email insertion into articles
-        if (config.switches.emailInArticle) {
-            emailArticle.init();
-        }
+    // Initalise email forms in iframes
+    Array.from(
+        document.getElementsByClassName('js-email-sub__iframe')
+    ).forEach(el => {
+        email.init(el);
+    });
 
-        // Initalise email forms in iframes
-        forEach(document.getElementsByClassName('js-email-sub__iframe'), el => {
+    // Listen for interactive load event and initalise forms
+    bean.on(window, 'interactive-loaded', () => {
+        Array.from(
+            document.querySelectorAll('.guInteractive .js-email-sub__iframe')
+        ).forEach(el => {
             email.init(el);
         });
-
-        // Listen for interactive load event and initalise forms
-        bean.on(window, 'interactive-loaded', () => {
-            forEach(qwery('.guInteractive .js-email-sub__iframe'), el => {
-                email.init(el);
-            });
-        });
-    }
+    });
 };
-export default {
-    init() {
-        robust.catchErrorsWithContext([
-            // Analytics comes at the top. If you think your thing is more important then please think again...
-            ['c-analytics', modules.loadAnalytics],
 
-            ['c-cookies-banner', cookiesBanner.init],
-            ['c-identity', identity.init],
-            ['c-adverts', userAdTargeting.requestUserSegmentsFromId],
-            ['c-discussion', modules.initDiscussion],
-            ['c-test-cookie', modules.testCookie],
-            ['c-event-listeners', modules.windowEventListeners],
-            ['c-breaking-news', modules.loadBreakingNews],
-            ['c-block-link', fauxBlockLink],
-            ['c-iframe', modules.checkIframe],
-            ['c-tabs', modules.showTabs],
-            ['c-top-nav', modules.initialiseTopNavItems],
-            ['c-init-nav', modules.initialiseNavigation],
-            ['c-toggles', modules.showToggles],
-            ['c-dates', modules.showRelativeDates],
-            ['c-clickstream', modules.initClickstream],
-            ['c-history', modules.updateHistory],
-            ['c-id-cookie-refresh', modules.idCookieRefresh],
-            ['c-history-nav', modules.showHistoryInMegaNav],
-            ['c-start-register', modules.startRegister],
-            ['c-smart-banner', customSmartAppBanner.init],
-            ['c-adblock', modules.showAdblockMessage],
-            ['c-cookies', modules.cleanupCookies],
-            ['c-localStorage', modules.cleanupLocalStorage],
-            ['c-overlay', modules.initOpenOverlayOnClick],
-            ['c-public-api', modules.initPublicApi],
-            ['c-tech-feedback', techFeedback.initTechFeedback],
-            ['c-media-listeners', mediaListener],
-            ['c-accessibility-prefs', accessibilityPrefs.initAccessibilityPreferences],
-            ['c-pinterest', modules.initPinterest],
-            ['c-hidden-share-toggle', hiddenShareToggle.hiddenShareToggle],
-            ['c-show-membership-engagement-banner', modules.membershipEngagementBanner],
-            ['c-email', modules.initEmail],
-            ['c-user-features', userFeatures.refresh.bind(userFeatures)],
-            ['c-membership', membership]
-        ]);
-    }
+const init = (): void => {
+    catchErrorsWithContext([
+        // Analytics comes at the top. If you think your thing is more important then please think again...
+        ['c-analytics', loadAnalytics],
+        ['c-cookies-banner', cookiesBanner.init],
+        ['c-identity', initIdentity],
+        ['c-adverts', requestUserSegmentsFromId],
+        ['c-discussion', initDiscussion],
+        ['c-test-cookie', testCookie],
+        ['c-event-listeners', windowEventListeners],
+        ['c-breaking-news', loadBreakingNews],
+        ['c-block-link', fauxBlockLink],
+        ['c-iframe', checkIframe],
+        ['c-tabs', showTabs],
+        ['c-top-nav', initialiseTopNavItems],
+        ['c-init-nav', initialiseNavigation],
+        ['c-toggles', showToggles],
+        ['c-dates', showRelativeDates],
+        ['c-clickstream', initClickstream],
+        ['c-history', updateHistory],
+        ['c-id-cookie-refresh', idCookieRefresh],
+        ['c-history-nav', showHistoryInMegaNav],
+        ['c-start-register', startRegister],
+        ['c-smart-banner', initCustomSmartAppBanner],
+        ['c-adblock', showAdblockMessage],
+        ['c-cookies', cleanupCookies],
+        ['c-localStorage', cleanupLocalStorage],
+        ['c-overlay', initOpenOverlayOnClick],
+        ['c-public-api', initPublicApi],
+        ['c-tech-feedback', initTechFeedback],
+        ['c-media-listeners', mediaListener],
+        ['c-accessibility-prefs', initAccessibilityPreferences],
+        ['c-pinterest', startPinterest],
+        ['c-hidden-share-toggle', hiddenShareToggle],
+        ['c-show-membership-engagement-banner', membershipEngagementBanner],
+        ['c-email', initEmail],
+        ['c-user-features', refreshUserFeatures],
+        ['c-membership', membership],
+    ]);
 };
+
+export { init };
