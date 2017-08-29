50d5edd04522d34fe75d344466aef600a6a89835
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-08-29 14:19:06 +0100

    Add global var

--- static/src/javascripts/projects/common/modules/article/twitter.js
+++ static/src/javascripts/projects/common/modules/article/twitter.js
@@ -7,6 +7,8 @@ import { getBreakpoint, getViewport } from 'lib/detect';
 import mediator from 'lib/mediator';
 import debounce from 'lodash/functions/debounce';
 
+declare var twttr: Object;
+
 const body: ?Element = document.querySelector(
     '.js-liveblog-body, .js-article__body, .js-article__body--minute-article'
 );
