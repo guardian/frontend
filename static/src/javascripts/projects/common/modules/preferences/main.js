186fcfc8a046181ed60bbdb8e0e1fc0de8e02225
Kate Whalen <whalenka88@gmail.com>
2017-08-29 15:27:20 +0100

    transform projects/common/modules/preferences/main.js content to ES6

--- static/src/javascripts/projects/common/modules/preferences/main.js
+++ static/src/javascripts/projects/common/modules/preferences/main.js
@@ -5,87 +5,87 @@ import history from 'common/modules/onward/history';
 import reduce from 'lodash/collections/reduce';
 import isEmpty from 'lodash/objects/isEmpty';
 export default {
-    init: function() {
-        var placeholder = document.getElementById('preferences-history-tags'),
+    init() {
+        const placeholder = document.getElementById('preferences-history-tags'),
+              initialiseSummaryTagsSettings = () => {
+                  const SummaryTagsList = React.createClass({
+                      getInitialState() {
+                          return {
+                              popular: history.getPopularFiltered()
+                          };
+                      },
+                      handleRemove(tag) {
+                          history.deleteFromSummary(tag);
+                          this.setState({
+                              popular: history.getPopularFiltered({
+                                  flush: true
+                              })
+                          });
+                          history.showInMegaNav();
+                      },
+                      render() {
+                          const self = this;
 
-            initialiseSummaryTagsSettings = function() {
-                var SummaryTagsList = React.createClass({
-                    getInitialState: function() {
-                        return {
-                            popular: history.getPopularFiltered()
-                        };
-                    },
-                    handleRemove: function(tag) {
-                        history.deleteFromSummary(tag);
-                        this.setState({
-                            popular: history.getPopularFiltered({
-                                flush: true
-                            })
-                        });
-                        history.showInMegaNav();
-                    },
-                    render: function() {
-                        var self = this,
-                            tags = reduce(this.state.popular, function(obj, tag) {
-                                obj[tag[0]] = React.DOM.span({
-                                        className: 'button button--small button--tag button--secondary'
-                                    },
-                                    React.DOM.button({
-                                        onClick: self.handleRemove.bind(self, tag[0]),
-                                        'data-link-name': 'remove | ' + tag[1]
-                                    }, 'X'),
-                                    React.DOM.a({
-                                        href: '/' + tag[0]
-                                    }, tag[1])
-                                );
-                                return obj;
-                            }, {}),
-                            helperText;
+                          const tags = reduce(this.state.popular, (obj, tag) => {
+                              obj[tag[0]] = React.DOM.span({
+                                      className: 'button button--small button--tag button--secondary'
+                                  },
+                                  React.DOM.button({
+                                      onClick: self.handleRemove.bind(self, tag[0]),
+                                      'data-link-name': 'remove | ' + tag[1]
+                                  }, 'X'),
+                                  React.DOM.a({
+                                      href: '/' + tag[0]
+                                  }, tag[1])
+                              );
+                              return obj;
+                          }, {});
 
-                        if (isEmpty(tags)) {
-                            helperText = '(You don\'t have any recently visited topics.)';
-                        } else {
-                            helperText = 'Remove individual topics by clicking \'X\' or switch off the functionality below. We respect your privacy and your shortcuts will never be made public.';
-                        }
-                        tags.helperText = React.DOM.p(null, helperText);
-                        return React.DOM.div(null, tags);
-                    }
-                });
+                          let helperText;
 
-                var SummaryTagsSettings = React.createClass({
-                    getInitialState: function() {
-                        return {
-                            enabled: history.showInMegaNavEnabled()
-                        };
-                    },
-                    handleToggle: function() {
-                        var isEnabled = !this.state.enabled;
+                          if (isEmpty(tags)) {
+                              helperText = '(You don\'t have any recently visited topics.)';
+                          } else {
+                              helperText = 'Remove individual topics by clicking \'X\' or switch off the functionality below. We respect your privacy and your shortcuts will never be made public.';
+                          }
+                          tags.helperText = React.DOM.p(null, helperText);
+                          return React.DOM.div(null, tags);
+                      }
+                  });
 
-                        this.setState({
-                            enabled: isEnabled
-                        });
-                        history.showInMegaNavEnable(isEnabled);
-                    },
-                    render: function() {
-                        var self = this,
-                            toggleAction = this.state.enabled ? 'OFF' : 'ON';
+                  const SummaryTagsSettings = React.createClass({
+                      getInitialState() {
+                          return {
+                              enabled: history.showInMegaNavEnabled()
+                          };
+                      },
+                      handleToggle() {
+                          const isEnabled = !this.state.enabled;
 
-                        return React.DOM.div({
-                            'data-link-name': 'suggested links'
-                        }, [
-                            React.DOM.p(null, 'These are based on the topics you visit most. You can access them at any time by opening the "all sections” menu.'),
-                            this.state.enabled ? React.createElement(SummaryTagsList) : null,
-                            React.DOM.button({
-                                onClick: self.handleToggle,
-                                className: 'button button--medium button--primary',
-                                'data-link-name': toggleAction
-                            }, 'Switch recently visited links ' + toggleAction)
-                        ]);
-                    }
-                });
+                          this.setState({
+                              enabled: isEnabled
+                          });
+                          history.showInMegaNavEnable(isEnabled);
+                      },
+                      render() {
+                          const self = this, toggleAction = this.state.enabled ? 'OFF' : 'ON';
 
-                React.render(React.createElement(SummaryTagsSettings), placeholder);
-            };
+                          return React.DOM.div({
+                              'data-link-name': 'suggested links'
+                          }, [
+                              React.DOM.p(null, 'These are based on the topics you visit most. You can access them at any time by opening the "all sections” menu.'),
+                              this.state.enabled ? React.createElement(SummaryTagsList) : null,
+                              React.DOM.button({
+                                  onClick: self.handleToggle,
+                                  className: 'button button--medium button--primary',
+                                  'data-link-name': toggleAction
+                              }, 'Switch recently visited links ' + toggleAction)
+                          ]);
+                      }
+                  });
+
+                  React.render(React.createElement(SummaryTagsSettings), placeholder);
+              };
 
         if (placeholder) {
             initialiseSummaryTagsSettings();
