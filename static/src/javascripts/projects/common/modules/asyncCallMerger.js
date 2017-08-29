31dcf731d439ad1ea75c0c698e622e1fc2d19410
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-08-29 16:36:55 +0100

    ES6 asyncMergeCalls

--- static/src/javascripts/projects/common/modules/asyncCallMerger.js
+++ static/src/javascripts/projects/common/modules/asyncCallMerger.js
@@ -1,26 +1,40 @@
+// @flow
+
 /**
  * This module is used to merge calls to async functions that use callbacks. Ensuring the target function is
  * called once only but the result is passed to all the call backs that require it.
  */
+
+type Status = 'init' | 'waiting' | 'complete';
+
+type Callback = (...as: any[]) => void;
+
+type TargetCallback = (f: Callback) => void;
+
 /**
  * Creates a function that will merge calls to the supplied target function
  * @param {function} function to be merged, the function takes one argument, a callback that will
  * be called with the result of the async request
  * @return {?Date} the current date
  */
-function mergeCalls(targetFunction) {
-    let callbacks, status, callbackArguments;
+const mergeCalls = (targetFunction: TargetCallback): TargetCallback => {
+    let callbacks: Callback[];
+    let callbackArguments: any[];
+    let status: Status;
 
-    function targetCallbackHandler(...args) {
+    const reset = (): void => {
+        [callbacks, status, callbackArguments] = [[], 'init', []];
+    };
+
+    const targetCallbackHandler: Callback = (...args) => {
         callbackArguments = args;
         status = 'complete';
+        callbacks.forEach(callback => {
+            callback(...callbackArguments);
+        });
+    };
 
-        for (let i = 0; i < callbacks.length; i++) {
-            callbacks[i].apply(null, callbackArguments);
-        }
-    }
-
-    function callMergingFunction(callback) {
+    const callMergingFunction: TargetCallback = callback => {
         if (status === 'init') {
             status = 'waiting';
             callbacks.push(callback);
@@ -30,21 +44,13 @@ function mergeCalls(targetFunction) {
         } else {
             callback(...callbackArguments);
         }
-    }
-
-    function reset() {
-        callbacks = [];
-        status = 'init';
-        callbackArguments = null;
-    }
+    };
 
     reset();
 
     callMergingFunction.reset = reset;
 
     return callMergingFunction;
-}
-
-export default {
-    mergeCalls
 };
+
+export { mergeCalls };
