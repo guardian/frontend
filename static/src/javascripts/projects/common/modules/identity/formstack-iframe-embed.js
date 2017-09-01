0ea917d0d8ca4ad10828a987b2430a904de14e96
GHaberis <george.haberis@theguardian.com>
2017-09-01 15:19:32 +0100

    transform projects/common/modules/identity/formstack-iframe-embed.js to ES6 module

--- static/src/javascripts/projects/common/modules/identity/formstack-iframe-embed.js
+++ static/src/javascripts/projects/common/modules/identity/formstack-iframe-embed.js
@@ -13,176 +13,172 @@
      - Chris Finch, CSD - Identity, March '14
 */
 
-define([
-    'bean',
-    'lib/$',
-    'common/modules/identity/api',
-    'lodash/objects/assign'
-], function (
-    bean,
-    $,
-    idApi,
-    assign
-) {
+import bean from 'bean';
+import $ from 'lib/$';
+import idApi from 'common/modules/identity/api';
+import assign from 'lodash/objects/assign';
 
-    function Formstack(el, formstackId, config) {
+function Formstack(el, formstackId, config) {
 
-        var self = this,
-            dom = {},
-            formId = formstackId.split('-')[0];
+    var self = this,
+        dom = {},
+        formId = formstackId.split('-')[0];
 
-        config = assign({
-            idClasses: {
-                form: 'form',
-                field: 'form-field',
-                note: 'form-field__note form-field__note--below',
-                label: 'label',
-                checkboxLabel: 'check-label',
-                textInput: 'text-input',
-                textArea: 'textarea textarea--no-resize',
-                submit: 'submit-input',
-                fieldError: 'form-field--error',
-                formError: 'form__error',
-                fieldset: 'formstack-fieldset',
-                required: 'formstack-required',
-                sectionHeader: 'formstack-heading',
-                sectionHeaderFirst: 'formstack-heading--first',
-                sectionText: 'formstack-section',
-                characterCount: 'formstack-count',
-                hide: 'is-hidden'
-            },
-            fsSelectors: {
-                form: '#fsForm' + formId,
-                field: '.fsRow',
-                note: '.fsSupporting, .showMobile',
-                label: '.fsLabel',
-                checkboxLabel: '.fsOptionLabel',
-                textInput: '.fsField[type="text"], .fsField[type="email"], .fsField[type="number"], .fsField[type="tel"]',
-                textArea: 'textarea.fsField',
-                submit: '.fsSubmitButton',
-                fieldError: '.fsValidationError',
-                formError: '.fsError',
-                fieldset: 'fieldset',
-                required: '.fsRequiredMarker',
-                sectionHeader: '.fsSectionHeading',
-                sectionHeaderFirst: '.fsSection:first-child .fsSectionHeading',
-                sectionText: '.fsSectionText',
-                characterCount: '.fsCounter',
-                hide: '.hidden, .fsHidden, .ui-datepicker-trigger'
-            },
-            hiddenSelectors: {
-                userId: '[type="number"]',
-                email: '[type="email"]'
+    config = assign({
+        idClasses: {
+            form: 'form',
+            field: 'form-field',
+            note: 'form-field__note form-field__note--below',
+            label: 'label',
+            checkboxLabel: 'check-label',
+            textInput: 'text-input',
+            textArea: 'textarea textarea--no-resize',
+            submit: 'submit-input',
+            fieldError: 'form-field--error',
+            formError: 'form__error',
+            fieldset: 'formstack-fieldset',
+            required: 'formstack-required',
+            sectionHeader: 'formstack-heading',
+            sectionHeaderFirst: 'formstack-heading--first',
+            sectionText: 'formstack-section',
+            characterCount: 'formstack-count',
+            hide: 'is-hidden'
+        },
+        fsSelectors: {
+            form: '#fsForm' + formId,
+            field: '.fsRow',
+            note: '.fsSupporting, .showMobile',
+            label: '.fsLabel',
+            checkboxLabel: '.fsOptionLabel',
+            textInput: '.fsField[type="text"], .fsField[type="email"], .fsField[type="number"], .fsField[type="tel"]',
+            textArea: 'textarea.fsField',
+            submit: '.fsSubmitButton',
+            fieldError: '.fsValidationError',
+            formError: '.fsError',
+            fieldset: 'fieldset',
+            required: '.fsRequiredMarker',
+            sectionHeader: '.fsSectionHeading',
+            sectionHeaderFirst: '.fsSection:first-child .fsSectionHeading',
+            sectionText: '.fsSectionText',
+            characterCount: '.fsCounter',
+            hide: '.hidden, .fsHidden, .ui-datepicker-trigger'
+        },
+        hiddenSelectors: {
+            userId: '[type="number"]',
+            email: '[type="email"]'
+        }
+    }, config);
+
+    self.init = function() {
+        // User object required to populate fields
+        var user = idApi.getUserOrSignIn();
+
+        self.dom(user);
+        $(el).removeClass(config.idClasses.hide);
+        $('html').addClass('iframed--overflow-hidden');
+
+        // Update iframe height
+        self.sendHeight();
+    };
+
+    self.dom = function(user) {
+        var selector, $userId, $email, html;
+
+        // Formstack generates some awful HTML, so we'll remove the CSS links,
+        // loop their selectors and add our own classes instead
+        dom.$form = $(config.fsSelectors.form).addClass(config.idClasses.form);
+        $('link', el).remove();
+
+        for (selector in config.fsSelectors) {
+            $(config.fsSelectors[selector], dom.$form).addClass(config.idClasses[selector]);
+        }
+
+        // Formstack also don't have capturable hidden fields,
+        // so we remove ID text inputs and append hidden equivalents
+        $userId = $(config.hiddenSelectors.userId, dom.$form).remove();
+        $email = $(config.hiddenSelectors.email, dom.$form).remove();
+
+        html = '<input type="hidden" name="' + $userId.attr('name') + '" value="' + user.id + '">' + '<input type="hidden" name="' + $email.attr('name') + '" value="' + user.primaryEmailAddress + '">';
+
+        dom.$form.append(html);
+
+        // Events
+        bean.on(window, 'unload', self.unload);
+        bean.on(dom.$form[0], 'submit', self.submit);
+
+        // Listen for message from top window,
+        // only message we are listening for is the iframe position..
+        window.addEventListener('message', function(event) {
+            var message = JSON.parse(event.data);
+            if (message.iframeTop) {
+                self.scrollToTopOfIframe(message.iframeTop);
             }
-        }, config);
+        }, false);
+    };
 
-        self.init = function () {
-            // User object required to populate fields
-            var user = idApi.getUserOrSignIn();
+    self.submit = function(event) {
+        event.preventDefault();
 
-            self.dom(user);
-            $(el).removeClass(config.idClasses.hide);
-            $('html').addClass('iframed--overflow-hidden');
+        setTimeout(function() {
+            // Remove any existing errors
+            $('.' + config.idClasses.formError).removeClass(config.idClasses.formError);
+            $('.' + config.idClasses.fieldError).removeClass(config.idClasses.fieldError);
 
-            // Update iframe height
-            self.sendHeight();
-        };
+            // Handle new errors
+            $(config.fsSelectors.formError, dom.$form).addClass(config.idClasses.formError);
+            $(config.fsSelectors.fieldError, dom.$form).addClass(config.idClasses.fieldError);
 
-        self.dom = function (user) {
-            var selector, $userId, $email, html;
+            // Update character count absolute positions
+            $(config.fsSelectors.textArea, el).each(function(textarea) {
+                bean.fire(textarea, 'keyup');
+            });
 
-            // Formstack generates some awful HTML, so we'll remove the CSS links,
-            // loop their selectors and add our own classes instead
-            dom.$form = $(config.fsSelectors.form).addClass(config.idClasses.form);
-            $('link', el).remove();
+            self.postMessage('get-position', 'get-position');
 
-            for (selector in config.fsSelectors) {
-                $(config.fsSelectors[selector], dom.$form).addClass(config.idClasses[selector]);
+            // if no errors, submit form
+            if ($(config.fsSelectors.formError, dom.$form).length === 0) {
+                dom.$form[0].submit();
             }
 
-            // Formstack also don't have capturable hidden fields,
-            // so we remove ID text inputs and append hidden equivalents
-            $userId = $(config.hiddenSelectors.userId, dom.$form).remove();
-            $email = $(config.hiddenSelectors.email, dom.$form).remove();
+        }, 100);
+    };
 
-            html = '<input type="hidden" name="' + $userId.attr('name') + '" value="' + user.id + '">'
-                + '<input type="hidden" name="' + $email.attr('name') + '" value="' + user.primaryEmailAddress + '">';
+    self.scrollToTopOfIframe = function(top) {
+        self.postMessage('scroll-to', 'scroll-to', 0, top);
+    };
 
-            dom.$form.append(html);
+    self.unload = function() {
+        // Listen for navigation to success page
+        self.sendHeight(true);
+    };
 
-            // Events
-            bean.on(window, 'unload', self.unload);
-            bean.on(dom.$form[0], 'submit', self.submit);
+    self.sendHeight = function() {
+        var body = document.body,
+            html = document.documentElement,
+            height = Math.max(body.scrollHeight, body.offsetHeight,
+                html.clientHeight, html.scrollHeight, html.offsetHeight);
 
-            // Listen for message from top window,
-            // only message we are listening for is the iframe position..
-            window.addEventListener('message', function (event) {
-                var message = JSON.parse(event.data);
-                if (message.iframeTop) { self.scrollToTopOfIframe(message.iframeTop); }
-            }, false);
+        self.postMessage('set-height', height);
+    };
+
+    self.postMessage = function(type, value, x, y) {
+
+        var message = {
+            type: type,
+            value: value,
+            href: window.location.href
         };
 
-        self.submit = function (event) {
-            event.preventDefault();
+        if (x) {
+            message.x = x;
+        }
+        if (y) {
+            message.y = y;
+        }
 
-            setTimeout(function () {
-                // Remove any existing errors
-                $('.' + config.idClasses.formError).removeClass(config.idClasses.formError);
-                $('.' + config.idClasses.fieldError).removeClass(config.idClasses.fieldError);
+        window.top.postMessage(JSON.stringify(message), '*');
+    };
 
-                // Handle new errors
-                $(config.fsSelectors.formError, dom.$form).addClass(config.idClasses.formError);
-                $(config.fsSelectors.fieldError, dom.$form).addClass(config.idClasses.fieldError);
+}
 
-                // Update character count absolute positions
-                $(config.fsSelectors.textArea, el).each(function (textarea) {
-                    bean.fire(textarea, 'keyup');
-                });
-
-                self.postMessage('get-position', 'get-position');
-
-                // if no errors, submit form
-                if ($(config.fsSelectors.formError, dom.$form).length === 0) {
-                    dom.$form[0].submit();
-                }
-
-            }, 100);
-        };
-
-        self.scrollToTopOfIframe = function (top) {
-            self.postMessage('scroll-to', 'scroll-to', 0, top);
-        };
-
-        self.unload = function () {
-            // Listen for navigation to success page
-            self.sendHeight(true);
-        };
-
-        self.sendHeight = function () {
-            var body = document.body,
-                html = document.documentElement,
-                height = Math.max(body.scrollHeight, body.offsetHeight,
-                                  html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-            self.postMessage('set-height', height);
-        };
-
-        self.postMessage = function (type, value, x, y) {
-
-            var message = {
-                type: type,
-                value: value,
-                href: window.location.href
-            };
-
-            if (x) { message.x = x; }
-            if (y) { message.y = y; }
-
-            window.top.postMessage(JSON.stringify(message), '*');
-        };
-
-    }
-
-    return Formstack;
-
-});
+export default Formstack;
