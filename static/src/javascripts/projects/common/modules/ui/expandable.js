20ee5c3f8cd55352dee753187ba456f0863de0f5
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-09-01 14:45:33 +0100

    transform projects/common/modules/ui/expandable.js content to ES6

--- static/src/javascripts/projects/common/modules/ui/expandable.js
+++ static/src/javascripts/projects/common/modules/ui/expandable.js
@@ -12,80 +12,87 @@ import bonzo from 'bonzo';
         showCount     : {Boolean} Whether to display the count in the CTA
         buttonAfterEl : {Element} Element to add the button after (defaults to last child of dom)
 */
-var Expandable = function(options) {
+const Expandable = options => {
+    const opts = options || {};
 
-    var opts = options || {},
-        dom = $(opts.dom), // root element of the trailblock
-        expanded = (opts.expanded === false) ? false : true, // true = open, false = closed
-        cta = document.createElement('button'),
-        showCount = (opts.showCount === false) ? false : true,
-        renderState = function() {
-            if (expanded) {
-                dom.removeClass('shut');
+    const // root element of the trailblock
+    dom = $(opts.dom);
+
+    let // true = open, false = closed
+    expanded = (opts.expanded === false) ? false : true;
+
+    const cta = document.createElement('button');
+    const showCount = (opts.showCount === false) ? false : true;
+
+    const renderState = () => {
+        if (expanded) {
+            dom.removeClass('shut');
+        } else {
+            dom.addClass('shut');
+        }
+    };
+
+    const getCount = () => parseInt(dom.attr('data-count'), 10);
+
+    const updateCallToAction = () => {
+        let text = 'Show ';
+        if (showCount) {
+            text += getCount() + ' ';
+        }
+        text += (expanded) ? 'fewer' : 'more';
+        cta.innerHTML = text;
+        cta.setAttribute('data-link-name', 'Show ' + ((expanded) ? 'more' : 'fewer'));
+        cta.setAttribute('data-is-ajax', '1');
+    };
+
+    const // Model
+    model = {
+
+        toggleExpanded() {
+            expanded = (expanded) ? false : true;
+            renderState();
+            updateCallToAction();
+        },
+
+        getCount,
+
+        isOpen() {
+            return (dom.hasClass('shut')) ? false : true;
+        }
+    };
+
+    const // View
+    view = {
+
+        updateCallToAction,
+
+        renderState,
+
+        renderCallToAction() {
+            bean.add(cta, 'click', () => {
+                model.toggleExpanded();
+            });
+            cta.className = 'cta';
+            if (opts.buttonAfterEl) {
+                bonzo(opts.buttonAfterEl).after(cta);
             } else {
-                dom.addClass('shut');
+                dom[0].appendChild(cta);
             }
+            view.updateCallToAction();
         },
-        getCount = function() {
-            return parseInt(dom.attr('data-count'), 10);
-        },
-        updateCallToAction = function() {
-            var text = 'Show ';
-            if (showCount) {
-                text += getCount() + ' ';
+
+        scrollToCallToAction() {
+            // feels a bit hacky but need to give the transition time to finish before scrolling
+            if (!expanded) {
+                window.setTimeout(() => {
+                    cta.scrollIntoView();
+                }, 550);
             }
-            text += (expanded) ? 'fewer' : 'more';
-            cta.innerHTML = text;
-            cta.setAttribute('data-link-name', 'Show ' + ((expanded) ? 'more' : 'fewer'));
-            cta.setAttribute('data-is-ajax', '1');
-        },
-        // Model
-        model = {
-
-            toggleExpanded: function() {
-                expanded = (expanded) ? false : true;
-                renderState();
-                updateCallToAction();
-            },
-
-            getCount: getCount,
-
-            isOpen: function() {
-                return (dom.hasClass('shut')) ? false : true;
-            }
-        },
-        // View
-        view = {
-
-            updateCallToAction: updateCallToAction,
-
-            renderState: renderState,
-
-            renderCallToAction: function() {
-                bean.add(cta, 'click', function() {
-                    model.toggleExpanded();
-                });
-                cta.className = 'cta';
-                if (opts.buttonAfterEl) {
-                    bonzo(opts.buttonAfterEl).after(cta);
-                } else {
-                    dom[0].appendChild(cta);
-                }
-                view.updateCallToAction();
-            },
-
-            scrollToCallToAction: function() {
-                // feels a bit hacky but need to give the transition time to finish before scrolling
-                if (!expanded) {
-                    window.setTimeout(function() {
-                        cta.scrollIntoView();
-                    }, 550);
-                }
-            }
-        };
+        }
+    };
 
     return {
-        init: function() {
+        init() {
 
             if (dom.hasClass('expandable-initialised') || !dom.html() || model.getCount() < 3) {
                 return false;
