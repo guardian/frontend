0279abd417d10f6782dfd8d5cbcc823bdbc67343
Alex Sanders <a@snd.rs>
2017-09-06 10:50:10 +0100

    doh! JUST DO NOT DEFINE IT

--- common/app/rendering/core/JavascriptRendering.scala
+++ common/app/rendering/core/JavascriptRendering.scala
@@ -56,7 +56,7 @@ trait JavascriptRendering extends Logging {
   private def prescript: ByteArrayInputStream = {
     val pre =
       """
-        |var global = global || this, self = self || this, window = undefined;
+        |var global = global || this, self = self || this;
         |
         |var console = {};
         |
