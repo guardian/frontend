40de08bb4dd35b5177f2686407ea9d61f6d41600
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-09-11 13:58:08 +0100

    Deleted an old import.

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,5 +1,4 @@
 // @flow
-import { acquisitionsSupportBaseline } from 'common/modules/experiments/tests/acquisitions-support-baseline';
 import { acquisitionsSupportUsRecurringContribution } from 'common/modules/experiments/tests/acquisitions-support-us-recurring-contributions';
 
 export const membershipEngagementBannerTests: $ReadOnlyArray<
