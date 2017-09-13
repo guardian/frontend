cdb4b3d63d66f67f716e4ff1bf7b072a495ccf9d
Alex Sanders <a@snd.rs>
2017-09-13 11:02:37 +0100

    react to expensively styled in h

--- ui/src/utils/h.jsx
+++ ui/src/utils/h.jsx
@@ -4,19 +4,24 @@
 // bespoke wrapper around preact's `h` that passes any CSS data
 // on `attributes.style` to styletron, then hands off to `preact#h`
 
-import { styled } from 'styletron-preact';
 import { h as preact_h } from 'preact';
+import { styled as cheaplyStyled } from 'styletron-preact';
+import { expensivelyStyled } from 'utils/expensively-styled';
 
 export default (
-    nodeName: string,
+    component: string,
     attributes: ?Object,
     ...children: Array<any>
 ) => {
-    const { style, ...otherAttributes } = attributes || {};
+    const { style, __expensiveStyle__, ...otherAttributes } = attributes || {};
 
-    return preact_h(
-        style ? styled(nodeName, style) : nodeName,
-        otherAttributes,
-        children
-    );
+    if (__expensiveStyle__) {
+        component = expensivelyStyled(component, __expensiveStyle__);
+    }
+
+    if (style) {
+        component = cheaplyStyled(component, style);
+    }
+
+    return preact_h(component, otherAttributes, children);
 };
