85a9a9d3453a24b37bffadb9206c63452dbe8152
Simon Adcock <simon.adcock@guardian.co.uk>
2017-09-18 12:12:08 +0100

    transform projects/common/modules/crosswords/constants.js to ES6 module

--- static/src/javascripts/projects/common/modules/crosswords/constants.js
+++ static/src/javascripts/projects/common/modules/crosswords/constants.js
@@ -1,7 +1,5 @@
-define(function () {
-    return {
-        cellSize: 31,
-        borderSize: 1,
-        numberSize: 9
-    };
-});
+export default {
+    cellSize: 31,
+    borderSize: 1,
+    numberSize: 9
+};
