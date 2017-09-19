ceec985115bb9290c502f933b03e0dd7e1072247
Alex Sanders <a@snd.rs>
2017-09-19 12:46:30 +0100

    manually handle emoition

--- ui/src/utils/h.jsx
+++ ui/src/utils/h.jsx
@@ -6,13 +6,22 @@
 
 import { h as preact_h } from 'preact';
 import { styled } from 'styletron-preact';
+import { css } from 'emotion';
 
 export default (
     component: string,
     attributes: ?Object,
     ...children: Array<any>
 ) => {
-    const { style, ...otherAttributes } = attributes || {};
+    const { style, expensiveCSS, ...otherAttributes } = attributes || {};
+
+    if (typeof expensiveCSS !== 'undefined' && Object.keys(expensiveCSS).length)
+        otherAttributes.className = [
+            css(expensiveCSS),
+            otherAttributes.className,
+        ]
+            .join(' ')
+            .trim();
 
     return preact_h(
         style ? styled(component, style) : component,
