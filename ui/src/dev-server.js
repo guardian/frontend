9deeed8078e59a25abe034ac2aed42950c38563c
Simon Adcock <simon.adcock@guardian.co.uk>
2017-09-19 10:37:57 +0100

    unused dependency

--- ui/src/dev-server.js
+++ ui/src/dev-server.js
@@ -1,7 +1,6 @@
 // @flow
 
 const express = require('express');
-const path = require('path');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
