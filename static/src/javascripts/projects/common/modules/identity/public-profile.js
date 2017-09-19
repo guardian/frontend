78e02843d124521a3ad11d62908af8c97f6ac0f1
GHaberis <george.haberis@theguardian.com>
2017-09-19 10:14:14 +0100

    transform projects/common/modules/identity/public-profile.js to ES6 module

--- static/src/javascripts/projects/common/modules/identity/public-profile.js
+++ static/src/javascripts/projects/common/modules/identity/public-profile.js
@@ -1,80 +1,69 @@
-define([
-    'bean',
-    'bonzo',
-    'lib/$',
-    'lib/config',
-    'lib/url',
-    'common/modules/component',
-    'common/modules/discussion/activity-stream',
-    'lodash/objects/mapValues'
-], function (
-    bean,
-    bonzo,
-    $,
-    config,
-    url,
-    component,
-    ActivityStream,
-    mapValues
-) {
-    function getActivityStream(cb) {
-        var activityStream, dataOpts = {
-            userId: 'data-user-id',
-            streamType: 'data-stream-type'
-        };
-        $('.js-activity-stream').each(function (el) {
-            var opts = mapValues(dataOpts, function (key) {
-                return el.getAttribute(key);
-            });
+import bean from 'bean';
+import bonzo from 'bonzo';
+import $ from 'lib/$';
+import config from 'lib/config';
+import url from 'lib/url';
+import component from 'common/modules/component';
+import ActivityStream from 'common/modules/discussion/activity-stream';
+import mapValues from 'lodash/objects/mapValues';
 
-            opts.page = url.getUrlVars().page || 1;
-
-            (activityStream = new ActivityStream.ActivityStream(opts)).fetch(el).then(function () {
-                bonzo(el).removeClass('activity-stream--loading');
-            });
-        }).addClass('activity-stream--loading');
-        cb(activityStream);
-        return activityStream;
-    }
-
-    function selectTab(el) {
-        $('.tabs__tab--selected').removeClass('tabs__tab--selected');
-        bonzo(el).parent().addClass('tabs__tab--selected');
-    }
-
-    function setupActivityStreamChanger(activityStream) {
-        bean.on(document.body, 'click', '.js-activity-stream-change', function (e) {
-            var el = e.currentTarget,
-                streamType = el.getAttribute('data-stream-type');
-            e.preventDefault();
-            selectTab(el);
-
-            activityStream.change({
-                page: 1,
-                streamType: streamType
-            });
-        });
-    }
-
-    function setupActivityStreamSearch(activityStream) {
-        bean.on(document.body, 'submit', '.js-activity-stream-search', function (e) {
-            var q = e.currentTarget.elements.q.value;
-            e.preventDefault();
-            selectTab($('a[data-stream-type="discussions"]'));
-            activityStream.change({
-                streamType: q !== '' ? 'search/' + encodeURIComponent(q) : 'comments'
-            });
-        });
-    }
-
-    function init() {
-        getActivityStream(function (activityStream) {
-            setupActivityStreamChanger(activityStream);
-            setupActivityStreamSearch(activityStream);
-        });
-    }
-
-    return {
-        init: init
+function getActivityStream(cb) {
+    var activityStream, dataOpts = {
+        userId: 'data-user-id',
+        streamType: 'data-stream-type'
     };
-});
+    $('.js-activity-stream').each(function(el) {
+        var opts = mapValues(dataOpts, function(key) {
+            return el.getAttribute(key);
+        });
+
+        opts.page = url.getUrlVars().page || 1;
+
+        (activityStream = new ActivityStream.ActivityStream(opts)).fetch(el).then(function() {
+            bonzo(el).removeClass('activity-stream--loading');
+        });
+    }).addClass('activity-stream--loading');
+    cb(activityStream);
+    return activityStream;
+}
+
+function selectTab(el) {
+    $('.tabs__tab--selected').removeClass('tabs__tab--selected');
+    bonzo(el).parent().addClass('tabs__tab--selected');
+}
+
+function setupActivityStreamChanger(activityStream) {
+    bean.on(document.body, 'click', '.js-activity-stream-change', function(e) {
+        var el = e.currentTarget,
+            streamType = el.getAttribute('data-stream-type');
+        e.preventDefault();
+        selectTab(el);
+
+        activityStream.change({
+            page: 1,
+            streamType: streamType
+        });
+    });
+}
+
+function setupActivityStreamSearch(activityStream) {
+    bean.on(document.body, 'submit', '.js-activity-stream-search', function(e) {
+        var q = e.currentTarget.elements.q.value;
+        e.preventDefault();
+        selectTab($('a[data-stream-type="discussions"]'));
+        activityStream.change({
+            streamType: q !== '' ? 'search/' + encodeURIComponent(q) : 'comments'
+        });
+    });
+}
+
+function init() {
+    getActivityStream(function(activityStream) {
+        setupActivityStreamChanger(activityStream);
+        setupActivityStreamSearch(activityStream);
+    });
+}
+
+export default {
+    init: init
+};
