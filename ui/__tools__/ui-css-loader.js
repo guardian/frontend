76ed2fe6c1243bf528f9e35f9060e4d64c40a555
Alex Sanders <a@snd.rs>
2017-09-19 12:47:42 +0100

    add UI css loader

---
+++ ui/__tools__/ui-css-loader.js
@@ -0,0 +1,89 @@
+// This loader breaks the webpack rule of 'do one thing' because we use it internally
+// and it makes life easier than passing loads of stuff about, reparsing it per transform etc
+
+import postcss from 'postcss';
+import customProperties from 'postcss-custom-properties';
+import decamelize from 'decamelize';
+
+import { parseCSS as emotionParser } from 'babel-plugin-emotion/lib/parser';
+
+import * as pasteup from '../src/pasteup';
+
+// convert pasteup exports to css vars
+// e.g. a.b.c => --a-b-c
+const toVars = (vars, prefix = false) =>
+    Object.keys(vars).reduce((cssVars, key) => {
+        const varName = [prefix, decamelize(key, '-')]
+            .filter(Boolean)
+            .join('-');
+
+        if (typeof vars[key] === 'object') {
+            return Object.assign({}, cssVars, toVars(vars[key], varName));
+        }
+
+        return Object.assign({}, cssVars, {
+            [varName]: vars[key],
+        });
+    }, {});
+
+// our own processing of the CSS source file
+const normaliseCSS = source =>
+    postcss()
+        .use(
+            customProperties({
+                variables: toVars(pasteup),
+            })
+        )
+        .process(source).css;
+
+// turn class selectors into reasonable keys
+// {'.header': 'blah} => {'header': 'blah}
+const normaliseKeys = styles =>
+    Object.keys(styles).reduce(
+        (normalised, key) =>
+            Object.assign({}, normalised, {
+                [key.replace(/^\./, '')]: styles[key],
+            }),
+        {}
+    );
+
+// splits rules into cheap/expensive clusters where possible which
+// can then be handled appropriately
+const categoriseStyles = styles =>
+    Object.keys(styles).reduce((categorisedStyles, key) => {
+        const rule = styles[key];
+
+        const categorisedStyle = Object.keys(
+            rule
+        ).reduce(({ style = {}, expensiveCSS = {} }, decl) => {
+            if (typeof rule[decl] !== 'object') {
+                return {
+                    expensiveCSS,
+                    style: Object.assign({}, style, {
+                        [decl]: rule[decl],
+                    }),
+                };
+            }
+            return {
+                style,
+                expensiveCSS: Object.assign({}, expensiveCSS, {
+                    [decl]: rule[decl],
+                }),
+            };
+        }, {});
+
+        return Object.assign(categorisedStyles, {
+            [key]: categorisedStyle,
+        });
+    }, {});
+
+module.exports = function uiCSS(source) {
+    const normalisedSource = normaliseCSS(source);
+
+    const { styles } = emotionParser(normalisedSource);
+
+    const normalisedStyles = normaliseKeys(styles);
+    const categorisedStyles = categoriseStyles(normalisedStyles);
+
+    return `module.exports = ${JSON.stringify(categorisedStyles)};`;
+};
