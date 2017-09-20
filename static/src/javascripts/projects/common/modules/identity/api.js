302e2cf4663fbabacde01af7f1ad6980bdcffc10
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-09-20 16:29:26 +0100

    transform projects/common/modules/identity/api.js content to ES6

--- static/src/javascripts/projects/common/modules/identity/api.js
+++ static/src/javascripts/projects/common/modules/identity/api.js
@@ -11,8 +11,9 @@ import asyncCallMerger from 'common/modules/asyncCallMerger';
  * We'll need to change this once there is some state change
  * TODO(jamesgorrie): Allow this to show policies too (not needed yet)
  */
-var Id = {},
-    userFromCookieCache = null;
+const Id = {};
+
+let userFromCookieCache = null;
 
 Id.cookieName = 'GU_U';
 Id.signOutCookieName = 'GU_SO';
@@ -21,7 +22,7 @@ Id.lastRefreshKey = 'identity.lastRefresh';
 Id.idApiRoot = null;
 Id.idUrl = null;
 
-Id.init = function() {
+Id.init = () => {
     Id.idApiRoot = config.page.idApiUrl;
     Id.idUrl = config.page.idUrl;
     mediator.emit('module:identity:api:loaded');
@@ -30,7 +31,7 @@ Id.init = function() {
 /**
  * Clears the caches and state, primarily for testing.
  */
-Id.reset = function() {
+Id.reset = () => {
     Id.getUserFromApi.reset();
     userFromCookieCache = null;
 };
@@ -49,16 +50,15 @@ Id.reset = function() {
  *
  * @return {?Object} the user information
  */
-Id.getUserFromCookie = function() {
+Id.getUserFromCookie = () => {
     if (userFromCookieCache === null) {
-        var cookieData = cookies.getCookie(Id.cookieName),
-            userData = cookieData ? JSON.parse(Id.decodeBase64(cookieData.split('.')[0])) : null;
+        const cookieData = cookies.getCookie(Id.cookieName), userData = cookieData ? JSON.parse(Id.decodeBase64(cookieData.split('.')[0])) : null;
         if (userData) {
-            var displayName = decodeURIComponent(userData[2]);
+            const displayName = decodeURIComponent(userData[2]);
             userFromCookieCache = {
                 id: userData[0],
                 primaryEmailAddress: userData[1], // not sure where this is stored now - not in the cookie any more
-                displayName: displayName,
+                displayName,
                 accountCreatedDate: userData[6],
                 emailVerified: userData[7],
                 rawResponse: cookieData
@@ -72,37 +72,31 @@ Id.getUserFromCookie = function() {
 /**
  * @return {string}
  */
-Id.getCookie = function() {
-    return cookies.getCookie(Id.cookieName);
-};
+Id.getCookie = () => cookies.getCookie(Id.cookieName);
 
 /**
  * @return {boolean}
  */
-Id.isUserLoggedIn = function() {
-    return Id.getUserFromCookie() !== null;
-};
+Id.isUserLoggedIn = () => Id.getUserFromCookie() !== null;
 
 /**
  * @return {string}
  */
-Id.getUrl = function() {
-    return Id.idUrl;
-};
+Id.getUrl = () => Id.idUrl;
 
 /**
  * Gets the currently logged in user data from the identity api
  * @param {function} callback
  */
 Id.getUserFromApi = asyncCallMerger.mergeCalls(
-    function(mergingCallback) {
+    mergingCallback => {
         if (Id.isUserLoggedIn()) {
             ajax.ajax({
                 url: Id.idApiRoot + '/user/me',
                 type: 'jsonp',
                 crossOrigin: true
             }).then(
-                function(response) {
+                response => {
                     if (response.status === 'ok') {
                         mergingCallback(response.user);
                     } else {
@@ -120,15 +114,15 @@ Id.getUserFromApi = asyncCallMerger.mergeCalls(
  * Gets the currently logged in user data from the identity api and
  * refreshes the users cookie at the same time.
  */
-Id.getUserFromApiWithRefreshedCookie = function() {
-    var endpoint = '/user/me',
-        request = ajax.ajax({
-            url: Id.idApiRoot + endpoint,
-            type: 'jsonp',
-            data: {
-                refreshCookie: true
-            }
-        });
+Id.getUserFromApiWithRefreshedCookie = () => {
+    const endpoint = '/user/me',
+          request = ajax.ajax({
+              url: Id.idApiRoot + endpoint,
+              type: 'jsonp',
+              data: {
+                  refreshCookie: true
+              }
+          });
 
     return request;
 };
@@ -136,12 +130,12 @@ Id.getUserFromApiWithRefreshedCookie = function() {
 /**
  * Returns user object when signed in, otherwise redirects to sign in with configurable absolute returnUrl
  */
-Id.getUserOrSignIn = function(returnUrl) {
+Id.getUserOrSignIn = returnUrl => {
     if (Id.isUserLoggedIn()) {
         return Id.getUserFromCookie();
     } else {
         returnUrl = encodeURIComponent(returnUrl || document.location.href);
-        var url = Id.getUrl() + '/signin?returnUrl=' + returnUrl;
+        const url = Id.getUrl() + '/signin?returnUrl=' + returnUrl;
         Id.redirectTo(url);
     }
 };
@@ -149,7 +143,7 @@ Id.getUserOrSignIn = function(returnUrl) {
 /**
  * Wrap window.location.href so it can be spied in unit tests
  */
-Id.redirectTo = function(url) {
+Id.redirectTo = url => {
     window.location.href = url;
 };
 
@@ -158,15 +152,13 @@ Id.redirectTo = function(url) {
  * @param {string} str
  * @return {string}
  */
-Id.decodeBase64 = function(str) {
-    return decodeURIComponent(escape(window.atob(str.replace(/-/g, '+').replace(/_/g, '/').replace(/,/g, '='))));
-};
+Id.decodeBase64 = str => decodeURIComponent(escape(window.atob(str.replace(/-/g, '+').replace(/_/g, '/').replace(/,/g, '='))));
 
 /**
  * @return {Boolean}
  */
-Id.hasUserSignedOutInTheLast24Hours = function() {
-    var cookieData = cookies.getCookie(Id.signOutCookieName);
+Id.hasUserSignedOutInTheLast24Hours = () => {
+    const cookieData = cookies.getCookie(Id.signOutCookieName);
 
     if (cookieData) {
         return ((Math.round(new Date().getTime() / 1000)) < (parseInt(cookieData, 10) + 86400));
@@ -178,43 +170,42 @@ Id.hasUserSignedOutInTheLast24Hours = function() {
  * Returns true if a there is no signed in user and the user has not signed in the last 24 hours
  */
 Id.shouldAutoSigninInUser = function() {
-    var signedInUser = !!cookies.getCookie(Id.cookieName),
-        checkFacebook = !!storage.local.get(Id.fbCheckKey);
+    const signedInUser = !!cookies.getCookie(Id.cookieName), checkFacebook = !!storage.local.get(Id.fbCheckKey);
     return !signedInUser && !checkFacebook && !this.hasUserSignedOutInTheLast24Hours();
 };
 
-Id.setNextFbCheckTime = function(nextFbCheckDue) {
+Id.setNextFbCheckTime = nextFbCheckDue => {
     storage.local.set(Id.fbCheckKey, {}, {
         expires: nextFbCheckDue
     });
 };
 
-Id.emailSignup = function(listId) {
-    var endpoint = '/useremails/' + Id.getUserFromCookie().id + '/subscriptions',
-        data = {
-            'listId': listId
-        },
-        request = ajax.ajax({
-            url: Id.idApiRoot + endpoint,
-            type: 'jsonp',
-            crossOrigin: true,
-            data: {
-                body: JSON.stringify(data),
-                method: 'post'
-            }
-        });
+Id.emailSignup = listId => {
+    const endpoint = '/useremails/' + Id.getUserFromCookie().id + '/subscriptions',
+          data = {
+              'listId': listId
+          },
+          request = ajax.ajax({
+              url: Id.idApiRoot + endpoint,
+              type: 'jsonp',
+              crossOrigin: true,
+              data: {
+                  body: JSON.stringify(data),
+                  method: 'post'
+              }
+          });
 
     return request;
 };
 
-Id.getUserEmailSignUps = function() {
+Id.getUserEmailSignUps = () => {
     if (Id.getUserFromCookie()) {
-        var endpoint = '/useremails/' + Id.getUserFromCookie().id,
-            request = ajax.ajax({
-                url: Id.idApiRoot + endpoint,
-                type: 'jsonp',
-                crossOrigin: true
-            });
+        const endpoint = '/useremails/' + Id.getUserFromCookie().id,
+              request = ajax.ajax({
+                  url: Id.idApiRoot + endpoint,
+                  type: 'jsonp',
+                  crossOrigin: true
+              });
 
         return request;
     }
@@ -222,40 +213,40 @@ Id.getUserEmailSignUps = function() {
     return Promise.resolve(null);
 };
 
-Id.sendValidationEmail = function() {
-    var endpoint = '/user/send-validation-email',
-        request = ajax.ajax({
-            url: Id.idApiRoot + endpoint,
-            type: 'jsonp',
-            crossOrigin: true,
-            data: {
-                method: 'post'
-            }
-        });
+Id.sendValidationEmail = () => {
+    const endpoint = '/user/send-validation-email',
+          request = ajax.ajax({
+              url: Id.idApiRoot + endpoint,
+              type: 'jsonp',
+              crossOrigin: true,
+              data: {
+                  method: 'post'
+              }
+          });
 
     return request;
 };
 
-Id.updateUsername = function(username) {
-    var endpoint = '/user/me',
-        data = {
-            'publicFields': {
-                'username': username,
-                'displayName': username
-            }
-        },
-        request = ajax.ajax({
-            url: Id.idApiRoot + endpoint,
-            type: 'json',
-            crossOrigin: true,
-            method: 'POST',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data),
-            withCredentials: true,
-            headers: {
-                'X-GU-ID-Client-Access-Token': 'Bearer ' + config.page.idApiJsClientToken
-            }
-        });
+Id.updateUsername = username => {
+    const endpoint = '/user/me',
+          data = {
+              'publicFields': {
+                  'username': username,
+                  'displayName': username
+              }
+          },
+          request = ajax.ajax({
+              url: Id.idApiRoot + endpoint,
+              type: 'json',
+              crossOrigin: true,
+              method: 'POST',
+              contentType: 'application/json; charset=utf-8',
+              data: JSON.stringify(data),
+              withCredentials: true,
+              headers: {
+                  'X-GU-ID-Client-Access-Token': 'Bearer ' + config.page.idApiJsClientToken
+              }
+          });
 
     return request;
 };
