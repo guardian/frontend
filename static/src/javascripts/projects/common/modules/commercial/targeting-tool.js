a7a8b308762b77afdb3304c0f8572e5ddd8369e9
Joseph Smith <joseph.smith@guardian.co.uk>
2017-09-21 11:53:51 +0100

    Prettier

--- static/src/javascripts/projects/common/modules/commercial/targeting-tool.js
+++ static/src/javascripts/projects/common/modules/commercial/targeting-tool.js
@@ -1,20 +1,24 @@
+// @flow
 import config from 'lib/config';
 
 const clean = (str: string) => str.trim().toLowerCase();
 
 const campaignsFor = (id: string): Array<object> => {
     try {
-        return config.page.campaigns.filter(campaign =>
-            campaign.fields && clean(campaign.fields.campaignId) === clean(id)
+        return config.page.campaigns.filter(
+            campaign =>
+                campaign.fields &&
+                clean(campaign.fields.campaignId) === clean(id)
         );
     } catch (e) {
         return [];
     }
 };
 
-const isAbTestTargeted = (test: ABTest): boolean => campaignsFor(test.campaignId).length > 0;
+const isAbTestTargeted = (test: ABTest): boolean =>
+    campaignsFor(test.campaignId).length > 0;
 
 export default {
     isAbTestTargeted,
-    campaignsFor
+    campaignsFor,
 };
