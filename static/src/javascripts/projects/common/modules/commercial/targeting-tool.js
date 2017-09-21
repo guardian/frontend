f787f7f77f8773a5f934acd87d77cb4f2737773d
Joseph Smith <joseph.smith@guardian.co.uk>
2017-09-21 11:51:54 +0100

    Function expressions and flow types

--- static/src/javascripts/projects/common/modules/commercial/targeting-tool.js
+++ static/src/javascripts/projects/common/modules/commercial/targeting-tool.js
@@ -1,32 +1,18 @@
 import config from 'lib/config';
 
-function clean(string) {
-    return string.trim().toLowerCase();
-}
+const clean = (str: string) => str.trim().toLowerCase();
 
-/**
- * Get the campaigns for a test.
- *
- * @param test      an A/B test object (with an 'id' property)
- * @return {Array}  campaigns associated with this test
- */
-function campaignsFor(id) {
+const campaignsFor = (id: string): Array<object> => {
     try {
-        return config.page.campaigns.filter(campaign => campaign.fields && clean(campaign.fields.campaignId) === clean(id));
+        return config.page.campaigns.filter(campaign =>
+            campaign.fields && clean(campaign.fields.campaignId) === clean(id)
+        );
     } catch (e) {
         return [];
     }
-}
+};
 
-/**
- * Check if any of the active campaigns (from the targeting tool) apply to a given A/B test.
- *
- * @param test        an A/B test object (with an 'id' property)
- * @return {Boolean}
- */
-function isAbTestTargeted(test) {
-    return campaignsFor(test.campaignId).length > 0;
-}
+const isAbTestTargeted = (test: ABTest): boolean => campaignsFor(test.campaignId).length > 0;
 
 export default {
     isAbTestTargeted,
