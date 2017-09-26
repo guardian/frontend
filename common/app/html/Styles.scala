47d83aa5af11e087138279469ad5078483ae0064
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-09-26 17:23:52 +0100

    Refactor Interactive top level template

--- common/app/html/Styles.scala
+++ common/app/html/Styles.scala
@@ -1,12 +1,21 @@
 package html
 
+import model.ApplicationContext
 import play.twirl.api.Html
 
 trait Styles {
-  def criticalCss: Html
+  def criticalCssLink: Html
+  def criticalCssInline: Html
   def linkCss: Html
   def oldIECriticalCss: Html
   def oldIELinkCss: Html
   def IE9CriticalCss: Html
   def IE9LinkCss: Html
+
+  def criticalCss(implicit context: ApplicationContext): Html =
+    if(context.environment.mode == play.api.Mode.Dev || !conf.switches.Switches.InlineCriticalCss.isSwitchedOn) {
+      criticalCssLink
+    } else {
+     criticalCssInline
+    }
 }
