6fffcec4ed349e6a44ec95c7662be6c8467d67db
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-09-28 11:52:30 +0100

    Minute has immersive header

--- article/app/pages/MinuteHtmlPage.scala
+++ article/app/pages/MinuteHtmlPage.scala
@@ -2,18 +2,17 @@ package pages
 
 import controllers.MinutePage
 import html.HtmlPage
-import html.HtmlPageHelpers._
 import model.ApplicationContext
 import play.api.mvc.RequestHeader
 import play.twirl.api.Html
-import views.html.fragments.minuteBody
+import views.html.fragments.{immersiveHeader, minuteBody}
 
 object MinuteHtmlPage extends HtmlPage[MinutePage] {
 
     def html(page: MinutePage)(implicit request: RequestHeader, applicationContext: ApplicationContext): Html = {
       implicit val p: MinutePage = page
       StoryHtmlPage.html(
-        header = guardianHeaderHtml(),
+        header = immersiveHeader(),
         content = minuteBody(page)
       )
     }
