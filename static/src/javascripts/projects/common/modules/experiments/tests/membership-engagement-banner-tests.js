d4c623e0f41ef3ec1cf5af57af76d2b45548ae72
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-09-29 16:11:54 +0100

    Delete switches and main file of test s&C test 3

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,6 +1,5 @@
 // @flow
-import { acquisitionsSupportUsRecurringContribution } from 'common/modules/experiments/tests/acquisitions-support-us-recurring-contributions';
 
 export const membershipEngagementBannerTests: $ReadOnlyArray<
     AcquisitionsABTest
-> = [acquisitionsSupportUsRecurringContribution];
+> = [];
