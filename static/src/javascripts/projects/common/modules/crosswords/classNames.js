572da3396332c02a5c34bb0b2025cd6f64272539
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-02 15:22:50 +0100

    transform projects/common/modules/crosswords/classNames.js to ES6 module

--- static/src/javascripts/projects/common/modules/crosswords/classNames.js
+++ static/src/javascripts/projects/common/modules/crosswords/classNames.js
@@ -1,9 +1,7 @@
-define(function () {
-
-    return function(props) {
-        return Object.keys(props)
-            .filter(function(f){ return props[f] === true; })
-            .join(' ');
-    };
-
-});
+export default function(props) {
+    return Object.keys(props)
+        .filter(function(f) {
+            return props[f] === true;
+        })
+        .join(' ');
+};
