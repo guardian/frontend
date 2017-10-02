1e7ae361f08b9353d3c8c8fe5510f0ead274527c
Alex Sanders <a@snd.rs>
2017-10-02 12:21:13 +0100

    lint

--- static/src/javascripts/projects/common/modules/identity/delete-account.js
+++ static/src/javascripts/projects/common/modules/identity/delete-account.js
@@ -1,33 +1,35 @@
+// @flow
 import $ from 'lib/$';
 import bean from 'bean';
 import fastdom from 'fastdom';
+
 const deleteButtonElm = $('#deleteButton')[0];
 const deleteFormElm = $('#deleteForm')[0];
 const deleteLoaderElm = $('#deleteLoader')[0];
 
-function disableDeleteButton() {
-    fastdom.write(() => {
-        deleteButtonElm && (deleteButtonElm.disabled = true);
-    });
-}
+const disableDeleteButton = (): void => {
+    if (deleteButtonElm) {
+        fastdom.write(() => {
+            deleteButtonElm.disabled = true;
+        });
+    }
+};
 
-function showLoader() {
-    fastdom.write(() => {
-        deleteLoaderElm && deleteLoaderElm.classList.remove("is-hidden");
-    });
-}
+const showLoader = (): void => {
+    if (deleteLoaderElm) {
+        fastdom.write(() => {
+            deleteLoaderElm.classList.remove('is-hidden');
+        });
+    }
+};
 
-function setupLoadingAnimation() {
+const setupLoadingAnimation = (): void => {
     if (deleteFormElm && deleteLoaderElm) {
         bean.on(deleteFormElm, 'submit', () => {
             disableDeleteButton();
             showLoader();
         });
     }
-}
-
-export default {
-    init() {
-        setupLoadingAnimation();
-    }
 };
+
+export { setupLoadingAnimation };
