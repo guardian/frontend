3b81bee3f942c5663f8b0449e8812c9ce8802795
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-02 14:31:19 +0100

    Run lint fix

--- static/src/javascripts/projects/common/modules/crosswords/cell.js
+++ static/src/javascripts/projects/common/modules/crosswords/cell.js
@@ -5,7 +5,6 @@ import { constants } from 'common/modules/crosswords/constants';
 import classNames from 'common/modules/crosswords/classNames';
 
 const Cell = React.createClass({
-
     onClick(event) {
         event.preventDefault();
         this.props.handleSelect(this.props.x, this.props.y);
@@ -17,46 +16,57 @@ const Cell = React.createClass({
 
         let cellNumber = null;
         if (this.props.number !== undefined) {
-            cellNumber = React.createElement('text', {
-                x: left + 1,
-                y: top + constants.numberSize,
-                key: 'number',
-                className: 'crossword__cell-number'
-            }, this.props.number);
+            cellNumber = React.createElement(
+                'text',
+                {
+                    x: left + 1,
+                    y: top + constants.numberSize,
+                    key: 'number',
+                    className: 'crossword__cell-number',
+                },
+                this.props.number
+            );
         }
 
         let cellValue = null;
         if (this.props.value !== undefined) {
-            cellValue = React.createElement('text', {
-                x: left + constants.cellSize * .5,
-                y: top + constants.cellSize * .675,
-                key: 'entry',
-                className: classNames({
-                    'crossword__cell-text': true,
-                    'crossword__cell-text--focussed': this.props.isFocussed,
-                    'crossword__cell-text--error': this.props.isError
-                }),
-                textAnchor: 'middle'
-            }, this.props.value);
+            cellValue = React.createElement(
+                'text',
+                {
+                    x: left + constants.cellSize * 0.5,
+                    y: top + constants.cellSize * 0.675,
+                    key: 'entry',
+                    className: classNames({
+                        'crossword__cell-text': true,
+                        'crossword__cell-text--focussed': this.props.isFocussed,
+                        'crossword__cell-text--error': this.props.isError,
+                    }),
+                    textAnchor: 'middle',
+                },
+                this.props.value
+            );
         }
 
-        return React.createElement('g', {
-                onClick: this.onClick
-            }, React.createElement('rect', {
+        return React.createElement(
+            'g',
+            {
+                onClick: this.onClick,
+            },
+            React.createElement('rect', {
                 x: left,
                 y: top,
                 width: constants.cellSize,
                 height: constants.cellSize,
                 className: classNames({
-                    'crossword__cell': true,
+                    crossword__cell: true,
                     'crossword__cell--focussed': this.props.isFocussed,
-                    'crossword__cell--highlighted': this.props.isHighlighted
-                })
+                    'crossword__cell--highlighted': this.props.isHighlighted,
+                }),
             }),
             cellNumber,
             cellValue
         );
-    }
+    },
 });
 
 export default Cell;
