c3e3864be6491dd967264dd3298c426386c8a5df
Alex Sanders <a@snd.rs>
2017-10-02 12:13:06 +0100

    transform projects/common/modules/identity/delete-account.js to ES6 module

--- static/src/javascripts/projects/common/modules/identity/delete-account.js
+++ static/src/javascripts/projects/common/modules/identity/delete-account.js
@@ -1,40 +1,33 @@
-define([
-    'lib/$',
-    'bean',
-    'fastdom'
-], function (
-    $,
-    bean,
-    fastdom
-) {
-    var deleteButtonElm = $('#deleteButton')[0];
-    var deleteFormElm = $('#deleteForm')[0];
-    var deleteLoaderElm = $('#deleteLoader')[0];
+import $ from 'lib/$';
+import bean from 'bean';
+import fastdom from 'fastdom';
+var deleteButtonElm = $('#deleteButton')[0];
+var deleteFormElm = $('#deleteForm')[0];
+var deleteLoaderElm = $('#deleteLoader')[0];
 
-    function disableDeleteButton() {
-        fastdom.write(function () {
-            deleteButtonElm && (deleteButtonElm.disabled = true);
+function disableDeleteButton() {
+    fastdom.write(function() {
+        deleteButtonElm && (deleteButtonElm.disabled = true);
+    });
+}
+
+function showLoader() {
+    fastdom.write(function() {
+        deleteLoaderElm && deleteLoaderElm.classList.remove("is-hidden");
+    });
+}
+
+function setupLoadingAnimation() {
+    if (deleteFormElm && deleteLoaderElm) {
+        bean.on(deleteFormElm, 'submit', function() {
+            disableDeleteButton();
+            showLoader();
         });
     }
+}
 
-    function showLoader() {
-        fastdom.write(function () {
-            deleteLoaderElm && deleteLoaderElm.classList.remove("is-hidden");
-        });
+export default {
+    init: function() {
+        setupLoadingAnimation();
     }
-
-    function setupLoadingAnimation() {
-        if (deleteFormElm && deleteLoaderElm) {
-            bean.on(deleteFormElm, 'submit', function() {
-                disableDeleteButton();
-                showLoader();
-            });
-        }
-    }
-
-    return {
-        init: function () {
-            setupLoadingAnimation();
-        }
-    };
-});
+};
