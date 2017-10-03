aaf67c8182c1d4c5c62f32940bc3c8b579d53023
Alex Ware <alex.ware@guardian.co.uk>
2017-10-03 10:22:28 +0100

    transform projects/membership/digitalpack-tab.js content to ES6

--- static/src/javascripts/projects/membership/digitalpack-tab.js
+++ static/src/javascripts/projects/membership/digitalpack-tab.js
@@ -6,35 +6,13 @@ import reportError from 'lib/report-error';
 import formatters from 'membership/formatters';
 import stripe from 'membership/stripe';
 
-var PACKAGE_COST = '.js-dig-package-cost',
-    PAYMENT_FORM = '.js-dig-card-details',
-    SUBSCRIBER_ID = '.js-dig-subscriber-id',
-    REMAINING_TRIAL_LENGTH = '.js-dig-remaining-trial-length',
-    REMAINING_TRIAL_LENGTH_CONTAINER = '.js-dig-remaining-trial-length-container',
-    PACKAGE_CURRENT_RENEWAL_DATE = '.js-dig-current-renewal-date',
-    PACKAGE_CURRENT_PERIOD_END = '.js-dig-current-period-end',
-    PACKAGE_CURRENT_PERIOD_START = '.js-dig-current-period-start',
-    PACKAGE_NEXT_PAYMENT_DATE = '.js-dig-next-payment-date',
-    PACKAGE_NEXT_PAYMENT_PRICE = '.js-dig-next-payment-price',
-    PACKAGE_NEXT_PAYMENT_CONTAINER = '.js-dig-next-payment-container',
-    PACKAGE_INTERVAL = '.js-dig-plan-interval',
-    DETAILS_JOIN_DATE = '.js-dig-join-date',
-    NOTIFICATION_CANCEL = '.js-dig-cancel-tier',
-    DIGITALPACK_DETAILS = '.js-dig-details',
-    DIGITALPACK_PRODUCT = '.js-dig-product',
-    UP_SELL = '.js-dig-up-sell',
-    DIG_INFO = '.js-dig-info',
-    LOADER = '.js-dig-loader',
-    IS_HIDDEN_CLASSNAME = 'is-hidden',
-    ERROR = '.js-dig-error';
+const PACKAGE_COST = '.js-dig-package-cost', PAYMENT_FORM = '.js-dig-card-details', SUBSCRIBER_ID = '.js-dig-subscriber-id', REMAINING_TRIAL_LENGTH = '.js-dig-remaining-trial-length', REMAINING_TRIAL_LENGTH_CONTAINER = '.js-dig-remaining-trial-length-container', PACKAGE_CURRENT_RENEWAL_DATE = '.js-dig-current-renewal-date', PACKAGE_CURRENT_PERIOD_END = '.js-dig-current-period-end', PACKAGE_CURRENT_PERIOD_START = '.js-dig-current-period-start', PACKAGE_NEXT_PAYMENT_DATE = '.js-dig-next-payment-date', PACKAGE_NEXT_PAYMENT_PRICE = '.js-dig-next-payment-price', PACKAGE_NEXT_PAYMENT_CONTAINER = '.js-dig-next-payment-container', PACKAGE_INTERVAL = '.js-dig-plan-interval', DETAILS_JOIN_DATE = '.js-dig-join-date', NOTIFICATION_CANCEL = '.js-dig-cancel-tier', DIGITALPACK_DETAILS = '.js-dig-details', DIGITALPACK_PRODUCT = '.js-dig-product', UP_SELL = '.js-dig-up-sell', DIG_INFO = '.js-dig-info', LOADER = '.js-dig-loader', IS_HIDDEN_CLASSNAME = 'is-hidden', ERROR = '.js-dig-error';
 
 function fetchUserDetails() {
     fetch(config.page.userAttributesApiUrl + '/me/mma-digitalpack', {
         mode: 'cors',
         credentials: 'include',
-    }).then(function(resp) {
-        return resp.json();
-    }).then(function(json) {
+    }).then(resp => resp.json()).then(json => {
         if (json && json.subscription) {
             hideLoader();
             populateUserDetails(json);
@@ -42,7 +20,7 @@ function fetchUserDetails() {
             hideLoader();
             displayDigitalPackUpSell();
         }
-    }).catch(function(err) {
+    }).catch(err => {
         hideLoader();
         displayErrorMessage();
         reportError(err, {
@@ -64,7 +42,7 @@ function hideLoader() {
  * }} userDetails
  */
 function populateUserDetails(userDetails) {
-    var glyph = userDetails.subscription.plan.currency;
+    const glyph = userDetails.subscription.plan.currency;
     $(SUBSCRIBER_ID).text(userDetails.subscription.subscriberId);
     $(DIGITALPACK_PRODUCT).text(userDetails.subscription.plan.name);
     $(PACKAGE_COST).text(formatters.formatAmount(userDetails.subscription.plan.amount, glyph));
@@ -73,7 +51,7 @@ function populateUserDetails(userDetails) {
     $(PACKAGE_CURRENT_PERIOD_START).text(formatters.formatDate(userDetails.subscription.start));
     $(PACKAGE_CURRENT_PERIOD_END).text(formatters.formatDate(userDetails.subscription.end));
     $(PACKAGE_CURRENT_RENEWAL_DATE).text(formatters.formatDate(userDetails.subscription.renewalDate));
-    var trialLeft = userDetails.subscription.trialLength;
+    const trialLeft = userDetails.subscription.trialLength;
     if (trialLeft > 0) {
         $(REMAINING_TRIAL_LENGTH).text(trialLeft + ' day' + (trialLeft != 1 ? 's' : ''));
         $(REMAINING_TRIAL_LENGTH_CONTAINER).removeClass(IS_HIDDEN_CLASSNAME);
