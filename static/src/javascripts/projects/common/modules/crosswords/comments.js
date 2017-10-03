ef1e811b77ade1690cba6a995ac7005c2f22b318
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-03 11:25:07 +0100

    Fix import name

--- static/src/javascripts/projects/common/modules/crosswords/comments.js
+++ static/src/javascripts/projects/common/modules/crosswords/comments.js
@@ -1,13 +1,13 @@
 // @flow
 import config from 'lib/config';
-import { DiscussionLoader } from 'common/modules/discussion/loader';
+import { Loader } from 'common/modules/discussion/loader';
 import qwery from 'qwery';
 
 function initCrosswordDiscussion() {
     if (config.switches.enableDiscussionSwitch && config.page.commentable) {
         var el = qwery('.discussion')[0];
         if (el) {
-            new DiscussionLoader.Loader().attachTo(el);
+            new Loader().attachTo(el);
         }
     }
 }
