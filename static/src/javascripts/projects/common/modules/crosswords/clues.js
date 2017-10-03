b9574d565bb1e202ea96b27db6b8d4b9138db8a3
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-03 10:14:06 +0100

    make fix-commits

--- static/src/javascripts/projects/common/modules/crosswords/clues.js
+++ static/src/javascripts/projects/common/modules/crosswords/clues.js
@@ -1,5 +1,5 @@
 // @flow
-import {createElement, createClass, findDOMNode} from 'react/addons';
+import { createElement, createClass, findDOMNode } from 'react/addons';
 import bean from 'bean';
 import fastdom from 'fastdom';
 import { classNames } from 'common/modules/crosswords/classNames';
@@ -7,56 +7,65 @@ import { isBreakpoint } from 'lib/detect';
 import { scrollTo } from 'lib/scroller';
 
 const Clue = createClass({
-
     onClick() {
         this.props.setReturnPosition();
     },
 
     render() {
-        return createElement('li',
+        return createElement(
+            'li',
             null,
-            createElement('a', {
-                    href: '#' + this.props.id,
+            createElement(
+                'a',
+                {
+                    href: `#${this.props.id}`,
                     onClick: this.onClick,
                     className: classNames({
-                        'crossword__clue': true,
+                        crossword__clue: true,
                         'crossword__clue--answered': this.props.hasAnswered,
                         'crossword__clue--selected': this.props.isSelected,
-                        'crossword__clue--display-group-order': JSON.stringify(this.props.number) !== this.props.humanNumber
-                    })
-                }, createElement('div', {
-                    className: 'crossword__clue__number'
-                }, this.props.humanNumber),
+                        'crossword__clue--display-group-order':
+                            JSON.stringify(this.props.number) !==
+                            this.props.humanNumber,
+                    }),
+                },
+                createElement(
+                    'div',
+                    {
+                        className: 'crossword__clue__number',
+                    },
+                    this.props.humanNumber
+                ),
                 createElement('div', {
                     className: 'crossword__clue__text',
                     dangerouslySetInnerHTML: {
-                        __html: this.props.clue
-                    }
+                        __html: this.props.clue,
+                    },
                 })
             )
         );
-    }
+    },
 });
 
 const Clues = createClass({
-
     getInitialState() {
         return {
-            showGradient: true
+            showGradient: true,
         };
     },
 
     componentDidMount() {
         this.$cluesNode = findDOMNode(this.refs.clues);
 
-        const height = this.$cluesNode.scrollHeight - this.$cluesNode.clientHeight;
+        const height =
+            this.$cluesNode.scrollHeight - this.$cluesNode.clientHeight;
 
         bean.on(this.$cluesNode, 'scroll', e => {
             const showGradient = height - e.currentTarget.scrollTop > 25;
 
             if (this.state.showGradient !== showGradient) {
                 this.setState({
-                    showGradient
+                    showGradient,
                 });
             }
         });
@@ -66,10 +75,13 @@ const Clues = createClass({
      * Scroll clues into view when they're activated (i.e. clicked in the grid)
      */
     componentDidUpdate(prev) {
-        if (isBreakpoint({
+        if (
+            isBreakpoint({
                 min: 'tablet',
-                max: 'leftCol'
-            }) && (!prev.focussed || prev.focussed.id !== this.props.focussed.id)) {
+                max: 'leftCol',
+            }) &&
+            (!prev.focussed || prev.focussed.id !== this.props.focussed.id)
+        ) {
             fastdom.read(() => {
                 this.scrollIntoView(this.props.focussed);
             });
@@ -77,83 +89,99 @@ const Clues = createClass({
     },
 
     scrollIntoView(clue) {
-
         const buffer = 100;
         const node = findDOMNode(this.refs[clue.id]);
-        const visible = node.offsetTop - buffer > this.$cluesNode.scrollTop &&
-            node.offsetTop + buffer < this.$cluesNode.scrollTop + this.$cluesNode.clientHeight;
+        const visible =
+            node.offsetTop - buffer > this.$cluesNode.scrollTop &&
+            node.offsetTop + buffer <
+                this.$cluesNode.scrollTop + this.$cluesNode.clientHeight;
 
         if (!visible) {
-            const offset = node.offsetTop - (this.$cluesNode.clientHeight / 2);
+            const offset = node.offsetTop - this.$cluesNode.clientHeight / 2;
             scrollTo(offset, 250, 'easeOutQuad', this.$cluesNode);
         }
     },
 
     render() {
         const headerClass = 'crossword__clues-header';
-        const cluesByDirection = direction => this.props.clues.filter(clue => clue.entry.direction === direction).map(clue => createElement(Clue, {
-            ref: clue.entry.id,
-            id: clue.entry.id,
-            key: clue.entry.id,
-            number: clue.entry.number,
-            humanNumber: clue.entry.humanNumber,
-            clue: clue.entry.clue,
-            hasAnswered: clue.hasAnswered,
-            isSelected: clue.isSelected,
-            setReturnPosition: () => {
-                this.props.setReturnPosition(window.scrollY);
-            }
-        }));
+        const cluesByDirection = direction =>
+            this.props.clues
+                .filter(clue => clue.entry.direction === direction)
+                .map(clue =>
+                    createElement(Clue, {
+                        ref: clue.entry.id,
+                        id: clue.entry.id,
+                        key: clue.entry.id,
+                        number: clue.entry.number,
+                        humanNumber: clue.entry.humanNumber,
+                        clue: clue.entry.clue,
+                        hasAnswered: clue.hasAnswered,
+                        isSelected: clue.isSelected,
+                        setReturnPosition: () => {
+                            this.props.setReturnPosition(window.scrollY);
+                        },
+                    })
+                );
 
         return createElement(
-            'div', {
-                className: 'crossword__clues--wrapper ' + (this.state.showGradient ? '' : 'hide-gradient')
+            'div',
+            {
+                className: `crossword__clues--wrapper ${this.state.showGradient
+                    ? ''
+                    : 'hide-gradient'}`,
             },
             createElement(
-                'div', {
+                'div',
+                {
                     className: 'crossword__clues',
-                    ref: 'clues'
+                    ref: 'clues',
                 },
                 createElement(
-                    'div', {
-                        className: 'crossword__clues--across'
+                    'div',
+                    {
+                        className: 'crossword__clues--across',
                     },
                     createElement(
-                        'h3', {
-                            className: headerClass
+                        'h3',
+                        {
+                            className: headerClass,
                         },
                         'Across'
                     ),
                     createElement(
-                        'ol', {
-                            className: 'crossword__clues-list'
+                        'ol',
+                        {
+                            className: 'crossword__clues-list',
                         },
                         cluesByDirection('across')
                     )
                 ),
                 createElement(
-                    'div', {
-                        className: 'crossword__clues--down'
+                    'div',
+                    {
+                        className: 'crossword__clues--down',
                     },
                     createElement(
-                        'h3', {
-                            className: headerClass
+                        'h3',
+                        {
+                            className: headerClass,
                         },
                         'Down'
                     ),
                     createElement(
-                        'ol', {
-                            className: 'crossword__clues-list'
+                        'ol',
+                        {
+                            className: 'crossword__clues-list',
                         },
                         cluesByDirection('down')
                     )
                 )
             ),
             createElement('div', {
-                className: 'crossword__clues__gradient'
+                className: 'crossword__clues__gradient',
             })
         );
-    }
+    },
 });
 
 export default Clues;
