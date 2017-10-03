189676714977b3284262be7bf731cbd04270ea6e
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-03 16:47:23 +0100

    es6 fixes

--- static/src/javascripts/projects/common/modules/crosswords/hidden-input.js
+++ static/src/javascripts/projects/common/modules/crosswords/hidden-input.js
@@ -1,24 +1,34 @@
-import React from 'react/addons';
+// @flow
+import { createClass, createElement, findDOMNode } from 'react/addons';
 import bonzo from 'bonzo';
 import fastdom from 'fastdom';
-import $ from 'lib/$';
-import scroller from 'lib/scroller';
-import detect from 'lib/detect';
-const HiddenInput = React.createClass({
+import { scrollTo } from 'lib/scroller';
+import { isBreakpoint } from 'lib/detect';
 
+const HiddenInput = createClass({
     getInitialState() {
         return {
-            value: this.props.value
+            value: this.props.value,
         };
     },
 
     componentDidUpdate() {
-        if (detect.isBreakpoint({
-                max: 'mobile'
-            })) {
+        if (
+            isBreakpoint({
+                max: 'mobile',
+            })
+        ) {
             fastdom.read(() => {
-                const offsets = bonzo(React.findDOMNode(this.refs.input)).offset();
-                scroller.scrollTo(offsets.top - offsets.height * 1.5 - $('.crossword__sticky-clue').offset().height, 250, 'easeOutQuad');
+                const offsets = bonzo(findDOMNode(this.refs.input)).offset();
+                const clueHeight = document.getElementsByClassName(
+                    'crossword__sticky-clue'
+                )[0].offsetHeight;
+
+                scrollTo(
+                    offsets.top - offsets.height * 1.5 - clueHeight,
+                    250,
+                    'easeOutQuad'
+                );
             });
         }
     },
@@ -26,7 +36,7 @@ const HiddenInput = React.createClass({
     handleChange(event) {
         this.props.crossword.insertCharacter(event.target.value.toUpperCase());
         this.setState({
-            value: ''
+            value: '',
         });
     },
 
@@ -47,12 +57,13 @@ const HiddenInput = React.createClass({
     },
 
     render() {
-        return React.createElement(
-            'div', {
+        return createElement(
+            'div',
+            {
                 className: 'crossword__hidden-input-wrapper',
-                ref: 'wrapper'
+                ref: 'wrapper',
             },
-            React.createElement('input', {
+            createElement('input', {
                 type: 'text',
                 className: 'crossword__hidden-input',
                 maxLength: '1',
@@ -65,10 +76,10 @@ const HiddenInput = React.createClass({
                 autoComplete: 'off',
                 spellCheck: 'false',
                 autoCorrect: 'off',
-                ref: 'input'
+                ref: 'input',
             })
         );
-    }
+    },
 });
 
-export default HiddenInput;
+export { HiddenInput };
