fa0c056c4babfb856b4c1631963e6ac50f413a59
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-03 11:22:56 +0100

    transform projects/common/modules/crosswords/comments.js content to ES6

--- static/src/javascripts/projects/common/modules/crosswords/comments.js
+++ static/src/javascripts/projects/common/modules/crosswords/comments.js
@@ -1,11 +1,15 @@
+// @flow
 import config from 'lib/config';
-import DiscussionLoader from 'common/modules/discussion/loader';
+import { DiscussionLoader } from 'common/modules/discussion/loader';
 import qwery from 'qwery';
-export default function() {
+
+function initCrosswordDiscussion() {
     if (config.switches.enableDiscussionSwitch && config.page.commentable) {
         var el = qwery('.discussion')[0];
         if (el) {
             new DiscussionLoader.Loader().attachTo(el);
         }
     }
-};
+}
+
+export { initCrosswordDiscussion };
