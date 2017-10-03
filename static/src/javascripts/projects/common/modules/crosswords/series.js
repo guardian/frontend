0fbd49e4bb7a4e8cb40337d0c084e70eb21459ae
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-03 17:25:27 +0100

    transform projects/common/modules/crosswords/series.js content to ES6

--- static/src/javascripts/projects/common/modules/crosswords/series.js
+++ static/src/javascripts/projects/common/modules/crosswords/series.js
@@ -1,12 +1,12 @@
 import config from 'lib/config';
 import proximityLoader from 'lib/proximity-loader';
 import Series from 'common/modules/onward/onward-content';
-export default function() {
 
-    var el = document.getElementsByClassName('js-onward');
+export const initSeries = () => {
+    const el = document.getElementsByClassName('js-onward');
 
     if (el.length > 0) {
-        proximityLoader.add(el[0], 1500, function() {
+        proximityLoader.add(el[0], 1500, () => {
             if (config.page.seriesId && config.page.showRelatedContent) {
                 new Series.OnwardContent(document.getElementsByClassName('js-onward'));
             }
