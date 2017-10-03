506bf527cf7c60151c9568d99d22c2e1db0bede7
GHaberis <george.haberis@theguardian.com>
2017-10-03 10:21:10 +0100

    transform projects/common/modules/navigation/membership.js content to ES6

--- static/src/javascripts/projects/common/modules/navigation/membership.js
+++ static/src/javascripts/projects/common/modules/navigation/membership.js
@@ -1,13 +1,13 @@
 import userFeatures from 'commercial/modules/user-features';
 import fastdom from 'lib/fastdom-promise';
 import $ from 'lib/$';
-var LAST_CLASS = 'brand-bar__item--split--last';
+const LAST_CLASS = 'brand-bar__item--split--last';
 
 function init() {
     if (userFeatures.isPayingMember()) {
-        var $becomeMemberLink = $('.js-become-member');
-        var $subscriberLink = $('.js-subscribe');
-        fastdom.write(function() {
+        const $becomeMemberLink = $('.js-become-member');
+        const $subscriberLink = $('.js-subscribe');
+        fastdom.write(() => {
             $becomeMemberLink.attr('hidden', 'hidden');
             $subscriberLink.removeClass(LAST_CLASS);
         });
