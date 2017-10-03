6d304377c36b12bda6ed3f431065d1362d616f3c
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-03 17:32:18 +0100

    fix flow errors

--- static/src/javascripts/projects/common/modules/crosswords/series.js
+++ static/src/javascripts/projects/common/modules/crosswords/series.js
@@ -3,17 +3,19 @@ import config from 'lib/config';
 import proximityLoader from 'lib/proximity-loader';
 import { OnwardContent } from 'common/modules/onward/onward-content';
 
-export const initSeries = () => {
-    const el = document.getElementsByClassName('js-onward');
+export const initSeries = (): void => {
+    const els = document.getElementsByClassName('js-onward');
 
-    if (el.length > 0) {
-        proximityLoader.add(el[0], 1500, () => {
+    if (els.length > 0) {
+        const el = els[0];
+
+        proximityLoader.add(el, 1500, () => {
             if (
                 config.get('page.seriesId') &&
                 config.get('page.showRelatedContent')
             ) {
                 // eslint-disable-next-line no-new
-                new OnwardContent(document.getElementsByClassName('js-onward'));
+                new OnwardContent(el);
             }
         });
     }
