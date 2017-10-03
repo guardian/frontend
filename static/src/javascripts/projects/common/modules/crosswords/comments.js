5b052d544581e13facbd6f0ba6e4fe552eb0ac09
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-03 11:25:53 +0100

    fix-commits

--- static/src/javascripts/projects/common/modules/crosswords/comments.js
+++ static/src/javascripts/projects/common/modules/crosswords/comments.js
@@ -3,13 +3,13 @@ import config from 'lib/config';
 import { Loader } from 'common/modules/discussion/loader';
 import qwery from 'qwery';
 
-function initCrosswordDiscussion() {
+const initCrosswordDiscussion = function() {
     if (config.switches.enableDiscussionSwitch && config.page.commentable) {
-        var el = qwery('.discussion')[0];
+        const el = qwery('.discussion')[0];
         if (el) {
             new Loader().attachTo(el);
         }
     }
-}
+};
 
 export { initCrosswordDiscussion };
