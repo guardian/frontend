cca4aaa8d727f2651f3fe4e1045566713a6496b4
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-03 17:22:14 +0100

    transform projects/common/modules/crosswords/series.js to ES6 module

--- static/src/javascripts/projects/common/modules/crosswords/series.js
+++ static/src/javascripts/projects/common/modules/crosswords/series.js
@@ -1,22 +1,15 @@
-define([
-    'lib/config',
-    'lib/proximity-loader',
-    'common/modules/onward/onward-content'
-], function (
-    config,
-    proximityLoader,
-    Series
-) {
-    return function () {
+import config from 'lib/config';
+import proximityLoader from 'lib/proximity-loader';
+import Series from 'common/modules/onward/onward-content';
+export default function() {
 
-        var el = document.getElementsByClassName('js-onward');
+    var el = document.getElementsByClassName('js-onward');
 
-        if (el.length > 0) {
-            proximityLoader.add(el[0], 1500, function () {
-                if (config.page.seriesId && config.page.showRelatedContent) {
-                    new Series.OnwardContent(document.getElementsByClassName('js-onward'));
-                }
-            });
-        }
-    };
-});
+    if (el.length > 0) {
+        proximityLoader.add(el[0], 1500, function() {
+            if (config.page.seriesId && config.page.showRelatedContent) {
+                new Series.OnwardContent(document.getElementsByClassName('js-onward'));
+            }
+        });
+    }
+};
