79b006d3f0b3e9fbccc0390fa878a6ad9270d41e
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-03 14:46:05 +0100

    PR tweaks

--- static/src/javascripts/projects/common/modules/crosswords/comments.js
+++ static/src/javascripts/projects/common/modules/crosswords/comments.js
@@ -1,11 +1,13 @@
 // @flow
 import config from 'lib/config';
 import { Loader } from 'common/modules/discussion/loader';
-import qwery from 'qwery';
 
-const initCrosswordDiscussion = function() {
-    if (config.switches.enableDiscussionSwitch && config.page.commentable) {
-        const el = qwery('.discussion')[0];
+const initCrosswordDiscussion = () => {
+    if (
+        config.get('switches.enableDiscussionSwitch') &&
+        config.get('page.commentable')
+    ) {
+        const el = document.getElementsByClassName('discussion')[0];
         if (el) {
             new Loader().attachTo(el);
         }
