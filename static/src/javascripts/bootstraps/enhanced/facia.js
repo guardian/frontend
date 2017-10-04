b26ef67bf3e406b63734b82ba5593d05c3e202f0
sndrs <a@snd.rs>
2017-10-04 14:25:41 +0100

    lint

--- static/src/javascripts/bootstraps/enhanced/facia.js
+++ static/src/javascripts/bootstraps/enhanced/facia.js
@@ -1,120 +1,98 @@
-define([
-    'bonzo',
-    'qwery',
-    'lib/$',
-    'lib/config',
-    'lib/detect',
-    'lib/mediator',
-    'lib/robust',
-    'lib/storage',
-    'lodash/collections/toArray',
-    'common/modules/accessibility/helpers',
-    'common/modules/experiments/ab',
-    'common/modules/business/stocks',
-    'facia/modules/onwards/geo-most-popular-front',
-    'facia/modules/ui/container-toggle',
-    'facia/modules/ui/container-show-more',
-    'facia/modules/ui/lazy-load-containers',
-    'facia/modules/ui/live-blog-updates',
-    'facia/modules/ui/snaps',
-    'facia/modules/ui/sponsorship',
-    'facia/modules/onwards/weather',
-    'lodash/functions/partial',
-], function (
-    bonzo,
-    qwery,
-    $,
-    config,
-    detect,
-    mediator,
-    robust,
-    storage,
-    toArray,
-    accessibility,
-    ab,
-    stocks,
-    geoMostPopularFront,
-    ContainerToggle,
-    containerShowMore,
-    lazyLoadContainers,
-    liveblogUpdates,
-    snaps,
-    sponsorship,
-    weather,
-    partial
-) {
+// @flow
+import $ from 'lib/$';
+import config from 'lib/config';
+import { isBreakpoint } from 'lib/detect';
+import mediator from 'lib/mediator';
+import { catchErrorsWithContext } from 'lib/robust';
+import { shouldHideFlashingElements } from 'common/modules/accessibility/helpers';
+import stocks from 'common/modules/business/stocks';
+import { GeoMostPopularFront } from 'facia/modules/onwards/geo-most-popular-front';
+import { ContainerToggle } from 'facia/modules/ui/container-toggle';
+import { init as initShowMore } from 'facia/modules/ui/container-show-more';
+import { lazyLoadContainers } from 'facia/modules/ui/lazy-load-containers';
+import { showUpdatesFromLiveBlog } from 'facia/modules/ui/live-blog-updates';
+import snaps from 'facia/modules/ui/snaps';
+import sponsorship from 'facia/modules/ui/sponsorship';
+import { Weather } from 'facia/modules/onwards/weather';
+import partial from 'lodash/functions/partial';
 
-    var modules = {
-            showSnaps: function () {
-                snaps.init();
-                mediator.on('modules:container:rendered', snaps.init);
-            },
+const modules = {
+    showSnaps() {
+        snaps.init();
+        mediator.on('modules:container:rendered', snaps.init);
+    },
 
-            showContainerShowMore: function () {
-                mediator.addListeners({
-                    'modules:container:rendered': containerShowMore.init,
-                    'page:front:ready': containerShowMore.init
-                });
-            },
+    showContainerShowMore() {
+        mediator.addListeners({
+            'modules:container:rendered': initShowMore,
+            'page:front:ready': initShowMore,
+        });
+    },
 
-            showContainerToggle: function () {
-                var containerToggleAdd = function (context) {
-                        $('.js-container--toggle', $(context || document)[0]).each(function (container) {
-                            var toggle = new ContainerToggle.ContainerToggle(container);
-                            toggle.addToggle();
-                        });
-                    };
-                mediator.addListeners({
-                    'page:front:ready': containerToggleAdd,
-                    'modules:geomostpopular:ready': partial(containerToggleAdd, '.js-popular-trails')
-                });
-            },
-
-            upgradeMostPopularToGeo: function () {
-                if (config.switches.geoMostPopular) {
-                    new geoMostPopularFront.GeoMostPopularFront().go();
-                }
-            },
-
-            showWeather: function () {
-                if (config.switches.weather) {
-                    mediator.on('page:front:ready', function () {
-                        weather.Weather.init();
-                    });
-                }
-            },
-
-            showLiveblogUpdates: function () {
-                if (detect.isBreakpoint({ min: 'desktop' })) {
-                    mediator.on('page:front:ready', function () {
-                        liveblogUpdates.showUpdatesFromLiveBlog();
-                    });
-                }
-            },
-
-            finished: function () {
-                mediator.emit('page:front:ready');
-            }
-
-        },
-
-        ready = function () {
-            robust.catchErrorsWithContext([
-                ['f-accessibility', accessibility.shouldHideFlashingElements],
-                ['f-snaps', modules.showSnaps],
-                ['f-show-more', modules.showContainerShowMore],
-                ['f-container-toggle', modules.showContainerToggle],
-                ['f-geo-most-popular', modules.upgradeMostPopularToGeo],
-                ['f-lazy-load-containers', lazyLoadContainers.lazyLoadContainers],
-                ['f-stocks', stocks],
-                ['f-sponsorship', sponsorship],
-                ['f-weather', modules.showWeather],
-                ['f-live-blog-updates', modules.showLiveblogUpdates],
-                ['f-finished', modules.finished]
-            ]);
+    showContainerToggle() {
+        const containerToggleAdd = context => {
+            $(
+                '.js-container--toggle',
+                $(context || document)[0]
+            ).each(container => {
+                const toggle = new ContainerToggle(container);
+                toggle.addToggle();
+            });
         };
+        mediator.addListeners({
+            'page:front:ready': containerToggleAdd,
+            'modules:geomostpopular:ready': partial(
+                containerToggleAdd,
+                '.js-popular-trails'
+            ),
+        });
+    },
 
-    return {
-        init: ready
-    };
-});
+    upgradeMostPopularToGeo() {
+        if (config.get('switches.geoMostPopular')) {
+            new GeoMostPopularFront().go();
+        }
+    },
+
+    showWeather() {
+        if (config.get('switches.weather')) {
+            mediator.on('page:front:ready', () => {
+                Weather.init();
+            });
+        }
+    },
+
+    showLiveblogUpdates() {
+        if (
+            isBreakpoint({
+                min: 'desktop',
+            })
+        ) {
+            mediator.on('page:front:ready', () => {
+                showUpdatesFromLiveBlog();
+            });
+        }
+    },
+
+    finished() {
+        mediator.emit('page:front:ready');
+    },
+};
+
+const init = () => {
+    catchErrorsWithContext([
+        ['f-accessibility', shouldHideFlashingElements],
+        ['f-snaps', modules.showSnaps],
+        ['f-show-more', modules.showContainerShowMore],
+        ['f-container-toggle', modules.showContainerToggle],
+        ['f-geo-most-popular', modules.upgradeMostPopularToGeo],
+        ['f-lazy-load-containers', lazyLoadContainers],
+        ['f-stocks', stocks],
+        ['f-sponsorship', sponsorship],
+        ['f-weather', modules.showWeather],
+        ['f-live-blog-updates', modules.showLiveblogUpdates],
+        ['f-finished', modules.finished],
+    ]);
+};
+
+export { init };
