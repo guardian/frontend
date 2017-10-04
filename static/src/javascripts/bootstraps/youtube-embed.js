e082a7f95c524915eb312962258fd8ab914cfee2
Alex Sanders <a@snd.rs>
2017-10-04 12:26:51 +0100

    lint

--- static/src/javascripts/bootstraps/youtube-embed.js
+++ static/src/javascripts/bootstraps/youtube-embed.js
@@ -1,9 +1,4 @@
-import youtube from 'bootstraps/enhanced/youtube';
+// @flow
+import { init } from 'bootstraps/enhanced/youtube';
 
-function init() {
-    youtube.init();
-}
-
-export default {
-    init
-};
+init();
