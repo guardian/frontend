f571cd5c5f5137f6449ef33bd135d6e1f331dd69
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-04 15:34:04 +0100

    es6 fixes

--- static/src/javascripts/projects/common/modules/commercial/video-ad-url.js
+++ static/src/javascripts/projects/common/modules/commercial/video-ad-url.js
@@ -1,25 +1,28 @@
+// @flow
 import config from 'lib/config';
-import urlUtils from 'lib/url';
-import buildPageTargeting from 'commercial/modules/build-page-targeting';
+import { constructQuery } from 'lib/url';
+import { buildPageTargeting } from 'commercial/modules/build-page-targeting';
 
-function getAdUrl() {
+const videoAdUrl = () => {
     const queryParams = {
         ad_rule: 1,
         correlator: new Date().getTime(),
-        cust_params: encodeURIComponent(urlUtils.constructQuery(buildPageTargeting.buildPageTargeting())),
+        cust_params: encodeURIComponent(
+            constructQuery(buildPageTargeting.buildPageTargeting())
+        ),
         env: 'vp',
         gdfp_req: 1,
         impl: 's',
-        iu: config.page.adUnit,
+        iu: config.get('page.adUnit'),
         output: 'xml_vast2',
         scp: encodeURIComponent('slot=video'),
         sz: '400x300',
-        unviewed_position_start: 1
+        unviewed_position_start: 1,
     };
 
-    return 'https://' + config.page.dfpHost + '/gampad/ads?' + urlUtils.constructQuery(queryParams);
-}
-
-export default {
-    get: getAdUrl
+    return `https://${config.get('page.dfpHost')}/gampad/ads?${constructQuery(
+        queryParams
+    )}`;
 };
+
+export { videoAdUrl };
