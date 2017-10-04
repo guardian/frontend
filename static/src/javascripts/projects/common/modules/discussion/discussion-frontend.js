223f694d0ce400a159fb5ee46e4d78cbf7eefd51
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-10-04 11:54:40 +0100

    ES6: Convert discussion/discussion-frontend

---
+++ static/src/javascripts/projects/common/modules/discussion/discussion-frontend.js
@@ -0,0 +1,75 @@
+// @flow
+
+import config from 'lib/config';
+import fastdom from 'fastdom';
+import { integerCommas } from 'lib/formatters';
+import { loadScript } from 'lib/load-script';
+import mediator from 'lib/mediator';
+import reportError from 'lib/report-error';
+import { Loader } from './loader';
+
+const loadDiscussionFrontend = (loader: Loader, opts: Object) => {
+    const updateCommentCount = (element: HTMLElement, value: string): void => {
+        const formatted = integerCommas(value);
+
+        if (formatted) {
+            fastdom.write(() => {
+                element.textContent = formatted;
+            });
+        }
+    };
+
+    const onDiscussionFrontendLoad = (emitter): void => {
+        emitter.on('error', (feature, error) => {
+            reportError(error, { feature: `discussion-${feature}` }, false);
+        });
+
+        emitter.once('comment-count', (value: string): void => {
+            if (value === 0) {
+                loader.setState('empty');
+            } else {
+                /* By the time discussion frontent loads, the number of comments
+                   might have changed. If there are other comment counts element
+                   in the page refresh their value. */
+                const otherValues = document.getElementsByClassName(
+                    'js_commentcount_actualvalue'
+                );
+
+                if (otherValues.length) {
+                    [...otherValues].forEach(el => {
+                        updateCommentCount(el, value);
+                    });
+                }
+            }
+
+            mediator.emit('comments-count-loaded');
+        });
+    };
+
+    const error = (err: Error): void => {
+        reportError(err, { feature: 'discussion' });
+    };
+
+    const init = (frontend: (opts: Object) => Promise<any>): void => {
+        frontend(opts)
+            .then(onDiscussionFrontendLoad)
+            .catch(error);
+    };
+
+    /* - Inject the net module to work around the lack of a global fetch
+         It can be removed once all browsers have window.fetch
+       - Well, it turns out that fetchJson uses reqwest which sends X-Requested-With
+         which is not allowed by Access-Control-Allow-Headers, so don't use reqwest
+         until discussion API is fixed
+       - Once fixed, or a global fetch is available through a polyfill, one can
+         modify discussion-frontend to remove `fetch` polyfill and pass, if needed,
+         opts.net = { json: fetchJson } */
+
+    return loadScript(config.get('page.discussionFrontendUrl'))
+        .then(() => {
+            init(window.guardian.app.discussion);
+        })
+        .catch(error);
+};
+
+export { loadDiscussionFrontend };
