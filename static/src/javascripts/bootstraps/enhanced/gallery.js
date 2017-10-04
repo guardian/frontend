d704087e63d7e80d35d09514b256342d23cac223
Alex Sanders <a@snd.rs>
2017-10-04 12:55:07 +0100

    transform bootstraps/enhanced/gallery.js to ES6 module

--- static/src/javascripts/bootstraps/enhanced/gallery.js
+++ static/src/javascripts/bootstraps/enhanced/gallery.js
@@ -1,38 +1,28 @@
-define([
-    'qwery',
-    'lib/config',
-    'lib/mediator',
-    'common/modules/component',
-    'bootstraps/enhanced/trail'
-], function (
-    qwery,
-    config,
-    mediator,
-    Component,
-    trail
-) {
-    var transcludeMostPopular = function () {
-            var mostViewed = new Component(),
-                container = qwery('.js-gallery-most-popular')[0];
+import qwery from 'qwery';
+import config from 'lib/config';
+import mediator from 'lib/mediator';
+import Component from 'common/modules/component';
+import trail from 'bootstraps/enhanced/trail';
+var transcludeMostPopular = function() {
+        var mostViewed = new Component(),
+            container = qwery('.js-gallery-most-popular')[0];
 
-            mostViewed.manipulationType = 'html';
-            mostViewed.endpoint = '/gallery/most-viewed.json';
-            mostViewed.ready = function () {
-                mediator.emit('page:new-content', container);
-            };
-            mostViewed.fetch(container, 'html');
-        },
-        ready = function () {
-            trail.initTrails();
-
-            mediator.emit('page:gallery:ready');
-            if (config.page.showRelatedContent) {
-                transcludeMostPopular();
-            }
+        mostViewed.manipulationType = 'html';
+        mostViewed.endpoint = '/gallery/most-viewed.json';
+        mostViewed.ready = function() {
+            mediator.emit('page:new-content', container);
         };
+        mostViewed.fetch(container, 'html');
+    },
+    ready = function() {
+        trail.initTrails();
 
-    return {
-        init: ready
+        mediator.emit('page:gallery:ready');
+        if (config.page.showRelatedContent) {
+            transcludeMostPopular();
+        }
     };
 
-});
+export default {
+    init: ready
+};
