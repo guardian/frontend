0ef98fb42762691cb77d4ab59a22dd682fd1c9df
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-05 17:23:53 +0100

    Flow fixes

--- static/src/javascripts/projects/common/modules/analytics/interaction-tracking.js
+++ static/src/javascripts/projects/common/modules/analytics/interaction-tracking.js
@@ -1,25 +1,56 @@
+// @flow
 import mediator from 'lib/mediator';
-import storage from 'lib/storage';
-import google from 'common/modules/analytics/google';
-import robust from 'lib/robust';
+import { session } from 'lib/storage';
+import { google } from 'common/modules/analytics/google';
+import { catchErrorsWithContext } from 'lib/robust';
+
 const NG_STORAGE_KEY = 'gu.analytics.referrerVars';
 let loc = document.location;
 
-function addHandlers() {
-    mediator.on('module:clickstream:interaction', trackNonClickInteraction);
+type Spec = {
+    validTarget: Boolean,
+    target: Any,
+    sameHost: Boolean,
+    samePage: Boolean,
+    tag: string,
+};
 
-    mediator.on('module:clickstream:click', spec => {
-        // We don't want tracking errors to terminate the event emitter, as
-        // this will mean other event listeners will not be called.
-        robust.catchErrorsWithContext([
-            ['c-analytics', () => {
-                trackClick(spec);
-            }],
-        ]);
-    });
-}
+//
 
-function trackClick(spec) {
+const isSponsorLogoLinkClick = target => target.hasAttribute('data-sponsor');
+
+// used where we don't have an element to pass as a tag, eg. keyboard interaction
+const trackNonClickInteraction = actionName => {
+    google.trackNonClickInteraction(actionName);
+};
+
+const trackSamePageLinkClick = (spec: Spec) => {
+    // Do not perform a same-page track link when there isn't a tag.
+    if (spec.tag) {
+        google.trackSamePageLinkClick(spec.target, spec.tag);
+    }
+};
+
+const trackInternalLinkClick = (spec: Spec) => {
+    // Store in session storage.
+    // GA and Omniture will both pick it up on next page load,
+    // then Omniture will remove it from storage.
+    const storeObj = {
+        path: loc.pathname,
+        tag: spec.tag || 'untracked',
+        time: new Date().getTime(),
+    };
+    session.set(NG_STORAGE_KEY, storeObj);
+};
+
+const trackExternalLinkClick = (spec: Spec) => {
+    // Execute the GA and Omniture tracking in parallel
+    // and rely on Omniture to provide a 500 ms delay so they both get a chance to complete.
+    // TODO when Omniture goes away, implement the delay ourselves.
+    google.trackExternalLinkClick(spec.target, spec.tag);
+};
+
+const trackClick = (spec: Spec) => {
     if (!spec.validTarget) {
         return;
     }
@@ -37,53 +68,35 @@ function trackClick(spec) {
     } else {
         trackExternalLinkClick(spec);
     }
-}
+};
 
-function isSponsorLogoLinkClick(target) {
-    return target.hasAttribute('data-sponsor');
-}
+const addHandlers = () => {
+    mediator.on('module:clickstream:interaction', trackNonClickInteraction);
 
-// used where we don't have an element to pass as a tag, eg. keyboard interaction
-function trackNonClickInteraction(actionName) {
-    google.trackNonClickInteraction(actionName);
-}
+    mediator.on('module:clickstream:click', spec => {
+        // We don't want tracking errors to terminate the event emitter, as
+        // this will mean other event listeners will not be called.
+        catchErrorsWithContext([
+            [
+                'c-analytics',
+                () => {
+                    trackClick(spec);
+                },
+            ],
+        ]);
+    });
+};
 
-function trackSamePageLinkClick(spec) {
-    // Do not perform a same-page track link when there isn't a tag.
-    if (spec.tag) {
-        google.trackSamePageLinkClick(spec.target, spec.tag);
-    }
-}
-
-function trackInternalLinkClick(spec) {
-    // Store in session storage.
-    // GA and Omniture will both pick it up on next page load,
-    // then Omniture will remove it from storage.
-    const storeObj = {
-        path: loc.pathname,
-        tag: spec.tag || 'untracked',
-        time: new Date().getTime()
-    };
-    storage.session.set(NG_STORAGE_KEY, storeObj);
-}
-
-function trackExternalLinkClick(spec) {
-    // Execute the GA and Omniture tracking in parallel
-    // and rely on Omniture to provide a 500 ms delay so they both get a chance to complete.
-    // TODO when Omniture goes away, implement the delay ourselves.
-    google.trackExternalLinkClick(spec.target, spec.tag);
-}
-
-function init(options = {}) {
+const init = (options = {}) => {
     if (options.location) {
         loc = options.location; // allow a fake location to be passed in for testing
     }
     addHandlers();
     mediator.emit('analytics:ready');
-}
+};
 
 export default {
     init,
     trackClick,
-    trackNonClickInteraction
+    trackNonClickInteraction,
 };
