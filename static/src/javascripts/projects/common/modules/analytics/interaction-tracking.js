45980e37bd131f54ac05c3df0a5a4b93ca90b0d7
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-05 15:30:43 +0100

    transform projects/common/modules/analytics/interaction-tracking.js to ES6 module

--- static/src/javascripts/projects/common/modules/analytics/interaction-tracking.js
+++ static/src/javascripts/projects/common/modules/analytics/interaction-tracking.js
@@ -1,96 +1,90 @@
-define([
-    'lib/mediator',
-    'lib/storage',
-    'common/modules/analytics/google',
-    'lib/robust'
-], function (
-    mediator,
-    storage,
-    google,
-    robust
-) {
-    var NG_STORAGE_KEY = 'gu.analytics.referrerVars';
-    var loc = document.location;
+import mediator from 'lib/mediator';
+import storage from 'lib/storage';
+import google from 'common/modules/analytics/google';
+import robust from 'lib/robust';
+var NG_STORAGE_KEY = 'gu.analytics.referrerVars';
+var loc = document.location;
 
-    function addHandlers() {
-        mediator.on('module:clickstream:interaction', trackNonClickInteraction);
+function addHandlers() {
+    mediator.on('module:clickstream:interaction', trackNonClickInteraction);
 
-        mediator.on('module:clickstream:click', function (spec) {
-            // We don't want tracking errors to terminate the event emitter, as
-            // this will mean other event listeners will not be called.
-            robust.catchErrorsWithContext([
-                ['c-analytics', function () { trackClick(spec); }],
-            ]);
-        });
+    mediator.on('module:clickstream:click', function(spec) {
+        // We don't want tracking errors to terminate the event emitter, as
+        // this will mean other event listeners will not be called.
+        robust.catchErrorsWithContext([
+            ['c-analytics', function() {
+                trackClick(spec);
+            }],
+        ]);
+    });
+}
+
+function trackClick(spec) {
+    if (!spec.validTarget) {
+        return;
     }
 
-    function trackClick(spec) {
-        if (!spec.validTarget) {
-            return;
-        }
+    if (isSponsorLogoLinkClick(spec.target)) {
+        return google.trackSponsorLogoLinkClick(spec.target);
+    }
 
-        if (isSponsorLogoLinkClick(spec.target)) {
-            return google.trackSponsorLogoLinkClick(spec.target);
-        }
-
-        if (spec.sameHost) {
-            if (spec.samePage) {
-                trackSamePageLinkClick(spec);
-            } else {
-                trackInternalLinkClick(spec);
-            }
+    if (spec.sameHost) {
+        if (spec.samePage) {
+            trackSamePageLinkClick(spec);
         } else {
-            trackExternalLinkClick(spec);
+            trackInternalLinkClick(spec);
         }
+    } else {
+        trackExternalLinkClick(spec);
     }
+}
 
-    function isSponsorLogoLinkClick(target) {
-        return target.hasAttribute('data-sponsor');
+function isSponsorLogoLinkClick(target) {
+    return target.hasAttribute('data-sponsor');
+}
+
+// used where we don't have an element to pass as a tag, eg. keyboard interaction
+function trackNonClickInteraction(actionName) {
+    google.trackNonClickInteraction(actionName);
+}
+
+function trackSamePageLinkClick(spec) {
+    // Do not perform a same-page track link when there isn't a tag.
+    if (spec.tag) {
+        google.trackSamePageLinkClick(spec.target, spec.tag);
     }
+}
 
-    // used where we don't have an element to pass as a tag, eg. keyboard interaction
-    function trackNonClickInteraction(actionName) {
-        google.trackNonClickInteraction(actionName);
-    }
-
-    function trackSamePageLinkClick(spec) {
-        // Do not perform a same-page track link when there isn't a tag.
-        if (spec.tag) {
-            google.trackSamePageLinkClick(spec.target, spec.tag);
-        }
-    }
-
-    function trackInternalLinkClick(spec) {
-        // Store in session storage.
-        // GA and Omniture will both pick it up on next page load,
-        // then Omniture will remove it from storage.
-        var storeObj = {
-            path: loc.pathname,
-            tag: spec.tag || 'untracked',
-            time: new Date().getTime()
-        };
-        storage.session.set(NG_STORAGE_KEY, storeObj);
-    }
-
-    function trackExternalLinkClick(spec) {
-        // Execute the GA and Omniture tracking in parallel
-        // and rely on Omniture to provide a 500 ms delay so they both get a chance to complete.
-        // TODO when Omniture goes away, implement the delay ourselves.
-        google.trackExternalLinkClick(spec.target, spec.tag);
-    }
-
-    function init(options) {
-        options = options || {};
-        if (options.location) {
-            loc = options.location; // allow a fake location to be passed in for testing
-        }
-        addHandlers();
-        mediator.emit('analytics:ready');
-    }
-
-    return {
-        init: init,
-        trackClick: trackClick,
-        trackNonClickInteraction: trackNonClickInteraction
+function trackInternalLinkClick(spec) {
+    // Store in session storage.
+    // GA and Omniture will both pick it up on next page load,
+    // then Omniture will remove it from storage.
+    var storeObj = {
+        path: loc.pathname,
+        tag: spec.tag || 'untracked',
+        time: new Date().getTime()
     };
-});
+    storage.session.set(NG_STORAGE_KEY, storeObj);
+}
+
+function trackExternalLinkClick(spec) {
+    // Execute the GA and Omniture tracking in parallel
+    // and rely on Omniture to provide a 500 ms delay so they both get a chance to complete.
+    // TODO when Omniture goes away, implement the delay ourselves.
+    google.trackExternalLinkClick(spec.target, spec.tag);
+}
+
+function init(options) {
+    options = options || {};
+    if (options.location) {
+        loc = options.location; // allow a fake location to be passed in for testing
+    }
+    addHandlers();
+    mediator.emit('analytics:ready');
+}
+
+export default {
+    init: init,
+    trackClick: trackClick,
+    trackNonClickInteraction: trackNonClickInteraction
+};
