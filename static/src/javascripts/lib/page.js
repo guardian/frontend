3f2d16faff049c0fb7c7c07a70d2101e10f6d8dc
sndrs <a@snd.rs>
2017-10-06 10:32:11 +0100

    mixed type

--- static/src/javascripts/lib/page.js
+++ static/src/javascripts/lib/page.js
@@ -22,22 +22,22 @@ const isit = (
 };
 
 const isMatch = (yes: yesable, no: noable): boolean => {
-    const teams = config.referencesOfType('pa-football-team');
+    const teams = config.get('referencesOfType')('pa-football-team');
     const match = config.get('page.footballMatch', {});
     // the order of this is important as, on occasion,
     // "minbymin" is tagged with "match reports" but should be considered "minbymin".
     const pageTypes = [
-        ['minbymin', config.page.isLiveBlog],
-        ['report', config.hasTone('Match reports')],
-        ['preview', config.hasSeries('Match previews')],
+        ['minbymin', config.get('page.isLiveBlog')],
+        ['report', config.get('hasTone')('Match reports')],
+        ['preview', config.get('hasSeries')('Match previews')],
         ['stats', match.id],
     ];
     const pageType = pageTypes.find(type => type[1] === true);
 
     Object.assign(match, {
-        date: config.webPublicationDateAsUrlPart(),
+        date: config.get('webPublicationDateAsUrlPart')(),
         teams,
-        isLive: config.page.isLive,
+        isLive: config.get('page.isLive'),
         pageType: pageType && pageType[0],
     });
 
@@ -62,13 +62,13 @@ const isCompetition = (yes: yesable): boolean => {
 };
 
 const isClockwatch = (yes: yesable): boolean =>
-    isit(config.hasSeries('Clockwatch'), yes);
+    isit(config.get('hasSeries')('Clockwatch'), yes);
 
 const isLiveClockwatch = (yes: yesable): boolean =>
-    isClockwatch(() => isit(config.page.isLive, yes));
+    isClockwatch(() => isit(config.get('page.isLive'), yes));
 
 const isFootballStatsPage = (yes: yesable): boolean =>
-    isit(config.page.hasOwnProperty('footballMatch'), yes);
+    isit(config.get('page.hasOwnProperty')('footballMatch'), yes);
 
 const belowArticleVisible = (yes: yesable, no: noable): boolean => {
     const el = $('.js-after-article')[0];
