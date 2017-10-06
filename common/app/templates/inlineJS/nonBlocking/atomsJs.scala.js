db95333223a21be80fa2788756728805ab16c55b
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-10-06 15:51:55 +0100

    Reify the JavaScript

--- common/app/templates/inlineJS/nonBlocking/atomsJs.scala.js
+++ common/app/templates/inlineJS/nonBlocking/atomsJs.scala.js
@@ -1,3 +1,17 @@
+@import play.twirl.api.HtmlFormat
 @(page: model.Page)(implicit context: model.ApplicationContext)
-
-@page.metadata.atomsJS.mkString
+(function () {
+  function loadAtoms() {
+    @* 
+     * each atomsJS element looks like this:
+     *   this.profile=(function(...){...}())
+     * we can just bind this to an arbitrary object that we can pass around.   
+     *@
+    @page.metadata.atomsJS.mkString(" ")
+  } 
+  var guardian = window.guardian || (window.guardian = {});
+  guardian.atoms = {};
+  loadAtoms.call(guardian.atoms);
+  console.log("Atoms are loaded!")
+  console.dir(guardian.atoms);
+}());
