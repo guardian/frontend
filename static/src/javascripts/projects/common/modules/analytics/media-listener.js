2f13aa99f340e6e2142fb37ab450d9a36f6edae1
Matthew Walls <matthew.walls@guardian.co.uk>
2017-10-09 11:34:21 +0100

    transform projects/common/modules/analytics/media-listener.js to ES6 module

--- static/src/javascripts/projects/common/modules/analytics/media-listener.js
+++ static/src/javascripts/projects/common/modules/analytics/media-listener.js
@@ -1,16 +1,11 @@
-define([
-    'lib/mediator'
-], function (
-    mediator
-) {
-    return function () {
-        if (window.matchMedia) {
-            var mql = window.matchMedia('print');
-            mql.addListener(function () {
-                if (mql.matches) {
-                    mediator.emit('module:clickstream:interaction', 'print');
-                }
-            });
-        }
-    };
-});
+import mediator from 'lib/mediator';
+export default function() {
+    if (window.matchMedia) {
+        var mql = window.matchMedia('print');
+        mql.addListener(function() {
+            if (mql.matches) {
+                mediator.emit('module:clickstream:interaction', 'print');
+            }
+        });
+    }
+};
