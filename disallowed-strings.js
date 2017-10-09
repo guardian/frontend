f60848c35af82470fcccf522a775915fc2d8963a
Joseph Smith <joseph.smith@guardian.co.uk>
2017-10-09 15:28:56 +0100

    Comment out existing rules

--- disallowed-strings.js
+++ disallowed-strings.js
@@ -5,20 +5,24 @@ const readerRevenueConfig = {
 };
 
 module.exports = [
-    {
-        regex: /membership\.theguardian\.com/,
-        ...readerRevenueConfig
-    },
-    {
-        regex: /contribute\.theguardian\.com/,
-        ...readerRevenueConfig
-    },
-    {
-        regex: /support\.theguardian\.com/,
-        ...readerRevenueConfig
-    },
-    {
-        regex: /subscribe\.theguardian\.com/,
-        ...readerRevenueConfig
-    }
+    // These are commented out because they would currently fail the build.
+    // Until we refactor to eliminate the duplicates, they can serve as examples
+    // in case anyone wants to add more immediately feasible rules!
+
+    // {
+    //     regex: /membership\.theguardian\.com/,
+    //     ...readerRevenueConfig
+    // },
+    // {
+    //     regex: /contribute\.theguardian\.com/,
+    //     ...readerRevenueConfig
+    // },
+    // {
+    //     regex: /support\.theguardian\.com/,
+    //     ...readerRevenueConfig
+    // },
+    // {
+    //     regex: /subscribe\.theguardian\.com/,
+    //     ...readerRevenueConfig
+    // }
 ];
