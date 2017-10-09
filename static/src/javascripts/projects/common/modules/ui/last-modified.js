70f095b280d40aa00260943a54129fc301343ac1
GHaberis <george.haberis@theguardian.com>
2017-10-09 10:38:45 +0100

    transform projects/common/modules/ui/last-modified.js to ES6 module

--- static/src/javascripts/projects/common/modules/ui/last-modified.js
+++ static/src/javascripts/projects/common/modules/ui/last-modified.js
@@ -1,27 +1,19 @@
-define([
-    'bean',
-    'fastdom',
-    'qwery',
-    'lib/$'
-], function (
-    bean,
-    fastdom,
-    qwery,
-    $
-) {
-    return function () {
-        var $jsLm = $('.js-lm');
+import bean from 'bean';
+import fastdom from 'fastdom';
+import qwery from 'qwery';
+import $ from 'lib/$';
+export default function() {
+    var $jsLm = $('.js-lm');
 
-        if ($jsLm.length > 0) {
-            fastdom.write(function () {
-                $('.js-wpd').addClass('content__dateline-wpd--modified');
-            });
+    if ($jsLm.length > 0) {
+        fastdom.write(function() {
+            $('.js-wpd').addClass('content__dateline-wpd--modified');
+        });
 
-            bean.on(qwery('.js-wpd')[0], 'click', function () {
-                fastdom.write(function () {
-                    $jsLm.toggleClass('u-h');
-                });
+        bean.on(qwery('.js-wpd')[0], 'click', function() {
+            fastdom.write(function() {
+                $jsLm.toggleClass('u-h');
             });
-        }
-    };
-});
+        });
+    }
+};
