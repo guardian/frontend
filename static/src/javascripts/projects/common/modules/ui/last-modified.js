9fad32823dc87c7ad49b61437b761c92452f8fac
GHaberis <george.haberis@theguardian.com>
2017-10-09 12:27:17 +0100

    fix validation

--- static/src/javascripts/projects/common/modules/ui/last-modified.js
+++ static/src/javascripts/projects/common/modules/ui/last-modified.js
@@ -3,7 +3,6 @@
 import fastdom from 'lib/fastdom-promise';
 
 const lastModified = (): void => {
-    
     fastdom
         .read(() => document.getElementsByClassName('js-lm')[0])
         .then(jsLm => {
@@ -11,14 +10,14 @@ const lastModified = (): void => {
                 fastdom.write(() => {
                     jsLm.classList.add('content__dateline-wpd--modified');
                 });
-        
+
                 jsLm.addEventListener('click', () => {
                     fastdom.write(() => {
                         jsLm.classList.toggle('u-h');
                     });
                 });
             }
-        })
+        });
 };
 
 export { lastModified };
