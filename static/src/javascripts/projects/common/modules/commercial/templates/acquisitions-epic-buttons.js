fa6ea44975044d9af70695f5941734ab408cb32d
Justin Pinner <justinpinner@gmail.com>
2017-10-12 14:16:06 +0100

    Change button text and set single-button style when using support domain

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-buttons.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-buttons.js
@@ -13,7 +13,7 @@ export const epicButtonsTemplate = (
              Make a contribution
             </a>
         </div>`;
-    const supportButton = `
+    const supportButtonBecome = `
         <div>
             <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member contributions__contribute--epic-member-top"
               href="${membershipUrl}"
@@ -21,10 +21,18 @@ export const epicButtonsTemplate = (
               Become a supporter
             </a>
         </div>`;
+    const supportButtonSupport = `
+        <div>
+            <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member contributions__contribute--epic-member-top contributions__contribute--epic-single-button"
+              href="${membershipUrl}"
+              target="_blank">
+              Support the Guardian
+            </a>
+        </div>`;
 
     return `
         <div class="contributions__amount-field">
-            ${supportButton}
+            ${!useSupportDomain ? supportButtonBecome : supportButtonSupport}
             ${!useSupportDomain ? contribButton : ''}
         </div>`;
 };
