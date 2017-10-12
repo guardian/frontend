e52765d89909d864209c2507c060063a9432ad8b
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-10-12 13:41:42 +0100

    Added support case object and checking for switches to setup the correct link

--- common/app/navigation/helpers/UrlHelpers.scala
+++ common/app/navigation/helpers/UrlHelpers.scala
@@ -1,6 +1,7 @@
 package navigation
 
 import conf.Configuration
+import conf.switches.Switches
 import play.api.libs.json.Json
 import play.api.mvc.RequestHeader
 import common.Edition
@@ -10,6 +11,9 @@ import ophan.thrift.event.AcquisitionSource
 object UrlHelpers {
   sealed trait ReaderRevenueSite {
     val url: String
+  }
+  case object Support extends ReaderRevenueSite {
+    val url: String = s"${Configuration.id.supportUrl}/uk"
   }
   case object Membership extends ReaderRevenueSite {
     val url: String = s"${Configuration.id.membershipUrl}/supporter"
@@ -113,9 +117,29 @@ object UrlHelpers {
   def getContributionOrSupporterUrl(editionId: String)(implicit request: RequestHeader): String =
     if (editionId == "us") {
       getReaderRevenueUrl(Contribute, NewHeader)
+    } else if (editionId == "uk" && Switches.SupportFrontendActive.isSwitchedOn) {
+      getReaderRevenueUrl(Support, NewHeader)
     } else {
       getReaderRevenueUrl(Membership, NewHeader)
     }
+
+  def getSupportOrMembershipUrl(position: Position)(implicit request: RequestHeader): String = {
+    val editionId = Edition (request).id.toLowerCase()
+    if (editionId == "uk" && Switches.SupportFrontendActive.isSwitchedOn) {
+      getReaderRevenueUrl (Support, position)
+    } else {
+      getReaderRevenueUrl (Membership, position)
+    }
+  }
+
+  def getSupportOrSubscriptionUrl(position: Position)(implicit request: RequestHeader): String = {
+    val editionId = Edition(request).id.toLowerCase()
+    if (editionId == "uk" && Switches.SupportFrontendActive.isSwitchedOn) {
+      getReaderRevenueUrl (Support, position)
+    } else {
+      getReaderRevenueUrl (Subscribe, position)
+    }
+  }
 
   object oldNav {
     def jobsUrl(edition: String)(implicit request: RequestHeader): String =
