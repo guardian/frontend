4daccd8d6904d02de128103ef6c4b13b4998b32f
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-13 12:22:40 +0100

    no need for default props in component

--- archive/app/ui/NotFound.scala
+++ archive/app/ui/NotFound.scala
@@ -2,8 +2,8 @@ package ui
 
 import rendering.Renderable
 import rendering.core.JavascriptProps
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsObject, JsValue, Json}
 
 object NotFound extends Renderable {
-  override def props: Option[JsValue] = Some(JavascriptProps.default().asJsValue())
+  override def props: Option[JsValue] = Some(JsObject(List()))
 }
