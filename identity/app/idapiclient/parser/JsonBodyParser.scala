dc03a45be0f651b039d3a1b8dd12b2d36e0dc163
Mario Galic <mario.galic@guardian.co.uk>
2017-10-13 10:56:36 +0100

    Cleanup JsonBodyParser

--- identity/app/idapiclient/parser/JsonBodyParser.scala
+++ identity/app/idapiclient/parser/JsonBodyParser.scala
@@ -12,23 +12,32 @@ trait JsonBodyParser extends Logging {
 
   def extractErrorFromResponse(json: JValue, statusCode: Int): List[Error]
 
-  def extract[T](extractJsonObj: JValue => JValue = {json => json})(httpResponseResponse: Response[HttpResponse])(implicit successType: Manifest[T]): Response[T] = {
+  def extract[T]
+      (extractJsonObj: JValue => JValue = identity)
+      (httpResponseResponse: Response[HttpResponse])
+      (implicit successType: Manifest[T]): Response[T] = {
+
     httpResponseResponse.right.flatMap { httpResponse =>
       try {
         httpResponse match {
           case HttpResponse(body, status, message) if status == 502 =>
             logger.error("API unavailable, 502")
             Left(List(Error("Bad gateway", "The server was not available", 502)))
+
           case HttpResponse(body, status, message) if status == 503 =>
             logger.error("API unavailable, 503")
             Left(List(Error("Service unavailable", "The service was not available", 503)))
+
           case HttpResponse(body, status, message) if status == 504 =>
             logger.error("API unavailable, 504")
             Left(List(Error("Gateway timeout", "The service did not respond", 504)))
+
           case HttpResponse(body, status, message) if status > 299 =>
             Left(extractErrorFromResponse(parse(body), httpResponse.statusCode))
+
           case HttpResponse(body, status, message) if successType == manifest[Unit] =>
             Right(()).asInstanceOf[Right[List[Error], T]]
+
           case HttpResponse(body, status, message) =>
             Right(extractJsonObj(parse(body)).extract[T])
         }
@@ -48,7 +57,6 @@ trait JsonBodyParser extends Logging {
   def extractUnit(httpResponseResponse: Response[HttpResponse]): Response[Unit] = {
     extract[Unit](_ => JNothing)(httpResponseResponse)
   }
-}
-object JsonBodyParser {
+
   def jsonField(field: String)(json: JValue): JValue = json \ field
 }
