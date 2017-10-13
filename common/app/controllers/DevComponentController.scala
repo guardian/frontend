a19051fee1c68905a56ba659cd812c48cf01e163
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-13 14:05:44 +0100

    delete component rendering play endpoint

--- common/app/controllers/DevComponentController.scala
+++ common/app/controllers/DevComponentController.scala
@@ -1,9 +1,7 @@
 package controllers
 
 import model.ApplicationContext
-import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
-import rendering.TestComponent
 import rendering.core.{JavascriptProps, Renderer}
 
 import scala.concurrent.ExecutionContext
@@ -13,10 +11,6 @@ class DevComponentController(
   val controllerComponents: ControllerComponents
 )(implicit ac: ApplicationContext, ec: ExecutionContext)
   extends BaseController {
-
-  def renderComponent(): Action[AnyContent] = Action.async { implicit request =>
-    renderer.render(TestComponent).map(Ok(_).withHeaders("Content-Type" -> "text/html"))
-  }
 
   def props(): Action[AnyContent] = Action { request =>
     Ok(JavascriptProps.default().asJsValue())
