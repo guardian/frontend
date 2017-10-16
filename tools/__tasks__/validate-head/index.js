eeadf732cefc27f767ea157b7760bd2571adbbce
Joseph Smith <joseph.smith@guardian.co.uk>
2017-10-16 18:49:40 +0100

    Add disallowed strings to pre-commit hook

--- tools/__tasks__/validate-head/index.js
+++ tools/__tasks__/validate-head/index.js
@@ -2,7 +2,6 @@ module.exports = {
     description: 'Validate commits',
     task: [
         require('./javascript'),
-        require('../validate/check-for-disallowed-strings'),
     ],
     concurrent: true,
 };
