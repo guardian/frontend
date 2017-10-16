a7dc85ee365dc95c412d0041eefd6f381ae4e130
Joseph Smith <joseph.smith@guardian.co.uk>
2017-10-16 18:07:35 +0100

    make fix

--- tools/__tasks__/validate/check-for-disallowed-strings.js
+++ tools/__tasks__/validate/check-for-disallowed-strings.js
@@ -6,46 +6,47 @@ module.exports = {
     description: 'Check for disallowed strings',
     task: () =>
         Promise.all(
-            disallowedStrings.map(({ regex, message, maxOccurrences, pathspecs }) =>
-                execa
-                    .stdout('git', [
-                        'grep',
-                        '-Ein',
-                        '--color',
-                        regex.source,
-                        ...pathspecs,
-                    ])
-                    .then(matches => matches.split('\n'))
-                    .then(matches => {
-                        if (matches.length > maxOccurrences) {
-                            const msg = [
-                                chalk.red(
-                                    `More than ${maxOccurrences} match for regex ${regex.source}`
-                                ),
-                                chalk.red(message),
-                                ...matches,
-                            ].join('\n');
+            disallowedStrings.map(
+                ({ regex, message, maxOccurrences, pathspecs }) =>
+                    execa
+                        .stdout('git', [
+                            'grep',
+                            '-Ein',
+                            '--color',
+                            regex.source,
+                            ...pathspecs,
+                        ])
+                        .then(matches => matches.split('\n'))
+                        .then(matches => {
+                            if (matches.length > maxOccurrences) {
+                                const msg = [
+                                    chalk.red(
+                                        `More than ${maxOccurrences} match for regex ${regex.source}`
+                                    ),
+                                    chalk.red(message),
+                                    ...matches,
+                                ].join('\n');
 
-                            const err = new Error();
-                            err.stdout = msg;
-                            throw err;
-                        }
-                    })
-                    .catch(err => {
-                        // git grep returns with error code 1 when there are no matches.
-                        // For us, this is not actually an error state so we swallow the
-                        // error by returning a fake resolved Promise.
-                        if (
-                            err.code === 1 &&
-                            err.stdout === '' &&
-                            err.stderr === ''
-                        ) {
-                            return Promise.resolve();
-                        }
+                                const err = new Error();
+                                err.stdout = msg;
+                                throw err;
+                            }
+                        })
+                        .catch(err => {
+                            // git grep returns with error code 1 when there are no matches.
+                            // For us, this is not actually an error state so we swallow the
+                            // error by returning a fake resolved Promise.
+                            if (
+                                err.code === 1 &&
+                                err.stdout === '' &&
+                                err.stderr === ''
+                            ) {
+                                return Promise.resolve();
+                            }
 
-                        // In all other cases, assume it's a real error
-                        return Promise.reject(err);
-                    })
+                            // In all other cases, assume it's a real error
+                            return Promise.reject(err);
+                        })
             )
         ),
     concurrent: true,
