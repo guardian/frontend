544fc9a65b0d0ef72af7b0ba3390f678891e7b4c
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-16 17:47:28 +0100

    reinstate main content div

--- common/app/html/HtmlPage.scala
+++ common/app/html/HtmlPage.scala
@@ -27,7 +27,16 @@ object HtmlPageHelpers {
       commercial.topBanner() when showTop && showAds,
       header() when showTop
     )
-    if(model.Page.getContent(page).exists(_.tags.hasSuperStickyBanner)) headerContent else bannerAndHeaderDiv(headerContent)
+    if(model.Page.getContent(page).exists(_.tags.hasSuperStickyBanner))
+      stacked(
+        headerContent,
+        mainContent() when !page.metadata.shouldHideHeaderAndTopAds
+      )
+    else
+      stacked(
+      bannerAndHeaderDiv(headerContent),
+      mainContent() when !page.metadata.shouldHideHeaderAndTopAds
+    )
   }
 
   def defaultBodyClasses()(implicit page: model.Page, request: RequestHeader, applicationContext: ApplicationContext): Map[String, Boolean] = {
