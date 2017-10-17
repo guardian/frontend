cb0d2db579a1d9cbabff069436f38537e024ddb6
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-17 12:03:39 +0100

    set default config params in default method

--- common/app/rendering/core/JavascriptProps.scala
+++ common/app/rendering/core/JavascriptProps.scala
@@ -3,13 +3,7 @@ package rendering.core
 import conf.Configuration
 import play.api.libs.json.{JsValue, Json, Writes}
 
-case class JavascriptPropsConfig(
-  bundleUrl: String = conf.Static("javascripts/ui.bundle.browser.js"),
-  polyfillioUrl: String =
-    if (conf.switches.Switches.PolyfillIO.isSwitchedOn) common.Assets.js.polyfillioUrl
-    else conf.Static("javascripts/vendor/polyfillio.fallback.js"),
-  beaconUrl: String = Configuration.debug.beaconUrl
-)
+case class JavascriptPropsConfig(bundleUrl: String, polyfillioUrl: String, beaconUrl: String)
 case class JavascriptProps(config: JavascriptPropsConfig){
   def asJsValue(): JsValue = Json.toJson(this)
 }
@@ -17,5 +11,13 @@ case class JavascriptProps(config: JavascriptPropsConfig){
 object JavascriptProps {
   implicit val javascriptPropsConfig: Writes[JavascriptPropsConfig] = Json.writes[JavascriptPropsConfig]
   implicit val javascriptProps: Writes[JavascriptProps] = Json.writes[JavascriptProps]
-  def default(): JavascriptProps = JavascriptProps(JavascriptPropsConfig())
+  def default(): JavascriptProps = {
+    val bundleUrl = conf.Static("javascripts/ui.bundle.browser.js")
+    val polyfillioUrl =
+      if (conf.switches.Switches.PolyfillIO.isSwitchedOn) common.Assets.js.polyfillioUrl
+      else conf.Static("javascripts/vendor/polyfillio.fallback.js")
+    val beaconUrl = Configuration.debug.beaconUrl
+
+    JavascriptProps(JavascriptPropsConfig(bundleUrl, polyfillioUrl, beaconUrl))
+  }
 }
