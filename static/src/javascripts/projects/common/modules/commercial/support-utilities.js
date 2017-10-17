f58fdc5a88ef25bf592a1e9461532287e5e98f9b
Justin Pinner <justinpinner@gmail.com>
2017-10-17 11:59:01 +0100

    Change default links for US

--- static/src/javascripts/projects/common/modules/commercial/support-utilities.js
+++ static/src/javascripts/projects/common/modules/commercial/support-utilities.js
@@ -3,11 +3,18 @@
 import config from 'lib/config';
 import { getSync as geolocationGetSync } from 'lib/geolocation';
 
-const supportBaseURL = 'https://support.theguardian.com/uk';
-const supportButtonCaption = 'Support the Guardian';
-const useSupportDomain = (): boolean =>
+const liveInUk =
     config.get('switches.ukSupporterTrafficToNewSupportFrontend') &&
     geolocationGetSync() === 'GB';
+const liveInUs =
+    config.get('switches.usSupporterTrafficToNewSupportFrontend') &&
+    geolocationGetSync() === 'US';
+const supportBaseURL = liveInUs
+    ? 'https://support.theguardian.com/us/contribute'
+    : 'https://support.theguardian.com/uk';
+const supportButtonCaption = 'Support the Guardian';
+
+const useSupportDomain = (): boolean => liveInUk || liveInUs;
 const selectBaseUrl = (defaultUrl: string = supportBaseURL): string =>
     useSupportDomain() ? supportBaseURL : defaultUrl;
 const selectEngagementBannerButtonCaption = (defaultCaption: string) =>
