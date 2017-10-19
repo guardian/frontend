ed650e538d12b053870a037dd4fc2f2fae399cbd
Nicolas Long <nicolaslong@gmail.com>
2017-10-19 17:32:20 +0100

    transform projects/common/modules/identity/cookierefresh.js content to ES6

--- static/src/javascripts/projects/common/modules/identity/cookierefresh.js
+++ static/src/javascripts/projects/common/modules/identity/cookierefresh.js
@@ -1,23 +1,25 @@
 import Id from 'common/modules/identity/api';
 import storage from 'lib/storage';
 
-function CookieRefresh() {
-    this.init = function() {
-        var lastRefreshKey = 'identity.lastRefresh';
-        if (storage.local.isAvailable() && Id.isUserLoggedIn()) {
-            var lastRefresh = storage.local.get(lastRefreshKey),
-                currentTime = new Date().getTime();
-            if (this.shouldRefreshCookie(lastRefresh, currentTime)) {
-                Id.getUserFromApiWithRefreshedCookie();
-                storage.local.set(lastRefreshKey, currentTime);
+class CookieRefresh {
+    constructor() {
+        this.init = function() {
+            const lastRefreshKey = 'identity.lastRefresh';
+            if (storage.local.isAvailable() && Id.isUserLoggedIn()) {
+                const lastRefresh = storage.local.get(lastRefreshKey), currentTime = new Date().getTime();
+                if (this.shouldRefreshCookie(lastRefresh, currentTime)) {
+                    Id.getUserFromApiWithRefreshedCookie();
+                    storage.local.set(lastRefreshKey, currentTime);
+                }
             }
-        }
-    };
+        };
+    }
 
-    CookieRefresh.prototype.shouldRefreshCookie = function(lastRefresh, currentTime) {
+    shouldRefreshCookie(lastRefresh, currentTime) {
         return (!lastRefresh) || (currentTime > (parseInt(lastRefresh, 10) + (1000 * 86400 * 30)));
-    };
+    }
 }
+
 export default CookieRefresh;
 
 
