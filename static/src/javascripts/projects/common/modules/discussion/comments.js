92f07eff987b0d834eb324c8f7825626d6448558
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-10-19 15:26:12 +0100

    Make fix

--- static/src/javascripts/projects/common/modules/discussion/comments.js
+++ static/src/javascripts/projects/common/modules/discussion/comments.js
@@ -459,10 +459,7 @@ class Comments extends Component {
         commentBox.render(parentCommentEl);
 
         commentBox.on('post:success', (comment: CommentType) => {
-            let responses = qwery(
-                '.d-thread--responses',
-                parentCommentEl
-            )[0];
+            let responses = qwery('.d-thread--responses', parentCommentEl)[0];
 
             if (!responses) {
                 responses = bonzo.create(
