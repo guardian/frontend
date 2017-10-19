68e200933246020f5e488fb094e570acc44add09
Nicolas Long <nicolaslong@gmail.com>
2017-10-19 17:32:19 +0100

    transform projects/common/modules/identity/cookierefresh.js to ES6 module

--- static/src/javascripts/projects/common/modules/identity/cookierefresh.js
+++ static/src/javascripts/projects/common/modules/identity/cookierefresh.js
@@ -1,29 +1,23 @@
-define([
-    'common/modules/identity/api',
-    'lib/storage'
-],
-function (
-    Id,
-    storage
-) {
-    function CookieRefresh() {
-        this.init = function () {
-            var lastRefreshKey =  'identity.lastRefresh';
-            if (storage.local.isAvailable() && Id.isUserLoggedIn()) {
-                var lastRefresh = storage.local.get(lastRefreshKey),
-                    currentTime = new Date().getTime();
-                if (this.shouldRefreshCookie(lastRefresh, currentTime)) {
-                    Id.getUserFromApiWithRefreshedCookie();
-                    storage.local.set(lastRefreshKey, currentTime);
-                }
-            }
-        };
+import Id from 'common/modules/identity/api';
+import storage from 'lib/storage';
 
-        CookieRefresh.prototype.shouldRefreshCookie = function (lastRefresh, currentTime) {
-            return (!lastRefresh) || (currentTime > (parseInt(lastRefresh, 10) + (1000 * 86400 * 30)));
-        };
-    }
-    return CookieRefresh;
-});
+function CookieRefresh() {
+    this.init = function() {
+        var lastRefreshKey = 'identity.lastRefresh';
+        if (storage.local.isAvailable() && Id.isUserLoggedIn()) {
+            var lastRefresh = storage.local.get(lastRefreshKey),
+                currentTime = new Date().getTime();
+            if (this.shouldRefreshCookie(lastRefresh, currentTime)) {
+                Id.getUserFromApiWithRefreshedCookie();
+                storage.local.set(lastRefreshKey, currentTime);
+            }
+        }
+    };
+
+    CookieRefresh.prototype.shouldRefreshCookie = function(lastRefresh, currentTime) {
+        return (!lastRefresh) || (currentTime > (parseInt(lastRefresh, 10) + (1000 * 86400 * 30)));
+    };
+}
+export default CookieRefresh;
 
 
