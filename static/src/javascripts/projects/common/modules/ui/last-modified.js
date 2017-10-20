2c595b3561e306a5c902898d03286e0746464491
NataliaLKB <natalialeebaltazar@gmail.com>
2017-10-20 11:54:06 +0100

    renaming variables for clarity

--- static/src/javascripts/projects/common/modules/ui/last-modified.js
+++ static/src/javascripts/projects/common/modules/ui/last-modified.js
@@ -5,20 +5,22 @@ import fastdom from 'lib/fastdom-promise';
 const lastModified = (): void => {
     fastdom
         .read(() => ({
-            jsLm: document.getElementsByClassName('js-lm')[0],
-            jsWpd: document.getElementsByClassName('js-wpd')[0],
+            lastModifiedElm: document.getElementsByClassName('js-lm')[0],
+            webPublicationDateElm: document.getElementsByClassName('js-wpd')[0],
         }))
         .then(els => {
-            const { jsLm, jsWpd } = els;
+            const { lastModifiedElm, webPublicationDateElm } = els;
 
-            if (jsLm) {
+            if (lastModifiedElm) {
                 fastdom.write(() => {
-                    jsWpd.classList.add('content__dateline-wpd--modified');
+                    webPublicationDateElm.classList.add(
+                        'content__dateline-wpd--modified'
+                    );
                 });
 
-                jsWpd.addEventListener('click', () => {
+                webPublicationDateElm.addEventListener('click', () => {
                     fastdom.write(() => {
-                        jsLm.classList.toggle('u-h');
+                        lastModifiedElm.classList.toggle('u-h');
                     });
                 });
             }
