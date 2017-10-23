e6c7a4f07ff986af118cc01545d269529b0d20a7
NataliaLKB <natalialeebaltazar@gmail.com>
2017-10-23 16:05:50 +0100

    refactoring fastdom in user-accounts

--- static/src/javascripts/projects/common/modules/navigation/user-account.js
+++ static/src/javascripts/projects/common/modules/navigation/user-account.js
@@ -3,30 +3,26 @@
 import fastdom from 'lib/fastdom-promise';
 import { getUserFromCookie, isUserLoggedIn } from 'common/modules/identity/api';
 
-const updateCommentLink = (): void => {
-    fastdom
-        .read(() => [...document.querySelectorAll('.js-show-comment-activity')])
-        .then(commentItems => {
-            const user = getUserFromCookie();
+const updateCommentLink = (commentItems): void => {
+    const user = getUserFromCookie();
 
-            if (user) {
-                commentItems.forEach(commentItem => {
-                    const commentLink = commentItem.querySelector(
-                        '.js-add-comment-activity-link'
-                    );
-
-                    if (commentLink) {
-                        fastdom.write(() => {
-                            commentItem.classList.remove('u-h');
-                            commentLink.setAttribute(
-                                'href',
-                                `https://profile.theguardian.com/user/id/${user.id}`
-                            );
-                        });
-                    }
-                });
-            }
+    if (user) {
+        commentItems.forEach(commentItem => {
+            fastdom
+                .read(() =>
+                    commentItem.querySelector('.js-add-comment-activity-link')
+                )
+                .then(commentLink =>
+                    fastdom.write(() => {
+                        commentItem.classList.remove('u-h');
+                        commentLink.setAttribute(
+                            'href',
+                            `https://profile.theguardian.com/user/id/${user.id}`
+                        );
+                    })
+                );
         });
+    }
 };
 
 const showMyAccountIfNecessary = (): void => {
@@ -34,22 +30,33 @@ const showMyAccountIfNecessary = (): void => {
         return;
     }
 
-    const signIns = [...document.querySelectorAll('.js-navigation-sign-in')];
-    const accountActionsLists = [
-        ...document.querySelectorAll('.js-navigation-account-actions'),
-    ];
-
     fastdom
-        .write(() => {
-            signIns.forEach(signIn => {
-                signIn.classList.add('u-h');
-            });
+        .read(() => ({
+            signIns: [...document.querySelectorAll('.js-navigation-sign-in')],
+            accountActionsLists: [
+                ...document.querySelectorAll('.js-navigation-account-actions'),
+            ],
+            commentItems: [
+                ...document.querySelectorAll('.js-show-comment-activity'),
+            ],
+        }))
+        .then(els => {
+            const { signIns, accountActionsLists, commentItems } = els;
 
-            accountActionsLists.forEach(accountActions => {
-                accountActions.classList.remove('u-h');
-            });
-        })
-        .then(updateCommentLink);
+            return fastdom
+                .write(() => {
+                    signIns.forEach(signIn => {
+                        signIn.classList.add('u-h');
+                    });
+
+                    accountActionsLists.forEach(accountActions => {
+                        accountActions.classList.remove('u-h');
+                    });
+                })
+                .then(() => {
+                    updateCommentLink(commentItems);
+                });
+        });
 };
 
 export { showMyAccountIfNecessary };
