51414a6f888e567aa6d0c2f53fb4b345eb88eb20
Justin Pinner <justinpinner@gmail.com>
2017-10-25 16:52:47 +0100

    WIP. So very WIP it's not even funny.

--- static/src/javascripts/bootstraps/enhanced/membership.js
+++ static/src/javascripts/bootstraps/enhanced/membership.js
@@ -1,8 +1,10 @@
 // @flow
 import { membershipTab } from 'membership/membership-tab';
 import { digitalpackTab } from 'membership/digitalpack-tab';
+import { recurringContributionTab } from 'membership/contributions-recurring-tab';
 
 export const init = (): void => {
     membershipTab();
     digitalpackTab();
+    recurringContributionTab();
 };
