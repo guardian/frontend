90bde9f58c8b152b9db8408689e31ca15c37e376
Joseph Smith <joseph.smith@guardian.co.uk>
2017-10-25 12:06:31 +0100

    Update control copy

--- static/src/javascripts/projects/common/modules/commercial/acquisitions-copy.js
+++ static/src/javascripts/projects/common/modules/commercial/acquisitions-copy.js
@@ -3,13 +3,16 @@ import {
     supportFrontendLiveInUk,
     supportFrontendLiveInUs,
 } from 'common/modules/commercial/support-utilities';
+import { getLocalCurrencySymbol } from 'lib/geolocation';
 
 // control
 const controlHeading = 'Since you’re here &hellip;';
 const controlP1 =
     '&hellip; we have a small favour to ask. More people are reading the Guardian than ever but advertising revenues across the media are falling fast. And <span class="contributions__highlight">unlike many news organisations, we haven’t put up a paywall &ndash; we want to keep our journalism as open as we can</span>. So you can see why we need to ask for your help. The Guardian’s independent, investigative journalism takes a lot of time, money and hard work to produce. But we do it because we believe our perspective matters &ndash; because it might well be your perspective, too.';
-const controlP2 =
-    ' If everyone who reads our reporting, who likes it, helps to support it, our future would be much more secure.';
+const controlP2FirstSentence =
+    ' If everyone who reads our reporting, who likes it, helps fund it, our future would be much more secure.';
+const controlP2 = (currencySymbol: string) =>
+    `${controlP2FirstSentence} <strong>For as little as ${currencySymbol}1, you can support the Guardian – and it only takes a minute. Thank you.</strong>`;
 
 // control regulars
 const controlHeadingRegulars = 'Hello again &hellip;';
@@ -37,19 +40,19 @@ const ctaLinkSentence = (
 export const control: AcquisitionsEpicTemplateCopy = {
     heading: controlHeading,
     p1: controlP1,
-    p2: controlP2,
+    p2: controlP2(getLocalCurrencySymbol()),
 };
 
 export const regulars: AcquisitionsEpicTemplateCopy = {
     heading: controlHeadingRegulars,
     p1: controlP1Regulars,
-    p2: controlP2,
+    p2: controlP2(getLocalCurrencySymbol()),
 };
 
 export const usLocalisedFlag = {
     heading: controlHeading,
     p1: usLocalisedFlagP1,
-    p2: controlP2,
+    p2: controlP2(getLocalCurrencySymbol()),
 };
 
 export const liveblog = (
@@ -57,7 +60,7 @@ export const liveblog = (
     contributionsUrl: string
 ): AcquisitionsEpicTemplateCopy => ({
     p1: `Since you’re here ${controlP1}`,
-    p2: `${controlP2} ${ctaLinkSentence(
+    p2: `${controlP2FirstSentence} ${ctaLinkSentence(
         membershipUrl,
         contributionsUrl
     )}. - Guardian HQ`,
