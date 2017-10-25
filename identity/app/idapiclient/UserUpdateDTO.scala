b7a966a910cf4b045b3bb384dbe3281b208b4052
Mario Galic <mario.galic@guardian.co.uk>
2017-10-25 16:30:05 +0100

    Remove statusFields from UserUpdateDTO

--- identity/app/idapiclient/UserUpdateDTO.scala
+++ identity/app/idapiclient/UserUpdateDTO.scala
@@ -4,15 +4,11 @@ import com.gu.identity.model._
 
 /**
   * Represents up-to-date user data DTO that is serialised and posted to IDAPI
-  *
-  * Note that this is not the User model from identity-model library, although
-  * it seems to have one-to-one mapping to User DO fields.
   */
 case class UserUpdateDTO (
   primaryEmailAddress: Option[String] = None,
   publicFields: Option[PublicFields] = None,
   privateFields: Option[PrivateFields] = None,
-  statusFields: Option[StatusFields] = None,
   dates: Option[UserDates] = None,
   password: Option[String] = None,
   userGroups: Option[Set[GroupMembership]] = None,
