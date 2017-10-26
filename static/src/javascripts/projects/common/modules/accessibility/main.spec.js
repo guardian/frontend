5075fd406c68900a440e4644665d28f0e39b1682
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-26 12:28:07 +0100

    mock user prefs

--- static/src/javascripts/projects/common/modules/accessibility/main.spec.js
+++ static/src/javascripts/projects/common/modules/accessibility/main.spec.js
@@ -2,18 +2,7 @@
 
 import { saveState, isOn } from 'common/modules/accessibility/main';
 
-jest.mock('common/modules/user-prefs', () => {
-    const storage = {};
-
-    return {
-        get(key) {
-            return storage[key];
-        },
-        set(key, value) {
-            storage[key] = value;
-        },
-    };
-});
+jest.mock('common/modules/user-prefs');
 
 describe('Accessibility', () => {
     it('saves the state', () => {
@@ -24,8 +13,8 @@ describe('Accessibility', () => {
 
         expect(isOn('flashing-elements')).toBe(true);
         expect(isOn('exploding-avocados')).toBe(false);
-
-        // state is on by default
+    });
+    it('state is on by default', () => {
         expect(isOn('jumping-weathervanes')).toBe(true);
     });
 });
