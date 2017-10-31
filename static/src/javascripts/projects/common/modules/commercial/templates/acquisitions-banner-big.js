e33434a08df11ab06446519460836df84e8280fe
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-10-31 17:26:29 +0000

    big variant done

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-big.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-big.js
@@ -2,13 +2,18 @@
 export const acquisitionsBannerBigTemplate = (
     params: EngagementBannerTemplateParams
 ) =>
-    `<div id="site-message__message">
-        <div class="site-message__message site-message__message--membership">
-            <span class = "membership__message-text big">${params.messageText}${params.ctaText}</span>
-            <span class="membership__paypal-container">
-                <img class="membership__paypal-logo" src="${params.paypalAndCreditCardImage}" alt="Paypal and credit card">
-                <span class="membership__support-button"><a class="message-button-rounded__cta ${params.colourClass}" href="${params.linkUrl}">${params.buttonCaption}${params.buttonSvg}</a></span>
-            </span>
+    `<div id="site-message__message" class="big-test-container">
+        <div class="site-message__message site-message__message--membership big-test">
+            <div class="membership__big-message-container">
+                <span class = "membership__message-text big">${params.messageText}</span>
+                <div class="membership__big--cta-with-paypal">             
+                    <span class = "membership__message-text small">${params.ctaText}</span>
+                    <span class="membership__paypal-container">
+                        <img class="membership__paypal-logo" src="${params.paypalAndCreditCardImage}" alt="Paypal and credit card">
+                        <span class="membership__support-button"><a class="message-button-rounded__cta ${params.colourClass}" href="${params.linkUrl}">${params.buttonCaption}${params.buttonSvg}</a></span>
+                    </span>
+                </div>
+             </div>
         </div>
         <a class="u-faux-block-link__overlay js-engagement-message-link" target="_blank" href="${params.linkUrl}" data-link-name="Read more link"></a>
     </div>`;
