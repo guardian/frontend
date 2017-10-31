660a7e90d0799e5b11b5d65cb13248f53f8aa2de
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-10-31 14:14:05 +0000

    Raven: Generalize "There is no space left matching" ignore rule

--- static/src/javascripts/lib/raven.js
+++ static/src/javascripts/lib/raven.js
@@ -25,7 +25,7 @@ const sentryOptions = {
 
     ignoreErrors: [
         "Can't execute code from a freed script",
-        'There is no space left matching rules from .js-article__body',
+        /There is no space left matching rules from/gi,
 
         // weatherapi/city.json frequently 404s and lib/fetch-json throws an error
         'Fetch error while requesting https://api.nextgen.guardianapps.co.uk/weatherapi/city.json:',
