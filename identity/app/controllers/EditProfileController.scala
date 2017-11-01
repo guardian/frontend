001cb48a92f808f884969bbcbc2967091b28d252
Mario Galic <mario.galic@guardian.co.uk>
2017-11-01 13:35:11 +0000

    Fix bound errors not showing up

--- identity/app/controllers/EditProfileController.scala
+++ identity/app/controllers/EditProfileController.scala
@@ -130,16 +130,15 @@ class EditProfileController(
     emailSubscriptions: List[String],
     availableLists: EmailNewsletters,
     idRequest: services.IdentityRequest,
-    identityUrlBuilder: IdentityUrlBuilder
-  )(implicit request: AuthRequest[AnyContent]): Result = {
-    if(request.toString.contains("/privacy/edit"))
-      NoCache(Redirect("/email-prefs"))
-    else
-      NoCache(Ok(views.html.profileForms(page, user, forms, idRequestParser(request), idUrlBuilder,emailPrefsForm,availableLists,emailSubscriptions)))
+    identityUrlBuilder: IdentityUrlBuilder)
+    (implicit request: AuthRequest[AnyContent]): Result = {
+
+      NoCache(Ok(views.html.profileForms(
+        page, user, forms, idRequestParser(request), idUrlBuilder, emailPrefsForm, availableLists, emailSubscriptions)))
   }
 
-
-  //Email preferences are submitted individually by a javascript call, not by the form submission. The form submission only deals with the consents.
+  // Email preferences are submitted individually by a javascript call,
+  // not by the form submission. The form submission only deals with the consents.
 
   def getEmailPrefsForm(request: AuthRequest[AnyContent]): Future[Form[EmailPrefsData]] = {
     import EmailPrefsData._
