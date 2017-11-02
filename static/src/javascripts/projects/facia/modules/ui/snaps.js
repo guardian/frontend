9692706c0db61232344578ffeccde75815b44ac7
Simon Adcock <simon.adcock@guardian.co.uk>
2017-11-02 16:41:27 +0000

    transform projects/facia/modules/ui/snaps.js content to ES6

--- static/src/javascripts/projects/facia/modules/ui/snaps.js
+++ static/src/javascripts/projects/facia/modules/ui/snaps.js
@@ -13,40 +13,35 @@ import relativeDates from 'common/modules/ui/relativedates';
 import FootballSnaps from 'facia/modules/ui/football-snaps';
 import once from 'lodash/functions/once';
 import find from 'lodash/collections/find';
-var clientProcessedTypes = ['document', 'fragment', 'json.html'],
-    snapIframes = [],
-    bindIframeMsgReceiverOnce = once(function() {
-        bean.on(window, 'message', function(event) {
-            var iframe = find(snapIframes, function(iframe) {
-                    return iframe.contentWindow === event.source;
-                }),
-                message;
-            if (iframe) {
-                message = JSON.parse(event.data);
-                if (message.type === 'set-height') {
-                    bonzo(iframe).parent().css('height', message.value);
-                }
-            }
-        });
-    });
+const clientProcessedTypes = ['document', 'fragment', 'json.html'],
+      snapIframes = [],
+      bindIframeMsgReceiverOnce = once(() => {
+          bean.on(window, 'message', event => {
+              const iframe = find(snapIframes, iframe => iframe.contentWindow === event.source);
+              let message;
+              if (iframe) {
+                  message = JSON.parse(event.data);
+                  if (message.type === 'set-height') {
+                      bonzo(iframe).parent().css('height', message.value);
+                  }
+              }
+          });
+      });
 
 function init() {
 
     // First, init any existing inlined embeds already on the page.
-    var inlinedSnaps = toArray($('.facia-snap-embed'));
+    const inlinedSnaps = toArray($('.facia-snap-embed'));
     inlinedSnaps.forEach(initInlinedSnap);
 
     // Second, init non-inlined embeds.
-    var snaps = toArray($('.js-snappable.js-snap'))
-        .filter(function(el) {
+    const snaps = toArray($('.js-snappable.js-snap'))
+        .filter(el => {
 
-            var isInlinedSnap = $(el).hasClass('facia-snap-embed'),
-                snapType = el.getAttribute('data-snap-type');
+            const isInlinedSnap = $(el).hasClass('facia-snap-embed'), snapType = el.getAttribute('data-snap-type');
             return !isInlinedSnap && snapType && clientProcessedTypes.indexOf(snapType) > -1;
         })
-        .filter(function(el) {
-            return el.getAttribute('data-snap-uri');
-        });
+        .filter(el => el.getAttribute('data-snap-uri'));
 
     snaps.forEach(initStandardSnap);
 }
@@ -59,9 +54,10 @@ function addCss(el, isResize) {
 }
 
 function setSnapPoint(el, isResize) {
-    var width, breakpoints,
-        $el = bonzo(el),
-        prefix = 'facia-snap-point--';
+    let width;
+    let breakpoints;
+    const $el = bonzo(el);
+    const prefix = 'facia-snap-point--';
 
     breakpoints = [{
         width: 0,
@@ -83,40 +79,38 @@ function setSnapPoint(el, isResize) {
         name: 'huge'
     }];
 
-    fastdom.read(function() {
+    fastdom.read(() => {
         width = el.offsetWidth;
     });
 
-    fastdom.write(function() {
-        breakpoints.map(function(breakpoint, i, arr) {
-                var isAdd = width >= breakpoint.width && (arr[i + 1] ? width < arr[i + 1].width : true);
+    fastdom.write(() => {
+        breakpoints.map((breakpoint, i, arr) => {
+                const isAdd = width >= breakpoint.width && (arr[i + 1] ? width < arr[i + 1].width : true);
                 breakpoint.action = isAdd ? 'addClass' : isResize ? 'removeClass' : false;
                 return breakpoint;
             })
-            .filter(function(breakpoint) {
-                return breakpoint.action;
-            })
-            .forEach(function(breakpoint) {
+            .filter(breakpoint => breakpoint.action)
+            .forEach(breakpoint => {
                 $el[breakpoint.action](prefix + breakpoint.name);
             });
     });
 }
 
 function injectIframe(el) {
-    var spec = bonzo(el).offset(),
-        minIframeHeight = Math.ceil((spec.width || 0) / 2),
-        maxIframeHeight = 400,
-        src = el.getAttribute('data-snap-uri'),
-        height = Math.min(Math.max(spec.height || 0, minIframeHeight), maxIframeHeight),
-        containerEl = bonzo.create('<div style="width: 100%; height: ' + height + 'px; ' +
-            'overflow: hidden; -webkit-overflow-scrolling:touch"></div>')[0],
-        iframe = bonzo.create('<iframe src="' + src + '" style="width: 100%; height: 100%; border: none;"></iframe>')[0];
+    const spec = bonzo(el).offset(),
+          minIframeHeight = Math.ceil((spec.width || 0) / 2),
+          maxIframeHeight = 400,
+          src = el.getAttribute('data-snap-uri'),
+          height = Math.min(Math.max(spec.height || 0, minIframeHeight), maxIframeHeight),
+          containerEl = bonzo.create('<div style="width: 100%; height: ' + height + 'px; ' +
+              'overflow: hidden; -webkit-overflow-scrolling:touch"></div>')[0],
+          iframe = bonzo.create('<iframe src="' + src + '" style="width: 100%; height: 100%; border: none;"></iframe>')[0];
 
     bonzo(containerEl).append(iframe);
     snapIframes.push(iframe);
     bindIframeMsgReceiverOnce();
 
-    fastdom.write(function() {
+    fastdom.write(() => {
         bonzo(el).empty().append(containerEl);
     });
 }
@@ -124,22 +118,20 @@ function injectIframe(el) {
 function fetchFragment(el, asJson) {
     fetch(el.getAttribute('data-snap-uri'), {
         mode: 'cors'
-    }).then(function(resp) {
+    }).then(resp => {
         if (resp.ok) {
-            return asJson ? resp.json().then(function(json) {
-                return json.html;
-            }) : resp.text();
+            return asJson ? resp.json().then(json => json.html) : resp.text();
         } else {
             return Promise.reject(new Error('Fetch error: ' + resp.statusText));
         }
-    }).then(function(resp) {
-        $.create(resp).each(function(html) {
-            fastdom.write(function() {
+    }).then(resp => {
+        $.create(resp).each(html => {
+            fastdom.write(() => {
                 bonzo(el).html(html);
             });
         });
         relativeDates.init(el);
-    }).catch(function(ex) {
+    }).catch(ex => {
         reportError(ex, {
             feature: 'snaps'
         });
@@ -147,8 +139,8 @@ function fetchFragment(el, asJson) {
 }
 
 function initStandardSnap(el) {
-    proximityLoader.addProximityLoader(el, 1500, function() {
-        fastdom.write(function() {
+    proximityLoader.addProximityLoader(el, 1500, () => {
+        fastdom.write(() => {
             bonzo(el).addClass('facia-snap-embed');
         });
         addCss(el);
@@ -168,7 +160,7 @@ function initStandardSnap(el) {
         }
 
         if (!detect.isIOS) {
-            mediator.on('window:throttledResize', function() {
+            mediator.on('window:throttledResize', () => {
                 addCss(el, true);
             });
         }
@@ -178,12 +170,12 @@ function initStandardSnap(el) {
 function initInlinedSnap(el) {
     addCss(el);
     if (!detect.isIOS) {
-        mediator.on('window:throttledResize', function() {
+        mediator.on('window:throttledResize', () => {
             addCss(el, true);
         });
     }
 }
 
 export default {
-    init: init
+    init
 };
