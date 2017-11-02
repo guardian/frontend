293a2443194ecbe1c012ff0759c98d38fcf115e7
guy.dawson <guy.dawson@theguardian.com>
2017-11-02 14:56:54 +0000

    enrich acquisition links in footer and header

---
+++ static/src/javascripts/projects/common/modules/commercial/acquisitions-link-enrichment.js
@@ -0,0 +1,45 @@
+import { addReferrerData } from 'common/modules/commercial/acquisitions-ophan';
+
+const addReferrerDataToAcquisitionLink = (rawUrl: string): string => {
+
+    const acquisitionDataField = 'acquisitionData';
+
+    let url;
+    try {
+        url = new URL(rawUrl);
+    } catch (e) {
+        return rawUrl;
+    }
+
+    let acquisitionData;
+    try {
+        acquisitionData = JSON.parse(url.searchParams.get(acquisitionDataField))
+    } catch (e) {
+        return rawUrl;
+    }
+
+    if (acquisitionData) {
+        acquisitionData = addReferrerData(acquisitionData);
+        url.searchParams.set(acquisitionDataField, JSON.stringify(acquisitionData));
+    }
+
+    return url.toString()
+};
+
+const ACQUISITION_LINK_CLASS = 'js-acquisition-link';
+
+const addReferrerDataToAcquisitionLinksOnPage = (): void => {
+
+    const links = Array.from(document.getElementsByClassName(ACQUISITION_LINK_CLASS));
+
+    links.forEach(el => {
+        const link = el.getAttribute('href');
+        if (link) {
+            el.setAttribute('href', addReferrerDataToAcquisitionLink(link))
+        }
+    })
+};
+
+export const init = (): void => {
+  addReferrerDataToAcquisitionLinksOnPage();
+};
