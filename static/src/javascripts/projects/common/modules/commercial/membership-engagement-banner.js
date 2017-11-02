e7793e203276873c64edf6ba469120f498d14443
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-11-02 07:47:38 +0000

    test

--- static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
@@ -57,9 +57,7 @@ const getUserVariantParams = (
 
         return userVariantParams;
     } else if (campaignId && userVariant) {
-        return {
-            campaignCode: buildCampaignCode(campaignId, userVariant.id),
-        };
+        return {};
     }
     return {};
 };
