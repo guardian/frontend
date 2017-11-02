a54acc097788e878a8dadcdd875d7912100b4e35
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-11-02 12:41:49 +0000

    ES6: Convert modules/ui/message.spec.js

---
+++ static/src/javascripts/projects/common/modules/ui/message.spec.js
@@ -0,0 +1,102 @@
+// @flow
+
+import $ from 'lib/$';
+import _userPrefs from 'common/modules/user-prefs';
+import { Message } from './message';
+
+jest.mock('common/modules/user-prefs', () => ({
+    get: jest.fn(),
+    set: jest.fn(),
+}));
+
+jest.mock('common/modules/analytics/register', () => ({
+    begin: jest.fn(),
+}));
+
+const userPrefs: any = _userPrefs;
+
+describe('Message', () => {
+    beforeEach(() => {
+        if (document.body) {
+            document.body.innerHTML = `
+                <div id="header" class="l-header"></div>
+                <div class="js-site-message is-hidden">
+                    <div class="js-site-message-copy">...</div>
+                    <button class="site-message__close"></button>
+                </div>
+                <div class="site-message--footer is-hidden js-footer-message">
+                    <div class="site-message__copy js-footer-site-message-copy u-cf"></div>
+                </div>'
+            `;
+        }
+    });
+
+    it('Show a message', () => {
+        new Message('foo').show('hello world');
+        expect($('.js-site-message-copy').text()).toContain('hello world');
+        expect($('.js-footer-site-message-copy').text()).not.toContain(
+            'hello world'
+        );
+        expect($('.js-site-message').hasClass('is-hidden')).toBeFalsy();
+    });
+
+    it('Show a pinnable message', () => {
+        new Message('foo', { pinOnHide: true }).show('hello world');
+        expect($('.js-site-message-copy').text()).toContain('hello world');
+        expect($('.js-footer-site-message-copy').text()).toContain(
+            'hello world'
+        );
+        expect($('.js-site-message').hasClass('is-hidden')).toBeFalsy();
+    });
+
+    it('Hide a message', () => {
+        const m = new Message('foo');
+        m.show('hello world');
+        m.hide();
+        expect($('.js-site-message').hasClass('is-hidden')).toBeTruthy();
+        expect($('.js-footer-message').hasClass('is-hidden')).toBeTruthy();
+    });
+
+    it('Hide a pinnable message', () => {
+        const m = new Message('foo', { pinOnHide: true });
+        m.show('hello world');
+        m.hide();
+        expect($('.js-site-message').hasClass('is-hidden')).toBeTruthy();
+        expect($('.js-footer-message').hasClass('is-hidden')).toBeFalsy();
+    });
+
+    it.skip('Remember the user has acknowledged the message', () => {
+        const m = new Message('foo');
+        m.acknowledge();
+        expect(userPrefs.set).toHaveBeenCalledWith('messages', []);
+        expect(m.hasSeen()).toBeTruthy();
+    });
+
+    it('Block messages from overwriting each other', () => {
+        const m1 = new Message('foo');
+        const m2 = new Message('bar');
+        m1.show('message one');
+        m2.show('message two');
+        expect($('.js-site-message-copy').text()).toContain('message one');
+    });
+
+    it("Allow 'important' messages to overwrite an existing message", () => {
+        const m1 = new Message('a');
+        const m2 = new Message('b', { important: true });
+        const m3 = new Message('c');
+        m1.show('message one');
+        m2.show('message two');
+        m3.show('message three');
+        expect($('.js-site-message-copy').text()).toContain('message two');
+    });
+
+    it('has option to stay permanently open', () => {
+        new Message('a', { permanent: true }).show('message one');
+        expect($('.site-message__close').hasClass('is-hidden')).toBeTruthy();
+    });
+
+    it('permanent message should have class added', () => {
+        new Message('a', { permanent: true }).show('message one');
+        expect($('.site-message--permanent').length).toEqual(1);
+    });
+});
