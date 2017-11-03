d1350e6454b29afc11401cf4172ef7ac15735def
Simon Adcock <simon.adcock@guardian.co.uk>
2017-11-03 13:47:20 +0000

    correct mock for lib/config

--- static/src/javascripts/lib/__mocks__/config.js
+++ static/src/javascripts/lib/__mocks__/config.js
@@ -2,12 +2,18 @@
 
 export default {
     get(path: string, defaultValue: any): any {
-        return path.split('.').reduce((acc: Object, prop: string): any => {
+        const value = path.split('.').reduce((acc: Object, prop: string): any => {
             if (acc[prop]) {
                 return acc[prop]
             }
 
             return defaultValue;
         }, this);
+
+        if (typeof value !== 'undefined') {
+            return value;
+        }
+
+        return defaultValue;
     }
 };
