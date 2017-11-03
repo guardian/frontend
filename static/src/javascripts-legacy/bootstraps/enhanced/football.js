5b39530aa0d4f25d56dd6e9ee5c65b6965d70098
GHaberis <george.haberis@theguardian.com>
2017-11-03 13:39:19 +0000

    test football fix

--- static/src/javascripts-legacy/bootstraps/enhanced/football.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/football.js
@@ -141,6 +141,39 @@ define([
         var extras = [],
             dropdownTemplate;
 
+        var isLiveClockwatch = new Promise(function (resolve, reject) {
+            page.isLiveClockwatch(function () {
+                var ml = new MatchListLive.MatchListLive('match-day', page.isCompetition() || 'premierleague', config.dateFromSlug()),
+                    $img = $('.media-primary'),
+                    $matchListContainer = $.create('<div class="football-matches__container" data-link-name="football-matches-clockwatch"></div>')
+                                              .css({ minHeight: $img[0] ? $img[0].offsetHeight : 0 });
+                             
+                $img.addClass('u-h');
+    
+                loading($matchListContainer[0], 'Fetching today’s matches…', { text: 'Impatient?', href: '/football/live' });
+    
+                $('.js-football-meta').append($matchListContainer);
+
+                var handleResponse = function(success) {
+                    if (!success || $('.football-match', $matchListContainer[0]).length === 0) {
+                        ml.destroy();
+                        $matchListContainer.remove();
+                        $img.removeClass('u-h');
+                    }
+                    
+                    $matchListContainer.css({ minHeight: 0 });
+                    loaded($matchListContainer[0]);
+                    resolve();
+                };
+                
+                ml.fetch($matchListContainer[0]).then(function() {
+                    handleResponse(true);
+                }).catch(function() {
+                    handleResponse(false);
+                });
+            });
+        });
+
         page.isMatch(function (match) {
             extras[0] = { ready: false };
             if (match.pageType === 'stats') {
@@ -223,33 +256,6 @@ define([
             }
         });
 
-        page.isLiveClockwatch(function () {
-            var ml = new MatchListLive.MatchListLive('match-day', page.isCompetition() || 'premierleague', config.dateFromSlug()),
-                $img = $('.media-primary'),
-                $matchListContainer = $.create('<div class="football-matches__container" data-link-name="football-matches-clockwatch"></div>')
-                                          .css({ minHeight: $img[0] ? $img[0].offsetHeight : 0 });
-
-            $img.addClass('u-h');
-
-            loading($matchListContainer[0], 'Fetching today’s matches…', { text: 'Impatient?', href: '/football/live' });
-
-            $('.js-football-meta').append($matchListContainer);
-            
-            ml.fetch($matchListContainer[0]).fail(function () {
-                ml.destroy();
-                $matchListContainer.remove();
-                $img.removeClass('u-h');
-            }).always(function () {
-                if ($('.football-match', $matchListContainer[0]).length === 0) {
-                    ml.destroy();
-                    $matchListContainer.remove();
-                    $img.removeClass('u-h');
-                }
-                $matchListContainer.css({ minHeight: 0 });
-                loaded($matchListContainer[0]);
-            });
-        });
-
         page.isFootballStatsPage(function () {
             $('.js-chart').each(function (el) {
                 new Doughnut().render(el);
@@ -291,6 +297,8 @@ define([
         });
 
         tagPageStats.tagPageStats();
+
+        return Promise.all([isLiveClockwatch]);
     }
 
     return {
--- static/test/javascripts-legacy/spec/common/football.spec.js
+++ static/test/javascripts-legacy/spec/common/football.spec.js
@@ -6,16 +6,40 @@ define([
         var injector = new Injector();
         var sut;
         var sandbox;
-    
+        var destroySpy;
+
+        function mockMatchListLive() {};
+        
         beforeEach(function (done) {
+            document.body.innerHTML = '<img class="media-primary"></img><div class="js-football-meta"></div>';
+
             sandbox = sinon.sandbox.create();
+
+            destroySpy = sandbox.spy();
+
+            mockMatchListLive.prototype.destroy = destroySpy;
+
+            injector.mock('lib/config', {
+                dateFromSlug: sinon.spy()
+            });
+
+            injector.mock('bean', {
+                on: sinon.spy()
+            });
+
+            injector.mock('common/modules/sport/football/tag-page-stats', {
+                tagPageStats: sinon.spy()
+            });
+
+            injector.mock('common/modules/sport/football/match-list-live', {
+                MatchListLive: mockMatchListLive
+            });
 
             injector.mock('lib/page', {
                 isMatch: sandbox.spy(),
                 isCompetition: sandbox.spy(),
                 isLiveClockwatch: function (cb) {
-                    console.log('***', cb);
-                    // cb();
+                    cb();
                 },
                 isFootballStatsPage: sandbox.spy()
             });
@@ -30,11 +54,59 @@ define([
         });
 
         afterEach(function () {
+            document.body.innerHTML = '';
             sandbox.restore();
         });
 
-        it('isLiveClockwatch', function () {
-            sut.init();
+        describe('isLiveClockwatch', function () {
+            var fetchSpy;
+
+            var addFootballElem = function (elem) {
+                var footballMatchElem = document.createElement('div');
+                
+                footballMatchElem.classList.add('football-match');
+                
+                footballMatchElem.appendChild(elem);
+
+                document.body.appendChild(footballMatchElem);
+            };
+
+            afterEach(function () {
+                expect(fetchSpy).toHaveBeenCalled();
+                expect(destroySpy).toHaveBeenCalled();
+                expect(document.querySelectorAll('.football-matches__container').length).toBe(0);
+                expect(document.querySelector('.media-primary').classList.contains('u-h')).toBeFalsy();
+            });
+            
+            it('handles successful fetch request', function (done) {
+                fetchSpy = sandbox.spy(function(elem) {
+                    return new Promise(function (resolve, reject) {
+                        addFootballElem(elem);
+                        resolve();
+                    });
+                });
+
+                mockMatchListLive.prototype.fetch = fetchSpy;
+
+                sut.init().then(function() {
+                   done();
+                });
+            });
+
+            it('handles failed fetch request', function (done) {
+                fetchSpy = sandbox.spy(function(elem) {
+                    return new Promise(function (resolve, reject) {
+                        addFootballElem(elem);
+                        reject();
+                    });
+                });
+
+                mockMatchListLive.prototype.fetch = fetchSpy;
+
+                sut.init().then(function() {
+                    done();
+                });
+            });
         });
     });
 });
