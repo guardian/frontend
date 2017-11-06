2e4c67b81a4df0219759fdf1efb526324d58ab58
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-11-06 15:10:23 +0000

    remove banner test, set new control

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-control.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-control.js
@@ -2,9 +2,16 @@
 export const acquisitionsBannerControlTemplate = (
     params: EngagementBannerTemplateParams
 ) =>
-    `<div id="site-message__message">
-        <div class="site-message__message site-message__message--membership">
-            <span class = "membership__message-text">${params.messageText} ${params.ctaText}</span>
+    `<div id="site-message__message long-test">
+        <div class="site-message__message site-message__message--membership long-test">
+            <div class="membership__message-text-long">
+                <span class = "membership__message-text">
+                    <strong>Unlike many news organisations, we haven't put up a paywall - we want to keep our journalism as open as we can.</strong>
+                    The Guardian's independent, investigative journalism
+                takes a lot of time, money and hard work to produce. But the revenue we get from advertising is falling,
+                so we increasingly need our readers to fund us. If everyone who reads our reporting, who likes it, helps
+                fund it, our future would be much more secure. <strong>${params.ctaText}</strong></span>
+            </div>
             <span class="membership__paypal-container">
                 <img class="membership__paypal-logo" src="${params.paypalAndCreditCardImage}" alt="Paypal and credit card">
                 <span class="membership__support-button"><a class="message-button-rounded__cta ${params.colourClass}" href="${params.linkUrl}">${params.buttonCaption}${params.buttonSvg}</a></span>
