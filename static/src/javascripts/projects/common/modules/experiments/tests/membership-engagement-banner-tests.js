bb96cfc057c7adb5109afb054f6908e959fbb805
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-11-07 12:19:20 +0000

    Remove big long banner test, set new control (#18179)
    * remove banner test, set new control
    
    * remove css

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,7 +1,4 @@
 // @flow
-
-import { AcquisitionsBannerBigLongTwo } from 'common/modules/experiments/tests/acquisitions-banner-big-long-two';
-
 export const membershipEngagementBannerTests: $ReadOnlyArray<
     AcquisitionsABTest
-> = [AcquisitionsBannerBigLongTwo];
+> = [];
