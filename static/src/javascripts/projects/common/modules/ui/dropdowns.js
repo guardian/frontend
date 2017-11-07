5e5f6336128a0004cce8bb6d871fba26796a754b
Laura <laura.gonzalez@guardian.co.uk>
2017-11-07 10:55:36 +0000

    lint code

--- static/src/javascripts/projects/common/modules/ui/dropdowns.js
+++ static/src/javascripts/projects/common/modules/ui/dropdowns.js
@@ -25,69 +25,89 @@ const init = (): void => {
         const container = (e.currentTarget: any).closest(containerSelector);
 
         if (container) {
+            fastdom
+                .read(() => {
+                    const content = container.querySelector(`.${contentCN}`);
+                    const isActive: boolean = container.classList.contains(
+                        'dropdown--active'
+                    );
+                    const isAnimated: boolean = container.classList.contains(
+                        'dropdown--animated'
+                    );
+                    const contentEstimatedHeight =
+                        content.offsetHeight !== undefined &&
+                        content.offsetHeight < window.innerHeight
+                            ? content.offsetHeight
+                            : window.innerHeight;
 
-            fastdom.read(() => {
-                const content = container.querySelector(`.${contentCN}`);
-                const isActive: boolean = container.classList.contains(
-                    'dropdown--active'
-                );
-                const isAnimated: boolean = container.classList.contains(
-                    'dropdown--animated'
-                );
-                const contentEstimatedHeight =
-                    content.offsetHeight !== undefined &&
-                    content.offsetHeight < window.innerHeight
-                        ? content.offsetHeight
-                        : window.innerHeight;
+                    /*
+                    by clamping the transition to windowHeight we avoid
+                    a very awkward effect where a long list doesn't really
+                    get any visual closing feedback in the first milliseconds
+                    because its starting to close from the bottom, which is off-screen
+                    */
 
-                /*
-                by clamping the transition to windowHeight we avoid
-                a very awkward effect where a long list doesn't really
-                get any visual closing feedback in the first milliseconds
-                because its starting to close from the bottom, which is off-screen
-                */
-
-                return {content, isActive, isAnimated, contentEstimatedHeight};
-
-            }).then(({content, isActive, isAnimated, contentEstimatedHeight}) => {
-
-                if (isAnimated && 'ontransitionend' in window) {
-
-                    fastdom.write(() => {
-                        container.style.pointerEvents = 'none';
-                        if (!isActive) {
-                            container.classList.toggle('dropdown--active');
-                        }
-                        content.style.height = isActive
-                            ? `${contentEstimatedHeight}px`
-                            : 0;
-                    }).then(()=>{
-                        requestAnimationFrame(() => {
-                            content.style.height = isActive
-                                ? 0
-                                : `${contentEstimatedHeight}px`;
-
-                            bean.one(content, 'transitionend', () => {
-                                fastdom.write(() => {
-                                    updateAria(container);
-                                    content.style.height = 'auto';
-                                    container.style.pointerEvents = 'all';
-                                    if (isActive) {
+                    return {
+                        content,
+                        isActive,
+                        isAnimated,
+                        contentEstimatedHeight,
+                    };
+                })
+                .then(
+                    ({
+                        content,
+                        isActive,
+                        isAnimated,
+                        contentEstimatedHeight,
+                    }) => {
+                        if (isAnimated && 'ontransitionend' in window) {
+                            fastdom
+                                .write(() => {
+                                    container.style.pointerEvents = 'none';
+                                    if (!isActive) {
                                         container.classList.toggle(
                                             'dropdown--active'
                                         );
                                     }
+                                    content.style.height = isActive
+                                        ? `${contentEstimatedHeight}px`
+                                        : 0;
+                                })
+                                .then(() => {
+                                    requestAnimationFrame(() => {
+                                        content.style.height = isActive
+                                            ? 0
+                                            : `${contentEstimatedHeight}px`;
+
+                                        bean.one(
+                                            content,
+                                            'transitionend',
+                                            () => {
+                                                fastdom.write(() => {
+                                                    updateAria(container);
+                                                    content.style.height =
+                                                        'auto';
+                                                    container.style.pointerEvents =
+                                                        'all';
+                                                    if (isActive) {
+                                                        container.classList.toggle(
+                                                            'dropdown--active'
+                                                        );
+                                                    }
+                                                });
+                                            }
+                                        );
+                                    });
                                 });
+                        } else {
+                            fastdom.write(() => {
+                                container.classList.toggle('dropdown--active');
+                                updateAria(container);
                             });
-                        });
-                    });
-                } else {
-                    fastdom.write(() => {
-                        container.classList.toggle('dropdown--active');
-                        updateAria(container);
-                    });
-                }
-            })
+                        }
+                    }
+                );
         }
     });
 };
