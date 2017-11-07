9768ca70d629d4db8e624b80b035c7ce7abc0cf0
Nicolas Long <nicolaslong@gmail.com>
2017-11-07 13:15:19 +0000

    Revert "Remove big long banner test, set new control (#18179)"
    This reverts commit bb96cfc057c7adb5109afb054f6908e959fbb805.

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,4 +1,7 @@
 // @flow
+
+import { AcquisitionsBannerBigLongTwo } from 'common/modules/experiments/tests/acquisitions-banner-big-long-two';
+
 export const membershipEngagementBannerTests: $ReadOnlyArray<
     AcquisitionsABTest
-> = [];
+> = [AcquisitionsBannerBigLongTwo];
