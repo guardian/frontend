74d0fe1d781fcfbd1413b15ca9dd0550602ee60d
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-11-07 16:06:01 +0000

    Revert "Revert "Remove big long banner test, set new control (#18179)"" (#18185)

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-control.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-control.js
@@ -4,7 +4,10 @@ export const acquisitionsBannerControlTemplate = (
 ) =>
     `<div id="site-message__message">
         <div class="site-message__message site-message__message--membership">
-            <span class = "membership__message-text">${params.messageText} ${params.ctaText}</span>
+            <div class="membership__message-text-long">
+                <span class = "membership__message-text">
+                    ${params.messageText} <strong>${params.ctaText}</strong></span>
+            </div>
             <span class="membership__paypal-container">
                 <img class="membership__paypal-logo" src="${params.paypalAndCreditCardImage}" alt="Paypal and credit card">
                 <span class="membership__support-button"><a class="message-button-rounded__cta ${params.colourClass}" href="${params.linkUrl}">${params.buttonCaption}${params.buttonSvg}</a></span>
