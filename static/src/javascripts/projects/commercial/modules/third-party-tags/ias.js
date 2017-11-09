d52edb7b46b6954f5f8a3638bf0c6dbc6dc9159d
Jon Norman <jon.norman@guardian.co.uk>
2017-11-09 16:50:24 +0000

    Only load the ias script in the header when in the test.

--- static/src/javascripts/projects/commercial/modules/third-party-tags/ias.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/ias.js
@@ -1,7 +1,10 @@
 // @flow
 import config from 'lib/config';
+import { getTestVariantId } from 'common/modules/experiments/utils';
 
 export const ias: ThirdPartyTag = {
-    shouldRun: config.switches.abIasAdTargeting,
+    shouldRun:
+        config.get('switches.abIasAdTargeting', false) &&
+        getTestVariantId('IasAdTargetingV2') === 'variant',
     url: '//cdn.adsafeprotected.com/iasPET.1.js',
 };
