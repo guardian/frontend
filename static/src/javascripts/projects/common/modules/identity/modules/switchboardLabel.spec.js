0bd0c9178370e0ac861b7b4a7f2647238acca749
Laura <laura.gonzalez@guardian.co.uk>
2017-11-14 17:06:39 +0000

    test switchboardLabel

---
+++ static/src/javascripts/projects/common/modules/identity/modules/switchboardLabel.spec.js
@@ -0,0 +1,23 @@
+import {addSpinner, removeSpinner} from './switchboardLabel';
+
+beforeEach(()=>{
+    document.body.innerHTML = `
+        <div class="originalClassName"></div>
+    `;
+});
+
+test('adds a spinner', () => {
+    const el = document.querySelector('.originalClassName');
+    addSpinner(el).then(()=>{
+        expect(el.classList.contains('is-updating')).toEqual(true);
+        expect(document.body.classList.contains('is-updating-cursor')).toEqual(true);
+    });
+});
+
+test('removes a spinner', () => {
+    const el = document.querySelector('.originalClassName');
+    addSpinner(el).then(()=>removeSpinner(el)).then(()=>{
+        expect(el.className).toEqual('originalClassName');
+        expect(document.body.classList.contains('is-updating-cursor')).toEqual(false);
+    });
+});
