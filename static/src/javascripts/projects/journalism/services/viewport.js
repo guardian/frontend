ce96a8b702bd98d19c301e952d9f6a41a95bafe4
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-14 12:29:05 +0000

    Remove IO polyfill

--- static/src/javascripts/projects/journalism/services/viewport.js
+++ static/src/javascripts/projects/journalism/services/viewport.js
@@ -1,16 +1,68 @@
 // @flow
-import {
-    observe as observeIO,
-    unobserve as unobserveIO,
-} from './impl/viewport-io';
-import {
-    observe as observeScroll,
-    unobserve as unobserveScroll,
-} from './impl/viewport-scroll';
 
-const viewport: ViewportService =
-    'IntersectionObserver' in window
-        ? { observe: observeIO, unobserve: unobserveIO }
-        : { observe: observeScroll, unobserve: unobserveScroll };
+type ObserverRecord = {
+    observer: window.IntersectionObserver,
+    registry: Array<{ callback: number => void, element: Element }>,
+};
+
+const cache: Map<number, ObserverRecord> = new Map();
+
+const observe = (
+    element: Element,
+    threshold: number,
+    callback: () => void
+): void => {
+    let record = cache.get(threshold);
+    if (!record) {
+        record = {
+            registry: [{ callback, element }],
+            observer: new window.IntersectionObserver(
+                (entries: window.IntersectionObserverEntry[]) => {
+                    const record2 = cache.get(threshold);
+                    if (!record2) return;
+
+                    entries.forEach(entry => {
+                        if (entry.isIntersecting) {
+                            const buddy = record2.registry.find(
+                                ({ element: e }) => e === entry.target
+                            );
+                            if (buddy) {
+                                buddy.callback(entry.intersectionRatio);
+                            }
+                        }
+                    });
+                },
+                { threshold }
+            ),
+        };
+    } else {
+        record.registry.push({ callback, element });
+    }
+    cache.set(threshold, record);
+    record.observer.observe(element);
+};
+
+const unobserve = (element: Element, threshold: number): void => {
+    const record = cache.get(threshold);
+    if (!record) return;
+
+    const buddyIdx = record.registry.findIndex(
+        ({ element: e }) => e === element
+    );
+    if (buddyIdx === -1) return;
+
+    record.registry.splice(buddyIdx, 1);
+
+    if (record.registry.length === 0) {
+        cache.delete(threshold);
+    } else {
+        cache.set(threshold, record);
+    }
+};
+
+const viewport: ViewportService = {
+    observe,
+    unobserve,
+};
 
 export { viewport };
