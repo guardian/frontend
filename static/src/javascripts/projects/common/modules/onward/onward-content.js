e3188d78fb4c3fd18333912ba1b24ef9add9709d
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-11-14 14:46:59 +0000

    ES6: Convert onward/onward-content.spec.js

--- static/src/javascripts/projects/common/modules/onward/onward-content.js
+++ static/src/javascripts/projects/common/modules/onward/onward-content.js
@@ -5,15 +5,12 @@ import mediator from 'lib/mediator';
 import { begin, error, end } from 'common/modules/analytics/register';
 import { Component } from 'common/modules/component';
 
-const getTag = (): string => {
-    const { nonKeywordTagIds, blogIds, seriesId } = config.get('page', {});
-
-    return [
-        ...nonKeywordTagIds.split(','),
-        ...blogIds.split(','),
-        ...[seriesId],
+const getTag = (): string =>
+    [
+        ...config.get('page.nonKeywordTagIds', '').split(','),
+        ...config.get('page.blogIds', '').split(','),
+        ...[config.get('page.seriesId')],
     ].shift();
-};
 
 const getShortUrl = (): string =>
     encodeURIComponent(config.get('page.shortUrl'));
