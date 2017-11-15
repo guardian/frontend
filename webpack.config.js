ddf91cbd7e06060ec537c41179708e6db83844d0
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-15 15:50:35 +0000

    Rename file

--- webpack.config.js
+++ webpack.config.js
@@ -86,7 +86,7 @@ module.exports = {
                 exclude: /(node_modules)/,
                 loader: 'svg-loader',
             },
-            ...require('@guardian/atom-renderer/webpack/css')(path.join(__dirname, 'static', 'src', 'stylesheets', 'atoms.scss')),
+            ...require('@guardian/atom-renderer/webpack/css')(path.join(__dirname, 'static', 'src', 'stylesheets', 'atoms-vars.scss')),
         ],
     },
     plugins: [
