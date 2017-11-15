f388546d9f3a7fe0bd1589921b23820ccd475adf
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-15 11:15:04 +0000

    Move CSS vars into SASS partial

--- webpack.config.js
+++ webpack.config.js
@@ -86,7 +86,7 @@ module.exports = {
                 exclude: /(node_modules)/,
                 loader: 'svg-loader',
             },
-            ...require('@guardian/atom-renderer/webpack/frontend'),
+            ...require('@guardian/atom-renderer/webpack/frontend')(require('src/stylesheets/atoms')),
         ],
     },
     plugins: [
