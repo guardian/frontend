2ba6fd939f24895f0f86ab01f54619d97751e146
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-11-15 15:51:49 +0000

    Cleanup

--- static/src/javascripts/projects/common/modules/async-call-merger.spec.js
+++ static/src/javascripts/projects/common/modules/async-call-merger.spec.js
@@ -1,13 +1,18 @@
 // @flow
 
+import Chance from 'chance';
+
 import { mergeCalls } from './async-call-merger';
+
+const chance = new Chance();
+const RESULT = chance.string();
 
 describe('AsyncCallMerger', () => {
     let targetFn;
     let targetFnWithMerging;
 
     beforeEach(() => {
-        targetFn = jest.fn();
+        targetFn = jest.fn(callback => callback(RESULT));
         targetFnWithMerging = mergeCalls(targetFn);
     });
 
@@ -25,23 +30,7 @@ describe('AsyncCallMerger', () => {
         const callback2 = jest.fn();
         targetFnWithMerging(callback2);
 
-        targetFn.mock.calls[0][0]('target result');
-
-        expect(callback1).toBeCalledWith('target result');
-        expect(callback2).toBeCalledWith('target result');
-    });
-
-    test('should call both callbacks with result of target callback when merged function is called after call to target has completed', () => {
-        const callback1 = jest.fn();
-        targetFnWithMerging(callback1);
-
-        // call callback passed to target fn
-        targetFn.mock.calls[0][0]('target result');
-
-        const callback2 = jest.fn();
-        targetFnWithMerging(callback2);
-
-        expect(callback1).toBeCalledWith('target result');
-        expect(callback2).toBeCalledWith('target result');
+        expect(callback1).toBeCalledWith(RESULT);
+        expect(callback2).toBeCalledWith(RESULT);
     });
 });
