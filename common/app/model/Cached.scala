55b9e3b19f021cc15efa560f7b13372507df15ea
Peter Colley <zvvvvt@gmail.com>
2017-11-17 13:47:52 +0000

    optimise imports

--- common/app/model/Cached.scala
+++ common/app/model/Cached.scala
@@ -3,11 +3,9 @@ package model
 import conf.switches.Switches.LongCacheSwitch
 import org.joda.time.DateTime
 import com.github.nscala_time.time.Implicits._
-import common.GuardianConfiguration
 import conf.Configuration
 import play.api.http.Writeable
 import play.api.mvc._
-
 import scala.math.{max, min}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.Duration
