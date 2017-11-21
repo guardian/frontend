3aa0dcca4d1c81374b19d1ffb65f6999ba6f58d9
Gustav Pursche <gustav.pursche@guardian.co.uk>
2017-11-21 12:28:59 +0000

    Bring back changes from @ghaberis in c8f42bccb4f093c26e16845d8b9f6c4ecc939e6b

--- static/src/javascripts/bootstraps/enhanced/football.js
+++ static/src/javascripts/bootstraps/enhanced/football.js
@@ -272,50 +272,57 @@ const init = () => {
         }
     });
 
+    isFootballStatsPage(() => {
+        $('.js-chart').each(el => {
+            new TableDoughnut().render(el);
+        });
+    });
+
     isLiveClockwatch(() => {
-        const ml = new MatchListLive.MatchListLive(
+        const ml = new MatchListLive(
             'match-day',
             isCompetition() || 'premierleague',
-            config.get('dateFromSlug')()
+            config.dateFromSlug()
         );
         const $img = $('.media-primary');
         const $matchListContainer = $.create(
-            '<div class="football-matches__container" data-link-name="football-matches-clockwatch"></div>'
-        ).css({
-            minHeight: $img[0] ? $img[0].offsetHeight : 0,
-        });
+            `
+            <div class="football-matches__container"
+                  data-link-name="football-matches-clockwatch"></div>
+        `
+        ).css({ minHeight: $img[0] ? $img[0].offsetHeight : 0 });
 
         $img.addClass('u-h');
+
         loading($matchListContainer[0], 'Fetching today’s matches…', {
             text: 'Impatient?',
             href: '/football/live',
         });
 
         $('.js-football-meta').append($matchListContainer);
-        ml
-            .fetch($matchListContainer[0])
-            .fail(() => {
+
+        const handleResponse = success => {
+            if (
+                !success ||
+                $('.football-match', $matchListContainer[0]).length === 0
+            ) {
                 ml.destroy();
                 $matchListContainer.remove();
                 $img.removeClass('u-h');
-            })
-            .always(() => {
-                if ($('.football-match', $matchListContainer[0]).length === 0) {
-                    ml.destroy();
-                    $matchListContainer.remove();
-                    $img.removeClass('u-h');
-                }
-                $matchListContainer.css({
-                    minHeight: 0,
-                });
-                loaded($matchListContainer[0]);
-            });
-    });
+            }
 
-    isFootballStatsPage(() => {
-        $('.js-chart').each(el => {
-            new TableDoughnut().render(el);
-        });
+            $matchListContainer.css({ minHeight: 0 });
+            loaded($matchListContainer[0]);
+        };
+
+        ml
+            .fetch($matchListContainer[0])
+            .then(() => {
+                handleResponse(true);
+            })
+            .catch(() => {
+                handleResponse(false);
+            });
     });
 
     // Binding
