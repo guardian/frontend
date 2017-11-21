fc9f0bbdcac6ec967993a190aed534a0ad917887
Laura <laura.gonzalez@guardian.co.uk>
2017-11-21 13:57:21 +0000

    add css & back button

--- static/src/javascripts/projects/common/modules/identity/wizard.js
+++ static/src/javascripts/projects/common/modules/identity/wizard.js
@@ -2,30 +2,46 @@
 /* eslint-disable no-underscore-dangle, react/sort-comp */
 
 import bean from 'bean';
+import $ from 'lib/$';
 import { Component } from 'common/modules/component';
 
 const stepClassname = 'manage-account-wizard__step';
-const progressButtonClassname = 'manage-account-wizard__controls';
+const nextButtonElClassname = 'js-manage-account-wizard__next';
+const prevButtonClassname = 'js-manage-account-wizard__prev';
 export const containerClassname = 'manage-account-wizard';
 
 export class Wizard extends Component {
     position: number;
     steps: NodeList<HTMLElement>;
-    el: HTMLElement;
+    nextButtonEls: NodeList<HTMLElement>;
+    prevButtonEls: NodeList<HTMLElement>;
 
-    constructor() {
+    constructor(): void {
         super();
+        this.useBem = true;
         this.componentClass = containerClassname;
     }
 
-    updateCounter() {
-        this.getElem('controls-pager').html(`${this.position} / ${this.steps.length}`);
+    updateCounter(): void {
+        if (this.position >= this.steps.length - 1) {
+            this.setState('completed');
+        } else {
+            this.removeState('completed');
+        }
+
+        const pagerEl = this.getElem('controls-pager');
+        if (pagerEl) {
+            pagerEl.innerText = `${this.position + 1} / ${this.steps.length}`;
+        }
     }
 
-    setPosition(positionAt: number) {
+    setPosition(positionAt: number): void {
         this.steps.forEach(stepEl => {
             stepEl.style.display = 'none';
         });
+        if (positionAt < 0) {
+            return this.setPosition(0);
+        }
         if (this.steps[positionAt]) {
             this.position = positionAt;
             this.steps[positionAt].style.display = 'block';
@@ -36,19 +52,29 @@ export class Wizard extends Component {
         }
     }
 
-    prerender() {
+    prerender(): void {
         super.prerender();
 
-        this.steps = this.elem.querySelectorAll(`.${stepClassname}`);
-        this.nextButtonEl = this.elem.querySelector(
-            `.${progressButtonClassname}`
-        );
+        if (!this.elems) this.elems = {};
+
+        this.steps = $(`.${stepClassname}`, this.elem);
+        this.nextButtonEls = $(`.${nextButtonElClassname}`, this.elem);
+        this.prevButtonEls = $(`.${prevButtonClassname}`, this.elem);
     }
 
-    ready() {
+    ready(): void {
         this.setPosition(0);
-        bean.on(this.nextButtonEl, 'click', () => {
-            this.setPosition(this.position + 1);
-        });
+        if (this.nextButtonEls)
+            this.nextButtonEls.forEach(elem => {
+                bean.on(elem, 'click', () => {
+                    this.setPosition(this.position + 1);
+                });
+            });
+        if (this.prevButtonEls)
+            this.prevButtonEls.forEach(elem => {
+                bean.on(elem, 'click', () => {
+                    this.setPosition(this.position - 1);
+                });
+            });
     }
 }
