d87747befba4a4a7a0c8992e1554450cdacce371
Laura <laura.gonzalez@guardian.co.uk>
2017-11-27 12:10:14 +0000

    improve type checking on loadEnhancers

--- static/src/javascripts/projects/common/modules/identity/modules/loadEnhancers.js
+++ static/src/javascripts/projects/common/modules/identity/modules/loadEnhancers.js
@@ -3,18 +3,22 @@
 import { _ as robust } from 'lib/robust';
 import fastdom from 'lib/fastdom-promise';
 
-const loadEnhancers = (loaders: Array<any>): void => {
-    loaders.forEach(([classname: string, action: Function]) =>
-        fastdom
+/* TODO:Improve type checking here */
+
+const loadEnhancers = (loaders: Array<Array<any>>): void => {
+    loaders.forEach(([classname: string, action: (x: HTMLElement) => void]) => {
+        if (typeof classname !== 'string') throw new Error('Invalid classname');
+        if (typeof action !== 'function') throw new Error('Invalid action');
+        return fastdom
             .read(() => [...document.querySelectorAll(classname)])
             .then(elements =>
-                elements.forEach((element: any) => {
+                elements.forEach((element: HTMLElement) => {
                     robust.catchAndLogError(classname, () => {
                         action(element);
                     });
                 })
-            )
-    );
+            );
+    });
 };
 
 export default loadEnhancers;
