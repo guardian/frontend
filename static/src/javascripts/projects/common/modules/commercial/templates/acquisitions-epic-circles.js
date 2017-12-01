5a7ae535d36fd25e4e755bdec164789db362d054
James Byers <Ap0c@users.noreply.github.com>
2017-12-01 18:24:37 +0000

    Added new stylesheet for the test, and began tweaking the look.

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-circles.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-circles.js
@@ -5,15 +5,13 @@ export const acquisitionsEpicCirclesTemplate = ({
     componentName,
     buttonTemplate,
     testimonialBlock = '',
-    epicClass = '',
 }: {
     copy: AcquisitionsEpicTemplateCopy,
     componentName: string,
     buttonTemplate: string,
     testimonialBlock?: string,
-    epicClass?: string,
 }) =>
-    `<div class="contributions__epic ${epicClass}" data-component="${
+    `<div class="contributions__epic contributions__epic--circles" data-component="${
         componentName
     }">
         <h2 class="contributions__title contributions__title--epic">
@@ -22,7 +20,8 @@ export const acquisitionsEpicCirclesTemplate = ({
         <p class="contributions__paragraph contributions__paragraph--epic">
             ${p1}
         </p>
-        <h3 class="contributions__epic-heading">we haven't put up a paywall</h3>
+        <h3 class="contributions__epic-heading">we haven't put up</h3>
+        <h3 class="contributions__epic-heading">a paywall</h3>
         ${testimonialBlock}
         <p class="contributions__paragraph contributions__paragraph--epic">
             ${p2}
