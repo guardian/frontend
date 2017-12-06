87ced5d50fc426ae5da0ed9b7212e0241c82d54a
Peter Colley <zvvvvt@gmail.com>
2017-12-06 16:50:15 +0000

    remove breaking import

--- common/app/common/configuration.scala
+++ common/app/common/configuration.scala
@@ -7,7 +7,6 @@ import java.util.Map.Entry
 import com.amazonaws.AmazonClientException
 import com.amazonaws.auth._
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.gu.cm.ClassPathConfigurationSource
 import com.typesafe.config.{ConfigException, ConfigFactory}
 import common.Environment.{app, awsRegion, stage}
 import conf.switches.Switches
