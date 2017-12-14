3f9698a024b805de0cdbf6f88506be3aaa8c19f9
joshua-david <joshrobson4@gmail.com>
2017-12-14 17:16:28 +0000

    Merge branch 'master' of https://github.com/guardian/frontend into jdr-garnett-media

--- static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/commercial/membership-engagement-banner.js
@@ -19,7 +19,7 @@ import {
 import { acquisitionsBannerControlTemplate } from 'common/modules/commercial/templates/acquisitions-banner-control';
 
 // change messageCode to force redisplay of the message to users who already closed it.
-const messageCode = 'engagement-banner-2017-11-24';
+const messageCode = 'engagement-banner-2017-12-14';
 
 const getUserTest = (): ?AcquisitionsABTest =>
     membershipEngagementBannerTests.find(
