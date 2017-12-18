54f5551da67118db99d4ff297118b1e357227da2
Laura <laura.gonzalez@guardian.co.uk>
2017-12-18 18:06:30 +0000

    Revert "migrate /{edit-profile}"
    This reverts commit dca50fae9c317c19a7cff44c15e47ae6421ca143.

--- identity/app/controllers/EditProfileController.scala
+++ identity/app/controllers/EditProfileController.scala
@@ -258,22 +258,18 @@ class EditProfileController(
 
     newsletterService.subscriptions(request.user.getId, idRequestParser(request).trackingData).map { emailFilledForm =>
 
-      NoCache(Ok(
-        IdentityHtmlPage.html(
-          content = views.html.profileForms(
-            page.metadata.id,
-            user,
-            forms,
-            idRequestParser(request),
-            idUrlBuilder,
-            emailFilledForm,
-            newsletterService.getEmailSubscriptions(emailFilledForm),
-            EmailNewsletters.all,
-            consentsUpdated,
-            consentHint
-          )
-        )(page, request, context)
-      ))
+      NoCache(Ok(views.html.profileForms(
+        page,
+        user,
+        forms,
+        idRequestParser(request),
+        idUrlBuilder,
+        emailFilledForm,
+        newsletterService.getEmailSubscriptions(emailFilledForm),
+        EmailNewsletters.all,
+        consentsUpdated,
+        consentHint
+      )))
 
     }
   }
