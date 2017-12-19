54c57ab3a7568df2a2347c325a1fa4d58f796571
Laura <laura.gonzalez@guardian.co.uk>
2017-12-19 11:12:25 +0000

    fix indent

--- identity/app/views/support/fragment/ConsentChannel.scala
+++ identity/app/views/support/fragment/ConsentChannel.scala
@@ -1,33 +1,37 @@
 package views.support.fragment
 
+import play.twirl.api.Html
+import views.support.fragment.Switch.ConsentSwitch
+import com.gu.identity.model.User
+
 // Helper for consentJourney.scala.html
 
 object ConsentChannel {
-    sealed trait ConsentChannelBehaviour
-    case object TextConsentChannel extends ConsentChannelBehaviour
-    case object PhoneConsentChannel extends ConsentChannelBehaviour
-    case object PostConsentChannel extends ConsentChannelBehaviour
+  sealed trait ConsentChannelBehaviour
+  case object TextConsentChannel extends ConsentChannelBehaviour
+  case object PhoneConsentChannel extends ConsentChannelBehaviour
+  case object PostConsentChannel extends ConsentChannelBehaviour
 
-    def checkboxName(behaviour: ConsentChannelBehaviour): String = {
-        behaviour match {
-            case TextConsentChannel => "Text"
-            case PhoneConsentChannel => "Phone"
-            case PostConsentChannel => "Post"
-        }
+  def checkboxName(behaviour: ConsentChannelBehaviour): String = {
+    behaviour match {
+      case TextConsentChannel => "Text"
+      case PhoneConsentChannel => "Phone"
+      case PostConsentChannel => "Post"
     }
+  }
 
-    def communicationChannelsForUser(user: com.gu.identity.model.User): List[ConsentChannelBehaviour] = {
-        val telephoneDefined = user.privateFields.telephoneNumber.isDefined
-        val postcodeDefined = user.privateFields.postcode.isDefined
+  def communicationChannelsForUser(user: User): List[ConsentChannelBehaviour] = {
+    val telephoneDefined = user.privateFields.telephoneNumber.isDefined
+    val postcodeDefined = user.privateFields.postcode.isDefined
 
-        val channels = List(
-            (TextConsentChannel -> telephoneDefined),
-            (PhoneConsentChannel -> telephoneDefined),
-            (PostConsentChannel -> postcodeDefined)
-        )
+    val channels = List(
+      (TextConsentChannel -> telephoneDefined),
+      (PhoneConsentChannel -> telephoneDefined),
+      (PostConsentChannel -> postcodeDefined)
+    )
 
-        channels.filter(_._2).map(_._1)
-    }
+    channels.filter(_._2).map(_._1)
+  }
 
 }
 
