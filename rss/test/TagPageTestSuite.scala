60817474716aaab3fc46864cf10ef2fd8d30e1c4
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-12-19 16:06:39 +0000

    add missing files

---
+++ rss/test/TagPageTestSuite.scala
@@ -0,0 +1,22 @@
+import org.scalatest.Suites
+import test.SingleServerSuite
+import controllers._
+import integrations._
+import services.{TagPageGroupingTest, TagPageTest}
+
+class TagPageTestSuite extends Suites (
+  new AllTagControllerTest,
+  new AllTagTemplateTest,
+  new CombinerControllerTest,
+  new CombinerFeatureTest,
+  new TagControllerTest,
+  new TagMetaDataTest,
+  new LatestTagControllerTest,
+  new TagPageTest,
+  new TagPageGroupingTest,
+  new TagFeatureTest,
+  new TagTemplateTest,
+) with SingleServerSuite {
+  override lazy val port: Int = 19003
+}
+
