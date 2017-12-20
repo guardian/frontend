df406cee66f5349d2a050f32e400f4d984722ac0
Joseph Smith <joseph.smith@guardian.co.uk>
2017-12-20 18:40:30 +0000

    Remove split CTA referrer test

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -146,13 +146,4 @@ trait ABTestSwitches {
     exposeClientSide = true
   )
 
-  Switch(
-    ABTests,
-    "ab-acquisitions-epic-split-cta-referrer",
-    "Test a split cta, and changing the product set for people who click on the epic",
-    owners = Seq(Owner.withGithub("jranks123")),
-    safeState = Off,
-    sellByDate = new LocalDate(2018, 1, 10),
-    exposeClientSide = true
-  )
 }
