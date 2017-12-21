0a7087bdc9b8d629e3d04face1fb814d28230707
Laura <laura.gonzalez@guardian.co.uk>
2017-12-21 12:16:16 +0000

    next arrow scrolls down when there's more content

--- static/src/javascripts/projects/common/modules/identity/consent-wizard.js
+++ static/src/javascripts/projects/common/modules/identity/consent-wizard.js
@@ -3,6 +3,9 @@
 import fastdom from 'lib/fastdom-promise';
 
 import loadEnhancers from './modules/loadEnhancers';
+import mediator from 'lib/mediator';
+import debounce from 'lodash/functions/debounce';
+import { scrollTo } from 'lib/scroller';
 import { newsletterCheckboxClassName } from './consents';
 import {
     wizardPageChangedEv,
@@ -112,6 +115,29 @@ const bindEmailConsentCounterToWizard = (wizardEl: HTMLElement): void => {
     });
 };
 
+const bindScrollForNextButton = (buttonEl: HTMLElement): void => {
+    const check = () =>
+        shouldNextButtonScroll().then(shouldIt =>
+            fastdom.write(() => {
+                buttonEl.classList.toggle(
+                    'manage-account__button--icon--rotate-down',
+                    shouldIt
+                );
+            })
+        );
+
+    mediator.on('window:throttledScroll', debounce(check, 100));
+
+    check();
+};
+
+const shouldNextButtonScroll = (): Promise<boolean> =>
+    fastdom.read(
+        () =>
+            window.scrollY + window.innerHeight + 100 <
+            document.body.offsetHeight
+    );
+
 const bindNextButton = (buttonEl: HTMLElement): void => {
     const wizardEl: ?Element = buttonEl.closest(
         '.manage-account-wizard--consent'
@@ -119,9 +145,19 @@ const bindNextButton = (buttonEl: HTMLElement): void => {
     if (wizardEl && wizardEl instanceof HTMLElement) {
         buttonEl.addEventListener('click', (ev: Event) => {
             ev.preventDefault();
-            getWizardInfoObject(wizardEl).then(wizardInfo =>
-                setPosition(wizardEl, wizardInfo.position + 1)
-            );
+            shouldNextButtonScroll().then(shouldIt => {
+                if (shouldIt) {
+                    scrollTo(
+                        window.scrollY + window.innerHeight * 0.75,
+                        250,
+                        'linear'
+                    );
+                } else {
+                    getWizardInfoObject(wizardEl).then(wizardInfo =>
+                        setPosition(wizardEl, wizardInfo.position + 1)
+                    );
+                }
+            });
         });
     } else {
         throw new Error(ERR_IDENTITY_CONSENT_WIZARD_MISSING);
@@ -159,6 +195,7 @@ const enhanceConsentWizard = (): void => {
         ['.manage-account-consent-wizard-counter', createEmailConsentCounter],
         ['.manage-account-wizard--consent', bindEmailConsentCounterToWizard],
         ['.js-manage-account-consent-wizard__next', bindNextButton],
+        ['.js-manage-account-consent-wizard__next', bindScrollForNextButton],
     ];
     loadEnhancers(loaders);
 };
