d2ad26542cf330cb245109adab27151f3d6437f9
jranks123 <jonathan.rankin@guardian.co.uk>
2017-12-21 09:52:53 +0000

    remove scala switches

--- static/src/javascripts/projects/common/modules/commercial/support-utilities.js
+++ static/src/javascripts/projects/common/modules/commercial/support-utilities.js
@@ -4,8 +4,10 @@ import { getSync as geolocationGetSync } from 'lib/geolocation';
 const useSupportDomain = (): boolean =>
     geolocationGetSync() === 'GB' || geolocationGetSync() === 'US';
 
+const supportPath = geolocationGetSync() === 'US' ? '/us/contribute' : '/uk';
+
 const supportBaseURL = useSupportDomain()
-    ? 'https://support.theguardian.com'
+    ? `https://support.theguardian.com${supportPath}`
     : 'https://membership.theguardian.com/supporter';
 
 export { useSupportDomain, supportBaseURL };
