221e121795b7b2536fa271c825d423040dfc80f6
GHaberis <george.haberis@theguardian.com>
2017-12-22 15:53:32 +0000

    update capi client to 11.46

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val identityLibVersion = "3.99"
   val awsVersion = "1.11.240"
   val faciaVersion = "2.5.0"
-  val capiVersion = "11.43"
+  val capiVersion = "11.46"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
