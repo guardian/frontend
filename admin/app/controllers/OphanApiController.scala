8a60a70a9d592dfdfdfe5748773da33ba3144f86
Peter Colley <zvvvvt@gmail.com>
2017-12-22 16:01:30 +0000

    put miscellaneous endpoints behind switches

--- admin/app/controllers/OphanApiController.scala
+++ admin/app/controllers/OphanApiController.scala
@@ -1,22 +1,30 @@
 package controllers.admin
 
 import common.ImplicitControllerExecutionContext
+import conf.switches.Switches
+import controllers.AdminAudit
 import play.api.mvc._
 import services.OphanApi
 import model.NoCache
 
 class OphanApiController(ophanApi: OphanApi, val controllerComponents: ControllerComponents) extends BaseController with ImplicitControllerExecutionContext {
 
-  def pageViews(path: String): Action[AnyContent] = Action.async { request =>
-    ophanApi.getBreakdown(path) map (body => NoCache(Ok(body) as "application/json"))
+  def pageViews(path: String): Action[AnyContent] = Action.async { implicit request =>
+    AdminAudit.endpointAuditF(Switches.AdminRemoveOphan) {
+      ophanApi.getBreakdown(path) map (body => NoCache(Ok(body) as "application/json"))
+    }
   }
 
-  def platformPageViews: Action[AnyContent] = Action.async { request =>
-    ophanApi.getBreakdown(platform = "next-gen", hours = 2) map (body => NoCache(Ok(body) as "application/json"))
+  def platformPageViews: Action[AnyContent] = Action.async { implicit request =>
+    AdminAudit.endpointAuditF(Switches.AdminRemoveOphan) {
+      ophanApi.getBreakdown(platform = "next-gen", hours = 2) map (body => NoCache(Ok(body) as "application/json"))
+    }
   }
 
-  def adsRenderTime: Action[AnyContent] = Action.async { request =>
-    ophanApi.getAdsRenderTime(request.queryString) map (body => NoCache(Ok(body) as "application/json"))
+  def adsRenderTime: Action[AnyContent] = Action.async { implicit request =>
+    AdminAudit.endpointAuditF(Switches.AdminRemoveOphan) {
+      ophanApi.getAdsRenderTime(request.queryString) map (body => NoCache(Ok(body) as "application/json"))
+    }
   }
 
 }
--- admin/app/controllers/admin/AnalyticsConfidenceController.scala
+++ admin/app/controllers/admin/AnalyticsConfidenceController.scala
@@ -1,6 +1,8 @@
 package controllers.admin
 
 import common.{ImplicitControllerExecutionContext, Logging}
+import conf.switches.Switches
+import controllers.AdminAudit
 import model.ApplicationContext
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import tools._
@@ -8,26 +10,28 @@ import tools._
 class AnalyticsConfidenceController(val controllerComponents: ControllerComponents)(implicit context: ApplicationContext)
   extends BaseController with Logging with ImplicitControllerExecutionContext {
   def renderConfidence(): Action[AnyContent] = Action.async { implicit request =>
-    for {
-      ophan <- CloudWatch.ophanConfidence()
-      google <- CloudWatch.googleConfidence()
-    } yield {
-      val ophanAverage = ophan.dataset.flatMap(_.values.headOption).sum / ophan.dataset.length
-      val googleAverage = google.dataset.flatMap(_.values.headOption).sum / google.dataset.length
+    AdminAudit.endpointAuditF(Switches.AdminRemoveAnalyticsConfidence) {
+      for {
+        ophan <- CloudWatch.ophanConfidence()
+        google <- CloudWatch.googleConfidence()
+      } yield {
+        val ophanAverage = ophan.dataset.flatMap(_.values.headOption).sum / ophan.dataset.length
+        val googleAverage = google.dataset.flatMap(_.values.headOption).sum / google.dataset.length
 
-      val ophanGraph = new AwsLineChart("Ophan confidence", Seq("Time", "%", "avg."), ChartFormat(Colour.`tone-comment-1`, Colour.success)) {
-        override lazy val dataset = ophan.dataset.map{ point =>
-          point.copy(values =  point.values :+ ophanAverage)
+        val ophanGraph = new AwsLineChart("Ophan confidence", Seq("Time", "%", "avg."), ChartFormat(Colour.`tone-comment-1`, Colour.success)) {
+          override lazy val dataset = ophan.dataset.map { point =>
+            point.copy(values = point.values :+ ophanAverage)
+          }
         }
-      }
 
-      val googleGraph = new AwsLineChart("Google confidence", Seq("Time", "%", "avg."), ChartFormat(Colour.`tone-comment-1`, Colour.success)) {
-        override lazy val dataset = google.dataset.map{ point =>
-          point.copy(values =  point.values :+ googleAverage)
+        val googleGraph = new AwsLineChart("Google confidence", Seq("Time", "%", "avg."), ChartFormat(Colour.`tone-comment-1`, Colour.success)) {
+          override lazy val dataset = google.dataset.map { point =>
+            point.copy(values = point.values :+ googleAverage)
+          }
         }
-      }
 
-      Ok(views.html.lineCharts(Seq(ophanGraph, googleGraph)))
+        Ok(views.html.lineCharts(Seq(ophanGraph, googleGraph)))
+      }
     }
   }
 }
