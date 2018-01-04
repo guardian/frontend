2d814c63cae668d512d3f694a64294b1ecf65087
Laura <laura.gonzalez@guardian.co.uk>
2018-01-04 14:09:13 +0000

    remove auto scroller

--- static/src/javascripts/projects/common/modules/identity/consent-wizard.js
+++ static/src/javascripts/projects/common/modules/identity/consent-wizard.js
@@ -114,30 +114,6 @@ const bindEmailConsentCounterToWizard = (wizardEl: HTMLElement): void => {
     });
 };
 
-const shouldNextButtonScroll = (): Promise<boolean> =>
-    fastdom.read(
-        () =>
-            window.scrollY + window.innerHeight + 100 <
-            (document.body ? document.body.clientHeight : 0)
-    );
-
-const bindScrollForNextButton = (buttonEl: HTMLElement): void => {
-    const check = () =>
-        shouldNextButtonScroll().then(shouldIt =>
-            fastdom.write(() => {
-                buttonEl.classList.toggle(
-                    'manage-account__button--icon--rotate-down',
-                    shouldIt
-                );
-            })
-        );
-
-    mediator.on('window:throttledScroll', debounce(check, 100));
-    window.addEventListener(wizardPageChangedEv, () => {
-        check();
-    });
-};
-
 const showWizard = (wizardEl: HTMLElement): Promise<void> =>
     fastdom.write(() => wizardEl.classList.remove('u-h'));
 
@@ -146,19 +122,9 @@ const bindNextButton = (buttonEl: HTMLElement): void => {
     if (wizardEl && wizardEl instanceof HTMLElement) {
         buttonEl.addEventListener('click', (ev: Event) => {
             ev.preventDefault();
-            shouldNextButtonScroll().then(shouldIt => {
-                if (shouldIt) {
-                    scrollTo(
-                        window.scrollY + window.innerHeight * 0.75,
-                        250,
-                        'linear'
-                    );
-                } else {
-                    getWizardInfoObject(wizardEl).then(wizardInfo =>
-                        setPosition(wizardEl, wizardInfo.position + 1)
-                    );
-                }
-            });
+            getWizardInfoObject(wizardEl).then(wizardInfo =>
+                setPosition(wizardEl, wizardInfo.position + 1)
+            );
         });
     } else {
         throw new Error(ERR_IDENTITY_CONSENT_WIZARD_MISSING);
@@ -195,7 +161,6 @@ const enhanceConsentWizard = (): void => {
         ['.identity-wizard--consent', bindEmailConsentCounterToWizard],
         ['.identity-wizard--consent', showWizard],
         ['.js-identity-consent-wizard__next', bindNextButton],
-        ['.js-identity-consent-wizard__next', bindScrollForNextButton],
     ];
     loadEnhancers(loaders);
 };
