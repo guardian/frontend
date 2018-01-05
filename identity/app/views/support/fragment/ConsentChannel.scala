0fb54caa5165e8a26e020300ac12fe3b0fa9e1be
Mario Galic <mario.galic@guardian.co.uk>
2018-01-05 14:04:31 +0000

    Factor out channelsIds

--- identity/app/views/support/fragment/ConsentChannel.scala
+++ identity/app/views/support/fragment/ConsentChannel.scala
@@ -4,11 +4,16 @@ import com.gu.identity.model.User
 import play.api.data.Field
 
 object ConsentChannel {
-  sealed abstract class ConsentChannelBehaviour(val channelName: String)
-
+  sealed abstract class ConsentChannelBehaviour(val id: String)
   case object TextConsentChannel  extends ConsentChannelBehaviour("sms")
   case object PhoneConsentChannel extends ConsentChannelBehaviour("phone")
   case object PostConsentChannel  extends ConsentChannelBehaviour("post")
+
+  private val channelsIds = List(
+    TextConsentChannel.id,
+    PhoneConsentChannel.id,
+    PostConsentChannel.id
+  )
 
   def channelsProvidedBy(user: User): List[ConsentChannelBehaviour] = {
     val telephoneDefined = user.privateFields.telephoneNumber.isDefined
@@ -26,14 +31,12 @@ object ConsentChannel {
   /** Field is a channel and user has provided details for this channel */
   def isUsersChannel(consentField: Field, user: User): Boolean = {
       consentField("id").value.exists { id =>
-        List("phone", "sms", "post").contains(id) && channelsProvidedBy(user).exists(_.channelName == id)
+        channelsIds.contains(id) && channelsProvidedBy(user).exists(_.id == id)
       }
   }
 
   def isChannel(consentField: Field): Boolean = {
-    consentField("id").value.exists { id =>
-      List("phone", "sms", "post").contains(id)
-    }
+    consentField("id").value.exists { id => channelsIds.contains(id) }
   }
 
 }
