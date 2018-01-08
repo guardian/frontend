1680a1c88b6e0ef8a1728270b777d777ce725670
Richard Nguyen <richard.nguyen@guardian.co.uk>
2018-01-08 10:14:15 +0000

    Fix the scheduling

--- admin/app/jobs/CommercialDfpReporting.scala
+++ admin/app/jobs/CommercialDfpReporting.scala
@@ -12,7 +12,7 @@ object CommercialDfpReporting {
 
   case class DfpReportRow(value: String)
 
-  private val dfpReports = Box[Map[Long, Seq[DfpReportRow]](Map.empty)
+  private val dfpReports = Box[Map[Long, Seq[DfpReportRow]]](Map.empty)
 
   val teamKPIReport = "All ab-test impressions and CPM"
   // These IDs correspond to queries saved in DFP's web console.
@@ -21,7 +21,7 @@ object CommercialDfpReporting {
   )
 
 
-  def update(dfpApi: DfpApi, executionContext: ExecutionContext): Unit = {
+  def update(dfpApi: DfpApi)(implicit executionContext: ExecutionContext): Future[Unit] = Future {
     for {
       (reportName, reportId) <- reportMappings.toSeq
     } {
@@ -55,11 +55,12 @@ class CommercialDfpReportingLifecycle(
   override def start(): Unit = {
     jobs.deschedule("CommercialDfpReportingJob")
 
+    CommercialDfpReporting.update(dfpApi)(ec)
+
     // 30 minutes between each log write.
     jobs.scheduleEveryNMinutes("CommercialDfpReportingJob", 30) {
       log.logger.info(s"Fetching commercial dfp report from dfp api")
-      CommercialDfpReporting.update(dfpApi, ec)
-      log.logger.info(s"Fetched commercial dfp report.")
+      CommercialDfpReporting.update(dfpApi)(ec)
     }
   }
 
