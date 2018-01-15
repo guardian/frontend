fef90954d3ac448f766115ebe4ecb1d4d1236ad2
GHaberis <george.haberis@theguardian.com>
2018-01-15 14:47:55 +0000

    remove redundant code

--- common/app/templates/inlineJS/blocking/applyRenderConditions.scala.js
+++ common/app/templates/inlineJS/blocking/applyRenderConditions.scala.js
@@ -92,24 +92,6 @@
         return getCookieValue('gu_paying_member') === 'true';
     }
 
-    function get_browser() {
-        var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
-        if(/trident/i.test(M[1])){
-            tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-            return {name:'IE',version:(tem[1]||'')};
-            }   
-        if(M[1]==='Chrome'){
-            tem=ua.match(/\bOPR|Edge\/(\d+)/)
-            if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-            }   
-        M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-        if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
-        return {
-          name: M[0],
-          version: M[1]
-        };
-     }
-
     function supportsPercentagePadding() {
         var firefoxMatch = navigator.userAgent.match(/Firefox\/([0-9]+)\./) || [];
 
