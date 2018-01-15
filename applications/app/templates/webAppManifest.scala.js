9449e48001db4ae197a8ba7f3dec66dbf6c74b20
Zef <zef.cherry-kynaston.casual@guardian.co.uk>
2018-01-15 06:07:15 +0000

    Chrome header colours

--- applications/app/templates/webAppManifest.scala.js
+++ applications/app/templates/webAppManifest.scala.js
@@ -23,8 +23,8 @@
             "src": "@{JavaScript(Static("images/favicons-garnett/roundel-114x114.png"))}"
         }
     ],
-        "theme_color": "#005689",
-        "background_color": "#ffffff",
+        "theme_color": "#e7edef",
+        "background_color": "#121212",
         "start_url": "/uk?INTCMP=webapp",
         "display": "browser",
         "orientation": "portrait",
