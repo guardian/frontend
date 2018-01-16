36123d7b7d84680aa8b056f51626055f403b8e1d
Joseph Smith <joseph.smith@guardian.co.uk>
2018-01-16 16:57:05 +0000

    Re-instate US Gun Campaign with new expiry

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -86,4 +86,14 @@ trait ABTestSwitches {
     exposeClientSide = true
   )
 
+  Switch(
+    ABTests,
+    "ab-acquisitions-us-gun-campaign-2017",
+    "Show a custom Epic for articles with the US gun campaign tag",
+    owners = Seq(Owner.withGithub("Mullefa")),
+    safeState = On,
+    sellByDate = new LocalDate(2018, 3, 1),
+    exposeClientSide = true
+  )
+
 }
--- static/src/javascripts/projects/common/modules/experiments/acquisition-test-selector.js
+++ static/src/javascripts/projects/common/modules/experiments/acquisition-test-selector.js
@@ -11,11 +11,13 @@ import { acquisitionsEpicLiveblog } from 'common/modules/experiments/tests/acqui
 import { acquisitionsEpicAlwaysAskIfTagged } from 'common/modules/experiments/tests/acquisitions-epic-always-ask-if-tagged';
 import { acquisitionsEpicThankYou } from 'common/modules/experiments/tests/acquisitions-epic-thank-you';
 import { colourTestEpicHoldback } from 'common/modules/experiments/tests/circles-epic-holdback';
+import { acquisitionsEpicUSGunCampaign } from 'common/modules/experiments/tests/acquisitions-epic-us-gun-campaign';
 
 /**
  * acquisition tests in priority order (highest to lowest)
  */
 const tests: $ReadOnlyArray<AcquisitionsABTest> = [
+    acquisitionsEpicUSGunCampaign,
     colourTestEpicHoldback,
     askFourEarning,
     acquisitionsEpicAlwaysAskIfTagged,
