21d99b8f8c9fff9272bc4ff962be6385b1fcc63b
Mario Galic <mario.galic@guardian.co.uk>
2018-01-17 12:49:59 +0000

    Initial commit

--- identity/app/services/RedirectDecisionService.scala
+++ identity/app/services/RedirectDecisionService.scala
@@ -5,58 +5,70 @@ import play.api.mvc.{ControllerComponents, RequestHeader}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-abstract class RedirectAccess {
-  private[services] def shouldRedirectOnUrl(pageId: String):Boolean = false
+sealed abstract class RedirectAccess {
+  def shouldRedirectOnUrl(pageId: String): Boolean = false
 }
 
 case object RedirectAccessEmailPrefs extends RedirectAccess {
-  override def shouldRedirectOnUrl(pageId: String):Boolean = pageId contains "email-prefs"
+  override def shouldRedirectOnUrl(pageId: String): Boolean = pageId contains "email-prefs"
 }
 
 case object RedirectAccessAllPages extends RedirectAccess {
-  override def shouldRedirectOnUrl(pageId: String):Boolean = true
+  override def shouldRedirectOnUrl(pageId: String): Boolean = true
 }
 
 
-abstract class RedirectDecision(val url: String, protected val redirectAccess: RedirectAccess = RedirectAccessAllPages){
-  def shouldRedirectOnUrl(pageId: String):Boolean = redirectAccess.shouldRedirectOnUrl(pageId)
+
+
+
+sealed abstract class RedirectDecision(
+    val url: String,
+    val redirectAccess: RedirectAccess = RedirectAccessAllPages) {
+
+  def shouldRedirectOnUrl(pageId: String): Boolean = redirectAccess.shouldRedirectOnUrl(pageId)
 }
 
 case object RedirectToEmailValidation extends RedirectDecision(
-  url = "/verify-email?isRepermissioningRedirect=true",
-  redirectAccess = RedirectAccessEmailPrefs
+  "/verify-email?isRepermissioningRedirect=true",
+  RedirectAccessEmailPrefs
 )
 
 case object RedirectToEmailValidationStrictly extends RedirectDecision(
-  url = "/verify-email?isRepermissioningRedirect=true",
-  redirectAccess = RedirectAccessAllPages
+  "/verify-email?isRepermissioningRedirect=true",
+  RedirectAccessAllPages
 )
 
 case object RedirectToConsents extends RedirectDecision(
-  url = "/consents",
-  redirectAccess = RedirectAccessEmailPrefs
+  "/consents",
+  RedirectAccessEmailPrefs
 )
 
 case object RedirectToNewsletterConsents extends RedirectDecision(
-  url = "/consents/newsletters",
-  redirectAccess = RedirectAccessEmailPrefs
+  "/consents/newsletters",
+  RedirectAccessEmailPrefs
 )
 
+
+/**
+  * Where users should be redirected to depends on two factors:
+  *   1. has user validated the email address
+  *   2. has user re-permissioned before
+  *
+  * @param newsletterService
+  * @param idRequestParser
+  * @param controllerComponents
+  */
 class RedirectDecisionService(
-                           newsletterService: NewsletterService,
-                           idRequestParser: IdRequestParser,
-                           controllerComponents: ControllerComponents
-                         ) {
+     newsletterService: NewsletterService,
+     idRequestParser: IdRequestParser,
+     controllerComponents: ControllerComponents) {
 
   private implicit lazy val ec: ExecutionContext = controllerComponents.executionContext
 
   def decideManageAccountRedirect[A](user: User, request: RequestHeader): Future[Option[RedirectDecision]] = {
 
-    def userHasRepermissioned: Boolean =
-      user.statusFields.hasRepermissioned.contains(true)
-
-    def userEmailValidated: Boolean =
-      user.statusFields.isUserEmailValidated
+    def userHasRepermissioned: Boolean = user.statusFields.hasRepermissioned.contains(true)
+    def userEmailValidated: Boolean = user.statusFields.isUserEmailValidated
 
     (userEmailValidated, userHasRepermissioned) match {
       case (false, false) =>
