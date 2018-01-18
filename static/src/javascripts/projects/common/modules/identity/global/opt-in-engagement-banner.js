a2989c9ff9313320bad90de1a806bb5f9a2361ac
Laura <laura.gonzalez@guardian.co.uk>
2018-01-18 14:40:39 +0000

    clean up js

--- static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
@@ -3,13 +3,31 @@
 import { getUserFromApi } from 'common/modules/identity/api';
 import { Message } from 'common/modules/ui/message';
 
-const messageCode = 'optin-campaign-jan-18';
+const messageCode: string = 'optin-campaign-jan-18';
+const medium: string = new URL(window.location.href).searchParams.get(
+    'utm_medium'
+);
+
+type ApiUser = {
+    statusFields: {
+        hasRepermissioned: Boolean,
+    },
+};
+
+const shouldDisplayOptInBanner = (): Promise<void> =>
+    new Promise((accept, reject) => {
+        if (medium === null || medium.toLowerCase() !== 'email')
+            return reject();
+        getUserFromApi((user: ApiUser) => {
+            if (user === null || !user.statusFields.hasRepermissioned) accept();
+            else reject();
+        });
+    });
 
 export const optInEngagementBannerInit = (): void => {
-    getUserFromApi(user => {
-        console.log(user);
-        if(!user.statusFields.hasRepermissioned) {
-            new Message(messageCode).show('You have to update your email settings. Visit My Account to do this')
-        }
+    shouldDisplayOptInBanner().then(() => {
+        new Message(messageCode).show(
+            'You have to update your email settings. Visit My Account to do this'
+        );
     });
-}
+};
