27ddf6cba65930cd9689d38bcb9681cdbdd400d8
Laura <laura.gonzalez@guardian.co.uk>
2018-01-19 12:21:38 +0000

    improve js

--- static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
@@ -2,8 +2,9 @@
 
 import { getUserFromApi } from 'common/modules/identity/api';
 import { Message } from 'common/modules/ui/message';
+import config from 'lib/config';
 
-const messageCode: string = 'optin-campaign-jan-18';
+const messageCode: string = 'gdpr-opt-in-jan-18';
 const medium: string = new URL(window.location.href).searchParams.get(
     'utm_medium'
 );
@@ -14,20 +15,45 @@ type ApiUser = {
     },
 };
 
-const shouldDisplayOptInBanner = (): Promise<void> =>
-    new Promise((accept, reject) => {
+const template = {
+    title: 'You have to update your email settings',
+    cta: `Update them now »`,
+    link: `${config.page.idUrl}/consents`,
+};
+
+const templateHtml: string = `
+    <div id="site-message__message">
+        <div class="site-message__message site-message__message--gdpr-opt-in">
+            <div class="gdpr-opt-in__message-text">
+                ${template.title}
+            </div>
+            <a target="_blank" href="${
+                template.link
+            }" data-link-name="gdrp-opt-in : banner-click">
+                ${template.cta}
+            </a>
+        </div>
+    </div>`;
+
+const shouldDisplayOptInBanner = (): Promise<boolean> =>
+    new Promise(decision => {
         if (medium === null || medium.toLowerCase() !== 'email')
-            return reject();
+            return decision(false);
         getUserFromApi((user: ApiUser) => {
-            if (user === null || !user.statusFields.hasRepermissioned) accept();
-            else reject();
+            if (user === null || !user.statusFields.hasRepermissioned)
+                decision(true);
+            else decision(false);
         });
     });
 
-export const optInEngagementBannerInit = (): void => {
-    shouldDisplayOptInBanner().then(() => {
-        new Message(messageCode).show(
-            'You have to update your email settings. Visit My Account to do this'
-        );
+const optInEngagementBannerInit = (): void => {
+    shouldDisplayOptInBanner().then((shouldIt: boolean) => {
+        if (shouldIt) {
+            new Message(messageCode, {
+                important: true,
+            }).show(templateHtml);
+        }
     });
 };
+
+export { optInEngagementBannerInit };
