b4f60573d0f9ae28618248cdd493256121c4d1a4
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-01-19 11:35:08 +0000

    Remove dep on fastdom-promise

--- static/src/javascripts/projects/atoms/services.js
+++ static/src/javascripts/projects/atoms/services.js
@@ -4,7 +4,7 @@
 // managed by the atoms team
 
 import ophan from 'ophan/ng';
-import fastdom from 'lib/fastdom-promise';
+import fastdom from 'fastdom';
 import { viewport } from './services/viewport';
 
 // Need to pass in the API to native services, something that looks
@@ -15,11 +15,20 @@ import { viewport } from './services/viewport';
 //    ...
 // }
 
+type FastdomAction = Function => void;
+
+const promisify = (f: FastdomAction) => (g: Function): Promise<any> =>
+    new Promise(resolve => {
+        f.call(fastdom, () => {
+            resolve(g());
+        });
+    });
+
 const services: Services = {
     ophan,
     dom: {
-        write: fastdom.write,
-        read: fastdom.read,
+        write: promisify(fastdom.write),
+        read: promisify(fastdom.read),
     },
     viewport,
 };
