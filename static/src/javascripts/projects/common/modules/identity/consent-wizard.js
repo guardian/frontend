6a79e71ab22a24a969000019e45c448fe5807d1f
Laura <laura.gonzalez@guardian.co.uk>
2018-01-22 14:06:11 +0000

    remove counter

--- static/src/javascripts/projects/common/modules/identity/consent-wizard.js
+++ static/src/javascripts/projects/common/modules/identity/consent-wizard.js
@@ -12,45 +12,10 @@ import {
 
 const ERR_IDENTITY_CONSENT_WIZARD_MISSING = 'Missing wizard element';
 
-const positions = {
-    consent: 'consent',
-    email: 'email',
-    endcard: 'endcard',
-};
-
-const getAcceptedCheckboxes = (
-    checkboxesEl: Array<HTMLLabelElement>
-): Array<HTMLLabelElement> =>
-    checkboxesEl.filter(
-        (checkboxEl: HTMLLabelElement) =>
-            checkboxEl.control instanceof HTMLInputElement &&
-            checkboxEl.control.checked
-    );
-
-const getAcceptedCheckboxCount = (
-    checkboxesEl: Array<HTMLLabelElement>
-): number => getAcceptedCheckboxes(checkboxesEl).length;
-
-const getEmailCheckboxes = (): Array<HTMLLabelElement> =>
-    ([...document.getElementsByClassName(newsletterCheckboxClassName)]: Array<
-        any
-    >).filter(el => el instanceof HTMLLabelElement);
-
-const updateCounterIndicator = (
-    indicatorEl: HTMLElement,
-    checkboxesEl: Array<HTMLLabelElement>
-): Promise<void> =>
-    fastdom.write(() => {
-        indicatorEl.innerText = getAcceptedCheckboxCount(checkboxesEl).toString(
-            10
-        );
-    });
-
-const showOrHideBackButtonEl = (
+const toggleBackButtonEl = (
     buttonBackEl: HTMLElement,
-    position: number
+    displayButtonBack: boolean
 ) => {
-    const displayButtonBack = position > 0;
     return fastdom.write(() => {
         buttonBackEl.setAttribute(
             'aria-hidden',
@@ -68,41 +33,19 @@ const showOrHideBackButtonEl = (
     });
 };
 
-const showOrHideEmailCounter = (
-    emailCounterEl: HTMLElement,
-    positionName: string
-) => {
-    const displayCounter = positionName === positions.email;
-    return fastdom.write(() => {
-        emailCounterEl.setAttribute(
-            'aria-hidden',
-            (!displayCounter).toString()
-        );
-        emailCounterEl.classList.toggle(
-            'identity-consent-wizard__revealable--visible',
-            displayCounter
-        );
-    });
-};
-
-const toggleLegalDisclaimer = (
+const toggleLegalDisclaimerEl = (
     legalDisclaimerEl: HtmlElement,
-    showOrHide: Boolean
+    showOrHide: boolean
 ) =>
     fastdom.write(() => {
         legalDisclaimerEl.classList.toggle('u-h', !showOrHide);
     });
 
-const bindEmailConsentCounterToWizard = (wizardEl: HTMLElement): void => {
+const bindWizardBlocks = (wizardEl: HTMLElement): void => {
     window.addEventListener(wizardPageChangedEv, ev => {
         if (ev.target === wizardEl) {
             fastdom
                 .read(() => [
-                    [
-                        ...document.getElementsByClassName(
-                            'identity-consent-wizard-counter'
-                        ),
-                    ][0],
                     [
                         ...document.getElementsByClassName(
                             'identity-consent-wizard-button-back'
@@ -117,21 +60,16 @@ const bindEmailConsentCounterToWizard = (wizardEl: HTMLElement): void => {
                 .then(
                     (
                         [
-                            counterEl: HTMLElement,
-                            buttonBackEl: HTMLElement,
+                            BackButtonEl: HTMLElement,
                             legalDisclaimerEl: HTMLElement,
                         ]
                     ) =>
                         Promise.all([
-                            showOrHideBackButtonEl(
-                                buttonBackEl,
-                                ev.detail.position
+                            toggleBackButtonEl(
+                                BackButtonEl,
+                                ev.detail.position > 0
                             ),
-                            showOrHideEmailCounter(
-                                counterEl,
-                                ev.detail.positionName
-                            ),
-                            toggleLegalDisclaimer(
+                            toggleLegalDisclaimerEl(
                                 legalDisclaimerEl,
                                 ev.detail.position === 0
                             ),
@@ -170,34 +108,10 @@ const bindNextButton = (buttonEl: HTMLElement): void => {
     }
 };
 
-const createEmailConsentCounter = (counterEl: HTMLElement): void => {
-    const indicatorEl = document.createElement('div');
-    const textEl = document.createElement('div');
-    const checkboxesEl = getEmailCheckboxes();
-
-    indicatorEl.classList.add('identity-consent-wizard-counter__indicator');
-    textEl.classList.add('identity-consent-wizard-counter__text');
-
-    updateCounterIndicator(indicatorEl, checkboxesEl);
-
-    checkboxesEl.forEach(checkboxEl => {
-        checkboxEl.addEventListener('change', () => {
-            updateCounterIndicator(indicatorEl, checkboxesEl);
-        });
-    });
-    fastdom.write(() => {
-        textEl.innerHTML = `of <strong>${
-            checkboxesEl.length
-        }</strong> selected`;
-        counterEl.append(indicatorEl);
-        counterEl.append(textEl);
-    });
-};
 
 const enhanceConsentWizard = (): void => {
     const loaders = [
-        ['.identity-consent-wizard-counter', createEmailConsentCounter],
-        ['.identity-wizard--consent', bindEmailConsentCounterToWizard],
+        ['.identity-wizard--consent', bindWizardBlocks],
         ['.identity-wizard--consent', showWizard],
         ['#identityWizardloadingError', hideLoading],
         ['.js-identity-consent-wizard__next', bindNextButton],
