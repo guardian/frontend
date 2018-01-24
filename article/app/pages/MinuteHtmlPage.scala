fcb39e615561ef56db644749935271ee58258d5b
Simon Adcock <simon.adcock@guardian.co.uk>
2018-01-24 12:23:18 +0000

    move minute to garnett template

--- article/app/pages/MinuteHtmlPage.scala
+++ article/app/pages/MinuteHtmlPage.scala
@@ -5,14 +5,14 @@ import html.HtmlPage
 import model.ApplicationContext
 import play.api.mvc.RequestHeader
 import play.twirl.api.Html
-import views.html.fragments.{immersiveHeader, minuteBody}
+import views.html.fragments.{immersiveGarnettHeader, minuteBody}
 
 object MinuteHtmlPage extends HtmlPage[MinutePage] {
 
     def html(page: MinutePage)(implicit request: RequestHeader, applicationContext: ApplicationContext): Html = {
       implicit val p: MinutePage = page
       StoryHtmlPage.html(
-        header = immersiveHeader(),
+        header = immersiveGarnettHeader(),
         content = minuteBody(page)
       )
     }
