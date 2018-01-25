231daed87d387ee0157819885cdda3a1b64a401f
Jon Norman <jon.norman@guardian.co.uk>
2018-01-25 14:57:57 +0000

    Restore previous label-setting logic.

--- static/src/javascripts/projects/commercial/modules/dfp/render-advert-label.spec.js
+++ static/src/javascripts/projects/commercial/modules/dfp/render-advert-label.spec.js
@@ -28,24 +28,6 @@ describe('Rendering advert labels', () => {
             `)
         );
 
-        // the override should work regardless of whether the other conditions are true, including an existing label
-        adverts.requiredOverride = bonzo(
-            bonzo.create(`
-                <div class="js-ad-slot">
-                    <div class="ad-slot__label">Advertisement</div>
-                    <div class="js-advertisement-label-required-true"></div>
-                </div>
-            `)
-        );
-
-        adverts.notRequiredOverride = bonzo(
-            bonzo.create(`
-                <div class="js-ad-slot">
-                    <div class="js-advertisement-label-required-false"></div>
-                </div>
-            `)
-        );
-
         adverts.frame = bonzo(
             bonzo.create('<div class="js-ad-slot ad-slot--frame"></div>')
         );
@@ -81,21 +63,5 @@ describe('Rendering advert labels', () => {
         renderAdvertLabel(adverts.frame[0]).then(() => {
             const label = adverts.frame[0].querySelector(labelSelector);
             expect(label).toBeNull();
-        }));
-
-    it(`Won't add a label to ads with explicit no-label override`, () =>
-        renderAdvertLabel(adverts.notRequiredOverride[0]).then(() => {
-            const label = adverts.notRequiredOverride[0].querySelector(
-                labelSelector
-            );
-            expect(label).toBeNull();
-        }));
-
-    it(`Will add a label to ads with explicit label override`, () =>
-        renderAdvertLabel(adverts.requiredOverride[0]).then(() => {
-            const labels = adverts.requiredOverride[0].querySelectorAll(
-                labelSelector
-            );
-            expect(labels.length).toBe(2);
         }));
 });
