f86e92e1144a981973578674ffcd80b76ba8253f
GHaberis <george.haberis@theguardian.com>
2018-01-26 14:58:57 +0000

    update flow dependency and flow-typed definitions

--- static/src/javascripts/__flow__/flow-typed/npm/fastdom_v0.8.5.js
+++
@@ -1,8 +0,0 @@
-declare module 'fastdom' {
-  declare module.exports: {
-    read(fn: Function, ctx: ?Object): number;
-    write(fn: Function, ctx: ?Object): number;
-    defer(frame: number, fn: Function, ctx: ?Object): number;
-    clear(id: number): void;
-  };
-}
