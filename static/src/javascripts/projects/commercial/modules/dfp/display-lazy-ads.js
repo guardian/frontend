360896eb8c6ff0e61181a4150f03320afa674b8e
Jon Norman <jon.norman@guardian.co.uk>
2018-02-05 12:21:27 +0000

    Bundle enable lazy load into lazy load and remove scroll listener.

--- static/src/javascripts/projects/commercial/modules/dfp/display-lazy-ads.js
+++ static/src/javascripts/projects/commercial/modules/dfp/display-lazy-ads.js
@@ -2,7 +2,7 @@
 import { dfpEnv } from 'commercial/modules/dfp/dfp-env';
 import { loadAdvert } from 'commercial/modules/dfp/load-advert';
 import { Advert } from 'commercial/modules/dfp/Advert';
-import { enableLazyLoad } from 'commercial/modules/dfp/enable-lazy-load';
+import { enableLazyLoad } from 'commercial/modules/dfp/lazy-load';
 import { updateAdvertMetric } from 'commercial/modules/dfp/performance-logging';
 
 const advertsToInstantlyLoad = ['dfp-ad--merchandising-high', 'dfp-ad--im'];
