77fd6ff8e4bce85681bd8dfbec805fa1bc64a39e
Jon Norman <jon.norman@guardian.co.uk>
2018-02-06 16:23:49 +0000

    Differentiate between loading an ad and refreshing an ad.

--- static/src/javascripts/projects/commercial/modules/dfp/load-advert.js
+++ static/src/javascripts/projects/commercial/modules/dfp/load-advert.js
@@ -2,7 +2,7 @@
 import { Advert } from 'commercial/modules/dfp/Advert';
 import { prebid } from 'commercial/modules/prebid/prebid';
 
-export const loadAdvert = (advert: Advert): void => {
+export const loadAdvert = (advert: Advert, refresh: boolean = false): void => {
     advert.whenSlotReady
         .catch(() => {
             // The display needs to be called, even in the event of an error.
@@ -12,6 +12,10 @@ export const loadAdvert = (advert: Advert): void => {
             return prebid.requestBids(advert);
         })
         .then(() => {
-            window.googletag.display(advert.id);
+            if (refresh) {
+                window.googletag.pubads().refresh([advert.slot]);
+            } else {
+                window.googletag.display(advert.id);
+            }
         });
 };
