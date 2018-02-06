6715fadd35c410576c055665613bbcd358be57bb
Jon Norman <jon.norman@guardian.co.uk>
2018-02-06 16:24:58 +0000

    Refresh ads when the viewable hook is triggered.

--- static/src/javascripts/projects/commercial/modules/dfp/on-slot-viewable.js
+++ static/src/javascripts/projects/commercial/modules/dfp/on-slot-viewable.js
@@ -3,6 +3,7 @@ import type { ImpressionViewableEvent } from 'commercial/types';
 
 import { Advert } from 'commercial/modules/dfp/Advert';
 import { getAdvertById } from 'commercial/modules/dfp/get-advert-by-id';
+import { loadAdvert } from 'commercial/modules/dfp/load-advert';
 
 const viweabilityThresholdMs = 30000;
 
@@ -10,7 +11,8 @@ export const onSlotViewable = (event: ImpressionViewableEvent): void => {
     const advert: ?Advert = getAdvertById(event.slot.getSlotElementId());
     if (advert) {
         setTimeout(() => {
-            /* do something here to refresh the ads */
+            advert.slot.setTargeting('refreshed', 'true');
+            loadAdvert(advert, true);
         }, viweabilityThresholdMs);
     }
 };
