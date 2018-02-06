39d670815f1d23ef3337f9370b61185d22733276
Alex Ware <alex.ware@guardian.co.uk>
2018-02-06 15:19:31 +0000

    Render the chosen country if the Country is not in the list.

--- identity/app/model/Countries.scala
+++ identity/app/model/Countries.scala
@@ -2,8 +2,14 @@ package model
 
 import com.gu.i18n.CountryGroup
 
+import scala.collection.immutable
+
 object Countries {
-  val all = CountryGroup.countries.map(_.name) 
+  val all = CountryGroup.countries.map(_.name)
+  def withCustom(maybeCountry: Option[String]): List[String] = maybeCountry match {
+    case Some(c) if !(all contains c) => c :: all
+    case _ => all
+  }
   val UK = CountryGroup.UK.name
   val US = CountryGroup.US.name
 }
