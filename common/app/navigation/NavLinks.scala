f59497e63f5ba17517586c0e9da6cd90c7792a12
Alex Sanders <a@snd.rs>
2018-02-12 13:42:42 +0000

    football: flip fixtures and competitions

--- common/app/navigation/NavLinks.scala
+++ common/app/navigation/NavLinks.scala
@@ -70,9 +70,9 @@ object NavLinks {
     children = List(
       NavLink("Live scores", "/football/live", "football/live"),
       NavLink("Tables", "/football/tables", "football/tables"),
-      NavLink("Competitions", "/football/competitions", "football/competitions"),
-      NavLink("Results", "/football/results", "football/results"),
       NavLink("Fixtures", "/football/fixtures", "football/fixtures"),
+      NavLink("Results", "/football/results", "football/results"),
+      NavLink("Competitions", "/football/competitions", "football/competitions"),
       NavLink("Clubs", "/football/teams", "football/teams")
     )
   )
