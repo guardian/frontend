701bce6006de76f479b6eff4cf551f5afe74280b
Mario Galic <mario.galic@guardian.co.uk>
2018-02-13 17:40:21 +0000

    Add professional networks newsletters

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ package com.gu
 import sbt._
 
 object Dependencies {
-  val identityLibVersion = "3.124"
+  val identityLibVersion = "3.125"
   val awsVersion = "1.11.240"
   val faciaVersion = "2.5.0"
   val capiVersion = "11.46"
