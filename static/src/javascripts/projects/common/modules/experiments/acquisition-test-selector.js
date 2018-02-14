6f96d0048c6500ba81c93a3a07740ff652fe3957
Joseph Smith <joseph.smith@guardian.co.uk>
2018-02-14 18:56:17 +0000

    Just export the acquisitionsTests, don't rename to abTestClashData

--- static/src/javascripts/projects/common/modules/experiments/acquisition-test-selector.js
+++ static/src/javascripts/projects/common/modules/experiments/acquisition-test-selector.js
@@ -14,19 +14,6 @@ import { colourTestEpicHoldback } from 'common/modules/experiments/tests/circles
 import { acquisitionsEpicUSGunCampaign } from 'common/modules/experiments/tests/acquisitions-epic-us-gun-campaign';
 import { acquisitionsEpicAusEnvCampaign } from 'common/modules/experiments/tests/acquisitions-epic-aus-env-campaign';
 
-/**
- * acquisition tests in priority order (highest to lowest)
- */
-const tests: $ReadOnlyArray<AcquisitionsABTest> = [
-    acquisitionsEpicAusEnvCampaign,
-    acquisitionsEpicUSGunCampaign,
-    colourTestEpicHoldback,
-    askFourEarning,
-    acquisitionsEpicAlwaysAskIfTagged,
-    acquisitionsEpicLiveblog,
-    acquisitionsEpicThankYou,
-];
-
 const isViewable = (v: Variant, t: ABTest): boolean => {
     if (!v.options || !v.options.maxViews) return false;
 
@@ -46,9 +33,22 @@ const isViewable = (v: Variant, t: ABTest): boolean => {
     return (withinViewLimit && enoughDaysBetweenViews) || isUnlimited;
 };
 
+/**
+ * acquisition tests in priority order (highest to lowest)
+ */
+export const acquisitionsTests: $ReadOnlyArray<AcquisitionsABTest> = [
+    acquisitionsEpicAusEnvCampaign,
+    acquisitionsEpicUSGunCampaign,
+    colourTestEpicHoldback,
+    askFourEarning,
+    acquisitionsEpicAlwaysAskIfTagged,
+    acquisitionsEpicLiveblog,
+    acquisitionsEpicThankYou,
+];
+
 export const getTest = (): ?ABTest => {
     const forcedTests = getForcedTests()
-        .map(({ testId }) => tests.find(t => t.id === testId))
+        .map(({ testId }) => acquisitionsTests.find(t => t.id === testId))
         .filter(Boolean);
 
     if (forcedTests.length)
@@ -57,7 +57,7 @@ export const getTest = (): ?ABTest => {
             return variant && testCanBeRun(t) && isViewable(variant, t);
         });
 
-    return tests.find(t => {
+    return acquisitionsTests.find(t => {
         const variant: ?Variant = variantFor(t);
         return (
             variant && testCanBeRun(t) && isInTest(t) && isViewable(variant, t)
