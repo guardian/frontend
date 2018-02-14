80a38b30d60acd9bc4206685309e9aa17afa2f59
Laura <laura.gonzalez@guardian.co.uk>
2018-02-14 13:30:45 +0000

    add info block

--- identity/app/views/support/fragment/ConsentBlock.scala
+++ identity/app/views/support/fragment/ConsentBlock.scala
@@ -5,7 +5,8 @@ import play.twirl.api.Html
 object ConsentBlock {
 
   sealed abstract class ConsentBlock(
-    val show: Boolean
+    val show: Boolean,
+    val name: String
   ) {}
 
   sealed trait ConsentStepHelpTextTrait
@@ -17,25 +18,26 @@ object ConsentBlock {
 
   case class ConsentBanner(
     title: String
-  ) extends ConsentBlock(show=true)
+  ) extends ConsentBlock(show=true, name=title)
 
+  case class ConsentInfo(
+    title: String,
+    help: List[ConsentStepHelpTextTrait] = Nil,
+  ) extends ConsentBlock(show=true, name=title)
 
   case class ConsentStep(
-    name: String,
+    override val name: String,
     title: String,
     help: List[ConsentStepHelpTextTrait] = Nil,
     content: Html = Html(""),
     override val show: Boolean = true
-  ) extends ConsentBlock(show)
+  ) extends ConsentBlock(show, name)
 
 
   def renderBlocks(steps:List[ConsentBlock]): List[Html] = {
     val displaySteps = steps.filter(_.show)
     displaySteps.zipWithIndex.map{ case (block, index) =>
-      block match {
-        case m: ConsentStep => views.html.consentJourneyFragments.step(m, index == 0, index == (displaySteps.size - 1))
-        case m: ConsentBanner => Html(s"<p class='form__success'>${m.title}</p>")
-      }
+      views.html.consentJourneyFragments.block(block, index == 0, index == (displaySteps.size - 1))
     }
   }
 
