62c6252a3bf8e22b372eb05a5ce987a381838914
Simon Adcock <simon.adcock@guardian.co.uk>
2018-02-15 13:41:22 +0000

    delete ui

--- ui/__config__/paths.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    ui: path.resolve(__dirname, '..'),
-    main: path.resolve(__dirname, '..', '..'),
-};
--- ui/src/__flow__/types.js
+++
@@ -1,2 +0,0 @@
-declare var SERVER: Boolean;
-declare var BROWSER: Boolean;
