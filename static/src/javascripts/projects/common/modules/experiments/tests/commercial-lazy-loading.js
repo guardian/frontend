40e22024eae920a905a2f5a875e243c0e1937c35
Richard Nguyen <richard.nguyen@guardian.co.uk>
2018-02-21 11:23:32 +0000

    Update test parameters

--- static/src/javascripts/projects/common/modules/experiments/tests/commercial-lazy-loading.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/commercial-lazy-loading.js
@@ -4,19 +4,19 @@ import config from 'lib/config';
 export const commercialLazyLoading: ABTest = {
     id: 'CommercialLazyLoading',
     start: '2018-02-16',
-    expiry: '2018-03-02',
+    expiry: '2018-03-30',
     author: 'Jon Norman',
     description:
         'This test alters the threshold for when lazy loaded adverts are displayed',
-    audience: 0.1,
-    audienceOffset: 0.5,
+    audience: 0.2,
+    audienceOffset: 0.7,
     successMeasure:
         'No negative impact on viewability, positive impact in number of impressions rendered.',
     audienceCriteria: 'All web traffic.',
     dataLinkNames: '',
     idealOutcome:
         'We find an optimised method for determining when ad slots should be rendered',
-    canRun: () => !config.get('tests.commercialBaselineControl'),
+    canRun: () => !config.get('tests.commercialBaselineControl') && !config.get('tests.commercialBaselineVariant'),
     variants: [
         {
             id: 'control',
