02e0a9162bef6091990ef9cc1873d34283206ea4
Francis Carr <francis.carr@guardian.co.uk>
2018-02-22 15:37:34 +0000

    Fix reference to old template

--- applications/app/pages/ContentHtmlPage.scala
+++ applications/app/pages/ContentHtmlPage.scala
@@ -10,8 +10,8 @@ import play.twirl.api.Html
 import views.html.fragments._
 import views.html.fragments.commercial.pageSkin
 import views.html.fragments.page.body.{bodyTag, breakingNewsDiv, mainContent, skipToMainContent}
-import views.html.fragments.page.head.stylesheets.{criticalStyleInline, criticalStyleLink, styles}
-import views.html.fragments.page.head.{fixIEReferenceErrors, headTag, titleTag, weAreHiring, orielEsiTag}
+import views.html.fragments.page.head.stylesheets.{criticalStyleInline, criticalStyleLink}
+import views.html.fragments.page.head.{fixIEReferenceErrors, headTag, titleTag, weAreHiring, orielScriptTag}
 import views.html.fragments.page.{devTakeShot, htmlTag}
 import views.html.{newspaperContent, quizAnswerContent}
 import html.HtmlPageHelpers.ContentCSSFile
@@ -46,7 +46,7 @@ object ContentHtmlPage extends HtmlPage[Page] {
     htmlTag(
       headTag(
         weAreHiring() when WeAreHiring.isSwitchedOn,
-        orielEsiTag() when OrielFullIntegration.isSwitchedOn,
+        orielScriptTag() when OrielFullIntegration.isSwitchedOn,
         titleTag(),
         metaData(),
         styles(allStyles),
