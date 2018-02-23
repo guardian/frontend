8bd22b6222825d12474490ef86596724496965e3
Laura <laura.gonzalez@guardian.co.uk>
2018-02-23 13:41:23 +0000

    refactor error queue

--- static/src/javascripts/projects/common/modules/identity/modules/show-errors.js
+++ static/src/javascripts/projects/common/modules/identity/modules/show-errors.js
@@ -6,7 +6,15 @@ const formErrorHolderClassName = 'js-errorHolder';
 
 const genericErrorMessage = 'Sorry, something went wrong';
 
-export const push = (error: mixed, action: string = 'none'): Promise<void> =>
+const errors = [];
+
+type IdentityRenderableError = {
+    message: string,
+    action: string,
+    times: number,
+};
+
+const renderError = (error: IdentityRenderableError): Promise<void> =>
     fastdom
         .read(() =>
             window.document.querySelector(`.${formErrorHolderClassName}`)
@@ -14,15 +22,13 @@ export const push = (error: mixed, action: string = 'none'): Promise<void> =>
         .then((errorHolderEl: HTMLElement) =>
             fastdom.write(() => {
                 const errorEl = document.createElement('div');
-                errorEl.innerHTML = `<p>${
-                    error instanceof Error && error.message
-                        ? error.message
-                        : genericErrorMessage
-                }. </p>`;
+                errorEl.innerHTML = `<p>${error.message}${
+                    error.times > 1 ? ` (${error.times} times)` : ``
+                }</p>`;
                 errorEl.className = formErrorClassName;
                 errorHolderEl.appendChild(errorEl);
 
-                switch (action) {
+                switch (error.action) {
                     case 'reload': {
                         const reloadCtaEl = document.createElement('a');
                         reloadCtaEl.innerHTML = 'Refresh this page';
@@ -38,3 +44,46 @@ export const push = (error: mixed, action: string = 'none'): Promise<void> =>
                 }
             })
         );
+
+const renderList = (): Promise<void> =>
+    fastdom
+        .read(() =>
+            window.document.querySelector(`.${formErrorHolderClassName}`)
+        )
+        .then((errorHolderEl: HTMLElement) =>
+            fastdom.write(() => {
+                while (errorHolderEl.firstChild) {
+                    errorHolderEl.removeChild(errorHolderEl.firstChild);
+                }
+            })
+        )
+        .then(() => Promise.all(errors.map(error => renderError(error))))
+        .then(() => Promise.resolve());
+
+export const push = (error: mixed, action: string = 'none'): Promise<void> => {
+    const message =
+        error instanceof Error && error.message
+            ? error.message
+            : genericErrorMessage;
+
+    const isDupeIndex = errors.findIndex(
+        (_: IdentityRenderableError) => _.message === message
+    );
+
+    if (isDupeIndex > -1) {
+        errors.push({
+            message,
+            action,
+            times: errors[isDupeIndex].times + 1,
+        });
+        errors.splice(isDupeIndex, 1);
+    } else {
+        errors.push({
+            message,
+            action,
+            times: 1,
+        });
+    }
+
+    return renderList();
+};
