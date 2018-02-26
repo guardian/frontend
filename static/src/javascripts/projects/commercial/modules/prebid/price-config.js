e03b6e07f1e75b9bf8a5fb26bc4bd7e9d5359e32
Kelvin Chappell <kelvin.chappell@theguardian.com>
2018-02-26 16:33:28 +0000

    Update Prebid price granularity

---
+++ static/src/javascripts/projects/commercial/modules/prebid/price-config.js
@@ -0,0 +1,28 @@
+// @flow
+
+import type { PrebidPriceGranularity } from 'commercial/modules/prebid/types';
+
+export const priceGranularity: PrebidPriceGranularity = {
+    buckets: [
+        {
+            min: 0,
+            max: 5,
+            increment: 0.05,
+        },
+        {
+            min: 5,
+            max: 10,
+            increment: 0.1,
+        },
+        {
+            min: 10,
+            max: 20,
+            increment: 0.5,
+        },
+        {
+            min: 20,
+            max: 30,
+            increment: 10,
+        },
+    ],
+};
