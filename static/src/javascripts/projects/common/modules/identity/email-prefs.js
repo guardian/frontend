5e248b96232a0deba3029fd577899e5cd2a0769c
Laura <laura.gonzalez@guardian.co.uk>
2018-02-27 13:33:55 +0000

    add alert

--- static/src/javascripts/projects/common/modules/identity/email-prefs.js
+++ static/src/javascripts/projects/common/modules/identity/email-prefs.js
@@ -4,6 +4,9 @@ import reqwest from 'reqwest';
 import fastdom from 'lib/fastdom-promise';
 import loadEnhancers from './modules/loadEnhancers';
 
+import {
+    show as showModal
+} from './modules/modal';
 import { push as pushError } from './modules/show-errors';
 import { addUpdatingState, removeUpdatingState } from './modules/button';
 import {
@@ -48,36 +51,17 @@ const bindHtmlPreferenceChange = (buttonEl: HTMLButtonElement): void => {
     );
 };
 
-const modalCloserBind = (buttonEl: HTMLElement): void => {
-    buttonEl.addEventListener('click', () => {
-        const modalEl: ?Element = buttonEl.closest('.identity-modal');
-        if (modalEl) {
-            modalEl.classList.remove('identity-modal--active');
-        }
-    });
-};
-
 const modalFormatToggle = (buttonEl: HTMLElement): void => {
-    buttonEl.addEventListener('click', () => {
-        fastdom
-            .read(
-                () =>
-                    document.getElementsByClassName(
-                        'identity-modal--newsletterFormat'
-                    )[0]
-            )
-            .then(modalEl => {
-                fastdom.write(() => {
-                    modalEl.classList.add('identity-modal--active');
-                });
-            });
-    });
+    buttonEl.addEventListener('click', () =>
+        showModal(
+            'newsletterFormat'
+        )
+    );
 };
 
 const enhanceEmailPrefs = (): void => {
     const loaders = [
         ['.js-save-button', bindHtmlPreferenceChange],
-        ['.js-identity-modal__closer', modalCloserBind],
         ['.js-email-subscription__formatFieldsetToggle', modalFormatToggle],
     ];
     loadEnhancers(loaders);
