f3a6891c6a2d30adfcbd00b0113a4c8705deae32
Joseph Smith <joseph.lochlann@gmail.com>
2018-02-28 17:49:08 +0000

    Extend US gun campaign switch (#19245)

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -52,7 +52,7 @@ trait ABTestSwitches {
     "Show a custom Epic for articles with the US gun campaign tag",
     owners = Seq(Owner.withGithub("Mullefa")),
     safeState = On,
-    sellByDate = new LocalDate(2018, 3, 1),
+    sellByDate = new LocalDate(2018, 3, 15),
     exposeClientSide = true
   )
 
