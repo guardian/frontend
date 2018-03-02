5971d597ac425ce0036aac33c427438ea43fbb45
Francis Carr <francis.carr@guardian.co.uk>
2018-03-02 11:03:43 +0000

    Add oriel full intrgration on galleries, interactives, tag and index pages

--- applications/app/pages/InteractiveHtmlPage.scala
+++ applications/app/pages/InteractiveHtmlPage.scala
@@ -1,7 +1,7 @@
 package pages
 
 import common.Edition
-import conf.switches.Switches.WeAreHiring
+import conf.switches.Switches.{OrielFullIntegration, WeAreHiring}
 import controllers.InteractivePage
 import html.{HtmlPage, Styles}
 import html.HtmlPageHelpers._
@@ -10,7 +10,7 @@ import play.api.mvc.RequestHeader
 import play.twirl.api.Html
 import views.html.fragments.commercial.pageSkin
 import views.html.fragments.page.body.{bodyTag, breakingNewsDiv, mainContent, skipToMainContent}
-import views.html.fragments.page.head.{fixIEReferenceErrors, headTag, titleTag, weAreHiring}
+import views.html.fragments.page.head._
 import views.html.fragments.page.head.stylesheets.{criticalStyleInline, criticalStyleLink, styles}
 import views.html.fragments.page.{devTakeShot, htmlTag}
 import views.html.fragments._
@@ -46,6 +46,7 @@ object InteractiveHtmlPage extends HtmlPage[InteractivePage] {
     htmlTag(
       headTag(
         weAreHiring() when WeAreHiring.isSwitchedOn,
+        orielScriptTag() when OrielFullIntegration.isSwitchedOn,
         titleTag(),
         metaData(),
         styles(allStyles),
