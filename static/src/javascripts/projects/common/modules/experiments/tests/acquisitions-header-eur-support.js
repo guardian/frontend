aed6db6ccf5d5d4b19ad0269ec0f56d2c49e2e46
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-03-02 12:23:58 +0000

    Created the header test

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-header-eur-support.js
@@ -0,0 +1,68 @@
+// @flow
+import { getSupporterPaymentRegion as geolocationGetSupporterPaymentRegion } from 'lib/geolocation';
+import { updateAcquisitionData } from 'common/modules/commercial/acquisitions-ophan';
+
+const componentType = 'ACQUISITIONS_HEADER';
+const abTestName = 'AcquisitionsHeaderEURSupport';
+const EURsupportURL = 'https://support.theguardian.com/eu';
+
+type variantName = 'control' | 'support_contribute';
+
+const modifySupportTheGuardianLink = (variant: variantName): void => {
+    [...document.querySelectorAll('.js-change-become-member-link')].forEach(
+        supportTheGuardianLink => {
+            if (!(supportTheGuardianLink instanceof HTMLAnchorElement)) {
+                return;
+            }
+
+            let supportTheGuardianURL = new URL(supportTheGuardianLink.href);
+
+            if (variant === 'support_contribute') {
+                supportTheGuardianURL = new URL(EURsupportURL);
+            }
+
+            const supportTheGuardianUrlWithTestData = updateAcquisitionData(
+                supportTheGuardianURL,
+                {
+                    abTest: {
+                        name: abTestName,
+                        variant,
+                    },
+                }
+            );
+
+            supportTheGuardianLink.href = supportTheGuardianUrlWithTestData.toString();
+    });
+};
+
+export const acquisitionsHeaderEURSupport: AcquisitionsABTest = {
+    id: abTestName,
+    campaignId: abTestName,
+    componentType,
+    start: '2018-03-07',
+    expiry: '2018-04-17',
+    author: 'Santiago Villa Fernandez',
+    description:
+        'Points the "support the guardian" link in the header to the eur version of the support site',
+    audience: 1,
+    audienceOffset: 0,
+    successMeasure: 'AV 2.0',
+    audienceCriteria: 'All EUR transaction web traffic.',
+    dataLinkNames: '',
+    idealOutcome: 'We get more money when we tailor the destination to the CTA',
+    canRun: () => geolocationGetSupporterPaymentRegion() === 'EU',
+    variants: [
+        {
+            id: 'control',
+            test: () => {
+                modifySupportTheGuardianLink('control');
+            },
+        },
+        {
+            id: 'support_contribute',
+            test: () => {
+                modifySupportTheGuardianLink('support_contribute');
+            },
+        },
+    ],
+};
