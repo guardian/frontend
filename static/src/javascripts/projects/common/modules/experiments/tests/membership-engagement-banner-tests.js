6e77858a8652b752786b9cc38dd17751a577a0eb
Joseph Smith <joseph.smith@guardian.co.uk>
2018-03-05 11:31:27 +0000

    Revert "banner cta test (#19139)"
    This reverts commit 4312d1ff8bf7145cd071ab4227883c864d7ec7e5.

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,8 +1,6 @@
 // @flow
-
-import { AcquisitionsBannerCtaContribute } from 'common/modules/experiments/tests/acquisitions-banner-cta-contribute';
 import { SupportEngagementBannerCircles } from 'common/modules/experiments/tests/support-engagement-banner-circles';
 
 export const membershipEngagementBannerTests: $ReadOnlyArray<
     AcquisitionsABTest
-> = [AcquisitionsBannerCtaContribute, SupportEngagementBannerCircles];
+> = [SupportEngagementBannerCircles];
