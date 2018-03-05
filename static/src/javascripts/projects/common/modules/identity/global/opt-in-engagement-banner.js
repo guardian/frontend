75bbbc61c8a092dc67e1469b46c49d488d957d4b
Laura <laura.gonzalez@guardian.co.uk>
2018-03-05 12:02:18 +0000

    guardian style date

--- static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
@@ -35,7 +35,7 @@ const targets = {
 
 const template: Template = {
     image: config.get('images.identity.opt-in'),
-    title: `We’re changing how we communicate with you. Let us know <strong>before April 30</strong> which emails you wish to continue receiving. <a data-link-name="gdpr-oi-campaign : alert : to-landing" href="${
+    title: `We’re changing how we communicate with you. Let us know <strong>before 30 April</strong> which emails you wish to continue receiving. <a data-link-name="gdpr-oi-campaign : alert : to-landing" href="${
         targets.landing
     }">Find out more</a> or click Continue.`,
     cta: `Continue`,
