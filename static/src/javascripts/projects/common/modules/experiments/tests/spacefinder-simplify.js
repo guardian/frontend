e5b6c2367d1f812e99f366de250f2fb789ee1f52
Jon Norman <jon.norman@guardian.co.uk>
2018-03-05 15:08:55 +0000

    Define the spacefinder test infrastructure.

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/spacefinder-simplify.js
@@ -0,0 +1,28 @@
+// @flow
+
+export const spacefinderSimplify: ABTest = {
+    id: 'SpacefinderSimplify',
+    start: '2017-03-06',
+    expiry: '2018-03-05',
+    author: 'Jon Norman',
+    description:
+        'This test alters the rules for inserting ads on desktop breakpoints.',
+    audience: 0.1,
+    audienceOffset: 0.7,
+    successMeasure: 'No negative impact on attention time',
+    audienceCriteria: 'All web traffic.',
+    dataLinkNames: '',
+    idealOutcome:
+        'We increase the number of ads in articles whilst understanding any aesthetic issues that arise.',
+    canRun: () => true,
+    variants: [
+        {
+            id: 'variant',
+            test: () => {},
+        },
+        {
+            id: 'control',
+            test: () => {},
+        },
+    ],
+};
