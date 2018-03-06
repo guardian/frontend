a71a336140de36a8eff2c99a3ead5c3df09acfb6
Simon Adcock <simon.adcock@guardian.co.uk>
2018-03-06 09:38:49 +0000

    refactor clickstream as class

--- static/src/javascripts/projects/common/modules/ui/clickstream.js
+++ static/src/javascripts/projects/common/modules/ui/clickstream.js
@@ -23,104 +23,99 @@ type Spec = {
     customEventProperties?: Object,
 };
 
-type ClickStream = {
-    getClickSpec: (Spec, ?boolean) => Spec | boolean,
-};
+class Clickstream {
+    constructor(opts?: Options = {}) {
+        // Allow a fake window.location to be passed in for testing
+        const location = opts.location || window.location;
 
-const Clickstream = (opts?: Options = {}): ClickStream => {
-    // Allow a fake window.location to be passed in for testing
-    const location = opts.location || window.location;
+        const filters = opts.filter || [];
 
-    const filters = opts.filter || [];
+        const filterSource = element => filters.filter(f => f === element);
 
-    const filterSource = element => filters.filter(f => f === element);
+        const compareHosts = (url?: string = ''): boolean => {
+            if (url.startsWith('mailto:')) {
+                return false;
+            }
 
-    const compareHosts = (url?: string = ''): boolean => {
-        if (url.startsWith('mailto:')) {
-            return false;
-        }
+            const urlHost = url.match(/:\/\/(.[^/]+)/);
 
-        const urlHost = url.match(/:\/\/(.[^/]+)/);
+            // Lack of a urlHost implies a relative url.
+            // For absolute urls we are protocol-agnostic,
+            // e.g. we should treat https://gu.com/foo -> http://gu.com/bar as a same-host link.
+            return !urlHost || urlHost[1] === location.hostname;
+        };
 
-        // Lack of a urlHost implies a relative url.
-        // For absolute urls we are protocol-agnostic,
-        // e.g. we should treat https://gu.com/foo -> http://gu.com/bar as a same-host link.
-        return !urlHost || urlHost[1] === location.hostname;
-    };
+        const getClickSpec = (spec: Spec, forceValid: ?boolean): Spec | boolean => {
+            if (!spec.el) {
+                return false;
+            }
 
-    const getClickSpec = (spec: Spec, forceValid: ?boolean): Spec | boolean => {
-        if (!spec.el) {
-            return false;
-        }
+            const el = spec.el;
+            const elName = el.tagName.toLowerCase();
+            const dataLinkName = el.getAttribute('data-link-name');
 
-        const el = spec.el;
-        const elName = el.tagName.toLowerCase();
-        const dataLinkName = el.getAttribute('data-link-name');
+            if (dataLinkName) {
+                spec.tags.unshift(dataLinkName);
+            }
 
-        if (dataLinkName) {
-            spec.tags.unshift(dataLinkName);
-        }
+            if (elName === 'body') {
+                spec.tag = spec.tags.join(' | ');
+                delete spec.el;
 
-        if (elName === 'body') {
-            spec.tag = spec.tags.join(' | ');
-            delete spec.el;
-
-            if (spec.validTarget && el.getAttribute('data-link-test')) {
-                spec.tag = `${el.getAttribute('data-link-test') ||
+                if (spec.validTarget && el.getAttribute('data-link-test')) {
+                    spec.tag = `${el.getAttribute('data-link-test') ||
                     ''} | ${spec.tag || ''}`;
+                }
+                return spec;
             }
-            return spec;
-        }
 
-        const customEventProperties = JSON.parse(
-            el.getAttribute('data-custom-event-properties') || '{}'
-        );
-        spec.customEventProperties = Object.assign(
-            customEventProperties,
-            spec.customEventProperties
-        );
+            const customEventProperties = JSON.parse(
+                el.getAttribute('data-custom-event-properties') || '{}'
+            );
+            spec.customEventProperties = Object.assign(
+                customEventProperties,
+                spec.customEventProperties
+            );
 
-        if (!spec.validTarget) {
-            spec.validTarget = filterSource(elName).length > 0 || !!forceValid;
-            if (spec.validTarget) {
-                const href = el.getAttribute('href');
-                spec.target = el;
-                spec.samePage =
-                    (href && href.startsWith('#')) ||
-                    elName === 'button' ||
-                    !!el.hasAttribute('data-is-ajax');
+            if (!spec.validTarget) {
+                spec.validTarget = filterSource(elName).length > 0 || !!forceValid;
+                if (spec.validTarget) {
+                    const href = el.getAttribute('href');
+                    spec.target = el;
+                    spec.samePage =
+                        (href && href.startsWith('#')) ||
+                        elName === 'button' ||
+                        !!el.hasAttribute('data-is-ajax');
 
-                spec.sameHost = spec.samePage || (!!href && compareHosts(href));
+                    spec.sameHost = spec.samePage || (!!href && compareHosts(href));
+                }
             }
-        }
 
-        // Pick up the nearest data-link-context
-        if (!spec.linkContext && el.getAttribute('data-link-context-path')) {
-            spec.linkContextPath =
-                el.getAttribute('data-link-context-path') || '';
-            spec.linkContextName =
-                el.getAttribute('data-link-context-name') || '';
-        }
+            // Pick up the nearest data-link-context
+            if (!spec.linkContext && el.getAttribute('data-link-context-path')) {
+                spec.linkContextPath =
+                    el.getAttribute('data-link-context-path') || '';
+                spec.linkContextName =
+                    el.getAttribute('data-link-context-name') || '';
+            }
 
-        spec.el = (el.parentNode: any);
-        return getClickSpec(spec);
-    };
+            spec.el = (el.parentNode: any);
+            return getClickSpec(spec);
+        };
 
-    // delegate, emit the derived tag
-    if (opts.addListener !== false) {
-        bean.add(document.body, 'click', (event: Event): void => {
-            const clickSpec = getClickSpec({
-                el: (event.target: any),
-                tags: [],
-                target: (event.target: any),
+        // delegate, emit the derived tag
+        if (opts.addListener !== false) {
+            bean.add(document.body, 'click', (event: Event): void => {
+                const clickSpec = getClickSpec({
+                    el: (event.target: any),
+                    tags: [],
+                    target: (event.target: any),
+                });
+
+                mediator.emit('module:clickstream:click', clickSpec);
             });
-            mediator.emit('module:clickstream:click', clickSpec);
-        });
+        }
     }
-
-    return {
-        getClickSpec,
-    };
-};
+}
 
 export { Clickstream };
