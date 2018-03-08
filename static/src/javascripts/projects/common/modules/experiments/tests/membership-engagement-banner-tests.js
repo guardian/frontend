6e53145933af971558554797ed131b1323ad41f0
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-03-08 15:47:04 +0000

    Added engagement AUD test

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,7 +1,12 @@
 // @flow
 import { SupportEngagementBannerCircles } from 'common/modules/experiments/tests/support-engagement-banner-circles';
 import { AcquisitionsEngagementBannerEurSupport } from 'common/modules/experiments/tests/acquisitions-engagement-banner-eur-support';
+import { AcquisitionsEngagementBannerAudSupport } from 'common/modules/experiments/tests/acquisitions-engagement-banner-aud-support';
 
 export const membershipEngagementBannerTests: $ReadOnlyArray<
     AcquisitionsABTest
-> = [SupportEngagementBannerCircles, AcquisitionsEngagementBannerEurSupport];
+> = [
+    SupportEngagementBannerCircles,
+    AcquisitionsEngagementBannerEurSupport,
+    AcquisitionsEngagementBannerAudSupport
+];
