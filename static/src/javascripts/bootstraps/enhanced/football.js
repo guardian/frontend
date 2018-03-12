5fc8136a01d1083a3b03fdf5d2cc7a8fd873bfba
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-03-12 12:25:39 -0400

    remove js for lint

--- static/src/javascripts/bootstraps/enhanced/football.js
+++ static/src/javascripts/bootstraps/enhanced/football.js
@@ -206,8 +206,6 @@ const init = (): void => {
     let dropdownTemplate;
 
     isMatch((match: Object): void => {
-        $('article').addClass('content--has-scores');
-
         extras[0] = {
             ready: false,
         };
