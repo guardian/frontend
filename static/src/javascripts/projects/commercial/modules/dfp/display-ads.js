7dc1c58c7d52ea30d70ff65514d4d8ea116d6795
Jerome Eteve <jerome.eteve@guardian.co.uk>
2018-03-16 11:12:18 +0000

    Avoid invoking prebids requestBids when on a pageSkin

--- static/src/javascripts/projects/commercial/modules/dfp/display-ads.js
+++ static/src/javascripts/projects/commercial/modules/dfp/display-ads.js
@@ -8,8 +8,13 @@ const displayAds = (): void => {
     window.googletag.pubads().enableSingleRequest();
     window.googletag.pubads().collapseEmptyDivs();
     window.googletag.enableServices();
-    // as this is an single request call, only need to make a single display call (to the first ad
-    // slot)
+
+    /*
+     *as this is an single request call, only need to make a single display call (to the first ad
+     * slot)
+     * This will load all adSlots in one go using gpt.js, even though we ask only for the first one,
+     * thanks to 'enableSingleRequest' being set.
+     */
     loadAdvert(dfpEnv.advertsToLoad[0]);
     dfpEnv.advertsToLoad.length = 0;
     pageSkin();
