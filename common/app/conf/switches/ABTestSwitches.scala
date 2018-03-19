858ef9504206a92ffe5593a83dc4fe5323899d79
Joseph Smith <joseph.lochlann@gmail.com>
2018-03-19 17:31:12 +0000

    Extend header test switch

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -92,7 +92,7 @@ trait ABTestSwitches {
     "Point the subscribe link in the header to a subscriptions-only version of the support site",
     owners = Seq(Owner.withGithub("joelochlann")),
     safeState = Off,
-    sellByDate = new LocalDate(2018, 3, 19),
+    sellByDate = new LocalDate(2018, 3, 26),
     exposeClientSide = true
   )
 
