9b45afd110b8c051c5730eac5e46e12a9353105e
Jerome Eteve <jerome.eteve@guardian.co.uk>
2018-03-22 10:13:42 +0000

    First version of dfp-safeframe.js

---
+++ static/vendor/javascripts/dfp-safeframe.js
@@ -0,0 +1,66 @@
+// @flow
+
+// this script can be returned by an ad server delivering a cross domain iframe, into which the
+// creative will be rendered, e.g. DFP delivering a SafeFrame
+
+// It is strongly inspired by Prebid.js/integrationExamples/gpt/x-domain/creative.js
+
+// Use this in combination with a creative modelled on:
+// https://www.google.com/dfp/59666047#delivery/PreviewCreative/orderId=2259406532&lineItemId=4615205472&creativeId=138228201638
+//
+
+var guardian_pb_safeframe = {
+    "adServerDomain": function(){
+        return window.location.protocol + '//tpc.googlesyndication.com';
+    },
+    "renderAd": function(ev) {
+        var key = ev.message ? 'message' : 'data';
+        var adObject = {};
+        try {
+            adObject = JSON.parse(ev[key]);
+        } catch (e) {
+            return;
+        }
+
+        if ( ! ( adObject.ad || adObject.adUrl ) ) {
+            return;
+        }
+
+        console.log("aObject contains an ad or a adUrl:" , adObject );
+
+        var doc = window.document;
+        var ad = adObject.ad;
+        var url = adObject.adUrl;
+        var width = adObject.width;
+        var height = adObject.height;
+        
+        if (adObject.mediaType === 'video') {
+            console.log('Error trying to write ad. mediaType "video" is not supported in adObject = ', adObject);
+            return;
+        }
+
+        if (ad) {
+            doc.write(ad);
+            doc.close();
+            return;
+        }
+        if (url) {
+            doc.write('<IFRAME SRC="' + url + '" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="' + width + '" HEIGHT="' + height + '"></IFRAME>');
+            doc.close();
+            return;
+        }
+    },
+
+    "requestAdFromPrebid": function( adId, publisherDomainURL ) {
+        var message = JSON.stringify({
+            message: 'Prebid Request',
+            adId: adId,
+            adServerDomain: this.adServerDomain()
+        });
+        window.parent.postMessage(message, publisherDomainURL);
+    },
+
+    "listenAdFromPrebid": function(){
+        window.addEventListener('message', this.renderAd, false);
+    }
+};
