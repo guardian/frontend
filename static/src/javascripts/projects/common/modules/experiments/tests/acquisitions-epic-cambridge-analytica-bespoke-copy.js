985827996afe82c258fafbbc9e1f7299c8ef0dab
jranks123 <jonathan.rankin@guardian.co.uk>
2018-03-23 15:44:38 +0000

    initial commit

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-epic-cambridge-analytica-bespoke-copy.js
@@ -0,0 +1,46 @@
+// @flow
+import { makeABTest } from 'common/modules/commercial/contributions-utilities';
+import { keywordExists } from 'lib/page';
+import { cambridgeCopy } from 'common/modules/commercial/acquisitions-copy';
+
+const abTestName = 'AcquisitionsEpicCambridgeAnalyticaBespokeCopy';
+
+export const acquisitionsEpicCambridgeAnalyticaBespokeCopy: EpicABTest = makeABTest(
+    {
+        id: abTestName,
+        campaignId: abTestName,
+
+        start: '2018-03-20',
+        expiry: '2018-04-10',
+
+        author: 'Jonathan Rankin',
+        description: 'Always ask on Cambridge analytica stories',
+        successMeasure: 'Conversion rate',
+        idealOutcome:
+            'We learn the impact of placing an ever-present ask on "moment" stories',
+
+        audienceCriteria: 'All',
+        audience: 1,
+        audienceOffset: 0,
+        canRun: () => keywordExists(['Cambridge Analytica']),
+
+        variants: [
+            {
+                id: 'control',
+                products: [],
+                options: {
+                    isUnlimited: true,
+                },
+            },
+            {
+                id: 'bespoke_copy',
+                products: [],
+                options: {
+                    copy: cambridgeCopy,
+                    testimonialBlock: '',
+                    isUnlimited: true,
+                },
+            },
+        ],
+    }
+);
