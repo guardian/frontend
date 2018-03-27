6750d64c4dc3db7ca25fd98bf7e264e5f333773a
Kelvin Chappell <kelvin.chappell@theguardian.com>
2018-03-27 14:02:07 +0100

    Put switch around endpoints to avoid uncontrolled logging

--- commercial/app/controllers/PrebidAnalyticsController.scala
+++ commercial/app/controllers/PrebidAnalyticsController.scala
@@ -1,19 +1,30 @@
 package commercial.controllers
 
 import common.Logging
-import model.TinyResponse
+import conf.switches.Switches.prebidAnalytics
+import model.Cached.WithoutRevalidationResult
+import model.{CacheTime, Cached, TinyResponse}
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.prettyPrint
 import play.api.mvc._
 
 class PrebidAnalyticsController(val controllerComponents: ControllerComponents) extends BaseController with Logging {
 
+  private def serve404[A](implicit request: Request[A]) =
+    Cached(CacheTime.NotFound)(WithoutRevalidationResult(NotFound))
+
   def insert(): Action[JsValue] = Action(parse.json) { implicit request =>
-    log.info(prettyPrint(request.body))
-    TinyResponse.noContent()
+    if (prebidAnalytics.isSwitchedOn) {
+      log.info(prettyPrint(request.body))
+      TinyResponse.noContent()
+    } else
+      serve404
   }
 
   def getOptions: Action[AnyContent] = Action { implicit request =>
-    TinyResponse.noContent(Some("PUT, OPTIONS"))
+    if (prebidAnalytics.isSwitchedOn)
+      TinyResponse.noContent(Some("OPTIONS, PUT"))
+    else
+      serve404
   }
 }
