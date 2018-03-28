ed83da656d38dfc2db9bd7f2e2a25c17932ace98
Calum Campbell <campbell.cj@hotmail.co.uk>
2018-03-28 13:37:30 +0100

    bump identity to 135

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ package com.gu
 import sbt._
 
 object Dependencies {
-  val identityLibVersion = "3.134"
+  val identityLibVersion = "3.135"
   val awsVersion = "1.11.240"
   val capiVersion = "11.51"
   val faciaVersion = "2.5.3"
