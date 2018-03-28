77344ccf0388741bfe90f997cc0067dc85c92530
jranks123 <jonathan.rankin@guardian.co.uk>
2018-03-28 16:03:44 +0100

    remove banner tests

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -128,26 +128,6 @@ trait ABTestSwitches {
 
   Switch(
     ABTests,
-    "ab-acquisitions-engagement-banner-uk-17-pence",
-    "Tests a CTA message that aims to push people towards recurring contributions in the UK",
-    owners = Seq(Owner.withGithub("jranks123")),
-    safeState = Off,
-    sellByDate = new LocalDate(2018, 4, 24),
-    exposeClientSide = true
-  )
-
-  Switch(
-    ABTests,
-    "ab-acquisitions-engagement-banner-us-23-cents",
-    "Tests a CTA message that aims to push people towards recurring contributions in the US",
-    owners = Seq(Owner.withGithub("jranks123")),
-    safeState = Off,
-    sellByDate = new LocalDate(2018, 4, 24),
-    exposeClientSide = true
-  )
-
-  Switch(
-    ABTests,
     "ab-acquisitions-epic-cambridge-analytica-always-ask-final",
     "turn on always ask for CA stories",
     owners = Seq(Owner.withGithub("jranks123")),
