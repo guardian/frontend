fd8578c9a5183eb1a1d3c09af2a1de8b68a1eb97
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-03-29 16:49:07 -0400

    font sizes innit

--- static/src/javascripts/projects/common/modules/charts/doughnut.js
+++ static/src/javascripts/projects/common/modules/charts/doughnut.js
@@ -155,7 +155,7 @@ const Doughnut = (data: Object, o: Object): bonzo => {
                         .text(datum.value)
                         .attr({
                             x: 0,
-                            dy: '1em',
+                            dy: '.8em',
                         })
                 );
         } else {
