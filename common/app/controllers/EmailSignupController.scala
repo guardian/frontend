d9024e73e24f1475939a40c9630fe114e8908321
Rahil Bohra <rahil@bohra.co.uk>
2018-04-04 12:20:06 +0100

    Add CSRF token to email widget (#19473)
    * CSRF Protection: engaged
    * csrf form field++
    * Remove unused arg
    * Add CSRF token to response
    * Remove CSRF check, it will be added later to avoid caching issues

--- common/app/controllers/EmailSignupController.scala
+++ common/app/controllers/EmailSignupController.scala
@@ -14,6 +14,7 @@ import play.api.data.validation.Constraints.emailAddress
 import play.api.libs.json._
 import play.api.libs.ws.{WSClient, WSResponse}
 import play.api.mvc._
+import play.filters.csrf.{CSRFAddToken, CSRFCheck}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -60,7 +61,7 @@ class EmailFormService(wsClient: WSClient) extends LazyLogging {
   }
 }
 
-class EmailSignupController(wsClient: WSClient, val controllerComponents: ControllerComponents)(implicit context: ApplicationContext) extends BaseController with ImplicitControllerExecutionContext with Logging {
+class EmailSignupController(wsClient: WSClient, val controllerComponents: ControllerComponents, csrfCheck: CSRFCheck, csrfAddToken: CSRFAddToken)(implicit context: ApplicationContext) extends BaseController with ImplicitControllerExecutionContext with Logging {
   val emailFormService = new EmailFormService(wsClient)
 
   val emailForm: Form[EmailForm] = Form(
@@ -77,25 +78,29 @@ class EmailSignupController(wsClient: WSClient, val controllerComponents: Contro
     Cached(60)(RevalidatableResult.Ok(views.html.emailLanding(emailLandingPage)))
   }
 
-  def renderForm(emailType: String, listId: Int): Action[AnyContent] = Action { implicit request =>
+  def renderForm(emailType: String, listId: Int): Action[AnyContent] = csrfAddToken {
+    Action { implicit request =>
+      val identityName = EmailNewsletter(listId)
+        .orElse(EmailNewsletter.fromV1ListId(listId))
+        .map(_.identityName)
 
-    val identityName = EmailNewsletter(listId)
-                        .orElse(EmailNewsletter.fromV1ListId(listId))
-                        .map(_.identityName)
-
-    identityName match {
-      case Some(listName) => Cached(1.day)(RevalidatableResult.Ok(views.html.emailFragment(emailLandingPage, emailType, listName)))
-      case _ => Cached(15.minute)(WithoutRevalidationResult(NotFound))
+      identityName match {
+        case Some(listName) => Cached(1.day)(RevalidatableResult.Ok(views.html.emailFragment(emailLandingPage, emailType, listName)))
+        case _ => Cached(15.minute)(WithoutRevalidationResult(NotFound))
+      }
     }
   }
 
-  def renderFormFromName(emailType: String, listName: String): Action[AnyContent] = Action { implicit request =>
-    val id = EmailNewsletter.fromIdentityName(listName).map(_.listIdV1)
-    id match {
-      case Some(listId) => Cached(1.day)(RevalidatableResult.Ok(views.html.emailFragment(emailLandingPage, emailType, listName)))
-      case _            => Cached(15.minute)(WithoutRevalidationResult(NotFound))
+  def renderFormFromName(emailType: String, listName: String): Action[AnyContent] = csrfAddToken {
+    Action { implicit request =>
+      val id = EmailNewsletter.fromIdentityName(listName).map(_.listIdV1)
+      id match {
+        case Some(listId) => Cached(1.day)(RevalidatableResult.Ok(views.html.emailFragment(emailLandingPage, emailType, listName)))
+        case _            => Cached(15.minute)(WithoutRevalidationResult(NotFound))
+      }
     }
   }
+
 
   def subscriptionResult(result: String): Action[AnyContent] = Action { implicit request =>
     Cached(7.days)(result match {
@@ -136,23 +141,23 @@ class EmailSignupController(wsClient: WSClient, val controllerComponents: Contro
         Future.successful(respond(InvalidEmail))},
 
       form => emailFormService.submit(form).map(_.status match {
-          case 200 | 201 =>
-            EmailSubmission.increment()
-            respond(Subscribed)
+        case 200 | 201 =>
+          EmailSubmission.increment()
+          respond(Subscribed)
 
-          case status =>
-            log.error(s"Error posting to ExactTarget: HTTP $status")
-            APIHTTPError.increment()
-            respond(OtherError)
+        case status =>
+          log.error(s"Error posting to ExactTarget: HTTP $status")
+          APIHTTPError.increment()
+          respond(OtherError)
 
-        }) recover {
-          case _: IllegalAccessException =>
-            respond(Subscribed)
-          case e: Exception =>
-            log.error(s"Error posting to ExactTarget: ${e.getMessage}")
-            APINetworkError.increment()
-            respond(OtherError)
-        })
+      }) recover {
+        case _: IllegalAccessException =>
+          respond(Subscribed)
+        case e: Exception =>
+          log.error(s"Error posting to ExactTarget: ${e.getMessage}")
+          APINetworkError.increment()
+          respond(OtherError)
+      })
   }
 
   def options(): Action[AnyContent] = Action { implicit request =>
