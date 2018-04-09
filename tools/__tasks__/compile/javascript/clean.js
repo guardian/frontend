d65123d6be27e2e9fa32fa9f35f96ac998253e91
Simon Adcock <simon.adcock@guardian.co.uk>
2018-04-09 17:04:45 +0100

    there is no transpiled dir

--- tools/__tasks__/compile/javascript/clean.js
+++ tools/__tasks__/compile/javascript/clean.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const rimraf = require('rimraf');
 
-const { target, hash, transpiled } = require('../../config').paths;
+const { target, hash } = require('../../config').paths;
 
 module.exports = {
-    description: 'Clear JS build artefacts',
+    description: 'Clear JS build artifacts',
     task: () => {
         rimraf.sync(path.resolve(target, 'javascripts'));
-        rimraf.sync(path.resolve(transpiled, 'javascripts'));
         rimraf.sync(path.resolve(hash, 'javascripts'));
     },
 };
