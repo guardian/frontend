86a3a657dbca0d455dee8c3f3a66b1c9ce1459a0
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-04-11 14:44:06 +0100

    Bump lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   val identityLibVersion = "3.135"
   val awsVersion = "1.11.240"
-  val capiVersion = "11.51"
+  val capiVersion = "12.0"
   val faciaVersion = "2.5.4"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
