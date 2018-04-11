7a47663a10bbd28965889eb2bcf9f6356aaec7e9
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-04-11 08:48:47 +0100

    Removed AUD support related tests

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,6 +1,5 @@
 // @flow
-import { AcquisitionsEngagementBannerAudSupport } from 'common/modules/experiments/tests/acquisitions-engagement-banner-aud-support';
 
 export const membershipEngagementBannerTests: $ReadOnlyArray<
     AcquisitionsABTest
-> = [AcquisitionsEngagementBannerAudSupport];
+> = [];
