8268f0d41ba8134ae3bb5ed2c0da85a68f44e4a8
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-04-13 16:57:13 +0100

    more database files.

--- common/app/services/SkimLinksCache.scala
+++ common/app/services/SkimLinksCache.scala
@@ -6,14 +6,12 @@ import scala.util.Try
 
 object SkimLinksCache extends Logging {
 
-  log.info("INITIALISING SKIMLINKSCACHE")
-
   // evil mutable variable here so that we can cache skimlinks on startup
   private var skimLinks = Set[String]()
 
   def getSkimLinks: Set[String] = {
     if (skimLinks.nonEmpty) {
-      log.info(s"Already got links: $skimLinks")
+      log.info(s"Already got ${skimLinks.size} links")
       skimLinks
     } else {
       log.info("Fetching and caching skimlinks")
@@ -25,14 +23,10 @@ object SkimLinksCache extends Logging {
       skimLinks
     }
   }
-  // initialise immediately upon startup
-  getSkimLinks
 
   def isSkimLink(link: String): Boolean = {
-    log.info(s"ANALYZING LINK FOR SKIMLINKS $link")
     val uri: Option[URL] = Try(new URL(link)).toOption
     uri.exists(u => {
-      log.info(s"url we're doing: $u")
       // strip the www. subdomain as it is not included in the list of domains from the skimlinks api
       val cleanedHost = u.getHost.replace("www.", "")
       getSkimLinks.contains(cleanedHost)
