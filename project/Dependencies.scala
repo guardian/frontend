da5d70939f91b7dbcfa0933e2b54e65f915632e3
Rahil Bohra <rahil.bohra.freelance@guardian.co.uk>
2018-04-13 12:06:26 +0100

    Add Optout Boxes to email prefs and consent journeys

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ package com.gu
 import sbt._
 
 object Dependencies {
-  val identityLibVersion = "3.134-SNAPSHOT"
+  val identityLibVersion = "3.137"
   val awsVersion = "1.11.240"
   val capiVersion = "11.51"
   val faciaVersion = "2.5.4"
