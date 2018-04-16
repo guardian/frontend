e5390643dd4c4106b270f310a9b975961de7aded
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-04-16 11:50:52 +0100

    Updated subscribe url

--- static/src/javascripts/projects/common/modules/commercial/support-utilities.js
+++ static/src/javascripts/projects/common/modules/commercial/support-utilities.js
@@ -1,5 +1,5 @@
 // @flow
 const supportContributeURL = 'https://support.theguardian.com/contribute';
-const supportSubscribeURL = 'https://support.theguardian.com/contribute';
+const supportSubscribeURL = 'https://support.theguardian.com/subscribe';
 
 export { supportContributeURL, supportSubscribeURL };
