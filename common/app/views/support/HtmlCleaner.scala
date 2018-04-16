fcbc8f9788a249513bb26224f97a72487293a19b
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-04-16 13:55:41 +0100

    Make skimlinks cache use atomic reference initialised as lifecycle event.

--- common/app/views/support/HtmlCleaner.scala
+++ common/app/views/support/HtmlCleaner.scala
@@ -802,9 +802,7 @@ object GarnettQuoteCleaner extends HtmlCleaner {
 case class AffiliateLinksCleaner(pageUrl: String, sectionId: String, showAffiliateLinks: Option[Boolean], contentType: String, appendDisclaimer: Boolean = true) extends HtmlCleaner with Logging {
 
   override def clean(document: Document): Document = {
-    log.info(s"CLEANING SKIMLINKS ${AffiliateLinks.isSwitchedOn} $sectionId $showAffiliateLinks ${AffiliateLinkSections.isSwitchedOn}")
     if (AffiliateLinks.isSwitchedOn && AffiliateLinksCleaner.shouldAddAffiliateLinks(AffiliateLinkSections.isSwitchedOn, sectionId, showAffiliateLinks)) {
-      log.info("SKIMLINKS SHOULD BE CLEANED")
       val cleanedResult = AffiliateLinksCleaner.replaceLinksInHtml(document, pageUrl)
       if (cleanedResult.affiliateLinksAdded && appendDisclaimer) {
         AffiliateLinksCleaner.insertAffiliateDisclaimer(cleanedResult.document, contentType)
