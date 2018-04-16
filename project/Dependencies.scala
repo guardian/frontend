6cf49d35bef0c66b7cc8e2a2a229a41c0d8e8e0f
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-04-16 11:55:08 +0100

    Bump facia scala client version to resolve capi client dependency problem.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val identityLibVersion = "3.139"
   val awsVersion = "1.11.240"
   val capiVersion = "11.55"
-  val faciaVersion = "2.5.4"
+  val faciaVersion = "2.5.5"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
