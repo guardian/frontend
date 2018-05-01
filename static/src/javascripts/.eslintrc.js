febbe0ce586107a33c27e88676fae83ae717caa8
Simon Adcock <simon.adcock@guardian.co.uk>
2018-05-01 20:28:12 +0100

    delete unused react-specific lint overrides

--- static/src/javascripts/.eslintrc.js
+++ static/src/javascripts/.eslintrc.js
@@ -30,17 +30,11 @@ module.exports = {
         // if we go more [p]react we should look at them.
         // not saying it's ok, but we don't reuse modules or
         // develop this stuff much. disabling for now.
-        'react/prefer-es6-class': 'off',
         'react/no-multi-comp': 'off',
         'react/no-find-dom-node': 'off',
         'react/jsx-no-bind': 'off',
-        'react/no-deprecated': 'off', // still on 0.13
-        'react/prop-types': 'off',
         'react/no-string-refs': 'off',
         'react/prefer-stateless-function': 'off',
-        'react/no-render-return-value': 'off',
-        'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4],
         'react/jsx-filename-extension': 'off',
 
         // disallow naming variables 'guardian', because
