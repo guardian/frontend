af77caf2fb301da2b3594e4e4da790950174c7d3
Regis Kuckaertz <regis_kuckaertz@33020.gnm.int>
2018-05-03 10:02:16 +0100

    Lint

--- static/src/javascripts/bootstraps/enhanced/audio.js
+++ static/src/javascripts/bootstraps/enhanced/audio.js
@@ -4,4 +4,4 @@ import { init as initTracking } from 'common/modules/atoms/audio-tracking';
 
 export const init = () => {
     initTracking();
-}
+};
--- static/src/javascripts/projects/common/modules/atoms/audio-tracking.js
+++ static/src/javascripts/projects/common/modules/atoms/audio-tracking.js
@@ -1,28 +1,34 @@
 // @flow
-import config from 'lib/config';
 import ophan from 'ophan/ng';
 
-const audioSelector = '.gu-media--audio'
+const audioSelector = '.gu-media--audio';
 
 // Sends a media event to Ophan
 const record = (mediaId: String, eventType: String) => {
-    ophan.record({ audio: { id: `gu-audio-${mediaId}`, eventType: `audio:content:${eventType}`}});
-}
+    ophan.record({
+        audio: {
+            id: `gu-audio-${mediaId}`,
+            eventType: `audio:content:${eventType}`,
+        },
+    });
+};
 
-const constant = a => b => a;
+const constant = a => b => a; // eslint-disable-line no-unused-vars
 
 // Determines whether a reader has listened up to pct% of the audio track
-const percent = (pct: number) => (event: Event): Boolean => 
-    event instanceof ProgressEvent && 
-    event.lengthComputable && 
-    Math.floor(event.loaded / event.total * 100) == pct;
+const percent = (pct: number) => (event: Event): Boolean =>
+    event instanceof ProgressEvent &&
+    event.lengthComputable &&
+    Math.floor(event.loaded / event.total * 100) === pct;
 
 // Sends an `eventType` only if `pred(event)`
-const recordIf = (pred: Event => Boolean, eventType: String): void => (event: Event) => {
+const recordIf = (pred: Event => Boolean, eventType: String): void => (
+    event: Event
+) => {
     if (pred(event)) {
         record(event.target.getAttribute('data-media-id'), eventType);
     }
-}
+};
 
 const init = (): void => {
     const audios = Array.from(document.querySelectorAll(audioSelector));
@@ -32,9 +38,9 @@ const init = (): void => {
         ['progress', recordIf(percent(25), '25')],
         ['progress', recordIf(percent(50), '50')],
         ['progress', recordIf(percent(75), '75')],
-        ['ended', recordIf(constant(true), 'end')]
-    };
-    
+        ['ended', recordIf(constant(true), 'end')],
+    ];
+
     events.forEach(([eventType, action]) => {
         // Just in case there is more than one audio on the page,
         // we delegate to the document
