12822fadd923b93515002df546812b56d2d0bb06
GHaberis <george.haberis@theguardian.com>
2018-05-03 12:14:47 +0100

    remove async nature of bannerPicker tests

--- static/src/javascripts/projects/common/modules/ui/bannerPicker.spec.js
+++ static/src/javascripts/projects/common/modules/ui/bannerPicker.spec.js
@@ -1,103 +1,192 @@
 // @flow
 import { init } from 'common/modules/ui/bannerPicker';
 
-const shortTrueCheck = () =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve(true);
-        }, 100);
-    });
-const shortFalseCheck = () =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve(false);
-        }, 100);
-    });
-const longFalseCheck = () =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve(false);
-        }, 200);
-    });
-const longTrueCheck = () =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve(true);
-        }, 200);
-    });
-const extraLongTrueCheck = () =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve(true);
-        }, 2500);
-    });
-const extraLongFalseCheck = () =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve(false);
-        }, 2500);
-    });
-
 describe('bannerMediator picks correct banner to show', () => {
+    // resultsSeq is the order banner's checks will resolve in the test
     const tests = [
         {
-            checks: [shortTrueCheck],
+            description:
+                'should call show() for successful bannerIndex 0 when no other checks in list',
+            resultsSeq: [
+                {
+                    bannerIndex: 0,
+                    value: true,
+                },
+            ],
             successfulIndex: 0,
         },
         {
-            checks: [shortFalseCheck, shortTrueCheck],
+            description:
+                'should call show() for successful bannerIndex 1 when bannerIndex 0 fails',
+            resultsSeq: [
+                {
+                    bannerIndex: 0,
+                    value: false,
+                },
+                {
+                    bannerIndex: 1,
+                    value: true,
+                },
+            ],
             successfulIndex: 1,
         },
         {
-            checks: [longFalseCheck, shortTrueCheck],
-            successfulIndex: 1,
-        },
-        {
-            checks: [longTrueCheck, shortTrueCheck],
+            description:
+                'should call show() for successful bannerIndex 0 even if bannerIndex 1 passes',
+            resultsSeq: [
+                {
+                    bannerIndex: 0,
+                    value: true,
+                },
+                {
+                    bannerIndex: 1,
+                    value: true,
+                },
+            ],
             successfulIndex: 0,
         },
         {
-            checks: [
-                longFalseCheck,
-                shortFalseCheck,
-                longTrueCheck,
-                shortTrueCheck,
-            ],
-            successfulIndex: 2,
-        },
-        {
-            checks: [extraLongTrueCheck, shortTrueCheck],
-            successfulIndex: 1,
-        },
-        {
-            checks: [extraLongTrueCheck, longFalseCheck, shortTrueCheck],
-            successfulIndex: 2,
-        },
-        {
-            checks: [
-                extraLongFalseCheck,
-                longTrueCheck,
-                shortTrueCheck,
-                extraLongTrueCheck,
+            description:
+                'should call show() for successful bannerIndex 1 when bannerIndex 1 fails',
+            resultsSeq: [
+                {
+                    bannerIndex: 0,
+                    value: false,
+                },
+                {
+                    bannerIndex: 1,
+                    value: true,
+                },
             ],
             successfulIndex: 1,
+        },
+        {
+            description:
+                'should call show() for successful bannerIndex 0 even if bannerIndex 1 passes first',
+            resultsSeq: [
+                {
+                    bannerIndex: 1,
+                    value: true,
+                },
+                {
+                    bannerIndex: 0,
+                    value: true,
+                },
+            ],
+            successfulIndex: 0,
+        },
+        {
+            description:
+                'should call show() for successful bannerIndex 2 when higher priority bannerIndexes 0 & 1 fail',
+            resultsSeq: [
+                {
+                    bannerIndex: 1,
+                    value: false,
+                },
+                {
+                    bannerIndex: 3,
+                    value: true,
+                },
+                {
+                    bannerIndex: 2,
+                    value: true,
+                },
+                {
+                    bannerIndex: 0,
+                    value: false,
+                },
+            ],
+            successfulIndex: 2,
         },
     ];
 
-    const createBanner = canShow => ({ canShow, show: jest.fn() });
-
     tests.forEach(test => {
-        it(`calls show() for banner at index ${test.successfulIndex}`, () => {
-            const bannerList = test.checks.map(createBanner);
+        it(test.description, () => {
+            const resolveList = [];
 
-            return init(bannerList).then(() => {
-                bannerList.forEach((check, index) => {
+            const newCheck = () => () =>
+                new Promise(resolve => {
+                    resolveList.push(resolve);
+                });
+
+            const createBanner = result => ({
+                index: result.bannerIndex,
+                canShow: newCheck(),
+                show: jest.fn(),
+            });
+
+            // create a list to be populated with banners
+            const banners = new Array(test.resultsSeq.length);
+
+            // create a banner for each result and add to bannerList
+            test.resultsSeq.forEach(result => {
+                banners[result.bannerIndex] = createBanner(result);
+            });
+
+            // pass banners to bannerPicker
+            const asyncTest = init(banners);
+
+            // resolve each banners check in the order specified by the result sequence
+            test.resultsSeq.forEach(result => {
+                resolveList[result.bannerIndex](result.value);
+            });
+
+            // test whether the banner canShow function has been called
+            return asyncTest.then(() => {
+                banners.forEach((banner, index) => {
                     if (index === test.successfulIndex) {
-                        expect(bannerList[index].show).toHaveBeenCalled();
+                        expect(banner.show).toHaveBeenCalled();
                     } else {
-                        expect(bannerList[index].show).not.toHaveBeenCalled();
+                        expect(banner.show).not.toHaveBeenCalled();
                     }
                 });
+            });
+        });
+    });
+});
+
+describe('bannerMediator picks correct banner to show when check timesout', () => {
+    it('should call show() for banner at index 1 if index 0 timesout', () => {
+        const resolveList = [];
+
+        const newCheck = () => () =>
+            new Promise(resolve => {
+                resolveList.push(resolve);
+            });
+
+        const banners = [
+            {
+                canShow: newCheck(),
+                show: jest.fn(),
+            },
+            {
+                canShow: newCheck(),
+                show: jest.fn(),
+            },
+        ];
+
+        const asyncTest = init(banners);
+
+        /**
+         * after resolving the canShow check for bannerIndex 1
+         * call jest.runOnlyPendingTimers, this will execute the timeout
+         * on bannerIndex 0, in affect simulating bannerIndex 0's canShow
+         * taking too long to resolve
+         * */
+        banners[1].canShow().then(() => {
+            jest.runOnlyPendingTimers();
+        });
+
+        // resolve bannerIndex 1 canshow check
+        resolveList[1](true);
+
+        return asyncTest.then(() => {
+            banners.forEach((banner, index) => {
+                if (index === 1) {
+                    expect(banner.show).toHaveBeenCalled();
+                } else {
+                    expect(banner.show).not.toHaveBeenCalled();
+                }
             });
         });
     });
