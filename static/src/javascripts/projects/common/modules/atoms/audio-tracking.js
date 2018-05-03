737db59572255a1b3c43bb29b4e33fb0f6bada70
Regis Kuckaertz <regis_kuckaertz@33020.gnm.int>
2018-05-03 10:32:52 +0100

    lowercase scala types

--- static/src/javascripts/projects/common/modules/atoms/audio-tracking.js
+++ static/src/javascripts/projects/common/modules/atoms/audio-tracking.js
@@ -4,7 +4,7 @@ import ophan from 'ophan/ng';
 const audioSelector = '.gu-media--audio';
 
 // Sends a media event to Ophan
-const record = (mediaId: String, eventType: String) => {
+const record = (mediaId: string, eventType: string) => {
     ophan.record({
         audio: {
             id: `gu-audio-${mediaId}`,
@@ -13,26 +13,26 @@ const record = (mediaId: String, eventType: String) => {
     });
 };
 
-const constant = a => b => a; // eslint-disable-line no-unused-vars
+const constant = <A>(a: A) => (): A => a;
 
 // Determines whether a reader has listened up to pct% of the audio track
-const percent = (pct: number) => (event: Event): Boolean =>
+const percent = (pct: number) => (event: Event): boolean =>
     event instanceof ProgressEvent &&
     event.lengthComputable &&
     Math.floor(event.loaded / event.total * 100) === pct;
 
 // Sends an `eventType` only if `pred(event)`
-const recordIf = (pred: Event => Boolean, eventType: String): void => (
+const recordIf = (pred: Event => boolean, eventType: string) => (
     event: Event
-) => {
+): void => {
     if (pred(event)) {
-        record(event.target.getAttribute('data-media-id') || '', eventType);
+        record(((event.target: any): HTMLElement).getAttribute('data-media-id') || '', eventType);
     }
 };
 
 const init = (): void => {
     const audios = Array.from(document.querySelectorAll(audioSelector));
-    const events = [
+    const events: Array<[string, Event => void]> = [
         ['canplay', recordIf(constant(true), 'READY')],
         ['playing', recordIf(constant(true), 'play')],
         ['progress', recordIf(percent(25), '25')],
@@ -41,7 +41,7 @@ const init = (): void => {
         ['ended', recordIf(constant(true), 'end')],
     ];
 
-    events.forEach(([eventType, action]: [String, Function]) => {
+    events.forEach(([eventType, action]: [string, Event => void]) => {
         // Just in case there is more than one audio on the page,
         // we delegate to the document
         document.addEventListener(
