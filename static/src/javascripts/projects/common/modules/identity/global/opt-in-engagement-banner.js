3b6ec13b053769c5d6daca2f5f9c5ff8fffd5766
Laura <laura.gonzalez@guardian.co.uk>
2018-05-11 16:13:10 +0100

    bugfix

--- static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/identity/global/opt-in-engagement-banner.js
@@ -9,7 +9,7 @@ import { local } from 'lib/storage';
 import ophan from 'ophan/ng';
 import mediator from 'lib/mediator';
 import userPrefs from 'common/modules/user-prefs';
-import { isParticipating } from 'common/modules/experiments/utils';
+import { getVariant, isInVariant } from 'common/modules/experiments/utils';
 import { signInEngagementBannerDisplay } from 'common/modules/experiments/tests/sign-in-engagement-banner-display';
 import type { LinkTargets, Template } from './opt-in-eb-template';
 import { makeTemplateHtml } from './opt-in-eb-template';
@@ -90,9 +90,12 @@ const shouldDisplayIfNotAlreadyDismissed = (): boolean =>
 
 const shouldDisplayBasedOnMedium = (): boolean => userVisitedViaNewsletter();
 
-/* Test must be running & user must be in variant */
-const shouldDisplayifNotInSignInTestVariant = (): boolean =>
-    !isParticipating(signInEngagementBannerDisplay);
+/* User must not be in variant */
+const shouldDisplayifNotInSignInTestVariant = (): boolean => {
+    const variant = getVariant(signInEngagementBannerDisplay, 'variant');
+    if (!variant) return true;
+    return !isInVariant(signInEngagementBannerDisplay, variant);
+};
 
 const getDisplayConditions = (): boolean[] => {
     const basics = [
