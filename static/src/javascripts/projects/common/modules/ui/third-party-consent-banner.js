53a302917323a6c694c951a720bbdf5d0ac8cc64
Laura <laura.gonzalez@guardian.co.uk>
2018-05-15 15:17:07 +0100

    add button styles

--- static/src/javascripts/projects/common/modules/ui/third-party-consent-banner.js
+++ static/src/javascripts/projects/common/modules/ui/third-party-consent-banner.js
@@ -5,7 +5,9 @@ import { Message } from 'common/modules/ui/message';
 const canShow: Promise<boolean> = () => Promise.resolve(true);
 
 const text = {
-    consent: 'We use cookies & ads & all that jazz.'
+    consent: 'We use cookies & ads & <a href="#">all that</a> jazz.',
+    agreeButton: 'I agree',
+    choicesButton: 'More choices',
 };
 
 const bindableClassNames = {
@@ -16,8 +18,8 @@ const bindableClassNames = {
 const templateHtml = `
     <div class="site-message--third-party-consent__intro">${text.consent}</div>
     <div class="site-message--third-party-consent__actions">
-        <button class="site-message--third-party-consent__button ${bindableClassNames.agree}">I agree</button>
-        <a href="#" class="site-message--third-party-consent__button ${bindableClassNames.choices}">More choices</a>
+        <a href="#" class="site-message--third-party-consent__button site-message--third-party-consent__button--link ${bindableClassNames.choices}">${text.choicesButton}</a>
+        <button class="site-message--third-party-consent__button site-message--third-party-consent__button--main ${bindableClassNames.agree}">${text.agreeButton}</button>
     </div>
 `;
 
