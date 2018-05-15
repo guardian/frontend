ef5f3b1f27d772567930317b532772b664339c12
Laura <laura.gonzalez@guardian.co.uk>
2018-05-15 16:09:52 +0100

    fix tests

--- static/src/javascripts/projects/common/modules/identity/ad-prefs.lib.spec.js
+++ static/src/javascripts/projects/common/modules/identity/ad-prefs.lib.spec.js
@@ -39,13 +39,13 @@ describe('getAdConsentState', () => {
 describe('setAdConsentState', () => {
     it('should set a full proper cookie', () => {
         setAdConsentState(testConsent, true);
-        expect(addCookie.mock.calls[0][0]).toMatch('GU_AD_CONSENT_TEST');
-        expect(addCookie.mock.calls[0][1]).toMatch('1');
-        expect(addCookie.mock.calls[0][2]).toBeGreaterThanOrEqual(365 * 4);
+        expect(addCookie.mock.calls[0][0]).toBe('GU_AD_CONSENT_TEST');
+        expect(addCookie.mock.calls[0][1]).toBe('1');
+        expect(addCookie.mock.calls[0][2]).toBe(365 * 6);
         expect(addCookie.mock.calls[0][3]).toBe(true);
     });
     it('should set a false cookie', () => {
         setAdConsentState(testConsent, false);
-        expect(addCookie.mock.calls[0][1]).toMatch('0');
+        expect(addCookie.mock.calls[0][1]).toBe('0');
     });
 });
