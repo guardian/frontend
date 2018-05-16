d30d6de8e494746e8e32225901efda4e89fb41b6
Kelvin Chappell <kelvin.chappell@theguardian.com>
2018-05-16 15:51:07 +0100

    Move ad-prefs lib to commercial module

--- static/src/javascripts/projects/commercial/modules/dfp/prepare-googletag.js
+++ static/src/javascripts/projects/commercial/modules/dfp/prepare-googletag.js
@@ -9,6 +9,10 @@ import raven from 'lib/raven';
 import sha1 from 'lib/sha1';
 import { session } from 'lib/storage';
 
+import {
+    getAdConsentState,
+    thirdPartyTrackingAdConsent,
+} from 'common/modules/commercial/ad-prefs.lib';
 import { buildPageTargeting } from 'common/modules/commercial/build-page-targeting';
 import { commercialFeatures } from 'common/modules/commercial/commercial-features';
 
@@ -36,10 +40,6 @@ import { init as resize } from 'commercial/modules/messenger/resize';
 import { init as scroll } from 'commercial/modules/messenger/scroll';
 import { init as type } from 'commercial/modules/messenger/type';
 import { init as viewport } from 'commercial/modules/messenger/viewport';
-import {
-    getAdConsentState,
-    thirdPartyTrackingAdConsent,
-} from 'common/modules/identity/ad-prefs.lib';
 
 initMessenger(
     type,
