386fb7bc3d0248dc48b5e2b8781b543e6b5ffe17
guy.dawson <guy.dawson@theguardian.com>
2018-05-16 17:29:13 +0100

    update dfp test

--- static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-epic-native-vs-dfp.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-epic-native-vs-dfp.js
@@ -3,6 +3,21 @@ import { makeABTest } from 'common/modules/commercial/contributions-utilities';
 import type { AdBlockEpicTemplate } from 'common/modules/commercial/contributions-utilities';
 import { createSlots } from "commercial/modules/dfp/create-slots";
 
+// The adblockInUse boolean in detect.js is (necessarily) a promise.
+// This does not interact well with the synchronous canRun() function expected by the AB testing framework.
+// To mitigate against this, create a function which returns false <=>
+// ad blocker is active or it is unresolved whether it is active.
+// Since the test running is predicated on this function returning true,
+// readers in the test will have ad blocker off,
+// but we might not include all readers who have ad blocker off.
+const isAdBlockerOff = (): boolean => {
+    const isActive = window.guardian.adBlockers.active;
+    if (isActive === null || isActive === undefined) {
+        return false;
+    }
+    return !isActive;
+};
+
 const adSlotEpicTemplate: AdBlockEpicTemplate = (): HTMLElement => {
     const adSlots = createSlots('epic', {});
     return adSlots[0];
@@ -11,28 +26,29 @@ const adSlotEpicTemplate: AdBlockEpicTemplate = (): HTMLElement => {
 export const acquisitionsEpicNativeVsDfp = makeABTest({
     id: 'AcquisitionsEpicNativeVsDfp',
     campaignId: 'epic_native_vs_dfp',
-    start: '2018-04-19',
-    expiry: '2018-05-19', // TODO
+    start: '2018-05-16', // TODO: update
+    expiry: '2018-05-19', // TODO: update
     author: 'Guy Dawson',
     description: 'See if there is any difference in annualised value between serving the Epic natively vs DFP',
     successMeasure: 'AV2.0',
     idealOutcome: 'There is no difference between these two methods of serving the Epic',
     audienceCriteria: 'All',
-    audience: 1, // TODO
-    audienceOffset: 0, // TODO
+    audience: 1, // TODO: update
+    audienceOffset: 0, // TODO: update
+    canRun: isAdBlockerOff,
     variants: [
         {
             id: 'control',
             products: ['CONTRIBUTION'],
             options: {
-                isUnlimited: true,
+                isUnlimited: true, // TODO: remove
             },
         },
         {
             id: 'dfp',
             products: ['CONTRIBUTION'],
             options: {
-                isUnlimited: true,
+                isUnlimited: true, // TODO: remove
                 adSlotEpicTemplate: adSlotEpicTemplate,
             },
         }
