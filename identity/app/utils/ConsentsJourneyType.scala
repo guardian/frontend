b48d02edc4de1d8e69e8045b52968ebcf5f82aff
Jon Soul <jonathan.soul@guardian.co.uk>
2018-05-22 20:03:38 +0100

    Remove newsletter types

--- identity/app/utils/ConsentsJourneyType.scala
+++ identity/app/utils/ConsentsJourneyType.scala
@@ -4,7 +4,6 @@ object ConsentsJourneyType {
 
   sealed trait AnyConsentsJourney
 
-  case object NewsletterConsentsJourney extends AnyConsentsJourney
   case object ThankYouConsentsJourney extends AnyConsentsJourney
   case object GdprCampaignConsentsJourney extends AnyConsentsJourney
   case object DefaultConsentsJourney extends AnyConsentsJourney
