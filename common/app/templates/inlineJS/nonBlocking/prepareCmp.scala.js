d8a8b4d59c4572f62f30207c5afd894561b2e379
Kate Whalen <whalenka88@gmail.com>
2018-05-24 12:38:25 +0100

    Move where we check that the CMP Service is switched on

--- common/app/templates/inlineJS/nonBlocking/prepareCmp.scala.js
+++ common/app/templates/inlineJS/nonBlocking/prepareCmp.scala.js
@@ -1,72 +1,69 @@
 @()(implicit context: model.ApplicationContext)
 
-@import conf.switches.Switches.enableConsentManagementService
 @import play.api.Mode.Dev
 
-@if(enableConsentManagementService.isSwitchedOn) {
-    try {
-        (function(document, window) {
-            if (!window.__cmp) {
-                window.__cmp = (function() {
-                    var listen = window.attachEvent || window.addEventListener;
-                    listen('message', function(event) {
-                        window.__cmp.receiveMessage(event);
-                    }, false);
+try {
+    (function(document, window) {
+        if (!window.__cmp) {
+            window.__cmp = (function() {
+                var listen = window.attachEvent || window.addEventListener;
+                listen('message', function(event) {
+                    window.__cmp.receiveMessage(event);
+                }, false);
 
-                    function addLocatorFrame() {
-                        if (!window.frames['__cmpLocator']) {
-                            if (document.body) {
-                                var frame = document.createElement('iframe');
-                                frame.style.display = 'none';
-                                frame.name = '__cmpLocator';
-                                document.body.appendChild(frame);
-                            } else {
-                                setTimeout(addLocatorFrame, 5);
-                            }
-                        }
-                    }
-                    addLocatorFrame();
-
-                    var commandQueue = [];
-                    var cmp = function(command, parameter, callback) {
-                        if (command === 'ping') {
-                            if (callback) {
-                                callback({
-                                    gdprAppliesGlobally: !!(window.__cmp && window.__cmp.config && window.__cmp.config.storeConsentGlobally),
-                                    cmpLoaded: false
-                                });
-                            }
+                function addLocatorFrame() {
+                    if (!window.frames['__cmpLocator']) {
+                        if (document.body) {
+                            var frame = document.createElement('iframe');
+                            frame.style.display = 'none';
+                            frame.name = '__cmpLocator';
+                            document.body.appendChild(frame);
                         } else {
-                            commandQueue.push({
-                                command: command,
-                                parameter: parameter,
-                                callback: callback
-                            });
+                            setTimeout(addLocatorFrame, 5);
                         }
                     }
-                    cmp.commandQueue = commandQueue;
-                    cmp.receiveMessage = function(event) {
-                        var data = event && event.data && event.data.__cmpCall;
-                        if (data) {
-                            commandQueue.push({
-                                callId: data.callId,
-                                command: data.command,
-                                parameter: data.parameter,
-                                event: event
+                }
+                addLocatorFrame();
+
+                var commandQueue = [];
+                var cmp = function(command, parameter, callback) {
+                    if (command === 'ping') {
+                        if (callback) {
+                            callback({
+                                gdprAppliesGlobally: !!(window.__cmp && window.__cmp.config && window.__cmp.config.storeConsentGlobally),
+                                cmpLoaded: false
                             });
                         }
-                    };
-                    cmp.config = {
-                        storeConsentGlobally: false,
-                        storePublisherData: false,
-                        logging: 'debug',
-                        gdprApplies: true,
+                    } else {
+                        commandQueue.push({
+                            command: command,
+                            parameter: parameter,
+                            callback: callback
+                        });
                     }
-                    return cmp;
-                }());
-            }
-        })(document, window);
-    } catch (e) {
-        @if(context.environment.mode == Dev) {throw (e)}
-    }
+                }
+                cmp.commandQueue = commandQueue;
+                cmp.receiveMessage = function(event) {
+                    var data = event && event.data && event.data.__cmpCall;
+                    if (data) {
+                        commandQueue.push({
+                            callId: data.callId,
+                            command: data.command,
+                            parameter: data.parameter,
+                            event: event
+                        });
+                    }
+                };
+                cmp.config = {
+                    storeConsentGlobally: false,
+                    storePublisherData: false,
+                    logging: 'debug',
+                    gdprApplies: true,
+                }
+                return cmp;
+            }());
+        }
+    })(document, window);
+} catch (e) {
+    @if(context.environment.mode == Dev) {throw (e)}
 }
