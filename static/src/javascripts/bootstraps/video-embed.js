4bc6eff003c98b26214e84b7c67c9c0f2eaea87d
Simon Adcock <simon.adcock@guardian.co.uk>
2018-06-05 18:07:31 +0100

    make webpack public path available in embed

--- static/src/javascripts/bootstraps/video-embed.js
+++ static/src/javascripts/bootstraps/video-embed.js
@@ -16,6 +16,12 @@ import titlebarTmpl from 'raw-loader!common/views/media/titlebar.html';
 import debounce from 'lodash/functions/debounce';
 import videojsOptions from 'common/modules/video/videojs-options';
 
+// Let webpack know where to get files from
+// __webpack_public_path__ is a special webpack variable
+// https://webpack.js.org/guides/public-path/#set-value-on-the-fly
+// eslint-disable-next-line camelcase,no-undef
+__webpack_public_path__ = `${config.get('page.assetsPath')}javascripts/`;
+
 const initLoadingSpinner = (player: any): void => {
     player.loadingSpinner.contentEl().innerHTML = loadingTmpl;
 };
