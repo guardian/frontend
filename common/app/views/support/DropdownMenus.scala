59da26dde0c56c467c113eb5ffed39145744b37a
Laura <laura.gonzalez@guardian.co.uk>
2018-06-05 13:33:51 +0100

    add new links

--- common/app/views/support/DropdownMenus.scala
+++ common/app/views/support/DropdownMenus.scala
@@ -16,19 +16,29 @@ object DropdownMenus {
   val accountDropdownMenu: List[DropdownMenuItem] = List(
     DropdownMenuItem(
       linkName = Some("comment activity"),
-      label = "Comment activity",
+      label = "Comments & Replies",
       classList = List("js-add-comment-activity-link"),
       parentClassList = List("u-h","js-show-comment-activity")
     ),
     DropdownMenuItem(
       href =Some(s"${Configuration.id.url}/public/edit"),
       linkName = Some("edit profile"),
-      label = "Edit profile",
+      label = "Public profile",
+    ),
+    DropdownMenuItem(
+      href =Some(s"${Configuration.id.url}/account/edit"),
+      linkName = Some("account details"),
+      label = "Account details",
+    ),
+    DropdownMenuItem(
+      href =Some(s"${Configuration.id.url}/digitalpack/edit"),
+      linkName = Some("subscriptions"),
+      label = "Digital Pack",
     ),
     DropdownMenuItem(
       href =Some(s"${Configuration.id.url}/email-prefs"),
       linkName = Some("email prefs"),
-      label = "Email preferences"
+      label = "Emails & marketing"
     ),
     DropdownMenuItem(
       href =Some(s"${Configuration.id.url}/password/change"),
