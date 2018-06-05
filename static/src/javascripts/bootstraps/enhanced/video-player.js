cf4fd1ce8372ecca0f8610f1a8f19e0dcfa47dd1
Simon Adcock <simon.adcock@guardian.co.uk>
2018-06-05 16:02:51 +0100

    video initialisation

--- static/src/javascripts/bootstraps/enhanced/video-player.js
+++ static/src/javascripts/bootstraps/enhanced/video-player.js
@@ -1,3 +1,40 @@
+import fastdom from 'lib/fastdom-promise';
+import $ from 'lib/$';
+import raven from 'lib/raven';
+
+const enhanceVideo = (
+    el: HTMLMediaElement,
+): void => {
+    // on play, fire GA event (ensure replays also fire event)
+
+    // on play, fire Ophan event (ensure replays also fire event)
+
+    // hide download button in Chrome
+    el.setAttribute('controlsList', 'nodownload');
+};
+
+const initPlayer = (): void => {
+    fastdom.read(() => {
+        $('.js-gu-media--enhance').each(el => {
+            enhanceVideo(el, false);
+        });
+    });
+};
+
+const initWithRaven = (): void => {
+    raven.wrap(
+        {
+            tags: {
+                feature: 'video',
+            },
+        },
+        () => {
+            initPlayer();
+        }
+    )();
+};
+
 export const initVideoPlayer = (): void => {
     console.log('Enhancing video');
+    initWithRaven();
 };
