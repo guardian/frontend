e4d19bc6bf4aabfb445a3ba201f3133eac23e0bc
Joseph Smith <joseph.smith@guardian.co.uk>
2018-06-13 11:31:28 +0100

    Don't parameterise quoteSvg

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-testimonial-block.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-testimonial-block.js
@@ -1,12 +1,13 @@
 // @flow
 import type { AcquisitionsEpicTestimonialTemplateParameters } from 'common/modules/commercial/acquisitions-epic-testimonial-parameters';
+import quoteSvg from 'svgs/icon/garnett-quote.svg';
 
 export const acquisitionsTestimonialBlockTemplate = (
     params: AcquisitionsEpicTestimonialTemplateParameters
 ) =>
     `<div class="epic__testimonial-container epic__testimonial-container--subtle">
         <div class="epic__testimonial-quote epic__testimonial-quote--subtle">
-            ${params.quoteSvg}
+            ${quoteSvg.markup}
         </div>
         <blockquote class="epic__testimonial-text">
             ${params.testimonialMessage}
