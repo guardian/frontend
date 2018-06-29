4aa03510a9d664a966fe9025b71e0d0a8968defe
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-29 11:50:49 +0100

    Update controller to send the raw payload

--- applications/app/controllers/CampaignsController.scala
+++ applications/app/controllers/CampaignsController.scala
@@ -16,30 +16,24 @@ class CampaignsController(
 )(implicit context: ApplicationContext)
   extends BaseController with ImplicitControllerExecutionContext with Logging {
 
-  private val root = Configuration.formstack.url + "/form/"
-  private val endpoint = "/submission.json"
-  private val token = Configuration.formstack.editorialOauthToken
-
-  def makeUrlSafe(formId: String): String = URLEncoder.encode(formId, "UTF-8")
+  private val endpoint = Configuration.journalism.campaignsUrl
 
   def formSubmit() = Action.async { implicit request =>
 
     val pageUrl: String = request.headers("referer")
     val jsonBody: Option[JsValue] = request.body.asJson
 
-
     jsonBody.map { json =>
-      val formId: String = makeUrlSafe((json \ "formId").as[String])
-
-        sendToFormstack(json, formId).flatMap { res =>
-          if (res.status == 201) {
-            Future.successful(Redirect(pageUrl))
-          }
-          else {
-            log.error(s"Reader contribution to callout was sent to Formstack but not saved correctly: ${res}")
-            Future(BadRequest("Sorry your story couldn't be saved"))
-          }
+      sendToFormstack(json).flatMap { res =>
+        if (res.status == 201) {
+          Future.successful(Redirect(pageUrl))
         }
+        else {
+          log.error(s"Reader contribution to callout was sent to Formstack but not saved correctly: ${res}")
+          println(s"Reader contribution to callout was sent to Formstack but not saved correctly: ${res}")
+          Future(BadRequest("Sorry your story couldn't be saved"))
+        }
+      }
     }.getOrElse{
       log.error(s"Reader contribution callout: No data submitted by the reader - post body was empty: ${jsonBody}")
       Future(BadRequest("Sorry no data was found"))
@@ -47,9 +41,8 @@ class CampaignsController(
 
   }
 
-  private def sendToFormstack(data: JsValue, formId: String): Future[WSResponse] = {
-    wsClient.url(root + formId + endpoint).withHttpHeaders(
-      "Authorization" -> s"Bearer $token",
+  private def sendToFormstack(data: JsValue): Future[WSResponse] = {
+    wsClient.url(endpoint).withHttpHeaders(
       "Accept" -> "application/json",
       "Content-Type" -> "application/json"
     ).post(data)
