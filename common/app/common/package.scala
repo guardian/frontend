c2c32c1ff7f650cd8b083f6cc515112996f6d39c
Matthew Walls <matthew.walls@guardian.co.uk>
2018-07-10 15:21:59 +0100

    Refactoring

--- common/app/common/package.scala
+++ common/app/common/package.scala
@@ -106,32 +106,6 @@ object `package` extends implicits.Strings with implicits.Requests with play.api
         RevalidatableResult.Ok(htmlResponse())
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   def renderHtml(html: Html, page: model.Page)(implicit request: RequestHeader, context: ApplicationContext): Result = Cached(page){
     RevalidatableResult.Ok(html)
   }
