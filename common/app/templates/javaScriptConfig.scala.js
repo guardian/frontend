4bb15e59db3ba50362deefeccc36c2b481ffa7c9
Nicolas Long <nicolaslong@gmail.com>
2018-07-12 13:53:49 +0100

    Add nav to json responses

--- common/app/templates/javaScriptConfig.scala.js
+++ common/app/templates/javaScriptConfig.scala.js
@@ -5,10 +5,12 @@
 @import play.api.libs.json.Json
 @import views.support.{CamelCase, JavaScriptPage, GoogleAnalyticsAccount}
 @import conf.Configuration.environment
+@import navigation.NavMenu
 
 @defining(Edition(request)) { edition =>
     {
         "page": @JavaScript(StringEncodings.jsonToJS(Json.stringify(JavaScriptPage.get(item, Edition(request), context.isPreview)))),
+        "nav": @JavaScript(Json.stringify(Json.toJson(NavMenu(item, edition)))),
         "switches" : { @{JavaScript(conf.switches.Switches.all.filter(_.exposeClientSide).map{ switch =>
             s""""${CamelCase.fromHyphenated(switch.name)}":${switch.isSwitchedOn}"""}.mkString(","))}
         },
