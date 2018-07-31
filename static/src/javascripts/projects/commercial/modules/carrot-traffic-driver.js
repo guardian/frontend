43d50d927c5183d25755603d3005f57acc55ae23
Francesca Hammond <francesca.hammond@guardian.co.uk>
2018-07-31 14:23:47 +0100

    Define Carrot breakpoint rules

--- static/src/javascripts/projects/commercial/modules/carrot-traffic-driver.js
+++ static/src/javascripts/projects/commercial/modules/carrot-traffic-driver.js
@@ -4,8 +4,49 @@ import { addSlot } from 'commercial/modules/dfp/add-slot';
 import { createSlots } from 'commercial/modules/dfp/create-slots';
 import { commercialFeatures } from 'common/modules/commercial/commercial-features';
 import { spaceFiller } from 'common/modules/article/space-filler';
+import { isBreakpoint } from 'lib/detect';
 
-const rules = {
+const defaultRules = {
+    bodySelector: '.js-article__body',
+    slotSelector: ' > p',
+    minAbove: 500,
+    minBelow: 400,
+    clearContentMeta: 0,
+    selectors: {
+        ' .element-rich-link': {
+            minAbove: 100,
+            minBelow: 400,
+        },
+        ' .element-image': {
+            minAbove: 440,
+            minBelow: 440,
+        },
+
+        ' .player': {
+            minAbove: 50,
+            minBelow: 50,
+        },
+        ' > h1': {
+            minAbove: 50,
+            minBelow: 50,
+        },
+        ' > h2': {
+            minAbove: 50,
+            minBelow: 50,
+        },
+        ' > *:not(p):not(h2):not(blockquote)': {
+            minAbove: 50,
+            minBelow: 50,
+        },
+        ' .ad-slot': {
+            minAbove: 100,
+            minBelow: 100,
+        },
+    },
+    fromBottom: true,
+};
+
+const mobileRules = {
     bodySelector: '.js-article__body',
     slotSelector: ' > p',
     minAbove: 500,
@@ -60,6 +101,7 @@ const insertSlot = (paras: HTMLElement[]): Promise<void> => {
 
 export const init = (): Promise<void> => {
     if (commercialFeatures.carrotTrafficDriver) {
+        const rules = isBreakpoint({max: 'tablet'}) ? mobileRules : defaultRules;
         return spaceFiller.fillSpace(rules, insertSlot, {
             waitForImages: false,
             waitForLinks: true,
