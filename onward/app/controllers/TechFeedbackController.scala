4b1bd0cf779b2061326a425a943d05d3fedc3dc5
Matthew Walls <matthewjwalls@googlemail.com>
2018-07-31 13:42:05 +0100

    Revert "Show error when submitting feedback fails"

--- onward/app/controllers/TechFeedbackController.scala
+++ onward/app/controllers/TechFeedbackController.scala
@@ -8,7 +8,7 @@ import model.Cached.RevalidatableResult
 import model.{ApplicationContext, Cached, MetaData, NoCache, SectionId, SimplePage}
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws._
 import play.api.mvc._
 
@@ -17,7 +17,7 @@ import scala.concurrent.duration._
 
 class TechFeedbackController(ws: WSClient, val controllerComponents: ControllerComponents) (implicit context: ApplicationContext) extends BaseController with Logging with ImplicitControllerExecutionContext {
 
-  def submitFeedback(path: String): Action[AnyContent] = Action.async { implicit request =>
+  def submitFeedback(path: String): Action[AnyContent] = Action { implicit request =>
 
     val feedbackForm = Form(
       tuple(
@@ -33,13 +33,7 @@ class TechFeedbackController(ws: WSClient, val controllerComponents: ControllerC
 
     log.info(s"feedback submitted for category: $category")
 
-    val page = model.SimplePage(MetaData.make(
-      request.path,
-      Some(SectionId.fromId("info")),
-      "Submit Feedback"
-    ))
-
-    val sendFeedback: Future[Result] = ws.url(Configuration.feedback.feedpipeEndpoint)
+    ws.url(Configuration.feedback.feedpipeEndpoint)
       .withRequestTimeout(6000.millis)
       .post(Json.obj(
         "category" -> java.net.URLEncoder.encode(category, "UTF-8"),
@@ -47,19 +41,15 @@ class TechFeedbackController(ws: WSClient, val controllerComponents: ControllerC
         "user" -> java.net.URLEncoder.encode(user, "UTF-8"),
         "extra" -> java.net.URLEncoder.encode(extra, "UTF-8"),
         "name" -> java.net.URLEncoder.encode(name, "UTF-8")
-      )).map((resp: WSResponse) => {
+      ))
 
-        if(resp.status != 200){
-          throw new Exception("Feedpipe api returned non-200")
-        }
+    val page = model.SimplePage(MetaData.make(
+      request.path,
+      Some(SectionId.fromId("info")),
+      "Your report has been sent"
+    ))
 
-        NoCache(Ok(views.html.feedbackSent(page, path)))
-
-      }) recover {
-        case t:Throwable => NoCache(Ok(views.html.feedbackError(page, path)))
-      }
-
-    sendFeedback
+    NoCache(Ok(views.html.feedbackSent(page, path)))
 
   }
 
