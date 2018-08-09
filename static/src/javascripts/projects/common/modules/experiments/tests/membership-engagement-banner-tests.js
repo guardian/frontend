fb0f414ad0a3c4dcbcd0779df4a8e11adfb8df5a
Joseph Smith <joseph.lochlann@gmail.com>
2018-08-09 12:05:16 +0100

    Update banner control (#20153)
    Remove test and update banner control

--- static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/membership-engagement-banner-tests.js
@@ -1,6 +1,5 @@
 // @flow strict
-import { AcquisitionsBannerEditorialIndependence } from 'common/modules/experiments/tests/acquisitions-banner-editorial-independence';
 
 export const membershipEngagementBannerTests: $ReadOnlyArray<
     AcquisitionsABTest
-> = [AcquisitionsBannerEditorialIndependence];
+> = [];
