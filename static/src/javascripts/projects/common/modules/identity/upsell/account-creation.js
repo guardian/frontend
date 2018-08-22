bd56aecfea827e7593987db5849eb06266e0d651
Laura <laura.gonzalez@guardian.co.uk>
2018-08-22 15:00:22 +0100

    initial

--- static/src/javascripts/projects/common/modules/identity/upsell/account-creation.js
+++ static/src/javascripts/projects/common/modules/identity/upsell/account-creation.js
@@ -1,7 +1,10 @@
 // @flow
 import { trackNonClickInteraction } from 'common/modules/analytics/google';
+import React, { render } from 'preact-compat';
+import fastdom from 'lib/fastdom-promise';
 import ophan from 'ophan/ng';
 import loadEnhancers from './../modules/loadEnhancers';
+import { AccountCreationForm } from './account-creation/AccountCreationForm';
 
 const trackInteraction = (interaction: string): void => {
     ophan.record({
@@ -11,8 +14,18 @@ const trackInteraction = (interaction: string): void => {
     trackNonClickInteraction(interaction);
 };
 
-const bindAccountCreation = (): void => {
+const bindAccountCreation = (el): void => {
     trackInteraction('set-password : display');
+    fastdom.write(() => {
+        render(
+            <AccountCreationForm
+                csrfToken="test1"
+                accountToken="test2"
+                userEmail="test3@test.test"
+            />,
+            el
+        );
+    });
 };
 
 const enhanceAccountCreation = (): void => {
