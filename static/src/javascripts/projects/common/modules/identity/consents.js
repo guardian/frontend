guardian/frontend

commit d0490d7a1e38bcf7f5443dfed53bffa8c018aad8

Remove empty consent nagware (#20214)

static/src/javascripts/projects/common/modules/identity/consents.js
@@ -14,7 +14,6 @@ import {
     bindAnalyticsEventsOnce as bindCheckboxAnalyticsEventsOnce,
 } from './modules/switch';
 import { getCsrfTokenFromElement } from './modules/fetchFormFields';
-import { show as showModal } from './modules/modal';
 
 import { prependSuccessMessage } from './modules/prependMessage';
 
@@ -191,14 +190,6 @@ const bindUnsubscribeFromAll = (buttonEl: HTMLButtonElement) => {
     });
 };
 
-const bindUnsubscribeFromAllModalConfirmation = (
-    buttonEl: HTMLButtonElement
-) => {
-    buttonEl.addEventListener('click', () => {
-        showModal('confirm-unsubscribe-all');
-    });
-};
-
 const updateNewsletterSwitch = (labelEl: HTMLElement): Promise<void> =>
     Promise.all([
         getCsrfTokenFromElement(labelEl),
@@ -366,10 +357,6 @@ const enhanceConsents = (): void => {
         [`.${checkAllCheckboxClassName}`, bindCheckAllSwitch],
         [`.${consentCheckboxClassName}`, bindConsentSwitch],
         [`.${newsletterCheckboxClassName}`, bindNewsletterSwitch],
-        [
-            `.${unsubscribeButtonClassName}`,
-            bindUnsubscribeFromAllModalConfirmation,
-        ],
         ['.js-confirm-unsubscribe-all', bindUnsubscribeFromAll],
     ];
     loadEnhancers(loaders);

identity/app/views/consentJourney.scala.html
@@ -148,36 +148,6 @@
     profilingConsentOptOutStep
 )}
 
-@confirmModal = {
-    <section class="identity-forms-message">
-        <h1 class="identity-header">Are you sure?</h1>
-
-            <div class="identity-consent-journey-modal-message identity-consent-journey-modal-message--inactive identity-consent-journey-modal-message--no-reminder">
-                <p>Looks like you haven't selected any newsletters or Guardian products and services.</p>
-
-                <p>This means you'll stop receiving <b>all</b> emails from us other than service communication.</p>
-
-                <p>Keeping you informed is a critical part of our job.</p>
-
-                <p><b>Are you sure you want to unsubscribe from all Guardian emails?</b></p>
-            </div>
-
-            <div class="identity-consent-journey-modal-message identity-consent-journey-modal-message--inactive  identity-consent-journey-modal-message--reminder">
-                <p>You've chosen not to receive the following emails from the Guardian:</p>
-
-                <div class="identity-consent-journey-modal__reminder"></div>
-
-                <p>Are you sure you want to unsubscribe from these email services?</p>
-            </div>
-
-        <footer class="identity-forms-message__options">
-            <a class="js-identity-modal__closer manage-account__button manage-account__button--main manage-account__button--center" data-link-name="consents : confirm : recheck">Let me look again</a>
-            <a class="manage-account__button manage-account__button--secondary manage-account__button--center js-identity-consent-journey-continue" data-link-name="consents : confirm : continue">Unsubscribe me</a>
-        </footer>
-
-    </section>
-}
-
 @displayJourney = {
     <div class="
         identity-consent-journey
@@ -220,9 +190,6 @@
                 </div>
             </form>
         </div>
-
-
-        @fragments.modal("confirm-consents", confirmModal)
 
     </div>
 

static/src/javascripts/projects/common/modules/identity/consent-journey.js
@@ -3,70 +3,6 @@
 import fastdom from 'lib/fastdom-promise';
 
 import loadEnhancers from './modules/loadEnhancers';
-import { getContents, show as showModal } from './modules/modal';
-
-const ERR_MALFORMED_HTML = 'Something went wrong';
-const ERR_MODAL_MALFORMED = 'Modal is malformed';
-
-const removeNewsletterReminders = (reminderEl: HTMLElement) =>
-    fastdom.write(() => {
-        if (reminderEl) {
-            while (reminderEl.firstChild) {
-                reminderEl.removeChild(reminderEl.firstChild);
-            }
-        }
-    });
-
-const insertNewsletterReminders = (
-    modalEl: HTMLElement,
-    uncheckedNewsletters: Array<string>
-): Promise<void> =>
-    fastdom
-        .read(() => {
-            const el = modalEl.querySelector(
-                `.identity-consent-journey-modal__reminder`
-            );
-            if (el) return el;
-            throw new Error(ERR_MODAL_MALFORMED);
-        })
-        .then(reminderEl => {
-            fastdom.write(() => {
-                removeNewsletterReminders(reminderEl).then(() =>
-                    uncheckedNewsletters.map(newsletterName => {
-                        const li = document.createElement('li');
-                        li.innerHTML = `<b>${newsletterName}</b>`;
-                        reminderEl.appendChild(li);
-                        return modalEl;
-                    })
-                );
-            });
-        });
-
-const showDiv = (modalEl: HTMLElement, divType: string): Promise<void> =>
-    fastdom
-        .read(() => ({
-            reminder: modalEl.querySelector(
-                `.identity-consent-journey-modal-message--reminder`
-            ),
-            noReminder: modalEl.querySelector(
-                `.identity-consent-journey-modal-message--no-reminder`
-            ),
-        }))
-        .then(modalAreas =>
-            fastdom.write(() => {
-                Object.keys(modalAreas).forEach(area => {
-                    if (area === divType) {
-                        modalAreas[area].classList.remove(
-                            'identity-consent-journey-modal-message--inactive'
-                        );
-                    } else {
-                        modalAreas[area].classList.add(
-                            'identity-consent-journey-modal-message--inactive'
-                        );
-                    }
-                });
-            })
-        );
 
 const showJourney = (journeyEl: HTMLElement): Promise<void> =>
     fastdom.write(() => journeyEl.classList.remove('u-h'));
@@ -74,99 +10,9 @@ const showJourney = (journeyEl: HTMLElement): Promise<void> =>
 const hideLoading = (loadingEl: HTMLElement): Promise<void> =>
     fastdom.write(() => loadingEl.remove());
 
-const getCheckboxInfo = (
-    journeyEl: HTMLElement,
-    section: string
-): Promise<Array<Object>> =>
-    fastdom.read(() => [
-        ...journeyEl.querySelectorAll(
-            `.identity-consent-journey-step--${section} input[type=checkbox]`
-        ),
-    ]);
-
-const getForm = (journeyEl: HTMLElement) =>
-    fastdom.read(
-        () =>
-            journeyEl.querySelector('form.js-identity-consent-journey-form') ||
-            new Error(ERR_MALFORMED_HTML)
-    );
-
-const shouldShowReminder = (journeyEl: HTMLElement) =>
-    Promise.all([
-        getCheckboxInfo(journeyEl, 'email'),
-        getCheckboxInfo(journeyEl, 'marketing-consents'),
-    ]).then(checkboxes => {
-        const allCheckboxes = [].concat(...checkboxes);
-        const unchecked = allCheckboxes.filter(_ => _.checked === false);
-
-        return {
-            unchecked,
-            total: allCheckboxes.length,
-        };
-    });
-
-const showJourneyAlert = (journeyEl: HTMLElement): void => {
-    const showNoReminderModal = () => {
-        showDiv(journeyEl, 'noReminder').then(() =>
-            showModal('confirm-consents')
-        );
-    };
-
-    const showReminderModal = checkboxInfo => {
-        showDiv(journeyEl, 'reminder').then(() => {
-            const consentOrNewsletterNames = checkboxInfo.unchecked.map(
-                uncheckedbox =>
-                    uncheckedbox.parentElement.querySelector(
-                        '.manage-account__switch-title'
-                    ).innerText
-            );
-            getContents('confirm-consents').then(modalEl => {
-                if (consentOrNewsletterNames.length > 0) {
-                    return insertNewsletterReminders(
-                        modalEl,
-                        consentOrNewsletterNames
-                    );
-                }
-            });
-            showModal('confirm-consents');
-        });
-    };
-
-    getForm(journeyEl).then(formEl => {
-        formEl.addEventListener('submit', ev => {
-            if (ev.isTrusted) {
-                ev.preventDefault();
-                shouldShowReminder(journeyEl).then(checkboxInfo => {
-                    if (checkboxInfo.unchecked.length === checkboxInfo.total) {
-                        showNoReminderModal();
-                    } else if (checkboxInfo.unchecked.length > 0) {
-                        showReminderModal(checkboxInfo);
-                    } else {
-                        formEl.submit();
-                    }
-                });
-            }
-        });
-    });
-};
-
-const submitJourneyAnyway = (buttonEl: HTMLElement): void => {
-    const journeyEl = ((buttonEl.closest(
-        '.identity-consent-journey'
-    ): any): HTMLElement);
-    if (!journeyEl) throw new Error(ERR_MALFORMED_HTML);
-    buttonEl.addEventListener('click', () => {
-        getForm(journeyEl).then(formEl => {
-            formEl.submit();
-        });
-    });
-};
-
 const enhanceConsentJourney = (): void => {
     const loaders = [
         ['.identity-consent-journey', showJourney],
-        ['.identity-consent-journey--with-alert', showJourneyAlert],
-        ['.js-identity-consent-journey-continue', submitJourneyAnyway],
         ['#identityConsentsLoadingError', hideLoading],
     ];
     loadEnhancers(loaders);

commit 59c34468a4054e9d90a14b349cda596da8919aca

Fix unsubscribe regression introduced by #20214

static/src/javascripts/projects/common/modules/identity/consents.js
@@ -357,7 +357,10 @@ const enhanceConsents = (): void => {
         [`.${checkAllCheckboxClassName}`, bindCheckAllSwitch],
         [`.${consentCheckboxClassName}`, bindConsentSwitch],
         [`.${newsletterCheckboxClassName}`, bindNewsletterSwitch],
-        ['.js-confirm-unsubscribe-all', bindUnsubscribeFromAll],
+        [
+            `.${unsubscribeButtonClassName}`,
+            bindUnsubscribeFromAll,
+        ],
     ];
     loadEnhancers(loaders);
 };
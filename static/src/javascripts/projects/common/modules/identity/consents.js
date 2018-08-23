59c34468a4054e9d90a14b349cda596da8919aca
Rahil Bohra <rahil@bohra.co.uk>
2018-08-23 13:32:02 +0100

    Fix unsubscribe regression introduced by #20214

--- static/src/javascripts/projects/common/modules/identity/consents.js
+++ static/src/javascripts/projects/common/modules/identity/consents.js
@@ -357,7 +357,10 @@ const enhanceConsents = (): void => {
         [`.${checkAllCheckboxClassName}`, bindCheckAllSwitch],
         [`.${consentCheckboxClassName}`, bindConsentSwitch],
         [`.${newsletterCheckboxClassName}`, bindNewsletterSwitch],
-        ['.js-confirm-unsubscribe-all', bindUnsubscribeFromAll],
+        [
+            `.${unsubscribeButtonClassName}`,
+            bindUnsubscribeFromAll,
+        ],
     ];
     loadEnhancers(loaders);
 };
