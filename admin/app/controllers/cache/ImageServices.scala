748e0fe99228e0a8b93fe73704c4011e48c16d72
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-08-29 15:18:08 +0100

    Update helper text, support clearing code images.

--- admin/app/controllers/cache/ImageServices.scala
+++ admin/app/controllers/cache/ImageServices.scala
@@ -3,14 +3,18 @@ package controllers.cache
 import java.net.URI
 
 import conf.AdminConfiguration.fastly
+import conf.Configuration.environment.stage
 import play.api.libs.ws.WSClient
 
-class ImageServices(wsClient: WSClient) {
+object ImageServices {
 
   // none of the stuff here is a state secret.
   // it is all authenticated
 
-  private val fastlyIOGuimCoUk = "1L0HRheo6sMtfQHnY1FU6C"
+  private val iGuimCoUk = "1L0HRheo6sMtfQHnY1FU6C"
+  private val iGuimCodeCoUk = "5CSDV7WcKwnIIHipZzt3po"
+
+  private val fastlyIOService = if (stage == "PROD") iGuimCoUk else iGuimCodeCoUk
 
   private val fastlyOriginCdns = Map(
     "static.guim.co.uk" -> "5qHts5Ev0rFxzm1DhCkmyA",
@@ -19,10 +23,10 @@ class ImageServices(wsClient: WSClient) {
     "sport.guim.co.uk" -> "1C2vPr3E26cRb4NXa0wMf3"
   )
 
-  // clear both the origin CDN and i.guim.co.uk
-  private def fastlyServiceIdsforOrigin(host: String): Seq[String] = Seq(fastlyOriginCdns(host), fastlyIOGuimCoUk)
+  // clear both the origin CDN and Fastly IO service (either i.guim.co.uk or i.guimcode.co.uk)
+  private def fastlyServiceIdsforOrigin(host: String): Seq[String] = Seq(fastlyOriginCdns(host), fastlyIOService)
 
-  def clearFastly(originUri: URI) {
+  def clearFastly(originUri: URI, wsClient: WSClient) {
     fastlyServiceIdsforOrigin(originUri.getHost).foreach { serviceId =>
       // This works because the "path" is set as a Surrogate Key for images in i.guim.co.uk
       // https://www.fastly.com/blog/surrogate-keys-part-1/
