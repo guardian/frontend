d0b93f24ba396fcf8327baa3cb4affc8bfd95675
Rahil Bohra <rahil@bohra.co.uk>
2018-09-04 12:05:12 +0100

    Integrate with Credentials Management API (#20254)
    * Integrate with the credentials API
    
    * Fix cross domain credentials api
    
    * Add new parameter for global access CORS fallback
    
    * Do not set allowed origin to null, it can be exploited
    
    * #20254 comments addressed
    
    * Set cookies directly from ajax response
    
    * Set-Cookies via user-agent instead of manually
    
    * Fix linting errors
    
    * Make mediation required
    
    * #20254 comments
    
    * #20254 comments: refactor credentials sign in JS
    
    * Fix linting errors
    
    * Remove password manager dismissed behaviour
    
    * Linting Fixes

--- common/app/model/Cors.scala
+++ common/app/model/Cors.scala
@@ -8,14 +8,14 @@ object Cors extends Results with implicits.Requests {
 
   private val defaultAllowHeaders = List("X-Requested-With","Origin","Accept","Content-Type")
 
-  def apply(result: Result, allowedMethods: Option[String] = None)(implicit request: RequestHeader): Result = {
+  def apply(result: Result, allowedMethods: Option[String] = None, fallbackAllowOrigin: String = "*")(implicit request: RequestHeader): Result = {
 
     val responseHeaders = (defaultAllowHeaders ++ request.headers.get("Access-Control-Request-Headers").toList) mkString ","
 
     request.headers.get("Origin") match {
       case None => result
       case Some(requestOrigin) =>
-        val allowedOrigin = ajax.corsOrigins.find(_ == requestOrigin).getOrElse("*")
+        val allowedOrigin = ajax.corsOrigins.find(_ == requestOrigin).getOrElse(fallbackAllowOrigin)
         val headers = allowedMethods.map("Access-Control-Allow-Methods" -> _).toList ++ List(
           "Access-Control-Allow-Origin" -> allowedOrigin,
           "Access-Control-Allow-Headers" -> responseHeaders,
