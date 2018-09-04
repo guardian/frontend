6da4a52d1b864869e0f585b012a54686951148bd
Rahil Bohra <rahil@bohra.co.uk>
2018-09-04 10:11:13 +0100

    #20254 comments: refactor credentials sign in JS

--- static/src/javascripts/projects/common/modules/identity/credentials-api-sign-in.js
+++ static/src/javascripts/projects/common/modules/identity/credentials-api-sign-in.js
@@ -7,6 +7,20 @@ import { ajaxSignIn } from 'common/modules/identity/api';
 import ophan from 'ophan-tracker-js';
 import { addCookie } from 'lib/cookies';
 
+const recordOphanCredentialsApiInteraction = (interactionType): void => {
+    ophan.record({
+        component: 'pwmanager-api',
+        value: interactionType,
+    });
+};
+
+const storeCredsAndResolvePromise = (creds): Promise<boolean> => {
+    // $FlowFixMe
+    navigator.credentials.store(creds);
+    recordOphanCredentialsApiInteraction('conversion');
+    return Promise.resolve(true);
+};
+
 export const signInWithSavedCredentials = (): Promise<boolean> => {
     if (identityFeatures.promptForSignIn) {
         // $FlowFixMe
@@ -19,20 +33,11 @@ export const signInWithSavedCredentials = (): Promise<boolean> => {
                 if (creds) {
                     return ajaxSignIn(creds)
                         .then(() => {
-                            ophan.record({
-                                component: 'pwmanager-api',
-                                value: 'conversion',
-                            });
-                            // $FlowFixMe
-                            navigator.credentials.store(creds);
-                            return Promise.resolve(true);
+                            return storeCredsAndResolvePromise(creds);
                         })
                         .catch(() => Promise.resolve(false));
                 }
-                ophan.record({
-                    component: 'pwmanager-api',
-                    value: 'impression',
-                });
+                recordOphanCredentialsApiInteraction('impression');
                 addCookie(IdentityCookies.PW_MANAGER_DISMISSED, 'true', 7);
                 return Promise.resolve(false);
             });
