019c6e08fa362ddd63585e760e9062541b58c3c7
Rahil Bohra <rahil@bohra.co.uk>
2018-09-05 10:57:52 +0100

    Remove Identity cookies on all routes to change/reset password

---
+++ identity/app/controllers/DiscardingIdentityCookies.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+import play.api.mvc.{Cookie, DiscardingCookie, Result}
+
+object DiscardingIdentityCookies {
+  def apply(result: Result): Result = result.discardingCookies(
+    DiscardingCookie("SC_GU_U"), DiscardingCookie("GU_U")
+  ).withCookies(
+    Cookie(
+      "GU_SO",
+      Instant.now().getEpochSecond.toString,
+      Some(Instant.now().plus(90, ChronoUnit.DAYS).getEpochSecond.toInt),
+      secure = true
+    )
+  )
+}
