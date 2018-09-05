95c84a35e5ec3fb5dfbe05f2dca46cb705a5914c
shaundillon <shaun.dillon@guardian.co.uk>
2018-09-05 16:58:49 +0100

    remove membership tab and redirect to manage

--- identity/app/controllers/editprofile/editprofile.scala
+++ identity/app/controllers/editprofile/editprofile.scala
@@ -7,7 +7,6 @@ package object editprofile {
   object PublicEditProfilePage extends IdentityPage("/public/edit", "Edit Public Profile")
   object AccountEditProfilePage extends IdentityPage("/account/edit", "Edit Account Details")
   object EmailPrefsProfilePage extends IdentityPage("/email-prefs", "Emails")
-  object MembershipEditProfilePage extends IdentityPage("/membership/edit", "Membership")
   object recurringContributionPage extends IdentityPage("/contribution/recurring/edit", "Contributions")
   object DigiPackEditProfilePage extends IdentityPage("/digitalpack/edit", "Digital Pack")
 
