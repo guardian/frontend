d0cf688a9b184d8d75beec3e73bc91b81657ab0c
Justin Pinner <justinpinner@gmail.com>
2018-09-05 14:53:55 +0100

    Fix cookies

--- static/src/javascripts/projects/common/modules/commercial/user-features.js
+++ static/src/javascripts/projects/common/modules/commercial/user-features.js
@@ -78,11 +78,11 @@ const persistResponse = (JsonResponse: () => void) => {
             adFreeDataIsPresent() &&
             !JsonResponse.adFree &&
             !forcedAdFreeMode &&
-            !JsonResponse.contectAccess.digitalPack
+            !JsonResponse.contentAccess.digitalPack
         ) {
             removeCookie(AD_FREE_USER_COOKIE);
         }
-        if (JsonResponse.adFree || JsonResponse.contectAccess.digitalPack) {
+        if (JsonResponse.adFree || JsonResponse.contentAccess.digitalPack) {
             addCookie(AD_FREE_USER_COOKIE, timeInDaysFromNow(2));
         }
     }
