fec9fba62cb5317b20fd022f1bda29e5eab134ae
Laura <laura.gonzalez@guardian.co.uk>
2018-09-06 16:33:44 +0100

    upsell

--- static/src/javascripts/projects/common/modules/identity/upsell/upsell.js
+++ static/src/javascripts/projects/common/modules/identity/upsell/upsell.js
@@ -5,6 +5,7 @@ import fastdom from 'lib/fastdom-promise';
 import ophan from 'ophan/ng';
 import loadEnhancers from './../modules/loadEnhancers';
 import { AccountCreationFlow } from './account-creation/AccountCreationFlow';
+import { OptOutsList } from './opt-outs/OptOutsList';
 
 const trackInteraction = (interaction: string): void => {
     ophan.record({
@@ -28,10 +29,17 @@ const bindAccountCreation = (el): void => {
     });
 };
 
-const enhanceAccountCreation = (): void => {
+const bindOptouts = (el): void => {
+    fastdom.write(() => {
+        render(<OptOutsList />, el);
+    });
+};
+
+const enhanceUpsell = (): void => {
     loadEnhancers([
         ['.js-identity-upsell-account-creation', bindAccountCreation],
+        ['.js-identity-upsell-optputs', bindOptouts],
     ]);
 };
 
-export { enhanceAccountCreation };
+export { enhanceUpsell };
