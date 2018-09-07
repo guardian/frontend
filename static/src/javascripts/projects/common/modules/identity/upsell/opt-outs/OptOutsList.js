d21178a80cfb111b86916b5b6571478107de0453
Laura <laura.gonzalez@guardian.co.uk>
2018-09-07 14:12:39 +0100

    add "save" button

--- static/src/javascripts/projects/common/modules/identity/upsell/opt-outs/OptOutsList.js
+++ static/src/javascripts/projects/common/modules/identity/upsell/opt-outs/OptOutsList.js
@@ -12,11 +12,15 @@ export class OptOutsList extends Component<
     {},
     {
         optouts: OptOut[],
+        isLoading: boolean,
+        hasUnsavedChanges: boolean
     }
 > {
     constructor(props: {}) {
         super(props);
         this.state = {
+            loading: false,
+            hasUnsavedChanges: true,
             optouts: [
                 {
                     id: 'phone',
@@ -44,24 +48,65 @@ export class OptOutsList extends Component<
             optoutsClone[i].checked = ev.currentTarget.checked;
             this.setState({
                 optouts: optoutsClone,
+                hasUnsavedChanges: true,
             });
         }
     };
 
+    updateChangesRemotely = (): Promise<void> =>new Promise((yay,nay)=>{
+        setTimeout(()=>{
+            yay();
+        },1000);
+    })
+
+    onSubmit = (ev: Event) => {
+        ev.preventDefault();
+        this.setState({
+            isLoading: true,
+            hasUnsavedChanges: false,
+        });
+        this.updateChangesRemotely().then(()=>{
+            this.setState({
+                isLoading: false,
+            });
+        }).catch(()=>{
+            alert('oops');
+        })
+        console.table(this.state.optouts);
+    }
+
     render() {
+        const {hasUnsavedChanges, isLoading} = this.state;
         return (
-            <div>
-                {this.state.optouts.map(({ title, checked, id }, i) => (
-                    <Checkbox
-                        title={title}
-                        key={id}
-                        checkboxHtmlProps={{
-                            checked,
-                            onChange: ev => this.onCheckboxChange(ev, i),
-                        }}
-                    />
-                ))}
-            </div>
+            <form onSubmit={ev => this.onSubmit(ev)}>
+                <div>
+                    {this.state.optouts.map(({ title, checked, id }, i) => (
+                        <Checkbox
+                            title={title}
+                            key={id}
+                            checkboxHtmlProps={{
+                                checked,
+                                onChange: ev => this.onCheckboxChange(ev, i),
+                            }}
+                        />
+                    ))}
+                </div>
+                <div class={'identity-upsell-button-with-proxy'}>
+                    <button
+                        type={"submit"}
+                        disabled={isLoading}
+                        className={"manage-account__button manage-account__button--main"}
+                    >
+                        Save changes
+                    </button>
+                    {!hasUnsavedChanges &&
+                        <span class={'identity-upsell-button-with-proxy__proxy identity-upsell-button-with-proxy__proxy--success'}>Changes saved</span>
+                    }
+                    {isLoading &&
+                        <span class={'identity-upsell-button-with-proxy__proxy'}>Loading</span>
+                    }
+                </div>
+            </form>
         );
     }
 }
