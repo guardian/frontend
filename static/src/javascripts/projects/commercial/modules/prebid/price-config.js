78b2f467d01c07746af1aa957b1d0656a04063de
Kelvin Chappell <kelvin.chappell@theguardian.com>
2018-09-11 15:12:14 +0100

    Prebid uses new fine-grained price buckets

--- static/src/javascripts/projects/commercial/modules/prebid/price-config.js
+++ static/src/javascripts/projects/commercial/modules/prebid/price-config.js
@@ -6,23 +6,13 @@ export const priceGranularity: PrebidPriceGranularity = {
     buckets: [
         {
             min: 0,
-            max: 5,
-            increment: 0.05,
+            max: 100,
+            increment: 0.01,
         },
         {
-            min: 5,
-            max: 10,
-            increment: 0.1,
-        },
-        {
-            min: 10,
-            max: 20,
-            increment: 0.5,
-        },
-        {
-            min: 20,
-            max: 40,
-            increment: 10,
+            min: 100,
+            max: 500,
+            increment: 1,
         },
     ],
 };
