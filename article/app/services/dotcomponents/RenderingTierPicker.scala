c3c328ca5a4a7e8c33e70180e8e72b9c41b1520e
Matthew Walls <matthew.walls@guardian.co.uk>
2018-09-13 14:41:34 +0100

    Always log out the picker decision regardless

--- article/app/services/dotcomponents/RenderingTierPicker.scala
+++ article/app/services/dotcomponents/RenderingTierPicker.scala
@@ -1,12 +1,13 @@
 package services.dotcomponents
 
+import common.Logging
 import experiments.{ActiveExperiments, Control, DotcomponentsRendering, Excluded, Participant}
 import model.PageWithStoryPackage
 import play.api.mvc.RequestHeader
 import services.dotcomponents.pickers.{RenderTierPickerStrategy, SimplePagePicker, WhitelistPicker}
 import implicits.Requests._
 
-object RenderingTierPicker {
+object RenderingTierPicker extends Logging {
 
   // todo: use injection for this
   val picker: RenderTierPickerStrategy = new SimplePagePicker()
@@ -19,9 +20,16 @@ object RenderingTierPicker {
       return RemoteRender
     }
 
-    // otherwise check if we are in the AB test, and are a supported article according to the picker
+    // log out whenever we find a supported article, for metrics purposes
 
     val isSupported = picker.getRenderTierFor(page, request)
+
+    isSupported match {
+      case RemoteRender => log.info(s"Article was remotely renderable ${page.metadata.id}")
+      case _ =>
+    }
+
+    // We use dotcomponents if we are in the AB test, and are a supported article according to the picker
 
     ActiveExperiments.groupFor(DotcomponentsRendering) match {
       case Participant => isSupported
