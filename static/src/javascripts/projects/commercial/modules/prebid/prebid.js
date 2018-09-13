b02fdf03cb7fd42ba1e05720cf0265a90c5c7103
Kelvin Chappell <kelvin.chappell@theguardian.com>
2018-09-13 14:31:34 +0100

    Workaround for bug restricting syncs per bidder

--- static/src/javascripts/projects/commercial/modules/prebid/prebid.js
+++ static/src/javascripts/projects/commercial/modules/prebid/prebid.js
@@ -29,7 +29,8 @@ const consentManagement = {
 };
 
 const userSync = {
-    syncsPerBidder: 0, // allow all syncs
+    // syncsPerBidder: 0, // allow all syncs - bug https://github.com/prebid/Prebid.js/issues/2781
+    syncsPerBidder: 999, // temporarily until above bug fixed
     filterSettings: {
         all: {
             bidders: '*', // allow all bidders to sync by iframe or image beacons
