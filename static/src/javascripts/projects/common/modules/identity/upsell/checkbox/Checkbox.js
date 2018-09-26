eee3c706d2126cbd36e26fd116228e3a8ff33e29
Lauren Emms <lauren.emms@guardian.co.uk>
2018-09-26 14:56:02 +0100

    Changed the Checkbox class into a functional component

--- static/src/javascripts/projects/common/modules/identity/upsell/checkbox/Checkbox.js
+++ static/src/javascripts/projects/common/modules/identity/upsell/checkbox/Checkbox.js
@@ -1,33 +1,34 @@
 // @flow
-import React, { Component } from 'preact-compat';
+import React from 'preact-compat';
 
-type CheckboxHtmlProps = {
+export type CheckboxHtmlProps = {
     checked: ?boolean,
     onChange: (ev: Event) => void,
 };
 
-type CheckboxProps = {
+export type CheckboxProps = {
     title: string,
     subtitle: ?string,
     checkboxHtmlProps: CheckboxHtmlProps,
 };
 
-export class Checkbox extends Component<CheckboxProps, { checked: boolean }> {
-    render() {
-        const { title, subtitle } = this.props;
-        return (
-            <label className="identity-upsell-checkbox" htmlFor={title}>
-                <span className="identity-upsell-checkbox__title">{title}</span>
+const Checkbox = (props: CheckboxProps) => {
+    const { title, subtitle } = props;
+    return (
+        <label className="identity-upsell-checkbox" htmlFor={title}>
+            <span className="identity-upsell-checkbox__title">{title}</span>
                 {subtitle && <span>{subtitle}</span>}
                 <input
                     type="checkbox"
                     id={title}
-                    {...this.props.checkboxHtmlProps}
+                    {...props.checkboxHtmlProps}
                 />
-                <span className="identity-upsell-checkbox__checkmark">
-                    <span className="identity-upsell-checkbox__checkmark_tick" />
-                </span>
-            </label>
-        );
-    }
+            <span className="identity-upsell-checkbox__checkmark">
+                <span className="identity-upsell-checkbox__checkmark_tick" />
+            </span>
+        </label>
+    );
 }
+
+
+export { Checkbox };
