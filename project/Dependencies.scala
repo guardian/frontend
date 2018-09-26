82de964b54f54645875aaa45d8d9ca9eb54a3ffd
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-09-26 14:45:22 +0100

    Bump CAPI deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ import sbt._
 object Dependencies {
   val identityLibVersion = "3.167"
   val awsVersion = "1.11.240"
-  val capiVersion = "12.7"
-  val faciaVersion = "2.6.3"
+  val capiVersion = "12.10"
+  val faciaVersion = "2.6.4"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
