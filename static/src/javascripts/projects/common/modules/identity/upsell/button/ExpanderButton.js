815239c7a12eb961c55904560a36b15e79b5baad
Laura <laura.gonzalez@guardian.co.uk>
2018-10-01 10:57:10 +0100

    add arrows

--- static/src/javascripts/projects/common/modules/identity/upsell/button/ExpanderButton.js
+++ static/src/javascripts/projects/common/modules/identity/upsell/button/ExpanderButton.js
@@ -1,5 +1,7 @@
 // @flow
 import React from 'preact-compat';
+import arrowDown from 'svgs/icon/arrow-down.svg';
+import arrowUp from 'svgs/icon/arrow-up.svg';
 
 type ExpanderButtonText = {
     more: string,
@@ -27,16 +29,24 @@ const ExpanderButton = ({
     isExpanded ? (
         <button
             data-link-name={linkName ? `${linkName} : shrink` : null}
-            className="manage-account__button manage-account__button--secondary"
+            className="manage-account__button manage-account__button--secondary manage-account__button--icon"
             onClick={() => onToggle(false)}>
             {(text || defaultExpanderButtonText).less}
+            <span
+                className="manage-account__button-react-icon"
+                dangerouslySetInnerHTML={{ __html: arrowUp.markup }}
+            />
         </button>
     ) : (
         <button
             data-link-name={linkName ? `${linkName} : expand` : null}
-            className="manage-account__button manage-account__button--secondary"
+            className="manage-account__button manage-account__button--secondary manage-account__button--icon"
             onClick={() => onToggle(true)}>
             {(text || defaultExpanderButtonText).more}
+            <span
+                className="manage-account__button-react-icon"
+                dangerouslySetInnerHTML={{ __html: arrowDown.markup }}
+            />
         </button>
     );
 export { ExpanderButton };
