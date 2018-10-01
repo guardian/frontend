460115a8d8f09dcd932b7867b9234ef0349b1fc1
Simon Adcock <simonadcock2@gmail.com>
2018-10-01 10:19:29 +0100

    No leading zeros 😓

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -171,7 +171,7 @@ trait FeatureSwitches {
     "If this is switched on then videos are enhanced using VideoJS",
     owners = Seq(Owner.withGithub("siadcock")),
     safeState = On,
-    sellByDate = new LocalDate(2019, 01, 29),
+    sellByDate = new LocalDate(2019, 1, 29),
     exposeClientSide = true
   )
 
