343ad6c8068c11587cb2e5d82cb66962e6c6ec65
Kelvin Chappell <kelvin.chappell@theguardian.com>
2018-10-03 16:21:03 +0100

    Fix broken test

--- common/test/views/support/AmpAdRtcConfigTest.scala
+++ common/test/views/support/AmpAdRtcConfigTest.scala
@@ -9,7 +9,7 @@ class AmpAdRtcConfigTest extends FlatSpec with Matchers {
   "toJsonString" should "hold Prebid server and Krux config when switches on" in {
     Switches.KruxSwitch.switchOn()
     Switches.prebidServer.switchOn()
-    val json = Json.parse(AmpAdRtcConfig.toJsonString(Some(1)))
+    val json = Json.parse(AmpAdRtcConfig.toJsonString)
     json shouldBe Json.obj(
       "urls" -> Json.arr(
         "https://cdn.krxd.net/userdata/v2/amp/2196ddf0-947c-45ec-9b0d-0a82fb280cb8?segments_key=x&kuid_key=kuid",
@@ -23,7 +23,7 @@ class AmpAdRtcConfigTest extends FlatSpec with Matchers {
   it should "hold no real-time config when switches off" in {
     Switches.KruxSwitch.switchOff()
     Switches.prebidServer.switchOff()
-    val json = Json.parse(AmpAdRtcConfig.toJsonString(Some(1)))
+    val json = Json.parse(AmpAdRtcConfig.toJsonString)
     json shouldBe JsNull
   }
 }
