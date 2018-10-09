cf34ae268f4b04a9499fa535047bb52c5efc5f15
Lauren Emms <lauren.emms@guardian.co.uk>
2018-10-09 15:57:29 +0100

    Added logging for testing X-Forwarded-For header in CODE

--- common/app/controllers/EmailSignupController.scala
+++ common/app/controllers/EmailSignupController.scala
@@ -50,13 +50,13 @@ class EmailFormService(wsClient: WSClient) extends LazyLogging with RemoteAddres
     Future.failed(new IllegalAccessException("Form was likely submitted by a bot."))
   } else {
     val idAccessClientToken = Configuration.id.apiClientToken
-    println(request.headers)
+    logger.info("emailFormService - request headers" + request.headers.toMap.toString())
     val consentMailerUrl = s"${Configuration.id.apiRoot}/consent-email"
     val consentMailerPayload = JsObject(Json.obj("email" -> form.email, "set-lists" -> List(form.listName)).fields)
     val headers = clientIp(request)
       .map(ip => List("X-Forwarded-For" -> ip))
       .getOrElse(List.empty).:+("X-GU-ID-Client-Access-Token" -> s"Bearer $idAccessClientToken")
-    println(headers)
+    logger.info("emailFormService - request headers" + headers)
 
     //FIXME: this should go via the identity api client / app
     wsClient
