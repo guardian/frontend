796ed781c2eceeee212c785cdfd485306645508d
Anna Leach <anna,leach@theguardian.com>
2018-10-11 13:26:48 +0100

    New series page - header & containers

---
+++ common/app/conf/AudioFlagship.scala
@@ -0,0 +1,11 @@
+package conf
+
+object AudioFlagship {
+  val tagName = "culture/series/thestart"
+  val title = "Flagship Audio"
+  val seriesId = "the-start"
+  val subscribeLinks = Map(
+    "Apple Podcasts" -> "https://itunes.apple.com/us/podcast/id1337773497",
+    "Google Podcasts" -> "https://www.google.com/podcasts?feed=aHR0cHM6Ly93d3cudGhlZ3VhcmRpYW4uY29tL2N1bHR1cmUvc2VyaWVzL3RoZXN0YXJ0L3BvZGNhc3QueG1s",
+    "Spotify" -> "https://open.spotify.com/show/1Y95YPvBTyQTLxTNQalQcZ")
+}
