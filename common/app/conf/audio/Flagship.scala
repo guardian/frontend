ba5e31e95ba8e553f756bbf213eadcac999c7344
tomrf1 <tom.forbes@theguardian.com>
2018-10-11 09:51:55 +0100

    Flagship constants object

---
+++ common/app/conf/audio/Flagship.scala
@@ -0,0 +1,10 @@
+package conf.audio
+
+object Flagship {
+  val FrontContainerIds = Seq(
+    "75ef80cd-2f3d-40d6-abf6-2021f88ece8e", //PROD
+    "c57a70c8-a00a-4a15-93a2-035b9221622b"  //CODE
+  )
+
+  val AlbumArtUrl = "https://media.guim.co.uk/79284468f1b259db7d713dc24ea9af2a3f5c9937/0_0_800_800/500.png"
+}
