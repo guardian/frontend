6a8738162b5596332d045b4e30be4c03b7c855fd
Francis Carr <francis.carr@guardian.co.uk>
2018-10-11 10:57:15 +0100

    Remove or from pt8

--- static/src/javascripts/projects/common/modules/commercial/build-page-targeting.js
+++ static/src/javascripts/projects/common/modules/commercial/build-page-targeting.js
@@ -167,7 +167,7 @@ const buildAppNexusTargetingObject = once(
             pt5: pageTargeting.k,
             pt6: pageTargeting.su,
             pt7: pageTargeting.bp,
-            pt8: pageTargeting.x || '', // OpenX cannot handle this being undefined
+            pt8: pageTargeting.x, // OpenX cannot handle this being undefined
             pt9: [
                 pageTargeting.gdncrm,
                 pageTargeting.pv,
