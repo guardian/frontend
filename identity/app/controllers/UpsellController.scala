0234c67d68c97ad06425fc44f514980e637a729b
Calum Campbell <campbell.cj@hotmail.co.uk>
2018-10-19 17:46:03 +0100

    fix token passing

--- identity/app/controllers/UpsellController.scala
+++ identity/app/controllers/UpsellController.scala
@@ -46,7 +46,7 @@ class UpsellController(
     with SafeLogging
     with IdentitySwitches {
 
-  def confirmEmailThankYou(returnUrl: Option[String]): Action[AnyContent] = if (IdentityUseFollowSwitches.isSwitchedOn) {
+  def confirmEmailThankYou(returnUrl: Option[String]): Action[AnyContent] = if (IdentityUseFollowSwitches.isSwitchedOn) csrfAddToken {
     authenticatedActions.consentAuthWithIdapiUserAction.async { implicit request =>
       val returnUrl = returnUrlVerifier.getVerifiedReturnUrl(request)
       val email = request.user.primaryEmailAddress
