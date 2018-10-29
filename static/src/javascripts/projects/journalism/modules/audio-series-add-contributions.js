c26abaa4cd43735dbeac76c97debffbdc89c6363
Anna Leach <anna,leach@theguardian.com>
2018-10-29 17:27:39 +0000

    Make contrb banner fancy

--- static/src/javascripts/projects/journalism/modules/audio-series-add-contributions.js
+++ static/src/javascripts/projects/journalism/modules/audio-series-add-contributions.js
@@ -1,11 +1,15 @@
 // @flow
 
 import fastdom from 'lib/fastdom-promise';
+import template from 'lodash/template';
 import audioContribBanner from 'raw-loader!journalism/views/audioSeriesContributions.html';
+import { supportContributeURL } from '../../common/modules/commercial/support-utilities';
 
 const renderContributionsBanner = el => {
+    const banner = template(audioContribBanner)({ supportContributeURL });
+
     fastdom.write(() => {
-        el.insertAdjacentHTML('afterend', audioContribBanner);
+        el.insertAdjacentHTML('afterend', banner);
     });
 };
 
