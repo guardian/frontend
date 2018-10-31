83a30dbf83838598e3ed0062b8f0dbb7fe546c03
Jon Soul <jonathan.soul@guardian.co.uk>
2018-10-31 16:55:26 +0000

    Update copy

--- static/src/javascripts/projects/common/modules/identity/upsell/page/OptOuts.js
+++ static/src/javascripts/projects/common/modules/identity/upsell/page/OptOuts.js
@@ -8,8 +8,7 @@ import { OptOutsList } from '../opt-outs/OptOutsList';
 export const OptOuts = (): React.Component => (
     <Block sideBySideBackwards title="Your communication preferences">
         <LegalTextBlock>
-            You can also change these settings later by visiting the Emails &
-            marketing section of your account.
+            You can always change these preferences later by visiting your account settings.
         </LegalTextBlock>
         <OptOutsList />
     </Block>
