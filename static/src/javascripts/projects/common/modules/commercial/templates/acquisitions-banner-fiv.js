f9265ec75f849ff495f6d25a6f3163d3dc5be9c9
John Duffell <john.duffell@guardian.co.uk>
2018-11-09 16:51:42 +0000

    update tracking and open links in a new window

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-fiv.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-fiv.js
@@ -65,14 +65,14 @@ export const acquisitionsBannerFivTemplate = (
             </a>
         </div>
         <div class="fiv-banner__cta">
-            <a class="button fiv-banner__button fiv-banner__button_contribute" href="${linkUrl}">
+            <a class="button fiv-banner__button fiv-banner__button_contribute" href="${linkUrl}" target="_blank">
                 Contribute${arrowRight.markup}
             </a>
             ${
                 window.location.hash.match(/[#&]fiv-gb(&.*)?$/) ||
                 (geolocationGetSync() === 'GB' &&
                     !window.location.hash.match(/[#&]fiv-row(&.*)?$/))
-                    ? `<a class="button fiv-banner__button fiv-banner__button_subscribe" href="${subscribeUrl}">
+                    ? `<a class="button fiv-banner__button fiv-banner__button_subscribe" href="${subscribeUrl}" target="_blank">
                     Subscribe${arrowRight.markup}
                 </a>`
                     : ``
