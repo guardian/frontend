1cd113dc365286b5b5745a801a9ecfd2daba0211
John Duffell <john.duffell@guardian.co.uk>
2018-11-09 13:22:10 +0000

    add the styling mostly to the FIV banner

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-fiv.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-banner-fiv.js
@@ -1,9 +1,13 @@
 // @flow
 import potentialTitleWide from 'svgs/fivbanner/potential-title-wide.svg';
+import potentialTitleMobile from 'svgs/fivbanner/potential-title-mobile.svg';
+import existingTitle from 'svgs/fivbanner/existing-title.svg';
 import circles from 'svgs/fivbanner/circles.svg';
 import marque36icon from 'svgs/icon/marque-36.svg';
 import closeCentralIcon from 'svgs/icon/close-central.svg';
 import arrowRight from 'svgs/icon/arrow-right.svg';
+import { getSync as geolocationGetSync } from 'lib/geolocation';
+import { shouldSeeReaderRevenue } from 'common/modules/commercial/user-features';
 
 export const acquisitionsBannerFivTemplate = (
     location: string,
@@ -26,29 +30,51 @@ export const acquisitionsBannerFivTemplate = (
     </div>
     <div class="fiv-banner__container">
         <div class="fiv-banner__text">
-            <div class="fiv-banner__headline fiv_banner__potential">
+        ${
+            window.location.hash.match(/[#&]fiv-potential(&.*)?$/) ||
+            (shouldSeeReaderRevenue &&
+                !window.location.hash.match(/[#&]fiv-existing(&.*)?$/))
+                ? `<div class="fiv-banner__headline fiv-banner__wide">
                 ${potentialTitleWide.markup}
+            </div><div class="fiv-banner__headline fiv-banner__mobile">
+                ${potentialTitleMobile.markup}
             </div>
             <div class="fiv-banner__text_bold">
                 …we remain editorially independent, our journalism free from commercial bias and our reporting open and accessible to all.
             </div>
             <div class="fiv-banner__text_normal">
                 Imagine what we could continue to achieve with the support of many more of you. Together we can be a force for change.
+            </div>`
+                : `<div class="fiv-banner__headline">
+                ${existingTitle.markup}
             </div>
+            <div class="fiv-banner__text_bold">
+                …to the 1,000,000 readers who have helped to keep The Guardian’s independent journalism open and accessible to all.
+            </div>
+            <div class="fiv-banner__text_normal">
+                Imagine what the next million could achieve. Please read this update from our editor-in-chief and continue supporting us.
+            </div>`
+        }
         </div>
         <div class="fiv-banner__onwards">
             <a class="fiv-banner__onwards" href="${linkUrl}">
                 Read our story${arrowRight.markup}
             </a>
         </div>
-        <div class="fiv-banner__cta">
-            <button class="button fiv-banner__button fiv-banner__button_contribute" href="${linkUrl}">
-                Contribute${arrowRight.markup}
-            </button>
-            <button class="button fiv-banner__button fiv-banner__button_subscribe" href="${linkUrl}">
+    </div>
+    <div class="fiv-banner__cta">
+        <button class="button fiv-banner__button fiv-banner__button_contribute" href="${linkUrl}">
+            Contribute${arrowRight.markup}
+        </button>
+        ${
+            window.location.hash.match(/[#&]fiv-gb(&.*)?$/) ||
+            (geolocationGetSync() === 'GB' &&
+                !window.location.hash.match(/[#&]fiv-row(&.*)?$/))
+                ? `<button class="button fiv-banner__button fiv-banner__button_subscribe" href="${linkUrl}">
                 Subscribe${arrowRight.markup}
-            </button>
-        </div>
+            </button>`
+                : ``
+        }
     </div>
     <a
         class="u-faux-block-link__overlay"
