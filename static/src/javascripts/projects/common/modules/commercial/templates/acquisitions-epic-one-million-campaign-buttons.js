6a427201941f3ea7e121b6aac0ff29bdbe7042bb
Joseph Smith <joseph.smith@guardian.co.uk>
2018-11-09 11:53:50 +0000

    Indentation

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-one-million-campaign-buttons.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-one-million-campaign-buttons.js
@@ -7,26 +7,26 @@ export const oneMillionCampaignButtonsTemplate = () =>
         ${
             geolocationGetSync() === 'GB'
                 ? `<div>
-                <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member contributions__contribute--subscribe"
-                  href="https://support.theguardian.com/subscribe"
-                  target="_blank">
-                  Subscribe
-                </a>
-            </div>
-            <div>
-                <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member"
-                  href="https://support.theguardian.com/contribute"
-                  target="_blank">
-                  Contribute
-                </a>
-            </div>`
+                       <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member contributions__contribute--subscribe"
+                         href="https://support.theguardian.com/subscribe"
+                         target="_blank">
+                         Subscribe
+                       </a>
+                   </div>
+                   <div>
+                       <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member"
+                         href="https://support.theguardian.com/contribute"
+                         target="_blank">
+                         Contribute
+                       </a>
+                   </div>`
                 : `<div>
-                <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member"
-                  href="https://support.theguardian.com/contribute"
-                  target="_blank">
-                  Support the Guardian
-                </a>
-            </div>`
+                       <a class="contributions__option-button contributions__contribute contributions__contribute--epic contributions__contribute--epic-member"
+                         href="https://support.theguardian.com/contribute"
+                         target="_blank">
+                         Support the Guardian
+                       </a>
+                   </div>`
         }
         <img class="contributions__payment-logos contributions__contribute--epic-member" src="${config.get(
             'images.acquisitions.paypal-and-credit-card',
