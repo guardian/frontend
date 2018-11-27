6db4eb5cf498f561998aef5912dbfb3e9fedc234
Alex Ware <alex.ware@guardian.co.uk>
2018-11-27 14:11:24 +0000

    change group

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -54,5 +54,5 @@ object FakeShowcase extends Experiment(
   description = "upgrades an article to showcase to see what that does",
   owners = Seq(Owner.withGithub("aware")),
   sellByDate = new LocalDate(2019, 6, 6),
-  participationGroup = Perc20A
+  participationGroup = Perc10A
 )
