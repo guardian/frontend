98fa5a4099e5515a4a9d0b9beb99a82c5bfdbf6f
Laura <laura.gonzalez@guardian.co.uk>
2018-11-27 17:25:43 +0000

    drama

---
+++ static/src/javascripts/projects/admin/bootstraps/drama.js
@@ -0,0 +1,178 @@
+// @flow
+const typewrite = (message, $target) =>
+    new Promise(yay => {
+        let i = 0;
+        const len = message.length;
+        const itv = setInterval(() => {
+            i += 1;
+            $target.innerText = [...message.split('').splice(0, i), 'X'].join(
+                ''
+            );
+            if (i >= len) {
+                clearInterval(itv);
+                $target.innerText = message;
+                setTimeout(() => {
+                    yay();
+                }, 400);
+            }
+        }, 10);
+    });
+
+const timeout = (to, $target) =>
+    new Promise(yay => {
+        const target = Date.now() + to * 1000;
+        const loop = () =>
+            requestAnimationFrame(() => {
+                const diff = target - Date.now();
+                $target.innerText = `00:00:${Math.ceil(diff / 1000)
+                    .toString()
+                    .padStart(2, '0')}:${(diff % 1000)
+                    .toString()
+                    .padStart(3, '0')}`;
+                if (diff > 0) {
+                    loop();
+                } else {
+                    $target.innerText = `00:00:00:000`;
+                    yay();
+                }
+            });
+        loop();
+    });
+
+const RawMessage = title => {
+    const $wrapper = document.createElement('div');
+    $wrapper.classList.add('admin-drama__message');
+    const $title = document.createElement('span');
+    const $subtitle = document.createElement('strong');
+    $title.innerText = title;
+    [$title, $subtitle].forEach(_ => {
+        $wrapper.append(_);
+    });
+    return {
+        $wrapper,
+        $subtitle,
+        $title,
+        destroy: () => {
+            $wrapper.dataset.out = true;
+            // setTimeout(()=>{$wrapper.remove()},200);
+        },
+    };
+};
+
+const Message = (title, subtitle) => {
+    const { $wrapper, $title, $subtitle, destroy } = RawMessage(title);
+    return {
+        $wrapper,
+        $title,
+        $subtitle,
+        destroy,
+        type: () => typewrite(subtitle, $subtitle),
+    };
+};
+
+const Timeout = (title, to) => {
+    const { $wrapper, $title, $subtitle, destroy } = RawMessage(title);
+    $wrapper.dataset.critical = true;
+    return {
+        $wrapper,
+        $title,
+        $subtitle,
+        destroy,
+        type: () => timeout(to, $subtitle),
+    };
+};
+
+const Line = (line, isHead = false) => {
+    const $wrapper = document.createElement('div');
+    if(isHead) $wrapper.classList.add('admin-drama__head');
+    return {
+        $wrapper,
+        type: () => typewrite(line, $wrapper),
+    };
+};
+
+const LineHead = line => Line(line, true);
+
+const text = [
+    LineHead('System online'),
+    Line('User authenticated'),
+    Line('... Initiating launch routine'),
+    Line('OK'),
+    Line('Launch routine initiated'),
+    LineHead('Action required'),
+    Line('Confirm launch procedure'),
+    Line('Input launch passphrase now'),
+];
+
+const start = ($switchboard, $holder) => {
+    window.scrollTo(0, 0);
+
+    const $drama = $holder.querySelector('.admin-drama-innermost');
+    const $bg = $holder.querySelector('.admin-drama');
+    const $input = $drama.querySelector('.admin-drama__input');
+
+    const $form = document.createElement('form');
+    const $txt = document.createElement('input');
+    const $status = Message('Status report', 'access granted');
+    const $countdown = Timeout('Launch in', 5);
+
+    $form.appendChild($txt);
+    $input.type = 'password';
+
+    document.documentElement.classList.add('drama-init');
+
+    setTimeout(() => {
+        $switchboard.append($holder);
+    }, 500);
+
+    setTimeout(() => {
+        text.reduce(
+            (promiseChain, currentTask) =>
+                promiseChain.then(chainResults => {
+                    const { $wrapper, type } = currentTask;
+                    $input.appendChild($wrapper);
+                    return type().then(currentResult => [
+                        ...chainResults,
+                        currentResult,
+                    ]);
+                }),
+            Promise.resolve([])
+        ).then(() => {
+            $input.appendChild($form);
+            $txt.focus();
+            $form.addEventListener('submit', ev => {
+                ev.preventDefault();
+                $input.dataset.disabled = true;
+                setTimeout(() => {
+                    $bg.dataset.white = true;
+                    $drama.appendChild($status.$wrapper);
+                    $status.type().then(() => {
+                        $status.destroy();
+                        $drama.appendChild($countdown.$wrapper);
+                        $countdown.type().then(() => {
+                            $countdown.destroy();
+                            $switchboard.submit();
+                        });
+                    });
+                }, 500);
+            });
+        });
+    }, 2000);
+};
+
+const init = () => {
+    const $switchboard = document.querySelector('#switchboard');
+    const $trigger = $switchboard.querySelector('.drama-trigger');
+    const $holder = document.createElement('div');
+    $holder.innerHTML = `
+        <div class="admin-drama admin-drama"><div class="admin-drama__shaker admin-drama-innermost">
+            <div class="admin-drama__input">
+            </div>
+        </div></div>
+        `;
+    $trigger.addEventListener('click', () => {
+        start($switchboard, $holder);
+    });
+};
+
+export { init };
