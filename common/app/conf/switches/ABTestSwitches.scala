b4f4f64775034b66e8d18d23358623a6d2549acd
Tom Forbes <tom.forbes@theguardian.com>
2018-11-30 15:34:16 +0000

    US epic test round 4 (#20784)
    * us round 4 epic test
    
    * change test name

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -228,7 +228,7 @@ trait ABTestSwitches {
 
   Switch(
     ABTests,
-    "ab-acquisitions-epic-us-top-ticker",
+    "ab-acquisitions-epic-us-top-ticker-round-four",
     "Tests an epic with top ticker in US",
     owners = Seq(Owner.withGithub("joelochlann")),
     safeState = Off,
