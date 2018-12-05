8af662dbd6c5910950d3c5bab55f225c30a871b1
Kate Whalen <whalenka88@gmail.com>
2018-12-05 16:04:03 +0000

    Correctly catch the error if no LOTCC in window

--- static/src/javascripts/projects/commercial/modules/lotame-data-extract.js
+++ static/src/javascripts/projects/commercial/modules/lotame-data-extract.js
@@ -2,6 +2,12 @@
 import { loadScript } from 'lib/load-script';
 import config from 'lib/config';
 
+const errorHandler = (error: Error) => {
+    // Lotame fails to load 0.04% of the time. We dont
+    // want to pollute our sentry
+    console.log('Failed to extract lotame data:', error);
+};
+
 const shouldLoadLotame = (): boolean => {
     const edition = config.get('page.edition');
 
@@ -18,23 +24,19 @@ const init = (start: () => void): Promise<void> => {
     if (!shouldLoadLotame) {
         return Promise.resolve();
     }
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
         loadScript('//ad.crwdcntrl.net/5/c=13271/pe=y/var=OzoneLotameData')
             .then(() => {
                 setTimeout(resolve, 1000);
             })
             .then(() => {
                 if ('LOTCC' in window && 'bcp' in window.LOTCC) {
-                    resolve(window.LOTCC.bcp());
+                    Promise.resolve(window.LOTCC.bcp());
                 } else {
-                    reject(Error('No LOTCC in window'));
+                    return Promise.reject(Error('No LOTCC in window'));
                 }
             })
-            .catch(error => {
-                // Lotame fails to load 0.04% of the time. We dont
-                // want to pollute our sentry
-                console.log('Failed to extract lotame data:', error);
-            });
+            .catch(errorHandler);
     });
 };
 
