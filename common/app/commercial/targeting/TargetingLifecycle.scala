db37064cba9de248041d371d37fa85fec143d3e6
Anna Leach <anna,leach@theguardian.com>
2018-12-10 18:53:52 +0000

    add logging to CampaignAgentrefresh

--- common/app/commercial/targeting/TargetingLifecycle.scala
+++ common/app/commercial/targeting/TargetingLifecycle.scala
@@ -1,15 +1,17 @@
 package commercial.targeting
 
 import app.LifecycleComponent
-import common.{JobScheduler, AkkaAsync}
+import common.LoggingField.LogFieldString
+import common.{AkkaAsync, JobScheduler, Logging}
 import play.api.inject.ApplicationLifecycle
+
 import scala.concurrent.duration._
 import scala.concurrent.{Future, ExecutionContext}
 
 class TargetingLifecycle(
                           appLifecycle: ApplicationLifecycle,
                           jobs: JobScheduler,
-                          akkaAsync: AkkaAsync)(implicit executionContext: ExecutionContext) extends LifecycleComponent {
+                          akkaAsync: AkkaAsync)(implicit executionContext: ExecutionContext) extends LifecycleComponent with Logging {
 
 
   appLifecycle.addStopHook {
@@ -20,9 +22,21 @@ class TargetingLifecycle(
 
   override def start(): Unit = {
     jobs.deschedule("TargetingCampaignRefreshJob")
+
+    logInfoWithCustomFields("CampaignAgent BEFORE refresh scheduled ",
+      customFields = List(
+        LogFieldString("campaign agent", s"$CampaignAgent")
+      )
+    )
+
     jobs.scheduleEvery("TargetingCampaignRefreshJob", 1.minutes) {
       CampaignAgent.refresh()
     }
+
+    logInfoWithCustomFields(s"CampaignAgent AFTER refresh",
+      customFields = List(
+        LogFieldString("campaign agent", s"$CampaignAgent")
+    ))
 
     akkaAsync.after1s {
       CampaignAgent.refresh()
