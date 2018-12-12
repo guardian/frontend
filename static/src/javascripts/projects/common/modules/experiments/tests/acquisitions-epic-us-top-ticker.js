ab663b80a3ce468a9958621b0bec8a245b96bf49
Emma Milner <ekmilner@gmail.com>
2018-12-12 15:58:52 +0000

    copy tweaks

--- static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-epic-us-top-ticker.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-epic-us-top-ticker.js
@@ -66,10 +66,10 @@ const noteFromRebecca: AcquisitionsEpicTemplateCopy = {
         'The other is how much tyrants and would-be tyrants fear a free press – and what your enemies think is often the best way to measure whether you matter. They know autocracy depend on keeping the public ignorant on some fronts and misinformed on others. ',
         'We’ve seen direct attacks on journalists in the past year, from the murder of Jamal Khashoggi to Trump’s incessant attacks on the media as &ldquo;the enemy of the people,&rdquo; and for a couple of decades we’ve seen the indirect attacks that are Silicon Valley’s siphoning off of advertising revenue and amplification of untruths for profit.',
         'It costs a lot to send someone out to cover a campaign or to investigate a crime; it’s hard work that requires expertise and support from our readers. This year, The Guardian has covered everything from tech to feminism to Trump to fossil fuel politics. It is our editorial independence that has allowed us to deliver this fearless reporting; an independence that’s sometimes hard to find in other US-based media. We hope you appreciate our efforts.',
-        'The Guardian is asking our US readers to help us raise $1 million dollars by the new year to report on the most important stories in 2019. Every contribution, big or small, will help us hit our goal.',
+        'We want to say a huge thank you to everyone who has supported The Guardian so far. We hope to pass our goal by early January 2019. Every contribution, big or small, will help us reach it.',
     ],
     highlightedText:
-        'I hope you’ll consider making a year-end gift today to show your ongoing support for our independent journalism. Thank you.',
+        'Please make a year-end gift today to show your ongoing support for our independent journalism. Thank you.',
 };
 const noteFromJohn: AcquisitionsEpicTemplateCopy = {
     heading: 'In these critical times &hellip;',
@@ -79,10 +79,10 @@ const noteFromJohn: AcquisitionsEpicTemplateCopy = {
         'In normal times we might not be making this appeal. But these are not normal times. Many of the values and beliefs we hold dear at The Guardian are under threat both here in the US and around the world. Facts, science, humanity, diversity and equality are being challenged daily. As is truth. Which is why we need your help.',
         'Powerful public figures choose lies over truths, prefer supposition over science; and select hate over humanity. The US administration is foremost among them; whether in denying climate science or hating on immigrants; giving succor to racists or targeting journalists and the media. Many of these untruths and attacks find fertile ground on social media where tech platforms seem unable to cauterise lies. As a result, fake is in danger of overriding fact.',
         'Almost 100 years ago, in 1921, the editor of The Guardian argued that the principal role of a newspaper was accurate reporting, insisting that &ldquo;facts are sacred.&rdquo; We still hold that to be true. The need for a robust, independent press has never been greater, but the challenge is more intense than ever as digital disruption threatens traditional media’s business model. We pride ourselves on not having a paywall because we believe truth should not come at a price for anyone. Our journalism remains open and accessible to everyone and with your help we can keep it that way.',
-        'The Guardian is asking our US readers to help us raise $1 million dollars by the new year to report on the most important stories in 2019. Every contribution, big or small, will help us hit our goal.',
+        'We want to say a huge thank you to everyone who has supported The Guardian so far. We hope to pass our goal by early January 2019. Every contribution, big or small, will help us reach it.',
     ],
     highlightedText:
-        'I hope you’ll consider making a year-end gift today to show your ongoing support for our independent journalism. Thank you.',
+        'Please make a year-end gift today to show your ongoing support for our independent journalism. Thank you.',
 };
 export const acquisitionsEpicUsTopTicker: EpicABTest = makeABTest({
     id: abTestName,
