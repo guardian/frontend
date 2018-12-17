f5fd8ebe5964ef277742fef6510a6788f915b4ed
Joseph Smith <joseph.smith@guardian.co.uk>
2018-12-17 10:16:25 +0000

    Pick winning US epic variant

--- static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-epic-us-top-ticker.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/acquisitions-epic-us-top-ticker.js
@@ -20,57 +20,6 @@ const createTemplate = (tickerPosition: TickerPosition): EpicTemplate => (
         tickerPosition,
     });
 
-const control: AcquisitionsEpicTemplateCopy = {
-    heading: 'In these critical times &hellip;',
-    paragraphs: [
-        '&hellip; help us protect independent journalism at a time when factual, trustworthy reporting is under threat by making a year-end gift to support The Guardian. We’re asking our US readers to help us raise one million dollars by the new year so that we can report on the stories that matter in 2019. Small or big, every contribution you give will help us reach our goal.',
-        'The Guardian’s editorial independence means that we can pursue difficult investigations, challenging the powerful and holding them to account. No one edits our editor and no one steers our opinion.',
-        'In 2018, The Guardian broke the story of Cambridge Analytica’s Facebook data breach; we recorded the human fallout from family separations; we charted the rise of the far right, and documented the growing impact of gun violence on Americans’ lives. We reported daily on climate change as a matter of urgent priority. It was readers’ support that made this work possible.',
-        'As 2019 approaches, we would like to ask for your ongoing support. In an era of disinformation campaigns and partisan bots, trustworthy news sources that sort facts from lies are under threat like never before. Unlike many others we haven’t put up a paywall – we want to keep The Guardian’s reporting open to everyone, regardless of what they can afford. But we depend on voluntary contributions from readers.',
-        'We’re in this together – with your support we can keep exposing the truth. We hope to pass our goal by early January 2019. We want to say a huge thank you to everyone who has supported The Guardian so far.',
-    ],
-    highlightedText:
-        'Please invest in our independent journalism today by making a year-end gift.',
-};
-
-const outsidePerspective: AcquisitionsEpicTemplateCopy = {
-    heading: 'In these critical times &hellip;',
-    paragraphs: [
-        '&hellip; help us protect independent journalism at a time when factual, trustworthy reporting is under threat by making a year-end gift to support The Guardian. We’re asking our US readers to help us raise one million dollars by the new year so that we can report on the stories that matter in 2019. Small or big, every contribution you give will help us reach our goal.',
-        'The Guardian’s editorial independence means that we can pursue difficult investigations, challenging the powerful and holding them to account. No one edits our editor and no one steers our opinion.',
-        'In 2018, The Guardian broke the story of Cambridge Analytica’s Facebook data breach; we recorded the human fallout from family separations; we charted the rise of the far right, and documented the growing impact of gun violence on Americans’ lives. We reported daily on climate change as a matter of urgent priority. It was readers’ support that made this work possible.',
-        '<div class="contributions__epic__quote">&ldquo;Having an outsider’s perspective on America is refreshing. I like that you are constructive, critical and humorous&hellip; sometimes all in one article. The range of voices in your journalists is engaging and provoking. Your long reads and investigative articles are exceptional. I also like your environmental coverage, the opinions page, your arts page, interviews, and all the odd bits of life that other publications don’t cover. It was time I showed my support for your hard work, providing independent journalism with integrity.&rdquo; – Luke, US 🇺🇸</div>',
-        'We’re in this together – with your support we can keep exposing the truth. We hope to pass our goal by early January 2019. We want to say a huge thank you to everyone who has supported The Guardian so far.',
-    ],
-    highlightedText:
-        'Please invest in our independent journalism today by making a year-end gift.',
-};
-const dialUpHope: AcquisitionsEpicTemplateCopy = {
-    heading: 'In these critical times &hellip;',
-    paragraphs: [
-        '&hellip; help us protect independent journalism at a time when factual, trustworthy reporting is under threat by making a year-end gift to support The Guardian. We’re asking our US readers to help us raise one million dollars by the new year so that we can report on the stories that matter in 2019. Small or big, every contribution you give will help us reach our goal.',
-        'The Guardian’s editorial independence means that we can pursue difficult investigations, challenging the powerful and holding them to account. No one edits our editor and no one steers our opinion.',
-        'In 2018, The Guardian broke the story of Cambridge Analytica’s Facebook data breach; we recorded the human fallout from family separations; we charted the rise of the far right, and documented the growing impact of gun violence on Americans’ lives. We reported daily on climate change as a matter of urgent priority. It was readers’ support that made this work possible.',
-        'As 2019 approaches, we would like to ask for your ongoing support. We believe that independent journalism has the power to drive change and build hope. And there are reasons for hope, from the inspiring wave of youth activism around gun violence to the wins of the #metoo movement; from a new dynamism countering the far-right to the most diverse (and yet still nowhere near diverse enough) Congress in American history that took shape after the recent midterms. People long to feel hopeful again, and rigorous journalism that is empirically sourced and emphatically delivered can be a powerful catalyst for change. ',
-        'We’re in this together – with your support we can keep exposing the truth. We hope to pass our goal by early January 2019. We want to say a huge thank you to everyone who has supported The Guardian so far.',
-    ],
-    highlightedText:
-        'Please invest in our independent journalism today by making a year-end gift.',
-};
-const noteFromRebecca: AcquisitionsEpicTemplateCopy = {
-    heading: 'In these critical times &hellip;',
-    paragraphs: [
-        '&hellip; Guardian columnist and author Rebecca Solnit urges you to show your support for independent journalism with a year-end gift to The Guardian. We are asking our US readers to help us raise  $1 million dollars by the new year to report on the most important stories in 2019. ',
-        'A note from Rebecca:',
-        '&ldquo;First they came for the journalists,&rdquo; said the young man’s sign. &ldquo;We don’t know what happened after that.&rdquo; It’s a brilliant comment that underscores two things. One is how utterly necessary it is to a free, powerful, informed public, to have the ability to act on what happens beyond our own horizon, to make choices about our governments whether by blockading a senate or parliament or electing people we’ve learned about from the news.',
-        'The other is how much tyrants and would-be tyrants fear a free press – and what your enemies think is often the best way to measure whether you matter. They know autocracy depend on keeping the public ignorant on some fronts and misinformed on others. ',
-        'We’ve seen direct attacks on journalists in the past year, from the murder of Jamal Khashoggi to Trump’s incessant attacks on the media as &ldquo;the enemy of the people,&rdquo; and for a couple of decades we’ve seen the indirect attacks that are Silicon Valley’s siphoning off of advertising revenue and amplification of untruths for profit.',
-        'It costs a lot to send someone out to cover a campaign or to investigate a crime; it’s hard work that requires expertise and support from our readers. This year, The Guardian has covered everything from tech to feminism to Trump to fossil fuel politics. It is our editorial independence that has allowed us to deliver this fearless reporting; an independence that’s sometimes hard to find in other US-based media. We hope you appreciate our efforts.',
-        'We want to say a huge thank you to everyone who has supported The Guardian so far. We hope to pass our goal by early January 2019. Every contribution, big or small, will help us reach it.',
-    ],
-    highlightedText:
-        'Please make a year-end gift today to show your ongoing support for our independent journalism. Thank you.',
-};
 const noteFromJohn: AcquisitionsEpicTemplateCopy = {
     heading: 'In these critical times &hellip;',
     paragraphs: [
@@ -84,6 +33,7 @@ const noteFromJohn: AcquisitionsEpicTemplateCopy = {
     highlightedText:
         'Please make a year-end gift today to show your ongoing support for our independent journalism. Thank you.',
 };
+
 export const acquisitionsEpicUsTopTicker: EpicABTest = makeABTest({
     id: abTestName,
     campaignId: abTestName,
@@ -99,50 +49,6 @@ export const acquisitionsEpicUsTopTicker: EpicABTest = makeABTest({
     locations: ['US'],
     useLocalViewLog: true,
     variants: [
-        {
-            id: 'control',
-            products: [],
-            options: {
-                copy: control,
-                template: createTemplate('TOP'),
-                onView: () => {
-                    initTicker('.js-epic-ticker');
-                },
-            },
-        },
-        {
-            id: 'US_EoY_R7_OutsidePT',
-            products: [],
-            options: {
-                copy: outsidePerspective,
-                template: createTemplate('TOP'),
-                onView: () => {
-                    initTicker('.js-epic-ticker');
-                },
-            },
-        },
-        {
-            id: 'US_EoY_R7_Hope',
-            products: [],
-            options: {
-                copy: dialUpHope,
-                template: createTemplate('TOP'),
-                onView: () => {
-                    initTicker('.js-epic-ticker');
-                },
-            },
-        },
-        {
-            id: 'US_EoY_R7_Rebecca',
-            products: [],
-            options: {
-                copy: noteFromRebecca,
-                template: createTemplate('TOP'),
-                onView: () => {
-                    initTicker('.js-epic-ticker');
-                },
-            },
-        },
         {
             id: 'US_EoY_R7_John',
             products: [],
