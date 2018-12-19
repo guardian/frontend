b317dd951cda1ce7ee5a57fb56155ce4749fc8f9
Tom Forbes <tom.forbes@theguardian.com>
2018-12-19 14:27:58 +0000

    Use automated contributions ticker file  (#20857)
    Use automated contributions ticker file

--- static/src/javascripts/projects/common/modules/commercial/ticker.js
+++ static/src/javascripts/projects/common/modules/commercial/ticker.js
@@ -3,7 +3,6 @@ import { getLocalCurrencySymbol } from 'lib/geolocation';
 import fetchJSON from 'lib/fetch-json';
 
 const count = {};
-let showCount;
 let goal;
 let total;
 
@@ -74,24 +73,20 @@ const animate = (parentElementSelector: string) => {
     }
 };
 
-const dataSuccessfullyFetched = () => total && goal && showCount;
+const dataSuccessfullyFetched = () => total && goal;
 
 const fetchDataAndAnimate = (parentElementSelector: string) => {
     if (dataSuccessfullyFetched()) {
         animate(parentElementSelector);
     } else {
-        fetchJSON(
-            'https://interactive.guim.co.uk/docsdata-test/1ySn7Ol2NQLvvSw_eAnVrPuuRnaGOxUmaUs6svtu_irU.json',
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                mode: 'cors',
-            }
-        ).then(data => {
-            showCount = data.sheets.Sheet1[0].showCount === 'TRUE';
-            total = parseInt(data.sheets.Sheet1[0].total, 10);
-            goal = parseInt(data.sheets.Sheet1[0].goal, 10);
+        fetchJSON('https://support.theguardian.com/ticker.json', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+        }).then(data => {
+            total = parseInt(data.total, 10);
+            goal = parseInt(data.goal, 10);
 
             if (dataSuccessfullyFetched()) {
                 animate(parentElementSelector);
