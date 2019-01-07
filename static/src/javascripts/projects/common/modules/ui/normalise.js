cd9136ca7f7a049076168d799b73a3e457bfad0c
Alex Sanders <a@snd.rs>
2019-01-07 09:35:46 +0000

    promisify

--- static/src/javascripts/projects/common/modules/ui/normalise.js
+++ static/src/javascripts/projects/common/modules/ui/normalise.js
@@ -2,89 +2,90 @@
 
 export const go = () => {
     try {
-        (async () => {
-            const codex: {
-                dictionary: { [string]: string },
-                transforms: { [string]: string },
-            } = await fetch(
-                'https://cdn.jsdelivr.net/gh/sndrs/nfn/nfn.json'
-            ).then(response => response.json());
-
-            Object.keys(codex.dictionary).forEach(key => {
-                codex.dictionary[key.toLowerCase()] = codex.dictionary[
-                    key
-                ].toLowerCase();
-            });
-
-            const dictionaryRegex = new RegExp(
-                `(\\W)(${Object.keys(codex.dictionary).join(
-                    '|'
-                )})(s|ed|ing|)(\\W)`,
-                'g'
-            );
-
-            const transformRegexs = Object.keys(codex.transforms).reduce(
-                (regexs, transform) => ({
-                    [codex.transforms[transform]]: new RegExp(transform, 'g'),
-                    ...regexs,
-                }),
-                {}
-            );
-
-            const transforms = Object.keys(transformRegexs);
-
-            const normalise = (s: string) => {
-                transforms.forEach(transform => {
-                    // eslint-disable-next-line no-param-reassign
-                    s = s.replace(transformRegexs[transform], transform);
+        fetch('https://cdn.jsdelivr.net/gh/sndrs/nfn/nfn.json')
+            .then(response => response.json())
+            .then(({ dictionary, transforms }) => {
+                Object.keys(dictionary).forEach(key => {
+                    dictionary[key.toLowerCase()] = dictionary[
+                        key
+                    ].toLowerCase();
                 });
-                return s.replace(
-                    dictionaryRegex,
-                    (match, p1, p2, p3, p4) =>
-                        p1 + codex.dictionary[p2] + p3 + p4
+
+                const dictionaryRegex = new RegExp(
+                    `(\\W)(${Object.keys(dictionary).join(
+                        '|'
+                    )})(s|ed|ing|)(\\W)`,
+                    'g'
                 );
-            };
 
-            let node;
+                const transformRegexs = Object.keys(transforms).reduce(
+                    (regexs, transform) => ({
+                        [transforms[transform]]: new RegExp(transform, 'g'),
+                        ...regexs,
+                    }),
+                    {}
+                );
 
-            // $FlowFixMe (tree walker is confused in flow)
-            const walker = document.createTreeWalker(
-                document.body,
-                NodeFilter.SHOW_TEXT,
-                null,
-                false
-            );
+                const transformers = Object.keys(transformRegexs);
 
-            // eslint-disable-next-line no-cond-assign
-            while ((node = walker.nextNode())) {
-                node.nodeValue = normalise(node.nodeValue);
-            }
+                const normalise = (s: string) => {
+                    transformers.forEach(transformer => {
+                        // eslint-disable-next-line no-param-reassign
+                        s = s.replace(
+                            transformRegexs[transformer],
+                            transformer
+                        );
+                    });
+                    return s.replace(
+                        dictionaryRegex,
+                        (match, p1, p2, p3, p4) => p1 + dictionary[p2] + p3 + p4
+                    );
+                };
 
-            const observer = new MutationObserver(mutations => {
-                mutations.forEach(mutation => {
-                    if (mutation.addedNodes) {
-                        Array.from(mutation.addedNodes).forEach(addedNode => {
-                            if (
-                                addedNode.nodeValue &&
-                                addedNode.parentNode &&
-                                addedNode.parentNode.nodeName !== 'STYLE' &&
-                                addedNode.parentNode.nodeName !== 'SCRIPT'
-                            ) {
-                                addedNode.nodeValue = normalise(
-                                    addedNode.nodeValue
-                                );
-                            }
-                        });
-                    }
+                let node;
+
+                // $FlowFixMe (tree walker is confused in flow)
+                const walker = document.createTreeWalker(
+                    document.body,
+                    NodeFilter.SHOW_TEXT,
+                    null,
+                    false
+                );
+
+                // eslint-disable-next-line no-cond-assign
+                while ((node = walker.nextNode())) {
+                    node.nodeValue = normalise(node.nodeValue);
+                }
+
+                const observer = new MutationObserver(mutations => {
+                    mutations.forEach(mutation => {
+                        if (mutation.addedNodes) {
+                            Array.from(mutation.addedNodes).forEach(
+                                addedNode => {
+                                    if (
+                                        addedNode.nodeValue &&
+                                        addedNode.parentNode &&
+                                        addedNode.parentNode.nodeName !==
+                                            'STYLE' &&
+                                        addedNode.parentNode.nodeName !==
+                                            'SCRIPT'
+                                    ) {
+                                        addedNode.nodeValue = normalise(
+                                            addedNode.nodeValue
+                                        );
+                                    }
+                                }
+                            );
+                        }
+                    });
+                });
+
+                observer.observe(document, {
+                    characterData: true,
+                    subtree: true,
+                    childList: true,
                 });
             });
-
-            observer.observe(document, {
-                characterData: true,
-                subtree: true,
-                childList: true,
-            });
-        })();
     } catch (e) {
         console.error(e); // eslint-disable-line no-console
     }
