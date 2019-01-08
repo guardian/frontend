5c2a2bd53920f6d44667aaaefcca1ad1cb56e493
Joseph Smith <joseph.smith@guardian.co.uk>
2019-01-08 21:33:24 +0000

    Prettier

--- static/src/javascripts/projects/common/modules/experiments/ab-url.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-url.js
@@ -4,9 +4,7 @@ import { testAndParticipationsToVariant } from './ab-utils';
 
 export const getForcedParticipationsFromUrl = (): Participations => {
     if (window.location.hash.startsWith('#ab')) {
-        const tokens = window.location.hash
-            .replace('#ab-', '')
-            .split(',');
+        const tokens = window.location.hash.replace('#ab-', '').split(',');
 
         return tokens.reduce((obj, token) => {
             const [testId, variantId] = token.split('=');
