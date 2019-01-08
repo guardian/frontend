7ee19b5381a6e90846d43a11be3efd5c2b3bd900
Joseph Smith <joseph.smith@guardian.co.uk>
2019-01-08 21:06:59 +0000

    lint

--- static/src/javascripts/projects/common/modules/experiments/ab-utils.spec.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-utils.spec.js
@@ -1,37 +1,26 @@
 // @flow
-// @flow
-// @flow
-// @flow
-// @flow
-// @flow
-// @flow
-// @flow
-// @flow
-// @flow
 
-// import { arrayToParticipations } from './ab-utils';
-//
-// describe('A/B local storage', () => {
-//     test('arrayToParticipations should correctly convert array to object', () => {
-//         expect(
-//             arrayToParticipations([
-//                 { testId: 't', variantId: 'v' },
-//                 { testId: 'j', variantId: 'g' },
-//             ])
-//         ).toEqual({
-//             t: { variant: 'v' },
-//             j: { variant: 'g' },
-//         });
-//
-//         expect(
-//             arrayToParticipations([
-//                 { testId: 't', variantId: 'v' },
-//                 { testId: 't', variantId: 'w' },
-//             ])
-//         ).toEqual({
-//             t: { variant: 'w' },
-//         });
-//
-//         expect(arrayToParticipations([])).toEqual({});
-//     });
-// });
+describe('A/B local storage', () => {
+    // test('arrayToParticipations should correctly convert array to object', () => {
+    //     expect(
+    //         arrayToParticipations([
+    //             { testId: 't', variantId: 'v' },
+    //             { testId: 'j', variantId: 'g' },
+    //         ])
+    //     ).toEqual({
+    //         t: { variant: 'v' },
+    //         j: { variant: 'g' },
+    //     });
+    //
+    //     expect(
+    //         arrayToParticipations([
+    //             { testId: 't', variantId: 'v' },
+    //             { testId: 't', variantId: 'w' },
+    //         ])
+    //     ).toEqual({
+    //         t: { variant: 'w' },
+    //     });
+    //
+    //     expect(arrayToParticipations([])).toEqual({});
+    // });
+});
