2ccd01f16d56f59a480d3f55d0dabaa361c65a6f
adamnfish <adam.fisher@guardian.co.uk>
2019-01-10 18:08:55 +0000

    Add RH Side ad slot logic for immersives
    We now only show RH-side adverts on immersives if there is space for
    the ad, and we adjust which ad sizes the slot will use to make the
    best use of whatever space exists. We also make the ad non-sticky in
    these cases so that the meassurements we've taken apply even as the
    user scrolls.
    
    If the page is immersive we find the vertical distance to the first
    immersive element and use this to dictate which (if any) ads are
    available. If the immersive has any immersive elements we disable
    stickiness for the RH-side slot.

--- static/src/javascripts/projects/commercial/modules/article-aside-adverts.spec.js
+++ static/src/javascripts/projects/commercial/modules/article-aside-adverts.spec.js
@@ -1,5 +1,6 @@
 // @flow
 /* eslint-disable no-new */
+import config from 'lib/config';
 import qwery from 'qwery';
 import mediator from 'lib/mediator';
 import { noop } from 'lib/noop';
@@ -12,11 +13,75 @@ jest.mock('common/modules/commercial/commercial-features', () => ({
     },
 }));
 
-const mockMainColHeight = (height: number) => {
-    jest.spyOn(fastdom, 'read').mockReturnValue(Promise.resolve(height));
+const fastdomReadSpy = jest.spyOn(fastdom, 'read');
+
+jest.mock('lib/config', () => {
+    return {
+        get: jest.fn()
+    }
+});
+const configSpy = jest.spyOn(config, 'get');
+
+const sharedBeforeEach = (domSnippet: string) => (() => {
+    jest.resetAllMocks();
+
+    if (document.body) {
+        document.body.innerHTML = domSnippet;
+    }
+
+    const pubAds = {
+        listeners: [],
+        addEventListener: jest.fn(function(eventName, callback) {
+            this.listeners[eventName] = callback;
+        }),
+        setTargeting: jest.fn(),
+        enableSingleRequest: jest.fn(),
+        collapseEmptyDivs: jest.fn(),
+        refresh: jest.fn(),
+    };
+    const sizeMapping = {
+        sizes: [],
+        addSize: jest.fn(function(width, sizes) {
+            this.sizes.unshift([width, sizes]);
+        }),
+        build: jest.fn(function() {
+            const tmp = this.sizes;
+            this.sizes = [];
+            return tmp;
+        }),
+    };
+    window.googletag = {
+        cmd: {
+            push(...args) {
+                args.forEach(command => {
+                    command();
+                });
+            },
+        },
+        pubads() {
+            return pubAds;
+        },
+        sizeMapping() {
+            return sizeMapping;
+        },
+        defineSlot: jest.fn(() => window.googletag),
+        defineOutOfPageSlot: jest.fn(() => window.googletag),
+        addService: jest.fn(() => window.googletag),
+        defineSizeMapping: jest.fn(() => window.googletag),
+        setTargeting: jest.fn(() => window.googletag),
+        enableServices: jest.fn(),
+        display: jest.fn(),
+    };
+    expect.hasAssertions();
+});
+
+const sharedAfterEach = () => {
+    if (document.body) {
+        document.body.innerHTML = '';
+    }
 };
 
-describe('Article Aside Adverts', () => {
+describe('Standard Article Aside Adverts', () => {
     const domSnippet = `
         <div class="js-content-main-column"></div>
         <div class="content__secondary-column js-secondary-column">
@@ -26,64 +91,8 @@ describe('Article Aside Adverts', () => {
         </div>
     `;
 
-    beforeEach(() => {
-        jest.resetAllMocks();
-
-        if (document.body) {
-            document.body.innerHTML = domSnippet;
-        }
-
-        const pubAds = {
-            listeners: [],
-            addEventListener: jest.fn(function(eventName, callback) {
-                this.listeners[eventName] = callback;
-            }),
-            setTargeting: jest.fn(),
-            enableSingleRequest: jest.fn(),
-            collapseEmptyDivs: jest.fn(),
-            refresh: jest.fn(),
-        };
-        const sizeMapping = {
-            sizes: [],
-            addSize: jest.fn(function(width, sizes) {
-                this.sizes.unshift([width, sizes]);
-            }),
-            build: jest.fn(function() {
-                const tmp = this.sizes;
-                this.sizes = [];
-                return tmp;
-            }),
-        };
-        window.googletag = {
-            cmd: {
-                push(...args) {
-                    args.forEach(command => {
-                        command();
-                    });
-                },
-            },
-            pubads() {
-                return pubAds;
-            },
-            sizeMapping() {
-                return sizeMapping;
-            },
-            defineSlot: jest.fn(() => window.googletag),
-            defineOutOfPageSlot: jest.fn(() => window.googletag),
-            addService: jest.fn(() => window.googletag),
-            defineSizeMapping: jest.fn(() => window.googletag),
-            setTargeting: jest.fn(() => window.googletag),
-            enableServices: jest.fn(),
-            display: jest.fn(),
-        };
-        expect.hasAssertions();
-    });
-
-    afterEach(() => {
-        if (document.body) {
-            document.body.innerHTML = '';
-        }
-    });
+    beforeEach(sharedBeforeEach(domSnippet));
+    afterEach(sharedAfterEach);
 
     it('should exist', () => {
         expect(init).toBeDefined();
@@ -91,7 +100,8 @@ describe('Article Aside Adverts', () => {
     });
 
     it('should have the correct size mappings and classes', done => {
-        mockMainColHeight(900000);
+        configSpy.mockReturnValueOnce(false);
+        fastdomReadSpy.mockReturnValue(Promise.resolve([900000, 0]));
         mediator.once('page:defaultcommercial:right', adSlot => {
             expect(adSlot.classList).toContain('js-sticky-mpu');
             expect(adSlot.getAttribute('data-mobile')).toBe(
@@ -103,11 +113,97 @@ describe('Article Aside Adverts', () => {
     });
 
     it('should mutate the ad slot in short articles', done => {
-        mockMainColHeight(10);
+        configSpy.mockReturnValueOnce(false);
+        fastdomReadSpy.mockReturnValue(Promise.resolve([10, 0]));
         mediator.once('page:defaultcommercial:right', adSlot => {
             expect(adSlot.classList).not.toContain('js-sticky-mpu');
             expect(adSlot.getAttribute('data-mobile')).toBe(
                 '1,1|2,2|300,250|300,274|fluid'
+            );
+            done();
+        });
+        init(noop, noop);
+    });
+});
+
+
+describe('Immersive Article Aside Adverts', () => {
+
+    const domSnippet = `
+        <div class="js-content-main-column">
+            <figure class="element element--immersive"></figure>
+            <figure class="element element--immersive"></figure>
+        </div>
+        <div class="content__secondary-column js-secondary-column">
+            <div class="ad-slot-container">
+                <div id="dfp-ad--right" class="js-ad-slot ad-slot ad-slot--right ad-slot--mpu-banner-ad js-sticky-mpu ad-slot--rendered" data-link-name="ad slot right" data-name="right" data-mobile="1,1|2,2|300,250|300,274|300,600|fluid"></div>
+            </div>
+        </div>
+    `;
+    beforeEach(sharedBeforeEach(domSnippet));
+    afterEach(sharedAfterEach);
+
+    it('should have correct test elements', () => {
+        expect(qwery('.js-content-main-column .element--immersive').length).toBe(2);
+    });
+
+    it('should remove sticky and return all slot sizes when there is enough space', done => {
+        fastdomReadSpy.mockReturnValueOnce(Promise.resolve([900001, 10000]));
+        configSpy.mockReturnValueOnce(true);
+
+        mediator.once('page:defaultcommercial:right', adSlot => {
+            expect(adSlot.classList).not.toContain('js-sticky-mpu');
+            const sizes = adSlot.getAttribute('data-mobile').split("|");
+            expect(sizes).toContain("1,1");
+            expect(sizes).toContain("2,2");
+            expect(sizes).toContain("300,250");
+            expect(sizes).toContain("300,274");
+            expect(sizes).toContain("300,600");
+            expect(sizes).toContain("fluid");
+            done();
+        });
+        init(noop, noop);
+    });
+
+    it('should remove sticky and return sizes that will fit when there is limited space', done => {
+        fastdomReadSpy.mockReturnValueOnce(Promise.resolve([900002, 260]));
+        configSpy.mockReturnValueOnce(true);
+
+        mediator.once('page:defaultcommercial:right', adSlot => {
+            expect(adSlot.classList).not.toContain('js-sticky-mpu');
+            const sizes = adSlot.getAttribute('data-mobile').split("|");
+            expect(sizes).toContain("1,1");
+            expect(sizes).toContain("2,2");
+            expect(sizes).toContain("300,250");
+            expect(sizes).not.toContain("300,274");
+            expect(sizes).not.toContain("300,600");
+            expect(sizes).not.toContain("fluid");
+            done();
+        });
+        init(noop, noop);
+    });
+});
+
+describe('Immersive Article (no immersive elements) Aside Adverts', () => {
+
+    const domSnippet = `
+        <div class="js-content-main-column"></div>
+        <div class="content__secondary-column js-secondary-column">
+            <div class="ad-slot-container">
+                <div id="dfp-ad--right" class="js-ad-slot ad-slot ad-slot--right ad-slot--mpu-banner-ad js-sticky-mpu ad-slot--rendered" data-link-name="ad slot right" data-name="right" data-mobile="1,1|2,2|300,250|300,274|300,600|fluid"></div>
+            </div>
+        </div>
+    `;
+    beforeEach(sharedBeforeEach(domSnippet));
+    afterEach(sharedAfterEach);
+
+    it('should have the correct size mappings and classes (leaves it untouched)', done => {
+        configSpy.mockReturnValueOnce(true);
+        fastdomReadSpy.mockReturnValue(Promise.resolve([900000, 0]));
+        mediator.once('page:defaultcommercial:right', adSlot => {
+            expect(adSlot.classList).toContain('js-sticky-mpu');
+            expect(adSlot.getAttribute('data-mobile')).toBe(
+                '1,1|2,2|300,250|300,274|300,600|fluid'
             );
             done();
         });
