2540dd604a9d183f21ef482c1349e62969e12049
Joseph Smith <joseph.smith@guardian.co.uk>
2019-01-14 14:59:41 +0000

    Add ab.spec.js tests

--- static/src/javascripts/projects/common/modules/experiments/__mocks__/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/__mocks__/ab-tests.js
@@ -1,6 +1,15 @@
 // @flow
 import { genAbTest } from '../__fixtures__/ab-test';
+import { genVariant } from 'common/modules/experiments/__fixtures__/ab-test';
 
-export const concurrentTests = [genAbTest('DummyTest'), genAbTest('DummyTest2')];
+export const concurrentTests = [
+    genAbTest('DummyTest'),
+    genAbTest('DummyTest2'),
+    genAbTest('DummyTest3CanRunIsFalse', false),
+    genAbTest('DummyTest4ControlCanRunIsFalse', true, '9999-01-01', [
+        genVariant('control', false),
+        genVariant('variant', true),
+    ]),
+];
 export const epicTests = [genAbTest('EpicTest'), genAbTest('EpicTest2')];
 export const engagementBannerTests = [genAbTest('BannerTest'), genAbTest('BannerTest2')];
