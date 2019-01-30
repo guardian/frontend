682a4cf4f9269bc03f245daa0b6fd38ea1915fd6
Tom Richards <tom.richards@guardian.co.uk>
2019-01-30 14:45:01 +0000

    deleted lots of code related to the reader revenue MMA tabs (which have all been migrated to manage-frontend)

--- static/src/javascripts/bootstraps/enhanced/membership.js
+++ static/src/javascripts/bootstraps/enhanced/membership.js
@@ -1,12 +1,6 @@
 // @flow
-import { membershipTab } from 'membership/membership-tab';
-import { digitalpackTab } from 'membership/digitalpack-tab';
-import { recurringContributionTab } from 'membership/contributions-recurring-tab';
 import { deleteOldData } from 'common/modules/commercial/user-features';
 
 export const init = (): void => {
     deleteOldData();
-    membershipTab();
-    digitalpackTab();
-    recurringContributionTab();
 };
