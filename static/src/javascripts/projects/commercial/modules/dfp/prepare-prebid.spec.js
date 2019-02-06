1d5a7eafaa11b802c3990393ddad6cdc9df37fff
Francis Carr <francis.carr@guardian.co.uk>
2019-02-06 17:20:44 +0000

    Add plugin dynamic-import-node for test context and use that Add tests

--- static/src/javascripts/projects/commercial/modules/dfp/prepare-prebid.spec.js
+++ static/src/javascripts/projects/commercial/modules/dfp/prepare-prebid.spec.js
@@ -3,7 +3,9 @@
 import { prebid } from 'commercial/modules/prebid/prebid';
 import { dfpEnv } from 'commercial/modules/dfp/dfp-env';
 import { commercialFeatures } from 'common/modules/commercial/commercial-features';
-import { init } from './prepare-prebid';
+import { init, _ } from './prepare-prebid';
+
+const { isGoogleWebPreview } = _;
 
 jest.mock('common/modules/commercial/commercial-features', () => ({
     commercialFeatures: {},
@@ -23,12 +25,23 @@ jest.mock('commercial/modules/prebid/bid-config', () => ({
     isInVariant: jest.fn(),
 }));
 
+const fakeUserAgent = (userAgent: string): void => {
+    Object.defineProperty(navigator, 'userAgent', {
+        get() {
+            return userAgent;
+        },
+        configurable: true,
+    });
+};
+
 describe('init', () => {
     let mockInitialise;
+    const originalUA = navigator.userAgent;
 
     beforeEach(() => {
         mockInitialise = jest.fn();
         (prebid: any).initialise = mockInitialise.bind(prebid);
+        fakeUserAgent(originalUA);
     });
 
     it('should initialise Prebid when external demand is Prebid and advertising is on and ad-free is off', () => {
@@ -37,6 +50,20 @@ describe('init', () => {
         commercialFeatures.adFree = false;
         init(jest.fn(), jest.fn());
         expect(mockInitialise).toBeCalled();
+    });
+
+    it('should initialise Prebid when useragent is not Google Web Preview', () => {
+        dfpEnv.externalDemand = 'prebid';
+        commercialFeatures.dfpAdvertising = true;
+        commercialFeatures.adFree = false;
+        init(jest.fn(), jest.fn());
+        expect(mockInitialise).toBeCalled();
+    });
+
+    it('should not initialise Prebid when useragent is Google Web Preview', () => {
+        fakeUserAgent('Google Web Preview');
+        init(jest.fn(), jest.fn());
+        expect(mockInitialise).not.toBeCalled();
     });
 
     it('should not initialise Prebid when no external demand', () => {
@@ -55,5 +82,19 @@ describe('init', () => {
         commercialFeatures.adFree = true;
         init(jest.fn(), jest.fn());
         expect(mockInitialise).not.toBeCalled();
+    });
+
+    it('isGoogleWebPreview should return false with no navigator or useragent', () => {
+        expect(isGoogleWebPreview()).toBe(false);
+    });
+
+    it('isGoogleWebPreview should return false with no navigator or useragent', () => {
+        fakeUserAgent('Firefox');
+        expect(isGoogleWebPreview()).toBe(false);
+    });
+
+    it('isGoogleWebPreview should return true with Google Web Preview useragent', () => {
+        fakeUserAgent('Google Web Preview');
+        expect(isGoogleWebPreview()).toBe(true);
     });
 });
