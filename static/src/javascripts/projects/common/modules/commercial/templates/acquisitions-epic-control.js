95964e8784660df086dc8ba32c5a69f7da45c880
tomrf1 <tom.forbes@theguardian.com>
2019-02-13 11:06:09 +0000

    styling

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-control.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-control.js
@@ -35,12 +35,12 @@ export const acquisitionsEpicControlTemplate = ({
             ${buttonTemplate || ''}
             
             ${footer ?
-                `<div class="contributions__one-million-footer">
+                `<div class="contributions__epic-footer">
                     ${
                         footer.map(line => {
                             const firstSpaceIndex = line.trim().indexOf(' ');
-                            return `<h2><span class="one-million-blue">${line.substring(0, firstSpaceIndex)}</span>${line.substring(firstSpaceIndex)}</h2>`;
-                        })
+                            return `<h2><span class="contributions__epic-footer-blue">${line.substring(0, firstSpaceIndex)}</span>${line.substring(firstSpaceIndex)}</h2>`;
+                        }).join('')
                     }
                 </div>`
             : ''}
