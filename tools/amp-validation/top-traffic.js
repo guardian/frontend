3d96cef11372fa7348d06e3bffdd74987745a736
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-02-14 14:28:56 +0000

    Update the Amp validator to fetch most read from Ophan for sections

--- tools/amp-validation/top-traffic.js
+++ tools/amp-validation/top-traffic.js
@@ -1,9 +1,19 @@
-const fetchEndpoints = require('./endpoints/ophan');
+const { fetch, getEndpointsFromResponse } = require('./endpoints/ophan');
 const run = require('./run');
 
-fetchEndpoints.then(
-    run({
-        checkIfAmp: true,
-        logErrors: false,
-    })
-);
+const sections = ['music', 'football'];
+const fetchPath = path => fetch(path).then(getEndpointsFromResponse);
+
+Promise.all(
+    sections
+        .map(section => fetchPath(`/api/mostread/${section}?count=25`))
+        .concat(fetchPath(`/api/mostread?count=30`))
+)
+    .then(urlArrays => [].concat(...urlArrays)) // Flatten the array of arrays of paths
+    .then(
+        run({
+            checkIfAmp: true,
+            logErrors: false,
+        })
+    )
+    .catch(err => console.log(err));
