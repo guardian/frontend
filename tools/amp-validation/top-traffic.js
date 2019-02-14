1ad1f65cd84048958d4cae8afff6a04a8e9c453e
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-02-14 14:44:50 +0000

    Add keyword tag to validator

--- tools/amp-validation/top-traffic.js
+++ tools/amp-validation/top-traffic.js
@@ -2,13 +2,16 @@ const { fetch, getEndpointsFromResponse } = require('./endpoints/ophan');
 const run = require('./run');
 
 const sections = ['music', 'football'];
+const keywordTags = ['info%2Fseries%2Fdigital-blog'];
 const fetchPath = path => fetch(path).then(getEndpointsFromResponse);
 
-Promise.all(
-    sections
-        .map(section => fetchPath(`/api/mostread/${section}?count=25`))
-        .concat(fetchPath(`/api/mostread?count=30`))
-)
+Promise.all([
+    fetchPath(`/api/mostread?count=30`),
+    ...keywordTags.map(keywordTag =>
+        fetchPath(`/api/mostread/keywordtag/${keywordTag}?count=25`)
+    ),
+    ...sections.map(section => fetchPath(`/api/mostread/${section}?count=25`)),
+])
     .then(urlArrays => [].concat(...urlArrays)) // Flatten the array of arrays of paths
     .then(
         run({
