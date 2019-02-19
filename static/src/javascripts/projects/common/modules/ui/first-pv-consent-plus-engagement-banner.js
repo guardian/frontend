bcc0169919a24a95f1d8fae28de8b9ab354b1100
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2019-02-19 11:57:35 +0000

    formatting

--- static/src/javascripts/projects/common/modules/ui/first-pv-consent-plus-engagement-banner.js
+++ static/src/javascripts/projects/common/modules/ui/first-pv-consent-plus-engagement-banner.js
@@ -22,7 +22,10 @@ import reportError from 'lib/report-error';
 
 const messageCode: string = 'first-pv-consent-plus-engagement-banner';
 
-const doubleBannerHtml = (engagementBannerHtml: string, customClass: string): string => `
+const doubleBannerHtml = (
+    engagementBannerHtml: string,
+    customClass: string
+): string => `
     <div class="site-message js-site-message js-double-site-message site-message--banner site-message--double-banner" tabindex="-1" role="dialog" aria-label="welcome" aria-describedby="site-message__message" data-component="AcquisitionsEngagementBannerStylingTweaks_control">
         <div class="js-engagement-banner-site-message ${customClass} site-message--engagement-banner">
             <div class="gs-container">
@@ -108,18 +111,19 @@ const show = (): Promise<boolean> => {
     trackFirstPvConsent();
     return getEngagementBannerTestToRun()
         .then(deriveEngagementBannerParams)
-        .then(params => {
-            return {
-                params,
-                html: engagementBannerParamsToHtml(params)
-            }
-        })
+        .then(params => ({
+            params,
+            html: engagementBannerParamsToHtml(params),
+        }))
         .then(paramsAndEngagementBannerHtml =>
             fastdom.write(() => {
-                const modifierClass =
-                    paramsAndEngagementBannerHtml.params.bannerModifierClass
-                        ? `site-message--${paramsAndEngagementBannerHtml.params.bannerModifierClass}`
-                        : '';
+                const modifierClass = paramsAndEngagementBannerHtml.params
+                    .bannerModifierClass
+                    ? `site-message--${
+                          paramsAndEngagementBannerHtml.params
+                              .bannerModifierClass
+                      }`
+                    : '';
                 const html = doubleBannerHtml(
                     paramsAndEngagementBannerHtml.html,
                     modifierClass
@@ -137,7 +141,7 @@ const show = (): Promise<boolean> => {
                 new Error(
                     `Could not show banner within double banner. ${
                         err.message
-                        }. Stack: ${err.stack}`
+                    }. Stack: ${err.stack}`
                 ),
                 { feature: 'engagement-banner' },
                 false
