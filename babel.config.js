becb55e9fb8b2e145b54a1be9cc27259aede71dd
Francis Carr <francis.carr@guardian.co.uk>
2019-02-20 14:24:01 +0000

    Remove unused jest module

--- babel.config.js
+++ babel.config.js
@@ -41,7 +41,6 @@ module.exports = {
             "plugins": [
                 "@babel/plugin-transform-runtime",
                 "@babel/plugin-proposal-class-properties",
-                "dynamic-import-node",
             ],
         },
         "internal": {
