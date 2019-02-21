92c91e565b7ec6e0d22c6b01fb8bea81c9854a9f
tomrf1 <tom.forbes@theguardian.com>
2019-02-21 13:57:58 +0000

    better canRun

--- static/src/javascripts/projects/common/modules/experiments/tests/adblock-ask.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/adblock-ask.js
@@ -1,4 +1,6 @@
 // @flow
+import { userIsSupporter } from 'common/modules/commercial/user-features';
+import { pageShouldHideReaderRevenue } from 'common/modules/commercial/contributions-utilities';
 
 const supportUrl =
     'https://support.theguardian.com/contribute?acquisitionData=%7B%22componentType%22%3A%22ACQUISITIONS_OTHER%22%2C%22source%22%3A%22GUARDIAN_WEB%22%2C%22campaignCode%22%3A%22shady_pie_open_2019%22%2C%22componentId%22%3A%22shady_pie_open_2019%22%7D&INTCMP=shady_pie_open_2019';
@@ -38,7 +40,7 @@ export const adblockTest: ABTest = {
     audienceCriteria: '',
     showForSensitive: true,
     canRun() {
-        return true;
+        return !userIsSupporter() && !pageShouldHideReaderRevenue();
     },
 
     variants: [
