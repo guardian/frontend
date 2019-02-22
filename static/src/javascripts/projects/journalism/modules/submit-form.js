425e281305292e9dcf204f931fb664f7730f8417
Anna Leach <anna,leach@theguardian.com>
2019-02-22 14:42:38 +0000

    fix file submission callouts

--- static/src/javascripts/projects/journalism/modules/submit-form.js
+++ static/src/javascripts/projects/journalism/modules/submit-form.js
@@ -3,6 +3,7 @@ import fastdom from 'lib/fastdom-promise';
 import fetch from 'lib/fetch';
 
 const isCheckbox = element => element.type === 'checkbox';
+const isFile = element => element.type === 'file';
 const isNamed = element => element.name > '';
 
 const disableButton = button => {
@@ -24,13 +25,21 @@ const showConfirmation = cForm => {
     }
 };
 
-const showError = cForm => {
+const showError = (cForm, msg) => {
     const errorField = cForm.querySelector('.error_box');
     fastdom.write(() => {
-        errorField.innerHTML =
-            '<p class="error">Sorry, there was a problem submitting your form. Please try again later.</p>';
+        errorField.innerHTML = `<p class="error">${msg}</p>`;
     });
     enableButton(cForm);
+};
+
+const showFileUploadError = (el, msg) => {
+    fastdom.write(() => {
+        const errorBox = document.createElement('p');
+        errorBox.textContent = msg;
+        errorBox.classList.add('error');
+        el.appendChild(errorBox);
+    });
 };
 
 const showWaiting = cForm => {
@@ -43,31 +52,66 @@ const showWaiting = cForm => {
     });
 };
 
-export const formatData = (elements: any) =>
+const readFile = (file, inputEl) =>
+    new Promise(res => {
+        const reader = new FileReader();
+        reader.addEventListener(
+            'load',
+            () => {
+                // remove 'data:*/*;base64,' from the start of the string
+                // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
+                const fileAsBase64 = reader.result
+                    .toString()
+                    .split('base64,')[1];
+                res(fileAsBase64);
+            },
+            false
+        );
+        reader.addEventListener('error', () => {
+            showFileUploadError(
+                inputEl,
+                'Sorry there was a problem with your file: we accept images, pdfs and .doc or .docx files'
+            );
+        });
+        reader.readAsDataURL(file);
+    });
+
+const getValueFromInput = (element, data) => {
+    if (isCheckbox(element)) {
+        return (data[element.name] || '').concat(`\n${element.value}`);
+    } else if (isFile(element) && element.files[0]) {
+        return readFile(element.files[0], element);
+    }
+    return element.value;
+};
+
+export const formatData = (elements: any): Promise<any> =>
     [].reduce.call(
         elements,
-        (data, element) => {
+        async (promise, element) => {
+            const data = await promise;
             if (isNamed(element)) {
-                if (isCheckbox(element)) {
-                    data[element.name] = (data[element.name] || '').concat(
-                        `\n${element.value}`
-                    );
-                } else {
-                    data[element.name] = element.value;
-                }
+                const elementValue = await getValueFromInput(element, data);
+                data[element.name] = elementValue;
+                return data;
             }
             return data;
         },
-        {}
+        Promise.resolve({})
     );
 
-export const submitForm = (e: any) => {
+export const submitForm = async (e: any) => {
     e.preventDefault();
     const cForm = e.target;
-    const data = formatData(cForm.elements);
+    const data = await formatData(cForm.elements);
+
+    if (data['twitter-handle'].length > 0) {
+        showError(cForm, 'Sorry we think you are a robot.');
+        return false;
+    }
     showWaiting(cForm);
 
-    return fetch('/formstack-campaign/submit', {
+    return fetch('https://callouts.code.dev-guardianapis.com', {
         method: 'post',
         body: JSON.stringify(data),
         headers: {
@@ -78,7 +122,10 @@ export const submitForm = (e: any) => {
         if (res.ok) {
             showConfirmation(cForm);
         } else {
-            showError(cForm);
+            showError(
+                cForm,
+                'Sorry, there was a problem submitting your form. Please try again later.'
+            );
         }
     });
 };
