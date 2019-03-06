f0a57baeeee0a9e6e50803e1ffbda62f23845086
Simon Adcock <simon.adcock@guardian.co.uk>
2019-03-06 08:52:28 +0000

    Extend related video switch

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -481,7 +481,7 @@ trait FeatureSwitches {
     "When ON show YouTube related video suggestions in YouTube media atoms",
     owners = Seq(Owner.withGithub("siadcock")),
     safeState = Off,
-    sellByDate = new LocalDate(2019, 3, 7),
+    sellByDate = new LocalDate(2019, 3, 26),
     exposeClientSide = true
   )
 
