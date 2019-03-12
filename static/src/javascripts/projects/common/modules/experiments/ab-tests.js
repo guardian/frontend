c45586157e7b9d501227221002e4267d391bbd5a
Francesca Hammond <francesca.hammond@guardian.co.uk>
2019-03-12 15:29:52 +0000

    WIP

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -2,6 +2,7 @@
 import { commercialPrebidSafeframe } from 'common/modules/experiments/tests/commercial-prebid-safeframe.js';
 import { commercialAdVerification } from 'common/modules/experiments/tests/commercial-ad-verification.js';
 import { commercialCmpCustomise } from 'common/modules/experiments/tests/commercial-cmp-customise.js';
+import { commercialOutbrainTesting } from 'common/modules/experiments/tests/commercial-outbrain-testing.js';
 import { askFourEarning } from 'common/modules/experiments/tests/contributions-epic-ask-four-earning';
 import { acquisitionsEpicAlwaysAskIfTagged } from 'common/modules/experiments/tests/acquisitions-epic-always-ask-if-tagged';
 import { adblockTest } from 'common/modules/experiments/tests/adblock-ask';
@@ -14,6 +15,7 @@ export const concurrentTests: $ReadOnlyArray<ABTest> = [
     commercialPrebidSafeframe,
     commercialAdVerification,
     commercialCmpCustomise,
+    commercialOutbrainTesting,
     adblockTest,
 ];
 
