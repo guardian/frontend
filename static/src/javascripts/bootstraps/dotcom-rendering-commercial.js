2c528a654c01aab1bb2289520a0f2c259c13b7b2
Pascal Honoré <pascal@alseyn.net>
2019-03-13 14:54:08 +0000

    Side effect free, but working version of dotcom-rendering-commercial.js

--- static/src/javascripts/bootstraps/dotcom-rendering-commercial.js
+++ static/src/javascripts/bootstraps/dotcom-rendering-commercial.js
@@ -1,25 +1,15 @@
 // @flow
+/*
 import config from 'lib/config';
 import { catchErrorsWithContext } from 'lib/robust';
 import { markTime } from 'lib/user-timing';
 import reportError from 'lib/report-error';
-// import { init as initHighMerch } from 'commercial/modules/high-merch';
-// import { init as initArticleAsideAdverts } from 'commercial/modules/article-aside-adverts';
-// import { init as initArticleBodyAdverts } from 'commercial/modules/article-body-adverts';
-// import { closeDisabledSlots } from 'commercial/modules/close-disabled-slots';
-// import { adFreeSlotRemove } from 'commercial/modules/ad-free-slot-remove';
-import { init as initCmpService } from 'commercial/modules/cmp/cmp';
-// import { init as initLotameCmp } from 'commercial/modules/cmp/lotame-cmp';
-// import { init as initLotameDataExtract } from 'commercial/modules/lotame-data-extract';
-import { trackConsent as trackCmpConsent } from 'commercial/modules/cmp/consent-tracker';
-// import { init as prepareAdVerification } from 'commercial/modules/ad-verification/prepare-ad-verification';
-import { init as prepareGoogletag } from 'commercial/modules/dfp/prepare-googletag';
-// import { init as preparePrebid } from 'commercial/modules/dfp/prepare-prebid';
-// import { init as initLiveblogAdverts } from 'commercial/modules/liveblog-adverts';
-// import { init as initStickyTopBanner } from 'commercial/modules/sticky-top-banner';
-import { init as initThirdPartyTags } from 'commercial/modules/third-party-tags';
-// import { init as initPaidForBand } from 'commercial/modules/paidfor-band';
-// import { paidContainers } from 'commercial/modules/paid-containers';
+*/
+// import { init as initCmpService } from 'commercial/modules/cmp/cmp';
+// import { trackConsent as trackCmpConsent } from 'commercial/modules/cmp/consent-tracker';
+// import { init as prepareGoogletag } from 'commercial/modules/dfp/prepare-googletag';
+// import { init as initThirdPartyTags } from 'commercial/modules/third-party-tags';
+/*
 import {
     defer,
     wrap,
@@ -28,37 +18,16 @@ import {
     primaryBaseline,
 } from 'commercial/modules/dfp/performance-logging';
 import { trackPerformance } from 'common/modules/analytics/google';
-// import { commercialFeatures } from 'common/modules/commercial/commercial-features';
-// import { initCheckDispatcher } from 'commercial/modules/check-dispatcher';
-// import { initCommentAdverts } from 'commercial/modules/comment-adverts';
-
+*/
+/*
 const commercialModules: Array<Array<any>> = [
-    // ['cm-adFreeSlotRemove', adFreeSlotRemove],
-    // ['cm-closeDisabledSlots', closeDisabledSlots],
-    ['cm-prepare-cmp', initCmpService],
-    ['cm-track-cmp-consent', trackCmpConsent],
-    ['cm-thirdPartyTags', initThirdPartyTags],
-    // ['cm-prepare-prebid', preparePrebid, true],
-    ['cm-prepare-googletag', prepareGoogletag, true],
-    // ['cm-checkDispatcher', initCheckDispatcher],
-    // ['cm-lotame-cmp', initLotameCmp],
-    // ['cm-lotame-data-extract', initLotameDataExtract, true],
+    //['cm-prepare-cmp', initCmpService],
+    //['cm-track-cmp-consent', trackCmpConsent],
+    //['cm-thirdPartyTags', initThirdPartyTags],
+    //['cm-prepare-googletag', prepareGoogletag, true],
 ];
-
-// if (!commercialFeatures.adFree) {
-//     commercialModules.push(
-//         ['cm-prepare-adverification', prepareAdVerification, true],
-//         ['cm-highMerch', initHighMerch],
-//         ['cm-articleAsideAdverts', initArticleAsideAdverts, true],
-//         ['cm-articleBodyAdverts', initArticleBodyAdverts, true],
-//         ['cm-liveblogAdverts', initLiveblogAdverts, true],
-//         ['cm-stickyTopBanner', initStickyTopBanner],
-//         ['cm-paidContainers', paidContainers],
-//         ['cm-paidforBand', initPaidForBand],
-//         ['cm-commentAdverts', initCommentAdverts]
-//     );
-// }
-
+*/
+/*
 const loadHostedBundle = (): Promise<void> => {
     if (config.page.isHosted) {
         return new Promise(resolve => {
@@ -96,7 +65,6 @@ const loadHostedBundle = (): Promise<void> => {
     }
     return Promise.resolve();
 };
-
 const loadModules = (): Promise<void> => {
     addStartTimeBaseline(primaryBaseline);
 
@@ -129,8 +97,9 @@ const loadModules = (): Promise<void> => {
         }
     );
 };
-
-const bootCommercial = (): Promise<void> => {
+*/
+const bootCommercial = () => {
+    /*
     markTime('commercial start');
 
     catchErrorsWithContext([
@@ -175,11 +144,7 @@ const bootCommercial = (): Promise<void> => {
                 feature: 'commercial',
             });
         });
+    */
 };
 
-// make sure we've patched the env before running the app
-if (window.guardian.polyfilled) {
-    bootCommercial();
-} else {
-    window.guardian.onPolyfilled = bootCommercial;
-}
+bootCommercial();
