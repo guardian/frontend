7f940b975bf8ea6e22d8cc2601d95102d944ab30
Joseph Smith <joseph.smith@guardian.co.uk>
2019-03-27 17:39:16 +0000

    Add football exclusion rule back in

--- static/src/javascripts/projects/common/modules/commercial/epic/epic-exclusion-rules.js
+++ static/src/javascripts/projects/common/modules/commercial/epic/epic-exclusion-rules.js
@@ -10,6 +10,9 @@ type pagePropertiesRelevantToEpic = {|
 // less money than others, so we don't want to waste an epic impression on them.
 // Especially since that epic impression deprives us of Outbrain money.
 export const defaultExclusionRules: pagePropertiesRelevantToEpic[] = [
+    // e.g. https://www.theguardian.com/football/2019/mar/27/gordon-taylor-departure-pfa-chief-executive
+    { section: 'football' },
+
     // e.g. https://www.theguardian.com/money/2018/dec/13/slime-toys-tested-fail-meet-eu-safety-standards-hamleys-christmas
     { section: 'money' },
 
