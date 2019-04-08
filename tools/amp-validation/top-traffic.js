6fe4464e3921aed48eee22a920103160e78661ec
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-04-08 10:31:55 +0100

    Prettier fixes

--- tools/amp-validation/top-traffic.js
+++ tools/amp-validation/top-traffic.js
@@ -1,28 +1,28 @@
-const { fetch, getEndpointsFromResponse } = require('./endpoints/ophan');
-const run = require('./run');
+const { fetch, getEndpointsFromResponse } = require("./endpoints/ophan");
+const run = require("./run");
 
 const sections = [
-    'technology',
-    'lifeandstyle',
-    'money',
-    'travel',
-    'education',
-    'politics',
-    'science',
-    'media',
-    'music',
-    'football',
-    'sport',
-    'games',
-    'stage',
-    'artanddesign',
-    'film',
-    'books',
-    'business',
-    'society',
-    'environment',
+    "technology",
+    "lifeandstyle",
+    "money",
+    "travel",
+    "education",
+    "politics",
+    "science",
+    "media",
+    "music",
+    "football",
+    "sport",
+    "games",
+    "stage",
+    "artanddesign",
+    "film",
+    "books",
+    "business",
+    "society",
+    "environment"
 ];
-const keywordTags = ['info%2Fseries%2Fdigital-blog'];
+const keywordTags = ["info%2Fseries%2Fdigital-blog"];
 const filterOut = "ng-interactive|picture";
 const fetchPath = path => fetch(path).then(getEndpointsFromResponse);
 
@@ -31,7 +31,7 @@ Promise.all([
     ...keywordTags.map(keywordTag =>
         fetchPath(`/api/mostread/keywordtag/${keywordTag}?count=25`)
     ),
-    ...sections.map(section => fetchPath(`/api/mostread/${section}?count=25`)),
+    ...sections.map(section => fetchPath(`/api/mostread/${section}?count=25`))
 ])
     .then(urlArrays => [].concat(...urlArrays)) // Flatten the array of arrays of paths
     .then(urls => urls.filter(url => !url.match(filterOut))) // Filter out unsupported urls
@@ -39,7 +39,7 @@ Promise.all([
         run({
             checkIfAmp: true,
             logErrors: false,
-            checkIfDotComponents: true,
+            checkIfDotComponents: true
         })
     )
     .catch(err => console.log(err));
