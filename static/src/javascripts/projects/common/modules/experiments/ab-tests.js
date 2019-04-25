625d96ee9aea3ef07c776b7d3212621f1c21536c
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-04-25 15:12:52 +0100

    Added LowLatencyWrapper supplied by Confiant

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -1,6 +1,7 @@
 // @flow
 import { commercialPrebidSafeframe } from 'common/modules/experiments/tests/commercial-prebid-safeframe.js';
 import { commercialAdVerification } from 'common/modules/experiments/tests/commercial-ad-verification.js';
+import { commercialAdVerificationQA } from 'common/modules/experiments/tests/commercial-ad-verification-qa.js';
 import { commercialCmpCustomise } from 'common/modules/experiments/tests/commercial-cmp-customise.js';
 import { commercialOutbrainTesting } from 'common/modules/experiments/tests/commercial-outbrain-testing.js';
 import { commercialConsentGlobalNoScroll } from 'common/modules/experiments/tests/commercial-consent-global-no-scroll.js';
@@ -15,6 +16,7 @@ import {
 export const concurrentTests: $ReadOnlyArray<ABTest> = [
     commercialPrebidSafeframe,
     commercialAdVerification,
+    commercialAdVerificationQA,
     commercialCmpCustomise,
     commercialOutbrainTesting,
     commercialConsentGlobalNoScroll,
