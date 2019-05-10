18d03204d2cfff51ace70181fd65ea8e3547843c
George Haberis <george.haberis@guardian.co.uk>
2019-05-10 14:46:26 +0100

    validation fix

--- static/src/javascripts/projects/commercial/modules/dfp/on-slot-render.js
+++ static/src/javascripts/projects/commercial/modules/dfp/on-slot-render.js
@@ -82,7 +82,11 @@ export const onSlotRender = (event: SlotRenderEndedEvent): void => {
             console.log('*** set ad size ***', event);
             advert.size = event.size;
         } else {
-            console.log('*** ignore ad size ***', event, event.size !== advert.size);
+            console.log(
+                '*** ignore ad size ***',
+                event,
+                event.size !== advert.size
+            );
         }
 
         if (event.creativeId !== undefined) {
