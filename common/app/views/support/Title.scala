2d3398d7d195901e5bc8767194ccd7205d072f22
Rob Phillips <robert.phillips@guardian.co.uk>
2019-05-16 12:46:00 +0100

    Creating new method for capitalisation and special cases

--- common/app/views/support/Title.scala
+++ common/app/views/support/Title.scala
@@ -34,17 +34,31 @@ object Title {
   }
 
   private def titleFromSectionId(sectionId: String): String = sectionId.toLowerCase match {
-    case "Uk-news" => "UK news"
     case "theobserver" => "The Observer"
     case _ => sectionId
   }
 
-  private def getSectionConsideringWebtitle(webTitle: String, section: Option[String]): String =
+  private def guCapitalization(str: String): String = {
+    str match {
+      case "uk-news" => "UK news"
+      case "us-news" => "US news"
+      case _ => str.capitalize
+    }
+  }
+
+  private def getSectionConsideringWebtitle(webTitle: String, section: Option[String]): String = {
     section.filter(_.nonEmpty)
       .filterNot(_.toLowerCase == webTitle.toLowerCase)
       .filterNot(SectionsToIgnore.contains)
       .map(titleFromSectionId)
-      .fold("") { s => s" | ${s.capitalize}"}
+      .fold("") { s => s" | ${guCapitalization(s)}" }}
+
+//  private def getSectionConsideringWebtitle(webTitle: String, section: Option[String]): String =
+//    section.filter(_.nonEmpty)
+//      .filterNot(_.toLowerCase == webTitle.toLowerCase)
+//      .filterNot(SectionsToIgnore.contains)
+//      .map(titleFromSectionId)
+//      .fold("") { s => s" | ${s.capitalize}"}
 
   private def pagination(page: Page) = page.metadata.pagination.filterNot(_.isFirstPage).map{ pagination =>
     s" | Page ${pagination.currentPage} of ${pagination.lastPage}"
