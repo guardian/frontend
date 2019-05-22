e6ed5ddc66a34054971a21e8c5bd9ff6caa1c8f6
Tom Forbes <tom.forbes@theguardian.com>
2019-05-22 08:45:24 +0100

    Show ticker if amount is 0 (#21433)
    * Show ticker if f count is 0
    
    * correct isNaN
    
    * formatting

--- static/src/javascripts/projects/common/modules/commercial/ticker.js
+++ static/src/javascripts/projects/common/modules/commercial/ticker.js
@@ -124,7 +124,8 @@ const animate = (parentElementSelector: string, tickerType: TickerType) => {
     }
 };
 
-const dataSuccessfullyFetched = () => total && goal;
+const dataSuccessfullyFetched = () =>
+    !(Number.isNaN(Number(total)) || Number.isNaN(Number(goal)));
 
 const fetchDataAndAnimate = (
     parentElementSelector: string,
