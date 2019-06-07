50b1f18aaed25ade4ea0508f67d0e2470604de5d
Tom Forbes <tom.forbes@theguardian.com>
2019-06-07 16:31:20 +0100

    unit test optionalStringToNumber

---
+++ static/src/javascripts/lib/string-utils.spec.js
@@ -0,0 +1,16 @@
+// @flow
+import { optionalStringToNumber } from 'lib/string-utils';
+
+describe('optionalStringToNumber', () => {
+    it('should return a number', () => {
+        expect(optionalStringToNumber('3')).toBe(3);
+    });
+
+    it('should return undefined if undefined', () => {
+        expect(optionalStringToNumber(undefined)).toBe(undefined);
+    });
+
+    it('should return undefined if NaN', () => {
+        expect(optionalStringToNumber('')).toBe(undefined);
+    });
+});
