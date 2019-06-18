bfb9f219128e3b9770ce0b2342f73111a874b08f
George Haberis <george.haberis@guardian.co.uk>
2019-06-18 10:41:26 +0100

    extend switch for another month

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -481,7 +481,7 @@ trait FeatureSwitches {
     "When ON show YouTube related video suggestions in YouTube media atoms",
     owners = Seq(Owner.withGithub("siadcock")),
     safeState = Off,
-    sellByDate = new LocalDate(2019, 6, 17),
+    sellByDate = new LocalDate(2019, 7, 17),
     exposeClientSide = true
   )
 
