ab6dc062727f63a494cee9323ea33d7c1bbe3dda
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2019-06-24 12:08:32 +0100

    Tidy up

--- static/src/javascripts/lib/load-script.js
+++ static/src/javascripts/lib/load-script.js
@@ -1,5 +1,9 @@
 // @flow
-const loadScript = (src: string, props?: Object, text?: string): Promise<void> => {
+const loadScript = (
+    src: string,
+    props?: Object,
+    text?: string
+): Promise<void> => {
     if (document.querySelector(`script[src="${src}"]`)) {
         return Promise.resolve();
     }
@@ -24,13 +28,11 @@ const loadScript = (src: string, props?: Object, text?: string): Promise<void> =
     });
 };
 
-
 /*
 * <script>
 /* ----- Begin Step 1 ----- */
-//Load the APS JavaScript Library
-//!function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}
-//("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
-
+// Load the APS JavaScript Library
+//! function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}
+// ("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
 
 export { loadScript };
