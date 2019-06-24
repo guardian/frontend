c59b1a2832b3ea8f631000a77c608e5dce3c5d31
Tom Forbes <tom.forbes@theguardian.com>
2019-06-24 11:40:00 +0100

    allow country name in leadSentence

--- static/src/javascripts/projects/common/modules/commercial/contributions-utilities.js
+++ static/src/javascripts/projects/common/modules/commercial/contributions-utilities.js
@@ -763,7 +763,10 @@ export const getEngagementBannerTestsFromGoogleDoc = (): Promise<
                             test: () => {},
 
                             engagementBannerParams: {
-                                leadSentence: row.leadSentence,
+                                leadSentence: buildBannerCopy(
+                                    row.leadSentence.trim(),
+                                    hasCountryName
+                                ),
                                 messageText: buildBannerCopy(
                                     row.messageText.trim(),
                                     hasCountryName
