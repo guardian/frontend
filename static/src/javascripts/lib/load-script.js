56bb83f1b7e0d2b4ecbb006bc9f098f697814b88
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2019-06-24 13:52:58 +0100

    Refactored A9 lib & unit tests

--- static/src/javascripts/lib/load-script.js
+++ static/src/javascripts/lib/load-script.js
@@ -1,9 +1,5 @@
 // @flow
-const loadScript = (
-    src: string,
-    props?: Object,
-    text?: string
-): Promise<void> => {
+const loadScript = (src: string, props?: Object): Promise<void> => {
     if (document.querySelector(`script[src="${src}"]`)) {
         return Promise.resolve();
     }
@@ -19,20 +15,10 @@ const loadScript = (
         script.onerror = () => {
             reject(new Error(`Failed to load script ${src}`));
         };
-        if (text) {
-            script.text = text;
-        }
         if (ref.parentNode) {
             ref.parentNode.insertBefore(script, ref);
         }
     });
 };
-
-/*
-* <script>
-/* ----- Begin Step 1 ----- */
-// Load the APS JavaScript Library
-//! function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}
-// ("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
 
 export { loadScript };
