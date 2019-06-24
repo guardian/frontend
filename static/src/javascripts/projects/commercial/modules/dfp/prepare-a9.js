3f3c171b7c171c4e91a38d15be65f767c17f8287
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2019-06-24 12:08:32 +0100

    Tidy up

--- static/src/javascripts/projects/commercial/modules/dfp/prepare-a9.js
+++ static/src/javascripts/projects/commercial/modules/dfp/prepare-a9.js
@@ -6,11 +6,9 @@ import { buildPageTargeting } from 'common/modules/commercial/build-page-targeti
 import once from 'lodash/once';
 import a9 from 'commercial/modules/prebid/a9';
 import { dfpEnv } from 'commercial/modules/dfp/dfp-env';
-import utils from 'commercial/modules/prebid/utils';
 import { isInUsRegion } from 'commercial/modules/prebid/utils';
-import {loadScript} from "../../../../lib/load-script";
 
-//TODO: this function is already exported in prepare prebid worth refactoring it out
+// TODO: this function is already exported in prepare prebid worth refactoring it out
 const isGoogleProxy: () => boolean = () =>
     !!(
         navigator &&
@@ -21,24 +19,46 @@ const isGoogleProxy: () => boolean = () =>
 
 let moduleLoadResult = Promise.resolve();
 if (!isGoogleProxy()) {
+    /*eslint-disable */
     moduleLoadResult = new Promise(resolve => {
-        !function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}
-        ("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
+        !(function(a9, a, p, s, t, A, g) {
+            if (a[a9]) return;
+            function q(c, r) {
+                a[a9]._Q.push([c, r]);
+            }
+            a[a9] = {
+                init: function() {
+                    q('i', arguments);
+                },
+                fetchBids: function() {
+                    q('f', arguments);
+                },
+                setDisplayBids: function() {},
+                targetingKeys: function() {
+                    return [];
+                },
+                _Q: [],
+            };
+            A = p.createElement(s);
+            A.async = !0;
+            A.src = t;
+            g = p.getElementsByTagName(s)[0];
+            // $FlowFixMe
+            g.parentNode.insertBefore(A, g);
+        })(
+            'apstag',
+            window,
+            document,
+            'script',
+            '//c.amazon-adsystem.com/aax2/apstag.js'
+        );
         resolve();
     });
+    /* eslint-enable */
 }
 
-/*
-export const init = (start: () => void): Promise<void> => {
-    start();
-    !function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}
-    return loadScript(config.get('libs.apstag').catch(errorHandler);
-    return Promise.resolve();
-};
-*/
-
-
-const isSwitchOn = dfpEnv.externalDemand === 'a9' || dfpEnv.externalDemand === 'all';
+const isSwitchOn =
+    dfpEnv.externalDemand === 'a9' || dfpEnv.externalDemand === 'all';
 
 const setupA9: () => Promise<void> = () =>
     moduleLoadResult.then(() => {
@@ -51,7 +71,7 @@ const setupA9: () => Promise<void> = () =>
             !isGoogleProxy()
         ) {
             buildPageTargeting();
-            a9.initialise(window);
+            a9.initialise();
         }
         return Promise.resolve();
     });
