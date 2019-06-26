2a05656543c77cbdef7bb704768f86b2bcb80251
Mullefa <gccdawson@gmail.com>
2019-06-26 11:13:17 +0100

    remove banner cta test

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -68,16 +68,6 @@ trait ABTestSwitches {
 
   Switch(
     ABTests,
-    "ab-acquisitions-banner-sign-in-cta",
-    "test whether having a sign-in CTA negatively impacts acquisitions",
-    owners = Owner.group(SwitchGroup.Identity),
-    safeState = On,
-    sellByDate = new LocalDate(2019, 6, 25),
-    exposeClientSide = true
-  )
-
-  Switch(
-    ABTests,
     "ab-commercial-prebid-safeframe",
     "Test the impact of serving prebid ads in safeframes",
     owners = Seq(Owner.withGithub("jeteve")),
