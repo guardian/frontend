3d72b5cb02dcb20da8d6259ccf39ba3abfbd6e71
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2019-06-27 13:34:51 +0100

    More tests

--- static/src/javascripts/projects/commercial/modules/prebid/a9.spec.js
+++ static/src/javascripts/projects/commercial/modules/prebid/a9.spec.js
@@ -2,6 +2,9 @@
 
 import a9 from 'commercial/modules/prebid/a9';
 import _a9lib from 'lib/apstag';
+import { getAdvertById as getAdvertById_ } from 'commercial/modules/dfp/get-advert-by-id';
+
+const getAdvertById: any = getAdvertById_;
 
 jest.mock('lib/raven');
 jest.mock('lib/apstag', () => ({
@@ -16,6 +19,10 @@ jest.mock('commercial/modules/dfp/Advert', () =>
     jest.fn().mockImplementation(() => ({ advert: jest.fn() }))
 );
 
+jest.mock('commercial/modules/dfp/get-advert-by-id', () => ({
+    getAdvertById: jest.fn(),
+}));
+
 beforeEach(async () => {
     jest.resetModules();
     window.apstag = a9lib;
@@ -28,10 +35,15 @@ describe('initialise', () => {
         expect(a9lib.init).toHaveBeenCalled();
     });
 
-    test('should request bids', () => {
+    test('should request bids', done => {
+        const dummyAdvert = {
+            size: [200, 200],
+            hasPrebidSize: false,
+        };
+        getAdvertById.mockImplementation(() => dummyAdvert);
         a9.initialise();
-        a9.requestBids().then(() => {
-            expect(a9lib.fetchBids).toHaveBeenCalledWith();
+        a9.requestBids(dummyAdvert, () => {}).then(() => {
+            expect(a9lib.fetchBids).toHaveBeenCalled();
             done();
         });
     });
