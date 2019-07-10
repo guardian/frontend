cfe782ee8c0d19da0e57614f660c7c664bd51631
Nicolas Long <nicolas.long@theguardian.com>
2019-07-10 14:32:21 +0100

    Revert "Revert "Migrate to version 3 dotcom-rendering model""
    This reverts commit d81e7c1a5a2a378164a8faf10dd42ac01c415e4f.

--- article/app/model/dotcomponents/DotcomponentsDataModel.scala
+++ article/app/model/dotcomponents/DotcomponentsDataModel.scala
@@ -8,19 +8,18 @@ import common.commercial.{CommercialProperties, EditionCommercialProperties, Pre
 import conf.Configuration.affiliatelinks
 import conf.switches.Switches
 import conf.{Configuration, Static}
+import controllers.ArticlePage
 import model.content.Atom
 import model.dotcomrendering.pageElements.{DisclaimerBlockElement, PageElement}
-import model.{Article, Canonical, LiveBlogPage, PageWithStoryPackage, SubMetaLinks}
-import navigation.NavMenu
+import model.{Canonical, LiveBlogPage, PageWithStoryPackage}
 import navigation.ReaderRevenueSite.{Support, SupportContribute, SupportSubscribe}
 import navigation.UrlHelpers._
+import navigation.{FlatSubnav, NavLink, NavMenu, ParentSubnav, Subnav}
+import org.joda.time.DateTime
 import play.api.libs.json._
 import play.api.mvc.RequestHeader
 import views.html.fragments.affiliateLinksDisclaimer
 import views.support.{AffiliateLinksCleaner, CamelCase, GUDateTimeFormat, ImgSrc, Item300}
-import ai.x.play.json.implicits.optionWithNull
-import controllers.ArticlePage
-import org.joda.time.{DateTime, DateTimeZone}
 
 
 // We have introduced our own set of objects for serializing data to the DotComponents API,
@@ -29,16 +28,12 @@ import org.joda.time.{DateTime, DateTimeZone}
 // only one reason for change.
 // exceptions: we do resuse the existing Nav & BlockElement classes right now
 
-case class TagProperties(
-    id: String,
-    tagType: String,
-    webTitle: String,
-    twitterHandle: Option[String],
-    bylineImageUrl: Option[String]
-)
-
 case class Tag(
-    properties: TagProperties
+  id: String,
+  `type`: String,
+  title: String,
+  twitterHandle: Option[String],
+  bylineImageUrl: Option[String]
 )
 
 case class Block(
@@ -52,13 +47,6 @@ case class Block(
     title: Option[String],
 )
 
-case class Blocks(
-    main: Option[Block],
-    body: List[Block],
-    keyEvents: List[Block],
-)
-
-// For liveblogs
 case class Pagination(
   currentPage: Int,
   totalPages: Int,
@@ -82,85 +70,10 @@ case class ReaderRevenueLinks(
   ampFooter: ReaderRevenueLink
 )
 
-case class Meta(
-  isImmersive: Boolean,
-  isHosted: Boolean,
-  shouldHideAds: Boolean,
-  hasStoryPackage: Boolean,
-  hasRelated: Boolean,
-  isCommentable: Boolean,
-  linkedData: List[LinkedData]
-)
-
-case class Tags(
-  authorIds: Option[String],
-  toneIds: Option[String],
-  keywordIds: Option[String],
-  commissioningDesks: Option[String],
-  all: List[Tag]
-)
-
-case class Content(
-  headline: String,
-  standfirst: Option[String],
-  main: String,
-  blocks: Blocks,
-  byline: String,
-  trailText: String
-)
-
 case class Commercial(
   editionCommercialProperties: Map[String, EditionCommercialProperties],
   prebidIndexSites: List[PrebidIndexSite],
   commercialProperties: Option[CommercialProperties], //DEPRECATED TO DELETE
-)
-
-// top-level structures
-
-case class DCSite(
-  ajaxUrl: String,
-  guardianBaseURL: String,
-  sentryHost: Option[String],
-  sentryPublicApiKey: Option[String],
-  switches: Map[String,Boolean],
-  beaconUrl: String,
-  subscribeWithGoogleApiUrl: String,
-  nav: NavMenu,
-  readerRevenueLinks: ReaderRevenueLinks,
-  commercialUrl: String
-)
-
-case class DCPage(
-  content: Content,
-  tags: Tags,
-  pagination: Option[Pagination],
-  author: String,
-  pageId: String,
-  pillar: Option[String],
-  webPublicationDate: Long,
-  webPublicationDateDisplay: String,
-  section: Option[String],
-  sectionLabel: String,
-  sectionUrl: String,
-  webTitle: String,
-  contentId: Option[String],
-  seriesId: Option[String],
-  editionId: String,
-  edition: String,
-  contentType: Option[String],
-  subMetaLinks: SubMetaLinks,
-  webURL: String,
-  starRating: Option[Int],
-  commercial: Commercial,
-  meta: Meta
-)
-
-// the composite data model
-
-case class DotcomponentsDataModel(
-  page: DCPage,
-  site: DCSite,
-  version: Int
 )
 
 object Block {
@@ -172,28 +85,8 @@ object Commercial {
   implicit val writes = Json.writes[Commercial]
 }
 
-object Blocks {
-  implicit val writes = Json.writes[Blocks]
-}
-
-object TagProperties {
-  implicit val writes = Json.writes[TagProperties]
-}
-
-object Content {
-  implicit val writes = Json.writes[Content]
-}
-
 object Tag {
   implicit val writes = Json.writes[Tag]
-}
-
-object Tags {
-  implicit val writes = Json.writes[Tags]
-}
-
-object Meta {
-  implicit val writes = Json.writes[Meta]
 }
 
 object ReaderRevenueLink {
@@ -208,19 +101,168 @@ object Pagination {
   implicit val writes = Json.writes[Pagination]
 }
 
-object DCPage {
-  implicit val writes = Json.writes[DCPage]
+case class Config(
+  ajaxUrl: String,
+  sentryPublicApiKey: String,
+  sentryHost: String,
+  switches: Map[String, Boolean],
+  dfpAccountId: String,
+  commercialUrl: String,
+)
+
+object Config {
+  implicit val writes = Json.writes[Config]
 }
 
-object DCSite {
-  implicit val writes = Json.writes[DCSite]
+case class SubMetaLink(
+  url: String,
+  title: String,
+)
+
+object SubMetaLink {
+  implicit val format = Json.format[SubMetaLink]
+
+  def apply(sml: model.SubMetaLink): SubMetaLink = {
+    SubMetaLink(
+      url = sml.link,
+      title = sml.text,
+    )
+  }
+}
+
+case class Author(
+  byline: String,
+  twitterHandle: Option[String],
+)
+
+object Author {
+  implicit val writes = Json.writes[Author]
+}
+
+case class Nav(
+  currentUrl: String,
+  pillars: Seq[NavLink],
+  otherLinks: Seq[NavLink],
+  brandExtensions: Seq[NavLink],
+  currentNavLink: Option[NavLink],
+  currentParent: Option[NavLink],
+  currentPillar: Option[NavLink],
+  subNavSections: Option[Subnav],
+  readerRevenueLinks: ReaderRevenueLinks,
+)
+
+object Nav {
+  implicit val navlinkWrites = Json.writes[NavLink]
+  implicit val flatSubnavWrites = Json.writes[FlatSubnav]
+  implicit val parentSubnavWrites = Json.writes[ParentSubnav]
+  implicit val subnavWrites = Writes[Subnav]{
+    case nav: FlatSubnav => flatSubnavWrites.writes(nav)
+    case nav: ParentSubnav => parentSubnavWrites.writes(nav)
+  }
+  implicit val writes = Json.writes[Nav]
+}
+
+case class DataModelV3(
+  version: Int,
+  headline: String,
+  standfirst: String,
+  webTitle: String,
+  mainMediaElements: List[PageElement],
+  main: String,
+  keyEvents: List[Block],
+  blocks: List[Block],
+  pagination: Option[Pagination],
+  author: Author,
+  webPublicationDate: String,
+  webPublicationDateDisplay: String, // TODO remove
+  editionLongForm: String,
+  editionId: String,
+  pageId: String,
+  tags: List[Tag],
+  pillar: String,
+  isImmersive: Boolean,
+  sectionLabel: String,
+  sectionUrl: String,
+  sectionName: Option[String],
+  subMetaSectionLinks: List[SubMetaLink],
+  subMetaKeywordLinks: List[SubMetaLink],
+  shouldHideAds: Boolean,
+  webURL: String,
+  linkedData: List[LinkedData],
+  config: Config,
+  guardianBaseURL: String,
+  contentType: String,
+  hasRelated: Boolean,
+  hasStoryPackage: Boolean,
+  beaconURL: String,
+  isCommentable: Boolean,
+  commercialProperties: Map[String, EditionCommercialProperties],
+  starRating: Option[Int],
+  trailText: String,
+  nav: Nav,
+)
+
+object DataModelV3 {
+  implicit val pageElementWrites: Writes[PageElement] = Json.writes[PageElement]
+
+  implicit val writes = new Writes[DataModelV3] {
+    def writes(model: DataModelV3) = Json.obj(
+      "version" -> model.version,
+      "headline" -> model.headline,
+      "standfirst" -> model.standfirst,
+      "webTitle" -> model.webTitle,
+      "mainMediaElements" -> Json.toJson(model.mainMediaElements),
+      "main" -> model.main,
+      "keyEvents" -> model.keyEvents,
+      "blocks" -> model.blocks,
+      "pagination" -> model.pagination,
+      "author" -> model.author,
+      "webPublicationDate" -> model.webPublicationDate,
+      "webPublicationDateDisplay" -> model.webPublicationDateDisplay,
+      "editionLongForm" -> model.editionLongForm,
+      "editionId" -> model.editionId,
+      "pageId" -> model.pageId,
+      "tags" -> model.tags,
+      "pillar" -> model.pillar,
+      "isImmersive" -> model.isImmersive,
+      "sectionLabel" -> model.sectionLabel,
+      "sectionUrl" -> model.sectionUrl,
+      "sectionName" -> model.sectionName,
+      "subMetaSectionLinks" -> model.subMetaSectionLinks,
+      "subMetaKeywordLinks" -> model.subMetaKeywordLinks,
+      "shouldHideAds" -> model.shouldHideAds,
+      "webURL" -> model.webURL,
+      "linkedData" -> model.linkedData,
+      "config" -> model.config,
+      "guardianBaseURL" -> model.guardianBaseURL,
+      "contentType" -> model.contentType,
+      "hasRelated" -> model.hasRelated,
+      "hasStoryPackage" -> model.hasStoryPackage,
+      "beaconURL" -> model.beaconURL,
+      "isCommentable" -> model.isCommentable,
+      "commercialProperties" -> model.commercialProperties,
+      "starRating" -> model.starRating,
+      "trailText" -> model.trailText,
+      "nav" -> model.nav,
+    )
+  }
+
+  def toJson(model: DataModelV3): String = {
+    def withoutNull(json: JsValue): JsValue = json match {
+      case JsObject(fields) => JsObject(fields.filterNot{ case (_, value) => value == JsNull })
+      case other => other
+    }
+
+    val jsValue = Json.toJson(model)
+    Json.stringify(withoutNull(jsValue))
+  }
 }
 
 object DotcomponentsDataModel {
 
   val VERSION = 2
 
-  def fromArticle(articlePage: PageWithStoryPackage, request: RequestHeader, blocks: APIBlocks): DotcomponentsDataModel = {
+  def fromArticle(articlePage: PageWithStoryPackage, request: RequestHeader, blocks: APIBlocks): DataModelV3 = {
 
     val article = articlePage.article
     val atoms: Iterable[Atom] = article.content.atoms.map(_.all).getOrElse(Seq())
@@ -346,7 +388,7 @@ object DotcomponentsDataModel {
         .map(block => toBlock(block, shouldAddAffiliateLinks, Edition(request)))
     }
 
-    val dcBlocks = Blocks(mainBlock, bodyBlocks, keyEvents.toList)
+    //val dcBlocks = Blocks(mainBlock, bodyBlocks, keyEvents.toList)
 
     val jsConfig = (k: String) => articlePage.getJavascriptConfig.get(k).map(_.as[String])
 
@@ -378,17 +420,13 @@ object DotcomponentsDataModel {
 
     val allTags = article.tags.tags.map(
       t => Tag(
-        TagProperties(
-          t.id,
-          t.properties.tagType,
-          t.properties.webTitle,
-          t.properties.twitterHandle,
-          t.properties.contributorLargeImagePath.map(src => ImgSrc(src, Item300))
-        )
+        t.id,
+        t.properties.tagType,
+        t.properties.webTitle,
+        t.properties.twitterHandle,
+        t.properties.contributorLargeImagePath.map(src => ImgSrc(src, Item300))
       )
     )
-
-    val navMenu = NavMenu(articlePage, Edition(request))
 
     val headerReaderRevenueLink: ReaderRevenueLink = ReaderRevenueLink(
       getReaderRevenueUrl(SupportContribute, Header)(request),
@@ -428,26 +466,20 @@ object DotcomponentsDataModel {
       ampFooterReaderRevenueLink
     )
 
-    val site = DCSite(
-      Configuration.ajax.url,
-      Configuration.site.host,
-      jsPageData.get("sentryHost"),
-      jsPageData.get("sentryPublicApiKey"),
-      switches,
-      Configuration.debug.beaconUrl,
-      Configuration.google.subscribeWithGoogleApiUrl,
-      navMenu,
-      readerRevenueLinks,
-      buildFullCommercialUrl("javascripts/graun.dotcom-rendering-commercial.js")
-    )
-
-    val tags = Tags(
-      jsConfig("authorIds"),
-      jsConfig("keywordIds"),
-      jsConfig("toneIds"),
-      jsConfig("commissioningDesks"),
-      allTags
-    )
+    val nav = {
+      val navMenu = NavMenu(articlePage, Edition(request))
+      Nav(
+        currentUrl = navMenu.currentUrl,
+        pillars = navMenu.pillars,
+        otherLinks = navMenu.otherLinks,
+        brandExtensions = navMenu.brandExtensions,
+        currentNavLink = navMenu.currentNavLink,
+        currentParent = navMenu.currentParent,
+        currentPillar = navMenu.currentPillar,
+        subNavSections = navMenu.subNavSections,
+        readerRevenueLinks = readerRevenueLinks,
+      )
+    }
 
     val commercial = Commercial(
       editionCommercialProperties =
@@ -460,72 +492,63 @@ object DotcomponentsDataModel {
       article.metadata.commercial,
     )
 
-    val author = article.tags.contributors.map(_.name) match {
+    val byline = article.tags.contributors.map(_.name) match {
       case Nil => "Guardian staff reporter"
       case contributors => contributors.mkString(",")
     }
 
-    val content = DCPage(
-      Content(
-        article.trail.headline,
-        article.fields.standfirst,
-        article.fields.main,
-        dcBlocks,
-        article.trail.byline.getOrElse(""),
-        trailText = article.trail.fields.trailText.getOrElse("")
-      ),
-      tags,
+    val config = Config(
+      ajaxUrl = Configuration.ajax.url,
+      sentryPublicApiKey = jsPageData.get("sentryPublicApiKey").getOrElse(""),
+      sentryHost = jsPageData.get("sentryHost").getOrElse(""),
+      switches = switches,
+      dfpAccountId = "", // TODO
+      commercialUrl = buildFullCommercialUrl("javascripts/graun.dotcom-rendering-commercial.js"),
+    )
+
+    val author = Author(
+      byline = byline,
+      twitterHandle = article.tags.contributors.headOption.flatMap(_.properties.twitterHandle)
+    )
+
+    DataModelV3(
+      version = 3,
+      headline = article.trail.headline,
+      standfirst = article.fields.standfirst.getOrElse(""),
+      webTitle = article.metadata.webTitle,
+      mainMediaElements = mainBlock.toList.flatMap(_.elements),
+      main = article.fields.main,
+      keyEvents = keyEvents.toList,
+      blocks = bodyBlocks,
       pagination = pagination,
-      author,
-      article.metadata.id,
-      article.metadata.pillar.map(_.toString),
-      article.trail.webPublicationDate.getMillis,
-      GUDateTimeFormat.formatDateTimeForDisplay(article.trail.webPublicationDate, request),
-      article.metadata.section.map(_.value),
-      article.content.sectionLabelName,
-      article.content.sectionLabelLink,
-      article.metadata.webTitle,
-      jsConfig("contentId"),   // source: content.scala
-      jsConfig("seriesId"),    // source: content.scala
-      Edition(request).id,
-      Edition(request).displayName,
-      jsConfig("contentType"),
-      article.content.submetaLinks,
-      article.metadata.webUrl,
-      article.content.starRating,
-      commercial,
-      meta = Meta (
-        article.isImmersive,
-        article.metadata.isHosted,
-        article.content.shouldHideAdverts,
-        articlePage.related.hasStoryPackage,
-        article.content.showInRelated,
-        article.trail.isCommentable,
-        linkedData
-      ),
+      author = author,
+      webPublicationDate = article.trail.webPublicationDate.toString, // TODO check format
+      webPublicationDateDisplay = GUDateTimeFormat.formatDateTimeForDisplay(article.trail.webPublicationDate, request),
+      editionLongForm = Edition(request).displayName, // TODO check
+      editionId = Edition(request).id,
+      pageId = article.metadata.id,
+      tags = allTags,
+      pillar = article.metadata.pillar.map(_.toString.toLowerCase).getOrElse("news"),
+      isImmersive = article.isImmersive,
+      sectionLabel = article.content.sectionLabelName,
+      sectionUrl = article.content.sectionLabelLink,
+      sectionName = article.metadata.section.map(_.value),
+      subMetaSectionLinks = article.content.submetaLinks.sectionLabels.map(SubMetaLink.apply),
+      subMetaKeywordLinks = article.content.submetaLinks.keywords.map(SubMetaLink.apply),
+      shouldHideAds = article.content.shouldHideAdverts,
+      webURL = article.metadata.webUrl,
+      linkedData = linkedData,
+      config = config,
+      guardianBaseURL = Configuration.amp.baseUrl,
+      contentType = jsConfig("contentType").getOrElse(""),
+      hasRelated = article.content.showInRelated,
+      hasStoryPackage = articlePage.related.hasStoryPackage,
+      beaconURL = Configuration.debug.beaconUrl,
+      isCommentable = article.trail.isCommentable,
+      commercialProperties = commercial.editionCommercialProperties,
+      starRating = article.content.starRating,
+      trailText = article.trail.fields.trailText.getOrElse(""),
+      nav = nav,
     )
-
-    DotcomponentsDataModel(
-      content,
-      site,
-      VERSION
-    )
-
-  }
-
-  def toJson(model: DotcomponentsDataModel): JsValue = {
-    // make what we have look a bit closer to what dotcomponents currently expects
-    implicit val DotComponentsDataModelWrites = new Writes[DotcomponentsDataModel] {
-      def writes(model: DotcomponentsDataModel) = Json.obj(
-        "page" -> model.page,
-        "site" -> model.site,
-        "version" -> model.version
-      )
-    }
-    Json.toJson(model)
-  }
-
-  def toJsonString(model: DotcomponentsDataModel): String = {
-    Json.stringify(toJson(model))
   }
 }
