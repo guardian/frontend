8b9ec18488921d040932b2adfef391c8fddbfac8
George Haberis <george.haberis@guardian.co.uk>
2019-07-24 12:20:40 +0100

    add initial implementation of cmp lib

---
+++ static/src/javascripts/projects/commercial/modules/cmp/lib/cmp.js
@@ -0,0 +1,53 @@
+// @flow
+
+type PurposeEvent = 'functional' | 'performance' | 'advertisement';
+
+type PurposeCallback = (state: boolean | null) => void;
+
+type Purpose = {
+    state: boolean | null,
+    callbacks: Array<PurposeCallback>,
+};
+
+let cmpIsReady = false;
+
+const purposes: { [PurposeEvent]: Purpose } = {
+    functional: {
+        state: null,
+        callbacks: [],
+    },
+    performance: {
+        state: null,
+        callbacks: [],
+    },
+    advertisement: {
+        state: null,
+        callbacks: [],
+    },
+};
+
+export const onConsentNotification = (
+    purposeName: PurposeEvent,
+    callback: PurposeCallback
+) => {
+    const purpose = purposes[purposeName];
+
+    if (cmpIsReady) {
+        callback(purpose.state);
+    }
+
+    purpose.callbacks.push(callback);
+};
+
+export const init = () => {
+    purposes.functional.state = true;
+    purposes.performance.state = true;
+    purposes.advertisement.state = true;
+
+    cmpIsReady = true;
+
+    Object.keys(purposes).forEach(key => {
+        const purpose = purposes[key];
+        purpose.callbacks.forEach(callback => callback(purpose.state));
+    });
+};
