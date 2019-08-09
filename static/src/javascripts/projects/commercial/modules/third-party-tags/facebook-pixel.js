7336440d57d0fdd731bde0c35dc8d251665fe943
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-08-09 16:38:42 +0100

    Rework facebook-pixel.js

--- static/src/javascripts/projects/commercial/modules/third-party-tags/facebook-pixel.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/facebook-pixel.js
@@ -1,19 +1,10 @@
 // @flow
 import config from 'lib/config';
-import {
-    getAdConsentState,
-    thirdPartyTrackingAdConsent,
-} from 'common/modules/commercial/ad-prefs.lib';
 
-export const fbPixel: () => ThirdPartyTag = () => {
-    const consent = getAdConsentState(thirdPartyTrackingAdConsent);
-    return {
-        shouldRun:
-            config.get('switches.facebookTrackingPixel') &&
-            (consent == null || consent),
-        url: `https://www.facebook.com/tr?id=${config.get(
-            'libs.facebookAccountId'
-        )}&ev=PageView&noscript=1`,
-        useImage: true,
-    };
-};
+export const fbPixel: () => ThirdPartyTag = () => ({
+    shouldRun: config.get('switches.facebookTrackingPixel'),
+    url: `https://www.facebook.com/tr?id=${config.get(
+        'libs.facebookAccountId'
+    )}&ev=PageView&noscript=1`,
+    useImage: true,
+});
