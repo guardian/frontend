9b4774fc19042c5740c25002b01c5fb0e9cee108
Pascal Honoré <pascal@alseyn.net>
2019-08-14 13:14:43 +0100

    Remove unused check: isUserInEmailAbTest

--- static/src/javascripts/projects/common/modules/check-mediator-checks.js
+++ static/src/javascripts/projects/common/modules/check-mediator-checks.js
@@ -4,7 +4,6 @@ export const checks = [
     'isOutbrainDisabled',
     'isUserInContributionsAbTest',
     'isUserNotInContributionsAbTest',
-    'isUserInEmailAbTest',
     'emailCanRunPreCheck',
     'listCanRun',
     'emailInArticleOutbrainEnabled',
