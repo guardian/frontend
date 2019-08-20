77652740c8047838c0b15ac8a252334660726889
George Haberis <george.haberis@guardian.co.uk>
2019-08-20 15:49:54 +0100

    remove cmp cotainer on post message

--- static/src/javascripts/projects/common/modules/ui/cmp-ui.js
+++ static/src/javascripts/projects/common/modules/ui/cmp-ui.js
@@ -1,9 +1,21 @@
 // @flow
-const CMP_URL = 'https://manage.thegulocal.com/consent';
+
+// TODO: this should be derived from config
+const CMP_DOMAIN = 'https://manage.theguardian.com';
+const CMP_URL = `${CMP_DOMAIN}/consent`;
+
+let container: ?HTMLElement;
+
+const receiveMessage = (event: MessageEvent) => {
+    if (event.origin === CMP_DOMAIN && container && container.parentNode) {
+        container.remove();
+    }
+};
 
 export const init = (): void => {
-    const container = document.createElement('div');
+    container = document.createElement('div');
     container.className = 'cmp-overlay';
+
     const iframe = document.createElement('iframe');
     iframe.src = CMP_URL;
     iframe.className = 'cmp-iframe';
@@ -13,4 +25,6 @@ export const init = (): void => {
     if (document.body) {
         document.body.appendChild(container);
     }
+
+    window.addEventListener('message', receiveMessage, false);
 };
