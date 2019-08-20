3d020cbeaf770f49ed5f2bbf7c40039b261e6ad8
George Haberis <george.haberis@guardian.co.uk>
2019-08-20 14:21:24 +0100

    add overlay

--- static/src/javascripts/projects/common/modules/ui/cmp-ui.js
+++ static/src/javascripts/projects/common/modules/ui/cmp-ui.js
@@ -2,11 +2,15 @@
 const CMP_URL = 'https://manage.thegulocal.com/consent';
 
 export const init = (): void => {
+    const container = document.createElement('div');
+    container.className = 'cmp-overlay';
     const iframe = document.createElement('iframe');
     iframe.src = CMP_URL;
     iframe.className = 'cmp-iframe';
 
+    container.appendChild(iframe);
+
     if (document.body) {
-        document.body.appendChild(iframe);
+        document.body.appendChild(container);
     }
 };
