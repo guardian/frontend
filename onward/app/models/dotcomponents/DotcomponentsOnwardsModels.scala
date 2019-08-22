4d4618ad74d3b05952a027725607d6ccac16c432
Philip McMahon <philip.mcmahon@guardian.co.uk>
2019-08-22 14:30:10 +0100

    Add endpoint for dotcom-rendering to fetch rich link data.

---
+++ onward/app/models/dotcomponents/DotcomponentsOnwardsModels.scala
@@ -0,0 +1,19 @@
+package models.dotcomponents
+
+import model.{DotcomContentType, Tag, Tags}
+import play.api.libs.json.Json
+
+case class RichLink(
+  tags: List[Tag],
+  cardStyle: String,
+  thumbnailUrl: Option[String],
+  headline: String,
+  contentType: Option[DotcomContentType],
+  starRating: Option[Int],
+  sponsorName: Option[String],
+  contributorImage: Option[String],
+  url: String)
+
+object RichLink {
+  implicit val writes = Json.writes[RichLink]
+}
