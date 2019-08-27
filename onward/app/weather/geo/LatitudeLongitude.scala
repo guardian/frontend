2cec9763c82745fa0223ab1a12fd1ae80b45238b
Philip McMahon <philip.mcmahon@guardian.co.uk>
2019-08-27 15:54:43 +0100

    In onward weather location controller. use fastly providedd lat/long/

--- onward/app/weather/geo/LatitudeLongitude.scala
+++ onward/app/weather/geo/LatitudeLongitude.scala
@@ -3,3 +3,12 @@ package weather.geo
 case class LatitudeLongitude(latitude: Double, longitude: Double) {
   override def toString: String = s"$latitude,$longitude"
 }
+
+object LatitudeLongitude {
+  def fromStringAnonymised(latLong: String): Option[LatitudeLongitude] = {
+    val coords = latLong.split(",").map(value => Math.round(value.toDouble * 100)/100.0)
+    if (coords.length == 2) {
+      Some(LatitudeLongitude(coords(0), coords(1)))
+    } else None
+  }
+}
