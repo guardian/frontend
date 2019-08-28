f6d5fc5342d4ac64b743a48a81ff118af3d964e3
vanessa <32312712+vlbee@users.noreply.github.com>
2019-08-28 17:43:14 +0100

    Add ContentAccess Class to MembersDataApiService

--- identity/app/services/MembersDataApiService.scala
+++ identity/app/services/MembersDataApiService.scala
@@ -1,16 +1,40 @@
 package services
 
 import common.{HttpStatusException, Logging}
-import play.api.libs.ws.{DefaultWSCookie, WSClient, WSCookie, WSResponse}
+import play.api.libs.ws.{DefaultWSCookie, WSClient, WSCookie}
 import play.api.mvc.{Cookie, Cookies}
-
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 import scala.concurrent.{ExecutionContext, Future}
 
-// TODO model member data
-case class MemberData()
+// Modeled on members-data-api Attributes - ConsentAccess
+// https://github.com/guardian/members-data-api/blob/master/membership-attribute-service/app/models/Attributes.scala
+case class ContentAccess(
+  isMember: Boolean, // TODO a friend membership should not prevent auto-account deletion but currently does
+  isPaidMember: Boolean,
+  isRecurringContributor: Boolean,
+  hasDigitalPack: Boolean,
+  isPaperSubscriber: Boolean,
+  isGuardianWeeklySubscriber: Boolean
+) {
+  def canProceedWithAutoDeletion: Boolean = !(isMember || isPaidMember || isRecurringContributor || hasDigitalPack || isPaperSubscriber || isGuardianWeeklySubscriber)
+  def hasPaidProducts: Boolean = isPaidMember || isRecurringContributor || hasDigitalPack || isPaperSubscriber || isGuardianWeeklySubscriber
+}
 
-class MembersDataApiService(wsClient: WSClient, config: conf.IdentityConfiguration)(implicit executionContext: ExecutionContext) extends Logging with implicits.WSRequests {
+object ContentAccess {
+  implicit val jsRead: Reads[ContentAccess] = (
+    (JsPath \ "member").read[Boolean] and
+      (JsPath \ "paidMember").read[Boolean] and
+      (JsPath \ "recurringContributor").read[Boolean] and
+      (JsPath \ "digitalPack").read[Boolean] and
+      (JsPath \ "paperSubscriber").read[Boolean] and
+      (JsPath \ "guardianWeeklySubscriber").read[Boolean]
+    ) (ContentAccess.apply _)
+}
 
+class MembersDataApiService(wsClient: WSClient, config: conf.IdentityConfiguration)(implicit executionContext: ExecutionContext) extends Logging {
+
+  // TODO is there an alternative to converting to WSCookie?
   private def toWSCookie(c: Cookie): WSCookie = {
     DefaultWSCookie(
       name = c.name,
@@ -23,19 +47,15 @@ class MembersDataApiService(wsClient: WSClient, config: conf.IdentityConfigurati
     )
   }
 
-  def getMembersData(cookies: Cookies): Future[WSResponse] = {
+  def getUserContentAccess(cookies: Cookies): Future[ContentAccess] = {
     val root = config.membersDataApiUrl
     val path = "/user-attributes/me"
     wsClient.url(s"$root$path").withCookies(cookies.map(c => toWSCookie(c)).toSeq: _*).get() flatMap { response =>
       response.status match {
         case 200 =>
-          println("200")
-          println(response.body)
-          Future.successful(response)
-        case _ =>
-          println(response.status)
-          println(response.statusText)
-          Future.failed(HttpStatusException(response.status, response.statusText))
+          val contentAccess = response.json \ "contentAccess"
+          Future.successful(contentAccess.as[ContentAccess])
+        case _ => Future.failed(HttpStatusException(response.status, response.statusText))
       }
     }
   }
