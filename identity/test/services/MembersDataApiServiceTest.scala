71cb10113a6b16152bc7b7c562540ef951e134c8
vanessa <32312712+vlbee@users.noreply.github.com>
2019-09-06 15:42:07 +0100

    Make tests DRY

--- identity/test/services/MembersDataApiServiceTest.scala
+++ identity/test/services/MembersDataApiServiceTest.scala
@@ -10,7 +10,6 @@ import org.mockito.Matchers._
 import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import play.api.mvc.{Cookie, Cookies}
 import play.api.libs.json.Json
-
 import scala.concurrent.Future
 
 class MembersDataApiServiceTest
@@ -59,7 +58,6 @@ class MembersDataApiServiceTest
       |}
     """.stripMargin
 
-
   private val config = mock[IdentityConfiguration]
   private val cookies = Cookies(Seq(Cookie("Cookie1", "hash", None, "/", None, false, true, None), Cookie("Cookie2", "hash", None, "/", None, false, true, None)))
 
@@ -70,65 +68,55 @@ class MembersDataApiServiceTest
 
   when(wsMock.url(any[String])).thenReturn(wsRequestMock)
   when(wsRequestMock.withCookies(any())).thenReturn(wsRequestMock)
+  when(wsRequestMock.get()).thenReturn(Future.successful(wsResponseMock))
 
   "getUserContentAccess" should
     "return ContentAccess data for user with valid cookies" in {
-    when(wsRequestMock.get()).thenReturn(Future.successful(wsResponseMock))
     when(wsResponseMock.json).thenReturn(Json.parse(validMdapiResponseJsonString))
     when(wsResponseMock.status).thenReturn(200)
     when(wsResponseMock.statusText).thenReturn("OK")
 
     val futureEither = MdapiService.getUserContentAccess(cookies)
     futureEither map { either => either.isRight shouldBe true }
-    futureEither map { either => either.right.value shouldEqual ContentAccess(true,true,false,true,false,true) }
+    futureEither map { either => either.right.value shouldEqual ContentAccess(true, true, false, true, false, true) }
   }
 
   it should "return MdapiServiceException if unable to extract ContentAccess from json response" in {
-    when(wsRequestMock.get()).thenReturn(Future.successful(wsResponseMock))
     when(wsResponseMock.json).thenReturn(Json.parse(invalidMdapiResponseJsonString))
     when(wsResponseMock.status).thenReturn(200)
 
     val futureEither = MdapiService.getUserContentAccess(cookies)
     futureEither map { either => either.isLeft shouldBe true }
-    futureEither map { either => either.left.value.message should include ("Failed to parse MDAPI response")}
+    futureEither map { either => either.left.value.message should include("Failed to parse MDAPI response") }
   }
 
   it should "return MdapiServiceException if service returns 404 status" in {
-    when(wsRequestMock.get()).thenReturn(Future.successful(wsResponseMock))
     when(wsResponseMock.json).thenReturn(Json.parse(valid404ResponseJsonString))
     when(wsResponseMock.status).thenReturn(404)
     when(wsResponseMock.statusText).thenReturn("Not Found")
 
     val futureEither = MdapiService.getUserContentAccess(cookies)
     futureEither map { either => either.isLeft shouldBe true }
-    futureEither map { either => either.left.value.message should include ("Failed to getUserContentAccess")}
+    futureEither map { either => either.left.value.message should include("Failed to getUserContentAccess") }
   }
 
   "ContentAccess.canProceedWithAutoDeletion" should
     "return true when all cases are false" in {
-    val contentAccess = ContentAccess(false, false, false, false, false, false)
-    contentAccess.canProceedWithAutoDeletion shouldBe true
+    ContentAccess(false, false, false, false, false, false).canProceedWithAutoDeletion shouldBe true
   }
-    it should "return false if any cases are true" in {
-      val contentAccess1 = ContentAccess(true, false, false, false, false, false)
-      val contentAccess2 = ContentAccess(false, false, false, false, false, true)
-      val contentAccess3 = ContentAccess(true, false, true, false, true, false)
-      val contentAccess4 = ContentAccess(true, true, true, true, true, true)
-      contentAccess1.canProceedWithAutoDeletion shouldBe false
-      contentAccess2.canProceedWithAutoDeletion shouldBe false
-      contentAccess3.canProceedWithAutoDeletion shouldBe false
-      contentAccess4.canProceedWithAutoDeletion shouldBe false
-    }
+  it should "return false if any cases are true" in {
+    ContentAccess(true, false, false, false, false, false).canProceedWithAutoDeletion shouldBe false
+    ContentAccess(false, false, false, false, false, true).canProceedWithAutoDeletion shouldBe false
+    ContentAccess(true, false, true, false, true, false).canProceedWithAutoDeletion shouldBe false
+    ContentAccess(true, true, true, true, true, true).canProceedWithAutoDeletion shouldBe false
+  }
 
   "ContentAccess.hasSubscription" should
     "return false when all cases are false" in {
-    val contentAccess = ContentAccess(false, false, false, false, false, false)
-    contentAccess.hasSubscription shouldBe false
+    ContentAccess(false, false, false, false, false, false).hasSubscription shouldBe false
   }
   it should "return true if any cases are true" in {
-    val contentAccess1 = ContentAccess(false, false, false, true, true, true)
-    val contentAccess2 = ContentAccess(false, false, false, false, false, true)
-    contentAccess1.hasSubscription shouldBe true
-    contentAccess2.hasSubscription shouldBe true
+    ContentAccess(false, false, false, true, true, true).hasSubscription shouldBe true
+    ContentAccess(false, false, false, false, false, true).hasSubscription shouldBe true
   }
 }
