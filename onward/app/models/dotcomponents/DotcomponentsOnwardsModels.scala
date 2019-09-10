b05f2bc3d7b317cae546545e79ef26f102cf89b8
Philip McMahon <philip.mcmahon@guardian.co.uk>
2019-09-10 16:39:59 +0100

    Convert arts pillar to culture in DCR rich link controller.

--- onward/app/models/dotcomponents/DotcomponentsOnwardsModels.scala
+++ onward/app/models/dotcomponents/DotcomponentsOnwardsModels.scala
@@ -1,6 +1,6 @@
 package models.dotcomponents
 
-import model.{DotcomContentType, Tag, Tags}
+import model.{DotcomContentType, Pillar, Tag, Tags}
 import play.api.libs.json.Json
 
 case class RichLink(
@@ -18,4 +18,15 @@ case class RichLink(
 
 object RichLink {
   implicit val writes = Json.writes[RichLink]
+
+
+  // note: these functions are duplicated in the article service (DotcomponentsDataModel - if duplicating again consider moving to common!)
+  def isPaidContent(tags: List[Tag]): Boolean = tags.exists(tag => tag.properties.tagType == "Tone" && tag.id == "tone/advertisement-features")
+  def findPillar(pillar: Option[Pillar], tags: List[Tag]): String = {
+    pillar.map { pillar =>
+      if (isPaidContent(tags)) "labs"
+      else if (pillar.toString.toLowerCase == "arts") "culture"
+      else pillar.toString.toLowerCase()
+    }.getOrElse("news")
+  }
 }
