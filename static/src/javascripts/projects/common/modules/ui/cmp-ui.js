c785f7d9b52468f5e2fe580c15e572bbe791b7f0
George Haberis <george.haberis@guardian.co.uk>
2019-09-24 15:14:36 +0100

    add cmp ui animation

--- static/src/javascripts/projects/common/modules/ui/cmp-ui.js
+++ static/src/javascripts/projects/common/modules/ui/cmp-ui.js
@@ -2,8 +2,10 @@
 import { isInVariantSynchronous } from 'common/modules/experiments/ab';
 import { commercialIabCompliant } from 'common/modules/experiments/tests/commercial-iab-compliant';
 import { cmpConfig, cmpUi } from '@guardian/consent-management-platform';
+import fastdom from 'lib/fastdom-promise';
 
 const CMP_READY_CLASS = 'cmp-iframe-ready';
+const CMP_ANIMATE_CLASS = 'cmp-iframe-animate';
 const OVERLAY_CLASS = 'cmp-overlay';
 const IFRAME_CLASS = 'cmp-iframe';
 let container: ?HTMLElement;
@@ -11,15 +13,37 @@ let uiPrepared: boolean = false;
 
 const onReadyCmp = (): void => {
     if (container && container.parentNode) {
-        container.classList.add(CMP_READY_CLASS);
+        fastdom
+            .write(() => {
+                if (container && container.parentNode) {
+                    container.classList.add(CMP_READY_CLASS);
+                }
+            })
+            .then(() => {
+                /**
+                 * Adding CMP_READY_CLASS changes display: none to display: block
+                 * on the overlay. You can't update this display property and transition
+                 * other properties of the container or the iframe in a single step because the display
+                 * property overrides the transitions. We therefore have this short setTimeout
+                 * before adding CMP_ANIMATE_CLASS to transition the overlay opacity and the iframe position.
+                 */
+                setTimeout(() => {
+                    if (container && container.parentNode) {
+                        container.classList.add(CMP_ANIMATE_CLASS);
+                    }
+                }, 0);
+            });
     }
 };
 
 const onCloseCmp = (): void => {
-    if (container && container.parentNode) {
-        container.classList.remove(CMP_READY_CLASS);
-        container.remove();
-    }
+    fastdom.write(() => {
+        if (container && container.parentNode) {
+            container.classList.remove(CMP_READY_CLASS);
+            container.classList.remove(CMP_ANIMATE_CLASS);
+            container.remove();
+        }
+    });
 };
 
 const prepareUi = (): void => {
