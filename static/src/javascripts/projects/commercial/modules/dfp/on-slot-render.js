b255a31b800c96c8339beb03af7cc46f9ce90787
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2019-09-25 12:23:07 +0100

    tests for new outstream size

--- static/src/javascripts/projects/commercial/modules/dfp/on-slot-render.js
+++ static/src/javascripts/projects/commercial/modules/dfp/on-slot-render.js
@@ -52,7 +52,7 @@ const reportEmptyResponse = (
 const outstreamSizes = [
     adSizes.outstreamDesktop.toString(),
     adSizes.outstreamMobile.toString(),
-    adSizes.outstreamGoogleDesktop.toString()
+    adSizes.outstreamGoogleDesktop.toString(),
 ];
 
 export const onSlotRender = (event: SlotRenderEndedEvent): void => {
@@ -90,11 +90,11 @@ export const onSlotRender = (event: SlotRenderEndedEvent): void => {
         }
         // Set refresh field based on the outcome of the slot render.
         const sizeString = advert.size && advert.size.toString();
-        console.log("SIZE STRING");
+        console.log('SIZE STRING');
         console.log(sizeString);
         const isNotFluid = sizeString !== '0,0';
         const isOutstream = outstreamSizes.includes(sizeString);
-        console.log("isOutstream");
+        console.log('isOutstream');
         console.log(isOutstream);
         const isNonRefreshableLineItem =
             event.lineItemId &&
