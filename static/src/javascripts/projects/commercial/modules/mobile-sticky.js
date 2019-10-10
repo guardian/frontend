e52cb13c31247b5dbeec1cfe74c9eaf58b71e02e
Pascal Honoré <pascal@alseyn.net>
2019-10-10 18:17:29 +0100

    Mobile sticky support for DCR

--- static/src/javascripts/projects/commercial/modules/mobile-sticky.js
+++ static/src/javascripts/projects/commercial/modules/mobile-sticky.js
@@ -3,29 +3,47 @@ import fastdom from 'lib/fastdom-promise';
 import { addSlot } from 'commercial/modules/dfp/add-slot';
 import { createSlots } from 'commercial/modules/dfp/create-slots';
 import { shouldIncludeMobileSticky } from 'commercial/modules/prebid/utils';
+import config from 'lib/config';
 
-const createAdWrapper = (): HTMLDivElement => {
-    const wrapper: HTMLDivElement = document.createElement('div');
-    wrapper.className = 'mobilesticky-container';
+const createAdWrapperClassic = () => {
+    const wrapper: HTMLElement = document.createElement('div');
+    wrapper.className = 'mobilesticky-container`';
     const adSlot = createSlots('mobile-sticky', {})[0];
     wrapper.appendChild(adSlot);
     return wrapper;
 };
 
+const createAdWrapperDCR = () => {
+    const wrapper = document.querySelector('.mobilesticky-container');
+    if (wrapper) {
+        const adSlot = createSlots('mobile-sticky', {})[0];
+        wrapper.appendChild(adSlot);
+    }
+    return wrapper;
+};
+
+const createAdWrapper = () => {
+    if (!config.get('isDotcomRendering', false)) {
+        return createAdWrapperClassic();
+    }
+    return createAdWrapperDCR();
+};
+
 export const init = (): Promise<void> => {
     if (shouldIncludeMobileSticky()) {
         const mobileStickyWrapper = createAdWrapper();
-
         return fastdom
             .write(() => {
-                if (document.body)
+                if (document.body && mobileStickyWrapper)
                     document.body.appendChild(mobileStickyWrapper);
             })
             .then(() => {
-                const mobileStickyAdSlot = mobileStickyWrapper.querySelector(
-                    '#dfp-ad--mobile-sticky'
-                );
-                if (mobileStickyAdSlot) addSlot(mobileStickyAdSlot, true);
+                if (mobileStickyWrapper) {
+                    const mobileStickyAdSlot = mobileStickyWrapper.querySelector(
+                        '#dfp-ad--mobile-sticky'
+                    );
+                    if (mobileStickyAdSlot) addSlot(mobileStickyAdSlot, true);
+                }
             });
     }
 
