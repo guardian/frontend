6df055131f2ab6d284b36df78efafd096cb6f6de
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-10-10 15:34:22 +0100

    Add openGraphData to the dotcomponentsDataModel

--- article/app/model/dotcomponents/DotcomponentsDataModel.scala
+++ article/app/model/dotcomponents/DotcomponentsDataModel.scala
@@ -210,6 +210,7 @@ case class DataModelV3(
   isAdFreeUser: Boolean,
   webURL: String,
   linkedData: List[LinkedData],
+  openGraphData: Map[String, String],
   config: JsObject,
   guardianBaseURL: String,
   contentType: String,
@@ -260,6 +261,7 @@ object DataModelV3 {
       "isAdFreeUser" -> model.isAdFreeUser,
       "webURL" -> model.webURL,
       "linkedData" -> model.linkedData,
+      "openGraphData" -> model.openGraphData,
       "config" -> model.config,
       "guardianBaseURL" -> model.guardianBaseURL,
       "contentType" -> model.contentType,
@@ -468,6 +470,8 @@ object DotcomponentsDataModel {
       }
     }
 
+    val openGraphData: Map[String, String] = articlePage.getOpenGraphProperties;
+
     val allTags = article.tags.tags.map(
       t => Tag(
         t.id,
@@ -594,6 +598,7 @@ object DotcomponentsDataModel {
       isAdFreeUser = views.support.Commercial.isAdFree(request),
       webURL = article.metadata.webUrl,
       linkedData = linkedData,
+      openGraphData = openGraphData,
       config = combinedConfig,
       guardianBaseURL = Configuration.site.host,
       contentType = jsConfig("contentType").getOrElse(""),
