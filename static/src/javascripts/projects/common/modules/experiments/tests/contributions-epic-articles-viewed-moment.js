9e3c63d9c9becf5183e4a32e8d222cf7e441e11d
Tom Forbes <tom.forbes@theguardian.com>
2019-10-10 14:34:18 +0100

    update copy

--- static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-articles-viewed-moment.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-articles-viewed-moment.js
@@ -21,11 +21,11 @@ const highlightedText =
 const controlCopy = {
     heading,
     paragraphs: [
-        '... The Guardian will not stay quiet. This is our pledge: we will continue to give global heating, wildlife extinction and pollution the urgent attention and prominence they demand. The Guardian recognises the climate emergency as the defining issue of our times.',
-        'Our independence means we are free to investigate and challenge inaction by those in power around the world. We will inform our readers about threats to our environment based on scientific facts, not on commercial or political interests. And we have made an important change to our style guide to ensure the language we use accurately describes the urgent environmental catastrophes facing the world.',
-        'The Guardian believes that the problems we face on the climate crisis are structural and that fundamental societal change is needed. We will keep reporting on the efforts of individuals and communities around the world who are fearlessly taking a stand for future generations and the preservation of our planet – and to inspire hope.',
+        '... the Guardian will not stay quiet. This is our pledge: we will continue to give global heating, wildlife extinction and pollution the urgent attention and prominence they demand. The Guardian recognises the climate emergency as the defining issue of our times.',
+        'Our independence means we are free to investigate and challenge inaction by those in power. We will inform our readers about threats to the environment based on scientific facts, not driven by commercial or political interests. And we have made several important changes to our style guide to ensure the language we use accurately reflects the environmental catastrophe.',
+        'The Guardian believes that the problems we face on the climate crisis are systemic and that fundamental societal change is needed. We will keep reporting on the efforts of individuals and communities around the world who are fearlessly taking a stand for future generations and the preservation of human life on earth. We want their stories to inspire hope. We will also report back on our own progress as an organisation, as we take important steps to address our impact on the environment.',
         'The Guardian made a choice: to keep our journalism open to all. We do not have a paywall because we believe everyone deserves access to factual information, regardless of where they live or what they can afford.',
-        'We hope you will consider supporting The Guardian’s open, independent reporting today. Every contribution from our readers, however big or small, is so valuable.',
+        'We hope you will consider supporting the Guardian’s open, independent reporting today. Every contribution from our readers, however big or small, is so valuable.',
     ],
     highlightedText,
 };
@@ -33,11 +33,11 @@ const controlCopy = {
 const variantCopy = {
     heading,
     paragraphs: [
-        '... The Guardian will not stay quiet. This is our pledge: we will continue to give global heating, wildlife extinction and pollution the urgent attention and prominence they demand. The Guardian recognises the climate emergency as the defining issue of our times.',
-        'Our independence means we are free to investigate and challenge inaction by those in power around the world. We will inform our readers about threats to our environment based on scientific facts, not on commercial or political interests. And we have made an important change to our style guide to ensure the language we use accurately describes the urgent environmental catastrophes facing the world.',
-        'The Guardian believes that the problems we face on the climate crisis are structural and that fundamental societal change is needed. We will keep reporting on the efforts of individuals and communities around the world who are fearlessly taking a stand for future generations and the preservation of our planet – and to inspire hope.',
+        '... the Guardian will not stay quiet. This is our pledge: we will continue to give global heating, wildlife extinction and pollution the urgent attention and prominence they demand. The Guardian recognises the climate emergency as the defining issue of our times.',
+        'Our independence means we are free to investigate and challenge inaction by those in power. We will inform our readers about threats to the environment based on scientific facts, not driven by commercial or political interests. And we have made several important changes to our style guide to ensure the language we use accurately reflects the environmental catastrophe.',
+        'The Guardian believes that the problems we face on the climate crisis are systemic and that fundamental societal change is needed. We will keep reporting on the efforts of individuals and communities around the world who are fearlessly taking a stand for future generations and the preservation of human life on earth. We want their stories to inspire hope. We will also report back on our own progress as an organisation, as we take important steps to address our impact on the environment.',
         `You’ve read ${articleViewCount} Guardian articles in the last month – made possible by our choice to keep Guardian journalism open to all. We do not have a paywall because we believe everyone deserves access to factual information, regardless of where they live or what they can afford.`,
-        'We hope you will consider supporting The Guardian’s open, independent reporting today. Every contribution from our readers, however big or small, is so valuable.',
+        'We hope you will consider supporting the Guardian’s open, independent reporting today. Every contribution from our readers, however big or small, is so valuable.',
     ],
     highlightedText,
 };
