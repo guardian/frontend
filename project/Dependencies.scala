a752a82de1091f1b156c88cbb9444592739205a5
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-10-10 09:04:10 +0100

    Bump capi to v15.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   val identityLibVersion = "3.185"
   val awsVersion = "1.11.240"
-  val capiVersion = "15.3"
+  val capiVersion = "15.4"
   val faciaVersion = "3.0.2"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
