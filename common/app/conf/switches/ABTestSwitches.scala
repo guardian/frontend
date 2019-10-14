521eca2e56657725a08960439af5b007bf3289b5
George Haberis <george.haberis@guardian.co.uk>
2019-10-14 11:33:10 +0100

    Update common/app/conf/switches/ABTestSwitches.scala
    Co-Authored-By: Ricardo Costa <48949546+ripecosta@users.noreply.github.com>

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -12,7 +12,7 @@ trait ABTestSwitches {
     "Test whether our new CMP UI obtains target consent rates",
     owners = Seq(Owner.withGithub("ghaberis")),
     safeState = Off,
-    sellByDate = new LocalDate(2019, 10, 18),
+    sellByDate = new LocalDate(2019, 12, 03),
     exposeClientSide = true
   )
 
