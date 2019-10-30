fc513194d8c48bb965646d230d72a8caa52712e2
TJ Silver <tjsilver@protonmail.com>
2019-10-30 18:09:31 +0000

    wip final push banner test

--- static/src/javascripts/projects/common/modules/experiments/tests/contributions-moment-banner-environment.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contributions-moment-banner-environment.js
@@ -23,11 +23,19 @@ const thankYouClosingSentence =
     'Thank you for supporting the Guardian – readers from around the world, like you, make this work possible.';
 const thankYouCTA = 'Support us again';
 
+const stayQuietFinalPushCopy = 'The climate emergency is the defining issue of our times. Since we published our environmental pledge, Guardian readers from more than 100 countries across the world have supported us. Many of you have told us how much you value our commitment: to be truthful, resolute and undeterred in pursuing this important journalism. ';
+
+const thankYouFinalPushCopy = 'The climate emergency is the defining issue of our times. Since we published our environmental pledge, Guardian readers from more than 100 countries across the world have supported us. Many of you have told us how much you value our commitment: to be truthful, resolute and undeterred in pursuing this important journalism. ';
+
+const closingSentenceFinalPush = 'Your support is galvanising – it makes our work possible and is critical for our future. Thank you.';
+
+
 // These test params must be set on engagementBannerParams *and* passed into canShowBannerSync
 // TODO - we need to rethink how banner tests are selected/displayed
 const userCohortParam = {
     momentBannerDefault: 'AllNonSupporters',
     momentBannerThankYou: 'AllExistingSupporters',
+    momentBannerEveryone: 'Everyone',
 };
 const minArticlesBeforeShowingBanner = 0;
 
@@ -39,8 +47,7 @@ const secondaryLinkUrl =
         ? 'https://www.theguardian.com/environment/2019/oct/16/climate-crisis-america-guardian-editor-john-mulholland?INTCMP=climate_pledge_2019'
         : 'https://www.theguardian.com/environment/ng-interactive/2019/oct/16/the-guardians-climate-pledge-2019?INTCMP=climate_pledge_2019';
 
-const secondaryLinkLabel =
-    geolocation === 'US' ? 'Hear from our editor' : 'Read our pledge';
+const secondaryLinkLabel = 'Why support matters';
 
 export const environmentMomentBannerNonSupporters: AcquisitionsABTest = {
     id: 'ContributionsBannerEnvironmentMomentNonSupporters',
@@ -122,6 +129,68 @@ export const environmentMomentBannerSupporters: AcquisitionsABTest = {
                 canShowBannerSync(
                     minArticlesBeforeShowingBanner,
                     userCohortParam.momentBannerThankYou
+                ),
+        },
+    ],
+};
+
+export const environmentMomentBannerFinalPush: AcquisitionsABTest = {
+    id: 'ContributionsBannerEnvironmentMomentFinalPush',
+    start: '2019-01-01',
+    expiry: '2020-09-30',
+    author: 'Thalia Silver',
+    description: 'environment moment',
+    audience: 1,
+    audienceOffset: 0,
+    successMeasure: 'AV per impression',
+    audienceCriteria: 'All',
+    idealOutcome: 'best AV possible',
+    canRun: () => true,
+    showForSensitive: true,
+    campaignId,
+    componentType: 'ACQUISITIONS_ENGAGEMENT_BANNER',
+    geolocation,
+    variants: [
+        {
+            id: 'stayquiet',
+            test: (): void => {}, // banner tests look at the bucket and vary the copy themselves
+            engagementBannerParams: {
+                messageText: stayQuietFinalPushCopy,
+                closingSentence: closingSentenceFinalPush,
+                template: acquisitionsBannerMomentTemplate,
+                bannerModifierClass: 'moment-banner',
+                minArticlesBeforeShowingBanner,
+                userCohort: userCohortParam.momentBannerEveryone,
+                titles: ['We will not stay quiet ', 'on the climate crisis'],
+                linkUrl,
+                secondaryLinkUrl,
+                secondaryLinkLabel,
+            },
+            canRun: () =>
+                canShowBannerSync(
+                    minArticlesBeforeShowingBanner,
+                    userCohortParam.momentBannerEveryone
+                ),
+        },
+        {
+            id: 'thankyou',
+            test: (): void => {}, // banner tests look at the bucket and vary the copy themselves
+            engagementBannerParams: {
+                messageText: thankYouFinalPushCopy,
+                closingSentence: closingSentenceFinalPush,
+                template: acquisitionsBannerMomentTemplate,
+                bannerModifierClass: 'moment-banner',
+                minArticlesBeforeShowingBanner,
+                userCohort: userCohortParam.momentBannerEveryone,
+                titles: ['Thank you for supporting ', 'our environmental pledge'],
+                linkUrl,
+                secondaryLinkUrl,
+                secondaryLinkLabel,
+            },
+            canRun: () =>
+                canShowBannerSync(
+                    minArticlesBeforeShowingBanner,
+                    userCohortParam.momentBannerEveryone
                 ),
         },
     ],
