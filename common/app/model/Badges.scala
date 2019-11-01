05159acce9aab4f6e9d26201e97ae0f233404759
Harry Fischer <harry.j.fischer@gmail.com>
2019-11-01 17:14:34 +0000

    Rever"Revert "Badges harry""
    This reverts commit 5c0a4b3eac95c2d0e1160a01fcd4bb52e6c15e4a.

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -65,11 +65,14 @@ object Badges {
   val theEmptyDoorway = Badge("cities/series/the-empty-doorway", Static("images/badges/the-empty-doorway.svg"))
   val yemenAtWar = Badge("world/series/yemen-at-war", Static("images/badges/yemen-at-war.svg"))
   val thePolluters = Badge("environment/series/the-polluters", Static("images/badges/the-polluters.svg"))
-
+  val youthJustice = Badge("society/youthjustice", Static("images/badges/childrenInTheDocks-Icon1.svg"))
+  val generalElection2019 = Badge("politics/general-election-2019", Static("images/badges/general-election2019-badge.svg"))
 
   val allBadges = Seq(newArrivals, brexitGamble, roadToTheVote, brexitFrontline, brexitDividedGenerations,
     brexitHowItCameToThis, londonVersus, beyondTheBlade, euElection, paradisePapers, cambridgeAnalytica, specialReport, nhs70, cricketWorldCup, womensWorldCup, greenBlood, usElections2020, ausElection,
-    midterm, theNewPopulism, theImplantFiles, theRealBorisJohnson, johnsonsPromises, rugbyWorldCup, behindTheLines, theEmptyDoorway, yemenAtWar, thePolluters)
+    midterm, theNewPopulism, theImplantFiles, theRealBorisJohnson,
+    johnsonsPromises, rugbyWorldCup, behindTheLines, theEmptyDoorway,
+    yemenAtWar, thePolluters, youthJustice, generalElection2019)
 
   def badgeFor(c: ContentType): Option[Badge] = {
     badgeForTags(c.tags.tags.map(_.id))
