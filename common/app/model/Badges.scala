51634abce998652ab6dd8a89345105772a17aaa8
Harry Fischer <harry.fischer@guardian.co.uk>
2019-11-06 10:21:48 +0000

    added election badge 2019

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -66,12 +66,13 @@ object Badges {
   val yemenAtWar = Badge("world/series/yemen-at-war", Static("images/badges/yemen-at-war.svg"))
   val thePolluters = Badge("environment/series/the-polluters", Static("images/badges/the-polluters.svg"))
   val youthJustice = Badge("society/youthjustice", Static("images/badges/childrenInTheDocks-Icon1.svg"))
+  val electionBadge = Badge("politics/general-election-2019", Static("images/badges/election-badge-2.svg"))
 
   val allBadges = Seq(newArrivals, brexitGamble, roadToTheVote, brexitFrontline, brexitDividedGenerations,
     brexitHowItCameToThis, londonVersus, beyondTheBlade, euElection, paradisePapers, cambridgeAnalytica, specialReport, nhs70, cricketWorldCup, womensWorldCup, greenBlood, usElections2020, ausElection,
     midterm, theNewPopulism, theImplantFiles, theRealBorisJohnson,
     johnsonsPromises, rugbyWorldCup, behindTheLines, theEmptyDoorway,
-    yemenAtWar, thePolluters, youthJustice)
+    yemenAtWar, thePolluters, youthJustice, electionBadge)
 
   def badgeFor(c: ContentType): Option[Badge] = {
     badgeForTags(c.tags.tags.map(_.id))
