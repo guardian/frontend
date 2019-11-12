ee27ed6575b1e57d16510dccdf115911b5c00c96
Tom Forbes <tom.forbes@theguardian.com>
2019-11-12 09:26:40 +0000

    Remove moment epic + banner (#21974)
    * Remove moment epics
    
    * remove moment banner

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -38,26 +38,6 @@ trait ABTestSwitches {
 
   Switch(
     ABTests,
-    "ab-contributions-epic-articles-viewed-month-moment-final",
-    "Moment epic which also states how many articles a user has viewed",
-    owners = Seq(Owner.withGithub("tomrf1")),
-    safeState = Off,
-    sellByDate = new LocalDate(2020, 1, 27),
-    exposeClientSide = true
-  )
-
-  Switch(
-    ABTests,
-    "ab-contributions-epic-articles-viewed-60-days-moment-final",
-    "Moment epic which also states how many articles a user has viewed",
-    owners = Seq(Owner.withGithub("tomrf1")),
-    safeState = Off,
-    sellByDate = new LocalDate(2020, 1, 27),
-    exposeClientSide = true
-  )
-
-  Switch(
-    ABTests,
     "ab-contributions-epic-learn-more-cta",
     "States how many articles a user has viewed in the epic in a month",
     owners = Seq(Owner.withGithub("jlieb10")),
@@ -81,16 +61,6 @@ trait ABTestSwitches {
     "ab-contributions-banner-articles-viewed",
     "show number of articles viewed in contributions banner",
     owners = Seq(Owner.withGithub("tomrf1")),
-    safeState = Off,
-    sellByDate = new LocalDate(2020, 9, 30),
-    exposeClientSide = true
-  )
-
-  Switch(
-    ABTests,
-    "ab-contributions-banner-environment-moment-final-push",
-    "moment banner test for final push",
-    owners = Seq(Owner.withGithub("jlieb10")),
     safeState = Off,
     sellByDate = new LocalDate(2020, 9, 30),
     exposeClientSide = true
--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -3,14 +3,11 @@ import { commercialPrebidSafeframe } from 'common/modules/experiments/tests/comm
 import { commercialCmpUiIab } from 'common/modules/experiments/tests/commercial-cmp-ui-iab';
 import { askFourEarning } from 'common/modules/experiments/tests/contributions-epic-ask-four-earning';
 import { articlesViewed } from 'common/modules/experiments/tests/contributions-epic-articles-viewed';
-import { articlesViewedMonthMomentFinal } from 'common/modules/experiments/tests/contributions-epic-articles-viewed-month-moment';
-import { articlesViewed60DaysMomentFinal } from 'common/modules/experiments/tests/contributions-epic-articles-viewed-60-days-moment';
 import { countryName } from 'common/modules/experiments/tests/contributions-epic-country-name';
 import { acquisitionsEpicAlwaysAskIfTagged } from 'common/modules/experiments/tests/acquisitions-epic-always-ask-if-tagged';
 import { adblockTest } from 'common/modules/experiments/tests/adblock-ask';
 import { articlesViewedBanner } from 'common/modules/experiments/tests/contribs-banner-articles-viewed';
 import { learnMore } from 'common/modules/experiments/tests/contributions-epic-learn-more-cta';
-import { environmentMomentBannerFinalPush } from 'common/modules/experiments/tests/contributions-moment-banner-environment';
 import { xaxisAdapterTest } from 'common/modules/experiments/tests/commercial-xaxis-adapter';
 import { appnexusUSAdapter } from 'common/modules/experiments/tests/commercial-appnexus-us-adapter';
 import { pangaeaAdapterTest } from 'common/modules/experiments/tests/commercial-pangaea-adapter';
@@ -25,8 +22,6 @@ export const concurrentTests: $ReadOnlyArray<ABTest> = [
 ];
 
 export const epicTests: $ReadOnlyArray<EpicABTest> = [
-    articlesViewedMonthMomentFinal,
-    articlesViewed60DaysMomentFinal,
     articlesViewed,
     learnMore,
     countryName,
@@ -36,5 +31,4 @@ export const epicTests: $ReadOnlyArray<EpicABTest> = [
 
 export const engagementBannerTests: $ReadOnlyArray<AcquisitionsABTest> = [
     articlesViewedBanner,
-    environmentMomentBannerFinalPush,
 ];
