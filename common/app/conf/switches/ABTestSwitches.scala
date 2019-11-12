17e10220c6bcee7dcef2e53275f1ad0672079a43
George Haberis <george.haberis@guardian.co.uk>
2019-11-12 12:12:13 +0000

    pass ab test variant to CMP UI

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -18,6 +18,16 @@ trait ABTestSwitches {
 
   Switch(
     ABTests,
+    "ab-commercial-cmp-ui-non-dismissable",
+    "Test whether our new CMP UI obtains target consent rates using non-dismissable variant",
+    owners = Seq(Owner.withGithub("ghaberis")),
+    safeState = Off,
+    sellByDate = new LocalDate(2019, 12, 3),
+    exposeClientSide = true
+  )
+
+  Switch(
+    ABTests,
     "ab-contributions-epic-ask-four-earning",
     "This places the epic on all articles for all users, with a limit of 4 impressions in any given 30 days",
     owners = Seq(Owner.withGithub("jranks123")),
