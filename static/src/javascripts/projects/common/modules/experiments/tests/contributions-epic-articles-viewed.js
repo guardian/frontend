96f07296b94b86879f53337f69f5dd71f46a4986
Tom Forbes <tom.forbes@theguardian.com>
2019-11-13 13:41:58 +0000

    remove country name

--- static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-articles-viewed.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contributions-epic-articles-viewed.js
@@ -5,29 +5,17 @@ import {
     buildEpicCopy,
 } from 'common/modules/commercial/contributions-utilities';
 import { getArticleViewCountForDays } from 'common/modules/onward/history';
-import { getCountryName, getSync as geolocationGetSync } from 'lib/geolocation';
+import { getSync as geolocationGetSync } from 'lib/geolocation';
 
 const geolocation = geolocationGetSync();
-const isUSUK = ['GB', 'US'].includes(geolocation);
 
 const highlightedText =
     'Support The Guardian from as little as %%CURRENCY_SYMBOL%%1 – and it only takes a minute. Thank you.';
 
-const USUKControlCopy = {
+const controlCopy = {
     heading: 'Since you’re here...',
     paragraphs: [
         '... we have a small favour to ask. More people are reading and supporting The Guardian’s independent, investigative journalism than ever before. And unlike many new organisations, we have chosen an approach that allows us to keep our journalism accessible to all, regardless of where they live or what they can afford. But we need your ongoing support to keep working as we do.',
-        'The Guardian will engage with the most critical issues of our time – from the escalating climate catastrophe to widespread inequality to the influence of big tech on our lives. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart.',
-        'Our editorial independence means we set our own agenda and voice our own opinions. Guardian journalism is free from commercial and political bias and not influenced by billionaire owners or shareholders. This means we can give a voice to those less heard, explore where others turn away, and rigorously challenge those in power.',
-        'We need your support to keep delivering quality journalism, to maintain our openness and to protect our precious independence. Every reader contribution, big or small, is so valuable.',
-    ],
-    highlightedText,
-};
-
-const ROWControlCopy = {
-    heading: 'More people in %%COUNTRY_NAME%%...',
-    paragraphs: [
-        '... like you, are reading and supporting The Guardian’s independent, investigative journalism than ever before. And unlike many new organisations, we have chosen an approach that allows us to keep our journalism accessible to all, regardless of where they live or what they can afford. But we need your ongoing support to keep working as we do.',
         'The Guardian will engage with the most critical issues of our time – from the escalating climate catastrophe to widespread inequality to the influence of big tech on our lives. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart.',
         'Our editorial independence means we set our own agenda and voice our own opinions. Guardian journalism is free from commercial and political bias and not influenced by billionaire owners or shareholders. This means we can give a voice to those less heard, explore where others turn away, and rigorously challenge those in power.',
         'We need your support to keep delivering quality journalism, to maintain our openness and to protect our precious independence. Every reader contribution, big or small, is so valuable.',
@@ -91,18 +79,12 @@ export const articlesViewed: EpicABTest = makeEpicABTest({
     geolocation,
     highPriority: true,
 
-    canRun: () => !!getCountryName(geolocation),
-
     variants: [
         {
             id: 'control',
             buttonTemplate: defaultButtonTemplate,
             products: [],
-            copy: buildEpicCopy(
-                isUSUK ? USUKControlCopy : ROWControlCopy,
-                !isUSUK,
-                geolocation
-            ),
+            copy: buildEpicCopy(controlCopy, false, geolocation),
         },
         buildArticleCountVariant({
             name: '5_articles_in_1_month',
