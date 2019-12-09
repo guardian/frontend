0655ac6a162452b6cd8cab29ae3b27f9eceeeaea
Joshua <joshuathomaslieberman@gmail.com>
2019-12-09 12:13:30 +0000

    removed version of banner without article count from article count test

--- static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-articles-viewed.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-articles-viewed.js
@@ -7,26 +7,18 @@ import {
     getSync as geolocationGetSync,
 } from 'lib/geolocation';
 import { getArticleViewCountForDays } from 'common/modules/onward/history';
-import { buildBannerCopy } from 'common/modules/commercial/contributions-utilities';
 
 // User must have read at least 5 articles in last 60 days
 const minArticleViews = 5;
 const articleCountDays = 60;
 
 const articleViewCount = getArticleViewCountForDays(articleCountDays);
-
 const geolocation = geolocationGetSync();
-const isUSUKAU = ['GB', 'US', 'AU'].includes(geolocation);
-
 const messageText =
     'Unlike many news organisations, we made a choice to keep our journalism free and available for all. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart. Every contribution, big or small, is so valuable – it is essential in protecting our editorial independence.';
 const ctaText = `<span class="engagement-banner__highlight"> Support The Guardian from as little as ${getLocalCurrencySymbol(
     geolocation
 )}1</span>`;
-const USUKAUControlLeadSentence =
-    'We chose a different approach. Will you support it?';
-const ROWControlLeadSentence =
-    'More people in %%COUNTRY_NAME%%, like you, are reading and supporting The Guardian’s independent, investigative journalism.';
 
 export const articlesViewedBanner: AcquisitionsABTest = {
     id: 'ContributionsBannerArticlesViewed',
@@ -46,22 +38,6 @@ export const articlesViewedBanner: AcquisitionsABTest = {
     componentType: 'ACQUISITIONS_ENGAGEMENT_BANNER',
     geolocation,
     variants: [
-        {
-            id: 'control',
-            test: (): void => {},
-            engagementBannerParams: {
-                leadSentence: buildBannerCopy(
-                    isUSUKAU
-                        ? USUKAUControlLeadSentence
-                        : ROWControlLeadSentence,
-                    !isUSUKAU,
-                    geolocation
-                ),
-                messageText,
-                ctaText,
-                template: acquisitionsBannerControlTemplate,
-            },
-        },
         {
             id: 'variant',
             test: (): void => {},
