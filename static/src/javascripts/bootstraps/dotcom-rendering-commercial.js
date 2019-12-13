ecb0b9f641f62ad1dbc9b8dedaff69a5d2259e72
Pascal Honoré <pascal@alseyn.net>
2019-12-13 00:14:23 +0000

    Call initPermutive on DCR
    Co-authored-by: Fares Basmadji <fares.basmadji@theguardian.co.uk>

--- static/src/javascripts/bootstraps/dotcom-rendering-commercial.js
+++ static/src/javascripts/bootstraps/dotcom-rendering-commercial.js
@@ -18,6 +18,7 @@ import { trackConsent as trackCmpConsent } from 'commercial/modules/cmp/consent-
 import { init as prepareAdVerification } from 'commercial/modules/ad-verification/prepare-ad-verification';
 import { init as prepareGoogletag } from 'commercial/modules/dfp/prepare-googletag';
 import { init as preparePrebid } from 'commercial/modules/dfp/prepare-prebid';
+import { initPermutive } from 'commercial/modules/dfp/prepare-permutive';
 import { init as initLiveblogAdverts } from 'commercial/modules/liveblog-adverts';
 import { init as initStickyTopBanner } from 'commercial/modules/sticky-top-banner';
 import { init as initThirdPartyTags } from 'commercial/modules/third-party-tags';
@@ -42,8 +43,8 @@ const commercialModules: Array<Array<any>> = [
 if (!commercialFeatures.adFree) {
     commercialModules.push(
         ['cm-prepare-prebid', preparePrebid],
-        ['cm-prepare-googletag', prepareGoogletag],
         ['cm-thirdPartyTags', initThirdPartyTags],
+        ['cm-prepare-googletag', () => initPermutive().then(prepareGoogletag)],
         ['cm-prepare-adverification', prepareAdVerification],
         ['cm-mobileSticky', initMobileSticky],
         ['cm-highMerch', initHighMerch],
