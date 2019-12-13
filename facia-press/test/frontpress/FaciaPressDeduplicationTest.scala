1f7d69a76f9c41b99288e678a1613af06738b99e
Pascal Honoré <pascal@alseyn.net>
2019-12-13 23:16:33 +0000

    Tiny test refectoring

--- facia-press/test/frontpress/FaciaPressDeduplicationTest.scala
+++ facia-press/test/frontpress/FaciaPressDeduplicationTest.scala
@@ -12,18 +12,18 @@ class FaciaPressDeduplicationTest extends FlatSpec with Matchers with FaciaPress
     val newSequence = PressedCollectionDeduplication.deduplication(sequence)
   }
 
-  "deduplication" should "curated elements are never removed" in new FaciaPressDeduplicationTestDataScope {
+  it should "curated elements are never removed" in new FaciaPressDeduplicationTestDataScope {
     newSequence(0).pressedCollection.curated.size shouldBe 4
     newSequence(1).pressedCollection.curated.size shouldBe 8
     newSequence(2).pressedCollection.curated.size shouldBe 21
     newSequence(3).pressedCollection.curated.size shouldBe 11
   }
 
-  "deduplication" should "remove duplicated backfill'ed content" in new FaciaPressDeduplicationTestDataScope {
+  it should "remove duplicated backfill'ed content" in new FaciaPressDeduplicationTestDataScope {
     newSequence(1).pressedCollection.backfill.size shouldBe 2
   }
 
-  "deduplication" should "remove all backfilled contents when possible" in new FaciaPressDeduplicationTestDataScope {
+  it should "remove all backfilled contents when possible" in new FaciaPressDeduplicationTestDataScope {
     newSequence(3).pressedCollection.backfill shouldBe empty
   }
 }
