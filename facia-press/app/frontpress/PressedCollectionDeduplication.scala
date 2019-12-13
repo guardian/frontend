c4f3a15c8205959b6e7afd691f5c39015dac8747
Pascal Honoré <pascal@alseyn.net>
2019-12-13 16:55:43 +0000

    Documentation of the logic

--- facia-press/app/frontpress/PressedCollectionDeduplication.scala
+++ facia-press/app/frontpress/PressedCollectionDeduplication.scala
@@ -29,6 +29,30 @@ object PressedCollectionDeduplication {
 
    */
 
+  /*
+
+      Pascal - 13th Dec 2019
+
+      Today I am introducing a logic that is as close to the target logic as possible, while handling two interesting cases:
+        1. Maintaining the integrity of the Most Popular container.
+        2. Allowing for backfill'ed only containers to work fine.
+
+      Considering the situation:
+        - The `PressedCollection`s are given in order `[pc_{1}, pc_{2}, ...., pc_{n-1}, pc_{n}, pc_{n+1}, ... ]`.
+        - During the fold we have computed `[pc_{1}, pc_{2}, ...., pc_{n-1}]` and we are given `pc_{n}`.
+
+      Define: `pc_{n}'i` = Deduplication at depth `i` of the backfilled elements of `pc_{n}` using the first `i` curated or backfilled elements of `[pc_{1}, pc_{2}, ...., pc_{n-1}]`
+
+      We compute `pc_{n}'i` for i \in { 1, ...., i_{max} } # Where `i_{max}` the maximum length of the curated or backfilled arrays of the entire collection `[pc_{1}, pc_{2}, ...., pc_{n-1}]`.
+
+      The intuitive meaning of `pc_{n}'i` is that the bigger the `i` the closer we are from the ideal situation of complete deduplication.
+
+      The having been said `pc_{n}'i` with large `i` can lead to Most Popular containers being only partially filled.
+
+      Therefore, once [pc_{n}'1, pc_{n}'2, ..., pc_{n}'i_{max}] has been computed, we take either the first one or the last possible one with at least 10 elements.
+
+   */
+
   def getHeaderURLsFromCuratedAndBackfilled(pCVs: Seq[PressedCollectionVisibility]): Seq[String] = {
     // Return the header urls of all curated or backfill elements of a sequence of `PressedCollectionVisibility`.
 
