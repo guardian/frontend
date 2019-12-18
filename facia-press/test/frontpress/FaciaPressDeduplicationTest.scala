ab1f89c158c1f9b4c95f799638a295acc2e27e7c
Pascal Honoré <pascal@alseyn.net>
2019-12-18 16:14:28 +0000

    Implement updated logic

--- facia-press/test/frontpress/FaciaPressDeduplicationTest.scala
+++ facia-press/test/frontpress/FaciaPressDeduplicationTest.scala
@@ -10,21 +10,13 @@ class FaciaPressDeduplicationTest extends FlatSpec with Matchers with FaciaPress
     PressedCollectionVisibility(collection1, 0),
     PressedCollectionVisibility(collection2, 0),
     PressedCollectionVisibility(collection3, 0),
-    PressedCollectionVisibility(collection4, 0)
+    PressedCollectionVisibility(collection4, 0),
+    PressedCollectionVisibility(collection5, 0)
   )
   // Note that the integer (0) passed as second argument of PressedCollectionVisibility.apply is irrelevant. We use
   // it because the current version of PressedCollectionDeduplication.deduplication still takes PressedCollectionVisibility
 
   val newSequence = PressedCollectionDeduplication.deduplication(sequence)
-
-  // What we want to see.
-
-  // 1. The curated elements are never removed
-
-  // 2. Just one element backfilled elements of collection1 is going to be removed (this is because after "link21" is
-  // removed, we will be with a collection which has exactly 10 elements)
-
-  // 3. All the backfilled elements of collection3 are going to be removed.
 
   it should "never remove curated elements" in {
     newSequence(0).pressedCollection.curated.size shouldBe sequence(0).pressedCollection.curated.size
@@ -34,12 +26,17 @@ class FaciaPressDeduplicationTest extends FlatSpec with Matchers with FaciaPress
     newSequence(4).pressedCollection.curated.size shouldBe sequence(4).pressedCollection.curated.size
   }
 
-  it should "deduplicate backfill'ed content" in {
-    newSequence(1).pressedCollection.backfill.size shouldBe 1 // Test that we do not deduplicate below 10 elements
-    newSequence(2).pressedCollection.backfill.size shouldBe 0 // Test that we deduplicated against curated and backfilled elements
+  it should "not deduplicate backfill'ed content if resulting in less than 10 elements" in {
+    newSequence(1).pressedCollection.backfill.size shouldBe 1 // Test that we do not deduplicate below 10 elements [1]
   }
 
-  it should "remove all backfilled contents when possible" in {
-    // newSequence(3).pressedCollection.backfill shouldBe empty
+  it should "deduplicate backfill'ed content against curated and backfilled elements" in {
+    newSequence(2).pressedCollection.backfill.size shouldBe 0 // Test that we deduplicate against curated and backfilled elements
+  }
+
+  it should "force remove backfilled contents when appearing in the previous container as curated (even if less than 10 elements)" in {
+    // .... thereby overidding [1]
+    newSequence(4).pressedCollection.backfill.size shouldBe 0
+    newSequence(5).pressedCollection.backfill.size shouldBe 1
   }
 }
