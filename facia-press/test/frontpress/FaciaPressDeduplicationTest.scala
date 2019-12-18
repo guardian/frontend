9d302373aeebfc0e4aabfe26dbe58a8720a47c88
Pascal Honoré <pascal@alseyn.net>
2019-12-18 15:16:40 +0000

    Better test for curated elements

--- facia-press/test/frontpress/FaciaPressDeduplicationTest.scala
+++ facia-press/test/frontpress/FaciaPressDeduplicationTest.scala
@@ -11,15 +11,25 @@ class FaciaPressDeduplicationTest extends FlatSpec with Matchers with FaciaPress
 
   val newSequence = PressedCollectionDeduplication.deduplication(sequence)
 
+  // What we want to see.
+
+  // 1. The curated elements are never removed
+
+  // 2. Just one element backfilled elements of collection1 is going to be removed (this is because after "link21" is
+  // removed, we will be with a collection which has exactly 10 elements)
+
+  // 3. All the backfilled elements of collection3 are going to be removed.
+
   it should "curated elements are never removed" in {
     newSequence(0).pressedCollection.curated.size shouldBe 4
     newSequence(1).pressedCollection.curated.size shouldBe 8
-    newSequence(2).pressedCollection.curated.size shouldBe 21
+    newSequence(2).pressedCollection.curated.size shouldBe 22
     newSequence(3).pressedCollection.curated.size shouldBe 11
   }
 
   it should "remove duplicated backfill'ed content" in {
     newSequence(1).pressedCollection.backfill.size shouldBe 2
+    // It is not 1, because we do not deduplicate if resulting in less than 10 elements.
   }
 
   it should "remove all backfilled contents when possible" in {
