cf8de4de45bc03fb2816448592e2269951c6daf3
Tom Forbes <tom.forbes@theguardian.com>
2019-12-19 14:40:25 +0000

    update copy

--- static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-us-eoy/common.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-us-eoy/common.js
@@ -11,12 +11,12 @@ export const getDaysLeftBeforeEOY2019 = (): number =>
     getDaysLeftInCampaign(new Date('Dec 21, 2019 23:59:59').getTime());
 
 export const daysLeftCopy = (daysLeft: number): string => {
-    switch(daysLeft) {
+    switch (daysLeft) {
         case 0:
-            return `It's the last day of 2019`;
+            return `It's the last day to contribute in 2019`;
         case 1:
-            return '1 day left in 2019';
+            return '1 day left to contribute in 2019';
         default:
-            return `${daysLeft} days left in 2019`;
+            return `${daysLeft} days left to contribute in 2019`;
     }
 };
