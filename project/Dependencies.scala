410bbbce1d530f08e362274eca4ae5d312e7d5a4
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2020-01-16 13:50:02 +0000

    Bumping versions of atom-renderer and content-api-client

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   val identityLibVersion = "3.193"
   val awsVersion = "1.11.240"
-  val capiVersion = "15.6"
+  val capiVersion = "15.9"
   val faciaVersion = "3.0.20"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
@@ -72,7 +72,7 @@ object Dependencies {
   val playJsonExtensions = "ai.x" %% "play-json-extensions" % playJsonExtensionsVersion
   val playJsonJoda = "com.typesafe.play" %% "play-json-joda" % playJsonVersion
   val playIteratees = "com.typesafe.play" %% "play-iteratees" % "2.6.1"
-  val atomRenderer = "com.gu" %% "atom-renderer" % "1.1.1"
+  val atomRenderer = "com.gu" %% "atom-renderer" % "1.1.2"
   val supportInternationalisation = "com.gu" %% "support-internationalisation" % "0.9"
   val capiAws = "com.gu" %% "content-api-client-aws" % "0.5"
   val okhttp = "com.squareup.okhttp3" % "okhttp" % "3.10.0"
