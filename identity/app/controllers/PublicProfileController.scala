3ffe393c1bd155253c9aa12ae3be4e794f73bb48
vanessa <32312712+vlbee@users.noreply.github.com>
2020-01-16 13:02:43 +0000

    Use DiscussionApiService.userHasPublicProfile in Identity PublicProfileController

--- identity/app/controllers/PublicProfileController.scala
+++ identity/app/controllers/PublicProfileController.scala
@@ -3,7 +3,7 @@ package controllers
 import model.Cached.RevalidatableResult
 import play.api.mvc._
 import common.ImplicitControllerExecutionContext
-import services.{IdRequestParser, IdentityUrlBuilder}
+import services.{DiscussionApiService, DiscussionApiServiceException, IdRequestParser, IdentityUrlBuilder}
 import utils.SafeLogging
 import model.{ApplicationContext, Cached, IdentityPage}
 import idapiclient.{IdApiClient, Response}
@@ -13,14 +13,15 @@ import com.gu.identity.model.User
 import pages.IdentityHtmlPage
 
 class PublicProfileController(
-  idUrlBuilder: IdentityUrlBuilder,
-  identityApiClient: IdApiClient,
-  idRequestParser: IdRequestParser,
-  val controllerComponents: ControllerComponents
-)(implicit context: ApplicationContext)
+                               idUrlBuilder: IdentityUrlBuilder,
+                               identityApiClient: IdApiClient,
+                               idRequestParser: IdRequestParser,
+                               discussionApi: DiscussionApiService,
+                               val controllerComponents: ControllerComponents
+                             )(implicit context: ApplicationContext)
   extends BaseController
-    with ImplicitControllerExecutionContext
-    with SafeLogging {
+  with ImplicitControllerExecutionContext
+  with SafeLogging {
 
   def renderProfileFromVanityUrl(vanityUrl: String, activityType: String): Action[AnyContent] = renderPublicProfilePage(
     "/user/" + vanityUrl,
@@ -28,24 +29,39 @@ class PublicProfileController(
     identityApiClient.userFromVanityUrl(vanityUrl)
   )
 
-  def renderProfileFromId(id: String, activityType: String): Action[AnyContent] = renderPublicProfilePage("/user/id/"+id, activityType, identityApiClient.user(id))
+  def renderProfileFromId(id: String, activityType: String): Action[AnyContent] = renderPublicProfilePage("/user/id/" + id, activityType, identityApiClient.user(id))
 
   def renderPublicProfilePage(url: String, activityType: String, futureUser: => Future[Response[User]]): Action[AnyContent] = Action.async { implicit request =>
-    futureUser.map {
+    futureUser.flatMap {
       case Left(errors) =>
         logger.info(s"public profile page returned errors ${errors.toString()}")
-        NotFound(views.html.errors._404())
+        Future {
+          NotFound(views.html.errors._404())
+        }
 
       case Right(user) =>
-        val title = user.publicFields.username.fold("public profile")(username => s"$username's public profile")
-        implicit val identityPage: IdentityPage = IdentityPage(url,  title, usesGuardianHeader = true)
-        Cached(60)(
-          RevalidatableResult.Ok(
-            IdentityHtmlPage.html(
-              views.html.publicProfilePage(identityPage, idRequestParser(request), idUrlBuilder, user, activityType)
+
+        discussionApi.userHasPublicProfile(user.id).value map {
+          case Left(error) =>
+            println(s"DAPI LEFT ERROR: ${error.message}")
+            NotFound(views.html.errors._404())
+          case Right(false) =>
+            println(s"DAPI RIGHT no public profile - not commented")
+            NotFound(views.html.errors._404())
+          case Right(true) =>
+            println(s"DAPI RIGHT has public profile - commented")
+            val title = user.publicFields.username.fold("public profile")(username => s"$username's public profile")
+            implicit val identityPage: IdentityPage = IdentityPage(url, title, usesGuardianHeader = true)
+            Cached(60)(
+              RevalidatableResult.Ok(
+                IdentityHtmlPage.html(
+                  views.html.publicProfilePage(identityPage, idRequestParser(request), idUrlBuilder, user, activityType)
+                )
+              )
             )
-          )
-        )
+        }
+
     }
   }
+
 }
