7fb883af836dd95b9115ee9d36afe92ea7517c7b
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-01-17 15:11:45 +0000

    Raise audience percentage

--- static/src/javascripts/projects/common/modules/experiments/tests/commercial-cmp-ui-banner-modal.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/commercial-cmp-ui-banner-modal.js
@@ -6,7 +6,7 @@ export const commercialCmpUiBannerModal: ABTest = {
     expiry: '2020-01-28',
     author: 'George Haberis',
     description: '2% participation AB test for the new banner/modal CMP UI',
-    audience: 0.02,
+    audience: 0.03,
     audienceOffset: 0.8,
     successMeasure: 'Our new banner/modal CMP UI obtains target consent rates',
     audienceCriteria: 'n/a',
