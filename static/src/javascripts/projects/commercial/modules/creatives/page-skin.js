bd4b6a45488cf1baaa6a1b70985c17e9984faab2
Ioanna Kyprianou <ioanna.kyprianou.contractor@guardian.co.uk>
2020-01-23 14:49:59 +0200

    Create Truskin for use on Australian Fronts
    This contains:
     - Fabric and skin both seamless
     - Header and footer shrunk to middle content width
     - Scrolling matched to be seamless
    
    Co-authored-by: Josh Buckland <buck06191@gmail.com>

--- static/src/javascripts/projects/commercial/modules/creatives/page-skin.js
+++ static/src/javascripts/projects/commercial/modules/creatives/page-skin.js
@@ -10,6 +10,7 @@ const pageSkin = (): void => {
     const hasPageSkin: boolean = config.get('page.hasPageSkin');
 
     const isInAUEdition = config.get('page.edition', '').toLowerCase() === 'au';
+    const adLabelHeight = 24;
 
     let topPosition: number = 0;
 
@@ -40,19 +41,66 @@ const pageSkin = (): void => {
         }
     };
 
-    const initTopPositionOnce = (): void => {
+    const initTopPositionOnce = (hasTruskin: boolean): void => {
         if (topPosition === 0) {
             const navHeader = document.getElementsByClassName('new-header')[0];
             if (navHeader) {
-                topPosition = navHeader.offsetTop + navHeader.offsetHeight;
+                topPosition = hasTruskin
+                    ? navHeader.offsetTop + adLabelHeight
+                    : navHeader.offsetTop + navHeader.offsetHeight;
             }
         }
     };
 
-    // This is to reposition the Page Skin to start where the navigation header ends.
+    const shrinkElement = (element: HTMLElement | null): void => {
+        const frontContainer = document.querySelector('.fc-container__inner');
+        if (element && frontContainer) {
+            element.style.cssText = `max-width: ${
+                frontContainer.clientWidth
+            }px; margin-right: auto; margin-left: auto;`;
+        }
+    };
+
     const repositionSkin = (): void => {
-        if (hasPageSkin && isInAUEdition) {
-            initTopPositionOnce();
+        const bodyElement = document.querySelector('body');
+        const hasTruskin = bodyElement
+            ? bodyElement.classList.contains('truskin-page-skin')
+            : false;
+        const header = document.querySelector('.new-header');
+        const footer = document.querySelector('.l-footer');
+        const topBannerAd = document.querySelector('.ad-slot--top-banner-ad');
+
+        if (hasTruskin && header && topBannerAd) {
+            initTopPositionOnce(hasTruskin);
+
+            shrinkElement(header);
+            shrinkElement(footer);
+
+            if (window.pageYOffset === 0) {
+                moveBackgroundVerticalPosition(topPosition);
+            }
+
+            const headerBoundaries = header.getBoundingClientRect();
+            const topBannerAdBoundaries = topBannerAd.getBoundingClientRect();
+            const headerPosition = headerBoundaries.top;
+            const fabricScrollStartPosition =
+                topBannerAdBoundaries.height +
+                adLabelHeight -
+                headerBoundaries.height;
+
+            if (
+                headerPosition <= fabricScrollStartPosition &&
+                topBannerAdBoundaries.bottom > 0
+            ) {
+                moveBackgroundVerticalPosition(topBannerAdBoundaries.bottom);
+            } else if (topBannerAdBoundaries.bottom <= 0) {
+                moveBackgroundVerticalPosition(0);
+            }
+        }
+
+        // This is to reposition the Page Skin to start where the navigation header ends.
+        if (!hasTruskin && hasPageSkin && isInAUEdition) {
+            initTopPositionOnce(false);
             if (window.pageYOffset === 0) {
                 moveBackgroundVerticalPosition(topPosition);
             } else if (window.pageXOffset <= topPosition) {
