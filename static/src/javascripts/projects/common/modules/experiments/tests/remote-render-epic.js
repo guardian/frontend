5365acad88a8676b699b968dd3bc49683f49e951
Tom Wey <tom.wey@theguardian.com>
2020-01-30 16:11:30 +0000

    Fix some flow errors
    * Remove top level initVariant key
    * Add geolocation
    * test() shouldn’t return anything

--- static/src/javascripts/projects/common/modules/experiments/tests/remote-render-epic.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/remote-render-epic.js
@@ -7,58 +7,61 @@ import $ from 'lib/$';
 
 
 const test = {
-    initVariant: {
-        id: 'RemoteRenderEpic',
-        campaignId: 'gdnwb_copts_memco_remote_epic_test_api',
+    id: 'RemoteRenderEpic',
+    campaignId: 'gdnwb_copts_memco_remote_epic_test_api',
 
-        highPriority: true,
+    highPriority: true,
 
-        start: '2020-01-01',
-        expiry: '2020-02-29',
+    start: '2020-01-01',
+    expiry: '2020-02-29',
 
-        author: 'Nicolas Long',
-        description:
-            'A/B test local vs remote render of default epic, to validate Slot Machine approach and work to date',
-        successMeasure: 'Conversion rate',
-        idealOutcome: 'No difference between control and variant',
+    author: 'Nicolas Long',
+    description:
+        'A/B test local vs remote render of default epic, to validate Slot Machine approach and work to date',
+    successMeasure: 'Conversion rate',
+    idealOutcome: 'No difference between control and variant',
 
-        audienceCriteria: 'All',
-        audience: 1,
-        audienceOffset: 0,
+    audienceCriteria: 'All',
+    audience: 1,
+    audienceOffset: 0,
 
-        variants: [
-            {
-                id: 'control',
-                products: ['CONTRIBUTION', 'MEMBERSHIP_SUPPORTER'],
-                buttonTemplate: epicButtonsTemplate,
-            },
-            {
-                id: 'remote',
-                products: ['CONTRIBUTION', 'MEMBERSHIP_SUPPORTER'],
-                test: async () => {
-                    const api = 'https://contributions.guardianapis.com/epic';
-                    const response = await fetch(api, {})
+    geolocation: undefined,
+
+    variants: [
+        {
+            id: 'control',
+            products: ['CONTRIBUTION', 'MEMBERSHIP_SUPPORTER'],
+            buttonTemplate: epicButtonsTemplate,
+        },
+        {
+            id: 'remote',
+            products: ['CONTRIBUTION', 'MEMBERSHIP_SUPPORTER'],
+            test: () => {
+                const api = 'https://contributions.guardianapis.com/epic';
+
+                fetch(api, {}).then(response => {
                     if (response.ok) {
-                        const json = await response.json();
-                        const html = json.html;
-                        const css = json.css;
-                        const markup = `<style>${css}</style>${html}`;
-                        const component = $.create(markup);
+                        response.json().then(json => {
+                            const html = json.html;
+                            const css = json.css;
+                            const markup = `<style>${css}</style>${html}`;
+                            const component = $.create(markup);
 
-                        return fastdom.write(() => {
-                            const target = document.querySelector('.submeta')
+                            return fastdom.write(() => {
+                                const target = document.querySelector('.submeta')
 
-                            if (!target) {
-                                return;
-                            }
+                                if (!target) {
+                                    return;
+                                }
 
-                            component.insertBefore(target);
+                                component.insertBefore(target);
+                            });
                         });
                     }
-                }
-            },
-        ],
-    },
+                });
+            }
+        },
+    ],
 }
 
 
