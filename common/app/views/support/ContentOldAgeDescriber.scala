b18a0abfd12c7459b6076227d9ef865827e0b76c
Rob Phillips <robert.phillips@guardian.co.uk>
2020-02-04 12:09:30 +0000

    Removing age notice warning for one legally sensitive article

--- common/app/views/support/ContentOldAgeDescriber.scala
+++ common/app/views/support/ContentOldAgeDescriber.scala
@@ -1,37 +1,45 @@
 package views.support
 
+
 import java.util.concurrent.TimeUnit
 import implicits.Dates.CapiRichDateTime
 import model.pressed.{CuratedContent, PressedStory}
 import org.joda.time.DateTime
 
+
 object ContentOldAgeDescriber extends ContentOldAgeDescriber
 
 class ContentOldAgeDescriber {
+
+   private val excludePaths = Set("artanddesign/2019/jun/18/serpentine-galleries-chief-resigns")
+
   def apply(content: model.Content): String = {
-    message(Some(content.trail.webPublicationDate))
+    message(Some(content.trail.webPublicationDate), Some(content.metadata.id))
   }
 
   def apply(content: PressedStory): String = {
-    message(Some(content.trail.webPublicationDate))
+    message(Some(content.trail.webPublicationDate), Some(content.metadata.id))
   }
 
   def apply(content: model.ContentType): String = {
-    message(Some(content.trail.webPublicationDate))
+    message(Some(content.trail.webPublicationDate), Some(content.metadata.id))
   }
 
   def apply(apiContent: com.gu.contentapi.client.model.v1.Content): String = {
-    message(apiContent.webPublicationDate.map(_.toJoda))
+    message(apiContent.webPublicationDate.map(_.toJoda), Some(apiContent.id))
   }
 
   def apply(curatedContent: CuratedContent): String = {
-    message(curatedContent.card.webPublicationDateOption)
+    message(curatedContent.card.webPublicationDateOption, curatedContent.properties.maybeContentId)
   }
 
-  private def message(maybePubDate: Option[DateTime]) = {
+  private def message(maybePubDate: Option[DateTime], id:Option[String]) = {
     maybePubDate.map(pubDate => {
       val warnLimitDays = 30
-      if (pubDate.isBefore(DateTime.now().minusDays(warnLimitDays))) {
+      if(excludePaths.contains(id.getOrElse(""))){
+        ""
+      }
+       else if (pubDate.isBefore(DateTime.now().minusDays(warnLimitDays))) {
         val ageMillis = DateTime.now().getMillis - pubDate.getMillis
         val years = TimeUnit.MILLISECONDS.toDays(ageMillis) / 365
         val months = TimeUnit.MILLISECONDS.toDays(ageMillis) / 31
@@ -45,8 +53,8 @@ class ContentOldAgeDescriber {
         else if (weeks >= 2) s"$weeks weeks"
         else if (weeks > 0) s"$weeks week"
         else s"$days days"
-
-      } else {
+      }
+      else {
         ""
       }
     }).getOrElse("")
