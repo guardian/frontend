a8f7a2ee8ac8f0c334e7d7f861178c2d7a23849b
Nicolas Long <nicolas.long@theguardian.com>
2020-02-10 17:01:06 +0000

    Set remote render test to 0%
    The motivation here is that we need to turn the test switch 'On'
    to be able to test on PROD, but we want to limit the test to
    opt-in only initially to verify it is working as expected.
    
    Unfortunately, we don't have a good mechanism to opt-in currently
    unless the relevant switch is set to 'On'.

--- static/src/javascripts/projects/common/modules/experiments/tests/remote-render-epic.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/remote-render-epic.js
@@ -67,7 +67,7 @@ const remoteRenderTest = {
     idealOutcome: 'No difference between control and variant',
 
     audienceCriteria: 'All',
-    audience: 0.1,
+    audience: 0,
     audienceOffset: 0,
 
     geolocation,
