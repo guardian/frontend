4b220d4ccae2edc5e913fef354217db57181cf5a
Mahesh Makani <maheshmakani@gmail.com>
2020-02-12 12:00:57 +0000

    sign in gate test 0 percent

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -10,6 +10,7 @@ import { pangaeaAdapterTest } from 'common/modules/experiments/tests/commercial-
 import { amazonA9Test } from 'common/modules/experiments/tests/amazon-a9';
 import { subscriptionsBannerNewYearCopyTest } from 'common/modules/experiments/tests/subscriptions-banner-new-year-copy';
 import { remoteRenderEpic } from 'common/modules/experiments/tests/remote-render-epic';
+import { signInGateSecundus } from 'common/modules/experiments/tests/sign-in-gate-first-test';
 
 export const concurrentTests: $ReadOnlyArray<ABTest> = [
     commercialPrebidSafeframe,
@@ -19,6 +20,7 @@ export const concurrentTests: $ReadOnlyArray<ABTest> = [
     appnexusUSAdapter,
     pangaeaAdapterTest,
     subscriptionsBannerNewYearCopyTest,
+    signInGateSecundus,
 ];
 
 export const priorityEpicTest: EpicABTest = remoteRenderEpic;
