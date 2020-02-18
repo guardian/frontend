bd0a06116b3187ddee79484f4cb661c430825cb9
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2020-02-18 08:55:58 +0000

    Update FeatureSwitches.scala

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -204,7 +204,7 @@ trait FeatureSwitches {
     "If this is switched on then videos are enhanced using VideoJS",
     owners = Seq(Owner.withGithub("gtrufitt")),
     safeState = On,
-    sellByDate = new LocalDate(2020, 2, 19),
+    sellByDate = new LocalDate(2020, 3, 31),
     exposeClientSide = true
   )
 
