2361eebba90dd865785e21f3616b24af12f29ea3
Mahesh Makani <maheshmakani@gmail.com>
2020-02-25 17:17:09 +0000

    comment about adding variants

--- static/src/javascripts/projects/common/modules/identity/sign-in-gate/variants/index.js
+++ static/src/javascripts/projects/common/modules/identity/sign-in-gate/variants/index.js
@@ -3,4 +3,5 @@ import type { SignInGateVariant } from '../types';
 import { signInGateVariant as control } from './control';
 import { signInGateVariant as variant } from './variant';
 
+// to add a variant, first import the variant in the SignInGateVariant type, and then add to this exported array
 export const variants: Array<SignInGateVariant> = [control, variant];
