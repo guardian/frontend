673cb3eb0e0354aa71601b9dfe6499f3f54df154
George Haberis <george.haberis@guardian.co.uk>
2020-02-27 15:46:03 +0000

    remove file

--- static/src/javascripts/projects/commercial/modules/third-party-tags/twitter-pixel.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import config from 'lib/config';
-
-export const twitterPixel: () => ThirdPartyTag = () => ({
-    shouldRun: config.get('switches.twitterTrackingPixel', false),
-    url: '',
-    useImage: true,
-});
