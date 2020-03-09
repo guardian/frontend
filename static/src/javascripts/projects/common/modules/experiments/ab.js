5432c364f280841cbb3b3d404c790e0451d59e6d
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-09 16:22:32 +0000

    Wraps compare variant code to check if its Article

--- static/src/javascripts/projects/common/modules/experiments/ab.js
+++ static/src/javascripts/projects/common/modules/experiments/ab.js
@@ -86,32 +86,34 @@ export const getEpicTestToRun = memoize(
                 if (config.get('switches.compareVariantDecision')) {
                     // To evaluate the new contributions service logic we send it the actual decision so that it can
                     // compare this against what it *thinks* is the right decision, and log differences.
-
                     // send ~ one in ten to reduce initial volume
                     if (Math.random() < 0.1) {
                         const page = config.get('page');
-                        const countryCode = geolocationGetSync();
-                        compareVariantDecision({
-                            targeting: {
-                                contentType: page.contentType,
-                                sectionName: page.section,
-                                shouldHideReaderRevenue:
-                                    page.shouldHideReaderRevenue,
-                                isMinuteArticle: config.hasTone('Minute'),
-                                isPaidContent: page.isPaidContent,
-                                tags: buildKeywordTags(page),
-                                countryCode,
-                                showSupportMessaging: !shouldNotBeShownSupportMessaging(),
-                                isRecurringContributor: isRecurringContributor(),
-                                lastOneOffContributionDate: getLastOneOffContributionDate(),
-                                mvtId: getMvtValue(),
-                                epicViewLog: getViewLog(),
-                            },
-                            expectedTest: result ? result.id : '',
-                            expectedVariant: result
-                                ? result.variantToRun.id
-                                : '',
-                        });
+                        // Only compare variants for Epics served in Articles
+                        if (page.contentType === 'Article') {
+                            const countryCode = geolocationGetSync();
+                            compareVariantDecision({
+                                targeting: {
+                                    contentType: page.contentType,
+                                    sectionName: page.section,
+                                    shouldHideReaderRevenue:
+                                        page.shouldHideReaderRevenue,
+                                    isMinuteArticle: config.hasTone('Minute'),
+                                    isPaidContent: page.isPaidContent,
+                                    tags: buildKeywordTags(page),
+                                    countryCode,
+                                    showSupportMessaging: !shouldNotBeShownSupportMessaging(),
+                                    isRecurringContributor: isRecurringContributor(),
+                                    lastOneOffContributionDate: getLastOneOffContributionDate(),
+                                    mvtId: getMvtValue(),
+                                    epicViewLog: getViewLog(),
+                                },
+                                expectedTest: result ? result.id : '',
+                                expectedVariant: result
+                                    ? result.variantToRun.id
+                                    : '',
+                            });
+                        }
                     }
                 }
 
