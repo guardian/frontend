daeac85c191a1dc8cbeb9ef20020542e14c6843d
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-03-13 15:24:46 +0000

    Improve unit testing

--- static/src/javascripts/projects/commercial/modules/comscore.spec.js
+++ static/src/javascripts/projects/commercial/modules/comscore.spec.js
@@ -5,11 +5,6 @@ import { init, _ } from './comscore';
 const getComscoreScripTags = (): NodeList<HTMLElement> =>
     document.querySelectorAll('script#comscore');
 
-const isValidComscoreScriptTag = (tag: HTMLElement) =>
-    tag instanceof HTMLScriptElement &&
-    tag.src === _.comscoreSrc &&
-    tag.async === true;
-
 describe('comscore init', () => {
     beforeEach(() => {
         config.set('switches.comscore', true);
@@ -23,39 +18,63 @@ describe('comscore init', () => {
         expect(comscoreTags.length).toBe(0);
     });
 
-    it('should drop the corect script tag if the comscore switch is on', () => {
+    it('should drop exactly one script tag if the comscore switch is on', () => {
         config.set('switches.comscore', true);
         init();
 
         const comscoreTags = getComscoreScripTags();
         expect(comscoreTags.length).toBe(1);
-        expect(isValidComscoreScriptTag(comscoreTags[0])).toBe(true);
+    });
+
+    it('should drop a script tag with the correct src if the comscore switch is on', () => {
+        config.set('switches.comscore', true);
+        init();
+
+        const comscoreTags = getComscoreScripTags();
+
+        if (comscoreTags[0] instanceof HTMLScriptElement) {
+            expect(comscoreTags[0].src).toEqual(_.comscoreSrc);
+        } else {
+            throw new Error('Obtained comscore tag is not a script');
+        }
+    });
+
+    it('should drop a script tag with the correct async mode if the comscore switch is on', () => {
+        config.set('switches.comscore', true);
+        init();
+
+        const comscoreTags = getComscoreScripTags();
+
+        if (comscoreTags[0] instanceof HTMLScriptElement) {
+            expect(comscoreTags[0].async).toBe(true);
+        } else {
+            throw new Error('Obtained comscore tag is not a script');
+        }
     });
 });
 
 describe('comscore getGlobals', () => {
-    it('returns an object with the correct comscorekw variable set', () => {
-        const randomStr = Math.random()
-            .toString(36)
-            .substring(2);
+    it('return an object with the c1 and c2 properties correctly set when called with "Network Front" as keywords', () => {
+        const expectedGlobals = { c1: _.comscoreC1, c2: _.comscoreC2 };
+        expect(_.getGlobals('Network Front')).toMatchObject(expectedGlobals);
+    });
 
-        expect(_.getGlobals('')).toMatchObject({ comscorekw: '' });
-        expect(_.getGlobals(randomStr)).toMatchObject({
-            comscorekw: randomStr,
-        });
+    it('return an object with the c1 and c2 properties correctly set when called with non-"Network Front" as keywords', () => {
+        const expectedGlobals = { c1: _.comscoreC1, c2: _.comscoreC2 };
+        expect(_.getGlobals('')).toMatchObject(expectedGlobals);
     });
 
     it('returns an object with no comscorekw variable set when called with "Network Front" as keywords', () => {
-        expect(_.getGlobals('Network Front')).toEqual(
-            // $FlowFixMe property not does actually exist
-            expect.not.objectContaining({ comscorekw: expect.any(Object) })
-        );
+        const comscoreGlobals = Object.keys(_.getGlobals('Network Front'));
+        expect(comscoreGlobals).not.toContain('comscorekw');
     });
 
-    it('always return an object with the c1 and c2 variables', () => {
-        const expectedGlobals = { c1: _.comscoreC1, c2: _.comscoreC2 };
+    it('returns an object with the correct comscorekw variable set', () => {
+        const keywords = 'These are the best keywords The greatest!';
 
-        expect(_.getGlobals('')).toMatchObject(expectedGlobals);
-        expect(_.getGlobals('Network Front')).toMatchObject(expectedGlobals);
+        expect(_.getGlobals('')).toMatchObject({ comscorekw: '' });
+        expect(_.getGlobals(keywords)).toMatchObject({
+            comscorekw: keywords,
+        });
     });
 });
