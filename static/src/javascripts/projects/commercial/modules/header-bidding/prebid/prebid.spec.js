edec044285201c13c70d00f44b5440bc1f96b2b9
damien-shaw-guardian <damien.shaw.freelancer@guardian.co.uk>
2020-03-17 12:09:59 +0000

    fix: prebid tests
    The prebid tests where failing because the jest test-url in package.json was changed, the fix changes the test urls in prebid.spec

--- static/src/javascripts/projects/commercial/modules/header-bidding/prebid/prebid.spec.js
+++ static/src/javascripts/projects/commercial/modules/header-bidding/prebid/prebid.spec.js
@@ -59,7 +59,7 @@ describe('initialise', () => {
             _disableAjaxTimeout: false,
             _mediaTypePriceGranularity: {},
             _priceGranularity: 'custom',
-            _publisherDomain: 'http://localhost',
+            _publisherDomain: 'http://test-url.com',
             _sendAllBids: true,
             _timeoutBuffer: 400,
             _useBidCache: false,
@@ -87,7 +87,7 @@ describe('initialise', () => {
             enableSendAllBids: true,
             mediaTypePriceGranularity: {},
             priceGranularity: 'custom',
-            publisherDomain: 'http://localhost',
+            publisherDomain: 'http://test-url.com',
             s2sConfig: {
                 adapter: 'prebidServer',
                 adapterOptions: {},
