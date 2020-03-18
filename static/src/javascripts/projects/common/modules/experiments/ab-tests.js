513932112dafd3021b9e2f16ac29f2886b205d8b
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-03-18 12:55:34 +0000

    Remove ab test import

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -2,7 +2,6 @@
 import { commercialPrebidSafeframe } from 'common/modules/experiments/tests/commercial-prebid-safeframe.js';
 import { askFourEarning } from 'common/modules/experiments/tests/contributions-epic-ask-four-earning';
 import { acquisitionsEpicAlwaysAskIfTagged } from 'common/modules/experiments/tests/acquisitions-epic-always-ask-if-tagged';
-import { adblockTest } from 'common/modules/experiments/tests/adblock-ask';
 import { articlesViewedBanner } from 'common/modules/experiments/tests/contribs-banner-articles-viewed';
 import { xaxisAdapterTest } from 'common/modules/experiments/tests/commercial-xaxis-adapter';
 import { appnexusUSAdapter } from 'common/modules/experiments/tests/commercial-appnexus-us-adapter';
@@ -17,7 +16,6 @@ import { commercialGptLazyLoad } from 'common/modules/experiments/tests/commerci
 
 export const concurrentTests: $ReadOnlyArray<ABTest> = [
     commercialPrebidSafeframe,
-    adblockTest,
     amazonA9Test,
     connatixTest,
     xaxisAdapterTest,
