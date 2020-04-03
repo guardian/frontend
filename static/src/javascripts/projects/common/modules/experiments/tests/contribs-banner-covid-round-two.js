edd7186edb70a13b45d19af3419842014f088f67
Joshua <joshuathomaslieberman@gmail.com>
2020-04-03 15:32:31 +0100

    updat

--- static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-covid-round-two.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-covid-round-two.js
@@ -14,17 +14,17 @@ const ctaText = 'Contribute';
 const secondaryLinkLabel = isUSorAU ? undefined : `Subscribe`;
 
 // Variant-specific parameters:
-const controlLeadSentence = `Thanks to your support we can continue to provide our vital reporting – in times of crisis and beyond. `;
-const accuracyLeadSentence = `Thanks to your support we can continue to provide accurate, independent news and calm explanation. `;
-const openLeadSentence = `Thanks to your support we can continue to provide quality, independent reporting that’s open to readers across the world. `;
+const controlLeadSentence = `Thanks to your support, we can continue to provide our vital reporting – in times of crisis and beyond. `;
+const powerLeadSentence = `Support from our readers allows us to deliver vital, fact-based reporting – in times of crisis and beyond. `;
+const independenceLeadSentence = `Thanks to your support, we can remain independent and keep reporting with accuracy and clarity. `;
 
 const controlMessageText = `Millions of readers across the world are visiting the Guardian every day for open, independent, accurate journalism. Trusted news has never been so important, and neither has your support.`;
-const accuracyMessageText = `No matter how uncertain the future feels, you can rely on us. Quality journalism can help us all make critical decisions about our lives, health and security – based on fact, not fiction. Trusted news has never been so important, and neither has your support.`;
-const openMessageText = `We keep our journalism free from a paywall because we believe everyone deserves equal access to accurate news and calm explanation. Trusted news has never been so important, and neither has your support.`;
+const powerMessageText = `Millions of readers across the world visit the Guardian every day for open, independent journalism that holds power to account. Trusted news has never been more crucial, and neither has your support.`;
+const independenceMessageText = `Now more than ever, the Guardian’s independence matters. Free from commercial or political bias, we bring you fact-checked news and trustworthy commentary each and every day. In these unprecedented times, independent reporting has never been so vital.`;
 
-export const contributionsCovidBannerRoundOne: AcquisitionsABTest = {
-    id: 'ContributionsCovidBannerRoundOne',
-    campaignId: 'covid_banner_1',
+export const contributionsCovidBannerRoundTwo: AcquisitionsABTest = {
+    id: 'ContributionsCovidBannerRoundTwo',
+    campaignId: 'covid_banner_2',
     start: '2020-03-26',
     expiry: '2020-06-26',
     author: 'Joshua Lieberman',
@@ -55,12 +55,12 @@ export const contributionsCovidBannerRoundOne: AcquisitionsABTest = {
             },
         },
         {
-            id: 'accuracy',
+            id: 'power',
             test: (): void => {},
             engagementBannerParams: {
                 titles,
-                messageText: accuracyMessageText,
-                leadSentence: accuracyLeadSentence,
+                messageText: powerMessageText,
+                leadSentence: powerLeadSentence,
                 ctaText,
                 template: acquisitionsBannerCovidTemplate,
                 hasTicker: true,
@@ -70,12 +70,12 @@ export const contributionsCovidBannerRoundOne: AcquisitionsABTest = {
             },
         },
         {
-            id: 'open',
+            id: 'independence',
             test: (): void => {},
             engagementBannerParams: {
                 titles,
-                messageText: openMessageText,
-                leadSentence: openLeadSentence,
+                messageText: independenceMessageText,
+                leadSentence: independenceLeadSentence,
                 ctaText,
                 template: acquisitionsBannerCovidTemplate,
                 hasTicker: true,
