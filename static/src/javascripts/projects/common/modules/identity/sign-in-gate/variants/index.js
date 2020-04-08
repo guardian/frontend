b2bacb469f467e218a5de6c4087c409cef21771e
Mahesh Makani <maheshmakani@gmail.com>
2020-04-08 09:58:53 +0100

    add scale to variants array for sign in gate

--- static/src/javascripts/projects/common/modules/identity/sign-in-gate/variants/index.js
+++ static/src/javascripts/projects/common/modules/identity/sign-in-gate/variants/index.js
@@ -4,10 +4,12 @@ import type { SignInGateVariant } from '../types';
 // import { signInGateVariant as example } from './example';
 import { signInGateVariant as control } from './control';
 import { signInGateVariant as variant } from './variant';
+import { signInGateVariant as scale } from './scale';
 
 // to add a variant, first import the variant in the SignInGateVariant type, and then add to this exported array
 export const variants: Array<SignInGateVariant> = [
     // example,
     control,
     variant,
+    scale,
 ];
