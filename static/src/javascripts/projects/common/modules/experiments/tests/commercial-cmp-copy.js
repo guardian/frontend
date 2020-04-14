5c10d99b9f22e66f6d8bdd78dc33624043a38bdc
Alex Sanders <a@snd.rs>
2020-04-14 11:17:38 +0100

    Create commercial-cmp-copy.js

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/commercial-cmp-copy.js
@@ -0,0 +1,28 @@
+// @flow strict
+
+import { shouldShow } from '@guardian/consent-management-platform';
+
+export const commercialCmpCopy: ABTest = {
+    id: 'CommercialCmpCopy',
+    start: '2020-04-14',
+    expiry: '2020-04-21',
+    author: 'Alex Sanders',
+    description: '1% a/b test for new CMP copy',
+    audience: 0.01,
+    audienceOffset: 0.0,
+    successMeasure: 'Consent rates do not drop',
+    audienceCriteria: 'n/a',
+    dataLinkNames: 'n/a',
+    idealOutcome: 'Consent rates do not drop',
+    canRun: shouldShow,
+    variants: [
+        {
+            id: 'control',
+            test: (): void => {},
+        },
+        {
+            id: 'variant',
+            test: (): void => {},
+        },
+    ],
+};
