b8e7ed8442eb819adc22f532ca1b5e3cf388053c
Pascal Honoré <pascal@alseyn.net>
2020-04-27 17:00:18 +0100

    Remove useless imports

--- common/app/model/content/Atom.scala
+++ common/app/model/content/Atom.scala
@@ -1,11 +1,8 @@
 package model.content
 
-import com.gu.contentapi.client.model.{v1 => contentapi}
-import com.gu.contentatom.renderer.{ArticleConfiguration, AudioSettings}
 import com.gu.contentatom.thrift.atom.media.{Asset => AtomApiMediaAsset, MediaAtom => AtomApiMediaAtom}
 import com.gu.contentatom.thrift.atom.timeline.{TimelineItem => TimelineApiItem}
 import com.gu.contentatom.thrift.{AtomData, Atom => AtomApiAtom, Image => AtomApiImage, ImageAsset => AtomApiImageAsset, atom => atomapi}
-import conf.Configuration
 import enumeratum._
 import model.{ImageAsset, ImageMedia, ShareLinkMeta}
 import org.apache.commons.lang3.time.DurationFormatUtils
