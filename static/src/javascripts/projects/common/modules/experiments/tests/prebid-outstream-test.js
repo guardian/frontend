067673f3abe73ba9e59e2c91674e23a9cf17fbd2
Francis Carr <francis.carr@guardian.co.uk>
2020-04-28 15:24:40 +0100

    Add Prebid outstream ab-test

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/prebid-outstream-test.js
@@ -0,0 +1,22 @@
+// @flow strict
+
+export const prebidOutstream: ABTest = {
+    id: 'prebid-outstream',
+    start: '2020-04-28',
+    expiry: '2020-05-31',
+    author: 'Francis Carr',
+    description: 'Test Prebid outstream with Ozone in inline1 (This enables it for all partners)',
+    audience: 0.0,
+    audienceOffset: 0.0,
+    successMeasure: 'We receive outstream video from Ozone and potentially other partners',
+    audienceCriteria: 'n/a',
+    dataLinkNames: 'n/a',
+    idealOutcome: 'Outstream works in inline1 alone with normal display ads',
+    canRun: () => true,
+    variants: [
+        {
+            id: 'variant',
+            test: (): void => {},
+        },
+    ],
+};
