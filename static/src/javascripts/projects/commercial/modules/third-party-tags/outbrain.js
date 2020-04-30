3eb79fcb74ae64a2aec63c5996a161aa7e318301
Pascal Honoré <pascal@alseyn.net>
2020-04-30 10:31:08 +0100

    Simplify result of getOutbrainComplianceTargeting in projects/commercial/modules/third-party-tags/outbrain.js

--- static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
@@ -1,45 +1,8 @@
 // @flow strict
-import { adblockInUse } from 'lib/detect';
-import { waitForCheck } from 'common/modules/check-mediator';
-
-type OutbrainPageConditions = {
-    noMerchSlotsExpected: boolean,
-    contributionsTestVisible: boolean,
-};
-
-const noMerchSlotsExpected = (): Promise<boolean> =>
-    // Loading Outbrain is dependent on successful return of high relevance component
-    // from DFP. AdBlock is blocking DFP calls so we are not getting any response and thus
-    // not loading Outbrain. As Outbrain is being partially loaded behind the adblock we can
-    // make the call instantly when we detect adBlock in use.
-    adblockInUse.then(
-        isUse =>
-            !document.getElementById('dfp-ad--merchandising-high') || !!isUse
-    );
-
-// These are the on-page conditions which influence the outbrain load. We don't need to wait for DFP to find the value of these.
-const getOutbrainPageConditions = (): Promise<OutbrainPageConditions> =>
-    Promise.all([
-        noMerchSlotsExpected(),
-        waitForCheck('isUserInContributionsAbTest'),
-    ]).then(([noMerchSlots, contributions]) => ({
-        noMerchSlotsExpected: noMerchSlots,
-        contributionsTestVisible: contributions,
-    }));
 
 export const getOutbrainComplianceTargeting = (): Promise<
     Map<string, string>
-> =>
-    getOutbrainPageConditions().then(pageConditions => {
-        if (
-            pageConditions.contributionsTestVisible
-        ) {
-            // This key value should be read as "the outbrain load cannot be compliant"
-            // (it could be non-compliant, or not loaded at all).
-            return new Map([['outbrain-compliant', 'false']]);
-        }
-        return new Map();
-    });
+> => Promise.resolve(new Map());
 
 /*
  Outbrain loading behaviour follows the truth table below. This module uses a number of checks
