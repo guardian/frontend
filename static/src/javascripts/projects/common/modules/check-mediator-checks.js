80495f821cbf5740d4824da7e1d2ff83e9a56134
Pascal Honoré <pascal@alseyn.net>
2020-04-30 08:33:37 +0100

    Remove outbrain entries in projects/common/modules/check-mediator-checks.js

--- static/src/javascripts/projects/common/modules/check-mediator-checks.js
+++ static/src/javascripts/projects/common/modules/check-mediator-checks.js
@@ -1,13 +1,9 @@
 // @flow
 
 export const checks = [
-    'isOutbrainDisabled',
     'isUserInContributionsAbTest',
     'isUserNotInContributionsAbTest',
     'hasHighPriorityAdLoaded',
     'hasLowPriorityAdLoaded',
     'hasLowPriorityAdNotLoaded',
-    'isOutbrainBlockedByAds',
-    'isOutbrainMerchandiseCompliant',
-    'isOutbrainMerchandiseCompliantOrBlockedByAds',
 ];
