ac0a29ef1503487390349568ecf9f234eec3b07d
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2020-04-30 15:28:40 +0100

    Add facia-scala-client special reports

--- article/app/services/dotcomponents/ArticlePicker.scala
+++ article/app/services/dotcomponents/ArticlePicker.scala
@@ -64,7 +64,16 @@ object ArticlePageChecks {
     "business/series/undercover-in-the-chicken-industry",
     "business/series/britains-debt-timebomb",
     "world/series/this-is-europe",
-    "environment/series/the-polluters"
+    "environment/series/the-polluters",
+    "news/series/hsbc-files",
+    "news/series/panama-papers",
+    "us-news/homan-square",
+    "uk-news/series/the-new-world-of-work",
+    "world/series/the-new-arrivals",
+    "news/series/nauru-files",
+    "us-news/series/counted-us-police-killings",
+    "australia-news/series/healthcare-in-detention",
+    "society/series/this-is-the-nhs"
   )
 
   def isNotImmersive(page: PageWithStoryPackage): Boolean = ! page.item.isImmersive
