8c01b94337edd41a5866409bb456f7850cf43d24
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2020-05-18 15:46:04 +0100

    Reduce Sentry sampling to 0.8%

--- static/src/javascripts/lib/raven.js
+++ static/src/javascripts/lib/raven.js
@@ -57,7 +57,7 @@ const sentryOptions = {
         const isIgnored =
             typeof data.tags.ignored !== 'undefined' && data.tags.ignored;
         const { enableSentryReporting } = config.get('switches');
-        const isInSample = Math.random() < 0.01; // 1%
+        const isInSample = Math.random() < 0.008; // 0.8%
 
         if (isDev && !isIgnored) {
             // Some environments don't support or don't always expose the console Object
