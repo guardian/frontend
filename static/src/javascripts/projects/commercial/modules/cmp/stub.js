54b31a1cfdb8cf0aee37d03b1cab443178b68824
Alex Sanders <a@snd.rs>
2020-05-18 17:27:39 +0100

    put CMP stub back behind switch

--- static/src/javascripts/projects/commercial/modules/cmp/stub.js
+++ static/src/javascripts/projects/commercial/modules/cmp/stub.js
@@ -1,82 +1,87 @@
-/* eslint-disable no-underscore-dangle */
-
 // @flow
-try {
-    (function stubCMP(document, window) {
-        if (!window.__cmp) {
-            window.__cmp = (function CMPSetup() {
-                const listen = window.attachEvent || window.addEventListener;
-                listen(
-                    'message',
-                    event => {
-                        window.__cmp.receiveMessage(event);
-                    },
-                    false
-                );
+/* eslint-disable no-underscore-dangle */
+import config from 'lib/config';
 
-                const locatorFrame = document.createElement('iframe');
-                locatorFrame.style.display = 'none';
-                locatorFrame.name = '__cmpLocator';
-                locatorFrame.setAttribute('aria-hidden', 'true');
+if (config.get('switches.enableConsentManagementService')) {
+    try {
+        (function stubCMP(document, window) {
+            if (!window.__cmp) {
+                window.__cmp = (function CMPSetup() {
+                    const listen =
+                        window.attachEvent || window.addEventListener;
+                    listen(
+                        'message',
+                        event => {
+                            window.__cmp.receiveMessage(event);
+                        },
+                        false
+                    );
 
-                const addLocatorFrame = () => {
-                    if (!window.frames.__cmpLocator) {
-                        if (document.body !== null) {
-                            document.body.appendChild(locatorFrame);
-                        } else {
-                            setTimeout(addLocatorFrame, 5);
+                    const locatorFrame = document.createElement('iframe');
+                    locatorFrame.style.display = 'none';
+                    locatorFrame.name = '__cmpLocator';
+                    locatorFrame.setAttribute('aria-hidden', 'true');
+
+                    const addLocatorFrame = () => {
+                        if (!window.frames.__cmpLocator) {
+                            if (document.body !== null) {
+                                document.body.appendChild(locatorFrame);
+                            } else {
+                                setTimeout(addLocatorFrame, 5);
+                            }
                         }
-                    }
-                };
-                addLocatorFrame();
+                    };
+                    addLocatorFrame();
 
-                const commandQueue = [];
-                const cmp = function CMP(command, parameter, callback) {
-                    if (command === 'ping') {
-                        if (callback) {
-                            callback({
-                                gdprAppliesGlobally: !!(
-                                    window.__cmp &&
-                                    window.__cmp.config &&
-                                    // this isn't actually our config object
-                                    // eslint-disable-next-line guardian-frontend/no-direct-access-config
-                                    window.__cmp.config.storeConsentGlobally
-                                ),
-                                cmpLoaded: false,
+                    const commandQueue = [];
+                    const cmp = function CMP(command, parameter, callback) {
+                        if (command === 'ping') {
+                            if (callback) {
+                                callback({
+                                    gdprAppliesGlobally: !!(
+                                        window.__cmp &&
+                                        window.__cmp.config &&
+                                        // this isn't actually our config object
+                                        // eslint-disable-next-line guardian-frontend/no-direct-access-config
+                                        window.__cmp.config.storeConsentGlobally
+                                    ),
+                                    cmpLoaded: false,
+                                });
+                            }
+                        } else {
+                            commandQueue.push({
+                                command,
+                                parameter,
+                                callback,
                             });
                         }
-                    } else {
-                        commandQueue.push({
-                            command,
-                            parameter,
-                            callback,
-                        });
-                    }
-                };
-                cmp.commandQueue = commandQueue;
-                cmp.receiveMessage = function CMPreceiveMessage(event) {
-                    const data = event && event.data && event.data.__cmpCall;
-                    if (data) {
-                        commandQueue.push({
-                            callId: data.callId,
-                            command: data.command,
-                            parameter: data.parameter,
-                            event,
-                        });
-                    }
-                };
-                cmp.config = {
-                    storeConsentGlobally: false,
-                    storePublisherData: false,
-                    logging: false,
-                    gdprApplies: true,
-                };
-                return cmp;
-            })();
+                    };
+                    cmp.commandQueue = commandQueue;
+                    cmp.receiveMessage = function CMPreceiveMessage(event) {
+                        const data =
+                            event && event.data && event.data.__cmpCall;
+                        if (data) {
+                            commandQueue.push({
+                                callId: data.callId,
+                                command: data.command,
+                                parameter: data.parameter,
+                                event,
+                            });
+                        }
+                    };
+                    cmp.config = {
+                        storeConsentGlobally: false,
+                        storePublisherData: false,
+                        logging: false,
+                        gdprApplies: true,
+                    };
+                    return cmp;
+                })();
+            }
+        })(document, window);
+    } catch (e) {
+        if (process.env.NODE_ENV !== 'production') {
+            throw e;
         }
-    })(document, window);
-} catch (e) {
-    if (process.env.NODE_ENV !== 'production') {
-        throw e;
     }
 }
