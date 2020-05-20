fecd82667e4f8daaba6b53c0c430f542a6372dc4
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-05-20 15:37:03 +0100

    Run Redplanet only in AUS and NZ regions

--- static/src/javascripts/projects/commercial/modules/dfp/redplanet.js
+++ static/src/javascripts/projects/commercial/modules/dfp/redplanet.js
@@ -3,43 +3,46 @@
 import config from 'lib/config';
 import {commercialFeatures} from "common/modules/commercial/commercial-features";
 import {onIabConsentNotification} from "@guardian/consent-management-platform";
+import {isInAuRegion} from "commercial/modules/header-bidding/utils";
 
 const initialise = (): void => {
-    onIabConsentNotification(state => {
-        const consentState =
-            state[1] && state[2] && state[3] && state[4] && state[5];
+    // Initialise Launchpad Tracker
+    window.launchpad('newTracker', 'launchpad', 'lpx.qantas.com', {
+        discoverRootDomain: true,
+        appId: 'the-guardian'
+    });
 
-        if (consentState) {
-            // Initialise Launchpad Tracker
-            window.launchpad('newTracker', 'launchpad', 'lpx.qantas.com', {
-                discoverRootDomain: true,
-                appId: 'the-guardian'
-            });
-
-            // Track Page Views
-            window.launchpad('trackUnstructEvent', {
-                'schema': 'iglu:com.qantas.launchpad/hierarchy/jsonschema/1-0-0',
-                'data': {
-                    'u1': '{site-name}', // optional
-                    'u2':  config.get('page.section'), // optional site-section
-                    'u3': '{sub-section}', // optional sub-section
-                    'u4': config.get('page.contentType'), // optional, eg: article, index, video
-                    'uid': 'bwid?' // optional. If you have a logged in user-id, set the value here OPHAN BROWSER ID
-                }
-            });
+    // Track Page Views
+    window.launchpad('trackUnstructEvent', {
+        'schema': 'iglu:com.qantas.launchpad/hierarchy/jsonschema/1-0-0',
+        'data': {
+            'u1': '{site-name}', // optional
+            'u2':  config.get('page.section'), // optional site-section
+            'u3': '{sub-section}', // optional sub-section
+            'u4': config.get('page.contentType'), // optional, eg: article, index, video
+            'uid': 'bwid?' // optional. If you have a logged in user-id, set the value here OPHAN BROWSER ID
         }
     });
 };
 
 const setupRedplanet: () => Promise<void> = () => {
-    if (commercialFeatures.launchpad) {
-        return import('lib/launchpad.js').then(() => {
-            initialise();
-            return Promise.resolve();
-        });
-    }
+    onIabConsentNotification(state => {
+        const consentState =
+            state[1] && state[2] && state[3] && state[4] && state[5];
 
+        if (consentState) {
+            return import('lib/launchpad.js').then(() => {
+                initialise();
+                return Promise.resolve();
+            });
+        }
+    });
     return Promise.resolve();
 };
 
-export const init = (): Promise<void> => setupRedplanet();
+export const init = (): Promise<void> => {
+    if (commercialFeatures.launchpad && isInAuRegion()) {
+        return setupRedplanet();
+    }
+    return Promise.resolve();
+}
