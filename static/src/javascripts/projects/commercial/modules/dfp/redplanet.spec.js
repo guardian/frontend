602188747872fa3894478bfdd5d8e37076a45026
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-05-21 17:23:56 +0100

    Added unit tests to redplanet

---
+++ static/src/javascripts/projects/commercial/modules/dfp/redplanet.spec.js
@@ -0,0 +1,93 @@
+// @flow
+
+import { commercialFeatures } from 'common/modules/commercial/commercial-features';
+import { onIabConsentNotification as onIabConsentNotification_ } from '@guardian/consent-management-platform';
+import { isInAuRegion as isInAuRegion_ } from 'commercial/modules/header-bidding/utils';
+import { init } from './redplanet';
+
+const onIabConsentNotification: any = onIabConsentNotification_;
+const isInAuRegion: any = isInAuRegion_;
+
+const trueConsentMock = (callback): void =>
+    callback({ '1': true, '2': true, '3': true, '4': true, '5': true });
+
+const falseConsentMock = (callback): void =>
+    callback({ '1': true, '2': true, '3': true, '4': true, '5': false });
+
+
+jest.mock('common/modules/commercial/commercial-features', () => ({
+    commercialFeatures: {},
+}));
+
+jest.mock('commercial/modules/dfp/Advert', () =>
+    jest.fn().mockImplementation(() => ({ advert: jest.fn() }))
+);
+
+jest.mock('commercial/modules/header-bidding/utils');
+
+jest.mock('common/modules/experiments/ab', () => ({
+    isInVariantSynchronous: jest.fn(),
+}));
+
+jest.mock('lib/cookies', () => ({
+    getCookie: jest.fn(),
+}));
+
+
+jest.mock('lib/launchpad', () => jest.fn());
+
+jest.mock('common/modules/commercial/build-page-targeting', () => ({
+    buildPageTargeting: jest.fn(),
+}));
+
+jest.mock('lib/load-script', () => ({
+    loadScript: () => Promise.resolve(),
+}));
+
+jest.mock('@guardian/consent-management-platform', () => ({
+    onIabConsentNotification: jest.fn(),
+}));
+
+window.launchpad = jest.fn().mockImplementationOnce(() => jest.fn());
+
+describe('init', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should initialise redplanet when all conditions are true', async () => {
+        commercialFeatures.launchpad = true;
+        isInAuRegion.mockReturnValue(true);
+        onIabConsentNotification.mockImplementation(trueConsentMock);
+        await init();
+        expect(window.launchpad).toBeCalled();
+    });
+
+    it('should not initialise redplanet when user consented false', async () => {
+        commercialFeatures.launchpad = true;
+        isInAuRegion.mockReturnValue(true);
+        onIabConsentNotification.mockImplementation(falseConsentMock);
+        await init();
+        expect(window.launchpad).not.toBeCalled();
+    });
+
+    it('should not initialise redplanet when launchpad conditions are false', async () => {
+        commercialFeatures.launchpad = false;
+        isInAuRegion.mockReturnValue(true);
+        onIabConsentNotification.mockImplementation(trueConsentMock);
+        await init();
+        expect(window.launchpad).not.toBeCalled();
+    });
+
+    it('should not initialise redplanet when user not in AUS regions', async () => {
+        commercialFeatures.launchpad = true;
+        isInAuRegion.mockReturnValue(false);
+        onIabConsentNotification.mockImplementation(trueConsentMock);
+        await init();
+        expect(window.launchpad).not.toBeCalled();
+    });
+});
