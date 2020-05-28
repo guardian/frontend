1cca6ac512546cc8889dd4ae09fc8e5d3cd1e78a
Michael Jacobson <contact@michaeljacobson.co.uk>
2020-05-28 10:08:57 +0100

    add hover styling to article count span

--- static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-articles-viewed-opt-out.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-articles-viewed-opt-out.js
@@ -14,7 +14,8 @@ const articleCountWeeks = 26;
 const articleViewCount = getArticleViewCountForWeeks(articleCountWeeks);
 const geolocation = geolocationGetSync();
 const leadSentence = 'We chose a different approach. Will you support it?'
-const messageText = `With news under threat, just when we need it most, the Guardian’s quality, fact-checked news and measured explanation has never mattered more. Our editorial independence is vital. We believe every one of us deserves to read honest reporting – that’s why we remain with you, open to all. And you’re visiting in your millions. You’ve read more than ${articleViewCount - 1} articles in the last six months. But at this crucial moment, advertising revenue is plummeting. We need you to help fill the gap. Every contribution, however big or small, is valuable – in times of crisis and beyond.`
+const articlesRead = `<span class="engagement-banner__articles-read">${articleViewCount - 1} articles</span>`
+const messageText = `With news under threat, just when we need it most, the Guardian’s quality, fact-checked news and measured explanation has never mattered more. Our editorial independence is vital. We believe every one of us deserves to read honest reporting – that’s why we remain with you, open to all. And you’re visiting in your millions. You’ve read more than ${articlesRead} articles in the last six months. But at this crucial moment, advertising revenue is plummeting. We need you to help fill the gap. Every contribution, however big or small, is valuable – in times of crisis and beyond.`
 const ctaText = `<span class="engagement-banner__highlight"> Support The Guardian from as little as ${getLocalCurrencySymbol(geolocation)}1.</span>`;
 
 export const contributionsBannerArticlesViewedOptOut: AcquisitionsABTest = {
