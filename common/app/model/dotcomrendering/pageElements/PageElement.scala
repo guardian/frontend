d0af113c129de81c50e69daf1e79402b01703d0d
Pascal Honoré <pascal@alseyn.net>
2020-05-29 17:22:26 +0100

    Enable ExplainerAtomBlockElement

--- common/app/model/dotcomrendering/pageElements/PageElement.scala
+++ common/app/model/dotcomrendering/pageElements/PageElement.scala
@@ -140,6 +140,7 @@ object PageElement {
       case _: AudioAtomBlockElement => true
       case _: VideoBlockElement => true
       case _: ContentAtomBlockElement => true
+      case _: ExplainerAtomBlockElement => true
 
       // TODO we should quick fail here for these rather than pointlessly go to DCR
       case table: TableBlockElement if table.isMandatory.exists(identity) => true
