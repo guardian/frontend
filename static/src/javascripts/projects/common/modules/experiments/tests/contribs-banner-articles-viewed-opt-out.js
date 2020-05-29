90096ab2a9f74e81e864425876d734ab3f66e6e6
Michael Jacobson <contact@michaeljacobson.co.uk>
2020-05-29 14:01:39 +0100

    add control variant to a/b test

--- static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-articles-viewed-opt-out.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/contribs-banner-articles-viewed-opt-out.js
@@ -19,6 +19,7 @@ const geolocation = geolocationGetSync();
 const leadSentence = 'We chose a different approach. Will you support it?'
 const articlesRead = articlesReadTooltipMarkup(articleViewCount)
 const messageText = `With news under threat, just when we need it most, the Guardian’s quality, fact-checked news and measured explanation has never mattered more. Our editorial independence is vital. We believe every one of us deserves to read honest reporting – that’s why we remain with you, open to all. And you’re visiting in your millions. You’ve read more than ${articlesRead} in the last six months. But at this crucial moment, advertising revenue is plummeting. We need you to help fill the gap. Every contribution, however big or small, is valuable – in times of crisis and beyond.`
+const controlMessageText = `With news under threat, just when we need it most, the Guardian’s quality, fact-checked news and measured explanation has never mattered more. Our editorial independence is vital. We believe every one of us deserves to read honest reporting – that’s why we remain with you, open to all. And you’re visiting in your millions. You’ve read more than ${articleViewCount.toString()} in the last six months. But at this crucial moment, advertising revenue is plummeting. We need you to help fill the gap. Every contribution, however big or small, is valuable – in times of crisis and beyond.`
 const ctaText = `<span class="engagement-banner__highlight"> Support The Guardian from as little as ${getLocalCurrencySymbol(geolocation)}1.</span>`;
 
 export const contributionsBannerArticlesViewedOptOut: AcquisitionsABTest = {
@@ -44,6 +45,16 @@ export const contributionsBannerArticlesViewedOptOut: AcquisitionsABTest = {
     componentType: 'ACQUISITIONS_ENGAGEMENT_BANNER',
     geolocation,
     variants: [
+        {
+          id: 'control',
+            test: (): void => {},
+            engagementBannerParams: {
+              leadSentence,
+                controlMessageText,
+                ctaText,
+                template: acquisitionsBannerControlTemplate
+            }
+        },
         {
             id: 'variant',
             test: (): void => {},
