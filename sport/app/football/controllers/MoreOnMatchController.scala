067578dca36d84367f3eb7d538392d3c3cf5ff5f
Pascal Honoré <pascal@alseyn.net>
2020-06-12 02:06:07 +0100

    MoreOnMatchController DCR Update

--- sport/app/football/controllers/MoreOnMatchController.scala
+++ sport/app/football/controllers/MoreOnMatchController.scala
@@ -7,12 +7,12 @@ import feed.CompetitionsService
 import football.model.FootballMatchTrail
 import implicits.{Football, Requests}
 import model.Cached.{RevalidatableResult, WithoutRevalidationResult}
-import model.{Cached, Content, ContentType, NoCache}
+import model.{Cached, Competition, Content, ContentType, NoCache, TeamColours}
 import org.joda.time.{DateTime, Days, LocalDate}
 import org.joda.time.format.DateTimeFormat
 import com.github.nscala_time.time.Imports
 import com.github.nscala_time.time.Imports._
-import pa.FootballMatch
+import pa.{FootballMatch, LineUp, LineUpTeam, MatchDayTeam}
 import play.api.libs.json._
 import play.api.mvc._
 import play.twirl.api.Html
@@ -34,6 +34,93 @@ case class MatchNav(
   lazy val hasMinByMin = minByMin.isDefined
   lazy val hasReport = matchReport.isDefined
   lazy val hasPreview = preview.isDefined
+}
+
+/*
+  Date: 12th June 2020
+  Author: Pascal
+
+  For the moment I am essentially reproducing here what I did for MatchController.scala
+  If the two sets of classes turn our to be the same (or one an extension of the other) then I will move
+  them to a single model file.
+ */
+
+sealed trait Ns1Answer
+case class Event1Answer(eventTime: String, eventType: String) extends Ns1Answer
+case class Player1Answer(id: String, name: String, position: String, lastName: String, substitute: Boolean, timeOnPitch: String, shirtNumber: String, events: Seq[EventAnswer]) extends Ns1Answer
+case class Team1Answer(
+   id: String,
+   name: String,
+   players: Seq[Player1Answer],
+   score: Int,
+   scorers: List[String],
+   possession: Int,
+   shotsOn: Int,
+   shotsOff: Int,
+   corners: Int,
+   fouls: Int,
+   colours: String,
+   crest: String
+) extends Ns1Answer
+case class Competition1Answer(fullName: Option[String]) extends Ns1Answer
+case class MatchData1Answer(
+  id: String,
+  isResult: Boolean,
+  homeTeam: Team1Answer,
+  awayTeam: Team1Answer,
+  competition: Competition1Answer,
+  isLive: Boolean,
+  venue: String,
+  comments: String
+) extends Ns1Answer
+
+object Ns1Answer {
+  val reportedEventTypes = List("booking", "dismissal", "substitution")
+  def make1Players(team: LineUpTeam): Seq[Player1Answer] = {
+    team.players.map{ player =>
+      val events = player.events.filter(event => NsAnswer.reportedEventTypes.contains(event.eventType)).map { event =>
+        EventAnswer(event.eventTime, event.eventType)
+      }
+      Player1Answer(player.id, player.name, player.position, player.lastName, player.substitute, player.timeOnPitch, player.shirtNumber, events)
+    }
+  }
+  def makeTeamAnswer(teamV1: MatchDayTeam, teamV2: LineUpTeam, teamPossession: Int, teamColour: String): Team1Answer = {
+    val players = make1Players(teamV2)
+    Team1Answer(
+      teamV1.id,
+      teamV1.name,
+      players = players,
+      score = teamV1.score.getOrElse(0),
+      scorers = teamV1.scorers.fold(Nil: List[String])(_.split(",").map(scorer => {
+        scorer.replace("(", "").replace(")", "")}).toList),
+      possession = teamPossession,
+      shotsOn = teamV2.shotsOn,
+      shotsOff = teamV2.shotsOff,
+      corners = teamV2.corners,
+      fouls = teamV2.fouls,
+      colours = teamColour,
+      crest = s"${Configuration.staticSport.path}/football/crests/120/${teamV1.id}.png"
+    )
+  }
+  def makeFromFootballMatch(theMatch: FootballMatch, lineUp: LineUp, competition: Option[Competition], isResult: Boolean, isLive: Boolean): MatchData1Answer = {
+    val teamColours = TeamColours(lineUp.homeTeam, lineUp.awayTeam)
+    MatchData1Answer(
+      id = theMatch.id,
+      isResult = isResult,
+      homeTeam = makeTeamAnswer(theMatch.homeTeam, lineUp.homeTeam, lineUp.homeTeamPossession, teamColours.home),
+      awayTeam = makeTeamAnswer(theMatch.awayTeam, lineUp.awayTeam, lineUp.awayTeamPossession, teamColours.away),
+      competition = Competition1Answer(competition.map(_.fullName)),
+      isLive = isLive,
+      venue = theMatch.venue.map(_.name).getOrElse(""),
+      comments = theMatch.comments.getOrElse("")
+    )
+  }
+
+  implicit val EventAnswerWrites: Writes[Event1Answer] = Json.writes[Event1Answer]
+  implicit val PlayerAnswerWrites: Writes[Player1Answer] = Json.writes[Player1Answer]
+  implicit val TeamAnswerWrites: Writes[Team1Answer] = Json.writes[Team1Answer]
+  implicit val CompetitionAnswerWrites: Writes[Competition1Answer] = Json.writes[Competition1Answer]
+  implicit val MatchDataAnswerWrites: Writes[MatchData1Answer] = Json.writes[MatchData1Answer]
 }
 
 class MoreOnMatchController(
@@ -59,40 +146,15 @@ class MoreOnMatchController(
         case _ => None
       }.getOrElse("")
 
-
       lazy val competition = competitionsService.competitionForMatch(theMatch.id)
 
-      related map { _ filter hasExactlyTwoTeams } map { filtered =>
+      for {
+        filtered <- related map { _ filter hasExactlyTwoTeams }
+        lineup <- competitionsService.getLineup(theMatch)
+      } yield {
         Cached(if(theMatch.isLive) 10 else 300) {
           if (request.forceDCR) {
-            JsonComponent(
-              "id" -> theMatch.id,
-              "isLive" -> theMatch.isLive,
-              "isResult" -> theMatch.isResult,
-              "comments" -> theMatch.comments.getOrElse(""),
-              "venue" -> theMatch.venue.map(_.name).getOrElse(""),
-              "homeTeam" -> Json.obj(
-                "name" -> theMatch.homeTeam.name,
-                "id" -> theMatch.homeTeam.id,
-                "score" -> theMatch.homeTeam.score,
-                "crest" -> s"${Configuration.staticSport.path}/football/crests/120/${theMatch.homeTeam.id}.png",
-                "scorers" -> theMatch.homeTeam.scorers.getOrElse("").split(",").map(scorer => {
-                  scorer.replace("(", "").replace(")", "")
-                })
-              ),
-              "awayTeam" -> Json.obj(
-                "name" -> theMatch.awayTeam.name,
-                "id" -> theMatch.awayTeam.id,
-                "score" -> theMatch.awayTeam.score,
-                "crest" -> s"${Configuration.staticSport.path}/football/crests/120/${theMatch.awayTeam.id}.png",
-                "scorers" -> theMatch.awayTeam.scorers.getOrElse("").split(",").map(scorer => {
-                  scorer.replace("(", "").replace(")", "")
-                })
-              ),
-              "competition" -> Json.obj(
-                "fullName" -> competition.map(_.fullName)
-              )
-            )
+            JsonComponent(Json.toJson(Ns1Answer.makeFromFootballMatch(theMatch, lineup, competition, theMatch.isResult, theMatch.isLive)))
           } else {
             JsonComponent(
               "nav" -> football.views.html.fragments.matchNav(populateNavModel(theMatch, filtered)),
