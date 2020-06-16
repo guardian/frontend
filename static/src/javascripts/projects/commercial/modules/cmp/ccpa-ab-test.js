41077e7fe6d6e05c8257f199ed69854a7b5db3a3
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-06-16 12:59:38 +0100

    Test using tests.ccpaCmp

--- static/src/javascripts/projects/commercial/modules/cmp/ccpa-ab-test.js
+++ static/src/javascripts/projects/commercial/modules/cmp/ccpa-ab-test.js
@@ -1,13 +1,14 @@
 // @flow
 // import { isInUsa } from 'common/modules/commercial/geo-utils';
+import config from 'lib/config';
 
 // let isInTest;
 
-// const isInServerSideTest = (): boolean => true;
+const isInServerSideTest = (): boolean => config.get('tests.ccpaCmp', false);
 
 // export const isInCcpaTest = (): boolean => {
 //     isInTest = isInTest || (isInUsa() && isInServerSideTest());
 //     return isInTest;
 // };
 
-export const isInCcpaTest = (): boolean => true;
+export const isInCcpaTest = (): boolean => isInServerSideTest();
