28535c30e0dc92c1742b0a68a843a157c21c4b22
Jon Soul <jonathan.soul@guardian.co.uk>
2020-06-16 20:02:21 +0100

    Disable placeholder banner from showing

--- static/src/javascripts/projects/common/modules/commercial/reader-revenue-banner.js
+++ static/src/javascripts/projects/common/modules/commercial/reader-revenue-banner.js
@@ -10,12 +10,20 @@ const show = (): Promise<boolean> => {
     renderBanner(data);
 };
 
-const canShow = (): Promise<boolean> =>
-    fetchBannerData()
+const canShow = (): Promise<boolean> => {
+    const enabled = false;
+
+    // Temporarily disable banner while service is developed to serve actual banner and logic
+    if(!enabled) {
+        return Promise.resolve(false);
+    }
+
+    return fetchBannerData()
         .then(moduleData => {
             data = moduleData;
             return data !== null;
         });
+};
 
 
 export const readerRevenueBanner: Banner = {
