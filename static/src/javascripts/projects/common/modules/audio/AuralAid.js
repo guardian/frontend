8d447ab280fb1c15043e8de27e375d91f125b03c
Jon Soul <jonathan.soul@guardian.co.uk>
2020-06-17 11:41:35 +0100

    Remove temporary any

--- static/src/javascripts/projects/common/modules/audio/AuralAid.js
+++ static/src/javascripts/projects/common/modules/audio/AuralAid.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 // $FlowFixMe
-const VisuallyHidden: any = styled('span')({
+const VisuallyHidden = styled('span')({
     height: '1px',
     overflow: 'hidden',
     position: 'absolute',
