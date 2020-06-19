7136644fcb34532f3b1a3a4d9e4630216471ca36
Jon Soul <jonathan.soul@guardian.co.uk>
2020-06-19 08:47:43 +0100

    Transpile dynamic import module

--- webpack.config.js
+++ webpack.config.js
@@ -92,10 +92,10 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /(\.js)|(\.mjs)$/,
                 // TODO: @guardian/dotcom-rendering is not properly published or pre-transpiled, so we have to
                 // transpile it as part of the frontend build step for now
-                exclude: /(node_modules(?!\/@guardian\/dotcom-rendering)|vendor\/)/,
+                exclude: /(node_modules\/(?!(@guardian\/dotcom-rendering)|(dynamic-import-polyfill))|vendor\/)/,
                 loader: 'babel-loader',
             },
             {
