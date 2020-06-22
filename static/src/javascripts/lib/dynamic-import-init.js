10a01afd52983f9c3bd9cc3f5d9cd37b285f5950
Jon Soul <jonathan.soul@guardian.co.uk>
2020-06-22 09:38:57 +0100

    Don't load shimport as well!

--- static/src/javascripts/lib/dynamic-import-init.js
+++ static/src/javascripts/lib/dynamic-import-init.js
@@ -39,7 +39,7 @@ export const init = (): void => {
 
     if (window.guardian.supportsModules) {
         initialiseDynamicImport();
+    } else {
+        initialiseDynamicImportLegacy();
     }
-
-    initialiseDynamicImportLegacy();
 };
