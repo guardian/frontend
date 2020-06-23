82b99d4bcdbf233540889226bf38b3ddd7e98b89
Tom Forbes <tom.forbes@theguardian.com>
2020-06-23 13:47:29 +0100

    switch

--- static/src/javascripts/projects/common/modules/commercial/reader-revenue-banner.js
+++ static/src/javascripts/projects/common/modules/commercial/reader-revenue-banner.js
@@ -2,6 +2,7 @@
 
 import type { Banner } from 'common/modules/ui/bannerPicker';
 import { fetchBannerData, renderBanner, type BannerDataResponse } from 'common/modules/commercial/contributions-service';
+import config from "lib/config";
 
 
 const messageCode = 'reader-revenue-banner';
@@ -11,10 +12,9 @@ let data: ?BannerDataResponse = null;
 const show = () => data ? renderBanner(data) : Promise.resolve(false);
 
 const canShow = (): Promise<boolean> => {
-    const enabled = true;
+    const enabled = config.get('switches.remoteBanner');
 
-    // Temporarily disable banner while service is developed to serve actual banner and logic
-    if(!enabled) {
+    if (!enabled) {
         return Promise.resolve(false);
     }
 
