ddc8765ba8496edade93776a3448324ed8db7157
Pascal Honoré <pascal@alseyn.net>
2020-06-29 00:25:55 +0100

    Refactor to remove potential for exceptions

--- common/app/model/dotcomrendering/pageElements/CalloutExtraction.scala
+++ common/app/model/dotcomrendering/pageElements/CalloutExtraction.scala
@@ -2,7 +2,9 @@ package model.dotcomrendering.pageElements
 
 import org.jsoup.Jsoup
 import play.api.libs.json._
+
 import scala.collection.JavaConverters._
+import scala.util.Try
 
 case class CalloutFormField(id: String, name: String, description: String, required: Boolean, hideLabel: Boolean, label: String)
 object CalloutFormField {
@@ -13,45 +15,50 @@ object CalloutExtraction {
   private def extractCampaignPerTagName(campaigns: JsValue, tagName: String): Option[JsObject] = {
     val campaigns2 = campaigns.asInstanceOf[JsArray].value
     campaigns2
-      .filter(c => ( c \ "fields" \ "tagName" ).as[String] == tagName)
+      .filter(c => ( c \ "fields" \ "tagName" ).asOpt[String].getOrElse("") == tagName)
       .map(_.asInstanceOf[JsObject]).headOption
   }
 
-  private def formFieldItemToCalloutFormField( item: JsValue ) : CalloutFormField = {
-    val id = (item \ "id").as[String]
-    val name = (item \ "name").as[String]
-    val description = (item \ "name").as[String]
-    val required: Boolean = (item \ "required").as[String] == "1"
-    val hideLabel: Boolean = (item \ "hide_label").as[String] == "1"
-    val label = (item \ "name").as[String]
-    CalloutFormField(id, name, description, required, hideLabel, label)
+  private def formFieldItemToCalloutFormField( item: JsValue ) : Option[CalloutFormField] = {
+    for {
+      id          <- (item \ "id").asOpt[String]
+      name        <- (item \ "name").asOpt[String]
+      description <- (item \ "name").asOpt[String]
+      required    <- (item \ "required").asOpt[String]
+      hideLabel   <- (item \ "hide_label").asOpt[String]
+      label       <- (item \ "name").asOpt[String]
+    } yield {
+      CalloutFormField(id, name, description, required == "1", hideLabel == "1", label)
+    }
   }
 
   private def campaignJsObjectToCalloutBlockElement(campaign: JsObject): Option[CalloutBlockElement] = {
-    val id = (campaign \ "id").as[String]
-    val activeFrom = (campaign \ "activeFrom").as[Long]
-    val displayOnSensitive = (campaign \ "displayOnSensitive").as[Boolean]
-    val formId = (campaign \ "fields" \ "formId").as[Int]
-    val title = (campaign \ "name").as[String]
-    val description = (campaign \ "fields" \ "description").as[String]
-    val tagName = (campaign \ "fields" \ "tagName").as[String]
-    val formFields1 = (campaign \ "fields" \ "formFields").get.as[JsArray]
-    val formFields2 = formFields1
-      .value
-      .map(formFieldItemToCalloutFormField(_))
-      .toList
-    val element = CalloutBlockElement(id, activeFrom, displayOnSensitive, formId, title, description, tagName, formFields2)
-    Some(element)
+    for {
+      id                 <- (campaign \ "id").asOpt[String]
+      activeFrom         <- (campaign \ "activeFrom").asOpt[Long]
+      displayOnSensitive <- (campaign \ "displayOnSensitive").asOpt[Boolean]
+      formId             <- (campaign \ "fields" \ "formId").asOpt[Int]
+      title              <- (campaign \ "name").asOpt[String]
+      description        <- (campaign \ "fields" \ "description").asOpt[String]
+      tagName            <- (campaign \ "fields" \ "tagName").asOpt[String]
+      formFields1        <- (campaign \ "fields" \ "formFields").asOpt[JsArray]
+    } yield {
+      val formFields2 = formFields1
+        .value
+        .flatMap(formFieldItemToCalloutFormField(_))
+        .toList
+      CalloutBlockElement(id, activeFrom, displayOnSensitive, formId, title, description, tagName, formFields2)
+    }
   }
 
   def extractCallout(html: String, campaigns: Option[JsValue]): Option[CalloutBlockElement] = {
     val doc = Jsoup.parseBodyFragment(html)
     val tagName = doc.getElementsByTag("div").asScala.headOption.map(_.attr("data-callout-tagname"))
     for {
-      name <- tagName
-      cpgs <- campaigns
+      name     <- tagName
+      cpgs     <- campaigns
       campaign <- extractCampaignPerTagName(cpgs, name)
-      element <- campaignJsObjectToCalloutBlockElement(campaign)
+      element  <- campaignJsObjectToCalloutBlockElement(campaign)
     } yield {
       element
     }
