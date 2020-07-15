27cfbcfe0a8a5e9dcf6aa1593a664c87e45dc2e8
Tom Wey <tom.wey@theguardian.com>
2020-07-15 17:29:11 +0100

    Dynamically load the web sdk

--- static/src/javascripts/projects/commercial/modules/braze.js
+++ static/src/javascripts/projects/commercial/modules/braze.js
@@ -1,39 +1,40 @@
 // @flow
 
 import { onIabConsentNotification } from '@guardian/consent-management-platform';
-import appboy from '@braze/web-sdk';
 
 const apiKey = 'XXX';
 const brazeUuid = 'XXX';
 let didAlreadyRun = false;
 
 export const init = (): Promise<any> => {
-    console.log("Initializing Braze", appboy);
+    console.log("Initializing Braze");
 
     onIabConsentNotification(state => {
-        console.log("consent state", state)
+        console.log("consent state", state);
         const canRun = !didAlreadyRun && state[1] && state[2] && state[3] && state[4] && state[5];
 
         if (canRun) {
-            didAlreadyRun = true;
+            import(/* webpackChunkName: "braze-web-sdk" */ '@braze/web-sdk').then(appboy => {
+                didAlreadyRun = true;
 
-            appboy.initialize(apiKey, {
-                enableLogging: true,
-                noCookies: true,
-                baseUrl: 'https://sdk.fra-01.braze.eu/api/v3',
-                enableHtmlInAppMessages: true
+                appboy.initialize(apiKey, {
+                    enableLogging: true,
+                    noCookies: true,
+                    baseUrl: 'https://sdk.fra-01.braze.eu/api/v3',
+                    enableHtmlInAppMessages: true
+                });
+
+                const f = function (configuration) {
+                    console.log(configuration);
+                    appboy.display.showInAppMessage(configuration);
+                    return true;
+                }
+
+                appboy.subscribeToInAppMessage(f);
+
+                appboy.changeUser(brazeUuid);
+                appboy.openSession();
             });
-
-            const f = function (configuration) {
-                console.log(configuration);
-                appboy.display.showInAppMessage(configuration);
-                return true;
-            }
-
-            appboy.subscribeToInAppMessage(f);
-
-            appboy.changeUser(brazeUuid);
-            appboy.openSession();
         }
     })
 
