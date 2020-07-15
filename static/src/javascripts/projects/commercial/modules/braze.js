28aab4cc81fadb429d0bbfbe0ce411f93083b466
Tom Wey <tom.wey@theguardian.com>
2020-07-15 16:12:16 +0100

    Naive integration with CMP

--- static/src/javascripts/projects/commercial/modules/braze.js
+++ static/src/javascripts/projects/commercial/modules/braze.js
@@ -1,30 +1,41 @@
 // @flow
 
+import { onIabConsentNotification } from '@guardian/consent-management-platform';
 import appboy from '@braze/web-sdk';
 
 const apiKey = 'XXX';
 const brazeUuid = 'XXX';
+let didAlreadyRun = false;
 
-export const init = () => {
+export const init = (): Promise<any> => {
     console.log("Initializing Braze", appboy);
 
-    appboy.initialize(apiKey, {
-        enableLogging: true,
-        noCookies: true,
-        baseUrl: 'https://sdk.fra-01.braze.eu/api/v3',
-        enableHtmlInAppMessages: true
-    });
+    onIabConsentNotification(state => {
+        console.log("consent state", state)
+        const canRun = !didAlreadyRun && state[1] && state[2] && state[3] && state[4] && state[5];
 
-    const f = function(configuration){
-        console.log(configuration);
-        appboy.display.showInAppMessage(configuration);
-        return true;
-    }
+        if (canRun) {
+            didAlreadyRun = true;
 
-    appboy.subscribeToInAppMessage(f);
+            appboy.initialize(apiKey, {
+                enableLogging: true,
+                noCookies: true,
+                baseUrl: 'https://sdk.fra-01.braze.eu/api/v3',
+                enableHtmlInAppMessages: true
+            });
 
-    appboy.changeUser(brazeUuid);
-    appboy.openSession();
+            const f = function (configuration) {
+                console.log(configuration);
+                appboy.display.showInAppMessage(configuration);
+                return true;
+            }
+
+            appboy.subscribeToInAppMessage(f);
+
+            appboy.changeUser(brazeUuid);
+            appboy.openSession();
+        }
+    })
 
     return Promise.resolve();
 }
