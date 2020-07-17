6a7f094dda89d81434ab285a7218ede3af015dbb
Mahesh Makani <maheshmakani@gmail.com>
2020-07-17 14:25:26 +0100

    remove unused import

--- static/src/javascripts/projects/common/modules/identity/sign-in-gate/variants/main/control.js
+++ static/src/javascripts/projects/common/modules/identity/sign-in-gate/variants/main/control.js
@@ -1,5 +1,5 @@
 // @flow
-import type { CurrentABTest, SignInGateVariant } from '../../types';
+import type { SignInGateVariant } from '../../types';
 import { componentName } from '../../component';
 import {
     hasUserDismissedGate,
