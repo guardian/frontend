fe1885490505cdfeac1f9d8ca6629b3bc9ef7b50
Max Duval <max@mxdvl.com>
2020-07-20 17:18:32 +0100

    add sourcepoint IDs to third-party scripts

--- static/src/javascripts/projects/commercial/modules/third-party-tags/facebook-pixel.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/facebook-pixel.js
@@ -6,5 +6,6 @@ export const fbPixel: () => ThirdPartyTag = () => ({
     url: `https://www.facebook.com/tr?id=${config.get(
         'libs.facebookAccountId'
     )}&ev=PageView&noscript=1`,
+    sourcepointId: '5e7e1298b8e05c54a85c52d2',
     useImage: true,
 });
