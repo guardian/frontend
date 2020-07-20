10eabab3261d41be3ad071b68a1453f9d6da594d
Pascal Honoré <pascal@alseyn.net>
2020-07-20 12:56:20 +0100

    Use the correct DocumentBlockElement

--- article/app/services/dotcomponents/ArticlePicker.scala
+++ article/app/services/dotcomponents/ArticlePicker.scala
@@ -4,8 +4,8 @@ import controllers.ArticlePage
 import experiments.{ActiveExperiments, Control, DCRBubble, DotcomRendering1, DotcomRendering2, Excluded, Experiment, Participant}
 import model.PageWithStoryPackage
 import implicits.Requests._
-import model.liveblog.{BlockElement, ContentAtomBlockElement, ImageBlockElement, InstagramBlockElement, PullquoteBlockElement, RichLinkBlockElement, TableBlockElement, TextBlockElement, TweetBlockElement}
-import model.dotcomrendering.pageElements.{DocumentBlockElement, SoundcloudBlockElement, VideoVimeoBlockElement, VideoYoutubeBlockElement, VideoFacebookBlockElement}
+import model.liveblog.{BlockElement, ContentAtomBlockElement, DocumentBlockElement, ImageBlockElement, InstagramBlockElement, PullquoteBlockElement, RichLinkBlockElement, TableBlockElement, TextBlockElement, TweetBlockElement}
+import model.dotcomrendering.pageElements.{SoundcloudBlockElement, VideoVimeoBlockElement, VideoYoutubeBlockElement, VideoFacebookBlockElement}
 import play.api.mvc.RequestHeader
 import views.support.Commercial
 
