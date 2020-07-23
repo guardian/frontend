c4d383cca97b9ef305becebfebbe4a7d00d26d25
Rhys Mills <rhystmills@gmail.com>
2020-07-23 14:59:01 +0100

    Retrieve Braze UUID from Identity, and wrap existing code in a callback for identity request.

--- static/src/javascripts/projects/commercial/modules/braze.js
+++ static/src/javascripts/projects/commercial/modules/braze.js
@@ -2,9 +2,10 @@
 
 import { onIabConsentNotification } from '@guardian/consent-management-platform';
 import config from 'lib/config';
+import { getUserFromApi } from '../../common/modules/identity/api';
 
-const brazeUuid = 'XXX';
 let didAlreadyRun = false;
+
 
 export const init = (): Promise<any> => {
     const brazeSwitch = config.get('switches.brazeSwitch');
@@ -12,35 +13,38 @@ export const init = (): Promise<any> => {
     const apiKey = config.get('page.brazeApiKey');
     if (!apiKey) return Promise.reject(new Error('Braze API key not set.'));
     console.log("Initializing Braze");
+    getUserFromApi(user => {
+        const brazeUuid = user.privateFields.brazeUuid;
+        onIabConsentNotification(state => {
+            console.log("consent state", state);
+            const canRun = !didAlreadyRun && state[1] && state[2] && state[3] && state[4] && state[5];
 
-    onIabConsentNotification(state => {
-        console.log("consent state", state);
-        const canRun = !didAlreadyRun && state[1] && state[2] && state[3] && state[4] && state[5];
+            if (canRun) {
+                import(/* webpackChunkName: "braze-web-sdk" */ '@braze/web-sdk').then(appboy => {
+                    didAlreadyRun = true;
 
-        if (canRun) {
-            import(/* webpackChunkName: "braze-web-sdk" */ '@braze/web-sdk').then(appboy => {
-                didAlreadyRun = true;
+                    appboy.initialize(apiKey, {
+                        enableLogging: true,
+                        noCookies: true,
+                        baseUrl: 'https://sdk.fra-01.braze.eu/api/v3',
+                        enableHtmlInAppMessages: true
+                    });
 
-                appboy.initialize(apiKey, {
-                    enableLogging: true,
-                    noCookies: true,
-                    baseUrl: 'https://sdk.fra-01.braze.eu/api/v3',
-                    enableHtmlInAppMessages: true
+                    const f = function (configuration) {
+                        console.log(configuration);
+                        appboy.display.showInAppMessage(configuration);
+                        return true;
+                    }
+
+                    appboy.subscribeToInAppMessage(f);
+
+                    appboy.changeUser(brazeUuid);
+                    appboy.openSession();
                 });
+            }
+        })
+    });
 
-                const f = function (configuration) {
-                    console.log(configuration);
-                    appboy.display.showInAppMessage(configuration);
-                    return true;
-                }
-
-                appboy.subscribeToInAppMessage(f);
-
-                appboy.changeUser(brazeUuid);
-                appboy.openSession();
-            });
-        }
-    })
 
     return Promise.resolve();
 }
