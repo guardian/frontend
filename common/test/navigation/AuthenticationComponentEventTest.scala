0aa54c10464e0e99acce346bb95d842a24fe14ea
Peter Colley <zvvvvt@gmail.com>
2020-07-27 09:49:03 +0100

    fix AuthenticationComponentEventTest

--- common/test/navigation/AuthenticationComponentEventTest.scala
+++ common/test/navigation/AuthenticationComponentEventTest.scala
@@ -6,7 +6,7 @@ import AuthenticationComponentEvent._
 class AuthenticationComponentEventTest extends FlatSpec with Matchers {
 
   "createAuthenticationComponentEventTuple" should "create a component event tuple with the parameters passed" in {
-    createAuthenticationComponentEventTuple(SigninHeaderId) shouldBe "componentEventParams" -> "componentType%3DIDENTITY_AUTHENTICATION%26componentId%3Dguardian_signin_header"
+    createAuthenticationComponentEventTuple(SigninHeaderId) shouldBe "componentEventParams" -> "componentType=IDENTITY_AUTHENTICATION&componentId=guardian_signin_header"
   }
 
   "createAuthenticationComponentEventParams" should "create component event parameters with the parameters passed" in {
