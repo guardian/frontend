2788a516d5d0ce06e22d140901b9bbbbb64e8641
Pascal Honoré <pascal@alseyn.net>
2020-07-27 14:00:47 +0100

    Correct the series url

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -72,7 +72,7 @@ object Badges {
   val auGreenRecovery = Badge("australia-news/series/the-green-recovery", Static("images/badges/green-recovery.svg"))
   val greenRecovery = Badge("environment/series/the-green-recovery", Static("images/badges/green-recovery.svg"))
   val culturePeril = Badge("culture/series/culture-in-peril", Static("images/badges/culture-badge.svg"))
-  val oneHundredDays = Badge("culture/series/climate-countdown", Static("images/badges/100days.svg"))
+  val oneHundredDays = Badge("us-news/series/climate-countdown", Static("images/badges/100days.svg"))
 
   val allBadges = Seq(newArrivals, brexitGamble, roadToTheVote, brexitFrontline, brexitDividedGenerations,
     brexitHowItCameToThis, londonVersus, beyondTheBlade, euElection, paradisePapers, cambridgeAnalytica, specialReport,
