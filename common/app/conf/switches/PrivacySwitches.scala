f06b10bd60676616897ef7c5af16d30d33134246
Alex Sanders <a@snd.rs>
2020-08-17 15:50:04 +0100

    create privacy switches

---
+++ common/app/conf/switches/PrivacySwitches.scala
@@ -0,0 +1,39 @@
+package conf.switches
+
+import conf.switches.Expiry.never
+import conf.switches.Owner.group
+import conf.switches.SwitchGroup.{Privacy}
+import org.joda.time.LocalDate
+
+trait PrivacySwitches {
+
+  val Cmp = Switch(
+    SwitchGroup.Privacy,
+    "consent-management",
+    "Enable consent management. Individual frameworks will also need to be switched on.",
+    owners = group(Commercial),
+    safeState = On,
+    sellByDate = never,
+    exposeClientSide = true
+  )
+
+  val TCFv2 = Switch(
+    SwitchGroup.Privacy,
+    "framework-tcfv2",
+    "Enable the TCFv2 framework (if consent-management is on).",
+    owners = group(Commercial),
+    safeState = On,
+    sellByDate = never,
+    exposeClientSide = true
+  )
+
+  val CCPA = Switch(
+    SwitchGroup.Privacy,
+    "framework-ccpa",
+    "Enable the CCPA framework (if consent-management is on).",
+    owners = group(Commercial),
+    safeState = On,
+    sellByDate = never,
+    exposeClientSide = true
+  )
+}
