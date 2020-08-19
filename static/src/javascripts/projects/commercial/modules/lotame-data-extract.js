edc78ace1f8ce2c5d49c48595d00d4a67fc75504
Max Duval <max@mxdvl.com>
2020-08-19 12:30:35 +0100

    update lotame-data-extract

--- static/src/javascripts/projects/commercial/modules/lotame-data-extract.js
+++ static/src/javascripts/projects/commercial/modules/lotame-data-extract.js
@@ -2,7 +2,6 @@
 import { commercialFeatures } from 'common/modules/commercial/commercial-features';
 import { loadScript } from 'lib/load-script';
 import config from 'lib/config';
-import { shouldUseSourcepointCmp } from 'commercial/modules/cmp/sourcepoint';
 import { onConsentChange } from '@guardian/consent-management-platform';
 
 const SOURCEPOINT_ID: string = '5ed6aeb1b8e05c241a63c71f';
@@ -23,7 +22,8 @@ const shouldLoadLotame = (): boolean => {
 let initialised: boolean = false;
 
 // Fetches Lotame Data for the Ozone project
-const loadLotamePromise = (): Promise<void>  => loadScript('//ad.crwdcntrl.net/5/c=13271/pe=y/var=OzoneLotameData')
+const loadLotamePromise = (): Promise<void> =>
+    loadScript('//ad.crwdcntrl.net/5/c=13271/pe=y/var=OzoneLotameData')
         .then(() => {
             if (config.get('isDotcomRendering', false)) {
                 // We do not need the LOTCC initialization for dotcom-rendering Ad Free
@@ -35,36 +35,35 @@ const loadLotamePromise = (): Promise<void>  => loadScript('//ad.crwdcntrl.net/5
                 return Promise.reject(Error('No LOTCC in window'));
             }
         })
-        .catch(errorHandler)
+        .catch(errorHandler);
 
 // and stores in in window.OzoneLotameData
 const init = (): Promise<void> => {
     if (!shouldLoadLotame || commercialFeatures.isSecureContact) {
         return Promise.resolve();
     }
-    if (shouldUseSourcepointCmp()) {
-        onConsentChange(state => {
-            let canRun: boolean = false;
-            if (state.tcfv2) {
-                // TCFv2 mode
-                if (
-                    typeof state.tcfv2.vendorConsents[SOURCEPOINT_ID] !== 'undefined'
-                ) {
-                    canRun = state.tcfv2.vendorConsents[SOURCEPOINT_ID];
-                } else {
-                    canRun = Object.values(state.tcfv2.consents).every(Boolean);
-                }
-            }
 
-            if (!initialised && canRun) {
-                initialised = true;
-                return loadLotamePromise();
+    onConsentChange(state => {
+        let canRun: boolean = false;
+        if (state.tcfv2) {
+            // TCFv2 mode
+            if (
+                typeof state.tcfv2.vendorConsents[SOURCEPOINT_ID] !==
+                'undefined'
+            ) {
+                canRun = state.tcfv2.vendorConsents[SOURCEPOINT_ID];
+            } else {
+                canRun = Object.values(state.tcfv2.consents).every(Boolean);
             }
-            return Promise.resolve();
-        });
-    } else {
-        return loadLotamePromise();
-    }
+        }
+
+        if (!initialised && canRun) {
+            initialised = true;
+            return loadLotamePromise();
+        }
+        return Promise.resolve();
+    });
+
     return Promise.resolve();
 };
 
