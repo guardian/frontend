c7240e1f20f1e86acc590c13fe7dd16469b6585d
Pascal Honoré <pascal@alseyn.net>
2020-09-01 17:03:07 +0100

    Better email headline extraction

--- facia/app/controllers/front/FrontHeadline.scala
+++ facia/app/controllers/front/FrontHeadline.scala
@@ -2,6 +2,7 @@ package controllers.front
 
 import common.Logging
 import model.Cached.{RevalidatableResult, WithoutRevalidationResult}
+import model.facia.PressedCollection
 import model.{Cached, PressedPage}
 import play.api.mvc.Results
 
@@ -11,12 +12,18 @@ object FrontHeadline extends Results with Logging {
     NotFound("Could not extract headline from front"),
   )
 
+  def collectionIsSuitableForHeadlineExtraction(collection: PressedCollection): Boolean = {
+    collection.curatedPlusBackfillDeduplicated.headOption match {
+      case None                 => false
+      case Some(pressedContent) => pressedContent.properties.webTitle.size > 0
+    }
+  }
+
   def renderEmailHeadline(faciaPage: PressedPage): Cached.CacheableResult = {
     val webTitle = for {
-      topCollection <- faciaPage.collections.headOption
+      topCollection <- faciaPage.collections.filter(collectionIsSuitableForHeadlineExtraction).headOption
       topCurated <- topCollection.curatedPlusBackfillDeduplicated.headOption
     } yield RevalidatableResult.Ok(topCurated.properties.webTitle)
-
     webTitle.getOrElse {
       log.warn(s"headline not found for ${faciaPage.id}")
       headlineNotFound
