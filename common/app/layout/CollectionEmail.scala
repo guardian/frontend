897e7a628cd0449c90f897ba3417bc6087d21285
Pascal Honoré <pascal@alseyn.net>
2020-09-03 18:25:09 +0100

    Revert "Merge pull request #22965 from guardian/ph-20200902-1332-email-snap"
    This reverts commit af692189cbc73c0752a7310b6f6353eb95227d59, reversing
    changes made to acdc06729cca569493ca5e3fc93d3167056e9840.

--- common/app/layout/CollectionEmail.scala
+++ common/app/layout/CollectionEmail.scala
@@ -35,16 +35,7 @@ object EmailContentContainer {
 
   private def pressedCollectionToContentContainer(pressedCollection: PressedCollection): EmailContentContainer = {
     val cards = pressedCollection.curatedPlusBackfillDeduplicated.flatMap(contentCard(_, pressedCollection.config))
-
-    /*
-        date: 03rd September 2020
-        author: Pascal
-        message: emailcards was introduced, as a subset of cards, to avoid interactive snaps in
-        emails (original request from Celine). `c.snapStuff.isDefined` seems to be the right way to do it.
-     */
-    val emailcards = cards.filterNot(c => c.snapStuff.isDefined)
-
-    fromCollectionAndCards(pressedCollection, emailcards)
+    fromCollectionAndCards(pressedCollection, cards)
   }
 
   def storiesCount(collectionConfig: CollectionConfig): Int =
