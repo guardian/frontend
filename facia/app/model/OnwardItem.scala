01e330dbea2986a0334c15ed510c9f575b1cad3d
Nicolas Long <nicolas.long@theguardian.com>
2020-09-10 16:10:53 +0100

    Adds a JSON endpoint for Facia containers
        GET /container/data/{containerID}.json
    
    This is for some upcoming onwards tests we'd like to do in DCR. As
    a result, some of the code is simply duplicated from Onwards for
    now. If successful, we should consolidate this code and decide where
    best to locate it.

---
+++ facia/app/model/OnwardItem.scala
@@ -0,0 +1,68 @@
+package model
+
+import com.github.nscala_time.time.Imports.DateTimeZone
+import common.{Edition, LinkTo}
+import model.facia.PressedCollection
+import model.pressed.PressedContent
+import play.api.libs.json.Json
+import views.support.{ImgSrc, RemoveOuterParaHtml}
+import implicits.FaciaContentFrontendHelpers._
+import play.api.mvc.RequestHeader
+
+// Temporarily copied from onward for the headlines test. We should move this/refactor if the test is successful.
+case class OnwardItem(
+    url: String,
+    linkText: String,
+    showByline: Boolean,
+    byline: Option[String],
+    image: Option[String],
+    ageWarning: Option[String],
+    isLiveBlog: Boolean,
+    pillar: String,
+    designType: String,
+    webPublicationDate: String,
+    headline: String,
+    mediaType: Option[String],
+    shortUrl: String,
+    kickerText: Option[String],
+    starRating: Option[Int],
+)
+
+object OnwardItem {
+
+  implicit def writes = Json.writes[OnwardItem]
+
+  private[this] def asOnwardItem(content: PressedContent, edition: Edition): OnwardItem = {
+    // a DCR hack that we should standardise
+    def pillarToString(pillar: Pillar): String = {
+      pillar.toString.toLowerCase() match {
+        case "arts" => "culture"
+        case other  => other
+      }
+    }
+
+    OnwardItem(
+      url = LinkTo(content.header.url, edition),
+      linkText = RemoveOuterParaHtml(content.properties.linkText.getOrElse(content.header.headline)).body,
+      showByline = content.properties.showByline,
+      byline = content.properties.byline,
+      image = content.trailPicture.flatMap(ImgSrc.getFallbackUrl),
+      ageWarning = content.ageWarning,
+      isLiveBlog = content.properties.isLiveBlog,
+      pillar = content.maybePillar.map(pillarToString).getOrElse("news"),
+      designType = content.properties.maybeContent.map(_.metadata.designType).getOrElse(Article).toString,
+      webPublicationDate = content.webPublicationDate.withZone(DateTimeZone.UTC).toString,
+      headline = content.header.headline,
+      mediaType = content.card.mediaType.map(_.toString()),
+      shortUrl = content.card.shortUrl,
+      kickerText = content.header.kicker.flatMap(_.properties.kickerText),
+      starRating = content.card.starRating,
+    ),
+  }
+
+  def fromCollection(collection: PressedCollection)(implicit request: RequestHeader): List[OnwardItem] = {
+    collection.curatedPlusBackfillDeduplicated
+      .take(10)
+      .map(pressed => asOnwardItem(pressed, Edition(request)))
+  }
+}
