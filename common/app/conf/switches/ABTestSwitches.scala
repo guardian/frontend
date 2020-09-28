3165750cb2e76f38306d0e670cdf293ca8e6b551
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-09-28 19:33:23 +0300

    increase connatix audience to 2%

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -52,7 +52,7 @@ trait ABTestSwitches {
     "Test Connatix Integration",
     owners = Seq(Owner.withGithub("ioanna0")),
     safeState = Off,
-    sellByDate = new LocalDate(2020, 10, 2),
+    sellByDate = new LocalDate(2020, 10, 4),
     exposeClientSide = true,
   )
 
