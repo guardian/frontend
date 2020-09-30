dcc722271fe20e5dcc03602a8f5e72082e870adf
Peter Colley <zvvvvt@gmail.com>
2020-09-30 17:05:49 +0100

    put back switch

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -71,7 +71,7 @@ trait ABTestSwitches {
     "ab-sign-in-gate-main-control",
     "Control audience for the sign in gate to 9% audience. Will never see the sign in gate.",
     owners = Seq(Owner.withGithub("coldlink")),
-    safeState = On,
+    safeState = Off,
     sellByDate = new LocalDate(2020, 12, 1),
     exposeClientSide = true,
   )
@@ -81,7 +81,7 @@ trait ABTestSwitches {
     "ab-sign-in-gate-main-variant",
     "Show sign in gate to 90% of users on 3rd article view, variant/full audience",
     owners = Seq(Owner.withGithub("coldlink")),
-    safeState = On,
+    safeState = Off,
     sellByDate = new LocalDate(2020, 12, 1),
     exposeClientSide = true,
   )
