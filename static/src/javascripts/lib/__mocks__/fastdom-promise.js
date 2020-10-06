25d223fd1ff44b59506a6899d46b366179fab1d7
Alex Sanders <a@snd.rs>
2020-10-06 15:23:25 +0100

    mock updated fastdom

--- static/src/javascripts/lib/__mocks__/fastdom-promise.js
+++ static/src/javascripts/lib/__mocks__/fastdom-promise.js
@@ -1,9 +1,7 @@
 // @flow
 /* eslint-disable no-unused-vars */
 export default {
-    read: (fn: Function, ctx: ?Object): Promise<any> => Promise.resolve(fn()),
-    write: (fn: Function, ctx: ?Object): Promise<any> => Promise.resolve(fn()),
-    defer: (frame: number, fn: Function, ctx: ?Object): Promise<any> =>
-        Promise.resolve(fn()),
+    measure: (fn: Function, ctx: ?Object): Promise<any> => Promise.resolve(fn()),
+    mutate: (fn: Function, ctx: ?Object): Promise<any> => Promise.resolve(fn()),
     clear: (id: number): void => {},
 };
