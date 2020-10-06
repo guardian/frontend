82c62ded8730c7eaaa9e41afa38577c3fa1609d0
Alex Sanders <a@snd.rs>
2020-10-06 15:23:57 +0100

    this can be simpler

--- static/src/javascripts/lib/fastdom-promise.js
+++ static/src/javascripts/lib/fastdom-promise.js
@@ -2,6 +2,4 @@
 import fastdom from 'fastdom';
 import fastdomPromised from 'fastdom/extensions/fastdom-promised';
 
-const { measure, mutate, clear } = fastdom.extend(fastdomPromised);
-
-export default { measure, mutate, clear }
+export default fastdom.extend(fastdomPromised);
