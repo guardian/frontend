5c436e8fdf8d810a0be55513a596b7441131ed94
Josh Buckland <buck06191@gmail.com>
2020-10-07 16:25:31 +0100

    Resize multiple iframes with same src

--- static/src/javascripts/bootstraps/standard/emailEmbeds.js
+++ static/src/javascripts/bootstraps/standard/emailEmbeds.js
@@ -1,26 +1,26 @@
 // @flow
 
-import config from 'lib/config';
-
 const initEmbedResize = () => {
-    const iframes: HTMLIFrameElement[] = ([
+    const allIframes: HTMLIFrameElement[] = ([
         ...document.querySelectorAll('.element-embed > .email-sub__iframe'),
     ]: any);
 
     window.addEventListener('message', event => {
-        const iframe: ?HTMLIFrameElement = iframes.find(i => {
+        const iframes: HTMLIFrameElement[] = allIframes.filter(i => {
             try {
                 return i.src === event.source.location.href;
             } catch (e) {
                 return false;
             }
         });
-        if (iframe) {
+        if (iframes.length !== 0) {
             try {
                 const message = JSON.parse(event.data);
                 switch (message.type) {
                     case 'set-height':
-                        iframe.height = message.value;
+                        iframes.forEach( iframe => {
+                            iframe.height = message.value;
+                        })
                         break;
                     default:
                 }
