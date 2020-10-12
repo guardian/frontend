279094865383a9fa1215fa7f48bad37a18417bee
Josh Buckland <buck06191@users.noreply.github.com>
2020-10-12 10:08:46 +0100

    Revert "Revert "Prep newsletter embeds for new designs""

--- common/app/model/EmailSubscription.scala
+++ common/app/model/EmailSubscription.scala
@@ -2,5 +2,7 @@ package model
 
 sealed trait SubscriptionResult
 case object Subscribed extends SubscriptionResult
-case object InvalidEmail extends SubscriptionResult
-case object OtherError extends SubscriptionResult
+
+sealed trait NonsuccessResult extends SubscriptionResult
+case object InvalidEmail extends NonsuccessResult
+case object OtherError extends NonsuccessResult
