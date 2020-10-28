257f7ed616e31701eddbd560a521ae3fc9f56d35
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-10-28 14:20:52 +0200

    lint fix

--- static/src/javascripts/projects/commercial/modules/messenger.js
+++ static/src/javascripts/projects/commercial/modules/messenger.js
@@ -3,10 +3,10 @@ import reportError from 'lib/report-error';
 import dfpOrigin from 'commercial/modules/messenger/dfp-origin';
 import { postMessage } from 'commercial/modules/messenger/post-message';
 
-const ALLOWED_HOSTS = [
+/*const ALLOWED_HOSTS = [
     dfpOrigin,
     `${window.location.protocol}//${window.location.host}`,
-];
+];*/
 const LISTENERS = {};
 let REGISTERED_LISTENERS = 0;
 
@@ -106,9 +106,9 @@ const formatError = (error, ...args) =>
 const onMessage = (event: Event): void => {
     let data;
     // We only allow communication with ads created by DFP
-    /*if (ALLOWED_HOSTS.indexOf(event.origin) < 0) {
+    /* if (ALLOWED_HOSTS.indexOf(event.origin) < 0) {
         return;
-    }*/
+    } */
 
     // #? This try-catch is a good target for splitting out into a seperate function
     try {
