73b92442942519e65dbce943f3fc9d530ef1592a
Mark Addis <mark.addis@guardian.co.uk>
2020-11-02 09:24:48 +0000

    Add DCR URL to Ipsos test article

--- static/src/javascripts/projects/commercial/modules/ipsosTaggingScript.js
+++ static/src/javascripts/projects/commercial/modules/ipsosTaggingScript.js
@@ -25,7 +25,7 @@ export const init = (): Promise<void> => {
         onConsentChange(state => {
             // Initial testing only
             console.log(getConsentFor('ipsos', state));
-            if(document.location.href === "https://www.theguardian.com/science/grrlscientist/2012/aug/07/3") {
+            if(document.location.href === "https://www.theguardian.com/science/grrlscientist/2012/aug/07/3" || document.location.href === "https://www.theguardian.com/science/grrlscientist/2012/aug/07/3?dcr=true") {
                 if (getConsentFor('ipsos', state)) {
                     loadIpsosScript();
                 }
