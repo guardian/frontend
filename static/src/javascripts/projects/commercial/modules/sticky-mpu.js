d703bd2ae83e05a1474938798db1a06ebc96fe8a
Ashish Puliyel <ashish.puliyel.freelancer@guardian.co.uk>
2020-11-04 12:01:43 +0000

    lint fix

--- static/src/javascripts/projects/commercial/modules/sticky-mpu.js
+++ static/src/javascripts/projects/commercial/modules/sticky-mpu.js
@@ -67,7 +67,7 @@ const stickyMpu = (adSlot: HTMLElement) => {
         '.js-article__body,.js-liveblog-body-content'
     );
 
-    const stickyPixelBoundary: number = 600; //this is the ad-height.
+    const stickyPixelBoundary: number = 600; // This is the ad-height.
 
     if (
         !referenceElement ||
