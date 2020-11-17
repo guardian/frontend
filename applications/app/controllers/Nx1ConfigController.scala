cb35292e46d8ba2f472d180445c67856ffe0608f
Pascal Honoré <pascal@alseyn.net>
2020-11-17 14:23:15 +0000

    Introduce Nx1Config

--- applications/app/controllers/Nx1ConfigController.scala
+++ applications/app/controllers/Nx1ConfigController.scala
@@ -3,12 +3,16 @@ package controllers
 import play.api.libs.json.{Format, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
+case class Nx1Config(something: String)
+
+object Nx1Config {
+  implicit val jf: Format[Nx1Config] = Json.format[Nx1Config]
+}
+
 class Nx1ConfigController(val controllerComponents: ControllerComponents) extends BaseController {
-
-  // implicit val jf: Format[FakeGeolocation] = Json.format[FakeGeolocation]
-
   def nx1Config: Action[AnyContent] =
     Action {
-      Ok("Testing")
+      val config = Nx1Config("for me")
+      Ok(Json.toJson(config))
     }
 }
