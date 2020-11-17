f35cc9b57cf3bac0f3f78e20c5f6230bc009f8ed
Pascal Honoré <pascal@alseyn.net>
2020-11-17 15:17:38 +0000

    Add switches to the nx1 config object

--- applications/app/controllers/Nx1ConfigController.scala
+++ applications/app/controllers/Nx1ConfigController.scala
@@ -2,17 +2,27 @@ package controllers
 
 import play.api.libs.json.{Format, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import views.support.CamelCase
 
-case class Nx1Config(something: String)
+case class Nx1Config(switches: Map[String, Boolean])
 
 object Nx1Config {
   implicit val jf: Format[Nx1Config] = Json.format[Nx1Config]
+
+  def make(): Nx1Config = {
+    val switches = conf.switches.Switches.all
+      .filter(_.exposeClientSide)
+      .foldLeft(Map.empty[String, Boolean])((acc, switch) => {
+        acc + (CamelCase.fromHyphenated(switch.name) -> switch.isSwitchedOn)
+      })
+    Nx1Config(switches = switches)
+  }
 }
 
 class Nx1ConfigController(val controllerComponents: ControllerComponents) extends BaseController {
   def nx1Config: Action[AnyContent] =
     Action {
-      val config = Nx1Config("for me")
+      val config = Nx1Config.make()
       Ok(Json.toJson(config))
     }
 }
