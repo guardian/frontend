8bc5cb791888e0a6be6d26b5a7f6ec7d57773ad1
Pascal Honoré <pascal@alseyn.net>
2020-11-18 13:24:03 +0000

    Add abTests to Nx1Config

--- applications/app/controllers/Nx1ConfigController.scala
+++ applications/app/controllers/Nx1ConfigController.scala
@@ -1,28 +1,39 @@
 package controllers
 
 import play.api.libs.json.{Format, Json}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, RequestHeader}
 import views.support.CamelCase
+import experiments.ActiveExperiments
+import experiments.ActiveExperiments._
 
-case class Nx1Config(switches: Map[String, Boolean])
+case class Nx1Config(switches: Map[String, Boolean], abTests: Map[String, String])
 
 object Nx1Config {
   implicit val jf: Format[Nx1Config] = Json.format[Nx1Config]
 
-  def make(): Nx1Config = {
+  def makeAbTestReport(implicit request: RequestHeader) = {
+    ActiveExperiments.allExperiments
+      .filter(e => isParticipating(e) || isControl(e))
+      .toSeq
+      .map { e => (e.description, e.value) }
+      .toMap
+  }
+
+  def make(request: RequestHeader): Nx1Config = {
     val switches = conf.switches.Switches.all
       .filter(_.exposeClientSide)
       .foldLeft(Map.empty[String, Boolean])((acc, switch) => {
         acc + (CamelCase.fromHyphenated(switch.name) -> switch.isSwitchedOn)
       })
-    Nx1Config(switches = switches)
+    val abTests = makeAbTestReport(request)
+    Nx1Config(switches = switches, abTests = abTests)
   }
 }
 
 class Nx1ConfigController(val controllerComponents: ControllerComponents) extends BaseController {
   def nx1Config: Action[AnyContent] =
-    Action {
-      val config = Nx1Config.make()
+    Action { implicit request =>
+      val config = Nx1Config.make(request)
       Ok(Json.toJson(config))
     }
 }
