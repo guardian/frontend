29d559d8e067f5716f2d49201c36a9134ab6e5d6
Mark Addis <mark.addis@guardian.co.uk>
2020-11-18 13:56:11 +0000

    Alias ophan/ng to remove it from the DCR commercial bundle

--- webpack.config.dcr.js
+++ webpack.config.dcr.js
@@ -17,8 +17,14 @@ config.entry = {
     ),
 };
 
+// The Ophan alias removes duplicating the js in the commercial bundle sent to DCR.
 module.exports = webpackMerge.smart(config, {
     output: {
         path: path.join(__dirname, 'static', 'target', 'javascripts'),
+    },
+    resolve: {
+        alias: {
+            "ophan/ng": path.join(__dirname, 'static', 'src', 'javascripts', 'projects', 'commercial', 'modules', 'ophan-dcr.js'),
+        },
     },
 });
