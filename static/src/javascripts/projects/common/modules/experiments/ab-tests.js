fd1f827c479fc18dd519ba79089b8c9157222132
Tom Forbes <tom.forbes@theguardian.com>
2020-11-19 09:05:32 +0000

    Liveblog epic design test R2 (#23262)
    * Liveblog epic design test R2:
    
    * date

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -4,6 +4,7 @@ import { contributionsBannerArticlesViewedOptOut } from 'common/modules/experime
 import { signInGateMainVariant } from 'common/modules/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from 'common/modules/experiments/tests/sign-in-gate-main-control';
 import { newsletterMerchUnitLighthouseControl, newsletterMerchUnitLighthouseVariant } from 'common/modules/experiments/tests/newsletter-merch-unit-test';
+import { liveblogEpicDesignTest } from 'common/modules/experiments/tests/liveblog-epic-design-test';
 
 export const concurrentTests: $ReadOnlyArray<ABTest> = [
     commercialPrebidSafeframe,
@@ -13,7 +14,7 @@ export const concurrentTests: $ReadOnlyArray<ABTest> = [
     newsletterMerchUnitLighthouseVariant
 ];
 
-export const epicTests: $ReadOnlyArray<EpicABTest> = [];
+export const epicTests: $ReadOnlyArray<EpicABTest> = [liveblogEpicDesignTest];
 
 export const engagementBannerTests: $ReadOnlyArray<AcquisitionsABTest> = [
     contributionsBannerArticlesViewedOptOut,
