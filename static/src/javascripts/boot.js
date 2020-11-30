ff7a63c30b54e2a8e107aff5d862b2f5cf6a64c5
Pascal Honoré <pascal@alseyn.net>
2020-11-30 06:48:09 +0000

    remove un-used import

--- static/src/javascripts/boot.js
+++ static/src/javascripts/boot.js
@@ -10,7 +10,7 @@ import { markTime } from 'lib/user-timing';
 import { captureOphanInfo } from 'lib/capture-ophan-info';
 import reportError from 'lib/report-error';
 import { cmp, onConsentChange } from '@guardian/consent-management-platform';
-import { getLocale, storage } from '@guardian/libs';
+import { storage } from '@guardian/libs';
 import { getCookie } from 'lib/cookies';
 import { getSync as geolocationGetSync } from 'lib/geolocation';
 import { trackPerformance } from 'common/modules/analytics/google';
