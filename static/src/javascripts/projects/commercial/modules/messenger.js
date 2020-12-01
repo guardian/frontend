1c84086a02cdffd5dc7958d7b49f5262b0717268
Ashish Puliyel <ashish.puliyel.freelancer@guardian.co.uk>
2020-12-01 11:31:09 +0000

    better comment

--- static/src/javascripts/projects/commercial/modules/messenger.js
+++ static/src/javascripts/projects/commercial/modules/messenger.js
@@ -81,8 +81,8 @@ const getIframe = (data: StandardMessage): ?HTMLElement => {
 // such as validating the anatomy of the payload and whitelisting
 // event type
 const isValidPayload = (payload: StandardMessage): boolean =>
-    !!payload && //typeof null==='object' because... javascript
     typeof payload === 'object' &&
+    !!payload && // Needed because typeof null==='object'
     'type' in payload &&
     'value' in payload &&
     'id' in payload &&
