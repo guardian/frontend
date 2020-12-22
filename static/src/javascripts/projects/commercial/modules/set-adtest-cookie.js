f6aea792193e3159f5befa9b086ff3d0a877e1e8
Ashish Puliyel <ashish.puliyel.freelancer@guardian.co.uk>
2020-12-22 12:56:05 +0000

    move setAdTestcookie to own module. Call in both DCR and Frontend commercial bundles

---
+++ static/src/javascripts/projects/commercial/modules/set-adtest-cookie.js
@@ -0,0 +1,16 @@
+import { getUrlVars } from 'lib/url';
+import { removeCookie, addCookie } from 'lib/cookies';
+
+
+const init = (): Promise<void> => {
+    const queryParams = getUrlVars();
+
+    if (queryParams.adtest === 'clear') {
+        removeCookie('adtest');
+    } else if (queryParams.adtest) {
+        addCookie('adtest', encodeURIComponent(queryParams.adtest), 10);
+    }
+    return Promise.resolve();
+};
+
+export { init };
