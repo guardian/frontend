625fccbf33eb572b4795d516d44de0b8849c20f6
Rob Phillips <robert.phillips@guardian.co.uk>
2021-01-06 09:04:39 +0000

    Extend switch to avoid holding up the main branch

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -62,7 +62,7 @@ trait ABTestSwitches {
     "Tests an additional 'curated' onwards container below the article body that is relevant to the article's pillar.",
     owners = Seq(Owner.withGithub("rcrphillips")),
     safeState = Off,
-    sellByDate = new LocalDate(2021, 1, 6),
+    sellByDate = new LocalDate(2021, 1, 12),
     exposeClientSide = true,
   )
 
