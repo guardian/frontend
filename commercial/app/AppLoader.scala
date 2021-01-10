d302d827ed22d66446bc84f4eca28825b73b3b4c
Pascal Honoré <pascal@alseyn.net>
2021-01-10 01:32:48 +0000

    Update AppLoader.scala

--- commercial/app/AppLoader.scala
+++ commercial/app/AppLoader.scala
@@ -14,12 +14,12 @@ import common.Logback.{LogbackOperationsPool, LogstashLifecycle}
 import conf.switches.SwitchboardLifecycle
 import conf.CachedHealthCheckLifeCycle
 import contentapi.{CapiHttpClient, ContentApiClient, HttpClient}
-import dev.{DevAssetsController}
+import dev.DevAssetsController
 import http.{CommonFilters, CorsHttpErrorHandler}
 import model.ApplicationIdentity
 import play.api.ApplicationLoader.Context
 import play.api.BuiltInComponentsFromContext
-import play.api.http.{HttpErrorHandler}
+import play.api.http.HttpErrorHandler
 import play.api.libs.ws.WSClient
 import play.api.mvc.EssentialFilter
 import play.api.routing.Router
