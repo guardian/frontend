c68628b1c9518c5b392c06d933bb3b7570b3d400
Pascal Honoré <pascal@alseyn.net>
2021-01-12 21:34:27 +0000

    Fix test

--- applications/test/ImageContentTemplateTest.scala
+++ applications/test/ImageContentTemplateTest.scala
@@ -1,4 +1,5 @@
 package test
+import scala.collection.JavaConverters._
 
 import org.scalatest.{DoNotDiscover, Matchers, FlatSpec}
 
@@ -6,13 +7,15 @@ import org.scalatest.{DoNotDiscover, Matchers, FlatSpec}
 
   it should "render a cartoon" in goTo("/commentisfree/cartoon/2013/jul/15/iain-duncan-smith-benefits-cap") { browser =>
     import browser._
-    el("[itemprop='headline']").toString should be("Steve Bell on Iain Duncan Smith and the benefits cap – cartoon")
+    el("[itemprop='headline']").html() should be(
+      "Steve Bell on Iain Duncan Smith and the benefits cap – cartoon",
+    )
     el(".media-primary img").attribute("src") should include("Steve-Bell-cartoon")
   }
 
   it should "render a picture" in goTo("/artanddesign/picture/2013/oct/08/photography") { browser =>
     import browser._
-    $("[itemprop='headline']").toString should be("Early erotica - a picture from the past")
+    $("[itemprop='headline']").texts().asScala.toList.mkString should be("Early erotica - a picture from the past")
     el(".media-primary img").attribute("src") should include("French-Nude-in-Body-Stock")
   }
 }
