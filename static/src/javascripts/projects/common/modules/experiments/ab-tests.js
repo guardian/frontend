e4cd03b9efc8e1f254132bb8c09d3d625be128a3
Mahesh Makani <maheshmakani@gmail.com>
2021-01-19 15:02:50 +0000

    add design test to list

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -1,12 +1,14 @@
 import { commercialPrebidSafeframe } from 'common/modules/experiments/tests/commercial-prebid-safeframe.js';
 import { signInGateMainVariant } from 'common/modules/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from 'common/modules/experiments/tests/sign-in-gate-main-control';
+import { signInGateDesignOpt } from 'common/modules/experiments/tests/sign-in-gate-design-opt';
 import { globalEoyHeaderTest } from 'common/modules/experiments/tests/global-eoy-header-test';
 
 export const concurrentTests = [
     commercialPrebidSafeframe,
     signInGateMainVariant,
     signInGateMainControl,
+    signInGateDesignOpt,
     globalEoyHeaderTest,
 ];
 
