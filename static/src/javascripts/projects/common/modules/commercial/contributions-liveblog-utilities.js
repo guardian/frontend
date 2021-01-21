0f98587a316606ad0433dc65fb9ddc24359d0446
Tom Forbes <tom.forbes@theguardian.com>
2021-01-21 13:38:49 +0000

    simplify liveblog

--- static/src/javascripts/projects/common/modules/commercial/contributions-liveblog-utilities.js
+++ static/src/javascripts/projects/common/modules/commercial/contributions-liveblog-utilities.js
@@ -1,43 +1,27 @@
 import $ from 'lib/$';
 import { mountDynamic } from "@guardian/automat-modules";
 
-// TODO - remove support for this?
-const INSERT_EPIC_AFTER_CLASS = 'js-insert-epic-after';
-
-const getBlocksToInsertEpicAfter = () => {
+const getBlockToInsertEpicAfter = () => {
     const blocks = document.getElementsByClassName('block');
-    const blocksToInsertManualEpicAfter = document.getElementsByClassName(
-        INSERT_EPIC_AFTER_CLASS
-    );
     const epicsAlreadyOnPage = document.getElementsByClassName('is-epic');
 
     const isLiveblogLongEnoughYet = blocks.length > 4;
 
-    if (
-        blocksToInsertManualEpicAfter.length ||
-        epicsAlreadyOnPage.length ||
-        !isLiveblogLongEnoughYet
-    ) {
-        return Array.from(blocksToInsertManualEpicAfter);
+    if (epicsAlreadyOnPage.length < 1 && isLiveblogLongEnoughYet) {
+        const autoBlockNum = Math.floor(Math.random() * 3);
+        return blocks[autoBlockNum];
     }
-
-    const autoBlockNum = Math.floor(Math.random() * 3);
-    const blockToInsertAutoEpicAfter = blocks[autoBlockNum];
-
-    return Array.from(blocksToInsertManualEpicAfter).concat(
-        blockToInsertAutoEpicAfter
-    );
 };
 
 export const setupRemoteEpicInLiveblog = (
     Component,
     props,
 ) => {
-    const blocks = getBlocksToInsertEpicAfter();
+    const block = getBlockToInsertEpicAfter();
     // Only insert 1 epic. The existing code will be cleaned up in a follow-up PR
-    if (blocks[0]) {
+    if (block) {
         const epic = $.create('<div class="block"/>');
-        epic.insertAfter(blocks[0]);
+        epic.insertAfter(block);
         mountDynamic(epic[0], Component, props, true);
 
         return epic[0];
