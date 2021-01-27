f3d369c5a7110702b6fcdee938fbe19d127ad730
Alex Sanders <a@snd.rs>
2021-01-27 16:11:38 +0000

    ignore import/no-unresolved warning in TS
    TS sorts it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-    extends: ['prettier'],
-    plugins: ['guardian-frontend', 'prettier'],
-    parser: 'babel-eslint',
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 6,
-    },
-    rules: {
-        // disallow naming variables 'guardian', because
-        // window.guardian is our global config/settings object
-        'id-denylist': ['error', 'guardian'],
-    },
-    ignorePatterns: ['javascripts.flow.archive'],
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            extends: '@guardian/eslint-config-typescript',
-        },
-    ],
+	extends: ['prettier'],
+	plugins: ['guardian-frontend', 'prettier'],
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 6,
+	},
+	rules: {
+		// disallow naming variables 'guardian', because
+		// window.guardian is our global config/settings object
+		'id-denylist': ['error', 'guardian'],
+	},
+	ignorePatterns: ['javascripts.flow.archive'],
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'],
+			extends: '@guardian/eslint-config-typescript',
+			rules: {
+				'import/no-unresolved': 0,
+			},
+		},
+	],
 };
