e7c340c06122097ee8693c9d4550c7b9c6a8b77b
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2021-01-28 13:43:00 +0200

    added slot id

--- static/src/javascripts/projects/commercial/modules/messenger/measure-ad-load.js
+++ static/src/javascripts/projects/commercial/modules/messenger/measure-ad-load.js
@@ -2,8 +2,6 @@ import { markTime } from 'lib/user-timing';
 
 const init = (register) => {
     register('measure-ad-load', (specs) => {
-        console.log("*** RECEIVED MEASURE AD LOAD EVENT ");
-        console.log(specs);
         if (specs.slotId === 'top-above-nav') {
             markTime('topAboveNav Ad loaded')
         }
