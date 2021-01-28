557228bd7cce397e681e2012049b9c14a9df90dc
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2021-01-28 13:45:43 +0200

    added creative wrapper as comment for reference

--- static/src/javascripts/projects/commercial/modules/messenger/measure-ad-load.js
+++ static/src/javascripts/projects/commercial/modules/messenger/measure-ad-load.js
@@ -1,5 +1,25 @@
 import { markTime } from 'lib/user-timing';
 
+// This message is intended to be used with a DFP creative wrapper.
+// For reference, the wrapper will post a message, like so:
+
+/*
+* <script  id='ad-load-%%CACHEBUSTER%%'>
+//send postMessage to commercial bundle
+const metaData = {
+    slotId: '%%PATTERN:slot%%'
+};
+top.window.postMessage(JSON.stringify(
+    {
+        id: 'bf724866-723c-6b0a-e5d7-ad61535f98b7',
+        slotId: '%%PATTERN:slot%%',
+        type: 'measure-ad-load',
+        value: metaData
+    }
+), '*');
+</script>
+* */
+
 const init = (register) => {
     register('measure-ad-load', (specs) => {
         if (specs.slotId === 'top-above-nav') {
