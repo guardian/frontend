1c72082bee498b24ca5c41fbb4269be352100439
Rebecca Thompson <becca.al.thompson@gmail.com>
2021-03-15 08:46:20 +0000

    remove sign-in gate copy test switch

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -1,9 +1,7 @@
 import { signInGateMainVariant } from 'common/modules/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from 'common/modules/experiments/tests/sign-in-gate-main-control';
-import { signInGateCopyOpt } from 'common/modules/experiments/tests/sign-in-gate-copy-opt';
 
 export const concurrentTests = [
     signInGateMainVariant,
-    signInGateMainControl,
-    signInGateCopyOpt
+    signInGateMainControl
 ];
