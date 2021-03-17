0177d2437a6349f4d47b831ff2e4c9f6442f04bb
Max Duval <max.duval@guardian.co.uk>
2021-03-17 10:14:49 -0400

    proper attribution

--- dev/nodeFetch.js
+++ dev/nodeFetch.js
@@ -3,6 +3,7 @@ global.fetch = require('node-fetch');
 global.Headers = require('node-fetch').Headers;
 
 // Not supported before Node 12, delete afterwards
+/*! fromentries. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 global.Object.fromEntries = (iterable) =>
     [...iterable].reduce((obj, [key, val]) => {
       obj[key] = val
