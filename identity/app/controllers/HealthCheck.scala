5bb3d86b90cfeda50f0c621c3436a9b7e1f0c593
Mahesh Makani <maheshmakani@gmail.com>
2021-03-19 11:55:45 +0000

    remove /privacy-settings and upsell routes

--- identity/app/controllers/HealthCheck.scala
+++ identity/app/controllers/HealthCheck.scala
@@ -9,5 +9,5 @@ import scala.concurrent.ExecutionContext
 class HealthCheck(wsClient: WSClient, val controllerComponents: ControllerComponents)(implicit
     executionContext: ExecutionContext,
 ) extends AllGoodCachedHealthCheck(
-      NeverExpiresSingleHealthCheck(routes.AdvertsManager.renderAdvertsManager(None).url),
+      NeverExpiresSingleHealthCheck("/"),
     )(wsClient, executionContext)
