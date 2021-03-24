2a13ff4f7882b5dba620b975b47b3f896fd787a4
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2021-03-24 17:07:30 +0000

    Bump the identity lib version.
    It contains new copy for the communication consents, this will update it.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ package com.gu
 import sbt._
 
 object Dependencies {
-  val identityLibVersion = "3.239"
+  val identityLibVersion = "3.247"
   val awsVersion = "1.11.240"
   val capiVersion = "17.10"
   val faciaVersion = "3.3.0"
