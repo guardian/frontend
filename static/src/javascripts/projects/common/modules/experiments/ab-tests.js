773f4d80fa7f48b968bb0077f6e63a8d45cd4d3f
Peter Colley <zvvvvt@gmail.com>
2021-03-29 10:02:17 +0100

    Revert "Merge pull request #23587 from guardian/mandatory-signingate-test"
    This reverts commit 7b8c89a6b66bdd2b75039c2485716a6a59a9732c, reversing
    changes made to ab8a31eba3d168e1af0661723fd827bd5f3bd241.

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.js
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.js
@@ -1,11 +1,9 @@
 import { signInGateMainVariant } from 'common/modules/experiments/tests/sign-in-gate-main-variant';
 import { signInGateMainControl } from 'common/modules/experiments/tests/sign-in-gate-main-control';
 import { puzzlesBanner } from 'common/modules/experiments/tests/puzzles-banner';
-import { signInGateMandoryTest } from "common/modules/experiments/tests/sign-in-gate-mandatory";
 
 export const concurrentTests = [
     signInGateMainVariant,
     signInGateMainControl,
-    puzzlesBanner,
-    signInGateMandoryTest
+    puzzlesBanner
 ];
