60060272031fcf6a37b4c3a948eef22246574d47
Ravi <7014230+arelra@users.noreply.github.com>
2021-03-30 15:10:01 +0100

    Fix top banner ad label layout shift (#23613)
    * Fix top banner ad label layout shift
    
    * Convert render-advert-label to TypeScript
    
    * Remove toggled ad labels when ad slots are removed.
    Refactor removal code
    
    * Convert remove-slots to TypeScript
    
    * Hide toggled ad label when label should not be displayed
    
    * Use nodelist.length for remove-slots.spec.ts
    
    * remove-slots refactor
    
    Co-authored-by: Ravi Lal <arelra@users.noreply.github.com>

--- static/src/javascripts/projects/commercial/modules/dfp/render-advert-label.spec.js
+++ static/src/javascripts/projects/commercial/modules/dfp/render-advert-label.spec.js
@@ -30,27 +30,37 @@ describe('Rendering advert labels', () => {
         adverts.frame = bonzo(
             bonzo.create('<div class="js-ad-slot ad-slot--frame"></div>')
         );
+
+        adverts.topAboveNav = bonzo(
+            bonzo.create(`<div class="js-ad-slot" id="dfp-ad--top-above-nav"></div>`)
+        );
     });
 
-    it(`Can add a label`, () =>
+    afterEach(() => {
+        if (document.body) {
+            document.body.innerHTML = '';
+        }
+    });
+
+    it('Can add a label', () =>
         renderAdvertLabel(adverts.withLabel[0]).then(() => {
             const label = adverts.withLabel[0].querySelector(labelSelector);
             expect(label).not.toBeNull();
         }));
 
-    it(`The label has a message`, () =>
+    it('The label has a message', () =>
         renderAdvertLabel(adverts.withLabel[0]).then(() => {
             const label = adverts.withLabel[0].querySelector(labelSelector);
             expect(label.textContent).toBe('Advertisement');
         }));
 
-    it(`Won't add a label if it has an attribute data-label='false'`, () =>
+    it('Will not add a label if it has an attribute data-label="false"', () =>
         renderAdvertLabel(adverts.labelDisabled[0]).then(() => {
             const label = adverts.labelDisabled[0].querySelector(labelSelector);
             expect(label).toBeNull();
         }));
 
-    it(`Won't add a label if the adSlot already has one`, () =>
+    it('Will not add a label if the adSlot already has one', () =>
         renderAdvertLabel(adverts.alreadyLabelled[0]).then(() => {
             const label = adverts.alreadyLabelled[0].querySelectorAll(
                 labelSelector
@@ -58,9 +68,54 @@ describe('Rendering advert labels', () => {
             expect(label.length).toBe(1);
         }));
 
-    it(`Won't add a label to frame ads`, () =>
+    it('Will not add a label to frame ads', () =>
         renderAdvertLabel(adverts.frame[0]).then(() => {
             const label = adverts.frame[0].querySelector(labelSelector);
             expect(label).toBeNull();
         }));
+
+    it('When the ad is top above nav and the label is toggleable, make the label visible and set width to ad width', () => {
+        if (document.body) {
+            document.body.innerHTML = `
+                <div>
+                    <div class="ad-slot__label ad-slot__label--toggle hidden">Advertisement</div>
+                    <div class="js-ad-slot" id="dfp-ad--top-above-nav"></div>
+                </div>
+            `;
+        }
+        Object.defineProperty(window.HTMLElement.prototype, 'offsetWidth', {
+            get: function() {
+              return this.id === 'dfp-ad--top-above-nav' ? 120 : 60;
+            }
+          });
+        return renderAdvertLabel(adverts.topAboveNav[0]).then(() => {
+            const label = document.querySelector(labelSelector);
+            expect(label.classList.contains('visible')).toBe(true);
+            expect(label.style.width).toEqual('120px');
+        });
+    });
+
+    it('When the ad is top above nav and the label is NOT toggleable, render the label dynamically', () =>
+        renderAdvertLabel(adverts.topAboveNav[0]).then(() => {
+            const label = adverts.topAboveNav[0].querySelector(labelSelector);
+            expect(label.textContent).toEqual('Advertisement');
+        }));
+
+    it('When the ad is top above nav and the label is toggleable, and the ad slot should not have a label applied, make the label display none so it is removed from layout', () => {
+        if (document.body) {
+            document.body.innerHTML = `
+                <div>
+                    <div class="ad-slot__label ad-slot__label--toggle hidden">Advertisement</div>
+                    <div class="js-ad-slot" id="dfp-ad--top-above-nav">
+                        <div class="ad-slot__label"></div>
+                    </div>
+                </div>
+            `;
+        }
+        return renderAdvertLabel(adverts.topAboveNav[0]).then(() => {
+            const label = document.querySelector('.ad-slot__label--toggle');
+            expect(label.classList.contains('visible')).toBe(false);
+            expect(label.style.display).toEqual('none');
+        });
+    });
 });
