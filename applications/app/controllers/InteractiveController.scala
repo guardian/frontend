50adc4ad14d55db1d39b7188d46115e204a340d4
Pascal Honoré <pascal@alseyn.net>
2021-04-22 13:07:27 +0100

    Remove println

--- applications/app/controllers/InteractiveController.scala
+++ applications/app/controllers/InteractiveController.scala
@@ -115,7 +115,6 @@ class InteractiveController(
   override def renderItem(path: String)(implicit request: RequestHeader): Future[Result] = {
     val requestFormat = request.getRequestFormat
     val renderingTier = ApplicationsInteractiveRendering.getRenderingTier(path)
-    println((requestFormat, renderingTier))
     (requestFormat, renderingTier) match {
       case (EmailFormat, _)                   => RenderItemLegacy(path: String)
       case (AmpFormat, FrontendLegacy)        => RenderItemLegacy(path: String)
