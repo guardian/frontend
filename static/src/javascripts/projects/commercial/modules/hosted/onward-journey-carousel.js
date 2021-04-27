c419b0e123583eaf2bf5fe083f7bbafcb2947bf0
Ravi Lal <arelra@users.noreply.github.com>
2021-04-27 13:05:23 +0100

    Remove qwery from commercial hosted onward journey carousel

--- static/src/javascripts/projects/commercial/modules/hosted/onward-journey-carousel.js
+++ static/src/javascripts/projects/commercial/modules/hosted/onward-journey-carousel.js
@@ -1,6 +1,6 @@
-import qwery from 'qwery';
 import fastdom from 'fastdom';
-import $ from '../../../../lib/$';
+import { $$ } from '../../../../lib/$$';
+
 
 class HostedCarousel {
 
@@ -15,41 +15,41 @@ class HostedCarousel {
 
         fastdom.mutate(() => {
             const translate = `translate(-${pageNo}00%, 0)`;
-            this.$carousel.css({
+            this.carousel.css({
                 '-webkit-transform': translate,
                 transform: translate,
             });
-            this.$dots.each((el, i) => {
-                $(el).toggleClass('highlighted', i % this.pageCount === pageNo);
+            this.dots.get().forEach((el, i) => {
+                el.classList.toggle('highlighted', i % this.pageCount === pageNo);
             });
-            this.$prevItem.css({
+            this.prevItem.css({
                 opacity: pageNo === 0 ? 0.5 : 1,
             });
-            this.$nextItem.css({
+            this.nextItem.css({
                 opacity: pageNo === this.pageCount - 1 ? 0.5 : 1,
             });
         });
     }
 
     bindButtons() {
-        this.$carousel = $('.js-carousel-pages');
-        this.$nextItem = $('.next-oj-item');
-        this.$prevItem = $('.prev-oj-item');
-        this.$dots = $('.js-carousel-dot');
-        this.pageCount = $('.carousel-page', this.$carousel).length;
+        this.carousel = $$('.js-carousel-pages');
+        this.nextItem = $$('.next-oj-item');
+        this.prevItem = $$('.prev-oj-item');
+        this.dots = $$('.js-carousel-dot');
+        this.pageCount = $$('.carousel-page', this.carousel.get(0)).get().length;
         this.index = 0;
 
-        if (this.$carousel.length) {
-            this.$nextItem.each(el => {
+        if (this.carousel.get().length) {
+            this.nextItem.get().forEach(el => {
                 el.addEventListener('click', this.moveCarouselBy.bind(this, 1));
             });
-            this.$prevItem.each(el => {
+            this.prevItem.get().forEach(el => {
                 el.addEventListener(
                     'click',
                     this.moveCarouselBy.bind(this, -1)
                 );
             });
-            this.$dots.each((el, i) => {
+            this.dots.get().forEach((el, i) => {
                 el.addEventListener(
                     'click',
                     this.moveCarouselTo.bind(this, i % this.pageCount)
@@ -60,7 +60,7 @@ class HostedCarousel {
 }
 
 export const initHostedCarousel = () => {
-    if (qwery('.js-carousel-pages').length) {
+    if ($$('.js-carousel-pages').get().length) {
         new HostedCarousel().bindButtons();
     }
     return Promise.resolve();
