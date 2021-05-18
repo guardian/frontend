34de1ce1abdefe5d268bfb2aae7b40a6c6b744c5
Edward Litten <ea.litten@gmail.com>
2021-05-18 16:58:47 +0100

    Add explanation

--- static/src/javascripts/projects/commercial/modules/dfp/Advert.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/Advert.ts
@@ -137,6 +137,7 @@ class Advert {
 			this.extraNodeClasses,
 			newClasses,
 		);
+		// IE11 does not support multiple arguments to classList.add/remove so do these one-by-ones
 		classesToRemove.forEach((cls) => this.node.classList.remove(cls));
 		newClasses.forEach((cls) => this.node.classList.add(cls));
 		this.extraNodeClasses = newClasses;
