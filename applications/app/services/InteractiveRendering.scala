e8c529e2938e53faa333682d0a95b0ad58ddab4f
Olly <9575458+OllysCoding@users.noreply.github.com>
2021-06-04 16:00:25 +0100

    Remove (now) redundant comment)

--- applications/app/services/InteractiveRendering.scala
+++ applications/app/services/InteractiveRendering.scala
@@ -7,25 +7,6 @@ sealed trait RenderingTier
 object DotcomRendering extends RenderingTier
 object FrontendLegacy extends RenderingTier
 
-/*
-  Date: 21st Jan 2020
-  Author: Pascal
-
-  This object was introduced in late 2020, to handle the routing between regular rendering of interactives
-  versus the code that had been written to handle the US Presidential Election Tracker amp page.
-
-  The tracker (an ng-interactive) didn't have a AMP page and there were two ways to provide one to it.
-  1. Implement the support for it in DCR, or
-  2. Implement support for it directly in the [applications] app, using the AMP page already present in CAPI.
-
-  The former would have taken too long so we went for the latter.
-
-  ----------------
-  Author: Pascal
-  Date: 21st April 2021
-
-  We are now moving towards supporting interactives in DCR 🙂
- */
 object USElectionTracker2020AmpPage extends RenderingTier
 
 object InteractiveRendering {
