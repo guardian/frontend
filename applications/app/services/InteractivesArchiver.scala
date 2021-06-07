9d273c4a89146dce49a985741d26b089fa312e78
Pascal Honoré <pascal@alseyn.net>
2021-06-07 15:46:22 +0100

    Introduce InteractivesArchiver

---
+++ applications/app/services/InteractivesArchiver.scala
@@ -0,0 +1,11 @@
+package services
+
+import play.api.mvc.RequestHeader
+import play.twirl.api.Html
+import scala.util.matching.Regex
+
+object InteractivesArchiver {
+  /*
+    This object was introduced in June 2021, to support the idea that we could possibly serve some interactives from a flat, possibly cleaned, version in S3.
+  */
+}
