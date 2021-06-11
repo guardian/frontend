9353d2556c568028a3154f4a9f2e9c396b4b52de
Mahesh Makani <maheshmakani@gmail.com>
2021-06-11 10:23:32 +0100

    chore(sign-in-gate): update sign in gate mandatory aus audience to 0.25

--- static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-aus-mandatory.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-aus-mandatory.js
@@ -5,8 +5,8 @@ export const signInGateAusMandatory = {
 	author: 'Mahesh Makani',
 	description:
 		'Compare mandatory gate (an article sigin gate without the dismiss button) with the main signin gate for australia only',
-	audience: 0.01,
-	audienceOffset: 0.89,
+	audience: 0.25,
+	audienceOffset: 0.65,
 	successMeasure: 'Users sign in or create a Guardian account',
 	audienceCriteria:
 		'3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss or reshown after 5 dismisses, not on help, info sections etc. exclude iOS 9 and guardian-live-australia, AUS only, only users with specific CMP consents. Suppresses other banners, and appears over epics',
