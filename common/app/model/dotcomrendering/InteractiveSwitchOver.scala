6a31a07e914148034635de0b788ce65e2f904437
Olly <9575458+OllysCoding@users.noreply.github.com>
2021-06-22 11:21:12 +0100

    add comment explaining interactive switch over

--- common/app/model/dotcomrendering/InteractiveSwitchOver.scala
+++ common/app/model/dotcomrendering/InteractiveSwitchOver.scala
@@ -3,5 +3,6 @@ package model.dotcomrendering
 import org.joda.time.DateTime
 
 object InteractiveSwitchOver {
+  // Any interactives published after this date will be rendered on DCR instead of on frontend
   val date = DateTime.parse("2021-06-21T00:00Z")
 }
