4022aab5793f5d8ff0520428f8d3c6f9feb000f9
Peter Colley <zvvvvt@gmail.com>
2021-06-28 10:18:01 +0100

    us mandatory gate switch

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -2,10 +2,12 @@ import { puzzlesBanner } from 'common/modules/experiments/tests/puzzles-banner';
 import { remoteRRHeaderLinksTest } from 'common/modules/experiments/tests/remote-header-test';
 import { signInGateMainControl } from 'common/modules/experiments/tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from 'common/modules/experiments/tests/sign-in-gate-main-variant';
+import { signInGateUsMandatory } from 'common/modules/experiments/tests/sign-in-gate-us-mandatory';
 
 export const concurrentTests: readonly ABTest[] = [
 	signInGateMainVariant,
 	signInGateMainControl,
+	signInGateUsMandatory,
 	puzzlesBanner,
 	remoteRRHeaderLinksTest,
 ];
