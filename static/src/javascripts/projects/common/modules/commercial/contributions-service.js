c70fbdb57a99028dc57bebfdf858d68ae78f307e
Tom Forbes <tom.forbes@theguardian.com>
2021-07-05 13:30:01 +0100

    Do not show RR header links on galleries

--- static/src/javascripts/projects/common/modules/commercial/contributions-service.js
+++ static/src/javascripts/projects/common/modules/commercial/contributions-service.js
@@ -499,6 +499,10 @@ export const fetchAndRenderHeaderLinks = async () => {
         return;
     }
 
+    if (config.get('page.contentType') === 'Gallery') {
+        return;
+    }
+
     try {
         const response = await getHeaderLinks(requestData);
         if (!response.data) {
