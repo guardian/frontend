e91c48739b162719cc993834853d883e9a5424bd
Mario Savarese <mario.savarese@theguardian.com>
2021-07-06 15:38:29 +0100

    add amIUsed to hide and hide-element

--- static/src/javascripts/projects/commercial/modules/hide-element.js
+++ static/src/javascripts/projects/commercial/modules/hide-element.js
@@ -1,4 +1,7 @@
+import { amIUsed } from 'commercial/sentinel';
 import fastdom from '../../../lib/fastdom-promise';
 
-export const hideElement =(element) =>
+export const hideElement = (element) => {
+    amIUsed('hide-element', 'hideElement')
     fastdom.mutate(() => element.classList.add('u-h'));
+}
