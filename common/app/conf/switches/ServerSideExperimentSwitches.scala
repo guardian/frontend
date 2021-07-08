c8b2029bd6b06ecfad1c7dc452b8a96e359ba6b4
Pascal Honoré <pascal@alseyn.net>
2021-07-08 22:39:50 +0100

    Tiny refactor in ServerSideExperimentSwitches

--- common/app/conf/switches/ServerSideExperimentSwitches.scala
+++ common/app/conf/switches/ServerSideExperimentSwitches.scala
@@ -3,15 +3,13 @@ package conf.switches
 import conf.switches.Expiry.never
 
 trait ServerSideExperimentSwitches {
-  val ServerSideExperiments = {
-    Switch(
-      SwitchGroup.ServerSideExperiments,
-      "server-side-tests",
-      "Enables the server side testing system",
-      owners = Seq(Owner.withGithub("johnduffell")),
-      safeState = Off,
-      sellByDate = never,
-      exposeClientSide = false,
-    )
-  }
+  val ServerSideExperiments = Switch(
+    SwitchGroup.ServerSideExperiments,
+    "server-side-experiments",
+    "Enables server side experiments",
+    owners = Seq(Owner.withGithub("shtukas")),
+    safeState = Off,
+    sellByDate = never,
+    exposeClientSide = false,
+  )
 }
