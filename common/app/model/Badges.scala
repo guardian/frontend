0c7f9ba49dcf86cbca2f2217b5ce9f37e33a2754
Harry Fischer <harry.fischer@guardian.co.uk>
2021-07-08 10:19:58 +0100

    adding tokyo 2020 badge

--- common/app/model/Badges.scala
+++ common/app/model/Badges.scala
@@ -92,6 +92,8 @@ object Badges {
     Badge("media/series/guardian-200", Static("images/badges/anniversary200.svg"))
   val euro2020 =
     Badge("football/euro-2020", Static("images/badges/euro-2020.svg"))
+  val tokyo2020 =
+    Badge("sport/olympic-games-2020", Static("images/badges/tokyo-2020.svg"))
 
   val allBadges = Seq(
     newArrivals,
@@ -137,6 +139,7 @@ object Badges {
     dreamsInterrupted,
     anniversary200,
     euro2020,
+    tokyo2020,
   )
 
   def badgeFor(c: ContentType): Option[Badge] = {
