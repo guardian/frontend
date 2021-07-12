e32877bea83add3800ccb63ceff9637854f8fedf
Peter Colley <zvvvvt@gmail.com>
2021-07-12 11:40:29 +0100

    turn off us mandatory gate

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -3,7 +3,6 @@ import { puzzlesBanner } from 'common/modules/experiments/tests/puzzles-banner';
 import { remoteRRHeaderLinksTest } from 'common/modules/experiments/tests/remote-header-test';
 import { signInGateMainControl } from 'common/modules/experiments/tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from 'common/modules/experiments/tests/sign-in-gate-main-variant';
-import { signInGateUsMandatory } from 'common/modules/experiments/tests/sign-in-gate-us-mandatory';
 import { commercialPartner } from './tests/commercial-partner';
 
 // keep in sync with ab-tests in dotcom-rendering
@@ -12,7 +11,6 @@ export const concurrentTests: readonly ABTest[] = [
 	commercialPartner,
 	signInGateMainVariant,
 	signInGateMainControl,
-	signInGateUsMandatory,
 	puzzlesBanner,
 	remoteRRHeaderLinksTest,
 ];
