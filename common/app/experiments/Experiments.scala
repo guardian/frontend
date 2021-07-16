3aeebb7f6b16ec18f67a62bee89453f6fe80ea62
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2021-07-16 14:27:53 +0100

    create back end AB test

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -30,3 +30,12 @@ object InteractiveLibrarianExp
       sellByDate = LocalDate.of(2021, 8, 31),
       participationGroup = Perc0B,
     )
+
+object TopAboveNavHeight
+    extends Experiment(
+      name = "top-above-nav-height",
+      description = "Set minimum height of top-above-nav ad slot to 250px rather than current height of 90px",
+      owners = Seq(Owner.withGithub("zekehuntergreen")),
+      sellByDate = LocalDate.of(2021, 8, 31),
+      participationGroup = Perc1A,
+    )
