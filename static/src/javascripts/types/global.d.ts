3229766aafbe649287ecd2c29202f62f4cbf8884
Max Duval <max.duval@guardian.co.uk>
2021-07-23 12:42:10 +0100

    convert prebid/prebid to typescript

--- static/src/javascripts/types/global.d.ts
+++ static/src/javascripts/types/global.d.ts
@@ -69,7 +69,8 @@ interface Config {
 
 interface PageConfig extends CommercialPageConfig {
 	isSensitive: boolean;
-	isFront: boolean; // https://github.com/guardian/frontend/blob/201cc7/common/app/model/meta.scala#L352
+	isFront: boolean; // https://github.com/guardian/frontend/blob/201cc764/common/app/model/meta.scala#L352
+	ajaxUrl: string; // https://github.com/guardian/frontend/blob/33db7bbd/common/app/views/support/JavaScriptPage.scala#L72
 }
 
 interface Ophan {
