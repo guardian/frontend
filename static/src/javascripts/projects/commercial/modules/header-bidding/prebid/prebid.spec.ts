007cb0fa19828b456d32d05b6cfb07532b1e065c
Max Duval <max.duval@guardian.co.uk>
2021-07-26 12:04:07 +0100

    format with prettier

--- static/src/javascripts/projects/commercial/modules/header-bidding/prebid/prebid.spec.ts
+++ static/src/javascripts/projects/commercial/modules/header-bidding/prebid/prebid.spec.ts
@@ -11,142 +11,142 @@ const getAdvertById = getAdvertById_ as jest.Mock;
 jest.mock('../../../../../lib/raven');
 
 jest.mock('../../dfp/Advert', () =>
-    jest.fn().mockImplementation(() => ({ advert: jest.fn() }))
+	jest.fn().mockImplementation(() => ({ advert: jest.fn() })),
 );
 
 jest.mock('./bid-config', () => ({
-    bids: jest.fn(),
+	bids: jest.fn(),
 }));
 
 jest.mock('../../dfp/get-advert-by-id', () => ({
-    getAdvertById: jest.fn(),
+	getAdvertById: jest.fn(),
 }));
 
 jest.mock('../../../../common/modules/experiments/ab', () => ({
-    isInVariantSynchronous: jest.fn(
-        (_testId, variantId) => variantId === 'variant'
-    ),
+	isInVariantSynchronous: jest.fn(
+		(_testId, variantId) => variantId === 'variant',
+	),
 }));
 
 const resetPrebid = () => {
-    delete window.pbjs;
-    jest.resetModules();
-    jest.requireActual('prebid.js/build/dist/prebid');
-}
+	delete window.pbjs;
+	jest.resetModules();
+	jest.requireActual('prebid.js/build/dist/prebid');
+};
 
 describe('initialise', () => {
-    beforeEach(() => {
-        resetPrebid();
-        config.set('switches.consentManagement', true);
-        config.set('switches.prebidUserSync', true);
-        config.set('switches.prebidAppNexus', true);
-        config.set('switches.prebidSonobi', true);
-        config.set('switches.prebidXaxis', true);
-        getAdvertById.mockReset();
-    });
+	beforeEach(() => {
+		resetPrebid();
+		config.set('switches.consentManagement', true);
+		config.set('switches.prebidUserSync', true);
+		config.set('switches.prebidAppNexus', true);
+		config.set('switches.prebidSonobi', true);
+		config.set('switches.prebidXaxis', true);
+		getAdvertById.mockReset();
+	});
 
-    test('should generate correct Prebid config when all switches on', () => {
-        prebid.initialise(window, 'tcfv2');
-        expect(window.pbjs?.getConfig()).toEqual({
-            _auctionOptions: {},
-            _bidderSequence: 'random',
-            _bidderTimeout: 1500,
-            _customPriceBucket: {
-                buckets: [
-                    {
-                        increment: 0.01,
-                        max: 100,
-                    },
-                    {
-                        increment: 1,
-                        max: 500,
-                    },
-                ],
-            },
-            _debug: false,
-            _deviceAccess: true,
-            _disableAjaxTimeout: false,
-            _maxNestedIframes: 10,
-            _mediaTypePriceGranularity: {},
-            _priceGranularity: 'custom',
-            _publisherDomain: 'http://testurl.theguardian.com',
-            _sendAllBids: true,
-            _timeoutBuffer: 400,
-            _useBidCache: false,
-            auctionOptions: {},
-            bidderSequence: 'random',
-            bidderTimeout: 1500,
-            consentManagement: {
-                gdpr: {
-                    cmpApi: 'iab',
-                    defaultGdprScope: true,
-                    timeout: 200,
-                }
-            },
-            customPriceBucket: {
-                buckets: [
-                    {
-                        increment: 0.01,
-                        max: 100,
-                    },
-                    {
-                        increment: 1,
-                        max: 500,
-                    },
-                ],
-            },
-            debug: false,
-            deviceAccess: true,
-            disableAjaxTimeout: false,
-            enableSendAllBids: true,
-            maxNestedIframes: 10,
-            mediaTypePriceGranularity: {},
-            priceGranularity: 'custom',
-            publisherDomain: 'http://testurl.theguardian.com',
-            s2sConfig: {
-                adapter: 'prebidServer',
-                adapterOptions: {},
-                enabled: false,
-                maxBids: 1,
-                syncUrlModifier: {},
-                timeout: 1000,
-            },
-            timeoutBuffer: 400,
-            useBidCache: false,
-            userSync: {
-                syncDelay: 3000,
-                syncEnabled: true,
-                syncsPerBidder: 0,
-                auctionDelay: 0,
-                filterSettings: {
-                    all: {
-                        bidders: '*',
-                        filter: 'include',
-                    },
-                },
-            },
-        });
-    });
+	test('should generate correct Prebid config when all switches on', () => {
+		prebid.initialise(window, 'tcfv2');
+		expect(window.pbjs?.getConfig()).toEqual({
+			_auctionOptions: {},
+			_bidderSequence: 'random',
+			_bidderTimeout: 1500,
+			_customPriceBucket: {
+				buckets: [
+					{
+						increment: 0.01,
+						max: 100,
+					},
+					{
+						increment: 1,
+						max: 500,
+					},
+				],
+			},
+			_debug: false,
+			_deviceAccess: true,
+			_disableAjaxTimeout: false,
+			_maxNestedIframes: 10,
+			_mediaTypePriceGranularity: {},
+			_priceGranularity: 'custom',
+			_publisherDomain: 'http://testurl.theguardian.com',
+			_sendAllBids: true,
+			_timeoutBuffer: 400,
+			_useBidCache: false,
+			auctionOptions: {},
+			bidderSequence: 'random',
+			bidderTimeout: 1500,
+			consentManagement: {
+				gdpr: {
+					cmpApi: 'iab',
+					defaultGdprScope: true,
+					timeout: 200,
+				},
+			},
+			customPriceBucket: {
+				buckets: [
+					{
+						increment: 0.01,
+						max: 100,
+					},
+					{
+						increment: 1,
+						max: 500,
+					},
+				],
+			},
+			debug: false,
+			deviceAccess: true,
+			disableAjaxTimeout: false,
+			enableSendAllBids: true,
+			maxNestedIframes: 10,
+			mediaTypePriceGranularity: {},
+			priceGranularity: 'custom',
+			publisherDomain: 'http://testurl.theguardian.com',
+			s2sConfig: {
+				adapter: 'prebidServer',
+				adapterOptions: {},
+				enabled: false,
+				maxBids: 1,
+				syncUrlModifier: {},
+				timeout: 1000,
+			},
+			timeoutBuffer: 400,
+			useBidCache: false,
+			userSync: {
+				syncDelay: 3000,
+				syncEnabled: true,
+				syncsPerBidder: 0,
+				auctionDelay: 0,
+				filterSettings: {
+					all: {
+						bidders: '*',
+						filter: 'include',
+					},
+				},
+			},
+		});
+	});
 
-    test('should generate correct Prebid config consent management in CCPA', () => {
-        prebid.initialise(window, 'ccpa');
-        expect(window.pbjs?.getConfig('consentManagement')).toEqual({
-            usp: {
-                cmpApi: 'iab',
-                timeout: 1500,
-            }
-        })
-    })
+	test('should generate correct Prebid config consent management in CCPA', () => {
+		prebid.initialise(window, 'ccpa');
+		expect(window.pbjs?.getConfig('consentManagement')).toEqual({
+			usp: {
+				cmpApi: 'iab',
+				timeout: 1500,
+			},
+		});
+	});
 
-    test('should generate correct Prebid config consent management in AUS', () => {
-        prebid.initialise(window, 'aus');
-        expect(window.pbjs?.getConfig('consentManagement')).toEqual({
-            usp: {
-                cmpApi: 'iab',
-                timeout: 1500,
-            }
-        })
-    })
+	test('should generate correct Prebid config consent management in AUS', () => {
+		prebid.initialise(window, 'aus');
+		expect(window.pbjs?.getConfig('consentManagement')).toEqual({
+			usp: {
+				cmpApi: 'iab',
+				timeout: 1500,
+			},
+		});
+	});
 
 	test('should generate correct Prebid config when consent management off', () => {
 		config.set('switches.consentManagement', false);
@@ -154,45 +154,45 @@ describe('initialise', () => {
 		expect(window.pbjs?.getConfig('consentManagement')).toBeUndefined();
 	});
 
-    test('should generate correct bidder settings', () => {
-        prebid.initialise(window);
-        expect(window.pbjs?.bidderSettings.xhb).toHaveProperty(
-            'adserverTargeting'
-        );
-    });
+	test('should generate correct bidder settings', () => {
+		prebid.initialise(window);
+		expect(window.pbjs?.bidderSettings.xhb).toHaveProperty(
+			'adserverTargeting',
+		);
+	});
 
-    describe('bidderSettings', () => {
+	describe('bidderSettings', () => {
+		beforeEach(() => {
+			config.set('switches.prebidXaxis', false);
+			config.set('switches.prebidImproveDigital', false);
+		});
 
-        beforeEach(() => {
-            config.set('switches.prebidXaxis', false);
-            config.set('switches.prebidImproveDigital', false);
-        })
+		test('should generate correct bidder settings when bidder switches are off', () => {
+			prebid.initialise(window);
+			expect(window.pbjs?.bidderSettings).toEqual({});
+		});
 
-        test('should generate correct bidder settings when bidder switches are off', () => {
-            prebid.initialise(window);
-            expect(window.pbjs?.bidderSettings).toEqual({});
-        });
+		test('should generate correct bidder settings when Xaxis is on', () => {
+			config.set('switches.prebidXaxis', true);
+			prebid.initialise(window);
+			expect(window.pbjs?.bidderSettings).toHaveProperty('xhb');
+		});
 
-        test('should generate correct bidder settings when Xaxis is on', () => {
-            config.set('switches.prebidXaxis', true);
-            prebid.initialise(window);
-            expect(window.pbjs?.bidderSettings).toHaveProperty('xhb');
-        });
+		test('should generate correct bidder settings when Improve Digital is on', () => {
+			config.set('switches.prebidImproveDigital', true);
+			prebid.initialise(window);
+			expect(window.pbjs?.bidderSettings).toHaveProperty(
+				'improvedigital',
+			);
+		});
+	});
 
-        test('should generate correct bidder settings when Improve Digital is on', () => {
-            config.set('switches.prebidImproveDigital', true);
-            prebid.initialise(window);
-            expect(window.pbjs?.bidderSettings).toHaveProperty('improvedigital');
-        });
-
-    })
-
-    test('should generate correct Prebid config when user-sync off', () => {
-        config.set('switches.prebidUserSync', false);
-        prebid.initialise(window);
+	test('should generate correct Prebid config when user-sync off', () => {
+		config.set('switches.prebidUserSync', false);
+		prebid.initialise(window);
 		// @ts-expect-error -- it works with the alternative type
-        expect(window.pbjs?.getConfig().userSync.syncEnabled).toEqual(false);
-    });
+		expect(window.pbjs?.getConfig().userSync.syncEnabled).toEqual(false);
+	});
 
 	test('should generate correct Prebid config when both Permutive and prebidPermutiveAudience are true', () => {
 		config.set('switches.permutive', true);
@@ -317,5 +317,4 @@ describe('initialise', () => {
 			},
 		);
 	});
-
 });
