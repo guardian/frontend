184d174f5f47efc5f1a4e64a8dc9140a2d30719e
nic.long <nicolas.long@theguardian.com>
2021-07-27 16:59:46 +0100

    Revert "Filter out unsafe content (#24026)" (#24032)
    This reverts commit b3cfe7235c726454bfbc67ea1e03491d1b683bd7.

--- article/app/utils/UnsafeContent.scala
+++
@@ -1,9 +0,0 @@
-package utils
-
-import common.GuLogging
-
-object UnsafeContent extends GuLogging {
-  def isVidme(content: String): Boolean = {
-    content.contains("vid.me") // todo make case insensitive
-  }
-}
