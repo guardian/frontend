d3b6f84908e6ea1014c56292ec9e825f3346852b
Mario Savarese <mario.savarese@theguardian.com>
2021-07-30 17:19:29 +0100

    convert to TS and remove all references to videojs

---
+++ static/src/javascripts/projects/commercial/modules/hosted/video.ts
@@ -0,0 +1,15 @@
+import { initHostedYoutube } from 'commercial/modules/hosted/youtube';
+
+export const initHostedVideo = (): Promise<void> => {
+	const youtubeIframe = document.querySelectorAll('.js-hosted-youtube-video');
+
+	if (!youtubeIframe.length) {
+		// Halt execution if there are no video containers on the page.
+		return Promise.resolve();
+	}
+
+	void new Promise(() => {
+		Array.from(youtubeIframe).forEach(initHostedYoutube);
+	});
+	return Promise.resolve();
+};
