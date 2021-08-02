5f6f371cb03c9ca2951d65b8dff6767febbaadc3
Max Duval <max.duval@guardian.co.uk>
2021-08-02 10:36:17 +0100

    convert `$` to native JS

--- static/src/javascripts/projects/commercial/adblock-ask.ts
+++ static/src/javascripts/projects/commercial/adblock-ask.ts
@@ -1,4 +1,3 @@
-import $ from 'lib/$';
 import config from '../../lib/config';
 import fastdom from '../../lib/fastdom-promise';
 import { pageShouldHideReaderRevenue } from '../common/modules/commercial/contributions-utilities';
@@ -7,7 +6,9 @@ import { shouldHideSupportMessaging } from '../common/modules/commercial/user-fe
 
 const supportUrl = `${supportSubscribeDigitalURL()}?acquisitionData=%7B%22componentType%22%3A%22ACQUISITIONS_OTHER%22%2C%22source%22%3A%22GUARDIAN_WEB%22%2C%22campaignCode%22%3A%22shady_pie_open_2019%22%2C%22componentId%22%3A%22shady_pie_open_2019%22%7D&INTCMP=shady_pie_open_2019`;
 
-const askHtml = `
+const askHtml = document.createElement('a');
+askHtml.classList.add('contributions__adblock');
+askHtml.innerHTML = `
 <div class="contributions__adblock">
     <a href="${supportUrl}">
         <img src="https://uploads.guim.co.uk/2020/10/02/Digisubs_MPU_c1_my_opt.png" width="300" alt="" />
@@ -23,16 +24,12 @@ const canShow = () =>
 export const initAdblockAsk = (): Promise<void> => {
 	if (!canShow()) return Promise.resolve();
 
-	return (
-		fastdom
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-return -- TODO: fix it
-			.measure(() => $('.js-aside-slot-container'))
-			.then((slot) => {
-				if (!slot) return;
-				return fastdom.mutate(() => {
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call -- TODO: fix it
-					slot.append(askHtml);
-				});
-			})
-	);
+	return fastdom
+		.measure(() => document.querySelector('.js-aside-slot-container'))
+		.then((slot) => {
+			if (!slot) return;
+			return fastdom.mutate(() => {
+				slot.append(askHtml);
+			});
+		});
 };
