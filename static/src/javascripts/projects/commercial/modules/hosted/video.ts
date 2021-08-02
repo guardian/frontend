e97f5c2678de9425439a08e7896455b1c510c14c
Mario Savarese <mario.savarese@theguardian.com>
2021-08-02 12:48:43 +0100

    add type annotation

--- static/src/javascripts/projects/commercial/modules/hosted/video.ts
+++ static/src/javascripts/projects/commercial/modules/hosted/video.ts
@@ -1,7 +1,9 @@
 import { initHostedYoutube } from 'commercial/modules/hosted/youtube';
 
 export const initHostedVideo = (): Promise<void> => {
-	const youtubeIframe = document.querySelectorAll('.js-hosted-youtube-video');
+	const youtubeIframe: NodeListOf<HTMLElement> = document.querySelectorAll(
+		'.js-hosted-youtube-video',
+	);
 
 	if (!youtubeIframe.length) {
 		// Halt execution if there are no video containers on the page.
