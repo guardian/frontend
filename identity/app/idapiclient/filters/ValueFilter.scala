a1d4e36e68085992ac7d81c899fbbd5e586ae1f2
Rebecca Thompson <becca.al.thompson@gmail.com>
2021-08-10 16:56:12 +0100

    delete API filters - the only place they are referenced is in tests

--- identity/app/idapiclient/filters/ValueFilter.scala
+++
@@ -1,7 +0,0 @@
-package client.filters
-
-case class ValueFilter(path: String, value: String) extends ApiFilter {
-  override def parameters: Iterable[(String, String)] = {
-    List(("filter", "%s:%s".format(path, value)))
-  }
-}
