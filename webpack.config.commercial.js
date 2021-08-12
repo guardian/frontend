bc4e3154b78a2cfd8aef3a10d0690d3e93aa88e2
Max Duval <max.duval@guardian.co.uk>
2021-08-12 20:33:55 +0100

    remove unused comment

--- webpack.config.commercial.js
+++ webpack.config.commercial.js
@@ -14,7 +14,6 @@ config.entry = {
     ),
 };
 
-// The Ophan alias removes duplicating the js in the commercial bundle sent to DCR.
 module.exports = webpackMerge.smart(config, {
     output: {
         path: path.join(__dirname, 'static', 'target', 'javascripts', 'commercial'),
