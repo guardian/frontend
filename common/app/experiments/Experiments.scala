cb0f480ceed0bad4ef38bbf7ecadc5230d98ea0a
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2021-08-13 15:10:24 +0100

    Revert "Introduce AB tests increasing top-above-nav ad slot height to improve core web vitals"

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -8,9 +8,6 @@ object ActiveExperiments extends ExperimentsDefinition {
   override val allExperiments: Set[Experiment] = Set(
     LiveblogRendering,
     InteractiveLibrarianExp,
-    TopAboveNavHeight150,
-    TopAboveNavHeight200,
-    TopAboveNavHeight250,
   )
 
   implicit val canCheckExperiment = new CanCheckExperiment(this)
@@ -32,31 +29,4 @@ object InteractiveLibrarianExp
       owners = Seq(Owner.withGithub("shtukas")),
       sellByDate = LocalDate.of(2021, 8, 31),
       participationGroup = Perc0B,
-    )
-
-object TopAboveNavHeight150
-    extends Experiment(
-      name = "top-above-nav-height-150",
-      description = "Set minimum height of top-above-nav ad slot to 150px rather than current height of 90px",
-      owners = Seq(Owner.withGithub("zekehuntergreen")),
-      sellByDate = LocalDate.of(2021, 8, 31),
-      participationGroup = Perc1A,
-    )
-
-object TopAboveNavHeight200
-    extends Experiment(
-      name = "top-above-nav-height-200",
-      description = "Set minimum height of top-above-nav ad slot to 200px rather than current height of 90px",
-      owners = Seq(Owner.withGithub("zekehuntergreen")),
-      sellByDate = LocalDate.of(2021, 8, 31),
-      participationGroup = Perc1B,
-    )
-
-object TopAboveNavHeight250
-    extends Experiment(
-      name = "top-above-nav-height-250",
-      description = "Set minimum height of top-above-nav ad slot to 250px rather than current height of 90px",
-      owners = Seq(Owner.withGithub("zekehuntergreen")),
-      sellByDate = LocalDate.of(2021, 8, 31),
-      participationGroup = Perc1C,
     )
