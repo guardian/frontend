12b4d744e865736a1a17c0552e89254763c1c472
Max Duval <max.duval@guardian.co.uk>
2021-08-17 14:19:50 +0100

    prevent clashes with main bundle
    added comments to explain how and why

--- webpack.config.commercial.js
+++ webpack.config.commercial.js
@@ -2,9 +2,13 @@ const path = require('path');
 const webpackMerge = require('webpack-merge');
 const config = require('./webpack.config.js');
 
+const standaloneEntry = 'commercial';
+if(Object.keys(config.entry).includes(standaloneEntry))
+    throw new Error(`Conflicting entry name: ${standaloneEntry}`)
+
 // override JS entry points
 config.entry = {
-	'commercial-standalone': path.join(
+	[standaloneEntry]: path.join(
 		__dirname,
 		'static',
 		'src',
@@ -16,6 +20,14 @@ config.entry = {
 
 module.exports = webpackMerge.smart(config, {
 	output: {
+        /**
+         * To prevent clashes with the main bundle, the JSONP loading function
+         * needs to be assigned a unique name.
+         *
+         * Typical errors were “Cannot read property `call` of undefined”
+         *
+         * **Wepback 4 only.** Renamed in Wepback 5 `chunkLoadingGlobal`
+         */
 		jsonpFunction: 'commercialJsonp',
 		path: path.join(
 			__dirname,
