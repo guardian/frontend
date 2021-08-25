587142ea1ae0d319cb9f2c8a1ad8af45632ab18a
David Lawes <david.lawes@guardian.co.uk>
2021-08-25 12:29:35 +0100

    add tests for cleaning

--- common/app/pagepresser/InteractiveImmersiveHtmlCleaner.scala
+++ common/app/pagepresser/InteractiveImmersiveHtmlCleaner.scala
@@ -14,8 +14,9 @@ object InteractiveImmersiveHtmlCleaner extends HtmlCleaner with implicits.WSRequ
   }
 
   override def clean(document: Document, convertToHttps: Boolean): Document = {
-    println("cleaning interactive immersive")
     universalClean(document)
+    removeTopBannerAds(document)
+    removeReaderRevenueCallouts(document)
     removeScripts(document)
     removeByTagName(document, "noscript")
     if (convertToHttps) secureDocument(document)
@@ -25,6 +26,39 @@ object InteractiveImmersiveHtmlCleaner extends HtmlCleaner with implicits.WSRequ
   override def removeScripts(document: Document): Document = {
     // TODO: decide how to handle scripts
     document
+  }
+
+  def removeTopBannerAds(document: Document): Document = {
+    removeByClass(document, "top-banner-ad-container js-top-banner")
+  }
+
+  def removeHeaderCallout(document: Document): Document = {
+    removeByClass(document, "new-header__cta-bar")
+  }
+
+  def removeFooterCallout(document: Document): Document = {
+    document
+      .getElementsByAttributeValue("data-link-name", "footer : contribute-cta")
+      .asScala
+      .foreach(_.remove())
+
+    document
+      .getElementsByAttributeValue("data-link-name", "footer : subscribe-cta")
+      .asScala
+      .foreach(_.remove())
+
+    // TODO: improve this?
+    document
+      .getElementsByClass("cta-bar__text")
+      .asScala
+      .foreach(_.remove())
+
+    document
+  }
+
+  def removeReaderRevenueCallouts(document: Document): Document = {
+    removeHeaderCallout(document)
+    removeFooterCallout(document)
   }
 
 }
---
+++ common/test/common/HtmlCleanerTest.scala
@@ -0,0 +1,103 @@
+package common
+
+import org.jsoup.Jsoup
+import org.scalatest.{FlatSpec, Matchers}
+
+import pagepresser.{InteractiveImmersiveHtmlCleaner}
+
+class HtmlCleanerTest extends FlatSpec with Matchers {
+
+  "InteractiveImmersiveHtmlCleaner" should "only clean interactive immersives" in {
+    val doc = Jsoup.parse("""<html>
+                          |<head>
+                          |<link rel="canonical" href="https://www.theguardian.com/world/ng-interactive/2020/apr/08/coronavirus-100">
+                          |</head>
+                          |<body class="is-immersive is-immersive-interactive"></body>
+                          |</html>""")
+
+    InteractiveImmersiveHtmlCleaner.canClean(doc) should be(true)
+  }
+
+ "Interactive immersives" should "have top banner ads removed" in {
+    val doc = Jsoup.parse("""<html>
+              |<body>
+              |<div id="bannerandheader">
+              |<div class="top-banner-ad-container js-top-banner"></div>
+              |<header></header>
+              |</div>
+              |</body>
+              |</html>""".stripMargin)
+
+    val want = Jsoup.parse("""<html>
+               |<body>
+               |<div id="bannerandheader">
+               |<header></header>
+               |</div>
+               |</body>
+               |</html>""".stripMargin).toString
+
+    InteractiveImmersiveHtmlCleaner.clean(doc, true).toString should equal(want)
+  }
+
+  it should "have reader revenue callouts in the header removed" in {
+    val doc = Jsoup.parse("""<html>
+              |<body>
+              |<div id="bannerandheader">
+              |<header>
+              |<nav>
+              |<div class="new-header__cta-bar"></div>
+              |</nav>
+              |</header>
+              |</div>
+              |</body>
+              |</html>""".stripMargin)
+
+    val want = Jsoup.parse("""<html>
+               |<body>
+               |<div id="bannerandheader">
+               |<header>
+               |<nav>
+               |</nav>
+               |</header>
+               |</div>
+               |</body>
+               |</html>""".stripMargin).toString
+
+    InteractiveImmersiveHtmlCleaner.clean(doc, true).toString should equal(want)
+  }
+
+  it should "have reader revenue callouts in the footer removed" in {
+    val doc = Jsoup.parse("""<html>
+              |<body>
+              |<footer>
+              |<div class="l-footer__secondary js-footer__secondary">
+              |<div class="colophon__lists-container">
+              |<ul class="colophon__list"></ul>
+              |<div class="colophon__list">
+              |<div class="cta-bar__text" /></div>
+              |<a class="cta-bar__cta js-acquisition-link" data-link-name="footer : contribute-cta"></a>
+              |<a class="cta-bar__cta js-acquisition-link" data-link-name="footer : subscribe-cta"></a>
+              |</div>
+              |</div>
+              |</footer>
+              |</body>
+              |</html>""".stripMargin)
+
+    val want = Jsoup.parse("""<html>
+              |<body>
+              |<footer>
+              |<div class="l-footer__secondary js-footer__secondary">
+              |<div class="colophon__lists-container">
+              |<ul class="colophon__list"></ul>
+              |<div class="colophon__list">
+              |</div>
+              |</div>
+              |</div>
+              |</footer>
+              |</body>
+              |</html>""".stripMargin).toString
+
+    InteractiveImmersiveHtmlCleaner.clean(doc, true).toString should equal(want)
+  }
+
+}
