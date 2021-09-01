953fac391b6d1de97efae3399d3280b5c14168f2
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-09-01 17:12:58 +0100

    Clean up; last usage of HTML takeFirstNElements.

--- common/app/common/HTML.scala
+++ common/app/common/HTML.scala
@@ -3,19 +3,7 @@ package common
 import org.apache.commons.lang.StringEscapeUtils
 import org.jsoup.Jsoup
 import org.jsoup.safety.Whitelist
-import scala.collection.JavaConverters._
 
 object HTML {
   def noHtml(headline: String): String = StringEscapeUtils.unescapeHtml(Jsoup.clean(headline, Whitelist.none()))
-
-  def takeFirstNElements(html: String, n: Int): String =
-    Jsoup
-      .parse(html)
-      .select("body")
-      .iterator
-      .asScala
-      .toList
-      .flatMap(_.children().asScala.take(n))
-      .map(_.toString)
-      .mkString("")
 }
