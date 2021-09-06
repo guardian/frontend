3b8d0b6f66fa57acb7501dced9735f1c0ae38414
Max Duval <max.duval@guardian.co.uk>
2021-09-06 08:17:42 +0100

    convert page-skin to TS

--- static/src/javascripts/types/global.d.ts
+++ static/src/javascripts/types/global.d.ts
@@ -74,6 +74,7 @@ interface PageConfig extends CommercialPageConfig {
 	isFront: boolean; // https://github.com/guardian/frontend/blob/201cc764/common/app/model/meta.scala#L352
 	ajaxUrl: string; // https://github.com/guardian/frontend/blob/33db7bbd/common/app/views/support/JavaScriptPage.scala#L72
 	isHosted: boolean; // https://github.com/guardian/frontend/blob/66afe02e/common/app/common/commercial/hosted/HostedMetadata.scala#L37
+	hasPageSkin: boolean; //https://github.com/guardian/frontend/blob/b952f6b9/common/app/views/support/JavaScriptPage.scala#L48
 	assetsPath: string;
 	frontendAssetsFullURL?: string; // only in DCR
 }
