683131d1e5e7fd1c5e4c0eb0679ba6b0d7c6b0b8
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-09-06 11:55:53 +0100

    Nudge start date for Facebook https urls.

--- common/app/model/meta.scala
+++ common/app/model/meta.scala
@@ -130,7 +130,7 @@ final case class Fields(
 
 object MetaData {
 
-  val StartDateForHttpsFacebookUrls: OffsetDateTime = OffsetDateTime.of(2021, 9, 3, 12, 0, 0, 0, ZoneOffset.UTC)
+  val StartDateForHttpsFacebookUrls: OffsetDateTime = OffsetDateTime.of(2021, 9, 6, 16, 0, 0, 0, ZoneOffset.UTC)
 
   def make(
       id: String,
