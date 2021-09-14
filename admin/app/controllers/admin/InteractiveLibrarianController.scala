c38edc8486058fb6e16bd3e790f235d25c2ea136
David Lawes <david.lawes@guardian.co.uk>
2021-09-14 11:30:09 +0100

    add worker to support sending messages to browser

--- admin/app/controllers/admin/InteractiveLibrarianController.scala
+++ admin/app/controllers/admin/InteractiveLibrarianController.scala
@@ -1,23 +1,44 @@
 package controllers.admin
 
+import akka.stream.scaladsl.Source
 import common.{AkkaAsync, GuLogging, ImplicitControllerExecutionContext}
 import conf.switches.Switches.InteractiveLibrarianAdminRoutes
 import model.ApplicationContext
+import play.api.http.ContentTypes
+import play.api.libs.EventSource
+import play.api.libs.EventSource.EventDataExtractor
 import play.api.libs.ws.WSClient
 import play.api.mvc._
 import services.dotcomrendering.InteractiveLibrarian
 
 import scala.concurrent.Future
 import java.net.URL
+import scala.util.Try
+import akka.stream.Materializer
+import akka.http.scaladsl.model.headers.LinkParams
+
+sealed trait PressResult { val path: String }
+final case class Success(path: String) extends PressResult
+final case class Failure(path: String, error: Throwable) extends PressResult
+
+object PressResult {
+  implicit val pressResultEvents: EventDataExtractor[PressResult] = EventDataExtractor({
+    case Success(path)        => s"Press succeeded for path: '$path'."
+    case Failure(path, error) => s"Press failed for path: '$path'. Error was: ${error.getMessage}."
+  })
+}
 
 class InteractiveLibrarianController(
-    wsClient: WSClient,
-    akkaAsync: AkkaAsync,
-    val controllerComponents: ControllerComponents,
-)(implicit context: ApplicationContext)
-    extends BaseController
-    with GuLogging
-    with ImplicitControllerExecutionContext {
+  wsClient: WSClient,
+  akkaAsync: AkkaAsync,
+  val controllerComponents: ControllerComponents,
+  )(implicit context: ApplicationContext, materializer: Materializer)
+  extends BaseController
+  with GuLogging
+  with ImplicitControllerExecutionContext {
+
+  val events = Source.queue[PressResult](bufferSize = 100)
+  val (queue, _) = events.preMaterialize()
 
   def pressForm(urlMsgs: List[String] = List.empty, fileMsgs: List[String] = List.empty): Action[AnyContent] =
     Action { implicit request =>
@@ -26,27 +47,56 @@ class InteractiveLibrarianController(
 
   def press(): Action[AnyContent] =
     Action { implicit request =>
-      // return pressInteractiveWaiting page (prevent form resubmission)
-
-      // start async process
-      // when async process complete return to pressInteractive page
-
       val body = request.body
-      val result = body.asFormUrlEncoded
+
+      println("press")
+      // for now repeating the function to extract path so i don't
+      // break the below, but also have a path to pass to the page
+      // that displays the waiting state
+      val path = body.asFormUrlEncoded.map { form =>
+        form("interactiveUrl").map { interactiveUrl =>
+          interactiveUrl.trim match {
+            case url if url.nonEmpty => getPath(url)
+            case _ => "Please specify a URL"
+          }
+        }
+      }
+
+      println(s"path ${path}")
+
+      body.asFormUrlEncoded
         .map { form =>
           form("interactiveUrl").map { interactiveUrl =>
             interactiveUrl.trim match {
               case url if url.nonEmpty =>
                 val path = getPath(url)
-                val res = InteractiveLibrarian.pressLiveContents(wsClient, path).map { message =>
-                  println(s"message ${message}")
-                  if (message.contains("Operation successful")) {
-                    val res = InteractiveLibrarian.readCleanWrite(path)
-                    println(s"clean message ${res}")
+
+                val result = for {
+                  _ <- InteractiveLibrarian.pressLiveContents(wsClient, path)
+                  (ok, errorMsg) = InteractiveLibrarian.readCleanWrite(path)
+                } yield {
+                  if (ok) {
+                    println("press success")
+                    "Pressed successfully!"
                   } else {
-                    "Failed to press"
+                    errorMsg // TODO make this better
                   }
                 }
+
+                println(s"path inside map ${path}")
+                result.onComplete({
+                  case scala.util.Success(_)     => {
+                    println(s"on complete, about to offer success to queue, $path")
+                    queue.offer(Success(path))
+                    println(s"${queue.size()}")
+                  }
+                  case scala.util.Failure(error) => {
+                    println(s"on complete, about to offer failure to queue, $path")
+                    queue.offer(Failure(path, error))
+                  }
+                }) // add to events queue
+
+                s"$path has been queued for pressing..."
 
               case _ => "URL not specified"
             }
@@ -54,8 +104,26 @@ class InteractiveLibrarianController(
         }
         .map(_.toList)
         .getOrElse(List.empty)
-      println(s"results ${result}")
-      Ok(views.html.pressInteractive())
+
+        println("returning doc")
+        // return page where we wait for results, or offer to press another page
+        // page we display must have js included to listen to events
+        Ok(views.html.pressInteractiveWaiting(path = path))
+    }
+
+  // Returns events for a pressed path. I.e. 'success' or some kind of failure.
+  def pressStatusUpdates(path: String): Action[AnyContent] =
+    Action { implicit request =>
+      val formattedPath = s"/$path"
+
+      val relevantEvents = events.filter(_.path == formattedPath)
+
+      println(s"path used to get events ${formattedPath}")
+      println(s"relevent events ${relevantEvents}")
+      Ok.chunked(relevantEvents via EventSource.flow)
+        .as(ContentTypes.EVENT_STREAM)
+        .withHeaders("Cache-Control" -> "no-cache")
+        .withHeaders("Connection" -> "keep-alive")
     }
 
   def getPath(url: String): String = {
