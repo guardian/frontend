de9424f2e70df751e0635c8c5abe278eed0495ca
Ravi Lal <arelra@users.noreply.github.com>
2021-09-22 16:12:44 +0100

    Convert get-ad-iframe to TypeScript

---
+++ static/src/javascripts/projects/commercial/modules/dfp/get-ad-iframe.ts
@@ -0,0 +1,45 @@
+interface HTMLIFrameElementIE extends HTMLIFrameElement {
+	readyState: string | undefined;
+}
+
+const getAdvertIframe = (adSlot: HTMLElement) =>
+	new Promise((resolve, reject) => {
+		// DFP will sometimes return empty iframes, denoted with a '__hidden__' parameter embedded in its ID.
+		// We need to be sure only to select the ad content frame.
+		const contentFrame = adSlot.querySelector<HTMLIFrameElement>(
+			'iframe:not([id*="__hidden__"])',
+		);
+
+		if (!contentFrame) {
+			reject();
+		} else if (
+			// According to Flow, readyState exists on the Document, not the HTMLIFrameElement
+			// Is this different for old IE?
+			(contentFrame as HTMLIFrameElementIE).readyState &&
+			(contentFrame as HTMLIFrameElementIE).readyState !== 'complete'
+		) {
+			// On IE, wait for the frame to load before interacting with it
+			const getIeIframe = (e: Event) => {
+				const updatedIFrame = e.srcElement as
+					| HTMLIFrameElementIE
+					| undefined;
+
+				if (updatedIFrame && updatedIFrame.readyState === 'complete') {
+					updatedIFrame.removeEventListener(
+						'readystatechange',
+						getIeIframe,
+					);
+					resolve(contentFrame);
+				}
+			};
+
+			contentFrame.addEventListener('readystatechange', getIeIframe);
+		} else {
+			resolve(contentFrame);
+		}
+	});
+
+const getAdIframe = (adSlot: HTMLElement): Promise<boolean> =>
+	getAdvertIframe(adSlot).then(() => Promise.resolve(true));
+
+export { getAdIframe };
