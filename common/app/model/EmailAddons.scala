a78b81dc93554530953c00dd54c9a3505a978739
pradasa <ana.pradas@gmail.com>
2021-09-22 10:29:03 +0100

    added banner for the rural network newsletter

--- common/app/model/EmailAddons.scala
+++ common/app/model/EmailAddons.scala
@@ -427,6 +427,11 @@ case object Documentaries extends FrontEmailMetadata {
   override val banner = Some("documentaries-2.png")
 }
 
+case object TheRuralNetwork extends FrontEmailMetadata {
+  val name = "The rural network"
+  override val banner = Some("rural-network.png")
+}
+
 object EmailAddons {
   val unsubscribePlaceholder = "%%unsub_center_url%%"
 
@@ -507,6 +512,7 @@ object EmailAddons {
     WordOfMouth,
     SavedForLater,
     TheGuideStayingIn,
+    TheRuralNetwork,
     DesignReview,
     Documentaries,
   )
