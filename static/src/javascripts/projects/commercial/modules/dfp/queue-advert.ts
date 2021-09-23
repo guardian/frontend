e43b60d88b25262e1445f33736c1cb1417f8b042
Ravi <7014230+arelra@users.noreply.github.com>
2021-09-23 11:19:15 +0100

    Update static/src/javascripts/projects/commercial/modules/dfp/queue-advert.ts
    Co-authored-by: Max Duval <max.duval@theguardian.com>

--- static/src/javascripts/projects/commercial/modules/dfp/queue-advert.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/queue-advert.ts
@@ -5,7 +5,7 @@ export const queueAdvert = (advert: Advert): void => {
 	dfpEnv.advertsToLoad.push(advert);
 	// Add to the array of ads to be refreshed (when the breakpoint changes)
 	// only if its `data-refresh` attribute isn't set to false.
-	if (advert.node.getAttribute('data-refresh') !== 'false') {
+	if (advert.node.dataset.refresh !== 'false') {
 		dfpEnv.advertsToRefresh.push(advert);
 	}
 };
