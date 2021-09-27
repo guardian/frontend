2a1de23adb6139089b86bab96c2e59a1d724b308
Ravi Lal <arelra@users.noreply.github.com>
2021-09-27 11:31:47 +0100

    Update fill-advert-slots to an async function

--- static/src/javascripts/projects/commercial/modules/dfp/fill-advert-slots.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/fill-advert-slots.ts
@@ -15,47 +15,44 @@ import { queueAdvert } from './queue-advert';
 //  article-body-adverts
 //  liveblog-adverts
 //  high-merch
-const fillAdvertSlots = (): Promise<void> => {
+const fillAdvertSlots = async (): Promise<void> => {
 	// This module has the following strict dependencies. These dependencies must be
 	// fulfilled before fillAdvertSlots can execute reliably. The bootstrap (commercial.js)
 	// initiates these dependencies, to speed up the init process. Bootstrap also captures the module performance.
 	const dependencies = [setupPrebidOnce(), removeDisabledSlots()];
 
-	return Promise.all(dependencies).then(() => {
-		// Quit if ad-free
-		if (commercialFeatures.adFree) {
-			return Promise.resolve();
-		}
-		const isDCRMobile =
-			(config.get('isDotcomRendering', false) as boolean) &&
-			getBreakpoint() === 'mobile';
-		// Get all ad slots
-		const adverts = [
-			...document.querySelectorAll<HTMLElement>(dfpEnv.adSlotSelector),
-		]
-			.filter((adSlot) => !(adSlot.id in dfpEnv.advertIds))
-			// TODO: find cleaner workaround
-			// we need to not init top-above-nav on mobile view in DCR
-			// as the DOM element needs to be removed and replaced to be inline
-			// refer to: 3562dc07-78e9-4507-b922-78b979d4c5cb
-			.filter(
-				(adSlot) =>
-					!(isDCRMobile && adSlot.id === 'dfp-ad--top-above-nav'),
-			)
-			.map((adSlot) => new Advert(adSlot));
-		const currentLength = dfpEnv.adverts.length;
-		dfpEnv.adverts = dfpEnv.adverts.concat(adverts);
-		adverts.forEach((advert, index) => {
-			dfpEnv.advertIds[advert.id] = currentLength + index;
-		});
-		adverts.forEach(queueAdvert);
-
-		if (dfpEnv.shouldLazyLoad()) {
-			displayLazyAds();
-		} else {
-			displayAds();
-		}
+	await Promise.all(dependencies);
+	// Quit if ad-free
+	if (commercialFeatures.adFree) {
+		return Promise.resolve();
+	}
+	const isDCRMobile =
+		(config.get('isDotcomRendering', false) as boolean) &&
+		getBreakpoint() === 'mobile';
+	// Get all ad slots
+	const adverts = [
+		...document.querySelectorAll<HTMLElement>(dfpEnv.adSlotSelector),
+	]
+		.filter((adSlot) => !(adSlot.id in dfpEnv.advertIds))
+		// TODO: find cleaner workaround
+		// we need to not init top-above-nav on mobile view in DCR
+		// as the DOM element needs to be removed and replaced to be inline
+		// refer to: 3562dc07-78e9-4507-b922-78b979d4c5cb
+		.filter(
+			(adSlot) => !(isDCRMobile && adSlot.id === 'dfp-ad--top-above-nav'),
+		)
+		.map((adSlot) => new Advert(adSlot));
+	const currentLength = dfpEnv.adverts.length;
+	dfpEnv.adverts = dfpEnv.adverts.concat(adverts);
+	adverts.forEach((advert, index) => {
+		dfpEnv.advertIds[advert.id] = currentLength + index;
 	});
+	adverts.forEach(queueAdvert);
+	if (dfpEnv.shouldLazyLoad()) {
+		displayLazyAds();
+	} else {
+		displayAds();
+	}
 };
 
 export { fillAdvertSlots };
