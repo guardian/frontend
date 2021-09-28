24ff0edaf6c78f483fe16d1838bf0199a5c6d1ac
Max Duval <max.duval@guardian.co.uk>
2021-09-28 10:07:04 +0100

    convert google analytics to TS

--- static/src/javascripts/projects/common/modules/tracking/google-analytics.js
+++
@@ -1,9 +0,0 @@
-import mediator from 'lib/mediator';
-import {onConsentChange, getConsentFor} from '@guardian/consent-management-platform';
-
-export const init = () => {
-  onConsentChange(state => {
-      const gaHasConsent = getConsentFor('google-analytics', state);
-      mediator.emit('ga:gaConsentChange', gaHasConsent);
-  })
-};
