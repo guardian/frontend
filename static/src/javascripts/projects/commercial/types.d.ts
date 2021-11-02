4b9f20c7cab75b484933bdc4650423653c390e9c
Max Duval <max.duval@theguardian.com>
2021-11-02 09:59:41 +0000

    remove duplicate types
    types from https://developers.google.com/publisher-tag/reference#type-definitions
    are included in `googletag` via @types/gpt-doubleclick

--- static/src/javascripts/projects/commercial/types.d.ts
+++ static/src/javascripts/projects/commercial/types.d.ts
@@ -1,10 +1,6 @@
 type AdSizes = Record<string, AdSize[]>;
 type AdSize = [width: number, height: number] | 'fluid';
 
-type MultiSize = AdSize[];
-type GeneralSize = AdSize | MultiSize;
-type SizeMapping = GeneralSize[];
-
 type ResponseInformation = {
 	advertiserId: string;
 	campaignId: string;
@@ -17,58 +13,6 @@ type SafeFrameConfig = {
 	allowOverlayExpansion: boolean;
 	allowPushExpansion: boolean;
 	sandbox: boolean;
-};
-
-type AddService = (s: string) => Slot;
-type ClearCategoryExclusions = () => Slot;
-type ClearTargeting = (s?: string) => Slot;
-type DefineSizeMapping = (asm: SizeMapping[]) => Slot;
-type Get = (s: string) => string | null | undefined;
-type GetString = () => string;
-type GetStrings = () => string[];
-type GetResponseInformation = () => ResponseInformation | null | undefined;
-type GetTargeting = (s: string) => string[];
-type Set = (s1: string, s2: string) => Slot;
-type SetString = (s: string) => Slot;
-type SetCollapseEmptyDiv = (b1: boolean, b2: boolean) => Slot;
-type SetForceSafeFrame = (b1: boolean) => Slot;
-type SetSafeFrameConfig = (sfc: SafeFrameConfig) => Slot;
-type SetTargeting = (s: string, a: string | string[]) => Slot;
-
-type Slot = {
-	addService: AddService;
-	clearCategoryExclusions: ClearCategoryExclusions;
-	clearTargeting: ClearTargeting;
-	defineSizeMapping: DefineSizeMapping;
-	get: Get;
-	getAdUnitPath: GetString;
-	getAttributeKeys: GetStrings;
-	getCategoryExclusions: GetStrings;
-	getOutOfPage: () => boolean;
-	getResponseInformation: GetResponseInformation;
-	getSlotElementId: GetString;
-	getTargeting: GetTargeting;
-	getTargetingKeys: GetStrings;
-	set: Set;
-	setCategoryExclusion: SetString;
-	setClickUrl: SetString;
-	setCollapseEmptyDiv: SetCollapseEmptyDiv;
-	setForceSafeFrame: SetForceSafeFrame;
-	setSafeFrameConfig: SetSafeFrameConfig;
-	setTargeting: SetTargeting;
-};
-
-type ImpressionViewableEvent = {
-	serviceName: string;
-	slot: Slot;
-};
-
-type ImpressionViewableEventCallback = (arg0: ImpressionViewableEvent) => void;
-
-type SlotVisibilityChangedEvent = {
-	inViewPercentage: number;
-	serviceName: string;
-	slot: Slot;
 };
 
 type GuAdSize = {
