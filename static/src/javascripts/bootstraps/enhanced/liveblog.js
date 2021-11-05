c6151bae46d2cb48999b629ea6b9977ade7fb7b6
Alina Boghiu <alina.boghiu@gmail.com>
2021-11-05 14:26:45 +0000

    adjust filter focus

--- static/src/javascripts/bootstraps/enhanced/liveblog.js
+++ static/src/javascripts/bootstraps/enhanced/liveblog.js
@@ -52,7 +52,7 @@ const keepTimestampsCurrent = () => {
 const setupListeners = () => {
     bean.on(document.body, 'change', '.live-blog__filter-switch-label', () => {
         const hasParam = window.location.search.includes(`filterKeyEvents=true`);
-        const param = `?filterKeyEvents=${hasParam ? 'false' : 'true'}#live-blog-content`;
+        const param = `?filterKeyEvents=${hasParam ? 'false' : 'true'}#liveblog-content`;
         window.location.assign(`${window.location.pathname}${param}`);
     })
 }
