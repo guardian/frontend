0249723b0d7bf373f40135864178d61022f6b541
Chris Jones <christopher.jones@guardian.co.uk>
2021-11-08 16:57:47 +0000

    Extend Prebid timeout AB test

--- static/src/javascripts/projects/common/modules/experiments/tests/prebid-timeout.ts
+++ static/src/javascripts/projects/common/modules/experiments/tests/prebid-timeout.ts
@@ -5,7 +5,7 @@ export const prebidTimeout: ABTest = {
 	id: 'PrebidTimeout',
 	author: 'Chris Jones (@chrislomaxjones)',
 	start: '2021-10-6',
-	expiry: '2021-11-8',
+	expiry: '2021-11-15',
 	audience: 3 / 100,
 	audienceOffset: 0,
 	audienceCriteria: 'All users',
