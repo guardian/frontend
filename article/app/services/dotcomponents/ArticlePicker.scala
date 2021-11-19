78fb2f1a4914943e982d30c21f4d5fb8ef452ac0
Rob Phillips <robert.phillips@guardian.co.uk>
2021-11-19 15:24:14 +0000

    Update ArticlePicker.scala

--- article/app/services/dotcomponents/ArticlePicker.scala
+++ article/app/services/dotcomponents/ArticlePicker.scala
@@ -28,6 +28,7 @@ object ArticlePageChecks {
       "https://interactive.guim.co.uk/page-enhancers/nav/boot.js", //another variant of the contents block
       "https://interactive.guim.co.uk/testing/2020/11/voterSlideshow/boot.js", //why on earth are there so many contents blocks?
       "https://uploads.guim.co.uk/2021/10/15/boot.js", //somehow there are even more
+      "https://interactive.guim.co.uk/embed/2018/02/divider/index.html", //A simple divider that renders fine in DCR
     )
 
     def unsupportedElement(blockElement: BlockElement) =
