606b0689968ba166818dc1eb08c1905d29a450f3
Max Duval <max.duval@theguardian.com>
2021-11-23 16:52:11 +0000

    lint files

--- static/src/javascripts/projects/common/modules/commercial/build-page-targeting.ts
+++ static/src/javascripts/projects/common/modules/commercial/build-page-targeting.ts
@@ -268,11 +268,9 @@ const buildAppNexusTargetingObject = once(
 			pt6: pageTargeting.su,
 			pt7: pageTargeting.bp,
 			pt8: pageTargeting.x, // OpenX cannot handle this being undefined
-			pt9: [
-				pageTargeting.pv,
-				pageTargeting.co,
-				pageTargeting.tn,
-			].join('|'),
+			pt9: [pageTargeting.pv, pageTargeting.co, pageTargeting.tn].join(
+				'|',
+			),
 			permutive: pageTargeting.permutive,
 		}),
 );
