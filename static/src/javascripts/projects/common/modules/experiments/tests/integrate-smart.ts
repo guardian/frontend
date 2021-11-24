90bf5a34f09703b5a857500f68470f9c72d53216
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2021-11-24 11:21:16 +0000

    change smart test audience offset from 96 to 98 to avoid overlap with Criteo

--- static/src/javascripts/projects/common/modules/experiments/tests/integrate-smart.ts
+++ static/src/javascripts/projects/common/modules/experiments/tests/integrate-smart.ts
@@ -7,7 +7,7 @@ export const integrateSmart: ABTest = {
 	start: '2021-11-23',
 	expiry: '2022-01-10',
 	audience: 2 / 100,
-	audienceOffset: 96 / 100,
+	audienceOffset: 98 / 100,
 	audienceCriteria: 'All users',
 	description:
 		'Integrate new Prebid bidder and measure revenue uplift / impact of commercial performance metrics',
