8301574e968f63dbb1fa5ee930c481f7ec319d19
Max Duval <max.duval@theguardian.com>
2021-11-24 14:00:28 +0000

    convert user-features to TS

--- static/src/javascripts/projects/common/modules/commercial/user-features.ts
+++ static/src/javascripts/projects/common/modules/commercial/user-features.ts
@@ -1,8 +1,10 @@
-import { getCookie, removeCookie, addCookie } from '../../../../lib/cookies';
+import { getCookie, isObject } from '@guardian/libs';
+import { noop } from 'lib/noop';
 import config from '../../../../lib/config';
+import { addCookie, removeCookie } from '../../../../lib/cookies';
 import { fetchJson } from '../../../../lib/fetch-json';
-import { isUserLoggedIn } from '../identity/api';
 import { dateDiffDays } from '../../../../lib/time-utils';
+import { isUserLoggedIn } from '../identity/api';
 
 // Persistence keys
 const USER_FEATURES_EXPIRY_COOKIE = 'gu_user_features_expiry';
@@ -35,35 +37,89 @@ const CONTRIBUTIONS_REMINDER_SIGNED_UP = {
 	daysToLive: 90,
 };
 
-const forcedAdFreeMode = !!window.location.hash.match(/[#&]noadsaf(&.*)?$/);
+// TODO: isn’t this duplicated from commercial features? https://git.io/JMvcu
+const forcedAdFreeMode = !!/[#&]noadsaf(&.*)?$/.exec(window.location.hash);
 
 const userHasData = () => {
 	const cookie =
-		getCookie(ACTION_REQUIRED_FOR_COOKIE) ||
-		getCookie(USER_FEATURES_EXPIRY_COOKIE) ||
-		getCookie(PAYING_MEMBER_COOKIE) ||
-		getCookie(RECURRING_CONTRIBUTOR_COOKIE) ||
-		getCookie(ONE_OFF_CONTRIBUTION_DATE_COOKIE) ||
-		getCookie(AD_FREE_USER_COOKIE) ||
-		getCookie(DIGITAL_SUBSCRIBER_COOKIE) ||
-		getCookie(HIDE_SUPPORT_MESSAGING_COOKIE);
+		getCookie({ name: ACTION_REQUIRED_FOR_COOKIE }) ??
+		getCookie({ name: USER_FEATURES_EXPIRY_COOKIE }) ??
+		getCookie({ name: PAYING_MEMBER_COOKIE }) ??
+		getCookie({ name: RECURRING_CONTRIBUTOR_COOKIE }) ??
+		getCookie({ name: ONE_OFF_CONTRIBUTION_DATE_COOKIE }) ??
+		getCookie({ name: AD_FREE_USER_COOKIE }) ??
+		getCookie({ name: DIGITAL_SUBSCRIBER_COOKIE }) ??
+		getCookie({ name: HIDE_SUPPORT_MESSAGING_COOKIE });
 	return !!cookie;
 };
 
-const accountDataUpdateWarning = () => getCookie(ACTION_REQUIRED_FOR_COOKIE);
+const accountDataUpdateWarning = (): string | null =>
+	getCookie({ name: ACTION_REQUIRED_FOR_COOKIE });
 
-const adFreeDataIsPresent = () => {
-	const cookieVal = getCookie(AD_FREE_USER_COOKIE);
+const adFreeDataIsPresent = (): boolean => {
+	const cookieVal = getCookie({ name: AD_FREE_USER_COOKIE });
+	if (!cookieVal) return false;
 	return !Number.isNaN(parseInt(cookieVal, 10));
 };
 
-const timeInDaysFromNow = (daysFromNow) => {
+const timeInDaysFromNow = (daysFromNow: number): string => {
 	const tmpDate = new Date();
 	tmpDate.setDate(tmpDate.getDate() + daysFromNow);
 	return tmpDate.getTime().toString();
 };
 
-const persistResponse = (JsonResponse) => {
+type LocalDate = `${number}-${string}-${string}`;
+
+/**
+ * This type is manually kept in sync with the Membership API:
+ * https://github.com/guardian/members-data-api/blob/a48acdebed6a334ceb4336ece275b9cf9b3d6bb7/membership-attribute-service/app/models/Attributes.scala#L134-L151
+ */
+type UserFeaturesResponse = {
+	userId: string;
+	tier?: string;
+	recurringContributionPaymentPlan?: string;
+	oneOffContributionDate?: LocalDate;
+	membershipJoinDate?: LocalDate;
+	digitalSubscriptionExpiryDate?: LocalDate;
+	paperSubscriptionExpiryDate?: LocalDate;
+	guardianWeeklyExpiryDate?: LocalDate;
+	liveAppSubscriptionExpiryDate?: LocalDate;
+	alertAvailableFor?: string;
+
+	showSupportMessaging: boolean;
+
+	contentAccess: {
+		member: boolean;
+		paidMember: boolean;
+		recurringContributor: boolean;
+		digitalPack: boolean;
+		paperSubscriber: boolean;
+		guardianWeeklySubscriber: boolean;
+	};
+};
+
+/**
+ * TODO: check that this validation is accurate
+ */
+const validateResponse = (
+	response: unknown,
+): response is UserFeaturesResponse => {
+	if (!isObject(response)) return false;
+
+	if (
+		typeof response.showSupportMessaging === 'boolean' &&
+		isObject(response.contentAccess) &&
+		typeof response.contentAccess.paidMember === 'boolean' &&
+		typeof response.contentAccess.recurringContributor === 'boolean' &&
+		typeof response.contentAccess.digitalPack === 'boolean'
+	) {
+		return true;
+	}
+
+	return true;
+};
+
+const persistResponse = (JsonResponse: UserFeaturesResponse) => {
 	addCookie(USER_FEATURES_EXPIRY_COOKIE, timeInDaysFromNow(1));
 	addCookie(PAYING_MEMBER_COOKIE, JsonResponse.contentAccess.paidMember);
 	addCookie(
@@ -86,7 +142,7 @@ const persistResponse = (JsonResponse) => {
 	}
 
 	removeCookie(ACTION_REQUIRED_FOR_COOKIE);
-	if ('alertAvailableFor' in JsonResponse) {
+	if (JsonResponse.alertAvailableFor) {
 		addCookie(ACTION_REQUIRED_FOR_COOKIE, JsonResponse.alertAvailableFor);
 	}
 
@@ -103,7 +159,7 @@ const persistResponse = (JsonResponse) => {
 	}
 };
 
-const deleteOldData = () => {
+const deleteOldData = (): void => {
 	// We expect adfree cookies to be cleaned up by the logout process, but what if the user's login simply times out?
 	removeCookie(USER_FEATURES_EXPIRY_COOKIE);
 	removeCookie(PAYING_MEMBER_COOKIE);
@@ -116,43 +172,53 @@ const deleteOldData = () => {
 };
 
 const requestNewData = () =>
-	fetchJson(`${config.get('page.userAttributesApiUrl')}/me`, {
-		mode: 'cors',
-		credentials: 'include',
-	})
+	fetchJson(
+		`${config.get<string>(
+			'page.userAttributesApiUrl',
+			'/USER_ATTRIBUTE_API_NOT_FOUND',
+		)}/me`,
+		{
+			mode: 'cors',
+			credentials: 'include',
+		},
+	)
+		.then((response) => {
+			if (!validateResponse(response))
+				throw new Error('invalid response');
+			return response;
+		})
 		.then(persistResponse)
-		.catch(() => {});
+		.catch(noop);
 
-const datedCookieIsOld = (datedCookieName) => {
-	const expiryDateFromCookie = getCookie(datedCookieName);
+const datedCookieIsOldOrMissing = (datedCookieName: string): boolean => {
+	const expiryDateFromCookie = getCookie({ name: datedCookieName });
+	if (!expiryDateFromCookie) return true;
 	const expiryTime = parseInt(expiryDateFromCookie, 10);
 	const timeNow = new Date().getTime();
 	return timeNow >= expiryTime;
 };
 
-const featuresDataIsMissing = () => !getCookie(USER_FEATURES_EXPIRY_COOKIE);
+const featuresDataIsOld = () =>
+	datedCookieIsOldOrMissing(USER_FEATURES_EXPIRY_COOKIE);
 
-const featuresDataIsOld = () => datedCookieIsOld(USER_FEATURES_EXPIRY_COOKIE);
-
-const adFreeDataIsOld = () => {
-	const switches = config.get('switches');
+const adFreeDataIsOld = (): boolean => {
+	const { switches } = window.guardian.config;
 	return (
-		switches.adFreeStrictExpiryEnforcement &&
-		datedCookieIsOld(AD_FREE_USER_COOKIE)
+		Boolean(switches.adFreeStrictExpiryEnforcement) &&
+		datedCookieIsOldOrMissing(AD_FREE_USER_COOKIE)
 	);
 };
 
-const userNeedsNewFeatureData = () =>
-	featuresDataIsMissing() ||
-	featuresDataIsOld() ||
-	(adFreeDataIsPresent() && adFreeDataIsOld());
+const userNeedsNewFeatureData = (): boolean =>
+	featuresDataIsOld() || (adFreeDataIsPresent() && adFreeDataIsOld());
 
-const userHasDataAfterSignout = () => !isUserLoggedIn() && userHasData();
+const userHasDataAfterSignout = (): boolean =>
+	!isUserLoggedIn() && userHasData();
 
 /**
  * Updates the user's data in a lazy fashion
  */
-const refresh = () => {
+const refresh = (): Promise<void> => {
 	if (isUserLoggedIn() && userNeedsNewFeatureData()) {
 		return requestNewData();
 	} else if (userHasDataAfterSignout() && !forcedAdFreeMode) {
@@ -162,22 +228,22 @@ const refresh = () => {
 };
 
 const supportSiteRecurringCookiePresent = () =>
-	getCookie(SUPPORT_RECURRING_CONTRIBUTOR_MONTHLY_COOKIE) != null ||
-	getCookie(SUPPORT_RECURRING_CONTRIBUTOR_ANNUAL_COOKIE) != null;
+	getCookie({ name: SUPPORT_RECURRING_CONTRIBUTOR_MONTHLY_COOKIE }) != null ||
+	getCookie({ name: SUPPORT_RECURRING_CONTRIBUTOR_ANNUAL_COOKIE }) != null;
 
 /**
  * Does our _existing_ data say the user is a paying member?
  * This data may be stale; we do not wait for userFeatures.refresh()
  */
-const isPayingMember = () =>
+const isPayingMember = (): boolean =>
 	// If the user is logged in, but has no cookie yet, play it safe and assume they're a paying user
-	isUserLoggedIn() && getCookie(PAYING_MEMBER_COOKIE) !== 'false';
+	isUserLoggedIn() && getCookie({ name: PAYING_MEMBER_COOKIE }) !== 'false';
 
 // Expects milliseconds since epoch
-const getSupportFrontendOneOffContributionTimestamp = () => {
-	const supportFrontendCookie = getCookie(
-		SUPPORT_ONE_OFF_CONTRIBUTION_COOKIE,
-	);
+const getSupportFrontendOneOffContributionTimestamp = (): number | null => {
+	const supportFrontendCookie = getCookie({
+		name: SUPPORT_ONE_OFF_CONTRIBUTION_COOKIE,
+	});
 
 	if (supportFrontendCookie) {
 		const ms = parseInt(supportFrontendCookie, 10);
@@ -188,8 +254,10 @@ const getSupportFrontendOneOffContributionTimestamp = () => {
 };
 
 // Expects YYYY-MM-DD format
-const getAttributesOneOffContributionTimestamp = () => {
-	const attributesCookie = getCookie(ONE_OFF_CONTRIBUTION_DATE_COOKIE);
+const getAttributesOneOffContributionTimestamp = (): number | null => {
+	const attributesCookie = getCookie({
+		name: ONE_OFF_CONTRIBUTION_DATE_COOKIE,
+	});
 
 	if (attributesCookie) {
 		const ms = Date.parse(attributesCookie);
@@ -201,11 +269,11 @@ const getAttributesOneOffContributionTimestamp = () => {
 
 // number returned is Epoch time in milliseconds.
 // null value signifies no last contribution date.
-const getLastOneOffContributionTimestamp = () =>
-	getSupportFrontendOneOffContributionTimestamp() ||
+const getLastOneOffContributionTimestamp = (): number | null =>
+	getSupportFrontendOneOffContributionTimestamp() ??
 	getAttributesOneOffContributionTimestamp();
 
-const getLastOneOffContributionDate = () => {
+const getLastOneOffContributionDate = (): LocalDate | null => {
 	const timestamp = getLastOneOffContributionTimestamp();
 
 	if (timestamp === null) {
@@ -220,12 +288,14 @@ const getLastOneOffContributionDate = () => {
 	return `${year}-${month}-${day}`;
 };
 
-const getLastRecurringContributionDate = () => {
+const getLastRecurringContributionDate = (): number | null => {
 	// Check for cookies, ensure that cookies parse, and ensure parsed results are integers
-	const monthlyCookie = getCookie(
-		SUPPORT_RECURRING_CONTRIBUTOR_MONTHLY_COOKIE,
-	);
-	const annualCookie = getCookie(SUPPORT_RECURRING_CONTRIBUTOR_ANNUAL_COOKIE);
+	const monthlyCookie = getCookie({
+		name: SUPPORT_RECURRING_CONTRIBUTOR_MONTHLY_COOKIE,
+	});
+	const annualCookie = getCookie({
+		name: SUPPORT_RECURRING_CONTRIBUTOR_ANNUAL_COOKIE,
+	});
 	const monthlyTime = monthlyCookie ? parseInt(monthlyCookie, 10) : null;
 	const annualTime = annualCookie ? parseInt(annualCookie, 10) : null;
 	const monthlyMS =
@@ -241,10 +311,10 @@ const getLastRecurringContributionDate = () => {
 		return Math.max(monthlyMS, annualMS);
 	}
 
-	return monthlyMS || annualMS || null;
+	return monthlyMS ?? annualMS ?? null;
 };
 
-const getDaysSinceLastOneOffContribution = () => {
+const getDaysSinceLastOneOffContribution = (): number | null => {
 	const lastContributionDate = getLastOneOffContributionTimestamp();
 	if (lastContributionDate === null) {
 		return null;
@@ -253,7 +323,7 @@ const getDaysSinceLastOneOffContribution = () => {
 };
 
 // defaults to last three months
-const isRecentOneOffContributor = (askPauseDays = 90) => {
+const isRecentOneOffContributor = (askPauseDays = 90): boolean => {
 	const daysSinceLastContribution = getDaysSinceLastOneOffContribution();
 	if (daysSinceLastContribution === null) {
 		return false;
@@ -262,7 +332,7 @@ const isRecentOneOffContributor = (askPauseDays = 90) => {
 };
 
 // true if the user has completed their ask-free period
-const isPostAskPauseOneOffContributor = (askPauseDays = 90) => {
+const isPostAskPauseOneOffContributor = (askPauseDays = 90): boolean => {
 	const daysSinceLastContribution = getDaysSinceLastOneOffContribution();
 	if (daysSinceLastContribution === null) {
 		return false;
@@ -270,16 +340,17 @@ const isPostAskPauseOneOffContributor = (askPauseDays = 90) => {
 	return daysSinceLastContribution > askPauseDays;
 };
 
-const isRecurringContributor = () =>
+const isRecurringContributor = (): boolean =>
 	// If the user is logged in, but has no cookie yet, play it safe and assume they're a contributor
-	(isUserLoggedIn() && getCookie(RECURRING_CONTRIBUTOR_COOKIE) !== 'false') ||
+	(isUserLoggedIn() &&
+		getCookie({ name: RECURRING_CONTRIBUTOR_COOKIE }) !== 'false') ||
 	supportSiteRecurringCookiePresent();
 
-const isDigitalSubscriber = () =>
-	getCookie(DIGITAL_SUBSCRIBER_COOKIE) === 'true';
+const isDigitalSubscriber = (): boolean =>
+	getCookie({ name: DIGITAL_SUBSCRIBER_COOKIE }) === 'true';
 
-const shouldNotBeShownSupportMessaging = () =>
-	getCookie(HIDE_SUPPORT_MESSAGING_COOKIE) === 'true';
+const shouldNotBeShownSupportMessaging = (): boolean =>
+	getCookie({ name: HIDE_SUPPORT_MESSAGING_COOKIE }) === 'true';
 
 /*
     Whenever the checks are updated, please make sure to update
@@ -289,7 +360,7 @@ const shouldNotBeShownSupportMessaging = () =>
     which this function is dependent on.
 */
 
-const shouldHideSupportMessaging = () =>
+const shouldHideSupportMessaging = (): boolean =>
 	shouldNotBeShownSupportMessaging() ||
 	isRecentOneOffContributor() || // because members-data-api is unaware of one-off contributions so relies on cookie
 	isRecurringContributor(); // guest checkout means that members-data-api isn't aware of all recurring contributions so relies on cookie
@@ -305,16 +376,16 @@ const readerRevenueRelevantCookies = [
 ];
 
 // For debug/test purposes
-const fakeOneOffContributor = () => {
+const fakeOneOffContributor = (): void => {
 	addCookie(SUPPORT_ONE_OFF_CONTRIBUTION_COOKIE, Date.now().toString());
 };
 
-const isAdFreeUser = () =>
+const isAdFreeUser = (): boolean =>
 	isDigitalSubscriber() || (adFreeDataIsPresent() && !adFreeDataIsOld());
 
 // Extend the expiry of the contributions cookie by 1 year beyond the date of the contribution
-const extendContribsCookieExpiry = () => {
-	const cookie = getCookie(SUPPORT_ONE_OFF_CONTRIBUTION_COOKIE);
+const extendContribsCookieExpiry = (): void => {
+	const cookie = getCookie({ name: SUPPORT_ONE_OFF_CONTRIBUTION_COOKIE });
 	if (cookie) {
 		const contributionDate = parseInt(cookie, 10);
 		if (Number.isInteger(contributionDate)) {
@@ -328,13 +399,13 @@ const extendContribsCookieExpiry = () => {
 	}
 };
 
-const canShowContributionsReminderFeature = () => {
-	const signedUpForReminder = !!getCookie(
-		CONTRIBUTIONS_REMINDER_SIGNED_UP.name,
-	);
-	return (
-		config.get('switches.showContributionReminder') && !signedUpForReminder
-	);
+const canShowContributionsReminderFeature = (): boolean => {
+	const signedUpForReminder = !!getCookie({
+		name: CONTRIBUTIONS_REMINDER_SIGNED_UP.name,
+	});
+	const { switches } = window.guardian.config;
+
+	return Boolean(switches.showContributionReminder) && !signedUpForReminder;
 };
 
 export {
