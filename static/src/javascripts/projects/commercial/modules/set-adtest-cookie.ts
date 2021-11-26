ab85c7980f57e7cd1417371051cdd9d27a7da7ed
Max Duval <max.duval@theguardian.com>
2021-11-26 14:34:47 +0000

    fix linting

--- static/src/javascripts/projects/commercial/modules/set-adtest-cookie.ts
+++ static/src/javascripts/projects/commercial/modules/set-adtest-cookie.ts
@@ -1,19 +1,20 @@
-import { getUrlVars } from "../../../lib/url";
-import { removeCookie, addCookie } from "@guardian/libs";
+import { removeCookie, setCookie } from '@guardian/libs';
+import { getUrlVars } from '../../../lib/url';
 
-const init = () => {
-  const queryParams = getUrlVars();
+const init = (): Promise<void> => {
+	const queryParams = getUrlVars();
 
-  if (queryParams.adtest === "clear") {
-    removeCookie({ name: "adtest" });
-  } else if (queryParams.adtest) {
-    setCookie({
-      name: "adtest",
-      value: encodeURIComponent(queryParams.adtest),
-      daysToLive: 10,
-    });
-  }
-  return Promise.resolve();
+	if (queryParams.adtest === 'clear') {
+		removeCookie({ name: 'adtest' });
+	} else if (queryParams.adtest) {
+		setCookie({
+			name: 'adtest',
+			value: encodeURIComponent(queryParams.adtest),
+			daysToLive: 10,
+		});
+	}
+
+	return Promise.resolve();
 };
 
 export { init };
