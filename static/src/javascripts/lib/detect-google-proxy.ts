52ed2e45c9863de550fe0c4622f4bed64f7bc80a
Max Duval <max.duval@theguardian.com>
2021-11-26 15:37:31 +0000

    add documentation

--- static/src/javascripts/lib/detect-google-proxy.ts
+++ static/src/javascripts/lib/detect-google-proxy.ts
@@ -1,5 +1,19 @@
+/**
+ * https://developers.google.com/search/docs/advanced/mobile/web-light
+ */
+const GOOGLE_WEB_LIGTH = 'googleweblight';
+
+/**
+ * This one is undocumented, not sure it actually exists.
+ */
+const GOOGLE_WEB_PREVIEW = 'Google Web Preview';
+
+/**
+ * Read more about Google Crawler here: https://developers.google.com/search/docs/advanced/crawling/overview-google-crawlers
+ * @returns whether this is a Google Proxy
+ */
 export const isGoogleProxy = (): boolean =>
 	Boolean(
-		navigator.userAgent.includes('Google Web Preview') ||
-			navigator.userAgent.includes('googleweblight'),
+		navigator.userAgent.includes(GOOGLE_WEB_PREVIEW) ||
+			navigator.userAgent.includes(GOOGLE_WEB_LIGTH),
 	);
