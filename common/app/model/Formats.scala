15586f2527b19d9fcb6e4ba73c2b2264e609fb98
Joshua <joshuathomaslieberman@gmail.com>
2021-12-01 15:51:07 +0000

    incorporate newsletter signup design type

--- common/app/model/Formats.scala
+++ common/app/model/Formats.scala
@@ -174,6 +174,7 @@ object PressedContentFormat {
         case JsString("Quiz")                 => JsSuccess(com.gu.contentapi.client.utils.Quiz)
         case JsString("GuardianLabs")         => JsSuccess(com.gu.contentapi.client.utils.GuardianLabs)
         case JsString("AdvertisementFeature") => JsSuccess(com.gu.contentapi.client.utils.AdvertisementFeature)
+        case JsString("NewsletterSignup")     => JsSuccess(com.gu.contentapi.client.utils.NewsletterSignup)
         case _                                => JsError(s"Unknown design type: '$json'")
       }
     override def writes(dt: DesignType): JsValue =
@@ -194,6 +195,7 @@ object PressedContentFormat {
         case com.gu.contentapi.client.utils.Quiz                 => JsString("Quiz")
         case com.gu.contentapi.client.utils.GuardianLabs         => JsString("GuardianLabs")
         case com.gu.contentapi.client.utils.AdvertisementFeature => JsString("AdvertisementFeature")
+        case com.gu.contentapi.client.utils.NewsletterSignup     => JsString("NewsletterSignup")
       }
   }
 
