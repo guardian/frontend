64675a69ead64d984028b2a79187f61719316492
Max Duval <max.duval@theguardian.com>
2021-12-02 11:47:34 +0000

    refactor logic
    and handle NaN

--- static/src/javascripts/projects/common/modules/analytics/mvt-cookie.ts
+++ static/src/javascripts/projects/common/modules/analytics/mvt-cookie.ts
@@ -1,21 +1,34 @@
 import { getCookie, setCookie } from '@guardian/libs';
 
 const MULTIVARIATE_ID_COOKIE = 'GU_mvt_id';
-/**
- * The full mvt ID interval is [1, 1000000]
- */
-const MAX_CLIENT_MVT_ID = 1_000_000;
+const MVT_ID_RANGE = {
+	min: 1,
+	max: 1_000_000,
+} as const;
 
-// For test purposes only.
-export const overwriteMvtCookie = (testId: number): void =>
+const overwriteMvtCookie = (testId: number): void =>
 	setCookie({
 		name: MULTIVARIATE_ID_COOKIE,
 		value: String(testId),
 		daysToLive: 365,
 	});
 
-export const getMvtValue = (): number =>
-	Number(getCookie({ name: MULTIVARIATE_ID_COOKIE, shouldMemoize: true }));
+/**
+ *
+ * @returns a `number` in the [1 - 1,000,000] range if the cookie is set.
+ * @returns `null` if the cookie is not set.
+ */
+export const getMvtValue = (): number | null => {
+	const cookieId = parseInt(
+		getCookie({ name: MULTIVARIATE_ID_COOKIE, shouldMemoize: true }) ??
+			'NaN',
+		10,
+	);
+
+	return Number.isNaN(cookieId)
+		? null
+		: Math.max(Math.min(cookieId, MVT_ID_RANGE.max), MVT_ID_RANGE.min);
+};
 
 /**
  * For test purposes only.
@@ -24,20 +37,20 @@ export const getMvtValue = (): number =>
  * but it's needed for certain A/B tests to work properly.
  */
 export const initMvtCookie = (): void => {
-	if (!getCookie({ name: MULTIVARIATE_ID_COOKIE, shouldMemoize: true })) {
-		setCookie({ name: MULTIVARIATE_ID_COOKIE, value: '1' });
+	if (!getCookie({ name: MULTIVARIATE_ID_COOKIE, shouldMemoize: false })) {
+		setCookie({
+			name: MULTIVARIATE_ID_COOKIE,
+			value: String(MVT_ID_RANGE.min),
+		});
 	}
 };
 
 export const incrementMvtCookie = (): void => {
-	const mvtId = parseInt(
-		getCookie({ name: 'GU_mvt_id', shouldMemoize: true }) ?? '0',
-		10,
-	);
+	const mvtId = getMvtValue();
 	if (mvtId) {
-		if (mvtId === MAX_CLIENT_MVT_ID) {
+		if (mvtId === MVT_ID_RANGE.max) {
 			// Wrap back to 1 if it would exceed the max
-			overwriteMvtCookie(1);
+			overwriteMvtCookie(MVT_ID_RANGE.min);
 		} else {
 			overwriteMvtCookie(mvtId + 1);
 		}
@@ -45,15 +58,19 @@ export const incrementMvtCookie = (): void => {
 };
 
 export const decrementMvtCookie = (): void => {
-	const mvtId = parseInt(getCookie({ name: 'GU_mvt_id' }) ?? '0', 10);
+	const mvtId = getMvtValue();
 	if (mvtId) {
-		if (mvtId === 0) {
-			// Wrap back to max if it would be less than 0
-			overwriteMvtCookie(MAX_CLIENT_MVT_ID);
+		if (mvtId === MVT_ID_RANGE.min) {
+			// Wrap back to max if it would be less than 1
+			overwriteMvtCookie(MVT_ID_RANGE.max);
 		} else {
 			overwriteMvtCookie(mvtId - 1);
 		}
 	}
 };
 
-export const getMvtNumValues = (): number => Number(MAX_CLIENT_MVT_ID);
+export const getMvtNumValues = (): 1_000_000 => MVT_ID_RANGE.max;
+
+export const _ = {
+	overwriteMvtCookie,
+};
