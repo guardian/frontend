88ed7a22366d1591b7ed7a858da1eda27e903abc
Chris Jones <christopher.jones@guardian.co.uk>
2021-12-03 13:14:14 +0000

    Convert DFP env to a class

--- static/src/javascripts/projects/commercial/modules/dfp/dfp-env.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/dfp-env.ts
@@ -2,9 +2,9 @@ import config from '../../../../lib/config';
 import { getUrlVars } from '../../../../lib/url';
 import type { Advert } from './Advert';
 
-interface DfpEnv {
+interface IDfpEnv {
 	renderStartTime: number;
-	adSlotSelector: string;
+	readonly adSlotSelector: string;
 	hbImpl: Record<string, boolean>;
 	lazyLoadEnabled: boolean;
 	lazyLoadObserve: boolean;
@@ -18,44 +18,63 @@ interface DfpEnv {
 
 const { switches } = window.guardian.config;
 
-export const dfpEnv: DfpEnv = {
+class DfpEnv implements IDfpEnv {
 	/* renderStartTime: integer. Point in time when DFP kicks in */
-	renderStartTime: -1,
+	renderStartTime: number;
 
 	/* adSlotSelector: string. A CSS selector to query ad slots in the DOM */
-	adSlotSelector: '.js-ad-slot',
+	readonly adSlotSelector: string;
 
 	/* hbImpl: Returns an object {'prebid': boolean, 'a9': boolean} to indicate which header bidding implementations are switched on */
 	hbImpl: {
-		// TODO: fix the Switch type upstream
-		prebid: switches.prebidHeaderBidding ?? false,
-		a9: switches.a9HeaderBidding ?? false,
-	},
+		prebid: boolean;
+		a9: boolean;
+	};
+
 	/* lazyLoadEnabled: boolean. Set to true when adverts are lazy-loaded */
-	lazyLoadEnabled: false,
+	lazyLoadEnabled: boolean;
 
 	/* lazyLoadObserve: boolean. Use IntersectionObserver in supporting browsers */
-	lazyLoadObserve: 'IntersectionObserver' in window,
+	lazyLoadObserve: boolean;
 
 	/* creativeIDs: array<string>. List of loaded creative IDs */
-	creativeIDs: [],
+	creativeIDs: string[];
 
 	/* advertIds: map<string -> int>. Keeps track of slot IDs and their position in the array of adverts */
-	advertIds: {},
+	advertIds: Record<string, number>;
 
 	/* advertsToLoad: array<Advert>. Lists adverts waiting to be loaded */
-	advertsToLoad: [],
+	advertsToLoad: Advert[];
 
 	/* advertsToRefresh: array<Advert>. Lists adverts refreshed when a breakpoint has been crossed */
-	advertsToRefresh: [],
+	advertsToRefresh: Advert[];
 
 	/* adverts: array<Advert>. Keeps track of adverts and their state */
-	adverts: [],
+	adverts: Advert[];
+
+	constructor() {
+		this.renderStartTime = -1;
+		this.adSlotSelector = '.js-ad-slot';
+		this.hbImpl = {
+			// TODO: fix the Switch type upstream
+			prebid: switches.prebidHeaderBidding ?? false,
+			a9: switches.a9HeaderBidding ?? false,
+		};
+		this.lazyLoadEnabled = false;
+		this.lazyLoadObserve = 'IntersectionObserver' in window;
+		this.creativeIDs = [];
+		this.advertIds = {};
+		this.advertsToLoad = [];
+		this.advertsToRefresh = [];
+		this.adverts = [];
+	}
 
 	/* shouldLazyLoad: () -> boolean. Determines whether ads should be lazy loaded */
 	shouldLazyLoad(): boolean {
 		// We do not want lazy loading on pageskins because it messes up the roadblock
 		// Also, if the special dll parameter is passed with a value of 1, we don't lazy load
 		return !config.get('page.hasPageSkin') && getUrlVars().dll !== '1';
-	},
-};
+	}
+}
+
+export const dfpEnv = new DfpEnv();
