febc131edafcfce0a786b380a4125effe5b14165
George B <705427+georgeblahblah@users.noreply.github.com>
2021-12-06 16:17:07 +0000

    Refactor email embed listener for resize messages with src

--- static/src/javascripts/bootstraps/standard/emailEmbeds.js
+++ static/src/javascripts/bootstraps/standard/emailEmbeds.js
@@ -1,18 +1,29 @@
+const allowedOrigins = [
+    'https://www.theguardian.com',
+    'https://theguardian.com',
+    'https://m.code.dev-theguardian.com',
+    'http://localhost:3000'
+]
 const initEmbedResize = () => {
     const allIframes = [].slice.call(
         document.querySelectorAll('.element-embed > .email-sub__iframe, #footer__email-form'),
     );
     window.addEventListener('message', event => {
+        if (!allowedOrigins.includes(event.origin)) return
+        const message = JSON.parse(event.data)
         const iframes = allIframes.filter(i => {
             try {
-                return i.src === event.source.location.href;
+                if (message.src)
+                    return i.src === message.src
+                else
+                    return i.src === event.source.location.href
             } catch (e) {
-                return false;
+                return false
             }
-        });
+        })
+
         if (iframes.length !== 0) {
             try {
-                const message = JSON.parse(event.data);
                 switch (message.type) {
                     case 'set-height':
                         iframes.forEach( iframe => {
