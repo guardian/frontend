538765c65eac6b4fd3ca4c23035804389541aeb5
George B <705427+georgeblahblah@users.noreply.github.com>
2021-12-08 14:11:49 +0000

    Revert change to emailEmbeds script
    We can assume the embed will be on the correct domain

--- static/src/javascripts/bootstraps/standard/emailEmbeds.js
+++ static/src/javascripts/bootstraps/standard/emailEmbeds.js
@@ -1,41 +1,32 @@
-const allowedOrigins = [
-    'https://www.theguardian.com',
-    'https://theguardian.com',
-    'https://m.code.dev-theguardian.com',
-    'http://localhost:3000'
-]
 const initEmbedResize = () => {
-    const allIframes = [].slice.call(
-        document.querySelectorAll('.element-embed > .email-sub__iframe, #footer__email-form'),
-    );
-    window.addEventListener('message', event => {
-        if (!allowedOrigins.includes(event.origin)) return
-        const message = JSON.parse(event.data)
-        const iframes = allIframes.filter(i => {
-            try {
-                if (message.src)
-                    return i.src === message.src
-                else
-                    return i.src === event.source.location.href
-            } catch (e) {
-                return false
-            }
-        })
+	const allIframes = [].slice.call(
+		document.querySelectorAll(
+			'.element-embed > .email-sub__iframe, #footer__email-form',
+		),
+	);
 
-        if (iframes.length !== 0) {
-            try {
-                switch (message.type) {
-                    case 'set-height':
-                        iframes.forEach( iframe => {
-                            iframe.height = message.value;
-                        })
-                        break;
-                    default:
-                }
-                // eslint-disable-next-line no-empty
-            } catch (e) {
-            }
-        }
-    });
+	window.addEventListener('message', (event) => {
+		const iframes = allIframes.filter((i) => {
+			try {
+				return i.src === event.source.location.href;
+			} catch (e) {
+				return false;
+			}
+		});
+		if (iframes.length !== 0) {
+			try {
+				const message = JSON.parse(event.data);
+				switch (message.type) {
+					case 'set-height':
+						iframes.forEach((iframe) => {
+							iframe.height = message.value;
+						});
+						break;
+					default:
+				}
+				// eslint-disable-next-line no-empty
+			} catch (e) {}
+		}
+	});
 };
 export { initEmbedResize };
