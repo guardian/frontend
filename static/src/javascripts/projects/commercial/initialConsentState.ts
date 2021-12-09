a2a6a033fd837c45926e33e0ad7d41912da06ba9
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2021-12-09 17:40:59 +0000

    Update initialConsentState comments

--- static/src/javascripts/projects/commercial/initialConsentState.ts
+++ static/src/javascripts/projects/commercial/initialConsentState.ts
@@ -1,6 +1,13 @@
 import { onConsentChange } from '@guardian/consent-management-platform';
 import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
 
+/**
+ * return a promise containing the first consent state provided by the user
+ * as soon as it becomes available.
+ * NOTE: depending on where this function is eventually used, it might be more appropriate
+ * for it to be defined in the consent-management-platform
+ * @returns Promise
+ */
 export const getInitialConsentState = (): Promise<ConsentState> => {
 	let resolveInitialState: (state: ConsentState) => void;
 	let rejectInitialState: (reason: 'Unknown framework') => void;
@@ -12,7 +19,7 @@ export const getInitialConsentState = (): Promise<ConsentState> => {
 	onConsentChange((state) => {
 		if (state.tcfv2) {
 			// For tcfv2 only, the first onConsentChange is fired before the user has
-			// interacted this the consent banner. We want to ignore this.
+			// interacted with the consent banner. We want to ignore this first consent state.
 			if (state.tcfv2.eventStatus !== 'cmpuishown')
 				resolveInitialState({ tcfv2: state.tcfv2 });
 			else return;
