cd63cb0c745cb17a5901b15a315eacb1de66c3a4
Rupert Bates <Rupert.Bates@guardian.co.uk>
2022-01-14 14:00:02 +0000

    Formatting again

--- common/app/navigation/helpers/UrlHelpers.scala
+++ common/app/navigation/helpers/UrlHelpers.scala
@@ -10,45 +10,18 @@ import scala.PartialFunction.condOpt
 
 object UrlHelpers {
 
-  private val uriEncoder = UriConfig.default.copy(
-    // The default encoder does not encode double quotes in the querystring
-    queryEncoder = PercentEncoder(PercentEncoder.QUERY_CHARS_TO_ENCODE + '"'),
-  )
-
-  def readerRevenueLinks(implicit request: RequestHeader): List[NavLink] =
-    List(
-      NavLink("Make a contribution", getReaderRevenueUrl(SupportContribute, SideMenu)),
-      NavLink(
-        "Print subscriptions",
-        getReaderRevenueUrl(SupportGuardianWeekly, SideMenu),
-        classList = Seq("js-subscribe"),
-      ),
-    )
-
-  def getReaderRevenueUrl(destination: ReaderRevenueSite, position: Position): String = {
-    val componentId = getComponentId(destination, position)
-    val componentType = getComponentType(position)
-
-    val acquisitionData = Json.obj(
-      // GUARDIAN_WEB corresponds to a value in the Thrift enum
-      // https://dashboard.ophan.co.uk/docs/thrift/acquisition.html#Enum_AcquisitionSource
-      // ACQUISITIONS_HEADER and ACQUISITIONS_FOOTER correspond to values in the Thrift enum
-      // https://dashboard.ophan.co.uk/docs/thrift/componentevent.html#Enum_ComponentType
-      "source" -> "GUARDIAN_WEB",
-      "componentType" -> componentType,
-    ) ++ componentId.fold(Json.obj())(c =>
-      Json.obj(
-        "componentId" -> c,
-      ),
-    )
-
-    import com.netaporter.uri.dsl._
-
-    // INTCMP is passed as a separate param because people look at it in Google Analytics
-    // It's set to the most specific thing (componentId) to maximise its usefulness
-    val url = destination.url ? ("INTCMP" -> componentId) & ("acquisitionData" -> acquisitionData.toString)
-    url.toString(uriEncoder)
-  }
+  sealed trait Position
+  case object Header extends Position
+  // SlimHeaderDropdown can only be seen on slim header content, e.g. interactives
+  // It's the icon with the three dots in the top left
+  case object SlimHeaderDropdown extends Position
+  // SideMenu is the full menu you get from clicking the hamburger icon on mobile
+  case object SideMenu extends Position
+  case object AmpHeader extends Position
+  case object Footer extends Position
+  case object AmpFooter extends Position
+  case object ManageMyAccountUpsell extends Position
+  case object ManageMyAccountCancel extends Position
 
   def getComponentId(destination: ReaderRevenueSite, position: Position): Option[String] = {
     condOpt((destination, position)) {
@@ -88,31 +61,50 @@ object UrlHelpers {
       case Footer | AmpFooter                                 => "ACQUISITIONS_FOOTER"
     }
 
+  def readerRevenueLinks(implicit request: RequestHeader): List[NavLink] =
+    List(
+      NavLink("Make a contribution", getReaderRevenueUrl(SupportContribute, SideMenu)),
+      NavLink(
+        "Print subscriptions",
+        getReaderRevenueUrl(SupportGuardianWeekly, SideMenu),
+        classList = Seq("js-subscribe"),
+      ),
+    )
+
+  private val uriEncoder = UriConfig.default.copy(
+    // The default encoder does not encode double quotes in the querystring
+    queryEncoder = PercentEncoder(PercentEncoder.QUERY_CHARS_TO_ENCODE + '"'),
+  )
+
+  def getReaderRevenueUrl(destination: ReaderRevenueSite, position: Position): String = {
+    val componentId = getComponentId(destination, position)
+    val componentType = getComponentType(position)
+
+    val acquisitionData = Json.obj(
+      // GUARDIAN_WEB corresponds to a value in the Thrift enum
+      // https://dashboard.ophan.co.uk/docs/thrift/acquisition.html#Enum_AcquisitionSource
+      // ACQUISITIONS_HEADER and ACQUISITIONS_FOOTER correspond to values in the Thrift enum
+      // https://dashboard.ophan.co.uk/docs/thrift/componentevent.html#Enum_ComponentType
+      "source" -> "GUARDIAN_WEB",
+      "componentType" -> componentType,
+    ) ++ componentId.fold(Json.obj())(c =>
+      Json.obj(
+        "componentId" -> c,
+      ),
+    )
+
+    import com.netaporter.uri.dsl._
+
+    // INTCMP is passed as a separate param because people look at it in Google Analytics
+    // It's set to the most specific thing (componentId) to maximise its usefulness
+    val url = destination.url ? ("INTCMP" -> componentId) & ("acquisitionData" -> acquisitionData.toString)
+    url.toString(uriEncoder)
+  }
+
   def getJobUrl(editionId: String): String =
     if (editionId == "au") {
       "https://jobs.theguardian.com/landingpage/2868291/jobs-australia-html/?INTCMP=jobs_au_web_newheader"
     } else {
       s"https://jobs.theguardian.com/?INTCMP=jobs_${editionId}_web_newheader"
     }
-
-  sealed trait Position
-
-  case object Header extends Position
-
-  // SlimHeaderDropdown can only be seen on slim header content, e.g. interactives
-  // It's the icon with the three dots in the top left
-  case object SlimHeaderDropdown extends Position
-
-  // SideMenu is the full menu you get from clicking the hamburger icon on mobile
-  case object SideMenu extends Position
-
-  case object AmpHeader extends Position
-
-  case object Footer extends Position
-
-  case object AmpFooter extends Position
-
-  case object ManageMyAccountUpsell extends Position
-
-  case object ManageMyAccountCancel extends Position
 }
