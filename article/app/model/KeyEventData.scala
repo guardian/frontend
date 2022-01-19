514853be32608a0235f2e8a574ef4c820ca51805
Anna Beddow <anna.beddow@gmail.com>
2022-01-19 09:55:25 +0000

    Add summary blocks into key event data

--- article/app/model/KeyEventData.scala
+++ article/app/model/KeyEventData.scala
@@ -8,21 +8,22 @@ object KeyEventData {
 
   // just for convenience for use from the templates
   def apply(maybeBlocks: Option[Blocks], timezone: DateTimeZone, filterKeyEvents: Boolean): Seq[KeyEventData] = {
-
-    val blocks =
-      maybeBlocks.toSeq.flatMap(blocks => blocks.requestedBodyBlocks.getOrElse(CanonicalLiveBlog.timeline, blocks.body))
-
+    val blocks = maybeBlocks.toSeq.flatMap(blocks => {
+      if (blocks.body.length > 0) {
+        blocks.body.filter(block => block.eventType == SummaryEvent || block.eventType == KeyEvent)
+      } else {
+        val keyEvent = blocks.requestedBodyBlocks.getOrElse(CanonicalLiveBlog.timeline, Seq())
+        val summaryEvent = blocks.requestedBodyBlocks.getOrElse(CanonicalLiveBlog.summary, Seq())
+        keyEvent ++ summaryEvent
+      }
+    })
     apply(blocks, timezone, filterKeyEvents)
   }
 
   def apply(blocks: Seq[BodyBlock], timezone: DateTimeZone, filterKeyEvents: Boolean): Seq[KeyEventData] = {
     val TimelineMaxEntries = 7
-    val latestSummary = blocks.find(_.eventType == SummaryEvent)
-    val keyEvents = blocks.filter(_.eventType == KeyEvent)
-    val filteredBlocks = if (filterKeyEvents) keyEvents else latestSummary.toSeq ++ keyEvents
-    val bodyBlocks = filteredBlocks.sortBy(_.publishedCreatedTimestamp).reverse.take(TimelineMaxEntries)
-
-    bodyBlocks.map { bodyBlock =>
+    val timelineBlocks = blocks.sortBy(_.publishedCreatedTimestamp).reverse.take(TimelineMaxEntries)
+    timelineBlocks.map { bodyBlock =>
       KeyEventData(bodyBlock.id, bodyBlock.referenceDateForDisplay().map(LiveBlogDate(_, timezone)), bodyBlock.title)
     }
   }
