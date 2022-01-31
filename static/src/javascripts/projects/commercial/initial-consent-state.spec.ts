de531b0aad288cad109b8bcf98b5368ec5f4ff19
Ravi Lal <arelra@users.noreply.github.com>
2022-01-31 11:29:55 +0000

    Add more tests for initial-consent-state

--- static/src/javascripts/projects/commercial/initial-consent-state.spec.ts
+++ static/src/javascripts/projects/commercial/initial-consent-state.spec.ts
@@ -1,12 +1,34 @@
 import { onConsentChange } from '@guardian/consent-management-platform';
 import type { Callback } from '@guardian/consent-management-platform/dist/types';
+import type { TCEventStatusCode } from '@guardian/consent-management-platform/dist/types/tcfv2';
 import { getInitialConsentState } from './initial-consent-state';
 
 jest.mock('@guardian/consent-management-platform', () => ({
 	onConsentChange: jest.fn(),
 }));
 
+const builTcfv2ConsentState = (eventStatus: string) => ({
+	tcfv2: {
+		consents: { 1: false },
+		eventStatus: eventStatus as TCEventStatusCode,
+		vendorConsents: {
+			['5efefe25b8e05c06542b2a77']: true,
+		},
+		addtlConsent: 'xyz',
+		gdprApplies: true,
+		tcString: 'YAAA',
+	},
+});
+
 describe('getInitialConsentState', () => {
+	test('tcfv2 with event-status not equal to `cmpuishown` resolves immediately', async () => {
+		const tcfv2ConsentState = builTcfv2ConsentState('tcloaded');
+		(onConsentChange as jest.Mock).mockImplementation((cb: Callback) =>
+			cb(tcfv2ConsentState),
+		);
+		const resolvedConsentState = await getInitialConsentState();
+		expect(resolvedConsentState).toBe(tcfv2ConsentState);
+	});
 	test('ccpa resolves immediately', async () => {
 		const ccpaConsentState = {
 			ccpa: {
@@ -30,5 +52,14 @@ describe('getInitialConsentState', () => {
 		);
 		const resolvedConsentState = await getInitialConsentState();
 		expect(resolvedConsentState).toBe(ausConsentState);
+	});
+	test('unknown region rejects', async () => {
+		const consentState = {};
+		(onConsentChange as jest.Mock).mockImplementation((cb: Callback) =>
+			cb(consentState),
+		);
+		await expect(getInitialConsentState()).rejects.toEqual(
+			'Unknown framework',
+		);
 	});
 });
