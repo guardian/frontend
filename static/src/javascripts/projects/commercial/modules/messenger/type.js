50c7e541a388773f41dbdf11c714217a9e687270
Chris Jones <christopher.jones@guardian.co.uk>
2022-01-31 17:20:02 +0000

    Add spacing between functions

--- static/src/javascripts/projects/commercial/modules/messenger/type.js
+++ static/src/javascripts/projects/commercial/modules/messenger/type.js
@@ -4,9 +4,11 @@ const setType = (type, adSlot) =>
 	fastdom.mutate(() => {
 		adSlot.classList.add(`ad-slot--${type || ''}`);
 	});
+
 const init = (register) => {
 	register('type', (specs, ret, iframe) =>
 		setType(specs, iframe && iframe.closest('.js-ad-slot')),
 	);
 };
+
 export { init };
