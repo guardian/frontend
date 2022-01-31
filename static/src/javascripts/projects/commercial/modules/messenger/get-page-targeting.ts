e289d74b23c660b4626f6e284d5853c6e85ef660
Chris Jones <christopher.jones@guardian.co.uk>
2022-01-31 17:09:02 +0000

    Add types to get-page-targeting.ts

--- static/src/javascripts/projects/commercial/modules/messenger/get-page-targeting.ts
+++ static/src/javascripts/projects/commercial/modules/messenger/get-page-targeting.ts
@@ -1,7 +1,10 @@
-import config from '../../../../lib/config';
+import type { RegisterListener } from '../messenger';
 
-const init = (register) => {
-	register('get-page-targeting', () => config.get('page.sharedAdTargeting'));
+const init = (register: RegisterListener): void => {
+	register(
+		'get-page-targeting',
+		() => window.guardian.config.page.sharedAdTargeting,
+	);
 };
 
 export { init };
