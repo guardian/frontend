391af6e08fa807e284d747c95e668534a9922d3d
Chris Jones <christopher.jones@guardian.co.uk>
2022-02-01 12:34:25 +0000

    Avoid renaming mocked fn in test

--- static/src/javascripts/projects/commercial/modules/messenger/click.spec.ts
+++ static/src/javascripts/projects/commercial/modules/messenger/click.spec.ts
@@ -1,4 +1,4 @@
-import { trackNativeAdLinkClick as _trackNativeAdLinkClick } from '../../../common/modules/analytics/google';
+import { trackNativeAdLinkClick } from '../../../common/modules/analytics/google';
 import { _ } from './click';
 
 const { sendClick } = _;
@@ -7,28 +7,28 @@ jest.mock('../../../common/modules/analytics/google', () => ({
 	trackNativeAdLinkClick: jest.fn(),
 }));
 
-const trackNativeAdLinkClick = _trackNativeAdLinkClick as jest.Mock;
-
 describe('Cross-frame messenger: sendClick', () => {
-	it('should call trackNativeAdLinkClick', () => {
-		trackNativeAdLinkClick.mockReset();
+	it('should call (trackNativeAdLinkClick as jest.Mock)', () => {
+		(trackNativeAdLinkClick as jest.Mock).mockReset();
 
 		const fakeAdSlot = document.createElement('div');
 		sendClick(fakeAdSlot, 'name');
 
-		expect(trackNativeAdLinkClick).toHaveBeenCalled();
-		expect(trackNativeAdLinkClick.mock.calls).toEqual([['', 'name']]);
+		expect(trackNativeAdLinkClick as jest.Mock).toHaveBeenCalled();
+		expect((trackNativeAdLinkClick as jest.Mock).mock.calls).toEqual([
+			['', 'name'],
+		]);
 	});
 
-	it('should call trackNativeAdLinkClick with div id', () => {
-		trackNativeAdLinkClick.mockReset();
+	it('should call (trackNativeAdLinkClick as jest.Mock) with div id', () => {
+		(trackNativeAdLinkClick as jest.Mock).mockReset();
 
 		const fakeAdSlot = document.createElement('div');
 		fakeAdSlot.setAttribute('id', 'fakeAdSlotId');
 		sendClick(fakeAdSlot, 'name');
 
-		expect(trackNativeAdLinkClick).toHaveBeenCalled();
-		expect(trackNativeAdLinkClick.mock.calls).toEqual([
+		expect(trackNativeAdLinkClick as jest.Mock).toHaveBeenCalled();
+		expect((trackNativeAdLinkClick as jest.Mock).mock.calls).toEqual([
 			['fakeAdSlotId', 'name'],
 		]);
 	});
