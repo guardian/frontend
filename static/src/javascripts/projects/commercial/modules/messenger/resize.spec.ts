af5140049bdafa4166a5c3f23e46297200f4d9c7
Chris Jones <christopher.jones@guardian.co.uk>
2022-02-02 12:09:48 +0000

    Update tests

--- static/src/javascripts/projects/commercial/modules/messenger/resize.spec.ts
+++ static/src/javascripts/projects/commercial/modules/messenger/resize.spec.ts
@@ -4,21 +4,18 @@ const { normalise, resize } = _;
 
 describe('Cross-frame messenger: resize', () => {
 	beforeEach(() => {
-		if (document.body) {
-			document.body.innerHTML = `
+		document.body.innerHTML = `
               <div id="slot01" class="js-ad-slot" style="width: 7px; height: 14px;" >
                 <div id="container01">
-                    <div id="iframe01" class="iframe" data-unit="ch"></div>
+                    <iframe id="iframe01" class="iframe" data-unit="ch"></iframe>
                 </div>
               </div>`;
-		}
+
 		expect.hasAssertions();
 	});
 
 	afterEach(() => {
-		if (document.body) {
-			document.body.innerHTML = '';
-		}
+		document.body.innerHTML = '';
 	});
 
 	describe('normalise function', () => {
@@ -47,36 +44,52 @@ describe('Cross-frame messenger: resize', () => {
 	});
 
 	describe('resize function', () => {
-		it('should resolve if the specs are empty', () => {
+		it('should not resize if the specs are empty', async () => {
 			const fakeAdSlot = document.createElement('div');
+
 			const fakeIframeContainer = document.createElement('div');
+			Object.assign(fakeIframeContainer.style, {
+				width: '20px',
+				height: '30px',
+			});
+
 			const fakeIframe = document.createElement('iframe');
-			const result = resize(
-				{},
-				fakeIframe,
-				fakeIframeContainer,
-				fakeAdSlot,
-			);
-			expect(result).toBeNull();
+			Object.assign(fakeIframe.style, {
+				width: '120px',
+				height: '220px',
+			});
+
+			await resize({}, fakeIframe, fakeIframeContainer, fakeAdSlot);
+
+			expect(fakeIframeContainer.style.width).toEqual('20px');
+			expect(fakeIframeContainer.style.height).toEqual('30px');
+			expect(fakeIframe.style.width).toEqual('120px');
+			expect(fakeIframe.style.height).toEqual('220px');
 		});
 
-		it('should set width and height of the iFrame and leave ad slot unchanged', () => {
+		it('should set width and height of the iFrame and leave ad slot unchanged', async () => {
 			const fallback = document.createElement('div');
+			const fallbackIframe = document.createElement('iframe');
 			const fakeIframeContainer =
-				document.getElementById('container01') || fallback;
-			const fakeIframe = document.getElementById('iframe01') || fallback;
-			const fakeAdSlot = document.getElementById('slot01') || fallback;
-			return resize(
+				document.getElementById('container01') ?? fallback;
+			const selectedIframe = document.getElementById('iframe01');
+			const fakeIframe =
+				selectedIframe instanceof HTMLIFrameElement
+					? selectedIframe
+					: fallbackIframe;
+			const fakeAdSlot = document.getElementById('slot01') ?? fallback;
+
+			await resize(
 				{ width: '20', height: '10' },
 				fakeIframe,
 				fakeIframeContainer,
 				fakeAdSlot,
-			).then(() => {
-				expect(fakeIframe.style.height).toBe('10px');
-				expect(fakeIframe.style.width).toBe('20px');
-				expect(fakeAdSlot.style.height).toBe('14px');
-				expect(fakeAdSlot.style.width).toBe('7px');
-			});
+			);
+
+			expect(fakeIframe.style.height).toBe('10px');
+			expect(fakeIframe.style.width).toBe('20px');
+			expect(fakeAdSlot.style.height).toBe('14px');
+			expect(fakeAdSlot.style.width).toBe('7px');
 		});
 	});
 });
