0d5675320305375e24e2be8a66843c8f872e0699
Marjan K <marjank@30728.gnm.int>
2022-02-07 09:21:31 +0000

    add test for isAMatchInProgress for different matchLive status

--- sport/test/football/feed/CompetitionsTest.scala
+++ sport/test/football/feed/CompetitionsTest.scala
@@ -22,40 +22,44 @@ import pa.{
 class CompetitionsTest extends FreeSpec with Matchers with OptionValues {
   "Competitions" - {
     Seq(
-      (Duration.ofSeconds(1), true),
-      (Duration.ofMinutes(1), true),
-      (Duration.ofMinutes(4).plusSeconds(59), true),
-      (Duration.ofMinutes(5), false),
-      (Duration.ofMinutes(10), false),
+      (false, Duration.ofSeconds(1), true),
+      (false, Duration.ofMinutes(1), true),
+      (false, Duration.ofMinutes(4).plusSeconds(59), true),
+      (false, Duration.ofMinutes(5), false),
+      (false, Duration.ofMinutes(10), false),
+      (true, Duration.ofMinutes(5), true),
+      (true, Duration.ofMinutes(10), true),
     ) foreach { testcase =>
-      (s"isAMatchInProgress returns ${testcase._2} if there's a match that started ${testcase._1} ago") in {
+      (s"isAMatchInProgress returns ${testcase._3} if there's a match that started ${testcase._2} ago") in {
         val matches: Seq[FootballMatch] =
-          FootballTestData.matchesWithLiveMatchAtCurrentMinusDuration(testcase._1)
+          FootballTestData.matchesWithLiveMatchAtCurrentMinusDuration(testcase._2, testcase._1)
         val testCompetition = FootballTestData.competitions(1).copy(matches = matches)
         val competitions = Competitions(Seq(testCompetition))
 
         val isAMatchInProgress = competitions.isAMatchInProgress(competitions.matches, FootballTestData.clock)
 
-        isAMatchInProgress should equal(testcase._2)
+        isAMatchInProgress should equal(testcase._3)
       }
     }
 
     Seq(
-      (Duration.ofSeconds(1), true),
-      (Duration.ofMinutes(1), true),
-      (Duration.ofMinutes(4).plusSeconds(59), true),
-      (Duration.ofMinutes(5), false),
-      (Duration.ofMinutes(10), false),
+      (false, Duration.ofSeconds(1), true),
+      (false, Duration.ofMinutes(1), true),
+      (false, Duration.ofMinutes(4).plusSeconds(59), true),
+      (false, Duration.ofMinutes(5), false),
+      (false, Duration.ofMinutes(10), false),
+      (true, Duration.ofMinutes(5), true),
+      (true, Duration.ofMinutes(10), true),
     ) foreach { testcase =>
-      (s"isAMatchInProgress returns ${testcase._2} if there's a match that will in ${testcase._1}") in {
+      (s"isAMatchInProgress returns ${testcase._3} if there's a match with liveMatch status ${testcase._1} that will start in ${testcase._2}") in {
         val matches: Seq[FootballMatch] =
-          FootballTestData.matchesWithLiveMatchAtCurrentPlusDuration(testcase._1)
+          FootballTestData.matchesWithLiveMatchAtCurrentPlusDuration(testcase._2, testcase._1)
         val testCompetition = FootballTestData.competitions(1).copy(matches = matches)
         val competitions = Competitions(Seq(testCompetition))
         val test = FootballTestData.today
         val isAMatchInProgress = competitions.isAMatchInProgress(competitions.matches, FootballTestData.clock)
 
-        isAMatchInProgress should equal(testcase._2)
+        isAMatchInProgress should equal(testcase._3)
       }
     }
   }
@@ -212,18 +216,18 @@ class CompetitionsTest extends FreeSpec with Matchers with OptionValues {
       )
 
     // These are the values I added to use spesifically in the tests
-    val matchesWithLiveMatchAtCurrentMinusDuration = (duration: Duration) => {
+    def matchesWithLiveMatchAtCurrentMinusDuration(duration: Duration, liveMatchStatus: Boolean = false) = {
       Seq(
         result("Bolton", "Derby", 1, 1, today.minusDays(1), Some("Bolton win 4-2 on penalties.")),
-        liveMatch(false, "Cardiff", "Brighton", 2, 0, today.minus(duration)),
+        liveMatch(liveMatchStatus, "Cardiff", "Brighton", 2, 0, today.minus(duration)),
         fixture("Wolves", "Burnley", today.plusDays(2)),
       )
     }
 
-    val matchesWithLiveMatchAtCurrentPlusDuration = (duration: Duration) => {
+    def matchesWithLiveMatchAtCurrentPlusDuration(duration: Duration, liveMatchStatus: Boolean = false) = {
       Seq(
         result("Bolton", "Derby", 1, 1, today.minusDays(1), Some("Bolton win 4-2 on penalties.")),
-        liveMatch(false, "Cardiff", "Brighton", 2, 0, today.plus(duration)),
+        liveMatch(liveMatchStatus, "Cardiff", "Brighton", 2, 0, today.plus(duration)),
         fixture("Wolves", "Burnley", today.plusDays(2)),
       )
     }
