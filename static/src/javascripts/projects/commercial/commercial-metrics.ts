22da1a525a3b7a00efff93885d9e0b59c85caaeb
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2022-02-17 14:48:48 +0000

    Inline initMetrics code

--- static/src/javascripts/projects/commercial/commercial-metrics.ts
+++ static/src/javascripts/projects/commercial/commercial-metrics.ts
@@ -1,6 +1,6 @@
 import {
-	bypassCommercialMetricsSampling,
 	initCommercialMetrics,
+	bypassCommercialMetricsSampling as switchOffSampling,
 } from '@guardian/commercial-core';
 import { shouldCaptureMetrics } from '../common/modules/analytics/shouldCaptureMetrics';
 
@@ -9,37 +9,24 @@ const { pageViewId } = window.guardian.ophan;
 const { browserId } = window.guardian.config.ophan;
 const { adBlockers } = window.guardian;
 
-let logged = false;
-const initMetrics = (): void => {
-	if (logged) return;
-	const args: [string, string | undefined, boolean, boolean?] =
-		adBlockers.active === undefined
-			? [pageViewId, browserId, isDev]
-			: [pageViewId, browserId, isDev, adBlockers.active];
-
-	logged = initCommercialMetrics(...args);
-};
-
 const init = (): Promise<void> => {
 	if (!window.guardian.config.switches.commercialMetrics) {
 		return Promise.resolve();
 	}
 
-	initMetrics();
+	const args: [string, string | undefined, boolean, boolean?] =
+		adBlockers.active === undefined
+			? [pageViewId, browserId, isDev]
+			: [pageViewId, browserId, isDev, adBlockers.active];
+
+	initCommercialMetrics(...args);
 
 	if (shouldCaptureMetrics()) {
-		bypassCommercialMetricsSampling();
+		// TODO: rename upstream
+		switchOffSampling();
 	}
 
 	return Promise.resolve();
 };
 
-/**
- * A way to override the metrics sampling.
- * It is safe to call this method repeatedly, as per [the `EventListener` docs](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#multiple_identical_event_listeners)
- */
-const captureCommercialMetrics = (): void => {
-	bypassCommercialMetricsSampling();
-};
-
-export { init, captureCommercialMetrics };
+export { init };
