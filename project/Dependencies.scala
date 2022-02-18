df7ab0d4452370143f4fba77315d2589c7b7e182
James Gorrie <james.gorrie@theguardian.com>
2022-02-18 14:09:34 +0000

    bump facia lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val identityLibVersion = "3.240-C5"
   val awsVersion = "1.11.240"
   val capiVersion = "17.24.1"
-  val faciaVersion = "3.3.8"
+  val faciaVersion = "3.3.9"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
