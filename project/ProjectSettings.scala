9c291c43acec2928ae1add6a65c0ff5473d99edc
Justin Pinner <justinpinner@gmail.com>
2022-02-23 17:42:27 +0000

    Got as far as running dev-build with this config

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -32,12 +32,12 @@ object ProjectSettings {
     Compile / packageDoc / publishArtifact := false,
     Compile / doc / sources := Seq.empty,
     Compile / doc := target.map(_ / "none").value,
-    scalaVersion := "2.12.13",
+    scalaVersion := "2.12.15",
     initialize := {
       val _ = initialize.value
       assert(
-        sys.props("java.specification.version") == "11",
-        "Java 11 is required for this project.",
+        sys.props("java.specification.version") == "17",
+        "Java 17 is required for this project.",
       )
     },
     cleanAll := Def.taskDyn {
--- setup.sh
+++ setup.sh
@@ -66,7 +66,7 @@ region = eu-west-1" > "$path/$filename"
 
 install_homebrew() {
   if mac && ! installed brew; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 }
 
