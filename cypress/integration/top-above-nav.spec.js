4c2316d5cf6e4353faa54d5e4b02660e421cc4a2
Chris Jones <christopher.jones@guardian.co.uk>
2022-02-24 17:14:59 +0000

    SPIKE: End to end commercial tests with Cypress (#24656)
    * Install Cypress
    
    * Setup Cypress and add simple tests
    
    * Add auto-generated Cypress files
    
    Cypress auto-generates these everytime it runs, if they don't exist.
    
    Check them in so you don't keep creating files that don't live in
    version control.
    
    * Don't include auto-generated files and directories
    
    * Sanitise the stage early
    
    * Change default timeout in config
    
    * Bump default timeout to 15secs
    
    * Don't scroll to bottom twice in slots test

---
+++ cypress/integration/top-above-nav.spec.js
@@ -0,0 +1,62 @@
+/// <reference types="cypress" />
+
+import { getStage, getTestUrl } from '../lib/util';
+import { getIframeBody } from '../lib/iframe';
+
+// Don't fail tests when uncaught exceptions occur
+// This is because scripts loaded on the page and unrelated to these tests can cause this
+Cypress.on('uncaught:exception', (err, runnable) => {
+	return false;
+});
+
+const stage = getStage();
+
+const pages = [
+	{
+		path: getTestUrl(stage, ''),
+		adTest: 'fixed-puppies',
+	},
+	{
+		path: getTestUrl(
+			stage,
+			'/politics/2022/feb/10/keir-starmer-says-stop-the-war-coalition-gives-help-to-authoritarians-like-putin',
+			{ isDcr: true },
+		),
+		adTest: 'fixed-puppies',
+	},
+	{
+		path: getTestUrl(
+			stage,
+			'/politics/live/2022/jan/31/uk-politics-live-omicron-nhs-workers-coronavirus-vaccines-no-10-sue-gray-report',
+			{ isDcr: false },
+		),
+		adTest: 'fixed-puppies',
+	},
+	{
+		path: getTestUrl(
+			stage,
+			'/sport/2022/feb/10/team-gb-winter-olympic-struggles-go-on-with-problems-for-skeleton-crew',
+			{ isDcr: true },
+		),
+		adTest: 'fixed-puppies',
+	},
+];
+
+describe('top-above-nav on pages', () => {
+	pages.forEach(({ path, adTest }) => {
+		it(`Test ${path} has top-above-nav slot and iframe`, () => {
+			cy.visit(`${path}?adtest=${adTest}`);
+
+			// Click "Yes, I'm happy" on the sourcepoint banner to obtain consent
+			getIframeBody('sp_message_iframe_597005')
+				.find('.btn-primary')
+				.click();
+
+			// Check that the top-above-nav ad slot is on the page
+			cy.get('#dfp-ad--top-above-nav').should('exist');
+
+			// Check that an iframe is placed inside the ad slot
+			cy.get('#dfp-ad--top-above-nav').find('iframe').should('exist');
+		});
+	});
+});
