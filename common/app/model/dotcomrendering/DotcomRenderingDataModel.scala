6534674d9011bf5df5081e31a5779ad0446a501c
Alina Boghiu <alina.boghiu@gmail.com>
2022-02-25 10:32:31 +0000

    Read latest block id from body blocks when first page is not requested (#24704)
    * Read latest block id from body blocks when first page is not requested
    
    * formatting
    
    * remove unused import

--- common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
+++ common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
@@ -259,8 +259,11 @@ object DotcomRenderingDataModel {
         .getOrElse(blocks.body.fold(Seq.empty[APIBlock])(_.filter(_.attributes.pinned.contains(true))))
         .headOption
 
-    val mostRecentBlockId =
-      blocks.requestedBodyBlocks.flatMap(_.get(CanonicalLiveBlog.firstPage).flatMap(_.headOption)).map(_.id)
+    val mostRecentBlockId = blocks.requestedBodyBlocks
+      .flatMap(_.get(CanonicalLiveBlog.firstPage))
+      .getOrElse(blocks.body.getOrElse(Seq.empty))
+      .headOption
+      .map(_.id)
 
     apply(
       page,
