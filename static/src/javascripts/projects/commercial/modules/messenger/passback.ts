360e3f4d0e90bc0dc9793f72e596e3e085ce664c
Ravi Lal <arelra@users.noreply.github.com>
2022-03-09 14:58:47 +0000

    Passback attempts to get slotId from the closest iFrame

--- static/src/javascripts/projects/commercial/modules/messenger/passback.ts
+++ static/src/javascripts/projects/commercial/modules/messenger/passback.ts
@@ -1,31 +1,36 @@
 import { adSlotIdPrefix } from '../dfp/dfp-env-globals';
 import type { RegisterListener } from '../messenger';
 
-type PassbackPayload = {
+type PassbackMessagePayload = {
 	slotId: string;
 	source: string;
 };
 
 const init = (register: RegisterListener): void => {
-	register('passback', (payloadFromCreative) => {
+	register('passback', (messagePayload, ret, iframe) => {
 		window.googletag.cmd.push(function () {
-			const payload = payloadFromCreative as PassbackPayload;
-			const { slotId, source } = payload;
-			const slotIdWithPrefix = `${adSlotIdPrefix}${slotId}`;
-			const slot = window.googletag
-				.pubads()
-				.getSlots()
-				.find((s) => s.getSlotElementId() === slotIdWithPrefix);
-			if (slot) {
-				// TODO use size mappings frrom commercial-core
-				slot.defineSizeMapping([
-					[[1024, 768], [[300, 250]]],
-					[[640, 480], [[300, 250]]],
-					[[0, 0], []],
-				]);
-				slot.setTargeting('passback', [source]);
-				slot.setTargeting('slot', slotId);
-				window.googletag.pubads().refresh([slot]);
+			const payload = messagePayload as PassbackMessagePayload;
+			const { slotId: slotIdFromMessage, source } = payload;
+			const slotIdFromIframe =
+				iframe?.closest<HTMLDivElement>('.ad-slot')?.dataset.name;
+			const slotId = slotIdFromMessage || slotIdFromIframe;
+			if (slotId) {
+				const slotIdWithPrefix = `${adSlotIdPrefix}${slotId}`;
+				const slot = window.googletag
+					.pubads()
+					.getSlots()
+					.find((s) => s.getSlotElementId() === slotIdWithPrefix);
+				if (slot) {
+					// TODO use size mappings frrom commercial-core
+					slot.defineSizeMapping([
+						[[1024, 768], [[300, 250]]],
+						[[640, 480], [[300, 250]]],
+						[[0, 0], []],
+					]);
+					slot.setTargeting('passback', [source]);
+					slot.setTargeting('slot', slotId);
+					window.googletag.pubads().refresh([slot]);
+				}
 			}
 		});
 	});
