58dfb124dddc215a3c3437b6069b894d637b641d
Ravi Lal <arelra@users.noreply.github.com>
2022-03-10 11:31:43 +0000

    Passback add comments

--- static/src/javascripts/projects/commercial/modules/messenger/passback.ts
+++ static/src/javascripts/projects/commercial/modules/messenger/passback.ts
@@ -6,16 +6,27 @@ type PassbackMessagePayload = {
 	source: string;
 };
 
+/**
+ * A listener for 'passback' messages from ad slot iFrames
+ * Ad providers will invoke 'passback' to tell us they have not filled this slot
+ * In which case we need to refresh the slot with another ad
+ */
 const init = (register: RegisterListener): void => {
 	register('passback', (messagePayload, ret, iframe) => {
 		window.googletag.cmd.push(function () {
 			const payload = messagePayload as PassbackMessagePayload;
 			const { slotId: slotIdFromMessage, source } = payload;
+			/**
+			 * Attempt to get the slotId from the calling iFrame as provided by messenger
+			 */
 			const slotIdFromIframe =
 				iframe?.closest<HTMLDivElement>('.ad-slot')?.dataset.name;
 			const slotId = slotIdFromMessage || slotIdFromIframe;
 			if (slotId) {
 				const slotIdWithPrefix = `${adSlotIdPrefix}${slotId}`;
+				/**
+				 * Find the live slot object from googletag
+				 */
 				const slot = window.googletag
 					.pubads()
 					.getSlots()
@@ -27,6 +38,10 @@ const init = (register: RegisterListener): void => {
 						[[640, 480], [[300, 250]]],
 						[[0, 0], []],
 					]);
+					/**
+					 * Set passback targeting param so the passback line item can negatively target
+					 * This ensures that passback line items do not match for this request
+					 */
 					slot.setTargeting('passback', [source]);
 					slot.setTargeting('slot', slotId);
 					window.googletag.pubads().refresh([slot]);
