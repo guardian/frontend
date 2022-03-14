15fcf7c7d841f92d663a953b96ffc9e2703fbd97
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-03-14 17:35:04 +0000

    Create commercial-lazy-load-margin.ts

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/commercial-lazy-load-margin.ts
@@ -0,0 +1,58 @@
+import type { ABTest } from '@guardian/ab-core';
+import { noop } from '../../../../../lib/noop';
+
+export const commercialLazyLoadMargin: ABTest = {
+	id: 'CommercialLazyLoadMargin',
+	start: '2022-03-17',
+	// test should be in place for a minimum of 14 days
+	expiry: '2020-04-01',
+	author: 'Zeke Hunter-Green',
+	description:
+		'Test various margins at which ads are lazily-loaded in order to find the optimal one',
+	audience: 5 / 100,
+	audienceOffset: 11 / 100,
+	successMeasure: 'Ad ratio, viewability, and CLS remain constant or improve',
+	audienceCriteria: 'n/a',
+	dataLinkNames: 'n/a',
+	idealOutcome:
+		'One of the variants shows a marked improvement in all of the metrics that we are considering',
+	canRun: () => true,
+	variants: [
+		{
+			id: 'control',
+			test: noop,
+		},
+		{
+			id: 'variant 1',
+			test: noop,
+		},
+		{
+			id: 'variant 2',
+			test: noop,
+		},
+		{
+			id: 'variant 3',
+			test: noop,
+		},
+		{
+			id: 'variant 4',
+			test: noop,
+		},
+		{
+			id: 'variant 5',
+			test: noop,
+		},
+		{
+			id: 'variant 6',
+			test: noop,
+		},
+		{
+			id: 'variant 7',
+			test: noop,
+		},
+		{
+			id: 'variant 8',
+			test: noop,
+		},
+	],
+};
