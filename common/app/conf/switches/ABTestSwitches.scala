c82c6d1078e108e9a09ffa4736e5cafbe9479969
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-03-14 17:34:20 +0000

    create switch for ab test

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -43,4 +43,14 @@ trait ABTestSwitches {
     sellByDate = Some(LocalDate.of(2022, 5, 2)),
     exposeClientSide = true,
   )
+
+  Switch(
+    ABTests,
+    "ab-commercial-lazy-load-margin",
+    "Test various margins at which ads are lazily-loaded in order to find the optimal one",
+    owners = Seq(Owner.withGithub("zekehuntergreen")),
+    safeState = Off,
+    sellByDate = Some(LocalDate.of(2022, 4, 1)),
+    exposeClientSide = true,
+  )
 }
