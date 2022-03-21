501116cf11599ec549d741b6ef13d4b1d18897d8
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-03-21 16:23:42 +0000

    remove event listener in stopListening

--- static/src/javascripts/projects/commercial/modules/liveblog-adverts.js
+++ static/src/javascripts/projects/commercial/modules/liveblog-adverts.js
@@ -17,13 +17,18 @@ let firstSlot;
 
 const startListening = () => {
 	// eslint-disable-next-line no-use-before-define
+	// frontend - remove when migration to DCR is complete
 	mediator.on('modules:autoupdate:updates', onUpdate);
+	// DCR
 	document.addEventListener('liveblog:blocks-updated', onUpdate);
 };
 
 const stopListening = () => {
 	// eslint-disable-next-line no-use-before-define
+	// frontend - remove when migration to DCR is complete
 	mediator.off('modules:autoupdate:updates', onUpdate);
+	// DCR
+	document.removeEventListener('liveblog:blocks-updated', onUpdate);
 };
 
 const getWindowHeight = (doc = document) => {
