c4fed10d96f7c13503c960075908851a642c9cca
David Lawes <david.lawes@guardian.co.uk>
2022-03-22 12:58:53 +0000

    add comments

--- common/app/common/Box.scala
+++ common/app/common/Box.scala
@@ -20,7 +20,13 @@ object Box {
   def apply[T](initialValue: T): Box[T] = new AtomicRefBox[T](initialValue)
 }
 
+// Note the omission of modify, map and flatMap
+// (https://github.com/guardian/box/blob/master/src/main/scala/com/gu/Box.scala#L38).
+// These functions aren't used in frontend and the modify function had not been
+// implemented correctly in the Box library.
 private class AtomicRefBox[T](initialValue: T) extends Box[T] {
+  // Note the name of this initial value. This has a different name those in
+  // the methods below to ensure the correct scope of named variables.
   private val ref: AtomicReference[T] = new AtomicReference[T](initialValue)
 
   def apply(): T = ref.get()
