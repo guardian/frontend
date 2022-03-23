c413d4cb19e3599c66977cac16ffa439f1e03e5f
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-03-23 16:23:34 +0000

    update experiment dates

--- static/src/javascripts/projects/common/modules/experiments/tests/commercial-gpt-lazy-load.ts
+++ static/src/javascripts/projects/common/modules/experiments/tests/commercial-gpt-lazy-load.ts
@@ -3,11 +3,11 @@ import { noop } from '../../../../../lib/noop';
 
 export const commercialGptLazyLoad: ABTest = {
 	id: 'CommercialGptLazyLoad',
-	start: '2022-03-14',
-	expiry: '2020-03-23',
+	start: '2022-03-24',
+	expiry: '2020-04-01',
 	author: 'Zeke Hunter-Green',
 	description:
-		'This test enables GPT enableLazyLoad as an alternative to our custom build lazy load solution',
+		'This test compares GPT enableLazyLoad to our custom-built lazy load solution',
 	audience: 1 / 100,
 	audienceOffset: 10 / 100,
 	successMeasure: 'Ad ratio and viewability remain constant or improve',
