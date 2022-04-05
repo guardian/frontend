bcadea1218c43359a22e07d56e06ef035af0de22
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-04-05 16:55:18 +0100

    Test right ad slot (#24853)
    * update iframe selector to id prefix
    
    * Move `pages` to fixtures file
    
    * Create right-slot.spec.js
    
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>
    Co-authored-by: Chris Owen <chris.j.owen@hotmail.co.uk>

---
+++ cypress/fixtures/pages.js
@@ -0,0 +1,44 @@
+import { getStage, getTestUrl } from '../lib/util';
+
+const stage = getStage();
+
+const fronts = [
+	{
+		path: getTestUrl(stage, 'uk'),
+		adTest: 'fixed-puppies',
+	},
+];
+
+const articles = [
+	{
+		path: getTestUrl(
+			stage,
+			'/politics/2022/feb/10/keir-starmer-says-stop-the-war-coalition-gives-help-to-authoritarians-like-putin',
+			{ isDcr: true },
+		),
+		adTest: 'fixed-puppies',
+	},
+	{
+		path: getTestUrl(
+			stage,
+			'/sport/2022/feb/10/team-gb-winter-olympic-struggles-go-on-with-problems-for-skeleton-crew',
+			{ isDcr: true },
+		),
+		adTest: 'fixed-puppies',
+	},
+];
+
+const liveblogs = [
+	{
+		path: getTestUrl(
+			stage,
+			'/politics/live/2022/jan/31/uk-politics-live-omicron-nhs-workers-coronavirus-vaccines-no-10-sue-gray-report',
+			{ isDcr: false },
+		),
+		adTest: 'fixed-puppies',
+	},
+];
+
+const pages = [...fronts, ...articles, ...liveblogs];
+
+export { pages, articles, liveblogs };
