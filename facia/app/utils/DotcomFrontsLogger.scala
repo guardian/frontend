18ed247bdfa552e68dc3d270a7d8670b12ba385a
AshCorr <ashcorr20@gmail.com>
2022-04-05 13:08:32 +0100

    refactor: Clean up of DotcomFrontsLogger

--- facia/app/utils/DotcomFrontsLogger.scala
+++ facia/app/utils/DotcomFrontsLogger.scala
@@ -2,82 +2,47 @@ package utils
 
 import common.GuLogging
 import common.LoggingField._
-import model.{PageWithStoryPackage, PressedPage}
-import model.liveblog.InteractiveBlockElement
+import model.PressedPage
 import play.api.mvc.RequestHeader
 
-import scala.util.{Random, Try}
+import scala.util.Random
 
-case class DotcomponentsLoggerFields(request: Option[RequestHeader]) {
-
-  lazy val customFields: List[LogField] = {
-
-    val requestHeaders: List[LogField] = request
-      .map { r: RequestHeader =>
-        List[LogField](
-          "req.method" -> r.method,
-          "req.url" -> r.uri,
-          "req.id" -> Random.nextInt(Integer.MAX_VALUE).toString,
-        )
-      }
-      .getOrElse(Nil)
-
-    requestHeaders
-
+case class DotcomFrontsLogger() extends GuLogging {
+  def logFieldFromRequest(request: RequestHeader): List[LogField] = {
+    List[LogField](
+      "req.method" -> request.method,
+      "req.url" -> request.uri,
+      "req.id" -> Random.nextInt(Integer.MAX_VALUE).toString,
+    )
   }
 
-}
-
-case class DotcomFrontsLogger(request: Option[RequestHeader]) extends GuLogging {
-
-  private def customFields: List[LogField] = DotcomponentsLoggerFields(request).customFields
-
-  def fieldsFromResults(results: Map[String, String]): List[LogField] =
-    results.map({ case (k, v) => LogFieldString(k, v) }).toList
+  def logFieldFromProperties(properties: Map[String, String]): List[LogField] =
+    properties.map({ case (k, v) => LogFieldString(k, v) }).toList
 
   def logFieldFromFront(faciaPage: PressedPage): List[LogField] = {
     List(
       LogFieldString(
-        "page.id",
-        faciaPage.id
+        "front.id",
+        faciaPage.id,
       ),
       LogFieldString(
-        "page.containers",
+        "front.collections",
         faciaPage.collections.map(_.collectionType).mkString(", "),
       ),
     )
   }
 
-  def logRequest(msg: String, results: Map[String, String], faciaPage: PressedPage)(implicit
+  def logRequest(msg: String, properties: Map[String, String], faciaPage: PressedPage)(implicit
       request: RequestHeader,
   ): Unit = {
-    withRequestHeaders(request).results(msg, results, faciaPage)
+    log(msg, logFieldFromProperties(properties) ++ logFieldFromRequest(request), faciaPage)
   }
 
-  def withRequestHeaders(rh: RequestHeader): DotcomFrontsLogger = {
-    copy(Some(rh))
+  def log(message: String, results: List[LogField], faciaPage: PressedPage): Unit = {
+    logInfoWithCustomFields(message, results ++ logFieldFromFront(faciaPage))
   }
-
-  def results(message: String, results: Map[String, String], faciaPage: PressedPage): Unit = {
-    logInfoWithCustomFields(message, customFields ++ fieldsFromResults(results) ++ logFieldFromFront(faciaPage))
-  }
-
-  def info(message: String): Unit = {
-    logInfoWithCustomFields(message, customFields)
-  }
-
-  def warn(message: String, error: Throwable): Unit = {
-    logWarningWithCustomFields(message, error, customFields)
-  }
-
-  def error(message: String, error: Throwable): Unit = {
-    logErrorWithCustomFields(message, error, customFields)
-  }
-
 }
 
 object DotcomFrontsLogger {
   val logger = DotcomFrontsLogger()
-
-  def apply(): DotcomFrontsLogger = DotcomFrontsLogger(None)
 }
