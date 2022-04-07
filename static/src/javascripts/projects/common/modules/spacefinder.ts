9b34d1ca48af9f181b3a183ddd2e98ff65cf6219
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-04-07 12:03:36 +0100

    spacefinder.js -> .ts, moved types to spacefinder

--- static/src/javascripts/projects/common/modules/spacefinder.ts
+++ static/src/javascripts/projects/common/modules/spacefinder.ts
@@ -1,17 +1,66 @@
 // total_hours_spent_maintaining_this = 80
 
-import fastdom from '../../../lib/fastdom-promise';
-import { mediator } from '../../../lib/mediator';
 import { memoize } from 'lodash-es';
-import { markCandidates } from './mark-candidates';
-
-import { onImagesLoadedFixed } from './on-images-loaded-fixed.js';
-import { onImagesLoadedBroken } from './on-images-loaded-broken.js';
 import { isInVariantSynchronous } from 'common/modules/experiments/ab';
 import { spacefinderOkrMegaTest } from 'common/modules/experiments/tests/spacefinder-okr-mega-test';
+import { noop } from 'lib/noop';
+import fastdom from '../../../lib/fastdom-promise';
+import { mediator } from '../../../lib/mediator';
+import { markCandidates } from './mark-candidates';
+import { onImagesLoadedBroken } from './on-images-loaded-broken.js';
+import { onImagesLoadedFixed } from './on-images-loaded-fixed.js';
 
-const query = (selector, context) => [
-	...(context ?? document).querySelectorAll(selector),
+type RuleSpacing = {
+	minAbove: number;
+	minBelow: number;
+};
+
+type SpacefinderItem = {
+	top: number;
+	bottom: number;
+	element: HTMLElement;
+};
+
+type SpacefinderRules = {
+	bodySelector: string;
+	body?: Node;
+	slotSelector: string;
+	// minimum from slot to top of page
+	absoluteMinAbove?: number;
+	// minimum from para to top of article
+	minAbove: number;
+	// minimum from (top of) para to bottom of article
+	minBelow: number;
+	// vertical px to clear the content meta element (byline etc) by. 0 to ignore
+	// used for carrot ads
+	clearContentMeta?: number;
+	// custom rules using selectors.
+	selectors: Record<string, RuleSpacing>;
+	// will run each slot through this fn to check if it must be counted in
+	filter?: (x: SpacefinderItem, lastWinner?: SpacefinderItem) => boolean;
+	// will remove slots before this one
+	startAt?: HTMLElement;
+	// will remove slots from this one on
+	stopAt?: HTMLElement;
+	// will reverse the order of slots (this is useful for lazy loaded content)
+	fromBottom?: boolean;
+};
+
+type SpacefinderWriter = (paras: HTMLElement[]) => Promise<void>;
+
+type SpacefinderOptions = {
+	waitForLinks?: boolean;
+	waitForImages?: boolean;
+	waitForInteractives?: boolean;
+	debug?: boolean;
+};
+
+type ExcludedItem = SpacefinderItem | HTMLElement;
+
+export type SpacefinderExclusions = Record<string, ExcludedItem[]>;
+
+const query = (selector: string, context?: HTMLElement) => [
+	...(context ?? document).querySelectorAll<HTMLElement>(selector),
 ];
 
 // maximum time (in ms) to wait for images to be loaded and rich links
@@ -37,10 +86,6 @@ const isIframeLoaded = (iframe) => {
 	} catch (err) {
 		return true;
 	}
-};
-
-const expire = (resolve) => {
-	window.setTimeout(resolve, LOADING_TIMEOUT);
 };
 
 const getFuncId = (rules) => rules.bodySelector || 'document';
@@ -103,9 +148,15 @@ const onInteractivesLoaded = memoize((rules) => {
 		  ).then(() => undefined);
 }, getFuncId);
 
-const filter = (list, filterElement) => {
-	const filtered = [];
-	const exclusions = [];
+const partitionCandidates = (
+	list: HTMLElement[],
+	filterElement: (
+		element: HTMLElement,
+		lastFilteredElement: HTMLElement,
+	) => boolean,
+) => {
+	const filtered: HTMLElement[] = [];
+	const exclusions: HTMLElement[] = [];
 	list.forEach((element) => {
 		if (filterElement(element, filtered[filtered.length - 1])) {
 			filtered.push(element);
@@ -152,7 +203,7 @@ const enforceRules = (measurements, rules, exclusions) => {
 	let result;
 
 	// enforce absoluteMinAbove rule
-	result = filter(
+	result = partitionCandidates(
 		candidates,
 		(candidate) =>
 			!rules.absoluteMinAbove ||
@@ -162,7 +213,7 @@ const enforceRules = (measurements, rules, exclusions) => {
 	candidates = result.filtered;
 
 	// enforce minAbove and minBelow rules
-	result = filter(candidates, (candidate) => {
+	result = partitionCandidates(candidates, (candidate) => {
 		const farEnoughFromTopOfBody = candidate.top >= rules.minAbove;
 		const farEnoughFromBottomOfBody =
 			candidate.top + rules.minBelow <= measurements.bodyHeight;
@@ -173,7 +224,7 @@ const enforceRules = (measurements, rules, exclusions) => {
 
 	// enforce content meta rule
 	if (rules.clearContentMeta) {
-		result = filter(
+		result = partitionCandidates(
 			candidates,
 			(c) =>
 				!!measurements.contentMeta &&
@@ -188,7 +239,7 @@ const enforceRules = (measurements, rules, exclusions) => {
 	if (rules.selectors) {
 		const selectorExclusions = [];
 		Object.keys(rules.selectors).forEach((selector) => {
-			result = filter(candidates, (candidate) =>
+			result = partitionCandidates(candidates, (candidate) =>
 				testCandidates(
 					rules.selectors[selector],
 					candidate,
@@ -207,7 +258,7 @@ const enforceRules = (measurements, rules, exclusions) => {
 	}
 
 	if (rules.filter) {
-		result = filter(candidates, rules.filter);
+		result = partitionCandidates(candidates, rules.filter);
 		exclusions.custom = result.exclusions;
 		candidates = result.filtered;
 	}
@@ -222,26 +273,36 @@ class SpaceError extends Error {
 		this.message = `There is no space left matching rules from ${rules.bodySelector}`;
 	}
 }
-
-const getReady = (rules, options) =>
+/**
+ * Wait for the page to be ready (images loaded, rich links upgraded, interactives loaded)
+ * or for LOADING_TIMEOUT to elapse, whichever comes first.
+ * @param  {SpacefinderRules} rules
+ * @param  {SpacefinderOptions} options
+ */
+const getReady = (rules: SpacefinderRules, options: SpacefinderOptions) =>
 	Promise.race([
-		new Promise(expire),
+		new Promise(() => {
+			window.setTimeout(noop, LOADING_TIMEOUT);
+		}),
 		Promise.all([
 			options.waitForImages ? onImagesLoaded(rules) : true,
 			options.waitForLinks ? onRichLinksUpgraded(rules) : true,
 			options.waitForInteractives ? onInteractivesLoaded(rules) : true,
 		]),
-	]).then(() => rules);
+	]);
 
-const getCandidates = (rules, exclusions) => {
+const getCandidates = (
+	rules: SpacefinderRules,
+	exclusions: SpacefinderExclusions,
+) => {
 	let candidates = query(rules.bodySelector + rules.slotSelector);
-	let result;
+	let result: HTMLElement[];
 	if (rules.fromBottom) {
 		candidates.reverse();
 	}
 	if (rules.startAt) {
 		let drop = true;
-		result = filter(candidates, (candidate) => {
+		result = partitionCandidates(candidates, (candidate: HTMLElement) => {
 			if (candidate === rules.startAt) {
 				drop = false;
 			}
@@ -252,7 +313,7 @@ const getCandidates = (rules, exclusions) => {
 	}
 	if (rules.stopAt) {
 		let keep = true;
-		result = filter(candidates, (candidate) => {
+		result = partitionCandidates(candidates, (candidate) => {
 			if (candidate === rules.stopAt) {
 				keep = false;
 			}
@@ -320,14 +381,16 @@ const returnCandidates = (rules, candidates) => {
 
 // Rather than calling this directly, use spaceFiller to inject content into the page.
 // SpaceFiller will safely queue up all the various asynchronous DOM actions to avoid any race conditions.
-const findSpace = (rules, options, excluded) => {
+const findSpace = (
+	rules: SpacefinderRules,
+	options: SpacefinderOptions = defaultOptions,
+	exclusions: SpacefinderExclusions = {},
+): HTMLElement[] => {
 	rules.body =
 		(rules.bodySelector && document.querySelector(rules.bodySelector)) ||
 		document;
 
-	const exclusions = excluded || {};
-
-	return getReady(rules, options || defaultOptions)
+	return getReady(rules, options)
 		.then(() => getCandidates(rules, exclusions))
 		.then((candidates) => getMeasurements(rules, candidates))
 		.then((measurements) => enforceRules(measurements, rules, exclusions))
@@ -340,4 +403,10 @@ export const _ = {
 	testCandidates, // exposed for unit testing
 };
 
-export { findSpace, SpaceError };
+export {
+	findSpace,
+	SpaceError,
+	SpacefinderRules,
+	SpacefinderWriter,
+	SpacefinderOptions,
+};
