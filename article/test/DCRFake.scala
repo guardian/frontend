d609867dd3d2ce79f9cd2b9d7126813af340ede6
Marjan Kalanaki <marji_sound@yahoo.com>
2022-04-11 09:15:46 +0100

    add getFilter to DCRFake

--- article/test/DCRFake.scala
+++ article/test/DCRFake.scala
@@ -2,12 +2,14 @@ package test
 
 import com.gu.contentapi.client.model.v1.{Block, Blocks}
 import model.Cached.RevalidatableResult
-import model.dotcomrendering.PageType
+import model.dotcomrendering.{FilterData, PageType}
 import model.{ApplicationContext, Cached, LiveBlogPage, PageWithStoryPackage}
+import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc.{RequestHeader, Result}
 import play.twirl.api.Html
 
+import scala.concurrent.duration.Duration
 import scala.concurrent.{ExecutionContext, Future}
 
 // It is always a mistake to rely on actual DCR output for tests.
@@ -24,6 +26,14 @@ class DCRFake(implicit context: ApplicationContext) extends renderers.DotcomRend
     Future(
       Cached(article)(RevalidatableResult.Ok(Html("FakeRemoteRender has found you out if you rely on this markup!"))),
     )
+  }
+
+  override def getFilters(
+      ws: WSClient,
+      liveblogId: String,
+      timeout: Duration = 60.seconds,
+  )(implicit request: RequestHeader): Future[Option[FilterData]] = {
+    Future.successful(None)
   }
 
   override def getBlocks(ws: WSClient, page: LiveBlogPage, blocks: Seq[Block])(implicit
