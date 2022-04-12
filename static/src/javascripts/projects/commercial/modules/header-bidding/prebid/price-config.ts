72911649be681e2873eedf123bcb900094b504bb
Chris Jones <christopher.jones@guardian.co.uk>
2022-04-12 17:03:42 +0100

    Add price granularity for ozone

--- static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.ts
+++ static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.ts
@@ -35,3 +35,56 @@ export const criteoPriceGranularity: PrebidPriceGranularity = {
 		},
 	],
 };
+
+/**
+ * Compute the price granularity for Ozone based on the width and height of the slot
+ */
+export const ozonePriceGranularity = (
+	width: number,
+	height: number,
+): PrebidPriceGranularity | undefined => {
+	switch ([width, height].join(',')) {
+		case '160,600':
+		case '300,600': {
+			return {
+				buckets: [
+					{
+						max: 10,
+						increment: 0.01,
+					},
+					{
+						max: 15,
+						increment: 0.1,
+					},
+					{
+						max: 50,
+						increment: 1,
+					},
+				],
+			};
+		}
+		case '728,90':
+		case '970,250':
+		case '300,250': {
+			return {
+				buckets: [
+					{
+						max: 12,
+						increment: 0.01,
+					},
+					{
+						max: 20,
+						increment: 0.1,
+					},
+					{
+						max: 50,
+						increment: 1,
+					},
+				],
+			};
+		}
+		default: {
+			return undefined;
+		}
+	}
+};
