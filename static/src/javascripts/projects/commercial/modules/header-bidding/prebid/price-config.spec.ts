8c41be326fe1557b4d9dd66f15b69cf94c5e8149
Chris Jones <christopher.jones@guardian.co.uk>
2022-04-13 09:51:49 +0100

    Perform deep equals on price config in test

--- static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.spec.ts
+++ static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.spec.ts
@@ -2,10 +2,36 @@ import { criteoPriceGranularity, priceGranularity } from './price-config';
 
 describe('priceGranularity', () => {
 	test('default should have correct number of buckets', () => {
-		expect(priceGranularity.buckets.length).toBe(2);
+		expect(priceGranularity).toEqual({
+			buckets: [
+				{
+					max: 100,
+					increment: 0.01,
+				},
+				{
+					max: 500,
+					increment: 1,
+				},
+			],
+		});
 	});
 
 	test('criteo should have correct number of buckets', () => {
-		expect(criteoPriceGranularity.buckets.length).toBe(3);
+		expect(criteoPriceGranularity).toEqual({
+			buckets: [
+				{
+					max: 12,
+					increment: 0.01,
+				},
+				{
+					max: 20,
+					increment: 0.05,
+				},
+				{
+					max: 500,
+					increment: 1,
+				},
+			],
+		});
 	});
 });
