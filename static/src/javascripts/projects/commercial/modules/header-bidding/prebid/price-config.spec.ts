4205f37183f9faf41b052156c8d72fbdcb07545c
Chris Jones <christopher.jones@guardian.co.uk>
2022-04-14 14:42:27 +0100

    Add test for ozone price granularity

--- static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.spec.ts
+++ static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.spec.ts
@@ -1,4 +1,8 @@
-import { criteoPriceGranularity, priceGranularity } from './price-config';
+import {
+	criteoPriceGranularity,
+	ozonePriceGranularity,
+	priceGranularity,
+} from './price-config';
 
 describe('priceGranularity', () => {
 	test('default should have correct buckets', () => {
@@ -34,4 +38,54 @@ describe('priceGranularity', () => {
 			],
 		});
 	});
+
+	const granularityOption1 = {
+		buckets: [
+			{
+				max: 10,
+				increment: 0.01,
+			},
+			{
+				max: 15,
+				increment: 0.1,
+			},
+			{
+				max: 50,
+				increment: 1,
+			},
+		],
+	};
+
+	const granularityOption2 = {
+		buckets: [
+			{
+				max: 12,
+				increment: 0.01,
+			},
+			{
+				max: 20,
+				increment: 0.1,
+			},
+			{
+				max: 50,
+				increment: 1,
+			},
+		],
+	};
+
+	test.each([
+		[[100, 100], undefined],
+		[[160, 600], granularityOption1],
+		[[300, 600], granularityOption1],
+		[[728, 90], granularityOption2],
+		[[970, 250], granularityOption2],
+		[[300, 250], granularityOption2],
+	])(
+		'Ozone slot with size %d,%d gives correct granularity',
+		([width, height], expectedGranularity) => {
+			expect(ozonePriceGranularity(width, height)).toEqual(
+				expectedGranularity,
+			);
+		},
+	);
 });
