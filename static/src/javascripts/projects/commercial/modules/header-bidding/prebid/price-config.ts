384767f36739200e652962a0f56f5c516d7a7ba4
Chris Jones <christopher.jones@guardian.co.uk>
2022-04-21 15:45:37 +0100

    Use ad sizes when determining price config

--- static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.ts
+++ static/src/javascripts/projects/commercial/modules/header-bidding/prebid/price-config.ts
@@ -1,3 +1,5 @@
+import { adSizes } from '@guardian/commercial-core';
+
 export type PrebidPriceGranularity = {
 	buckets: Array<{
 		precision?: number;
@@ -43,48 +45,52 @@ export const ozonePriceGranularity = (
 	width: number,
 	height: number,
 ): PrebidPriceGranularity | undefined => {
-	switch ([width, height].join(',')) {
-		case '160,600':
-		case '300,600': {
-			return {
-				buckets: [
-					{
-						max: 10,
-						increment: 0.01,
-					},
-					{
-						max: 15,
-						increment: 0.1,
-					},
-					{
-						max: 50,
-						increment: 1,
-					},
-				],
-			};
-		}
-		case '728,90':
-		case '970,250':
-		case '300,250': {
-			return {
-				buckets: [
-					{
-						max: 12,
-						increment: 0.01,
-					},
-					{
-						max: 20,
-						increment: 0.1,
-					},
-					{
-						max: 50,
-						increment: 1,
-					},
-				],
-			};
-		}
-		default: {
-			return undefined;
-		}
+	const sizeString = [width, height].join(',');
+
+	if (
+		sizeString === adSizes.skyscraper.toString() ||
+		sizeString === adSizes.halfPage.toString()
+	) {
+		return {
+			buckets: [
+				{
+					max: 10,
+					increment: 0.01,
+				},
+				{
+					max: 15,
+					increment: 0.1,
+				},
+				{
+					max: 50,
+					increment: 1,
+				},
+			],
+		};
 	}
+
+	if (
+		sizeString === adSizes.leaderboard.toString() ||
+		sizeString === adSizes.billboard.toString() ||
+		sizeString === adSizes.mpu.toString()
+	) {
+		return {
+			buckets: [
+				{
+					max: 12,
+					increment: 0.01,
+				},
+				{
+					max: 20,
+					increment: 0.1,
+				},
+				{
+					max: 50,
+					increment: 1,
+				},
+			],
+		};
+	}
+
+	return undefined;
 };
