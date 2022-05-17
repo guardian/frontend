5c6280b6b8b26cb41e373199b36ffd75b342a227
David Blatcher <dv.bltchr@gmail.com>
2022-05-17 17:17:54 +0100

    dont duplicate emailIframeTracking code in iframeCaptcha file

--- common/app/templates/inlineJS/nonBlocking/signUpIframeCaptcha.scala.js
+++ common/app/templates/inlineJS/nonBlocking/signUpIframeCaptcha.scala.js
@@ -1,27 +1,5 @@
 @()
 
-const sendEvent = (payload, eventType) => {
-    const msg = {
-        id: 'xxxxxxxxxx'.replace(/x/g, () =>
-            // eslint-disable-next-line no-bitwise
-            ((Math.random() * 36) | 0).toString(36)
-        ),
-        type: `ophan-iframe-${eventType}`,
-        iframeId: window.frameElement ? window.frameElement.id : null,
-        value: payload,
-    };
-    window.parent.postMessage(msg, '*');
-    return msg.id;
-};
-
-const getClickEvent = (el) => {
-    return {
-        clickComponent: el.getAttribute('data-component'), clickLinkNames: [el.getAttribute('data-link-name')]
-    }
-}
-
-
-
 window.addEventListener('message', (event) => {
 	const allowedOrigins = ['https://www.theguardian.com'];
 	if (!allowedOrigins.includes(event.origin)) return;
