f99ca71d8cbc9ce8ea4db33a3781139cf7011769
David Blatcher <dv.bltchr@gmail.com>
2022-05-17 15:19:19 +0100

    move iframe code to scala.js file

---
+++ common/app/templates/inlineJS/nonBlocking/signUpIframeCaptcha.scala.js
@@ -0,0 +1,108 @@
+@()
+
+window.addEventListener('message', (event) => {
+	const allowedOrigins = ['https://www.theguardian.com'];
+	if (!allowedOrigins.includes(event.origin)) return;
+	if (event.data === 'resize') resizeToCurrentHeight();
+});
+setupSubmitListener();
+resizeToCurrentHeight();
+
+function resizeToCurrentHeight() {
+	iframeMessenger.resize();
+}
+
+const resizeToOriginalHeight = (() => {
+	const originalHeight = document.body.clientHeight;
+	return () => sendResizeMessage(originalHeight);
+})();
+
+function sendResizeMessage(height) {
+	iframeMessenger.resize(height);
+}
+
+function resizeToFitCaptcha() {
+	sendResizeMessage(500);
+}
+
+function setupSubmitListener() {
+	const submitButton = document.querySelector(
+		'.email-sub__form button[type=submit]'
+	);
+	submitButton.addEventListener('click', (e) => onSubmit(e));
+}
+
+function validateEmail(input) {
+	return input.indexOf('.') !== -1;
+}
+
+function validateForm() {
+	const formElement = document.querySelector('form');
+	const submitButton = formElement.querySelector('[type=submit]');
+	const emailField = formElement.querySelector('[type=email]');
+	if (!submitButton || !emailField) {
+		alert('missing field');
+		return false;
+	}
+
+	const emailValue = emailField.value;
+
+	if (
+		!emailValue ||
+		emailValue.length > maxEmailLength ||
+		!validateEmail(emailValue)
+	) {
+		alert('INVALID INPUT');
+		return false;
+	}
+
+	return true;
+}
+
+function onSubmit(e) {
+	e.preventDefault();
+
+	var formIsValid = validateForm();
+
+	console.log({ formIsValid });
+
+	if (formIsValid) {
+		(function (d, script) {
+			script = d.createElement('script');
+			script.type = 'text/javascript';
+			script.async = true;
+			script.defer = true;
+			script.src =
+				'https://www.google.com/recaptcha/api.js?onload=onRecaptchaScriptLoaded&render=explicit';
+			d.getElementsByTagName('head')[0].appendChild(script);
+		})(document);
+	}
+}
+
+function onRecaptchaScriptLoaded() {
+	resizeToFitCaptcha();
+	const captchaContainer = document.getElementsByClassName(
+		'grecaptcha_container'
+	)[0];
+	grecaptcha.render(captchaContainer, {
+		sitekey: window.guardian.config.page.googleRecaptchaSiteKey,
+		callback: onCaptchaCompleted,
+		'error-callback': onCaptchaError,
+		'expired-callback': onCaptchaExpired,
+		size: 'invisible',
+	});
+	grecaptcha.execute();
+}
+
+function onCaptchaCompleted(token) {
+	resizeToOriginalHeight();
+	document.querySelector('.email-sub__form').submit();
+}
+
+function onCaptchaError() {
+	resizeToOriginalHeight();
+}
+
+function onCaptchaExpired() {
+	resizeToOriginalHeight();
+}
