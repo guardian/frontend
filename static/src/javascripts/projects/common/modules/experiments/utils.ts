4832e2d37d7d4253a41242e28d33d20a41602cb0
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-06-20 15:21:16 +0100

    remove logs

--- static/src/javascripts/projects/common/modules/experiments/utils.ts
+++ static/src/javascripts/projects/common/modules/experiments/utils.ts
@@ -2,9 +2,6 @@ import { bypassCommercialMetricsSampling } from '@guardian/commercial-core';
 import { bypassCoreWebVitalsSampling } from '@guardian/libs';
 
 export const bypassMetricsSampling = (): void => {
-	console.log('overriding commercial sampling');
 	void bypassCommercialMetricsSampling();
-
-	console.log('overriding CWV sampling');
 	bypassCoreWebVitalsSampling();
 };
