306aaf5b7d3bf4526b447b27f32642eccffe69fd
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2022-06-21 13:34:11 +1000

    Remove auto rich links functionlity

--- static/src/javascripts/projects/common/modules/article/rich-links.js
+++ static/src/javascripts/projects/common/modules/article/rich-links.js
@@ -1,10 +1,9 @@
 import fastdom from 'lib/fastdom-promise';
 import config from 'lib/config';
-import { isBreakpoint, getBreakpoint } from 'lib/detect';
+import { isBreakpoint } from 'lib/detect';
 import { fetchJson } from 'lib/fetch-json';
 import { mediator } from 'lib/mediator';
 import reportError from 'lib/report-error';
-import { spaceFiller } from 'common/modules/article/space-filler';
 import { commercialFeatures } from 'common/modules/commercial/commercial-features';
 
 const richLinkTag = ({ href }) =>
@@ -94,84 +93,6 @@ const upgradeRichLink = (el) => {
     return Promise.resolve();
 };
 
-const getSpacefinderRules = () => ({
-    bodySelector: '.js-article__body',
-    slotSelector: ' > p',
-    minAbove: 200,
-    minBelow: 250,
-    clearContentMeta: 50,
-    selectors: {
-        ' > h2': {
-            minAbove: getBreakpoint() === 'mobile' ? 20 : 0,
-            minBelow: 200,
-        },
-        ' > *:not(p):not(h2):not(blockquote)': {
-            minAbove: 35,
-            minBelow: 300,
-        },
-        ' .ad-slot': {
-            minAbove: 150,
-            minBelow: 200,
-        },
-        ' .element-rich-link': {
-            minAbove: 500,
-            minBelow: 500,
-        },
-        ' .element--showcase': {
-            minAbove: ['desktop', 'leftCol', 'wide'].includes(getBreakpoint())
-                ? 200
-                : 0,
-            minBelow: 0,
-        }
-    },
-});
-
-// Tag-targeted rich links can be absolute
-const testIfDuplicate = (richLinkHref) =>
-    richLinkHref.includes(config.get('page.richLink'));
-
-const insertTagRichLink = () => {
-    let insertedEl;
-
-    const richLinkHrefs = Array.from(
-        document.querySelectorAll('.element-rich-link a')
-    ).map((el) => (el).href);
-
-    const isDuplicate = richLinkHrefs.some(testIfDuplicate);
-    const isSensitive =
-        config.get('page.shouldHideAdverts') ||
-        !config.get('page.showRelatedContent');
-    // Richlinks are just generally unhappy in numbered list articles
-    // Example https://theguardian.com/environment/2020/jun/19/why-you-should-go-animal-free-arguments-in-favour-of-meat-eating-debunked-plant-based
-    const isNumberedListArticle = document.querySelectorAll('.js-article__body--type-numbered-list').length > 0;
-
-    if (
-        config.get('page.richLink') &&
-        !config.get('page.richLink').includes(config.get('page.pageId')) &&
-        !isSensitive &&
-        !isDuplicate &&
-        !isNumberedListArticle
-    ) {
-        return spaceFiller
-            .fillSpace(getSpacefinderRules(), (paras) => {
-                const html = richLinkTag({
-                    href: config.get('page.richLink'),
-                });
-                return fastdom.mutate(() => {
-                    paras[0].insertAdjacentHTML('beforebegin', html);
-                    insertedEl = paras[0].previousElementSibling;
-                });
-            })
-            .then(didInsert => {
-                if (didInsert && insertedEl) {
-                    return upgradeRichLink(insertedEl);
-                }
-            });
-    }
-
-    return Promise.resolve();
-};
-
 const upgradeRichLinks = () => {
     Array.from(
         document.getElementsByClassName('element-rich-link--not-upgraded')
@@ -181,6 +102,4 @@ const upgradeRichLinks = () => {
 export {
     richLinkTag,
     upgradeRichLinks,
-    insertTagRichLink,
-    getSpacefinderRules,
 };
