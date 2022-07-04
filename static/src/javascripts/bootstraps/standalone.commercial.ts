35a8a3a1b11c059be69795d36d300078801f93fc
Robert Kenny <kenoir@users.noreply.github.com>
2022-07-04 10:48:44 +0100

    Update static/src/javascripts/bootstraps/standalone.commercial.ts
    Co-authored-by: Max Duval <max.duval@theguardian.com>

--- static/src/javascripts/bootstraps/standalone.commercial.ts
+++ static/src/javascripts/bootstraps/standalone.commercial.ts
@@ -3,6 +3,8 @@
  * meaning it is not part of another webpack build process, and
  * can be imported as a JS <script>.
  *
+ * See PR https://github.com/guardian/frontend/pull/24058
+ *
  * The standalone commercial bundle is bundled from source files
  * here in Frontend, but is served from https://assets.guim.co.uk
  * in production DCR and Frontend.
