838afa0c720ada834301d6926ae286528925c55e
Anna Beddow <anna.beddow@gmail.com>
2022-07-04 08:48:47 +0100

    Update common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
    Co-authored-by: Ashleigh Carr <ashcorr20@gmail.com>

--- common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
+++ common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
@@ -426,8 +426,7 @@ object DotcomRenderingDataModel {
 
     val matchData = makeMatchData(page)
 
-    val selectedTopics = topicResult.map(x => Seq(Topic(x.`type`, x.name)))
-
+    val selectedTopics = topicResult.map(Seq(Topic(_.`type`, _.name)))
     DotcomRenderingDataModel(
       author = author,
       badge = Badges.badgeFor(content).map(badge => DCRBadge(badge.seriesTag, badge.imageUrl)),
