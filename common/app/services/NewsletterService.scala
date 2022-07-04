b076da89ef2289c580a3b1f493c4f17d9519c429
David Blatcher <dv.bltchr@gmail.com>
2022-07-04 15:38:29 +0100

    Newsletter service will determine newsletter from article tags

--- common/app/services/NewsletterService.scala
+++ common/app/services/NewsletterService.scala
@@ -2,16 +2,47 @@ package services
 import services.newsletters.NewsletterSignupAgent
 import services.newsletters.model.NewsletterResponse
 import common._
-import model.{ArticlePage, PageWithStoryPackage, LiveBlogPage}
+import model.{ArticlePage, PageWithStoryPackage, LiveBlogPage, Tag}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class NewsletterService(newsletterSignupAgent: NewsletterSignupAgent) {
+  private val EMBED_TAG_PREFIX = "newsletter-embed"
 
-  def getNewsletterForArticle(article: ArticlePage): Option[NewsletterResponse] = {
-    None
+  private def getNewsletterTag(tags: List[Tag]) = {
+    tags.find(t => t.properties.id.contains(EMBED_TAG_PREFIX))
   }
-  def getNewsletterForLiveBlog(blog: LiveBlogPage): Option[NewsletterResponse] = {
-    None
+
+  private def getNewsletterName(tag: Tag) = {
+    tag.properties.id.stripPrefix(EMBED_TAG_PREFIX.+("-"))
+  }
+
+  def getNewsletterForArticle(articlePage: ArticlePage): Option[NewsletterResponse] = {
+
+    for {
+      tag <- getNewsletterTag((articlePage.article.tags.tags))
+      newsletterName = getNewsletterName(tag)
+      newsletterEither = newsletterSignupAgent.getNewsletterByName(newsletterName)
+      newsletter <- newsletterEither match {
+        case Left(value)  => None
+        case Right(value) => value
+      }
+    } yield {
+      newsletter
+    }
+
+  }
+  def getNewsletterForLiveBlog(blogPage: LiveBlogPage): Option[NewsletterResponse] = {
+    for {
+      tag <- getNewsletterTag((blogPage.article.tags.tags))
+      newsletterName = getNewsletterName(tag)
+      newsletterEither = newsletterSignupAgent.getNewsletterByName(newsletterName)
+      newsletter <- newsletterEither match {
+        case Left(value)  => None
+        case Right(value) => value
+      }
+    } yield {
+      newsletter
+    }
   }
 }
