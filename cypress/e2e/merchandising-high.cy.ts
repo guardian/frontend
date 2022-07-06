aee4136b5e2aea58841f3dbb0e6ee76d5cd583cb
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-07-06 17:48:16 +0100

    split merchandising and merchandising-high test files
    - test breakpoints
    - add duration to calls to scrollIntoView to decrease chance that layout shift will cause test to fail

--- cypress/e2e/merchandising-high.cy.ts
+++ cypress/e2e/merchandising-high.cy.ts
@@ -1,7 +1,7 @@
 import { breakpoints } from '../fixtures/breakpoints';
 import { articles, liveblogs } from '../fixtures/pages';
 
-describe('merchandising slot on pages', () => {
+describe('merchandising-high slot on pages', () => {
 	[...articles, ...liveblogs].forEach(({ path, adTest }) => {
 		Object.entries(breakpoints).forEach(([breakpoint, width]) => {
 			it(`Test ${path} has correct slot and iframe at breakpoint ${breakpoint}`, () => {
@@ -12,14 +12,16 @@ describe('merchandising slot on pages', () => {
 				// Click "Yes, I'm happy" on the sourcepoint banner to obtain consent
 				cy.getIframeBody('sp_message_iframe_').find('.btn-primary').click();
 
-				// Check that the merchandising ad slot is on the page
-				cy.get('#dfp-ad--merchandising').should('exist');
+				// Check that the merchandising-high ad slot is on the page
+				cy.get('#dfp-ad--merchandising-high').should('exist');
 
 				// creative isn't loaded unless slot is in view
-				cy.get('#dfp-ad--merchandising').scrollIntoView({duration: 4000});
+				cy.get('#dfp-ad--merchandising-high').scrollIntoView({duration: 4000});
 
-				// Check that an iframe is placed inside the merchandising ad slot
-				cy.get('#dfp-ad--merchandising').find('iframe', {timeout: 10000}).should('exist');
+				// Check that an iframe is placed inside the merchandising-high ad slot
+				cy.get('#dfp-ad--merchandising-high')
+					.find('iframe')
+					.should('exist');
 			});
 		});
 	});
