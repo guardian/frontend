61bdd42d9f87a0e76f54270937a4f405da723f05
Emma Imber <emma-jo.imber@guardian.co.uk>
2022-07-07 12:02:38 +0100

    Update geo-utils to typescript

---
+++ static/src/javascripts/projects/common/modules/commercial/geo-utils.ts
@@ -0,0 +1,31 @@
+import { getCountryCode } from '../../../../lib/geolocation';
+
+// cache the users location so we only have to look it up once
+let geo: string | undefined;
+const currentGeoLocation = (): string => {
+	geo = geo ?? getCountryCode();
+	return geo;
+};
+
+export const isInUk = (): boolean => currentGeoLocation() === 'GB';
+
+export const isInUsa = (): boolean => currentGeoLocation() === 'US';
+
+export const isInCanada = (): boolean => currentGeoLocation() === 'CA';
+
+export const isInAustralia = (): boolean => currentGeoLocation() === 'AU';
+
+export const isInNewZealand = (): boolean => currentGeoLocation() === 'NZ';
+
+export const isInUsOrCa = (): boolean => isInUsa() || isInCanada();
+
+export const isInAuOrNz = (): boolean => isInAustralia() || isInNewZealand();
+
+export const isInRow = (): boolean =>
+	!isInUk() && !isInUsOrCa() && !isInAuOrNz();
+
+export const _ = {
+	resetModule: (): void => {
+		geo = undefined;
+	},
+};
