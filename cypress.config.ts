38be33513c674dd1b8738b587e54c7a20cc6892b
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-07-07 15:40:16 +0100

    set cypress config numTestsKeptInMemory: 0

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,4 +13,7 @@ export default defineConfig({
       return require('./cypress/plugins/index.ts').default(on, config)
     },
   },
+  // Test files like merchandising.cy.ts that take a few minutes to run exceed the memory limit
+  // causing the browser to crash. https://github.com/cypress-io/cypress/issues/1906
+  "numTestsKeptInMemory": 0
 })
