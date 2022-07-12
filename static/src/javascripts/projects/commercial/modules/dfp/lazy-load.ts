6fe2e30713c524f61c3dbeb1117010e9a4e11d00
Simon Byford <simon.byford@guardian.co.uk>
2022-07-12 16:26:55 +0100

    Remove `CommercialEndOfQuarter2Test`
    This end-of-quarter test needs to be adapted to use the server-side testing framework. This is because we wish to measure the impact of a particular server-side change (see: https://github.com/guardian/dotcom-rendering/pull/4942). The first step is removing the original client-side test.

--- static/src/javascripts/projects/commercial/modules/dfp/lazy-load.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/lazy-load.ts
@@ -1,19 +1,12 @@
 import { log } from '@guardian/libs';
 import { once } from 'lodash-es';
-import { isInVariantSynchronous } from 'common/modules/experiments/ab';
-import { commercialEndOfQuarter2Test } from 'common/modules/experiments/tests/commercial-end-of-quarter-2-test';
 import type { Advert } from './Advert';
 import { dfpEnv } from './dfp-env';
 import { getAdvertById } from './get-advert-by-id';
 import { loadAdvert, refreshAdvert } from './load-advert';
 
 const decideLazyLoadMargin = () => {
-	const enableNewLazyLoadMargin = !isInVariantSynchronous(
-		commercialEndOfQuarter2Test,
-		'control',
-	);
-
-	const lazyLoadMargin = enableNewLazyLoadMargin ? '20%' : '200px';
+	const lazyLoadMargin = '20%';
 
 	log('commercial', `Using lazy load margin of ${lazyLoadMargin}`);
 
