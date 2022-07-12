6418e17261f477eb5397f82a367fddff9ee96faa
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-07-12 13:11:49 +0100

    make prebid test more genric and reusable

--- cypress/e2e/prebid.cy.ts
+++ cypress/e2e/prebid.cy.ts
@@ -1,58 +1,55 @@
 import { pages } from '../fixtures/pages';
+import { bidderURLs, wins } from '../fixtures/prebid';
 
-const prebidURLs = [
-	'https://ib.adnxs.com/ut/v3/prebid',
-	'https://htlb.casalemedia.com/cygnus**',
-	'https://hbopenbid.pubmatic.com/translator?source=prebid-client',
-	'https://prg.smartadserver.com/prebid/v1',
-	'https://rtb.openx.net/sync/prebid**',
-	'https://elb.the-ozone-project.com/openrtb2/auction',
-	'https://bidder.criteo.com/cdb**',
-	'https://pixel.adsafeprotected.com/services/pub**',
-	'https://ad.360yield.com/pb',
-];
+const interseptGamRequest = () =>
+	cy.intercept(
+		{
+			url: 'https://securepubads.g.doubleclick.net/gampad/ads?**',
+		},
+		function (req) {
+			const url = new URL(req.url);
 
+			const targetingParams = decodeURIComponent(
+				url.searchParams.get('prev_scp') || '',
+			);
+			const targeting = new URLSearchParams(targetingParams);
+			if (targeting.get('hb_bidder') === 'criteo') {
+				Object.entries(wins.criteo.targeting).forEach(
+					([key, value]) => {
+						expect(targeting.get(key)).to.equal(value);
+					},
+				);
+			}
+		},
+	);
 
 describe('targeting', () => {
 	before(() => {
-		prebidURLs.forEach(url => {
+		bidderURLs.forEach((url) => {
 			cy.intercept(url, (req) => {
-				if(req.url.includes('https://bidder.criteo.com/cdb')) {
-					const body = JSON.parse(req.body);
-					body.slots[0].impid = 'banner-ad-div';
-					body.slots[0].sizes = ['300x250', '728x90'];
-					body.publisher.networkid = 497747;
-
-					req.body = JSON.stringify(body);
-
-					req.continue();
+				if (req.url.includes(wins.criteo.url)) {
+					req.reply({ body: wins.criteo.response });
 				} else {
-
-				req.reply({
-					delay: 2500,
-					body: ''
-				  })
+					req.reply({
+						statusCode: 204,
+					});
 				}
 			});
 		});
 	});
 
-	it(`intercepts prebid requests`, () => {
-		const { path, adTest } = pages[0];
+	it(`prebid winner should display ad and send targeting to GAM`, () => {
+		const { path } = pages[0];
 
-		// cy.visit(`${path}?adtest=${adTest}&pbjs_debug=true`);
-		cy.visit(`${path}?pbjs_debug=true`);
+		interseptGamRequest();
 
-		cy.window().then((win) => {
-			win.guardian.logger.subscribeTo('commercial');
-		});
+		cy.visit(`${path}?adrefresh=false`);
 
 		// Click "Yes, I'm happy" on the sourcepoint banner to obtain consent
 		cy.getIframeBody('sp_message_iframe_').find('.btn-primary').click();
 
-		// Check that the top-above-nav ad slot is on the page
-		cy.get('#dfp-ad--top-above-nav').should('exist');
-
-		cy.wait(30000)
+		cy.getIframeBody('google_ads_iframe_')
+			.find('[data-cy="test-creative"]')
+			.should('exist');
 	});
 });
