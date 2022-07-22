6e9e5c42ea28357fd604d7d1a84cb3c193c1ce4c
Alina Boghiu <alina_boghiu@31224.gnm.int>
2022-07-22 13:31:15 +0100

    formatting

--- common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
+++ common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
@@ -10,7 +10,19 @@ import conf.Configuration
 import experiments.ActiveExperiments
 import model.dotcomrendering.DotcomRenderingUtils._
 import model.dotcomrendering.pageElements.{PageElement, TextCleaner}
-import model.{ArticleDateTimes, Badges, CanonicalLiveBlog, ContentFormat, ContentPage, GUDateTimeFormatNew, InteractivePage, LiveBlogPage, PageWithStoryPackage, Topic, TopicResult}
+import model.{
+  ArticleDateTimes,
+  Badges,
+  CanonicalLiveBlog,
+  ContentFormat,
+  ContentPage,
+  GUDateTimeFormatNew,
+  InteractivePage,
+  LiveBlogPage,
+  PageWithStoryPackage,
+  Topic,
+  TopicResult,
+}
 import navigation._
 import play.api.libs.json._
 import play.api.mvc.RequestHeader
