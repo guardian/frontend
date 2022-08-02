5831fd1e78d60ef619741ef90e7065216ad99633
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-08-02 13:01:32 +0100

    use async await in commercial.ts

--- static/src/javascripts/bootstraps/commercial.ts
+++ static/src/javascripts/bootstraps/commercial.ts
@@ -3,17 +3,17 @@ import { init as initCommon } from './commercial.common';
 import { init as initConsented } from './commercial.consented';
 import { init as initConsentless } from './commercial.consentless';
 
-const init = (): Promise<void> =>
-	initCommon().then(() =>
-		onConsent().then((consentState) => {
-			if (consentState.canTarget) {
-				initConsented();
-			} else {
-				// init opt-out-advertising
-				console.log('NO CONSENT!!');
-				void initConsentless();
-			}
-		}),
-	);
+const init = async (): Promise<void> => {
+	await initCommon();
+	const consentState = await onConsent();
+
+	if (consentState.canTarget) {
+		initConsented();
+	} else {
+		// init opt-out-advertising
+		console.log('NO CONSENT!!');
+		void initConsentless();
+	}
+};
 
 void init();
