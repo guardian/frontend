609a3d2c450669da0a832e99417d7e750f71381f
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-08-03 12:03:38 +0100

    Fix Typescript linting errors

--- static/src/javascripts/projects/common/modules/identity/cookierefresh.ts
+++ static/src/javascripts/projects/common/modules/identity/cookierefresh.ts
@@ -1,30 +1,43 @@
+import { storage } from '@guardian/libs';
 import {
-	isUserLoggedIn,
 	getUserFromApiWithRefreshedCookie,
+	isUserLoggedIn,
 	refreshOktaSession,
 } from 'common/modules/identity/api';
-import { storage } from '@guardian/libs';
 
 const days30InMillis: number = 1000 * 60 * 60 * 24 * 30;
 
 const shouldRefreshCookie: (
-	lastRefresh: any,
+	lastRefresh: string | null,
 	currentTime: number,
-) => boolean = (lastRefresh: any, currentTime: number) => {
+) => boolean = (lastRefresh: string | null, currentTime: number) => {
 	// Cookies auto-refresh after 30 days
 	return (
 		!lastRefresh || currentTime - parseInt(lastRefresh, 10) > days30InMillis
 	);
 };
 
+const safelyGet = (value: unknown) => {
+	switch (typeof value) {
+		case 'string':
+			return value;
+		default:
+			return null;
+	}
+};
+
 const init: () => void = () => {
 	const lastRefreshKey = 'identity.lastRefresh';
 
 	if (storage.local.isAvailable() && isUserLoggedIn()) {
-		const currentTime = new Date().getTime();
-		const lastRefresh = storage.local.get(lastRefreshKey);
+		const currentTime: number = new Date().getTime();
+		const lastRefresh: string | null = safelyGet(
+			storage.local.get(lastRefreshKey),
+		);
 		if (shouldRefreshCookie(lastRefresh, currentTime)) {
-			getUserFromApiWithRefreshedCookie();
+			getUserFromApiWithRefreshedCookie().catch(() => {
+				/* do nothing */
+			});
 			storage.local.set(lastRefreshKey, currentTime);
 			refreshOktaSession(encodeURIComponent(document.location.href));
 		}
