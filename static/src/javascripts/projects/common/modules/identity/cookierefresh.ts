95bc1c17aa5573432624b59c316be6fd73b7a5ff
Raphael Kabo <raphael.kabo@theguardian.com>
2022-08-08 17:38:16 +0100

    fix: bugfix types for localStorage fetch

--- static/src/javascripts/projects/common/modules/identity/cookierefresh.ts
+++ static/src/javascripts/projects/common/modules/identity/cookierefresh.ts
@@ -8,36 +8,33 @@ import {
 const days30InMillis: number = 1000 * 60 * 60 * 24 * 30;
 
 const shouldRefreshCookie: (
-	lastRefresh: string | null,
+	lastRefresh: unknown | null,
 	currentTime: number,
-) => boolean = (lastRefresh: string | null, currentTime: number) => {
-	// Cookies auto-refresh after 30 days
+) => boolean = (lastRefresh: unknown | null, currentTime: number) => {
+	// The cookie should be refreshed in two cases:
+	// 1) There is no last refresh value, or it's falsy
+	// 2) The last refresh value is older than 30 days.
+	// We cast the lastRefresh value to a string, then parse it as an integer
+	// because we don't know what type it might be.
 	return (
-		!lastRefresh || currentTime - parseInt(lastRefresh, 10) > days30InMillis
+		!lastRefresh ||
+		currentTime - parseInt(String(lastRefresh), 10) > days30InMillis
 	);
 };
 
-const safelyGet = (value: unknown) => {
-	switch (typeof value) {
-		case 'string':
-			return value;
-		default:
-			return null;
-	}
-};
-
-const init: () => void = () => {
+const init: () => void = async () => {
 	const lastRefreshKey = 'identity.lastRefresh';
 
 	if (storage.local.isAvailable() && isUserLoggedIn()) {
 		const currentTime: number = new Date().getTime();
-		const lastRefresh: string | null = safelyGet(
-			storage.local.get(lastRefreshKey),
-		);
+		// The storage API could return any type handled by JSON.parse, so
+		// we will assume the type is 'any' and always parse the value into
+		// an integer.
+		// TODO: Rewrite this to make use of the storage API's built-in
+		// expiry mechanism: https://github.com/guardian/frontend/pull/25306#discussion_r937843037
+		const lastRefresh: unknown | null = storage.local.get(lastRefreshKey);
 		if (shouldRefreshCookie(lastRefresh, currentTime)) {
-			getUserFromApiWithRefreshedCookie().catch(() => {
-				/* do nothing */
-			});
+			await getUserFromApiWithRefreshedCookie();
 			storage.local.set(lastRefreshKey, currentTime);
 			refreshOktaSession(encodeURIComponent(document.location.href));
 		}
