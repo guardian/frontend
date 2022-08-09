60313b7a3b524f1b34e60554f13b6c8ed9151787
Raphael Kabo <raphael.kabo@theguardian.com>
2022-08-09 15:14:59 +0100

    feat: update tests for new shouldRefreshCookie logic

--- static/src/javascripts/projects/common/modules/identity/cookierefresh.spec.ts
+++ static/src/javascripts/projects/common/modules/identity/cookierefresh.spec.ts
@@ -8,12 +8,19 @@ test('should return true for a user who has never refreshed cookies', () => {
 	expect(shouldRefreshCookie(null, now)).toBe(true);
 });
 
-test('should return false for a user who has not refreshed within 30 days', () => {
-	const daysAgo31 = (new Date().getTime() - 1000 * 86400 * 31).toString();
+test('should return true for 0 or a value which cannot be parsed as a number', () => {
+	expect(shouldRefreshCookie('notANumber', now)).toBe(true);
+	expect(shouldRefreshCookie(undefined, now)).toBe(true);
+	expect(shouldRefreshCookie(false, now)).toBe(true);
+	expect(shouldRefreshCookie(0, now)).toBe(true);
+});
+
+test('should return true for a user who has not refreshed within 30 days', () => {
+	const daysAgo31 = new Date().getTime() - 1000 * 86400 * 31;
 	expect(shouldRefreshCookie(daysAgo31, now)).toBe(true);
 });
 
 test('should return false for a user who has refreshed within 30 days', () => {
-	const daysAgo5 = (new Date().getTime() - 1000 * 86400 * 5).toString();
+	const daysAgo5 = new Date().getTime() - 1000 * 86400 * 5;
 	expect(shouldRefreshCookie(daysAgo5, now)).toBe(false);
 });
