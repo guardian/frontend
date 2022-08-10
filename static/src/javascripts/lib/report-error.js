94fc781e605e6167333109461339241ef5b968c8
Dominik Lander <lander45@hotmail.co.uk>
2022-08-10 18:29:37 +0100

    Add sample rate parameter to report-error

--- static/src/javascripts/lib/report-error.js
+++ static/src/javascripts/lib/report-error.js
@@ -1,14 +1,18 @@
 /*
-   Report errors to Sentry with optional tags metadata
-   We optionally re-throw the error to halt execution and to ensure the error is
-   still logged to the console via browsers built-in logging for uncaught
-   exceptions. This is optional because sometimes we log errors for tracking
-   user data.
+        Report errors to Sentry with optional tags metadata.
+        We optionally re-throw the error to halt execution and to ensure the error
+    is still logged to the console via browsers built-in logging for uncaught
+    exceptions. This is optional because sometimes we log errors for tracking
+    user data.
+        A sample rate is for when an error is anticipated to be reported highly frequently
+    to the point where we may encroach on our rate limits. A rate of 0.1 will report 1 in every
+    10 errors. A rate of 0.0001 will report 1 in every 10,000 errors.
 */
-
 import raven from './raven';
 
-const reportError = (err, tags, shouldThrow = true) => {
+const reportError = (err, tags, shouldThrow = true, sampleRate = 1) => {
+	if (!isInSample(sampleRate)) return;
+
 	raven.captureException(err, { tags });
 	if (shouldThrow) {
 		// Flag to ensure it is not reported to Sentry again via global handlers
@@ -18,4 +22,7 @@ const reportError = (err, tags, shouldThrow = true) => {
 	}
 };
 
+const isInSample = (sampleRate) => Math.random() <= sampleRate;
+
 export default reportError;
+export { isInSample };
--- static/src/javascripts/lib/report-error.spec.js
+++ static/src/javascripts/lib/report-error.spec.js
@@ -1,41 +1,80 @@
-import reportError from './report-error';
+import reportError, { isInSample } from './report-error';
 
 jest.mock('raven-js', () => ({
-    config() {
-        return this;
-    },
-    install() {
-        return this;
-    },
-    captureException: jest.fn(),
+	config() {
+		return this;
+	},
+	install() {
+		return this;
+	},
+	captureException: jest.fn(),
 }));
 
 const fakeRaven = require('raven-js');
 
 describe('report-error', () => {
-    const error = new Error('Something broke.');
-    const metaData = { test: true };
-    const ravenMetaData = { tags: metaData };
+	const error = new Error('Something broke.');
+	const metaData = { test: true };
+	const ravenMetaData = { tags: metaData };
 
-    test('Does not throw an error', () => {
-        expect(() => {
-            reportError(error, metaData, false);
-        }).not.toThrowError(error);
+	test('Does not throw an error', () => {
+		expect(() => {
+			reportError(error, metaData, false);
+		}).not.toThrowError(error);
 
-        expect(fakeRaven.captureException).toHaveBeenCalledWith(
-            error,
-            ravenMetaData
-        );
-    });
+		expect(fakeRaven.captureException).toHaveBeenCalledWith(
+			error,
+			ravenMetaData,
+		);
+	});
 
-    test('Throws an error', () => {
-        expect(() => {
-            reportError(error, metaData);
-        }).toThrowError(error);
+	test('Throws an error', () => {
+		expect(() => {
+			reportError(error, metaData);
+		}).toThrowError(error);
 
-        expect(fakeRaven.captureException).toHaveBeenCalledWith(
-            error,
-            ravenMetaData
-        );
-    });
+		expect(fakeRaven.captureException).toHaveBeenCalledWith(
+			error,
+			ravenMetaData,
+		);
+	});
+
+	test('Throws an error if sample rate is GREATER than random number', () => {
+		jest.spyOn(global.Math, 'random').mockReturnValue(0.2);
+		expect(() => {
+			reportError(error, metaData, true, 0.5);
+		}).toThrowError(error);
+	});
+
+	test('Does not throw an error if sample rate is LESS than random number', () => {
+		jest.spyOn(global.Math, 'random').mockReturnValue(0.8);
+		expect(() => {
+			reportError(error, metaData, true, 0.5);
+		}).not.toThrowError(error);
+	});
+
+	describe('isInSample', () => {
+		it.each([0, 0.5, 0.99])(
+			'returns true if sample rate is 1',
+			(randomNumber) => {
+				jest.spyOn(global.Math, 'random').mockReturnValue(randomNumber);
+
+				expect(isInSample(1)).toBe(true);
+			},
+		);
+
+		test('returns TRUE if sample rate is GREATER than random number', () => {
+			const sampleRate = 0.1;
+			jest.spyOn(global.Math, 'random').mockReturnValue(0.08);
+
+			expect(isInSample(sampleRate)).toBe(true);
+		});
+
+		test('returns FALSE if sample rate is LESS than random number', () => {
+			const sampleRate = 0.1;
+			jest.spyOn(global.Math, 'random').mockReturnValue(0.18);
+
+			expect(isInSample(sampleRate)).toBe(false);
+		});
+	});
 });
