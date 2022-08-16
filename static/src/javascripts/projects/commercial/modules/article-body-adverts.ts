5ca18f77745118d6620ec263985b79c97c2c7c3c
Simon Byford <simon.byford@guardian.co.uk>
2022-08-16 16:21:37 +0100

    Rename `includeContainer` to `isSticky`
    The old name was misleading because all ads now include a container

--- static/src/javascripts/projects/commercial/modules/article-body-adverts.ts
+++ static/src/javascripts/projects/commercial/modules/article-body-adverts.ts
@@ -218,11 +218,11 @@ const addDesktopInlineAds = (isInline1: boolean): Promise<boolean> => {
 		(sfdebug === '1' && isInline1) || (sfdebug === '2' && !isInline1);
 
 	const insertAds: SpacefinderWriter = async (paras) => {
-		// Include containers on the non-inline1 pass
+		// Make ads sticky on the non-inline1 pass
 		// i.e. inline2, inline3, etc...
-		const includeContainer = !isInline1;
+		const isSticky = !isInline1;
 
-		if (includeContainer) {
+		if (isSticky) {
 			const stickyContainerHeights = await computeStickyHeights(
 				paras,
 				articleBodySelector,
@@ -248,7 +248,7 @@ const addDesktopInlineAds = (isInline1: boolean): Promise<boolean> => {
 
 				let containerClasses = '';
 
-				if (includeContainer) {
+				if (isSticky) {
 					containerClasses += getStickyContainerClassname(i);
 				}
 
@@ -258,7 +258,7 @@ const addDesktopInlineAds = (isInline1: boolean): Promise<boolean> => {
 				}
 
 				const containerOptions = {
-					sticky: includeContainer,
+					sticky: isSticky,
 					className: containerClasses,
 					enableDebug,
 				};
