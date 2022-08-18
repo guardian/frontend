b50dea24582048f7c79f4cc182a934ffd04f7f68
Tom Wey <tom.wey@theguardian.com>
2022-08-18 09:59:23 +0100

    Fix linting error

--- static/src/javascripts/projects/common/modules/experiments/utils.ts
+++ static/src/javascripts/projects/common/modules/experiments/utils.ts
@@ -3,5 +3,6 @@ import { bypassCoreWebVitalsSampling } from '@guardian/libs';
 
 export const bypassMetricsSampling = (): void => {
 	void bypassCommercialMetricsSampling();
+	/* eslint-disable-next-line @typescript-eslint/no-floating-promises -- this is an async method */
 	bypassCoreWebVitalsSampling();
 };
