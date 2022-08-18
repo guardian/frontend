75493325f3c41085e16f18b924fc8633088404ae
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-08-18 10:21:58 +0100

    Unset adfree cookie for users in constentless ads test (#25404)
    * Unset the ad free cookie in bootConsentless
    
    passing reason: consent opt out
    
    * Add comment

--- static/src/javascripts/bootstraps/standalone.commercial.ts
+++ static/src/javascripts/bootstraps/standalone.commercial.ts
@@ -19,6 +19,10 @@ import { initFixedSlots } from 'commercial/modules/consentless/init-fixed-slots'
 import { initConsentless } from 'commercial/modules/consentless/prepare-ootag';
 import { isInVariantSynchronous } from 'common/modules/experiments/ab';
 import { consentlessAds } from 'common/modules/experiments/tests/consentlessAds';
+import {
+	AdFreeCookieReasons,
+	maybeUnsetAdFreeCookie,
+} from 'lib/manage-ad-free-cookie';
 import reportError from '../lib/report-error';
 import { catchErrorsWithContext } from '../lib/robust';
 import { initAdblockAsk } from '../projects/commercial/adblock-ask';
@@ -205,6 +209,14 @@ const bootCommercial = async (): Promise<void> => {
 };
 
 const bootConsentless = async (): Promise<void> => {
+	/*  In the consented ad stack, we set the ad free cookie for users who
+		don't consent to targeted ads in order to hide empty ads slots.
+		We remove the cookie here so that we can show Opt Out ads.
+		TODO: Stop setting ad free cookie for users who opt out when
+		consentless ads are rolled out to all users.
+ 	*/
+	maybeUnsetAdFreeCookie(AdFreeCookieReasons.ConsentOptOut);
+
 	await Promise.all([
 		initConsentless(),
 		initFixedSlots(),
