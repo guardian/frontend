0ae6b9a07db60c8699962892ab5404522f9124d2
Ioanna Kokkini <ioannakok@hotmail.com>
2022-08-26 15:04:44 +0100

    Move FrontJsonFapi.scala from facia to common
    We want to be able to also use it in `article/app/agents/CuratedContentAgent.scala`
    
    Co-authored-by: Ashleigh Carr <ashcorr20@gmail.com>
    Co-authored-by: Pete Faulconbridge <pete.faulconbridge@guardian.co.uk>
    Co-authored-by: Pete Faulconbridge <peter.faulconbridge@gmail.com>

--- facia/app/controllers/FaciaControllers.scala
+++ facia/app/controllers/FaciaControllers.scala
@@ -1,10 +1,10 @@
 package controllers
 
 import com.softwaremill.macwire._
-import controllers.front.FrontJsonFapiLive
 import model.ApplicationContext
 import play.api.libs.ws.WSClient
 import play.api.mvc.ControllerComponents
+import services.fronts.FrontJsonFapiLive
 
 trait FaciaControllers {
   def frontJsonFapiLive: FrontJsonFapiLive
