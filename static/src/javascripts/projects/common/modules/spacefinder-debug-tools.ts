67f149223e62eb7b6476a89ac90af7dcac7f3d78
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-08-31 15:45:58 +0100

    convert to TS

--- static/src/javascripts/projects/common/modules/spacefinder-debug-tools.ts
+++ static/src/javascripts/projects/common/modules/spacefinder-debug-tools.ts
@@ -1,45 +1,61 @@
-const markCandidates = (exclusions, winners, options, rules) => {
-	if (!options?.debug) return winners;
+import type {
+	SpacefinderExclusions,
+	SpacefinderItem,
+	SpacefinderOptions,
+	SpacefinderRules,
+} from './spacefinder';
 
-    try {
-        const colours = {
-            red: 'rgb(255 178 178)',
-            orange: 'rgb(255 213 178)',
-            yellow: 'rgb(254 255 178)',
-            blue: 'rgb(178 248 255)',
-            purple: 'rgb(178 178 255)',
-            pink: 'rgb(255 178 242)',
-            green: 'rgb(178 255 184)',
-        };
+const markCandidates = (
+	exclusions: SpacefinderExclusions,
+	winners: SpacefinderItem[],
+	options: SpacefinderOptions,
+	rules: SpacefinderRules,
+): SpacefinderItem[] => {
+	if (!options.debug) return winners;
 
-        const exclusionTypes = {
-            absoluteMinAbove: {
-                colour: colours.red,
-                reason: 'Too close to the top of page',
-            },
-            aboveAndBelow: {
-                colour: colours.orange,
-                reason: 'Too close to top or bottom of article',
-            },
-            isStartAt: {
-                colour: colours.purple,
-                reason: 'Spacefinder is starting from this position',
-            },
-            startAt: {
-                colour: colours.orange,
-                reason: 'Before the starting element',
-            },
-            custom: {
-                colour: colours.yellow,
-                reason: 'Too close to other winner',
-            },
-        };
+	try {
+		const colours = {
+			red: 'rgb(255 178 178)',
+			orange: 'rgb(255 213 178)',
+			yellow: 'rgb(254 255 178)',
+			blue: 'rgb(178 248 255)',
+			purple: 'rgb(178 178 255)',
+			pink: 'rgb(255 178 242)',
+			green: 'rgb(178 255 184)',
+		};
 
-        const addDistanceExplainer = (element, text) => {
-            const explainer = document.createElement('div');
-            explainer.className = 'distanceExplainer sfdebug';
-            explainer.appendChild(document.createTextNode(text));
-            explainer.style.cssText = `
+		const exclusionTypes = {
+			absoluteMinAbove: {
+				colour: colours.red,
+				reason: 'Too close to the top of page',
+			},
+			aboveAndBelow: {
+				colour: colours.orange,
+				reason: 'Too close to top or bottom of article',
+			},
+			isStartAt: {
+				colour: colours.purple,
+				reason: 'Spacefinder is starting from this position',
+			},
+			startAt: {
+				colour: colours.orange,
+				reason: 'Before the starting element',
+			},
+			custom: {
+				colour: colours.yellow,
+				reason: 'Too close to other winner',
+			},
+		} as const;
+
+		const isExclusionType = (
+			type: string,
+		): type is keyof typeof exclusionTypes => type in exclusionTypes;
+
+		const addDistanceExplainer = (element: HTMLElement, text: string) => {
+			const explainer = document.createElement('div');
+			explainer.className = 'distanceExplainer sfdebug';
+			explainer.appendChild(document.createTextNode(text));
+			explainer.style.cssText = `
                 position:absolute;
                 right:0;
                 background-color:${colours.red};
@@ -47,38 +63,41 @@ const markCandidates = (exclusions, winners, options, rules) => {
                 font-family: sans-serif;
                 z-index:20;
             `;
-            element.before(explainer);
-        };
+			element.before(explainer);
+		};
 
-        const addHoverListener = (candidate, tooClose) => {
-            tooClose.forEach((opponent) => {
-                candidate.addEventListener('mouseenter', () => {
-                    opponent.element.style.cssText = `
+		const addHoverListener = (
+			candidate: HTMLElement,
+			tooClose: Exclude<SpacefinderItem['meta'], undefined>['tooClose'],
+		) => {
+			tooClose.forEach((opponent) => {
+				candidate.addEventListener('mouseenter', () => {
+					opponent.element.style.cssText = `
                         box-shadow: 0px 0px 0px 10px ${colours.red};
                         z-index:10;
                         position:relative
                     `;
 
-                    addDistanceExplainer(
-                        opponent.element,
-                        `${opponent.actual}px/${opponent.required}px`,
-                    );
-                });
+					addDistanceExplainer(
+						opponent.element,
+						`${opponent.actual}px/${opponent.required}px`,
+					);
+				});
 
-                candidate.addEventListener('mouseleave', () => {
-                    opponent.element.style.cssText = '';
-                    document
-                        .querySelectorAll('.distanceExplainer')
-                        .forEach((el) => el.remove());
-                });
-            });
-        };
+				candidate.addEventListener('mouseleave', () => {
+					opponent.element.style.cssText = '';
+					document
+						.querySelectorAll('.distanceExplainer')
+						.forEach((el) => el.remove());
+				});
+			});
+		};
 
-        const addExplainer = (element, text) => {
-            const explainer = document.createElement('div');
-            explainer.className = 'sfdebug';
-            explainer.appendChild(document.createTextNode(text));
-            explainer.style.cssText = `
+		const addExplainer = (element: HTMLElement, text: string) => {
+			const explainer = document.createElement('div');
+			explainer.className = 'sfdebug';
+			explainer.appendChild(document.createTextNode(text));
+			explainer.style.cssText = `
                 position:absolute;
                 right:0;
                 background-color:#fffffff7;
@@ -86,48 +105,53 @@ const markCandidates = (exclusions, winners, options, rules) => {
                 border-radius:0 0 0 10px;
                 font-family: sans-serif;
             `;
-            element.before(explainer);
-        };
+			element.before(explainer);
+		};
 
-        // Mark losing candidates
-        for (const [key, arr] of Object.entries(exclusions)) {
-            arr.forEach((exclusion) => {
-                const type = exclusionTypes[key];
+		// Mark losing candidates
+		for (const [key, arr] of Object.entries(exclusions)) {
+			arr.forEach((exclusion) => {
+				const type = isExclusionType(key) && exclusionTypes[key];
 
-                const element = exclusion instanceof Element ? exclusion : exclusion.element;
-                const meta = exclusion instanceof Element ? null : exclusion.meta;
+				const element =
+					exclusion instanceof Element
+						? exclusion
+						: exclusion.element;
+				const meta =
+					exclusion instanceof Element ? null : exclusion.meta;
 
-                if (element == rules?.startAt) {
-                    const type = exclusionTypes.isStartAt;
-                    addExplainer(element, type.reason);
-                    element.style.cssText += `background:${type.colour}`;
-                }else if (type) {
-                    addExplainer(element, type.reason);
-                    element.style.cssText += `background:${type.colour}`;
-                } else if (meta?.tooClose?.length > 0) {
-                    addExplainer(element, 'Too close to other element');
-                    element.style.cssText += `background:${colours.blue}`;
-                    addHoverListener(element, meta?.tooClose);
-                } else {
-                    addExplainer(element, `Unknown key: ${key}`);
-                    element.style.cssText += `background:${colours.pink}`;
-                }
-            });
-        }
+				if (element == rules.startAt) {
+					const type = exclusionTypes.isStartAt;
+					addExplainer(element, type.reason);
+					element.style.cssText += `background:${type.colour}`;
+				} else if (type) {
+					addExplainer(element, type.reason);
+					element.style.cssText += `background:${type.colour}`;
+				} else if (meta && meta.tooClose.length > 0) {
+					addExplainer(element, 'Too close to other element');
+					element.style.cssText += `background:${colours.blue}`;
+					addHoverListener(element, meta.tooClose);
+				} else {
+					addExplainer(element, `Unknown key: ${key}`);
+					element.style.cssText += `background:${colours.pink}`;
+				}
+			});
+		}
 
-        // Mark winning candidates
-        winners.forEach((winner) => {
-            addExplainer(winner.element, 'Winner');
-            winner.element.style.cssText += `background:${colours.green};`;
-        });
+		// Mark winning candidates
+		winners.forEach((winner) => {
+			addExplainer(winner.element, 'Winner');
+			winner.element.style.cssText += `background:${colours.green};`;
+		});
 
-        return winners;
-    } catch (e) {
-        console.error('SFDebug Error', e);
-    }
+		return winners;
+	} catch (e) {
+		console.error('SFDebug Error', e);
+		return winners;
+	}
 };
 
-const debugMinAbove = (body, minAbove) => {
+const debugMinAbove = (body: HTMLElement, minAbove: number): void => {
 	body.style.position = 'relative';
 
 	const minAboveIndicator = document.createElement('div');
