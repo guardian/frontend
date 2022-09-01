c6ba53b7043cd0896b035d2a56beb688fb4156e2
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-09-01 10:08:36 +0100

    Add test for refresh method of CuratedContentAgent

--- article/test/agents/CuratedContentAgentTest.scala
+++ article/test/agents/CuratedContentAgentTest.scala
@@ -1,10 +1,14 @@
 package agents
 
+import com.gu.contentapi.client.utils.format.NewsPillar
+import common.editions.International
 import org.scalatest.BeforeAndAfterAll
-import model.dotcomrendering.OnwardCollectionResponse
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import test._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class CuratedContentAgentTest
     extends AnyFlatSpec
@@ -16,8 +20,20 @@ class CuratedContentAgentTest
     with WithTestApplicationContext
     with WithTestContentApiClient
     with WithTestFrontJsonFapi {
+
+  private val frontPaths =
+    List("uk", "us", "world-test-2-do-not-edit", "international", "us/sport", "au", "us-news")
+
   "CuratedContentAgentTest" should "initialise with trails being an empty Seq" in {
     val curatedContentAgent = new CuratedContentAgent(fapi)
     curatedContentAgent.getCuratedContent shouldBe Map.empty
   }
+
+  "CuratedContentAgentTest" should "not be empty after refresh" in {
+    val curatedContentAgent = new CuratedContentAgent(fapi)
+    Await.result(curatedContentAgent.refreshPaths(frontPaths), 4.second)
+    curatedContentAgent.getCuratedContentAdFree.size should be > (0)
+    curatedContentAgent.getTrails(NewsPillar, International, true).size should be > (0)
+  }
+
 }
