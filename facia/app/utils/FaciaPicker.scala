2da51a086381a513577ae8565f398f6b2ac02f66
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-09-04 17:03:10 +0100

    refactor: Updated `SUPPORTED_COLLECTIONS`

--- facia/app/utils/FaciaPicker.scala
+++ facia/app/utils/FaciaPicker.scala
@@ -11,12 +11,50 @@ class FaciaPicker extends GuLogging {
   // To check which collections are supported by DCR and update this set please check:
   // https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/lib/DecideContainer.tsx
   // and https://github.com/guardian/dotcom-rendering/issues/4720
-  val SUPPORTED_COLLECTIONS: Set[String] =
-    Set("dynamic/fast", "dynamic/slow", "fixed/small/slow-IV", "fixed/large/slow-XIV")
+  private val SUPPORTED_COLLECTIONS: Set[String] =
+    Set(
+      /*
+        "dynamic/slow-mpu",
+          pending https://github.com/guardian/dotcom-rendering/issues/5926 and
+          https://github.com/guardian/dotcom-rendering/issues/5821
+      */
 
-  def dcrSupportsAllCollectionTypes(faciaPage: PressedPage): Boolean = {
-    faciaPage.collections.forall(collection => SUPPORTED_COLLECTIONS.contains(collection.collectionType))
-  }
+      /*
+        "fixed/small/slow-V-mpu",
+          pending https://github.com/guardian/dotcom-rendering/issues/5926
+      */
+
+      /*
+        "fixed/medium/slow-XII-mpu",
+          pending https://github.com/guardian/dotcom-rendering/issues/5926
+      */
+
+      /*
+        "dynamic/package",
+          pending https://github.com/guardian/dotcom-rendering/issues/5196 and
+          https://github.com/guardian/dotcom-rendering/issues/5267
+      */
+
+      /*
+        "news/most-popular"
+          pending https://github.com/guardian/frontend/issues/25448 and
+          https://github.com/guardian/dotcom-rendering/issues/5902
+      */
+
+      /*
+        "dynamic/fast"
+          pending https://github.com/guardian/dotcom-rendering/issues/5782
+      */
+
+      "dynamic/slow",
+      "fixed/small/slow-I",
+      "fixed/small/slow-III",
+      "fixed/small/slow-IV",
+      "fixed/small/slow-V-third",
+      "fixed/medium/slow-VI",
+      "fixed/large/slow-XIV",
+      "fixed/large/slow-XIV"
+    )
 
   def getTier(faciaPage: PressedPage, path: String)(implicit request: RequestHeader): RenderType = {
     val participatingInTest = ActiveExperiments.isParticipating(DCRFronts)
