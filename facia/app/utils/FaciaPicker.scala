5d636b7f38b7938822f485764e75838758f6721f
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-09-05 08:05:07 +0100

    fix: We should define `SUPPORTED_COLLECTIONS` ahead of using it

--- facia/app/utils/FaciaPicker.scala
+++ facia/app/utils/FaciaPicker.scala
@@ -8,6 +8,54 @@ import play.api.mvc.RequestHeader
 
 object FrontChecks {
 
+  // To check which collections are supported by DCR and update this set please check:
+  // https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/lib/DecideContainer.tsx
+  // and https://github.com/guardian/dotcom-rendering/issues/4720
+  private val SUPPORTED_COLLECTIONS: Set[String] =
+    Set(
+      /*
+      "dynamic/slow-mpu",
+        pending https://github.com/guardian/dotcom-rendering/issues/5926 and
+        https://github.com/guardian/dotcom-rendering/issues/5821
+       */
+
+      /*
+      "fixed/small/slow-V-mpu",
+        pending https://github.com/guardian/dotcom-rendering/issues/5926
+       */
+
+      /*
+      "fixed/medium/slow-XII-mpu",
+        pending https://github.com/guardian/dotcom-rendering/issues/5926
+       */
+
+      /*
+      "dynamic/package",
+        pending https://github.com/guardian/dotcom-rendering/issues/5196 and
+        https://github.com/guardian/dotcom-rendering/issues/5267
+       */
+
+      /*
+      "news/most-popular"
+        pending https://github.com/guardian/frontend/issues/25448 and
+        https://github.com/guardian/dotcom-rendering/issues/5902
+       */
+
+      /*
+      "dynamic/fast"
+        pending https://github.com/guardian/dotcom-rendering/issues/5782
+       */
+
+      "dynamic/slow",
+      "fixed/small/slow-I",
+      "fixed/small/slow-III",
+      "fixed/small/slow-IV",
+      "fixed/small/slow-V-third",
+      "fixed/medium/slow-VI",
+      "fixed/large/slow-XIV",
+      "fixed/large/slow-XIV",
+    )
+
   def allCollectionsAreSupported(faciaPage: PressedPage): Boolean = {
     faciaPage.collections.forall(collection => SUPPORTED_COLLECTIONS.contains(collection.collectionType))
   }
@@ -48,54 +96,6 @@ class FaciaPicker extends GuLogging {
       ("hasNoSlideshows", FrontChecks.hasNoSlideshows(faciaPage)),
     )
   }
-
-  // To check which collections are supported by DCR and update this set please check:
-  // https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/lib/DecideContainer.tsx
-  // and https://github.com/guardian/dotcom-rendering/issues/4720
-  private val SUPPORTED_COLLECTIONS: Set[String] =
-    Set(
-      /*
-        "dynamic/slow-mpu",
-          pending https://github.com/guardian/dotcom-rendering/issues/5926 and
-          https://github.com/guardian/dotcom-rendering/issues/5821
-       */
-
-      /*
-        "fixed/small/slow-V-mpu",
-          pending https://github.com/guardian/dotcom-rendering/issues/5926
-       */
-
-      /*
-        "fixed/medium/slow-XII-mpu",
-          pending https://github.com/guardian/dotcom-rendering/issues/5926
-       */
-
-      /*
-        "dynamic/package",
-          pending https://github.com/guardian/dotcom-rendering/issues/5196 and
-          https://github.com/guardian/dotcom-rendering/issues/5267
-       */
-
-      /*
-        "news/most-popular"
-          pending https://github.com/guardian/frontend/issues/25448 and
-          https://github.com/guardian/dotcom-rendering/issues/5902
-       */
-
-      /*
-        "dynamic/fast"
-          pending https://github.com/guardian/dotcom-rendering/issues/5782
-       */
-
-      "dynamic/slow",
-      "fixed/small/slow-I",
-      "fixed/small/slow-III",
-      "fixed/small/slow-IV",
-      "fixed/small/slow-V-third",
-      "fixed/medium/slow-VI",
-      "fixed/large/slow-XIV",
-      "fixed/large/slow-XIV",
-    )
 
   def getTier(faciaPage: PressedPage, path: String)(implicit request: RequestHeader): RenderType = {
     val participatingInTest = ActiveExperiments.isParticipating(DCRFronts)
