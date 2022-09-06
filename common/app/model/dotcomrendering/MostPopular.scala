331f42b0107c6a8d2e92d918fd82971cc49384ca
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-09-06 17:16:48 +0100

    Add onwardsSource and format to OnwardCollectionResponse

--- common/app/model/dotcomrendering/MostPopular.scala
+++ common/app/model/dotcomrendering/MostPopular.scala
@@ -1,10 +1,17 @@
 package model.dotcomrendering
 
+import com.gu.contentapi.client.utils.format.{ArticleDesign, NewsPillar, StandardDisplay}
+import model.ContentFormat
 import play.api.libs.json._
+
 
 case class OnwardCollectionResponse(
     heading: String,
     trails: Seq[Trail],
+    // todo: remove default if this pattern is adopted
+    onwardsSource: String = "unknown-source", //Option[OnwardSource],
+    // todo: remove default if this pattern is adopted
+    format: ContentFormat = ContentFormat(design = ArticleDesign, theme = NewsPillar, display = StandardDisplay)
 )
 object OnwardCollectionResponse {
   implicit val collectionWrites = Json.writes[OnwardCollectionResponse]
