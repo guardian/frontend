88a0f9f41e88d52953fce9ee1ac211500a4dfeba
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-09-06 12:32:37 +0100

    Update static/src/javascripts/projects/commercial/modules/messenger/background.ts
    Co-authored-by: Simon Adcock <simonadcock2@gmail.com>

--- static/src/javascripts/projects/commercial/modules/messenger/background.ts
+++ static/src/javascripts/projects/commercial/modules/messenger/background.ts
@@ -88,7 +88,7 @@ const setCtaURL = (
 	ctaURLAnchor.appendChild(backgroundParent);
 	ctaURLAnchor.style.width = '100%';
 	ctaURLAnchor.style.height = '100%';
-	ctaURLAnchor.style.display = 'block';
+	ctaURLAnchor.style.display = 'inline-block';
 	return ctaURLAnchor;
 };
 
