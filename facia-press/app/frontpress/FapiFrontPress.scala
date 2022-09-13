ad0a65bf09ef310c7facea95d1e18519ffe995fb
Roberto Tyley <roberto.tyley@gmail.com>
2022-09-13 18:18:40 +0100

    Use handy conveniece method

--- facia-press/app/frontpress/FapiFrontPress.scala
+++ facia-press/app/frontpress/FapiFrontPress.scala
@@ -286,7 +286,7 @@ trait FapiFrontPress extends EmailFrontPress with GuLogging {
           "us" -> FaciaPressMetrics.UsPressLatencyMetric,
           "au" -> FaciaPressMetrics.AuPressLatencyMetric,
         )
-        if (Edition.allWithBetaEditions.map(_.id.toLowerCase).contains(path)) {
+        if (Edition.byId(path).isDefined) {
           metricsByPath.get(path).foreach { metric =>
             metric.recordDuration(pressDuration.toDouble)
           }
@@ -506,8 +506,7 @@ trait FapiFrontPress extends EmailFrontPress with GuLogging {
   //This will turn au/culture into culture. We want to stay consistent with the manual entry and autogeneration
   private def removeLeadEditionFromSectionId(sectionId: String): String =
     sectionId.split('/').toList match {
-      case edition :: tail if Edition.allWithBetaEditions.map(_.id.toLowerCase).contains(edition.toLowerCase) =>
-        tail.mkString("/")
+      case edition :: tail if Edition.byId(edition).isDefined => tail.mkString("/")
       case _ => sectionId
     }
 
