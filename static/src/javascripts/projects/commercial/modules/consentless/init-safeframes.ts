f9fa3f771fc77daf6a36cf33411b904715d970b0
Chris Jones <chrislomaxjones96@gmail.com>
2022-09-14 09:56:45 +0100

    Inject safeframe styles client-side

--- static/src/javascripts/projects/commercial/modules/consentless/init-safeframes.ts
+++ static/src/javascripts/projects/commercial/modules/consentless/init-safeframes.ts
@@ -1,4 +1,28 @@
 import { loadScript } from '@guardian/libs';
+import fastdom from '../../../../lib/fastdom-promise';
+
+/**
+ * Insert styling necessary for Opt-Out served safeframes.
+ *
+ * We insert these dynamically to ensure they are applied across all platforms
+ * and only when in the variant of the consentless test. This could be revisited
+ * in the future, and whether we can apply these styles server-side.
+ *
+ */
+const insertSafeframeStyles = (): Promise<void> => {
+	const css = `
+		.iab_sf {
+			margin: auto;
+		}
+	`;
+
+	const style = document.createElement('style');
+	style.appendChild(document.createTextNode(css));
+
+	return fastdom.mutate(() => {
+		document.head.appendChild(style);
+	});
+};
 
 const initSafeframes = async (): Promise<void> => {
 	const safeframeScripts = [
@@ -17,6 +41,8 @@ const initSafeframes = async (): Promise<void> => {
 		renderFile: 'https://cdn.optoutadvertising.com/script/sf/r.html',
 		positions: {},
 	});
+
+	void insertSafeframeStyles();
 };
 
 export { initSafeframes };
