3c16e6f302a7d0ef8e54dbde117811a1c505e14e
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-09-29 16:46:57 +0100

    convert get-stylesheet test to ts

--- static/src/javascripts/projects/commercial/modules/messenger/get-stylesheet.spec.ts
+++ static/src/javascripts/projects/commercial/modules/messenger/get-stylesheet.spec.ts
@@ -2,53 +2,30 @@ import { _ } from './get-stylesheet';
 
 const getStyles = _.getStyles;
 
-let styleSheets;
+let styleSheets: StyleSheetList;
 
 describe('Cross-frame messenger: get stylesheets', () => {
 	beforeEach((done) => {
-		if (document.body) {
-			document.body.innerHTML = `
+		document.body.innerHTML = `
             <style class="webfont"></style>
             <style class="webfont" data-cache-name="GuardianSansWeb"></style>
             <style class="webfont" data-cache-name="GuardianSansTextWeb"></style>
             <style class="notawebfont"></style>`;
-		}
 
 		// Why are we having to mess about with the DOM? JSDOM does not recognise
 		// `ownerNode`: https://github.com/tmpvar/jsdom/issues/992
-		styleSheets = Array.prototype.map.call(
-			document.querySelectorAll('style'),
-			(style) =>
-				Object.assign(
-					{
-						ownerNode: Object.assign(
-							{
-								matches: (selector) => {
-									const res = Array.prototype.slice.call(
-										document.querySelectorAll(selector),
-									);
-									return res.indexOf(style) > -1;
-								},
-							},
-							style,
-						),
-					},
-					style,
-				),
-		);
+		// @ts-expect-error -- this is a stub
+		styleSheets = Array.from(document.styleSheets).map((style, i) => {
+			const ownerNode = document.querySelectorAll('style')[i];
+			Object.assign(style, { ownerNode });
+			return style;
+		});
 
 		done();
 	});
 
 	afterEach(() => {
-		if (document.body) {
-			document.body.innerHTML = '';
-		}
-	});
-
-	it('should return nothing if there is no CSS selector', () => {
-		expect(getStyles({})).toBeNull();
-		expect(getStyles({ dontcare: 'hello' }, [])).toBeNull();
+		document.body.innerHTML = '';
 	});
 
 	it('should return all webfonts available', () => {
