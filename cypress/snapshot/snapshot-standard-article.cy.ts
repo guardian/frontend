a594c818777d789d8a9cf039406eb9bfc9c73eda
Ravi <7014230+arelra@users.noreply.github.com>
2022-09-30 17:47:03 +0100

    Stub all content related api requests

--- cypress/snapshot/snapshot-standard-article.cy.ts
+++ cypress/snapshot/snapshot-standard-article.cy.ts
@@ -1,5 +1,7 @@
 import { articles } from '../fixtures/pages';
 import { mostReadGeo } from '../fixtures/api/most-read-geo';
+import { mostRead } from '../fixtures/api/most-read';
+import { regularStories } from '../fixtures/api/regular-stories';
 import '@percy/cypress';
 
 const stubApiRequest = (url: string, response: object) => cy.intercept(
@@ -7,9 +9,21 @@ const stubApiRequest = (url: string, response: object) => cy.intercept(
 	(req) => req.reply({ body: response }),
 );
 
+const stubApiRequests = () => {
+	// stub most viewed right
+	stubApiRequest('https://api.nextgen.guardianapps.co.uk/most-read-geo.json?dcr=true', mostReadGeo);
+	// stub headlines
+	stubApiRequest('https://api.nextgen.guardianapps.co.uk/container/data/uk-alpha/news/regular-stories.json', regularStories);
+	// stub most viewed bottom
+	stubApiRequest('https://api.nextgen.guardianapps.co.uk/most-read/politics.json?dcr=true', mostRead);
+}
+
 describe('Visually snapshot standard article', () => {
 	[articles[0]].forEach(({ path }) => {
 		it(`snapshots ${path}`, () => {
+			// stub all api requests
+			stubApiRequests();
+			// load article
 			cy.visit(path);
 			cy.allowAllConsent();
 			// check we have top-above-nav
@@ -17,8 +31,6 @@ describe('Visually snapshot standard article', () => {
 			cy.findAdSlotIframeBySlotId('dfp-ad--top-above-nav').should(
 				'exist',
 			);
-			// stub most viewed right
-			stubApiRequest('https://api.nextgen.guardianapps.co.uk/most-read-geo.json?dcr=true', mostReadGeo);
 			// hydrate all islands
 			cy.hydrate();
 			// snapshot
