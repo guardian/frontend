33b4134c69797d9dc9c1b7c6d96ceab4382f392e
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-10-04 14:01:47 +0100

    Add confiant ab test

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/confiant-sdk-update.ts
@@ -0,0 +1,21 @@
+import type { ABTest } from '@guardian/ab-core';
+import { noop } from '../../../../../lib/noop';
+
+export const confiantSDKUpdateTest: ABTest = {
+	id: 'ConfiantSDKUpdate',
+	start: '2022-10-04',
+	expiry: '2022-10-30',
+	author: 'Jake Lee Kennedy',
+	description: 'Test the new Confiant SDK, to share with the Confiant team',
+	audience: 0,
+	audienceOffset: 0,
+	audienceCriteria: 'Opt in',
+	successMeasure: 'No change/improved ad load times',
+	canRun: () => true,
+	variants: [
+		{
+			id: 'variant',
+			test: () => noop,
+		},
+	],
+};
