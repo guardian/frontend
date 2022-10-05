9ae5e47759067e9738f1d0b3ff447ba1f0487ea0
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-10-05 10:22:47 +0100

    test name wrong

--- static/src/javascripts/projects/common/modules/experiments/tests/confiant-sdk-update.ts
+++ static/src/javascripts/projects/common/modules/experiments/tests/confiant-sdk-update.ts
@@ -2,7 +2,7 @@ import type { ABTest } from '@guardian/ab-core';
 import { noop } from '../../../../../lib/noop';
 
 export const confiantSDKUpdateTest: ABTest = {
-	id: 'ConfiantSDKUpdate',
+	id: 'ConfiantSdkUpdate',
 	start: '2022-10-04',
 	expiry: '2022-10-28',
 	author: 'Jake Lee Kennedy',
