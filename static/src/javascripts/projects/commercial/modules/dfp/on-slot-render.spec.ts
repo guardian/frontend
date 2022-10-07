0a4476c985a3e0be3596357d59afa31fe4200c62
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-10-07 12:16:12 +0100

    add a test to make sure finishRendering is called

---
+++ static/src/javascripts/projects/commercial/modules/dfp/on-slot-render.spec.ts
@@ -0,0 +1,55 @@
+import type { Advert } from './Advert';
+import { onSlotRender } from './on-slot-render';
+
+let advert: Partial<Advert>;
+
+const baseEvent = {
+	isEmpty: false,
+	slot: {
+		getSlotElementId: jest.fn().mockReturnValue('dfp-ad--top-above-nav'),
+	},
+};
+
+jest.mock('../../../../lib/raven');
+
+jest.mock('./empty-advert');
+
+// const renderAdvert = jest.fn().mockReturnValue(Promise.resolve(true));
+
+jest.mock('./render-advert', () => ({
+	renderAdvert: jest.fn().mockReturnValue(Promise.resolve(true)),
+}));
+
+jest.mock('./get-advert-by-id', () => ({
+	getAdvertById: jest.fn().mockImplementation(() => advert),
+}));
+
+describe('onSlotRender', () => {
+	it('if rendered call set call finishedRendering on the advert', () => {
+		const event = { ...baseEvent, size: [300, 250], isEmpty: false };
+		advert = {
+			id: 'dfp-ad--top-above-nav',
+			finishedRendering: (isRendered: boolean) => {
+				expect(isRendered).toBe(true);
+			},
+		};
+		// @ts-expect-error - we are mocking the function
+		onSlotRender(event);
+		// expect(advert.finishedRendering).toBeCalledWith(true);
+	});
+
+	it('if not rendered call set call finishedRendering on the advert', () => {
+		const event = {
+			...baseEvent,
+			isEmpty: true,
+		};
+		advert = {
+			id: 'dfp-ad--top-above-nav',
+			finishedRendering: (isRendered: boolean) => {
+				expect(isRendered).toBe(false);
+			},
+		};
+		// @ts-expect-error - we are mocking the function
+		onSlotRender(event);
+	});
+});
