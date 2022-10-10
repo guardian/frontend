9405105ccbcb89c36a3eb32bb115338f8d34e4bf
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-10-10 10:00:12 +0100

    Advert.ts cleanup (#25559)
    * remove unused timing stuff
    
    * remove advert.ts IE11 class fallback
    
    * put class changes in fastdom
    
    * remove filterClasses function it makes more sense inline
    
    * update of this.extraNodeClasses needs to be inside fastdom callback
    
    * rebuild commercial bundle graph
    
    * remove more old tracking code
    
    * update mocks
    
    * rebuild commercial bundle graph
    
    * remove useless state flags
    
    * add back a clearer `finishedRendering` 'event'
    
    * add a test to make sure finishRendering is called
    
    * update to updateExtraSlotClasses
    
    * remove redundant fastdom
    
    * tidy up

--- static/src/javascripts/projects/commercial/modules/dfp/Advert.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/Advert.ts
@@ -5,20 +5,9 @@ import {
 } from '@guardian/commercial-core';
 import type { AdSize, SizeMapping, SlotName } from '@guardian/commercial-core';
 import { breakpoints } from '../../../../lib/detect';
+import fastdom from '../../../../lib/fastdom-promise';
 import { breakpointNameToAttribute } from './breakpoint-name-to-attribute';
 import { buildGoogletagSizeMapping, defineSlot } from './define-slot';
-
-type Resolver = (x: boolean) => void;
-
-type Timings = {
-	createTime: number | null;
-	startLoading: number | null;
-	stopLoading: number | null;
-	startRendering: number | null;
-	stopRendering: number | null;
-	loadingMethod: number | null;
-	lazyWaitComplete: number | null;
-};
 
 const stringToTuple = (size: string): AdSizeTuple => {
 	const dimensions = size.split(',', 2).map(Number);
@@ -84,26 +73,10 @@ class Advert {
 	size: AdSize | 'fluid' | null = null;
 	slot: googletag.Slot;
 	isEmpty: boolean | null = null;
-	isLoading = false;
-	isRendering = false;
-	isLoaded = false;
 	isRendered = false;
 	shouldRefresh = false;
-	whenLoaded: Promise<boolean>;
-	whenLoadedResolver: Resolver | null = null;
-	whenRendered: Promise<boolean>;
-	whenRenderedResolver: Resolver | null = null;
 	whenSlotReady: Promise<void>;
 	extraNodeClasses: string[] = [];
-	timings: Timings = {
-		createTime: null,
-		startLoading: null,
-		stopLoading: null,
-		startRendering: null,
-		stopRendering: null,
-		loadingMethod: null,
-		lazyWaitComplete: null,
-	};
 	hasPrebidSize = false;
 	lineItemId: number | null = null;
 
@@ -119,61 +92,35 @@ class Advert {
 
 		this.slot = slotDefinition.slot;
 		this.whenSlotReady = slotDefinition.slotReady;
-
-		this.whenLoaded = new Promise((resolve: Resolver) => {
-			this.whenLoadedResolver = resolve;
-		}).then((isLoaded: boolean): boolean => {
-			this.isLoaded = isLoaded;
-			return isLoaded;
-		});
-
-		this.whenRendered = new Promise((resolve: Resolver) => {
-			this.whenRenderedResolver = resolve;
-		}).then((isRendered) => {
-			this.isRendered = isRendered;
-			return isRendered;
-		});
 	}
 
-	static filterClasses = (
-		oldClasses: string[],
-		newClasses: string[],
-	): string[] =>
-		oldClasses.filter((oldClass) => !newClasses.includes(oldClass));
-
-	startLoading(): void {
-		this.isLoading = true;
-		this.timings.startLoading = window.performance.now();
+	/**
+	 * Call this method once the ad has been rendered, it will set the
+	 * `isRendered` flag to true, which is used to determine whether to load
+	 * or refresh the ad
+	 *
+	 * @param isRendered was an advert rendered
+	 */
+	finishedRendering(isRendered: boolean): void {
+		this.isRendered = isRendered;
 	}
 
-	stopLoading(isLoaded: boolean): void {
-		this.isLoading = false;
-		if (this.whenLoadedResolver) {
-			this.whenLoadedResolver(isLoaded);
-		}
-		this.timings.stopLoading = window.performance.now();
-	}
-
-	startRendering(): void {
-		this.isRendering = true;
-		this.timings.startRendering = window.performance.now();
-	}
-
-	stopRendering(isRendered: boolean): void {
-		this.isRendering = false;
-		if (this.whenRenderedResolver) {
-			this.whenRenderedResolver(isRendered);
-		}
-	}
-
-	updateExtraSlotClasses(...newClasses: string[]): void {
-		const classesToRemove = Advert.filterClasses(
-			this.extraNodeClasses,
-			newClasses,
+	/**
+	 * Update the "extra" classes for this slot e.g. `ad-slot--outstream`, so that the base classes
+	 * like `ad-slot` etc. are never removed
+	 *
+	 * @param newClasses An array of classes to set on the slot
+	 **/
+	async updateExtraSlotClasses(...newClasses: string[]): Promise<void> {
+		const classesToRemove = this.extraNodeClasses.filter(
+			(c) => !newClasses.includes(c),
 		);
-		// IE11 does not support multiple arguments to classList.add/remove so do these one-by-ones
-		classesToRemove.forEach((cls) => this.node.classList.remove(cls));
-		newClasses.forEach((cls) => this.node.classList.add(cls));
+
+		await fastdom.mutate(() => {
+			this.node.classList.remove(...classesToRemove);
+			this.node.classList.add(...newClasses);
+		});
+
 		this.extraNodeClasses = newClasses;
 	}
 
@@ -236,6 +183,5 @@ class Advert {
 export { Advert };
 
 export const _ = {
-	filterClasses: Advert.filterClasses,
 	getSlotSizeMapping,
 };
