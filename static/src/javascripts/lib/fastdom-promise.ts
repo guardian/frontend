15870f1e3a658bdc5713a0feae96d495c78a23c6
Chris Jones <chrislomaxjones96@gmail.com>
2022-10-11 15:36:27 +0100

    Permit default export for fastdom

--- static/src/javascripts/lib/fastdom-promise.ts
+++ static/src/javascripts/lib/fastdom-promise.ts
@@ -1,4 +1,5 @@
 import fastdom from 'fastdom';
 import fastdomPromised from 'fastdom/extensions/fastdom-promised';
 
+// eslint-disable-next-line import/no-default-export -- Historically used a default export here
 export default fastdom.extend(fastdomPromised);
