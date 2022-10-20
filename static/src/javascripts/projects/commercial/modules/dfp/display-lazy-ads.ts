bf540ef31db0b8f5153330fef4e4ac3d1910ef98
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-10-20 16:59:11 +0100

    old addListener still used

--- static/src/javascripts/projects/commercial/modules/dfp/display-lazy-ads.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/display-lazy-ads.ts
@@ -1,4 +1,5 @@
 import { partition } from '../../../../lib/partition';
+import { pageSkin } from '../creatives/page-skin';
 import { dfpEnv } from './dfp-env';
 import { enableLazyLoad } from './lazy-load';
 import { loadAdvert } from './load-advert';
@@ -19,6 +20,7 @@ const displayLazyAds = (): void => {
 
 	instantLoadAdverts.forEach(loadAdvert);
 	lazyLoadAdverts.forEach(enableLazyLoad);
+	pageSkin();
 };
 
 export { displayLazyAds };
