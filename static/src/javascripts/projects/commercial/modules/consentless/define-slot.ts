bea875fda102fe02c53994bdb9b3a89b2878d5c8
Chris Jones <chrislomaxjones96@gmail.com>
2022-10-25 09:38:43 +0100

    Remove ad slot if empty callback is fired

--- static/src/javascripts/projects/commercial/modules/consentless/define-slot.ts
+++ static/src/javascripts/projects/commercial/modules/consentless/define-slot.ts
@@ -1,20 +1,29 @@
+import { log } from '@guardian/libs';
+import fastdom from 'fastdom';
 import { renderConsentlessAdvertLabel } from './render-advert-label';
 
 const defineSlot = (slot: HTMLElement, slotName: string): void => {
 	const slotId = slot.id;
+
+	const filledCallback = () => {
+		log('commercial', `Filled consentless ${slotId}`);
+		void renderConsentlessAdvertLabel(slot);
+	};
+
+	const emptyCallback = () => {
+		log('commercial', `Empty consentless ${slotId}`);
+		fastdom.mutate(() => {
+			slot.remove();
+		});
+	};
 
 	window.ootag.queue.push(() => {
 		window.ootag.defineSlot({
 			adSlot: slotName,
 			targetId: slotId,
 			id: slotId,
-			filledCallback: () => {
-				void renderConsentlessAdvertLabel(slot);
-				console.log(`filled consentless ${slotId}`);
-			},
-			emptyCallback: () => {
-				console.log(`empty consentless ${slotId}`);
-			},
+			filledCallback,
+			emptyCallback,
 		});
 	});
 };
