543bc5005273c1dfd13b0af1978d6b0f92c3be77
Ravi <7014230+arelra@users.noreply.github.com>
2022-10-27 17:13:46 +0100

    Fix robust errors

--- static/src/javascripts/lib/robust.ts
+++ static/src/javascripts/lib/robust.ts
@@ -6,8 +6,8 @@
 import { convertError, reportError } from './report-error';
 
 type ModuleFunction = () => void;
-type Module = [string, ModuleFunction]
-type Modules = Array<Module>;
+type Module = [string, ModuleFunction];
+type Modules = Module[];
 
 const catchErrors = (fn: ModuleFunction): Error | undefined => {
 	let error: Error | undefined;
@@ -21,10 +21,12 @@ const catchErrors = (fn: ModuleFunction): Error | undefined => {
 	return error;
 };
 
-const logError = (moduleName: string, error: Error, tags?: Record<string, string>) => {
-	if (window.console && window.console.warn) {
-		window.console.warn('Caught error.', error.stack);
-	}
+const logError = (
+	moduleName: string,
+	error: Error,
+	tags?: Record<string, string>,
+): void => {
+	window.console.warn('Caught error.', error.stack);
 
 	if (tags) {
 		reportError(error, { module: moduleName, ...tags }, false);
@@ -33,18 +35,25 @@ const logError = (moduleName: string, error: Error, tags?: Record<string, string
 	}
 };
 
-const catchAndLogError = (name: string, fn: ModuleFunction, tags?: Record<string, string>) => {
-	const error =catchErrors(fn);
+const catchAndLogError = (
+	name: string,
+	fn: ModuleFunction,
+	tags?: Record<string, string>,
+): void => {
+	const error = catchErrors(fn);
 
 	if (error) {
 		logError(name, error, tags);
 	}
 };
 
-const catchErrorsWithContext = (modules: Modules, tags?: Record<string, string>) => {
+const catchErrorsWithContext = (
+	modules: Modules,
+	tags?: Record<string, string>,
+): void => {
 	modules.forEach(([name, fn]) => catchAndLogError(name, fn, tags));
 };
 
 export { catchErrorsWithContext, logError };
-export type { Modules }
-export const _ = { catchAndLogError};
+export type { Modules };
+export const _ = { catchAndLogError };
