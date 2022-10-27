0032688248bb22f31b89ecb2bd786980d4ec8734
Ravi <7014230+arelra@users.noreply.github.com>
2022-10-27 17:09:34 +0100

    Fix robust.spec errors

--- static/src/javascripts/lib/robust.spec.ts
+++ static/src/javascripts/lib/robust.spec.ts
@@ -5,67 +5,67 @@ import type { Modules } from './robust';
 const { catchAndLogError } = _;
 
 jest.mock('lib/report-error', () => ({
-    convertError: jest.fn((e) => e),
+	convertError: jest.fn((e: Error) => e),
 	reportError: jest.fn(),
 }));
 
 let origConsoleWarn: typeof window.console.warn;
 
 beforeEach(() => {
-    jest.clearAllMocks();
-    origConsoleWarn = window.console.warn;
-    window.console.warn = jest.fn();
+	jest.clearAllMocks();
+	origConsoleWarn = window.console.warn;
+	window.console.warn = jest.fn();
 });
 
 afterEach(() => {
-    window.console.warn = origConsoleWarn;
+	window.console.warn = origConsoleWarn;
 });
 
 describe('robust', () => {
-    const ERROR = new Error('Something broke.');
-    const META = { module: 'test' };
+	const ERROR = new Error('Something broke.');
+	const META = { module: 'test' };
 
-    const noError = () => true;
+	const noError = () => true;
 
-    const throwError = () => {
-        throw ERROR;
-    };
+	const throwError = () => {
+		throw ERROR;
+	};
 
-    test('catchAndLogError()', () => {
-        expect(() => {
-            catchAndLogError('test', noError);
-        }).not.toThrowError();
+	test('catchAndLogError()', () => {
+		expect(() => {
+			catchAndLogError('test', noError);
+		}).not.toThrowError();
 
-        expect(() => {
-            catchAndLogError('test', throwError);
-        }).not.toThrowError(ERROR);
+		expect(() => {
+			catchAndLogError('test', throwError);
+		}).not.toThrowError(ERROR);
 
-        expect(convertError).toHaveBeenCalledTimes(1);
-        expect(window.console.warn).toHaveBeenCalledTimes(1);
-    });
+		expect(convertError).toHaveBeenCalledTimes(1);
+		expect(window.console.warn).toHaveBeenCalledTimes(1);
+	});
 
-    test('catchAndLogError() - default reporter with no error', () => {
-        catchAndLogError('test', noError);
-        expect(convertError).toHaveBeenCalledTimes(0);
-        expect(reportError).not.toHaveBeenCalled();
-    });
+	test('catchAndLogError() - default reporter with no error', () => {
+		catchAndLogError('test', noError);
+		expect(convertError).toHaveBeenCalledTimes(0);
+		expect(reportError).not.toHaveBeenCalled();
+	});
 
-    test('catchAndLogError() - default reporter with error', () => {
-        catchAndLogError('test', throwError);
-        expect(convertError).toHaveBeenCalledTimes(1);
-        expect(reportError).toHaveBeenCalledWith(ERROR, META, false);
-    });
+	test('catchAndLogError() - default reporter with error', () => {
+		catchAndLogError('test', throwError);
+		expect(convertError).toHaveBeenCalledTimes(1);
+		expect(reportError).toHaveBeenCalledWith(ERROR, META, false);
+	});
 
-    test('catchErrorsWithContext()', () => {
-        const runner = jest.fn();
+	test('catchErrorsWithContext()', () => {
+		const runner = jest.fn();
 
-        const MODULES = [
-            ['test-1', runner],
-            ['test-2', runner],
-            ['test-3', runner],
-        ] as Modules;
+		const MODULES = [
+			['test-1', runner],
+			['test-2', runner],
+			['test-3', runner],
+		] as Modules;
 
-        catchErrorsWithContext(MODULES);
-        expect(runner).toHaveBeenCalledTimes(MODULES.length);
-    });
+		catchErrorsWithContext(MODULES);
+		expect(runner).toHaveBeenCalledTimes(MODULES.length);
+	});
 });
