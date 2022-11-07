9459a8135c925d8b2ad68f6ff8dec625df711185
Emma Imber <emma-jo.imber@guardian.co.uk>
2022-11-07 16:36:36 +0000

    Add consentless labels to the party

--- static/src/javascripts/projects/commercial/modules/consentless/render-advert-label.ts
+++ static/src/javascripts/projects/commercial/modules/consentless/render-advert-label.ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-misused-promises
+-- Fastdom measure and mutate signatures are Promise<void>
+-- Nested fastdom measure-mutate promises throw the error:
+-- "Promise returned in function argument where a void return was expected"
+*/
+
 import crossIcon from 'svgs/icon/cross.svg';
 import fastdom from '../../../../lib/fastdom-promise';
 import { shouldRenderLabel } from '../dfp/render-advert-label';
 
-const createAdCloseDiv = (): HTMLElement => {
+export const createAdCloseDiv = (): HTMLElement => {
 	const closeDiv: HTMLElement = document.createElement('button');
 	closeDiv.className = 'ad-slot__close-button';
 	closeDiv.innerHTML = crossIcon.markup;
@@ -15,12 +21,19 @@ const createAdCloseDiv = (): HTMLElement => {
 	return closeDiv;
 };
 
-const createAdLabel = (): HTMLElement => {
-	const adLabel = document.createElement('div');
-	adLabel.className = 'ad-slot__label';
-	adLabel.innerHTML = 'Advertisement';
-	adLabel.appendChild(createAdCloseDiv());
-	return adLabel;
+export const renderConsentlessAdvertLabel = (
+	adSlotNode: HTMLElement,
+): Promise<Promise<void>> => {
+	return fastdom.measure(() => {
+		if (shouldRenderLabel(adSlotNode)) {
+			return fastdom.mutate(() => {
+				//when the time comes to use a different ad label for consentless, we can update
+				//the attribute name that we set below and add a css selector accordingly
+				adSlotNode.setAttribute('data-label-show', 'true');
+			});
+		}
+		return Promise.resolve();
+	});
 };
 
 // TODO: flesh out this function once we have a better idea of what we want it to look like
@@ -34,63 +47,3 @@ const createAdLabel = (): HTMLElement => {
 // 	consentlessLabelInfo.innerHTML = `Opt Out: Why am I seeing this?`;
 // 	adLabelNode.appendChild(consentlessLabelInfo);
 // };
-
-/**
- *  **Dynamic labels:**
- *  Advert labels are historically inserted dynamically as a child of the advert slot node.
- *  This causes a cumulative layout shift (CLS) as content below is pushed down. This is
- *  particularly noticeable when ads are refreshed as the advert slot contents are deleted.
- *
- *  **Toggled labels:**
- *  To prevent CLS the label inserted on the server with its visibility initially hidden.
- *  Its visibility and width is toggled once the ad and its width is known.
- *  Currently only for dfp-ad--top-above-nav.
- * @param {HTMLElement} adSlotNode
- */
-export const renderConsentlessAdvertLabel = (
-	adSlotNode: HTMLElement,
-): Promise<Promise<void>> => {
-	let renderDynamic = true;
-	const shouldRender = shouldRenderLabel(adSlotNode);
-
-	// eslint-disable-next-line @typescript-eslint/no-misused-promises -- fastdom miss-typed
-	return fastdom.measure(async () => {
-		if (adSlotNode.id === 'dfp-ad--top-above-nav') {
-			const labelToggle = document.querySelector<HTMLElement>(
-				'.ad-slot__label.ad-slot__label--toggle',
-			);
-			// const adLabelNode =
-			// adSlotNode.querySelector<HTMLElement>('.ad-slot__label');
-
-			// if (adLabelNode) {
-			// insertConsentlessLabelInfo(adLabelNode);
-			// }
-
-			if (labelToggle) {
-				// found a toggled label so don't render dynamically
-				renderDynamic = false;
-				if (shouldRender) {
-					await fastdom.mutate(() => {
-						labelToggle.classList.remove('hidden');
-						labelToggle.classList.add('visible');
-					});
-				} else {
-					// some ads should not have a label
-					// for example fabric ads can have an embedded label
-					// so don't display and remove from layout
-					await fastdom.mutate(() => {
-						labelToggle.style.display = 'none';
-					});
-				}
-			}
-		}
-
-		if (renderDynamic && shouldRender) {
-			const adLabelNode = createAdLabel();
-			await fastdom.mutate(() => {
-				adSlotNode.prepend(adLabelNode);
-				// insertConsentlessLabelInfo(adLabelNode);
-			});
-		}
-	});
-};
