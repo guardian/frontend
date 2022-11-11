ad26eb6efcd22552301184561591cf6ccd67f8ce
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-11-11 11:17:31 +0000

    make sure ootag finishes

--- static/src/javascripts/projects/commercial/modules/consentless/prepare-ootag.ts
+++ static/src/javascripts/projects/commercial/modules/consentless/prepare-ootag.ts
@@ -1,36 +1,46 @@
-import { buildPageTargetingConsentless } from '@guardian/commercial-core';
+import {
+	buildPageTargetingConsentless,
+	initMessenger,
+} from '@guardian/commercial-core';
 import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
 import { loadScript } from '@guardian/libs';
 import { commercialFeatures } from 'common/modules/commercial/commercial-features';
+import { reportError } from 'lib/report-error';
+import { init as getStyles } from '../messenger/get-stylesheet';
+
+initMessenger([getStyles], [], reportError);
 
 function initConsentless(consentState: ConsentState): Promise<void> {
-	// Stub the command queue
-	// @ts-expect-error -- it’s a stub, not the whole OO tag object
-	window.ootag = {
-		queue: [],
-	};
-	window.ootag.queue.push(function () {
-		window.ootag.initializeOo({
-			publisher: 33,
-			noLogging: 0,
-			alwaysNoConsent: 1,
+	return new Promise((resolve) => {
+		// Stub the command queue
+		// @ts-expect-error -- it’s a stub, not the whole OO tag object
+		window.ootag = {
+			queue: [],
+		};
+		window.ootag.queue.push(function () {
+			window.ootag.initializeOo({
+				publisher: 33,
+				noLogging: 0,
+				alwaysNoConsent: 1,
+				noRequestsOnPageLoad: 1,
+			});
+
+			Object.entries(
+				buildPageTargetingConsentless(
+					consentState,
+					commercialFeatures.adFree,
+				),
+			).forEach(([key, value]) => {
+				if (!value) {
+					return;
+				}
+				window.ootag.addParameter(key, value);
+			});
+			resolve();
 		});
 
-		Object.entries(
-			buildPageTargetingConsentless(
-				consentState,
-				commercialFeatures.adFree,
-			),
-		).forEach(([key, value]) => {
-			if (!value) {
-				return;
-			}
-			window.ootag.addParameter(key, value);
-		});
+		void loadScript('//cdn.optoutadvertising.com/script/ooguardian.v4.js');
 	});
-
-	void loadScript('//cdn.optoutadvertising.com/script/ooguardian.v3.min.js');
-	return Promise.resolve();
 }
 
 export { initConsentless };
