4566b08bd232b2c522fd11c71bf4618f2a6e24ac
Tom Forbes <tom.forbes@theguardian.com>
2022-11-14 16:32:09 +0000

    handle all menu items

--- static/src/javascripts/projects/common/modules/navigation/user-account.ts
+++ static/src/javascripts/projects/common/modules/navigation/user-account.ts
@@ -1,111 +1,156 @@
-import fastdom from 'lib/fastdom-promise';
 import { getUserFromCookie, isUserLoggedIn } from 'common/modules/identity/api';
+import fastdom from 'lib/fastdom-promise';
 import { bufferedNotificationListener } from '../bufferedNotificationListener';
 
 const updateCommentLink = (commentItems: Element[]): void => {
-    const user = getUserFromCookie();
+	const user = getUserFromCookie();
 
-    if (user) {
-        commentItems.forEach(commentItem => {
-            fastdom
-                .measure(() =>
-                    commentItem.querySelector('.js-add-comment-activity-link')
-                )
-                .then(commentLink => {
+	if (user) {
+		commentItems.forEach((commentItem) => {
+			void fastdom
+				.measure(() =>
+					commentItem.querySelector('.js-add-comment-activity-link'),
+				)
+				.then((commentLink) => {
 					if (commentLink) {
-						fastdom.mutate(() => {
+						void fastdom.mutate(() => {
 							commentItem.classList.remove('u-h');
 							commentLink.setAttribute(
 								'href',
-								`https://profile.theguardian.com/user/id/${user.id}`
+								`https://profile.theguardian.com/user/id/${user.id}`,
 							);
-						})
+						});
 					}
 				});
-        });
-    }
+		});
+	}
+};
+
+const groupNotificationsByTarget = (
+	notifications: NotificationEvent[],
+): Record<string, string[]> => {
+	const notificationsMap: Record<string, string[]> = {};
+	notifications.forEach(({ message, target }) => {
+		if (Array.isArray(notificationsMap[target])) {
+			notificationsMap[target].push(message);
+		} else {
+			notificationsMap[target] = [message];
+		}
+	});
+	return notificationsMap;
 };
 
 const showNotifications = (notifications: NotificationEvent[]): void => {
-    fastdom
-        .measure(() => ({
-            badge: document.querySelector('.js-user-account-notification-badge'),
-            menu: document.querySelector('#my-account-dropdown'),
-        }))
-        .then(els => {
-            const { badge, menu } = els;
+	void fastdom
+		.measure(() => ({
+			badge: document.querySelector(
+				'.js-user-account-notification-badge',
+			),
+			menu: document.querySelector('#my-account-dropdown'),
+		}))
+		.then((els) => {
+			const { badge, menu } = els;
 
-            if (notifications.length > 0 && badge && menu) {
+			if (notifications.length > 0 && badge && menu) {
 				// Show the notification badge
 				badge.classList.remove('is-hidden');
 
-                // const menuItem = menu.querySelector(`a[data-link-name=${id}]`)
-                // if (menuItem) {
-                //     // Add the notifications and dot to the relevant menu item(s)
-                //     const labelEl = document.createElement('div');
-                //     labelEl.innerText = 'Settings';
-                //     const notificationEls = notifications.map(({message}) => {
-                //         const el = document.createElement('div');
-                //         el.classList.add('dropdown-menu__notification');
-                //         el.innerText = message;
-                //         return el;
-                //     });
-                //     menuItem.innerHTML = '';
-                //     labelEl.classList.add('top-bar__user-account-notification-badge');
-                //     [labelEl, ...notificationEls].forEach(e => menuItem.appendChild(e))
-                // }
-            }
-        });
+				// Add messages to the relevant menu item(s)
+				const groupedNotifications =
+					groupNotificationsByTarget(notifications);
+
+				Object.entries(groupedNotifications).map(
+					([target, messages]) => {
+						const menuItem = menu.querySelector(
+							`a[data-link-id=${target}]`,
+						);
+						if (menuItem?.textContent) {
+							const labelEl = document.createElement('div');
+							labelEl.innerText = menuItem.textContent.trim();
+
+							const messageEls = messages.map((message) => {
+								const messageEl = document.createElement('div');
+								messageEl.classList.add(
+									'dropdown-menu__notification',
+								);
+								messageEl.innerText = message;
+								return messageEl;
+							});
+
+							menuItem.innerHTML = '';
+							labelEl.classList.add(
+								'top-bar__user-account-notification-badge',
+							);
+							[labelEl, ...messageEls].forEach((e) =>
+								menuItem.appendChild(e),
+							);
+						}
+					},
+				);
+			}
+		});
 };
 
 const showMyAccountIfNecessary = (): void => {
-    if (!isUserLoggedIn()) {
-        // return;
-    }
+	if (!isUserLoggedIn()) {
+		// return;
+	}
 
-    fastdom
-        .measure(() => ({
-            signIns: Array.from(
-                document.querySelectorAll('.js-navigation-sign-in')
-            ),
-            accountActionsLists: Array.from(
-                document.querySelectorAll('.js-navigation-account-actions')
-            ),
-            commentItems: Array.from(
-                document.querySelectorAll('.js-show-comment-activity')
-            ),
-        }))
-        .then(els => {
-            const { signIns, accountActionsLists, commentItems } = els;
-            return fastdom
-                .mutate(() => {
-                    signIns.forEach(signIn => {
-                        signIn.remove();
-                    });
-                    accountActionsLists.forEach(accountActions => {
-                        accountActions.classList.remove('is-hidden');
-                    });
+	void fastdom
+		.measure(() => ({
+			signIns: Array.from(
+				document.querySelectorAll('.js-navigation-sign-in'),
+			),
+			accountActionsLists: Array.from(
+				document.querySelectorAll('.js-navigation-account-actions'),
+			),
+			commentItems: Array.from(
+				document.querySelectorAll('.js-show-comment-activity'),
+			),
+		}))
+		.then((els) => {
+			const { signIns, accountActionsLists, commentItems } = els;
+			return fastdom
+				.mutate(() => {
+					signIns.forEach((signIn) => {
+						signIn.remove();
+					});
+					accountActionsLists.forEach((accountActions) => {
+						accountActions.classList.remove('is-hidden');
+					});
 
-                    Array.from(
-                        document.querySelectorAll('.js-user-account-trigger')
-                    ).forEach(accountTrigger => {
-                        accountTrigger.classList.remove('is-hidden');
-                    });
-                })
-                .then(() => {
-                    updateCommentLink(commentItems);
+					Array.from(
+						document.querySelectorAll('.js-user-account-trigger'),
+					).forEach((accountTrigger) => {
+						accountTrigger.classList.remove('is-hidden');
+					});
+				})
+				.then(() => {
+					updateCommentLink(commentItems);
 
-                    let notifications: NotificationEvent[] = [];
+					let notifications: NotificationEvent[] = [
+						{
+							target: 'billing',
+							message: 'Your card has expired.',
+						},
+						{
+							target: 'edit_profile',
+							message: 'Add your contact details.',
+						},
+						{
+							target: 'edit_profile',
+							message: 'Set your public details.',
+						},
+					];
+					showNotifications(notifications); // TODO - remove
 
-                    bufferedNotificationListener.on(
-                        (event) => {
-                            console.log("I got a notification!", event.detail);
-                            notifications = [...notifications, event.detail];
-                            showNotifications(notifications);
-                        },
-                    );
-                });
-        });
+					bufferedNotificationListener.on((event) => {
+						console.log('I got a notification!', event.detail);
+						notifications = [...notifications, event.detail];
+						showNotifications(notifications);
+					});
+				});
+		});
 };
 
 export { showMyAccountIfNecessary };
