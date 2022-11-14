21e34eb51ea001bc3cc214f7759738630da8c29f
Emma Imber <emma-jo.imber@guardian.co.uk>
2022-11-14 15:35:38 +0000

    Don't show ad label for fluid ads

--- static/src/javascripts/projects/commercial/modules/dfp/render-advert-label.ts
+++ static/src/javascripts/projects/commercial/modules/dfp/render-advert-label.ts
@@ -13,6 +13,7 @@ const shouldRenderLabel = (adSlotNode: HTMLElement): boolean =>
 		adSlotNode.classList.contains('ad-slot--frame') ||
 		adSlotNode.classList.contains('ad-slot--gc') ||
 		adSlotNode.classList.contains('u-h') ||
+		adSlotNode.classList.contains('ad-slot--fluid') ||
 		// set for out-of-page (1x1) and empty (2x2) ads
 		adSlotNode.classList.contains('ad-slot--collapse') ||
 		adSlotNode.getAttribute('data-label') === 'false' ||
