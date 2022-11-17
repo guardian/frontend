d122d6461e943d276360e26021807d177517717b
Ioanna Kokkini <ioannakok@hotmail.com>
2022-11-17 11:22:25 +0000

    Update CardStylePicker in facia-press to understand a card is of type SpecialReportAlt
    At the moment there is only one campaign with id SpecialReportAlt which is of type SpecialReportAlt

--- common/app/model/CardStylePicker.scala
+++ common/app/model/CardStylePicker.scala
@@ -1,6 +1,6 @@
 package model
 
-import com.gu.facia.api.utils.{CardStyle, FaciaContentUtils, SpecialReport}
+import com.gu.facia.api.utils.{CardStyle, FaciaContentUtils, SpecialReport, SpecialReportAlt}
 import com.gu.facia.client.models.{MetaDataCommonFields, TrailMetaData}
 import com.gu.targeting.client.{Campaign, ReportFields}
 import com.gu.contentapi.client.model.v1.{Content => CapiContent}
@@ -9,11 +9,13 @@ import commercial.targeting.CampaignAgent
 
 object CardStylePicker {
 
-  def apply(content: CapiContent, trailMetaData: MetaDataCommonFields): CardStyle = {
+  def apply(content: CapiContent): CardStyle = {
     val tags = content.tags.map(_.id).toSeq
     extractCampaigns(tags) match {
       case Nil => CardStyle(content, TrailMetaData.empty)
-      case _   => SpecialReport
+      case campaign :: _ =>
+        if (campaign.id.toString.toLowerCase() == "specialreportalt") SpecialReportAlt else SpecialReport
+      case _ => SpecialReport
     }
   }
 
@@ -21,7 +23,9 @@ object CardStylePicker {
     val tags = FaciaContentUtils.tags(content).map(_.id)
     extractCampaigns(tags) match {
       case Nil => FaciaContentUtils.cardStyle(content)
-      case _   => SpecialReport
+      case campaign :: _ =>
+        if (campaign.id.toString.toLowerCase() == "specialreportalt") SpecialReportAlt else SpecialReport
+      case _ => SpecialReport
     }
   }
 
