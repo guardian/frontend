7b7a6784ed26d7710ad78febcbc37c83e6acbf57
Ravi <7014230+arelra@users.noreply.github.com>
2022-11-21 11:49:20 +0000

    Bump @guardian/commercial-core@^4.25.0

--- static/src/javascripts/projects/common/modules/commercial/build-page-targeting.ts
+++ static/src/javascripts/projects/common/modules/commercial/build-page-targeting.ts
@@ -3,6 +3,7 @@ import { buildPageTargeting } from '@guardian/commercial-core';
 import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
 import { log } from '@guardian/libs';
 import { once } from 'lodash-es';
+import { getSynchronousParticipations } from 'common/modules/experiments/ab';
 import { removeFalseyValues } from '../../../commercial/modules/header-bidding/utils';
 import { commercialFeatures } from './commercial-features';
 
@@ -44,8 +45,9 @@ const getPageTargeting = (consentState: ConsentState): PageTargeting => {
 	const { page } = window.guardian.config;
 
 	const pageTargeting = buildPageTargeting({
-		consentState,
 		adFree: commercialFeatures.adFree,
+		clientSideParticipations: getSynchronousParticipations(),
+		consentState,
 	});
 
 	// third-parties wish to access our page targeting, before the googletag script is loaded.
