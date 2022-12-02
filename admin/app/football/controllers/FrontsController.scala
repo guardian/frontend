66ca50d3bac67b086bd2793d42062998aab04a9f
Georges Lebreton <georges.lebreton@guardian.co.uk>
2022-12-02 14:45:02 +0000

    initial work to align matcday option with results and fixtures

--- admin/app/football/controllers/FrontsController.scala
+++ admin/app/football/controllers/FrontsController.scala
@@ -42,18 +42,43 @@ class FrontsController(
       }
     }
 
-  def matchDay: Action[AnyContent] =
-    Action.async { implicit request =>
-      val snapFields = SnapFields(
-        SNAP_TYPE,
-        SNAP_CSS,
-        s"$host/football/live.json",
-        s"${Configuration.site.host}/football/live",
-        "Live matches",
-        "Today's matches",
-      )
-      previewFrontsComponent(snapFields)
+  def matchDayRedirect: Action[AnyContent] =
+    Action { implicit request =>
+      val submission = request.body.asFormUrlEncoded.get
+      val competitionId = submission("competition").head
+      Cached(60)(WithoutRevalidationResult(SeeOther(s"/admin/football/fronts/live/$competitionId")))
     }
+
+  def matchDay(competitionId: String): Action[AnyContent] =
+  Action.async { implicit request =>
+    val foResult =
+      if ("all" == competitionId) {
+        val snapFields = SnapFields(
+          SNAP_TYPE,
+          SNAP_CSS,
+          s"$host/football/live.json",
+          s"${Configuration.site.host}/football/live",
+          "Live matches",
+          "Today's matches",
+        )
+        FutureOpt.fromFuture(previewFrontsComponent(snapFields))
+      } else {
+        for {
+          season <- getCompetition(competitionId)
+          competitionName = PA.competitionName(season)
+          snapFields = SnapFields(
+            SNAP_TYPE,
+            SNAP_CSS,
+            s"$host/football/$competitionId/live.json",
+            s"${Configuration.site.host}/football/live",
+            s"$competitionName results",
+            s"View today's live $competitionName matches",
+          )
+          previewContent <- FutureOpt.fromFuture(previewFrontsComponent(snapFields))
+        } yield previewContent
+      }
+    foResult.getOrElse(NoCache(NotFound(views.html.football.error(s"Competition $competitionId not found"))))
+  }
 
   def resultsRedirect: Action[AnyContent] =
     Action { implicit request =>
