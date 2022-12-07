83904a2875dc987f8842d047a9004783d6673265
Chris Jones <chrislomaxjones96@gmail.com>
2022-12-07 14:16:45 +0000

    Remove server-side test

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -12,7 +12,6 @@ object ActiveExperiments extends ExperimentsDefinition {
       TableOfContents,
       EuropeNetworkFront,
       DCRJavascriptBundle,
-      BillboardsInMerchSlots,
     )
   implicit val canCheckExperiment = new CanCheckExperiment(this)
 }
@@ -72,13 +71,4 @@ object EuropeNetworkFront
       owners = Seq(Owner.withGithub("rowannekabalan")),
       sellByDate = LocalDate.of(2023, 3, 1),
       participationGroup = Perc0D,
-    )
-
-object BillboardsInMerchSlots
-    extends Experiment(
-      name = "billboards-in-merch-slots",
-      description = "Test commercial impact of serving billboard sized ads in merchandising slots",
-      owners = Seq(Owner.withGithub("@guardian/commercial-dev")),
-      sellByDate = LocalDate.of(2023, 2, 1),
-      participationGroup = Perc0E,
     )
