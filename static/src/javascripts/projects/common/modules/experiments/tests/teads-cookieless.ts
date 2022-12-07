a3c239cb9545604543ae75b08ac0cdc392329331
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-12-07 14:32:56 +0000

    Add ab test

---
+++ static/src/javascripts/projects/common/modules/experiments/tests/teads-cookieless.ts
@@ -0,0 +1,19 @@
+import type { ABTest } from '@guardian/ab-core';
+import { bypassMetricsSampling } from '../utils';
+
+export const teadsCookieless: ABTest = {
+	id: 'TeadsCookieless',
+	start: '2022-12-07',
+	expiry: '2023-12-31',
+	author: 'Jake Lee Kennedy',
+	description: 'Test the impact of enabling the Teads cookieless tag',
+	audience: 1 / 100,
+	audienceOffset: 5 / 100,
+	audienceCriteria: 'Opt in',
+	successMeasure: 'No significant impact to UX',
+	canRun: () => true,
+	variants: [
+		{ id: 'control', test: () => bypassMetricsSampling },
+		{ id: 'variant', test: () => bypassMetricsSampling },
+	],
+};
