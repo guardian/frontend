3b71a3079dc6ffdda15a04934fbc7ee9ce6575c4
James Gorrie <jamesgorrie@users.noreply.github.com>
2022-12-07 11:12:47 +0000

    fix: bump OfferHttp3 experiement sellByDate to 2023

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -40,7 +40,7 @@ object OfferHttp3
       name = "offer-http3",
       description = "Offer HTTP3 by providing the header and redirecting URLs to enable loading of assets with HTTP3",
       owners = Seq(Owner.withGithub("paulmr")),
-      sellByDate = LocalDate.of(2022, 12, 6),
+      sellByDate = LocalDate.of(2023, 1, 10),
       participationGroup = Perc0B,
     )
 
