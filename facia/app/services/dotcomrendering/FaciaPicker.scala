61955b493f9a4ba005ff487a8f3011eedc8d10ef
Nicolas Long <nicolas.long@theguardian.com>
2022-12-09 18:00:00 +0000

    Revert "Merge pull request #25746 from guardian/fixed-thrasher-in-FaciaPicker"
    This reverts commit ea9986f1b17ac782bbf1fea9cfaad73a4dd93dc5, reversing
    changes made to 2d5be49843f063cec49f442fafe41b415aabf9ca.

--- facia/app/services/dotcomrendering/FaciaPicker.scala
+++ facia/app/services/dotcomrendering/FaciaPicker.scala
@@ -15,11 +15,6 @@ object FrontChecks {
   val SUPPORTED_COLLECTIONS: Set[String] =
     Set(
       /*
-    "fixed/thrasher",
-      pending https://github.com/guardian/dotcom-rendering/issues/5134
-       */
-
-      /*
     "dynamic/slow-mpu",
       pending https://github.com/guardian/dotcom-rendering/issues/5926
        */
