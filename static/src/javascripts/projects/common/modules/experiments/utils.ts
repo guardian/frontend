d445224a860f86ef8ecd2b455384134dcd2a45d5
Tom Wey <tom.wey@theguardian.com>
2022-12-09 15:57:11 +0000

    Upgrade @guardian/libs to v10.1.1
    This version introduces a new Ophan component event type which we'd like
    to start using.

--- static/src/javascripts/projects/common/modules/experiments/utils.ts
+++ static/src/javascripts/projects/common/modules/experiments/utils.ts
@@ -1,5 +1,5 @@
 import { bypassCommercialMetricsSampling } from '@guardian/commercial-core';
-import { bypassCoreWebVitalsSampling } from '@guardian/libs';
+import { bypassCoreWebVitalsSampling } from '@guardian/core-web-vitals';
 
 export const bypassMetricsSampling = (): void => {
 	void bypassCommercialMetricsSampling();
