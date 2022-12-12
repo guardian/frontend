5f5599d9c83849554342f04f95279edf733f1a8e
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-12-12 16:46:50 +0000

    Fixes for teads cookieless tag AB test and increase to 1% (#25770)

--- static/src/javascripts/projects/commercial/modules/teads-cookieless.ts
+++ static/src/javascripts/projects/commercial/modules/teads-cookieless.ts
@@ -3,7 +3,7 @@ import {
 	onConsent,
 	onConsentChange,
 } from '@guardian/consent-management-platform';
-import { loadScript, setCookie } from '@guardian/libs';
+import { getCookie, loadScript } from '@guardian/libs';
 import { isInVariantSynchronous } from 'common/modules/experiments/ab';
 import { teadsCookieless as teadsCookielessTest } from 'common/modules/experiments/tests/teads-cookieless';
 
@@ -12,10 +12,14 @@ const initTeadsCookieless = async (): Promise<void> => {
 
 	const hasConsent = getConsentFor('teads', consentState);
 
+	// Teads only runs on these content types, so lets not give them any more data than necessary
+	const allowedContentTypes = ['Article', 'LiveBlog'];
+
 	if (
 		hasConsent &&
 		window.guardian.config.switches.teadsCookieless &&
-		isInVariantSynchronous(teadsCookielessTest, 'variant')
+		isInVariantSynchronous(teadsCookielessTest, 'variant') &&
+		allowedContentTypes.includes(window.guardian.config.page.contentType)
 	) {
 		window.teads_analytics = window.teads_analytics ?? {};
 		window.teads_analytics.analytics_tag_id = 'PUB_2167';
@@ -35,9 +39,15 @@ const initTeadsCookieless = async (): Promise<void> => {
 
 onConsentChange((consentState) => {
 	const hasConsent = getConsentFor('teads', consentState);
-
-	if (!hasConsent) {
-		setCookie({ name: '_tfpvi', value: '' });
+	const teadsCookie = getCookie({ name: '_tfpvi' });
+	if (!hasConsent && teadsCookie) {
+		/*
+		Teads sets a cookie called _tfpvi, which is used to track users across sites.
+		We need to delete this cookie if the user has not consented to Teads.
+		We can't use the @guardian/libs setCookie function here, because it normalizes
+		the domain to theguardian.com but the cookie is set on www.theguardian.com
+		*/
+		document.cookie = '_tfpvi=;path=/';
 	}
 });
 
--- static/src/javascripts/projects/common/modules/experiments/tests/teads-cookieless.ts
+++ static/src/javascripts/projects/common/modules/experiments/tests/teads-cookieless.ts
@@ -4,10 +4,10 @@ import { bypassMetricsSampling } from '../utils';
 export const teadsCookieless: ABTest = {
 	id: 'TeadsCookieless',
 	start: '2022-12-07',
-	expiry: '2023-12-31',
+	expiry: '2022-12-31',
 	author: 'Jake Lee Kennedy',
 	description: 'Test the impact of enabling the Teads cookieless tag',
-	audience: 0,
+	audience: 1 / 100,
 	audienceOffset: 5 / 100,
 	audienceCriteria: 'Opt in',
 	successMeasure: 'No significant impact to UX',
