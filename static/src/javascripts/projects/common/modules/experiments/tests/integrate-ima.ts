38b705b35a3e559ed25a90c0dca21942ba264358
Ravi <7014230+arelra@users.noreply.github.com>
2022-12-20 17:12:29 +0000

    Extend integrate-ima ab test

--- static/src/javascripts/projects/common/modules/experiments/tests/integrate-ima.ts
+++ static/src/javascripts/projects/common/modules/experiments/tests/integrate-ima.ts
@@ -4,7 +4,7 @@ import { noop } from '../../../../../lib/noop';
 export const integrateIma: ABTest = {
 	id: 'IntegrateIma',
 	start: '2022-07-14',
-	expiry: '2022-12-30',
+	expiry: '2023-04-04',
 	author: 'Zeke Hunter-Green',
 	description:
 		'Test the commercial impact of replacing YouTube ads with Interactive Media Ads on first-party videos',
