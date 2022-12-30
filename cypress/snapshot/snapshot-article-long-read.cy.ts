062e95157562351b42ddf24484e3a96ce022854e
Ravi <7014230+arelra@users.noreply.github.com>
2022-12-30 17:00:57 +0000

    New snapshot article - long read

---
+++ cypress/snapshot/snapshot-article-long-read.cy.ts
@@ -0,0 +1,24 @@
+import { getStage, getTestUrl } from '../lib/util';
+import { stubApiRequests } from '../lib/stubApiRequests';
+import '@percy/cypress';
+
+describe('Visually snapshot long read article', () => {
+	it(`snapshots long read article`, () => {
+		const path = getTestUrl(
+			getStage(),
+			'/business/2022/apr/14/a-day-in-the-life-of-almost-every-vending-machine-in-the-world',
+			{ isDcr: true },
+		);
+		// stub all api requests
+		stubApiRequests();
+		// load article
+		cy.visit(path);
+		cy.allowAllConsent();
+		// scroll to and hydrate all islands
+		cy.hydrate();
+		// snapshot
+		cy.percySnapshot('article-long-read', {
+			widths: [320, 740, 1300],
+		});
+	});
+});
