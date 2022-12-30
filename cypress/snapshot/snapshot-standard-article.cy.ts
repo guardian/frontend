8107f4ecc74cf8f9d551b19eeb4be7604476d784
Ravi <7014230+arelra@users.noreply.github.com>
2022-12-30 11:29:20 +0000

    Move overrideGamRequest to lib

--- cypress/snapshot/snapshot-standard-article.cy.ts
+++ cypress/snapshot/snapshot-standard-article.cy.ts
@@ -25,64 +25,11 @@ const stubApiRequests = () => {
 	);
 };
 
-const overrideGamRequest = (
-	matchRequestParam: string,
-	rewriteUrl: (reqUrl: string) => string,
-) =>
-	cy.intercept(
-		'https://securepubads.g.doubleclick.net/gampad/ads**',
-		(req) => {
-			if (req.url.includes(matchRequestParam)) {
-				req.url = rewriteUrl(req.url);
-			}
-		},
-	);
-
-/**
- * GAM intermitently renders a 320x250 MPU ad in the top-above-nav slot when in a Cypress test
- * For now we hardcode size targeting on the GAM request while we investigate
- */
-const rewriteGamRequestForTopAboveNav = (reqUrl: string) => {
-	console.log('URL before rewrite:', reqUrl);
-	const iframeDimensionParams = new RegExp(/&isw=([0-9]+)&ish=([0-9]+)/).exec(
-		reqUrl,
-	);
-	const browserDimensionParams = new RegExp(
-		/&biw=([0-9]+)&bih=([0-9]+)/,
-	).exec(reqUrl);
-	const psz = new RegExp(/&psz=([0-9]+x[0-9]+)/).exec(reqUrl);
-	const prevIuSzs = new RegExp(/&prev_iu_szs=(.*?)&/).exec(reqUrl);
-
-	let newReqUrl = reqUrl;
-	if (iframeDimensionParams && browserDimensionParams && psz && prevIuSzs) {
-		console.log('iframeDimensionParams:', iframeDimensionParams);
-		console.log('browserDimensionParams:', browserDimensionParams);
-		console.log('psz:', psz);
-		console.log('preVszs:', prevIuSzs);
-		newReqUrl = newReqUrl.replace(iframeDimensionParams[0], '');
-		newReqUrl = newReqUrl.replace(
-			browserDimensionParams[0],
-			'&biw=1000&bih=660',
-		);
-		newReqUrl = newReqUrl.replace(psz[0], '&psz=1000x293');
-		newReqUrl = newReqUrl.replace(
-			prevIuSzs[1],
-			encodeURIComponent('728x90|940x230|900x250|970x250'),
-		);
-	}
-	console.log('URL after rewrite:', newReqUrl);
-	return newReqUrl;
-};
-
 describe('Visually snapshot standard article', () => {
 	[articles[0]].forEach(({ path }) => {
 		it(`snapshots ${path}`, () => {
 			// stub all api requests
 			stubApiRequests();
-			overrideGamRequest(
-				encodeURIComponent('slot=top-above-nav'),
-				(reqUrl) => rewriteGamRequestForTopAboveNav(reqUrl),
-			);
 			// load article
 			cy.visit(path);
 			cy.allowAllConsent();
