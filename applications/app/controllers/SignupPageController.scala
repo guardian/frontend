846e526096c7fbd332463a32a438334a6a74b7f8
David Blatcher <dv.bltchr@gmail.com>
2023-01-10 17:06:13 +0000

    throw an error if the path requests dcr=true

--- applications/app/controllers/SignupPageController.scala
+++ applications/app/controllers/SignupPageController.scala
@@ -5,7 +5,7 @@ import model.{ApplicationContext, Cached, NoCache}
 import model.Cached.RevalidatableResult
 import pages.NewsletterHtmlPage
 import play.api.libs.ws.WSClient
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, RequestHeader, Result}
 import play.filters.csrf.CSRFAddToken
 import services.newsletters.GroupedNewslettersResponse.GroupedNewslettersResponse
 import services.newsletters.NewsletterSignupAgent
@@ -25,22 +25,46 @@ class SignupPageController(
 
   val defaultCacheDuration: Duration = 15.minutes
 
+  def getShouldUseRemoteRender()(implicit
+      request: RequestHeader,
+  ): Boolean = {
+    request.getQueryString("dcr").contains("true")
+  }
+
+  def localRenderNewslettersPage()(implicit
+      request: RequestHeader,
+  ): Result = {
+    val groupedNewsletters: Either[String, GroupedNewslettersResponse] =
+      newsletterSignupAgent.getGroupedNewsletters()
+    groupedNewsletters match {
+      case Right(groupedNewsletters) =>
+        Cached(defaultCacheDuration)(
+          RevalidatableResult.Ok(
+            NewsletterHtmlPage.html(StaticPages.simpleNewslettersPage(request.path, groupedNewsletters)),
+          ),
+        )
+      case Left(e) =>
+        log.error(s"API call to get newsletters failed: $e")
+        NoCache(InternalServerError)
+    }
+  }
+
+  def remoteRenderNewslettersPage()(implicit
+      request: RequestHeader,
+  ): Result = {
+
+    NoCache(InternalServerError)
+  }
+
   def renderNewslettersPage(): Action[AnyContent] =
     csrfAddToken {
       Action { implicit request =>
-        val groupedNewsletters: Either[String, GroupedNewslettersResponse] =
-          newsletterSignupAgent.getGroupedNewsletters()
+        val renderDcr: Boolean = getShouldUseRemoteRender()
 
-        groupedNewsletters match {
-          case Right(groupedNewsletters) =>
-            Cached(defaultCacheDuration)(
-              RevalidatableResult.Ok(
-                NewsletterHtmlPage.html(StaticPages.simpleNewslettersPage(request.path, groupedNewsletters)),
-              ),
-            )
-          case Left(e) =>
-            log.error(s"API call to get newsletters failed: $e")
-            NoCache(InternalServerError)
+        if (renderDcr) {
+          remoteRenderNewslettersPage()
+        } else {
+          localRenderNewslettersPage()
         }
       }
     }
