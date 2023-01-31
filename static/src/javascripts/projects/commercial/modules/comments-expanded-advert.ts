c375b44e59fcfb0fbef75a4d1989e80bcd9ce09a
Dominik Lander <dominik.lander@guardian.co.uk>
2023-01-31 10:14:48 +0000

    Refactored comments-expanded script to insert ad

--- static/src/javascripts/projects/commercial/modules/comments-expanded-advert.ts
+++ static/src/javascripts/projects/commercial/modules/comments-expanded-advert.ts
@@ -8,9 +8,8 @@ import { addSlot } from './dfp/add-slot';
 const tallestCommentAd = adSizes.mpu.height + AD_LABEL_HEIGHT;
 const tallestCommentsExpandedAd = adSizes.halfPage.height + AD_LABEL_HEIGHT;
 
-const insertAd = (): Promise<void> => {
-	const anchor = document.querySelector('.commentsRightColumn');
-	if (!anchor) return Promise.resolve();
+const insertAd = (anchor: HTMLElement): Promise<void> => {
+	log('commercial', 'Inserting comments-expanded advert');
 
 	const slot = createAdSlot('comments-expanded', {
 		name: 'comments-expanded',
@@ -38,45 +37,75 @@ const insertAd = (): Promise<void> => {
 		});
 };
 
+const getRightColumn = async (): Promise<HTMLElement> => {
+	return fastdom.measure(() => {
+		const rightColumn: HTMLElement | null = document.querySelector(
+			'.commentsRightColumn',
+		);
+
+		if (!rightColumn) throw new Error('Could not find right column.');
+
+		return rightColumn;
+	});
+};
+
+const isEnoughSpaceForAd = (rightColumnNode: HTMLElement): boolean => {
+	// Only insert a second advert into the right-hand rail if there is enough space.
+	// There is enough space if the right-hand rail is larger than:
+	// (the largest possible heights of both adverts) + (the gap between the two adverts)
+	const minHeightToPlaceAd =
+		tallestCommentAd + tallestCommentsExpandedAd + window.innerHeight;
+
+	return rightColumnNode.offsetHeight >= minHeightToPlaceAd;
+};
+
+const createResizeObserver = (rightColumnNode: HTMLElement) => {
+	// When the comments load and are rendered, the height of the right column
+	// will expand and there might be enough space to insert the ad.
+	const resizeObserver = new ResizeObserver(() => {
+		if (isEnoughSpaceForAd(rightColumnNode)) {
+			void insertAd(rightColumnNode);
+
+			log('commercial', 'Removing resize observer');
+			resizeObserver.unobserve(rightColumnNode);
+		}
+	});
+
+	log('commercial', 'Creating resize observer');
+	resizeObserver.observe(rightColumnNode);
+};
+
+/**
+ * Create a comments-expanded ad immediately if there is enough space for it. If not, then it
+ * is possible that we are still waiting for the Discussion API to load the comments, so we
+ * wait for the comments to load before checking again whether there is enough space to load an ad.
+ */
+const handleCommentsExpandedEvent = async (): Promise<void> => {
+	if (!commercialFeatures.commentAdverts) {
+		log(
+			'commercial',
+			'Adverts in comments are disabled in commercialFeatures',
+		);
+		return;
+	}
+
+	const rightColumnNode = await getRightColumn();
+
+	if (isEnoughSpaceForAd(rightColumnNode)) {
+		void insertAd(rightColumnNode);
+		return;
+	}
+
+	// Watch the right column and insert an ad when the comments are loaded.
+	createResizeObserver(rightColumnNode);
+
+	return;
+};
+
 export const initCommentsExpandedAdverts = (): Promise<void> => {
 	document.addEventListener('comments-expanded', () => {
 		log('commercial', 'comments expanded event received');
-
-		if (!commercialFeatures.commentAdverts) {
-			log(
-				'commercial',
-				'Adverts in comments are disabled in commercialFeatures',
-			);
-			return;
-		}
-
-		// Do not insert an ad if there is not enough space for one.
-		// This will likely occur if there are only a few comments.
-		// TODO Use Mutation Observer
-		void fastdom.measure(() => {
-			const rightColumn = document.querySelector('.commentsRightColumn');
-			if (!rightColumn) return;
-
-			const rightColumnHeight = (rightColumn as HTMLElement).offsetHeight;
-
-			// Only insert a second advert into the right-hand rail if there is enough space.
-			// There is enough space if the right-hand rail is larger than:
-			// (the largest possible heights of both adverts) + (the gap between the two adverts)
-			const minHeightToPlaceAd =
-				tallestCommentAd +
-				tallestCommentsExpandedAd +
-				window.innerHeight;
-
-			if (rightColumnHeight < minHeightToPlaceAd) {
-				log(
-					'commercial',
-					'Not enough space to insert comments-expanded advert',
-				);
-				return;
-			}
-
-			void insertAd();
-		});
+		void handleCommentsExpandedEvent();
 	});
 
 	return Promise.resolve();
