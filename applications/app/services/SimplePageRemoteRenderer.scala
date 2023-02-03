0bfb40014d6c8bf4e6afaaf1ba0a1b8059dc8432
David Blatcher <dv.bltchr@gmail.com>
2023-02-03 10:40:33 +0000

    add urls from config

--- applications/app/services/SimplePageRemoteRenderer.scala
+++ applications/app/services/SimplePageRemoteRenderer.scala
@@ -5,7 +5,8 @@ import play.api.mvc.{Action, RequestHeader, Result}
 import play.api.libs.ws.{WSClient, WSResponse}
 import play.api.libs.json.{JsValue, Json, JsObject}
 import model.{ApplicationContext, DotcomContentType, Cached, NoCache, MetaData, SectionId, SimplePage}
-import common.{CanonicalLink, Chronos, Edition, Localisation, RichRequestHeader}
+import common.{CanonicalLink, Edition}
+import conf.Configuration
 import services.newsletters.model.NewsletterResponse
 import services.NewsletterData
 
@@ -48,6 +49,9 @@ object SimplePageRemoteRenderer {
       "id" -> page.metadata.id,
       "editionId" -> edition.id,
       "editionLongForm" -> Edition(request).displayName,
+      "beaconURL" -> Configuration.debug.beaconUrl,
+      "subscribeUrl" -> Configuration.id.subscribeUrl,
+      "contributionsServiceUrl" -> Configuration.contributionsService.url,
       "webTitle" -> page.metadata.webTitle,
       "description" -> page.metadata.description,
       "config" -> page.getJavascriptConfig,
