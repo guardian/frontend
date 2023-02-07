e2d074c7ab6a91ef4f139809789cdd90347f08da
Pete F <pete.faulconbridge@theguardian.com>
2023-02-07 10:56:09 +0000

    Remove redundant check
    The removed code was checking whether the current date was either side
    of two other dates, but both of those dates are now in the past so it
    always returns true and can be removed.

--- common/app/conf/audio/FlagshipContainer.scala
+++ common/app/conf/audio/FlagshipContainer.scala
@@ -8,14 +8,10 @@ trait FlagshipContainer {
 
   val londonTimezone = ZoneId.of("Europe/London")
 
-  // Aug 17 at 3am
-  private val tifOnHolsStart = ZonedDateTime.of(2019, 8, 17, 3, 15, 0, 0, londonTimezone)
-  // Sep 2 at 2am
-  private val tifOnHolsEnd = ZonedDateTime.of(2019, 9, 2, 3, 15, 0, 0, londonTimezone)
-
   //The container should appear at 03:15 on Monday, and disappear at 03:15 on Saturday
   private val threeHoursFifteenMinutes = Duration.ofHours(3) plus Duration.ofMinutes(15)
   private val weekend = Set(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
+
   private def isWeekend(dateTime: ZonedDateTime): Boolean =
     weekend(dateTime.minus(threeHoursFifteenMinutes).getDayOfWeek())
 
@@ -26,7 +22,5 @@ trait FlagshipContainer {
   def isFlagshipContainer(id: String): Boolean = containerIds.contains(id)
 
   def displayFlagshipContainer(now: ZonedDateTime = ZonedDateTime.now(londonTimezone)): Boolean =
-    switch.isSwitchedOn &&
-      (now.isBefore(tifOnHolsStart) || now.isAfter(tifOnHolsEnd)) &&
-      !isWeekend(now)
+    switch.isSwitchedOn && !isWeekend(now)
 }
