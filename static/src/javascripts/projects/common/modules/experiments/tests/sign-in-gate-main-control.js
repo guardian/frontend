a29bf3d6e0a03a54fc0d0d4df1a1a99f6f9750a1
Lindsey Dew <lindsey.megan.dew@gmail.com>
2023-02-08 15:14:33 +0000

    Update the audience segmentation in main

--- static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-main-control.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-main-control.js
@@ -8,8 +8,8 @@ export const signInGateMainControl = {
 	author: 'Mahesh Makani',
 	description:
 		'Show sign in gate to 100% of users on 3rd article view of simple article templates, with higher priority over banners and epic. Control Audience.',
-	audience: 0.1,
-	audienceOffset: 0.9,
+	audience: 0.2,
+	audienceOffset: 0.7,
 	successMeasure: 'N/A - User does not see gate, only to compare to variant.',
 	audienceCriteria:
 		'3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
