21ed98598ce610591c74ce18dc45f620c5d861c6
Lindsey Dew <lindsey.megan.dew@gmail.com>
2023-02-08 14:15:44 +0000

    Update audience offset and outcome

--- static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-copy-test-variant.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-copy-test-variant.js
@@ -4,13 +4,13 @@ export const signInGateCopyTestJan2023 = {
 	expiry: '2023-05-01',
 	author: 'Lindsey Dew',
 	description: 'Test varying the copy in the call to action for sign in gate',
-	audience: 0.1,
+	audience: 0.2,
 	audienceOffset: 0.0,
 	successMeasure: 'Users sign in or create a Guardian account',
 	audienceCriteria: '3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
 	dataLinkNames: 'SignInGateCopyTest',
 	idealOutcome:
-		'Increase the number of users signed in whilst running at a reasonable scale',
+		'One variants performs at least 2% better than the control and/OR 10% better than the other variant. Neither variant performs 5% worse than the control',
 	showForSensitive: false,
 	canRun: () => true,
 	variants: [
