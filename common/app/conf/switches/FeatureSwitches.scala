aac9a4774fc2ed505d0053ab25b6583a2b38bd6d
Lindsey Dew <lindsey.megan.dew@gmail.com>
2023-02-23 11:42:41 +0000

    Rename the switch name

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -512,9 +512,9 @@ trait FeatureSwitches {
     sellByDate = never,
     exposeClientSide = true,
   )
-  val PersonaliseSignInAfterCheckoutGate = Switch(
+  val PersonaliseSignInGateAfterCheckout = Switch(
     SwitchGroup.Feature,
-    "personalise-sign-in-after-checkout-gate",
+    "personalise-sign-in-gate-after-checkout",
     "When ON, the sign in gate shows a personalised message to subscribers and supporters",
     owners = Seq(Owner.withEmail("personalisation@guardian.co.uk")),
     safeState = On,
