c0667aaaea37e23c3ead6369f12958d900bce577
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2023-04-04 13:53:24 +0100

    eager-prebid ab test 5 -> 6%

--- static/src/javascripts/projects/common/modules/experiments/tests/eager-prebid.ts
+++ static/src/javascripts/projects/common/modules/experiments/tests/eager-prebid.ts
@@ -6,7 +6,7 @@ export const eagerPrebid: ABTest = {
 	author: '@commercial-dev',
 	start: '2023-03-23',
 	expiry: '2023-05-01',
-	audience: 5 / 100,
+	audience: 6 / 100,
 	audienceOffset: 0 / 100,
 	audienceCriteria: 'All pageviews',
 	successMeasure:
