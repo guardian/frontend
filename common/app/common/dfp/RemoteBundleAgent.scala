a633f7b5bf1afaf90c5f0cffbe8ab5f163f487d2
Chris Jones <chrislomaxjones96@gmail.com>
2023-04-14 14:49:38 +0100

    Try to avoid retrieving from param store in tests

--- common/app/common/dfp/RemoteBundleAgent.scala
+++ common/app/common/dfp/RemoteBundleAgent.scala
@@ -15,34 +15,39 @@ import services.ParameterStore
 import scala.concurrent.{ExecutionContext, Future}
 
 object RemoteBundleAgent {
-  // Retrieve the name of the static assets bucket from Parameter Store
-  private lazy val parameterStore = new ParameterStore(Configuration.aws.region)
+  private val commercialPath = "test_commercial_bundles"
 
-  private val bucket = parameterStore.get("/account/services/dotcom-static.bucket")
-
-  // TODO can we compute this URL?
-  private val assetMapKey = "CODE/frontend-static/test_commercial_bundles/assets.map"
+  private val assetMapKey = s"CODE/frontend-static/${commercialPath}/assets.map"
 
   private val commmercialBundleEntrypoint = "commercial-standalone.js"
 
   private lazy val remoteBundleAgent = Box[Option[String]](None)
 
-  def jsonToAssetMap(json: String): Option[Map[String, String]] =
+  private def jsonToAssetMap(json: String): Option[Map[String, String]] =
     Json.parse(json).validate[Map[String, String]] match {
       case JsSuccess(m, _) => Some(m)
       case JsError(_)      => None
     }
 
+  private def fullUrl(url: String): String = {
+    // TODO what about PROD and dev?
+    s"https://assets-code.guim.co.uk/${commercialPath}/${url}"
+  }
+
   private def grabRemoteBundleUrlFromStore(): Option[String] = {
+    // Retrieve the name of the static assets bucket from Parameter Store
+    val parameterStore = new ParameterStore(Configuration.aws.region)
+    val bucket = parameterStore.get("/account/services/dotcom-static.bucket")
+
     val commercialBundleUrl: Option[String] =
       S3.get(assetMapKey, bucket)(UTF8)
         .flatMap(jsonToAssetMap)
         .flatMap(assetMap => assetMap.get(commmercialBundleEntrypoint))
-        // TODO can we compute this URL?
-        .map(url => "https://assets-code.guim.co.uk/test_commercial_bundles/" + url)
+        .map(fullUrl)
 
     // TODO Remove this later
-    println(s"Grabbing remote bundle URL from S3. Found: ${commercialBundleUrl.getOrElse("nothing")}")
+    println(s"Grabbing remote bundle URL from S3. Found: ${commercialBundleUrl
+      .getOrElse("nothing")}\n ${assetMapKey} ${bucket} ${S3.get(assetMapKey, bucket)(UTF8)}")
 
     commercialBundleUrl
   }
