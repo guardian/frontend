5436a40c847a999af79894515c1851c1ce80a5c3
Ravi <7014230+arelra@users.noreply.github.com>
2023-04-18 23:17:10 +0100

    Format check-node-env.js

--- tools/check-node-env.js
+++ tools/check-node-env.js
@@ -5,58 +5,60 @@ const path = require('path');
 const childProcess = require('child_process');
 
 const checkVersion = (target, semver) =>
-new Promise((resolve) => {
-        // https://www.npmjs.com/package/check-node-version
-        childProcess.spawn('npx', ['-y', 'check-node-version', `--${target}`, `${semver}`], {
-            stdio: 'inherit',
-        })
-        .on('close', code => {
-            // quit the process if the version check fails
-            // check-node-version will output an error message
-            if (code !== 0) process.exit(code);
-            resolve(true);
-        })
-        .on('error', () => {
-            process.exit(code);
-        });
-    });
+	new Promise((resolve) => {
+		// https://www.npmjs.com/package/check-node-version
+		childProcess
+			.spawn(
+				'npx',
+				['-y', 'check-node-version', `--${target}`, `${semver}`],
+				{
+					stdio: 'inherit',
+				},
+			)
+			.on('close', (code) => {
+				// quit the process if the version check fails
+				// check-node-version will output an error message
+				if (code !== 0) process.exit(code);
+				resolve(true);
+			})
+			.on('error', () => {
+				process.exit(code);
+			});
+	});
 
-const logSuccess = (msg) =>
-    console.log(`\x1b[32m✔\x1b[0m ${msg}`);
+const logSuccess = (msg) => console.log(`\x1b[32m✔\x1b[0m ${msg}`);
 
-const logFail = (msg) =>
-    console.log(`\x1b[31m✗\x1b[0m ${msg}`);
+const logFail = (msg) => console.log(`\x1b[31m✗\x1b[0m ${msg}`);
 
 const checkNodeEnv = async () => {
+	// check node
+	const nvmrcVersion = fs
+		.readFileSync(path.join(__dirname, '../', '.nvmrc'), 'utf8')
+		.trim();
+	await checkVersion('node', nvmrcVersion);
+	logSuccess(`Node ${nvmrcVersion}`);
 
-    // check node
-    const nvmrcVersion = fs
-        .readFileSync(path.join(__dirname, '../', '.nvmrc'), 'utf8')
-        .trim();
-    await checkVersion('node', nvmrcVersion);
-    logSuccess(`Node ${nvmrcVersion}`);
+	// check yarn
+	childProcess.exec('yarn --version', async (e, version) => {
+		const foundYarnVersion = version.trim();
+		const enginesYarnVersion = require('../package.json').engines.yarn;
 
-    // check yarn
-    childProcess.exec('yarn --version', async (e, version) => {
-        const foundYarnVersion = version.trim();
-        const enginesYarnVersion = require('../package.json').engines.yarn;
-
-        if (foundYarnVersion) {
-            await checkVersion('yarn', enginesYarnVersion);
-            logSuccess(`Yarn ${foundYarnVersion}`);
-        } else {
-            // else install yarn with npm (mainly for TeamCity)
-            logFail(`Yarn not found!`);
-            console.log(`Installing yarn ${enginesYarnVersion} via npm`);
-            childProcess
-                .spawn('npm', ['i', '-g', `yarn@${enginesYarnVersion}`], {
-                    stdio: 'inherit',
-                })
-                .on('close', code => {
-                    if (code !== 0) process.exit(code);
-                });
-        }
-    });
+		if (foundYarnVersion) {
+			await checkVersion('yarn', enginesYarnVersion);
+			logSuccess(`Yarn ${foundYarnVersion}`);
+		} else {
+			// else install yarn with npm (mainly for TeamCity)
+			logFail(`Yarn not found!`);
+			console.log(`Installing yarn ${enginesYarnVersion} via npm`);
+			childProcess
+				.spawn('npm', ['i', '-g', `yarn@${enginesYarnVersion}`], {
+					stdio: 'inherit',
+				})
+				.on('close', (code) => {
+					if (code !== 0) process.exit(code);
+				});
+		}
+	});
 };
 
 checkNodeEnv();
