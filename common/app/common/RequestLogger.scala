7ab8d00dc0eda3a591d75254aa9b46dac63a39c3
Roberto Tyley <roberto.tyley@gmail.com>
2023-04-21 17:40:58 +0100

    Make request a required parameter of RequestLogger
    It looks like this field was only optional to make writing tests
    slightly easier, but in actual use, a request *is* always present, and
    the implementation of `RequestLoggerFields` was being unnecessarily
    complicated by having to deal with the `Option`.
    
    The tests can cope with having to supply a little FakeRequest, it does
    them no harm.
    
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>

--- common/app/common/RequestLogger.scala
+++ common/app/common/RequestLogger.scala
@@ -7,7 +7,7 @@ import play.api.routing.Router
 
 import scala.util.Random
 
-case class RequestLoggerFields(request: Option[RequestHeader], response: Option[Result], stopWatch: Option[StopWatch]) {
+case class RequestLoggerFields(request: RequestHeader, response: Option[Result], stopWatch: Option[StopWatch]) {
 
   private lazy val requestHeadersFields: List[LogField] = {
     val allowListedHeaderNames = Set(
@@ -27,7 +27,7 @@ case class RequestLoggerFields(request: Option[RequestHeader], response: Option[
       "Accept-Encoding", // TODO remove if seen after 2021/09/03
     )
 
-    val allHeadersFields = request.map(_.headers.toMap.mapV(_.mkString(","))).getOrElse(Map.empty)
+    val allHeadersFields = request.headers.toMap.mapV(_.mkString(","))
 
     val allowListedHeaders = (for {
       headerName <- allowListedHeaderNames
@@ -42,15 +42,11 @@ case class RequestLoggerFields(request: Option[RequestHeader], response: Option[
     }
   }
   private lazy val customFields: List[LogField] = {
-    val requestHeaders: List[LogField] = request
-      .map { r: RequestHeader =>
-        List[LogField](
-          "req.method" -> r.method,
-          "req.url" -> r.uri,
-          "req.id" -> Random.nextInt(Integer.MAX_VALUE).toString,
-        )
-      }
-      .getOrElse(Nil)
+    val requestHeaders: List[LogField] = List[LogField](
+      "req.method" -> request.method,
+      "req.url" -> request.uri,
+      "req.id" -> Random.nextInt(Integer.MAX_VALUE).toString,
+    )
 
     val responseHeaders: List[LogField] = response
       .map { r: Result =>
@@ -70,15 +66,13 @@ case class RequestLoggerFields(request: Option[RequestHeader], response: Option[
       }
       .getOrElse(Nil)
 
-    val actionInfo: List[LogField] = request
-      .map { r: RequestHeader =>
-        val handlerDefOpt = r.attrs.get(Router.Attrs.HandlerDef)
-        List[LogField](
-          "action.controller" -> handlerDefOpt.map(_.controller).getOrElse("unknown"),
-          "action.method" -> handlerDefOpt.map(_.method).getOrElse("unknown"),
-        )
-      }
-      .getOrElse(Nil)
+    val actionInfo: List[LogField] = {
+      val handlerDefOpt = request.attrs.get(Router.Attrs.HandlerDef)
+      List[LogField](
+        "action.controller" -> handlerDefOpt.map(_.controller).getOrElse("unknown"),
+        "action.method" -> handlerDefOpt.map(_.method).getOrElse("unknown"),
+      )
+    }
 
     requestHeaders ++ responseHeaders ++ stopWatchHeaders ++ actionInfo
   }
@@ -86,21 +80,13 @@ case class RequestLoggerFields(request: Option[RequestHeader], response: Option[
   def toList: List[LogField] = customFields ++ requestHeadersFields
 }
 
-case class RequestLogger(request: Option[RequestHeader], response: Option[Result], stopWatch: Option[StopWatch])
+case class RequestLogger(request: RequestHeader, response: Option[Result], stopWatch: Option[StopWatch])
     extends GuLogging {
 
   private def allFields: List[LogField] = RequestLoggerFields(request, response, stopWatch).toList
 
-  def withRequestHeaders(rh: RequestHeader): RequestLogger = {
-    copy(Some(rh), response, stopWatch)
-  }
-
   def withResponse(resp: Result): RequestLogger = {
     copy(request, Some(resp), stopWatch)
-  }
-
-  def withStopWatch(sw: StopWatch): RequestLogger = {
-    copy(request, response, Some(sw))
   }
 
   def info(message: String): Unit = {
@@ -112,8 +98,4 @@ case class RequestLogger(request: Option[RequestHeader], response: Option[Result
   def error(message: String, error: Throwable): Unit = {
     logErrorWithCustomFields(message, error, allFields)
   }
-}
-
-object RequestLogger {
-  def apply(): RequestLogger = RequestLogger(None, None, None)
 }
