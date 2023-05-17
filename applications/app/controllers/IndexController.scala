071ca7954b6098467f6ca3bc40ac685057ebc674
Olly <9575458+OllysCoding@users.noreply.github.com>
2023-05-17 10:39:07 +0100

    Create initial datamodel for DCR index pages

--- applications/app/controllers/IndexController.scala
+++ applications/app/controllers/IndexController.scala
@@ -4,6 +4,7 @@ import common._
 import contentapi.{ContentApiClient, SectionsLookUp}
 import model.Cached.RevalidatableResult
 import model._
+import model.dotcomrendering.{DotcomIndexPageRenderingDataModel, PageType}
 import pages.IndexHtmlPage
 import play.api.mvc.{ControllerComponents, RequestHeader, Result}
 import services.IndexPage
@@ -20,7 +21,17 @@ class IndexController(
         val body = TrailsToRss(model.page.metadata, model.trails)
         RevalidatableResult(Ok(body).as("text/xml; charset=utf-8"), body)
       } else if (request.isJson) {
-        JsonComponent(views.html.fragments.indexBody(model))
+        if (request.forceDCR) {
+          JsonComponent.fromWritable(
+            DotcomIndexPageRenderingDataModel(
+              page = model,
+              request = request,
+              pageType = PageType(model, request, context),
+            ),
+          )
+        } else {
+          JsonComponent(views.html.fragments.indexBody(model))
+        }
       } else {
         RevalidatableResult.Ok(IndexHtmlPage.html(model))
       }
