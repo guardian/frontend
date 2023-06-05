59d3ef8e92917e4c561cfba55ba73ac2f7ce81a4
Ravi <7014230+arelra@users.noreply.github.com>
2023-06-05 10:10:27 +0100

    Extend sign-in-gate-copy-test-jan-2023

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -81,7 +81,7 @@ trait ABTestSwitches {
     "Test the impact of changing the copy in the sign in gate",
     owners = Seq(Owner.withEmail("personalisation@guardian.co.uk")),
     safeState = Off,
-    sellByDate = Some(LocalDate.of(2023, 6, 5)),
+    sellByDate = Some(LocalDate.of(2023, 7, 3)),
     exposeClientSide = true,
   )
 
