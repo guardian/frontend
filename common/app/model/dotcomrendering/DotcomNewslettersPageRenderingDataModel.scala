d27d713ea79325cdef0710ad5c4d03f699590acf
David Blatcher <dv.bltchr@gmail.com>
2023-07-06 10:05:01 +0100

    add illustrationCard to NewsletterData case class

--- common/app/model/dotcomrendering/DotcomNewslettersPageRenderingDataModel.scala
+++ common/app/model/dotcomrendering/DotcomNewslettersPageRenderingDataModel.scala
@@ -101,6 +101,15 @@ object DotcomNewslettersPageRenderingDataModel {
     Json.stringify(DotcomRenderingUtils.withoutNull(jsValue))
   }
 
+  private def getIllustration(identityName: String): Option[String] = {
+    if (identityName == "afternoon-update") {
+      return Option.apply(
+        "https://i.guim.co.uk/img/media/50b02d4e4a32def95d8d26cc852549e6bd83f037/0_0_1850_1111/500.jpg?width=250&quality=45&s=6325bb62e51f4af4d43e8bafb28b3e6d",
+      )
+    }
+    Option.empty[String]
+  }
+
   private def convertNewsletterResponseToData(response: NewsletterResponse): NewsletterData = {
     NewsletterData(
       response.identityName,
@@ -112,6 +121,7 @@ object DotcomNewslettersPageRenderingDataModel {
       response.group,
       response.emailEmbed.successDescription,
       response.regionFocus,
+      getIllustration(response.identityName),
     )
   }
 }
