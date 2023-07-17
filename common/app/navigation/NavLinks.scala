aa3e6347bc50e188d035231ec0f9d826808d85ff
Charlotte <43961396+cemms1@users.noreply.github.com>
2023-07-17 11:46:16 +0100

    remove 2023 text from women's world cup subnav item

--- common/app/navigation/NavLinks.scala
+++ common/app/navigation/NavLinks.scala
@@ -107,7 +107,7 @@ object NavLinks {
     "Football",
     "/football",
     children = List(
-      NavLink("Women's World Cup 2023", "/football/womens-world-cup-2023", Some("football/womens-world-cup-2023")),
+      NavLink("Women's World Cup", "/football/womens-world-cup-2023", Some("football/womens-world-cup-2023")),
       NavLink("Live scores", "/football/live", Some("football/live")),
       NavLink("Tables", "/football/tables", Some("football/tables")),
       NavLink("Fixtures", "/football/fixtures", Some("football/fixtures")),
