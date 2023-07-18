c87515b087074e106190e74435d23379648949b4
George B <705427+georgeblahblah@users.noreply.github.com>
2023-07-18 09:44:54 +0100

    Return `404` for requests to render Formstack forms
    We are ending support for submitting Formstack forms as an authenticated
    user in the Identity application. This functionality was introduced in
    e329b290bfe2ac03cfd6e43710f041659221d4c9.

--- identity/app/controllers/FormstackController.scala
+++ identity/app/controllers/FormstackController.scala
@@ -23,41 +23,14 @@ class FormstackController(
     with ImplicitControllerExecutionContext
     with SafeLogging {
 
-  import authenticatedActions.fullAuthAction
-
   val page = IdentityPage("/form", "Form")
 
   def formstackFormEmbed(formReference: String): Action[AnyContent] =
     formstackForm(formReference, true)
 
   def formstackForm(formReference: String, composer: Boolean): Action[AnyContent] =
-    fullAuthAction.async { implicit request =>
-      if (Switches.IdentityFormstackSwitch.isSwitchedOn) {
-        FormstackForm
-          .extractFromSlug(formReference)
-          .map { formstackForm =>
-            formStackApi.checkForm(formstackForm).map {
-              case Right(_) => {
-                logger.trace(s"Rendering formstack form ${formstackForm.formId}")
-                if (composer) {
-                  Ok(views.html.formstack.formstackFormComposer(page, formstackForm))
-                } else {
-                  Ok(views.html.formstack.formstackForm(page, formstackForm))
-                }
-              }
-              case Left(errors) => {
-                logger.warn(s"Unable to render formstack form ${formstackForm.formReference}, $errors")
-                new Status(errors.map(_.statusCode).max)(views.html.formstack.formstackFormNotFound(page))
-              }
-            }
-          }
-          .getOrElse {
-            Future.successful(NotFound(views.html.formstack.formstackFormNotFound(page)))
-          }
-      } else {
-        logger.info(s"formstack switched off, attempt to access $formReference failed")
-        Future.successful(NotFound(views.html.errors._404()))
-      }
+    Action { implicit request =>
+      NotFound(views.html.formstack.formstackFormNotFound(page))
     }
 
   def complete: Action[AnyContent] =
