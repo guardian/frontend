227fa6bb919ead1420dc2d28a3acd28d1cf52851
Sophie <sophiemacmillan93@gmail.com>
2023-08-01 09:24:41 +0100

    Extend OfferHttp3 experiment

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -90,6 +90,6 @@ object OfferHttp3
       name = "offer-http3",
       description = "Offer HTTP3 by providing the header and redirecting URLs to enable loading of assets with HTTP3",
       owners = Seq(Owner.withGithub("paulmr")),
-      sellByDate = LocalDate.of(2023, 7, 31),
+      sellByDate = LocalDate.of(2023, 8, 8),
       participationGroup = Perc1E,
     )
