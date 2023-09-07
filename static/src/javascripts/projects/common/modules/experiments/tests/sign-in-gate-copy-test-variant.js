69897089a480f6c89564ab85d9f034725eb0b3e5
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-09-07 16:01:21 +0100

    Update audience and expiry

--- static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-copy-test-variant.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-copy-test-variant.js
@@ -1,10 +1,10 @@
 export const signInGateCopyTestRepeatSept2023 = {
 	id: 'SignInGateCopyTestRepeatSept2023',
 	start: '2023-08-30',
-	expiry: '2023-10-01',
+	expiry: '2025-12-01',
 	author: 'Lindsey Dew',
 	description: 'Test varying the copy in the call to action for sign in gate',
-	audience: 1.0,
+	audience: 0.9,
 	audienceOffset: 0.0,
 	successMeasure: 'Users sign in or create a Guardian account',
 	audienceCriteria: '3rd article of the day, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
