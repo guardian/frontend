400d75b27f7a805b957672bb06ab13d51430dbdf
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-09-18 17:10:42 +0100

    extend offer-http3

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -104,7 +104,7 @@ object OfferHttp3
       name = "offer-http3",
       description = "Offer HTTP3 by providing the header and redirecting URLs to enable loading of assets with HTTP3",
       owners = Seq(Owner.withGithub("paulmr")),
-      sellByDate = LocalDate.of(2023, 9, 15),
+      sellByDate = LocalDate.of(2023, 9, 29),
       participationGroup = Perc1E,
     )
 
