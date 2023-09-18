b4f0628a6f8fe215863e5d5433f9b49a16ec46b7
Rik Roots <rik.roots@gmail.com>
2023-09-18 16:49:48 +0100

    fix linting issues

--- static/src/javascripts/projects/common/modules/commercial/braze/brazeBanner.tsx
+++ static/src/javascripts/projects/common/modules/commercial/braze/brazeBanner.tsx
@@ -80,10 +80,9 @@ const renderBanner = (
 					? getOptionsHeadersWithOkta(authStatus)
 					: {};
 
-			const fetchEmail = (): (() => Promise<null>) =>
-				() => {
-					return Promise.resolve(null);
-				};
+			const fetchEmail = (): (() => Promise<string | null>) => () => {
+				return Promise.resolve(null);
+			};
 
 			// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- IE does not support shadow DOM, so instead we just render
 			if (!container.attachShadow) {
