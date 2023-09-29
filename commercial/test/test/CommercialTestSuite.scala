b46518d0f8c6fc20e38656dcb6a9e6cd9746fa31
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-09-29 11:06:48 +0100

    remove more test stuff

--- commercial/test/test/CommercialTestSuite.scala
+++ commercial/test/test/CommercialTestSuite.scala
@@ -1,18 +1,15 @@
 package commercial.test
 
 import commercial.model.capi.LookupTest
-import commercial.model.merchandise.{books, events, jobs}
+import commercial.model.merchandise.{events, jobs}
 import org.scalatest.Suites
 import test.SingleServerSuite
 
 class CommercialTestSuite
     extends Suites(
-      new books.MagentoBestsellersFeedTest,
-      new books.MagentoExceptionTest,
       new jobs.JobTest,
       new events.EventbriteMasterclassFeedParsingTest,
       new events.SingleEventbriteMasterclassParsingTest,
       new LookupTest,
-      new books.BookTest,
     )
     with SingleServerSuite {}
