3f6cd7bcf97f17f4fc93f4a6ac8efbb8b7b9a72e
Charlotte <charlotte.emms@theguardian.com>
2023-10-11 13:22:07 +0100

    rework logging for articles and include DCAR consideration

--- article/app/controllers/ArticleController.scala
+++ article/app/controllers/ArticleController.scala
@@ -134,7 +134,6 @@ class ArticleController(
       case HtmlFormat | AmpFormat =>
         Future.successful(common.renderHtml(ArticleHtmlPage.html(article), article))
       case AppsFormat =>
-        log.info("[ArticleRendering] path executing in dotcom rendering for apps (DCAR)")
         remoteRenderer.getAppsArticle(
           ws,
           article,
