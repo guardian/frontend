b50598efd6ac2795ce73441e3808fa4372601a9b
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-10-17 16:15:24 +0100

    Use prettier

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -9,7 +9,7 @@ import { prebidKargo } from './tests/prebid-kargo';
 import { remoteRRHeaderLinksTest } from './tests/remote-header-test';
 import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
-import {signInGateTimesOfDay} from "./tests/sign-in-gate-times-of-day";
+import { signInGateTimesOfDay } from './tests/sign-in-gate-times-of-day';
 
 // keep in sync with ab-tests in dotcom-rendering
 // https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/experiments/ab-tests.ts
