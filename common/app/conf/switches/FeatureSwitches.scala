aee55b5de1891db9c0d4aeb4d7ef767265be585d
Charlotte <charlotte.emms@theguardian.com>
2023-10-18 14:58:44 +0100

    add dark mode switch for dcar project

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -542,4 +542,14 @@ trait FeatureSwitches {
     exposeClientSide = true,
   )
 
+  val DarkModeInApps = Switch(
+    SwitchGroup.Feature,
+    "dark-mode-apps",
+    "If this switch is on, we will allow dark mode in apps articles",
+    owners = Seq(Owner.withEmail("dotcom.platform@theguardian.com")),
+    safeState = Off,
+    // This is a random date in the future but this switch should be removed far before then
+    sellByDate = Some(LocalDate.of(2024, 6, 5)),
+    exposeClientSide = false,
+  )
 }
