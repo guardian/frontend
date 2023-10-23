2f398ba36c8084ec7e5d9d7c5ab31011be893146
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-10-23 10:45:08 +0100

    Update the audience figures and duration

--- static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-times-of-day.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-times-of-day.js
@@ -2,13 +2,13 @@
 
 export const signInGateTimesOfDay = {
     id: 'SignInGateTimesOfDay',
-    start: '2020-05-20',
-    expiry: '2023-12-29',
+    start: '2023-10-23',
+    expiry: '2023-11-23',
     author: 'Lindsey Dew',
     description:
         'Show sign in gate more frequently to users in the morning',
-    audience: 0.4,
-    audienceOffset: 0.5,
+    audience: 0.5,
+    audienceOffset: 0.4,
     successMeasure: 'Increase registration and sign ins by 5%',
     audienceCriteria:
         '1st article of the day, max 5 articles, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
