db20d42b486f720f44d75f2cb925e6c9d01ae3cb
Charlotte Emms <43961396+cemms1@users.noreply.github.com>
2023-11-17 12:09:32 +0000

    Revert "Use new newsletter data model for rendering sign-up form pages (#26549)"
    This reverts commit fa46f7ac7a5bf5fea681cbc12e54e897862a1411.

--- common/app/services/newsletters/model/NewsletterResponse.scala
+++ common/app/services/newsletters/model/NewsletterResponse.scala
@@ -53,7 +53,6 @@ case class NewsletterResponseV2(
     signupPage: Option[String],
     exampleUrl: Option[String],
     category: String, // "article-based", "article-based-legacy", "fronts-based", "manual-send", "other"]
-    emailConfirmation: Boolean,
 )
 
 object NewsletterResponseV2 {
