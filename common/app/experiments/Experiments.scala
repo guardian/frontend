dfe6689fd6186beffd3ec6e43d53911dc166c397
George B <705427+georgeblahblah@users.noreply.github.com>
2023-12-01 11:59:50 +0000

    Extend experiment `offer-http3` (#26734)

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -61,6 +61,6 @@ object OfferHttp3
       name = "offer-http3",
       description = "Offer HTTP3 by providing the header and redirecting URLs to enable loading of assets with HTTP3",
       owners = Seq(Owner.withGithub("paulmr")),
-      sellByDate = LocalDate.of(2023, 11, 30),
+      sellByDate = LocalDate.of(2024, 1, 3),
       participationGroup = Perc1E,
     )
