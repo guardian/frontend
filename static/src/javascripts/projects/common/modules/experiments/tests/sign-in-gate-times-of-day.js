a09bd8179a92fd132f43c5cb2aba804567c38a5d
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-12-04 14:34:15 +0000

    Update audience segmentation

--- static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-times-of-day.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-times-of-day.js
@@ -7,15 +7,15 @@ export const signInGateTimesOfDay = {
     author: 'Lindsey Dew',
     description:
         'Show sign in gate more frequently to users in the morning',
-    audience: 0.5,
-    audienceOffset: 0.4,
+    audience: 0.45,
+    audienceOffset: 0.45,
     successMeasure: 'Increase registration and sign ins by 5%',
     audienceCriteria:
         '1st article of the day, max 5 articles, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
     ophanComponentId: 'morning',
     dataLinkNames: 'SignInGateTimesOfDay',
     idealOutcome:
-        'TODO',
+        'Increase registration and sign ins by 5%',
     showForSensitive: false,
     canRun: () => true,
     variants: [
