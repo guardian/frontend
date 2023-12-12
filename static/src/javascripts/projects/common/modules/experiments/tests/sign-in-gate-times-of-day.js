ff7fc76ee2483f2fcf9cdcc85779fd95d666618e
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2023-12-12 17:38:06 +0000

    Update the test definition

--- static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-times-of-day.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/sign-in-gate-times-of-day.js
@@ -3,12 +3,12 @@
 export const signInGateTimesOfDay = {
     id: 'SignInGateTimesOfDay',
     start: '2023-12-11',
-    expiry: '2023-12-15',
+    expiry: '2023-12-18',
     author: 'Lindsey Dew',
     description:
         'Show sign in gate more frequently to users in the morning',
-    audience: 0.45,
-    audienceOffset: 0.45,
+    audience: 0.08,
+    audienceOffset: 0.82,
     successMeasure: 'Increase registration and sign ins by 5%',
     audienceCriteria:
         '1st article of the day, max 5 articles, lower priority than consent banner, simple articles (not gallery, live etc.), not signed in, not shown after dismiss, not on help, info sections etc. Exclude iOS 9 and guardian-live-australia. Suppresses other banners, and appears over epics',
@@ -20,7 +20,11 @@ export const signInGateTimesOfDay = {
     canRun: () => true,
     variants: [
         {
-            id: 'morning',
+            id: 'times-of-day-morning',
+            test: () => {},
+        },
+        {
+            id: 'times-of-day-control',
             test: () => {},
         },
     ],
