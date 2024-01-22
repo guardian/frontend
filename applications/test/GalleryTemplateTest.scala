07a3e70491161a213af8ed0e5b214534c924e003
Ioanna Kokkini <ioannakok@hotmail.com>
2024-01-22 17:59:13 +0000

    Revert "Change tested url in one of the `GalleryTemplateTest` tests"
    This reverts commit 15ea231970a647617084a5b36e1263b5207e9492.

--- applications/test/GalleryTemplateTest.scala
+++ applications/test/GalleryTemplateTest.scala
@@ -2,7 +2,7 @@ package test
 
 import conf.switches.Switches.FacebookShareUseTrailPicFirstSwitch
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.{DoNotDiscover}
+import org.scalatest.DoNotDiscover
 import org.scalatest.matchers.should.Matchers
 
 import scala.jdk.CollectionConverters._
@@ -55,11 +55,10 @@ import scala.jdk.CollectionConverters._
 
   it should "select the largest main picture for the opengraph image when FacebookShareUseTrailPicFirstSwitch is OFF" in {
     FacebookShareUseTrailPicFirstSwitch.switchOff()
-    goTo("/community/gallery/2024/jan/17/a-salty-sea-dog-and-a-vintage-wall-readers-best-photos") { browser =>
+    goTo("/lifeandstyle/gallery/2014/nov/24/flying-dogs-in-pictures") { browser =>
       import browser._
-      val image = $("meta[property='og:image']").attributes("content").asScala.head
-      image should include(
-        "81d3038d96dab501ff33286e9df00a8c76b08f1c",
+      $("meta[property='og:image']").attributes("content").asScala.head should include(
+        "e3867edb-e9d5-4be9-9c51-12258b686869-1498x2040.jpeg",
       )
     }
   }
