acf4e89849704ee23622e36ed24b138436d313c2
Tom Wey <tom.wey@theguardian.com>
2024-02-06 12:51:40 +0000

    Remove @guardian/braze-components
    DRR are happy that frontend represents a small enough percentage of
    pageviews and Braze message impressions that we can stop rendering
    Braze messages here.
    
    There is some Braze code remaining here. If a user changes their
    consents or logs out we still want to clear any data set locally by
    either our own code or the Braze web SDK. I've inlined a small amount of
    code from @guardian/braze-components around clearing the local message
    cache.

--- static/src/javascripts/projects/common/modules/commercial/braze/buildBrazeMessaging.ts
+++ static/src/javascripts/projects/common/modules/commercial/braze/buildBrazeMessaging.ts
@@ -1,24 +1,9 @@
-import type {
-	BrazeCardsInterface,
-	BrazeMessagesInterface,
-} from '@guardian/braze-components';
-import {
-	BrazeCards,
-	BrazeMessages,
-	LocalMessageCache,
-	NullBrazeCards,
-	NullBrazeMessages,
-} from '@guardian/braze-components/logic';
-import { log } from '@guardian/libs';
-import ophan from 'ophan/ng';
-import config from '../../../../../lib/config';
-import { reportError } from '../../../../../lib/report-error';
-import { measureTiming } from '../../../../commercial/measure-timing';
+import { log, storage } from '@guardian/libs';
+import { reportError } from 'lib/report-error';
 import { checkBrazeDependencies } from './checkBrazeDependencies';
 import {
 	clearHasCurrentBrazeUser,
 	hasCurrentBrazeUser,
-	setHasCurrentBrazeUser,
 } from './hasCurrentBrazeUser';
 
 const SDK_OPTIONS = {
@@ -28,6 +13,15 @@ const SDK_OPTIONS = {
 	sessionTimeoutInSeconds: 1,
 	minimumIntervalBetweenTriggerActionsInSeconds: 0,
 	devicePropertyAllowlist: [],
+};
+
+const localStorageKeyBase = 'gu.brazeMessageCache';
+const clearLocalMessageCache = (): void => {
+	const slotNames = ['Banner', 'EndOfArticle'];
+	slotNames.forEach((slotName) => {
+		const key = `${localStorageKeyBase}.${slotName}`;
+		storage.local.remove(key);
+	});
 };
 
 const maybeWipeUserData = async (
@@ -48,7 +42,7 @@ const maybeWipeUserData = async (
 				importedAppboy.wipeData();
 			}
 
-			LocalMessageCache.clear();
+			clearLocalMessageCache();
 
 			clearHasCurrentBrazeUser();
 
@@ -59,83 +53,16 @@ const maybeWipeUserData = async (
 	}
 };
 
-interface BrazeMessaging {
-	brazeMessages: BrazeMessagesInterface;
-	brazeCards: BrazeCardsInterface;
-}
-export const buildBrazeMessaging = async (): Promise<BrazeMessaging> => {
+export const handleBraze = async (): Promise<void> => {
 	const dependenciesResult = await checkBrazeDependencies();
 
 	if (!dependenciesResult.isSuccessful) {
-		const { failure, data } = dependenciesResult;
-
-		log(
-			'tx',
-			`Not attempting to show Braze messages. Dependency ${
-				failure.field
-			} failed with ${String(failure.data)}.`,
-		);
+		const { data } = dependenciesResult;
 
 		await maybeWipeUserData(
 			data.apiKey as string | undefined,
 			data.brazeUuid as string | undefined,
 			data.consent as boolean,
 		);
-
-		return {
-			brazeMessages: new NullBrazeMessages(),
-			brazeCards: new NullBrazeCards(),
-		};
-	}
-
-	try {
-		const sdkLoadTiming = measureTiming('braze-sdk-load');
-		sdkLoadTiming.start();
-
-		const { default: importedAppboy } = await import(
-			/* webpackChunkName: "braze-web-sdk-core" */ '@braze/web-sdk-core'
-		);
-
-		const sdkLoadTimeTaken = sdkLoadTiming.end();
-		ophan.record({
-			component: 'braze-sdk-load-timing',
-			value: sdkLoadTimeTaken,
-		});
-
-		importedAppboy.initialize(
-			dependenciesResult.data.apiKey as string,
-			SDK_OPTIONS,
-		);
-
-		const errorHandler = (error: Error) => {
-			reportError(error, {}, false);
-		};
-		const brazeMessages = new BrazeMessages(
-			importedAppboy,
-			LocalMessageCache,
-			errorHandler,
-		);
-		const brazeCards = config.get('switches.brazeContentCards')
-			? new BrazeCards(importedAppboy, errorHandler)
-			: new NullBrazeCards();
-
-		setHasCurrentBrazeUser();
-		importedAppboy.changeUser(dependenciesResult.data.brazeUuid as string);
-		importedAppboy.openSession();
-
-		return {
-			brazeMessages,
-			brazeCards,
-		};
-	} catch (error) {
-		if (error instanceof Error) {
-			const msg = `Error building Braze Messages: ${error.message}`;
-			log('tx', msg);
-		}
-
-		return {
-			brazeMessages: new NullBrazeMessages(),
-			brazeCards: new NullBrazeCards(),
-		};
 	}
 };
