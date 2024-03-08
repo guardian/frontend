7d00aae41d23693657f9eaa5d10b15c4fadbb11a
Andy Gallagher <fredex42@gmail.com>
2024-03-08 14:11:18 +0000

    Correctly synthesizing all @-fields in a fairly accurate test

--- common/test/model/dotcomrendering/LinkedDataTest.scala
+++ common/test/model/dotcomrendering/LinkedDataTest.scala
@@ -1,6 +1,6 @@
 package model.dotcomrendering
 
-import com.gu.contentapi.client.model.schemaorg.SchemaRecipe
+import com.gu.contentapi.client.model.schemaorg.{RecipeStep, SchemaRecipe}
 import com.gu.contentapi.client.model.v1.{CapiDateTime, SchemaOrg, Tag, TagType, Content => ApiContent}
 import com.gu.contentapi.client.utils.CapiModelEnrichment.RichOffsetDateTime
 import model.{Article, Content, ContentType, DotcomContentType, MetaData, RelatedContent}
@@ -61,12 +61,34 @@ class LinkedDataTest extends AnyFlatSpec with Matchers with MockitoSugar {
           _atContext = "context",
           _atType = "recipe",
           name = Some("Test recipe"),
-          description = Some("This is yummy")
+          description = Some("This is yummy"),
+          image = Some("https://path.to/image/on/server.jpg"),
+          datePublished = Some("2012-01-02T03:04:05Z"),
+          url = Some("https://path.to/content/on/server.html"),
+          recipeCategory = Some(Seq("test","food")),
+          recipeCuisine = Some(Seq("test","British")),
+          recipeIngredient = Some(Seq("23 litres of sprunge", "6 baked beans")),
+          recipeInstructions = Some(Seq(
+            RecipeStep(
+              _atType = "HowToStep", text = "Open the can", name = Some("Open"), url = None, image = None
+            ),
+            RecipeStep(
+              _atType = "HowToStep", text = "Pour the contents", name = Some("Pour"), url = None, image = None
+            ),
+          )),
+          recipeYield = Some(Seq("1 serving")),
+          prepTime = Some("30 seconds"),
+          cookTime = Some("10 hours"),
+          totalTime = Some("10 hours 30 seconds"),
+          author = Some(com.gu.contentapi.client.model.schemaorg.AuthorInfo(_atType = "Person", name = "John Smith", sameAs = None))
         )))
       )),
     )
     Article.make(Content.make(item))
   }
+
+  /// This string should always correct validate at https://validator.schema.org/
+  val expectedRecipeJson = """{"@context":"http://schema.org","@type":"Recipe","name":"Test recipe","description":"This is yummy","image":"https://path.to/image/on/server.jpg","datePublished":"2012-01-02T03:04:05Z","url":"https://path.to/content/on/server.html","recipeCategory":["test","food"],"recipeCuisine":["test","British"],"recipeIngredient":["23 litres of sprunge","6 baked beans"],"recipeInstructions":[{"@type":"HowToStep","text":"Open the can","name":"Open"},{"@type":"HowToStep","text":"Pour the contents","name":"Pour"}],"recipeYield":["1 serving"],"prepTime":"30 seconds","cookTime":"10 hours","totalTime":"10 hours 30 seconds","author":{"@type":"Person","name":"John Smith"}}"""
 
   "LinkedData.forArticle" should "return news article linkedData" in {
     val linkedData = LinkedData.forArticle(
@@ -91,6 +113,8 @@ class LinkedDataTest extends AnyFlatSpec with Matchers with MockitoSugar {
     linkedData.headOption.map(_.`@type`) shouldEqual(Some("NewsArticle"))
     linkedData.length shouldEqual(3)
 
-    println(Json.toJson(linkedData(2)).toString())
+    val jsonString = Json.toJson(linkedData(2)).toString()
+    println(jsonString)
+    jsonString shouldEqual(expectedRecipeJson)
   }
 }
