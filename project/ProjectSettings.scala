7cad4972aec759441370356a6794ccad8c1f47ce
Mariot Chauvin <mariot.chauvin@gmail.com>
2024-03-18 11:55:46 +0000

    Update to latest scala 2.13.x

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -32,7 +32,7 @@ object ProjectSettings {
     Compile / packageDoc / publishArtifact := false,
     Compile / doc / sources := Seq.empty,
     Compile / doc := target.map(_ / "none").value,
-    scalaVersion := "2.13.12",
+    scalaVersion := "2.13.13",
     initialize := {
       val _ = initialize.value
       assert(
@@ -77,7 +77,6 @@ object ProjectSettings {
     // These settings are needed for forking, which in turn is needed for concurrent restrictions.
     Test / javaOptions += "-DAPP_SECRET=this_is_not_a_real_secret_just_for_tests",
     Test / javaOptions += "-Xmx2048M",
-    Test / javaOptions += "-XX:+UseConcMarkSweepGC",
     Test / javaOptions += "-XX:ReservedCodeCacheSize=128m",
     Test / baseDirectory := file("."),
     Test / envVars := Map("STAGE" -> testStage),
