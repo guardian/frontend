162611e7f4314a0a1654f3b711b5907413bc64ae
Alina Boghiu <alina.boghiu@gmail.com>
2024-03-28 12:07:42 +0000

    Extend expiring tests - we still need these

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -35,7 +35,7 @@ object AdaptiveSite
       name = "adaptive-site",
       description = "Enables serving an adaptive version of the site that responds to page performance",
       owners = Seq(Owner.withEmail("dotcom.platform@theguardian.com")),
-      sellByDate = LocalDate.of(2024, 4, 2),
+      sellByDate = LocalDate.of(2024, 5, 2),
       participationGroup = Perc1A,
     )
 
@@ -53,7 +53,7 @@ object DCRTagPages
       name = "dcr-tag-pages",
       description = "Render tag pages with DCR",
       owners = Seq(Owner.withGithub("dotcom.platform@theguardian.com")),
-      sellByDate = LocalDate.of(2024, 4, 1),
+      sellByDate = LocalDate.of(2024, 5, 1),
       participationGroup = Perc2C,
     )
 
