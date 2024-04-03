d2fc3483f7ee5bbc9a25d00276b16e7557ca3741
Tom Richards <tom.richards@guardian.co.uk>
2024-04-03 13:14:36 +0100

    add `dev-nginx` config for `admin` and `preview` so they can be hit on https://frontend.local.dev-gutools.co.uk and https://preview.local.dev-gutools.co.uk respectively.

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -11,8 +11,10 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 dev-nginx setup-cert ${DOMAIN}
 dev-nginx link-config ${SOURCE_DIR}/frontend.conf
+dev-nginx setup-app ./nginx-mapping.yml # for preview and admin
 dev-nginx restart-nginx
 
 echo -e "💯 Done! You can now run frontend locally on https://${DOMAIN}"
+echo -e "You can also (separately) run admin and preview locally on https://frontend.local.dev-gutools.co.uk and https://preview.local.dev-gutools.co.uk respectively."
 echo -e "👤 To setup Dotcom Identity Frontend please follow identity-platform README."
 echo -e "👋"
