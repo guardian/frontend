73065bd64873c86ae23919af61b9164dc71b1c70
Max Duval <max.duval@theguardian.com>
2024-04-04 11:02:09 +0100

    feat(DeeplyRead): only update if exactly 10 items (#27015)
    and add logging to identify when this is not the case

--- common/app/agents/DeeplyReadAgent.scala
+++ common/app/agents/DeeplyReadAgent.scala
@@ -67,7 +67,7 @@ class DeeplyReadAgent(contentApiClient: ContentApiClient, ophanApi: OphanApi) ex
               Future
                 .sequence(constructedTrail)
                 .map { maybeTrails =>
-                  (edition, maybeTrails.flatten)
+                  (edition, maybeTrails.flatten.take(10))
                 }
 
           }
@@ -80,8 +80,12 @@ class DeeplyReadAgent(contentApiClient: ContentApiClient, ophanApi: OphanApi) ex
         val map = trailsList.toMap
         for {
           (edition, list) <- map
-        } yield log.info(s"Deeply Read in ${edition.displayName}: ${list.map(_.url).toString()}")
-        deeplyReadItems.alter(map)
+        } yield log.info(s"Deeply Read in ${edition.displayName}, ${list.size} items: ${list.map(_.url).toString()}")
+
+        val mapWithTenItems = map.filter { case (_, list) => list.size == 10 }
+        log.info(s"Updating the followings keys: ${mapWithTenItems.keys.toString()}")
+
+        deeplyReadItems.alter(deeplyReadItems.get() ++ mapWithTenItems)
       })
   }
 
