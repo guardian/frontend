ffd95b9659a2bf1403ba7cfb94d6a2ccc20a10d7
Charlotte Emms <43961396+cemms1@users.noreply.github.com>
2024-04-12 11:39:46 +0100

    Set up AB tests for upcoming `Header`, `Nav` and `Masthead` updates (#27034)

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -15,6 +15,8 @@ object ActiveExperiments extends ExperimentsDefinition {
       AdaptiveSite,
       CommercialMegaTest,
       DCRTagPages,
+      UpdatedHeaderDesign,
+      MastheadWithHighlights,
     )
   implicit val canCheckExperiment: CanCheckExperiment = new CanCheckExperiment(this)
 }
@@ -53,4 +55,23 @@ object AMPGlobalAddressList
       owners = Seq(Owner.withEmail("commercial.dev@theguardian.com")),
       sellByDate = LocalDate.of(2024, 4, 30),
       participationGroup = Perc0A,
+    )
+
+object UpdatedHeaderDesign
+    extends Experiment(
+      name = "updated-header-design",
+      description = "Shows updated design of Header and Nav components",
+      owners = Seq(Owner.withGithub("cemms1")),
+      sellByDate = LocalDate.of(2024, 9, 30),
+      participationGroup = Perc0B,
+    )
+
+object MastheadWithHighlights
+    extends Experiment(
+      name = "masthead-with-highlights",
+      description =
+        "Shows new masthead component, with highlights container, in place of current header/navigation and top bar",
+      owners = Seq(Owner.withGithub("cemms1")),
+      sellByDate = LocalDate.of(2024, 9, 30),
+      participationGroup = Perc0C,
     )
