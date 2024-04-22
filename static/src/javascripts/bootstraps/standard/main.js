4084c884de345be075099439ea55b0abe60a782a
Charlotte Emms <43961396+cemms1@users.noreply.github.com>
2024-04-22 10:01:43 +0100

    remove header-top-nav switch (#27067)

--- static/src/javascripts/bootstraps/standard/main.js
+++ static/src/javascripts/bootstraps/standard/main.js
@@ -38,11 +38,11 @@ const showHiringMessage = () => {
         if (!config.get('page.isDev') && config.get('switches.weAreHiring')) {
             window.console.log(
                 '\n' +
-                    '%cHello.\n' +
-                    '\n' +
-                    '%cWe are hiring – ever thought about joining us? \n' +
-                    '%chttps://workforus.theguardian.com/careers/product-engineering%c \n' +
-                    '\n',
+                '%cHello.\n' +
+                '\n' +
+                '%cWe are hiring – ever thought about joining us? \n' +
+                '%chttps://workforus.theguardian.com/careers/product-engineering%c \n' +
+                '\n',
                 'font-family: Georgia, serif; font-size: 32px; color: #052962',
                 'font-family: Georgia, serif; font-size: 16px; color: #767676',
                 'font-family: Helvetica Neue, sans-serif; font-size: 11px; text-decoration: underline; line-height: 1.2rem; color: #767676',
@@ -73,8 +73,8 @@ const addScrollHandler = () => {
         'scroll',
         userPrefs.get('use-idle-callback') && 'requestIdleCallback' in window
             ? () => {
-                  window.requestIdleCallback(onScroll);
-              }
+                window.requestIdleCallback(onScroll);
+            }
             : onScroll,
         { passive: true }
     );
@@ -162,9 +162,7 @@ const bootStandard = () => {
 
     ophan.setEventEmitter(mediator);
 
-    if(window.guardian.config.switches.headerTopNav
-        && document.querySelector('.header-top-nav')
-        ) {
+    if (document.querySelector('.header-top-nav')) {
         headerTopNavInit();
     } else {
         newHeaderInit();
