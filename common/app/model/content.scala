a156297f07581d267e5815abd18d4aa8386970f4
Jake Lee Kennedy <jlkennedy127@gmail.com>
2024-04-25 12:50:35 +0100

    use both methods to find the source

--- common/app/model/content.scala
+++ common/app/model/content.scala
@@ -707,7 +707,7 @@ object Video {
     val contentType = DotcomContentType.Video
     val elements = content.elements
     val section = content.metadata.sectionId
-    val source: Option[String] = content.media.headOption.flatMap(_.source)
+    val source = elements.videos.find(_.properties.isMain).flatMap(_.videos.source).orElse(content.media.headOption.flatMap(_.source))
 
     val javascriptConfig: Map[String, JsValue] = Map(
       "isPodcast" -> JsBoolean(content.tags.isPodcast),
