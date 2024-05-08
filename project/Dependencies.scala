bd474e9a7d1bea20c5286c645ead2b034d8b50cd
Matthias Kurz <m.kurz@irregular.at>
2024-05-08 13:54:03 +0200

    Play JSON 3.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
-  val playJsonVersion = "3.0.2"
+  val playJsonVersion = "3.0.3"
   val apacheCommonsLang = "org.apache.commons" % "commons-lang3" % "3.11"
   val awsCore = "com.amazonaws" % "aws-java-sdk-core" % awsVersion
   val awsCloudwatch = "com.amazonaws" % "aws-java-sdk-cloudwatch" % awsVersion
