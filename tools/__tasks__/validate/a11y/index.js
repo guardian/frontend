4cc13c86d512c312549910e0438932fa67a3e5b9
Anna Beddow <anna.beddow@gmail.com>
2024-05-23 12:10:45 +0100

    Remove ally validation broken script and relevant packages
    Co-authored-by: Jamie B <53781962+JamieB-gu@users.noreply.github.com>
    Co-authored-by: Dom Lander <dominik.lander@guardian.co.uk>

--- tools/__tasks__/validate/a11y/index.js
+++
@@ -1,8 +0,0 @@
-const checkNetwork = require('../../lib/check-network');
-const { domain, port } = require('./config');
-
-module.exports = {
-    description:
-        'Ok, analysing accessibility for you... This may take a while ⏳',
-    task: [checkNetwork(domain, port), require('./a11y')],
-};
