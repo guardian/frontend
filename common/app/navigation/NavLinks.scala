fb978da0dfe32315b57da0d309c098368f73b1e5
Ioanna Kokkini <ioannakok@users.noreply.github.com>
2024-06-07 15:59:37 +0100

    Revert "Add Euro 2024 to the nav bar"

--- common/app/navigation/NavLinks.scala
+++ common/app/navigation/NavLinks.scala
@@ -110,7 +110,6 @@ object NavLinks {
 
   /* SPORT */
 
-  private val euro2024 = NavLink("Euro 2024", "football/euro-2024", Some("football/euro-2024"))
   private val footballScores = NavLink("Live scores", "/football/live", Some("football/live"))
   private val footballTables = NavLink("Tables", "/football/tables", Some("football/tables"))
   private val footballFixtures = NavLink("Fixtures", "/football/fixtures", Some("football/fixtures"))
@@ -124,7 +123,6 @@ object NavLinks {
     "Football",
     "/football",
     children = List(
-      euro2024,
       footballScores,
       footballTables,
       footballFixtures,
@@ -137,7 +135,6 @@ object NavLinks {
     "Soccer",
     "/us/soccer",
     children = List(
-      euro2024,
       footballScores,
       footballTables,
       soccerSchedules,
