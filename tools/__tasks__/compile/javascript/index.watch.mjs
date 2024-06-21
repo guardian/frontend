30c1675d9c5c9adc16e1ad372791b2bd7c160e06
Max Duval <max.duval@theguardian.com>
2024-06-21 14:49:49 +0100

    fix: more default imports

--- tools/__tasks__/compile/javascript/index.watch.mjs
+++ tools/__tasks__/compile/javascript/index.watch.mjs
@@ -1,9 +1,9 @@
 export default {
 	description: 'Prepare JS for development',
 	task: [
-		await import('../inline-svgs/index.mjs'),
-		await import('./clean.mjs'),
-		await import('./copy.mjs'),
-		await import('./bundle-polyfills.mjs'),
+		(await import('../inline-svgs/index.mjs')).default,
+		(await import('./clean.mjs')).default,
+		(await import('./copy.mjs')).default,
+		(await import('./bundle-polyfills.mjs')).default,
 	],
 };
