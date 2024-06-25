2311edd572b1624ea8a2e39cb9d1f17b4b1fce51
Max Duval <max.duval@theguardian.com>
2024-06-25 10:38:11 +0100

    feat: only validate head pre push
    do not format untouched files

---
+++ tools/__tasks__/validate-head/scala.mjs
@@ -0,0 +1,28 @@
+import chalk from 'chalk';
+import execa from 'execa';
+
+const config = '--error';
+
+const error = (ctx) => {
+	ctx.messages.push(
+		`Run ${chalk.blue('./sbt scalafmt')} to format Scala files.`,
+	);
+};
+
+/** @type {import('listr2').ListrTask} */
+const task = {
+	title: 'Check Scala formatting',
+	task: (ctx, task) =>
+		task.newListr(
+			[
+				{
+					title: 'scalafmtCheck',
+					task: () => execa('./sbt', ['scalafmtCheck', config]),
+					onError: error,
+				},
+			],
+			{ concurrent: !!ctx.verbose ? false : true },
+		),
+};
+
+export default task;
