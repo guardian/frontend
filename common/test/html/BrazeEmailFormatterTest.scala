87fedd1673df92a4dafdda179463f293385bc11d
Ioanna Kokkini <ioannakok@hotmail.com>
2024-07-11 15:32:36 +0100

    Bump various dependencies

--- common/test/html/BrazeEmailFormatterTest.scala
+++ common/test/html/BrazeEmailFormatterTest.scala
@@ -55,8 +55,10 @@ class BrazeEmailFormatterTest extends AnyFlatSpec with Matchers {
         |<head></head>
         |<body>
         |<h2>Heading text</h2>
-        |<p>Paragraph.</p><br>
-        |<div><span>My name is:</span> Bill
+        |<p>Paragraph.</p>
+        |<br>
+        |<div>
+        |<span>My name is:</span> Bill
         |</div><a href="/link/?##braze_utm##">some link</a> <a href="https://www.theguardian.com/environment/2018/sep/26/dont-post-crisp-packets-royal-mail-begs-packaging-protesters?##braze_utm##">article link</a>
         |<table>
         |<tbody>
@@ -101,7 +103,8 @@ class BrazeEmailFormatterTest extends AnyFlatSpec with Matchers {
       """<!doctype html>
         |<html>
         |<head></head>
-        |<body><a href="%%unsub_center_url%%">unsubscribe</a>
+        |<body>
+        |<a href="%%unsub_center_url%%">unsubscribe</a>
         |</body>
         |</html>""".stripMargin
 
