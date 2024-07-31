656bf6bdb703c1136e1148c747ac167f7fbef740
Sue <sookburt@gmail.com>
2024-07-31 13:04:49 +0100

    Remove switch and AB test elements for sticky liveblog ask AB Test

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -34,14 +34,4 @@ trait ABTestSwitches {
     sellByDate = Some(LocalDate.of(2024, 9, 30)),
     exposeClientSide = true,
   )
-
-  Switch(
-    ABTests,
-    "ab-sticky-live-blog-ask-test",
-    "A sticky reader revenue ask on the left column of a liveblog",
-    owners = Seq(Owner.withEmail("growth@theguardian.com")),
-    safeState = Off,
-    sellByDate = Some(LocalDate.of(2024, 7, 31)),
-    exposeClientSide = true,
-  )
 }
--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -3,7 +3,6 @@ import { mpuWhenNoEpic } from './tests/mpu-when-no-epic';
 import { remoteRRHeaderLinksTest } from './tests/remote-header-test';
 import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
-import { stickyLiveBlogAskTest } from './tests/sticky-liveblog-ask';
 
 // keep in sync with ab-tests in dotcom-rendering
 // https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/experiments/ab-tests.ts
@@ -12,5 +11,4 @@ export const concurrentTests: readonly ABTest[] = [
 	signInGateMainControl,
 	remoteRRHeaderLinksTest,
 	mpuWhenNoEpic,
-	stickyLiveBlogAskTest
 ];
