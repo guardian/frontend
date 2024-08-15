27c81c44bc2a2be5dd67c43721241e8e64ab8ecc
Anna Beddow <anna.beddow@gmail.com>
2024-08-15 11:11:39 +0100

    format files

--- common/app/layout/slices/FlexibleSpecial.scala
+++ common/app/layout/slices/FlexibleSpecial.scala
@@ -17,12 +17,12 @@ object FlexibleSpecial extends DynamicContainer {
   override protected def standardSlices(storiesIncludingBackfill: Seq[Story], firstSlice: Option[Slice]): Seq[Slice] = {
 
     storiesIncludingBackfill.length match {
-      case 0     => Nil
-      case 1     => Seq(FullMedia100)
-      case 2     => Seq(ThreeQuarterQuarter)
-      case 3     => Seq(ThreeQuarterTallQuarter2)
-      case 4     => Seq(ThreeQuarterTallQuarter1Ql2)
-      case 5     => Seq(FullMedia100, QuarterQuarterQuarterQuarter)
+      case 0 => Nil
+      case 1 => Seq(FullMedia100)
+      case 2 => Seq(ThreeQuarterQuarter)
+      case 3 => Seq(ThreeQuarterTallQuarter2)
+      case 4 => Seq(ThreeQuarterTallQuarter1Ql2)
+      case 5 => Seq(FullMedia100, QuarterQuarterQuarterQuarter)
       // This case doesn't look _quite_ right. We end up with a row of four
       // and then a row of three, slightly stretched. There isn't a layout
       // which caters for this currently, we'll follow up on this separately.
