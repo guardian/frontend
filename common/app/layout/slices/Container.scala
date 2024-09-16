ac2e0e651e1463a08e1be94cc21542c80b8b56eb
Charlotte <charlotte.emms@theguardian.com>
2024-09-16 11:14:19 +0100

    add basic flexible general structure to support container in frontend
    Co-authored-by: Anna Beddow <anna.beddow@gmail.com>
    Co-authored-by: Georges Lebreton <georges.lebreton@guardian.co.uk>

--- common/app/layout/slices/Container.scala
+++ common/app/layout/slices/Container.scala
@@ -34,6 +34,7 @@ object Container extends GuLogging {
       ("nav/media-list", NavMediaList),
       ("news/most-popular", MostPopular),
       ("flexible/special", Dynamic(FlexibleSpecial)),
+      ("flexible/general", Dynamic(FlexibleGeneral)),
     ) ++ FixedContainers.all.mapV(Fixed.apply) ++ EmailLayouts.all.mapV(Email.apply)
 
   /** So that we don't blow up at runtime, which would SUCK */
