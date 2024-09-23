2b05c843ca24bbbcc6c4ea61cc5eb51d48cbe321
Anna Beddow <anna.beddow@gmail.com>
2024-09-23 15:56:51 +0100

    Remove unused code

--- common/app/layout/slices/FlexibleContainer.scala
+++ common/app/layout/slices/FlexibleContainer.scala
@@ -3,20 +3,11 @@ package layout.slices
 import common.Seqs._
 import layout.slices.Story._
 
-/** Used for calculating the final slice -- any stories that are not standard are considered 'bigs' for the standard
-  * slice
-  */
-private[slices] case class BigsAndStandards(
-    bigs: Seq[Story],
-    standards: Seq[Story],
-)
-
 private[slices] trait FlexibleContainer {
   protected def standardSlices(stories: Seq[Story], firstSlice: Option[Slice]): Seq[Slice]
 
-  protected def optionalFirstSlice(stories: Seq[Story]): Option[(Slice, Seq[Story])] = {
-    None
-  }
+  protected def optionalFirstSlice(stories: Seq[Story]): Option[(Slice, Seq[Story])]
+
 
   final def slicesFor(stories: Seq[Story]): Option[Seq[Slice]] = {
     if (stories.nonEmpty && stories.isDescending && stories.forall(story => story.group >= 0 && story.group <= 3)) {
