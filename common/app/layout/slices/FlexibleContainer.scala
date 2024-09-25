1e7079c496001f3edde120e042e2b9eff33735df
Anna Beddow <anna.beddow@guardian.co.uk>
2024-09-25 10:01:14 +0100

    Seperate FlexibleContainers into their own class (#27495)

---
+++ common/app/layout/slices/FlexibleContainer.scala
@@ -0,0 +1,33 @@
+package layout.slices
+
+import common.Seqs._
+import layout.slices.Story._
+
+private[slices] trait FlexibleContainer {
+  protected def standardSlices(stories: Seq[Story], firstSlice: Option[Slice]): Seq[Slice]
+
+  protected def optionalFirstSlice(stories: Seq[Story]): Option[(Slice, Seq[Story])]
+
+  final def slicesFor(stories: Seq[Story]): Option[Seq[Slice]] = {
+    if (stories.nonEmpty && stories.isDescending && stories.forall(story => story.group >= 0 && story.group <= 3)) {
+      optionalFirstSlice(stories) map { case (firstSlice, remaining) =>
+        Some(firstSlice +: standardSlices(remaining, Some(firstSlice)))
+      } getOrElse {
+        Some(standardSlices(stories, None))
+      }
+    } else {
+      None
+    }
+  }
+
+  final def containerDefinitionFor(stories: Seq[Story]): Option[ContainerDefinition] = {
+    slicesFor(stories) map { slices =>
+      ContainerDefinition(
+        slices,
+        slicesWithoutMPU = slices,
+        mobileShowMore = DesktopBehaviour,
+        Set.empty,
+      )
+    }
+  }
+}
---
+++ common/app/layout/slices/FlexibleContainers.scala
@@ -0,0 +1,18 @@
+package layout.slices
+
+import model.pressed.PressedContent
+
+object FlexibleContainers {
+  val all: Map[String, FlexibleContainer] = Map(
+    ("flexible/special", FlexibleSpecial),
+    ("flexible/general", FlexibleGeneral),
+  )
+
+  def apply(collectionType: Option[String], items: Seq[PressedContent]): Option[ContainerDefinition] = {
+    for {
+      typ <- collectionType
+      flexibleContainer <- all.get(typ)
+      definition <- flexibleContainer.containerDefinitionFor(items.map(Story.fromFaciaContent))
+    } yield definition
+  }
+}
