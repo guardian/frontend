083726e0553a9fb24947643312295f30ca7e245f
Dominik Lander <dominik.lander@guardian.co.uk>
2024-10-08 17:38:44 +0100

    Rename US big events test

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -2,7 +2,7 @@ import type { ABTest } from '@guardian/ab-core';
 import { remoteRRHeaderLinksTest } from './tests/remote-header-test';
 import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
-import { usBigEvents } from './tests/us-big-events';
+import { UsaExpandableMarketingCard } from './tests/usa-expandable-marketing-card';
 
 // keep in sync with ab-tests in dotcom-rendering
 // https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/experiments/ab-tests.ts
@@ -10,5 +10,5 @@ export const concurrentTests: readonly ABTest[] = [
 	signInGateMainVariant,
 	signInGateMainControl,
 	remoteRRHeaderLinksTest,
-	usBigEvents,
+	UsaExpandableMarketingCard,
 ];
