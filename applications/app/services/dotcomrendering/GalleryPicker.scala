92848d64c98afe103b2ed666c2f80c868f63cc24
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-10-28 12:38:39 +0000

    Prevent galleries rendering in DCAR (#27556)
    Prevent galleries ever rendering in DCAR while we're working on them. This can be reverted or updated when we're ready to release them.

--- applications/app/services/dotcomrendering/GalleryPicker.scala
+++ applications/app/services/dotcomrendering/GalleryPicker.scala
@@ -7,37 +7,13 @@ import play.api.mvc.RequestHeader
 import utils.DotcomponentsLogger
 
 object GalleryPicker extends GuLogging {
-
-  /** Add to this function any logic for including/excluding a gallery article from being rendered with DCR
-    *
-    * Currently defaulting to false until we implement image articles in DCR
-    */
-  private def dcrCouldRender(galleryPage: GalleryPage): Boolean = {
-    false
-  }
-
   def getTier(
       galleryPage: GalleryPage,
   )(implicit
       request: RequestHeader,
   ): RenderType = {
+    DotcomponentsLogger.logger.logRequest(s"path executing in web", Map.empty, galleryPage.gallery)
 
-    val participatingInTest = false // until we create a test for this content type
-    val dcrCanRender = dcrCouldRender(galleryPage)
-
-    val tier = {
-      if (request.forceDCROff) LocalRender
-      else if (request.forceDCR) RemoteRender
-      else if (dcrCanRender && participatingInTest) RemoteRender
-      else LocalRender
-    }
-
-    if (tier == RemoteRender) {
-      DotcomponentsLogger.logger.logRequest(s"path executing in dotcomponents", Map.empty, galleryPage.gallery)
-    } else {
-      DotcomponentsLogger.logger.logRequest(s"path executing in web", Map.empty, galleryPage.gallery)
-    }
-
-    tier
+    LocalRender
   }
 }
