1e5c7ecdfdbcd8f1ded2db7926236114882c62fb
David Blatcher <dv.bltchr@gmail.com>
2024-10-30 17:13:38 +0000

    add model

--- common/app/services/newsletters/model/NewsletterResponse.scala
+++ common/app/services/newsletters/model/NewsletterResponse.scala
@@ -73,3 +73,29 @@ object NewslettersGetResponseV2Body {
   implicit val newslettersGetResponseV2BodyWrites: OWrites[NewslettersGetResponseV2Body] =
     Json.writes[NewslettersGetResponseV2Body]
 }
+
+case class NewsletterLayoutGroup (
+  title: String,
+  subtitle: Option[String],
+  newsletters: List[String],
+)
+
+object NewsletterLayoutGroup {
+    implicit val newsletterLayoutGroupReads: Reads[NewsletterLayoutGroup] =
+    Json.reads[NewsletterLayoutGroup]
+  implicit val newsletterLayoutGroupWrites: OWrites[NewsletterLayoutGroup] =
+    Json.writes[NewsletterLayoutGroup]
+}
+
+case class NewsletterLayoutsResponseBody(
+    ok: Boolean,
+    total: Int,
+    data: Map[String, List[NewsletterLayoutGroup]]
+)
+
+object NewsletterLayoutsResponseBody {
+    implicit val newsletterLayoutsResponseBodyReads: Reads[NewsletterLayoutsResponseBody] =
+    Json.reads[NewsletterLayoutsResponseBody]
+  implicit val newsletterLayoutsResponseBodyWrites: OWrites[NewsletterLayoutsResponseBody] =
+    Json.writes[NewsletterLayoutsResponseBody]
+}
