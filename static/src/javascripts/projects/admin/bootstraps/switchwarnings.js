e959462ee8de858702ce45e4d28430c704bfb1c5
Imogen Hardy <imogen.hardy@guardian.co.uk>
2024-11-11 15:20:43 +0000

    track amount of high impact switches turned off

--- static/src/javascripts/projects/admin/bootstraps/switchwarnings.js
+++ static/src/javascripts/projects/admin/bootstraps/switchwarnings.js
@@ -1,25 +1,35 @@
 const init = () => {
-    const $switchboard = ((document.querySelector(
-        '#switchboard'
-    )));
-    if (!$switchboard) return;
+	const $switchboard = document.querySelector('#switchboard');
+	if (!$switchboard) return;
 
-    const $highImpactSwitches = $switchboard.querySelectorAll('[data-high-impact="true"]');
-    const $saveButton = document.querySelector('input[value="Save"]');
+	const $highImpactSwitches = Array.from(
+		$switchboard.querySelectorAll('[data-high-impact="true"]'),
+	);
+	const $saveButton = document.querySelector('input[value="Save"]');
 
-    if (!$highImpactSwitches.length) return;
+	if (!$highImpactSwitches.length) return;
 
-    $highImpactSwitches.forEach($highImpactSwitch => {
-        $highImpactSwitch.addEventListener('change', (event) => {
-            event.preventDefault();
-            const changeCancelled = !confirm($highImpactSwitch.dataset.impactWarning);
-            if (changeCancelled) {
-                event.currentTarget.checked = !event.currentTarget.checked;
-            } else {
-                $saveButton.classList.add('btn-danger');
-            }
-        });
-    })
+	$highImpactSwitches.forEach(($highImpactSwitch) => {
+		$highImpactSwitch.addEventListener('change', (event) => {
+			event.preventDefault();
+
+			const changeCancelled = !confirm(
+				$highImpactSwitch.dataset.impactWarning,
+			);
+			if (changeCancelled) {
+				event.currentTarget.checked = !event.currentTarget.checked;
+			}
+
+			const makeButtonScary = $highImpactSwitches.some(
+				(highImpactSwitch) => !highImpactSwitch.checked,
+			);
+			if (makeButtonScary) {
+				$saveButton.classList.add('btn-danger');
+			} else {
+				$saveButton.classList.remove('btn-danger');
+			}
+		});
+	});
 };
 
-export { init }
+export { init };
