bfc92998cd1ce978c497b69e3aed4f3e49988614
Roberto Tyley <52038+rtyley@users.noreply.github.com>
2025-01-08 18:37:10 +0000

    Enable ETag-caching on Facia API requests
    With https://github.com/guardian/facia-scala-client/pull/287, the Facia client
    has been updated so that it can take advantage of ETag-caching, using the
    https://github.com/guardian/etag-caching library already used in Frontend since
    August 2023 with https://github.com/guardian/frontend/pull/26338.
    
    As some changes were necessary in the `etag-caching` library to facilitate this,
    we need to update the version of `etag-caching` used by Frontend to v7.0.0.
    
    We're also upgrading the S3 client (used by Frontend's instance of Facia's
    `ApiClient`) to AWS SDK v2, as `etag-caching` already provides a convenient
    implementation of `S3ByteArrayFetching` (see https://github.com/guardian/etag-caching/pull/65)
    for AWS SDK v2 - if we continued using AWS SDK v1 here in Frontend, we'd have
    to create an implementation for it.
    
    ## See also
    
    * https://github.com/guardian/maintaining-scala-projects/issues/9
    * https://github.com/guardian/facia-scala-client/pull/287

--- common/app/utils/AWSv2.scala
+++ common/app/utils/AWSv2.scala
@@ -6,6 +6,9 @@ import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.regions.Region.EU_WEST_1
 import software.amazon.awssdk.services.s3.{S3AsyncClient, S3AsyncClientBuilder}
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest
+import software.amazon.awssdk.services.sts.{StsClient, StsClientBuilder}
 
 object AWSv2 {
   val region: Region = EU_WEST_1
@@ -16,10 +19,25 @@ object AWSv2 {
   lazy val credentials: AwsCredentialsProvider =
     credentialsForDevAndProd("frontend", InstanceProfileCredentialsProvider.create())
 
-  def build[T, B <: AwsClientBuilder[B, T]](builder: B): T =
-    builder.credentialsProvider(credentials).region(region).build()
+  def build[T, B <: AwsClientBuilder[B, T]](builder: B, creds: AwsCredentialsProvider = credentials): T =
+    builder.credentialsProvider(creds).region(region).build()
 
-  val S3Async: S3AsyncClient = build[S3AsyncClient, S3AsyncClientBuilder](
+  def buildS3AsyncClient(creds: AwsCredentialsProvider): S3AsyncClient = build[S3AsyncClient, S3AsyncClientBuilder](
     S3AsyncClient.builder().httpClientBuilder(NettyNioAsyncHttpClient.builder().maxConcurrency(250)),
+    creds,
   )
+
+  val S3Async: S3AsyncClient = buildS3AsyncClient(credentials)
+
+  val STS: StsClient = build[StsClient, StsClientBuilder](StsClient.builder())
+
+  def stsCredentials(devProfile: String, roleArn: String): AwsCredentialsProvider = credentialsForDevAndProd(
+    devProfile,
+    StsAssumeRoleCredentialsProvider
+      .builder()
+      .stsClient(STS)
+      .refreshRequest(AssumeRoleRequest.builder.roleSessionName("frontend").roleArn(roleArn).build)
+      .build(),
+  )
+
 }
