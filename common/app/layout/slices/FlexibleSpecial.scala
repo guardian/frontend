5ee5f2606226af67260b08eca2056bb1c1138c63
Georges Lebreton <102960844+Georges-GNM@users.noreply.github.com>
2025-01-09 15:05:50 +0000

    Wip commit - passing max items to flex gen slice definition

--- common/app/layout/slices/FlexibleSpecial.scala
+++ common/app/layout/slices/FlexibleSpecial.scala
@@ -13,7 +13,7 @@ object FlexibleSpecial extends FlexibleContainer {
     }
   }
 
-  override protected def standardSlices(storiesIncludingBackfill: Seq[Story], firstSlice: Option[Slice]): Seq[Slice] = {
+  override protected def standardSlices(storiesIncludingBackfill: Seq[Story], firstSlice: Option[Slice], flexGenMaxItems: Int): Seq[Slice] = {
 
     storiesIncludingBackfill.length match {
       case 0 => Nil
