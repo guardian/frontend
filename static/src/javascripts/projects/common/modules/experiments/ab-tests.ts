6e8af76524a50eb78c1be5ac897fa5e38ed9e4f3
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-01-27 14:12:46 +0000

    Update ab-tests.ts

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -1,6 +1,6 @@
 import type { ABTest } from '@guardian/ab-core';
-import { consentOrPayBannerTest } from './tests/consent-or-pay-banner';
 import { auxiaSignInGate } from './tests/auxia-sign-in-gate';
+import { consentOrPayBannerTest } from './tests/consent-or-pay-banner';
 import { remoteRRHeaderLinksTest } from './tests/remote-header-test';
 import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
