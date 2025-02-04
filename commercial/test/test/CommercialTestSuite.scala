b7cc6a7d62f2d40c92510c9eb68c599cd2ff86c8
Jake Lee Kennedy <jlkennedy127@gmail.com>
2025-02-04 17:16:15 +0000

    delete old tests

--- commercial/test/test/CommercialTestSuite.scala
+++ commercial/test/test/CommercialTestSuite.scala
@@ -1,13 +1,11 @@
 package commercial.test
 
 import commercial.model.capi.LookupTest
-import commercial.model.merchandise.jobs
 import org.scalatest.Suites
 import test.SingleServerSuite
 
 class CommercialTestSuite
     extends Suites(
-      new jobs.JobTest,
       new LookupTest,
     )
     with SingleServerSuite {}
