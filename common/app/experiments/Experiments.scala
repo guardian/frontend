3d374af8192fafebdd590d20d928d645856462e9
Dominik Lander <dominik.lander@guardian.co.uk>
2025-02-06 16:28:25 +0000

    Add 0% server-side experiment for looping video test
    Co-authored-by: Freddie Preece <freddiepreece@outlook.com>

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -12,6 +12,7 @@ object ActiveExperiments extends ExperimentsDefinition {
   override val allExperiments: Set[Experiment] =
     Set(
       EuropeBetaFront,
+      LoopVideoTest,
       DCRCrosswords,
       DarkModeWeb,
     )
@@ -29,6 +30,15 @@ object EuropeBetaFront
       ),
       sellByDate = LocalDate.of(2025, 4, 2),
       participationGroup = Perc0A,
+    )
+
+object LoopVideoTest
+    extends Experiment(
+      name = "loop-video-test",
+      description = "Test looping videos effect on Core Web Vitals",
+      owners = Seq(Owner.withEmail("fronts.and.curation@guardian.co.uk")),
+      sellByDate = LocalDate.of(2025, 5, 28),
+      participationGroup = Perc0B,
     )
 
 object DCRCrosswords
