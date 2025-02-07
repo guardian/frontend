809f46fac054fc15a18a561dcadc4b62781c788a
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-02-07 10:58:09 +0000

    Add Consent or Pay switch

--- common/app/conf/switches/PrivacySwitches.scala
+++ common/app/conf/switches/PrivacySwitches.scala
@@ -20,4 +20,16 @@ trait PrivacySwitches {
       "Warning: Requires ExCo sign-off. Disabling this switch will cost £160,000/day in ad-revenue, impact marketing, impact reader revenue...",
     ),
   )
+
+  val ConsentOrPay = Switch(
+    group = SwitchGroup.Feature,
+    name = "consent-or-pay",
+    description = "Enable consent or pay logic",
+    owners =
+      Seq(Owner.withEmail("Transparency.and.consent@guardian.co.uk"), Owner.withEmail("identitydev@guardian.co.uk")),
+    safeState = On,
+    sellByDate = never,
+    exposeClientSide = true,
+    highImpact = true,
+  )
 }
