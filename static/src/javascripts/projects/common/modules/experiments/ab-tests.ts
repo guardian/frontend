2a5e27ea89a01e7e0e7219a9bcc1145ebb1852d0
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2025-02-12 16:07:26 +0000

    Add ab test for 5:4 images in articles

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -3,6 +3,7 @@ import { auxiaSignInGate } from './tests/auxia-sign-in-gate';
 import { remoteRRHeaderLinksTest } from './tests/remote-header-test';
 import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
+import {fiveFourImages} from "./tests/five-four-images";
 
 // keep in sync with ab-tests in dotcom-rendering
 // https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/experiments/ab-tests.ts
@@ -11,4 +12,5 @@ export const concurrentTests: readonly ABTest[] = [
 	signInGateMainControl,
 	remoteRRHeaderLinksTest,
 	auxiaSignInGate,
+	fiveFourImages,
 ];
