1083d7cadb6d6cb7cba545818a90fcce0821a320
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-02-19 18:06:43 +0000

    Remove unused files

--- static/src/javascripts/projects/common/modules/userFeatures/cookies/hideSupportMessaging.ts
+++
@@ -1,9 +0,0 @@
-import { getCookie } from '@guardian/libs';
-
-export const HIDE_SUPPORT_MESSAGING_COOKIE = 'gu_hide_support_messaging';
-
-export const hideSupportMessaging = (): boolean =>
-	getHideSupportMessagingCookie() !== null;
-
-export const getHideSupportMessagingCookie = (): string | null =>
-	getCookie({ name: HIDE_SUPPORT_MESSAGING_COOKIE });
