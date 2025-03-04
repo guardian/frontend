f52a597daf9a0b862352c05a2fbf483a60d2c61f
Dina Hafez <dina.hafez@guardian.co.uk>
2025-03-04 10:50:39 +0000

    Setup switch for prebid bid cache

--- static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
+++ static/src/javascripts/projects/common/modules/experiments/ab-tests.ts
@@ -6,7 +6,7 @@ import { signInGateMainControl } from './tests/sign-in-gate-main-control';
 import { signInGateMainVariant } from './tests/sign-in-gate-main-variant';
 
 // keep in sync with ab-tests in dotcom-rendering
-// https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/experiments/ab-tests.ts
+// https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/experiments/ab-tests.ts
 export const concurrentTests: readonly ABTest[] = [
 	signInGateMainVariant,
 	signInGateMainControl,
