3a1cdb633ed866025a13fd82c05217c8072f7ac3
Anna Beddow <anna.beddow@gmail.com>
2025-03-06 18:02:21 +0000

    Bump facia

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val awsVersion = "1.12.780"
   val awsSdk2Version = "2.26.31"
   val capiVersion = "34.0.0"
-  val faciaVersion = "16.0.0"
+  val faciaVersion = "16.1.0-PREVIEW.absublink-limiting.2025-03-06T1724.2a877e81"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
