dca9507b5d8c2888145c2ddfafb741676f64d75e
Jake <jake.kennedy@guardian.co.uk>
2025-03-12 17:15:43 +0000

    add a couple of commercial test switches (#27841)

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -48,4 +48,26 @@ trait ABTestSwitches {
     exposeClientSide = true,
     highImpact = false,
   )
+
+  Switch(
+    ABTests,
+    "ab-prebid-bid-cache",
+    "Test the impact of enabling prebid bid caching",
+    owners = Seq(Owner.withEmail("commercial.dev@theguardian.com")),
+    safeState = Off,
+    sellByDate = Some(LocalDate.of(2025, 3, 28)),
+    exposeClientSide = true,
+    highImpact = false,
+  )
+
+  Switch(
+    ABTests,
+    "ab-the-trade-desk",
+    "Test the impact of disabling the trade desk for some of our users",
+    owners = Seq(Owner.withEmail("commercial.dev@theguardian.com")),
+    safeState = Off,
+    sellByDate = Some(LocalDate.of(2025, 3, 28)),
+    exposeClientSide = true,
+    highImpact = false,
+  )
 }
