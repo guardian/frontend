d9b2c39d2191bbd193029ba2088be762e8effee8
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2025-03-20 16:19:46 +0000

    Okta tidy up

--- common/app/conf/switches/IdentitySwitches.scala
+++ common/app/conf/switches/IdentitySwitches.scala
@@ -1,32 +1,3 @@
 package conf.switches
 
-<<<<<<< Updated upstream
-import conf.switches.Expiry.never
-
-trait IdentitySwitches {
-
-  val IdentityCookieRefreshSwitch = Switch(
-    SwitchGroup.Identity,
-    "id-cookie-refresh",
-    "If switched on, users cookies will be refreshed.",
-    owners = Seq(Owner.withGithub("johnduffell")),
-    safeState = Off,
-    sellByDate = never,
-    exposeClientSide = true,
-    highImpact = false,
-  )
-
-  val Okta = Switch(
-    group = SwitchGroup.Identity,
-    name = "okta",
-    description = "Use Okta for authentication",
-    owners = Seq(Owner.withGithub("@guardian/dotcom-platform")),
-    safeState = Off,
-    sellByDate = never,
-    exposeClientSide = true,
-    highImpact = false,
-  )
-}
-=======
 trait IdentitySwitches {}
->>>>>>> Stashed changes
