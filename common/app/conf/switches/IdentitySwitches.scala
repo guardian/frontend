b0176187172939c7a4db3192b22d1189b2f71e69
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2025-04-02 16:08:37 +0100

    Keep cookie refresh functionality

--- common/app/conf/switches/IdentitySwitches.scala
+++ common/app/conf/switches/IdentitySwitches.scala
@@ -1,3 +1,17 @@
 package conf.switches
 
-trait IdentitySwitches {}
+import conf.switches.Expiry.never
+
+trait IdentitySwitches {
+
+  val IdentityCookieRefreshSwitch = Switch(
+    SwitchGroup.Identity,
+    "id-cookie-refresh",
+    "If switched on, users cookies will be refreshed.",
+    owners = Seq(Owner.withGithub("johnduffell")),
+    safeState = Off,
+    sellByDate = never,
+    exposeClientSide = true,
+    highImpact = false,
+  )
+}
