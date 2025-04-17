d1fbc6840eb279ae47b4320c9206a5e575f46d44
Demetrios_Skamiotis <demetrios_skamiotis@36930_mac.gnm.int>
2025-04-17 12:36:58 +0100

    reomve defer permutive AB test switch

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -40,17 +40,6 @@ trait ABTestSwitches {
 
   Switch(
     ABTests,
-    "ab-defer-permutive-load",
-    "Test the impact of deferring the Permutive script load",
-    owners = Seq(Owner.withEmail("commercial.dev@theguardian.com")),
-    safeState = Off,
-    sellByDate = Some(LocalDate.of(2025, 4, 18)),
-    exposeClientSide = true,
-    highImpact = false,
-  )
-
-  Switch(
-    ABTests,
     "ab-a9-bid-response-winner",
     "The test will enable checking the A9 bid response and determining a winning ad",
     owners = Seq(Owner.withEmail("commercial.dev@theguardian.com")),
