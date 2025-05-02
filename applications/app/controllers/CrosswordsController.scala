4baa9aa724d6b9e3f6820fd25a8ab1c0474e9143
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2025-05-02 12:35:22 +0100

    Use CrosswordData for editions

--- applications/app/controllers/CrosswordsController.scala
+++ applications/app/controllers/CrosswordsController.scala
@@ -350,7 +350,13 @@ class CrosswordEditionsController(
     "crosswords/series/quiptic",
   ).mkString("|")
 
-  private def parseCrosswords(response: SearchResponse): EditionsCrosswordRenderingDataModel =
-    EditionsCrosswordRenderingDataModel(response.results.flatMap(_.crossword))
+  private def parseCrosswords(response: SearchResponse): EditionsCrosswordRenderingDataModel = {
+    val collectedItems = response.results.collect {
+      case content if content.crossword.isDefined =>
+        CrosswordData.fromCrossword(content.crossword.get, content)
+    }
+    val crosswordDataItems: scala.collection.immutable.Seq[CrosswordData] = collectedItems.toList
+    EditionsCrosswordRenderingDataModel(crosswordDataItems)
+  }
 
 }
