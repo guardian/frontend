765f3c38eb1bbc30deda3b4f79d6bc78e09dcf2f
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2025-05-06 10:25:50 +0100

    Editions crossword refactor

--- common/app/model/dotcomrendering/pageElements/EditionsCrosswordRenderingDataModel.scala
+++ common/app/model/dotcomrendering/pageElements/EditionsCrosswordRenderingDataModel.scala
@@ -1,41 +1,15 @@
 package model.dotcomrendering.pageElements
-
-import com.gu.contentapi.client.model.v1.Crossword
-import com.gu.contentapi.json.CirceEncoders._
-import io.circe.syntax._
-import model.Cached.CapiRichDateTime
 import model.CrosswordData
 import play.api.libs.json.{JsValue, Json}
 
 case class EditionsCrosswordRenderingDataModel(
-    crosswords: Iterable[Crossword],
-    newCrosswords: Iterable[CrosswordData],
+    crosswords: Iterable[CrosswordData],
 )
 
 object EditionsCrosswordRenderingDataModel {
-  def apply(
-      crosswords: Iterable[Crossword],
-      newCrosswords: Iterable[CrosswordData],
-  ): EditionsCrosswordRenderingDataModel =
-    new EditionsCrosswordRenderingDataModel(
-      crosswords.map(crossword => {
-        val shipSolutions =
-          crossword.dateSolutionAvailable
-            .map(_.toJoda.isBeforeNow)
-            .getOrElse(crossword.solutionAvailable)
-
-        if (shipSolutions) {
-          crossword
-        } else {
-          crossword.copy(entries = crossword.entries.map(_.copy(solution = None)))
-        }
-      }),
-      newCrosswords,
-    )
   def toJson(model: EditionsCrosswordRenderingDataModel): JsValue = {
     Json.obj(
-      "crosswords" -> Json.parse(model.crosswords.asJson.deepDropNullValues.toString()),
-      "newCrosswords" -> Json.toJson(model.newCrosswords),
+      "newCrosswords" -> Json.toJson(model.crosswords),
     )
   }
 }
