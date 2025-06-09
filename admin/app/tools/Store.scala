84d1875f179ec8566ba5f3f2adc84b30d9ff0a30
Demetrios <dskamiotis@gmail.com>
2025-06-09 14:19:57 +0100

    remove survey-sponsorships from writing to S3

--- admin/app/tools/Store.scala
+++ admin/app/tools/Store.scala
@@ -24,9 +24,6 @@ trait Store extends GuLogging with Dates {
   def getTopStories: Option[String] = S3.get(topStoriesKey)
   def putTopStories(config: String): Unit = { S3.putPublic(topStoriesKey, config, "application/json") }
 
-  def putSurveySponsorships(adUnitJson: String): Unit = {
-    S3.putPrivate(dfpSurveySponsorshipDataKey, adUnitJson, defaultJsonEncoding)
-  }
   def putDfpPageSkinAdUnits(adUnitJson: String): Unit = {
     S3.putPrivate(dfpPageSkinnedAdUnitsKey, adUnitJson, defaultJsonEncoding)
   }
