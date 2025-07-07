e1700cd02fcdf644f8731d4c6acf1dddf0c1aca6
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2025-07-07 12:57:14 +0100

    reduce frequency of preview rugby jobs

--- sport/app/rugby/conf/RugbyLifecycle.scala
+++ sport/app/rugby/conf/RugbyLifecycle.scala
@@ -1,7 +1,8 @@
 package rugby.conf
 
 import app.LifecycleComponent
-import common.{PekkoAsync, JobScheduler}
+import common.{JobScheduler, PekkoAsync}
+import model.ApplicationContext
 import play.api.inject.ApplicationLifecycle
 import rugby.feed.CapiFeed
 import rugby.jobs.RugbyStatsJob
@@ -15,7 +16,7 @@ class RugbyLifecycle(
     pekkoAsync: PekkoAsync,
     rugbyStatsJob: RugbyStatsJob,
     capiFeed: CapiFeed,
-)(implicit ec: ExecutionContext)
+)(implicit ec: ExecutionContext, context: ApplicationContext)
     extends LifecycleComponent {
 
   protected val initializationTimeout: FiniteDuration = 10.seconds
@@ -24,12 +25,16 @@ class RugbyLifecycle(
     rugbyStatsJob.fetchFixturesAndResults()
 
     jobs.deschedule("FixturesAndResults")
-    jobs.schedule("FixturesAndResults", "30 * * * * ?") {
+    // if preview "Every hour at 50 minutes" (e.g., 1:50, 2:50, 3:50)
+    // otherwise "At 30 seconds past the minute, every minute" (e.g., 00:30, 01:30, 02:30, etc.)
+    jobs.schedule("FixturesAndResults", if (context.isPreview) "0 50 * * * ?" else "30 * * * * ?") {
       rugbyStatsJob.fetchFixturesAndResults()
     }
 
     jobs.deschedule("MatchNavArticles")
-    jobs.schedule("MatchNavArticles", "35 0/30 * * * ?") {
+    // if preview "Every hour at 55 minutes" (e.g., 1:55, 2:55, 3:55)
+    // otherwise "At 35 seconds past the minute, every 30 minutes" (e.g., 00:35, 00:05, 01:35, 01:05, etc.)
+    jobs.schedule("MatchNavArticles", if (context.isPreview) "0 55 * * * ?" else "35 0/30 * * * ?") {
       val refreshedNavContent = capiFeed.getMatchArticles(rugbyStatsJob.getAllResults())
       rugbyStatsJob.sendMatchArticles(refreshedNavContent)
     }
