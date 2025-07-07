f197abaa3c5453aa5cc0a4f7551cbe3b71849d60
Charley_Campbell <charley.campbell@guardian.co.uk>
2025-07-07 16:54:43 +0100

    Bumped facia version and contentApiModelsJson

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val awsVersion = "1.12.782"
   val awsSdk2Version = "2.30.38"
   val capiVersion = "35.0.0"
-  val faciaVersion = "21.0.0"
+  val faciaVersion = "22.0.0"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
@@ -33,7 +33,7 @@ object Dependencies {
   val commonsIo = "commons-io" % "commons-io" % "2.16.1"
   val cssParser = "net.sourceforge.cssparser" % "cssparser" % "0.9.30"
   val contentApiClient = "com.gu" %% "content-api-client" % capiVersion
-  val contentApiModelsJson = "com.gu" %% "content-api-models-json" % "27.1.0"
+  val contentApiModelsJson = "com.gu" %% "content-api-models-json" % "29.0.0"
   val dfpAxis = "com.google.api-ads" % "dfp-axis" % "5.8.0"
   val faciaFapiScalaClient = "com.gu" %% "fapi-client-play30" % faciaVersion
   val identityCookie = "com.gu.identity" %% "identity-cookie" % identityLibVersion
