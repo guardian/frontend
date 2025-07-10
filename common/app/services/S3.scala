guardian/frontend

commit 54708876910d523c7b482cf15dc65257d47f6388

Migrate to v2 of the AWS Java SDK for S3 (#27969)
The first part of the S3 migration.

Panda settings require an instance of an `S3BucketLoader`. Panda comes
with a default implementation for v1, but not v2, so this change creates
one.

This first requires a synchronous S3 client, which has been added to the
`AWSv2` object alongside the existing asynchronous S3 client. Creating
it here, rather than in the controller as before, allows it to be used
more widely.

Then the `getObject` method can be applied, with a `ResponseTransformer`
to retrieve the "content" of the response, i.e. the object itself.

We're removing `S3Client` as it became unused with
https://github.com/guardian/frontend/pull/27391

We've also added support for `getPresignedUrl`, which was introduced with
https://github.com/guardian/frontend/pull/28000 after we started this PR!

Co-authored-by: Roberto Tyley <52038+rtyley@users.noreply.github.com>

common/app/services/S3.scala
@@ -1,168 +1,129 @@
 package services
 
-import com.amazonaws.services.s3.model.CannedAccessControlList.{Private, PublicRead}
-import com.amazonaws.services.s3.model._
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3Client}
-import com.amazonaws.util.StringInputStream
 import com.gu.etagcaching.aws.s3.ObjectId
 import common.GuLogging
 import conf.Configuration
 import model.PressedPageType
 import org.joda.time.DateTime
 import services.S3.logS3ExceptionWithDevHint
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL.{PRIVATE, PUBLIC_READ}
+import software.amazon.awssdk.services.s3.model._
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
+import utils.AWSv2
 
 import java.io._
+import java.nio.charset.StandardCharsets.UTF_8
+import java.time.Duration.ofMinutes
+import java.time.Instant
 import java.util.zip.GZIPOutputStream
 import scala.io.{Codec, Source}
-import java.util.Date
-import java.time.Instant
-import java.util.concurrent.TimeUnit
 
 trait S3 extends GuLogging {
 
   lazy val bucket = Configuration.aws.frontendStoreBucket
 
-  lazy val client: Option[AmazonS3] = Configuration.aws.credentials.map { credentials =>
-    AmazonS3Client.builder
-      .withCredentials(credentials)
-      .withRegion(conf.Configuration.aws.region)
-      .build()
+  lazy private val client = AWSv2.S3Sync
+
+  def handleS3Errors[T](key: String)(thunk: => T): Option[T] = {
+    val objectId = ObjectId(bucket, key)
+    try {
+      Some(thunk)
+    } catch {
+      case e: NoSuchKeyException if e.statusCode == 404 =>
+        log.warn(s"not found at ${objectId.s3Uri}")
+        None
+      case e: software.amazon.awssdk.services.s3.model.S3Exception =>
+        logS3ExceptionWithDevHint(objectId, e)
+        None
+      case e: Exception =>
+        throw e
+    }
   }
 
-  private def withS3Result[T](key: String)(action: S3Object => T): Option[T] =
-    client.flatMap { client =>
-      val objectId = ObjectId(bucket, key)
-      try {
-        val request = new GetObjectRequest(bucket, key)
-        val result = client.getObject(request)
-        log.info(s"S3 got ${result.getObjectMetadata.getContentLength} bytes from ${result.getKey}")
+  def get(key: String)(implicit codec: Codec): Option[String] = handleS3Errors(key)(getResponseAndContent(key)._2)
 
-        // http://stackoverflow.com/questions/17782937/connectionpooltimeoutexception-when-iterating-objects-in-s3
-        try {
-          Some(action(result))
-        } catch {
-          case e: Exception =>
-            throw e
-        } finally {
-          result.close()
-        }
-      } catch {
-        case e: AmazonS3Exception if e.getStatusCode == 404 =>
-          log.warn(s"not found at ${objectId.s3Uri}")
-          None
-        case e: AmazonS3Exception =>
-          logS3ExceptionWithDevHint(objectId, e)
-          None
-        case e: Exception =>
-          throw e
-      }
-    }
+  def getPresignedUrl(key: String): Option[String] = handleS3Errors(key) {
+    val presignRequest = GetObjectPresignRequest.builder
+      .signatureDuration(ofMinutes(10))
+      .getObjectRequest(GetObjectRequest.builder.bucket(bucket).key(key).build)
+      .build()
 
-  def get(key: String)(implicit codec: Codec): Option[String] =
-    withS3Result(key) { result =>
-      Source.fromInputStream(result.getObjectContent).mkString
-    }
+    S3.presigner.presignGetObject(presignRequest).url.toExternalForm
+  }
 
-  def getPresignedUrl(key: String): Option[String] =
-    client.flatMap { client =>
-      val objectId = ObjectId(bucket, key)
-      val futureInstant = Instant.now().plusMillis(TimeUnit.MINUTES.toMillis(10))
-      try {
-        Some(client.generatePresignedUrl(bucket, key, Date.from(futureInstant)).toExternalForm())
-      } catch {
-        case e: AmazonS3Exception if e.getStatusCode == 404 =>
-          log.warn(s"not found at ${objectId.s3Uri}")
-          None
-        case e: AmazonS3Exception =>
-          logS3ExceptionWithDevHint(objectId, e)
-          None
-        case e: Exception =>
-          throw e
-      }
-    }
+  private def toDateTime(instant: Instant): DateTime = new DateTime(instant.toEpochMilli)
 
-  def getWithLastModified(key: String): Option[(String, DateTime)] =
-    withS3Result(key) { result =>
-      val content = Source.fromInputStream(result.getObjectContent).mkString
-      val lastModified = new DateTime(result.getObjectMetadata.getLastModified)
-      (content, lastModified)
-    }
+  def getWithLastModified(key: String): Option[(String, DateTime)] = handleS3Errors(key) {
+    val (resp, content) = getResponseAndContent(key)
+    val lastModified = toDateTime(resp.lastModified())
+    (content, lastModified)
+  }
 
-  def getLastModified(key: String): Option[DateTime] =
-    withS3Result(key) { result =>
-      new DateTime(result.getObjectMetadata.getLastModified)
+  private def getResponseAndContent(key: String)(implicit codec: Codec): (GetObjectResponse, String) = {
+    val request = GetObjectRequest.builder().bucket(bucket).key(key).build()
+    val resp = client.getObject(request)
+    val objectResponse = resp.response()
+    log.info(s"S3 got ${objectResponse.contentLength} bytes from $key")
+    try {
+      val content = Source.fromInputStream(resp).mkString
+      (objectResponse, content)
+    } finally {
+      resp.close()
     }
+  }
+
+  def getLastModified(key: String): Option[DateTime] = handleS3Errors(key) {
+    val request = HeadObjectRequest.builder().bucket(bucket).key(key).build()
+    toDateTime(client.headObject(request).lastModified())
+  }
 
   def putPublic(key: String, value: String, contentType: String): Unit = {
-    put(key: String, value: String, contentType: String, PublicRead)
-  }
-
-  def putPublic(key: String, file: File, contentType: String): Unit = {
-    val request = new PutObjectRequest(bucket, key, file).withCannedAcl(PublicRead)
-    client.foreach(_.putObject(request))
+    put(key: String, value: String, contentType: String, PUBLIC_READ)
   }
 
   def putPrivate(key: String, value: String, contentType: String): Unit = {
-    put(key: String, value: String, contentType: String, Private)
+    put(key: String, value: String, contentType: String, PRIVATE)
   }
 
   def putPrivateGzipped(key: String, value: String, contentType: String): Unit = {
-    putGzipped(key, value, contentType, Private)
+    val os = new ByteArrayOutputStream()
+    val gzippedStream = new GZIPOutputStream(os)
+    gzippedStream.write(value.getBytes(UTF_8))
+    gzippedStream.flush()
+    gzippedStream.close()
+
+    val request = PutObjectRequest
+      .builder()
+      .bucket(bucket)
+      .key(key)
+      .acl(PRIVATE)
+      .cacheControl("no-cache,no-store")
+      .contentType(contentType)
+      .contentEncoding("gzip")
+      .build()
+
+    client.putObject(request, RequestBody.fromBytes(os.toByteArray))
   }
 
-  private def putGzipped(
-      key: String,
-      value: String,
-      contentType: String,
-      accessControlList: CannedAccessControlList,
-  ): Unit = {
-    lazy val request = {
-      val metadata = new ObjectMetadata()
+  private def put(key: String, value: String, contentType: String, accessControlList: ObjectCannedACL): Unit = {
+    val request = PutObjectRequest
+      .builder()
+      .bucket(bucket)
+      .key(key)
+      .acl(accessControlList)
+      .cacheControl("no-cache,no-store")
+      .contentType(contentType)
+      .build()
 
-      metadata.setCacheControl("no-cache,no-store")
-      metadata.setContentType(contentType)
-      metadata.setContentEncoding("gzip")
-
-      val valueAsBytes = value.getBytes("UTF-8")
-      val os = new ByteArrayOutputStream()
-      val gzippedStream = new GZIPOutputStream(os)
-      gzippedStream.write(valueAsBytes)
-      gzippedStream.flush()
-      gzippedStream.close()
-
-      metadata.setContentLength(os.size())
-
-      new PutObjectRequest(bucket, key, new ByteArrayInputStream(os.toByteArray), metadata)
-        .withCannedAcl(accessControlList)
-    }
-
-    try {
-      client.foreach(_.putObject(request))
-    } catch {
-      case e: Exception =>
-        throw e
-    }
-  }
-
-  private def put(key: String, value: String, contentType: String, accessControlList: CannedAccessControlList): Unit = {
-    val metadata = new ObjectMetadata()
-    metadata.setCacheControl("no-cache,no-store")
-    metadata.setContentType(contentType)
-    metadata.setContentLength(value.getBytes("UTF-8").length)
-
-    val request =
-      new PutObjectRequest(bucket, key, new StringInputStream(value), metadata).withCannedAcl(accessControlList)
-
-    try {
-      client.foreach(_.putObject(request))
-    } catch {
-      case e: Exception =>
-        throw e
-    }
+    client.putObject(request, RequestBody.fromString(value, UTF_8))
   }
 }
 
 object S3 extends S3 {
+  val presigner = S3Presigner.create
+
   def logS3ExceptionWithDevHint(s3ObjectId: ObjectId, e: Exception): Unit = {
     val errorMsg = s"Unable to fetch S3 object (${s3ObjectId.s3Uri})"
     val hintMsg = "Hint: your AWS credentials might be missing or expired. You can fetch new ones using Janus."
@@ -190,7 +151,6 @@ object S3FrontsApi extends S3 {
 object S3Archive extends S3 {
   override lazy val bucket: String =
     if (Configuration.environment.isNonProd) "aws-frontend-archive-code" else "aws-frontend-archive"
-  def getHtml(path: String): Option[String] = get(path)
 }
 
 object S3ArchiveOriginals extends S3 {

common/app/utils/AWSv2.scala
@@ -5,7 +5,7 @@ import software.amazon.awssdk.awscore.client.builder.AwsClientBuilder
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.regions.Region.EU_WEST_1
-import software.amazon.awssdk.services.s3.{S3AsyncClient, S3AsyncClientBuilder}
+import software.amazon.awssdk.services.s3.{S3AsyncClient, S3AsyncClientBuilder, S3Client, S3ClientBuilder}
 import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider
 import software.amazon.awssdk.services.sts.model.AssumeRoleRequest
 import software.amazon.awssdk.services.sts.{StsClient, StsClientBuilder}
@@ -28,6 +28,8 @@ object AWSv2 {
   )
 
   val S3Async: S3AsyncClient = buildS3AsyncClient(credentials)
+
+  val S3Sync: S3Client = build[S3Client, S3ClientBuilder](S3Client.builder())
 
   val STS: StsClient = build[StsClient, StsClientBuilder](StsClient.builder())
 

admin/app/controllers/AdminControllers.scala
@@ -1,6 +1,6 @@
 package controllers
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import software.amazon.awssdk.services.s3.S3Client
 import com.softwaremill.macwire._
 import common.PekkoAsync
 import controllers.admin._
@@ -15,6 +15,7 @@ import play.api.mvc.ControllerComponents
 import services.{OphanApi, ParameterStoreService, RedirectService}
 import conf.Configuration.aws.mandatoryCredentials
 import org.apache.pekko.stream.Materializer
+import utils.AWSv2
 
 trait AdminControllers {
   def pekkoAsync: PekkoAsync
@@ -44,20 +45,12 @@ trait AdminControllers {
   def dfpApi: DfpApi
   def parameterStoreService: ParameterStoreService
 
-  private lazy val s3Client = AmazonS3ClientBuilder
-    .standard()
-    .withRegion(Regions.EU_WEST_1)
-    .withCredentials(
-      mandatoryCredentials,
-    )
-    .build()
-
   lazy val auth = new GuardianAuthWithExemptions(
     controllerComponents,
     wsClient,
     toolsDomainPrefix = "frontend",
     oauthCallbackPath = routes.GuardianAuthWithExemptions.oauthCallback.path,
-    s3Client,
+    AWSv2.S3Sync,
     system = "frontend-admin",
     extraDoNotAuthenticatePathPrefixes = Seq(
       // Date: 06 July 2021

article/app/AppLoader.scala
@@ -1,11 +1,9 @@
 import _root_.commercial.targeting.TargetingLifecycle
-import org.apache.pekko.actor.{ActorSystem => PekkoActorSystem}
 import app.{FrontendApplicationLoader, FrontendBuildInfo, FrontendComponents}
 import com.softwaremill.macwire._
 import common._
 import common.dfp.DfpAgentLifecycle
-import concurrent.BlockingOperations
-import conf.{CachedHealthCheckLifeCycle, Configuration}
+import conf.CachedHealthCheckLifeCycle
 import conf.switches.SwitchboardLifecycle
 import contentapi.{CapiHttpClient, ContentApiClient, HttpClient}
 import controllers.{ArticleControllers, HealthCheck}
@@ -13,16 +11,16 @@ import dev.{DevAssetsController, DevParametersHttpRequestHandler}
 import http.{CommonFilters, CorsHttpErrorHandler}
 import jobs.StoreNavigationLifecycleComponent
 import model.ApplicationIdentity
+import org.apache.pekko.actor.{ActorSystem => PekkoActorSystem}
 import play.api.ApplicationLoader.Context
 import play.api.BuiltInComponentsFromContext
 import play.api.http.{HttpErrorHandler, HttpRequestHandler}
 import play.api.mvc.EssentialFilter
 import play.api.routing.Router
 import router.Routes
-import services.fronts.FrontJsonFapiLive
 import services.newsletters.{NewsletterApi, NewsletterSignupAgent, NewsletterSignupLifecycle}
 import services.ophan.SurgingContentAgentLifecycle
-import services.{NewspaperBooksAndSectionsAutoRefresh, OphanApi, S3Client, S3ClientImpl, SkimLinksCacheLifeCycle}
+import services.{NewspaperBooksAndSectionsAutoRefresh, OphanApi, SkimLinksCacheLifeCycle}
 
 class AppLoader extends FrontendApplicationLoader {
   override def buildComponents(context: Context): FrontendComponents =

article/app/controllers/ArticleControllers.scala
@@ -6,8 +6,8 @@ import model.ApplicationContext
 import play.api.libs.ws.WSClient
 import play.api.mvc.ControllerComponents
 import renderers.DotcomRenderingService
-import services.{NewsletterService, NewspaperBookSectionTagAgent, NewspaperBookTagAgent, S3Client}
 import services.newsletters.NewsletterSignupAgent
+import services.{NewsletterService, NewspaperBookSectionTagAgent, NewspaperBookTagAgent}
 
 trait ArticleControllers {
   def contentApiClient: ContentApiClient

article/app/services/S3Client.scala
@@ -1,97 +1,0 @@
-package services
-
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.{GetObjectRequest, S3Object}
-import com.amazonaws.util.IOUtils
-import common.GuLogging
-import play.api.libs.json.{JsError, JsSuccess, Json, Reads}
-
-import scala.concurrent.Future
-import scala.jdk.CollectionConverters._
-import scala.reflect.ClassTag
-import scala.util.{Failure, Success, Try}
-
-trait S3Client[T] {
-  def getListOfKeys(): Future[List[String]]
-
-  def getObject(key: String)(implicit read: Reads[T]): Future[T]
-}
-
-class S3ClientImpl[T](optionalBucket: Option[String])(implicit genericType: ClassTag[T])
-    extends S3Client[T]
-    with S3
-    with GuLogging {
-
-  def getListOfKeys(): Future[List[String]] = {
-    getClient { client =>
-      Try {
-        val s3ObjectList = client.listObjects(getBucket()).getObjectSummaries().asScala.toList
-        s3ObjectList.map(_.getKey)
-      } match {
-        case Success(value) =>
-          log.info(s"got list of ${value.length} $genericTypeName items from S3")
-          Future.successful(value)
-        case Failure(exception) =>
-          log.error(s"failed in getting the list of $genericTypeName items from S3", exception)
-          Future.failed(exception)
-      }
-    }
-  }
-
-  def getObject(key: String)(implicit read: Reads[T]): Future[T] = {
-    getClient { client =>
-      Try {
-        val request = new GetObjectRequest(getBucket(), key)
-        parseResponse(client.getObject(request))
-      }.flatten match {
-        case Success(value) =>
-          log.info(s"got $genericTypeName response from S3 for key ${key}")
-          Future.successful(value)
-        case Failure(exception) =>
-          log.error(s"S3 retrieval failed for $genericTypeName key ${key}", exception)
-          Future.failed(exception)
-      }
-    }
-  }
-
-  private def getBucket() = {
-    optionalBucket.getOrElse(
-      throw new RuntimeException(s"bucket config is empty for $genericTypeName, make sure config parameter has value"),
-    )
-  }
-
-  private def getClient[T](callS3: AmazonS3 => Future[T]) = {
-    client
-      .map { callS3(_) }
-      .getOrElse(Future.failed(new RuntimeException("No client exists for S3Client")))
-  }
-
-  private def parseResponse(s3Object: S3Object)(implicit read: Reads[T]): Try[T] = {
-    val json = Json.parse(asString(s3Object))
-
-    Json.fromJson[T](json) match {
-      case JsSuccess(response, __) =>
-        log.debug(s"Parsed $genericTypeName response from S3 for key ${s3Object.getKey}")
-        Success(response)
-      case JsError(errors) =>
-        val errorPaths = errors.map { error => error._1.toString() }.mkString(",")
-        log.error(s"Error parsing $genericTypeName response from S3 for key ${s3Object.getKey} paths: ${errorPaths}")
-        Failure(
-          new Exception(
-            s"could not parse S3 $genericTypeName response json. Errors paths(s): $errors",
-          ),
-        )
-    }
-  }
-
-  private def asString(s3Object: S3Object): String = {
-    val s3ObjectContent = s3Object.getObjectContent
-    try {
-      IOUtils.toString(s3ObjectContent)
-    } finally {
-      s3ObjectContent.close()
-    }
-  }
-
-  private def genericTypeName = genericType.runtimeClass.getSimpleName
-}

article/test/TestAppLoader.scala
@@ -1,12 +1,8 @@
 import app.FrontendComponents
-import services.S3Client
-import org.mockito.Mockito._
-import org.scalatestplus.mockito.MockitoSugar
 import play.api.ApplicationLoader.Context
 import play.api.BuiltInComponentsFromContext
 import renderers.DotcomRenderingService
-import test.WithTestContentApiClient
-import test.DCRFake
+import test.{DCRFake, WithTestContentApiClient}
 
 trait TestComponents extends WithTestContentApiClient {
   self: AppComponents =>

common/app/http/GuardianAuthWithExemptions.scala
@@ -1,7 +1,8 @@
 package http
 
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.AmazonS3
+import software.amazon.awssdk.core.sync.ResponseTransformer
+import software.amazon.awssdk.services.s3.S3Client
 import com.gu.pandomainauth.action.AuthActions
 import com.gu.pandomainauth.model.AuthenticatedUser
 import com.gu.pandomainauth.{PanDomain, PanDomainAuthSettingsRefresher, S3BucketLoader}
@@ -22,7 +23,7 @@ class GuardianAuthWithExemptions(
     override val wsClient: WSClient,
     toolsDomainPrefix: String,
     oauthCallbackPath: String,
-    s3Client: AmazonS3,
+    s3Client: S3Client,
     system: String,
     extraDoNotAuthenticatePathPrefixes: Seq[String],
     requiredEditorialPermissionName: String,
@@ -57,7 +58,13 @@ class GuardianAuthWithExemptions(
   override lazy val panDomainSettings = PanDomainAuthSettingsRefresher(
     domain = toolsDomainSuffix,
     system,
-    S3BucketLoader.forAwsSdkV1(s3Client, "pan-domain-auth-settings"),
+    new S3BucketLoader {
+      def inputStreamFetching(key: String) =
+        s3Client.getObject(
+          _.bucket("pan-domain-auth-settings").key(key),
+          ResponseTransformer.toInputStream(),
+        )
+    },
   )
 
   override def authCallbackUrl = s"https://$toolsDomainPrefix.$toolsDomainSuffix$oauthCallbackPath"

commit 84736a7232d9e8b94be3fe2ab42fc4484b343fa1

Fix adding region to S3 Presigner - broken in #27969
Co-authored-by: Roberto Tyley <52038+rtyley@users.noreply.github.com>

common/app/services/S3.scala
@@ -50,7 +50,7 @@ trait S3 extends GuLogging {
       .getObjectRequest(GetObjectRequest.builder.bucket(bucket).key(key).build)
       .build()
 
-    S3.presigner.presignGetObject(presignRequest).url.toExternalForm
+    AWSv2.S3PresignerSync.presignGetObject(presignRequest).url.toExternalForm
   }
 
   private def toDateTime(instant: Instant): DateTime = new DateTime(instant.toEpochMilli)
@@ -122,8 +122,6 @@ trait S3 extends GuLogging {
 }
 
 object S3 extends S3 {
-  val presigner = S3Presigner.create
-
   def logS3ExceptionWithDevHint(s3ObjectId: ObjectId, e: Exception): Unit = {
     val errorMsg = s"Unable to fetch S3 object (${s3ObjectId.s3Uri})"
     val hintMsg = "Hint: your AWS credentials might be missing or expired. You can fetch new ones using Janus."

common/app/utils/AWSv2.scala
@@ -5,6 +5,7 @@ import software.amazon.awssdk.awscore.client.builder.AwsClientBuilder
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.regions.Region.EU_WEST_1
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.{S3AsyncClient, S3AsyncClientBuilder, S3Client, S3ClientBuilder}
 import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider
 import software.amazon.awssdk.services.sts.model.AssumeRoleRequest
@@ -30,6 +31,8 @@ object AWSv2 {
   val S3Async: S3AsyncClient = buildS3AsyncClient(credentials)
 
   val S3Sync: S3Client = build[S3Client, S3ClientBuilder](S3Client.builder())
+
+  val S3PresignerSync: S3Presigner = S3Presigner.builder().credentialsProvider(credentials).region(region).build()
 
   val STS: StsClient = build[StsClient, StsClientBuilder](StsClient.builder())
 