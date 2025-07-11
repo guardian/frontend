5f968675643c134c5d79770f71c40f92b9e23491
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2025-07-11 17:35:23 +0100

    add comment for the cron jobs in onwards lifecycle

--- onward/app/feed/OnwardJourneyLifecycle.scala
+++ onward/app/feed/OnwardJourneyLifecycle.scala
@@ -42,19 +42,30 @@ class OnwardJourneyLifecycle(
 
     descheduleAll()
 
-    // Spreading the jobs in a way that they all run have 30 seconds apart from each others
+    // Spreading the jobs to avoid running them all simultaneously
     // Every 5 minutes
+    // 00m:00s, 05m:00s, 10m:00s, etc
     jobs.schedule("MostPopularAgentRefreshJob", "0 0/5 * * * ?") { mostPopularAgent.refresh() }
+    // 01m:00s, 06m:00s, 11m:00s, etc
     jobs.schedule("GeoMostPopularAgentRefreshJob", "0 1/5 * * * ?") { geoMostPopularAgent.refresh() }
+    // 01m:00s, 06m:00s, 11m:00s, etc
     jobs.schedule("DeeplyReadAgentRefreshJob", "0 2/5 * * * ?") { deeplyReadAgent.refresh() }
 
     // Every 30 minutes
+    // 03m:00s, 33m:00s, 03m:00s, etc
     jobs.schedule("MostViewedVideoAgentRefreshJob", "0 3/30 * * * ?") { mostViewedVideoAgent.refresh() }
+    // 04m:00s, 34m:00s, 04m:00s, etc
     jobs.schedule("MostViewedAudioAgentRefreshJob", "0 4/30 * * * ?") { mostViewedAudioAgent.refresh() }
+    // Added 30 second offset to avoid conflicting with the jobs that run every 5 minutes
+    // 05m:30s, 35m:30s, 05m:30s, etc
     jobs.schedule("MostViewedGalleryAgentRefreshJob", "30 5/30 * * * ?") { mostViewedGalleryAgent.refresh() }
+    // Added 30 second offset to avoid conflicting with the jobs that run every 5 minutes
+    // 06m:30s, 36m:30s, 06m:30s, etc
     jobs.schedule("MostReadAgentRefreshJob", "30 6/30 * * * ?") { mostReadAgent.refresh() }
 
     // Every 60 minutes
+    // Added 30 second offset to avoid conflicting with the jobs that run every 5 minutes
+    // 07m:30s, 37m:30s, 07m:30s, etc
     jobs.schedule("DayMostPopularAgentRefreshJob", "30 7/60 * * * ?") { dayMostPopularAgent.refresh() }
 
     pekkoAsync.after1s {
