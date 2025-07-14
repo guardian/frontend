cd5e3702e2c3eb8c2a20c9bfd7113486ecda74dd
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2025-07-14 13:53:52 +0100

    Update common/app/controllers/EmailSignupController.scala
    Co-authored-by: David Blatcher <30567854+dblatcher@users.noreply.github.com>

--- common/app/controllers/EmailSignupController.scala
+++ common/app/controllers/EmailSignupController.scala
@@ -276,7 +276,7 @@ class EmailSignupController(
 
   def logNewsletterNotFoundError(newsletterName: String)(implicit request: RequestHeader): Unit = {
     logInfoWithRequestId(
-      s"The newsletter $newsletterName used in an email sign-up form could not be found by the NewsletterSignupAgent. It may no longer exist.",
+      s"The newsletter $newsletterName used in an email sign-up form could not be found by the NewsletterSignupAgent. It may no longer exist or $newsletterName may be an outdated reference number.",
     )
   }
 
