417153708c68ae4a731385be9abef8f1f1d882af
Charlotte <charlotte.emms@theguardian.com>
2025-07-29 15:03:35 +0100

    add caching for the result itself as well as using the agent for infrequent refreshing of data

--- commercial/app/controllers/AdmiralController.scala
+++ commercial/app/controllers/AdmiralController.scala
@@ -1,8 +1,12 @@
 package commercial.controllers
 
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import agents.AdmiralAgent
 import common.{GuLogging, ImplicitControllerExecutionContext}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import model.Cached
+import model.Cached.RevalidatableResult
+
+import scala.concurrent.duration._
 
 class AdmiralController(admiralAgent: AdmiralAgent, val controllerComponents: ControllerComponents)
     extends BaseController
@@ -11,7 +15,9 @@ class AdmiralController(admiralAgent: AdmiralAgent, val controllerComponents: Co
     with implicits.Requests {
 
   def getBootstrapScript: Action[AnyContent] =
-    Action {
-      Ok(admiralAgent.getBootstrapScript)
+    Action { implicit request =>
+      // Cache the result for 10 minutes
+      // We only actually refresh the agent and fetch the bootstrap script once every 2 hours
+      Cached(10.minutes)(RevalidatableResult.Ok(admiralAgent.getBootstrapScript))
     }
 }
