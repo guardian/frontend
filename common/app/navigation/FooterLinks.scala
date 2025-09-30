3c32a121464aab1d8fb341db3404a625a7a09099
Anna Beddow <anna.beddow@gmail.com>
2025-09-30 10:20:42 +0100

    Lift up helper methods to improve the structure of this file

--- common/app/navigation/FooterLinks.scala
+++ common/app/navigation/FooterLinks.scala
@@ -11,8 +11,7 @@ case class FooterLink(
 
 object FooterLinks {
 
-  // Footer column one
-
+  // Helpers
   val complaintsAndCorrections =
     FooterLink("Complaints & corrections", "/info/complaints-and-corrections", "complaints")
   val secureDrop = FooterLink("SecureDrop", "https://www.theguardian.com/securedrop", "securedrop")
@@ -34,6 +33,47 @@ object FooterLinks {
     )
   def workForUs(edition: String): FooterLink =
     FooterLink("Work for us", "https://workforus.theguardian.com", s"${edition} : footer : work for us")
+  def allTopics(edition: String): FooterLink =
+    FooterLink("All topics", "/index/subjects/a", s"${edition} : footer : all topics")
+  def allWriters(edition: String): FooterLink =
+    FooterLink("All writers", "/index/contributors", s"${edition} : footer : all contributors")
+  val digitalNewspaperArchive: FooterLink =
+    FooterLink("Digital newspaper archive", "https://theguardian.newspapers.com", "digital newspaper archive")
+  def taxStrategy(edition: String): FooterLink =
+    FooterLink(
+      "Tax strategy",
+      "https://uploads.guim.co.uk/2025/09/05/Tax_strategy_for_the_year_ended_31_March_2025.pdf",
+      s"${edition} : footer : tax strategy",
+    )
+  def facebook(edition: String): FooterLink =
+    FooterLink("Facebook", "https://www.facebook.com/theguardian", s"${edition} : footer : facebook")
+  def youtube(edition: String): FooterLink =
+    FooterLink("YouTube", "https://www.youtube.com/user/TheGuardian", s"${edition} : footer : youtube")
+  def linkedin(edition: String): FooterLink =
+    FooterLink("LinkedIn", "https://www.linkedin.com/company/theguardian", s"${edition} : footer : linkedin")
+  def instagram(edition: String): FooterLink =
+    FooterLink("Instagram", "https://www.instagram.com/guardian", s"${edition} : footer : instagram")
+  def newsletters(edition: String): FooterLink = {
+    FooterLink(
+      text = "Newsletters",
+      url = s"/email-newsletters?INTCMP=DOTCOM_FOOTER_NEWSLETTER_${edition.toUpperCase}",
+      dataLinkName = s"$edition : footer : newsletters",
+    )
+
+    def modernSlaveryActStatement(edition: String): FooterLink = {
+      FooterLink(
+        "Modern Slavery Act",
+        "https://uploads.guim.co.uk/2025/09/05/Modern_Slavery_Statement_2025.pdf",
+        s"$edition : footer : modern slavery act statement",
+      )
+  }
+    def tipUsOff(edition: String): FooterLink = {
+      FooterLink("Tip us off", "https://www.theguardian.com/tips", s"$edition : footer : tips")
+
+    }
+
+
+  /* Column one */
 
   val ukListOne = List(
     FooterLink("About us", "/about", "uk : footer : about us"),
@@ -82,35 +122,7 @@ object FooterLinks {
     FooterLink("Contact us", "/help/contact-us", "international : footer : contact us"),
   )
 
-  // Footer column two
-
-  def allTopics(edition: String): FooterLink =
-    FooterLink("All topics", "/index/subjects/a", s"${edition} : footer : all topics")
-  def allWriters(edition: String): FooterLink =
-    FooterLink("All writers", "/index/contributors", s"${edition} : footer : all contributors")
-  val digitalNewspaperArchive: FooterLink =
-    FooterLink("Digital newspaper archive", "https://theguardian.newspapers.com", "digital newspaper archive")
-  def taxStrategy(edition: String): FooterLink =
-    FooterLink(
-      "Tax strategy",
-      "https://uploads.guim.co.uk/2025/09/05/Tax_strategy_for_the_year_ended_31_March_2025.pdf",
-      s"${edition} : footer : tax strategy",
-    )
-  def facebook(edition: String): FooterLink =
-    FooterLink("Facebook", "https://www.facebook.com/theguardian", s"${edition} : footer : facebook")
-  def youtube(edition: String): FooterLink =
-    FooterLink("YouTube", "https://www.youtube.com/user/TheGuardian", s"${edition} : footer : youtube")
-  def linkedin(edition: String): FooterLink =
-    FooterLink("LinkedIn", "https://www.linkedin.com/company/theguardian", s"${edition} : footer : linkedin")
-  def instagram(edition: String): FooterLink =
-    FooterLink("Instagram", "https://www.instagram.com/guardian", s"${edition} : footer : instagram")
-  def newsletters(edition: String): FooterLink =
-    FooterLink(
-      text = "Newsletters",
-      url = s"/email-newsletters?INTCMP=DOTCOM_FOOTER_NEWSLETTER_${edition.toUpperCase}",
-      dataLinkName = s"$edition : footer : newsletters",
-    )
-
+  /* Column two */
   val ukListTwo = List(
     allTopics("uk"),
     allWriters("uk"),
@@ -164,7 +176,7 @@ object FooterLinks {
     newsletters("international"),
   )
 
-  // Footer column three
+  /* Column three */
 
   val ukListThree = List(
     FooterLink("Advertise with us", "https://advertising.theguardian.com", "uk : footer : advertise with us"),
