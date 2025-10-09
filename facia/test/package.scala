5b2495f2b26aa0ba461498c70d0e5895cdc0b029
Ioanna Kokkini <ioannakok@hotmail.com>
2025-10-09 09:52:59 +0100

    Remove FluentLenium
    The project is no longer maintained: https://github.com/FluentLenium/FluentLenium
    
    oe are removing FluentLenium because the project is no longer maintained. https://github.com/FluentLenium/FluentLenium. This commit removes some code that never ran and a comment referring to FluentLenium.

--- facia/test/package.scala
+++ facia/test/package.scala
@@ -1,18 +1,7 @@
 package test
 
-import io.fluentlenium.core.domain.FluentWebElement
 import org.scalatest.Suites
 import services.dotcomrendering.FaciaPickerTest
-
-object `package` {
-
-  implicit class WebElement2rich(element: FluentWebElement) {
-    lazy val href = element.attribute("href")
-
-    def hasAttribute(name: String): Boolean = element.attribute(name) != null
-  }
-
-}
 
 class FaciaTestSuite
     extends Suites(
