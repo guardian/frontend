62055a290803b56c3d3d01a038de5d1dda914343
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2025-10-28 14:57:03 +0000

    Pan domain header auth check
    Co-Authored-By: Andrew Nowak <10963046+andrew-nowak@users.noreply.github.com>

--- common/app/http/GuardianAuthWithExemptions.scala
+++ common/app/http/GuardianAuthWithExemptions.scala
@@ -12,6 +12,7 @@ import conf.Configuration.aws.mandatoryCredentials
 import model.ApplicationContext
 import org.apache.pekko.stream.Materializer
 import play.api.Mode
+import play.api.http.HttpEntity
 import play.api.libs.ws.WSClient
 import play.api.mvc._
 
@@ -31,7 +32,8 @@ class GuardianAuthWithExemptions(
     val mat: Materializer,
     context: ApplicationContext,
 ) extends AuthActions
-    with BaseController {
+    with BaseController
+    with PanDomainAuthenticated {
 
   private val outer = this
 
@@ -100,6 +102,19 @@ class GuardianAuthWithExemptions(
     def apply(nextFilter: RequestHeader => Future[Result])(request: RequestHeader): Future[Result] = {
       if (doNotAuthenticate(request)) {
         nextFilter(request)
+      } else if (request.headers.hasHeader(AUTHORIZATION)) {
+        evaluatePandaAuth(request.headers.get(AUTHORIZATION).get, permissions) match {
+          case Some(AuthenticationSuccess(_)) => nextFilter(request)
+          case Some(AuthenticationFailure(status, _)) =>
+            Future.successful(new Result(ResponseHeader(status), HttpEntity.NoEntity))
+          case _ =>
+            Future.successful(
+              Results.Forbidden(
+                s"You do not have permission to access $system. " +
+                  s"You should contact Central Production to request '$requiredEditorialPermissionName' permission.",
+              ),
+            )
+        }
       } else {
         AuthAction.authenticateRequest(request) { user =>
           if (permissions.hasPermission(requiredPermission, user.email)) {
