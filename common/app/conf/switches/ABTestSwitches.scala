9c8fc0ec56e761077798536b60d0b2df18b208e2
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2025-10-28 12:45:53 +0000

    Update ABTestSwitches.scala

--- common/app/conf/switches/ABTestSwitches.scala
+++ common/app/conf/switches/ABTestSwitches.scala
@@ -59,15 +59,4 @@ trait ABTestSwitches {
     exposeClientSide = true,
     highImpact = false,
   )
-
-  Switch(
-    ABTests,
-    "ab-ses-test",
-    "Ses test description",
-    owners = Seq(Owner.withEmail("daniel.clifton@guardian.co.uk")),
-    safeState = Off,
-    sellByDate = Some(LocalDate.of(2025, 10, 29)),
-    exposeClientSide = true,
-    highImpact = false,
-  )
 }
