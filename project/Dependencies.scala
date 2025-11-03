df8674aff9b930b18162a098dac126e50c636ba0
Emma Imber <emma-jo.imber@guardian.co.uk>
2025-11-03 14:45:28 +0000

    Bump capi and facia version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Dependencies {
   val identityLibVersion = "4.31"
   val awsVersion = "1.12.791"
   val awsSdk2Version = "2.35.10"
-  val capiVersion = "38.0.0-PREVIEW.filterbump-cpai-version.2025-10-17T1452.d9337833"
-  val faciaVersion = "24.0.0-PREVIEW.filterfapi-bump.2025-10-17T1515.5e0e8d5b"
+  val capiVersion = "38.0.0"
+  val faciaVersion = "24.0.0"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
   val jerseyVersion = "1.19.4"
@@ -32,8 +32,7 @@ object Dependencies {
   val commonsIo = "commons-io" % "commons-io" % "2.16.1"
   val cssParser = "net.sourceforge.cssparser" % "cssparser" % "0.9.30"
   val contentApiClient = "com.gu" %% "content-api-client" % capiVersion
-  val contentApiModelsJson =
-    "com.gu" %% "content-api-models-json" % "32.0.0"
+  val contentApiModelsJson = "com.gu" %% "content-api-models-json" % "32.0.0"
   val faciaFapiScalaClient = "com.gu" %% "fapi-client-play30" % faciaVersion
   val identityCookie = "com.gu.identity" %% "identity-cookie" % identityLibVersion
 
