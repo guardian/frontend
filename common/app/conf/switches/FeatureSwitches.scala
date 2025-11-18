aefa37cf24ba4c9d07c0dd8c4bbda52f2fb77f26
Charlotte <charlotte.emms@theguardian.com>
2025-11-18 12:42:57 +0000

    add a feature switch for rendering hosted content pages in DCR

--- common/app/conf/switches/FeatureSwitches.scala
+++ common/app/conf/switches/FeatureSwitches.scala
@@ -638,4 +638,15 @@ trait FeatureSwitches {
     exposeClientSide = true,
     highImpact = false,
   )
+
+  val DCRHostedContent = Switch(
+    group = SwitchGroup.Feature,
+    name = "dcr-hosted-content",
+    description = "Render hosted content pages with DCR",
+    owners = Seq(Owner.withEmail("commercial.dev@guardian.co.uk")),
+    safeState = Off,
+    sellByDate = never,
+    exposeClientSide = false,
+    highImpact = false,
+  )
 }
