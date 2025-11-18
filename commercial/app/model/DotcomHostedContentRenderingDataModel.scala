69210e25b1e21b17b0afb50df197b4e7f9f0c02e
Charlotte <charlotte.emms@theguardian.com>
2025-11-18 17:22:31 +0000

    add very basic DCR data model for hosted content pages

---
+++ commercial/app/model/DotcomHostedContentRenderingDataModel.scala
@@ -0,0 +1,143 @@
+package model
+
+import com.gu.contentapi.client.model.v1.{Content => ApiContent}
+import common.commercial.hosted._
+import model.dotcomrendering.DotcomRenderingUtils._
+import play.api.libs.json._
+import play.api.mvc.RequestHeader
+
+// -----------------------------------------------------------------
+// DCR DataModel
+// -----------------------------------------------------------------
+
+case class DotcomRenderingHostedContentModel(
+    // general / shared
+    id: String,
+    url: String,
+    encodedUrl: String,
+    campaign: Option[HostedCampaign],
+    title: String,
+    mainImageUrl: String,
+    thumbnailUrl: String,
+    standfirst: String,
+    cta: HostedCallToAction,
+    metadata: MetaData,
+    name: String,
+    owner: String,
+    logo: HostedLogo,
+    fontColour: Colour,
+
+    // article
+    body: Option[String],
+    mainPicture: Option[String],
+    mainPictureCaption: Option[String],
+    content: Option[Content],
+
+    // video
+    video: Option[HostedVideo],
+
+    // gallery
+    images: List[HostedGalleryImage],
+)
+
+object DotcomRenderingHostedContentModel {
+
+  // Implicit Json writes
+  implicit val dcrContentWrites: Writes[DotcomRenderingHostedContentModel] =
+    Json.writes[DotcomRenderingHostedContentModel]
+  implicit val campaignWrites: Writes[HostedCampaign] = Json.writes[HostedCampaign]
+  implicit val ctaWrites: Writes[HostedCallToAction] = Json.writes[HostedCallToAction]
+  implicit val contentWrites: Writes[Content] = Json.writes[Content]
+  implicit val metadataWrites: Writes[MetaData] = Json.writes[MetaData]
+  implicit val videoWrites: Writes[HostedVideo] = Json.writes[HostedVideo]
+  implicit val imagesWrites: Writes[HostedGalleryImage] = Json.writes[HostedGalleryImage]
+
+  def toJson(model: DotcomRenderingHostedContentModel): JsValue = {
+    val jsValue = Json.toJson(model)
+    withoutNull(jsValue)
+  }
+
+  def forArticle(
+      item: ApiContent,
+  )(implicit request: RequestHeader): DotcomRenderingHostedContentModel = {
+    val page = HostedArticlePage.fromContent(item).get
+    DotcomRenderingHostedContentModel(
+      id = page.id,
+      url = page.url,
+      encodedUrl = page.encodedUrl,
+      campaign = page.campaign,
+      title = page.title,
+      mainImageUrl = page.mainImageUrl,
+      thumbnailUrl = page.thumbnailUrl,
+      standfirst = page.standfirst,
+      cta = page.cta,
+      metadata = page.metadata,
+      name = page.name,
+      owner = page.owner,
+      logo = page.logo,
+      fontColour = page.fontColour,
+      body = Some(page.body),
+      mainPicture = Some(page.mainPicture),
+      mainPictureCaption = Some(page.mainPictureCaption),
+      content = Some(page.content),
+      video = None,
+      images = List.empty,
+    )
+  }
+
+  def forVideo(
+      item: ApiContent,
+  )(implicit request: RequestHeader): DotcomRenderingHostedContentModel = {
+    val page = HostedVideoPage.fromContent(item).get
+    DotcomRenderingHostedContentModel(
+      id = page.id,
+      url = page.url,
+      encodedUrl = page.encodedUrl,
+      campaign = page.campaign,
+      title = page.title,
+      mainImageUrl = page.mainImageUrl,
+      thumbnailUrl = page.thumbnailUrl,
+      standfirst = page.standfirst,
+      cta = page.cta,
+      metadata = page.metadata,
+      name = page.name,
+      owner = page.owner,
+      logo = page.logo,
+      fontColour = page.fontColour,
+      body = None,
+      mainPicture = None,
+      mainPictureCaption = None,
+      content = None,
+      video = Some(page.video),
+      images = List.empty,
+    )
+  }
+
+  def forGallery(
+      item: ApiContent,
+  )(implicit request: RequestHeader): DotcomRenderingHostedContentModel = {
+    val page = HostedGalleryPage.fromContent(item).get
+    DotcomRenderingHostedContentModel(
+      id = page.id,
+      url = page.url,
+      encodedUrl = page.encodedUrl,
+      campaign = page.campaign,
+      title = page.title,
+      mainImageUrl = page.mainImageUrl,
+      thumbnailUrl = page.thumbnailUrl,
+      standfirst = page.standfirst,
+      cta = page.cta,
+      metadata = page.metadata,
+      name = page.name,
+      owner = page.owner,
+      logo = page.logo,
+      fontColour = page.fontColour,
+      body = None,
+      mainPicture = None,
+      mainPictureCaption = None,
+      content = None,
+      video = None,
+      images = page.images,
+    )
+  }
+}
