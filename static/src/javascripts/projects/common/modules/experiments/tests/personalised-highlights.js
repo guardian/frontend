85db28ce2ff4e9645d987a862fa05b46789b950d
Dominik Lander <dominik.lander@guardian.co.uk>
2025-11-27 09:38:48 +0000

    Extend test beyond Monday.

--- static/src/javascripts/projects/common/modules/experiments/tests/personalised-highlights.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/personalised-highlights.js
@@ -1,16 +1,15 @@
-
 export const personalisedHighlights = {
 	id: 'PersonalisedHighlights',
 	start: '2025-11-17',
-	expiry: '2025-12-01',
+	expiry: '2026-01-28',
 	author: 'Anna Beddow',
-	description: 'Allow user behaviour to personalise the ordering of cards in the highlights container.',
-    audience: 0.75,
-    audienceOffset: 0.75,
+	description:
+		'Allow user behaviour to personalise the ordering of cards in the highlights container.',
+	audience: 0.75,
+	audienceOffset: 0.75,
 	successMeasure: '',
 	audienceCriteria: 'All users, globally.',
-    idealOutcome:
-        'Increase click through rate on highlights container',
+	idealOutcome: 'Increase click through rate on highlights container',
 	showForSensitive: true,
 
 	canRun: () => true,
@@ -19,18 +18,17 @@ export const personalisedHighlights = {
 			id: 'control',
 			test: () => {},
 		},
-        {
-            id: 'click-tracking',
-            test: () => {},
-        },
-        {
-            id: 'view-tracking',
-            test: () => {}
-        },
-        {
-            id: 'click-and-view-tracking',
-            test: () => {},
-        },
+		{
+			id: 'click-tracking',
+			test: () => {},
+		},
+		{
+			id: 'view-tracking',
+			test: () => {},
+		},
+		{
+			id: 'click-and-view-tracking',
+			test: () => {},
+		},
 	],
 };
-
