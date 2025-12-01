52c4785fb5ca2ba61ba186c401bd474b10d507a9
Roberto Tyley <roberto.tyley@gmail.com>
2025-12-01 23:25:18 +0000

    Basic Live-Harness endpoint functionality
    This is a tweaked version of the work done in https://github.com/guardian/frontend/pull/28417,
    but built upon the refactors done in https://github.com/guardian/frontend/pull/28437, which
    allows this implementation to be smaller.
    
    In `LiveHarnessController.renderLiveHarness()`:
    
    * The `ArticleController.mapAndRender()` & `InteractiveController.modelAndRender()`
      methods introduced in #28437 are useful not only within those controllers, but also
      more suitable for the work needed in `renderLiveHarness()`, meaning that each case
      (`ContentType.Article` & `ContentType.Interactive`) can be handled much more concisely -
      a single line each.
    * We can avoid transforming the `JsResult` to an `Either`, and just match on
      `JsSuccess`/`JsError` rather than `Right`/`Left`
    * The `match` statement on the `Option[ContentType]` can be done slightly more concisely
      using `collect` : https://www.scala-lang.org/api/2.13.18/scala/Option.html#collect[B](pf:PartialFunction[A,B]):Option[B]
    
    In `LiveHarness`:
    
    * The important code in `turnLocalAtomsIntoAtomBlocksAndElements()` - that makes an
      `InteractiveAtom` or `BlockElement` out of a `LiveHarnessInteractiveAtom` - has moved
      onto `LiveHarnessInteractiveAtom` itself (so all the references copying data from
      `localAtom.xxx` fields become just references to `xxx` field itself)
    * A fiddly bit of functionality - updating the `content` within a `ContentPage`, which
      varies depending on whether it's a `ArticlePage` or `InteractivePage` - has been isolated
      into a `PageUpdater` type class. This allows us to have much clearer code elsewhere -
      without having to define a content-updating method for every single `ContentPage` type,
      just the two we care about. Consequently, there's not a need for either of the two
      `injectInteractiveAtomsIntoPage()` methods that used to exist - just a single `inject()`
      method, which does the work of `addLocalAtomsToParent()` too.
    * We use the handy `splitAt()` method to make a bit of code in `addLocalAtomsToParent()` a
      bit smaller.
    
    In `InteractiveController`:
    
    * A small new `modelAndRenderHtml()` method is required, to match the behaviour from #28417
      which short-circuits a lot of logic to ensure that `renderHtml()` is called.
    
    Co-Authored-By: Jonathon Herbert <jonathon.herbert@guardian.co.uk>
    Co-Authored-By: Frederick O'Brien <frederick.obrien@guardian.co.uk>

--- preview/app/controllers/LiveHarnessController.scala
+++ preview/app/controllers/LiveHarnessController.scala
@@ -1,10 +1,63 @@
 package controllers
 
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import com.gu.contentapi.client.model.v1.ContentType
+import common.{GuLogging, ImplicitControllerExecutionContext}
+import contentapi.ContentApiClient
+import model.{ApplicationContext, ArticleBlocks, GenericFallback}
+import play.api.libs.json.{JsError, JsSuccess, JsValue}
+import play.api.libs.ws.WSClient
+import play.api.mvc.{Action, BaseController, ControllerComponents}
+import renderers.DotcomRenderingService
+import services.{CAPILookup, NewsletterService}
+import utils.LiveHarness.inject
+import utils.LiveHarnessInteractiveAtom
+
 import scala.concurrent.Future
 
-class LiveHarnessController(val controllerComponents: ControllerComponents) extends BaseController {
-  def renderLiveHarness(path: String): Action[AnyContent] = Action.async { implicit request =>
-    Future.successful(Ok("Hello, world!"))
+class LiveHarnessController(
+    contentApiClient: ContentApiClient,
+    val controllerComponents: ControllerComponents,
+    ws: WSClient,
+    newsletterService: NewsletterService,
+)(implicit val context: ApplicationContext)
+    extends BaseController
+    with GuLogging
+    with ImplicitControllerExecutionContext {
+
+  private val renderingService = DotcomRenderingService()
+
+  private val capiLookup: CAPILookup = new CAPILookup(contentApiClient)
+
+  private val articleController =
+    new ArticleController(
+      contentApiClient,
+      controllerComponents,
+      ws,
+      renderingService,
+      newsletterService,
+    )
+
+  private val interactiveController =
+    new InteractiveController(
+      contentApiClient,
+      ws,
+      controllerComponents,
+      renderingService,
+    )
+
+  def renderLiveHarness(path: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[List[LiveHarnessInteractiveAtom]] match {
+      case JsSuccess(atoms, _) =>
+        capiLookup.lookup(path, Some(ArticleBlocks)).flatMap { response =>
+          response.content
+            .map(_.`type`)
+            .collect {
+              case ContentType.Article     => articleController.mapAndRender(path, GenericFallback)(inject(atoms))
+              case ContentType.Interactive => interactiveController.modelAndRenderHtml(response)(inject(atoms))
+            }
+            .getOrElse(Future(BadRequest))
+        }
+      case JsError(errors) => Future(BadRequest(errors.toString()))
+    }
   }
 }
