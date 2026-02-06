416e03eac172ad4319a86325dd85aae1e0a2294f
Marjan Kalanaki <15894063+marjisound@users.noreply.github.com>
2026-02-06 11:54:52 +0000

    Replace percent log sampling with rate limiting
    Co-authored-by: Ioanna Kokkini <ioannakok@hotmail.com>
    Co-authored-by: Roberto Tyley <52038+rtyley@users.noreply.github.com>

--- common/app/http/TooManyHeadersFilter.scala
+++ common/app/http/TooManyHeadersFilter.scala
@@ -1,5 +1,6 @@
 package http
 
+import com.google.common.util.concurrent.RateLimiter
 import org.apache.pekko.stream.Materializer
 import play.api.Logger
 import play.api.mvc.{Filter, RequestHeader, Result}
@@ -11,10 +12,11 @@ class TooManyHeadersFilter(implicit val mat: Materializer) extends Filter {
   private val MAX_HEADERS = 64
 
   private val logging: Logger = Logger(this.getClass)
+  private val rateLimiter = RateLimiter.create(0.1) // 1 permit per 10 seconds
 
   override def apply(f: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = {
-    // Log a warning for requests with an excessive number of headers, sampling at ~1%
-    if (rh.headers.keys.size > MAX_HEADERS && Random.nextInt(100) == 0) {
+    // Log a warning for requests with an excessive number of headers, sampling with rate limiter at 1 permit in 10 seconds
+    if (rh.headers.keys.size > MAX_HEADERS && rateLimiter.tryAcquire()) {
       logging.warn(
         s"Request with too many headers: ${rh.headers.keys.size} headers: ${rh.headers.keys.mkString(",")}",
       )
