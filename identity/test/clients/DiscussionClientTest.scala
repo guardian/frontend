b16641a08641776a11f48a359d33dfb671caf0d9
Alex Dufournet <3389563+alexduf@users.noreply.github.com>
2026-02-13 10:58:22 +0100

    Upgrade mockito

--- identity/test/clients/DiscussionClientTest.scala
+++ identity/test/clients/DiscussionClientTest.scala
@@ -2,7 +2,7 @@ package clients
 
 import conf.IdentityConfiguration
 import metadata.MetaDataMatcher.convertToAnyShouldWrapper
-import org.mockito.Matchers.any
+import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatestplus.mockito.MockitoSugar
