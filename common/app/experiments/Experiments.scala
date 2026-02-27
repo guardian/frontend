7e9ce6de2dc6bac729dc15e1b10ee1bd1067c648
Georges Lebreton <102960844+Georges-GNM@users.noreply.github.com>
2026-02-27 14:14:30 +0000

    Use new ab test framework for tag page storylines (#28629)

--- common/app/experiments/Experiments.scala
+++ common/app/experiments/Experiments.scala
@@ -12,7 +12,6 @@ object ActiveExperiments extends ExperimentsDefinition {
   override val allExperiments: Set[Experiment] =
     Set(
       DarkModeWeb,
-      TagPageStorylines,
     )
   implicit val canCheckExperiment: CanCheckExperiment = new CanCheckExperiment(this)
 }
@@ -24,13 +23,4 @@ object DarkModeWeb
       owners = Seq(Owner.withEmail("dotcom.platform@theguardian.com")),
       sellByDate = LocalDate.of(2027, 1, 26),
       participationGroup = Perc0D,
-    )
-
-object TagPageStorylines
-    extends Experiment(
-      name = "tag-page-storylines",
-      description = "Enable AI storylines content on web tag pages",
-      owners = Seq(Owner.withEmail("ai.dev@theguardian.com")),
-      sellByDate = LocalDate.of(2026, 2, 27),
-      participationGroup = Perc0E,
     )
