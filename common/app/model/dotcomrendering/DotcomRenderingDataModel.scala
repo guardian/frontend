8be935ec5f44aeb40d3665377747e018189227dd
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2026-04-29 14:18:21 +0100

    Remove redundant top-level recipes field from DCR payload

--- common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
+++ common/app/model/dotcomrendering/DotcomRenderingDataModel.scala
@@ -162,9 +162,6 @@ object DotcomRenderingDataModel {
         "isAdFreeUser" -> model.isAdFreeUser,
         "webURL" -> model.webURL,
         "linkedData" -> model.linkedData,
-        "recipes" -> JsArray(model.linkedData.collect { case r: RecipeLinkedData =>
-          RecipeLinkedData.formats.writes(r)
-        }),
         "openGraphData" -> model.openGraphData,
         "twitterData" -> model.twitterData,
         "config" -> model.config,
