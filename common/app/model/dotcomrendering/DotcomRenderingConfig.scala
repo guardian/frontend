dd8a59ab8c9fd8719a89f0d2bd17d8a3a6849060
Ravi <7014230+arelra@users.noreply.github.com>
2026-05-07 14:58:22 +0100

    Remove legacy config properties from the config object passed to DCR.

--- common/app/model/dotcomrendering/DotcomRenderingConfig.scala
+++ common/app/model/dotcomrendering/DotcomRenderingConfig.scala
@@ -11,6 +11,8 @@ import play.api.mvc.RequestHeader
 import views.support.{CamelCase, JavaScriptPage}
 
 object DotcomRenderingConfig {
+  val removeProperties: Seq[String] = Seq("thumbnail")
+
   def apply(
       page: Page,
       request: RequestHeader,
@@ -35,6 +37,15 @@ object DotcomRenderingConfig {
     val jsPageConfig: Map[String, JsValue] =
       JavaScriptPage.getMap(page, Edition(request), isPreview, request)
 
-    Json.toJsObject(config).deepMerge(JsObject(jsPageConfig))
+    val combinedConfig = Json.toJsObject(config).deepMerge(JsObject(jsPageConfig))
+
+    /** Remove legacy config not used by DCR
+      *
+      * See: https://github.com/guardian/dotcom-rendering/pull/15767
+      *
+      * Properties removed here should also be deleted from the LegacyConfig type in DCR
+      * https://github.com/guardian/dotcom-rendering/blob/7f862e949d50bf98a4505995121502cfbd8daaf6/dotcom-rendering/src/types/config.ts#L94-L100
+      */
+    JsObject(combinedConfig.fields.filterNot { case (key, _) => removeProperties.contains(key) })
   }
 }
