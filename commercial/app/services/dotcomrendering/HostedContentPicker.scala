80fc3c82d687116230850a64941b6368de4d6d6d
Charlotte <charlotte.emms@theguardian.com>
2026-05-20 17:07:02 +0100

    switch must be on to remote render hosted content. it then requires test participation to render via DCR

--- commercial/app/services/dotcomrendering/HostedContentPicker.scala
+++ commercial/app/services/dotcomrendering/HostedContentPicker.scala
@@ -27,8 +27,8 @@ object HostedContentPicker extends GuLogging {
   def decideTier(isGallery: Boolean = false)(implicit
       request: RequestHeader,
   ): RenderType = {
-    if (Switches.DCRHostedContent.isSwitchedOff && !isUserInTestGroup("commercial-hosted-content", "preview"))
-      LocalRender
+    if (Switches.DCRHostedContent.isSwitchedOff) LocalRender
+    else if (!isUserInTestGroup("commercial-hosted-content", "preview")) LocalRender
     // Gallery pages are not supported in DCR yet
     else if (isGallery) LocalRender
     else if (request.forceDCROff) LocalRender
