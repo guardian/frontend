f0795393a40226c7aa76486438f8c7f1d0d7ee6a
Charlotte <charlotte.emms@theguardian.com>
2026-06-04 15:56:34 +0100

    update hosted picker logic to use DCR for non gallery and use the test participation for gallery articles

--- commercial/test/services/dotcomrendering/HostedContentPickerTest.scala
+++ commercial/test/services/dotcomrendering/HostedContentPickerTest.scala
@@ -16,9 +16,23 @@ import test.TestRequest
   }
 
   it should "return LocalRender if isGallery is true" in {
+    val testRequest = TestRequest("hosted-content-path")
+    val tier = HostedContentPicker.decideTier(isGallery = true)(testRequest)
+    tier should be(LocalRender)
+  }
+
+  it should "return LocalRender if not in the test group whilst forcing DCR for isGallery = true" in {
     val testRequest = TestRequest("hosted-content-path?dcr=true")
     val tier = HostedContentPicker.decideTier(isGallery = true)(testRequest)
     tier should be(LocalRender)
+  }
+
+  it should "return RemoteRender if in the test group and forcing DCR for isGallery = true" in {
+    val testRequest = TestRequest("hosted-content-path?dcr=true").withHeaders(
+      "X-GU-Server-AB-Tests" -> "commercial-hosted-content:preview",
+    )
+    val tier = HostedContentPicker.decideTier(isGallery = true)(testRequest)
+    tier should be(RemoteRender)
   }
 
   it should "return LocalRender if forcing DCR to be off via the query parameter" in {
@@ -30,7 +44,7 @@ import test.TestRequest
   it should "return RemoteRender if forcing DCR to be on via the query parameter" in {
     val testRequest = TestRequest("hosted-content-path?dcr=true")
     val tier = HostedContentPicker.decideTier()(testRequest)
-    tier should be(LocalRender)
+    tier should be(RemoteRender)
   }
 
   it should "return LocalRender if not in the test group without forcing DCR" in {
@@ -39,17 +53,9 @@ import test.TestRequest
     tier should be(LocalRender)
   }
 
-  it should "return RemoteRender if in the test group without forcing DCR" in {
-    val testRequest = TestRequest("hosted-content-path").withHeaders(
-      "X-GU-Server-AB-Tests" -> "commercial-hosted-content:preview",
-    )
-    val tier = HostedContentPicker.decideTier()(testRequest)
-    tier should be(RemoteRender)
-  }
-
-  it should "return LocalRender otherwise" in {
+  it should "return RemoteRender otherwise" in {
     val testRequest = TestRequest("hosted-content-path")
     val tier = HostedContentPicker.decideTier()(testRequest)
-    tier should be(LocalRender)
+    tier should be(RemoteRender)
   }
 }
