f24fba8cac319c27d7b1c79d9d7a94a7aa6a3722
Marjan Kalanaki <15894063+marjisound@users.noreply.github.com>
2026-06-12 14:30:38 +0100

    Increase the frequency of the live football match job
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

--- sport/app/football/conf/context.scala
+++ sport/app/football/conf/context.scala
@@ -66,7 +66,7 @@ class FootballLifecycle(
     }
 
     // if preview "Every hour at 40 minutes" otherwise "Every minute"
-    jobs.schedule("MaybeMatchDayAgentRefreshJob", if (context.isPreview) "0 41 * * * ?" else "0 0/1 * * * ?") {
+    jobs.schedule("MaybeMatchDayAgentRefreshJob", if (context.isPreview) "0 41 * * * ?" else "0/30 * * * * ?") {
       competitionsService.maybeRefreshLiveMatches(defaultClock)
     }
 
