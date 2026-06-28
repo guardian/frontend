d6c0bd02a97d631615fabb0a9390d4c495115789
Ravi <7014230+arelra@users.noreply.github.com>
2026-06-29 00:00:46 +0100

    DCAR Sites shell

--- applications/app/controllers/ApplicationsControllers.scala
+++ applications/app/controllers/ApplicationsControllers.scala
@@ -41,6 +41,7 @@ trait ApplicationsControllers {
   lazy val youtubeController = wire[YoutubeController]
   lazy val nx1ConfigController = wire[Nx1ConfigController]
   lazy val componentController = wire[ComponentController]
+  lazy val shellController = wire[ShellController]
 
   // A fake geolocation controller to test it locally
   lazy val geolocationController = wire[FakeGeolocationController]
