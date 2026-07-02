836926c7b548e86d4484a41bd578f328c9124848
Georges Lebreton <georges.lebreton@guardian.co.uk>
2026-07-02 17:24:16 +0100

    wip

--- common/app/model/EnrichedContent.scala
+++ common/app/model/EnrichedContent.scala
@@ -1,5 +1,15 @@
 package model.pressed
 
+import model.dotcomrendering.pageElements.{
+  AudioAtomBlockElement,
+  CallToActionAtomBlockElement,
+  ExplainerAtomBlockElement,
+  GuideAtomBlockElement,
+  ProfileAtomBlockElement,
+  QABlockElement,
+  TimelineAtomBlockElement,
+}
+
 // EnrichedContent is an optionally-present field of the PressedContent class.
 // It contains additional content that has been pre-fetched by facia-press, to
 // enable facia-server-side rendering of FAPI content, such as embeds.
@@ -7,8 +17,30 @@ final case class EnrichedContent(
     embedHtml: Option[String],
     embedCss: Option[String],
     embedJs: Option[String],
+    // Atom block elements pre-fetched by facia-press so that atoms placed on
+    // fronts (as snaps) can be rendered server-side. Each field mirrors the
+    // equivalent DCR block element for that atom type.
+    // Spike: for now each atom type is its own optional field.
+    GuideAtom: Option[GuideAtomBlockElement],
+    QandaAtom: Option[QABlockElement],
+    ProfileAtom: Option[ProfileAtomBlockElement],
+    TimelineAtom: Option[TimelineAtomBlockElement],
+    AudioAtom: Option[AudioAtomBlockElement],
+    ExplainerAtom: Option[ExplainerAtomBlockElement],
+    CtaAtom: Option[CallToActionAtomBlockElement],
 )
 
 object EnrichedContent {
-  val empty = EnrichedContent(None, None, None)
+  val empty = EnrichedContent(
+    embedHtml = None,
+    embedCss = None,
+    embedJs = None,
+    GuideAtom = None,
+    QandaAtom = None,
+    ProfileAtom = None,
+    TimelineAtom = None,
+    AudioAtom = None,
+    ExplainerAtom = None,
+    CtaAtom = None,
+  )
 }
