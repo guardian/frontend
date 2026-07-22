f561b8bd5891f0ee8604a52a4fcb8d5d32ca6847
Andrew Howe-Ely <114918544+andrewHEguardian@users.noreply.github.com>
2026-07-22 11:53:44 +0100

    Update capiVersion and contentApiModelsJson versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   val identityLibVersion = "4.31"
   val awsVersion = "2.44.12"
-  val capiVersion = "45.0.0-PREVIEW.aheproduct-summary-element.2026-06-25T1543.510abfb8"
+  val capiVersion = "47.0.0"
   val faciaVersion = "33.0.0-PREVIEW.aheproduct-summary-element.2026-06-26T0856.2185d64d"
   val dispatchVersion = "0.13.1"
   val romeVersion = "1.0"
@@ -30,7 +30,7 @@ object Dependencies {
   val cssParser = "net.sourceforge.cssparser" % "cssparser" % "0.9.30"
   val contentApiClient = "com.gu" %% "content-api-client" % capiVersion
   val contentApiModelsJson =
-    "com.gu" %% "content-api-models-json" % "44.0.0-PREVIEW.aheproduct-summary-element.2026-06-25T1458.232a9450"
+    "com.gu" %% "content-api-models-json" % "46.0.0"
   val faciaFapiScalaClient = "com.gu" %% "fapi-client-play30" % faciaVersion
   val identityCookie = "com.gu.identity" %% "identity-cookie" % identityLibVersion
 
