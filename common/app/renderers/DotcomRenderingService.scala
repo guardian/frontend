bb3e9c197ff750fe220b9de6718c32351b11caec
akash1810 <akash1810@users.noreply.github.com>
2026-07-27 10:30:33 +0100

    feat: Log DCR response length
    Updates https://github.com/guardian/frontend/pull/28984 to include the content length of the DCR response as a marker.

--- common/app/renderers/DotcomRenderingService.scala
+++ common/app/renderers/DotcomRenderingService.scala
@@ -104,6 +104,7 @@ class DotcomRenderingService extends GuLogging with ResultWithPreconnectPreload 
         "internal-request.request-uri" -> request.uri.toString,
         "internal-request.payload-content-length" -> payload.toString.length,
         "internal-request.canonical-url" -> canonicalUrl,
+        "internal-request.content-length" -> r.body.length,
       )
 
       logInfoWithCustomFields(s"Request to DCR completed with status ${r.status} in ${duration}ms", markers)
