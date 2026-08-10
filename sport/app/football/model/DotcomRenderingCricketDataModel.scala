f29d2293d5b74ff245e687785338a436fac1a455
Jake Lee Kennedy <1731150+Jakeii@users.noreply.github.com>
2026-08-10 11:49:13 +0100

    Remove old cricket match page

--- sport/app/football/model/DotcomRenderingCricketDataModel.scala
+++ sport/app/football/model/DotcomRenderingCricketDataModel.scala
@@ -1,71 +1,8 @@
 package football.model
 
-import common.{CanonicalLink, Edition}
-import conf.Configuration
-import cricket.controllers.CricketMatchPage
 import cricketModel.{Innings, InningsWicket, Match, Team}
-import model.ApplicationContext
 import model.dotcomrendering.DotcomRenderingUtils.withoutNull
-import model.dotcomrendering.{DotcomRenderingConfig, PageFooter, PageType}
-import navigation.{FooterLinks, Nav}
-import play.api.libs.json.{JsObject, JsValue, Json, Writes}
-import play.api.mvc.RequestHeader
-
-case class DotcomRenderingCricketDataModel(
-    cricketMatch: Match,
-    nav: Nav,
-    editionId: String,
-    guardianBaseURL: String,
-    config: JsObject,
-    pageFooter: PageFooter,
-    isAdFreeUser: Boolean,
-    contributionsServiceUrl: String,
-    canonicalUrl: String,
-    pageId: String,
-    paId: String,
-)
-
-object DotcomRenderingCricketDataModel {
-  def apply(
-      page: CricketMatchPage,
-  )(implicit
-      request: RequestHeader,
-      context: ApplicationContext,
-  ): DotcomRenderingCricketDataModel = {
-    val edition = Edition(request)
-    val nav = Nav(page, edition)
-
-    val pageType: PageType = PageType(page, request, context)
-
-    val combinedConfig = DotcomRenderingConfig(
-      page = page,
-      request = request,
-      isPreview = pageType.isPreview,
-    )
-
-    DotcomRenderingCricketDataModel(
-      page.theMatch,
-      nav = nav,
-      editionId = edition.id,
-      guardianBaseURL = Configuration.site.host,
-      config = combinedConfig,
-      pageFooter = PageFooter(FooterLinks.getFooterByEdition(edition)),
-      isAdFreeUser = views.support.Commercial.isAdFree(request),
-      contributionsServiceUrl = Configuration.contributionsService.url,
-      canonicalUrl = CanonicalLink(request, page.metadata.webUrl),
-      pageId = page.metadata.id,
-      paId = page.team.paId,
-    )
-  }
-
-  implicit val dotcomRenderingCricketDataModelFormat: Writes[DotcomRenderingCricketDataModel] =
-    Json.writes[DotcomRenderingCricketDataModel]
-
-  def toJson(model: DotcomRenderingCricketDataModel): JsValue = {
-    val jsValue = Json.toJson(model)
-    withoutNull(jsValue)
-  }
-}
+import play.api.libs.json.{JsObject, JsValue, Json}
 
 object CricketScoreBoardDataModel {
   private def getTeam(team: Team) = {
