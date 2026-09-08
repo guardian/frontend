91b8b4edc098e83b486590744a02bc07708914d7
William Mead <285652121+williammead@users.noreply.github.com>
2026-09-08 17:07:06 +0100

    upgrading identity from v6.0.0 to v7.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ package com.gu
 import sbt._
 
 object Dependencies {
-  val identityLibVersion = "6.0.0"
+  val identityLibVersion = "7.0.0"
   val awsVersion = "2.49.6"
   val capiVersion = "49.0.0"
   val faciaVersion = "39.0.0"
