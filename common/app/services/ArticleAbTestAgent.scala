9539ffaf1cc35e3f0b85758ee2e4fd7116c289aa
Anna Beddow <anna.beddow@gmail.com>
2026-09-16 14:10:05 +0100

    Add refresh method to ArticleAbTestAgent for fetching active A/B tests

--- common/app/services/ArticleAbTestAgent.scala
+++ common/app/services/ArticleAbTestAgent.scala
@@ -1,10 +1,13 @@
 package services
+import com.gu.contentapi.client.model.v1.VariantId.B
 import common.{Box, GuLogging}
 import contentapi.ContentApiClient
 
+import scala.concurrent.{ExecutionContext, Future}
+
 case class ArticleAbTest(a: String, b: String)
 
-class ArticleAbTestAgent extends GuLogging {
+class ArticleAbTestAgent(contentApiClient: ContentApiClient) extends GuLogging {
   private val testsBox = Box[List[ArticleAbTest]](Nil)
 
   def tests: List[ArticleAbTest] = testsBox.get()
@@ -17,6 +20,39 @@ class ArticleAbTestAgent extends GuLogging {
       ArticleAbTest(articleAPath, articleBPath) :: existing.filterNot(_.a == articleAPath)
     }
 
+  def setAll(newTests: List[ArticleAbTest]): Unit =
+    testsBox.alter(newTests)
+
   def remove(articleAPath: String): Unit =
     testsBox.alter(_.filterNot(_.a == articleAPath))
+
+  private def refresh()(implicit ec: ExecutionContext): Future[Unit] = {
+
+    val activeAbTestQuery = contentApiClient
+      .search()
+      .containsActiveAbTest()
+
+    val futureContentWithActiveAbTests = contentApiClient.getResponse(activeAbTestQuery)
+
+    for {
+      contentWithActiveAbTests <- futureContentWithActiveAbTests
+    } yield {
+      val content = contentWithActiveAbTests.results
+
+      val newTests = content.flatMap { c =>
+        val maybeActiveTest = c.abTests.getOrElse(Seq.empty).find(_.ended.isEmpty)
+        maybeActiveTest
+          .flatMap(test => test.variantLinks.collectFirst { case link if link.variantId == B => link.linkedShortPath })
+          .map(bPath => ArticleAbTest(c.id, bPath))
+      }.toList
+      setAll(newTests)
+    }
+   }
 }
+
+
+/*todo
+*  make call to capi to request article A/B test data
+*  update box with the fetched article A/B test data
+*  Add lifecycle management for the ArticleAbTestAgent
+* */
