688b47fd187fd0fef53c3b34e458e854d2618ea3
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-09-22 17:11:25 +0100

    TEST - show latestprice in URL - revert before merge!

--- common/app/views/support/HtmlCleaner.scala
+++ common/app/views/support/HtmlCleaner.scala
@@ -14,7 +14,7 @@ import navigation.ReaderRevenueSite
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element, TextNode}
 import play.api.mvc.RequestHeader
-import services.SkimLinksCache
+import services.{SkimLinksCache, AffiliateProductPriceCache}
 
 import scala.jdk.CollectionConverters._
 import scala.collection.mutable
@@ -927,11 +927,12 @@ object AffiliateLinksCleaner {
 
   def linkToSkimLink(link: String, pageUrl: String, skimlinksId: String, abTests: Map[String, String]): String = {
     val urlEncodedLink = URLEncode(ensureHttps(link))
+    val latestPrice = AffiliateProductPriceCache.getLatestPrice(ensureHttps(link))
     val xcustParam = if (abTests.nonEmpty) {
       val xcust = URLEncode("abTestParticipations" + abTests.map { case (k, v) => s"|$k:$v" }.mkString)
       s"&xcust=$xcust"
     } else ""
-    s"https://go.skimresources.com/?id=$skimlinksId&url=$urlEncodedLink&sref=$host$pageUrl$xcustParam"
+    s"https://go.skimresources.com/?id=$skimlinksId&url=$urlEncodedLink&sref=$host$pageUrl$xcustParam&latestPrice=$latestPrice"
   }
 
   def contentHasAlwaysOffTag(tagPaths: List[String], alwaysOffTags: Set[String]): Boolean = {
