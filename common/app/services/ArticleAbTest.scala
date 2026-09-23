9196ca0b38fd26e66ee10404d535cae9284a5337
Anna Beddow <anna.beddow@gmail.com>
2026-09-23 08:55:45 +0100

    Add basic ab test agent that can add, update, remove and check against what tests are in the test box.

---
+++ common/app/services/ArticleAbTest.scala
@@ -0,0 +1,25 @@
+package services
+import common.{Box, GuLogging}
+
+case class ArticleAbTest(a: String, b: String)
+
+class ArticleAbTestAgent() extends GuLogging {
+  private val testsBox = Box[List[ArticleAbTest]](Nil)
+
+  def tests: List[ArticleAbTest] = testsBox.get()
+
+  def variantFor(articleAPath: String): Option[String] =
+    testsBox.get().find(_.a == articleAPath).map(_.b)
+
+  def upsert(articleAPath: String, articleBPath: String): Unit =
+    testsBox.alter { existing =>
+      ArticleAbTest(articleAPath, articleBPath) :: existing.filterNot(_.a == articleAPath)
+    }
+
+  def setAll(newTests: List[ArticleAbTest]): Unit =
+    testsBox.alter(newTests)
+
+  def remove(articleAPath: String): Unit =
+    testsBox.alter(_.filterNot(_.a == articleAPath))
+
+}
